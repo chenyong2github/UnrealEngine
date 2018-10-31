806c0c0f12bf5985c057f97b436243050b8f0fa0
Jason Bestimt <jason.bestimt@epicgames.com>
2018-10-31 13:40:23 -0400

    Fixing full path for include
    #RB:none
    
    [CL 4528348 by Jason Bestimt in Dev-VR branch]

--- Engine/Source/Runtime/WindowsMixedReality/WindowsMixedRealityRuntimeSettings/Private/WindowsMixedRealityRuntimeSettings.cpp
+++ Engine/Source/Runtime/WindowsMixedReality/WindowsMixedRealityRuntimeSettings/Private/WindowsMixedRealityRuntimeSettings.cpp
@@ -3,7 +3,7 @@
 #include "WindowsMixedRealityRuntimeSettings.h"
 #include "Misc/ConfigCacheIni.h"
 #include "CoreGlobals.h"
-#include "Package.h"
+#include "UObject/Package.h"
 
 UWindowsMixedRealityRuntimeSettings* UWindowsMixedRealityRuntimeSettings::WMRSettingsSingleton = nullptr;
 
