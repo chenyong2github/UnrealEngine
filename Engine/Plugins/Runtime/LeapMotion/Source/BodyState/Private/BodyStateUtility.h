426c70d569ceaab64d30e56f9e202501e18c6548
getnamo <getnamo@gmail.com>
2019-01-07 11:01:32 -0500

    PR #4831: Leap motion v3 (Contributed by getnamo)
    #jira UE-61130
    #rb me
    
    [CL 4684906 by Keli Hlodversson in Dev-VR branch]

---
+++ Engine/Plugins/Runtime/LeapMotion/Source/BodyState/Private/BodyStateUtility.h
@@ -0,0 +1,18 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(BodyStateLog, Log, All);
+
+class BODYSTATE_API FBodyStateUtility
+{
+public:
+	//Math Utility
+	static FRotator CombineRotators(FRotator A, FRotator B);
+	static float AngleBetweenVectors(FVector A, FVector B);
+
+	template<typename T>
+	static FString EnumToString(const FString& enumName, const T value);
+};
---
+++ Engine/Plugins/Runtime/LeapMotion/Source/LeapMotionEditor/Public/LeapMotionEditorModule.h
@@ -0,0 +1,10 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Runtime/Core/Public/Modules/ModuleInterface.h"
+
+class FLeapMotionEditorModule : public IModuleInterface
+{
+public:
+};
