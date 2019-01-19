ed914235e93bab5ed2fcd75d6c19785a2adf7589
Joe Conley <Joe.Conley@epicgames.com>
2019-01-18 21:45:36 -0500

    Updating Copyright in files that were not in Main when it was updated.
    #rb trivial
    
    [CL 4758167 by Joe Conley in Dev-VR branch]

--- Engine/Plugins/Runtime/AR/Google/GoogleARCore/Source/GoogleARCoreBase/Private/GoogleARCorePermissionHandler.cpp
+++ Engine/Plugins/Runtime/AR/Google/GoogleARCore/Source/GoogleARCoreBase/Private/GoogleARCorePermissionHandler.cpp
@@ -1,4 +1,4 @@
-// Copyright 2017 Google Inc.
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "GoogleARCorePermissionHandler.h"
 #include "AndroidPermissionFunctionLibrary.h"
