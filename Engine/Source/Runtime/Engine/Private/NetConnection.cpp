bbb8057a5e268a1c7bb644ce1c08fec5950a625c
Ben Marsh <Ben.Marsh@epicgames.com>
2019-01-26 14:33:56 -0500

    Merging //UE4/Dev-Main to Dev-Build (//UE4/Dev-Build)
    #rb none
    #rnx
    
    [CL 4820573 by Ben Marsh in Dev-Build branch]

--- Engine/Source/Runtime/Engine/Private/NetConnection.cpp
+++ Engine/Source/Runtime/Engine/Private/NetConnection.cpp
@@ -66,11 +66,11 @@ UNetConnection* UNetConnection::GNetConnectionBeingCleanedUp = NULL;
 
 UNetConnection::UNetConnection(const FObjectInitializer& ObjectInitializer)
 :	UPlayer(ObjectInitializer)
-,	Driver				( NULL )
+,	Driver				( nullptr )
 ,	PackageMapClass		( UPackageMapClient::StaticClass() )
-,	PackageMap			( NULL )
-,	ViewTarget			( NULL )
-,   OwningActor			( NULL )
+,	PackageMap			( nullptr )
+,	ViewTarget			( nullptr )
+,   OwningActor			( nullptr )
 ,	MaxPacket			( 0 )
 ,	InternalAck			( false )
 ,	MaxPacketHandlerBits ( 0 )
@@ -86,7 +86,7 @@ UNetConnection::UNetConnection(const FObjectInitializer& ObjectInitializer)
 
 ,	AllowMerge			( false )
 ,	TimeSensitive		( false )
-,	LastOutBunch		( NULL )
+,	LastOutBunch		( nullptr )
 ,	SendBunchHeader		( MAX_BUNCH_HEADER_BITS )
 
 ,	StatPeriod			( 1.f  )
@@ -298,7 +298,7 @@ void UNetConnection::InitHandler()
 
 			Handler->InitializeDelegates(FPacketHandlerLowLevelSendTraits::CreateUObject(this, &UNetConnection::LowLevelSend));
 			Handler->NotifyAnalyticsProvider(Driver->AnalyticsProvider, Driver->AnalyticsAggregator);
-			Handler->Initialize(Mode, MaxPacket * 8, false);
+			Handler->Initialize(Mode, MaxPacket * 8, false, nullptr, nullptr, Driver->NetDriverName);
 
 
 			// Add handling for the stateless connect handshake, for connectionless packets, as the outermost layer
@@ -2533,7 +2533,13 @@ void UNetConnection::Tick()
 			Driver->Time,
 			Timeout,
 			*Describe());
-		UE_LOG(LogNet, Warning, TEXT("%s"), *Error);
+		
+		static double LastTimePrinted = 0.0f;
+		if (FPlatformTime::Seconds() - LastTimePrinted > GEngine->NetErrorLogInterval)
+		{
+			UE_LOG(LogNet, Warning, TEXT("%s"), *Error);
+			LastTimePrinted = FPlatformTime::Seconds();
+		}
 
 		if (!bPendingDestroy)
 		{
