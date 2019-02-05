0bf7d6ac1cf3099a423cdc5314b6a1686e0572c6
Ben Marsh <Ben.Marsh@epicgames.com>
2019-02-05 15:05:51 -0500

    UGS: Fix the "Show More" link not being visible at the bottom of the list of changes.
    #rb none
    #jira UE-69528
    
    [CL 4902837 by Ben Marsh in Dev-Build branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Controls/WorkspaceControl.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Controls/WorkspaceControl.cs
@@ -1543,7 +1543,7 @@ namespace UnrealGameSync
 				Layout.MainText = String.Format("{0}. Fetching {1} more from server...  ", ShowingChanges, PerforceMonitor.PendingMaxChanges - CurrentMaxChanges);
 				Layout.LinkText = "Cancel";
 			}
-			else if(PerforceMonitor.CurrentMaxChanges >= NumChanges)
+			else if(PerforceMonitor.CurrentMaxChanges > NumChanges)
 			{
 				Layout.MainText = ShowingChanges;
 				Layout.LinkText = "";
