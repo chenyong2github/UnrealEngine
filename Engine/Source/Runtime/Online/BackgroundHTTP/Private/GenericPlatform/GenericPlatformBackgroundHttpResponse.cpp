afe99602e01fb383de2b3f2a1825a64ae95d0f60
Joe Conley <Joe.Conley@epicgames.com>
2019-02-14 16:50:03 -0500

    Back out revision 2 from //UE4/Dev-VR/Engine/Source/Runtime/Online/BackgroundHTTP/Private/GenericPlatform/GenericPlatformBackgroundHttpResponse.cpp
    Reverting change, this was not the correct fix for the non-unity build
    
    #rb rollback
    
    [CL 4999519 by Joe Conley in Dev-VR branch]

--- Engine/Source/Runtime/Online/BackgroundHTTP/Private/GenericPlatform/GenericPlatformBackgroundHttpResponse.cpp
+++ Engine/Source/Runtime/Online/BackgroundHTTP/Private/GenericPlatform/GenericPlatformBackgroundHttpResponse.cpp
@@ -4,7 +4,6 @@
 
 #include "Interfaces/IHttpResponse.h"
 #include "Misc/FileHelper.h"
-#include "HTML5/HTML5PlatformBackgroundHttp.h"
 
 FGenericPlatformBackgroundHttpResponse::FGenericPlatformBackgroundHttpResponse(int32 InResponseCode, const FString& InTempContentFilePath)
 {
