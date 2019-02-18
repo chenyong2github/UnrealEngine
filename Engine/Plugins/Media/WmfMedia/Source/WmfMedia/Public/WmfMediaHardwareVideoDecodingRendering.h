11813b449d54b787d84b1c70cb9cee34b509145d
stuart hill <Stuart.Hill@epicgames.com>
2019-02-18 14:52:47 -0500

    Restore backed out CL 4982351 Implemented WMF hardware accelerated video decoding support.
    #rnx
    #rb none
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: stuart.hill
    #ROBOMERGE-SOURCE: CL 4992832 via CL 4992861 via CL 4992918 via CL 4996526
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5046383 by stuart hill in Dev-Anim branch]

--- Engine/Plugins/Media/WmfMedia/Source/WmfMedia/Public/WmfMediaHardwareVideoDecodingRendering.h
+++ Engine/Plugins/Media/WmfMedia/Source/WmfMedia/Public/WmfMediaHardwareVideoDecodingRendering.h
@@ -9,5 +9,5 @@ class FWmfMediaHardwareVideoDecodingTextureSample;
 class FWmfMediaHardwareVideoDecodingParameters
 {
 public:
-	static bool ConvertTextureFormat_RenderThread(FWmfMediaHardwareVideoDecodingTextureSample* InSample, FTexture2DRHIRef InDstTexture);
+	WMFMEDIA_API static bool ConvertTextureFormat_RenderThread(FWmfMediaHardwareVideoDecodingTextureSample* InSample, FTexture2DRHIRef InDstTexture);
 };
