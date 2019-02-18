3e77a1c7d21e429078fab56e2e9a906b7767b4f7
Stuart Hill <Stuart.Hill@epicgames.com>
2019-02-18 15:06:46 -0500

    Some fixes for build issues after integrate. Some fixes for #includes
    #jira UE-70201
    #rnx
    #rb none
    
    [CL 5046927 by Stuart Hill in Main branch]

--- Engine/Plugins/Media/WmfMedia/Source/WmfMedia/Private/Wmf/WmfMediaHardwareVideoDecodingTextureSample.cpp
+++ Engine/Plugins/Media/WmfMedia/Source/WmfMedia/Private/Wmf/WmfMediaHardwareVideoDecodingTextureSample.cpp
@@ -1,10 +1,10 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
+#include "WmfMediaHardwareVideoDecodingTextureSample.h"
+
 #include "WmfMediaPrivate.h"
 
 #if WMFMEDIA_SUPPORTED_PLATFORM
-
-#include "WmfMediaHardwareVideoDecodingTextureSample.h"
 
 ID3D11Texture2D* FWmfMediaHardwareVideoDecodingTextureSample::InitializeSourceTexture(const TRefCountPtr<ID3D11Device>& InD3D11Device, FTimespan InTime, FTimespan InDuration, const FIntPoint& InDim, uint8 InFormat, EMediaTextureSampleFormat InMediaTextureSampleFormat)
 {
