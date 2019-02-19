c8d36328f5d63d8d276afc81ede18c472fb03fb5
dan oconnor <Dan.OConnor@epicgames.com>
2019-02-19 15:02:16 -0500

    References to the old CDO that are referenced in other blueprints are now updated by the blueprint compilation manager #jira None #rb Phillip.Kavan
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: dan.oconnor
    #ROBOMERGE-SOURCE: CL 4923177 in //UE4/Release-4.22/... via CL 4923178
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5067444 by dan oconnor in Dev-Networking branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintCompilationManager.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintCompilationManager.cpp
@@ -1644,6 +1644,15 @@ void FBlueprintCompilationManagerImpl::ReinstanceBatch(TArray<FReinstancingJob>&
 		UClass* OldClass = CurrentReinstancer->DuplicatedClass;
 		if(OldClass)
 		{
+			UClass* NewClass = CurrentReinstancer->ClassToReinstance;
+            if (NewClass && 
+				OldClass->ClassDefaultObject && 
+				NewClass->ClassDefaultObject &&
+				OldClass->ClassDefaultObject != NewClass->ClassDefaultObject)
+            {
+                OldArchetypeToNewArchetype.Add(OldClass->ClassDefaultObject, NewClass->ClassDefaultObject);
+            }
+
 			TArray<UObject*> ArchetypeObjects;
 			GetObjectsOfClass(OldClass, ArchetypeObjects, false);
 			
