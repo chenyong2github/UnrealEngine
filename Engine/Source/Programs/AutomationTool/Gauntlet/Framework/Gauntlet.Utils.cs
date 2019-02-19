1522c6bb8f5aac08c95c057684155b6de34df4ca
josh engebretson <Josh.Engebretson@epicgames.com>
2019-02-19 02:55:51 -0500

    Wrap Gauntlet CopyDirectory FileInfo creation in an exception handler for better error logging
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: josh.engebretson
    #ROBOMERGE-SOURCE: CL 4998738 via CL 4998739 via CL 4999913 via CL 5001681
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core)
    
    [CL 5060467 by josh engebretson in Dev-Core branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Framework/Gauntlet.Utils.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Framework/Gauntlet.Utils.cs
@@ -881,8 +881,20 @@ namespace Gauntlet
 						DestPath = Transform(DestPath);
 					}
 
-					FileInfo DestInfo = new FileInfo(DestPath);
-					FileInfo SrcInfo = new FileInfo(Path.Combine(SourceDir.FullName, RelativePath));
+					string SourcePath = Path.Combine(SourceDir.FullName, RelativePath);
+					FileInfo DestInfo;
+					FileInfo SrcInfo;
+
+					// wrap FileInfo creation with exception handler as can throw and want informative error
+					try
+					{
+						DestInfo = new FileInfo(DestPath);
+						SrcInfo = new FileInfo(SourcePath);
+					}
+					catch (Exception Ex)
+					{
+						throw new Exception(string.Format("FileInfo creation failed for Source:{0}, Dest:{1}, with: {2}", SourcePath, DestPath, Ex.Message));
+					}
 
 					// ensure directory exists
 					DestInfo.Directory.Create();
