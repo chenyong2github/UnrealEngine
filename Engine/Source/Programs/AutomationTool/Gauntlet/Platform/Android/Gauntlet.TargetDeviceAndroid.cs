f6c9e8246eab9abd19b4dfcb0d6c4edb5d9b5e3f
josh engebretson <Josh.Engebretson@epicgames.com>
2019-02-26 01:03:30 -0500

    Make Android log suspending optional to reduce spam
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: josh.engebretson
    #ROBOMERGE-SOURCE: CL 5183570 via CL 5184002 via CL 5185639 via CL 5185720
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 5194940 by josh engebretson in Dev-Build branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Platform/Android/Gauntlet.TargetDeviceAndroid.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Platform/Android/Gauntlet.TargetDeviceAndroid.cs
@@ -485,7 +485,7 @@ namespace Gauntlet
 					if (!IsExistingDevice)
 					{
 						// disconnect
-						RunAdbGlobalCommand(string.Format("disconnect {0}", DeviceName));
+						RunAdbGlobalCommand(string.Format("disconnect {0}", DeviceName), true, false, true);
 
 						Log.Info("Disconnected {0}", DeviceName);
 					}					
@@ -1006,14 +1006,14 @@ namespace Gauntlet
 		/// <param name="Wait"></param>
 		/// <param name="Input"></param>
 		/// <returns></returns>
-		public IProcessResult RunAdbDeviceCommand(string Args, bool Wait=true, bool bShouldLogCommand = false)
+		public IProcessResult RunAdbDeviceCommand(string Args, bool Wait=true, bool bShouldLogCommand = false, bool bPauseErrorParsing = false)
 		{
 			if (string.IsNullOrEmpty(DeviceName) == false)
 			{
 				Args = string.Format("-s {0} {1}", DeviceName, Args);
 			}
 
-			return RunAdbGlobalCommand(Args, Wait, bShouldLogCommand);
+			return RunAdbGlobalCommand(Args, Wait, bShouldLogCommand, bPauseErrorParsing);
 		}
 
 		/// <summary>
@@ -1047,7 +1047,7 @@ namespace Gauntlet
 		/// <param name="Args"></param>
 		/// <param name="Wait"></param>
 		/// <returns></returns>
-		public static IProcessResult RunAdbGlobalCommand(string Args, bool Wait = true, bool bShouldLogCommand = false)
+		public static IProcessResult RunAdbGlobalCommand(string Args, bool Wait = true, bool bShouldLogCommand = false, bool bPauseErrorParsing = false)
 		{
 			CommandUtils.ERunOptions RunOptions = CommandUtils.ERunOptions.AppMustExist | CommandUtils.ERunOptions.NoWaitForExit;
 
@@ -1067,7 +1067,7 @@ namespace Gauntlet
 
 			IProcessResult Process;
 
-			using (var PauseEC = new ScopedSuspendECErrorParsing())
+			using (bPauseErrorParsing ? new ScopedSuspendECErrorParsing() : null)
 			{
 				Process = AndroidPlatform.RunAdbCommand(null, null, Args, null, RunOptions);
 
@@ -1083,7 +1083,7 @@ namespace Gauntlet
 		public void AllowDeviceSleepState(bool bAllowSleep)
 		{
 			string CommandLine = "shell svc power stayon " + (bAllowSleep ? "false" : "usb");
-			RunAdbDeviceCommand(CommandLine);
+			RunAdbDeviceCommand(CommandLine, true, false, true);
 		}
 
 		public void KillRunningProcess(string AndroidPackageName)
