ec233a8a36928717f2a570506f22a1de2ad88370
david hibbitts <David.Hibbitts@epicgames.com>
2019-02-26 16:38:16 -0500

    SceneCaptureComponent now responds to OnVisibilityChanged events and sets the ProxyMesh Visibility to match the component's visibility
    #jira UE-70407
    #rb none
    
    #ROBOMERGE-SOURCE: CL 5146216 in //UE4/Release-4.22/...
    #ROBOMERGE-BOT: RELEASE (Release-4.22 -> Main)
    
    [CL 5207480 by david hibbitts in Main branch]

--- Engine/Source/Runtime/Engine/Private/Components/SceneCaptureComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SceneCaptureComponent.cpp
@@ -141,7 +141,8 @@ void USceneCaptureComponent::OnRegister()
 			ProxyMeshComponent->SetIsVisualizationComponent(true);
 			ProxyMeshComponent->SetStaticMesh(CaptureMesh);
 			ProxyMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
-			ProxyMeshComponent->bHiddenInGame = true;
+			ProxyMeshComponent->bVisible = bVisible; // Match the visibility of the component
+			ProxyMeshComponent->bHiddenInGame = true; // Hidden in game should always be true as this is a visualization component
 			ProxyMeshComponent->CastShadow = false;
 			ProxyMeshComponent->CreationMethod = CreationMethod;
 			ProxyMeshComponent->RegisterComponentWithWorld(GetWorld());
@@ -399,6 +400,15 @@ void USceneCaptureComponent::UpdateDeferredCaptures(FSceneInterface* Scene)
 
 	// All scene captures for this world have been updated
 	SceneCapturesToUpdateMap.Remove(World);
+}
+
+void USceneCaptureComponent::OnVisibilityChanged()
+{
+	if (ProxyMeshComponent != nullptr)
+	{
+		ProxyMeshComponent->SetVisibility(bVisible);
+	}
+	Super::OnVisibilityChanged();
 }
 
 void USceneCaptureComponent::OnUnregister()
