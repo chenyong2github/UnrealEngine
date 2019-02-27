5524e97b29aec71199ca0ec34f47dba0095bac64
michael dupuis <Michael.Dupuis@xa.epicgames.com>
2019-02-26 21:00:54 -0500

    Currently the wait for the Parallel task was done during the destruction of the FViewInfo, which mean that if the task were using some stuff from the View, they could have been destroyed, resulting in using gargage. We now wait in the FViewInfo for the async task before doing any destruction work. #rb Krzysztof.Narkowicz #jira UE-69790 [FYI] Jeremy.Moore
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: michael.dupuis
    #ROBOMERGE-SOURCE: CL 5113813 in //UE4/Release-4.22/... via CL 5206631
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5220130 by michael dupuis in Dev-Networking branch]

--- Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
+++ Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
@@ -1530,7 +1530,7 @@ void FProjectedShadowInfo::ClearTransientArrays()
 	DynamicSubjectTranslucentMeshElements.Empty();
 
 	ShadowDepthPassVisibleCommands.Empty();
-	ShadowDepthPass.Empty();
+	ShadowDepthPass.WaitForTasksAndEmpty();
 
 	SubjectMeshCommandBuildRequests.Empty();
 
