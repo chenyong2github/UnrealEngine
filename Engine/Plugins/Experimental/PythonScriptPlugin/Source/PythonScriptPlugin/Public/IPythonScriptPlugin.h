0b131462f91e4146c2a2d3ea99492c4d8821b16d
jamie dale <Jamie.Dale@epicgames.com>
2019-02-28 18:12:55 -0500

    Added extra control over executing Python commands
    You're now able to get back the execution result and log output from running a Python command, as well as control the execution mode of the command, and whether it should be run in "unattended" mode.
    
    This is handled by the new IPythonScriptPlugin::ExecPythonCommandEx function which takes a FPythonCommandEx struct containing the input flags, mode, and command, as well as fields to fill in with the result and log output.
    
    The mode can be set to one of the following:
      - ExecuteFile: Execute the Python command as a file. This allows you to execute either a literal Python script containing multiple statements, or a file with optional arguments.
      - ExecuteStatement: Execute the Python command as a single statement. This will execute a single statement and print the result. This mode cannot run files.
      - EvaluateStatement: Evaluate the Python command as a single statement. This will evaluate a single statement and return the result. This mode cannot run files.
    
    ExecuteFile is the default to preserve existing behavior, however, since ExecuteStatement allows for a REPL like environment, there is now an additional command executor available ("Python (REPL)") which lets you build and test Python scripts in a similar manner to an interactive command line Python environment.
    
    [FYI] Aaron.Carlisle
    #rb Chris.Gagnon
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: jamie.dale
    #ROBOMERGE-SOURCE: CL 5229258 via CL 5236098 via CL 5236204
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5254791 by jamie dale in Dev-VR branch]

--- Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Public/IPythonScriptPlugin.h
+++ Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Public/IPythonScriptPlugin.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreTypes.h"
-#include "Modules/ModuleInterface.h"
+#include "Misc/EnumClassFlags.h"
 #include "Modules/ModuleManager.h"
+#include "Modules/ModuleInterface.h"
+
+struct FPythonCommandEx;
 
 class IPythonScriptPlugin : public IModuleInterface
 {
@@ -27,6 +30,12 @@ public:
 	 * @return true if the command ran successfully, false if there were errors (the output log will show the errors).
 	 */
 	virtual bool ExecPythonCommand(const TCHAR* InPythonCommand) = 0;
+
+	/**
+	 * Execute the given Python command.
+	 * @return true if the command ran successfully, false if there were errors.
+	 */
+	virtual bool ExecPythonCommandEx(FPythonCommandEx& InOutPythonCommand) = 0;
 	
 	/**
 	 * Delegate called after Python has been initialized.
@@ -38,3 +47,122 @@ public:
 	 */
 	virtual FSimpleMulticastDelegate& OnPythonShutdown() = 0;
 };
+
+/** Types of log output that Python can give. */
+enum class EPythonLogOutputType : uint8
+{
+	/** This log was informative. */
+	Info,
+	/** This log was a warning. */
+	Warning,
+	/** This log was an error. */
+	Error,
+};
+
+/** Flags that can be specified when running Python commands. */
+enum class EPythonCommandFlags : uint8
+{
+	/** No special behavior. */
+	None = 0,
+	/** Run the Python command in "unattended" mode (GIsRunningUnattendedScript set to true), which will suppress certain pieces of UI. */
+	Unattended = 1<<0,
+};
+ENUM_CLASS_FLAGS(EPythonCommandFlags);
+
+/** Controls the execution mode used for the Python command. */
+enum class EPythonCommandExecutionMode : uint8
+{
+	/** Execute the Python command as a file. This allows you to execute either a literal Python script containing multiple statements, or a file with optional arguments. */
+	ExecuteFile,
+	/** Execute the Python command as a single statement. This will execute a single statement and print the result. This mode cannot run files. */
+	ExecuteStatement,
+	/** Evaluate the Python command as a single statement. This will evaluate a single statement and return the result. This mode cannot run files. */
+	EvaluateStatement,
+};
+
+/** Log output entry captured from Python. */
+struct FPythonLogOutputEntry
+{
+	/** The type of the log output. */
+	EPythonLogOutputType Type = EPythonLogOutputType::Info;
+
+	/** The log output string. */
+	FString Output;
+};
+
+/** Extended information when executing Python commands. */
+struct FPythonCommandEx
+{
+	/** Flags controlling how the command should be run. */
+	EPythonCommandFlags Flags = EPythonCommandFlags::None;
+
+	/** Controls the mode used to execute the command. */
+	EPythonCommandExecutionMode ExecutionMode = EPythonCommandExecutionMode::ExecuteFile;
+
+	/** The command to run. This may be literal Python code, or a file (with optional arguments) to run. */
+	FString Command;
+
+	/** The result of running the command. On success, for EvaluateStatement mode this will be the actual result of running the command, and will be None in all other cases. On failure, this will be the error information (typically a Python exception trace). */
+	FString CommandResult;
+
+	/** The log output captured while running the command. */
+	TArray<FPythonLogOutputEntry> LogOutput;
+};
+
+inline const TCHAR* LexToString(EPythonLogOutputType InType)
+{
+	switch (InType)
+	{
+	case EPythonLogOutputType::Info:
+		return TEXT("Info");
+	case EPythonLogOutputType::Warning:
+		return TEXT("Warning");
+	case EPythonLogOutputType::Error:
+		return TEXT("Error");
+	default:
+		break;
+	}
+	return TEXT("<Unknown EPythonLogOutputType>");
+}
+
+inline const TCHAR* LexToString(EPythonCommandExecutionMode InMode)
+{
+	switch (InMode)
+	{
+	case EPythonCommandExecutionMode::ExecuteFile:
+		return TEXT("ExecuteFile");
+	case EPythonCommandExecutionMode::ExecuteStatement:
+		return TEXT("ExecuteStatement");
+	case EPythonCommandExecutionMode::EvaluateStatement:
+		return TEXT("EvaluateStatement");
+	default:
+		break;
+	}
+	return TEXT("<Unknown EPythonCommandExecutionMode>");
+}
+
+inline bool LexTryParseString(EPythonCommandExecutionMode& OutMode, const TCHAR* InBuffer)
+{
+	if (FCString::Stricmp(InBuffer, TEXT("ExecuteFile")) == 0)
+	{
+		OutMode = EPythonCommandExecutionMode::ExecuteFile;
+		return true;
+	}
+	if (FCString::Stricmp(InBuffer, TEXT("ExecuteStatement")) == 0)
+	{
+		OutMode = EPythonCommandExecutionMode::ExecuteStatement;
+		return true;
+	}
+	if (FCString::Stricmp(InBuffer, TEXT("EvaluateStatement")) == 0)
+	{
+		OutMode = EPythonCommandExecutionMode::EvaluateStatement;
+		return true;
+	}
+	return false;
+}
+
+inline void LexFromString(EPythonCommandExecutionMode& OutMode, const TCHAR* InBuffer)
+{
+	OutMode = EPythonCommandExecutionMode::ExecuteFile;
+	LexTryParseString(OutMode, InBuffer);
+}
