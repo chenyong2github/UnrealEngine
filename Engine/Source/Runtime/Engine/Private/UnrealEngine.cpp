bf78b06ac7be0004814d0ed0da55a168e81a0b4f
ben zeigler <Ben.Zeigler@epicgames.com>
2019-03-06 18:16:23 -0500

    #jira UE-70487 Fix crash when calling GetPackageGuid on a nonexistent package, check was accidentally added during a refactor #rb marc.audy [CODEREVIEW] robert.manuszewski
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: ben.zeigler
    #ROBOMERGE-SOURCE: CL 5297923 in //UE4/Release-4.22/... via CL 5309021
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5325477 by ben zeigler in Dev-Anim branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -8703,7 +8703,6 @@ FGuid UEngine::GetPackageGuid(FName PackageName, bool bForPIE)
 	UPackage* PackageToReset = nullptr;
 	FLinkerLoad* Linker = LoadPackageLinker(nullptr, *PackageName.ToString(), LoadFlags, nullptr, nullptr, nullptr, [&PackageToReset, &Result](FLinkerLoad* InLinker)
 	{
-		check(InLinker);
 		if (InLinker != nullptr && InLinker->LinkerRoot != nullptr)
 		{
 			Result = InLinker->LinkerRoot->GetGuid();
@@ -8711,8 +8710,10 @@ FGuid UEngine::GetPackageGuid(FName PackageName, bool bForPIE)
 		}
 	});
 
-	ResetLoaders(PackageToReset);
-	Linker = nullptr;
+	if (PackageToReset)
+	{
+		ResetLoaders(PackageToReset);
+	}
 
 	return Result;
 }
