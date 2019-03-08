7f5c2a223fc3546b41b1fa8b9c4857c4236a8e7d
Helge Mathee <Helge.Mathee@epicgames.com>
2019-03-08 15:12:32 -0500

    #rb lina.halper
    Build break fix for header change
    
    [CL 5352011 by Helge Mathee in Dev-Anim branch]

--- Engine/Source/Runtime/AnimGraphRuntime/Public/KismetAnimationTypes.h
+++ Engine/Source/Runtime/AnimGraphRuntime/Public/KismetAnimationTypes.h
@@ -8,7 +8,6 @@
 
 /**
  *	An easing type defining how to ease float values.
- */
  *	The FPositionHistory is a container to record position changes
  *	over time. This is used to calculate velocity of a bone, for example.
  *	The FPositionArray also tracks the last index used to allow for 
