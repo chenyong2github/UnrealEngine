2b6b79599202e91479ccbb2092e24d179dc6adcb
martin wilson <Martin.Wilson@epicgames.com>
2019-03-14 19:06:20 -0400

    Fix uniform sampled curve decompression
    #jira UE-68388
    #rb Nicholas.Frechette
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: martin.wilson
    #ROBOMERGE-SOURCE: CL 5354033 in //UE4/Release-4.22/... via CL 5367782
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5405513 by martin wilson in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/Animation/AnimCompressionDerivedData.h
+++ Engine/Source/Runtime/Engine/Private/Animation/AnimCompressionDerivedData.h
@@ -50,7 +50,7 @@ public:
 		// This is a version string that mimics the old versioning scheme. If you
 		// want to bump this version, generate a new guid using VS->Tools->Create GUID and
 		// return it here. Ex.
-		return TEXT("691013A9702847FE9A47302771888030");
+		return TEXT("AA0E2605005B4B2EBAACD66B5578C97E");
 	}
 
 	virtual FString GetPluginSpecificCacheKeySuffix() const override;
