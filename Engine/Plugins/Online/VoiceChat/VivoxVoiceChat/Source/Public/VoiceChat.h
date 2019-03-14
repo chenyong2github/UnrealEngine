0e994005779634471991dd8c583fbba3723f82f7
michael kirzinger <Michael.Kirzinger@epicgames.com>
2019-03-14 17:37:24 -0400

    Restore changes lost due to force integrate that wiped out the vivox plugin
    #rb none
    #jira UE-71248
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: michael.kirzinger
    #ROBOMERGE-SOURCE: CL 5349130 in //UE4/Main/...
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5402857 by michael kirzinger in Dev-Networking branch]

--- Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Public/VoiceChat.h
+++ Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Public/VoiceChat.h
@@ -60,6 +60,22 @@ enum class EVoiceChatTransmitMode
 	Channel
 };
 
+struct FVoiceChatCallStats
+{
+	/** Call length in seconds */
+	double CallLength;
+	/** Minimum measured network latency in seconds. Zero if no measurements made */
+	double LatencyMinMeasuredSeconds;
+	/** Maximum measured network latency in seconds. Zero if no measurements made */
+	double LatencyMaxMeasuredSeconds;
+	/** Average measured network latency in seconds. Zero if no measurements made */
+	double LatencyAverageMeasuredSeconds;
+	/** Total number of packets, both received and lost */
+	int PacketsNumTotal;
+	/** Total number of lost packets */
+	int PacketsNumLost;
+};
+
 DECLARE_DELEGATE_OneParam(FOnVoiceChatConnectCompleteDelegate, const FVoiceChatResult& /* Result */);
 DECLARE_DELEGATE_OneParam(FOnVoiceChatDisconnectCompleteDelegate, const FVoiceChatResult& /* Result */);
 DECLARE_DELEGATE_TwoParams(FOnVoiceChatLoginCompleteDelegate, const FString& /* PlayerName */, const FVoiceChatResult& /* Result */);
@@ -68,8 +84,9 @@ DECLARE_DELEGATE_TwoParams(FOnVoiceChatChannelJoinCompleteDelegate, const FStrin
 DECLARE_DELEGATE_TwoParams(FOnVoiceChatChannelLeaveCompleteDelegate, const FString& /* ChannelName */, const FVoiceChatResult& /* Result */);
 
 DECLARE_MULTICAST_DELEGATE(FOnVoiceChatAvailableAudioDevicesChangedDelegate);
-DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatDisconnectedDelegate, const FVoiceChatResult& /* Reason */);
 DECLARE_MULTICAST_DELEGATE(FOnVoiceChatReconnectedDelegate);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatDisconnectedDelegate, const FVoiceChatResult& /* Reason */);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatCallStatsUpdatedDelegate, const FVoiceChatCallStats& /* CallStats */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatChannelExitedDelegate, const FString& /* ChannelName */, const FVoiceChatResult& /* Reason */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatPlayerAddedDelegate, const FString& /* ChannelName */, const FString& /* PlayerName */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatPlayerRemovedDelegate, const FString& /* ChannelName */, const FString& /* PlayerName */);
@@ -338,6 +355,11 @@ public:
 	 * Delegate triggered when we unexpectedly leave a voice channel
 	 */
 	virtual FOnVoiceChatChannelExitedDelegate& OnVoiceChatChannelExited() = 0;
+
+	/**
+	 * Delegate triggered when a call is ended, providing the stats for the call.
+	 */
+	virtual FOnVoiceChatCallStatsUpdatedDelegate& OnVoiceChatCallStatsUpdated() = 0;
 
 	/**
 	 * Set the 3d position of the player
