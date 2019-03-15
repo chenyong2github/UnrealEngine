c0ffefcd13db35f84896a65d13b9730c4ae56bda
steve robb <Steve.Robb@epicgames.com>
2019-03-15 13:01:45 -0400

    Fix to stop UClass CDOs trying to have their token streams reassembled on hot reload.
    #rb robert.manuszewski
    #jira UE-71566
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: steve.robb
    #ROBOMERGE-SOURCE: CL 5415461 in //UE4/Release-4.22/... via CL 5415463
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5418411 by steve robb in Dev-VR branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
@@ -4381,7 +4381,7 @@ void UClass::AssembleReferenceTokenStreams()
 				Class->GetDefaultObject(); // Force the default object to be constructed if it isn't already
 			}
 			// Assemble reference token stream for garbage collection/ RTGC.
-			if (!Class->HasAnyClassFlags(CLASS_TokenStreamAssembled))
+			if (!Class->HasAnyFlags(RF_ClassDefaultObject) && !Class->HasAnyClassFlags(CLASS_TokenStreamAssembled))
 			{
 				Class->AssembleReferenceTokenStream();
 			}
