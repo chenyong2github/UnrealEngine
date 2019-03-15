300bdbdf71bb1fbef51ee8ffba499b79a41057c0
krzysztof narkowicz <Krzysztof.Narkowicz@epicgames.com>
2019-03-15 05:25:43 -0400

    Recreate uniform expression uniform buffer when material instance resource is initialized. This forced uniform expression UB to be recreated and fixes the check that we are trying to update uniform expression UB which doesn't match current layout. #rb none #jira UE-71222
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: krzysztof.narkowicz
    #ROBOMERGE-SOURCE: CL 5397062 in //UE4/Release-4.22/... via CL 5397063
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5409910 by krzysztof narkowicz in Dev-Anim branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
@@ -484,7 +484,7 @@ void UMaterialInstance::InitResources()
 
 	PropagateDataToMaterialProxy();
 
-	CacheMaterialInstanceUniformExpressions(this);
+	CacheMaterialInstanceUniformExpressions(this, true);
 }
 
 const UMaterial* UMaterialInstance::GetMaterial() const
