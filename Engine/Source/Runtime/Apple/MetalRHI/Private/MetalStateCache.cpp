b451143330a4b372f954731635415ad739c5fa09
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2019-03-20 03:20:12 -0400

    Fixed: Game texture becomes distorted after joining any game mode in iOS [CODEREVIEW] mark.satterthwaite
    #ROBOMERGE-SOURCE: CL 5466273 via CL 5466274 via CL 5466275 via CL 5466475
    
    [CL 5466481 by dmitriy dyomin in Main branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalStateCache.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalStateCache.cpp
@@ -534,7 +534,15 @@ bool FMetalStateCache::SetRenderTargetsInfo(FRHISetRenderTargetsInfo const& InRe
 				ERenderTargetStoreAction HighLevelStoreAction = RenderTargetView.StoreAction;
 				ERenderTargetLoadAction HighLevelLoadAction = RenderTargetView.LoadAction;
 				
-				if (Surface.MSAATexture)
+				// on iOS with memory-less MSAA textures we can't load them
+                // in case high level code wants to load and render to MSAA target, set attachment to a resolved texture
+				const bool bUseResolvedTexture = (
+					PLATFORM_IOS && 
+					Surface.MSAATexture && 
+					Surface.MSAATexture.GetStorageMode() == mtlpp::StorageMode::Memoryless && 
+					HighLevelLoadAction == ERenderTargetLoadAction::ELoad);
+				
+				if (Surface.MSAATexture && !bUseResolvedTexture)
 				{
 					bool bMemoryless = false;
 #if PLATFORM_IOS
