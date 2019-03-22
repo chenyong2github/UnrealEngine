4aa6cc806b5bfcefada6a254277f7fed3977908d
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2019-03-22 18:34:49 -0400

    Fixed case where skeletal mesh with "Render Static" option would crash editor in mobile preview #jira UE-71891 #rb none #lockdown cristina.riveron
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: dmitriy.dyomin
    #ROBOMERGE-SOURCE: CL 5500215 in //UE4/Release-4.22/... via CL 5500216
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5517078 by dmitriy dyomin in Dev-VR branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
@@ -3747,8 +3747,7 @@ void FSkeletalMeshSceneProxy::DrawStaticElements(FStaticPrimitiveDrawInterface* 
 					BatchElement.MaxVertexIndex = LODData.GetNumVertices() - 1;
 					BatchElement.NumPrimitives = Section.NumTriangles;
 					BatchElement.IndexBuffer = LODData.MultiSizeIndexContainer.GetIndexBuffer();
-					BatchElement.PrimitiveUniformBuffer = GetUniformBuffer();
-								
+													
 					PDI->DrawMesh(MeshElement, ScreenSize);
 				}
 			}
