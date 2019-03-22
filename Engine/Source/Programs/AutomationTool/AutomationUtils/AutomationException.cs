4deddf9efde39d45fa212dd61771cb3fb93bd7cc
ben marsh <Ben.Marsh@epicgames.com>
2019-03-22 17:45:16 -0400

    Remove superfluous errors when a recursive UAT command fails, or a child process fails. They should already output their own error messages, and adding additional errors is just noise.
    #jira
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 5496878 via CL 5496883 via CL 5499236 via CL 5499309
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5513418 by ben marsh in Dev-Networking branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/AutomationException.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/AutomationException.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,6 +63,27 @@ namespace AutomationTool
 		Error_AndroidOBBError = 155,
 	};
 
+	/// <summary>
+	/// How the exception should be output
+	/// </summary>
+	public enum AutomationExceptionOutputFormat
+	{
+		/// <summary>
+		/// Do not output any message; just output the exception details to the log.
+		/// </summary>
+		Silent,
+
+		/// <summary>
+		/// Output a brief summary to the console, and the exception details to the log
+		/// </summary>
+		Minimal,
+
+		/// <summary>
+		/// Treat the exception as an error
+		/// </summary>
+		Error
+	}
+
     /// <summary>
     /// Exception class used by the AutomationTool to throw exceptions. Allows setting an exit code that will be passed to the entry routine to return to the system on program exit.
     /// If no exit code is given, Error_Unkonwn is used.
@@ -70,6 +91,7 @@ namespace AutomationTool
     public class AutomationException : System.Exception
 	{
         public ExitCode ErrorCode = ExitCode.Error_Unknown;
+		public AutomationExceptionOutputFormat OutputFormat = AutomationExceptionOutputFormat.Error;
 
 		public AutomationException(string Msg)
 			:base(Msg)
