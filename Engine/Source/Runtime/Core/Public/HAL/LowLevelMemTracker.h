0ec43cc58bf00f2993f9eb7d792dfbd92bb59c2f
david harvey <David.Harvey@epicgames.com>
2019-03-27 13:03:46 -0400

    [LLM] adding new SceneRender tag, moving up to 50 MB from EngineMisc to new SceneRender tag.
    #rb anthony.bills
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 5587772 via CL 5587773 via CL 5587774 via CL 5587775 via CL 5588458 via CL 5588493
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 5596073 by david harvey in Dev-Build branch]

--- Engine/Source/Runtime/Core/Public/HAL/LowLevelMemTracker.h
+++ Engine/Source/Runtime/Core/Public/HAL/LowLevelMemTracker.h
@@ -189,6 +189,7 @@ enum class ELLMTagSet : uint8
 	macro(Textures,								"Textures",						GET_STATFNAME(STAT_TexturesLLM),							GET_STATFNAME(STAT_TexturesSummaryLLM),			-1)\
 	macro(TextureMetaData,						"TextureMetaData",				GET_STATFNAME(STAT_TextureMetaDataLLM),						GET_STATFNAME(STAT_TexturesSummaryLLM),			-1)\
 	macro(RenderTargets,						"RenderTargets",				GET_STATFNAME(STAT_RenderTargetsLLM),						GET_STATFNAME(STAT_EngineSummaryLLM),			-1)\
+	macro(SceneRender,							"SceneRender",					GET_STATFNAME(STAT_SceneRenderLLM),							GET_STATFNAME(STAT_EngineSummaryLLM),			-1)\
 	macro(RHIMisc,								"RHIMisc",						GET_STATFNAME(STAT_RHIMiscLLM),								GET_STATFNAME(STAT_EngineSummaryLLM),			-1)\
 	macro(AsyncLoading,							"AsyncLoading",					GET_STATFNAME(STAT_AsyncLoadingLLM),						GET_STATFNAME(STAT_EngineSummaryLLM),			-1)\
 	macro(UObject,								"UObject",						GET_STATFNAME(STAT_UObjectLLM),								GET_STATFNAME(STAT_UObjectSummaryLLM),			-1)\
