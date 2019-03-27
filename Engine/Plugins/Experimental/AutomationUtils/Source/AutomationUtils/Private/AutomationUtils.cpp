3d2abcb907c9a0dcadad3d10281379454e446844
william ewen <William.Ewen@epicgames.com>
2019-03-27 01:04:26 -0400

    Adding AutomationUtils experimental plugin, which will house various optional gameplay automation utilities and supporting tools This initial commit adds:  - A BlueprintFunctionLibrary that houses a command to take an AutomationScreenshot at gameplay, without having to have FTest running  - A Commandlet that will do AutomationScreenshot comparisons off-line: this will facilitate taking screenshots on whatever platform client, then doing the image diffs in batch after those are complete.
    #rnx
    #rb Andrew.Grant
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: william.ewen
    #ROBOMERGE-SOURCE: CL 5555024 via CL 5556747 via CL 5556868
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5580279 by william ewen in Dev-VR branch]

---
+++ Engine/Plugins/Experimental/AutomationUtils/Source/AutomationUtils/Private/AutomationUtils.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Modules/ModuleManager.h"
+
+//Module Implementation Call
+IMPLEMENT_MODULE(FDefaultModuleImpl, AutomationUtils)
+
