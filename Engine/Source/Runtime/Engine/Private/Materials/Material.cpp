6cebd5767981884e98b9775b23fcfb16e7faaa9a
krzysztof narkowicz <Krzysztof.Narkowicz@epicgames.com>
2019-03-27 01:05:26 -0400

    Fixed random memory stomp during DDC cooking (most likely introduced in CL 3767205) by waiting for CacheShaders render commands before destroying FMaterialResources.
    Race condition flow:
    * Main thread calls FMaterial::CacheShaders (UDerivedDataCacheCommandlet::Main->BeginCacheForCookedPlatformData->FMaterial::CacheShaders)
    * FMaterial::CacheShaders enqueues UpdateRenderThreadShaderMap render thread command
    * Main thread then deletes FMaterial by calling ClearAllCachedCookedPlatformData from UDerivedDataCacheCommandlet::Main
    * Depending on the garbage collector and render thread timings, UpdateRenderThreadShaderMap may be executed after the delete and it will stomp random memory
    
    This can be reproduced 100% by:
    * Insert FWindowsPlatformProcess::Sleep(1.0f) into UpdateRenderThreadShaderMap in FMaterial::CacheShaders
    * Force GC to be run every frame with gc.StressTestGC=1 and gc.ForceCollectGarbageEveryFrame=1
    * Run d:\ue4_release_4_22\Samples\Showcases\VirtualCamera\VirtualCamera.uproject -run=DerivedDataCache -TargetPlatform=Windows+WindowsNoEditor -fill -DDC=CreateInstalledProjectPak -StompMalloc -stdout -CrashForUAT -unattended -NoLogTimes
    
    #rb Chris.Bunner
    #jira UE-71809
    #lockdown Cristina.Riveron
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: krzysztof.narkowicz
    #ROBOMERGE-SOURCE: CL 5557232 in //UE4/Release-4.22/... via CL 5557239
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5580436 by krzysztof narkowicz in Dev-VR branch]

--- Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
@@ -4128,6 +4128,9 @@ bool UMaterial::IsCachedCookedPlatformDataLoaded( const ITargetPlatform* TargetP
 
 void UMaterial::ClearCachedCookedPlatformData( const ITargetPlatform *TargetPlatform )
 {
+	// Make sure that all CacheShaders render thead commands are finished before we destroy FMaterialResources.
+	FlushRenderingCommands();
+
 	TArray<FMaterialResource*> *CachedMaterialResourcesForPlatform = CachedMaterialResourcesForCooking.Find( TargetPlatform );
 	if ( CachedMaterialResourcesForPlatform != NULL )
 	{
@@ -4141,6 +4144,9 @@ void UMaterial::ClearCachedCookedPlatformData( const ITargetPlatform *TargetPlat
 
 void UMaterial::ClearAllCachedCookedPlatformData()
 {
+	// Make sure that all CacheShaders render thead commands are finished before we destroy FMaterialResources.
+	FlushRenderingCommands();
+
 	for ( auto It : CachedMaterialResourcesForCooking )
 	{
 		TArray<FMaterialResource*> &CachedMaterialResourcesForPlatform = It.Value;
