a2f19c217cedc2a93f3f1551adb72e98edf9aae5
lauren ridge <Lauren.Ridge@epicgames.com>
2019-03-28 17:06:08 -0400

    Adding editor-only camera for scene captures #rb Jason.Stasik #rnx
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: lauren.ridge
    #ROBOMERGE-SOURCE: CL 5630789 via CL 5633146 via CL 5633151
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 5636665 by lauren ridge in Dev-Build branch]

---
+++ Engine/Source/Editor/Blutility/Classes/EditorUtilityCamera.h
@@ -0,0 +1,19 @@
+/**
+ * Base class of any editor-only camera actors. Should be used when creating editor-only capture tools, etc.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "UObject/ScriptMacros.h"
+#include "EditorUtilityCamera.generated.h"
+
+
+UCLASS(Abstract, Blueprintable)
+class BLUTILITY_API AEditorUtilityCamera : public ACameraActor
+{
+	GENERATED_UCLASS_BODY()
+
+
+};
