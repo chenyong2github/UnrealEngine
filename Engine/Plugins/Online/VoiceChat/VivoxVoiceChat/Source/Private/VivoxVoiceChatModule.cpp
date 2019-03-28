c78301152b80fcdc653b6077018cf6b9f09d2fc5
michael kirzinger <Michael.Kirzinger@epicgames.com>
2019-03-28 13:41:34 -0400

    vivoxclientapi: allocator overrides, remove stl containers from interface, fix off by one error that resulted in the maximum account name length being one less than it should have been VivoxVoiceChat plugin: set vivoxclientapi allocator, update for interface changes
    [at]chris.varnsverry, [at]sam.zamani, [at]billy.mccarroll, [at]peter.sauerbrei
    
    
    #ROBOMERGE-SOURCE: CL 5628922 via CL 5628923 via CL 5628924 via CL 5628925 via CL 5631850
    
    [CL 5632056 by michael kirzinger in Main branch]

--- Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Private/VivoxVoiceChatModule.cpp
+++ Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Private/VivoxVoiceChatModule.cpp
@@ -4,12 +4,29 @@
 
 #include "Modules/ModuleManager.h"
 
+#include "vivoxclientapi/util.h"
+
 IMPLEMENT_MODULE(FVivoxVoiceChatModule, VivoxVoiceChat);
 
 extern TUniquePtr<FVivoxVoiceChat> CreateVivoxObject();
 
+static void* VivoxClientApiAllocate(size_t bytes)
+{
+	LLM_SCOPE(ELLMTag::AudioVoiceChat);
+	return FMemory::Malloc(bytes);
+}
+
+static void VivoxClientApiDeallocate(void* ptr)
+{
+	LLM_SCOPE(ELLMTag::AudioVoiceChat);
+	FMemory::Free(ptr);
+}
+
 void FVivoxVoiceChatModule::StartupModule()
 {
+	// VivoxClientApi allocator hooks need to be setup here as they are used in CreateVivoxObject
+	VivoxClientApi::SetMemFunctions(&VivoxClientApiAllocate, &VivoxClientApiDeallocate);
+
 	VivoxObj = CreateVivoxObject();
 	if (VivoxObj.IsValid())
 	{
