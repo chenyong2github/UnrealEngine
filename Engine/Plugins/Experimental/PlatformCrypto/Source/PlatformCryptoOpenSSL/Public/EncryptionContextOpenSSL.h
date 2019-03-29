c4f91bc04f8073497ed6a3ef2703274a26efa1a2
graeme thornton <Graeme.Thornton@epicgames.com>
2019-03-29 17:25:32 -0400

    Add RSA support to the PlatformCrypto plugin  - NOTE: Only implemented for platforms that support OpenSSL. Other platforms have empty implementations.
    #rb ryan.gerleve
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: graeme.thornton
    #ROBOMERGE-SOURCE: CL 5644647 via CL 5644649 via CL 5644650 via CL 5644651 via CL 5644711 via CL 5648418
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5656964 by graeme thornton in Dev-Networking branch]

--- Engine/Plugins/Experimental/PlatformCrypto/Source/PlatformCryptoOpenSSL/Public/EncryptionContextOpenSSL.h
+++ Engine/Plugins/Experimental/PlatformCrypto/Source/PlatformCryptoOpenSSL/Public/EncryptionContextOpenSSL.h
@@ -19,6 +19,15 @@ public:
 	TArray<uint8> Decrypt_AES_256_ECB(const TArrayView<const uint8> Ciphertext, const TArrayView<const uint8> Key, EPlatformCryptoResult& OutResult);
 
 	bool DigestVerify_PS256(const TArrayView<const char> Message, const TArrayView<const uint8> Signature, const TArrayView<const uint8> PKCS1Key);
+
+	TPlatformCryptoRSAKey CreateKey_RSA(const TArrayView<const uint8> PublicExponent, const TArrayView<const uint8> PrivateExponent, const TArrayView<const uint8> Modulus);
+	void DestroyKey_RSA(TPlatformCryptoRSAKey Key);
+	int32 GetKeySize_RSA(TPlatformCryptoRSAKey Key);
+	int32 GetMaxDataSize_RSA(TPlatformCryptoRSAKey Key);
+	int32 EncryptPublic_RSA(TArrayView<const uint8> Source, TArray<uint8>& Dest, TPlatformCryptoRSAKey Key);
+	int32 EncryptPrivate_RSA(TArrayView<const uint8> Source, TArray<uint8>& Dest, TPlatformCryptoRSAKey Key);
+	int32 DecryptPublic_RSA(TArrayView<const uint8> Source, TArray<uint8>& Dest, TPlatformCryptoRSAKey Key);
+	int32 DecryptPrivate_RSA(TArrayView<const uint8> Source, TArray<uint8>& Dest, TPlatformCryptoRSAKey Key);
 };
 
 typedef FEncryptionContextOpenSSL FEncryptionContext;
