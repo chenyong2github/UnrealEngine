af606b3aec6ee19e7a70ef1f003e8235c34e1ad1
lauren ridge <Lauren.Ridge@epicgames.com>
2019-03-29 01:32:01 -0400

    Adding editor-only camera for scene captures #rb Jason.Stasik #rnx
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: lauren.ridge
    #ROBOMERGE-SOURCE: CL 5630789 via CL 5633146 via CL 5633151
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core)
    
    [CL 5644029 by lauren ridge in Dev-Core branch]

---
+++ Engine/Source/Editor/Blutility/Private/EditorUtilityCamera.cpp
@@ -0,0 +1,11 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "EditorUtilityCamera.h"
+
+
+/////////////////////////////////////////////////////
+
+AEditorUtilityCamera::AEditorUtilityCamera(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
