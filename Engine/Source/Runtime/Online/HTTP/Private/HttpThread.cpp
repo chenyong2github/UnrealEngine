9f23b5053086557e8fb9281152af94a89edba524
sam zamani <sam.zamani@epicgames.com>
2019-04-01 21:05:34 -0400

    #http Temp fix for code optimization issue with http thread inner loop sleep
    [at]rob.cannaday
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: sam.zamani
    #ROBOMERGE-SOURCE: CL 5672233 via CL 5672237 via CL 5672675 via CL 5672736 via CL 5675838 via CL 5675926
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 5678760 by sam zamani in Dev-Build branch]

--- Engine/Source/Runtime/Online/HTTP/Private/HttpThread.cpp
+++ Engine/Source/Runtime/Online/HTTP/Private/HttpThread.cpp
@@ -67,6 +67,8 @@ bool FHttpThread::Init()
 	return true;
 }
 
+// @todo samz - needed due to InnerLoopEnd,InnerLoopBegin 
+PRAGMA_DISABLE_OPTIMIZATION
 uint32 FHttpThread::Run()
 {
 	// Arrays declared outside of loop to re-use memory
@@ -107,6 +109,8 @@ uint32 FHttpThread::Run()
 	}
 	return 0;
 }
+PRAGMA_ENABLE_OPTIMIZATION
+
 
 void FHttpThread::Tick()
 {
