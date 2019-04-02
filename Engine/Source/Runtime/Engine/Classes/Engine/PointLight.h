80d9505e1ef39f888b09e2c3668a7ce98d850253
Marc Audy <Marc.Audy@epicgames.com>
2019-04-02 11:46:36 -0400

    Fix PointLight, RectLight, and SpotLight so that the light component can have properties modified when subclassed in blueprints #rb #jira UE-67184
    [CL 5687191 by Marc Audy in Dev-Framework branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/PointLight.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/PointLight.h
@@ -12,7 +12,7 @@ class APointLight : public ALight
 {
 	GENERATED_UCLASS_BODY()
 
-	UPROPERTY(BlueprintReadOnly, Category="Light", meta=(ExposeFunctionCategories="PointLight,Rendering|Lighting"))
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Light", meta=(ExposeFunctionCategories="PointLight,Rendering|Lighting"))
 	class UPointLightComponent* PointLightComponent;
 
 	// BEGIN DEPRECATED (use component functions now in level script)
