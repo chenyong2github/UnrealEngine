cdf2c18d9d14207473a84f7728d5ab446f653e53
derek ehrman <derek.ehrman@epicgames.com>
2019-04-02 21:09:21 -0400

    Changing the rich text API back to have a Create and renaming Get to GetStaticInstance to fix backwards compat and make it a bit clearer what each is doing.
    #jira UE-72162
    
    #rb trivial
    
    [FYI] Nick.Darnell
    
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: derek.ehrman
    #ROBOMERGE-SOURCE: CL 5690634 via CL 5690651 via CL 5690652 via CL 5690898 via CL 5695189 via CL 5695295
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 5700742 by derek ehrman in Dev-Build branch]

--- Engine/Source/Runtime/Slate/Public/Framework/Text/RichTextMarkupProcessing.h
+++ Engine/Source/Runtime/Slate/Public/Framework/Text/RichTextMarkupProcessing.h
@@ -14,7 +14,8 @@
 class SLATE_API FDefaultRichTextMarkupParser : public IRichTextMarkupParser
 {
 public:
-	static TSharedRef< FDefaultRichTextMarkupParser > Get();
+	static TSharedRef< FDefaultRichTextMarkupParser > Create();
+	static TSharedRef< FDefaultRichTextMarkupParser > GetStaticInstance();
 
 public:
 	virtual void Process(TArray<FTextLineParseResults>& Results, const FString& Input, FString& Output) override;
