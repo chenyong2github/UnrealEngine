737c9a4ebbfff76455a4c29c121248a6473e6910
derek ehrman <derek.ehrman@epicgames.com>
2019-04-02 21:09:24 -0400

    Changing the rich text API back to have a Create and renaming Get to GetStaticInstance to fix backwards compat and make it a bit clearer what each is doing.
    #jira UE-72162
    
    #rb trivial
    
    [FYI] Nick.Darnell
    
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: derek.ehrman
    #ROBOMERGE-SOURCE: CL 5690634 via CL 5690651 via CL 5690652 via CL 5690898 via CL 5695189 via CL 5695295
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5700759 by derek ehrman in Dev-Anim branch]

--- Engine/Plugins/Tests/RuntimeTests/Source/RuntimeTests/Private/Slate/RichTextMarkupProcessingTest.cpp
+++ Engine/Plugins/Tests/RuntimeTests/Source/RuntimeTests/Private/Slate/RichTextMarkupProcessingTest.cpp
@@ -100,7 +100,7 @@ namespace
 bool FRTFProcessingTest::RunTest (const FString& Parameters)
 {
 #if UE_ENABLE_ICU
-	TSharedRef< FDefaultRichTextMarkupParser > RichTextMarkupProcessing = FDefaultRichTextMarkupParser::Get();
+	TSharedRef< FDefaultRichTextMarkupParser > RichTextMarkupProcessing = FDefaultRichTextMarkupParser::GetStaticInstance();
 
 	// Text
 	{
