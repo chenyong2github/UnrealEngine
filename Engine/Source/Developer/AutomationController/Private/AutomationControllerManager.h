580c69ce86a8f96f47a689f63ae0a0288fc099c7
william ewen <William.Ewen@epicgames.com>
2019-04-02 21:13:52 -0400

    FunctionalTest: Adding ClientDescriptor field to the index.json output reports (in FAutomatedTestPassResults) #rnx
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: william.ewen
    #ROBOMERGE-SOURCE: CL 5692559 via CL 5695909 via CL 5695947
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5701911 by william ewen in Dev-Anim branch]

--- Engine/Source/Developer/AutomationController/Private/AutomationControllerManager.h
+++ Engine/Source/Developer/AutomationController/Private/AutomationControllerManager.h
@@ -15,6 +15,8 @@
 #include "Async/Future.h"
 #include "ImageComparer.h"
 #include "Interfaces/IScreenShotManager.h"
+#include "Misc/EngineVersion.h"
+#include "HAL/PlatformProperties.h"
 #include "AutomationControllerManager.generated.h"
 
 USTRUCT()
@@ -81,7 +83,8 @@ struct FAutomatedTestPassResults
 
 public:
 	FAutomatedTestPassResults()
-		: ReportCreatedOn(0)
+		: ClientDescriptor()
+		, ReportCreatedOn(0)
 		, Succeeded(0)
 		, SucceededWithWarnings(0)
 		, Failed(0)
@@ -89,7 +92,24 @@ public:
 		, TotalDuration(0)
 		, ComparisonExported(false)
 	{
+		if (FEngineVersion::Current().HasChangelist())
+		{
+			ClientDescriptor = FEngineVersion::Current().GetBranch()
+				+ TEXT(" - ")
+				+ FString::FromInt(FEngineVersion::Current().GetChangelist())
+				+ TEXT(" - ");
+		}
+
+		if (FPlatformProperties::RequiresCookedData())
+		{
+			ClientDescriptor += TEXT("Cooked ");
+		}
+
+		ClientDescriptor += FPlatformProperties::IniPlatformName();
 	}
+
+	UPROPERTY()
+	FString ClientDescriptor;
 
 	UPROPERTY()
 	FDateTime ReportCreatedOn;
