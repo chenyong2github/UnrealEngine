028d296f7a3af2d5073cc7d929243c7455688df3
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2019-04-08 13:22:41 -0400

    Add Support for drag/drop of Collection on the Foliage Palette (sharing of FT Presets) (Support was added to anything that uses AssetUtil::ExtractAssetDataFromDrag)
    [FYI] robert.osborne
    #rb matt.kuhlenschmidt, jamie.dale
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 5762914 via CL 5763787 via CL 5763822
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5780319 by patrick enfedaque in Dev-Networking branch]

---
+++ Engine/Source/Editor/UnrealEd/Private/DragAndDrop/CollectionDragDropOp.cpp
@@ -0,0 +1,33 @@
+#include "DragAndDrop/CollectionDragDropOp.h"
+#include "ICollectionManager.h"
+#include "CollectionManagerModule.h"
+#include "AssetRegistryModule.h"
+#include "IAssetRegistry.h"
+
+TArray<FAssetData>  FCollectionDragDropOp::GetAssets() const
+{
+	ICollectionManager& CollectionManager = FModuleManager::LoadModuleChecked<FCollectionManagerModule>("CollectionManager").Get();
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+	IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+
+	TArray<FName> AssetPaths;
+	for (const FCollectionNameType& CollectionNameType : Collections)
+	{
+		TArray<FName> CollectionAssetPaths;
+		CollectionManager.GetAssetsInCollection(CollectionNameType.Name, CollectionNameType.Type, CollectionAssetPaths);
+		AssetPaths.Append(CollectionAssetPaths);
+	}
+
+	TArray<FAssetData> AssetDatas;
+	AssetDatas.Reserve(AssetPaths.Num());
+	for (const FName& AssetPath : AssetPaths)
+	{
+		FAssetData AssetData = AssetRegistry.GetAssetByObjectPath(AssetPath);
+		if (AssetData.IsValid())
+		{
+			AssetDatas.AddUnique(AssetData);
+		}
+	}
+	
+	return AssetDatas;
+}
