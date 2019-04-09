14cb75e585baf4fe7c1f943419f33e9639cf4c4b
david harvey <David.Harvey@epicgames.com>
2019-04-09 10:28:10 -0400

    Upgrade to VC 15.9.11 toolchain - includes PGO optimization bug fix.
    #rb ben.woodhouse
    [FYI] ben.marsh, chris bunner, nick darnell
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 5806911 via CL 5806913 via CL 5806914 via CL 5806966 via CL 5806970
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5808335 by david harvey in Dev-Networking branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
@@ -452,7 +452,7 @@ namespace UnrealBuildTool
 		/// <summary>
 		/// The default compiler version to be used, if installed. 
 		/// </summary>
-		static readonly VersionNumber DefaultVisualStudioToolChainVersion = VersionNumber.Parse("14.16.27023");
+		static readonly VersionNumber DefaultVisualStudioToolChainVersion = VersionNumber.Parse("14.16.27023.1");
 
 		/// <summary>
 		/// The default Windows SDK version to be used, if installed.
