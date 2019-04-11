c627be06078c01b9d52336e03217325cbe605b7f
bob tellez <Bob.Tellez@epicgames.com>
2019-04-11 10:33:20 -0400

    Back out changelist 5824471. Causes windows server to freeze at startup
    [FYI] Chris.Bunner
    
    
    #ROBOMERGE-SOURCE: CL 5826025 via CL 5826026 via CL 5826027 via CL 5830113
    
    [CL 5830242 by bob tellez in Main branch]

--- Engine/Source/Runtime/Core/Public/Windows/WindowsPlatformMisc.h
+++ Engine/Source/Runtime/Core/Public/Windows/WindowsPlatformMisc.h
@@ -241,9 +241,6 @@ struct CORE_API FWindowsPlatformMisc
 	static EConvertibleLaptopMode GetConvertibleLaptopMode();
 
 	static IPlatformChunkInstall* GetPlatformChunkInstall();
-
-	static void PumpMessagesOutsideMainLoop();
-
 };
 
 
