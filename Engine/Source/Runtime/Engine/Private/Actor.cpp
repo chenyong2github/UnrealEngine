a84933472bca1b9d5769856fde28b6a93e294ce5
bill colby <Bill.Colby@epicgames.com>
2019-04-12 14:31:59 -0400

    Fix for null pointer crash in Actor::Tick
    #rb [at]Tim.Tillotson
    
    [at]Tim.Tillotson [at]Josh.May [at]Shovaen.Patel
    
    #rnx
    
    
    
    
    #ROBOMERGE-SOURCE: CL 5839983 via CL 5839988 via CL 5862991
    
    [CL 5863315 by bill colby in Main branch]

--- Engine/Source/Runtime/Engine/Private/Actor.cpp
+++ Engine/Source/Runtime/Engine/Private/Actor.cpp
@@ -988,7 +988,10 @@ void AActor::Tick( float DeltaSeconds )
 		// DeltaSeconds to make up the frames that they missed (because they wouldn't have missed any).
 		// So pass in the world's DeltaSeconds value rather than our specific DeltaSeconds value.
 		UWorld* MyWorld = GetWorld();
-		MyWorld->GetLatentActionManager().ProcessLatentActions(this, MyWorld->GetDeltaSeconds());
+		if (MyWorld)
+		{
+			MyWorld->GetLatentActionManager().ProcessLatentActions(this, MyWorld->GetDeltaSeconds());
+		}
 	}
 
 	if (bAutoDestroyWhenFinished)
