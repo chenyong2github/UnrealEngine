83f6eb66b900f368b8c212c16c17ba6284553fd8
mark satterthwaite <Mark.Satterthwaite@epicgames.com>
2019-04-12 15:37:35 -0400

    For non-Shipping Mac builds enable my internal GPU/Metal debugging tools by default to provide more information about problems - this means that bugs that aren't 100% reproducible should still let QA report more useful information. Also fixes the location of where we enabled the GPU debugging for macOS so that it is actually useful.
    #rb none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: mark.satterthwaite
    #ROBOMERGE-SOURCE: CL 5822643 via CL 5822651 via CL 5822796 via CL 5827842 via CL 5827926
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 5868291 by mark satterthwaite in Dev-Networking branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalCommandQueue.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalCommandQueue.cpp
@@ -251,9 +251,6 @@ FMetalCommandQueue::FMetalCommandQueue(mtlpp::Device InDevice, uint32 const MaxN
 			if (MaxShaderVersion >= 4)
 			{
 				Features |= EMetalFeaturesTextureBuffers;
-            
-                IConsoleVariable* GPUCrashDebuggingCVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.GPUCrashDebugging"));
-                GMetalCommandBufferDebuggingEnabled = (GPUCrashDebuggingCVar && GPUCrashDebuggingCVar->GetInt() != 0) || FParse::Param(FCommandLine::Get(),TEXT("metalgpudebug"));
             }
             if (MaxShaderVersion >= 5)
             {
@@ -277,6 +274,14 @@ FMetalCommandQueue::FMetalCommandQueue(mtlpp::Device InDevice, uint32 const MaxN
 					Features |= EMetalFeaturesHeaps;
 				}
 			}
+            
+#if UE_BUILD_SHIPPING
+			IConsoleVariable* GPUCrashDebuggingCVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.GPUCrashDebugging"));
+			GMetalCommandBufferDebuggingEnabled = (GPUCrashDebuggingCVar && GPUCrashDebuggingCVar->GetInt() != 0) || FParse::Param(FCommandLine::Get(),TEXT("metalgpudebug"));
+#else
+			// Force the command-buffer debugging on for the moment to see if we can't work out what the heck is going on on macOS
+			GMetalCommandBufferDebuggingEnabled = true;
+#endif
 		}
     }
     else if ([Device.GetName().GetPtr() rangeOfString:@"Nvidia" options:NSCaseInsensitiveSearch].location != NSNotFound)
