f7333fb1db1da537e13a3302a7a00991f24c854f
richard malo <Richard.Malo@epicgames.com>
2019-04-12 10:10:01 -0400

    Fixed another case of invalid override Material in ULandscapeComponent [FYI] patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 5831531 via CL 5835463
    
    [CL 5836268 by richard malo in Main branch]

--- Engine/Source/Runtime/Landscape/Private/Landscape.cpp
+++ Engine/Source/Runtime/Landscape/Private/Landscape.cpp
@@ -3066,10 +3066,13 @@ void ULandscapeComponent::SerializeStateHashes(FArchive& Ar)
 
 	for (FLandscapeComponentMaterialOverride& MaterialOverride : OverrideMaterials)
 	{
-		UMaterialInterface::TMicRecursionGuard RecursionGuard;
-		FGuid LocalStateId = MaterialOverride.Material->GetMaterial_Concurrent(RecursionGuard)->StateId;
-		Ar << LocalStateId;
-		Ar << MaterialOverride.LODIndex;
+		if (MaterialOverride.Material != nullptr)
+		{
+			UMaterialInterface::TMicRecursionGuard RecursionGuard;
+			FGuid LocalStateId = MaterialOverride.Material->GetMaterial_Concurrent(RecursionGuard)->StateId;
+			Ar << LocalStateId;
+			Ar << MaterialOverride.LODIndex;
+		}
 	}	
 
 	ALandscapeProxy* Proxy = GetLandscapeProxy();
