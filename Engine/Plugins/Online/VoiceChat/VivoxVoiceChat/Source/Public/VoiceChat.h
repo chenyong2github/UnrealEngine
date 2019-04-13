58f7aeba7926f3ded6e0a932088be5ba74ea2991
michael kirzinger <Michael.Kirzinger@epicgames.com>
2019-04-12 21:34:29 -0400

    VivoxVoiceChatPlugin: Add delegates for when connection, login, and channel state changes
    [CODEREVIEW] [at]chris.varnsverry
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: michael.kirzinger
    #ROBOMERGE-SOURCE: CL 5836487 via CL 5837926 via CL 5838908 via CL 5838940
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core)
    
    [CL 5881057 by michael kirzinger in Dev-Core branch]

--- Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Public/VoiceChat.h
+++ Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Public/VoiceChat.h
@@ -85,8 +85,12 @@ DECLARE_DELEGATE_TwoParams(FOnVoiceChatChannelLeaveCompleteDelegate, const FStri
 
 DECLARE_MULTICAST_DELEGATE(FOnVoiceChatAvailableAudioDevicesChangedDelegate);
 DECLARE_MULTICAST_DELEGATE(FOnVoiceChatReconnectedDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnVoiceChatConnectedDelegate);
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatDisconnectedDelegate, const FVoiceChatResult& /* Reason */);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatLoggedInDelegate, const FString& /* PlayerName */);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatLoggedOutDelegate, const FString& /* PlayerName */);
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatCallStatsUpdatedDelegate, const FVoiceChatCallStats& /* CallStats */);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnVoiceChatChannelJoinedDelegate, const FString& /* ChannelName */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatChannelExitedDelegate, const FString& /* ChannelName */, const FVoiceChatResult& /* Reason */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatPlayerAddedDelegate, const FString& /* ChannelName */, const FString& /* PlayerName */);
 DECLARE_MULTICAST_DELEGATE_TwoParams(FOnVoiceChatPlayerRemovedDelegate, const FString& /* ChannelName */, const FString& /* PlayerName */);
@@ -269,7 +273,12 @@ public:
 	virtual bool IsConnected() const = 0;
 
 	/**
-	 * Delegate triggered when we are unexpectedly disconnected from voice chat
+	 * Delegate triggered when we are connected to voice chat
+	 */
+	virtual FOnVoiceChatConnectedDelegate& OnVoiceChatConnected() = 0;
+
+	/**
+	 * Delegate triggered when we are disconnected from voice chat
 	 */
 	virtual FOnVoiceChatDisconnectedDelegate& OnVoiceChatDisconnected() = 0;
 
@@ -312,6 +321,16 @@ public:
 	virtual bool IsLoggedIn() const = 0;
 
 	/**
+	 * Delegate triggered when we are logged in to voice chat
+	 */
+	virtual FOnVoiceChatLoggedInDelegate& OnVoiceChatLoggedIn() = 0;
+
+	/**
+	 * Delegate triggered when we are logged out from voice chat
+	 */
+	virtual FOnVoiceChatLoggedOutDelegate& OnVoiceChatLoggedOut() = 0;
+
+	/**
 	 * Get the player name used to log in
 	 *
 	 * @return player name used to log in
@@ -352,7 +371,12 @@ public:
 	virtual void LeaveChannel(const FString& ChannelName, const FOnVoiceChatChannelLeaveCompleteDelegate& Delegate) = 0;
 
 	/**
-	 * Delegate triggered when we unexpectedly leave a voice channel
+	 * Delegate triggered when we join a voice channel
+	 */
+	virtual FOnVoiceChatChannelJoinedDelegate& OnVoiceChatChannelJoined() = 0;
+
+	/**
+	 * Delegate triggered when we leave a voice channel
 	 */
 	virtual FOnVoiceChatChannelExitedDelegate& OnVoiceChatChannelExited() = 0;
 
