a462007615feb3a18af8ec0e2da2c2464a9ffa18
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2019-04-16 17:14:10 -0400

    - Landscape Layer System:  Refactor Update Code to simplify UpdateCollisionLayerData and UpdateCollisionHeightData
    #rb richard.malo, michael.dupuis
    #rnx
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 5915242 via CL 5915325 via CL 5915330
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 5926024 by patrick enfedaque in Dev-VR branch]

--- Engine/Source/Runtime/Landscape/Classes/LandscapeComponent.h
+++ Engine/Source/Runtime/Landscape/Classes/LandscapeComponent.h
@@ -860,6 +860,14 @@ public:
 
 protected:
 
+#if WITH_EDITOR
+	void RecreateCollisionComponent(bool bUseSimpleCollision);
+	void UpdateCollisionHeightBuffer(int32 InComponentX1, int32 InComponentY1, int32 InComponentX2, int32 InComponentY2, int32 InCollisionMipLevel, int32 InHeightmapSizeU, int32 InHeightmapSizeV,
+		const FColor* const InHeightmapTextureMipData, uint16* CollisionHeightData, uint16* GrassHeightData,
+		const FColor* const InXYOffsetTextureMipData, uint16* CollisionXYOffsetData);
+	void UpdateDominantLayerBuffer(int32 InComponentX1, int32 InComponentY1, int32 InComponentX2, int32 InComponentY2, int32 InCollisionMipLevel, int32 InWeightmapSizeU, int32 InDataLayerIdx, const TArray<uint8*>& InCollisionDataPtrs, uint8* DominantLayerData);
+#endif
+
 	/** Whether the component type supports static lighting. */
 	virtual bool SupportsStaticLighting() const override
 	{
