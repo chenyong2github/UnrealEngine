5fb8b76931e64c8bc9df1c92ae06d3d81a4ddb00
brian bekich <Brian.Bekich@epicgames.com>
2019-04-16 09:54:36 -0400

    Adding analytics support for replay streamers
    #ROBOMERGE-SOURCE: CL 5915573 via CL 5915574 via CL 5915575 via CL 5915829
    
    [CL 5915847 by brian bekich in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/NetDriver.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/NetDriver.h
@@ -735,7 +735,7 @@ public:
 	void UpdateStandbyCheatStatus(void);
 
 	/** Sets the analytics provider */
-	void ENGINE_API SetAnalyticsProvider(TSharedPtr<IAnalyticsProvider> InProvider);
+	virtual void ENGINE_API SetAnalyticsProvider(TSharedPtr<IAnalyticsProvider> InProvider);
 
 #if DO_ENABLE_NET_TEST
 	FPacketSimulationSettings	PacketSimulationSettings;
