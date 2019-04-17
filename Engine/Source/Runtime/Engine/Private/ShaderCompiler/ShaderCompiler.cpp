bcf26f67e3fcab77fe481e92a1d415be5e53dde4
Juan Canada <Juan.Canada@epicgames.com>
2019-04-17 09:53:55 -0400

    Fixed compilation errors in Linux. #rb arciel.rekman
    [CL 5936364 by Juan Canada in Dev-Rendering branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -3015,6 +3015,8 @@ void GlobalBeginCompileShader(
 		Input.Environment.SetDefine(TEXT("COMPUTESHADER"), Target.Frequency == SF_Compute);
 #if RHI_RAYTRACING
 		Input.Environment.SetDefine(TEXT("RAYHITGROUPSHADER"), Target.Frequency == SF_RayHitGroup);
+		Input.Environment.SetDefine(TEXT("RAYGENSHADER"), Target.Frequency == SF_RayGen);
+		Input.Environment.SetDefine(TEXT("RAYMISSSHADER"), Target.Frequency == SF_RayMiss);
 #endif
 	}
 
