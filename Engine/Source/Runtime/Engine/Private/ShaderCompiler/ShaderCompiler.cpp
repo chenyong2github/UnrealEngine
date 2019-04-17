95bc193166f59766fb7bad9e7401a0c107cb8c23
juan canada <Juan.Canada@epicgames.com>
2019-04-17 13:54:16 -0400

    Fix shader compilation in windows with ray tracing enabled. #rb none [FYI] arciel rekman, yuriy.odonnell #jira none
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: juan.canada
    #ROBOMERGE-SOURCE: CL 5934139 in //UE4/Release-4.22/... via CL 5934140
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 5947418 by juan canada in Dev-Anim branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -2990,6 +2990,7 @@ void GlobalBeginCompileShader(
 #if RHI_RAYTRACING
 		Input.Environment.SetDefine(TEXT("RAYHITGROUPSHADER"), Target.Frequency == SF_RayHitGroup);
 		Input.Environment.SetDefine(TEXT("RAYGENSHADER"), Target.Frequency == SF_RayGen);
+		Input.Environment.SetDefine(TEXT("RAYMISSSHADER"), Target.Frequency == SF_RayMiss);
 #endif
 	}
 
