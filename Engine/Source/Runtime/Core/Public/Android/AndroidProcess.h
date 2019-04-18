1e6ab4c70456059766d7c94caea9788ffe904e8d
david harvey <David.Harvey@epicgames.com>
2019-04-18 05:49:30 -0400

    Implement FPlatformProcess::GetCurrentCoreNumber
    #rb anthony.bills
    
    
    #ROBOMERGE-SOURCE: CL 5974941 via CL 5975387
    
    [CL 5975410 by david harvey in Main branch]

--- Engine/Source/Runtime/Core/Public/Android/AndroidProcess.h
+++ Engine/Source/Runtime/Core/Public/Android/AndroidProcess.h
@@ -34,6 +34,7 @@ struct CORE_API FAndroidPlatformProcess : public FGenericPlatformProcess
 	static const TCHAR* ComputerName();
 	static void SetThreadAffinityMask( uint64 AffinityMask );
 	static uint32 GetCurrentProcessId();
+	static uint32 GetCurrentCoreNumber();
 	static const TCHAR* BaseDir();
 	static const TCHAR* ExecutableName(bool bRemoveExtension = true);
 	static class FRunnableThread* CreateRunnableThread();
