207f9934642928547bb65451dd0f6ee3917f18dd
Juan Canada <Juan.Canada@epicgames.com>
2019-04-18 15:19:16 -0400

    Fixed post merge compilation issues #rb none
    [CL 5992745 by Juan Canada in Dev-Rendering branch]

--- Engine/Source/Runtime/MRMesh/Private/MRMeshComponent.cpp
+++ Engine/Source/Runtime/MRMesh/Private/MRMeshComponent.cpp
@@ -325,7 +325,7 @@ private:
 						Mesh.MaterialRenderProxy = MaterialProxy;
 
 						FDynamicPrimitiveUniformBuffer& DynamicPrimitiveUniformBuffer = Collector.AllocateOneFrameResource<FDynamicPrimitiveUniformBuffer>();
-						DynamicPrimitiveUniformBuffer.Set(GetLocalToWorld(), GetLocalToWorld(), InfiniteBounds, InfiniteBounds, true, false, bDrawsVelocity(), false);
+						DynamicPrimitiveUniformBuffer.Set(GetLocalToWorld(), GetLocalToWorld(), InfiniteBounds, InfiniteBounds, true, false, DrawsVelocity(), false);
 						BatchElement.PrimitiveUniformBufferResource = &DynamicPrimitiveUniformBuffer.UniformBuffer;
 
 						BatchElement.FirstIndex = 0;
