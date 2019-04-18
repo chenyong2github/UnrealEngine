373e4a59675511b02ef41e412f74d44361bfa9aa
Dan Oconnor <Dan.OConnor@epicgames.com>
2019-04-18 11:03:37 -0400

    Conform bHideFromSceneOutliner to pattern established in 4822786. uint8 packs better. #rb Marc.Audy #rnx #jira none
    [CL 5983812 by Dan Oconnor in Dev-Framework branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/World.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/World.h
@@ -464,11 +464,10 @@ public:
 
 #if WITH_EDITOR
 	/** Determines whether the begin play cycle will run on the spawned actor when in the editor. */
-	uint8 bTemporaryEditorActor:1;
+	uint8	bTemporaryEditorActor:1;
 
 	/* Determines wether or not the actor should be hidden from the Scene Outliner */
-	uint16 bHideFromSceneOutliner : 1;
-	
+	uint8	bHideFromSceneOutliner:1;
 #endif
 	
 	/* Flags used to describe the spawned actor/object instance. */
