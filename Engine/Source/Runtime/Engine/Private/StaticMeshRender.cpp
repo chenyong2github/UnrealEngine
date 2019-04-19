563bf33ea017b232db73cacf5d7625af7ce86d9e
arne schober <Arne.Schober@epicgames.com>
2019-04-19 13:50:34 -0400

    FN - Add more debug info for FORT-164411 #RB none
    #ROBOMERGE-SOURCE: CL 6014291 via CL 6014296 via CL 6014307 via CL 6014315 via CL 6016809
    
    [CL 6016872 by arne schober in Main branch]

--- Engine/Source/Runtime/Engine/Private/StaticMeshRender.cpp
+++ Engine/Source/Runtime/Engine/Private/StaticMeshRender.cpp
@@ -136,7 +136,13 @@ FStaticMeshSceneProxy::FStaticMeshSceneProxy(UStaticMeshComponent* InComponent, 
 #endif
 {
 	check(RenderData);
-	checkf(RenderData->IsInitialized(), TEXT("Uninitialized Renderdata for Mesh: %s"), *InComponent->GetStaticMesh()->GetFName().ToString());
+	checkf(RenderData->IsInitialized(), TEXT("Uninitialized Renderdata for Mesh: %s, Mesh NeedsLoad: %i, Mesh NeedsPostLoad: %i, Mesh Loaded: %i, Mesh NeedInit: %i")
+		, *StaticMesh->GetFName().ToString()
+		, StaticMesh->HasAnyFlags(RF_NeedLoad)
+		, StaticMesh->HasAnyFlags(RF_NeedPostLoad)
+		, StaticMesh->HasAnyFlags(RF_LoadCompleted)
+		, StaticMesh->HasAnyFlags(RF_NeedInitialization)
+	);
 
 	const auto FeatureLevel = GetScene().GetFeatureLevel();
 
