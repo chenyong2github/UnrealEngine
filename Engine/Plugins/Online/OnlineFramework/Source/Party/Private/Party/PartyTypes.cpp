83573641a096d9e20846d79e21898c0132ec70c5
dan hertzka <Dan.Hertzka@epicgames.com>
2019-04-19 13:49:56 -0400

    Addressed race condition causing stale platform session information to persist when a party member is promoted to leader as a result of the leader leaving
    #rb [at]Don.Eubanks
    [at]Rob.Cannaday
    
    
    #ROBOMERGE-SOURCE: CL 6014032 via CL 6014037 via CL 6014135 via CL 6014170 via CL 6016750
    
    [CL 6016827 by dan hertzka in Main branch]

--- Engine/Plugins/Online/OnlineFramework/Source/Party/Private/Party/PartyTypes.cpp
+++ Engine/Plugins/Online/OnlineFramework/Source/Party/Private/Party/PartyTypes.cpp
@@ -109,14 +109,27 @@ bool FJoinPartyResult::WasSuccessful() const
 // FOnlinePartyRepDataBase
 //////////////////////////////////////////////////////////////////////////
 
-void FOnlinePartyRepDataBase::LogPropertyChanged(const TCHAR* OwningStructTypeName, const TCHAR* ProperyName, bool bFromReplication) const
+void FOnlinePartyRepDataBase::LogSetPropertyFailure(const TCHAR* OwningStructTypeName, const TCHAR* PropertyName) const
+{
+	const USocialParty* OwningParty = GetOwnerParty();
+	if (ensure(OwningParty))
+	{
+		UE_LOG(LogParty, Warning, TEXT("Failed to modify RepData property [%s::%s] in party [%s] - local member [%s] does not have authority."),
+			OwningStructTypeName,
+			PropertyName,
+			*OwningParty->ToDebugString(),
+			*OwningParty->GetOwningLocalMember().ToDebugString(false));
+	}
+}
+
+void FOnlinePartyRepDataBase::LogPropertyChanged(const TCHAR* OwningStructTypeName, const TCHAR* PropertyName, bool bFromReplication) const
 {
 	const USocialParty* OwningParty = GetOwnerParty();
 	
 	// Only thing this lacks is the id of the party member for member rep data changes
 	UE_LOG(LogParty, VeryVerbose, TEXT("RepData property [%s::%s] changed %s in party [%s]"),
 		OwningStructTypeName,
-		ProperyName,
+		PropertyName,
 		bFromReplication ? TEXT("remotely") : TEXT("locally"),
 		ensure(OwningParty) ? *OwningParty->ToDebugString() : TEXT("unknown"));
 }
