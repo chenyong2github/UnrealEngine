9628030b5bc834b5b21931efc99930cb7fa9bd55
satchit subramanian <Satchit.Subramanian@epicgames.com>
2019-04-20 01:06:41 -0400

    Add setting to stop simulating rigidbody nodes below a minimum timestep.
    #rb Steven.Barnett
    [FYI] Lina.Halper
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: satchit.subramanian
    #ROBOMERGE-SOURCE: CL 6023527 via CL 6023529 via CL 6023813 via CL 6023852 via CL 6024825 via CL 6024947
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 6029443 by satchit subramanian in Dev-Build branch]

--- Engine/Source/Runtime/AnimGraphRuntime/Public/BoneControllers/AnimNode_AnimDynamics.h
+++ Engine/Source/Runtime/AnimGraphRuntime/Public/BoneControllers/AnimNode_AnimDynamics.h
@@ -471,7 +471,7 @@ private:
 	float TimeDebt;
 
 	// Cached physics settings. We cache these on initialise to avoid the cost of accessing UPhysicsSettings a lot each frame
-	float AnimDynamicsMinDeltaTime;
+	float AnimPhysicsMinDeltaTime;
 	float MaxPhysicsDeltaTime;
 	float MaxSubstepDeltaTime;
 	int32 MaxSubsteps;
