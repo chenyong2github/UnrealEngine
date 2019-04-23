02cd35f4c663f740fb79e0708cc8c77d1069491e
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2019-04-23 13:02:38 -0400

    - Fix New Foliage Type Crash in Multi-level map - Fix Replace Foliage Type
    #jira UE-72875
    #rb richard.malo
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 6053720 via CL 6054670 via CL 6054674
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim)
    
    [CL 6058633 by patrick enfedaque in Dev-Anim branch]

--- Engine/Source/Runtime/Foliage/Private/InstancedFoliage.cpp
+++ Engine/Source/Runtime/Foliage/Private/InstancedFoliage.cpp
@@ -1945,10 +1945,10 @@ void FFoliageInfo::RemoveFromBaseHash(int32 InstanceIndex)
 // Destroy existing clusters and reassign all instances to new clusters
 void FFoliageInfo::ReallocateClusters(AInstancedFoliageActor* InIFA, UFoliageType* InSettings)
 {
-	if (Implementation->IsInitialized())
-	{
-		Implementation->Uninitialize();
-	}
+	// In case Foliage Type Changed recreate implementation
+	Implementation.Reset();
+	CreateImplementation(InSettings);
+	
 	// Remove everything
 	TArray<FFoliageInstance> OldInstances;
 	Exchange(Instances, OldInstances);
@@ -2823,6 +2823,10 @@ FFoliageInfo* AInstancedFoliageActor::AddMesh(UFoliageType* InType)
 	Modify();
 
 	FFoliageInfo* Info = &*FoliageInfos.Add(InType);
+	if (Info && !Info->Implementation.IsValid())
+	{
+		Info->CreateImplementation(InType);
+	}
 	Info->FoliageTypeUpdateGuid = InType->UpdateGuid;
 	InType->IsSelected = true;
 
