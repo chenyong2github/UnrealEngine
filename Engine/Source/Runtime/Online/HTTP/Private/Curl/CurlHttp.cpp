a81f24e6e0d637b344f015249001ca056e4db882
evan kinney <Evan.Kinney@epicgames.com>
2019-04-23 18:55:02 -0400

    fix a thread safety issue in the curl module where an HTTP response would be considered complete before the result code is set and make the bool atomic to prevent further shenanigans. also, ultimately fail requests that return OK but have an invalid/zero HTTP response code.
    [REVIEW] [at]brian.bekich
    
    
    #ROBOMERGE-SOURCE: CL 6067856 via CL 6069180
    
    [CL 6069234 by evan kinney in Main branch]

--- Engine/Source/Runtime/Online/HTTP/Private/Curl/CurlHttp.cpp
+++ Engine/Source/Runtime/Online/HTTP/Private/Curl/CurlHttp.cpp
@@ -1042,6 +1042,13 @@ void FCurlHttpRequest::FinishedRequest()
 			{
 				Response->ContentLength = static_cast< int32 >(ContentLengthDownload);
 			}
+
+			if (Response->HttpCode <= 0)
+			{
+				UE_LOG(LogHttp, Warning, TEXT("%p: invalid HTTP response code received. URL: %s, HTTP code: %d, content length: %d, actual payload size: %d"),
+					this, *GetURL(), Response->HttpCode, Response->ContentLength, Response->Payload.Num());
+				Response->bSucceeded = false;
+			}
 		}
 	}
 	
