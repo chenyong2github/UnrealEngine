c2108b6c17f3cc8515d26f9dbf78f522f515c952
michael kirzinger <Michael.Kirzinger@epicgames.com>
2019-04-24 17:24:42 -0400

    Update Vivox SDK to 4.9.002.31944
    #ROBOMERGE-SOURCE: CL 6081030 via CL 6081037 via CL 6086448
    
    [CL 6086506 by michael kirzinger in Main branch]

--- Engine/Source/ThirdParty/Vivox/vivox-sdk/Include/vivox-config.h
+++ Engine/Source/ThirdParty/Vivox/vivox-sdk/Include/vivox-config.h
@@ -23,13 +23,13 @@
  */
 
 #pragma once
-#define SDK_VERSION "4.9.0002.31672"
+#define SDK_VERSION "4.9.0002.31944"
 #define SDK_BRANCH "HEAD"
 
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 9
 /* so VERSION_MICRO (.e.g. 0008) is not interpreted as octal */
 #define VERSION_MICRO (10002 - 10000)
-#define VERSION_BUILD 31672
+#define VERSION_BUILD 31944
 
-#define VERSION_HASH "86742c7e"
+#define VERSION_HASH "cd26a60b"
