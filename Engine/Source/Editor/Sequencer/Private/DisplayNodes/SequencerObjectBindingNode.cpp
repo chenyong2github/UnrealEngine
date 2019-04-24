e832e1280536eb3a465cad6807db56372f1f7d42
Max Chen <Max.Chen@epicgames.com>
2019-04-24 14:03:25 -0400

    Sequencer: Allow adding tracks for all object classes, not restricted to selected object binding class
    #jira UE-73219
    #rb none
    
    [CL 6080786 by Max Chen in Dev-Sequencer branch]

--- Engine/Source/Editor/Sequencer/Private/DisplayNodes/SequencerObjectBindingNode.cpp
+++ Engine/Source/Editor/Sequencer/Private/DisplayNodes/SequencerObjectBindingNode.cpp
@@ -901,7 +901,7 @@ TSharedRef<SWidget> FSequencerObjectBindingNode::HandleAddTrackComboButtonGetMen
 		FGuid Guid = ObjectBindingNode->GetObjectBinding();
 		for (auto RuntimeObject : Sequencer.FindBoundObjects(Guid, Sequencer.GetFocusedTemplateID()))
 		{
-			if (RuntimeObject->GetClass() == ObjectClass)
+			////if (RuntimeObject->GetClass() == ObjectClass)
 			{
 				ObjectBindings.AddUnique(Guid);
 				continue;
