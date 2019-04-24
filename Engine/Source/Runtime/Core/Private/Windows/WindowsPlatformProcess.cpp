e86b3435682261c77580904009ab6af23edb1c2e
chris bunner <chris.bunner@epicgames.com>
2019-04-24 17:07:38 -0400

    Strip usage_other CSV stat as it's inaccurate and can be inferred. #rb None
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: chris.bunner
    #ROBOMERGE-SOURCE: CL 6078273 via CL 6078278 via CL 6080587 via CL 6080729
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 6084514 by chris bunner in Dev-Build branch]

--- Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformProcess.cpp
+++ Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformProcess.cpp
@@ -495,7 +495,7 @@ bool FWindowsPlatformProcess::GetApplicationMemoryUsage(uint32 ProcessId, SIZE_T
 	return bSuccess;
 }
 
-bool FWindowsPlatformProcess::GetPerFrameProcessorUsage(uint32 ProcessId, float& ProcessUsageFraction, float& OtherUsageFraction, float& IdleUsageFraction)
+bool FWindowsPlatformProcess::GetPerFrameProcessorUsage(uint32 ProcessId, float& ProcessUsageFraction, float& IdleUsageFraction)
 {
 	bool bSuccess = true;
 
@@ -580,11 +580,10 @@ bool FWindowsPlatformProcess::GetPerFrameProcessorUsage(uint32 ProcessId, float&
 	{
 		ProcessUsageFraction = LastProcessTime;
 		IdleUsageFraction = LastIdleTime;
-		OtherUsageFraction = FMath::Clamp(1.f - (LastProcessTime + LastIdleTime), 0.f, 1.f);
 	}
 	else
 	{
-		ProcessUsageFraction = OtherUsageFraction = IdleUsageFraction = 0.f;
+		ProcessUsageFraction = IdleUsageFraction = 0.f;
 	}
 
 	return bSuccess;
