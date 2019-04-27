8ef1762b5626e493e7274e50a64196b20065d2f3
helge mathee <Helge.Mathee@epicgames.com>
2019-04-26 21:57:14 -0400

    Merging
    //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Plugins/Experimental/ControlRig/Source/ControlRig/...
    
    to //Fortnite/Main/Engine/Plugins/Experimental/ControlRig/Source/ControlRig/...
    
    Original CL: 6083346
    
    #jira: UEA-514
    #rb lina.halper
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 6098953 via CL 6100691 via CL 6100776
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 6124597 by helge mathee in Dev-Build branch]

---
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/Units/Highlevel/Hierarchy/RigUnit_SpringIK.cpp
@@ -0,0 +1,164 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Units/Highlevel/Hierarchy/RigUnit_SpringIK.h"
+#include "Units/RigUnitContext.h"
+
+void FRigUnit_SpringIK::Execute(const FRigUnitContext& Context)
+{
+	FRigHierarchy* Hierarchy = (FRigHierarchy*)(Context.HierarchyReference.Get());
+	if (Hierarchy == nullptr)
+	{
+		return;
+	}
+
+	if (Context.State == EControlRigState::Init)
+	{
+		BoneIndex = Hierarchy->GetIndex(Bone);
+		return;
+	}
+
+	if (BoneIndex == INDEX_NONE)
+	{
+		return;
+	}
+
+	if (Primary.Weight <= SMALL_NUMBER && Secondary.Weight <= SMALL_NUMBER)
+	{
+		return;
+
+	}
+	FTransform Transform = Hierarchy->GetGlobalTransform(BoneIndex);
+
+	if (Primary.Weight > SMALL_NUMBER)
+	{
+		FVector Target = Primary.Target;
+
+		if (PrimaryCachedSpaceName != Primary.Space)
+		{
+			if (Primary.Space == NAME_None)
+			{
+				PrimaryCachedSpaceIndex = INDEX_NONE;
+			}
+			else
+			{
+				PrimaryCachedSpaceIndex = Hierarchy->GetIndex(Primary.Space);
+			}
+			PrimaryCachedSpaceName = Primary.Space;
+		}
+
+		if (PrimaryCachedSpaceIndex != INDEX_NONE)
+		{
+			FTransform Space = Hierarchy->GetGlobalTransform(PrimaryCachedSpaceIndex);
+			if (Primary.Kind == EControlRigVectorKind::Direction)
+			{
+				Target = Space.TransformVectorNoScale(Target);
+			}
+			else
+			{
+				Target = Space.TransformPositionNoScale(Target);
+			}
+		}
+
+		if (Context.DrawInterface != nullptr && DebugSettings.bEnabled)
+		{
+			const FLinearColor Color = FLinearColor(0.f, 1.f, 1.f, 1.f);
+			if (Primary.Kind == EControlRigVectorKind::Direction)
+			{
+				Context.DrawInterface->DrawLine(DebugSettings.WorldOffset, Transform.GetLocation(), Transform.GetLocation() + Target * DebugSettings.Scale, Color);
+			}
+			else
+			{
+				Context.DrawInterface->DrawLine(DebugSettings.WorldOffset, Transform.GetLocation(), Target, Color);
+				Context.DrawInterface->DrawBox(DebugSettings.WorldOffset, FTransform(FQuat::Identity, Target, FVector(1.f, 1.f, 1.f) * DebugSettings.Scale * 0.1f), Color);
+			}
+		}
+
+		if (Primary.Kind == EControlRigVectorKind::Location)
+		{
+			Target = Target - Transform.GetLocation();
+		}
+
+		if (!Target.IsNearlyZero() && !Primary.Axis.IsNearlyZero())
+		{
+			Target = Target.GetSafeNormal();
+			FVector Axis = Transform.TransformVectorNoScale(Primary.Axis).GetSafeNormal();
+			if (Primary.Weight < 1.f - SMALL_NUMBER)
+			{
+				Target = FMath::Lerp<FVector>(Axis, Target, Primary.Weight).GetSafeNormal();
+			}
+			FQuat Rotation = FQuat::FindBetweenNormals(Axis, Target);
+			Transform.SetRotation((Rotation * Transform.GetRotation()).GetNormalized());
+		}
+	}
+
+	if (Secondary.Weight > SMALL_NUMBER)
+	{
+		FVector Target = Secondary.Target;
+
+		if (SecondaryCachedSpaceName != Secondary.Space)
+		{
+			if (Secondary.Space == NAME_None)
+			{
+				SecondaryCachedSpaceIndex = INDEX_NONE;
+			}
+			else
+			{
+				SecondaryCachedSpaceIndex = Hierarchy->GetIndex(Secondary.Space);
+			}
+			SecondaryCachedSpaceName = Secondary.Space;
+		}
+
+		if (SecondaryCachedSpaceIndex != INDEX_NONE)
+		{
+			FTransform Space = Hierarchy->GetGlobalTransform(SecondaryCachedSpaceIndex);
+			if (Secondary.Kind == EControlRigVectorKind::Direction)
+			{
+				Target = Space.TransformVectorNoScale(Target);
+			}
+			else
+			{
+				Target = Space.TransformPositionNoScale(Target);
+			}
+		}
+
+		if (Context.DrawInterface != nullptr && DebugSettings.bEnabled)
+		{
+			const FLinearColor Color = FLinearColor(0.f, 0.2f, 1.f, 1.f);
+			if (Secondary.Kind == EControlRigVectorKind::Direction)
+			{
+				Context.DrawInterface->DrawLine(DebugSettings.WorldOffset, Transform.GetLocation(), Transform.GetLocation() + Target * DebugSettings.Scale, Color);
+			}
+			else
+			{
+				Context.DrawInterface->DrawLine(DebugSettings.WorldOffset, Transform.GetLocation(), Target, Color);
+				Context.DrawInterface->DrawBox(DebugSettings.WorldOffset, FTransform(FQuat::Identity, Target, FVector(1.f, 1.f, 1.f) * DebugSettings.Scale * 0.1f), Color);
+			}
+		}
+
+		if (Secondary.Kind == EControlRigVectorKind::Location)
+		{
+			Target = Target - Transform.GetLocation();
+		}
+
+		if (!Primary.Axis.IsNearlyZero())
+		{
+			FVector PrimaryAxis = Transform.TransformVectorNoScale(Primary.Axis).GetSafeNormal();
+			Target = Target - FVector::DotProduct(Target, PrimaryAxis) * PrimaryAxis;
+		}
+
+		if (!Target.IsNearlyZero() && !Secondary.Axis.IsNearlyZero())
+		{
+			Target = Target.GetSafeNormal();
+
+			FVector Axis = Transform.TransformVectorNoScale(Secondary.Axis).GetSafeNormal();
+			if (Secondary.Weight < 1.f - SMALL_NUMBER)
+			{
+				Target = FMath::Lerp<FVector>(Axis, Target, Secondary.Weight).GetSafeNormal();
+			}
+			FQuat Rotation = FQuat::FindBetweenNormals(Axis, Target);
+			Transform.SetRotation((Rotation * Transform.GetRotation()).GetNormalized());
+		}
+	}
+
+	Hierarchy->SetGlobalTransform(BoneIndex, Transform, bPropagateToChildren);
+}
