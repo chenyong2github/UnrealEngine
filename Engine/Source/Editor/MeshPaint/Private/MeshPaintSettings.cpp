a6b296c358998b6e66fbdacf89ea209f4107deae
sebastian nordgren <sebastian.nordgren@epicgames.com>
2019-05-02 04:15:08 -0400

    #jira UE-11832 Added commands to adjust the brush falloff and strength to mesh painting mode.
    #rb jurre.debaare
    
    [CL 6248956 by sebastian nordgren in Dev-Editor branch]

--- Engine/Source/Editor/MeshPaint/Private/MeshPaintSettings.cpp
+++ Engine/Source/Editor/MeshPaint/Private/MeshPaintSettings.cpp
@@ -16,7 +16,13 @@ UPaintBrushSettings::UPaintBrushSettings(const FObjectInitializer& ObjectInitial
 	BrushRadiusMin = 0.01f, BrushRadiusMax = 250000.0f;
 
 	GConfig->GetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushRadius"), BrushRadius, GEditorPerProjectIni);
-	BrushRadius = (float)FMath::Clamp(BrushRadius, BrushRadiusMin, BrushRadiusMax);
+	BrushRadius = FMath::Clamp(BrushRadius, BrushRadiusMin, BrushRadiusMax);
+
+	GConfig->GetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushStrength"), BrushStrength, GEditorPerProjectIni);
+	BrushStrength = FMath::Clamp(BrushRadius, 0.f, 1.f);
+
+	GConfig->GetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushFalloff"), BrushFalloffAmount, GEditorPerProjectIni);
+	BrushFalloffAmount = FMath::Clamp(BrushFalloffAmount, 0.f, 1.f);
 }
 
 UPaintBrushSettings::~UPaintBrushSettings()
@@ -29,10 +35,32 @@ void UPaintBrushSettings::SetBrushRadius(float InRadius)
 	GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushRadius"), BrushRadius, GEditorPerProjectIni);
 }
 
+void UPaintBrushSettings::SetBrushStrength(float InStrength)
+{
+	BrushStrength = FMath::Clamp(InStrength, 0.f, 1.f);
+	GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushStrength"), BrushStrength, GEditorPerProjectIni);
+}
+
+void UPaintBrushSettings::SetBrushFalloff(float InFalloff)
+{
+	BrushFalloffAmount = FMath::Clamp(InFalloff, 0.f, 1.f);
+	GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushFalloff"), BrushFalloffAmount, GEditorPerProjectIni);
+}
+
 void UPaintBrushSettings::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
 {
 	if (PropertyChangedEvent.Property && PropertyChangedEvent.Property->GetFName() == GET_MEMBER_NAME_CHECKED(UPaintBrushSettings, BrushRadius) && PropertyChangedEvent.ChangeType != EPropertyChangeType::Interactive)
 	{
 		GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushRadius"), BrushRadius, GEditorPerProjectIni);
+	}
+
+	if (PropertyChangedEvent.Property && PropertyChangedEvent.Property->GetFName() == GET_MEMBER_NAME_CHECKED(UPaintBrushSettings, BrushStrength) && PropertyChangedEvent.ChangeType != EPropertyChangeType::Interactive)
+	{
+		GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushStrength"), BrushStrength, GEditorPerProjectIni);
+	}
+
+	if (PropertyChangedEvent.Property && PropertyChangedEvent.Property->GetFName() == GET_MEMBER_NAME_CHECKED(UPaintBrushSettings, BrushFalloffAmount) && PropertyChangedEvent.ChangeType != EPropertyChangeType::Interactive)
+	{
+		GConfig->SetFloat(TEXT("MeshPaintEdit"), TEXT("DefaultBrushFalloff"), BrushFalloffAmount, GEditorPerProjectIni);
 	}
 }
