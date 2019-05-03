835913d174ee37e25ca523833b17c734f14d4ca4
andrew grant <Andrew.Grant@epicgames.com>
2019-05-03 05:36:23 -0400

    Missed file from 6258941
    #jira #rb na
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 6258972 in //UE4/Main/...
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core)
    
    [CL 6270845 by andrew grant in Dev-Core branch]

--- Engine/Source/Developer/AutomationController/AutomationController.Build.cs
+++ Engine/Source/Developer/AutomationController/AutomationController.Build.cs
@@ -10,8 +10,7 @@ namespace UnrealBuildTool.Rules
 				new string[]
 				{
 					"Core",
-					"CoreUObject",
-					"Engine"
+					"CoreUObject"
 				}
 			); 
 			
@@ -27,7 +26,7 @@ namespace UnrealBuildTool.Rules
 					"ScreenShotComparisonTools",
 					"HTTP",
                     "AssetRegistry"
-                }
+				}
 			);
 
             if (Target.bBuildEditor)
