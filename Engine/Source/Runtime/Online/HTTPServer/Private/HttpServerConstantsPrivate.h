fd6065b4de1401ba16a2bd89a1f33e79d81daf58
mitchell fisher <Mitchell.Fisher@epicgames.com>
2019-05-15 15:06:26 -0400

    [HttpServer] Fix clang linkage discrepencies
    #rb none
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: mitchell.fisher
    #ROBOMERGE-SOURCE: CL 6455774 via CL 6456186 via CL 6456274
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build)
    
    [CL 6505003 by mitchell fisher in Dev-Build branch]

--- Engine/Source/Runtime/Online/HTTPServer/Private/HttpServerConstantsPrivate.h
+++ Engine/Source/Runtime/Online/HTTPServer/Private/HttpServerConstantsPrivate.h
@@ -3,33 +3,33 @@
 
 #include "CoreMinimal.h"
 
-struct FHttpServerHeaderKeys
+namespace FHttpServerHeaderKeys
 {
-	static constexpr TCHAR CONTENT_TYPE[] = TEXT("content-type");
-	static constexpr TCHAR CONTENT_LENGTH[] = TEXT("content-length");
-	static constexpr TCHAR CONNECTION[] = TEXT("connection");
-	static constexpr TCHAR KEEP_ALIVE[] = TEXT("keep-alive");
+	const FString CONTENT_TYPE = TEXT("content-type");
+	const FString CONTENT_LENGTH = TEXT("content-length");
+	const FString CONNECTION = TEXT("connection");
+	const FString KEEP_ALIVE = TEXT("keep-alive");
 };
 
-struct FHttpServerErrorStrings
+namespace FHttpServerErrorStrings
 {
 	// Connection
-	static constexpr TCHAR SocketClosedFailure[] = TEXT("errors.com.epicgames.httpserver.socket_closed_failure");
-	static constexpr TCHAR SocketRecvFailure[] = TEXT("errors.com.epicgames.httpserver.socket_recv_failure");
-	static constexpr TCHAR SocketSendFailure[] = TEXT("errors.com.epicgames.httpserver.socket_send_failure");
+	const FString SocketClosedFailure = TEXT("errors.com.epicgames.httpserver.socket_closed_failure");
+	const FString SocketRecvFailure = TEXT("errors.com.epicgames.httpserver.socket_recv_failure");
+	const FString SocketSendFailure = TEXT("errors.com.epicgames.httpserver.socket_send_failure");
 
 	// Routing
-	static constexpr TCHAR NotFound[] = TEXT("errors.com.epicgames.httpserver.route_handler_not_found");
+	const FString NotFound = TEXT("errors.com.epicgames.httpserver.route_handler_not_found");
 
 	// Serialization
-	static constexpr TCHAR MalformedRequestSize[] = TEXT("errors.com.epicgames.httpserver.malformed_request_size");
-	static constexpr TCHAR MalformedRequestHeaders[] = TEXT("errors.com.epicgames.httpserver.malformed_request_header");
-	static constexpr TCHAR MissingRequestHeaders[] = TEXT("errors.com.epicgames.httpserver.missing_request_headers");
-	static constexpr TCHAR MalformedRequestBody[] = TEXT("errors.com.epicgames.httpserver.malformed_request_body");
-	static constexpr TCHAR UnknownRequestVerb[] = TEXT("errors.com.epicgames.httpserver.unknown_request_verb");
-	static constexpr TCHAR UnsupportedHttpVersion[] = TEXT("errors.com.epicgames.httpserver.unsupported_http_version");
-	static constexpr TCHAR InvalidContentLengthHeader[] = TEXT("errors.com.epicgames.httpserver.invalid_content_length_header");
-	static constexpr TCHAR MissingContentLengthHeader[] = TEXT("errors.com.epicgames.httpserver.missing_content_length_header");
-	static constexpr TCHAR MismatchedContentLengthBodyTooLarge[] =  TEXT("errors.com.epicgames.httpserver.mismatched_content_length_body_too_large");
+	const FString MalformedRequestSize = TEXT("errors.com.epicgames.httpserver.malformed_request_size");
+	const FString MalformedRequestHeaders = TEXT("errors.com.epicgames.httpserver.malformed_request_header");
+	const FString MissingRequestHeaders = TEXT("errors.com.epicgames.httpserver.missing_request_headers");
+	const FString MalformedRequestBody = TEXT("errors.com.epicgames.httpserver.malformed_request_body");
+	const FString UnknownRequestVerb = TEXT("errors.com.epicgames.httpserver.unknown_request_verb");
+	const FString UnsupportedHttpVersion = TEXT("errors.com.epicgames.httpserver.unsupported_http_version");
+	const FString InvalidContentLengthHeader = TEXT("errors.com.epicgames.httpserver.invalid_content_length_header");
+	const FString MissingContentLengthHeader = TEXT("errors.com.epicgames.httpserver.missing_content_length_header");
+	const FString MismatchedContentLengthBodyTooLarge =  TEXT("errors.com.epicgames.httpserver.mismatched_content_length_body_too_large");
 };
 
