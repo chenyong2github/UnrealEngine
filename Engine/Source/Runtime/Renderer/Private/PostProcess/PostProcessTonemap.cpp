0c4f223bb5ac04b5e318c0000a40e9275f0957cf
kevin ortegren <Kevin.Ortegren@epicgames.com>
2019-05-15 14:21:12 -0400

    Alpha channel writing in tonemapper is now only enabled for FXAA
    #rb guillaume.abadie
    #rnx
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: kevin.ortegren
    #ROBOMERGE-SOURCE: CL 6423678 via CL 6424411 via CL 6424415
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 6498064 by kevin ortegren in Dev-VR branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessTonemap.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessTonemap.cpp
@@ -1107,14 +1107,15 @@ void FRCPassPostProcessTonemap::Process(FRenderingCompositePassContext& Context)
 			{
 				FGraphicsPipelineStateInitializer GraphicsPSOInit;
 				Context.RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
-				if (Context.View.AntiAliasingMethod == AAM_None)
+				if (Context.View.AntiAliasingMethod == AAM_FXAA)
 				{
-					// Skip writing to alpha when AA is disabled. Luminance in the alpha channel is unused in this case. 
-					GraphicsPSOInit.BlendState = TStaticBlendStateWriteMask<CW_RGB>::GetRHI();
+					// Alpha is used to store luminance for post process AA
+					GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();
 				}
 				else
 				{
-					GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();
+					// Skip writing to alpha, luminance in the alpha channel is unused in this case. 
+					GraphicsPSOInit.BlendState = TStaticBlendStateWriteMask<CW_RGB>::GetRHI();
 				}
 				GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
 				GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
