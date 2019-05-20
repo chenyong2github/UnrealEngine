7b3d4d9b1bcd3e52818e671b518fb7ae6b4c858a
joe conley <Joe.Conley@epicgames.com>
2019-05-20 13:35:41 -0400

    Fixing oculus crash when first enabling it in an empty project #jira UE-74722 #rb Ethan.Geller #lockdown Cristina.Riveron
    #ROBOMERGE-VERSION: 348-6547088
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: aaron.mcleran
    #ROBOMERGE-SOURCE: CL 6537148 in //UE4/Release-4.22/... via CL 6537149
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 6577015 by joe conley in Dev-VR branch]

--- Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAmbisonicSpatializer.cpp
+++ Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAmbisonicSpatializer.cpp
@@ -58,25 +58,23 @@ void FOculusAmbisonicsMixer::EncodeToAmbisonics(const uint32 SourceId, const FAm
 
 void FOculusAmbisonicsMixer::OnOpenDecodingStream(const uint32 StreamId, UAmbisonicsSubmixSettingsBase* InSettings, FAmbisonicsDecoderPositionalData& SpecifiedOutputPositions)
 {
-	if (InSettings == nullptr) 
+	UOculusAmbisonicsSettings* Settings = Cast<UOculusAmbisonicsSettings>(InSettings);
+	if (Settings)
 	{
-		return; // PAS
+		ovrAudioAmbisonicStream NewStream = nullptr;
+		ovrAudioAmbisonicFormat StreamFormat = (Settings->ChannelOrder == EAmbisonicFormat::AmbiX ? ovrAudioAmbisonicFormat_AmbiX : ovrAudioAmbisonicFormat_FuMa);
+		ovrResult OurResult = OVRA_CALL(ovrAudio_CreateAmbisonicStream)(Context, SampleRate, BufferLength, StreamFormat, 1, &NewStream);
+
+		check(OurResult == 0);
+		check(NewStream != nullptr);
+
+		ovrAudioAmbisonicSpeakerLayout SpeakerLayout = (Settings->SpatializationMode == EAmbisonicMode::SphericalHarmonics) ?
+			ovrAudioAmbisonicSpeakerLayout_SphericalHarmonics : ovrAudioAmbisonicSpeakerLayout_Icosahedron;
+		OurResult = OVRA_CALL(ovrAudio_SetAmbisonicSpeakerLayout)(NewStream, SpeakerLayout);
+
+		check(OurResult == 0);
+		OpenStreams.Add(StreamId, NewStream);
 	}
-	UOculusAmbisonicsSettings* Settings = CastChecked<UOculusAmbisonicsSettings>(InSettings);
-
-	ovrAudioAmbisonicStream NewStream = nullptr;
-	ovrAudioAmbisonicFormat StreamFormat = (Settings->ChannelOrder == EAmbisonicFormat::AmbiX ? ovrAudioAmbisonicFormat_AmbiX : ovrAudioAmbisonicFormat_FuMa);
-	ovrResult OurResult = OVRA_CALL(ovrAudio_CreateAmbisonicStream)(Context, SampleRate, BufferLength, StreamFormat, 1, &NewStream);
-
-	check(OurResult == 0);
-	check(NewStream != nullptr);
-
-	ovrAudioAmbisonicSpeakerLayout SpeakerLayout = (Settings->SpatializationMode == EAmbisonicMode::SphericalHarmonics) ?
-		ovrAudioAmbisonicSpeakerLayout_SphericalHarmonics : ovrAudioAmbisonicSpeakerLayout_Icosahedron;
-	OurResult = OVRA_CALL(ovrAudio_SetAmbisonicSpeakerLayout)(NewStream, SpeakerLayout);
-
-	check(OurResult == 0);
-	OpenStreams.Add(StreamId, NewStream);
 }
 
 void FOculusAmbisonicsMixer::OnCloseDecodingStream(const uint32 StreamId)
