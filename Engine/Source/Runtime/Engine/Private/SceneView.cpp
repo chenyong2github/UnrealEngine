9d98e73c37c6510db3fad6f58250ef6da4e502d2
john white <John.White@epicgames.com>
2019-05-20 14:15:29 -0400

    HDR - Add HDR display modes to Windows editor when running with D3D11 RHI and on Windows 10 > 1803
    #rb ben.woodhouse
    
    #ROBOMERGE-VERSION: 349-6578295
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: john.white
    #ROBOMERGE-SOURCE: CL 6542674 via CL 6542890 via CL 6543185
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 6578746 by john white in Dev-VR branch]

--- Engine/Source/Runtime/Engine/Private/SceneView.cpp
+++ Engine/Source/Runtime/Engine/Private/SceneView.cpp
@@ -2364,6 +2364,7 @@ FSceneViewFamily::FSceneViewFamily(const ConstructionValues& CVS)
 	SceneCaptureSource(SCS_FinalColorLDR),
 	SceneCaptureCompositeMode(SCCM_Overwrite),
 	bWorldIsPaused(false),
+	bIsHDR(false),
 	GammaCorrection(CVS.GammaCorrection),
 	SecondaryViewFraction(1.0f),
 	SecondaryScreenPercentageMethod(ESecondaryScreenPercentageMethod::LowerPixelDensitySimulation),
