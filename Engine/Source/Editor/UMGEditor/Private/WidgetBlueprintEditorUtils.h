efdd2892b99b743f044ae7c29c5340394ff7f58e
nick darnell <Nick.Darnell@epicgames.com>
2019-05-20 13:54:26 -0400

    Integrating 6533688 UMG Editor: Change the copy/paste in the Hierarchy view to keep the current name of object and add _X at the end instead of generating a new name using the Widget Type everytime there is a name clash.
    #rb dave.belanger
    [FYI] vincent.gauthier
    
    #ROBOMERGE-VERSION: 348-6547088
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: nick.darnell
    #ROBOMERGE-SOURCE: CL 6537957 via CL 6538846 via CL 6539861 via CL 6539976
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR)
    
    [CL 6577802 by nick darnell in Dev-VR branch]

--- Engine/Source/Editor/UMGEditor/Private/WidgetBlueprintEditorUtils.h
+++ Engine/Source/Editor/UMGEditor/Private/WidgetBlueprintEditorUtils.h
@@ -86,4 +86,6 @@ private:
 	static void ReplaceWidgetWithChildren(TSharedRef<FWidgetBlueprintEditor> BlueprintEditor, UWidgetBlueprint* BP, FWidgetReference Widget);
 
 	static void ReplaceWidgets(TSharedRef<FWidgetBlueprintEditor> BlueprintEditor, UWidgetBlueprint* BP, TSet<FWidgetReference> Widgets, UClass* WidgetClass);
+
+	static FString FindNextValidName(UWidgetTree* WidgetTree, const FString& Name);
 };
