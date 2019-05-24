455da3ca8ba12e8d36418d5a5d27eeb61a59ad91
helge mathee <Helge.Mathee@epicgames.com>
2019-05-24 13:24:58 -0400

    Control Rig Backwards compatibility for loading old controlg rigs (prior execute context change)
    #jira UE-75047
    #rb helge.mathee
    
    Merging
    
    //Tasks/Fortnite/Dev-UEA-ControlRig/...
    
    to //Fortnite/Main/Dev-UEA-ControlRig/...
    
    
    #ROBOMERGE-SOURCE: CL 6630836 via CL 6632691
    #ROBOMERGE-BOT: (vundefined-6620334)
    
    [CL 6632821 by helge mathee in Main branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigDeveloper/Private/Graph/ControlRigGraph.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigDeveloper/Private/Graph/ControlRigGraph.cpp
@@ -61,6 +61,22 @@ void UControlRigGraph::PostLoad()
 		// perform fixes on the graph for backwards compatibility
 		if (GetLinkerCustomVersion(FControlRigObjectVersion::GUID) < FControlRigObjectVersion::RemovalOfHierarchyRefPins)
 		{
+#if WITH_EDITORONLY_DATA
+			for (UEdGraphNode* Node : Nodes)
+			{
+				UControlRigGraphNode* RigNode = Cast<UControlRigGraphNode>(Node);
+				if (RigNode != nullptr)
+				{
+					// store the nodes connected to outputs of hierarchy refs.
+					// this is done for backwards compatibility
+					if (RigNode->HasAnyFlags(RF_NeedPostLoad))
+					{
+						RigNode->CacheHierarchyRefConnectionsOnPostLoad();
+					}
+				}
+			}
+#endif
+
 			for (UEdGraphNode* Node : Nodes)
 			{
 				UControlRigGraphNode* RigNode = Cast<UControlRigGraphNode>(Node);
@@ -118,10 +134,28 @@ void UControlRigGraph::OnBlueprintCompiledPostLoad(UBlueprint* InCompiledBluepri
 {
 	if (GetLinkerCustomVersion(FControlRigObjectVersion::GUID) < FControlRigObjectVersion::RemovalOfHierarchyRefPins)
 	{
-		UBlueprint* Blueprint = Cast<UBlueprint>(GetOuter());
-		ensure(InCompiledBlueprint == Blueprint);
-		Blueprint->OnCompiled().Remove(BlueprintOnCompiledHandle);
+		UControlRigBlueprint* RigBlueprint = Cast<UControlRigBlueprint>(GetOuter());
+		ensure(InCompiledBlueprint == RigBlueprint);
+		RigBlueprint->OnCompiled().Remove(BlueprintOnCompiledHandle);
 		BlueprintOnCompiledHandle.Reset();
+
+		struct FOutStandingLink
+		{
+			FOutStandingLink() {}
+			FOutStandingLink(const FName& InA, const FName& InB, const FName& InC, const FName& InD)
+			{
+				A = InA;
+				B = InB;
+				C = InC;
+				D = InD;
+			}
+
+			FName A;
+			FName B;
+			FName C;
+			FName D;
+		};
+		TArray<FOutStandingLink> OutStandingLinks;
 
 		// create a new "begin execution" unit for each branch
 		for (UControlRigGraphNode* RigNode : FoundHierarchyRefVariableNodes)
@@ -132,53 +166,70 @@ void UControlRigGraph::OnBlueprintCompiledPostLoad(UBlueprint* InCompiledBluepri
 				int32 NodePosX = ConnectedNode->NodePosX - 200;
 				int32 NodePosY = ConnectedNode->NodePosY;
 
-				FName MemberName = FControlRigBlueprintUtils::AddUnitMember(Blueprint, FRigUnit_BeginExecution::StaticStruct());
-				if (MemberName != NAME_None)
+				if (RigBlueprint->ModelController->AddNode(FRigUnit_BeginExecution::StaticStruct()->GetFName(), FVector2D((float)NodePosX, (float)NodePosY)))
 				{
-					UControlRigGraphNode* BeginExecutionNode = FControlRigBlueprintUtils::InstantiateGraphNodeForProperty(this, MemberName, FVector2D((float)NodePosX, (float)NodePosY));
-					ensure(BeginExecutionNode);
-
-					UEdGraphPin* OutputPin = BeginExecutionNode->Pins[0];
-					for (UEdGraphPin* InputPin : ConnectedNode->Pins)
+					FName BeginExecNode = RigBlueprint->LastNameFromNotification;
+					if (BeginExecNode != NAME_None)
 					{
-						if (InputPin->Direction != EEdGraphPinDirection::EGPD_Input)
+						for (UEdGraphPin* InputPin : ConnectedNode->Pins)
 						{
-							continue;
-						}
-						if (InputPin->PinType.PinCategory != UEdGraphSchema_K2::PC_Struct)
-						{
-							continue;
-						}
-						if (InputPin->PinType.PinSubCategoryObject != FControlRigExecuteContext::StaticStruct())
-						{
-							continue;
-						}
+							if (InputPin->Direction != EEdGraphPinDirection::EGPD_Input)
+							{
+								continue;
+							}
+							if (InputPin->PinType.PinCategory != UEdGraphSchema_K2::PC_Struct)
+							{
+								continue;
+							}
+							if (InputPin->PinType.PinSubCategoryObject != FRigHierarchyRef::StaticStruct())
+							{
+								continue;
+							}
 
-						GetControlRigGraphSchema()->TryCreateConnection(OutputPin, InputPin);
+							OutStandingLinks.Add(FOutStandingLink(BeginExecNode, TEXT("ExecuteContext"), ConnectedNode->PropertyName, TEXT("ExecuteContext")));
+						}
 					}
 				}
 			}
 
-			FBlueprintEditorUtils::RemoveNode(Blueprint, RigNode, true);
+			FBlueprintEditorUtils::RemoveNode(RigBlueprint, RigNode, true);
 		}
 
 		// wire up old hierarchy ref connections to new execution connections
 		for (UControlRigGraphNode* RigNode : FoundHierarchyRefMutableNodes)
 		{
-			if (RigNode->GetExecutionVariableInfo().Num() > 0)
+			for (UEdGraphPin* OutputPin : RigNode->Pins)
 			{
-				TSharedRef<FControlRigField> RigNodeExecutionInfo = RigNode->GetExecutionVariableInfo()[0];
-				TArray<UControlRigGraphNode*>& ConnectedNodes = FoundHierarchyRefConnections.FindChecked(RigNode);
-				for (UControlRigGraphNode* ConnectedNode : ConnectedNodes)
+				if (OutputPin->Direction != EEdGraphPinDirection::EGPD_Output)
 				{
-					if (ConnectedNode->GetExecutionVariableInfo().Num() > 0)
+					continue;
+				}
+				if (OutputPin->PinType.PinCategory != UEdGraphSchema_K2::PC_Struct)
+				{
+					continue;
+				}
+				if (OutputPin->PinType.PinSubCategoryObject != FRigHierarchyRef::StaticStruct())
+				{
+					continue;
+				}
+
+				for (UEdGraphPin* InputPin : OutputPin->LinkedTo)
+				{
+					if (InputPin->Direction != EEdGraphPinDirection::EGPD_Input)
 					{
-						TSharedRef<FControlRigField> ConnectedNodeExecutionInfo = ConnectedNode->GetExecutionVariableInfo()[0];
-						if (RigNodeExecutionInfo->OutputPin && ConnectedNodeExecutionInfo->InputPin)
-						{
-							GetControlRigGraphSchema()->TryCreateConnection(RigNodeExecutionInfo->OutputPin, ConnectedNodeExecutionInfo->InputPin);
-						}
+						continue;
 					}
+					if (InputPin->PinType.PinCategory != UEdGraphSchema_K2::PC_Struct)
+					{
+						continue;
+					}
+					if (InputPin->PinType.PinSubCategoryObject != FRigHierarchyRef::StaticStruct())
+					{
+						continue;
+					}
+
+					UControlRigGraphNode* InputNode = Cast<UControlRigGraphNode>(InputPin->GetOwningNode());
+					OutStandingLinks.Add(FOutStandingLink(RigNode->PropertyName, TEXT("ExecuteContext"), InputNode->PropertyName, TEXT("ExecuteContext")));
 				}
 			}
 		}
@@ -192,7 +243,12 @@ void UControlRigGraph::OnBlueprintCompiledPostLoad(UBlueprint* InCompiledBluepri
 			}
 		}
 
-		FBlueprintEditorUtils::MarkBlueprintAsStructurallyModified(Blueprint);
+		for (const FOutStandingLink& Link : OutStandingLinks)
+		{
+			RigBlueprint->ModelController->MakeLink(Link.A, Link.B, Link.C, Link.D);
+		}
+
+		FBlueprintEditorUtils::MarkBlueprintAsStructurallyModified(RigBlueprint);
 
 		FoundHierarchyRefVariableNodes.Reset();
 		FoundHierarchyRefMutableNodes.Reset();
