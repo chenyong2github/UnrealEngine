bc0b385deefa3506ab3e6b5ba7219497d90732cd
michael daum <michael.daum@epicgames.com>
2019-05-29 16:56:12 -0400

    Fix clang build #rb none #rnx
    [CL 6668954 by michael daum in Dev-Editor branch]

--- Engine/Plugins/Experimental/ProxyLODPlugin/Source/ProxyLOD/Public/ProxyLODParameterization.h
+++ Engine/Plugins/Experimental/ProxyLODPlugin/Source/ProxyLOD/Public/ProxyLODParameterization.h
@@ -11,8 +11,7 @@ class PROXYLODMESHREDUCTION_API IPoxyLODParameterization
 {
 public:
 	static TUniquePtr<IPoxyLODParameterization> CreateTool();
-
-
+	virtual ~IPoxyLODParameterization(){}
 	virtual bool ParameterizeMeshDescription(FMeshDescription& MeshDescription, int32 Width, int32 Height, float GutterSpace, 
 		                                     float Stretch, int32 ChartNum, bool bUseNormals, bool bRecomputeTangentSpace, bool bPrintDebugMessages) const = 0;
 };
