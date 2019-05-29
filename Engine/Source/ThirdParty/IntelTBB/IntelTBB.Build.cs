ed26d8bdfaabdf82bad6cf5b551ab38f8d467b7b
Jason Bestimt <jason.bestimt@epicgames.com>
2019-05-29 14:25:01 -0400

    Fix for TBBmalloc with Clang
    #RB:none
    
    [CL 6665712 by Jason Bestimt in Dev-VR branch]

--- Engine/Source/ThirdParty/IntelTBB/IntelTBB.Build.cs
+++ Engine/Source/ThirdParty/IntelTBB/IntelTBB.Build.cs
@@ -4,27 +4,17 @@ using UnrealBuildTool;
 
 public class IntelTBB : ModuleRules
 {
-	public IntelTBB(ReadOnlyTargetRules Target) : base(Target)
-	{
-		Type = ModuleType.External;
+    public IntelTBB(ReadOnlyTargetRules Target) : base(Target)
+    {
+        Type = ModuleType.External;
 
-// @ATG_CHANGE : BEGIN HoloLens support
-		if ((Target.Platform == UnrealTargetPlatform.Win64) || 
+        if ((Target.Platform == UnrealTargetPlatform.Win64) ||
             (Target.Platform == UnrealTargetPlatform.Win32) ||
             (Target.Platform == UnrealTargetPlatform.HoloLens))
-// @ATG_CHANGE : END
-		{
-			string IntelTBBPath = Target.UEThirdPartySourceDirectory + "IntelTBB/";
-			switch (Target.WindowsPlatform.Compiler)
-			{
-                case WindowsCompiler.VisualStudio2019:
-                case WindowsCompiler.VisualStudio2017:
-				case WindowsCompiler.VisualStudio2015_DEPRECATED: IntelTBBPath += "IntelTBB-4.4u3/"; break;
-			}
+        {
+            string IntelTBBPath = Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.4u3/";
+            PublicSystemIncludePaths.Add(IntelTBBPath + "Include");
 
-			PublicSystemIncludePaths.Add(IntelTBBPath + "Include");
-
-// @ATG_CHANGE : BEGIN HoloLens support
             if (Target.WindowsPlatform.Compiler >= WindowsCompiler.VisualStudio2015_DEPRECATED)
             {
                 string PlatformSubpath = Target.WindowsPlatform.Architecture == WindowsArchitecture.ARM32 || Target.WindowsPlatform.Architecture == WindowsArchitecture.x86 ? "Win32" : "Win64";
@@ -37,34 +27,33 @@ public class IntelTBB : ModuleRules
                     PublicLibraryPaths.Add(System.String.Format("{0}lib/{1}/vc14/", IntelTBBPath, PlatformSubpath));
                 }
             }
-// @ATG_CHANGE : END
 
-			// Disable the #pragma comment(lib, ...) used by default in MallocTBB...
-			// We want to explicitly include the library.
-			PublicDefinitions.Add("__TBBMALLOC_BUILD=1");
+            // Disable the #pragma comment(lib, ...) used by default in MallocTBB...
+            // We want to explicitly include the library.
+            PublicDefinitions.Add("__TBBMALLOC_BUILD=1");
 
-			string LibName = "tbbmalloc";
-			if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT)
-			{
-				LibName += "_debug";
-			}
-			LibName += ".lib";
-			PublicAdditionalLibraries.Add(LibName);
-		}
+            string LibName = "tbbmalloc";
+            if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT)
+            {
+                LibName += "_debug";
+            }
+            LibName += ".lib";
+            PublicAdditionalLibraries.Add(LibName);
+        }
 		else if (Target.Platform == UnrealTargetPlatform.Mac)
-		{
-			PublicSystemIncludePaths.AddRange(
-				new string[] {
-					Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/include",
-				}
-			);
+        {
+            PublicSystemIncludePaths.AddRange(
+                new string[] {
+                    Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/include",
+                }
+            );
 
-			PublicAdditionalLibraries.AddRange(
-				new string[] {
-					Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/lib/Mac/libtbb.a",
-					Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/lib/Mac/libtbbmalloc.a",
-				}
-			);
-		}
-	}
+            PublicAdditionalLibraries.AddRange(
+                new string[] {
+                    Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/lib/Mac/libtbb.a",
+                    Target.UEThirdPartySourceDirectory + "IntelTBB/IntelTBB-4.0/lib/Mac/libtbbmalloc.a",
+                }
+            );
+        }
+    }
 }
