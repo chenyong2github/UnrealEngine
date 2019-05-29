1917fe7132bee364f1115479d46926906467baab
Ryan Schmidt <Ryan.Schmidt@epicgames.com>
2019-05-29 14:21:54 -0400

    copy up CL 6664465 ProxyLOD - Expose mesh UV parameterization tools for use elsewhere #rb david.hill #fyi david.hill #rnx
    [CL 6665689 by Ryan Schmidt in Dev-Editor branch]

--- Engine/Plugins/Experimental/ProxyLODPlugin/Source/ProxyLOD/Private/ProxyLODMeshParameterization.cpp
+++ Engine/Plugins/Experimental/ProxyLODPlugin/Source/ProxyLOD/Private/ProxyLODMeshParameterization.cpp
@@ -11,8 +11,31 @@ THIRD_PARTY_INCLUDES_END
 #include "ProxyLODThreadedWrappers.h"
 #include "ProxyLODMeshUtilities.h"
 
-
 bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& TextureAtlasDesc, const bool VertexColorParts)
+{
+	// desired parameters for ISO-Chart method
+
+	// MaxChartNum = 0 will allow any number of charts to be generated.
+
+	const size_t MaxChartNumber = 0;
+
+	// Let the polys in the partitions stretch some..  1.f will let it stretch freely
+
+	const float MaxStretch = 0.125f; // Question.  Does this override the pIMT?
+	
+	// Note: I tried constructing this from the normals, but that resulted in some large planar regions being really compressed in the
+	// UV chart.
+	const bool bComputeIMTFromVertexNormal = false;
+
+	// No Op
+	auto NoOpCallBack = [](float percent)->HRESULT {return  S_OK; };
+
+	return GenerateUVs(InOutMesh, TextureAtlasDesc, VertexColorParts, MaxStretch, MaxChartNumber, bComputeIMTFromVertexNormal, NoOpCallBack);
+}
+
+bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& TextureAtlasDesc, const bool VertexColorParts, 
+	                      const float MaxStretch, const size_t MaxChartNumber, const bool bComputeIMTFromVertexNormal, 
+	                      std::function<HRESULT __cdecl(float percentComplete)> StatusCallBack, float* MaxStretchOut, size_t* NumChartsOut)
 {
 
 	std::vector<uint32> DirextXAdjacency;
@@ -21,11 +44,6 @@ bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& 
 
 	if (!bValidMesh) return false;
 
-
-	// No Op
-	auto NoOpCallBack = [](float percent)->HRESULT {return  S_OK; };
-
-	// Gather data for UVAtlas Create
 
 	// Data from the existing mesh
 
@@ -39,16 +57,6 @@ bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& 
 	// The mesh adjacency
 
 	const uint32* adjacency = DirextXAdjacency.data();
-
-	// desired parameters for ISO-Chart method
-
-	// MaxChartNum = 0 will allow any number of charts to be generated.
-
-	const size_t MaxChartNumber = 0;
-
-	// Let the polys in the partitions stretch some..  1.f will let it stretch freely
-
-	const float MaxStretch = 0.125f; // Question.  Does this override the pIMT?
 
 
 	// Size of the texture atlas
@@ -66,21 +74,36 @@ bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& 
 	std::vector<uint32> facePartitioning;
 
 	// Capture stats about the result.
-	float maxStretchOut = 0.f;
-	size_t numChartsOut = 0;
+	float maxStretchUsed = 0.f;
+	size_t numChartsUsed = 0;
 
-	// per-triangle IMT from per-vertex data(normal).  Note: I tried constructing this from
-	// the normals, but that resulted in some large planar regions being really compressed in the
-	// UV chart.
+	
 
 	float * pIMTArray = new float[NumFaces * 3];
-	for (int32 f = 0; f < NumFaces; ++f)
+	if (!bComputeIMTFromVertexNormal)
 	{
-		int32 offset = 3 * f;
+		for (int32 f = 0; f < NumFaces; ++f)
 		{
-			pIMTArray[offset    ] = 1.f;
-			pIMTArray[offset + 1] = 0.f;
-			pIMTArray[offset + 2] = 1.f;
+			int32 offset = 3 * f;
+			{
+				pIMTArray[offset] = 1.f;
+				pIMTArray[offset + 1] = 0.f;
+				pIMTArray[offset + 2] = 1.f;
+			}
+		}
+	}
+	else
+	{
+		// per-triangle IMT from per-vertex data(normal). 
+
+		const TArray<FVector>& Normals = InOutMesh.Normal;
+		const float* PerVertSignal = (float*)Normals.GetData();
+		size_t SignalStride = 3 * sizeof(float);
+		HRESULT IMTResult = UVAtlasComputeIMTFromPerVertexSignal(Pos, NumVerts, indices, DXGI_FORMAT_R32_UINT, NumFaces, PerVertSignal, 3, SignalStride, StatusCallBack, pIMTArray);
+
+		if (FAILED(IMTResult))
+		{
+			return false;
 		}
 	}
 
@@ -89,10 +112,18 @@ bool ProxyLOD::GenerateUVs(FVertexDataMesh& InOutMesh, const FTextureAtlasDesc& 
 		MaxChartNumber, MaxStretch,
 		width, height, gutter,
 		adjacency, NULL /*false adj*/, pIMTArray /*IMTArray*/,
-		NoOpCallBack, DirectX::UVATLAS_DEFAULT_CALLBACK_FREQUENCY,
+		StatusCallBack, DirectX::UVATLAS_DEFAULT_CALLBACK_FREQUENCY,
 		DirectX::UVATLAS_DEFAULT, vb, ib,
-		&facePartitioning, &vertexRemapArray, &maxStretchOut, &numChartsOut);
+		&facePartitioning, &vertexRemapArray, &maxStretchUsed, &numChartsUsed);
 
+	if (MaxStretchOut)
+	{
+		*MaxStretchOut = maxStretchUsed;
+	}
+	if (NumChartsOut)
+	{
+		*NumChartsOut = numChartsUsed;
+	}
 	if (FAILED(hr)) return false;
 
 	if (pIMTArray) delete[] pIMTArray;
---
+++ Engine/Plugins/Experimental/ProxyLODPlugin/Source/ProxyLOD/Public/ProxyLODParameterization.h
@@ -0,0 +1,18 @@
+// Copyright 1998 - 2019 Epic Games, Inc.All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+struct FMeshDescription;
+
+
+class PROXYLODMESHREDUCTION_API IPoxyLODParameterization
+{
+public:
+	static TUniquePtr<IPoxyLODParameterization> CreateTool();
+
+
+	virtual bool ParameterizeMeshDescription(FMeshDescription& MeshDescription, int32 Width, int32 Height, float GutterSpace, 
+		                                     float Stretch, int32 ChartNum, bool bUseNormals, bool bRecomputeTangentSpace, bool bPrintDebugMessages) const = 0;
+};
