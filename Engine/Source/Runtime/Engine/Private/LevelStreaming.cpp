06281dac5faf2206df5a78efb0ec0f9498ffda34
marc audy <Marc.Audy@epicgames.com>
2019-05-31 16:21:18 -0400

    Store the number of loading levels on the world rather than iterating seeking out the information #jira UE-74811 #rb Michael.Noland #rnx
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 6604740 via CL 6605910 via CL 6605965
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v365-6733468)
    
    [CL 6736899 by marc audy in Dev-VR branch]

--- Engine/Source/Runtime/Engine/Private/LevelStreaming.cpp
+++ Engine/Source/Runtime/Engine/Private/LevelStreaming.cpp
@@ -1001,6 +1001,7 @@ bool ULevelStreaming::RequestLevel(UWorld* PersistentWorld, bool bAllowLevelLoad
 		if (FPackageName::DoesPackageExist(PackageNameToLoadFrom))
 		{
 			CurrentState = ECurrentState::Loading;
+			FWorldNotifyStreamingLevelLoading::Started(PersistentWorld);
 			
 			ULevel::StreamedLevelsOwningWorld.Add(DesiredPackageName, PersistentWorld);
 			UWorld::WorldTypePreLoadMap.FindOrAdd(DesiredPackageName) = PersistentWorld->WorldType;
@@ -1036,6 +1037,13 @@ bool ULevelStreaming::RequestLevel(UWorld* PersistentWorld, bool bAllowLevelLoad
 void ULevelStreaming::AsyncLevelLoadComplete(const FName& InPackageName, UPackage* InLoadedPackage, EAsyncLoadingResult::Type Result)
 {
 	CurrentState = ECurrentState::LoadedNotVisible;
+	if (UWorld* World = GetWorld())
+	{
+		if (World->GetStreamingLevels().Contains(this))
+		{
+			FWorldNotifyStreamingLevelLoading::Finished(World);
+		}
+	}
 
 	if (InLoadedPackage)
 	{
