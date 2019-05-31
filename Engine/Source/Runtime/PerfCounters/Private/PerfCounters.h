1070063d295d433946888548f1f89bae81f4a5cc
ryan gerleve <Ryan.Gerleve@epicgames.com>
2019-05-31 17:22:26 -0400

    [PerfCounters] PerfCounters HTTP interface leverages HTTPServer module + Internal HTTP bind/listen/read/write functionality has been replaced with HTTPServer module usage + All endpoints/functionality are identical. + Minor structural change: PerfCountersModule now stores a TSharedPtr to FPerfCounters rather than a raw pointer.  This change has been made to facilitate usage of AsShared() within FPerfCounters to facilitate safe and non-intrusive captures.
    #rb David.Nikdel
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: mitchell.fisher
    #ROBOMERGE-SOURCE: CL 6597353 via CL 6601388 via CL 6601461
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v365-6733468)
    
    [CL 6742223 by ryan gerleve in Dev-Networking branch]

--- Engine/Source/Runtime/PerfCounters/Private/PerfCounters.h
+++ Engine/Source/Runtime/PerfCounters/Private/PerfCounters.h
@@ -8,8 +8,12 @@
 #include "Containers/Ticker.h"
 #include "ProfilingDebugging/Histogram.h"
 
-class ISocketSubsystem;
-class FSocket;
+#include "HttpResultCallback.h"
+#include "HttpPath.h"
+#include "HttpRouteHandle.h"
+
+class IHttpRouter;
+struct FHttpServerRequest;
 class FZeroLoad;
 class FInternetAddr;
 
@@ -19,6 +23,7 @@ class FPerfCounters
 	: public FTickerObjectBase
 	, public FSelfRegisteringExec
 	, public IPerfCounters
+	, public TSharedFromThis<FPerfCounters>
 {
 public:
 
@@ -57,71 +62,25 @@ public:
 private:
 	
 	void TickZeroLoad(float DeltaTime);
-	void TickSocket(float DeltaTime);
 	void TickSystemCounters(float DeltaTime);
 
 	/**
-	 * Simple response structure for returning output to requestor
+	 * Processes a /stats request
+	 *
+	 * @param Request The incoming request
+	 * @param OnComplete The invokable response result callback
+	 * @return true if this request was handled herein, false otherwise
 	 */
-	struct FResponse
-	{
-		/** http header */
-		FString Header;
-		/** http body */
-		FString Body;
-		/** http response code */
-		int32 Code;
-
-		FResponse() :
-			Code(0)
-		{}
-	};
-	
-	/**
-	 * Simple connection structure for keeping track of incoming/active connections
-	 */
-	struct FPerfConnection
-	{
-		/** accepted external socket */
-		FSocket* Connection;
-		/** time connection has existed */
-		float ElapsedTime;
-
-		FPerfConnection() :
-			Connection(nullptr),
-			ElapsedTime(0.0f)
-		{}
-
-		FPerfConnection(FSocket* InConnection) :
-			Connection(InConnection),
-			ElapsedTime(0.0f)
-		{}
-
-		bool operator==(const FPerfConnection& Other) const
-		{
-			return Connection == Other.Connection;
-		}
-	};
-
-	/** Reference to the socket subsystem in use */
-	ISocketSubsystem* SocketSubsystem;
-
-	/** Scratch IP address to save on memory allocations */
-	TSharedPtr<FInternetAddr> ScratchIPAddr;
-
-	/** all active connections */
-	TArray<FPerfConnection> Connections;
+	bool ProcessStatsRequest(const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete);
 
 	/**
-	 * Process the incoming request from an active socket
+	 * Process an /exec request
 	 *
-	 * @param Buffer data sent from the requestor
-	 * @param BufferLen size of the data sent
-	 * @param Response [out] response to be given to the requestor
-	 *
-	 * @return true if the response is valid, false otherwise
+	 * @param Request The incoming request
+	 * @param OnComplete The invokable response result callback
+	 * @return true if this request was handled herein, false otherwise
 	 */
-	bool ProcessRequest(uint8* Buffer, int32 BufferLen, FResponse& Response);
+	bool ProcessExecRequest(const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete);
 
 	/** Unique name of this instance */
 	FString UniqueInstanceId;
@@ -138,9 +97,6 @@ private:
 	/** Bound callback for script command execution */
 	FPerfCounterExecCommandCallback ExecCmdCallback;
 
-	/* Listen socket for outputting JSON on request */
-	FSocket* Socket;
-
 	/** Map of performance histograms. */
 	TPerformanceHistogramMap PerformanceHistogramMap;
 
@@ -149,4 +105,13 @@ private:
 
 	/** Zero-load thread runnable */
 	FRunnableThread* ZeroLoadRunnable;
+
+	/** Http request/response interface */
+	TSharedPtr<IHttpRouter> HttpRouter = nullptr;
+
+	/** Route handle for /stats binding */
+	FHttpRouteHandle StatsRouteHandle = nullptr;
+
+	/** Route handle for /exec binding */
+	FHttpRouteHandle ExecRouteHandle = nullptr;
 };
