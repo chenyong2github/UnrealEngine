0185cb0136bcff634a9890052603937ec3c1b9b3
helge mathee <Helge.Mathee@epicgames.com>
2019-05-31 14:34:51 -0400

    Kismet: Move NodeSectionID enum to BlueprintEditor.h
    Merging
    
    //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Source/Editor/Kismet/...
    
    to //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Source/Editor/Kismet/...
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 6604509 via CL 6605541 via CL 6605587
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim) (v364-6709755)
    
    [CL 6718909 by helge mathee in Dev-Anim branch]

--- Engine/Source/Editor/Kismet/Private/SMyBlueprint.h
+++ Engine/Source/Editor/Kismet/Private/SMyBlueprint.h
@@ -26,27 +26,6 @@ struct FEdGraphSchemaAction_K2Struct;
 struct FGraphActionNode;
 struct FGraphActionSort;
 
-/* Enums to use when grouping the blueprint members in the list panel. The order here will determine the order in the list */
-namespace NodeSectionID
-{
-	enum Type
-	{
-		NONE = 0,
-		GRAPH,					// Graph
-		FUNCTION,				// Functions
-		FUNCTION_OVERRIDABLE,	// Overridable functions
-		INTERFACE,				// Interface
-		MACRO,					// Macros
-		VARIABLE,				// Variables
-		COMPONENT,				// Components
-		DELEGATE,				// Delegate/Event
-		USER_ENUM,				// User defined enums
-		LOCAL_VARIABLE,			// Local variables
-		USER_STRUCT,			// User defined structs
-		USER_SORTED				// User sorted categories
-	};
-};
-
 class FMyBlueprintCommands : public TCommands<FMyBlueprintCommands>
 {
 public:
