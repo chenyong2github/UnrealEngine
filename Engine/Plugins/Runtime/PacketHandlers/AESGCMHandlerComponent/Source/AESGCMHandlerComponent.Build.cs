ab30ac3391e4a80fe15e8810231a8f19f44bba8b
Brian Bekich <Brian.Bekich@epicgames.com>
2019-05-31 15:57:18 -0400

    Adding experimental AES 256 GCM packet handler
    #jira UENET-913
    #rb ryan.gerleve
    
    [CL 6732538 by Brian Bekich in Dev-Networking branch]

---
+++ Engine/Plugins/Runtime/PacketHandlers/AESGCMHandlerComponent/Source/AESGCMHandlerComponent.Build.cs
@@ -0,0 +1,59 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class AESGCMHandlerComponent : ModuleRules
+{
+    public AESGCMHandlerComponent(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"PacketHandler",
+				"PlatformCrypto",
+			}
+			);
+
+		PublicIncludePathModuleNames.AddRange(
+			new string[]
+			{
+				"PlatformCrypto"
+			}
+			);
+
+		if (Target.Platform == UnrealTargetPlatform.XboxOne)
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"PlatformCryptoBCrypt",
+				}
+				);
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Switch)
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"PlatformCryptoSwitch",
+				}
+				);
+		}
+		else
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"PlatformCryptoOpenSSL",
+				}
+				);
+		}
+	}
+}
