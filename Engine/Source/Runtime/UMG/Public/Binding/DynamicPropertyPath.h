063b303010e192360e1e085bba3b03a07f31c147
helge mathee <Helge.Mathee@epicgames.com>
2019-05-31 17:48:48 -0400

    Re-enable temporarily deprecated property path constructor.
    #jira UE-74819
    [FYI] nick.darnell
    
    Merging
    
    //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Source/...
    
    to //Fortnite/Main/Engine/Source/...
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 6600780 via CL 6601960 via CL 6602069
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v365-6733468)
    
    [CL 6746503 by helge mathee in Dev-Networking branch]

--- Engine/Source/Runtime/UMG/Public/Binding/DynamicPropertyPath.h
+++ Engine/Source/Runtime/UMG/Public/Binding/DynamicPropertyPath.h
@@ -22,7 +22,6 @@ public:
 	FDynamicPropertyPath(const FString& Path);
 
 	/** */
-	UE_DEPRECATED(4.23, "FDynamicPropertyPath(const FString& Path)")
 	FDynamicPropertyPath(const TArray<FString>& PropertyChain);
 
 	/** Get the value represented by this property path */
