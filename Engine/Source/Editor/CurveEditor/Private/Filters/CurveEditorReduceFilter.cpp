2de74779e37cba5c5af0021797d42cbb35c67d1d
max chen <Max.Chen@epicgames.com>
2019-05-31 19:08:55 -0400

    Curve Editor: Update copyright notices
    #rb none
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: max.chen
    #ROBOMERGE-SOURCE: CL 6644152 via CL 6644170 via CL 6644179
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim) (v365-6733468)
    
    [CL 6758514 by max chen in Dev-Anim branch]

--- Engine/Source/Editor/CurveEditor/Private/Filters/CurveEditorReduceFilter.cpp
+++ Engine/Source/Editor/CurveEditor/Private/Filters/CurveEditorReduceFilter.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "Filters/CurveEditorReduceFilter.h"
 #include "CurveEditor.h"
