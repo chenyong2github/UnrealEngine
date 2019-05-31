d31f0ecb5077c85c7f9eac748629cc3062aee256
gil gribb <gil.gribb@epicgames.com>
2019-05-31 13:35:16 -0400

    UE4 - Reworked virtual memory API to support page caches and heterogeneous memory architectures. Switch is the only platform with substantive changes, the rest are just API upgrades. #rb none #jira none
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: gil.gribb
    #ROBOMERGE-SOURCE: CL 6663163 via CL 6664245 via CL 6664629
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build) (v363-6677109)
    
    [CL 6706188 by gil gribb in Dev-Build branch]

--- Engine/Source/Runtime/Core/Public/Android/AndroidPlatformMemory.h
+++ Engine/Source/Runtime/Core/Public/Android/AndroidPlatformMemory.h
@@ -26,10 +26,57 @@ struct CORE_API FAndroidPlatformMemory : public FGenericPlatformMemory
 	static const FPlatformMemoryConstants& GetConstants();
 	static EPlatformMemorySizeBucket GetMemorySizeBucket();
 	static FMalloc* BaseAllocator();
-	static void* BinnedAllocFromOS( SIZE_T Size );
-	static void BinnedFreeToOS( void* Ptr, SIZE_T Size );
-	static bool MemoryRangeDecommit(void* Ptr, SIZE_T Size);
+	static void* BinnedAllocFromOS(SIZE_T Size);
+	static void BinnedFreeToOS(void* Ptr, SIZE_T Size);
 
+	class FPlatformVirtualMemoryBlock : public FBasicVirtualMemoryBlock
+	{
+	public:
+
+		FPlatformVirtualMemoryBlock()
+		{
+		}
+
+		FPlatformVirtualMemoryBlock(void *InPtr, uint32 InVMSizeDivVirtualSizeAlignment)
+			: FBasicVirtualMemoryBlock(InPtr, InVMSizeDivVirtualSizeAlignment)
+		{
+		}
+		FPlatformVirtualMemoryBlock(const FPlatformVirtualMemoryBlock& Other) = default;
+		FPlatformVirtualMemoryBlock& operator=(const FPlatformVirtualMemoryBlock& Other) = default;
+
+		void Commit(size_t InOffset, size_t InSize);
+		void Decommit(size_t InOffset, size_t InSize);
+		void FreeVirtual();
+
+		FORCEINLINE void CommitByPtr(void *InPtr, size_t InSize)
+		{
+			Commit(size_t(((uint8*)InPtr) - ((uint8*)Ptr)), InSize);
+		}
+
+		FORCEINLINE void DecommitByPtr(void *InPtr, size_t InSize)
+		{
+			Decommit(size_t(((uint8*)InPtr) - ((uint8*)Ptr)), InSize);
+		}
+
+		FORCEINLINE void Commit()
+		{
+			Commit(0, GetActualSize());
+		}
+
+		FORCEINLINE void Decommit()
+		{
+			Decommit(0, GetActualSize());
+		}
+
+		FORCEINLINE size_t GetActualSize() const
+		{
+			return VMSizeDivVirtualSizeAlignment * GetVirtualSizeAlignment();
+		}
+
+		static FPlatformVirtualMemoryBlock AllocateVirtual(size_t Size, size_t InAlignment = FPlatformVirtualMemoryBlock::GetVirtualSizeAlignment());
+		static size_t GetCommitAlignment();
+		static size_t GetVirtualSizeAlignment();
+	};
 
 	static bool GetLLMAllocFunctions(void*(*&OutAllocFunction)(size_t), void(*&OutFreeFunction)(void*, size_t), int32& OutAlignment);
 	//~ End FGenericPlatformMemory Interface
