7bc7f8bb23d47020b5bfbb033db6d5ba91dfb1e2
CarlMagnus Nordin <CarlMagnus.Nordin@epicgames.com>
2019-06-03 03:31:43 -0400

    Fixed incorrect define for enabling tracing of log messages #rb none
    [CL 6781675 by CarlMagnus Nordin in Dev-Core branch]

--- Engine/Source/Runtime/Core/Public/Logging/LogTrace.h
+++ Engine/Source/Runtime/Core/Public/Logging/LogTrace.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreTypes.h"
+#include "Trace/Trace.h"
 
-#if !IS_PROGRAM && !UE_BUILD_SHIPPING
+#if UE_TRACE_ENABLED && !UE_BUILD_SHIPPING
 #define LOGTRACE_ENABLED 1
 #else
 #define LOGTRACE_ENABLED 0
