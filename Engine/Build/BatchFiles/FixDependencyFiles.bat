31de091d17244cdbb89c762b0f23ea218b516eab
Joakim Lindqvist <Joakim.Lindqvist@epicgames.com>
2019-06-03 09:03:05 -0400

    Fixed issue in batch scripts were directory to batch script was not quoted and as such would cause issue when special characters was part of the path.
    #jira UE-71740
    #fyi ben.marsh
    
    [CL 6795995 by Joakim Lindqvist in Dev-Build branch]

--- Engine/Build/BatchFiles/FixDependencyFiles.bat
+++ Engine/Build/BatchFiles/FixDependencyFiles.bat
@@ -1,5 +1,5 @@
 @echo off
-pushd %~dp0..\..\
+pushd "%~dp0..\..\"
 for /f "tokens=*" %%X in ('dir /b /s /a:-d Content\Editor\Slate\*.PNG') do ren "%%X" "%%~nX.png"
 for /f "tokens=*" %%X in ('dir /b /s /a:-d Content\Slate\*.PNG') do ren "%%X" "%%~nX.png"
 
