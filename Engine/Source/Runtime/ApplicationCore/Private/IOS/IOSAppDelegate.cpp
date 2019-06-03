e7cc108f2bfd4c304f050c7de246bb159ab10f9a
josh adams <josh.adams@epicgames.com>
2019-06-03 11:05:12 -0400

    - Enabled going in and out of high quality voice chat mode (echo cancellation, etc, but makes game audio quiet, so it's an optional thing) #rb michael.kirzinger [FYI] michael.kirzinger
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: josh.adams
    #ROBOMERGE-SOURCE: CL 6670165 via CL 6670167 via CL 6670237 via CL 6670444 via CL 6672048 via CL 6673780
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v365-6733468)
    
    [CL 6800655 by josh adams in Dev-Networking branch]

--- Engine/Source/Runtime/ApplicationCore/Private/IOS/IOSAppDelegate.cpp
+++ Engine/Source/Runtime/ApplicationCore/Private/IOS/IOSAppDelegate.cpp
@@ -524,7 +524,7 @@ static IOSAppDelegate* CachedDelegate = nil;
 					
 					if ([self IsFeatureActive:EAudioFeature::VoiceChat])
 					{
-						Mode = AVAudioSessionModeVoiceChat;
+						Mode = self.bHighQualityVoiceChatEnabled ? AVAudioSessionModeVoiceChat : AVAudioSessionModeDefault;
 #if !PLATFORM_TVOS
 						// allow bluetooth for chatting and such
 						Options |= AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionAllowBluetoothA2DP;
@@ -604,7 +604,8 @@ static IOSAppDelegate* CachedDelegate = nil;
 						
 						if (@available(iOS 10, *))
 						{
-							[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:AVAudioSessionModeDefault options:opts error:&ActiveError];
+							AVAudioSessionMode VoiceChatMode = self.bHighQualityVoiceChatEnabled ? AVAudioSessionModeVoiceChat : AVAudioSessionModeDefault;
+							[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:VoiceChatMode options:opts error:&ActiveError];
 						}
 						else
 						{
@@ -677,7 +678,8 @@ static IOSAppDelegate* CachedDelegate = nil;
 					
 					if (@available(iOS 10, *))
 					{
-						[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:AVAudioSessionModeDefault options:opts error:&ActiveError];
+						AVAudioSessionMode VoiceChatMode = self.bHighQualityVoiceChatEnabled ? AVAudioSessionModeVoiceChat : AVAudioSessionModeDefault;
+						[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:VoiceChatMode options:opts error:&ActiveError];
 					}
 					else
 					{
@@ -707,7 +709,8 @@ static IOSAppDelegate* CachedDelegate = nil;
 			// Necessary for voice chat if audio is not active
 			if (@available(iOS 10, *))
 			{
-				[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:AVAudioSessionModeDefault options:opts error:&ActiveError];
+				AVAudioSessionMode VoiceChatMode = self.bHighQualityVoiceChatEnabled ? AVAudioSessionModeVoiceChat : AVAudioSessionModeDefault;
+				[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord mode:VoiceChatMode options:opts error:&ActiveError];
 			}
 			else
 			{
@@ -737,6 +740,11 @@ static IOSAppDelegate* CachedDelegate = nil;
 {
 	AVAudioSession* Session = [AVAudioSession sharedInstance];
 	return Session.otherAudioPlaying;
+}
+
+-(void)EnableHighQualityVoiceChat:(bool)bEnable
+{
+	self.bHighQualityVoiceChatEnabled = bEnable;
 }
 
 - (void)EnableVoiceChat:(bool)bEnable
