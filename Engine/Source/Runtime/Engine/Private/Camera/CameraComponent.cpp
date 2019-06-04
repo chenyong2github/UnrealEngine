f906c9edec27d072b81da7949381965b05635fa0
Ben Hoffman <Ben.Hoffman@epicgames.com>
2019-06-04 16:15:30 -0400

    Update UCameraComponent to have an editable Camera mesh that can be marked as visible at runtime.
    #jira UE-16011
    
    #RB Fred Kimberley
    
    [CL 6839518 by Ben Hoffman in Dev-Framework branch]

--- Engine/Source/Runtime/Engine/Private/Camera/CameraComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Camera/CameraComponent.cpp
@@ -37,6 +37,7 @@ UCameraComponent::UCameraComponent(const FObjectInitializer& ObjectInitializer)
 	}
 
 	bUseControllerViewRotation_DEPRECATED = true; // the previous default value before bUsePawnControlRotation replaced this var.
+	bCameraMeshHiddenInGame = true;
 #endif
 
 	FieldOfView = 90.0f;
@@ -62,6 +63,18 @@ void UCameraComponent::OnUpdateTransform(EUpdateTransformFlags UpdateTransformFl
 }
 
 #if WITH_EDITORONLY_DATA
+void UCameraComponent::PostEditChangeProperty(FPropertyChangedEvent & PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName PropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : FName();
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(UCameraComponent, bCameraMeshHiddenInGame))
+	{
+		OnCameraMeshHiddenChanged();
+	}
+
+	RefreshVisualRepresentation();
+}
 
 void UCameraComponent::AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector)
 {
@@ -85,7 +98,7 @@ void UCameraComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
 		DrawFrustum->DestroyComponent();
 	}
 }
-#endif
+#endif	// WITH_EDITORONLY_DATA
 
 #if WITH_EDITOR
 FText UCameraComponent::GetFilmbackText() const
@@ -106,7 +119,7 @@ void UCameraComponent::OnRegister()
 			ProxyMeshComponent->SetIsVisualizationComponent(true);
 			ProxyMeshComponent->SetStaticMesh(CameraMesh);
 			ProxyMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
-			ProxyMeshComponent->bHiddenInGame = true;
+			ProxyMeshComponent->bHiddenInGame = bCameraMeshHiddenInGame;
 			ProxyMeshComponent->CastShadow = false;
 			ProxyMeshComponent->CreationMethod = CreationMethod;
 			ProxyMeshComponent->RegisterComponentWithWorld(GetWorld());
@@ -123,7 +136,7 @@ void UCameraComponent::OnRegister()
 	}
 
 	RefreshVisualRepresentation();
-#endif
+#endif	// WITH_EDITORONLY_DATA
 
 	Super::OnRegister();
 }
@@ -185,6 +198,12 @@ void UCameraComponent::RefreshVisualRepresentation()
 		DrawFrustum->MarkRenderStateDirty();
 	}
 
+	// Update the proxy camera mesh if necessary
+	if (ProxyMeshComponent && ProxyMeshComponent->GetStaticMesh() != CameraMesh)
+	{
+		ProxyMeshComponent->SetStaticMesh(CameraMesh);
+	}
+
 	ResetProxyMeshTransform();
 }
 
@@ -222,15 +241,6 @@ void UCameraComponent::RestoreFrustumColor()
 		//Cam->DrawFrustum->FrustumColor = DefCam->DrawFrustum->FrustumColor;
 	}
 }
-#endif	// WITH_EDITORONLY_DATA
-
-#if WITH_EDITORONLY_DATA
-void UCameraComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-
-	RefreshVisualRepresentation();
-}
 
 void UCameraComponent::Serialize(FArchive& Ar)
 {
@@ -242,6 +252,16 @@ void UCameraComponent::Serialize(FArchive& Ar)
 	}
 }
 #endif	// WITH_EDITORONLY_DATA
+
+void UCameraComponent::OnCameraMeshHiddenChanged()
+{
+#if WITH_EDITORONLY_DATA
+	if (ProxyMeshComponent)
+	{
+		ProxyMeshComponent->bHiddenInGame = bCameraMeshHiddenInGame;
+	}
+#endif
+}
 
 void UCameraComponent::GetCameraView(float DeltaTime, FMinimalViewInfo& DesiredView)
 {
