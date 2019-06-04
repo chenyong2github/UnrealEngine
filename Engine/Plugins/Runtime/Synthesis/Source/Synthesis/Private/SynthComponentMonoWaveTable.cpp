51f422866c599138419352b44e2f0229f7c1babe
Rob Gay <Rob.Gay@epicgames.com>
2019-06-04 14:59:44 -0400

    Fix copyright misspelling #rb none #jira UE-75502
    [CL 6837701 by Rob Gay in Main branch]

--- Engine/Plugins/Runtime/Synthesis/Source/Synthesis/Private/SynthComponentMonoWaveTable.cpp
+++ Engine/Plugins/Runtime/Synthesis/Source/Synthesis/Private/SynthComponentMonoWaveTable.cpp
@@ -1,4 +1,4 @@
-// Copyrigxht 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 
 #include "SynthComponents/SynthComponentMonoWaveTable.h"
