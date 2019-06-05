687301a9ce2acfa4d4eaff4a76c016aa43b0ee1f
Jeremy Moore <Jeremy.Moore@epicgames.com>
2019-06-05 09:34:09 -0400

    #jira UE-75130 Landscape component recreates material instances when material is destroyed by delete asset dialog #rb none
    [CL 6847972 by Jeremy Moore in Dev-Editor branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeEdit.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeEdit.cpp
@@ -431,7 +431,11 @@ void ULandscapeComponent::UpdateMaterialInstances_Internal(FMaterialUpdateContex
 
 			UMaterialInstanceConstant* MobileCombinationMaterialInstance = GetCombinationMaterial(&Context, MobileWeightmapLayerAllocations, MaterialLOD, true);
 			MobileCombinationMaterialInstances[MobileMaterialIndex] = MobileCombinationMaterialInstance;
-			Context.AddMaterialInstance(MobileCombinationMaterialInstance);
+
+			if (MobileCombinationMaterialInstance != nullptr)
+			{
+				Context.AddMaterialInstance(MobileCombinationMaterialInstance);
+			}
 						
 			++MobileMaterialIndex;
 		}
@@ -4497,7 +4501,7 @@ void ULandscapeComponent::PostEditChangeProperty(FPropertyChangedEvent& Property
 	const FName PropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : NAME_None;
 	const FName MemberPropertyName = PropertyChangedEvent.MemberProperty ? PropertyChangedEvent.MemberProperty->GetFName() : NAME_None;
 
-	if (PropertyName == FName(TEXT("OverrideMaterial")) || MemberPropertyName == FName(TEXT("OverrideMaterials")))
+	if (PropertyName == FName(TEXT("OverrideMaterial")) || MemberPropertyName == FName(TEXT("OverrideMaterials")) || MemberPropertyName == FName(TEXT("MaterialPerLOD_Key")))
 	{
 		bool RecreateMaterialInstances = true;
 
