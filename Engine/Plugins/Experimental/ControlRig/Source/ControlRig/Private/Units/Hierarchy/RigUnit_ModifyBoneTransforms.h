c68e99131f350371afd47eca9496dfaf6fcdebe1
helge mathee <Helge.Mathee@epicgames.com>
2019-06-11 10:41:36 -0400

    #jira UE-75161
    ControlRig: Added modify transforms node.
    
    #rb lina.halper
    
    Merging
    
    //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Plugins/Experimental/ControlRig/Source/...
    
    to //Tasks/Fortnite/Dev-UEA-ControlRig/Engine/Plugins/Experimental/ControlRig/Source/...
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 6663338 via CL 6664264 via CL 6664727
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v366-6836689)
    
    [CL 6930617 by helge mathee in Dev-VR branch]

---
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/Units/Hierarchy/RigUnit_ModifyBoneTransforms.h
@@ -0,0 +1,108 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Units/RigUnit.h"
+#include "RigUnit_ModifyBoneTransforms.generated.h"
+
+UENUM()
+enum class EControlRigModifyBoneMode : uint8
+{
+	/** Override existing local transform */
+	OverrideLocal,
+
+	/** Override existing local transform */
+	OverrideGlobal,
+
+	/** 
+	 * Additive to existing local transform.
+	 * Input transform is added within the bone's space.
+	 */
+	AdditiveLocal,
+
+	/**
+     * Additive to existing global transform.
+     * Input transform is added as a global offset in the root of the hierarchy.
+	 */
+	AdditiveGlobal,
+
+	/** MAX - invalid */
+	Max UMETA(Hidden),
+};
+
+USTRUCT()
+struct FRigUnit_ModifyBoneTransforms_PerBone
+{
+	GENERATED_BODY()
+
+	FRigUnit_ModifyBoneTransforms_PerBone()
+		: Bone(NAME_None)
+	{
+	}
+
+	/**
+	 * The name of the Bone to set the transform for.
+	 */
+	UPROPERTY(meta = (Input, BoneName, Constant))
+	FName Bone;
+
+	/**
+	 * The transform value to set for the given Bone.
+	 */
+	UPROPERTY(meta = (Input))
+	FTransform Transform;
+};
+
+
+/**
+ * SetBoneTransform is used to perform a change in the hierarchy by setting a single bone's transform.
+ */
+USTRUCT(meta=(DisplayName="Modify Transforms", Category="Hierarchy", DocumentationPolicy="Strict", Keywords = "ModifyBone"))
+struct FRigUnit_ModifyBoneTransforms : public FRigUnitMutable
+{
+	GENERATED_BODY()
+
+	FRigUnit_ModifyBoneTransforms()
+		: Weight(1.f)
+		, WeightMinimum(0.f)
+		, WeightMaximum(1.f)
+		, Mode(EControlRigModifyBoneMode::AdditiveLocal)
+	{}
+
+	virtual void Execute(const FRigUnitContext& Context) override;
+
+	/**
+	 * The bones to modify.
+	 */
+	UPROPERTY(meta = (Input, ExpandByDefault, DefaultArraySize=1))
+	TArray<FRigUnit_ModifyBoneTransforms_PerBone> BoneToModify;
+
+	/**
+	 * At 1 this sets the transform, between 0 and 1 the transform is blended with previous results.
+	 */
+	UPROPERTY(meta = (Input))
+	float Weight;
+
+	/**
+	 * The minimum of the weight - defaults to 0.0
+	 */
+	UPROPERTY(meta = (Input, Constant))
+	float WeightMinimum;
+
+	/**
+	 * The maximum of the weight - defaults to 1.0
+	 */
+	UPROPERTY(meta = (Input, Constant))
+	float WeightMaximum;
+
+	/**
+	 * Defines if the bone's transform should be set
+	 * in local or global space, additive or override.
+	 */
+	UPROPERTY(meta = (Input, Constant))
+	EControlRigModifyBoneMode Mode;
+
+	// Used to cache the internally used bone index
+	UPROPERTY()
+	TArray<int32> CachedBoneIndices;
+};
