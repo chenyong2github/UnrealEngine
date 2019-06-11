c9e656f8881a9ac4069db06bf6f5aceeaa880476
Jonathan Koelzer <Jonathan.Koelzer@epicgames.com>
2019-06-11 12:05:41 -0400

    Removed unused Engine dependency from MeshConversion module #RB David.Hill
    [CL 6936969 by Jonathan Koelzer in Dev-Editor branch]

--- Engine/Plugins/Experimental/GeometryProcessing/Source/MeshConversion/MeshConversion.Build.cs
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/MeshConversion/MeshConversion.Build.cs
@@ -20,8 +20,7 @@ public class MeshConversion : ModuleRules
         PrivateDependencyModuleNames.AddRange(
             new string[]
             {
-                "CoreUObject",
-                "Engine"
+                "CoreUObject"
 			}
 		);
     }
