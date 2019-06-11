6cad3f3dc71b33ce528ed63618bd3fb2f7b711ff
PJ Kack <PJ.Kack@epicgames.com>
2019-06-11 13:18:01 -0400

    Optimize UAssetManager::OnObjectPreSave by reducing temporary string allocations.
    #rb robert.manuszewski
    
    [CL 6939084 by PJ Kack in Dev-Core branch]

--- Engine/Source/Runtime/Engine/Private/AssetManager.cpp
+++ Engine/Source/Runtime/Engine/Private/AssetManager.cpp
@@ -3507,9 +3507,14 @@ void UAssetManager::OnInMemoryAssetDeleted(UObject *Object)
 void UAssetManager::OnObjectPreSave(UObject* Object)
 {
 	// If this is in the asset manager dictionary, make sure it actually has a primary asset id that matches
-	FPrimaryAssetId FoundPrimaryAssetId = GetPrimaryAssetIdForPath(FSoftObjectPath(Object));
+	const bool bIsAssetOrClass = Object->IsAsset() || Object->IsA(UClass::StaticClass()); 
+	if (!bIsAssetOrClass)
+	{
+		return;
+	}
 
-	if ((Object->IsAsset() || Object->IsA(UClass::StaticClass())) && FoundPrimaryAssetId.IsValid())
+	FPrimaryAssetId FoundPrimaryAssetId = GetPrimaryAssetIdForPath(*Object->GetPathName());
+	if (FoundPrimaryAssetId.IsValid())
 	{
 		TSharedRef<FPrimaryAssetTypeData>* FoundType = AssetTypeMap.Find(FoundPrimaryAssetId.PrimaryAssetType);
 		FPrimaryAssetId ObjectPrimaryAssetId = Object->GetPrimaryAssetId();
