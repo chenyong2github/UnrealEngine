fae021127ce4a56c5f6bf452e5239894f742ab53
richard malo <Richard.Malo@epicgames.com>
2019-06-11 21:17:09 -0400

    - Moved ALandscapeBlueprintCustomBrush in an editor module (also renamed as ALandscapeBlueprintBrush). - Added runtime base class ALandscapeBlueprintBrushBase. This was done so that landscape blueprints brushes can access editor functions. #rb patrick.enfedaque #rnx
    #ROBOMERGE-OWNER: richard.malo
    #ROBOMERGE-AUTHOR: richard.malo
    #ROBOMERGE-SOURCE: CL 6930726 via CL 6942986
    #ROBOMERGE-BOT: (v366-6836689)
    
    [CL 6946114 by richard malo in Main branch]

---
+++ Engine/Source/Editor/LandscapeEditor/Public/LandscapeBlueprintBrush.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "GameFramework/Actor.h"
+#include "LandscapeBlueprintBrushBase.h"
+
+#include "LandscapeBlueprintBrush.generated.h"
+
+UCLASS(Abstract, Blueprintable, hidecategories = (Replication, Input, LOD, Actor, Cooking, Rendering))
+class ALandscapeBlueprintBrush : public ALandscapeBlueprintBrushBase
+{
+	GENERATED_UCLASS_BODY()
+};
+
--- Engine/Source/Runtime/Landscape/Public/LandscapeBlueprintBrushBase.h
+++ Engine/Source/Runtime/Landscape/Public/LandscapeBlueprintBrushBase.h
@@ -6,68 +6,58 @@
 #include "UObject/ObjectMacros.h"
 #include "GameFramework/Actor.h"
 
-#include "LandscapeBPCustomBrush.generated.h"
+#include "LandscapeBlueprintBrushBase.generated.h"
 
-UCLASS(Abstract, Blueprintable, hidecategories = (Replication, Input, LOD, Actor, Cooking, Rendering))
-class LANDSCAPE_API ALandscapeBlueprintCustomBrush : public AActor
+UCLASS(Abstract, NotBlueprintable)
+class LANDSCAPE_API ALandscapeBlueprintBrushBase : public AActor
 {
 	GENERATED_UCLASS_BODY()
 
-private:
-	UPROPERTY(Category= "Settings", EditAnywhere)
+protected:
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(DuplicateTransient)
+	class ALandscape* OwningLandscape;
+
+	UPROPERTY(Category = "Settings", EditAnywhere)
 	bool AffectHeightmap;
 
-	UPROPERTY(Category= "Settings", EditAnywhere)
+	UPROPERTY(Category = "Settings", EditAnywhere)
 	bool AffectWeightmap;
 
 	UPROPERTY(Category = "Settings", EditAnywhere)
 	TArray<FName> AffectedWeightmapLayers;
 
-#if WITH_EDITORONLY_DATA
-	UPROPERTY(DuplicateTransient)
-	class ALandscape* OwningLandscape;
-
-	UPROPERTY(NonTransactional, DuplicateTransient)
-	bool bIsCommited;
-
 	UPROPERTY(Transient)
 	bool bIsVisible;
 #endif
+
 public:
-
-	virtual bool ShouldTickIfViewportsOnly() const override;
-	virtual void Tick(float DeltaSeconds) override;
-
-	bool IsAffectingHeightmap() const { return AffectHeightmap; }
-	bool IsAffectingWeightmap() const { return AffectWeightmap; }
-	bool IsAffectingWeightmapLayer(const FName& InLayerName) const;
-
 	UFUNCTION(BlueprintImplementableEvent)
 	UTextureRenderTarget2D* Render(bool InIsHeightmap, UTextureRenderTarget2D* InCombinedResult);
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void Initialize(const FTransform& InLandscapeTransform, const FIntPoint& InLandscapeSize, const FIntPoint& InLandscapeRenderTargetSize);
 
-	UFUNCTION(BlueprintCallable, Category="Landscape")
+	UFUNCTION(BlueprintCallable, Category = "Landscape")
 	void RequestLandscapeUpdate();
 
 #if WITH_EDITOR
-	void SetCommitState(bool InCommited);
-	bool IsCommited() const { return bIsCommited; }
-
-	bool IsVisible() const { return bIsVisible; }
-	void SetIsVisible(bool bInIsVisible);
-
-	void SetAffectsHeightmap(bool bInAffectsHeightmap);
-	void SetAffectsWeightmap(bool bInAffectsWeightmap);
-
 	void SetOwningLandscape(class ALandscape* InOwningLandscape);
 	class ALandscape* GetOwningLandscape() const;
 
+	bool IsAffectingHeightmap() const { return AffectHeightmap; }
+	bool IsAffectingWeightmap() const { return AffectWeightmap; }
+	bool IsAffectingWeightmapLayer(const FName& InLayerName) const;
+	bool IsVisible() const { return bIsVisible; }
+	
+	void SetIsVisible(bool bInIsVisible);
+	void SetAffectsHeightmap(bool bInAffectsHeightmap);
+	void SetAffectsWeightmap(bool bInAffectsWeightmap);
+
+	virtual bool ShouldTickIfViewportsOnly() const override;
+	virtual void Tick(float DeltaSeconds) override;
 	virtual void PostEditMove(bool bFinished) override;
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
-
 	virtual void Destroyed() override;
 #endif
 };
-
