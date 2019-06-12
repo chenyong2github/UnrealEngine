10f9e790800ec3a4516ee757250a1ab2f5bb0f62
Graeme Thornton <Graeme.Thornton@epicgames.com>
2019-06-12 05:14:10 -0400

    Undo my soft reference change because I want to resubmit in a single CL so I can reference it easily in a UDN post
    #rb none
    #rnx
    
    [CL 6953124 by Graeme Thornton in Dev-Core branch]

--- Engine/Source/Runtime/EngineSettings/Classes/GameMapsSettings.h
+++ Engine/Source/Runtime/EngineSettings/Classes/GameMapsSettings.h
@@ -121,11 +121,9 @@ class ENGINESETTINGS_API UGameMapsSettings
 
 public:
 
-#if WITH_EDITORONLY_DATA
 	/** If set, this map will be loaded when the Editor starts up. */
 	UPROPERTY(config, EditAnywhere, Category=DefaultMaps, meta=(AllowedClasses="World"))
 	FSoftObjectPath EditorStartupMap;
-#endif
 
 	/** The default options that will be appended to a map being loaded. */
 	UPROPERTY(config, EditAnywhere, Category=DefaultMaps, AdvancedDisplay)
