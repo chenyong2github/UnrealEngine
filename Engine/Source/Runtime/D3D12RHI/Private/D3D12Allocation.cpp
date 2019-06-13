380ed9a319b711b0329ddfd9b83f2502edf19470
Emil Persson <Emil.Persson@epicgames.com>
2019-06-13 12:35:00 -0400

    Proper alignment check - as discussed in UDN: https://udn.unrealengine.com/questions/501803/alignment-issues-in-fd3d12buddyallocatorallocate.html #rb none
    [CL 6971889 by Emil Persson in Dev-Rendering branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Allocation.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Allocation.cpp
@@ -269,6 +269,7 @@ void FD3D12BuddyAllocator::Allocate(uint32 SizeInBytes, uint32 Alignment, FD3D12
 	}
 #endif
 	const uint32 AlignedOffsetFromResourceBase = AllocationBlockOffset + Padding;
+	check((AlignedOffsetFromResourceBase % Alignment) == 0);
 
 	// Setup the info that this allocator
 	FD3D12BuddyAllocatorPrivateData& PrivateData = ResourceLocation.GetBuddyAllocatorPrivateData();
@@ -293,12 +294,6 @@ void FD3D12BuddyAllocator::Allocate(uint32 SizeInBytes, uint32 Alignment, FD3D12
 	else
 	{
 		// Place resources are intialized elsewhere
-	}
-
-	if (Alignment != 0)
-	{
-		check(uint64(ResourceLocation.GetMappedBaseAddress()) % Alignment == 0);
-		check(uint64(ResourceLocation.GetGPUVirtualAddress()) % Alignment == 0);
 	}
 
 	// track the allocation
