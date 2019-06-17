c1311eb22479ff1a3184affdb6ddbd31a12e6846
michael galetzka <Michael.Galetzka@xa.epicgames.com>
2019-06-17 14:53:23 -0400

    * Add compile target (cpu, gpu) as constant for static switch selection * Fixed a bug where not all module inputs were culled correctly
    #rb none
    
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: michael.galetzka
    #ROBOMERGE-SOURCE: CL 6914907 via CL 6919061 via CL 6919153
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim) (v367-6836689)
    
    [CL 7050844 by michael galetzka in Dev-Anim branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Public/NiagaraModule.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Public/NiagaraModule.h
@@ -131,6 +131,7 @@ public:
 	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_RandomSeed() { return Emitter_RandomSeed; }
 	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_SpawnRate() { return Emitter_SpawnRate; }
 	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_SpawnInterval() { return Emitter_SpawnInterval; }
+	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_SimulationTarget() { return Emitter_SimulationTarget; }
 	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_InterpSpawnStartDt() { return Emitter_InterpSpawnStartDt; }
 	FORCEINLINE static const FNiagaraVariable&  GetVar_Emitter_SpawnGroup() { return Emitter_SpawnGroup; }
 
@@ -224,6 +225,7 @@ private:
 	static FNiagaraVariable Emitter_Age;
 	static FNiagaraVariable Emitter_LocalSpace;
 	static FNiagaraVariable Emitter_Determinism;
+	static FNiagaraVariable Emitter_SimulationTarget;
 	static FNiagaraVariable Emitter_RandomSeed;
 	static FNiagaraVariable Emitter_SpawnRate;
 	static FNiagaraVariable Emitter_SpawnInterval;
