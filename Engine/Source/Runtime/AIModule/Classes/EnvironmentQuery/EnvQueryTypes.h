b9e1b58122d3020897b17faf3409d1d47ff7e500
iniside <xeavien@gmail.com>
2019-06-18 03:19:38 -0400

    PR #5955: Add Multiply Score Operator to EnvQuery (Contributed by iniside)
    #rb mieszko.zielinski
    #jira UE-76382
    
    [CL 7061736 by Mieszko Zielinski in Dev-Framework branch]

--- Engine/Source/Runtime/AIModule/Classes/EnvironmentQuery/EnvQueryTypes.h
+++ Engine/Source/Runtime/AIModule/Classes/EnvironmentQuery/EnvQueryTypes.h
@@ -150,6 +150,7 @@ namespace EEnvTestScoreOperator
 		AverageScore	UMETA(Tooltip = "Use average score from all contexts"),
 		MinScore		UMETA(Tooltip = "Use minimum score from all contexts"),
 		MaxScore		UMETA(Tooltip = "Use maximum score from all contexts"),
+		Multiply		UMETA(Tooltip = "Multiply scores from all contexts"),
 	};
 }
 
@@ -1202,6 +1203,9 @@ public:
 				{
 					ItemScore = Score;
 				}
+				break;
+			case EEnvTestScoreOperator::Multiply:
+				ItemScore *= Score;
 				break;
 			}
 		}
