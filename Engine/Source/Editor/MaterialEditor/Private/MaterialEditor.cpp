aa70895e4856192f2a8de7a2556eb4279b979d3f
Jason Stasik <Jason.Stasik@epicgames.com>
2019-06-19 15:42:58 -0400

    Fix broken include #jira none #rb none
    [CL 7096413 by Jason Stasik in Dev-Editor branch]

--- Engine/Source/Editor/MaterialEditor/Private/MaterialEditor.cpp
+++ Engine/Source/Editor/MaterialEditor/Private/MaterialEditor.cpp
@@ -127,7 +127,7 @@
 
 #include "SMaterialParametersOverviewWidget.h"
 #include "IPropertyRowGenerator.h"
-#include "TextProperty.h"
+#include "UObject/TextProperty.h"
 
 #define LOCTEXT_NAMESPACE "MaterialEditor"
 
