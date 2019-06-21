bd151740daa693f014f396fb9d9f193616910e19
nick shin <nick.shin@xa.epicgames.com>
2019-06-21 14:47:53 -0400

    HTML5 - python forking will still error while trying to terminate already terminated processes
    - added same "WindowsError code 5" handler found in forking.py
    
    #jira UE-76388 //UE4/Release-4.23 - Compile UE4Game HTML5 - clang++.exe access is denied
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: nick.shin
    #ROBOMERGE-SOURCE: CL 7094004 in //UE4/Release-4.23/... via CL 7094007
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v367-6836689)
    
    [CL 7153501 by nick shin in Dev-VR branch]

--- Engine/Binaries/ThirdParty/Python/Win64/Lib/multiprocessing/forking.py
+++ Engine/Binaries/ThirdParty/Python/Win64/Lib/multiprocessing/forking.py
@@ -310,9 +310,14 @@ else:
             if self.returncode is None:
                 try:
                     _subprocess.TerminateProcess(int(self._handle), TERMINATE)
-                except WindowsError:
+# EPIC EDIT start -- nick.shin 2019-06-13 -- UE-76260
+                except WindowsError,e:
                     if self.wait(timeout=0.1) is None:
-                        raise
+                        # ERROR_ACCESS_DENIED (winerror 5) is received when the
+                        # process already died.
+                        if e.winerror != 5:
+                            raise
+# EPIC EDIT end -- nick.shin 2019-06-13 -- UE-76260
 
     #
     #
