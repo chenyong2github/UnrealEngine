89d4fd0780ab2391403dda32c16454014cdd3cdc
shaun kime <Shaun.Kime@epicgames.com>
2019-06-21 13:31:57 -0400

    Now handling reset of GPU systems and ensuring that we don't propagate old data.
    #rb Uriel.Doyon
    #tests engine tests pass except for known issue in static switch tests
    #jira UE-76376
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 7047826 via CL 7052280 via CL 7052338
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v367-6836689)
    
    [CL 7146286 by shaun kime in Dev-VR branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xC543A0CC, 0xD850F04D, 0xBBD65664, 0xF181DFD1);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x18B0ACF1, 0x208646CD, 0x91C04B94, 0x3717A7A6 );
