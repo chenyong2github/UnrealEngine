b650c8d3685cead62930f6fb676f72220cccf6eb
morten vassvik <Morten.Vassvik@xa.epicgames.com>
2019-06-21 04:15:27 -0400

    Parameter default rework checkpoint before break.
     - Adds a details customization panel for UNiagaraScriptVariables selected in the Parameters panel.
        - Supports static switch variable as well.
        - Renaming or deleting a static switch *node* will properly delete it from the Parameters panel, and vice versa. Changing the type of the static switch variable in the node correctly reflects in the Parameters panel.
        - Adds per-graph description metadata for non-module parameters, you can now explain the purpose of each introduced attribute.
        - Selectively hides irrelevant metadata for static switch parameters and non-module parameters.
        - Adds a proper value widget leveraging NiagaraTypeUtilities to the details panel which mirrors the value in the default pin in the graph.
    
    - Bugfix: Adding a new variable from an Add pin in the graph will now generate a proper unique name.
    - Verified several metadata related JIRAs as being fixed by this rewor:
        - UE-74744
        - UE-75168
        - UE-76184
    
    #rb frank.fella
    [FYI] shaun.kime, frank.fella
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: morten.vassvik
    #ROBOMERGE-SOURCE: CL 7112046 via CL 7116527 via CL 7116749
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build) (v367-6836689)
    
    [CL 7138217 by morten vassvik in Dev-Build branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraScriptVariable.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraScriptVariable.h
@@ -5,15 +5,20 @@
 #include "NiagaraTypes.h"
 #include "NiagaraScriptVariable.generated.h"
 
+/*
+* Used to store variable data and metadata per graph. 
+*/
 UCLASS()
 class UNiagaraScriptVariable : public UObject
 {
 public:
 	GENERATED_UCLASS_BODY()
 
+	/** Variable type, name and data. The data is not persistent, but used as a buffer when interfacing elsewhere. */
 	UPROPERTY()
 	FNiagaraVariable Variable;
 
-	UPROPERTY(EditAnywhere, Category = Metadata, meta=(NoResetToDefault, ShowOnlyInnerProperties))
+	/** The metadata associated with this script variable. */
+	UPROPERTY(EditAnywhere, Category = "Script Variable Data")
 	FNiagaraVariableMetaData Metadata;
 };
