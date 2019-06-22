3acf68323cc1e1316ec12048026ee1ece356541c
patrick laflamme <patrick.laflamme@epicgames.com>
2019-06-22 04:01:18 -0400

    #jira UE-76687 //UE4/Main - Incremental UE4Editor Win64 - Undeclared identifiers in ConcertFrontendUtils.h   - Added missing include to SImage.h
    #rb Francis.Hurteau
    
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: patrick.laflamme
    #ROBOMERGE-SOURCE: CL 7140837 in //UE4/Release-4.23/... via CL 7140838
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v367-6836689)
    
    [CL 7167622 by patrick laflamme in Dev-VR branch]

--- Engine/Plugins/Developer/Concert/ConcertSync/ConcertSyncClient/Source/ConcertSyncUI/Public/ConcertFrontendUtils.h
+++ Engine/Plugins/Developer/Concert/ConcertSync/ConcertSyncClient/Source/ConcertSyncUI/Public/ConcertFrontendUtils.h
@@ -6,6 +6,7 @@
 #include "ConcertActionDefinition.h"
 #include "EditorStyleSet.h"
 #include "Widgets/Layout/SBorder.h"
+#include "Widgets/Images/SImage.h"
 #include "Widgets/Input/SButton.h"
 #include "Widgets/Text/STextBlock.h"
 #include "Widgets/Layout/SBox.h"
