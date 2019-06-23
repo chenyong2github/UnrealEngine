8e6f4bd021c1e22c8a12e11c95b3482bcdeb9aed
ben marsh <Ben.Marsh@epicgames.com>
2019-06-23 09:07:48 -0400

    Disable remote execution of ISPC. Suspect this is causing spurious mismatched case issues.
    #rb none
    #rnx
    #jira UE-76072
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 7173969 in //UE4/Release-4.23/... via CL 7173985
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v367-6836689)
    
    [CL 7193573 by ben marsh in Dev-Core branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
@@ -1378,8 +1378,8 @@ namespace UnrealBuildTool
 				CompileAction.CommandPath = EnvVars.ISPCCompilerPath;
 				CompileAction.StatusDescription = Path.GetFileName(ISPCFile.AbsolutePath);
 
-				// Resource tool can run remotely if possible
-				CompileAction.bCanExecuteRemotely = true;
+				// Disable remote execution to workaround mismatched case on XGE
+				CompileAction.bCanExecuteRemotely = false;
 
 				List<string> Arguments = new List<string>();
 
@@ -1457,8 +1457,8 @@ namespace UnrealBuildTool
 				CompileAction.CommandPath = EnvVars.ISPCCompilerPath;
 				CompileAction.StatusDescription = Path.GetFileName(ISPCFile.AbsolutePath);
 
-				// Resource tool can run remotely if possible
-				CompileAction.bCanExecuteRemotely = true;
+				// Disable remote execution to workaround mismatched case on XGE
+				CompileAction.bCanExecuteRemotely = false;
 
 				List<string> Arguments = new List<string>();
 
