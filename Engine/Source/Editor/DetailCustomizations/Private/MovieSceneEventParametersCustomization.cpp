4d3527a85608f21f7dddb2c1e7d9b83861a175e9
marc audy <Marc.Audy@epicgames.com>
2019-07-09 00:52:25 -0400

    Fix crash setting struct type in the defaults of a blueprint MovieSceneEventParameters property #jira UE-76946 #rb [CODEREVIEW] Andrew.Rodham
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 7234311 in //UE4/Release-4.23/... via CL 7234313
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v367-6836689)
    
    [CL 7242652 by marc audy in Dev-Core branch]

--- Engine/Source/Editor/DetailCustomizations/Private/MovieSceneEventParametersCustomization.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/MovieSceneEventParametersCustomization.cpp
@@ -107,7 +107,7 @@ void FMovieSceneEventParametersCustomization::OnStructChanged(const FAssetData& 
 	FScopedTransaction Transaction(LOCTEXT("SetParameterStructText", "Set Parameter Structure"));
 
 	FEditPropertyChain PropertyChain;
-	PropertyChain.SetActivePropertyNode(PropertyHandle->GetProperty());
+	PropertyChain.AddHead(PropertyHandle->GetProperty());
 
 	// Fire off the pre-notify
 	FNotifyHook* Hook = PropertyUtilities->GetNotifyHook();
