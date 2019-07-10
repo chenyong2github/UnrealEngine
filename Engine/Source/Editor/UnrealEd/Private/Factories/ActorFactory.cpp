36ca5f23c9d1b81f1b445ce995c1d2e6b113add3
Jamie Dale <Jamie.Dale@epicgames.com>
2019-07-10 15:47:03 -0400

    Updated string
    #jira UETOOL-1400
    #rb none
    
    [CL 7266062 by Jamie Dale in Dev-Editor branch]

--- Engine/Source/Editor/UnrealEd/Private/Factories/ActorFactory.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Factories/ActorFactory.cpp
@@ -461,7 +461,7 @@ UActorFactoryDeferredDecal
 UActorFactoryDeferredDecal::UActorFactoryDeferredDecal(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 { 
-	DisplayName = LOCTEXT("DeferredDecalDisplayName", "Deferred Decal");
+	DisplayName = LOCTEXT("DecalDisplayName", "Decal");
 	NewActorClass = ADecalActor::StaticClass();
 	bUseSurfaceOrientation = true;
 }
