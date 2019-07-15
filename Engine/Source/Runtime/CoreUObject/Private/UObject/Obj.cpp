35d8c04a9ec0925462a45ae63287fce306a82f23
Robert Manuszewski <Robert.Manuszewski@epicgames.com>
2019-07-15 14:26:35 -0400

    Fixed a few issues with running the final Garbage Collector pass when exiting:
    + Fixed a crash when exiting early during init caused by disregard for GC set not being closed
    + Fixed a crash when exiting early during init caused by classes not being fully initialized
    + Added guards to prevent other threads from manipulating UObjects when the final GC runs
    + The final GC will not use multithreaded destruction as it will not help with GC speed when there's no time limit for GC
    
    #rb none
    #jira none
    
    [CL 7310890 by Robert Manuszewski in Dev-Core branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/Obj.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/Obj.cpp
@@ -4345,6 +4345,9 @@ void StaticUObjectInit()
 void ShutdownGarbageCollection();
 void CleanupLinkerAnnotations();
 void CleanupCachedArchetypes();
+void AcquireGCLock();
+void ReleaseGCLock();
+extern int32 GMultithreadedDestructionEnabled;
 
 //
 // Shut down the object manager.
@@ -4359,12 +4362,26 @@ void StaticExit()
 	// Delete all linkers are pending destroy
 	DeleteLoaders();
 
+	// We'll be destroying objects without time limit during exit purge
+	// so doing it on a separate thread doesn't make anything faster,
+	// also the exit purge is not a standard GC pass so no need to overcompilcate things
+	GMultithreadedDestructionEnabled = false;
+
 	// Cleanup root.
 	if (GObjTransientPkg != NULL)
 	{
 		GObjTransientPkg->RemoveFromRoot();
 		GObjTransientPkg = NULL;
 	}
+	
+	// This can happen when we run into an error early in the init process
+	if (GUObjectArray.IsOpenForDisregardForGC())
+	{
+		GUObjectArray.CloseDisregardForGC();
+	}
+
+	// Make sure no other threads manipulate UObjects
+	AcquireGCLock();
 
 	GatherUnreachableObjects(false);
 	IncrementalPurgeGarbage(false);
@@ -4417,6 +4434,8 @@ void StaticExit()
 		GatherUnreachableObjects(false);
 		IncrementalPurgeGarbage(false);
 	}
+
+	ReleaseGCLock();
 
 	ShutdownGarbageCollection();
 	UObjectBaseShutdown();
