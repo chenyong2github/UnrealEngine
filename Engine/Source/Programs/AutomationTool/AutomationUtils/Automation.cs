e941b39e1f003c103cd3fce85d79f94280157726
andrew grant <Andrew.Grant@epicgames.com>
2019-07-17 09:48:25 -0400

    Switched UAT build process on Mac to use msbuild. Enabled parallel msbuild for Mac Added HashCollection class that can hold and serialize a collection of content hashes Added quick dependency checking to UAT build by hashing csproj, input and output files for all modules Reduced some logging when not running with -verbose
    #rb Ben.Marsh
    [FYI] Ben.Marsh
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 7290522 via CL 7290525 via CL 7292711 via CL 7292732 via CL 7308901 via CL 7315952
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v371-7306989)
    
    [CL 7394727 by andrew grant in Dev-Networking branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/Automation.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/Automation.cs
@@ -103,13 +103,14 @@ namespace AutomationTool
 		public static CommandLineArg NoP4 = new CommandLineArg("-NoP4");
 		public static CommandLineArg P4 = new CommandLineArg("-P4");
         public static CommandLineArg Compile = new CommandLineArg("-Compile");
-        /// <summary>
-        /// This command is LEGACY because we used to run UAT.exe to compile scripts by default.
-        /// Now we only compile by default when run via RunUAT.bat, which still understands -nocompile.
-        /// However, the batch file simply passes on all arguments, so UAT will choke when encountering -nocompile.
-        /// Keep this CommandLineArg around so that doesn't happen.
-        /// </summary>
-        public static CommandLineArg NoCompileLegacyDontUse = new CommandLineArg("-NoCompile");
+		public static CommandLineArg IgnoreDependencies = new CommandLineArg("-IgnoreDependencies");
+		/// <summary>
+		/// This command is LEGACY because we used to run UAT.exe to compile scripts by default.
+		/// Now we only compile by default when run via RunUAT.bat, which still understands -nocompile.
+		/// However, the batch file simply passes on all arguments, so UAT will choke when encountering -nocompile.
+		/// Keep this CommandLineArg around so that doesn't happen.
+		/// </summary>
+		public static CommandLineArg NoCompileLegacyDontUse = new CommandLineArg("-NoCompile");
         public static CommandLineArg NoCompileEditor = new CommandLineArg("-NoCompileEditor");
 		public static CommandLineArg Help = new CommandLineArg("-Help");
 		public static CommandLineArg List = new CommandLineArg("-List");
