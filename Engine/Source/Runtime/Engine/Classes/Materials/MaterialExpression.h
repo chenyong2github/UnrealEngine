59153e6fa75c4448f944247ede0d139fe816873e
lauren ridge <Lauren.Ridge@epicgames.com>
2019-07-17 00:10:45 -0400

    Being able to hide parameters that are not connected in the material graph #rb Chris.Bunner #rnx
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: lauren.ridge
    #ROBOMERGE-SOURCE: CL 7261669 via CL 7267921 via CL 7268090
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v371-7306989)
    
    [CL 7341819 by lauren ridge in Dev-Core branch]

--- Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpression.h
+++ Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpression.h
@@ -418,6 +418,8 @@ class ENGINE_API UMaterialExpression : public UObject
 	virtual FName GetParameterName() const { return NAME_None; }
 	virtual void SetParameterName(const FName& Name) {}
 
+	virtual bool HasConnectedOutputs() const;
+
 #endif // WITH_EDITOR
 
 	/** Checks whether any inputs to this expression create a loop */
@@ -433,6 +435,5 @@ protected:
 	 */
 	bool ContainsInputLoopInternal(TArray<class FMaterialExpressionKey>& ExpressionStack, TSet<class FMaterialExpressionKey>& VisitedExpressions, const bool bStopOnFunctionCall);
 };
-
 
 
