9c9ca10ead2853f51191536a91b1b44b92d650bc
thomas ross <Thomas.Ross@epicgames.com>
2019-07-16 23:14:18 -0400

    Adding weights to bundle manager status. Adding helper class to monitor bundle + prereq status as a single %. #tests iOS Client #rb none [CODEREVIEW] [at]Justin.Marcus,[at]Daniel.Lamb
    #ROBOMERGE-OWNER: lina.halper
    #ROBOMERGE-AUTHOR: thomas.ross
    #ROBOMERGE-SOURCE: CL 7238071 via CL 7238129 via CL 7247123 via CL 7247172
    #ROBOMERGE-BOT: ANIM (Main -> Dev-Anim) (v371-7306989)
    
    [CL 7332742 by thomas ross in Dev-Anim branch]

---
+++ Engine/Source/Runtime/InstallBundleManager/Public/BundlePrereqCombinedStatusHelper.h
@@ -0,0 +1,85 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericPlatform/GenericPlatformInstallBundleManager.h"
+
+//Handles calculating the bundle status by combining progress from all of its
+//Prerequisites. Allows you to display one progress percent that is weighted based on all
+//bundles' values.
+class FBundlePrereqCombinedStatusHelper
+{
+public:
+	//provide all our needed combined status information in 1 struct
+	struct FCombinedBundleStatus
+	{
+		//Collapses all the bundle manager states into one of a few states so that you can show simple text based on this enum
+		enum class ECombinedBundleStateEnum : int32
+		{
+			Unknown
+			,Initializing
+			,Updating
+			,CompilingShaders
+			,NoUpdateRequired
+			,Finished
+		};
+		
+		float ProgressPercent;
+		ECombinedBundleStateEnum CombinedState;
+		bool bIsPaused;
+		bool bDoesCurrentStateSupportPausing;
+		
+		FCombinedBundleStatus()
+		: ProgressPercent(0.f)
+		, CombinedState(ECombinedBundleStateEnum::Unknown)
+		, bIsPaused(false)
+		, bDoesCurrentStateSupportPausing(false)
+		{
+		}
+	};
+	
+public:
+	FBundlePrereqCombinedStatusHelper();
+	~FBundlePrereqCombinedStatusHelper();
+	
+	//Setup tracking for all bundles required in the supplied BundleContentState
+	void SetBundlesToTrackFromContentState(FInstallBundleContentState& BundleContentState);
+	
+	//Get current CombinedBundleStatus for everything setup to track
+	const FCombinedBundleStatus& GetCurrentCombinedState() const;
+	
+private:
+	bool Tick(float dt);
+	void UpdateBundleCache();
+	void UpdateCombinedStatus();
+	
+	void SetupDelegates();
+	void CleanUpDelegates();
+	
+	//Called so we can track when a bundle is finished
+	void OnBundleInstallComplete(FInstallBundleResultInfo CompletedBundleInfo);
+	
+	float GetCombinedProgressPercent();
+	float GetIndividualWeightedProgressPercent(FInstallBundleStatus& Bundle);
+	
+private:
+	//All bundles we need including pre-reqs
+	TArray<FName> RequiredBundleNames;
+	
+	//Internal Cache of all bundle statuses to track progress
+	TMap<FName, FInstallBundleStatus> BundleStatusCache;
+	
+	//Bundle weights that determine what % of the overall install each bundle represents
+	TMap<FName, float> CachedBundleWeights;
+	
+	FCombinedBundleStatus CurrentCombinedStatus;
+	
+	//How to weight downloads vs. installs
+	float DownloadWeight;
+	float InstallWeight;
+	
+	bool bBundleNeedsUpdate;
+	
+	IPlatformInstallBundleManager* InstallBundleManager;
+	FDelegateHandle TickHandle;
+};
