3e9996eead32ce57570e90fbdd1b95afa543e821
marc audy <Marc.Audy@epicgames.com>
2019-07-17 08:36:13 -0400

    PR #6026: Add const qualifier to AActor::ActorLineTraceSingle (Contributed by Othereum) #jira UE-77106 #rb Me
    #ROBOMERGE-OWNER: ryan.vance
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 7282737 in //UE4/Release-4.23/... via CL 7282738
    #ROBOMERGE-BOT: DEVVR (Main -> Dev-VR) (v371-7306989)
    
    [CL 7389239 by marc audy in Dev-VR branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
@@ -2389,7 +2389,7 @@ public:
 	 * @param  Params          Additional parameters used for the trace
 	 * @return TRUE if a blocking hit is found
 	 */
-	bool ActorLineTraceSingle(struct FHitResult& OutHit, const FVector& Start, const FVector& End, ECollisionChannel TraceChannel, const struct FCollisionQueryParams& Params);
+	bool ActorLineTraceSingle(struct FHitResult& OutHit, const FVector& Start, const FVector& End, ECollisionChannel TraceChannel, const struct FCollisionQueryParams& Params) const;
 
 	/** 
 	 * returns Distance to closest Body Instance surface. 
