b50371a07a8ec740e33996dbcd1fe8fcb674def6
ben marsh <Ben.Marsh@epicgames.com>
2019-07-17 03:41:51 -0400

    Few CIS fixes.
    #rb none
    #rnx
    #jira
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 7321421 in //UE4/Release-4.23/... via CL 7321423
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v371-7306989)
    
    [CL 7370282 by ben marsh in Dev-Core branch]

--- Engine/Source/Developer/Windows/LiveCoding/Private/External/LC_Environment.cpp
+++ Engine/Source/Developer/Windows/LiveCoding/Private/External/LC_Environment.cpp
@@ -7,7 +7,7 @@
 
 namespace
 {
-	static const DWORD MAX_VARIABLE_SIZE = 1024u;
+	static const DWORD MAX_ENVIRONMENT_VARIABLE_SIZE = 1024u;
 }
 
 
@@ -34,9 +34,9 @@ void environment::SetVariable(const wchar_t* variable, const wchar_t* value)
 std::wstring environment::GetVariable(const wchar_t* variable, const wchar_t* defaultValue)
 {
 	std::wstring value;
-	value.resize(MAX_VARIABLE_SIZE);
+	value.resize(MAX_ENVIRONMENT_VARIABLE_SIZE);
 
-	const DWORD result = ::GetEnvironmentVariableW(variable, &value[0], MAX_VARIABLE_SIZE);
+	const DWORD result = ::GetEnvironmentVariableW(variable, &value[0], MAX_ENVIRONMENT_VARIABLE_SIZE);
 	if (result == 0)
 	{
 		// if a default value was provided, return that instead
