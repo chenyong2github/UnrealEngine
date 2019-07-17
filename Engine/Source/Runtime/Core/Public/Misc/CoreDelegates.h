a9367ebd6a7dbd553fbc2b843f9ccd056861c72b
sebastian nordgren <sebastian.nordgren@epicgames.com>
2019-07-17 07:53:50 -0400

    Added FCoreDelegates::OnBeforeHandleSystemError, this is to move registry writing for editor analytics before main crash reporting, since killing the editor process once the "Fatal error!" dialog pops up (and then freezes for a while during crash reporting) would cause an abnormal shutdown to be reported.
    This should help reduce the number of crashes being reported as abnormal shutdowns because the analytics are never written out.
    
    #jira UETOOL-1719
    
    #rb chris.gagnon
    
    [CL 7386698 by sebastian nordgren in Dev-Editor branch]

--- Engine/Source/Runtime/Core/Public/Misc/CoreDelegates.h
+++ Engine/Source/Runtime/Core/Public/Misc/CoreDelegates.h
@@ -181,8 +181,13 @@ public:
 
 	// Callback when an ensure has occurred
 	static FOnHandleSystemEnsure OnHandleSystemEnsure;
+
 	// Callback when an error (crash) has occurred
 	static FOnHandleSystemError OnHandleSystemError;
+
+	// Callback when an error (crash) has occurred. Runs before the crash reporter.
+	// This should be used very carefully.
+	static FOnHandleSystemError OnBeforeHandleSystemError;
 
 	// Called when an actor label is changed
 	static FOnActorLabelChanged OnActorLabelChanged;
