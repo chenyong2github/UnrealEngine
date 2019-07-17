21d8aac5fd16f9d4378845cc4aaa254c53f42f83
lauren ridge <Lauren.Ridge@epicgames.com>
2019-07-16 23:44:22 -0400

    Removing blutility dependencies in VREditor module, making data validation an editor plugin #rb Dan.OConnor #rnx #jira none
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: lauren.ridge
    #ROBOMERGE-SOURCE: CL 7266213 in //UE4/Release-4.23/... via CL 7266215
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build) (v371-7306989)
    
    [CL 7337895 by lauren ridge in Dev-Build branch]

--- Engine/Source/Editor/Blutility/Classes/EditorUtilityBlueprint.h
+++ Engine/Source/Editor/Blutility/Classes/EditorUtilityBlueprint.h
@@ -16,18 +16,13 @@ class BLUTILITY_API UEditorUtilityBlueprint : public UBlueprint
 {
 	GENERATED_UCLASS_BODY()
 
-#if WITH_EDITOR
 	// UBlueprint interface
 	virtual bool SupportedByDefaultBlueprintFactory() const override
 	{
 		return false;
 	}
-	
-#endif
 
-#if WITH_EDITORONLY_DATA
 protected:
 	virtual void LoadModulesRequiredForCompilation() override;
-#endif
 // End of UBlueprint interface
 };
