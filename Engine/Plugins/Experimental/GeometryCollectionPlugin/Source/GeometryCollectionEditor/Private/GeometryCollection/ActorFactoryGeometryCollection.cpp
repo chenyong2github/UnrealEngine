4cbe6295bbe479b25128783a22cd3d61daf21a31
louise rasmussen <louise.rasmussen@epicgames.com>
2019-07-17 08:24:23 -0400

    Fracture: Removes GeometryCollectionFactory and GeometryCollectionCacheFactory to prohibit creation of Geometry Collections outside of the Fracture Panel.
    #JIRA UE-76740
    #JIRA UE-75522
    #rb matt.kuhlenschmidt
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: louise.rasmussen
    #ROBOMERGE-SOURCE: CL 7289496 in //UE4/Release-4.23/... via CL 7289501
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v371-7306989)
    
    [CL 7388537 by louise rasmussen in Dev-Networking branch]

--- Engine/Plugins/Experimental/GeometryCollectionPlugin/Source/GeometryCollectionEditor/Private/GeometryCollection/ActorFactoryGeometryCollection.cpp
+++ Engine/Plugins/Experimental/GeometryCollectionPlugin/Source/GeometryCollectionEditor/Private/GeometryCollection/ActorFactoryGeometryCollection.cpp
@@ -4,7 +4,6 @@
 #include "GeometryCollection/GeometryCollectionActor.h"
 #include "GeometryCollection/GeometryCollection.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
-#include "GeometryCollection/GeometryCollectionFactory.h"
 #include "Misc/FileHelper.h"
 #include "ActorFactories/ActorFactory.h"
 #include "AssetData.h"
