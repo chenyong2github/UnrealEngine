3f2ba54242cf0b0962d3fcd786883797c7828987
james hopkin <James.Hopkin@epicgames.com>
2019-07-22 06:10:17 -0400

    #stomp Fixed shaky reliance on case of command FNames. Now explicitly upper cases all Stomp commands. Also fixed rogue 'auto' redundantly copying (FName,FString) pairs. #rb Jamie.Dale
    #ROBOMERGE-SOURCE: CL 7473719 via CL 7473796
    #ROBOMERGE-BOT: (v371-7306989)
    
    [CL 7473825 by james hopkin in Main branch]

--- Engine/Source/Runtime/Online/Stomp/Private/StompCommand.cpp
+++ Engine/Source/Runtime/Online/Stomp/Private/StompCommand.cpp
@@ -4,23 +4,25 @@
 
 #if WITH_STOMP
 
-#define DEFINE_COMMAND(Name, Value) const FStompCommand Name ## Command = TEXT(#Value)
+#define DEFINE_COMMAND(Name) const FStompCommand Name ## Command = TEXT(#Name)
 
 const FStompCommand HeartbeatCommand = FStompCommand();
-DEFINE_COMMAND(Connect, CONNECT);
-DEFINE_COMMAND(Connected, CONNECTED);
-DEFINE_COMMAND(Send, SEND);
-DEFINE_COMMAND(Subscribe, SUBSCRIBE);
-DEFINE_COMMAND(Unsubscribe, UNSUBSCRIBE);
-DEFINE_COMMAND(Begin, BEGIN);
-DEFINE_COMMAND(Commit, COMMIT);
-DEFINE_COMMAND(Abort, ABORT);
-DEFINE_COMMAND(Ack, ACK);
-DEFINE_COMMAND(Nack, NACK);
-DEFINE_COMMAND(Disconnect, DISCONNECT);
-DEFINE_COMMAND(Message, MESSAGE);
-DEFINE_COMMAND(Receipt, RECEIPT);
-DEFINE_COMMAND(Error, ERROR);
+
+// note: commands are converted to upper case to send over the wire
+DEFINE_COMMAND(Connect);
+DEFINE_COMMAND(Connected);
+DEFINE_COMMAND(Send);
+DEFINE_COMMAND(Subscribe);
+DEFINE_COMMAND(Unsubscribe);
+DEFINE_COMMAND(Begin);
+DEFINE_COMMAND(Commit);
+DEFINE_COMMAND(Abort);
+DEFINE_COMMAND(Ack);
+DEFINE_COMMAND(Nack);
+DEFINE_COMMAND(Disconnect);
+DEFINE_COMMAND(Message);
+DEFINE_COMMAND(Receipt);
+DEFINE_COMMAND(Error);
 
 #undef DEFINE_COMMAND
 
