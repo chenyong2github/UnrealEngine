f44a1a0f340cac32a7ff9950ed7ecb8eba4f0cc1
marc audy <Marc.Audy@epicgames.com>
2019-07-23 12:39:41 -0400

    Prevent multi-gate from having more than 32 entries. #jira UE-77727 #rb Ben.Hoffman
    #ROBOMERGE-SOURCE: CL 7553683 in //UE4/Release-4.23/...
    #ROBOMERGE-BOT: RELEASE (Release-4.23 -> Main) (v372-7473910)
    
    [CL 7553701 by marc audy in Main branch]

--- Engine/Source/Editor/BlueprintGraph/Classes/K2Node_MultiGate.h
+++ Engine/Source/Editor/BlueprintGraph/Classes/K2Node_MultiGate.h
@@ -34,29 +34,34 @@ class UK2Node_MultiGate : public UK2Node_ExecutionSequence
 	virtual void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
 	//~ End UK2Node Interface
 
+	//~ IK2Node_AddPinInterface interface
+	virtual bool CanAddPin() const override;
+	//~ End IK2Node_AddPinInterface interface
+
 	/** Getting pin access */
-	BLUEPRINTGRAPH_API UEdGraphPin* GetResetPin() const;
-	BLUEPRINTGRAPH_API UEdGraphPin* GetIsRandomPin() const;
-	BLUEPRINTGRAPH_API UEdGraphPin* GetLoopPin() const;
-	BLUEPRINTGRAPH_API UEdGraphPin* GetStartIndexPin() const;
-	BLUEPRINTGRAPH_API void GetOutPins(TArray<UEdGraphPin*>& OutPins) const;
+	UEdGraphPin* GetResetPin() const;
+	UEdGraphPin* GetIsRandomPin() const;
+	UEdGraphPin* GetLoopPin() const;
+	UEdGraphPin* GetStartIndexPin() const;
+ 	void GetOutPins(TArray<UEdGraphPin*>& OutPins) const;
+	int32 GetNumOutPins() const;
 
 	/** Gets the name and class of the MarkBit function from the KismetNodeHelperLibrary */
-	BLUEPRINTGRAPH_API void GetMarkBitFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetMarkBitFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the HasUnmarkedBit function from the KismetNodeHelperLibrary */
-	BLUEPRINTGRAPH_API void GetHasUnmarkedBitFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetHasUnmarkedBitFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the MarkFirstUnmarkedBit function from the KismetNodeHelperLibrary */
-	BLUEPRINTGRAPH_API void GetUnmarkedBitFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetUnmarkedBitFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the Greater_IntInt function from the KismetMathLibrary */
-	BLUEPRINTGRAPH_API void GetConditionalFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetConditionalFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the EqualEqual_IntInt function from the KismetMathLibrary */
-	BLUEPRINTGRAPH_API void GetEqualityFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetEqualityFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the NotEqual_BoolBool function from the KismetMathLibrary */
-	BLUEPRINTGRAPH_API void GetBoolNotEqualFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetBoolNotEqualFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the PrintString function */
-	BLUEPRINTGRAPH_API void GetPrintStringFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetPrintStringFunction(FName& FunctionName, UClass** FunctionClass);
 	/** Gets the name and class of the ClearAllBits function from the KismetNodeHelperLibrary */
-	BLUEPRINTGRAPH_API void GetClearAllBitsFunction(FName& FunctionName, UClass** FunctionClass);
+	void GetClearAllBitsFunction(FName& FunctionName, UClass** FunctionClass);
 
 private:
 	// Returns the exec output pin name for a given 0-based index
