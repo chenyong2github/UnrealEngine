ddaa33c85ea1fd4af9c93a40dc83444818f33260
steve smith <steve.smith@epicgames.com>
2019-07-31 16:32:20 -0400

    Fix collection-modified-during-ranged-for assertion in late-update class. Potential race-conditions between game thread and rendering thread could cause the game thread to modify the late update list while the rendering thread was consuming data. The fix is to wrap all collection accesses and index modifications with a critical section/scoped lock. #jira UE-73467 #rb mike.beach ryan.vance
    #ROBOMERGE-SOURCE: CL 7675065 in //UE4/Release-4.23/...
    #ROBOMERGE-BOT: RELEASE (Release-4.23 -> Main) (v381-7673386)
    
    [CL 7675066 by steve smith in Main branch]

--- Engine/Source/Runtime/HeadMountedDisplay/Public/MotionControllerComponent.h
+++ Engine/Source/Runtime/HeadMountedDisplay/Public/MotionControllerComponent.h
@@ -153,7 +153,6 @@ private:
 		virtual void BeginRenderViewFamily(FSceneViewFamily& InViewFamily) override;
 		virtual void PreRenderView_RenderThread(FRHICommandListImmediate& RHICmdList, FSceneView& InView) override {}
 		virtual void PreRenderViewFamily_RenderThread(FRHICommandListImmediate& RHICmdList, FSceneViewFamily& InViewFamily) override;
-		virtual void PostRenderViewFamily_RenderThread(FRHICommandListImmediate& RHICmdList, FSceneViewFamily& InViewFamily) override;
 		virtual int32 GetPriority() const override { return -10; }
 		virtual bool IsActiveThisFrame(class FViewport* InViewport) const;
 
