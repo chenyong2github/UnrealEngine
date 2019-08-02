58f14f63c83b4fb4d789c37ef223f4bf901f07e3
Mark Satterthwaite <Mark.Satterthwaite@epicgames.com>
2019-08-02 17:28:09 -0400

    Simplify the fma defines so they don't cause compile errors. #rb none #jira N/A
    [CL 7710477 by Mark Satterthwaite in Dev-Rendering branch]

--- Engine/Source/Developer/Apple/MetalShaderFormat/Private/MetalDerivedData.cpp
+++ Engine/Source/Developer/Apple/MetalShaderFormat/Private/MetalDerivedData.cpp
@@ -2066,17 +2066,15 @@ bool FMetalShaderOutputCooker::Build(TArray<uint8>& OutData)
 				// Fragment shaders and compute shaders need not use FMAs.
 				if (Frequency == HSF_PixelShader || Frequency == HSF_ComputeShader)
 				{
-					FMADefine = "#include <metal_stdlib>\n\n"
-								"#define fma(a, b, c) (((a) * (b)) + (c))\n\n";
+					FMADefine = "#include <metal_stdlib>\n\n";
 				}
 				// Plain vertex & domain shaders need only use FMAs on Metal 1.2-2.0
 				else
 				{
 					FMADefine = std::string("#include <metal_stdlib>\n\n"
 									 "#if __METAL_VERSION__ < 120 || __METAL_VERSION__ >= 210\n"
-									 "\t#define fma(a, b, c) (((a) * (b)) + (c))\n"
-									 "#else\n"
-									 "\t#define fma(a, b, c) fma((a), (b), (c))\n"
+									 "\ttemplate<typename T> T ue4_fma(T a, T b, T c) { return (((a) * (b)) + (c)); }\n"
+									 "\t#define fma ue4_fma\n"
 									 "#endif\n\n");
 				}
 			
