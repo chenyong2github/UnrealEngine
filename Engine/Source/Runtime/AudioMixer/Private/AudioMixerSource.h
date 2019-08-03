5e759178f50806c05d81600c3b009bd1e5e56335
maxwell hayes <Maxwell.Hayes@epicgames.com>
2019-08-02 20:29:16 -0400

    Changed AudioMixerSource::GetPlaybackPercent() to return the last playback percentage if a sound is stopping (instead of 0.0f)
    #jira none
    #rb Aaron.McLeran
    
    #ROBOMERGE-SOURCE: CL 7711484 in //UE4/Release-4.23/...
    #ROBOMERGE-BOT: RELEASE (Release-4.23 -> Main) (v385-7708028)
    
    [CL 7711485 by maxwell hayes in Main branch]

--- Engine/Source/Runtime/AudioMixer/Private/AudioMixerSource.h
+++ Engine/Source/Runtime/AudioMixer/Private/AudioMixerSource.h
@@ -157,6 +157,7 @@ namespace Audio
 		FChannelMapInfo ChannelMaps[(int32) ESubmixChannelFormat::Count];
 
 		float PreviousAzimuth;
+		float PreviousPlaybackPercent;
 
 		FSpatializationParams SpatializationParams;
 
