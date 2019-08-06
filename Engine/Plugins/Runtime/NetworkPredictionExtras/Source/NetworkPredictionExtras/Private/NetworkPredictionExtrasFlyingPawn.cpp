5470e137b2b9c059585b242eff4f353b0c174115
david ratti <David.Ratti@epicgames.com>
2019-08-06 10:30:17 -0400

    copyrights
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: david.ratti
    #ROBOMERGE-SOURCE: CL 7646264 via CL 7646358 via CL 7646392
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build) (v388-7785529)
    
    [CL 7789047 by david ratti in Dev-Build branch]

--- Engine/Plugins/Runtime/NetworkPredictionExtras/Source/NetworkPredictionExtras/Private/NetworkPredictionExtrasFlyingPawn.cpp
+++ Engine/Plugins/Runtime/NetworkPredictionExtras/Source/NetworkPredictionExtras/Private/NetworkPredictionExtrasFlyingPawn.cpp
@@ -1,3 +1,5 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
 #include "NetworkPredictionExtrasFlyingPawn.h"
 #include "Components/InputComponent.h"
 #include "Movement/FlyingMovement.h"
