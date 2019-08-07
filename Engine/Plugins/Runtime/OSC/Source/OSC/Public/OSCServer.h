ea52f364bec9458c397b53380bfff26ae36e19de
rob gay <Rob.Gay@epicgames.com>
2019-08-07 17:19:32 -0400

    OSC Plugin Refactor: - Code standards clean-up - Fix hang when socket fails attempting to send packets - Fix unbounded BP API (resulting in crashes) - Make OSCAddress first-class type     - Allows us to do more specific string parsing things with OSCAddresses as part of the OSC spec (TODO: matching with their light filtering spec) - More convenience functions for BP API - More logging/warning/errors when attempting to stand-up server/client
    #rb phill.popp
    [FYI] aaron.mcleran
    [FYI] ethan.geller
    #jira none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rob.gay
    #ROBOMERGE-SOURCE: CL 7709995 in //UE4/Main/...
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7844948 by rob gay in Dev-Networking branch]

---
+++ Engine/Plugins/Runtime/OSC/Source/OSC/Public/OSCServer.h
@@ -0,0 +1,118 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Containers/CircularQueue.h"
+#include "UObject/Object.h"
+#include "Interfaces/IPv4/IPv4Address.h"
+#include "Interfaces/IPv4/IPv4Endpoint.h"
+#include "Common/UdpSocketReceiver.h"
+#include "OSCMessage.h"
+#include "OSCBundle.h"
+#include "OSCServer.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOSCReceivedMessageEvent, const FOSCMessage &, Message);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOSCReceivedBundleEvent, const FOSCBundle &, Bundle);
+
+class FSocket;
+
+UCLASS(BlueprintType)
+class OSC_API UOSCServer : public UObject
+{
+	GENERATED_UCLASS_BODY()
+
+	struct OscCircularBuffer
+	{
+	public:
+
+		OscCircularBuffer(uint32 CircularQueueSize)
+			: Buffer(CircularQueueSize)
+		{}
+
+		OscCircularBuffer()
+			: Buffer(1)
+		{ }
+
+		TCircularQueue<TSharedPtr<FOSCPacket>> Buffer;
+	};
+
+public:
+	/** Gets whether or not to loopback if ReceiveIPAddress provided is multicast. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	bool GetMulticastLoopback() const;
+
+	/** Returns whether server is actively listening to incoming messages. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	bool IsActive() const;
+
+	/** Sets the IP address and port to listen for OSC data. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void Listen();
+
+	/** Set the address and port of server. Fails if server is currently active. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	bool SetAddress(const FString& ReceiveIPAddress, int32 Port);
+
+	/** Set whether or not to loopback if ReceiveIPAddress provided is multicast. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void SetMulticastLoopback(bool bMulticastLoopback);
+
+	/** Stop and tidy up network socket. */
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void Stop();
+
+	/** Event that gets called when an OSC message is received. */
+	UPROPERTY(BlueprintAssignable, Category = "Audio|OSC")
+	FOSCReceivedMessageEvent OnOscReceived;
+
+	/** Event that gets called when an OSC bundle is received. */
+	UPROPERTY(BlueprintAssignable, Category = "Audio|OSC")
+	FOSCReceivedBundleEvent OnOscBundleReceived;
+	
+	/** When true, only process received events from whitelisted clients. */
+	UPROPERTY(BlueprintReadWrite, Category = "Audio|OSC")
+	bool bWhitelistClients;
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void AddWhitelistedClient(const FString& IPAddress);
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void RemoveWhitelistedClient(const FString& IPAddress);
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	void ClearWhitelistedClients();
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|OSC")
+	TSet<FString> GetWhitelistedClients() const;
+
+protected:
+
+	void BeginDestroy() override;
+	
+	/** Callback that receives data from a socket. */
+	void Callback(const FArrayReaderPtr& Data, const FIPv4Endpoint& Endpoint);
+	
+	/** Circular buffer that stores OSC packets. */
+	OscCircularBuffer OSCPackets;
+	
+	/** Socket used to listen for OSC packets. */
+	FSocket* Socket;
+
+	/** UDP receiver. */
+	FUdpSocketReceiver* SocketReceiver;
+
+	/** Set of client addresses whitelisted to process packets from. */
+	TSet<uint32> ClientWhitelist;
+
+	/** OSC buffer lock. */
+	int32 OSCBufferLock;
+
+	/** IPAddress to listen for OSC packets on.  If unset, defaults to LocalHost */
+	FIPv4Address ReceiveIPAddress;
+
+	/** Port to listen for OSC packets on. */
+	int32 Port;
+
+	/** Whether or not to loopback if address provided is multicast */
+	bool bMulticastLoopback;
+};
