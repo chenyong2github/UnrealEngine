6237c20b2521b3f940b0072eef6f36470809444f
daniel rahier <Daniel.Rahier@epicgames.com>
2019-08-07 16:22:34 -0400

    Maya plugins are built in the same location as Mobu plugins now (Engine/Binaries/Win64/NotForLicensees) #jira UE-77291 #rb simon.therriault [FYI] patrick.boutot
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: daniel.rahier
    #ROBOMERGE-SOURCE: CL 7705747 in //UE4/Release-4.23/... via CL 7705986
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7843971 by daniel rahier in Dev-Networking branch]

--- Engine/Source/Programs/MayaLiveLinkPlugin/MayaLiveLinkPlugin2019.Target.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-using UnrealBuildTool;
-
-public class MayaLiveLinkPlugin2019Target : MayaLiveLinkPluginTargetBase
-{
-	public MayaLiveLinkPlugin2019Target(TargetInfo Target) : base(Target, "MayaLiveLinkPlugin2019")
-	{
-	}
-}
