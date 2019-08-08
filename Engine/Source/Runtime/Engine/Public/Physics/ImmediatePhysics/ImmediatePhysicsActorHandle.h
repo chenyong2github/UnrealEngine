62081aa82164b6b4f2f6516e36fb6057641d7492
Robert Manuszewski <Robert.Manuszewski@epicgames.com>
2019-08-08 10:45:38 -0400

    Copying //UE4/Dev-Main to Dev-Core (//UE4/Dev-Core) (CIS FIX)
    #rb none
    
    [CL 7880927 by Robert Manuszewski in Dev-Core branch]

---
+++ Engine/Source/Runtime/Engine/Public/Physics/ImmediatePhysics/ImmediatePhysicsActorHandle.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Physics/ImmediatePhysics/ImmediatePhysicsPhysX/ImmediatePhysicsActorHandle_PhysX.h"
+#include "Physics/ImmediatePhysics/ImmediatePhysicsChaos/ImmediatePhysicsActorHandle_Chaos.h"
+
+#include "Physics/ImmediatePhysics/ImmediatePhysicsDeclares.h"
