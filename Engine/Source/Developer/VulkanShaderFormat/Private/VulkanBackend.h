143c5bb45b0d53400dfb01a672978d64f4df4c76
juan canada <Juan.Canada@epicgames.com>
2019-08-08 14:51:28 -0400

    Cherry-pick 7106794: Added "GL_OES_shader_image_atomic" extension to Vulkan backend for Lumin platform.
    #jira UE-75890
    #rb Rolando.Caloca
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: juan.canada
    #ROBOMERGE-SOURCE: CL 7768628 in //UE4/Release-4.23/... via CL 7768929
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7888271 by juan canada in Dev-Networking branch]

--- Engine/Source/Developer/VulkanShaderFormat/Private/VulkanBackend.h
+++ Engine/Source/Developer/VulkanShaderFormat/Private/VulkanBackend.h
@@ -11,10 +11,12 @@ class FVulkanLanguageSpec : public ILanguageSpec
 {
 protected:
 	bool bShareSamplers;
+	bool bRequiresOESExtensions;
 
 public:
-	FVulkanLanguageSpec(bool bInShareSamplers)
+	FVulkanLanguageSpec(bool bInShareSamplers, bool bInRequiresOESExtensions)
 		: bShareSamplers(bInShareSamplers)
+		, bRequiresOESExtensions(bInRequiresOESExtensions)
 	{}
 
 	virtual bool SupportsDeterminantIntrinsic() const override
@@ -39,6 +41,9 @@ public:
 	virtual bool AllowsSharingSamplers() const override { return bShareSamplers; }
 
 	virtual bool RequiresNegateDDY() const override { return false; }
+
+public:
+	bool RequiresOESExtensions() const { return bRequiresOESExtensions; }
 };
 
 class ir_variable;
