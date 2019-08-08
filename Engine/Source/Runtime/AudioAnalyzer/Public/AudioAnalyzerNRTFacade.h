081db4817562634d68ea142b26a8f76ad928afe9
phil popp <Phil.Popp@epicgames.com>
2019-08-08 15:54:46 -0400

    Adding Audio Analyzer framework #rb Aaron.McLeran
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: phil.popp
    #ROBOMERGE-SOURCE: CL 7776579 via CL 7782054 via CL 7782243
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7896111 by phil popp in Dev-Networking branch]

---
+++ Engine/Source/Runtime/AudioAnalyzer/Public/AudioAnalyzerNRTFacade.h
@@ -0,0 +1,36 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IAudioAnalyzerNRTInterface.h"
+
+namespace Audio
+{
+
+	IAnalyzerNRTFactory* GetAnalyzerNRTFactory(FName InFactoryName);
+
+	/** FAnalyzerNRTBatch
+	 *
+	 * FAnalyzerNRTBatch provides a simplified interface for running 
+	 * analyzer factories over complete audio resources.
+	 */
+	class AUDIOANALYZER_API FAnalyzerNRTBatch
+	{
+		public:
+			/**
+			 * Create an FAnalyzerNRTBatch with the analyzer settings and factory name.
+			 */
+			FAnalyzerNRTBatch(TUniquePtr<IAnalyzerNRTSettings> InSettings, const FName& InFactoryName);
+
+			/**
+			 * Analyze an entire PCM16 encoded audio object.  Audio for the entire sound should be contained within InRawWaveData.
+			 */
+			TUniquePtr<IAnalyzerNRTResult> AnalyzePCM16Audio(const TArray<uint8>& InRawWaveData, int32 InNumChannels, float InSampleRate);
+
+		private:
+
+			TUniquePtr<IAnalyzerNRTSettings> Settings;
+			FName FactoryName;
+	};
+}
