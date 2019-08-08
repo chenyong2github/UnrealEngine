cac529f60514cb1d997e104e77971702f0e58821
matt kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2019-08-08 16:37:35 -0400

    Moved 2D geometry tools out of paper2D and into engine in GeomTools.h/cpp so they can be shared with others
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: matt.kuhlenschmidt
    #ROBOMERGE-SOURCE: CL 7788991 via CL 7797053 via CL 7797279
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7901556 by matt kuhlenschmidt in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Public/GeomTools.h
+++ Engine/Source/Runtime/Engine/Public/GeomTools.h
@@ -160,4 +160,43 @@ namespace FGeomTools
 
 	/** Util to see if P lies within triangle created by A, B and C. */
 	ENGINE_API bool PointInTriangle(const FVector& A, const FVector& B, const FVector& C, const FVector& P, const float InsideTriangleDotProductEpsilon = 0.0f);
+
 };
+
+
+class ENGINE_API FGeomTools2D
+{
+public:
+	// Corrects the polygon winding to match bNegativeWinding
+	// Ie. If the vertex order doesn't match the winding, it is reversed
+	// Returns empty array of points if num points < 3
+	static void CorrectPolygonWinding(TArray<FVector2D>& OutVertices, const TArray<FVector2D>& Vertices, const bool bNegativeWinding);
+
+	// Returns true if the points forming a polygon have CCW winding
+	// Returns true if the polygon isn't valid
+	static bool IsPolygonWindingCCW(const TArray<FVector2D>& Points);
+	static bool IsPolygonWindingCCW(const TArray<FIntPoint>& Points);
+
+	// Checks that these polygons can be successfully triangulated	
+	static bool ArePolygonsValid(const TArray<TArray<FVector2D>>& Polygons);
+
+	// Merge additive and subtractive polygons, split them up into additive polygons
+	// Assumes all polygons and overlapping polygons are valid, and the windings match the setting on the polygon
+	static TArray<TArray<FVector2D>> ReducePolygons(const TArray<TArray<FVector2D>>& Polygons, const TArray<bool>& PolygonNegativeWinding);
+
+	// Triangulate a polygon. Check notes in implementation
+	static bool TriangulatePoly(TArray<FVector2D>& OutTris, const TArray<FVector2D>& PolygonVertices, bool bKeepColinearVertices = false);
+
+	// 2D version of RemoveRedundantTriangles from GeomTools
+	static void RemoveRedundantTriangles(TArray<FVector2D>& OutTriangles, const TArray<FVector2D>& InTriangles);
+
+	// Generate convex shapes
+	static void GenerateConvexPolygonsFromTriangles(TArray<TArray<FVector2D>>& OutPolygons, const TArray<FVector2D>& InTriangles);
+
+	// Generate convex hull from points
+	static void GenerateConvexHullFromPoints(TArray<FVector2D>& OutConvexHull, TArray<FVector2D>& Points);
+
+	// Returns true if TestPoint is inside the polygon defined by PolygonPoints
+	static bool IsPointInPolygon(const FVector2D& TestPoint, const TArray<FVector2D>& PolygonPoints);
+};
+
