ae3ca49451dab188f8982402b39edf2affc8af18
rob gay <Rob.Gay@epicgames.com>
2019-08-08 15:07:39 -0400

    Remove code optimization from build cs #rb none #jira none
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rob.gay
    #ROBOMERGE-SOURCE: CL 7772382 in //UE4/Release-4.23/... via CL 7772384
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v389-7813075)
    
    [CL 7889969 by rob gay in Dev-Networking branch]

--- Engine/Plugins/Runtime/OSC/Source/OSC/OSC.Build.cs
+++ Engine/Plugins/Runtime/OSC/Source/OSC/OSC.Build.cs
@@ -6,8 +6,6 @@ namespace UnrealBuildTool.Rules
 	{
         public OSC(ReadOnlyTargetRules Target) : base(Target)
 		{
-            OptimizeCode = CodeOptimization.Never;
-
             PublicDependencyModuleNames.AddRange(
 				new string[] {
                     "Core",
