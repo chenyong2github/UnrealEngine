631b9e3814d4affa412fc68c71eb89889988606c
Patrick Kelly <Patrick.Kelly@epicgames.com>
2019-08-13 15:26:25 -0400

    Fix hair shading artifacts in ray tracing SkyLight pass
    #rb charles.derousiers
    #jira ue-78851
    
    [CL 8003124 by Patrick Kelly in Dev-Rendering branch]

--- Engine/Source/Runtime/Renderer/Private/RayTracing/RaytracingSkylight.cpp
+++ Engine/Source/Runtime/Renderer/Private/RayTracing/RaytracingSkylight.cpp
@@ -47,7 +47,14 @@ static float GRayTracingSkyLightMaxRayDistance = 1.0e7;
 static FAutoConsoleVariableRef CVarRayTracingSkyLightMaxRayDistance(
 	TEXT("r.RayTracing.SkyLight.MaxRayDistance"),
 	GRayTracingSkyLightMaxRayDistance,
-	TEXT("Sets the samples-per-pixel for ray tracing SkyLight (default = 1.0e7)")
+	TEXT("Sets the max ray distance for ray tracing SkyLight (default = 1.0e7)")
+);
+
+static float GRayTracingSkyLightMaxShadowThickness = 1.0e3;
+static FAutoConsoleVariableRef CVarRayTracingSkyLightMaxShadowThickness(
+	TEXT("r.RayTracing.SkyLight.MaxShadowThickness"),
+	GRayTracingSkyLightMaxShadowThickness,
+	TEXT("Sets the max shadow thickness for translucent materials for ray tracing SkyLight (default = 1.0e3)")
 );
 
 static int32 GRayTracingSkyLightSamplingStopLevel = 0;
@@ -859,6 +866,7 @@ void FDeferredShadingSceneRenderer::GenerateSkyLightVisibilityRays(
 	uint32 SamplesPerPixel = GRayTracingSkyLightSamplesPerPixel >= 0 ? GRayTracingSkyLightSamplesPerPixel : Scene->SkyLight->SamplesPerPixel;
 	SkyLightData.SamplesPerPixel = SamplesPerPixel;
 	SkyLightData.MaxRayDistance = GRayTracingSkyLightMaxRayDistance;
+	SkyLightData.MaxShadowThickness = GRayTracingSkyLightMaxShadowThickness;
 	SkyLightData.SamplingStopLevel = GRayTracingSkyLightSamplingStopLevel;
 
 	// Sampling state
