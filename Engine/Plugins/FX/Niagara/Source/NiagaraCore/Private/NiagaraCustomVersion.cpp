155110e85e6c2812808931de9a7f48f80df6762f
marc audy <Marc.Audy@epicgames.com>
2019-08-13 12:47:05 -0400

    Forced Niagara to recompile simulation script since they seem out of sync and generate artefacts. #jira UE-77728 #rb shaun.kime
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: uriel.doyon
    #ROBOMERGE-SOURCE: CL 7914397 in //UE4/Release-4.23/... via CL 7935793
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v396-7974030)
    
    [CL 7986410 by marc audy in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xA1C84E62, 0x19E6437F, 0x9B095E4A, 0x2F0F8ADE);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xBEACE717, 0x0D3A4051, 0xA442CF5E, 0x1CF94ACE);
