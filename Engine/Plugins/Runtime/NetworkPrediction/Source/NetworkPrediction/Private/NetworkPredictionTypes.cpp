cfb05cd05f04cb9f6b47ba37777606ce530ee2b8
david ratti <David.Ratti@epicgames.com>
2019-08-14 12:14:33 -0400

    Continued WIP on NetworkPrediction -Refactor on TReplicators* -Settling in on new ticking/update scheme -Auto and simulated proxy should be working again
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.ratti
    #ROBOMERGE-SOURCE: CL 7996977 via CL 8006534 via CL 8006618
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v398-8029740)
    
    [CL 8035719 by david ratti in Dev-Networking branch]

--- Engine/Plugins/Runtime/NetworkPrediction/Source/NetworkPrediction/Private/NetworkPredictionTypes.cpp
+++ Engine/Plugins/Runtime/NetworkPrediction/Source/NetworkPrediction/Private/NetworkPredictionTypes.cpp
@@ -112,3 +112,14 @@ FScopedBandwidthLimitBypass::~FScopedBandwidthLimitBypass()
 		CachedNetConnection->QueuedBits = RestoreBits - CachedNetConnection->SendBuffer.GetNumBits();
 	}
 }
+
+FNetSimTickParametersBase::FNetSimTickParametersBase(AActor* OwningActor)
+{
+	// Default implementation that will probably be good for most actors
+	Role = OwningActor->GetLocalRole();
+	const bool bHasNetConnection = OwningActor->GetNetConnection();
+
+	// The only default case we really don;t want generating local input is on the server when there is a net owning player (auto proxy)
+	// This implies that we will (sim) extrapolate by default. To opt out of extrapolation, set to false or you can just not tick the net sim
+	bGenerateLocalInputCmds = !(Role == ROLE_Authority && bHasNetConnection);
+}
