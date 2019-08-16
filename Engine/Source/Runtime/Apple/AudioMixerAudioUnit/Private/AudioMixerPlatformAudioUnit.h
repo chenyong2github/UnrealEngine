945e34d8f19fd24b0d5eb4cb7cf9f31dd1ab439d
aaron mcleran <Aaron.McLeran@epicgames.com>
2019-08-16 04:37:40 -0400

    Rolling back CL 7951971 due to mac crash regression
    [FYI] maxwell.hayes
    #rb none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: aaron.mcleran
    #ROBOMERGE-SOURCE: CL 8035677 via CL 8040368 via CL 8042927 via CL 8046539 via CL 8065848
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v400-8057353)
    
    [CL 8086356 by aaron mcleran in Dev-Networking branch]

--- Engine/Source/Runtime/Apple/AudioMixerAudioUnit/Private/AudioMixerPlatformAudioUnit.h
+++ Engine/Source/Runtime/Apple/AudioMixerAudioUnit/Private/AudioMixerPlatformAudioUnit.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "AudioMixer.h"
-#include "DSP/Dsp.h"
+
 #include <AudioToolbox/AudioToolbox.h>
 #include <AudioUnit/AudioUnit.h>
 
@@ -40,18 +40,11 @@ namespace Audio
 		virtual ICompressedAudioInfo* CreateCompressedAudioInfo(USoundWave* InSoundWave) override;
 		virtual FString GetDefaultDeviceName() override;
 		virtual FAudioPlatformSettings GetPlatformSettings() const override;
-		virtual int32 GetNumFrames(const int32 InNumReqestedFrames) override;
-		virtual void ResumeContext() override;
-		virtual void SuspendContext() override;
-		
-		/** Whether or not the platform supports realtime decompression. */
-		virtual bool SupportsRealtimeDecompression() const override { return true; }
-		
-		/** Whether or not the platform disables caching of decompressed PCM data (i.e. to save memory on fixed memory platforms) */
-		virtual bool DisablePCMAudioCaching() const override { return true; }
-		
-	  //~ End IAudioMixerPlatformInterface
-		
+        virtual int32 GetNumFrames(const int32 InNumReqestedFrames) override;
+        virtual void ResumeContext() override;
+        virtual void SuspendContext() override;
+        //~ End IAudioMixerPlatformInterface
+        
 	private:
 		AudioStreamBasicDescription OutputFormat;
 
@@ -66,27 +59,14 @@ namespace Audio
 		AUGraph     AudioUnitGraph;
 		AUNode      OutputNode;
 		AudioUnit	OutputUnit;
-		uint8*      SubmittedBufferPtr;
-		int32 SubmittedBytes = 0;
+        uint8*      SubmittedBufferPtr;
+        int32 SubmittedBytes = 0;
 
-		int32       RemainingBytesInCurrentSubmittedBuffer;
-		int32       BytesPerSubmittedBuffer;
-		
-		double GraphSampleRate;
-
-		// We may have to grow the circular buffer capacity since Audio Unit callback size is not guaranteed to be constant
-		// Currently, this just zero's-out and reallocates, so it will pop. (We always keep largest capacity)
-		void GrowCircularBufferIfNeeded(const int32 InNumSamplesPerRenderCallback, const int32 InNumSamplesPerDeviceCallback);
-
-		// This buffer is pushed to and popped from in the SubmitBuffer callback. 
-		// This is required for devices that require frame counts per callback that are not powers of two.
-		Audio::TCircularAudioBuffer<int8> CircularOutputBuffer;
-
-		int32 NumSamplesPerRenderCallback;
-		int32 NumSamplesPerDeviceCallback;
-		mutable bool bInternalPlatformSettingsInitialized{ false };
-		mutable FAudioPlatformSettings InternalPlatformSettings;
-		
+        int32       RemainingBytesInCurrentSubmittedBuffer;
+        int32       BytesPerSubmittedBuffer;
+        
+        double GraphSampleRate;
+        
 		bool PerformCallback(AudioBufferList* OutputBufferData);
 		void HandleError(const TCHAR* InLogOutput, bool bTeardown = true);
 		static OSStatus AudioRenderCallback(void* RefCon, AudioUnitRenderActionFlags* ActionFlags,
