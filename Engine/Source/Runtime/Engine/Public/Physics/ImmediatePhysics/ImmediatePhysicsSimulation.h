328178189a332cc5e0468e32060ecdffa70c0286
Jack Porter <jack.porter@epicgames.com>
2019-08-16 09:09:06 -0400

    Fix integration records #rb None
    [CL 8090584 by Jack Porter in Dev-Mobile branch]

---
+++ Engine/Source/Runtime/Engine/Public/Physics/ImmediatePhysics/ImmediatePhysicsSimulation.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Physics/ImmediatePhysics/ImmediatePhysicsPhysX/ImmediatePhysicsSimulation_PhysX.h"
+#include "Physics/ImmediatePhysics/ImmediatePhysicsChaos/ImmediatePhysicsSimulation_Chaos.h"
+
+#include "Physics/ImmediatePhysics/ImmediatePhysicsDeclares.h"
