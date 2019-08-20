9eea4d34b3a397ca987e9351bbdff99c5a2d21c7
Rex Hill <Rex.Hill@epicgames.com>
2019-08-20 13:34:22 -0400

    Fixed widget component mouse input #jira UE-78688 #rb Matt.Kuhlenschmidt #fyi nick.darnell
    [CL 8127453 by Rex Hill in Dev-Editor branch]

--- Engine/Source/Runtime/SlateCore/Private/Input/HittestGrid.cpp
+++ Engine/Source/Runtime/SlateCore/Private/Input/HittestGrid.cpp
@@ -259,7 +259,7 @@ TArray<FWidgetAndPointer> FHittestGrid::GetBubblePath(FVector2D DesktopSpaceCoor
 				{
 					if (BestHitWidgetData.CustomPath.IsValid())
 					{
-						const TArray<FWidgetAndPointer> BubblePathExtension = BestHitWidgetData.CustomPath.Pin()->GetBubblePathAndVirtualCursors(FirstHitWidget->GetPaintSpaceGeometry(), DesktopSpaceCoordinate, bIgnoreEnabledStatus);
+						const TArray<FWidgetAndPointer> BubblePathExtension = BestHitWidgetData.CustomPath.Pin()->GetBubblePathAndVirtualCursors(FirstHitWidget->GetTickSpaceGeometry(), DesktopSpaceCoordinate, bIgnoreEnabledStatus);
 						Path.Append(BubblePathExtension);
 					}
 				}
