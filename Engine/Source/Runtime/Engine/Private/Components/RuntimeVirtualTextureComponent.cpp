9c1f023af64ff86af7d9d359401355bbdce8cc4d
jeremy moore <Jeremy.Moore@epicgames.com>
2019-08-24 12:00:56 -0400

    Add thumbnail previews for runtime virtual texture
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 8126213 via CL 8132037 via CL 8156568
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v401-8057353)
    
    [CL 8244748 by jeremy moore in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/Components/RuntimeVirtualTextureComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/RuntimeVirtualTextureComponent.cpp
@@ -44,6 +44,12 @@ void URuntimeVirtualTextureComponent::DestroyRenderState_Concurrent()
 	Super::DestroyRenderState_Concurrent();
 }
 
+FTransform URuntimeVirtualTextureComponent::GetVirtualTextureTransform() const
+{
+	// Transform is based on bottom left of the URuntimeVirtualTextureComponent unit box (which is centered on the origin)
+	return FTransform(FVector(-0.5f, -0.5f, 0.f)) * GetComponentTransform();
+}
+
 #if WITH_EDITOR
 
 void URuntimeVirtualTextureComponent::SetRotation()
