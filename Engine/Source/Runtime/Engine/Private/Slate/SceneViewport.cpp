1d078ea11e2b3cfb7daa3547c1cc6a8f37858664
Cody Albert <cody.albert@epicgames.com>
2019-08-27 19:03:57 -0400

    Removed clearing of viewport focus for all users when the primary keyboard user focuses something other than the viewport. Updated hard-coded checks to User 0 to instead fetch the keyboard user ID.
    #rb chris.gagnon
    
    [CL 8356797 by Cody Albert in Dev-Editor branch]

--- Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
+++ Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
@@ -1058,7 +1058,7 @@ FReply FSceneViewport::OnFocusReceived(const FFocusEvent& InFocusEvent)
 {
 	CurrentReplyState = FReply::Handled(); 
 
-	if ( InFocusEvent.GetUser() == 0 )
+	if ( InFocusEvent.GetUser() == FSlateApplication::Get().GetUserIndexForKeyboard() )
 	{
 		if ( ViewportClient != nullptr )
 		{
@@ -1107,7 +1107,7 @@ FReply FSceneViewport::OnFocusReceived(const FFocusEvent& InFocusEvent)
 void FSceneViewport::OnFocusLost( const FFocusEvent& InFocusEvent )
 {
 	// If the focus loss event isn't the for the primary 'keyboard' user, don't worry about it.
-	if ( InFocusEvent.GetUser() != 0 )
+	if ( InFocusEvent.GetUser() != FSlateApplication::Get().GetUserIndexForKeyboard() )
 	{
 		return;
 	}
@@ -1119,19 +1119,6 @@ void FSceneViewport::OnFocusLost( const FFocusEvent& InFocusEvent )
 	{
 		FScopedConditionalWorldSwitcher WorldSwitcher(ViewportClient);
 		ViewportClient->LostFocus(this);
-
-		TSharedPtr<SWidget> ViewportWidgetPin = ViewportWidget.Pin();
-		if ( ViewportWidgetPin.IsValid() )
-		{
-			FSlateApplication::Get().ForEachUser([&] (FSlateUser* User) {
-				if ( User->GetFocusedWidget() == ViewportWidgetPin )
-				{
-					FSlateApplication::Get().ClearUserFocus(User->GetUserIndex());
-				}
-			});
-		}
-
-		UE_LOG(LogSlate, Log, TEXT("FSceneViewport::OnFocusLost() reason %d"), (int32)InFocusEvent.GetCause());
 	}
 }
 
