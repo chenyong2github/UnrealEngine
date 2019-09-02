09db6805e82bede3449251abf2f36196a050d7e8
michael forot <Michael.Forot@epicgames.com>
2019-09-02 11:45:45 -0400

    Manual Import of the CL 7711015  (integrate->edit) #rb none
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: michael.forot
    #ROBOMERGE-SOURCE: CL 8385100 via CL 8385693 via CL 8385710
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v404-8404397)
    
    [CL 8404924 by michael forot in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,6 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x563D9FF1, 0x1E9042CC, 0xA8BF7AC4, 0xC9366901);
-
-
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xAA5883AE, 0xCB3648DB, 0xAC0BF434, 0xFEA4BE4);
