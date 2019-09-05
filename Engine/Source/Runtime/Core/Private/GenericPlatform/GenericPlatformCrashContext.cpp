76a28140048794b554d906d211aad8d1b44d0841
brandon schaefer <Brandon.Schaefer@epicgames.com>
2019-09-05 04:40:21 -0400

    Report the value of GIsRequestingExit during a crash
    #rb Arciel.Rekman
    
    
    #ROBOMERGE-SOURCE: CL 8494500 via CL 8494501 via CL 8494615 via CL 8494620 via CL 8501239
    #ROBOMERGE-BOT: (v406-8472469)
    
    [CL 8502707 by brandon schaefer in Main branch]

--- Engine/Source/Runtime/Core/Private/GenericPlatform/GenericPlatformCrashContext.cpp
+++ Engine/Source/Runtime/Core/Private/GenericPlatform/GenericPlatformCrashContext.cpp
@@ -337,6 +337,7 @@ void FGenericCrashContext::SerializeContentToBuffer() const
 	AddCrashProperty( TEXT( "AppDefaultLocale" ), *NCachedCrashContextProperties::DefaultLocale );
 	AddCrashProperty( TEXT( "BuildVersion" ), FApp::GetBuildVersion() );
 	AddCrashProperty( TEXT( "IsUE4Release" ), NCachedCrashContextProperties::bIsUE4Release );
+	AddCrashProperty( TEXT( "IsRequestingExit" ), GIsRequestingExit );
 
 	// Remove periods from user names to match AutoReporter user names
 	// The name prefix is read by CrashRepository.AddNewCrash in the website code
