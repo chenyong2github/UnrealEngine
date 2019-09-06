9e168a634629bbd61f4ebf568ecaad5f54220632
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2019-09-06 07:53:54 -0400

    Added UAnimInstance::ClearLayerOverlay
    This new call allows overlaid layer classes to be selectively reset to their defaults, allowing partial graph resets instead of needing to restore and rebuild state each time.
    Also added a call to get a specific layer sub instance by class, a useful counterpart to the new functionality.
    
    #rb Jurre.deBaare
    
    
    #ROBOMERGE-SOURCE: CL 8527698 via CL 8528899
    #ROBOMERGE-BOT: (v406-8472469)
    
    [CL 8528938 by thomas sarkanen in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/SkeletalMeshComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/SkeletalMeshComponent.h
@@ -787,16 +787,29 @@ public:
 	void GetSubInstancesByTag(FName InTag, TArray<UAnimInstance*>& OutSubInstances) const;
 
 	/** 
-	 * Runs through all layer nodes, attempting to find nodes that are implemented by the specified class, then sets up a sub instance of the class for each.
-	 * Allocates one sub instance to run each of the groups specified in the class, so state is shared.
-	 * If InClass is null, then layers are reset to their defaults.
+	 * Runs through all layer nodes, attempting to find layer nodes that are implemented by the specified class, then sets up a sub instance of the class for each.
+	 * Allocates one sub instance to run each of the groups specified in the class, so state is shared. If a layer is not grouped (ie. NAME_None), then state is not shared
+	 * and a separate sub-instance is allocated for each layer node.
+	 * If InClass is null, then all layers are reset to their defaults.
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Components|SkeletalMesh|Layers")
 	void SetLayerOverlay(TSubclassOf<UAnimInstance> InClass);
 
-	/** Gets the sub instance corresponding to the specified group */
+	/** 
+	 * Runs through all layer nodes, attempting to find layer nodes that are currently running the specified class, then resets each to its default value.
+	 * State sharing rules are as with SetLayerOverlay.
+	 * If InClass is null, does nothing.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Components|SkeletalMesh|Layers")
+	void ClearLayerOverlay(TSubclassOf<UAnimInstance> InClass);
+
+	/** Gets the layer sub instance corresponding to the specified group */
 	UFUNCTION(BlueprintCallable, Category = "Components|SkeletalMesh|Layers")
 	UAnimInstance* GetLayerSubInstanceByGroup(FName InGroup) const;
+
+	/** Gets the first layer sub instance corresponding to the specified class */
+	UFUNCTION(BlueprintCallable, Category = "Components|SkeletalMesh|Layers")
+	UAnimInstance* GetLayerSubInstanceByClass(TSubclassOf<UAnimInstance> InClass) const;
 
 	/** 
 	 * Returns whether there are any valid instances to run, currently this means whether we have
