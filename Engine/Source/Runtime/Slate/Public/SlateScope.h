10a775509722ac7d64a973bc9d2dc690dba5f0bd
dan hertzka <Dan.Hertzka@epicgames.com>
2019-09-06 18:57:57 -0400

    Removed #pragma once from the top of SlateScope.h to resolve static analysis error - Still compiles fine, and now matches PartyPackage.h exactly (which SA has no issue with), so this should resolve the SA error
    #jira UE-79912
    
    
    #ROBOMERGE-SOURCE: CL 8556974 via CL 8562610
    #ROBOMERGE-BOT: (v406-8472469)
    
    [CL 8562789 by dan hertzka in Main branch]

--- Engine/Source/Runtime/Slate/Public/SlateScope.h
+++ Engine/Source/Runtime/Slate/Public/SlateScope.h
@@ -1,6 +1,6 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
-#pragma once
+// Can't be #pragma once because we want this to be interpreted differently depending on the module that is referencing the including file
 
 // SLATE_MODULE is defined private to the module in Slate.build.cs
 // This allows us to establish a scope that is public within the Slate module itself, but protected from all consumers of the module
