a703fee91bcbf39cfa9720cb6cd72e151d4d23ce
yoan stamant <Yoan.Stamant@epicgames.com>
2019-09-06 16:30:54 -0400

    NavigationDataHandler: prevent registration of an element that should not be registered when processing the parent chain. One problematic scenario is when all components from an actor are unregistered and one of them is the navigation parent of the others. In this case, the parent may be unregistered before some of its children and then calls to UpdateNavOctreeParentChain from a child may queue back the unregistered parent. UpdateNavOctreeParentChain will now consider the actual status of the parent (Added or queued for add) before registering it or its children.
    [at]mieszko.zielinski [at]guillaume.guay
    #rb mieszko.zielinski, guillaume.guay
    
    
    #ROBOMERGE-SOURCE: CL 8552772 via CL 8557168
    #ROBOMERGE-BOT: (v406-8472469)
    
    [CL 8557314 by yoan stamant in Main branch]

--- Engine/Source/Runtime/NavigationSystem/Public/NavigationDataHandler.h
+++ Engine/Source/Runtime/NavigationSystem/Public/NavigationDataHandler.h
@@ -15,7 +15,16 @@ struct NAVIGATIONSYSTEM_API FNavigationDataHandler
 	void RemoveNavOctreeElementId(const FOctreeElementId& ElementId, int32 UpdateFlags);
 	FSetElementId RegisterNavOctreeElement(UObject& ElementOwner, INavRelevantInterface& ElementInterface, int32 UpdateFlags);
 	void AddElementToNavOctree(const FNavigationDirtyElement& DirtyElement);
-	void UnregisterNavOctreeElement(UObject& ElementOwner, INavRelevantInterface& ElementInterface, int32 UpdateFlags);
+
+	/** Removes associated NavOctreeElement and invalidates associated pending updates. Also removes object from the list of children
+	* of the NavigationParent, if any.
+	* @param ElementOwner		Object for which we must remove associated NavOctreeElement
+	* @param ElementInterface	Object associated NavRelevantInterface to access NavigationParent when registered as children
+	* @param UpdateFlags		Flags indicating in which context the method is called to allow/forbid certain operations
+	*
+	* @return True if associated NavOctreeElement has been removed or pending update has been invalidated; false otherwise.
+	*/
+	bool UnregisterNavOctreeElement(UObject& ElementOwner, INavRelevantInterface& ElementInterface, int32 UpdateFlags);
 	void UpdateNavOctreeElement(UObject& ElementOwner, INavRelevantInterface& ElementInterface, int32 UpdateFlags);
 	void UpdateNavOctreeParentChain(UObject& ElementOwner, bool bSkipElementOwnerUpdate = false);
 	bool UpdateNavOctreeElementBounds(UActorComponent& Comp, const FBox& NewBounds, const FBox& DirtyArea);	
