13a88838af3acb2cc3ecfb7bacc1043699c404d3
richard malo <Richard.Malo@epicgames.com>
2019-09-10 18:08:55 -0400

    Resubmitted cl 8576092 without circular dependency (Fixed 'Foliage' Engine Show Flag ignored by Actors painted by Foliage Tool) #rb patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 8579862 via CL 8617176
    #ROBOMERGE-BOT: (v409-8614070)
    
    [CL 8628801 by richard malo in Main branch]

--- Engine/Source/Runtime/Engine/Public/PrimitiveSceneProxy.h
+++ Engine/Source/Runtime/Engine/Public/PrimitiveSceneProxy.h
@@ -1042,6 +1042,8 @@ private:
 
 	/** Whether this should only draw in any editing mode*/
 	uint32 DrawInAnyEditMode : 1;
+
+	uint32 bIsFoliage : 1;
 #endif
 
 	/** Used for precomputed visibility */
