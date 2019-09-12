c8e559477f8be350b45dc895813a8b015be78c8a
Max Chen <Max.Chen@epicgames.com>
2019-09-12 14:57:10 -0400

    Sequencer: Fix incorrect enum initialization
    #rb andrew.rodham
    #jira UE-80189
    
    [CL 8659228 by Max Chen in Main branch]

--- Engine/Source/Runtime/MovieScene/Public/Evaluation/Blending/MovieSceneBlendType.h
+++ Engine/Source/Runtime/MovieScene/Public/Evaluation/Blending/MovieSceneBlendType.h
@@ -10,8 +10,6 @@
 UENUM()
 enum class EMovieSceneBlendType : uint8
 {
-	None = 0 UMETA(Hidden),
-
 	/** Blends all other weighted values together as an average of the total weight */
 	Absolute			= 0x1,
 	/** Applies this value as a sum total of all other additives */
