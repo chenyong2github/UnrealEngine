d87aa5d91fb96337a5702bdb5791ae4449bd22b6
Steve Robb <Steve.Robb@epicgames.com>
2019-09-12 11:07:57 -0400

    New non-member FStructuredArchiveField type are now recognised by UnrealHeaderTool. Refactoring to avoid the use of metadata which ought not to exist for passing data between the parser and the generator. Test case added.
    #rb graeme.thornton
    
    [CL 8655304 by Steve Robb in Dev-Core branch]

--- Engine/Source/Programs/UnrealHeaderTool/Private/CodeGenerator.cpp
+++ Engine/Source/Programs/UnrealHeaderTool/Private/CodeGenerator.cpp
@@ -2729,28 +2729,24 @@ void FNativeClassHeaderGenerator::ExportClassFromSourceFileInner(
 		ClassRange.Validate();
 	}
 
-	bool bHasSerializeToFArchive = Class->HasMetaData(TEXT("SerializeToFArchive"));
-	bool bHasSerializeToFStructuredArchive = Class->HasMetaData(TEXT("SerializeToFStructuredArchive"));
-
 	FString GeneratedSerializeFunctionCPP;
 	FString GeneratedSerializeFunctionHeaderMacroName;
 
-	if (bHasSerializeToFArchive != bHasSerializeToFStructuredArchive)
+	if (const FArchiveTypeDefinePair* ArchiveTypeDefinePair = GClassSerializerMap.Find(Class))
 	{
 		FString EnclosingDefines;
 		FUHTStringBuilder Boilerplate, BoilerPlateCPP;
 		FString MacroNameHeader, MacroNameCPP;
 		GeneratedSerializeFunctionHeaderMacroName = SourceFile.GetGeneratedMacroName(ClassData, TEXT("_ARCHIVESERIALIZER"));
 
-		if (!bHasSerializeToFArchive)
+		EnclosingDefines = ArchiveTypeDefinePair->EnclosingDefine;
+		if (ArchiveTypeDefinePair->ArchiveType == ESerializerArchiveType::StructuredArchiveRecord)
 		{
-			EnclosingDefines = Class->GetMetaData(TEXT("SerializeToFStructuredArchive"));
 			MacroNameHeader = TEXT("DECLARE_FARCHIVE_SERIALIZER");
 			MacroNameCPP = TEXT("IMPLEMENT_FARCHIVE_SERIALIZER");
 		}
 		else
 		{
-			EnclosingDefines = Class->GetMetaData(TEXT("SerializeToFArchive"));
 			MacroNameHeader = TEXT("DECLARE_FSTRUCTUREDARCHIVE_SERIALIZER");
 			MacroNameCPP = TEXT("IMPLEMENT_FSTRUCTUREDARCHIVE_SERIALIZER");
 		}
@@ -5228,18 +5224,15 @@ FNativeClassHeaderGenerator::FNativeClassHeaderGenerator(
 				}
 			}
 
-			bool bHasSerializeToFArchive = Class->HasMetaData(TEXT("SerializeToFArchive"));
-			bool bHasSerializeToFStructuredArchive = Class->HasMetaData(TEXT("SerializeToFStructuredArchive"));
-
-			if (bHasSerializeToFArchive != bHasSerializeToFStructuredArchive)
+			if (const FArchiveTypeDefinePair* ArchiveTypeDefinePair = GClassSerializerMap.Find(Class))
 			{
-				if (!bAddedStructuredArchiveFromArchiveHeader && !bHasSerializeToFArchive)
+				if (!bAddedStructuredArchiveFromArchiveHeader && ArchiveTypeDefinePair->ArchiveType == ESerializerArchiveType::StructuredArchiveRecord)
 				{
 					RelativeIncludes.AddUnique(TEXT("Serialization/StructuredArchive.h"));
 					bAddedStructuredArchiveFromArchiveHeader = true;
 				}
 
-				if (!bAddedArchiveUObjectFromStructuredArchiveHeader && !bHasSerializeToFStructuredArchive)
+				if (!bAddedArchiveUObjectFromStructuredArchiveHeader && ArchiveTypeDefinePair->ArchiveType == ESerializerArchiveType::Archive)
 				{
 					RelativeIncludes.AddUnique(TEXT("Serialization/ArchiveUObjectFromStructuredArchive.h"));
 					bAddedArchiveUObjectFromStructuredArchiveHeader = true;
