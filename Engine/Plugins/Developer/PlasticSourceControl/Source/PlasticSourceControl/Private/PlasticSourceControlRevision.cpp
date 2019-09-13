a592a28d1a79a7941dc066ae05b13f4441330f56
Rex Hill <Rex.Hill@epicgames.com>
2019-09-13 12:38:38 -0400

    Fix null pointer dereference in plastic source control #rb none #rnx
    [CL 8675131 by Rex Hill in Dev-Editor branch]

--- Engine/Plugins/Developer/PlasticSourceControl/Source/PlasticSourceControl/Private/PlasticSourceControlRevision.cpp
+++ Engine/Plugins/Developer/PlasticSourceControl/Source/PlasticSourceControl/Private/PlasticSourceControlRevision.cpp
@@ -37,11 +37,15 @@ bool FPlasticSourceControlRevision::Get( FString& InOutFilename ) const
 	{
 		bCommandSuccessful = true; // if the temp file already exists, reuse it directly
 	}
-	else
+	else if (State)
 	{
 		// Format the revision specification of the file, like revid:1230@repo@server:8087
 		const FString RevisionSpecification = FString::Printf(TEXT("revid:%d@%s"), RevisionId, *State->RepSpec);
 		bCommandSuccessful = PlasticSourceControlUtils::RunDumpToFile(PathToPlasticBinary, RevisionSpecification, InOutFilename);
+	}
+	else
+	{
+		bCommandSuccessful = false;
 	}
 	return bCommandSuccessful;
 }
