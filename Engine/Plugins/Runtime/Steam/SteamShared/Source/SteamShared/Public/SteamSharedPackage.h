1abf52351abbd963df36b0a98b24dbbe528f641d
Jake Leonard <jake.leonard@xa.epicgames.com>
2019-09-16 15:11:14 -0400

    Adding includetool override to remove CIS warnings for Steam package headers
    #JIRA: none
    #rnx
    #rb: none
    
    [CL 8698383 by Jake Leonard in Dev-Networking branch]

--- Engine/Plugins/Runtime/Steam/SteamShared/Source/SteamShared/Public/SteamSharedPackage.h
+++ Engine/Plugins/Runtime/Steam/SteamShared/Source/SteamShared/Public/SteamSharedPackage.h
@@ -5,6 +5,8 @@
 // Intended to be the last include in an exported class definition
 // Properly defines some members as "public to the module" vs "private to the consumer/user"
 
+// [[ IncludeTool: Inline ]]
+
 #undef PACKAGE_SCOPE
 #ifdef STEAMSHARED_PACKAGE
 #define PACKAGE_SCOPE public
