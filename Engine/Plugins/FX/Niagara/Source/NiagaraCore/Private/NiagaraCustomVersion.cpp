bcf079ea3881ad9da4b0890f4cb7e6f9d42ce1e9
shaun kime <Shaun.Kime@epicgames.com>
2019-09-16 09:51:18 -0400

    Change the SpawnRate and SpawnBurst_Instantaneous Niagara modules to account for the new fx.NiagaraGlobalSpawnCountScale CVar.
    There's three parts to this change:
    1) Hooking in the global spawn count scale.
    2) Clamping the burst spawn count to 1.
    3) Moving the scalability application to an earlier point in the SpawnRate module. The module ended up using two values (pre and post scaled) of the spawn rate in different locations. This might change some autotests.
    
    Updates the Niagara custom script version.
    
    #rb none
    
    
    #ROBOMERGE-OWNER: shaun.kime
    #ROBOMERGE-AUTHOR: morten.vassvik
    #ROBOMERGE-SOURCE: CL 8677274 via CL 8677318 via CL 8687985
    #ROBOMERGE-BOT: (v422-8689730)
    
    [CL 8690986 by shaun kime in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,6 +8,6 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3BD04C41, 0x37635E34, 0xDBC85417, 0x62BFD88C);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE2AC5117, 0x5B44424D, 0x91265F00, 0xA1AEB559);
 
 
