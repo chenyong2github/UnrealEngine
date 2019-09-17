b929d04f0df69edbf488500d34079bf7ccb578cc
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2019-09-17 11:01:22 -0400

    Do not create RVT resources on unsupported platforms #fiy jeremy.moore
    #ROBOMERGE-SOURCE: CL 8741179 via CL 8741182 via CL 8748529
    #ROBOMERGE-BOT: (v422-8689730)
    
    [CL 8748662 by dmitriy dyomin in Main branch]

--- Engine/Source/Runtime/Engine/Private/VT/RuntimeVirtualTexture.cpp
+++ Engine/Source/Runtime/Engine/Private/VT/RuntimeVirtualTexture.cpp
@@ -363,8 +363,15 @@ void URuntimeVirtualTexture::Initialize(IVirtualTexture* InProducer, FTransform 
 	WorldToUVTransformParameters[0] = VolumeToWorld.GetTranslation();
 	WorldToUVTransformParameters[1] = VolumeToWorld.GetUnitAxis(EAxis::X) * 1.f / VolumeToWorld.GetScale3D().X;
 	WorldToUVTransformParameters[2] = VolumeToWorld.GetUnitAxis(EAxis::Y) * 1.f / VolumeToWorld.GetScale3D().Y;
-
-	InitResource(InProducer, VolumeToWorld);
+	
+	if (UseVirtualTexturing(GMaxRHIFeatureLevel))
+	{
+		InitResource(InProducer, VolumeToWorld);
+	}
+	else
+	{
+		InitNullResource();		
+	}
 }
 
 void URuntimeVirtualTexture::Release()
