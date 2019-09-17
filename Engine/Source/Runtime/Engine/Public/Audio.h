aa725dada58bfe6a2d8a7825eaa2d53fa7d890a9
aaron mcleran <Aaron.McLeran@epicgames.com>
2019-09-17 04:52:59 -0400

    Adding LPF control to sound classes and sound mixes.
    - Hopefully fixed android compile error
    
    #rb Rob.Gay, Dan.Reynolds, Jimmy.Smith
    [FYI] Seth.Weedin
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: aaron.mcleran
    #ROBOMERGE-SOURCE: CL 8622766 via CL 8630051 via CL 8631618
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v422-8689730)
    
    [CL 8735242 by aaron mcleran in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Public/Audio.h
+++ Engine/Source/Runtime/Engine/Public/Audio.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AudioDefines.h"
 #include "Stats/Stats.h"
 #include "HAL/ThreadSafeBool.h"
 #include "Sound/SoundClass.h"
@@ -26,85 +27,6 @@ class USoundSourceBus;
 struct FActiveSound;
 struct FWaveInstance;
 struct FSoundSourceBusSendInfo;
-
-
-ENGINE_API DECLARE_LOG_CATEGORY_EXTERN(LogAudio, Warning, All);
-
-// Special log category used for temporary programmer debugging code of audio
-ENGINE_API DECLARE_LOG_CATEGORY_EXTERN(LogAudioDebug, Display, All);
- 
-/** 
- * Maximum number of channels that can be set using the ini setting
- */
-#define MAX_AUDIOCHANNELS				64
-
-
-/** 
- * Length of sound in seconds to be considered as looping forever
- */
-#define INDEFINITELY_LOOPING_DURATION	10000.0f
-
-/**
- * Some defaults to help cross platform consistency
- */
-#define SPEAKER_COUNT					6
-
-#define DEFAULT_LOW_FREQUENCY			600.0f
-#define DEFAULT_MID_FREQUENCY			1000.0f
-#define DEFAULT_HIGH_FREQUENCY			2000.0f
-
-#define MAX_VOLUME						4.0f
-#define MIN_PITCH						0.4f
-#define MAX_PITCH						2.0f
-
-#define MIN_SOUND_PRIORITY				0.0f
-#define MAX_SOUND_PRIORITY				100.0f
-
-#define DEFAULT_SUBTITLE_PRIORITY		10000.0f
-
-/**
- * Some filters don't work properly with extreme values, so these are the limits 
- */
-#define MIN_FILTER_GAIN					0.126f
-#define MAX_FILTER_GAIN					7.94f
-
-#define MIN_FILTER_FREQUENCY			20.0f
-#define MAX_FILTER_FREQUENCY			20000.0f
-
-#define MIN_FILTER_BANDWIDTH			0.1f
-#define MAX_FILTER_BANDWIDTH			2.0f
-
-/**
- * Audio stats
- */
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Active Sounds" ), STAT_ActiveSounds, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN(TEXT("Audio Evaluate Concurrency"), STAT_AudioEvaluateConcurrency, STATGROUP_Audio, );
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Audio Sources" ), STAT_AudioSources, STATGROUP_Audio , );
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Wave Instances" ), STAT_WaveInstances, STATGROUP_Audio , );
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Wave Instances Dropped" ), STAT_WavesDroppedDueToPriority, STATGROUP_Audio , );
-DECLARE_DWORD_COUNTER_STAT_EXTERN(TEXT("Virtualized Loops"), STAT_AudioVirtualLoops, STATGROUP_Audio, );
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Audible Wave Instances Dropped" ), STAT_AudibleWavesDroppedDueToPriority, STATGROUP_Audio , );
-DECLARE_DWORD_COUNTER_STAT_EXTERN(TEXT("Max Channels"), STAT_AudioMaxChannels, STATGROUP_Audio, );
-DECLARE_DWORD_COUNTER_STAT_EXTERN(TEXT("Max Stopping Sources"), STAT_AudioMaxStoppingSources, STATGROUP_Audio, );
-DECLARE_DWORD_COUNTER_STAT_EXTERN( TEXT( "Finished delegates called" ), STAT_AudioFinishedDelegatesCalled, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Finished delegates time" ), STAT_AudioFinishedDelegates, STATGROUP_Audio , );
-DECLARE_MEMORY_STAT_EXTERN( TEXT( "Audio Memory Used" ), STAT_AudioMemorySize, STATGROUP_Audio , );
-DECLARE_FLOAT_ACCUMULATOR_STAT_EXTERN( TEXT( "Audio Buffer Time" ), STAT_AudioBufferTime, STATGROUP_Audio , );
-DECLARE_FLOAT_ACCUMULATOR_STAT_EXTERN( TEXT( "Audio Buffer Time (w/ Channels)" ), STAT_AudioBufferTimeChannels, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Gathering WaveInstances" ), STAT_AudioGatherWaveInstances, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Processing Sources" ), STAT_AudioStartSources, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Updating Sources" ), STAT_AudioUpdateSources, STATGROUP_Audio , ENGINE_API);
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Updating Effects" ), STAT_AudioUpdateEffects, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Source Init" ), STAT_AudioSourceInitTime, STATGROUP_Audio , ENGINE_API);
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Source Create" ), STAT_AudioSourceCreateTime, STATGROUP_Audio , ENGINE_API);
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Submit Buffers" ), STAT_AudioSubmitBuffersTime, STATGROUP_Audio , ENGINE_API);
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Decompress Audio" ), STAT_AudioDecompressTime, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Decompress Vorbis" ), STAT_VorbisDecompressTime, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Prepare Audio Decompression" ), STAT_AudioPrepareDecompressionTime, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Prepare Vorbis Decompression" ), STAT_VorbisPrepareDecompressionTime, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Finding Nearest Location" ), STAT_AudioFindNearestLocation, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Decompress Streamed" ), STAT_AudioStreamedDecompressTime, STATGROUP_Audio , );
-DECLARE_CYCLE_STAT_EXTERN( TEXT( "Buffer Creation" ), STAT_AudioResourceCreationTime, STATGROUP_Audio , );
 
 /**
  * Channel definitions for multistream waves
@@ -349,6 +271,9 @@ public:
 
 	/** The low pass filter frequency to use */
 	float LowPassFilterFrequency;
+
+	/** The low pass filter frequency to use from sound class. */
+	float SoundClassFilterFrequency;
 
 	/** The low pass filter frequency to use if the sound is occluded. */
 	float OcclusionFilterFrequency;
