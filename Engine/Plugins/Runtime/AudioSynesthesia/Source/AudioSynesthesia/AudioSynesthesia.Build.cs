d2fad9544843ec805521f57e903008ce3d8893f4
phil popp <Phil.Popp@epicgames.com>
2019-09-23 17:15:55 -0400

    adding loudness analyzer module #rb Ethan.Geller Jimmy.Smith Aaron.McLeran Rob.Gay
    #ROBOMERGE-OWNER: phil.popp
    #ROBOMERGE-AUTHOR: phil.popp
    #ROBOMERGE-SOURCE: CL 8990880 via CL 8991013
    #ROBOMERGE-BOT: (v441-8974111)
    
    [CL 8996521 by phil popp in Main branch]

--- Engine/Plugins/Runtime/AudioSynesthesia/Source/AudioSynesthesia/AudioSynesthesia.Build.cs
+++ Engine/Plugins/Runtime/AudioSynesthesia/Source/AudioSynesthesia/AudioSynesthesia.Build.cs
@@ -17,6 +17,7 @@ namespace UnrealBuildTool.Rules
 					"CoreUObject",
 					"Engine",
 					"AudioMixer",
+					"AudioMixerCore",
                     "AudioAnalyzer"
                 }
             );
