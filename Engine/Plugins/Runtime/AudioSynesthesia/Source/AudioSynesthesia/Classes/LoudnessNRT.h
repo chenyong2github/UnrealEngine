e927d3f3144e68f976a973c547252ff21d5e94db
phil popp <Phil.Popp@epicgames.com>
2019-09-24 19:57:22 -0400

    Adding loudness analyzer to AudioSynesthesia #rb Aaron.McLeran Ethan.Geller Rob.Gay Jimmy.Smith
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: phil.popp
    #ROBOMERGE-SOURCE: CL 8976833 via CL 8976850 via CL 8978206
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v443-9013191)
    
    [CL 9065335 by phil popp in Dev-Networking branch]

---
+++ Engine/Plugins/Runtime/AudioSynesthesia/Source/AudioSynesthesia/Classes/LoudnessNRT.h
@@ -0,0 +1,117 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "AudioSynesthesiaNRT.h"
+#include "LoudnessNRT.generated.h"
+
+/** ELoudnessNRTCurveTypeEnum
+ *
+ * Enumeration of available equal loudness curves. Loudness curves based on IEC 61672-1:2013 Electroacoustics - Sound level meters - Part 1: Specifications.
+ */
+UENUM(BlueprintType)
+enum class ELoudnessNRTCurveTypeEnum : uint8
+{
+	/** Loudness Curve A Weighting. Commonly used in environmental sound measurements. Best for low level sounds. */
+	A 		UMETA(DisplayName="A"),
+	/** Loudness Curve B Weighting. Relative to "A", gives more precedence to frequencies below 1kHz. */
+	B		UMETA(DisplayName="B"),
+	/** Loudness Curve C Weighting. Relative to "A" and "B", gives more precedence to frequencies below 1kHz. */
+	C		UMETA(DisplayName="C"),
+	/** Loudness Curve D Weighting. Simlar to "B" but with an emphasis on presence in the 2kHz-6KHz frequency range. */
+	D		UMETA(DisplayName="D"),
+	/** No loudness curve weighting. */
+	None	UMETA(DisplayName="None"),
+};
+
+/** ULoudnessNRTSettings
+ *
+ * Settings for a ULoudnessNRT analyzer.
+ */
+UCLASS(Blueprintable)
+class AUDIOSYNESTHESIA_API ULoudnessNRTSettings : public UAudioSynesthesiaNRTSettings
+{
+	GENERATED_BODY()
+	public:
+
+		ULoudnessNRTSettings();
+
+		/** Number of seconds between loudness measurements */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=AudioAnalyzer, meta = (ClampMin = "0.01", ClampMax = "0.25"))
+		float AnalysisPeriod;
+
+		/** Minimum analysis frequency for calculating loudness. */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=AudioAnalyzer, meta = (ClampMin = "20.0", ClampMax = "20000"))
+		float MinimumFrequency;
+
+		/** Maximum analysis frequency for calculating loudness. */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=AudioAnalyzer, meta = (ClampMin = "20.0", ClampMax = "20000"))
+		float MaximumFrequency;
+
+		/** Type of equal loudness curve used in calculations */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=AudioAnalyzer)
+		ELoudnessNRTCurveTypeEnum CurveType;
+
+		/** dB level to denote silence.  Used when calculating normalized loudness. */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, AdvancedDisplay, Category=AudioAnalyzer, meta = (ClampMin = "-100.0", ClampMax="0"))
+		float NoiseFloorDb;
+
+		/** Convert ULoudnessNRTSettings to FLoudnessNRTSettings */
+		TUniquePtr<Audio::IAnalyzerNRTSettings> GetSettings();
+};
+
+
+/** ULoudnessNRT
+ *
+ * ULoudnessNRT calculates the temporal evolution of perceptual loudness for a given 
+ * sound. Loudness is available for individual channels or the overall sound asset. Normalized 
+ * loudness values convert the range to 0.0 to 1.0 where 0.0 is the noise floor and 1.0 is the
+ * maximum loudness of the particular sound.
+ */
+UCLASS(Blueprintable)
+class AUDIOSYNESTHESIA_API ULoudnessNRT : public UAudioSynesthesiaNRT
+{
+	GENERATED_BODY()
+
+	public:
+
+		ULoudnessNRT();
+
+		/** The settings for the audio analyzer.  */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=AudioAnalyzer)
+		ULoudnessNRTSettings* Settings;
+
+		/** Get the overall loudness of the analyzed sound at a given time. */
+		UFUNCTION(BlueprintCallable, Category="Audio Analyzer")
+		void GetLoudnessAtTime(const float InSeconds, float& OutLoudness);
+
+		/** Get a specific channel loudness of the analyzed sound at a given time. */
+		UFUNCTION(BlueprintCallable, Category="Audio Analyzer")
+		void GetChannelLoudnessAtTime(const float InSeconds, const int32 InChannel, float& OutLoudness);
+
+		/**
+		 * Get the normalized overall loudness of the analyzed sound at a given time. Normalized loudness
+		 * is always between 0.0 to 1.0. 0.0 refers to the noise floor while 1.0 refers to the maximum 
+		 * loudness in the sound.
+		 */
+		UFUNCTION(BlueprintCallable, Category="Audio Analyzer")
+		void GetNormalizedLoudnessAtTime(const float InSeconds, float& OutLoudness);
+
+		/**
+		 * Get a specific channel normalized loudness of the analyzed sound at a given time. Normalized 
+		 * loudness is always between 0.0 to 1.0. 0.0 refers to the noise floor while 1.0 refers to the 
+		 * maximum loudness in the sound.
+		 */
+		UFUNCTION(BlueprintCallable, Category="Audio Analyzer")
+		void GetNormalizedChannelLoudnessAtTime(const float InSeconds, const int32 InChannel, float& OutLoudness);
+
+		/** Convert ULoudnessNRTSettings to FLoudnessNRTSettings */
+ 		TUniquePtr<Audio::IAnalyzerNRTSettings> GetSettings() override;
+
+	protected:
+
+		/** Return the name of the IAudioAnalyzerNRTFactory associated with this UAudioAnalyzerNRT */
+		FName GetAnalyzerNRTFactoryName() const override;
+
+};
+
