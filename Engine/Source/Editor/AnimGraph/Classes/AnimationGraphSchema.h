e8953b56819f010e6b1cbf837eef416f9327a214
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2019-09-26 23:16:44 -0400

    Renaming an animation interface graph now also fixes up layer nodes that use that graph
    #rb Jurre.deBaare
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: thomas.sarkanen
    #ROBOMERGE-SOURCE: CL 9123175 via CL 9123176 via CL 9123177
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v443-9013191)
    
    [CL 9174815 by thomas sarkanen in Dev-Networking branch]

--- Engine/Source/Editor/AnimGraph/Classes/AnimationGraphSchema.h
+++ Engine/Source/Editor/AnimGraph/Classes/AnimationGraphSchema.h
@@ -99,6 +99,9 @@ class UAnimationGraphSchema : public UEdGraphSchema_K2
 	/** Conforms an anim graph to an interface function */
 	ANIMGRAPH_API static void ConformAnimGraphToInterface(UBlueprint* InBlueprint, UEdGraph& InGraph, UFunction* InFunction);
 
+	/** Conforms anim layer nodes to an interface desc by GUID */
+	ANIMGRAPH_API static void ConformAnimLayersByGuid(const UAnimBlueprint* InAnimBlueprint, const FBPInterfaceDescription& CurrentInterfaceDesc);
+
 	UE_DEPRECATED(4.24, "Function renamed, please use GetPositionForNewLinkedInputPoseNode")
 	ANIMGRAPH_API static FVector2D GetPositionForNewSubInputNode(UEdGraph& InGraph) { return GetPositionForNewLinkedInputPoseNode(InGraph); }
 
