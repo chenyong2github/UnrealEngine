85956ca3b04aa5e1303060b2794b8c5226271da3
jeremy moore <Jeremy.Moore@epicgames.com>
2019-09-29 16:50:18 -0400

    Prepare for batched runtime virtual texture updates
    #ROBOMERGE-SOURCE: CL 9289236 via CL 9290276 via CL 9290348 via CL 9290392
    #ROBOMERGE-BOT: (v452-9288972)
    
    [CL 9290546 by jeremy moore in Main branch]

--- Engine/Source/Runtime/Renderer/Private/SceneCore.h
+++ Engine/Source/Runtime/Renderer/Private/SceneCore.h
@@ -228,7 +228,7 @@ public:
 	/** Whether the mesh batch can be used for rendering to a virtual texture. */
 	uint8 bRenderToVirtualTexture : 1;
 	/** What virtual texture material type this mesh batch should be rendered with. */
-	uint8 RuntimeVirtualTextureMaterialType : ERuntimeVirtualTextureMaterialType_NumBits;
+	uint8 RuntimeVirtualTextureMaterialType : RuntimeVirtualTexture::MaterialType_NumBits;
 
 	/** Cached from vertex factory to avoid dereferencing VF in InitViews. */
 	uint8 bSupportsCachingMeshDrawCommands : 1;
