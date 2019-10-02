b89851176ea01551d1abd52fb52ccb82c9e05b9c
matt kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2019-10-02 08:37:41 -0400

    Add a callback on UWorld for when world post processing volumes are about to be processed.  This way systems that have many post processing volumes can do some pre-processing and avoid complex per-post process volume calculations.
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: matt.kuhlenschmidt
    #ROBOMERGE-SOURCE: CL 9284566 via CL 9284574 via CL 9284575 via CL 9284576 via CL 9284577
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v456-9359915)
    
    [CL 9363286 by matt kuhlenschmidt in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/World.cpp
+++ Engine/Source/Runtime/Engine/Private/World.cpp
@@ -7510,6 +7510,63 @@ void UWorld::InitializeSubsystems()
 	}
 }
 
+static void DoPostProcessVolume(IInterface_PostProcessVolume* Volume, FVector ViewLocation, FSceneView* SceneView)
+{
+	const FPostProcessVolumeProperties VolumeProperties = Volume->GetProperties();
+	if (!VolumeProperties.bIsEnabled)
+	{
+		return;
+	}
+
+	float DistanceToPoint = 0.0f;
+	float LocalWeight = FMath::Clamp(VolumeProperties.BlendWeight, 0.0f, 1.0f);
+
+	if (!VolumeProperties.bIsUnbound)
+	{
+		float SquaredBlendRadius = VolumeProperties.BlendRadius * VolumeProperties.BlendRadius;
+		Volume->EncompassesPoint(ViewLocation, 0.0f, &DistanceToPoint);
+
+		if (DistanceToPoint >= 0)
+		{
+			if (DistanceToPoint > VolumeProperties.BlendRadius)
+			{
+				// outside
+				LocalWeight = 0.0f;
+			}
+			else
+			{
+				// to avoid div by 0
+				if (VolumeProperties.BlendRadius >= 1.0f)
+				{
+					LocalWeight *= 1.0f - DistanceToPoint / VolumeProperties.BlendRadius;
+
+					check(LocalWeight >= 0 && LocalWeight <= 1.0f);
+				}
+			}
+		}
+		else
+		{
+			LocalWeight = 0;
+		}
+	}
+
+	if (LocalWeight > 0)
+	{
+		SceneView->OverridePostProcessSettings(*VolumeProperties.Settings, LocalWeight);
+	}
+}
+
+void UWorld::AddPostProcessingSettings(FVector ViewLocation, FSceneView* SceneView)
+{
+	OnBeginPostProcessSettings.Broadcast(ViewLocation);
+
+	for (IInterface_PostProcessVolume* PPVolume : PostProcessVolumes)
+	{
+		DoPostProcessVolume(PPVolume, ViewLocation, SceneView);
+	}
+}
+
+
 /**
 * Dump visible actors in current world.
 */
