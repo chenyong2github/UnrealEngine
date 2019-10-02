ec144747e387e342facbb7890bb5e1035b2f68fc
allan bentham <Allan.Bentham@epicgames.com>
2019-10-02 15:15:12 -0400

    Fixes for loading compute shaders when binary cache and LRU are active. #rb chris.babcock, Dmitriy.Dyomin
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: allan.bentham
    #ROBOMERGE-SOURCE: CL 9317455 via CL 9317962 via CL 9317964 via CL 9317966 via CL 9317970 via CL 9317977
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v456-9359915)
    
    [CL 9379505 by allan bentham in Dev-Networking branch]

--- Engine/Source/Runtime/OpenGLDrv/Public/OpenGLDrv.h
+++ Engine/Source/Runtime/OpenGLDrv/Public/OpenGLDrv.h
@@ -1032,6 +1032,7 @@ public:
 			false);
 	}
 
+	class FOpenGLLinkedProgram* GetLinkedComputeProgram(FRHIComputeShader* ComputeShaderRHI);
 
 	FBoundShaderStateRHIRef RHICreateBoundShaderState_OnThisThread(FRHIVertexDeclaration* VertexDeclaration, FRHIVertexShader* VertexShader, FRHIHullShader* HullShader, FRHIDomainShader* DomainShader, FRHIPixelShader* PixelShader, FRHIGeometryShader* GeometryShader, bool FromPSOFileCache);
 	void RHIPerFrameRHIFlushComplete();
