dc085184599d72781e4737d8c9bbb9fc79828f79
phil popp <Phil.Popp@epicgames.com>
2019-10-03 13:33:32 -0400

    Adding AudioMixer dependency and fixing include in order to fix build. #jira UEAU-262 #rb none [FYI] Marc.Audy
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: phil.popp
    #ROBOMERGE-SOURCE: CL 9397304 in //UE4/Main/...
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking) (v456-9359915)
    
    [CL 9402524 by phil popp in Dev-Networking branch]

--- Engine/Plugins/Runtime/AudioSynesthesia/Source/AudioSynesthesia/AudioSynesthesia.Build.cs
+++ Engine/Plugins/Runtime/AudioSynesthesia/Source/AudioSynesthesia/AudioSynesthesia.Build.cs
@@ -18,6 +18,7 @@ namespace UnrealBuildTool.Rules
 					"Engine",
 					"SignalProcessing",
 					"AudioMixerCore",
+					"AudioMixer",
                     "AudioAnalyzer"
                 }
             );
