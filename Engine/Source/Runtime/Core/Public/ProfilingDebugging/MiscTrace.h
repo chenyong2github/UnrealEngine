c86eba744e3c1a76d1300f91059f60fceb92cec0
Martin Ridgers <Martin.Ridgers@epicgames.com>
2019-10-17 09:23:47 -0400

    Allow the user to control if Misc and Stats tracing is compiled in or not.
    #rb cm
    #rnx
    
    [CL 9631383 by Martin Ridgers in Dev-Core branch]

--- Engine/Source/Runtime/Core/Public/ProfilingDebugging/MiscTrace.h
+++ Engine/Source/Runtime/Core/Public/ProfilingDebugging/MiscTrace.h
@@ -6,10 +6,12 @@
 #include "Trace/Config.h"
 #include "ProfilingDebugging/FormatArgsTrace.h"
 
+#if !defined(MISCTRACE_ENABLED)
 #if UE_TRACE_ENABLED && !UE_BUILD_SHIPPING
 #define MISCTRACE_ENABLED 1
 #else
 #define MISCTRACE_ENABLED 0
+#endif
 #endif
 
 enum ETraceFrameType
