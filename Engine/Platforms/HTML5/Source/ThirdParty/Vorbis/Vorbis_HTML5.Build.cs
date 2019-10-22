a62426396a8c3064dccbda91eb640211bed3f3f8
nick shin <nick.shin@xa.epicgames.com>
2019-10-22 16:50:01 -0400

    HTML5 update 3rd party libs build scripts (for binary lib files moved to "lib" folders) - part 2 of 2
    #jira UE-80050  Remove HTML5 code from engine
    #rb none
    #rnx
    
    #ROBOMERGE-SOURCE: CL 9751537 in //UE4/Release-4.24/...
    #ROBOMERGE-BOT: RELEASE (Release-4.24 -> Main) (v545-9751379)
    
    [CL 9751540 by nick shin in Main branch]

--- Engine/Platforms/HTML5/Source/ThirdParty/Vorbis/Vorbis_HTML5.Build.cs
+++ Engine/Platforms/HTML5/Source/ThirdParty/Vorbis/Vorbis_HTML5.Build.cs
@@ -7,11 +7,9 @@ using Tools.DotNETCommon;
 public class Vorbis_HTML5 : Vorbis
 {
 	protected override string LibRootDirectory { get { return Target.HTML5Platform.PlatformThirdPartySourceDirectory; } }
-	protected override string VorbisLibPath { get { return Path.GetDirectoryName(base.VorbisLibPath); } }
 
 	public Vorbis_HTML5(ReadOnlyTargetRules Target) : base(Target)
 	{
-		// library path
 		PublicAdditionalLibraries.Add(VorbisLibPath + "/libvorbis" + Target.HTML5OptimizationSuffix + ".bc");
 	}
 }
