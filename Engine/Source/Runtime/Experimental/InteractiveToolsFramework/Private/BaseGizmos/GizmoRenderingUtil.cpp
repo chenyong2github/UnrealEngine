193ce3952cff980512ff9b2d76739d2bd22ea0b0
ryan schmidt <Ryan.Schmidt@epicgames.com>
2019-10-30 16:51:02 -0400

    Copy fixes for several major UI bugs in Modeling Tools from Dev-Editor CL 9875272: Handle case where user disables Gizmo in DrawPolygon, resulting in it being nullptr in ctrl+click handler. Also update Gizmo position after it is re-shown. CL 9888557: resolve flickering/instability with Gizmo constant-size calculation and axis filtering CL 9888589: snap test in PolyEdit needs to be performed on world-space points, not local-space #rb none #jira none #rnx
    #ROBOMERGE-SOURCE: CL 9895478 in //UE4/Release-4.24/...
    #ROBOMERGE-BOT: RELEASE (Release-4.24 -> Main) (v558-9892490)
    
    [CL 9895484 by ryan schmidt in Main branch]

--- Engine/Source/Runtime/Experimental/InteractiveToolsFramework/Private/BaseGizmos/GizmoRenderingUtil.cpp
+++ Engine/Source/Runtime/Experimental/InteractiveToolsFramework/Private/BaseGizmos/GizmoRenderingUtil.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "BaseGizmos/GizmoRenderingUtil.h"
+#include "RHI.h"
 
 
 const FSceneView* GizmoRenderingUtil::FindActiveSceneView(
@@ -44,20 +45,59 @@ const FSceneView* GizmoRenderingUtil::FindActiveSceneView(
 }
 
 
+static double VectorDifferenceSqr(const FVector2D& A, const FVector2D& B)
+{
+	double ax = A.X, ay = A.Y;
+	double bx = B.X, by = B.Y;
+	ax -= bx;
+	ay -= by;
+	return ax*ax + ay*ay;
+}
+
+static double VectorDifferenceSqr(const FVector& A, const FVector& B)
+{
+	double ax = A.X, ay = A.Y, az = A.Z;
+	double bx = B.X, by = B.Y, bz = B.Z;
+	ax -= bx; 
+	ay -= by;
+	az -= bz;
+	return ax*ax + ay*ay + az*az;
+}
+
+// duplicates FSceneView::WorldToPixel but in double where possible (unfortunately WorldToScreen still in float)
+static FVector2D WorldToPixelDouble(const FSceneView* View, const FVector& Location)
+{
+	FVector4 ScreenPoint = View->WorldToScreen(Location);
+
+	double InvW = (ScreenPoint.W > 0.0 ? 1.0 : -1.0) / (double)ScreenPoint.W;
+	double Y = (GProjectionSignY > 0.0) ? (double)ScreenPoint.Y : 1.0 - (double)ScreenPoint.Y;
+
+	const FIntRect& UnscaledViewRect = View->UnscaledViewRect;
+	double PosX = (double)UnscaledViewRect.Min.X + (0.5 + (double)ScreenPoint.X * 0.5 * InvW) * (double)UnscaledViewRect.Width();
+	double PosY = (double)UnscaledViewRect.Min.Y + (0.5 - Y * 0.5 * InvW) * (double)UnscaledViewRect.Height();
+
+	return FVector2D((float)PosX, (float)PosY);
+}
+
 
 float GizmoRenderingUtil::CalculateLocalPixelToWorldScale(
 	const FSceneView* View,
 	const FVector& Location)
 {
-	FVector2D PixelA, PixelB;
-	View->WorldToPixel(Location, PixelA);
-	FVector OffsetPointWorld = Location + 0.01f*View->GetViewRight() + 0.01f*View->GetViewUp();
-	View->WorldToPixel(OffsetPointWorld, PixelB);
+	// To calculate this scale at Location, we project Location to screen and also project a second
+	// point at a small distance from Location in a camera-perpendicular plane, then measure 2D/3D distance ratio. 
+	// However, because some of the computations are done in float, there will be enormous numerical error 
+	// when the camera is very far from the location if the distance is relatively small. The "W" value
+	// below gives us a sense of this distance, so we make the offset relative to that
+	// (this does do one redundant WorldToScreen)
+	FVector4 LocationScreenPoint = View->WorldToScreen(Location);
+	float OffsetDelta = LocationScreenPoint.W * 0.01f;
 
-	float PixelDelta = (PixelA - PixelB).Size();
-	float WorldDelta = (Location - OffsetPointWorld).Size();
+	FVector2D PixelA = WorldToPixelDouble(View, Location);
+	FVector OffsetPointWorld = Location + OffsetDelta*View->GetViewRight() + OffsetDelta*View->GetViewUp();
+	FVector2D PixelB = WorldToPixelDouble(View, OffsetPointWorld);
 
-	float PixelToWorldScale = WorldDelta / PixelDelta;
-
-	return PixelToWorldScale;
+	double PixelDeltaSqr = VectorDifferenceSqr(PixelA, PixelB);
+	double WorldDeltaSqr = VectorDifferenceSqr(Location, OffsetPointWorld);
+	return (float)(sqrt(WorldDeltaSqr / PixelDeltaSqr));
 }
