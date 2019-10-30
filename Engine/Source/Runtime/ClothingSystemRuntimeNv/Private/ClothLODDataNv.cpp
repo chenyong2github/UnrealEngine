89ffedd265338f4102390386ae523ea97600a673
kriss gossart <Kriss.Gossart@xa.epicgames.com>
2019-10-30 14:12:32 -0400

    Reworked the Chaos Cloth Convex collisions to use a Chaos TConvex implicit. Also: -Regenerate the convexes' surface points for the legacy apex cloth assets (apparently the only use of convexes in apex import was to emulate box collisions). -Add debug draw visualization for the Chaos Cloth editor.
    #rb Jaco.VanDyk
    [FYI] Benn.Gallagher
    
    
    #ROBOMERGE-SOURCE: CL 9894041 via CL 9894049
    #ROBOMERGE-BOT: (v558-9892490)
    
    [CL 9894053 by kriss gossart in Main branch]

--- Engine/Source/Runtime/ClothingSystemRuntimeNv/Private/ClothLODDataNv.cpp
+++ Engine/Source/Runtime/ClothingSystemRuntimeNv/Private/ClothLODDataNv.cpp
@@ -9,3 +9,28 @@ UClothLODDataNv::UClothLODDataNv(const FObjectInitializer& Init)
 
 UClothLODDataNv::~UClothLODDataNv()
 {}
+
+void FClothLODData::MigrateTo(UClothLODDataNv* LodData) const
+{
+	PhysicalMeshData.MigrateTo(LodData->PhysicalMeshData);
+	LodData->CollisionData = CollisionData;
+#if WITH_CHAOS
+	// Rebuild surface points so that the legacy Apex convex collision data can also be used with Chaos
+	for (auto& Convex : LodData->CollisionData.Convexes)
+	{
+		if (!Convex.SurfacePoints.Num())
+		{
+			Convex.RebuildSurfacePoints();
+		}
+	}
+#endif // #if WITH_CHAOS
+#if WITH_EDITORONLY_DATA
+	LodData->ParameterMasks.SetNum(ParameterMasks.Num());
+	for (int i = 0; i < ParameterMasks.Num(); i++)
+	{
+		ParameterMasks[i].MigrateTo(&LodData->ParameterMasks[i]);
+	}
+#endif // WITH_EDITORONLY_DATA
+	LodData->TransitionUpSkinData = TransitionUpSkinData;
+	LodData->TransitionDownSkinData = TransitionDownSkinData;
+}
