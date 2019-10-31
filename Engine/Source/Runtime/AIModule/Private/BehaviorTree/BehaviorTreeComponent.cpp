7efceceb253284a831273fff714fc9cad1a7fc4f
yoan stamant <Yoan.Stamant@epicgames.com>
2019-10-31 14:23:18 -0400

    BehaviorTree: prevent tree restarts from decorator nodes requesting execution while processing higher priority execution.
    [at]lukasz.furman [at]maxime.mercier
    #rb lukasz.furman, maxime.mercier
    
    
    #ROBOMERGE-SOURCE: CL 9904958 via CL 9905213
    #ROBOMERGE-BOT: (v558-9892490)
    
    [CL 9921849 by yoan stamant in Main branch]

--- Engine/Source/Runtime/AIModule/Private/BehaviorTree/BehaviorTreeComponent.cpp
+++ Engine/Source/Runtime/AIModule/Private/BehaviorTree/BehaviorTreeComponent.cpp
@@ -857,6 +857,16 @@ void UBehaviorTreeComponent::RequestExecution(UBTCompositeNode* RequestedOn, int
 		return;
 	}
 
+	// while processing execution request, we do not request execution from nodes 
+	// with a priority lower than the processed node since it may no longer be
+	// in the active branch and will force a restart of the tree
+	if (InProgressExecutionNodeIndex.IsSet() && InProgressExecutionNodeIndex.TakesPriorityOver(ExecutionIdx) && RequestedBy->IsA(UBTDecorator::StaticClass()))
+	{
+		UE_VLOG(GetOwner(), LogBehaviorTree, Warning, TEXT("> skip: requesting execution from a lower priority node: %s < %s"), *ExecutionIdx.Describe(), *InProgressExecutionNodeIndex.Describe());
+		StoreDebuggerRestart(DebuggerNode, InstanceIdx, false);
+		return;
+	}
+
 	// when it's aborting and moving to higher priority node:
 	if (bSwitchToHigherPriority)
 	{
@@ -1461,6 +1471,8 @@ void UBehaviorTreeComponent::ProcessExecutionRequest()
 
 	if (!SearchData.bPostponeSearch)
 	{
+		PendingExecution.ExecutionNodeIndex = FBTNodeIndex(ExecutionRequest.ExecuteInstanceIdx, ExecutionRequest.ExecuteNode->GetExecutionIndex());
+
 		// clear request accumulator
 		ExecutionRequest = FBTNodeExecutionInfo();
 
@@ -1499,6 +1511,7 @@ void UBehaviorTreeComponent::ProcessPendingExecution()
 	}
 
 	FBTPendingExecutionInfo SavedInfo = PendingExecution;
+	InProgressExecutionNodeIndex = PendingExecution.ExecutionNodeIndex;
 	PendingExecution = FBTPendingExecutionInfo();
 
 	// collect all aux nodes that have lower priority than new task
@@ -1530,6 +1543,8 @@ void UBehaviorTreeComponent::ProcessPendingExecution()
 	{
 		OnTreeFinished();
 	}
+
+	InProgressExecutionNodeIndex = FBTNodeIndex();
 }
 
 void UBehaviorTreeComponent::RollbackSearchChanges()
