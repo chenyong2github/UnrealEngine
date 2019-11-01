b88003e0683213200c7e6c5a770801450e01f0dc
Jon Nabozny <Jon.Nabozny@epicgames.com>
2019-11-01 10:28:27 -0400

    Fixup deprecation warnings in project Templates
    #jira UE-82910
    #rb None
    
    [CL 9956549 by Jon Nabozny in Main branch]

--- Templates/TP_VehicleAdv/Source/TP_VehicleAdv/TP_VehicleAdvPawn.cpp
+++ Templates/TP_VehicleAdv/Source/TP_VehicleAdv/TP_VehicleAdvPawn.cpp
@@ -272,10 +272,10 @@ void ATP_VehicleAdvPawn::Tick(float Delta)
 	{
 		if ( (InputComponent) && (bInCarCameraActive == true ))
 		{
-			FRotator HeadRotation = InternalCamera->RelativeRotation;
+			FRotator HeadRotation = InternalCamera->GetRelativeRotation();
 			HeadRotation.Pitch += InputComponent->GetAxisValue(LookUpBinding);
 			HeadRotation.Yaw += InputComponent->GetAxisValue(LookRightBinding);
-			InternalCamera->RelativeRotation = HeadRotation;
+			InternalCamera->SetRelativeRotation(HeadRotation);
 		}
 	}	
 
