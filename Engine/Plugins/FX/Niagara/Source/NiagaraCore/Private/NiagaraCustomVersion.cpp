a9ae00f2fb6aef235144056f2ae447ca3655fe5e
shaun kime <Shaun.Kime@epicgames.com>
2019-11-04 16:29:09 -0500

    Fixing custom hlsl node to support DataInterfaces. Once you name the DI, you can invoke any supported DI method with a "." followed by the function invocation. We detect this behind the scenes and setup all the necessary connections. Can also now rename on pin creation.
    #rb frank.fella
    #jira UE-82867
    
    
    #ROBOMERGE-SOURCE: CL 9973687 via CL 9973692
    #ROBOMERGE-BOT: (v560-9963197)
    
    [CL 9973693 by shaun kime in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x79007C85, 0x2A9CA54A, 0xA7DABF24, 0x30A4EB07);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3A9CFB8E, 0x7E17484D, 0xA42624B4, 0xF2F4D750);
