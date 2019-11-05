859d962f6507b1efe7c651b2c318904f93b53a98
Lauren Barnes <lauren.barnes@epicgames.com>
2019-11-05 11:12:57 -0500

    Removed animation and extended duration for a notification toast #jira UE-82265 #rb trivial
    [CL 10016038 by Lauren Barnes in Dev-Editor branch]

--- Engine/Source/Editor/KismetWidgets/Private/SPinTypeSelector.cpp
+++ Engine/Source/Editor/KismetWidgets/Private/SPinTypeSelector.cpp
@@ -822,7 +822,9 @@ void SPinTypeSelector::OnSelectPinType(FPinTypeTreeItem InItem, FName InPinCateg
 		// inform user via toast why the type change was exceptional and clear IsMap/IsSetness because this type cannot be hashed:
 		const FText NotificationText = FText::Format(LOCTEXT("TypeCannotBeHashed", "Container type cleared because '{0}' does not have a GetTypeHash function. Maps and Sets require a hash function to insert and find elements"), UEdGraphSchema_K2::TypeToText(NewTargetPinType));
 		FNotificationInfo Info(NotificationText);
-		Info.ExpireDuration = 8.0f;
+		Info.FadeInDuration = 0.0f;
+		Info.FadeOutDuration = 0.0f;
+		Info.ExpireDuration = 10.0f;
 		FSlateNotificationManager::Get().AddNotification(Info);
 	}
 
