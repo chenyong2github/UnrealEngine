b42cd564906d1951c25541255b346fe6b9a497b2
devon penney <Devon.Penney@epicgames.com>
2019-11-08 17:27:57 -0500

    update custom version for error in main w/ crashing shader stage emitters #rb none
    #ROBOMERGE-SOURCE: CL 10088985 via CL 10088997
    #ROBOMERGE-BOT: (v574-10069753)
    
    [CL 10089070 by devon penney in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x633BEBEF, 0xF8DE4C65, 0xBCFBC403, 0x0D404C2B);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xDD83EBDB, 0x347040EC, 0x8077BE9F, 0x7635CD04);
 
