db55b4e63bf97dec4ff333e82d26bff3d28b16c5
jeremy moore <Jeremy.Moore@epicgames.com>
2019-11-08 18:01:04 -0500

    Add support for export of runtime virtual texture streaming mips to BMP #rb none
    #ROBOMERGE-OWNER: jeremy.moore
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 10089374 via CL 10089378 via CL 10089380 via CL 10089383
    #ROBOMERGE-BOT: (v574-10069753)
    
    [CL 10089425 by jeremy moore in Main branch]

--- Engine/Source/Editor/UnrealEd/Classes/Exporters/TextureExporterBMP.h
+++ Engine/Source/Editor/UnrealEd/Classes/Exporters/TextureExporterBMP.h
@@ -17,8 +17,16 @@ class UTextureExporterBMP : public UExporter
 	GENERATED_UCLASS_BODY()
 
 	virtual bool SupportsObject(UObject* Object) const override;
-	virtual bool ExportBinary( UObject* Object, const TCHAR* Type, FArchive& Ar, FFeedbackContext* Warn, int32 FileIndex = 0, uint32 PortFlags=0 ) override;
+	virtual int32 GetFileCount(UObject* Object) const override;
+	virtual FString GetUniqueFilename(const TCHAR* Filename, int32 FileIndex, int32 FileCount) override;
+	virtual bool ExportBinary(UObject* Object, const TCHAR* Type, FArchive& Ar, FFeedbackContext* Warn, int32 FileIndex = 0, uint32 PortFlags = 0) override;
+
+protected:
+	UTexture2D* GetExportTexture(UObject* Object) const;
 };
 
-
-
+UCLASS()
+class URuntimeVirtualTextureExporterBMP : public UTextureExporterBMP
+{
+	GENERATED_UCLASS_BODY()
+};
