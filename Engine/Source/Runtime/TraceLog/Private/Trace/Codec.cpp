4230b2dc436d4b4fdef205c46e54709a92f582b4
Martin Ridgers <Martin.Ridgers@epicgames.com>
2019-11-13 03:57:24 -0500

    Fixed MacOS and LZ4 symbol collision
    #rb none
    #rnx
    
    [CL 10121720 by Martin Ridgers in Dev-Core branch]

--- Engine/Source/Runtime/TraceLog/Private/Trace/Codec.cpp
+++ Engine/Source/Runtime/TraceLog/Private/Trace/Codec.cpp
@@ -8,9 +8,10 @@
 #	pragma warning(disable : 6239)
 #endif
 
-extern "C" {
+namespace Lz4
+{
 #include "ThirdParty/LZ4/lz4.c"
-} // extern "C"
+}
 
 #if defined(_MSC_VER)
 #	pragma warning(pop)
@@ -22,7 +23,7 @@ namespace Private {
 ////////////////////////////////////////////////////////////////////////////////
 int32 Encode(const void* Src, int32 SrcSize, void* Dest, int32 DestSize)
 {
-	return LZ4_compress_fast(
+	return Lz4::LZ4_compress_fast(
 		(const char*)Src,
 		(char*)Dest,
 		SrcSize,
@@ -34,7 +35,7 @@ int32 Encode(const void* Src, int32 SrcSize, void* Dest, int32 DestSize)
 ////////////////////////////////////////////////////////////////////////////////
 TRACELOG_API int32 Decode(const void* Src, int32 SrcSize, void* Dest, int32 DestSize)
 {
-	return LZ4_decompress_safe((const char*)Src, (char*)Dest, SrcSize, DestSize);
+	return Lz4::LZ4_decompress_safe((const char*)Src, (char*)Dest, SrcSize, DestSize);
 }
 
 } // namespace Private
