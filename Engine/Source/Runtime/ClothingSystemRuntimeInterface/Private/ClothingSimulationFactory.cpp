dc9c5553751969a32b487bec2b76ea5308f8bcdf
kriss gossart <Kriss.Gossart@xa.epicgames.com>
2019-11-13 10:33:19 -0500

    Set the correct default clothing simulation factory based on the target build defines. Add a new function that returns the correct default clothing simulation factory. Add a new p.Cloth.DefaultClothingFactoryClass console variable to change the default at runtime. Update the SkeletalMeshComponent code to use the correct default clothing simulation factory. Deprecate the IClothingSimulationFactoryClassProvider::GetDefaultSimulationFactoryClass() and add a new IClothingSimulationFactoryClassProvider::GetSimulationFactoryClass() function to remove any ambiguity about what the correct way to get the provider's class, or the default class.
    #rb Ryan.Kautzman, Jaco.VanDyk
    
    
    #ROBOMERGE-SOURCE: CL 10124312 via CL 10124362 via CL 10124409
    #ROBOMERGE-BOT: (v587-10111126)
    
    [CL 10124441 by kriss gossart in Main branch]

--- Engine/Source/Runtime/ClothingSystemRuntimeInterface/Private/ClothingSimulationFactory.cpp
+++ Engine/Source/Runtime/ClothingSystemRuntimeInterface/Private/ClothingSimulationFactory.cpp
@@ -1,5 +1,49 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "ClothingSimulationFactory.h"
+#include "HAL/IConsoleManager.h"
+#include "Features/IModularFeatures.h"
 
 const FName IClothingSimulationFactoryClassProvider::FeatureName = TEXT("ClothingSimulationFactoryClassProvider");
+
+namespace ClothingSimulationFactoryConsoleVariables
+{
+	TAutoConsoleVariable<FString> CVarDefaultClothingSimulationFactoryClass(
+		TEXT("p.Cloth.DefaultClothingSimulationFactoryClass"),
+#if WITH_CHAOS_CLOTHING
+		TEXT("ChaosClothingSimulationFactory"),  // Chaos is the default provider when Chaos Cloth is enabled
+#elif WITH_APEX_CLOTHING
+		TEXT("ClothingSimulationFactoryNv"),  // otherwise it's nv cloth
+#endif
+		TEXT("The class name of the default clothing simulation factory.\n")
+		TEXT("Known providers are:\n")
+#if WITH_CHAOS_CLOTHING
+		TEXT("ChaosClothingSimulationFactory\n")
+#endif
+#if WITH_APEX_CLOTHING
+		TEXT("ClothingSimulationFactoryNv\n")
+#endif
+		, ECVF_Cheat);
+}
+
+TSubclassOf<class UClothingSimulationFactory> UClothingSimulationFactory::GetDefaultClothingSimulationFactoryClass()
+{
+	TSubclassOf<UClothingSimulationFactory> DefaultClothingSimulationFactoryClass = nullptr;
+
+	const FString DefaultClothingSimulationFactoryClassName = ClothingSimulationFactoryConsoleVariables::CVarDefaultClothingSimulationFactoryClass.GetValueOnAnyThread();
+
+	TArray<IClothingSimulationFactoryClassProvider*> ClassProviders = IModularFeatures::Get().GetModularFeatureImplementations<IClothingSimulationFactoryClassProvider>(IClothingSimulationFactoryClassProvider::FeatureName);
+	for (const auto& ClassProvider : ClassProviders)
+	{
+		if (ClassProvider)
+		{
+			DefaultClothingSimulationFactoryClass = ClassProvider->GetClothingSimulationFactoryClass();
+
+			if (DefaultClothingSimulationFactoryClass->GetName() == DefaultClothingSimulationFactoryClassName)
+			{
+				break;
+			}
+		}
+	}
+	return DefaultClothingSimulationFactoryClass;
+}
