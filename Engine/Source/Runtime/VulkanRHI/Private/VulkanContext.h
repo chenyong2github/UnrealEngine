237b6951f810289675e6d366565b7864a7a0d36b
jonas meyer <Jonas.Meyer@epicgames.com>
2019-11-15 07:46:51 -0500

    Implement support for ReadOnly Depth/Writable Stencil in vulkan. Fixes an ensure on startup & Vulkan validation errors. #rb none #jira UE-84449
    #ROBOMERGE-SOURCE: CL 10236703 in //UE4/Release-4.24/...
    #ROBOMERGE-BOT: RELEASE (Release-4.24 -> Main) (v591-10236483)
    
    [CL 10236704 by jonas meyer in Main branch]

--- Engine/Source/Runtime/VulkanRHI/Private/VulkanContext.h
+++ Engine/Source/Runtime/VulkanRHI/Private/VulkanContext.h
@@ -212,6 +212,7 @@ public:
 	virtual void RHICopyTexture(FRHITexture* SourceTexture, FRHITexture* DestTexture, const FRHICopyTextureInfo& CopyInfo) final override;
 	virtual void RHITransitionResources(EResourceTransitionAccess TransitionType, FRHITexture** InRenderTargets, int32 NumTextures) final override;
 	virtual void RHITransitionResources(EResourceTransitionAccess TransitionType, EResourceTransitionPipeline TransitionPipeline, FRHIUnorderedAccessView** InUAVs, int32 NumUAVs, FRHIComputeFence* WriteComputeFence) final override;
+	virtual void RHITransitionResources(FExclusiveDepthStencil DepthStencilMode, FRHITexture* DepthTexture) final override;
 	virtual void RHICopyToStagingBuffer(FRHIVertexBuffer* SourceBuffer, FRHIStagingBuffer* DestinationStagingBuffer, uint32 Offset, uint32 NumBytes) final override;
 	virtual void RHIWriteGPUFence(FRHIGPUFence* Fence) final override;
 
