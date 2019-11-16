0bc80be79461979f9359374edb77cec583dc79e8
devon penney <Devon.Penney@epicgames.com>
2019-11-16 10:49:39 -0500

    update custom version for default shader stage index #rb none
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: devon.penney
    #ROBOMERGE-SOURCE: CL 10270719 via CL 10270737 via CL 10270745
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v591-10236483)
    
    [CL 10279940 by devon penney in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xDD83EBDB, 0x347040EC, 0x8077BE9F, 0x7635CD04);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x61E361A4, 0xD3C94ADB, 0xA93C3D8A, 0x507707CC);
 
