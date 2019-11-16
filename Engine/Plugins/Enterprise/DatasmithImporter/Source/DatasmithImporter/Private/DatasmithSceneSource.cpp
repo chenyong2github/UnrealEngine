446c61afff32a837540270c2c71c6fe382ba0df6
david lesage <David.Lesage@epicgames.com>
2019-11-16 10:33:02 -0500

    #jira 84521 Add Creo extension in the list of ZUp_RightHanded format Improve FDatasmithSceneSource::SetSourceFile in case of creo extension (asm.1) #rb johan.duparc
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.lesage
    #ROBOMERGE-SOURCE: CL 10267498 in //UE4/Release-4.24/... via CL 10267499
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v591-10236483)
    
    [CL 10279642 by david lesage in Dev-Networking branch]

--- Engine/Plugins/Enterprise/DatasmithImporter/Source/DatasmithImporter/Private/DatasmithSceneSource.cpp
+++ Engine/Plugins/Enterprise/DatasmithImporter/Source/DatasmithImporter/Private/DatasmithSceneSource.cpp
@@ -14,6 +14,11 @@ void FDatasmithSceneSource::SetSourceFile(const FString& InFilePath)
 	FilePath = InFilePath;
 	SceneDeducedName = FPaths::GetBaseFilename(FilePath);
 	FileExtension = FPaths::GetExtension(FilePath);
+	if (FCString::IsNumeric(*FileExtension))
+	{
+		FileExtension = FPaths::GetExtension(SceneDeducedName) + TEXT(".") + FileExtension;
+		SceneDeducedName = FPaths::GetBaseFilename(SceneDeducedName);
+	}
 }
 
 void FDatasmithSceneSource::SetSceneName(const FString& InSceneName)
