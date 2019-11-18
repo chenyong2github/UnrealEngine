2178d5f028aeea36a3d93a8078940935e59d73f7
andrey yamashev <andrey.yamashev@xa.epicgames.com>
2019-11-18 08:52:46 -0500

    Added DisplayClusterRootActor
    #jira UE-84448
    #rb simon.tourangeau
    
    #ROBOMERGE-SOURCE: CL 10285483 in //UE4/Release-4.24/...
    #ROBOMERGE-BOT: RELEASE (Release-4.24 -> Main) (v591-10236483)
    
    [CL 10285486 by andrey yamashev in Main branch]

---
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Public/DisplayClusterRootActor.h
@@ -0,0 +1,82 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Camera/PlayerCameraManager.h"
+
+#include "DisplayClusterEnums.h"
+
+#include "DisplayClusterRootActor.generated.h"
+
+class UDisplayClusterRootComponent;
+class UMaterial;
+
+
+/**
+ * VR root. This contains nDisplay VR hierarchy in the game.
+ */
+UCLASS()
+class DISPLAYCLUSTER_API ADisplayClusterRootActor
+	: public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADisplayClusterRootActor(const FObjectInitializer& ObjectInitializer);
+
+	inline UDisplayClusterRootComponent* GetDisplayClusterRootComponent() const
+	{ return DisplayClusterRootComponent; }
+
+public:
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// AActor
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	virtual void BeginPlay() override;
+	virtual void BeginDestroy() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	
+public:
+	bool GetShowProjectionScreens() const
+	{ return bShowProjectionScreens; }
+
+	UMaterial* GetProjectionScreenMaterial() const
+	{ return ProjectionScreensMaterial; }
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "DisplayCluster", meta = (DisplayName = "Exit when ESC pressed"))
+	bool bExitOnEsc;
+
+	UPROPERTY(EditAnywhere, Category = "DisplayCluster", meta = (DisplayName = "Show projection screens"))
+	bool bShowProjectionScreens;
+
+	UPROPERTY(EditAnywhere, Category = "DisplayCluster", meta = (DisplayName = "Projection screens material"))
+	UMaterial* ProjectionScreensMaterial;
+
+#if WITH_EDITORONLY_DATA
+public:
+	FString GetEditorConfigPath() const
+	{ return EditorConfigPath; }
+
+	FString GetEditorNodeId() const
+	{ return EditorNodeId; }
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "DisplayCluster (Editor only)", meta = (DisplayName = "Config file"))
+	FString EditorConfigPath;
+
+	UPROPERTY(EditAnywhere, Category = "DisplayCluster (Editor only)", meta = (DisplayName = "Node ID"))
+	FString EditorNodeId;
+#endif
+
+protected:
+	/** Camera component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DisplayCluster")
+	UDisplayClusterRootComponent* DisplayClusterRootComponent;
+
+private:
+	// Current operation mode
+	EDisplayClusterOperationMode OperationMode = EDisplayClusterOperationMode::Disabled;
+};
