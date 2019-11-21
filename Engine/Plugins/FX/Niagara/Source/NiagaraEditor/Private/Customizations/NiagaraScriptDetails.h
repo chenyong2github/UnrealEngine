7babd6ab137cb0bdf7cac40eefd5ee4978cb9c4c
frank fella <Frank.Fella@epicgames.com>
2019-11-20 22:34:12 -0500

    Niagara - Fix non-unity issues.
    #rb none
    
    
    #ROBOMERGE-OWNER: marc.audy
    #ROBOMERGE-AUTHOR: frank.fella
    #ROBOMERGE-SOURCE: CL 10331288 via CL 10332656 via CL 10332657
    #ROBOMERGE-BOT: (v593-10286020)
    
    [CL 10332658 by frank fella in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Customizations/NiagaraScriptDetails.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Customizations/NiagaraScriptDetails.h
@@ -4,11 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "IDetailCustomization.h"
+#include "IPropertyTypeCustomization.h"
 #include "Input/Reply.h"
 
 class FNiagaraScriptViewModel;
 class INiagaraParameterCollectionViewModel;
 class FNiagaraMetaDataCustomNodeBuilder;
+class IPropertyHandle;
+class FDetailWidgetRow;
+class IDetailChildrenBuilder;
+class IPropertyTypeCustomizationUtils;
 
 class FNiagaraScriptDetails : public IDetailCustomization
 {
