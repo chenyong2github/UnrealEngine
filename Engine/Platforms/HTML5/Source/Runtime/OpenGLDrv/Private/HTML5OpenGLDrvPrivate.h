9a07d6970f4b4df755a185807d37e61a0a4329fb
nick shin <nick.shin@xa.epicgames.com>
2019-11-21 19:28:32 -0500

    [RoboMerge] Change 10325146 caused blockage in RELEASE Release-4.24
    "I will merge conflict CL #10325146 to Main myself."
    
    CL #10325146: HTML5 platforms extension deleting from perforce repo
    
    #jira UE-80050  Remove HTML5 code from engine
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: nick.shin
    #ROBOMERGE-SOURCE: CL 10325680 in //UE4/Main/...
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v594-10333955)
    
    [CL 10364612 by nick shin in Dev-Networking branch]

--- Engine/Platforms/HTML5/Source/Runtime/OpenGLDrv/Private/HTML5OpenGLDrvPrivate.h
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-/*=============================================================================
-	*HTML5*OpenGLDrvPrivate.h: Platform Extension: Private HTML5 OpenGL includes
-=============================================================================*/
-
-#include "HTML5OpenGL.h"
-
