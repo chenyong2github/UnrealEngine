f3408195e2367622f71adfe39e186fccdac32a1b
nick shin <nick.shin@xa.epicgames.com>
2019-11-24 14:49:36 -0500

    [RoboMerge] Change 10325146 caused blockage in RELEASE Release-4.24
    "I will merge conflict CL #10325146 to Main myself."
    
    CL #10325146: HTML5 platforms extension deleting from perforce repo
    
    #jira UE-80050  Remove HTML5 code from engine
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: nick.shin
    #ROBOMERGE-SOURCE: CL 10325680 in //UE4/Main/...
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v594-10333955)
    
    [CL 10386824 by nick shin in Dev-Core branch]

--- Engine/Platforms/HTML5/Build/BatchFiles/old/CookSwinger.bat
+++
@@ -1,5 +0,0 @@
-set GAME=UE4
-pushd ..\..\..\..\
-cd Engine\Binaries\Win64\
-UE4Editor-Cmd.exe ..\..\..\Samples\SampleGames\SwingNinja\SwingNinja.uproject -run=cook -targetplatform=HTML5 -Map=P_Starting+SimpleLevel2
-popd 
