8b51ee9226d55132c2f85b78165d7f7d8c3141e1
alejandro aguilar <AlejandroAguilar@themultiplayerguys.com>
2019-11-25 03:54:16 -0500

    Exposing maximum number of concurrent players for hotfixing
    [at]Sam.Zamani [at]Rob.Cannaday
    
    
    #ROBOMERGE-SOURCE: CL 10407771 via CL 10407773 via CL 10407774
    #ROBOMERGE-BOT: (v594-10333955)
    
    [CL 10407775 by alejandro aguilar in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/GameViewportClient.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/GameViewportClient.h
@@ -71,7 +71,8 @@ public:
 	/** Array of the screen data needed for all the different splitscreen configurations */
 	TArray<struct FSplitscreenData> SplitscreenInfo;
 
-	int32 MaxSplitscreenPlayers;
+	UPROPERTY(Config)
+	int32 MaxSplitscreenPlayers = 4;
 
 	/** if true then the title safe border is drawn */
 	uint32 bShowTitleSafeZone:1;
