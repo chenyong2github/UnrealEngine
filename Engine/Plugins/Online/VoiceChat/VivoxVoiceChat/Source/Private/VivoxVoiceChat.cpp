9eba97d817d6fe954487b2a151c1847ef8261b8e
michael kirzinger <Michael.Kirzinger@epicgames.com>
2019-11-25 17:31:59 -0500

    Fix CIS
    #ROBOMERGE-SOURCE: CL 10432103 via CL 10432107
    #ROBOMERGE-BOT: (v594-10333955)
    
    [CL 10432110 by michael kirzinger in Main branch]

--- Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Private/VivoxVoiceChat.cpp
+++ Engine/Plugins/Online/VoiceChat/VivoxVoiceChat/Source/Private/VivoxVoiceChat.cpp
@@ -2070,7 +2070,7 @@ FString FVivoxVoiceChat::GetChannelNameFromUri(const VivoxClientApi::Uri& Channe
 		int32 Channel3dParametersIndex = Channel.Find(TEXT("!p-"));
 		if (Channel3dParametersIndex != INDEX_NONE)
 		{
-			Channel = Channel.LeftInline(Channel3dParametersIndex);
+			Channel.LeftInline(Channel3dParametersIndex);
 		}
 		return Channel;
 	}
