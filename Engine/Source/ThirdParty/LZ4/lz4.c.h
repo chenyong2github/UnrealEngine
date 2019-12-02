1e7955b0a11c816a972b09498df08c0e99f27034
Martin Ridgers <Martin.Ridgers@epicgames.com>
2019-12-02 10:50:28 -0500

    Modified LZ4 so it can be optionally wrapped in a namespace if LZ4_NAMESPACE is defined
    #rb none
    #rnx
    #ushell-unshelve of 10483450
    
    [CL 10483704 by Martin Ridgers in Dev-Core branch]

--- Engine/Source/ThirdParty/LZ4/lz4.c.h
+++ Engine/Source/ThirdParty/LZ4/lz4.c.h
@@ -242,6 +242,9 @@ static int g_debuglog_enable = 1;
 **************************************/
 #if defined(__cplusplus) || (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) /* C99 */)
 # include <stdint.h>
+
+LZ4_BEGIN_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
+
   typedef  uint8_t BYTE;
   typedef uint16_t U16;
   typedef uint32_t U32;
@@ -611,9 +614,13 @@ int LZ4_sizeofState() { return LZ4_STREAMSIZE; }
 /*-************************************
 *  Internal Definitions used in Tests
 **************************************/
+LZ4_END_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
+
 #if defined (__cplusplus)
 extern "C" {
 #endif
+
+LZ4_BEGIN_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
 
 int LZ4_compress_forceExtDict (LZ4_stream_t* LZ4_dict, const char* source, char* dest, int srcSize);
 
@@ -621,9 +628,13 @@ int LZ4_decompress_safe_forceExtDict(const char* source, char* dest,
                                      int compressedSize, int maxOutputSize,
                                      const void* dictStart, size_t dictSize);
 
+LZ4_END_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
+
 #if defined (__cplusplus)
 }
 #endif
+
+LZ4_BEGIN_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
 
 /*-******************************
 *  Compression functions
@@ -2410,3 +2421,5 @@ char* LZ4_slideInputBuffer (void* state)
 }
 
 #endif   /* LZ4_COMMONDEFS_ONLY */
+
+LZ4_END_NAMESPACE // EPIC MOD : Wrap library in an optional namespace
