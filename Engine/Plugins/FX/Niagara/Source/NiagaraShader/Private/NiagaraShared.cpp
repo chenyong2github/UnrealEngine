f4d82c44fe5121ea50774c408f63c08992eab4a4
graeme thornton <Graeme.Thornton@epicgames.com>
2019-12-02 07:34:31 -0500

    Make Niagara material loading correctly pass the "loaded from cooked material" parameter around so we can load cooked data
    #rb simon.tovey
    
    
    #ROBOMERGE-SOURCE: CL 10482431 via CL 10482432
    #ROBOMERGE-BOT: (v606-10482310)
    
    [CL 10482433 by graeme thornton in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShared.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShared.cpp
@@ -145,7 +145,7 @@ void FNiagaraShaderScript::RegisterShaderMap()
 {
 	if (GameThreadShaderMap)
 	{
-		GameThreadShaderMap->RegisterSerializedShaders(false);
+		GameThreadShaderMap->RegisterSerializedShaders(bLoadedFromCookedMaterial);
 	}
 }
 
@@ -176,6 +176,11 @@ void FNiagaraShaderScript::SerializeShaderMap(FArchive& Ar)
 {
 	bool bCooked = Ar.IsCooking();
 	Ar << bCooked;
+
+	if (Ar.IsLoading())
+	{
+		bLoadedFromCookedMaterial = bCooked;
+	}
 
 	if (FPlatformProperties::RequiresCookedData() && !bCooked && Ar.IsLoading())
 	{
@@ -211,7 +216,7 @@ void FNiagaraShaderScript::SerializeShaderMap(FArchive& Ar)
 			if (bValid)
 			{
 				TRefCountPtr<FNiagaraShaderMap> LoadedShaderMap = new FNiagaraShaderMap();
-				LoadedShaderMap->Serialize(Ar);
+				LoadedShaderMap->Serialize(Ar, true, true);
 
 				// Toss the loaded shader data if this is a server only instance
 				//@todo - don't cook it in the first place
