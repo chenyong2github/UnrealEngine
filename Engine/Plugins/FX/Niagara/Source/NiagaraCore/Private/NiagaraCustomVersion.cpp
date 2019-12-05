f5a6fbfdf77534889fdd66e2078f3ec052dbd3e4
arne schober <Arne.Schober@epicgames.com>
2019-12-05 14:40:50 -0500

    FNM - implemented niagarashader atomic compaticion for Console using wave intrinsics. Performance improvement on XB1S is about 33% and PS4P shows 10%. (The XB1 implementation was provided by Stu) #RB Stu.McKenna #jira
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: arne.schober
    #ROBOMERGE-SOURCE: CL 10491045 via CL 10491260 via CL 10491302
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v606-10482310)
    
    [CL 10547862 by arne schober in Dev-Core branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6D2E1207, 0x63F99C45, 0xBC426396, 0xFBF80045);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x02ABFEE7, 0x88B02847, 0x85046386, 0x8BD6F349);
 
