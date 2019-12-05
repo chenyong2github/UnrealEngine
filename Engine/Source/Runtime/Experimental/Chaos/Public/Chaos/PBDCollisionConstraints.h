be5dc3d91a204e37b18db2d80b366f544fdb4e87
brice criswell <Brice.Criswell@epicgames.com>
2019-12-05 10:40:26 -0500

    Chaos : Collisions
    - renaming and cleanup.
    
    
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 10517145 via CL 10517146 via CL 10517148
    #ROBOMERGE-BOT: (v606-10482310)
    
    [CL 10517152 by brice criswell in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDCollisionConstraints.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDCollisionConstraints.h
@@ -57,7 +57,7 @@ public:
 	using FConstraintContainerHandle = TPBDCollisionConstraintHandle<T, d>;
 	using FConstraintBase = TCollisionConstraintBase<T, d>;
  	using FPointContactConstraint = TRigidBodyPointContactConstraint<T, d>;
-	using FPlaneContactConstraint = TRigidBodyIterativeContactConstraint<T, d>;
+	using FIterativeContactConstraint = TRigidBodyIterativeContactConstraint<T, d>;
 	using FConstraintHandleAllocator = TConstraintHandleAllocator<TPBDCollisionConstraints<T, d>>;
 	using FConstraintContainerHandleKey = typename TPBDCollisionConstraintHandle<T, d>::FHandleKey;
 
@@ -221,7 +221,7 @@ private:
 	const TPBDRigidsSOAs<T,d>& Particles;
 
 	TArray<FPointContactConstraint> PointConstraints;
-	TArray<FPlaneContactConstraint> PlaneConstraints;
+	TArray<FIterativeContactConstraint> IterativeConstraints;
 
 	TMap< FConstraintContainerHandleKey, FConstraintContainerHandle* > Manifolds;
 	TArray<FConstraintContainerHandle*> Handles;
