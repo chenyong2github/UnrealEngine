9bfc5e223c05449d8e49af082186dd7a7783ea53
rui figueira <Rui.Figueira@epicgames.com>
2019-12-05 20:33:04 -0500

    Missing #include
    #jira UE-85346
    #rb NONE
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rui.figueira
    #ROBOMERGE-SOURCE: CL 10502035 in //UE4/Main/...
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v606-10482310)
    
    [CL 10576335 by rui figueira in Dev-Networking branch]

--- Engine/Source/Runtime/AVEncoder/Public/AVEncoder.cpp
+++ Engine/Source/Runtime/AVEncoder/Public/AVEncoder.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "AVEncoder.h"
+#include "AVEncoderCommon.h"
 
 #if PLATFORM_WINDOWS
 	#include "Microsoft/Windows/NvVideoEncoder.h"
