44c1cc78b1134ed085c483a6433e9f261c653f97
uriel doyon <uriel.doyon@xa.epicgames.com>
2019-12-09 15:59:53 -0500

    Added a general texture streaming framework to allow custom implementations to fill in mip data (from other source than the cooked mips). User must derive a class from UTextureMipDataProviderFactory and add it to the UTexture::AssetUserData. This one has to allocate a custom implementation of FTextureMipDataProvider to fill the streamed in mips with it's own strategy. [REVIEW] jian.riu
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: uriel.doyon
    #ROBOMERGE-SOURCE: CL 10516726 via CL 10516727 via CL 10516730
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v608-10590470)
    
    [CL 10617443 by uriel doyon in Dev-Networking branch]

---
+++ Engine/Source/Runtime/Engine/Private/Streaming/Texture2DMipDataProvider_DDC.h
@@ -0,0 +1,47 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+/*=============================================================================
+Texture2DMipDataProvider_DDC.h : Implementation of FTextureMipDataProvider using DDC requests.
+=============================================================================*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Streaming/TextureMipDataProvider.h"
+
+#if WITH_EDITORONLY_DATA
+
+/**
+* FTexture2DMipDataProvider_DDC implements FTextureMipAllocator using DDC requests from FDerivedDataCacheInterface.
+*/
+class FTexture2DMipDataProvider_DDC : public FTextureMipDataProvider
+{
+public:
+
+	FTexture2DMipDataProvider_DDC();
+	~FTexture2DMipDataProvider_DDC();
+
+
+	// ********************************************************
+	// ********* FTextureMipDataProvider implementation **********
+	// ********************************************************
+
+	void Init(const FTextureUpdateContext& Context, const FTextureUpdateSyncOptions& SyncOptions) final override;
+	int32 GetMips(const FTextureUpdateContext& Context, int32 StartingMipIndex, const FTextureMipInfoArray& MipInfos, const FTextureUpdateSyncOptions& SyncOptions) final override;
+	bool PollMips(const FTextureUpdateSyncOptions& SyncOptions) final override;
+	void CleanUp(const FTextureUpdateSyncOptions& SyncOptions) final override;
+	void Cancel(const FTextureUpdateSyncOptions& SyncOptions) final override;
+	ETickThread GetCancelThread() const final override;
+
+protected:
+
+	// Helper to route incomplete DDC requests to the FAbandonedDDCHandleManager.
+	void ReleaseDDCHandles();
+
+private:
+
+	// The list of DDC handle for each mips (0 based on the highest mip).
+	TArray<uint32, TInlineAllocator<MAX_TEXTURE_MIP_COUNT>> DDCHandles;
+};
+
+#endif //WITH_EDITORONLY_DATA
