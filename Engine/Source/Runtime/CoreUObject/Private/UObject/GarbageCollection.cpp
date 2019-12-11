84d3c7babf992f5f6dcd089f80f2ac473db848dc
Robert Manuszewski <Robert.Manuszewski@epicgames.com>
2019-12-11 02:23:33 -0500

    Fixing GC crashes when running with nativized blueprints
    #rb none
    
    [CL 10646806 by Robert Manuszewski in Dev-Core branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
@@ -2591,7 +2591,7 @@ void UClass::AssembleReferenceTokenStream(bool bForce)
 		{
 			check(ClassAddReferencedObjects != NULL);
 			const bool bKeepOuter = GetFName() != NAME_Package;
-			const bool bKeepClass = !(GetClassFlags() & CLASS_Native);
+			const bool bKeepClass = !(GetClassFlags() & CLASS_Native) || IsA(UDynamicClass::StaticClass());
 
 			ClassAddReferencedObjectsType AddReferencedObjectsFn = nullptr;
 #if !WITH_EDITOR
