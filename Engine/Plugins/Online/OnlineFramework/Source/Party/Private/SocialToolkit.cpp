05cba443accc5602232aaf939c37862ef0dc8b5e
michael ekberg <michael.ekberg@epicgames.com>
2019-12-12 10:14:47 -0500

    Added 'On<X>Complete' stubs in the SocialToolkit and called them from within their respective 'Handle<X>Complete' handlers. The game can now react to these notifications while preserving a robust API.
    [at]Don.Eubanks, [at]Matt.Stone, [at]Dan.Hertzka
    #tests Compile DebugGame Editor Win64 / Development Editor Win64 / Shipping Client / Ran it in PIE and verified flow.
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: michael.ekberg
    #ROBOMERGE-SOURCE: CL 10654255 via CL 10654260 via CL 10654273 via CL 10654283
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v610-10636431)
    
    [CL 10672607 by michael ekberg in Dev-Networking branch]

--- Engine/Plugins/Online/OnlineFramework/Source/Party/Private/SocialToolkit.cpp
+++ Engine/Plugins/Online/OnlineFramework/Source/Party/Private/SocialToolkit.cpp
@@ -691,6 +691,8 @@ void USocialToolkit::HandleReadFriendsListComplete(int32 LocalUserNum, bool bWas
 	}
 
 	HandleExistingPartyInvites(SubsystemType);
+
+	OnReadFriendsListComplete(LocalUserNum, bWasSuccessful, ListName, ErrorStr, SubsystemType);
 }
 
 void USocialToolkit::HandleQueryBlockedPlayersComplete(const FUniqueNetId& UserId, bool bWasSuccessful, const FString& ErrorStr, ESocialSubsystem SubsystemType)
@@ -714,6 +716,8 @@ void USocialToolkit::HandleQueryBlockedPlayersComplete(const FUniqueNetId& UserI
 			//@todo DanH: Only bother retrying on primary
 		}
 	}
+
+	OnQueryBlockedPlayersComplete(UserId, bWasSuccessful, ErrorStr, SubsystemType);
 }
 
 void USocialToolkit::HandleQueryRecentPlayersComplete(const FUniqueNetId& UserId, const FString& Namespace, bool bWasSuccessful, const FString& ErrorStr, ESocialSubsystem SubsystemType)
@@ -737,6 +741,8 @@ void USocialToolkit::HandleQueryRecentPlayersComplete(const FUniqueNetId& UserId
 			//@todo DanH: Only bother retrying on primary
 		}
 	}
+
+	OnQueryRecentPlayersComplete(UserId, Namespace, bWasSuccessful, ErrorStr, SubsystemType);
 }
 
 void USocialToolkit::HandleRecentPlayersAdded(const FUniqueNetId& LocalUserId, const TArray<TSharedRef<FOnlineRecentPlayer>>& NewRecentPlayers, ESocialSubsystem SubsystemType)
@@ -913,6 +919,8 @@ void USocialToolkit::HandleDeleteFriendComplete(int32 InLocalUserNum, bool bWasS
 			FormerFriend->NotifyUserUnfriended(SubsystemType);
 		}
 	}
+
+	OnDeleteFriendComplete(InLocalUserNum, bWasSuccessful, DeletedFriendId, ListName, ErrorStr, SubsystemType);
 }
 
 void USocialToolkit::HandleAcceptFriendInviteComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& InviterUserId, const FString& ListName, const FString& ErrorStr)
@@ -964,6 +972,8 @@ void USocialToolkit::HandleBlockPlayerComplete(int32 LocalUserNum, bool bWasSucc
 				}
 			});
 	}
+
+	OnBlockPlayerComplete(LocalUserNum, bWasSuccessful, BlockedPlayerId, ListName, ErrorStr, SubsystemType);
 }
 
 void USocialToolkit::HandleUnblockPlayerComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UnblockedPlayerId, const FString& ListName, const FString& ErrorStr, ESocialSubsystem SubsystemType)
@@ -976,6 +986,8 @@ void USocialToolkit::HandleUnblockPlayerComplete(int32 LocalUserNum, bool bWasSu
 			UnblockedUser->NotifyUserUnblocked(SubsystemType);
 		}
 	}
+
+	OnUnblockPlayerComplete(LocalUserNum, bWasSuccessful, UnblockedPlayerId, ListName, ErrorStr, SubsystemType);
 }
 
 //@todo DanH recent players: Where is the line for this between backend and game to update this stuff? #required
