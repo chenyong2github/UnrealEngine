21d132bb25639ab876c4b856461eaacf385bc6c9
uriel doyon <uriel.doyon@xa.epicgames.com>
2019-12-12 17:57:22 -0500

    Refactored Niagara GPU particles simulation aroun a few new functions : - InitConstants() : init constant accessed in the spawn and update logic. - InitSpawnVariables() : init properties for newly spawned particles. - LoadUpdateVariables() : Load properties from last simulation result. - ConditionalInterpolateParameters() : call InterpolateParameters() if required. - TransferAttibutes() : transfer data from MapSpawn to MapUpdate. - StoreUpdateVariables() : acquire a write index and store the resulting properties. - CopyInstance() : not yet implemented on GPU particles.
    Implemented a non intrinsics fallback for acquiring destination indices using group shared.
    
    Niagara GPU simulation shader THREADGROUP_SIZE is now platform dependent to optimize group shared syncs.
    
    #rb stu.mckenna, arne.schober
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: uriel.doyon
    #ROBOMERGE-SOURCE: CL 10596488 via CL 10596517 via CL 10596523
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v610-10636431)
    
    [CL 10695757 by uriel doyon in Dev-Core branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xAD86D23D, 0xA2C94302, 0xB4865706, 0x82E01289);
-
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xBCB9A1E7, 0xB6F54078, 0xBC9895E2, 0x1D5D4C0F);
