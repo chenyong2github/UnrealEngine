711cab84b13413bd917159c6e7489265b572d479
stefan boberg <Stefan.Boberg@epicgames.com>
2019-12-12 16:19:03 -0500

    Added initialization to satisfy PVS
    #rb trivial
    
    
    #ROBOMERGE-SOURCE: CL 10689961 via CL 10689970
    #ROBOMERGE-BOT: (v610-10636431)
    
    [CL 10689973 by stefan boberg in Main branch]

--- Engine/Source/Runtime/Core/Public/Containers/IndirectArray.h
+++ Engine/Source/Runtime/Core/Public/Containers/IndirectArray.h
@@ -188,7 +188,7 @@ public:
 		if (Ar.IsLoading())
 		{
 			// Load array.
-			int32 NewNum;
+			int32 NewNum = 0;
 			Ar << NewNum;
 			Empty(NewNum);
 			for (int32 Index = 0; Index < NewNum; Index++)
