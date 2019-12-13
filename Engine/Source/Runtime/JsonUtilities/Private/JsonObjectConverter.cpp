618f0aedc7aba0d3e3bea8aa71b3495dfd71a21a
bob tellez <Bob.Tellez@epicgames.com>
2019-12-13 03:56:40 -0500

    Backout CL 10127870. Probable cause for EdTest ensure
    [FYI] Steve.Robb
    
    #ROBOMERGE-OWNER: robert.manuszewski
    #ROBOMERGE-AUTHOR: bob.tellez
    #ROBOMERGE-SOURCE: CL 10631941 via CL 10631943 via CL 10631944
    #ROBOMERGE-BOT: CORE (Main -> Dev-Core) (v610-10636431)
    
    [CL 10706426 by bob tellez in Dev-Core branch]

--- Engine/Source/Runtime/JsonUtilities/Private/JsonObjectConverter.cpp
+++ Engine/Source/Runtime/JsonUtilities/Private/JsonObjectConverter.cpp
@@ -184,7 +184,7 @@ TSharedPtr<FJsonValue> FJsonObjectConverter::ObjectJsonCallback(FProperty* Prope
 {
 	if (FObjectProperty* ObjectProperty = CastField<FObjectProperty>(Property))
 	{
-		if (!ObjectProperty->HasAnyPropertyFlags(CPF_Transient)) // We are taking Transient to mean we don't want to serialize to Json either (could make a new flag if nessasary)
+		if (!ObjectProperty->HasAnyFlags(RF_Transient)) // We are taking Transient to mean we don't want to serialize to Json either (could make a new flag if nessasary)
 		{
 			TSharedRef<FJsonObject> Out = MakeShared<FJsonObject>();
 
