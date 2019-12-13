76f11f1b0c838f182a35ef0401d283aab9a0c15d
devin doucette <devin.doucette@epicgames.com>
2019-12-13 16:31:17 -0500

    Implement LexFromString from FStringView for primitive types
    These implementations are not optimized yet. They exist to allow conversion from FString to FStringView.
    
    #rb Matt.Peters, Steve.Robb
    
    [CL 10720867 by devin doucette in Dev-Core branch]

---
+++ Engine/Source/Runtime/Core/Public/String/LexFromString.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreTypes.h"
+
+class FStringView;
+
+CORE_API void LexFromString(int8& OutValue,   const FStringView& InString);
+CORE_API void LexFromString(int16& OutValue,  const FStringView& InString);
+CORE_API void LexFromString(int32& OutValue,  const FStringView& InString);
+CORE_API void LexFromString(int64& OutValue,  const FStringView& InString);
+CORE_API void LexFromString(uint8& OutValue,  const FStringView& InString);
+CORE_API void LexFromString(uint16& OutValue, const FStringView& InString);
+CORE_API void LexFromString(uint32& OutValue, const FStringView& InString);
+CORE_API void LexFromString(uint64& OutValue, const FStringView& InString);
+CORE_API void LexFromString(float& OutValue,  const FStringView& InString);
+CORE_API void LexFromString(double& OutValue, const FStringView& InString);
+CORE_API void LexFromString(bool& OutValue,   const FStringView& InString);
