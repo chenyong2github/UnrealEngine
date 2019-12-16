6d714d644f2bfdd646628f53618c0c235efa1942
Patrick Boutot <Patrick.Boutot@epicgames.com>
2019-12-16 09:44:35 -0500

    Fix CIS. Missing include introduced with CL 10730014. #rb none
    [CL 10733482 by Patrick Boutot in Dev-VirtualProduction branch]

--- Engine/Source/Runtime/Core/Private/Misc/Timecode.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/Timecode.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "Misc/Timecode.h"
+#include "HAL/IConsoleManager.h"
 
 
 static TAutoConsoleVariable<bool> CVarUseDropFormatTimecodeByDefaultWhenSupported(
