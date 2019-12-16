03537d859593395618a3bdab2da74a08fd1d4cc6
austin crismore <Austin.Crismore@epicgames.com>
2019-12-16 15:18:12 -0500

    Added missing SupportType funciton to Niagara Emitter Track.
    #jira UE-85714
    #rb Frank.Fella
    
    
    #ROBOMERGE-SOURCE: CL 10742287 via CL 10742325
    #ROBOMERGE-BOT: (v610-10636431)
    
    [CL 10742330 by austin crismore in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Sequencer/NiagaraSequence/MovieSceneNiagaraEmitterTrack.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Sequencer/NiagaraSequence/MovieSceneNiagaraEmitterTrack.h
@@ -76,6 +76,7 @@ public:
 	virtual void RemoveAllAnimationData() override { }
 	virtual bool HasSection(const UMovieSceneSection& Section) const override;
 	virtual void AddSection(UMovieSceneSection& Section) override;
+	virtual bool SupportsType(TSubclassOf<UMovieSceneSection> SectionClass) const override;
 	virtual void RemoveSection(UMovieSceneSection& Section) override;
 	virtual void RemoveSectionAt(int32 SectionIndex) override;
 	virtual bool IsEmpty() const override;
