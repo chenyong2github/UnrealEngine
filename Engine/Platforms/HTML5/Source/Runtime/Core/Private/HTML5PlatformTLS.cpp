017c7b9ba2d0c5ddb45ccf1ad185eb7bc70810f4
nick shin <nick.shin@Ben.Marsh_Unrealbot-02_GitHub>
2019-12-17 11:20:27 -0500

    [RoboMerge] Change 10325146 caused blockage in RELEASE Release-4.24
    "I will merge conflict CL #10325146 to Main myself."
    
    CL #10325146: HTML5 platforms extension deleting from perforce repo
    
    #jira UE-80050  Remove HTML5 code from engine
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ben.marsh
    #ROBOMERGE-AUTHOR: nick.shin
    #ROBOMERGE-SOURCE: CL 10325680 in //UE4/Main/...
    #ROBOMERGE-BOT: BUILD (Main -> Dev-Build) (v610-10636431)
    
    [CL 10764558 by nick shin in Dev-Build branch]

--- Engine/Platforms/HTML5/Source/Runtime/Core/Private/HTML5PlatformTLS.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-/*=============================================================================
-	HTML5PlatformTLS.cpp: HTML5 implementations of TLS functions
-=============================================================================*/
-
-#include "HTML5PlatformTLS.h"
-#include "Containers/Array.h"
-
