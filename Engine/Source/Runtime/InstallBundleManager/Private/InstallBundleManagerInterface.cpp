610e8b3fa33369058fd26a2508ec6f83285681f4
justin marcus <Justin.Marcus@epicgames.com>
2019-12-19 15:15:16 -0500

    Install Bundle Manager Refactor - Cleanup Interface - Fix some failing asserts - Fix engine sleeping during PSO caching - Enable New Bundle Manager
    [at]Daniel.Lamb [at]Hongyi.Yu
    
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: justin.marcus
    #ROBOMERGE-SOURCE: CL 10708901 via CL 10714861 via CL 10714946 via CL 10715183
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v610-10636431)
    
    [CL 10824155 by justin marcus in Dev-Networking branch]

--- Engine/Source/Runtime/InstallBundleManager/Private/InstallBundleManagerInterface.cpp
+++ Engine/Source/Runtime/InstallBundleManager/Private/InstallBundleManagerInterface.cpp
@@ -39,3 +39,43 @@ IInstallBundleManager* IInstallBundleManager::GetPlatformInstallBundleManager()
 	return Manager;
 }
 
+FInstallBundleRequestInfo IInstallBundleManager::RequestUpdateContent(FName BundleName, EInstallBundleRequestFlags Flags)
+{
+	return RequestUpdateContent(MakeArrayView(&BundleName, 1), Flags);
+}
+
+void IInstallBundleManager::GetContentState(FName BundleName, EInstallBundleGetContentStateFlags Flags, bool bAddDependencies, FInstallBundleGetContentStateDelegate Callback, FName RequestTag /*= TEXT("None")*/)
+{
+	return GetContentState(MakeArrayView(&BundleName, 1), Flags, bAddDependencies, MoveTemp(Callback), RequestTag);
+}
+
+void IInstallBundleManager::RequestRemoveContentOnNextInit(FName RemoveName, TArrayView<const FName> KeepNames /*= TArrayView<const FName>()*/)
+{
+	RequestRemoveContentOnNextInit(MakeArrayView(&RemoveName, 1), KeepNames);
+}
+
+void IInstallBundleManager::CancelRequestRemoveContentOnNextInit(FName BundleName)
+{
+	CancelRequestRemoveContentOnNextInit(MakeArrayView(&BundleName, 1));
+}
+
+void IInstallBundleManager::CancelUpdateContent(FName BundleName, EInstallBundleCancelFlags Flags)
+{
+	CancelUpdateContent(MakeArrayView(&BundleName, 1), Flags);
+}
+
+void IInstallBundleManager::PauseUpdateContent(FName BundleName)
+{
+	PauseUpdateContent(MakeArrayView(&BundleName, 1));
+}
+
+void IInstallBundleManager::ResumeUpdateContent(FName BundleName)
+{
+	ResumeUpdateContent(MakeArrayView(&BundleName, 1));
+}
+
+void IInstallBundleManager::UpdateContentRequestFlags(FName BundleName, EInstallBundleRequestFlags AddFlags, EInstallBundleRequestFlags RemoveFlags)
+{
+	UpdateContentRequestFlags(MakeArrayView(&BundleName, 1), AddFlags, RemoveFlags);
+}
+
