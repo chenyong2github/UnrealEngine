d4d8177970250f59df2d84ffc521f12f88794471
ben marsh <Ben.Marsh@epicgames.com>
2019-12-19 15:35:43 -0500

    Merging latest UGS fixes (incorrect compatible changelist in hotfix releases, option in issue settings window not being enabled)
    #rb none
    #rnx
    #jira UE-85874
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 10727579 in //UE4/Release-4.24/... via CL 10727580
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v610-10636431)
    
    [CL 10826048 by ben marsh in Dev-Networking branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.182.*")]
-[assembly: AssemblyFileVersion("1.182.0.0")]
+[assembly: AssemblyVersion("1.184.*")]
+[assembly: AssemblyFileVersion("1.184.0.0")]
