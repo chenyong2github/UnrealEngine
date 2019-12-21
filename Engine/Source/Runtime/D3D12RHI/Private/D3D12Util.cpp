4a3f95144a0c6c003e523f21a1985ac748082b24
rolando caloca <Rolando.Caloca@epicgames.com>
2019-12-21 15:41:04 -0500

    UE4R - Only show this message in windows #rb none #lockdown Cristina.Riveron #jira UE-86062
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rolando.caloca
    #ROBOMERGE-SOURCE: CL 10852994 in //UE4/Release-4.24/... via CL 10852996
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v610-10636431)
    
    [CL 10865846 by rolando caloca in Dev-Networking branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Util.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Util.cpp
@@ -110,7 +110,9 @@ static FString GetD3D12ErrorString(HRESULT ErrorCode, ID3D12Device* Device)
 		D3DERR(DXGI_ERROR_INVALID_CALL)
 		D3DERR(E_NOINTERFACE)
 		D3DERR(DXGI_ERROR_DEVICE_REMOVED)
+#if PLATFORM_WINDOWS
 		EMBED_DXGI_ERROR_LIST(D3DERR, )
+#endif
 		default:
 			ErrorCodeText = FString::Printf(TEXT("%08X"), (int32)ErrorCode);
 	}
