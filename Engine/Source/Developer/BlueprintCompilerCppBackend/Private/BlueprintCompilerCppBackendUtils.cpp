471d972e62aa227d77d9af578197637cbc90c49f
ryan durand <Ryan.Durand@epicgames.com>
2019-12-26 15:32:37 -0500

    Updating copyright for Engine Developer.
    #rnx
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 10869240 via CL 10869516 via CL 10869902
    #ROBOMERGE-BOT: (v613-10869866)
    
    [CL 10870584 by ryan durand in Main branch]

--- Engine/Source/Developer/BlueprintCompilerCppBackend/Private/BlueprintCompilerCppBackendUtils.cpp
+++ Engine/Source/Developer/BlueprintCompilerCppBackend/Private/BlueprintCompilerCppBackendUtils.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "BlueprintCompilerCppBackendUtils.h"
 #include "Misc/App.h"
--- Engine/Source/Developer/TraceServices/Private/Common/Utils.cpp
+++ Engine/Source/Developer/TraceServices/Private/Common/Utils.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Utils.h"
 #include "TraceServices/Containers/Tables.h"
