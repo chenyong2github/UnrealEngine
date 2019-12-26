627baf970a57ac2d9772fc8f7eb098b82a6ecd06
ryan durand <Ryan.Durand@epicgames.com>
2019-12-26 15:33:43 -0500

    Updating copyright for Engine Editor.
    #rnx
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 10869241 via CL 10869527 via CL 10869904
    #ROBOMERGE-BOT: (v613-10869866)
    
    [CL 10870586 by ryan durand in Main branch]

--- Engine/Source/Editor/PhysicsAssetEditor/Private/PhysicsAssetGraph/PhysicsAssetGraphSchema.cpp
+++ Engine/Source/Editor/PhysicsAssetEditor/Private/PhysicsAssetGraph/PhysicsAssetGraphSchema.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "PhysicsAssetGraphSchema.h"
 #include "Toolkits/GlobalEditorCommonCommands.h"
--- Engine/Source/Editor/UnrealEd/Private/Factories/PackFactory.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Factories/PackFactory.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 /*=============================================================================
 	PackFactory.cpp: Factory for importing asset and feature packs
