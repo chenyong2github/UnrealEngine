5954c1f85be2dc553325388d6591b556ee16d97f
ryan durand <Ryan.Durand@epicgames.com>
2019-12-26 23:08:00 -0500

    Updating remaining copyrights in Engine.
    #rnx
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 10869250 via CL 10869537 via CL 10869906
    #ROBOMERGE-BOT: (v613-10869866)
    
    [CL 10870965 by ryan durand in Main branch]

--- Engine/Source/ThirdParty/SDL2/docker/docker-build-sdl2.sh
+++ Engine/Source/ThirdParty/SDL2/docker/docker-build-sdl2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Unreal Engine 4 Build script for SDL2
-## Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+## Copyright Epic Games, Inc. All Rights Reserved.
 
 # Should be run in docker image, launched something like this (see RunMe.sh script):
 #   docker run --name ${ImageName} -v ${SCRIPT_DIR}/../../Vulkan:/Vulkan -v ${SDL_DIR}:/SDL-gui-backend -v ${SCRIPT_DIR}:/src ${Image} /src/docker-build-sdl2.sh
--- Engine/Source/ThirdParty/mtlpp/mtlpp-master-7efad47/interpose/MTIObject.hpp
+++ Engine/Source/ThirdParty/mtlpp/mtlpp-master-7efad47/interpose/MTIObject.hpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #ifndef MTIObject_hpp
 #define MTIObject_hpp
