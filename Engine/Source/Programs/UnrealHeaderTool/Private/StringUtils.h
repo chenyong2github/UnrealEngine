f71a47c374771fa115dff06aee049ef01e3c6644
marc audy <Marc.Audy@epicgames.com>
2019-12-30 15:05:12 -0500

    UHT optimizations part 1 - basic cleanup, minimal measurable gains #jira #rnx
    #ROBOMERGE-SOURCE: CL 10872174 via CL 10872175 via CL 10872176
    #ROBOMERGE-BOT: (v620-10870533)
    
    [CL 10872177 by marc audy in Main branch]

--- Engine/Source/Programs/UnrealHeaderTool/Private/StringUtils.h
+++ Engine/Source/Programs/UnrealHeaderTool/Private/StringUtils.h
@@ -10,21 +10,21 @@
  *
  * @param InClassName Class Name with a prefix to be stripped
  */
-FString GetClassNameWithPrefixRemoved(const FString InClassName);
+FString GetClassNameWithPrefixRemoved(const FString& InClassName);
 
 /** 
  * Attempts to strip the given class name of its affixed prefix. If no prefix exists, will return unchanged string.
  *
  * @param InClassName Class Name with a prefix to be stripped
  */
-FString GetClassNameWithoutPrefix(const FString& InClassNameOrFilename);
+FString GetClassNameWithoutPrefix(FString InClassNameOrFilename);
 
 /** 
  * Attempts to get class prefix. If the given class name does not start with a valid Unreal Prefix, it will return an empty string.
  *
  * @param InClassName Name w/ potential prefix to check
  */
-FString GetClassPrefix(const FString InClassName);
+FString GetClassPrefix(const FString& InClassName);
 
 /** 
  * Attempts to get class prefix. If the given class name does not start with a valid Unreal Prefix, it will return an empty string.
@@ -32,7 +32,7 @@ FString GetClassPrefix(const FString InClassName);
  * @param InClassName Name w/ potential prefix to check
  * @param out bIsLabeledDeprecated Reference param set to True if the class name is marked as deprecated
  */
-FString GetClassPrefix(const FString InClassName, bool& bIsLabeledDeprecated);
+FString GetClassPrefix(const FString& InClassName, bool& bIsLabeledDeprecated);
 
 /**
 * Generates hash for text, ignoring characters such as \r and the terminating \0
