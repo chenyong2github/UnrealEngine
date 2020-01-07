54168a659e6ed54f6bcbc9ac33d807854962d121
Ryan Durand <Ryan.Durand@epicgames.com>
2020-01-07 17:07:47 -0500

    Updating more copyrights to remove years.
    #rnx
    #rb none
    #jira none
    
    [CL 10898929 by Ryan Durand in Main branch]

--- Engine/Source/Runtime/Core/Private/IO/IoDispatcherFileBackend.cpp
+++ Engine/Source/Runtime/Core/Private/IO/IoDispatcherFileBackend.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "IO/IoDispatcherFileBackend.h"
 #include "Misc/ScopeRWLock.h"
