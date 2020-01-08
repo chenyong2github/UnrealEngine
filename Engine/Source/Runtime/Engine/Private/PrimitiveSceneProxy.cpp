430869b5def07843d30aaf6c64362c04c09b55d1
Mi Wang <Mi.Wang@epicgames.com>
2020-01-08 09:40:25 -0500

    Fix a problem that movable objects never get reflection captures updated in forward renderer. #rb Wei.Liu
    [CL 10904095 by Mi Wang in Dev-Mobile branch]

--- Engine/Source/Runtime/Engine/Private/PrimitiveSceneProxy.cpp
+++ Engine/Source/Runtime/Engine/Private/PrimitiveSceneProxy.cpp
@@ -403,6 +403,12 @@ void FPrimitiveSceneProxy::SetTransform(const FMatrix& InLocalToWorld, const FBo
 	LocalBounds = InLocalBounds;
 	ActorPosition = InActorPosition;
 	
+	// Update cached reflection capture.
+	if (PrimitiveSceneInfo)
+	{
+		PrimitiveSceneInfo->bNeedsCachedReflectionCaptureUpdate = true;
+	}
+	
 	UpdateUniformBuffer();
 	
 	// Notify the proxy's implementation of the change.
