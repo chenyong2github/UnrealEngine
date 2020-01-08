48a61ce08de889fc614ef07ab7c0ffeb144b3dd7
nick darnell <Nick.Darnell@epicgames.com>
2020-01-08 13:59:18 -0500

    Slate - Faster and safer draw element submitting for invalidation painting. This should fix issues with difficult to reproduce crashes involving cached element lists that have been deleted but are being accessed (suspected).
    Improved CSV stats we're tracking for cached elements.  This version should fix the issue with weakptrs getting corrupted.
    
    #author Matt.Kuhlenschmidt
    #editor Nick.Darnell
    #rb Nick.Darnell
    [FYI] Matt.Kuhlenschmidt, Andrew.Ladenberger
    
    
    
    #ROBOMERGE-SOURCE: CL 10906690 via CL 10906695 via CL 10906699
    #ROBOMERGE-BOT: (v626-10872990)
    
    [CL 10906703 by nick darnell in Main branch]

--- Engine/Source/Runtime/SlateCore/Public/Rendering/SlateRenderBatch.h
+++ Engine/Source/Runtime/SlateCore/Public/Rendering/SlateRenderBatch.h
@@ -16,7 +16,6 @@ struct FSlateDataPayload;
 class FSlateRenderBatch
 {
 public:
-	//FSlateRenderBatch(uint32 InLayer, const FSlateElementBatch& InBatch, TSharedPtr<FSlateRenderDataHandle, ESPMode::ThreadSafe> InRenderHandle, int32 InNumVertices, int32 InNumIndices, int32 InVertexOffset, int32 InIndexOffset);
 	FSlateRenderBatch(
 		int32 InLayer,
 		const FShaderParams& InShaderParams,
@@ -185,3 +184,6 @@ public:
 	uint8 bIsMerged : 1;
 
 };
+
+static_assert(TIsTriviallyCopyConstructible<FSlateRenderBatch>::Value == true, "FSlateRenderBatch must be mem copyable");
+static_assert(TIsTriviallyDestructible<FSlateRenderBatch>::Value == true, "FSlateRenderBatch must be trivially destructible");
