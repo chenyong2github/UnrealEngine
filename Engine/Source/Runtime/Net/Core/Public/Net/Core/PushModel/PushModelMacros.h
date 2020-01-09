3b3eb87e75b8b6cfd59b26a33ac45b0b44c85989
jon nabozny <Jon.Nabozny@epicgames.com>
2020-01-09 15:57:29 -0500

    Push Model Replication Core. Provides the basic interface, machinery, and codegen for allowing games to notify the networking system when properties have been changed.
    [at]Ryan.Gerleve, [at]Brian.Bekich, [at]Peter.Engstrom, [at]Mattias.Hornlund, [at]LouisPhilippe.Seguin, [at]Bart.Hawthorne, [at]Marc.Audy, [at]Dan.OConnor
    #rb Dan.Oconnor, Peter.Engstrom, Steve.Robb
    
    
    #ROBOMERGE-OWNER: jon.nabozny
    #ROBOMERGE-AUTHOR: jon.nabozny
    #ROBOMERGE-SOURCE: CL 10931598 via CL 10931603 via CL 10931604
    #ROBOMERGE-BOT: (v626-10872990)
    
    [CL 10931773 by jon nabozny in Main branch]

---
+++ Engine/Source/Runtime/Net/Core/Public/Net/Core/PushModel/PushModelMacros.h
@@ -0,0 +1,28 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#ifndef WITH_PUSH_MODEL
+#define WITH_PUSH_MODEL 0
+#endif
+
+#define DISABLE_PUSH_MODEL_VALIDATION 0
+#define WITH_PUSH_VALIDATION_SUPPORT (WITH_PUSH_MODEL && !(UE_BUILD_SHIPPING || UE_BUILD_TEST || DISABLE_PUSH_MODEL_VALIDATION))
+
+#if WITH_PUSH_MODEL
+
+// This macro could be moved somewhere else and be made more generic if we want.
+
+#define REPLICATED_BASE_CLASS(ClassName) \
+private: \
+int32 NetPushId_Internal = INDEX_NONE; \
+virtual void SetNetPushIdDynamic(const int32 InNetPushId) override { NetPushId_Internal = InNetPushId; } \
+virtual int32 GetNetPushIdDynamic() const override { return GetNetPushId(); } \
+public: \
+int32 GetNetPushId() const { return NetPushId_Internal; }
+
+#else
+
+#define REPLICATED_BASE_CLASS(ClassName)
+
+#endif // WITH_PUSH_MODEL
