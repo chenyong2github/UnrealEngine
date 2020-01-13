121c3be32f15cdd5cbe01c463e4c2e35faeab8e1
Martin Ridgers <Martin.Ridgers@epicgames.com>
2020-01-13 06:45:40 -0500

    Fixed some copyright lines now that the year is no longer required. #rb none #rnx
    [CL 10950654 by Martin Ridgers in Dev-Core branch]

--- Engine/Plugins/Animation/GameplayInsights/Source/GameplayInsights/Private/SkeletalMeshCurvesTrack.cpp
+++ Engine/Plugins/Animation/GameplayInsights/Source/GameplayInsights/Private/SkeletalMeshCurvesTrack.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SkeletalMeshCurvesTrack.h"
 #include "GameplayProvider.h"
