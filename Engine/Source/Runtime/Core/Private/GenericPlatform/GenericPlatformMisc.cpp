3176ce88171a7d46de96831f55c83559c9dae9bf
ben marsh <Ben.Marsh@epicgames.com>
2020-01-15 22:57:27 -0500

    Fix crash on startup when running an unpackaged project from a different drive than the one containing the engine.
    #rb none
    #rnx
    #jira UE-86654
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 10995529 in //UE4/Release-4.24/... via CL 10995530
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v633-10983880)
    
    [CL 11019948 by ben marsh in Dev-Networking branch]

--- Engine/Source/Runtime/Core/Private/GenericPlatform/GenericPlatformMisc.cpp
+++ Engine/Source/Runtime/Core/Private/GenericPlatform/GenericPlatformMisc.cpp
@@ -831,7 +831,7 @@ const TCHAR* FGenericPlatformMisc::EngineDir()
 		{
 			EngineDirectory = DefaultEngineDir;
 		}
-		else if (GForeignEngineDir != NULL && FPlatformFileManager::Get().GetPlatformFile().DirectoryExists(*(FPlatformProcess::BaseDir() / FString(GForeignEngineDir) / TEXT("Binaries"))))
+		else if (GForeignEngineDir != NULL && FPlatformFileManager::Get().GetPlatformFile().DirectoryExists(*(FString(GForeignEngineDir) / TEXT("Binaries"))))
 		{
 			EngineDirectory = GForeignEngineDir;
 		}
