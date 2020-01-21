5be933875602d4fed87cf97b169203f3b0dfcb9e
Patrick Laflamme <patrick.laflamme@epicgames.com>
2020-01-21 15:19:49 -0500

    Fixed linker error in Disaster Recovery code (missing #pragma once in a new file).
    #rb none
    #rnx
    
    [CL 11075951 by Patrick Laflamme in Dev-VirtualProduction branch]

--- Engine/Plugins/Developer/Concert/DisasterRecoveryClient/Source/DisasterRecoveryClient/Private/DisasterRecoveryUtil.h
+++ Engine/Plugins/Developer/Concert/DisasterRecoveryClient/Source/DisasterRecoveryClient/Private/DisasterRecoveryUtil.h
@@ -1,15 +1,17 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
 #include "CoreMinimal.h"
 
 namespace DisasterRecoveryUtil
 {
 	/** Returns the container name for the settings. */
-	inline static FName GetSettingsContainerName() { return "Project"; }
+	static inline FName GetSettingsContainerName() { return "Project"; }
 
 	/** Returns the category name for the settings. */
-	inline static FName GetSettingsCategoryName() { return "Plugins"; }
+	static inline FName GetSettingsCategoryName() { return "Plugins"; }
 
 	/** Returns the section name for the settings. */
-	inline static FName GetSettingsSectionName() { return "Disaster Recovery"; }
+	static inline FName GetSettingsSectionName() { return "Disaster Recovery"; }
 }
