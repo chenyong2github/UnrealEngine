16265b2105c381b32ac960c6ecaed0a6a563a556
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-01-22 13:12:55 -0500

    Fix for copywrite text regression from CL 11082540 #rb none
    #ROBOMERGE-SOURCE: CL 11082964 via CL 11083821
    #ROBOMERGE-BOT: (v637-11041722)
    
    [CL 11083894 by rob krajcarski in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraShader.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraShader.h
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 /*=============================================================================
 	MaterialShader.h: Shader base classes
