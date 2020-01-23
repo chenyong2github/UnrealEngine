2916575e1b90ba53785d622b64aa713587654908
chad garyet <Chad.Garyet@epicgames.com>
2020-01-23 10:48:42 -0500

    move over symstore/pdbstr copy fixes #rb none
    #ROBOMERGE-OWNER: chad.garyet
    #ROBOMERGE-AUTHOR: chad.garyet
    #ROBOMERGE-SOURCE: CL 11052902 via CL 11052945 via CL 11055161
    #ROBOMERGE-BOT: (v640-11091645)
    
    [CL 11093700 by chad garyet in Main branch]

--- Engine/Source/Programs/AutomationTool/Win/WinPlatform.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Win/WinPlatform.Automation.cs
@@ -395,6 +395,8 @@ public abstract class BaseWinPlatform : Platform
 		{
 			DateTime Start = DateTime.Now;
 			DirectoryReference TempSymStoreDir = DirectoryReference.Combine(RootDirectory, "Saved", "SymStore");
+			DirectoryReference.CreateDirectory(TempSymStoreDir);
+			DeleteDirectoryContents(TempSymStoreDir);
 
 			string TempFileName = Path.GetTempFileName();
 			try
