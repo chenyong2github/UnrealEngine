5fdc55b9235e31368bef47f91d8fb6f4fc0b5217
ben salem <Ben.Salem@epicgames.com>
2020-01-27 16:29:35 -0500

    Remove hardcoded timeout from BootTest test controller - this should be defined by the test node in csharp. #rb clayton.langford
    #ROBOMERGE-SOURCE: CL 11121195 via CL 11121197 via CL 11121202
    #ROBOMERGE-BOT: (v640-11091645)
    
    [CL 11121211 by ben salem in Main branch]

--- Engine/Plugins/Experimental/Gauntlet/Source/Gauntlet/Private/GauntletTestControllerBootTest.cpp
+++ Engine/Plugins/Experimental/Gauntlet/Source/Gauntlet/Private/GauntletTestControllerBootTest.cpp
@@ -10,12 +10,4 @@ void UGauntletTestControllerBootTest::OnTick(float TimeDelta)
 	{
 		EndTest(0);
 	}
-	else
-	{
-		if (GetTimeInCurrentState() > 300)
-		{
-			UE_LOG(LogGauntlet, Error, TEXT("Failing boot test after 300 secs!"));
-			EndTest(-1);
-		}
-	}
 }
