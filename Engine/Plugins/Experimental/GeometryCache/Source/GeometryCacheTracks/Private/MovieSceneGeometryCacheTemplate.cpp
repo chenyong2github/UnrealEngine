df2f44efe47173ddc59d87941c3724c6144f88cd
Anousack Kitisa <Anousack.Kitisa@epicgames.com>
2020-01-29 16:53:41 -0500

    Undo 11130815 for further investigation of Mac build error.
    #jira UE-87690
    
    #rb none
    
    [CL 11168401 by Anousack Kitisa in Dev-VirtualProduction branch]

--- Engine/Plugins/Experimental/GeometryCache/Source/GeometryCacheTracks/Private/MovieSceneGeometryCacheTemplate.cpp
+++ Engine/Plugins/Experimental/GeometryCache/Source/GeometryCacheTracks/Private/MovieSceneGeometryCacheTemplate.cpp
@@ -87,19 +87,12 @@ struct FGeometryCacheExecutionToken
 					UGeometryCacheComponent* GeometryComp = GeometryMeshComponentFromObject(Obj);
 					if (GeometryComp)
 					{
-						// Set the GeometryCache on the component only if it's set and valid in the Params
-						if (Params.GeometryCacheAsset && Params.GeometryCacheAsset != GeometryComp->GetGeometryCache())
+						if (Params.GeometryCacheAsset != GeometryComp->GetGeometryCache())
 						{
 							UGeometryCache* GeomCache = Params.GeometryCacheAsset;
 							{
 								GeometryComp->SetGeometryCache(GeomCache);
 							}
-						}
-						else
-						{
-							// It could be unset if the Params was referencing a transient GeometryCache
-							// In that case, use the GeometryCache that is set on the component
-							Params.GeometryCacheAsset = GeometryComp->GetGeometryCache();
 						}
 						Player.SavePreAnimatedState(*GeometryComp, FPreAnimatedGeometryCacheTokenProducer::GetAnimTypeID(), FPreAnimatedGeometryCacheTokenProducer());
 						GeometryComp->SetManualTick(true);
