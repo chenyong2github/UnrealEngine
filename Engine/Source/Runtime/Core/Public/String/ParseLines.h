1e26a384a7f0ece7c51cee186fd83c80e9dbc7bb
devin doucette <devin.doucette@epicgames.com>
2020-01-29 15:14:51 -0500

    Add UE::String::ParseLines, UE::String::ParseTokens[Multiple]
    - UE::String::ParseLines can be used to split a string view on line endings to find the non-empty lines in the view. Intended to replace functions like FString::ParseIntoArrayLines and FFileHelper::LoadFileToStringArray.
    - UE::String::ParseTokens[Multiple] can be used to split a string view on one or more single-character or multi-character delimiters. Intended to replace FString::ParseIntoArray. Separate handling of single-character delimiters reduced the cost by 27% in the case being profiled.
    - Extend Algo::CompareByPredicate to allow the input ranges to be different types and to take them by universal reference.
    - Extend TArrayView and MakeArrayView to allow array views to be created from initializer lists.
    
    #rb Matt.Peters, Steve.Robb
    #ushell-cherrypick of 10720885 by Devin.Doucette
    #ushell-cherrypick of 10812084 by Devin.Doucette
    
    
    #ROBOMERGE-SOURCE: CL 11162858 via CL 11162872 via CL 11162884
    #ROBOMERGE-BOT: (v640-11091645)
    
    [CL 11166820 by devin doucette in Main branch]

---
+++ Engine/Source/Runtime/Core/Public/String/ParseLines.h
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreTypes.h"
+#include "Templates/Function.h"
+
+class FStringView;
+
+namespace UE
+{
+namespace String
+{
+	/**
+	 * Visit every line in the input view as terminated by any of CRLF, CR, LF.
+	 *
+	 * Empty lines are visited by this function.
+	 *
+	 * @param View The string view to split into lines.
+	 * @param Visitor A function that is called for each line.
+	 */
+	CORE_API void ParseLines(const FStringView& View, const TFunctionRef<void(FStringView)>& Visitor);
+}
+}
