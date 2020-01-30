527a7a15070a62c6495bea33d4b141c9246dd43b
Martin Ridgers <Martin.Ridgers@epicgames.com>
2020-01-30 07:30:05 -0500

    Moved Asio library into Engine/Source/ThirdParty so as to not upset code and include analysis.
    #rb sbo,jb (I told them about it over lunch)
    #rnx
    
    [CL 11174492 by Martin Ridgers in Dev-Core branch]

--- Engine/Source/Developer/TraceAnalysis/TraceAnalysis.Build.cs
+++ Engine/Source/Developer/TraceAnalysis/TraceAnalysis.Build.cs
@@ -6,6 +6,7 @@ public class TraceAnalysis : ModuleRules
 {
 	public TraceAnalysis(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PrivateDependencyModuleNames.Add("Asio");
 		PrivateDependencyModuleNames.Add("Cbor");
 		PrivateDependencyModuleNames.Add("Core");
 		PrivateDependencyModuleNames.Add("DirectoryWatcher");
