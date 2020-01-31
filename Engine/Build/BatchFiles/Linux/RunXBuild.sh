a1a18458160c870217e2dba42eee9150f7893ea4
andrew grant <Andrew.Grant@epicgames.com>
2020-01-30 20:49:07 -0500

    Adding -w to the grep (now -wi) in the bash script that builds UBT so it isn't triggered by warnings that involved a variable with 'Error' in its name.
    (In my case it was 'Missing XML comment for publicly visible type or member 'ReadOnlyTargetRules.bUndefinedIdentifierErrors').
    
    [FYI] ben.marsh
    
    
    #ROBOMERGE-SOURCE: CL 11191147 via CL 11191149
    #ROBOMERGE-BOT: (v640-11091645)
    
    [CL 11191151 by andrew grant in Main branch]

--- Engine/Build/BatchFiles/Linux/RunXBuild.sh
+++ Engine/Build/BatchFiles/Linux/RunXBuild.sh
@@ -20,7 +20,7 @@ if [ ! -f Build/BatchFiles/Linux/RunXBuild.sh ]; then
 	exit 1
 fi
 
-xbuild /verbosity:quiet /nologo "$@" |grep -i error
+xbuild /verbosity:quiet /nologo "$@" |grep -wi error
 if [ $? -ne 1 ]; then
 	exit 1
 else
