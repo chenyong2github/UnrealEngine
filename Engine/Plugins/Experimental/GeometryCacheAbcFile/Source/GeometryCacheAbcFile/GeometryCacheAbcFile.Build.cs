5bc89f7cf774415ad0d41bc9628a3e2b9809b319
Anousack Kitisa <Anousack.Kitisa@epicgames.com>
2020-02-03 13:29:44 -0500

    Copied CL 11188915 from //UE4/Dev-VirtualProduction.
    GeometryCache from Alembic file without importing. (Fixed for Mac build)
    
    #rb Jurre.deBaare
    #rb Johan.Duparc
    
    [CL 11207881 by Anousack Kitisa in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Experimental/GeometryCacheAbcFile/Source/GeometryCacheAbcFile/GeometryCacheAbcFile.Build.cs
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GeometryCacheAbcFile : ModuleRules
+{
+	public GeometryCacheAbcFile(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PublicDependencyModuleNames.AddRange(
+			new string[] {
+				"AlembicLibrary",
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"GeometryCache",
+				"RenderCore",
+				"RHI"
+			}
+		);
+	}
+}
