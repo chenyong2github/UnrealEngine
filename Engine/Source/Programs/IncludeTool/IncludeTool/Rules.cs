35a7ce4cab8dee60551ad01aecc1930043a23383
Josh Adams <josh.adams@epicgames.com>
2020-02-10 10:51:38 -0500

    - Added lz4.h to be marked as External header so IncludeTool analysis won't report inconsistent preprocessor settings (lz4hc.h includes twice lz4.h with different defines set) #rb ben.marsh #rnx
    [CL 11303941 by Josh Adams in Main branch]

--- Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
+++ Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
@@ -202,6 +202,10 @@ namespace IncludeTool
 			{
 				return true;
 			}
+			if (NormalizedPath.EndsWith("/lz4.h") || NormalizedPath.EndsWith("/lz4hc.h"))
+			{
+				return true;
+			}
 			return false;
 		}
 
