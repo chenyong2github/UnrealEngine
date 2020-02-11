9ea8f2bfbca8c4e90a205a95d9fb6d994d0d711f
phil popp <Phil.Popp@epicgames.com>
2020-02-10 19:20:09 -0500

    static analysis warning fixes #jira UE-88398 #rb ethan.geller
    #ROBOMERGE-SOURCE: CL 11313391 via CL 11316150
    #ROBOMERGE-BOT: (v653-11302973)
    
    [CL 11316302 by phil popp in Main branch]

--- Engine/Source/Runtime/SignalProcessing/Public/DSP/EQ.h
+++ Engine/Source/Runtime/SignalProcessing/Public/DSP/EQ.h
@@ -42,14 +42,14 @@ namespace Audio
 
 	private:
 
-		// The number of bands in the equalizer
-		int32 NumBands;
-
 		// The number of channels in the equalizer
 		int32 NumChannels;
 
 		// The array of biquad filters
-		FBiquadFilter* FilterBands;
+		TArray<FBiquadFilter> FilterBands;
+		
+		// Temporary array for processing audio.
+		TArray<float> WorkBuffer;
 	};
 
 }
