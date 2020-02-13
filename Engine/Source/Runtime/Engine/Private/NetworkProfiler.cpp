1d572095b90d7b95df5bee0b251b13500c2f3367
louisphilippe seguin <LouisPhilippe.Seguin@epicgames.com>
2020-02-13 13:04:59 -0500

    Added "NetProfile Autostop" console command * Starts a nprof recording but stops it after 30 secs. * Time value can be changed by adding Time=Seconds in the console command. ex: "NetProfile Autostop Time=60" to capture for one minute
    #rb Ryan.Gerleve
    
    
    
    #ROBOMERGE-SOURCE: CL 11421475 via CL 11421479 via CL 11421487
    #ROBOMERGE-BOT: (v654-11333218)
    
    [CL 11421490 by louisphilippe seguin in Main branch]

--- Engine/Source/Runtime/Engine/Private/NetworkProfiler.cpp
+++ Engine/Source/Runtime/Engine/Private/NetworkProfiler.cpp
@@ -840,6 +840,16 @@ bool FNetworkProfiler::Exec( UWorld * InWorld, const TCHAR* Cmd, FOutputDevice &
 	{
 		EnableTracking( false );
 	} 
+	else if (FParse::Command(&Cmd, TEXT("AUTOSTOP")))
+	{
+		EnableTracking(true);
+
+		// Default to 30secs
+		float AutoStopTime = 30.f;
+		FParse::Value(Cmd, TEXT("TIME="), AutoStopTime);
+
+		InWorld->GetTimerManager().SetTimer(AutoStopTimerHandle, FTimerDelegate::CreateRaw(this, &FNetworkProfiler::AutoStopTracking), AutoStopTime, false);
+	}
 	else 
 	{
 		// Default to toggle
@@ -858,6 +868,14 @@ bool FNetworkProfiler::Exec( UWorld * InWorld, const TCHAR* Cmd, FOutputDevice &
 	}
 
 	return true;
+}
+
+void FNetworkProfiler::AutoStopTracking()
+{
+	if (bIsTrackingEnabled)
+	{
+		EnableTracking(false);
+	}
 }
 
 #endif	//#if USE_NETWORK_PROFILER
