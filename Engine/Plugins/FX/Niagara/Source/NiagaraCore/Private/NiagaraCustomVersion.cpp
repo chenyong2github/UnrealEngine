3c7bb9f4c0ff8c64a1906d67c0e007edc02d6b28
mihnea balta <Mihnea.Balta@epicgames.com>
2020-02-13 05:54:59 -0500

    Fixed: if multiple instances of a DI were used in a script, the HLSL translator produced all function combinations for each instance.
    DataInterfaceRegisteredFunctions accumulated all registered functions from all the instances, then the generator iterated over it for each instance, which was incorrect. The information was already available per instance in FNiagaraScriptDataInterfaceCompileInfo.RegisteredFunctions, so we can just use that and remove DataInterfaceRegisteredFunctions entirely.
    
    Already submitted this once as 11386264, but that introduced a new bug, because RegisteredFunctions can contain duplicates. Did it properly this time, so that GetFunctionHLSL is called once per unique function instance.
    
    #rb nicholas.goldstein
    
    
    #ROBOMERGE-SOURCE: CL 11416526 via CL 11416570
    #ROBOMERGE-BOT: (v654-11333218)
    
    [CL 11416580 by mihnea balta in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xEBAD283C, 0x50794391, 0x9D128C9D, 0xA1EDBF7A);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6B0875B7, 0x486C447E, 0x8FF73178, 0xAE5C0EFC);
