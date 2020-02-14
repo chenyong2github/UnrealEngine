3800e106fa0159ee5edea938a51024f8a18591f2
Max Chen <Max.Chen@epicgames.com>
2020-02-14 11:42:34 -0500

    Take Recorder: Rename NewTake to ClearPendingTake. Users just want the ability to clear the pending level sequence take rather than also clearing out the sources.
    #jira UE-88493
    #rb mike.zyracki, matt.hoffman
    
    [CL 11444855 by Max Chen in 4.25 branch]

--- Engine/Plugins/VirtualProduction/Takes/Source/TakeRecorder/Private/Widgets/STakeRecorderPanel.h
+++ Engine/Plugins/VirtualProduction/Takes/Source/TakeRecorder/Private/Widgets/STakeRecorderPanel.h
@@ -59,7 +59,7 @@ public:
 
 	TSharedPtr<STakeRecorderCockpit> GetCockpitWidget() const { return CockpitWidget; }
 
-	void NewTake();
+	void ClearPendingTake();
 
 private:
 
@@ -96,7 +96,7 @@ private:
 
 	FReply OnBackToPendingTake();
 
-	FReply OnNewTake();
+	FReply OnClearPendingTake();
 
 	FReply OnReviewLastRecording();
 
