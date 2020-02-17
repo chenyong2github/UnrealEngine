dea6fe97262bd36c2ee70e7446d9b5a8adb3d081
anousack kitisa <Anousack.Kitisa@epicgames.com>
2020-02-17 13:45:28 -0500

    Copied 11307851 from Dev-VirtualProduction.
    Fixed crash in PIE with an empty GeometryCacheAbcFile actor in the level.
    Fixed crash when clearing the Alembic File Path on the GeometryCacheAbcFile component.
    
    #jira UE-88384
    
    #rb none
    
    #ROBOMERGE-SOURCE: CL 11461755 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v654-11333218)
    
    [CL 11461758 by anousack kitisa in 4.25-Plus branch]

--- Engine/Plugins/Experimental/GeometryCacheAbcFile/Source/GeometryCacheAbcFile/Public/GeometryCacheTrackAbcFile.h
+++ Engine/Plugins/Experimental/GeometryCacheAbcFile/Source/GeometryCacheAbcFile/Public/GeometryCacheTrackAbcFile.h
@@ -20,6 +20,7 @@ class GEOMETRYCACHEABCFILE_API UGeometryCacheTrackAbcFile : public UGeometryCach
 public:
 
 	//~ Begin UGeometryCacheTrack Interface.
+	virtual const bool UpdateMatrixData(const float Time, const bool bLooping, int32& InOutMatrixSampleIndex, FMatrix& OutWorldMatrix) override;
 	virtual const bool UpdateMeshData(const float Time, const bool bLooping, int32& InOutMeshSampleIndex, FGeometryCacheMeshData*& OutMeshData) override;
 	virtual const bool UpdateBoundsData(const float Time, const bool bLooping, const bool bIsPlayingBackward, int32& InOutBoundsSampleIndex, FBox& OutBounds) override;
 	virtual const FGeometryCacheTrackSampleInfo& GetSampleInfo(float Time, const bool bLooping) override;
