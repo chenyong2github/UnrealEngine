51248e2a3e853059850c996a7d948b62655da029
Max Chen <Max.Chen@epicgames.com>
2020-02-18 12:31:44 -0500

    Sequencer: Improve editing of blending camera cuts.
    Split helper method to fixup consecutive sections, making a blending and non-blending one.
    Don't edit easing curves because they're already computed in tracks' UpdateEasing method (and better).
    
    #rb ludovic.chabant
    #jira none
    
    [CL 11502967 by Max Chen in 4.25 branch]

--- Engine/Source/Runtime/MovieScene/Public/MovieSceneCommonHelpers.h
+++ Engine/Source/Runtime/MovieScene/Public/MovieSceneCommonHelpers.h
@@ -47,7 +47,9 @@ public:
 	 * @param Section The section that was modified 
 	 * @param bDelete Was this a deletion?
 	 */
-	static void FixupConsecutiveSections(TArray<UMovieSceneSection*>& Sections, UMovieSceneSection& Section, bool bDelete, bool bOnlyOnSameRow = false, bool bAllowOverlapBlending = false);
+	static void FixupConsecutiveSections(TArray<UMovieSceneSection*>& Sections, UMovieSceneSection& Section, bool bDelete);
+
+	static void FixupConsecutiveBlendingSections(TArray<UMovieSceneSection*>& Sections, UMovieSceneSection& Section, bool bDelete);
 
 	/*
  	 * Sort consecutive sections so that they are in order based on start time
