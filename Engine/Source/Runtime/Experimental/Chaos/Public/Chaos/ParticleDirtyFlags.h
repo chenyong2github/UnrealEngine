393f87e71a2b6309e8af763ac9d340c2a35d3488
ori cohen <Ori.Cohen@epicgames.com>
2020-02-19 16:39:15 -0500

    Changes to particle dirty properties
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 11553727 via CL 11553729 via CL 11553734
    #ROBOMERGE-BOT: (v654-11333218)
    
    [CL 11553739 by ori cohen in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ParticleDirtyFlags.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ParticleDirtyFlags.h
@@ -75,19 +75,11 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 
 	struct FDirtyIdx
 	{
-		EParticleProperty Property : 5;
-		uint32 Entry : 27;
-
-		static_assert((uint32)EParticleProperty::NumProperties <= 31,"Cannot have more than 31 properties");
-
-		EParticleFlags GetPropertyFlag() const
-		{
-			return (EParticleFlags)(1 << (uint32)Property);
-		}
+		uint32 Entry;
 
 		bool operator==(const FDirtyIdx& Rhs) const
 		{
-			return ((const uint32&)*this) == ((const uint32&)Rhs);
+			return Entry == Rhs.Entry;
 		}
 	};
 
@@ -106,6 +98,15 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 			FreeIndices.Add(Idx);
 		}
 
+		T Pop(int32 Idx)
+		{
+			FreeIndices.Add(Idx);
+			T Result;
+			Swap(Result,Elements[Idx]);
+			Elements[Idx].~T();
+			return Result;
+		}
+
 		int32 Write(const T& Element)
 		{
 			const int32 Idx = GetFree();
@@ -113,18 +114,26 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 			return Idx;
 		}
 
-		int32 Write(T&& Element)
+		void Update(int32 Entry, const T& Element)
 		{
-			const int32 Idx = GetFree();
-			Elements[Idx] = MoveTemp(Element);
-			return Idx;
+			Elements[Entry] = Element;
 		}
 
 	private:
 
 		int32 GetFree()
 		{
-			return FreeIndices.Num() ? FreeIndices.Pop(/*bAllowShrinking=*/false) : Elements.AddUninitialized(1);
+			//todo: can we avoid default constructors? maybe if std::is_trivially_copyable
+			if(FreeIndices.Num())
+			{
+				int32 NewIdx = FreeIndices.Pop();
+				Elements[NewIdx] = T();
+				return NewIdx;
+			}
+			else
+			{
+				return Elements.AddDefaulted(1);
+			}
 		}
 
 		TArray<T> Elements;
@@ -134,25 +143,12 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 	class FDirtyPropertiesManager
 	{
 	public:
-#define PARTICLE_PROPERTY(PropName, Type) FDirtyIdx Write##PropName(Type const & Val){ FDirtyIdx Result; Result.Property = EParticleProperty::PropName; Result.Entry = Dirty##PropName##Pool.Write(Val); return Result; };
+#define PARTICLE_PROPERTY(PropName, Type) \
+const TDirtyElementPool<Type>& Get##PropName##Pool() const { return Dirty##PropName##Pool; }\
+TDirtyElementPool<Type>& Get##PropName##Pool() { return Dirty##PropName##Pool; }
+
 #include "ParticleProperties.h"
 #undef PARTICLE_PROPERTY
-
-#define PARTICLE_PROPERTY(PropName, Type) Type const & Read##PropName(const FDirtyIdx Idx) const { ensure(Idx.Property == EParticleProperty::PropName); return Dirty##PropName##Pool.Read(Idx.Entry);};
-#include "ParticleProperties.h"
-#undef PARTICLE_PROPERTY
-
-	void FreeProperty(FDirtyIdx Idx)
-	{
-		switch((uint32)Idx.Property)
-		{
-#define PARTICLE_PROPERTY(PropName, Type) case EParticleProperty::PropName: { Dirty##PropName##Pool.Free(Idx.Entry); break; }
-#include "ParticleProperties.h"
-#undef PARTICLE_PROPERTY
-		default: check(false);
-
-		}
-	}
 	private:
 
 		//creates a TDirtyElementPool for each property
@@ -174,12 +170,6 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 			return Flags.IsDirty(CheckBits);
 		}
 
-		void DirtyProperty(FDirtyIdx DirtyIdx)
-		{
-			Flags.MarkDirty(DirtyIdx.GetPropertyFlag());
-			DirtyProperties.Add(DirtyIdx);
-		}
-
 		~FDirtyProperties()
 		{
 			//Make sure to call Clean before killing this guy
@@ -188,32 +178,54 @@ using ChaosGeometrySp = TSharedPtr<FImplicitObject,ESPMode::ThreadSafe>;
 
 		void Clean(FDirtyPropertiesManager& DirtyPropertyManager)
 		{
-			for(const FDirtyIdx Idx : DirtyProperties)
+			if(!Flags.IsClean())
 			{
-				DirtyPropertyManager.FreeProperty(Idx);
+#define PARTICLE_PROPERTY(PropName, Type) if(Flags.IsDirty(EParticleFlags::PropName)){ Pop##PropName(DirtyPropertyManager); }
+#include "ParticleProperties.h"
+#undef PARTICLE_PROPERTY
 			}
-
-			Flags.Clear();
 		}
 
-#define PARTICLE_PROPERTY(PropName, Type) Type const& Read##PropName(const FDirtyPropertiesManager& DirtyPropertyManager) const\
-{\
-	for(const FDirtyIdx Idx : DirtyProperties)\
-	{\
-		if(Idx.Property == EParticleProperty::PropName)\
-		{\
-			return DirtyPropertyManager.Read##PropName(Idx);\
-		}\
-	}\
-	check(false);\
-	static Type Error;\
-	return Error;\
-}
+#define PARTICLE_PROPERTY(PropName, Type)\
+Type const& Read##PropName(const FDirtyPropertiesManager& DirtyPropertiesManager) const { return ReadImp(PropName, DirtyPropertiesManager.Get##PropName##Pool()); }\
+Type Pop##PropName(FDirtyPropertiesManager& DirtyPropertiesManager) { return PopImp(PropName, EParticleFlags::PropName, DirtyPropertiesManager.Get##PropName##Pool()); }\
+void Write##PropName(FDirtyPropertiesManager& DirtyPropertiesManager, Type const& Val) { WriteImp(PropName, EParticleFlags::PropName, Val, DirtyPropertiesManager.Get##PropName##Pool()); }
+
 #include "ParticleProperties.h"
 #undef PARTICLE_PROPERTY
 
 	private:
-		TArray<FDirtyIdx,TInlineAllocator<4>> DirtyProperties;
 		FParticleDirtyFlags Flags;
+#define PARTICLE_PROPERTY(PropName, Type) FDirtyIdx PropName;
+#include "ParticleProperties.h"
+#undef PARTICLE_PROPERTY
+
+		template <typename T>
+		void WriteImp(FDirtyIdx& Prop, EParticleFlags Flag, const T& Val, TDirtyElementPool<T>& Pool)
+		{
+			if(Flags.IsDirty(Flag))
+			{
+				Pool.Update(Prop.Entry,Val);
+			}
+			else
+			{
+				Prop.Entry = Pool.Write(Val);
+				Flags.MarkDirty(Flag);
+			}
+		}
+
+		template <typename T>
+		const T& ReadImp(const FDirtyIdx Prop, const TDirtyElementPool<T>& Pool) const
+		{
+			return Pool.Read(Prop.Entry);
+		}
+
+		template <typename T>
+		T PopImp(const FDirtyIdx Prop, EParticleFlags Flag, TDirtyElementPool<T>& Pool)
+		{
+			ensure(Flags.IsDirty(Flag));
+			Flags.MarkClean(Flag);
+			return Pool.Pop(Prop.Entry);
+		}
 	};
 }
