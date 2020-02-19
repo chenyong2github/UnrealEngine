53efdb3b8310b2447e12701a319ccc3c295b580c
homam bahnassi <Homam.Bahnassi@epicgames.com>
2020-02-19 10:12:22 -0500

    Integrating CompositePlane plugins to 4.25 #jira ue-87653 #rb patrick.boutot
    #ROBOMERGE-SOURCE: CL 11537316 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v654-11333218)
    
    [CL 11537320 by homam bahnassi in 4.25-Plus branch]

---
+++ Engine/Plugins/VirtualProduction/CompositePlane/Source/CompositePlane/CompositePlane.Build.cs
@@ -0,0 +1,31 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CompositePlane : ModuleRules
+{
+	public CompositePlane(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+                "Core",
+                "CoreUObject",
+				"Engine",
+                "Projects",
+                "Slate",
+				"SlateCore",
+			}
+			);
+
+        if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[] {
+                        "UnrealEd",
+                        "PlacementMode",
+                }
+            );
+        }
+	}
+}
