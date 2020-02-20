c860aefd10ffcd5dd29f32cd05cbbbada53ed2f4
anousack kitisa <Anousack.Kitisa@epicgames.com>
2020-02-20 11:32:26 -0500

    Bones metadata from FBX are now transferred to AnimSequence at import.
    #jira UE-79340
    
    #rb JeanMichel.Dignard
    
    #ROBOMERGE-SOURCE: CL 11565927 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v654-11333218)
    
    [CL 11565949 by anousack kitisa in 4.25-Plus branch]

--- Engine/Source/Editor/UnrealEd/Private/SkeletalMeshEdit.cpp
+++ Engine/Source/Editor/UnrealEd/Private/SkeletalMeshEdit.cpp
@@ -1715,6 +1715,12 @@ bool UnFbx::FFbxImporter::ImportAnimation(USkeleton* Skeleton, UAnimSequence * D
 		FComponentReregisterContext ReregisterContext(*Iter);
 	}
 
+	// Import bone metadata to AnimSequence
+	for (FbxNode* SkeletonNode : SortedLinks)
+	{
+		ImportNodeCustomProperties(DestSeq, SkeletonNode, true);
+	}
+
 	return true;
 }
 
