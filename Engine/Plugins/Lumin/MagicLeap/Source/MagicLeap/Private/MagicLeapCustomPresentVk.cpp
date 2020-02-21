f117131132b67943bac8c0d642086088a1aab27b
steve smith <steve.smith@epicgames.com>
2020-02-21 17:50:43 -0500

    Merge from MagicLeap-Staging: Don't render the depth target to MLGraphics in ZI. Only reallocate depth target if requested size matches expected one. This workaround is required because this depth allocation function can receive requests for targets other than the one for eye stereo render (like when using a SceneCaptureComponent). #jira UE-89353 #rb steve.smith
    [CL 11579626 by steve smith in 4.25 branch]

--- Engine/Plugins/Lumin/MagicLeap/Source/MagicLeap/Private/MagicLeapCustomPresentVk.cpp
+++ Engine/Plugins/Lumin/MagicLeap/Source/MagicLeap/Private/MagicLeapCustomPresentVk.cpp
@@ -61,8 +61,11 @@ void FMagicLeapCustomPresentVulkan::FinishRendering()
 		const uint32 vp_width = static_cast<uint32>(FrameInfo.viewport.w);
 		const uint32 vp_height = static_cast<uint32>(FrameInfo.viewport.h);
 
+#if PLATFORM_LUMIN
+		// ZI doesn't make use of depth
 		// Clear MLGraphics depth textures since on Vulkan they are not pre-cleared. (needs to be done on RHI thread)
 		FMagicLeapHelperVulkan::ClearImage(FrameInfo.depth_id, FLinearColor::Black, 0 /* BaseMipLevel */, 1 /* LevelCount */, 0 /* BaseArrayLayer */, FrameInfo.num_virtual_cameras, true);
+#endif // PLATFORM_LUMIN
 
 		// Alias the render target with an srgb image description for proper color space output.
 		if (RenderTargetTextureAllocation != VK_NULL_HANDLE && LastAliasedRenderTarget != RenderTargetTexture)
@@ -73,7 +76,6 @@ void FMagicLeapCustomPresentVulkan::FinishRendering()
 				FMagicLeapHelperVulkan::DestroyImageSRGB(RenderTargetTextureSRGB);
 				RenderTargetTextureSRGB = VK_NULL_HANDLE;
 				RenderTargetTextureSRGB = reinterpret_cast<void *>(FMagicLeapHelperVulkan::AliasImageSRGB((uint64)RenderTargetTextureAllocation, RenderTargetTextureAllocationOffset, vp_width * 2, vp_height));
-				check(RenderTargetTextureSRGB != VK_NULL_HANDLE);
 			}
 			LastAliasedRenderTarget = RenderTargetTexture;
 			UE_LOG(LogMagicLeap, Log, TEXT("Aliased render target for correct sRGB ouput."));
@@ -83,10 +85,16 @@ void FMagicLeapCustomPresentVulkan::FinishRendering()
 		FMagicLeapHelperVulkan::BlitImage((uint64)FinalTarget, 0, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.color_id, 0, 0, 0, 0, vp_width, vp_height, 1);
 		FMagicLeapHelperVulkan::BlitImage((uint64)FinalTarget, vp_width, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.color_id, 1, 0, 0, 0, vp_width, vp_height, 1);
 
-		const VkImage DepthTarget = static_cast<FVulkanTexture2D*>(Plugin->DepthBuffer->GetTexture2D())->Surface.Image;
+#if PLATFORM_LUMIN
+		// ZI doesn't make use of depth
+		if (Plugin->DepthBuffer != nullptr)
+		{
+			const VkImage DepthTarget = static_cast<FVulkanTexture2D*>(Plugin->DepthBuffer->GetTexture2D())->Surface.Image;
 
-		FMagicLeapHelperVulkan::BlitImage((uint64)DepthTarget, 0, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.depth_id, 0, 0, 0, 0, vp_width, vp_height, 1, true);
-		FMagicLeapHelperVulkan::BlitImage((uint64)DepthTarget, vp_width, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.depth_id, 1, 0, 0, 0, vp_width, vp_height, 1, true);
+			FMagicLeapHelperVulkan::BlitImage((uint64)DepthTarget, 0, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.depth_id, 0, 0, 0, 0, vp_width, vp_height, 1, true);
+			FMagicLeapHelperVulkan::BlitImage((uint64)DepthTarget, vp_width, 0, 0, vp_width, vp_height, 1, (uint64)FrameInfo.depth_id, 1, 0, 0, 0, vp_width, vp_height, 1, true);
+		}
+#endif // PLATFORM_LUMIN
 
 		FMagicLeapHelperVulkan::SignalObjects((uint64)FrameInfo.virtual_cameras[0].sync_object, (uint64)FrameInfo.virtual_cameras[1].sync_object, (uint64)FrameInfo.wait_sync_object);
 
