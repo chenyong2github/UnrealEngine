bf735e227424ccc55a26fc974ce04bb250284698
chris babcock <Chris.Babcock@epicgames.com>
2020-02-21 16:07:00 -0500

    Update libwebsockets for Android to NDK 21
    #jira UE-80046
    #ue4
    #android
    #rb Brandon.Schaefer
    
    #ROBOMERGE-SOURCE: CL 11578756 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v654-11333218)
    
    [CL 11578767 by chris babcock in 4.25-Plus branch]

--- Engine/Source/ThirdParty/libWebSockets/AndroidCrossBuildUE4.bat
+++ Engine/Source/ThirdParty/libWebSockets/AndroidCrossBuildUE4.bat
@@ -5,7 +5,7 @@ REM run this batch file
 
 @echo off
 
-set UEENGINEDIR=D:\bacchus\Engine
+set UEENGINEDIR=D:\ue4_4.25\Engine
 set UEOPENSSL=%UEENGINEDIR%\Source\ThirdParty\OpenSSL\1_0_1s
 set UELIBWEBSOCKET=%UEENGINEDIR%\Source\ThirdParty\libWebSockets\libwebsockets
 set UELIBWEBSOCKETINCLUDE=%UELIBWEBSOCKET%\include\Android
@@ -21,7 +21,7 @@ rem NOTE: do not need difference includes for each architecture since identical 
 rem ARMv7
 mkdir tempwork
 cd tempwork
-%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-19" -DANDROID_ABI="armeabi-v7a" -DANDROID_STL=gnustl_shared
+%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-19" -DANDROID_ABI="armeabi-v7a" -DANDROID_STL=libc++_shared
 %NDKROOT%\prebuilt\windows-x86_64\bin\make.exe
 mkdir %UELIBWEBSOCKETLIB%\ARMv7
 copy lib\libwebsockets.a %UELIBWEBSOCKETLIB%\ARMv7
@@ -33,7 +33,7 @@ rmdir /S /Q tempwork
 rem ARM64
 mkdir tempwork
 cd tempwork
-%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-21" -DANDROID_ABI="arm64-v8a" -DANDROID_STL=gnustl_shared
+%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-21" -DANDROID_ABI="arm64-v8a" -DANDROID_STL=libc++_shared
 %NDKROOT%\prebuilt\windows-x86_64\bin\make.exe
 mkdir %UELIBWEBSOCKETLIB%\ARM64
 copy lib\libwebsockets.a %UELIBWEBSOCKETLIB%\ARM64
@@ -43,7 +43,7 @@ rmdir /S /Q tempwork
 rem x86
 mkdir tempwork
 cd tempwork
-%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-19" -DANDROID_ABI="x86" -DANDROID_STL=gnustl_shared
+%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-19" -DANDROID_ABI="x86" -DANDROID_STL=libc++_shared
 %NDKROOT%\prebuilt\windows-x86_64\bin\make.exe
 mkdir %UELIBWEBSOCKETLIB%\x86
 copy lib\libwebsockets.a %UELIBWEBSOCKETLIB%\x86
@@ -53,7 +53,7 @@ rmdir /S /Q tempwork
 rem x64
 mkdir tempwork
 cd tempwork
-%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-21" -DANDROID_ABI="x86_64" -DANDROID_STL=gnustl_shared
+%UEENGINEDIR%\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe ..\..  -DOPENSSL_INCLUDE_DIR=%UEOPENSSL%\include\Android -DLWS_OPENSSL_LIBRARIES=%OPENSSL_LIBRARIES% -DOPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIBRARIES%\ARMv7\libcrypto.a -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIBRARIES%\ARMv7libssl.a" -DLWS_WITHOUT_TESTAPPS=ON -DLWS_WITH_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=%UEENGINEDIR%\Source\ThirdParty\PhysX\Externals\CMakeModules\Android\android.toolchain.cmake -G "MinGW Makefiles" -DTARGET_BUILD_PLATFORM=Android -DANDROID_NDK=%NDKROOT% -DCMAKE_MAKE_PROGRAM=%NDKROOT%\prebuilt\windows-x86_64\bin\make.exe -DANDROID_NATIVE_API_LEVEL="android-21" -DANDROID_ABI="x86_64" -DANDROID_STL=libc++_shared
 %NDKROOT%\prebuilt\windows-x86_64\bin\make.exe
 mkdir %UELIBWEBSOCKETLIB%\x64
 copy lib\libwebsockets.a %UELIBWEBSOCKETLIB%\x64
