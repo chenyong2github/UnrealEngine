f767dc0b9a461cac5ac246f1d009f99bae62d3ac
shaun kime <Shaun.Kime@epicgames.com>
2020-02-21 08:31:30 -0500

    Fixed issue where repeated function calls fail to register write functions for DI's
    #jira none
    [FYI] Devon.Penney
    #rb none
    
    #ROBOMERGE-SOURCE: CL 11575439 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v654-11333218)
    
    [CL 11575440 by shaun kime in 4.25-Plus branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x410E4624, 0xFD724393, 0x87D800BE, 0x2B8217CE);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x34F24743, 0x1FA645F1, 0xA99731B3, 0x55A71FD3);
