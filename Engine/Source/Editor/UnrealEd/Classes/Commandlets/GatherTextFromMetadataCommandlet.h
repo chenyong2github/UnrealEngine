1b215b30d62eecaeef7fbda78239a37f0775e30e
Jamie Dale <Jamie.Dale@epicgames.com>
2020-02-21 13:29:34 -0500

    Fixed incorrect property meta-data filtering from the UProperty -> FProperty changes
    #jira
    #rb none
    #rnx
    
    [CL 11577116 by Jamie Dale in 4.25 branch]

--- Engine/Source/Editor/UnrealEd/Classes/Commandlets/GatherTextFromMetadataCommandlet.h
+++ Engine/Source/Editor/UnrealEd/Classes/Commandlets/GatherTextFromMetadataCommandlet.h
@@ -29,16 +29,67 @@ public:
 
 private:
 	void GatherTextFromUObjects(const TArray<FString>& IncludePaths, const TArray<FString>& ExcludePaths, const FGatherParameters& Arguments);
-	void GatherTextFromUObject(UField* const Field, const FGatherParameters& Arguments, const FName InPlatformName);
-	void GatherTextFromField(FField* const Field, const FGatherParameters& Arguments, const FName InPlatformName);
-	bool ShouldGatherFromField(const UField* Field);
+
+	void GatherTextFromField(UField* Field, const FGatherParameters& Arguments, const FName InPlatformName);
+	void GatherTextFromField(FField* Field, const FGatherParameters& Arguments, const FName InPlatformName);
+	
+	template <typename FieldType>
+	bool ShouldGatherFromField(const FieldType* Field, const bool bIsEditorOnly);
+
+	template <typename FieldType>
+	void GatherTextFromFieldImpl(FieldType* Field, const FGatherParameters& Arguments, const FName InPlatformName);
+
+	template <typename FieldType>
+	void EnsureFieldDisplayNameImpl(FieldType* Field, const bool bIsBool);
 
 private:
 	bool ShouldGatherFromEditorOnlyData;
 
-	/** Array of field types (eg, UProperty, UFunction, UScriptStruct, etc) that should be included or excluded in the current gather */
-	TArray<const UClass*> FieldTypesToInclude;
-	TArray<const UClass*> FieldTypesToExclude;
+	struct FFieldClassFilter
+	{
+		explicit FFieldClassFilter(const FFieldClass* InFieldClass)
+			: FieldClass(InFieldClass)
+			, ObjectClass(nullptr)
+		{
+		}
+
+		explicit FFieldClassFilter(const UClass* InObjectClass)
+			: FieldClass(nullptr)
+			, ObjectClass(InObjectClass)
+		{
+		}
+
+		FString GetName() const
+		{
+			if (FieldClass)
+			{
+				return FieldClass->GetName();
+			}
+			if (ObjectClass)
+			{
+				return ObjectClass->GetName();
+			}
+			return FString();
+		}
+
+		bool TestClass(const FFieldClass* InFieldClass) const
+		{
+			return FieldClass && InFieldClass->IsChildOf(FieldClass);
+		}
+
+		bool TestClass(const UClass* InObjectClass) const
+		{
+			return ObjectClass && InObjectClass->IsChildOf(ObjectClass);
+		}
+
+	private:
+		const FFieldClass* FieldClass;
+		const UClass* ObjectClass;
+	};
+
+	/** Array of field types (eg, FProperty, UFunction, UScriptStruct, etc) that should be included or excluded in the current gather */
+	TArray<FFieldClassFilter> FieldTypesToInclude;
+	TArray<FFieldClassFilter> FieldTypesToExclude;
 
 	/** Array of field owner types (eg, UMyClass, FMyStruct, etc) that should have fields within them included or excluded in the current gather */
 	TArray<const UStruct*> FieldOwnerTypesToInclude;
