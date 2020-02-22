dc0aad3f61661b68348621696fef79a227a61929
daniel coelho <daniel.coelho@xa.epicgames.com>
2020-02-22 17:03:08 -0500

    USD: Widgets to edit prim attributes;
    Allows editing purpose and Kind attributes;
    Tweak PrimInfo UI to remove double-header;
    Change default new prim to 'assembly' kind as it likely is going to be;
    Fix purpose component tag not updating when purpose changes;
    Fix components being added directly to Stage Actor;
    
    #jira UE-87797
    #rb JeanMichel.Dignard
    
    #ROBOMERGE-SOURCE: CL 11575085 in //UE4/Release-4.25/... via CL 11575087
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Main) (v654-11333218)
    
    [CL 11585229 by daniel coelho in Main branch]

--- Engine/Plugins/Importers/USDImporter/Source/USDStageEditor/Private/Widgets/SUSDPrimPropertiesList.cpp
+++ Engine/Plugins/Importers/USDImporter/Source/USDStageEditor/Private/Widgets/SUSDPrimPropertiesList.cpp
@@ -2,21 +2,25 @@
 
 #include "SUSDPrimPropertiesList.h"
 
-#include "UnrealUSDWrapper.h"
+#include "USDLog.h"
 #include "USDMemory.h"
 #include "USDStageActor.h"
 #include "USDStageModule.h"
 #include "USDTypesConversion.h"
+#include "UnrealUSDWrapper.h"
 
 #include "EditorStyleSet.h"
 #include "Engine/World.h"
 #include "Modules/ModuleManager.h"
+#include "ScopedTransaction.h"
 
 #if USE_USD_SDK
 #include "USDIncludesStart.h"
 
 #include "pxr/base/tf/stringUtils.h"
+#include "pxr/usd/kind/registry.h"
 #include "pxr/usd/usd/prim.h"
+#include "pxr/usd/usdGeom/tokens.h"
 
 #include "USDIncludesEnd.h"
 
@@ -27,9 +31,119 @@ namespace UsdPrimPropertiesListConstants
 {
 	const FMargin LeftRowPadding( 6.0f, 2.5f, 2.0f, 2.5f );
 	const FMargin RightRowPadding( 3.0f, 2.5f, 2.0f, 2.5f );
+	const FMargin ComboBoxItemPadding( 3.0f, 0.0f, 2.0f, 0.0f );
 
 	const TCHAR* NormalFont = TEXT("PropertyWindow.NormalFont");
 }
+
+namespace UsdPrimPropertiesListImpl
+{
+	static TMap<FString, TArray<TSharedPtr<FString>>> TokenDropdownOptions;
+
+	void ResetOptions(const FString& TokenName)
+	{
+		TokenDropdownOptions.Remove(TokenName);
+	}
+
+	TArray<TSharedPtr<FString>>* GetTokenDropdownOptions(const FString& TokenName)
+	{
+		TArray<TSharedPtr<FString>>* FoundOptions = TokenDropdownOptions.Find(TokenName);
+		if (FoundOptions)
+		{
+			return FoundOptions;
+		}
+
+		if (TokenName == TEXT("Kind"))
+		{
+			TArray<TSharedPtr<FString>> Options;
+			{
+				FScopedUsdAllocs Allocs;
+
+				std::vector<pxr::TfToken> Kinds = pxr::KindRegistry::GetAllKinds();
+				Options.Reserve(Kinds.size());
+
+				for (const pxr::TfToken& Kind : Kinds)
+				{
+					Options.Add(MakeShared<FString>(UsdToUnreal::ConvertToken(Kind)));
+				}
+
+				// They are supposed to be in an unspecified order, so let's make them consistent
+				Options.Sort([](const TSharedPtr<FString>& A, const TSharedPtr<FString>& B)
+				{
+					return A.IsValid() && B.IsValid() && (*A < *B);
+				});
+			}
+			return &UsdPrimPropertiesListImpl::TokenDropdownOptions.Add(TokenName, Options);
+		}
+		else if (TokenName == UsdToUnreal::ConvertToken(pxr::UsdGeomTokens->purpose))
+		{
+			TArray<TSharedPtr<FString>> Options =
+			{
+				MakeShared<FString>(UsdToUnreal::ConvertToken(pxr::UsdGeomTokens->default_)),
+				MakeShared<FString>(UsdToUnreal::ConvertToken(pxr::UsdGeomTokens->proxy)),
+				MakeShared<FString>(UsdToUnreal::ConvertToken(pxr::UsdGeomTokens->render)),
+				MakeShared<FString>(UsdToUnreal::ConvertToken(pxr::UsdGeomTokens->guide)),
+			};
+			return &UsdPrimPropertiesListImpl::TokenDropdownOptions.Add(TokenName, Options);
+		}
+
+		return FoundOptions;
+	}
+
+	void SetPrimAttribute(const FString& PrimPath, const FString& AttributeName, const FString& Value)
+	{
+		FScopedTransaction Transaction(FText::Format(
+			LOCTEXT("SetPrimAttribute", "Set value '{0}' for attribute '{1}' of prim '{2}'"),
+			FText::FromString(Value),
+			FText::FromString(AttributeName),
+			FText::FromString(PrimPath)
+		));
+
+		IUsdStageModule& UsdStageModule = FModuleManager::Get().LoadModuleChecked< IUsdStageModule >("UsdStage");
+		AUsdStageActor* UsdStageActor = &UsdStageModule.GetUsdStageActor(GWorld);
+
+		FScopedUsdAllocs UsdAllocs;
+
+		bool bSuccess = false;
+
+		pxr::TfToken AttributeNameToken = UnrealToUsd::ConvertToken(*AttributeName).Get();
+
+		if (pxr::UsdStageRefPtr UsdStage = UsdStageActor->GetUsdStage())
+		{
+			pxr::UsdPrim UsdPrim = UsdStage->GetPrimAtPath(UnrealToUsd::ConvertPath(*PrimPath).Get());
+			if (UsdPrim)
+			{
+				if (AttributeName == TEXT("Kind"))
+				{
+					bSuccess = IUsdPrim::SetKind(UsdPrim, UnrealToUsd::ConvertToken(*Value).Get());
+				}
+				else if (AttributeNameToken == pxr::UsdGeomTokens->purpose)
+				{
+					pxr::UsdAttribute Attribute = UsdPrim.GetAttribute(AttributeNameToken);
+					bSuccess = Attribute.Set(UnrealToUsd::ConvertToken(*Value).Get());
+				}
+			}
+		}
+
+		if (!bSuccess)
+		{
+			UE_LOG(LogUsd, Error, TEXT("Failed to set value '%s' for attribute '%s' of prim '%s'"), *Value, *AttributeName, *PrimPath);
+		}
+	}
+}
+
+enum class EPrimPropertyWidget : uint8
+{
+	Text,
+	Dropdown,
+};
+
+struct FUsdPrimProperty : public TSharedFromThis< FUsdPrimProperty >
+{
+	FString Label;
+	FString Value;
+	EPrimPropertyWidget WidgetType = EPrimPropertyWidget::Text;
+};
 
 class SUsdPrimPropertyRow : public SMultiColumnTableRow< TSharedPtr< FUsdPrimProperty > >
 {
@@ -45,8 +159,11 @@ public:
 protected:
 	FText GetLabel() const { return FText::FromString( UsdPrimProperty->Label ); }
 	FText GetValue() const { return FText::FromString( UsdPrimProperty->Value ); }
-	
+	FText GetValueOrNone() const { return FText::FromString( UsdPrimProperty->Value.IsEmpty()? TEXT("none") : UsdPrimProperty->Value ); }
+
 private:
+	TSharedRef< SWidget > GenerateTextWidget(const TAttribute<FText>& Attribute);
+
 	TSharedPtr< FUsdPrimProperty > UsdPrimProperty;
 };
 
@@ -58,22 +175,52 @@ void SUsdPrimPropertyRow::Construct( const FArguments& InArgs, const TSharedPtr<
 }
 
 TSharedRef< SWidget > SUsdPrimPropertyRow::GenerateWidgetForColumn( const FName& ColumnName )
-{	
+{
 	TSharedRef< SWidget > ColumnWidget = SNullWidget::NullWidget;
 
 	if ( ColumnName == TEXT("PropertyName") )
 	{
-		SAssignNew( ColumnWidget, STextBlock )
-		.Text( this, &SUsdPrimPropertyRow::GetLabel )
-		.Font( FEditorStyle::GetFontStyle( UsdPrimPropertiesListConstants::NormalFont ) )
-		.Margin( UsdPrimPropertiesListConstants::LeftRowPadding );
+		ColumnWidget = SUsdPrimPropertyRow::GenerateTextWidget({this, &SUsdPrimPropertyRow::GetLabel});
 	}
 	else
 	{
-		SAssignNew( ColumnWidget, STextBlock )
-		.Text( this, &SUsdPrimPropertyRow::GetValue )
-		.Font( FEditorStyle::GetFontStyle( UsdPrimPropertiesListConstants::NormalFont ) )
-		.Margin( UsdPrimPropertiesListConstants::RightRowPadding );
+		if (UsdPrimProperty->WidgetType == EPrimPropertyWidget::Text)
+		{
+			ColumnWidget = GenerateTextWidget({this, &SUsdPrimPropertyRow::GetValue});
+		}
+		else if (UsdPrimProperty->WidgetType == EPrimPropertyWidget::Dropdown)
+		{
+			TArray<TSharedPtr<FString>>* Options = UsdPrimPropertiesListImpl::GetTokenDropdownOptions(UsdPrimProperty->Label);
+
+			// Show a dropdown if we know the available options for that token
+			if (Options)
+			{
+				SAssignNew(ColumnWidget, SComboBox<TSharedPtr<FString>>)
+				.OptionsSource(Options)
+				.OnGenerateWidget_Lambda([&](TSharedPtr<FString> Option)
+				{
+					return SUsdPrimPropertyRow::GenerateTextWidget(FText::FromString(*Option));
+				})
+				.OnSelectionChanged_Lambda([&](TSharedPtr<FString> ChosenOption, ESelectInfo::Type SelectInfo)
+				{
+					TSharedPtr<ITypedTableView<TSharedPtr<FUsdPrimProperty>>> PinnedParent = OwnerTablePtr.Pin();
+					TSharedPtr<SUsdPrimPropertiesList> ParentList = StaticCastSharedPtr<SUsdPrimPropertiesList>(PinnedParent);
+
+					UsdPrimPropertiesListImpl::SetPrimAttribute(ParentList->GetPrimPath(), UsdPrimProperty->Label, *ChosenOption);
+				})
+				[
+					SNew(STextBlock)
+					.Text(this, &SUsdPrimPropertyRow::GetValueOrNone)
+					.Font(FEditorStyle::GetFontStyle(UsdPrimPropertiesListConstants::NormalFont))
+					.Margin(UsdPrimPropertiesListConstants::ComboBoxItemPadding)
+				];
+			}
+			// Fallback to just displaying a simple text box
+			else
+			{
+				ColumnWidget = SUsdPrimPropertyRow::GenerateTextWidget({this, &SUsdPrimPropertyRow::GetValue});
+			}
+		}
 	}
 
 	return SNew( SHorizontalBox )
@@ -91,17 +238,28 @@ void SUsdPrimPropertyRow::SetUsdPrimProperty( const TSharedPtr< FUsdPrimProperty
 	UsdPrimProperty = InUsdPrimProperty;
 }
 
+TSharedRef< SWidget > SUsdPrimPropertyRow::GenerateTextWidget(const TAttribute<FText>& Attribute)
+{
+	return SNew(STextBlock)
+	.Text(Attribute)
+	.Font(FEditorStyle::GetFontStyle(UsdPrimPropertiesListConstants::NormalFont))
+	.Margin(UsdPrimPropertiesListConstants::RightRowPadding);
+}
+
 void SUsdPrimPropertiesList::Construct( const FArguments& InArgs, const TCHAR* InPrimPath )
 {
 	PrimPath = InPrimPath;
 
 	GeneratePropertiesList( InPrimPath );
 
+	// Clear map as usd file may have additional Kinds now
+	UsdPrimPropertiesListImpl::ResetOptions(TEXT("Kind"));
+
 	SAssignNew( HeaderRowWidget, SHeaderRow )
 
 	+SHeaderRow::Column( FName( TEXT("PropertyName") ) )
-	.DefaultLabel( LOCTEXT( "PropertyName", "Name" ) )
-	.FillWidth( 20.f )
+	.DefaultLabel( LOCTEXT( "PropertyName", "Property Name" ) )
+	.FillWidth( 25.f )
 
 	+SHeaderRow::Column( FName( TEXT("PropertyValue") ) )
 	.DefaultLabel( LOCTEXT( "PropertyValue", "Value" ) )
@@ -161,7 +319,7 @@ void SUsdPrimPropertiesList::GeneratePropertiesList( const TCHAR* InPrimPath )
 		FUsdPrimProperty PrimNameProperty;
 		PrimNameProperty.Label = TEXT("Name");
 		PrimNameProperty.Value = PrimName;
-	
+
 		PrimProperties.Add( MakeSharedUnreal< FUsdPrimProperty >( MoveTemp( PrimNameProperty ) ) );
 	}
 
@@ -177,6 +335,7 @@ void SUsdPrimPropertiesList::GeneratePropertiesList( const TCHAR* InPrimPath )
 		FUsdPrimProperty PrimKindProperty;
 		PrimKindProperty.Label = TEXT("Kind");
 		PrimKindProperty.Value = PrimKind;
+		PrimKindProperty.WidgetType = EPrimPropertyWidget::Dropdown;
 
 		PrimProperties.Add( MakeSharedUnreal< FUsdPrimProperty >( MoveTemp( PrimKindProperty ) ) );
 	}
@@ -192,9 +351,11 @@ void SUsdPrimPropertiesList::GeneratePropertiesList( const TCHAR* InPrimPath )
 			FUsdPrimProperty PrimAttributeProperty;
 			PrimAttributeProperty.Label = UsdToUnreal::ConvertString( PrimAttribute.GetName().GetString() );
 
+			// Just the Purpose attribute for now
+			PrimAttributeProperty.WidgetType = PrimAttribute.GetName() == pxr::UsdGeomTokens->purpose ? EPrimPropertyWidget::Dropdown : EPrimPropertyWidget::Text;
+
 			pxr::VtValue VtValue;
 			PrimAttribute.Get( &VtValue, TimeCode );
-
 			FString AttributeValue = UsdToUnreal::ConvertString( pxr::TfStringify( VtValue ).c_str() );
 
 			PrimAttributeProperty.Value = MoveTemp( AttributeValue );
