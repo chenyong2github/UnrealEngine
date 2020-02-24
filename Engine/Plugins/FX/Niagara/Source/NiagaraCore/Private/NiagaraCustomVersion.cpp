831890c86af14b459c00abeb15799c8bf6f4bb37
mihnea balta <Mihnea.Balta@epicgames.com>
2020-02-24 14:14:53 -0500

    Fixed unbound UAV parameters causing crashes on Vulkan and consoles. * Added functions to the instance batcher to retrieve empty UAVs from a pool which gets reset after each dispatch. * Changed the GetDummy*Buffer() functions in NiagaraRenderer to return an FRHIShaderResourceView* instead of an FRWBuffer. We never want to use these for UAVs. * Fixed a validation error in FNiagaraDataInterfaceProxyNeighborGrid3D (ClearUAVUint used on SINT buffers).
    Also merged CL 11535205 to fix some game/render thread races in UNiagaraDataInterfaceGrid2DCollection.
    
    #jira none
    #rb Stu.McKenna
    
    [CL 11589537 by mihnea balta in 4.25 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x34F24743, 0x1FA645F1, 0xA99731B3, 0x55A71FD3);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0XC9A39220, 0X1AE54A45, 0X8A01CC89, 0X64E91B28);
