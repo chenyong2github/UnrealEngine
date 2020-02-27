f29fdc90b92748d795719257ec0c0ced17defbca
Wei Liu <Wei.Liu@epicgames.com>
2020-02-26 21:40:58 -0500

    Support LightShaft work with MSAA on IOS.
    Make all postprocess passes including postprocess materials work properly with MSAA on IOS.
    
    #jira UE-88184
    
    #rb dmitriy.dyomin, allan.bentham, mi.wang, jack.porter
    
    [CL 11646197 by Wei Liu in 4.25 branch]

--- Engine/Source/Runtime/Renderer/Private/SceneVisibility.cpp
+++ Engine/Source/Runtime/Renderer/Private/SceneVisibility.cpp
@@ -4095,9 +4095,6 @@ void FSceneRenderer::PostVisibilityFrameSetup(FILCUpdatePrimTaskData& OutILCTask
 			{
 				VisibleLightViewInfo.bInViewFrustum = true;
 
-				static const auto MobileMSAAVar = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.MobileMSAA"));
-				bool bNotMobileMSAA = !(MobileMSAAVar ? MobileMSAAVar->GetValueOnRenderThread() > 1 : false);
-
 				// Setup single sun-shaft from direction lights for mobile.
 				if(bCheckLightShafts && LightSceneInfo->bEnableLightShaftBloom)
 				{
@@ -4118,7 +4115,7 @@ void FSceneRenderer::PostVisibilityFrameSetup(FILCUpdatePrimTaskData& OutILCTask
 						&& (LightSceneInfo->Proxy->GetLightType() == LightType_Directional 
 							|| DistanceToBlurOrigin < LightSceneInfo->Proxy->GetRadius() * PointLightRadiusFadeFactor))
 					{
-						View.bLightShaftUse = bNotMobileMSAA;
+						View.bLightShaftUse = true;
 						View.LightShaftCenter.X = ProjectedBlurOrigin.X / ProjectedBlurOrigin.W;
 						View.LightShaftCenter.Y = ProjectedBlurOrigin.Y / ProjectedBlurOrigin.W;
 						// TODO: Might want to hookup different colors for these.
