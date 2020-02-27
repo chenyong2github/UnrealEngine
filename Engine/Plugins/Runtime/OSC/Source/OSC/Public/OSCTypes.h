c7e7bba0b52a305147ac3e7d0ddbaa02f81e140c
Rob Gay <Rob.Gay@epicgames.com>
2020-02-27 13:24:09 -0500

    Change names of OSC Types to avoid collisions with common macro names & reduce type tag size to uint8 #rb ethan.geller #jira UE-89566
    [CL 11672362 by Rob Gay in Main branch]

--- Engine/Plugins/Runtime/OSC/Source/OSC/Public/OSCTypes.h
+++ Engine/Plugins/Runtime/OSC/Source/OSC/Public/OSCTypes.h
@@ -6,22 +6,22 @@
 #include "Math/Color.h"
 
 
-enum class EOSCTypeTag {
-
-	INT32 = 'i',
-	FLOAT = 'f',
-	DOUBLE = 'd',
-	STRING = 's',
-	BLOB = 'b',
-	TIME = 't',
-	INT64 = 'h',
-	CHAR = 'c',
-	TRUE = 'T',
-	FALSE = 'F',
-	NIL = 'N',
-	INFINITUM = 'I',
-	COLOR = 'r',
-	TERMINATE = '\0'
+enum class EOSCTypeTag : uint8
+{
+	OSC_INT32		= 'i',
+	OSC_FLOAT		= 'f',
+	OSC_DOUBLE		= 'd',
+	OSC_STRING		= 's',
+	OSC_BLOB		= 'b',
+	OSC_TIME		= 't',
+	OSC_INT64		= 'h',
+	OSC_CHAR		= 'c',
+	OSC_TRUE		= 'T',
+	OSC_FALSE		= 'F',
+	OSC_NIL			= 'N',
+	OSC_INFINITUM	= 'I',
+	OSC_COLOR		= 'r',
+	OSC_TERMINATE	= '\0'
 };
 
 
@@ -29,63 +29,63 @@ class FOSCType
 {
 public:
 	explicit FOSCType(int32 Value)
-		: TypeTag(EOSCTypeTag::INT32)
+		: TypeTag(EOSCTypeTag::OSC_INT32)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(int64 Value)
-		: TypeTag(EOSCTypeTag::INT64)
+		: TypeTag(EOSCTypeTag::OSC_INT64)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(ANSICHAR Value)
-		: TypeTag(EOSCTypeTag::CHAR)
+		: TypeTag(EOSCTypeTag::OSC_CHAR)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(uint64 Value)
-		: TypeTag(EOSCTypeTag::TIME)
+		: TypeTag(EOSCTypeTag::OSC_TIME)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(float Value)
-		: TypeTag(EOSCTypeTag::FLOAT)
+		: TypeTag(EOSCTypeTag::OSC_FLOAT)
 		, Data(Value)
 		, Blob()
 	
 	{
 	}
 	explicit FOSCType(double Value)
-		: TypeTag(EOSCTypeTag::DOUBLE)
+		: TypeTag(EOSCTypeTag::OSC_DOUBLE)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(bool Value)
-		: TypeTag(Value ? EOSCTypeTag::TRUE : EOSCTypeTag::FALSE)
+		: TypeTag(Value ? EOSCTypeTag::OSC_TRUE : EOSCTypeTag::OSC_FALSE)
 		, Data(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(const FString& Value)
-		: TypeTag(EOSCTypeTag::STRING)
+		: TypeTag(EOSCTypeTag::OSC_STRING)
 		, Data(0)
 		, String(Value)
 		, Blob()
 	{
 	}
 	explicit FOSCType(const TArray<uint8>& Value)
-		: TypeTag(EOSCTypeTag::BLOB)
+		: TypeTag(EOSCTypeTag::OSC_BLOB)
 		, Data(0)
 		, Blob(Value)
 	{
 	}
 	explicit FOSCType(FColor Value)
-		: TypeTag(EOSCTypeTag::COLOR)
+		: TypeTag(EOSCTypeTag::OSC_COLOR)
 		, Data(0)
 		, Blob()
 		, Color(Value)
@@ -101,38 +101,38 @@ public:
 
 	EOSCTypeTag GetTypeTag() const { return TypeTag; }
 
-	bool IsInt32() const { return TypeTag == EOSCTypeTag::INT32; }
+	bool IsInt32() const { return TypeTag == EOSCTypeTag::OSC_INT32; }
 	int32 GetInt32() const { return Data.Int32; }
 
-	bool IsInt64() const { return TypeTag == EOSCTypeTag::INT64; }
+	bool IsInt64() const { return TypeTag == EOSCTypeTag::OSC_INT64; }
 	int64 GetInt64() const { return Data.Int64; }
 
-	bool IsTimeTag() const { return TypeTag == EOSCTypeTag::TIME; }
+	bool IsTimeTag() const { return TypeTag == EOSCTypeTag::OSC_TIME; }
 	uint64 GetTimeTag() const { return Data.Time; }
 
-	bool IsBool() const { return TypeTag == EOSCTypeTag::TRUE || TypeTag == EOSCTypeTag::FALSE; }
+	bool IsBool() const { return TypeTag == EOSCTypeTag::OSC_TRUE || TypeTag == EOSCTypeTag::OSC_FALSE; }
 	bool GetBool() const { return Data.Bool; }
 
-	bool IsChar() const { return TypeTag == EOSCTypeTag::CHAR; }
+	bool IsChar() const { return TypeTag == EOSCTypeTag::OSC_CHAR; }
 	ANSICHAR GetChar() const { return Data.Char; }
 
-	bool IsFloat() const { return TypeTag == EOSCTypeTag::FLOAT; }
+	bool IsFloat() const { return TypeTag == EOSCTypeTag::OSC_FLOAT; }
 	float GetFloat() const { return Data.Float; }
 	
-	bool IsDouble() const { return TypeTag == EOSCTypeTag::DOUBLE; }
+	bool IsDouble() const { return TypeTag == EOSCTypeTag::OSC_DOUBLE; }
 	double GetDouble() const { return Data.Double; }
 
-	bool IsString() const { return TypeTag == EOSCTypeTag::STRING; }
+	bool IsString() const { return TypeTag == EOSCTypeTag::OSC_STRING; }
 	FString GetString() const { return String; }
 
-	bool IsBlob() const { return TypeTag == EOSCTypeTag::BLOB; }
+	bool IsBlob() const { return TypeTag == EOSCTypeTag::OSC_BLOB; }
 	TArray<uint8> GetBlob() const { return Blob; }
 
-	bool IsColor() const { return TypeTag == EOSCTypeTag::COLOR; }
+	bool IsColor() const { return TypeTag == EOSCTypeTag::OSC_COLOR; }
 	FColor GetColor()  const { return Color; }
 
-	bool IsNil() const { return TypeTag == EOSCTypeTag::NIL; }
-	bool IsInfinitum() const { return TypeTag == EOSCTypeTag::INFINITUM; }
+	bool IsNil() const { return TypeTag == EOSCTypeTag::OSC_NIL; }
+	bool IsInfinitum() const { return TypeTag == EOSCTypeTag::OSC_INFINITUM; }
 
 private:
 	union DataTypes
