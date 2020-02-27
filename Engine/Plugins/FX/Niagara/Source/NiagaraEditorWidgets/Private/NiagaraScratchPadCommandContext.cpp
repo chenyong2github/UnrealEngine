4e61c0622e981418855a1d2ef24647d2435c976b
frank fella <Frank.Fella@epicgames.com>
2020-02-27 17:18:34 -0500

    Merging 11604247 - Niagara - Several fixes for scratchpad UX. + Selection refactor. + Pinning support. + Proper apply support. + Cut/Copy/Paste support. + Fix trailing numbers on script names when creating new scripts and renaming scripts. + General UI engine consistency.
    #jira UE-89466
    #rb none
    
    #ROBOMERGE-SOURCE: CL 11703518 in //UE4/Release-4.25/... via CL 11703596
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Main) (v656-11643781)
    
    [CL 11703701 by frank fella in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditorWidgets/Private/NiagaraScratchPadCommandContext.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditorWidgets/Private/NiagaraScratchPadCommandContext.cpp
@@ -2,6 +2,9 @@
 
 #include "NiagaraScratchPadCommandContext.h"
 #include "ViewModels/NiagaraScratchPadViewModel.h"
+#include "ViewModels/NiagaraScratchPadScriptViewModel.h"
+#include "NiagaraEditorModule.h"
+#include "NiagaraEditorCommands.h"
 
 #include "Framework/Notifications/NotificationManager.h"
 #include "Logging/LogMacros.h"
@@ -36,41 +39,29 @@ bool FNiagaraScratchPadCommandContext::ProcessCommandBindings(const FKeyEvent& I
 	return GetCommands()->ProcessCommandBindings(InKeyEvent);
 }
 
-bool FNiagaraScratchPadCommandContext::AddEditMenuItems(FMenuBuilder& MenuBuilder)
+void FNiagaraScratchPadCommandContext::AddMenuItems(FMenuBuilder& MenuBuilder)
 {
-	bool bSupportsCut = false;
-	bool bSupportsCopy = false;
-	bool bSupportsPaste = false;
-	bool bSupportsDelete = true;
-	if (bSupportsCut || bSupportsCopy || bSupportsPaste || bSupportsDelete)
+	MenuBuilder.BeginSection("ScriptEdit", LOCTEXT("ScriptEditActions", "Edit"));
 	{
-		MenuBuilder.BeginSection("EntryEdit", LOCTEXT("EntryEditActions", "Edit"));
-		{
-			if (bSupportsCut)
-			{
-				TAttribute<FText> CanCutToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanCutSelectedScriptsToolTip));
-				MenuBuilder.AddMenuEntry(FGenericCommands::Get().Cut, NAME_None, TAttribute<FText>(), CanCutToolTip);
-			}
-			if (bSupportsCopy)
-			{
-				TAttribute<FText> CanCopyToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanCopySelectedScriptsToolTip));
-				MenuBuilder.AddMenuEntry(FGenericCommands::Get().Copy, NAME_None, TAttribute<FText>(), CanCopyToolTip);
-			}
-			if (bSupportsPaste)
-			{
-				TAttribute<FText> CanPasteToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanPasteSelectedScriptsToolTip));
-				MenuBuilder.AddMenuEntry(FGenericCommands::Get().Paste, NAME_None, TAttribute<FText>(), CanPasteToolTip);
-			}
-			if (bSupportsDelete)
-			{
-				TAttribute<FText> CanDeleteToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanDeleteSelectedScriptsToolTip));
-				MenuBuilder.AddMenuEntry(FGenericCommands::Get().Delete, NAME_None, TAttribute<FText>(), CanDeleteToolTip);
-			}
-		}
-		MenuBuilder.EndSection();
-		return true;
+		TAttribute<FText> CanCutToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanCutSelectedScriptsToolTip));
+		MenuBuilder.AddMenuEntry(FGenericCommands::Get().Cut, NAME_None, TAttribute<FText>(), CanCutToolTip);
+
+		TAttribute<FText> CanCopyToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanCopySelectedScriptsToolTip));
+		MenuBuilder.AddMenuEntry(FGenericCommands::Get().Copy, NAME_None, TAttribute<FText>(), CanCopyToolTip);
+
+		TAttribute<FText> CanPasteToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanPasteSelectedScriptsToolTip));
+		MenuBuilder.AddMenuEntry(FGenericCommands::Get().Paste, NAME_None, TAttribute<FText>(), CanPasteToolTip);
+
+		TAttribute<FText> CanDeleteToolTip = TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateSP(this, &FNiagaraScratchPadCommandContext::GetCanDeleteSelectedScriptsToolTip));
+		MenuBuilder.AddMenuEntry(FGenericCommands::Get().Delete, NAME_None, TAttribute<FText>(), CanDeleteToolTip);
 	}
-	return false;
+	MenuBuilder.EndSection();
+
+	MenuBuilder.BeginSection("Script", LOCTEXT("ScriptActions", "Script"));
+	{
+		MenuBuilder.AddMenuEntry(FNiagaraEditorModule::Get().Commands().Apply);
+	}
+	MenuBuilder.EndSection();
 }
 
 void FNiagaraScratchPadCommandContext::SetupCommands()
@@ -87,53 +78,64 @@ void FNiagaraScratchPadCommandContext::SetupCommands()
 	Commands->MapAction(FGenericCommands::Get().Delete, FUIAction(
 		FExecuteAction::CreateSP(this, &FNiagaraScratchPadCommandContext::DeleteSelectedScripts),
 		FCanExecuteAction::CreateSP(this, &FNiagaraScratchPadCommandContext::CanDeleteSelectedScripts)));
+	Commands->MapAction(FNiagaraEditorModule::Get().Commands().Apply, FUIAction(
+		FExecuteAction::CreateSP(this, &FNiagaraScratchPadCommandContext::ApplyChangesToSelectedScripts),
+		FCanExecuteAction::CreateSP(this, &FNiagaraScratchPadCommandContext::CanApplyChangesToSelectedScripts)));
 }
 
 bool FNiagaraScratchPadCommandContext::CanCutSelectedScripts() const
 {
-	return false;
+	return ScratchPadViewModel->GetActiveScriptViewModel().IsValid();
 }
 
 FText FNiagaraScratchPadCommandContext::GetCanCutSelectedScriptsToolTip() const
 {
-	return FText();
+	return LOCTEXT("CutToolTip", "Cut the selected scratch pad script.");
 }
 
 void FNiagaraScratchPadCommandContext::CutSelectedScripts() const
 {
+	if (CanCutSelectedScripts())
+	{
+		FScopedTransaction Transaction(LOCTEXT("CutScratchPadScriptTransaction", "Cut the selected scratch pad script to the system clipboard."));
+		ScratchPadViewModel->CopyActiveScript();
+		ScratchPadViewModel->DeleteActiveScript();
+	}
 }
 
 bool FNiagaraScratchPadCommandContext::CanCopySelectedScripts() const
 {
-	return false;
+	return ScratchPadViewModel->GetActiveScriptViewModel().IsValid();
 }
 
 FText FNiagaraScratchPadCommandContext::GetCanCopySelectedScriptsToolTip() const
 {
-	return FText();
+	return LOCTEXT("CopyToolTip", "Copy the selected scratch pad script to the system clipboard.");
 }
 
 void FNiagaraScratchPadCommandContext::CopySelectedScripts() const
 {
+	ScratchPadViewModel->CopyActiveScript();
 }
 
 bool FNiagaraScratchPadCommandContext::CanPasteSelectedScripts() const
 {
-	return false;
+	return ScratchPadViewModel->CanPasteScript();
 }
 
 FText FNiagaraScratchPadCommandContext::GetCanPasteSelectedScriptsToolTip() const
 {
-	return FText();
+	return LOCTEXT("PasteToolTip", "Paste a scratch pad script from the system clipboard.");;
 }
 
 void FNiagaraScratchPadCommandContext::PasteSelectedScripts() const
 {
+	ScratchPadViewModel->PasteScript();
 }
 
 bool FNiagaraScratchPadCommandContext::CanDeleteSelectedScripts() const
 {
-	return true;
+	return ScratchPadViewModel->GetActiveScriptViewModel().IsValid();
 }
 
 FText FNiagaraScratchPadCommandContext::GetCanDeleteSelectedScriptsToolTip() const
@@ -144,6 +146,19 @@ FText FNiagaraScratchPadCommandContext::GetCanDeleteSelectedScriptsToolTip() con
 void FNiagaraScratchPadCommandContext::DeleteSelectedScripts() const
 {
 	ScratchPadViewModel->DeleteActiveScript();
+}
+
+bool FNiagaraScratchPadCommandContext::CanApplyChangesToSelectedScripts() const
+{
+	return ScratchPadViewModel->GetActiveScriptViewModel().IsValid() && ScratchPadViewModel->GetActiveScriptViewModel()->CanApplyChanges();
+}
+
+void FNiagaraScratchPadCommandContext::ApplyChangesToSelectedScripts() const
+{
+	if (CanApplyChangesToSelectedScripts())
+	{
+		ScratchPadViewModel->GetActiveScriptViewModel()->ApplyChanges();
+	}
 }
 
 #undef LOCTEXT_NAMESPACE
