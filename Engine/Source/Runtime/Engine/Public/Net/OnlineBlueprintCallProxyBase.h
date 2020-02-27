a073b8a90207e49fd3ffdfa25cbe97035269a8f6
michael noland <michael.noland@epicgames.com>
2020-02-27 17:17:01 -0500

    Blueprints: Merged UK2Node_LatentOnlineCall into UK2Node_AsyncAction, and moved it from the Kismet module into BlueprintGraph #jira UE-89731 #rb marc.audy
    #ROBOMERGE-SOURCE: CL 11703390 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11703446 by michael noland in 4.25-Plus branch]

--- Engine/Source/Runtime/Engine/Public/Net/OnlineBlueprintCallProxyBase.h
+++ Engine/Source/Runtime/Engine/Public/Net/OnlineBlueprintCallProxyBase.h
@@ -5,16 +5,14 @@
 #include "UObject/ObjectMacros.h"
 #include "UObject/Object.h"
 #include "UObject/ScriptMacros.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
 #include "OnlineBlueprintCallProxyBase.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEmptyOnlineDelegate);
 
 UCLASS(MinimalAPI)
-class UOnlineBlueprintCallProxyBase : public UObject
+class UOnlineBlueprintCallProxyBase : public UBlueprintAsyncActionBase
 {
 	GENERATED_UCLASS_BODY()
 
-	// Called to trigger the actual online action once the delegates have been bound
-	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly="true"), Category="Online")
-	virtual void Activate();
 };
