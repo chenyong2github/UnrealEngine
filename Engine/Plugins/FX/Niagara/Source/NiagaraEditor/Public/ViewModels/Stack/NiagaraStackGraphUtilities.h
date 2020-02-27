6d4ffff5747fec511345e69cf1ad023e091a5744
frank fella <Frank.Fella@epicgames.com>
2020-02-26 22:22:16 -0500

    Merging 11592184 from main - Niagara - Fix an issue where data interfaces defined in emitters could end up with name collisions which would caused unintended sharing of data interface objects.  When registering data interfaces while compiling emitters the names are made unique.
    #jira UE-77625
    #jira UE-89058
    #rb none
    
    #ROBOMERGE-SOURCE: CL 11646462 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11646463 by frank fella in 4.25-Plus branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/Stack/NiagaraStackGraphUtilities.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/Stack/NiagaraStackGraphUtilities.h
@@ -150,8 +150,6 @@ namespace FNiagaraStackGraphUtilities
 	TOptional<FName> GetNamespaceForScriptUsage(ENiagaraScriptUsage ScriptUsage);
 	ENiagaraParameterScope GetScopeForScriptUsage(ENiagaraScriptUsage ScriptUsage);
 	
-	void GetOwningEmitterAndScriptForStackNode(UNiagaraNode& StackNode, UNiagaraSystem& OwningSystem, UNiagaraEmitter*& OutEmitter, UNiagaraScript*& OutScript);
-
 	bool IsValidDefaultDynamicInput(UNiagaraScript& OwningScript, UEdGraphPin& DefaultPin);
 
 	bool CanWriteParameterFromUsage(FNiagaraVariable Parameter, ENiagaraScriptUsage Usage);
