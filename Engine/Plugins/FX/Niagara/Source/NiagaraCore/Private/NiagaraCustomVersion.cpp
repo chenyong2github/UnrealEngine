6b08ecb3334ed0f0f621badb2200bc2a7374c5a2
mihnea balta <Mihnea.Balta@epicgames.com>
2020-02-27 12:49:15 -0500

    Simulation stages can kill particles.
    Also:
    * fix for dead particles not being removed if the emitter had stages
    * workaround for a bug in HLSLcc which caused emitters with stages to have 0 particles on Vulkan and GLES.
    * MaxUpdateIterations was always used, even if use shader stages was disabled. This caused some regular GPU emitters to incorrectly run multiple dispatches.
    
    #jira none
    #rb stu.mckenna
    
    #ROBOMERGE-SOURCE: CL 11668844 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11668875 by mihnea balta in 4.25-Plus branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x292C71D3, 0x55F64C04, 0xA9655053, 0xC8C523E1);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x318C2E9D, 0xB5794FBD, 0xABE176B1, 0xC31904C9);
