6a66e00991b4e1a181a8b89bd7d8113e4680f9ad
steve smith <steve.smith@epicgames.com>
2020-02-27 13:32:29 -0500

    Fix missing header guard in SteamVR public header. #jira UE-89678 #rb trivial
    #ROBOMERGE-SOURCE: CL 11673036 in //UE4/Release-4.25/... via CL 11673058
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Main) (v656-11643781)
    
    [CL 11673101 by steve smith in Main branch]

--- Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRInputDevice/Public/SteamVRInputPublic.h
+++ Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRInputDevice/Public/SteamVRInputPublic.h
@@ -28,6 +28,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
+#pragma once
+
 struct FSteamVRSkeletonTransform_t
 {
 	FTransform Wrist;
