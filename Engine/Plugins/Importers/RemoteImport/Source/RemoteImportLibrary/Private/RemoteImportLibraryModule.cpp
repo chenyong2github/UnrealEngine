ba4e85af2a396a30c8bac3a86605494bffdc2e03
johan duparc <Johan.Duparc@epicgames.com>
2020-02-27 19:45:29 -0500

    Copy of 11160369 from //UE4/Dev-Enterprise:
    RemoteImport:
    Add module RemoteImportLibrary: base types used in RemoteImport features
    
    #rb jeanluc.corenthin
    #jira UEENT-3394
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: johan.duparc
    #ROBOMERGE-SOURCE: CL 11577101 in //UE4/Release-4.25/... via CL 11577107 via CL 11585371
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v656-11643781)
    
    [CL 11713328 by johan duparc in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Importers/RemoteImport/Source/RemoteImportLibrary/Private/RemoteImportLibraryModule.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "RemoteImportLibraryModule.h"
+#include "RemoteImportLibraryLog.h"
+
+#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FRemoteImportLibraryModule, RemoteImportLibrary);
+
+DEFINE_LOG_CATEGORY(LogRemoteImportLibrary)
