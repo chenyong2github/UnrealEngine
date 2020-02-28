945bf3f97837a015ddd1a6814a90e6ef669035a1
daniel coelho <daniel.coelho@xa.epicgames.com>
2020-02-27 19:56:42 -0500

    VariantManager: Fixes related to nested SwitchActors
    Fix parent SwitchActor clearing choices of child SwitchActors;
    Fix child SwitchActors being able to set children as visible when they themselves are unselected;
    
    #jira UE-89296
    #rb JeanMichel.Dignard
    [FYI] Thomas.Convard
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: daniel.coelho
    #ROBOMERGE-SOURCE: CL 11588400 in //UE4/Release-4.25/... via CL 11588406 via CL 11588411
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v656-11643781)
    
    [CL 11713964 by daniel coelho in Dev-Tools-Staging branch]

--- Engine/Plugins/Enterprise/VariantManagerContent/Source/VariantManagerContent/Private/SwitchActor.cpp
+++ Engine/Plugins/Enterprise/VariantManagerContent/Source/VariantManagerContent/Private/SwitchActor.cpp
@@ -5,6 +5,35 @@
 #include "Algo/Sort.h"
 #include "CoreMinimal.h"
 
+namespace SwitchActorImpl
+{
+	int32 DiscoverSelectedOption(const TArray<AActor*>& Actors)
+	{
+		int32 SingleVisibleChildIndex = INDEX_NONE;
+		for (int32 Index = 0; Index < Actors.Num(); ++Index)
+		{
+			const AActor* Actor = Actors[Index];
+			if (USceneComponent* ActorRoot = Actor->GetRootComponent())
+			{
+				if (ActorRoot->IsVisible())
+				{
+					if (SingleVisibleChildIndex == INDEX_NONE)
+					{
+						SingleVisibleChildIndex = Index;
+					}
+					else
+					{
+						SingleVisibleChildIndex = INDEX_NONE;
+						break;
+					}
+				}
+			}
+		}
+
+		return SingleVisibleChildIndex;
+	}
+}
+
 ASwitchActor::ASwitchActor(const FObjectInitializer& Init)
 	: Super(Init)
 {
@@ -16,8 +45,9 @@ ASwitchActor::ASwitchActor(const FObjectInitializer& Init)
 
 TArray<AActor*> ASwitchActor::GetOptions() const
 {
+	const bool bResetArray = false;
 	TArray<AActor*> Result;
-	GetAttachedActors(Result, false);
+	GetAttachedActors(Result, bResetArray);
 
 	// We have to sort these by FName because the attach order is not guaranteed
 	// It seems to invert when going into PIE, for example
@@ -31,30 +61,7 @@ TArray<AActor*> ASwitchActor::GetOptions() const
 
 int32 ASwitchActor::GetSelectedOption() const
 {
-	TArray<AActor*> Actors = GetOptions();
-
-	int32 SingleVisibleChildIndex = INDEX_NONE;
-	for (int32 Index = 0; Index < Actors.Num(); ++Index)
-	{
-		const AActor* Actor = Actors[Index];
-		if (USceneComponent* ActorRoot = Actor->GetRootComponent())
-		{
-			if (ActorRoot->IsVisible())
-			{
-				if (SingleVisibleChildIndex == INDEX_NONE)
-				{
-					SingleVisibleChildIndex = Index;
-				}
-				else
-				{
-					SingleVisibleChildIndex = INDEX_NONE;
-					break;
-				}
-			}
-		}
-	}
-
-	return SingleVisibleChildIndex;
+	return LastSelectedOption;
 }
 
 void ASwitchActor::SelectOption(int32 OptionIndex)
@@ -66,18 +73,32 @@ void ASwitchActor::SelectOption(int32 OptionIndex)
 		return;
 	}
 
+	Modify();
+
+	// So that we set our children to visible only if we ourselves are visible,
+	// which helps this actor behave nicely when child of another switchactor that hasn't selected us
+	bool bOurVisibility = this->GetRootComponent()->GetVisibleFlag();
+
 	for (int32 Index = 0; Index < Actors.Num(); ++Index)
 	{
-		if (AActor* Actor = Actors[Index])
+		AActor* Actor = Actors[Index];
+		if (ASwitchActor* ChildSwitchActor = Cast<ASwitchActor>(Actor))
+		{
+			ChildSwitchActor->SetVisibility(bOurVisibility && Index == OptionIndex);
+		}
+		else if(Actor)
 		{
 			if (USceneComponent* ActorRootComponent = Actor->GetRootComponent())
 			{
 				ActorRootComponent->Modify();
-				ActorRootComponent->SetVisibility(Index == OptionIndex, true);
+
+				const bool bPropagateToChildren = true;
+				ActorRootComponent->SetVisibility(bOurVisibility && Index == OptionIndex, bPropagateToChildren);
 			}
 		}
 	}
 
+	LastSelectedOption = OptionIndex;
 	OnSwitchActorSwitch.Broadcast(OptionIndex);
 }
 
@@ -86,4 +107,22 @@ FOnSwitchActorSwitch& ASwitchActor::GetOnSwitchDelegate()
 	return OnSwitchActorSwitch;
 }
 
+void ASwitchActor::SetVisibility(bool bVisible)
+{
+	Modify();
+
+	const bool bPropagateToChildren = true;
+	this->GetRootComponent()->SetVisibility(bVisible, bPropagateToChildren);
+	SelectOption(LastSelectedOption);
+}
+
+void ASwitchActor::PostLoad()
+{
+	Super::PostLoad();
+
+	if (LastSelectedOption == INDEX_NONE)
+	{
+		LastSelectedOption = SwitchActorImpl::DiscoverSelectedOption(GetOptions());
+	}
+}
 
