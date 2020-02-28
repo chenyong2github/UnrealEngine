dcdcaf384eefea66abbd0cca3d79e9ffd72d8ed5
nicholas goldstein <nicholas.goldstein@epicgames.com>
2020-02-28 12:48:13 -0500

    Add postload pass for NiagaraGraph to fixup null script variables in existing assets.
    #jira UE-89643
    #rb frank.fella
    
    [CL 11751651 by nicholas goldstein in 4.25 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Public/NiagaraCustomVersion.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Public/NiagaraCustomVersion.h
@@ -144,6 +144,8 @@ struct FNiagaraCustomVersion
 
 		PrecompileNamespaceFixup, // Promote parameters used across script executions to the Dataset, and Demote unused parameters.
 
+		FixNullScriptVariables, // Postload fixup in UNiagaraGraph to fixup VariableToScriptVariable map entries being null. 
+
 		// DO NOT ADD A NEW VERSION UNLESS YOU HAVE TALKED TO THE NIAGARA LEAD. Mismanagement of these versions can lead to data loss if it is adjusted in multiple streams simultaneously.
 		// -----<new versions can be added above this line>  -------------------------------------------------
 		VersionPlusOne,
