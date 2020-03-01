ac047add8f78c3b81f7f45538f0a81cb3790b567
simon therriault <Simon.Therriault@epicgames.com>
2020-03-01 16:54:42 -0500

    Merging CL# 11619735
    - Fixing default livelink preset not loading when depending on other module. Moved to PostEngineInit instead.
    
    #rb alejandro.arango
    #jira UE-89513
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: simon.therriault
    #ROBOMERGE-SOURCE: CL 11743984 in //UE4/Release-4.25/... via CL 11743996 via CL 11776423
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v656-11643781)
    
    [CL 11808912 by simon therriault in Dev-Tools-Staging branch]

--- Engine/Plugins/VirtualProduction/TimedDataMonitor/Source/TimedDataMonitorEditor/Private/STimedDataMonitorPanel.cpp
+++ Engine/Plugins/VirtualProduction/TimedDataMonitor/Source/TimedDataMonitorEditor/Private/STimedDataMonitorPanel.cpp
@@ -362,23 +362,45 @@ void STimedDataMonitorPanel::Construct(const FArguments& InArgs)
 				.FillWidth(0.5f)
 				.Padding(0.f, 0.f, 10.f, 0.f)
 				[
-					SNew(SBorder)
-					.HAlign(HAlign_Center)
-					.VAlign(VAlign_Center)
-					.Padding(FMargin(4.f, 4.f, 4.f, 4.f))
+					SNew(SVerticalBox)
+					+ SVerticalBox::Slot()
+					.AutoHeight()
 					[
-						SAssignNew(TimedDataGenlockWidget, STimedDataGenlock, SharedThis<STimedDataMonitorPanel>(this))
+						SNew(STextBlock)
+						.Text(LOCTEXT("GenlockLabel", "Genlock"))
+					]
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						SNew(SBorder)
+						.HAlign(HAlign_Center)
+						.VAlign(VAlign_Center)
+						.Padding(FMargin(4.f, 4.f, 4.f, 4.f))
+						[
+							SAssignNew(TimedDataGenlockWidget, STimedDataGenlock, SharedThis<STimedDataMonitorPanel>(this))
+						]
 					]
 				]
 				+ SHorizontalBox::Slot()
 				.FillWidth(0.5f)
 				.Padding(0.f)
 				[
-					SNew(SBorder)
-					.HAlign(HAlign_Center)
-					.VAlign(VAlign_Center)
+					SNew(SVerticalBox)
+					+ SVerticalBox::Slot()
+					.AutoHeight()
 					[
-						SAssignNew(TimedDataTimecodeWidget, STimedDataTimecodeProvider, SharedThis<STimedDataMonitorPanel>(this))
+						SNew(STextBlock)
+						.Text(LOCTEXT("TimecodeLabel", "Timecode Provider"))
+					]
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						SNew(SBorder)
+						.HAlign(HAlign_Center)
+						.VAlign(VAlign_Center)
+						[
+							SAssignNew(TimedDataTimecodeWidget, STimedDataTimecodeProvider, SharedThis<STimedDataMonitorPanel>(this))
+						]
 					]
 				]
 			]
