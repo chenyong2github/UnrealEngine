2c00357703fb7e47b9b9e4d4bc20d8a6be806012
arne schober <Arne.Schober@epicgames.com>
2020-03-02 16:02:49 -0500

    RL - xbox instrumented driver can cause a cmdBuffer corruption under high load of Barreir transitions. #RB #Jira UE-89031
    #ROBOMERGE-SOURCE: CL 11845949 in //UE4/Release-4.25/... via CL 11845959
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Main) (v656-11643781)
    
    [CL 11845983 by arne schober in Main branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12DirectCommandListManager.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12DirectCommandListManager.cpp
@@ -769,7 +769,25 @@ uint32 FD3D12CommandListManager::GetResourceBarrierCommandList(FD3D12CommandList
 			LogResourceBarriers(BarrierDescs.Num(), BarrierDescs.GetData(), hResourceBarrierList.CommandList());
 #endif
 
-			hResourceBarrierList->ResourceBarrier(BarrierDescs.Num(), BarrierDescs.GetData());
+#if USE_PIX && PLATFORM_XBOXONE 
+			//there was a bug in the instrumented driver that corrupts the cmdBuffer if more than 2000 Barrieres are submitted at once
+			if (BarrierDescs.Num() > 1900)
+			{
+				int Num = BarrierDescs.Num();
+				D3D12_RESOURCE_BARRIER* Ptr = BarrierDescs.GetData();
+				while (Num > 0)
+				{
+					int DispatchNum = FMath::Min(Num, 1900);
+					hResourceBarrierList->ResourceBarrier(DispatchNum, Ptr);
+					Ptr += 1900;
+					Num -= 1900;
+				}
+			}
+			else
+#endif
+			{
+				hResourceBarrierList->ResourceBarrier(BarrierDescs.Num(), BarrierDescs.GetData());
+			}
 		}
 
 		return BarrierDescs.Num();
