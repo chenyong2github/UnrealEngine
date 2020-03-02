919a0afddf0022d9cc35b8b0e3d188f6b53bf1ae
Per Larsson <Per.Larsson@epicgames.com>
2020-03-02 07:32:25 -0500

    Added UAT project parameter validation when using -iostore without -pak. #jira UE-87771 #rb trivial
    [CL 11821697 by Per Larsson in 4.25 branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/ProjectParams.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/ProjectParams.cs
@@ -2738,6 +2738,11 @@ namespace AutomationTool
 			{
 				throw new AutomationException("DiscVersion is required for generating remaster package.");
 			}*/
+
+			if ((IoStore && !SkipIoStore) && (!Pak || SkipPak))
+			{
+				throw new AutomationException("-iostore can only be used with -pak");
+			}
 		}
 
 		protected bool bLogged = false;
