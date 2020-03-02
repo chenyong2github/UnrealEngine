09aeacf21a19d302824f0264766cce7f0e3c9b3e
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2020-03-02 07:14:18 -0500

    Fix crash when undoing curve edits
    Switched curve viewer to use 'named curve' model the same as the curve editor. This is needed because the tree view doesnt refresh straight away, so tracks dont get removed until the tick after an undo is fired, while the undo event gets pushed to the track even when it isnt hooked up to the tree. Using the named curve approach means that the invalid track doesnt end up accessing a deleted curve.
    
    #jira UE-88277 - Crash in Editor After Editing an Animation in the Curve Editor and Rapidly Pressing CTRL + Z
    #rb Jurre.deBaare
    
    #ROBOMERGE-SOURCE: CL 11820722 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11820774 by thomas sarkanen in 4.25-Plus branch]

--- Engine/Source/Editor/Persona/Private/SAnimSequenceCurveEditor.h
+++ Engine/Source/Editor/Persona/Private/SAnimSequenceCurveEditor.h
@@ -5,6 +5,10 @@
 #include "IAnimSequenceCurveEditor.h"
 #include "Widgets/DeclarativeSyntaxSupport.h"
 #include "EditorUndoClient.h"
+#include "RichCurveEditorModel.h"
+#include "Animation/SmartName.h"
+#include "Animation/AnimCurveTypes.h"
+#include "CurveEditorTypes.h"
 
 class FCurveEditor;
 class UAnimSequenceBase;
@@ -13,6 +17,32 @@ class SCurveEditorTree;
 class IPersonaPreviewScene;
 class SCurveEditorPanel;
 class FTabManager;
+
+// Model that references a named curve, rather than a raw pointer, so we avoid issues with 
+// reallocation of the curves arrays under the UI
+class FRichCurveEditorModelNamed : public FRichCurveEditorModel
+{
+public:
+	FRichCurveEditorModelNamed(const FSmartName& InName, ERawCurveTrackTypes InType, int32 InCurveIndex, UAnimSequenceBase* InAnimSequence, FCurveEditorTreeItemID InTreeId = FCurveEditorTreeItemID())
+		: FRichCurveEditorModel(InAnimSequence)
+		, Name(InName)
+		, AnimSequence(InAnimSequence)
+		, CurveIndex(InCurveIndex)
+		, Type(InType)
+		, TreeId(InTreeId)
+	{
+	}
+
+	virtual bool IsValid() const override;
+	virtual FRichCurve& GetRichCurve() override;
+	virtual const FRichCurve& GetReadOnlyRichCurve() const override;
+
+	FSmartName Name;
+	TWeakObjectPtr<UAnimSequenceBase> AnimSequence;
+	int32 CurveIndex;
+	ERawCurveTrackTypes Type;
+	FCurveEditorTreeItemID TreeId;
+};
 
 class SAnimSequenceCurveEditor : public IAnimSequenceCurveEditor, public FEditorUndoClient
 {
