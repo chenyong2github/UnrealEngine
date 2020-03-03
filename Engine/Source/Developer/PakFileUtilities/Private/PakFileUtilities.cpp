11e37e7d882dd866546d76ae20fc75201a9ffd5b
Graeme Thornton <Graeme.Thornton@epicgames.com>
2020-03-03 17:00:12 -0500

    UnrealPak: Fix patch generation crash when the crypto.json file for the source data isn't explicitly specified. In that case, just use the target crypto keychain
    #jira UE-89835
    #rb none
    
    [CL 11888409 by Graeme Thornton in Dev-Core branch]

--- Engine/Source/Developer/PakFileUtilities/Private/PakFileUtilities.cpp
+++ Engine/Source/Developer/PakFileUtilities/Private/PakFileUtilities.cpp
@@ -5153,6 +5153,10 @@ bool ExecuteUnrealPak(const TCHAR* CmdLine)
 				LoadKeyChainFromFile(PatchReferenceCryptoKeysFilename, PatchKeyChain);
 				ApplyEncryptionKeys(PatchKeyChain);
 			}
+			else
+			{
+				PatchKeyChain = KeyChain;
+			}
 
 			UE_LOG(LogPakFile, Display, TEXT("Generating patch from %s."), *CmdLineParameters.SourcePatchPakFilename, true );
 
