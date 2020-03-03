ba219e7f8ce479d8e173a4d5b7858b2228240f12
jaco vandyk <Jaco.VanDyk@epicgames.com>
2020-03-03 16:29:11 -0500

    Chaos Collision: Box vs Convex excessive jittering workaround -> don't use iterative manifolds
    #rb none
    [FYI] Michael.Lentine, Chris.Caulfield
    #jira
    
    
    #ROBOMERGE-SOURCE: CL 11869011 via CL 11873765 via CL 11874028
    #ROBOMERGE-BOT: (v656-11643781)
    
    [CL 11886553 by jaco vandyk in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/CollisionResolution.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/CollisionResolution.cpp
@@ -1605,7 +1605,14 @@ namespace Chaos
 		{
 			TRigidTransform<T, d> ParticleImplicit0TM = Transform0.GetRelativeTransform(Collisions::GetTransform(Particle0));
 			TRigidTransform<T, d> ParticleImplicit1TM = Transform1.GetRelativeTransform(Collisions::GetTransform(Particle1));
-			if (Chaos_Collision_UseManifolds)
+
+
+			EImplicitObjectType Implicit0Type = Particle0->Geometry()->GetType();
+			EImplicitObjectType Implicit1Type = Particle1->Geometry()->GetType();
+
+			// Note: This TBox check is a temporary workaround to avoid jitter in cases of Box vs Convex; investigation ongoing
+			// We need to improve iterative manifolds for this case
+			if (Chaos_Collision_UseManifolds && Implicit0Type != TBox<FReal, 3>::StaticType() && Implicit1Type != TBox<FReal, 3>::StaticType())
 			{
 				FRigidBodyMultiPointContactConstraint Constraint = FRigidBodyMultiPointContactConstraint(Particle0, Implicit0, ParticleImplicit0TM, Particle1, Implicit1, ParticleImplicit1TM, EContactShapesType::ConvexConvex);
 				UpdateConvexConvexManifold(Constraint, Transform0, Transform1, CullDistance);
