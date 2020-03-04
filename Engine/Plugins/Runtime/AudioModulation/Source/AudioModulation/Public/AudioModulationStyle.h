e22a36f8f6fbc6bae75d3e0fe75e18ad33953f40
rob gay <Rob.Gay@epicgames.com>
2020-03-04 15:43:39 -0500

    Fix bad move/add merge #rb none #jira none #rnx
    #ROBOMERGE-SOURCE: CL 11914493 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11914519 by rob gay in 4.25-Plus branch]

--- Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Public/AudioModulationStyle.h
+++ Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Public/AudioModulationStyle.h
@@ -2,15 +2,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
 #include "Math/Color.h"
 
+#include "AudioModulationStyle.generated.h"
 
-class FAudioModulationStyle
+UCLASS()
+class AUDIOMODULATION_API UAudioModulationStyle : public UBlueprintFunctionLibrary
 {
 public:
-	static const FColor GetVolumeBusColor()  { return FColor(33, 183, 0); }
-	static const FColor GetPitchBusColor()   { return FColor(181, 21, 0); }
-	static const FColor GetLPFBusColor()     { return FColor(0, 156, 183); }
-	static const FColor GetHPFBusColor()     { return FColor(94, 237, 183); }
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|Modulation|Style")
+	static const FColor GetVolumeBusColor() { return FColor(33, 183, 0); }
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|Modulation|Style")
+	static const FColor GetPitchBusColor() { return FColor(181, 21, 0); }
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|Modulation|Style")
+	static const FColor GetLPFBusColor() { return FColor(0, 156, 183); }
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|Modulation|Style")
+	static const FColor GetHPFBusColor() { return FColor(94, 237, 183); }
+
+	UFUNCTION(BlueprintCallable, Category = "Audio|Modulation|Style")
 	static const FColor GetControlBusColor() { return FColor(215, 180, 210); }
 };
