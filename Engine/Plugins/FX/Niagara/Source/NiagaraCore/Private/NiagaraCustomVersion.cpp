07624a13ba2f58cec87d0500442888dcb4e2cef7
shaun kime <Shaun.Kime@epicgames.com>
2020-03-03 23:31:08 -0500

    Allowing rejection samping #jira UE-76161, UE-85665 #rb none [FYI] simon.tovey, rob.krajcarski, frank.fella, stu.mckenna
    #ROBOMERGE-SOURCE: CL 11894281 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 11894283 by shaun kime in 4.25-Plus branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE599EF89, 0x9D574012, 0x99E29018, 0x4682F9EC);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0C1CF412, 0xA3B240BB, 0x900EBA38, 0x0CE36DDA );
