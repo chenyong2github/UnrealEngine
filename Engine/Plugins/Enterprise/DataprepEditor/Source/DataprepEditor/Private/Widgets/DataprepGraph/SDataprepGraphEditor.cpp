7b34bc88f34cb179c4e3231228da287fa0281379
JeanLuc Corenthin <JeanLuc.Corenthin@epicgames.com>
2020-03-04 11:08:26 -0500

    Fixed annoying movement when entering and leaving the graph panel on a drag and drop
    #jira UE-86875
    #rb none
    
    [CL 11896573 by JeanLuc Corenthin in 4.25 branch]

--- Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepEditor/Private/Widgets/DataprepGraph/SDataprepGraphEditor.cpp
+++ Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepEditor/Private/Widgets/DataprepGraph/SDataprepGraphEditor.cpp
@@ -349,7 +349,7 @@ void SDataprepGraphEditor::OnDragEnter(const FGeometry & MyGeometry, const FDrag
 	if (TrackGraphNodePtr.IsValid() && DragNodeOp.IsValid())
 	{
 		// Inform the Drag and Drop operation that we are hovering over this node.
-		DragNodeOp->SetGraphPanel(TrackGraphNodePtr.Pin()->GetOwnerPanel());
+		DragNodeOp->SetTrackNode(TrackGraphNodePtr.Pin());
 	}
 
 	SGraphEditor::OnDragEnter(MyGeometry, DragDropEvent);
@@ -371,8 +371,8 @@ void SDataprepGraphEditor::OnDragLeave(const FDragDropEvent & DragDropEvent)
 	TSharedPtr<FDataprepDragDropOp> DragNodeOp = DragDropEvent.GetOperationAs<FDataprepDragDropOp>();
 	if (TrackGraphNodePtr.IsValid() && DragNodeOp.IsValid())
 	{
-		// Inform the Drag and Drop operation that we are hovering over this node.
-		DragNodeOp->SetGraphPanel(TSharedPtr<SGraphPanel>());
+		// Inform the Drag and Drop operation that we are not hovering over this node anymore.
+		DragNodeOp->SetTrackNode(TSharedPtr<SDataprepGraphTrackNode>());
 	}
 
 	SGraphEditor::OnDragLeave(DragDropEvent);
@@ -383,9 +383,6 @@ FReply SDataprepGraphEditor::OnDrop(const FGeometry& MyGeometry, const FDragDrop
 	TSharedPtr<FDataprepDragDropOp> DragNodeOp = DragDropEvent.GetOperationAs<FDataprepDragDropOp>();
 	if (TrackGraphNodePtr.IsValid() && DragNodeOp.IsValid())
 	{
-		//const FVector2D NodeAddPosition = (MyGeometry.AbsoluteToLocal( DragDropEvent.GetScreenSpacePosition() ) / LastZoomAmount) + LastLocation;
-		//return DragNodeOp->DroppedOnPanel(SharedThis(this), DragDropEvent.GetScreenSpacePosition(), NodeAddPosition, *GetCurrentGraph()).EndDragDrop();
-
 		return FReply::Handled().EndDragDrop();
 	}
 
