6b2ca49ec92498fab9eff2763a93715008aa783d
ludovic chabant <Ludovic.Chabant@epicgames.com>
2020-03-05 13:46:17 -0500

    Sequencer: improvements to camera shake source tracks.
    Add a second track so that there are 2 ways to start and preview camera shakes: a "controlled" track where sections control when the shake starts and stops, and a "trigger" track which is a "fire-and-forget" way to trigger shakes.
    Moved all the shake previewing into a separate class so that both tracks can use it.
    
    Controlled shakes track improvements:
    - Handle blending camera shakes out.
    - Show a marker for the end of the shake if it has a duration, for when the section is longer than that.
    - Show shake intensity curve if the shake has blend in/out times.
    - Give new sections an appropriate length based on their shake's duration.
    
    #rb max.chen
    #jira UE-89420
    #jira UE-89421
    
    
    #ROBOMERGE-SOURCE: CL 11938191 via CL 11943265 via CL 11943542 via CL 11943870
    #ROBOMERGE-BOT: (v656-11643781)
    
    [CL 11946435 by ludovic chabant in Main branch]

---
+++ Engine/Source/Runtime/MovieSceneTracks/Public/Tracks/MovieSceneCameraShakeSourceTriggerTrack.h
@@ -0,0 +1,42 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "MovieSceneTrack.h"
+#include "MovieSceneCameraShakeSourceTriggerTrack.generated.h"
+
+UCLASS()
+class MOVIESCENETRACKS_API UMovieSceneCameraShakeSourceTriggerTrack
+	: public UMovieSceneTrack
+{
+public:
+
+	GENERATED_BODY()
+
+	UMovieSceneCameraShakeSourceTriggerTrack(const FObjectInitializer& Obj);
+
+	// UMovieSceneTrack interface
+	virtual bool SupportsType(TSubclassOf<UMovieSceneSection> SectionClass) const override;
+	virtual UMovieSceneSection* CreateNewSection() override;
+	virtual bool HasSection(const UMovieSceneSection& Section) const override;
+	virtual void AddSection(UMovieSceneSection& Section) override;
+	virtual void RemoveSection(UMovieSceneSection& Section) override;
+	virtual void RemoveSectionAt(int32 SectionIndex) override;
+	virtual void RemoveAllAnimationData() override;
+	virtual bool IsEmpty() const override;
+	virtual const TArray<UMovieSceneSection*>& GetAllSections() const override;
+	virtual void PostCompile(FMovieSceneEvaluationTrack& Track, const FMovieSceneTrackCompilerArgs& Args) const override;
+
+#if WITH_EDITORONLY_DATA
+	virtual FText GetDisplayName() const override;
+#endif
+
+protected:
+
+	/** All the sections in this track */
+	UPROPERTY()
+	TArray<UMovieSceneSection*> Sections;
+};
+
