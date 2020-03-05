3481fc1d07a3ace221be0b94c9a441a21a025b07
michael sartain <Michael.Sartain@epicgames.com>
2020-03-05 12:25:03 -0500

    Fix Linux build break (SEnumComboBox.h -> SEnumCombobox.h)
    #jira none
    #rb none
    [FYI] Brandon.Schaefer
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: michael.sartain
    #ROBOMERGE-SOURCE: CL 11585021 in //UE4/Release-4.25/... via CL 11585062 via CL 11586147
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v656-11643781)
    
    [CL 11939378 by michael sartain in Dev-Networking branch]

--- Engine/Source/Editor/EditorWidgets/Private/SEnumComboBox.cpp
+++ Engine/Source/Editor/EditorWidgets/Private/SEnumComboBox.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "SEnumComboBox.h"
+#include "SEnumCombobox.h"
 #include "EditorStyleSet.h"
 #include "Types/SlateEnums.h"
 #include "Widgets/SToolTip.h"
