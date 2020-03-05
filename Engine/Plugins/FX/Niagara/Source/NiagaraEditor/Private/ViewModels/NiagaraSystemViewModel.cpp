01d151901e55ebeb7ac3b8b8e889f00dea3480cc
frank fella <Frank.Fella@epicgames.com>
2020-03-05 14:46:20 -0500

    Merging 11640024 from main - Niagara - Fix additional determinism issues related to looping in the editor and looping in the level sequencer.
    #jira UE-89603
    #rb none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: frank.fella
    #ROBOMERGE-SOURCE: CL 11646495 in //UE4/Release-4.25/... via CL 11646496 via CL 11646497
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v656-11643781)
    
    [CL 11953502 by frank fella in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/ViewModels/NiagaraSystemViewModel.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/ViewModels/NiagaraSystemViewModel.cpp
@@ -1689,7 +1689,7 @@ void FNiagaraSystemViewModel::SequencerTimeChanged()
 				// We don't want to reset the current time if we're scrubbing.
 				if (CurrentStatus == EMovieScenePlayerStatus::Playing)
 				{
-					ResetSystem(ETimeResetMode::AllowResetTime, EMultiResetMode::ResetThisInstance, EReinitMode::ResetSystem);
+					ResetSystem(ETimeResetMode::AllowResetTime, EMultiResetMode::ResetThisInstance, EReinitMode::ReinitializeSystem);
 				}
 				else
 				{
