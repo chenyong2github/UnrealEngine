cc30454b1a5af1f7d4a05f02c452f93dd70745c5
jian ru <Jian.Ru@epicgames.com>
2020-03-05 20:50:41 -0500

    Make sure thread-safe access to cvars in new shadow code
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: jian.ru
    #ROBOMERGE-SOURCE: CL 11638886 via CL 11638938 via CL 11638970 via CL 11832894
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v656-11643781)
    
    [CL 11972339 by jian ru in Dev-Networking branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderCore.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderCore.cpp
@@ -267,6 +267,19 @@ RENDERCORE_API int32 GetCVarForceLOD()
 	return Ret;
 }
 
+RENDERCORE_API int32 GetCVarForceLOD_AnyThread()
+{
+	int32 Ret = -1;
+
+#if EXPOSE_FORCE_LOD
+	{
+		Ret = CVarForceLOD.GetValueOnAnyThread();
+	}
+#endif // EXPOSE_FORCE_LOD
+
+	return Ret;
+}
+
 RENDERCORE_API int32 GetCVarForceLODShadow()
 {
 	int32 Ret = -1;
@@ -274,6 +287,19 @@ RENDERCORE_API int32 GetCVarForceLODShadow()
 #if EXPOSE_FORCE_LOD
 	{
 		Ret = CVarForceLODShadow.GetValueOnRenderThread();
+	}
+#endif // EXPOSE_FORCE_LOD
+
+	return Ret;
+}
+
+RENDERCORE_API int32 GetCVarForceLODShadow_AnyThread()
+{
+	int32 Ret = -1;
+
+#if EXPOSE_FORCE_LOD
+	{
+		Ret = CVarForceLODShadow.GetValueOnAnyThread();
 	}
 #endif // EXPOSE_FORCE_LOD
 
