cddaad1e451fcbe836c823116a97cec261be0f4d
kevin ortegren <Kevin.Ortegren@epicgames.com>
2020-03-05 22:36:43 -0500

    Added MID creation flags for the BP create function. The only flags right now i None and Transient. It's implemented as a bitflag mask so that multiple flags can be set and manipulated throught BP. This iniitial implementation is to give users the option to create MIDs as transient objects, never to be exported.
    [FYI] Ben.Ingram, Steve.Robb
    #rb fyi
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: kevin.ortegren
    #ROBOMERGE-SOURCE: CL 11816743 via CL 11816753 via CL 11816785 via CL 11878062
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v656-11643781)
    
    [CL 11992400 by kevin ortegren in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Classes/Kismet/KismetMaterialLibrary.h
+++ Engine/Source/Runtime/Engine/Classes/Kismet/KismetMaterialLibrary.h
@@ -3,16 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Misc/EnumClassFlags.h"
 #include "UObject/ObjectMacros.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "KismetMaterialLibrary.generated.h"
 
 class UMaterialParameterCollection;
 
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class EMIDCreationFlags : uint8
+{
+	None					UMETA(DisplayName = "None"),
+	Transient = 1 << 0		UMETA(DisplayName = "Transient")
+};
+ENUM_CLASS_FLAGS(EMIDCreationFlags)
+
 UCLASS(MinimalAPI, meta=(ScriptName="MaterialLibrary"))
 class UKismetMaterialLibrary : public UBlueprintFunctionLibrary
 {
-	GENERATED_UCLASS_BODY()
+	GENERATED_BODY()
+
+public:
 
 	/** Sets a scalar parameter value on the material collection instance. Logs if ParameterName is invalid. */
 	UFUNCTION(BlueprintCallable, Category="Rendering|Material", meta=(Keywords="SetFloatParameterValue", WorldContext="WorldContextObject", MaterialParameterCollectionFunction = "true"))
@@ -32,5 +43,5 @@ class UKismetMaterialLibrary : public UBlueprintFunctionLibrary
 
 	/** Creates a Dynamic Material Instance which you can modify during gameplay. */
 	UFUNCTION(BlueprintCallable, Category="Rendering|Material", meta=(WorldContext="WorldContextObject", MaterialParameterCollectionFunction = "true"))
-	static ENGINE_API class UMaterialInstanceDynamic* CreateDynamicMaterialInstance(UObject* WorldContextObject, class UMaterialInterface* Parent, FName OptionalName = NAME_None);
+	static ENGINE_API class UMaterialInstanceDynamic* CreateDynamicMaterialInstance(UObject* WorldContextObject, class UMaterialInterface* Parent, FName OptionalName = NAME_None, EMIDCreationFlags CreationFlags = EMIDCreationFlags::None);
 };
