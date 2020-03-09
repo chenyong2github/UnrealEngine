f2753dde7a97fd0689634b361c7faf72cba146f7
uriel doyon <uriel.doyon@xa.epicgames.com>
2020-03-08 20:51:22 -0400

    Fixed Niagara static mesh color sampling when asset doesn't have vertex colors. #jira UE-89906 #rb none
    #ROBOMERGE-SOURCE: CL 12056649 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v656-11643781)
    
    [CL 12056650 by uriel doyon in 4.25-Plus branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceStaticMesh.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceStaticMesh.h
@@ -229,6 +229,7 @@ public:
 	static const FString InstanceWorldVelocityName;
 	static const FString AreaWeightedSamplingName;
 	static const FString NumTexCoordName;
+	static const FString UseColorBufferName;
 
 public:
 	void GetNumTriangles(FVectorVMContext& Context);
