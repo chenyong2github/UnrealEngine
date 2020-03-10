3b81e777215a86b0573a5046f083f50d2c134184
Jeff Fisher <Jeff.Fisher@epicgames.com>
2020-03-10 15:03:58 -0400

    UE-89446 Remove Leap Motion plugin from engine because it is no longer supported -Deleting all LeapMotion files.  Removing reference to sdk from InstalledEngineFilters. #jira UE-89446 #rb build
    [CL 12114664 by Jeff Fisher in 4.25 branch]

--- Engine/Plugins/Runtime/LeapMotion/Source/LeapMotionEditor/Private/LeapMotionEditorModule.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LeapMotionEditorModule.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FLeapMotionEditorModule, LeapMotionEditor);
