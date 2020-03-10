f75073361b2eece7f875257c4584b610f44e48c9
james hopkin <James.Hopkin@epicgames.com>
2020-03-10 17:28:07 -0400

    Added Algo::Compare, to compare contiguous ranges using operator== on each element [at]Steve.Robb
    #ROBOMERGE-SOURCE: CL 12104601 via CL 12104633 via CL 12104642 via CL 12116510
    #ROBOMERGE-BOT: (v657-12064184)
    
    [CL 12119035 by james hopkin in Main branch]

--- Engine/Source/Runtime/Core/Public/Algo/Compare.h
+++ Engine/Source/Runtime/Core/Public/Algo/Compare.h
@@ -8,6 +8,39 @@
 namespace Algo
 {
 	/**
+	* Compares two contiguous containers using operator== to compare pairs of elements.
+	*
+	* @param  InputA     Container of elements that are used as the first argument to operator==.
+	* @param  InputB     Container of elements that are used as the second argument to operator==.
+	*
+	* @return Whether the containers are the same size and operator== returned true for every pair of elements.
+	*/
+	template <typename InAT, typename InBT>
+	constexpr bool Compare(InAT&& InputA, InBT&& InputB)
+	{
+		const SIZE_T SizeA = GetNum(InputA);
+		const SIZE_T SizeB = GetNum(InputB);
+
+		if (SizeA != SizeB)
+		{
+			return false;
+		}
+
+		auto* A = GetData(InputA);
+		auto* B = GetData(InputB);
+
+		for (SIZE_T Count = SizeA; Count; --Count)
+		{
+			if (!(*A++ == *B++))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	* Compares two contiguous containers using a predicate to compare pairs of elements.
 	*
 	* @param  InputA     Container of elements that are used as the first argument to the predicate.
