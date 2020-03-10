ccb3e58bf2b3a7e81b32ef938306c1b19af4b0aa
Frank Fella <Frank.Fella@epicgames.com>
2020-03-09 22:18:58 -0400

    Merging 11928357 from main -  Niagara - First round of UI perf improvements.
    #jira UE-80769
    #rb shaun.kime
    
    [CL 12098391 by Frank Fella in 4.25 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/NiagaraSystemViewModel.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/NiagaraSystemViewModel.h
@@ -316,6 +316,10 @@ private:
 	/** Sends message jobs to FNiagaraMessageManager for all compile events from the last compile. */
 	void SendLastCompileMessageJobs() const;
 
+	void InvalidateCachedCompileStatus();
+
+	void TickCompileStatus();
+
 	/** Sets up the preview component and System instance. */
 	void SetupPreviewComponentAndInstance();
 
@@ -571,4 +575,9 @@ private:
 	UNiagaraSystemSelectionViewModel* SelectionViewModel;
 
 	UNiagaraScratchPadViewModel* ScriptScratchPadViewModel;
+
+	TArray<UNiagaraScript*> ScriptsToCheckForStatus;
+	TArray<ENiagaraScriptCompileStatus> ScriptCompileStatuses;
+
+	TOptional<ENiagaraScriptCompileStatus> LatestCompileStatusCache;
 };
