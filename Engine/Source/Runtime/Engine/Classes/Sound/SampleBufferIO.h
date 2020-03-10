90a2f56bb7cc04b49b84284c0c16d2cefdc40465
maxwell hayes <Maxwell.Hayes@epicgames.com>
2020-03-10 09:08:11 -0400

    Adding editor time functionality to FSoundWavePCMLoader and minor bug fixe for IR asset import (Convolution Reverb)
    #jira none
    #rb Ethan.Geller
    [FYI] Ethan.Geller
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: maxwell.hayes
    #ROBOMERGE-SOURCE: CL 12049473 in //UE4/Release-4.25/... via CL 12049474 via CL 12076155
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v657-12064184)
    
    [CL 12102827 by maxwell hayes in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Classes/Sound/SampleBufferIO.h
+++ Engine/Source/Runtime/Engine/Classes/Sound/SampleBufferIO.h
@@ -28,8 +28,8 @@ namespace Audio
 	public:
 		FSoundWavePCMLoader();
 
-		// Loads a USoundWave, call on game thread.
-		void LoadSoundWave(USoundWave* InSoundWave, TFunction<void(const USoundWave* SoundWave, const Audio::FSampleBuffer& OutSampleBuffer)> OnLoaded);
+		// Loads a USoundWave, call on game thread. Unless called with bSynchnous set to true, this class will require Update() to be called on the game thread.
+		void LoadSoundWave(USoundWave* InSoundWave, TFunction<void(const USoundWave* SoundWave, const Audio::FSampleBuffer& OutSampleBuffer)> OnLoaded, bool bSynchrounous = false);
 
 
 		// Update the loading state. 
