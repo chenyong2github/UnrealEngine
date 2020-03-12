35b37772b2ffa7286cc55cc7cf03bfd99923f75f
wei liu <Wei.Liu@epicgames.com>
2020-03-12 06:50:06 -0400

    Fix a crash of Histogram CS on IOS A8 devices.
    Fix some legacy tooltips for eyeadaptation.
    
    Change the disable eyeadaptation condition for low-end ios devices to be consistent with android devices using r.EyeAdaptationQuality instead of r.Mobile.EyeAdaptation.
    
    #jira none
    
    #rb Dmitriy.Dyomin, Carl.Lloyd, Jack.Porter, Mi.Wang
    
    #ROBOMERGE-SOURCE: CL 12147778 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v659-12123632)
    
    [CL 12147779 by wei liu in 4.25-Plus branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/RendererSettings.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/RendererSettings.h
@@ -130,9 +130,9 @@ namespace EAutoExposureMethodUI
 {
 	enum Type
 	{
-		/** Not supported on mobile, requires compute shader to construct 64 bin histogram */
+		/** requires compute shader to construct 64 bin histogram */
 		AEM_Histogram  UMETA(DisplayName = "Auto Exposure Histogram"),
-		/** Not supported on mobile, faster method that computes single value by downsampling */
+		/** faster method that computes single value by downsampling */
 		AEM_Basic      UMETA(DisplayName = "Auto Exposure Basic"),
 		/** Uses camera settings. */
 		AEM_Manual   UMETA(DisplayName = "Manual"),
