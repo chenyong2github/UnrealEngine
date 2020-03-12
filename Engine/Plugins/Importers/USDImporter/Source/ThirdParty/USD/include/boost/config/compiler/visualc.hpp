c7b014909b294d455177ff44ce2de00453b2b602
jeanmichel dignard <JeanMichel.Dignard@epicgames.com>
2020-03-12 13:44:43 -0400

    USD boost: removed pragma message about vc++ unknown version.
    #jira none
    #rb none
    #rnx
    
    #ROBOMERGE-SOURCE: CL 12149997 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v659-12123632)
    
    [CL 12150003 by jeanmichel dignard in 4.25-Plus branch]

--- Engine/Plugins/Importers/USDImporter/Source/ThirdParty/USD/include/boost/config/compiler/visualc.hpp
+++ Engine/Plugins/Importers/USDImporter/Source/ThirdParty/USD/include/boost/config/compiler/visualc.hpp
@@ -330,7 +330,7 @@
 #if (_MSC_VER > 1911)
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
-#  else
-#     pragma message("Unknown compiler version - please run the configure tests and report the results")
+//#  else
+//#     pragma message("Unknown compiler version - please run the configure tests and report the results")
 #  endif
 #endif
