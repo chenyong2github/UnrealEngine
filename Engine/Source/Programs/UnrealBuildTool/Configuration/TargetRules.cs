8b55cb40459b895614b7ff3efa33b61d67d0eb35
ben marsh <Ben.Marsh@epicgames.com>
2020-03-16 16:09:55 -0400

    Make sure UBT and HoloLens platforms agree on the architecture being compiled. The architecture setting is now driven by the target being built, and cannot be overridden later. The HoloLens platform returns the default architecture value to UBT, ensuring that intermediate folders and the like are named consistently between builds where an architecture isn't specified (and it uses the default), and and builds where an architecture is explicitly specified.
    #rb Steve.Smith
    #rnx
    #jira
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 12167048 in //UE4/Release-4.25/... via CL 12167060 via CL 12167071
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v662-12191386)
    
    [CL 12222070 by ben marsh in Dev-Networking branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/TargetRules.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/TargetRules.cs
@@ -1467,7 +1467,7 @@ namespace UnrealBuildTool
 		/// HoloLens-specific target settings.
 		/// </summary>
 		[ConfigSubObject]
-		public HoloLensTargetRules HoloLensPlatform = new HoloLensTargetRules();
+		public HoloLensTargetRules HoloLensPlatform;
 
 		/// <summary>
 		/// Constructor.
@@ -1482,6 +1482,7 @@ namespace UnrealBuildTool
 			this.ProjectFile = Target.ProjectFile;
 			this.Version = Target.Version;
 			this.WindowsPlatform = new WindowsTargetRules(this);
+			this.HoloLensPlatform = new HoloLensTargetRules(Target);
 
 			// Read settings from config files
 			foreach (object ConfigurableObject in GetConfigurableObjects())
