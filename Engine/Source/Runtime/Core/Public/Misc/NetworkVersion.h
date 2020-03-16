e2f3a21f8aff8eeddbe5cbf5b13d8b40def34ac6
brian bekich <Brian.Bekich@epicgames.com>
2020-03-16 16:12:42 -0400

    Change backwards compatibile class net cache export serialization to include asset path so they will be properly unique Bump network version for this change
    #rb jon.nabozny, ryan.gerleve
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: brian.bekich
    #ROBOMERGE-SOURCE: CL 12167806 via CL 12167819 via CL 12167868 via CL 12167870
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v662-12191386)
    
    [CL 12222301 by brian bekich in Dev-Networking branch]

--- Engine/Source/Runtime/Core/Public/Misc/NetworkVersion.h
+++ Engine/Source/Runtime/Core/Public/Misc/NetworkVersion.h
@@ -46,6 +46,7 @@ enum EEngineNetworkVersionHistory
 	HISTORY_FIX_ENUM_SERIALIZATION = 12,			// Bump version to fix enum net serialization issues.
 	HISTORY_OPTIONALLY_QUANTIZE_SPAWN_INFO = 13,	// Bump version to conditionally disable quantization for Scale, Location, and Velocity when spawning network actors.
 	HISTORY_JITTER_IN_HEADER = 14,					// Bump version since we added jitter clock time to packet headers and removed remote saturation
+	HISTORY_CLASSNETCACHE_FULLNAME = 15,			// Bump version to use full paths in GetNetFieldExportGroupForClassNetCache
 	// New history items go above here.
 
 	HISTORY_ENGINENETVERSION_PLUS_ONE,
