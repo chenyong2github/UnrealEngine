08bd5ee697ea7e7a640d2e71c745d1461fe2544d
rex hill <Rex.Hill@epicgames.com>
2020-03-16 13:49:16 -0400

    Fix compile error #rnx #jira [FYI] Jian.Ru
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rex.hill
    #ROBOMERGE-SOURCE: CL 12139561 via CL 12139573 via CL 12139623 via CL 12139629
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v662-12191386)
    
    [CL 12212110 by rex hill in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/Streaming/LandscapeMeshMobileUpdate.h
+++ Engine/Source/Runtime/Engine/Private/Streaming/LandscapeMeshMobileUpdate.h
@@ -7,7 +7,7 @@ LandscapeMeshMobileUpdate.h: Helpers to stream in and out mobile landscape verte
 #pragma once
 
 #include "CoreMinimal.h"
-#include "RHIResources.h"
+#include "RHI.h"
 #include "RenderAssetUpdate.h"
 #include "Async/AsyncFileHandle.h"
 
