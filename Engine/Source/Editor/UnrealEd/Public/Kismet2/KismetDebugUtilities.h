b834f5e761c11b5d5c73f360cb3bf2718f5f87a8
ryan gerleve <ryan.gerleve@epicgames.com>
2020-03-16 11:25:01 -0400

    Add a check when debugging in blueprint for output params when traversing the stack frame to avoid a crash
    #rb fred.kimberley
    #jira UE-90008
    
    
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.hoffman
    #ROBOMERGE-SOURCE: CL 12113001 in //UE4/Release-4.25/... via CL 12113004 via CL 12113008
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v662-12191386)
    
    [CL 12202536 by ryan gerleve in Dev-Networking branch]

--- Engine/Source/Editor/UnrealEd/Public/Kismet2/KismetDebugUtilities.h
+++ Engine/Source/Editor/UnrealEd/Public/Kismet2/KismetDebugUtilities.h
@@ -249,9 +249,21 @@ protected:
 	static void GetDebugInfo_InContainer(int32 Index, FDebugInfo& DebugInfo, FProperty* Property, const void* Data);
 	static void GetDebugInfoInternal(FDebugInfo& DebugInfo, FProperty* Property, const void* PropertyValue);
 
-	// helper function for converting between blueprint and debuggable data
-	// output params are only valid if the return result is EWatchTextResult::EWTR_Valid
-	static EWatchTextResult FindDebuggingData(UBlueprint* Blueprint, UObject* ActiveObject, const UEdGraphPin* WatchPin, FProperty*& OutProperty, void*& OutData, void*& OutDelta, UObject*& OutParent, TArray<UObject*>& SeenObjects);
+	/**
+	* @brief	Helper function for converting between blueprint and debuggable data
+	*			output params are only valid if the return result is EWatchTextResult::EWTR_Valid
+	* 
+	* @param 	Blueprint		Active blueprint that is being debugged
+	* @param 	ActiveObject	Instance of the object that is being debugged
+	* @param 	WatchPin		The pin where this debug breakpoint is from
+	* @param 	OutProperty		Property of interest
+	* @param 	OutData			Populated with the property address of interest
+	* @param 	OutDelta		Populated with the same thing as OutData
+	* @param 	OutParent		Populated with the active object
+	* @param 	SeenObjects		Used to track what objects have been traversed to find the OutProperty address
+	* @return	EWTR_Valid if the debug data could be found, otherwise an appropriate error code
+	*/
+	static EWatchTextResult FindDebuggingData(UBlueprint* Blueprint, UObject* ActiveObject, const UEdGraphPin* WatchPin, FProperty*& OutProperty, void*& OutData, void*& OutDelta, UObject*& OutParent, TArray<UObject*>& SeenObjects, bool* OutbIsInDebuggingFrame = nullptr);
 
 private:
 	FKismetDebugUtilities() {}
