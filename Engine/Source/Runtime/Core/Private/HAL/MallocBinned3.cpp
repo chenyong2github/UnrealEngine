decf9b91d19dea3222807914e5278f3712274bb2
bob tellez <Bob.Tellez@epicgames.com>
2020-03-16 14:32:58 -0400

    #UE4 Made FlushThreadCacheMaxWaitTime usable from ini/commandline in normal builds. This isn't really a runtime tweak, just a way to control the threshold for a warning
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: bob.tellez
    #ROBOMERGE-SOURCE: CL 12146897 via CL 12149219 via CL 12149302
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v662-12191386)
    
    [CL 12215168 by bob tellez in Dev-Networking branch]

--- Engine/Source/Runtime/Core/Private/HAL/MallocBinned3.cpp
+++ Engine/Source/Runtime/Core/Private/HAL/MallocBinned3.cpp
@@ -66,14 +66,15 @@ static FAutoConsoleVariableRef GMallocBinned3AllocExtraCVar(
 	TEXT("When we do acquire the lock, how many blocks cached in TLS caches. In no case will we grab more than a page.")
 	);
 
+#endif
+
 double GMallocBinned3FlushThreadCacheMaxWaitTime = 0.02f;
 static FAutoConsoleVariableRef GMallocBinned3FlushThreadCacheMaxWaitTimeCVar(
 	TEXT("MallocBinned3.FlushThreadCacheMaxWaitTime"),
 	GMallocBinned3FlushThreadCacheMaxWaitTime,
-	TEXT("The threshold of time before warning about FlushCurrentThreadCache taking too long (seconds).")
+	TEXT("The threshold of time before warning about FlushCurrentThreadCache taking too long (seconds)."),
+	ECVF_ReadOnly
 );
-
-#endif
 
 #if BINNED3_ALLOCATOR_STATS
 int64 Binned3AllocatedSmallPoolMemory = 0; // memory that's requested to be allocated by the game
@@ -1315,16 +1316,11 @@ void FMallocBinned3::FlushCurrentThreadCache()
 	}
 
 	// These logs must happen outside the above mutex to avoid deadlocks
-#if BINNED3_ALLOW_RUNTIME_TWEAKING
-	double MaxWaitTimeThreshold = GMallocBinned3FlushThreadCacheMaxWaitTime;
-#else
-	double MaxWaitTimeThreshold = 0.02f;
-#endif
-	if (WaitForMutexTime > MaxWaitTimeThreshold)
+	if (WaitForMutexTime > GMallocBinned3FlushThreadCacheMaxWaitTime)
 	{
 		UE_LOG(LogMemory, Warning, TEXT("FMallocBinned3 took %6.2fms to wait for mutex for trim."), WaitForMutexTime * 1000.0f);
 	}
-	if (WaitForMutexAndTrimTime > MaxWaitTimeThreshold)
+	if (WaitForMutexAndTrimTime > GMallocBinned3FlushThreadCacheMaxWaitTime)
 	{
 		UE_LOG(LogMemory, Warning, TEXT("FMallocBinned3 took %6.2fms to wait for mutex AND trim."), WaitForMutexAndTrimTime * 1000.0f);
 	}
