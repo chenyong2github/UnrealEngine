5bd8eb82fe6a7be4830324c2b131e8b1cdcee457
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-03-17 13:07:11 -0400

    Diverge UE4Main and Release4.25's NiagaraCustomVersion to ensure Niagara effects properly rebuild #jira UE-90709 #rb none
    [CL 12233384 by rob krajcarski in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0A1C1D5F, 0xBE674CC0, 0x91115FF8, 0x07E209F3 );
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xC9457034, 0x12D44E17, 0xBB38C2BB, 0x9FF38441);
