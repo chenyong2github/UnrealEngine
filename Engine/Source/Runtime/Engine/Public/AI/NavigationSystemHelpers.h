715ffbccada23cd73ecd12bd429fdd4398c616be
maxime mercier <Maxime.Mercier@epicgames.com>
2020-03-18 09:15:58 -0400

    Fix chaos export height field slice version, used when doing lazy octree gather geometry
    [at]aris.theophanidis [at]yoan.stamant
    #rnx
    
    
    #ROBOMERGE-SOURCE: CL 12235737 via CL 12235740 via CL 12244074
    #ROBOMERGE-BOT: (v667-12241502)
    
    [CL 12244212 by maxime mercier in Main branch]

--- Engine/Source/Runtime/Engine/Public/AI/NavigationSystemHelpers.h
+++ Engine/Source/Runtime/Engine/Public/AI/NavigationSystemHelpers.h
@@ -43,13 +43,14 @@ struct FNavigableGeometryExport
 	virtual void ExportPxTriMesh32Bit(physx::PxTriangleMesh const * const TriMesh, const FTransform& LocalToWorld) = 0;
 	virtual void ExportPxConvexMesh(physx::PxConvexMesh const * const ConvexMesh, const FTransform& LocalToWorld) = 0;
 	virtual void ExportPxHeightField(physx::PxHeightField const * const HeightField, const FTransform& LocalToWorld) = 0;
+	virtual void ExportPxHeightFieldSlice(const FNavHeightfieldSamples& PrefetchedHeightfieldSamples, const int32 NumRows, const int32 NumCols, const FTransform& LocalToWorld, const FBox& SliceBox) = 0;
 #endif // WITH_PHYSX
 #if WITH_CHAOS
 	virtual void ExportChaosTriMesh(const Chaos::FTriangleMeshImplicitObject* const TriMesh, const FTransform& LocalToWorld) = 0;
 	virtual void ExportChaosConvexMesh(const FKConvexElem* const Convex, const FTransform& LocalToWorld) = 0;
 	virtual void ExportChaosHeightField(const Chaos::THeightField<float>* const Heightfield, const FTransform& LocalToWorld) = 0;
+	virtual void ExportChaosHeightFieldSlice(const FNavHeightfieldSamples& PrefetchedHeightfieldSamples, const int32 NumRows, const int32 NumCols, const FTransform& LocalToWorld, const FBox& SliceBox) = 0;
 #endif
-	virtual void ExportHeightFieldSlice(const FNavHeightfieldSamples& PrefetchedHeightfieldSamples, const int32 NumRows, const int32 NumCols, const FTransform& LocalToWorld, const FBox& SliceBox) = 0;
 	virtual void ExportRigidBodySetup(UBodySetup& BodySetup, const FTransform& LocalToWorld) = 0;
 	virtual void ExportCustomMesh(const FVector* VertexBuffer, int32 NumVerts, const int32* IndexBuffer, int32 NumIndices, const FTransform& LocalToWorld) = 0;
 	
