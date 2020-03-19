e0c164fb9141a19a3d5ebf401aa76e73bef4a6fa
tony wong <Tony.Wong@epicgames.com>
2020-03-19 00:59:12 -0400

    UE4: Compile fix. #JIRA: None #rnx #rb none
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: tony.wong
    #ROBOMERGE-SOURCE: CL 12253883 in //UE4/Main/...
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v668-12245121)
    
    [CL 12284950 by tony wong in Dev-Networking branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
@@ -1235,7 +1235,7 @@ namespace UnrealBuildTool
 				ExtractScript.AppendLine(String.Format("unzip -q -o {0} -d {1}", Utils.MakePathSafeToUseWithCommandLine(Framework.ZipFile.FullName), Utils.MakePathSafeToUseWithCommandLine(Framework.FrameworkDirectory.ParentDirectory.FullName))); // Zip contains folder with the same name, hence ParentDirectory
 				ExtractScript.AppendLine(String.Format("touch {0}", Utils.MakePathSafeToUseWithCommandLine(Framework.ExtractedTokenFile.AbsolutePath)));
 
-				FileItem ExtractScriptFileItem = FileItem.CreateIntermediateTextFile(new FileReference(Framework.OutputDirectory.FullName + ".sh"), ExtractScript.ToString());
+				FileItem ExtractScriptFileItem = Graph.CreateIntermediateTextFile(new FileReference(Framework.OutputDirectory.FullName + ".sh"), ExtractScript.ToString());
 
 				Action UnzipAction = Graph.CreateAction(ActionType.BuildProject);
 				UnzipAction.CommandPath = new FileReference("/bin/sh");
