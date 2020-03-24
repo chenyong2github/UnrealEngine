bb68dfaed640f7a206f8bacc281fe12a9e7788e7
Frank Fella <Frank.Fella@epicgames.com>
2020-03-24 17:03:13 -0400

    Niagara - Prevent a crash when fixing up deprecated modules.
    #jira UE-91059
    #rb none
    
    [CL 12393666 by Frank Fella in 4.25 branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraParameterStore.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraParameterStore.cpp
@@ -604,6 +604,13 @@ void FNiagaraParameterStore::RenameParameter(const FNiagaraVariable& Param, FNam
 	check(!ParameterOffsets.Num()); // Migration to SortedParameterOffsets
 #endif
 
+	if (Param.GetName() == NewName)
+	{
+		// Early out here to prevent crashes later on due to delta size mismatches when the newly named
+		// parameter isn't added.
+		return;
+	}
+
 	int32 Idx = IndexOf(Param);
 	if(Idx != INDEX_NONE)
 	{
