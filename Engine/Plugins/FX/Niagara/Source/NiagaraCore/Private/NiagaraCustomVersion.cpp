95cfbf37a527bfb384eb79406f9a07d075c35852
Stu McKenna <Stu.McKenna@epicgames.com>
2020-03-31 13:18:38 -0400

    - Bump version GUID to fix conflicts
    #rb none
    #jira UE-91230
    #rnx
    
    [CL 12498484 by Stu McKenna in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x62AE44D9, 0x32500143, 0x99B84C1B, 0x354729F9);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x9E046792, 0x5F59194A, 0x9CDF3576, 0xC5AAAF52);
