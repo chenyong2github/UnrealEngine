a9c2a30a2f2645470e9c956e76a5977754bdb913
robomerge <robomerge@epicgames.com>
2020-04-01 03:05:01 -0400

    #UE4 Fix problem where handles would incorrectly become children of other handles
    [FYI] Brian.Bekich
    
    #ROBOMERGE-SOURCE: CL 12504623 via CL 12504624 via CL 12504625
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v673-12478461)
    
    [CL 12504626 by robomerge in Main branch]

--- Engine/Source/Runtime/Engine/Private/AssetManager.cpp
+++ Engine/Source/Runtime/Engine/Private/AssetManager.cpp
@@ -1206,29 +1206,8 @@ TSharedPtr<FStreamableHandle> UAssetManager::ChangeBundleStateForPrimaryAssets(c
 					continue;
 				}
 
-				bool bIsSuperSet = false;
-				if (NewBundles->Num() > NameData->PendingState.BundleNames.Num())
-				{
-					bIsSuperSet = true;
-					for (int i = 0; i < NameData->PendingState.BundleNames.Num(); i++)
-					{
-						if (!NewBundles->Contains(NameData->PendingState.BundleNames[i]))
-						{
-							bIsSuperSet = false;
-							break;
-						}
-					}
-				}
-
-				if (bIsSuperSet)
-				{
-					ExistingHandles.Add(NameData->PendingState.Handle);
-				}
-				else
-				{
-					// Clear pending state
-					NameData->PendingState.Reset(true);
-				}
+				// Clear pending state
+				NameData->PendingState.Reset(true);
 			}
 			else if (NameData->CurrentState.IsValid() && NameData->CurrentState.BundleNames == *NewBundles)
 			{
