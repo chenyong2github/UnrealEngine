ac1fda7a5857c3d2bdfff75437d35fcc6f40348d
chris caulfield <Chris.Caulfield@epicgames.com>
2020-04-02 15:17:17 -0400

    Chaos - clean up includes - ImmediatePhysicsSimulation_Chaos.h
    [FYI] ori.cohen
    #rb none
    
    #ROBOMERGE-SOURCE: CL 12567297 via CL 12567386 via CL 12567396 via CL 12567428 via CL 12567466
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v675-12543919)
    
    [CL 12567500 by chris caulfield in Main branch]

--- Engine/Source/Runtime/Engine/Public/Physics/ImmediatePhysics/ImmediatePhysicsChaos/ImmediatePhysicsSimulation_Chaos.h
+++ Engine/Source/Runtime/Engine/Public/Physics/ImmediatePhysics/ImmediatePhysicsChaos/ImmediatePhysicsSimulation_Chaos.h
@@ -4,17 +4,6 @@
 
 #include "Physics/ImmediatePhysics/ImmediatePhysicsChaos/ImmediatePhysicsCore_Chaos.h"
 
-#define TEMP_HEADER_CHAOS_LEVEL_1
-#include "Chaos/ChaosDebugDrawDeclares.h"
-#include "Chaos/Collision/NarrowPhase.h"
-#include "Chaos/Collision/ParticlePairBroadPhase.h"
-#include "Chaos/Collision/ParticlePairCollisionDetector.h"
-#include "Chaos/Evolution/PBDMinEvolution.h"
-#include "Chaos/PBDCollisionConstraints.h"
-#include "Chaos/PBDConstraintRule.h"
-#include "Chaos/PBDJointConstraints.h"
-#undef TEMP_HEADER_CHAOS_LEVEL_1
-
 #include "Engine/EngineTypes.h"
 #include "Templates/UniquePtr.h"
 
@@ -27,10 +16,10 @@ namespace ImmediatePhysics_Chaos
 		FSimulation();
 		~FSimulation();
 
-		int32 NumActors() const { return ActorHandles.Num(); }
+		int32 NumActors() const;
 
-		FActorHandle* GetActorHandle(int32 ActorHandleIndex) { return ActorHandles[ActorHandleIndex]; }
-		const FActorHandle* GetActorHandle(int32 ActorHandleIndex) const { return ActorHandles[ActorHandleIndex]; }
+		FActorHandle* GetActorHandle(int32 ActorHandleIndex);
+		const FActorHandle* GetActorHandle(int32 ActorHandleIndex) const;
 
 		/** Create a static body and add it to the simulation */
 		FActorHandle* CreateStaticActor(FBodyInstance* BodyInstance);
@@ -81,53 +70,8 @@ namespace ImmediatePhysics_Chaos
 		void DebugDrawDynamicParticles(const int32 MinDebugLevel, const int32 MaxDebugLevel, const FColor& Color);
 		void DebugDrawConstraints(const int32 MinDebugLevel, const int32 MaxDebugLevel, const float ColorScale);
 
-		using FCollisionConstraints = Chaos::FPBDCollisionConstraints;
-		using FCollisionDetector = Chaos::FParticlePairCollisionDetector;
-		using FRigidParticleSOAs = Chaos::TPBDRigidsSOAs<FReal, 3>;
-		using FParticleHandle = Chaos::TGeometryParticleHandle<FReal, Dimensions>;
-		using FParticlePair = Chaos::TVector<Chaos::TGeometryParticleHandle<Chaos::FReal, 3>*, 2>;
-
-		// @todo(ccaulfield): Look into these...
-		TArray<FParticlePair> ActivePotentiallyCollidingPairs;
-		Chaos::TArrayCollectionArray<bool> CollidedParticles;
-		Chaos::TArrayCollectionArray<Chaos::TSerializablePtr<Chaos::FChaosPhysicsMaterial>> ParticleMaterials;
-		Chaos::TArrayCollectionArray<TUniquePtr<Chaos::FChaosPhysicsMaterial>> PerParticleMaterials;
-		Chaos::TArrayCollectionArray<Chaos::FVec3> ParticlePrevXs;
-		Chaos::TArrayCollectionArray<Chaos::FRotation3> ParticlePrevRs;
-
-		FRigidParticleSOAs Particles;
-		Chaos::FPBDJointConstraints Joints;
-		FCollisionConstraints Collisions;
-		Chaos::FParticlePairBroadPhase BroadPhase;
-		Chaos::FNarrowPhase NarrowPhase;
-		FCollisionDetector CollisionDetector;
-		Chaos::TSimpleConstraintRule<Chaos::FPBDJointConstraints> JointsRule;
-		Chaos::TSimpleConstraintRule<FCollisionConstraints> CollisionsRule;
-		Chaos::FPBDMinEvolution Evolution;
-
-
-		/** Mapping from entity index to handle */
-		// @todo(ccaulfield): we now have handles pointing to handles which is inefficient - we can do better than this, but don't want to change API yet
-		TArray<FActorHandle*> ActorHandles;
-		int32 NumActiveDynamicActorHandles;
-
-		/** Mapping from constraint index to handle */
-		TArray<FJointHandle*> JointHandles;
-
-		/** Slow to access. */
-		// @todo(ccaulfield): Optimize
-		TMap<const FParticleHandle*, TSet<const FParticleHandle*>> IgnoreCollisionParticlePairTable;
-
-		TArray<FParticlePair> PotentiallyCollidingPairs;
-
-		FTransform SimulationSpaceTransform;
-
-		FReal FixedStepTime;
-		FReal RollingAverageStepTime;
-		int32 NumRollingAverageStepTimes;
-		int32 MaxNumRollingAverageStepTimes;
-
-		bool bActorsDirty;
+		struct FImplementation;
+		TUniquePtr<FImplementation> Implementation;
 	};
 
 }
