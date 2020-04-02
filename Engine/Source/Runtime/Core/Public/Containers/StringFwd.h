35c79a3f096aca198ad67c080a3bc1512a4e2b4b
devin doucette <devin.doucette@epicgames.com>
2020-04-02 11:28:19 -0400

    Made the string builder types more generic
    The string builder type hierarchy has been simplified to TStringBuilderBase<CharType> and TStringBuilderWithBuffer<CharType, BufferSize> that derives from it. All other builder types are now aliases of these two, and there are now aliases for wide builders.
    
    The string literal path has been removed because it could cause unexpected behavior with const char arrays and it broke the use case (Builder << (Condition ? "A" : "B")). In practice, appending a string literal will typically calculate the length during compilation and compile to a call to Append(Str, Len).
    
    The append operators have been simplified to append anything convertible to a string view.
    
    #rb Steve.Robb, Johan.Torp
    [RN] minor Core
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: devin.doucette
    #ROBOMERGE-SOURCE: CL 12448058 via CL 12448099 via CL 12457556 via CL 12457617 via CL 12457725
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v673-12478461)
    
    [CL 12550707 by devin doucette in Dev-Networking branch]

--- Engine/Source/Runtime/Core/Public/Containers/StringFwd.h
+++ Engine/Source/Runtime/Core/Public/Containers/StringFwd.h
@@ -5,14 +5,31 @@
 #include "CoreTypes.h"
 
 // String Builder
-class FStringBuilderBase;
-class FAnsiStringBuilderBase;
-template <int32 N>
-class TStringBuilder;
-template <int32 N>
-class TAnsiStringBuilder;
+
+template <typename CharType> class TStringBuilderBase;
+template <typename CharType, int32 BufferSize> class TStringBuilderWithBuffer;
+
+/** The base string builder type for TCHAR. */
+using FStringBuilderBase = TStringBuilderBase<TCHAR>;
+/** The base string builder type for ANSICHAR. */
+using FAnsiStringBuilderBase = TStringBuilderBase<ANSICHAR>;
+/** The base string builder type for WIDECHAR. */
+using FWideStringBuilderBase = TStringBuilderBase<WIDECHAR>;
+
+/** An extendable string builder for TCHAR. */
+template <int32 BufferSize> using TStringBuilder = TStringBuilderWithBuffer<TCHAR, BufferSize>;
+/** An extendable string builder for ANSICHAR. */
+template <int32 BufferSize> using TAnsiStringBuilder = TStringBuilderWithBuffer<ANSICHAR, BufferSize>;
+/** An extendable string builder for WIDECHAR. */
+template <int32 BufferSize> using TWideStringBuilder = TStringBuilderWithBuffer<WIDECHAR, BufferSize>;
+
+/** A fixed-size string builder for TCHAR. */
+template <int32 BufferSize> using TFixedStringBuilder UE_DEPRECATED(4.25, "'TFixedStringBuilder' is deprecated. Please use 'TStringBuilder' instead!") = TStringBuilderWithBuffer<TCHAR, BufferSize>;
+/** A fixed-size string builder for ANSICHAR. */
+template <int32 BufferSize> using TFixedAnsiStringBuilder UE_DEPRECATED(4.25, "'TFixedAnsiStringBuilder' is deprecated. Please use 'TAnsiStringBuilder' instead!") = TStringBuilderWithBuffer<ANSICHAR, BufferSize>;
 
 // String View
+
 class FStringView;
 class FAnsiStringView;
 using FWideStringView = FStringView;
