ee50c489c699b76ce70358800b0a2e6ba592d20a
max chen <Max.Chen@epicgames.com>
2020-04-06 13:24:00 -0400

    Sequencer: Fix bake transform so that it iterates through the selected node guides, rather than through the selected nodes since the selection can change during the bake process.
    #rb none
    
    #ROBOMERGE-SOURCE: CL 12624959 via CL 12624961 via CL 12624963 via CL 12624965
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v675-12543919)
    
    [CL 12624969 by max chen in Main branch]

--- Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
+++ Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
@@ -1926,6 +1926,7 @@ void FSequencer::BakeTransform()
 
 	FocusedMovieScene->Modify();
 
+	TArray<FGuid> Guids;
 	for (const TSharedRef<FSequencerDisplayNode>& Node : Selection.GetSelectedOutlinerNodes())
 	{
 		if (Node->GetType() != ESequencerNode::Object)
@@ -1933,10 +1934,15 @@ void FSequencer::BakeTransform()
 			continue;
 		}
 
-		FFrameTime ResetTime = PlayPosition.GetCurrentPosition();
 		auto ObjectBindingNode = StaticCastSharedRef<FSequencerObjectBindingNode>(Node);
-
 		FGuid Guid = ObjectBindingNode->GetObjectBinding();
+
+		Guids.Add(Guid);
+	}
+
+	FFrameTime ResetTime = PlayPosition.GetCurrentPosition();
+	for (FGuid Guid : Guids)
+	{
 		for (auto RuntimeObject : FindBoundObjects(Guid, ActiveTemplateIDs.Top()) )
 		{
 			AActor* Actor = Cast<AActor>(RuntimeObject.Get());
