58ab981387158d070a0d8d69d5bfa8db391df041
juan canada <Juan.Canada@epicgames.com>
2020-04-07 20:39:29 -0400

    Fixed crash in ray tracing when Show Static Meshes flag was unchecked.
    #rb none
    #jira UE-91646
    #lockdown cristina.riveron
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: juan.canada
    #ROBOMERGE-SOURCE: CL 12658370 in //UE4/Release-4.25/... via CL 12658372 via CL 12658379 via CL 12658383
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v675-12543919)
    
    [CL 12668734 by juan canada in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/StaticMeshRender.cpp
+++ Engine/Source/Runtime/Engine/Private/StaticMeshRender.cpp
@@ -1637,6 +1637,11 @@ void FStaticMeshSceneProxy::GetDynamicMeshElements(const TArray<const FSceneView
 #if RHI_RAYTRACING
 void FStaticMeshSceneProxy::GetDynamicRayTracingInstances(FRayTracingMaterialGatheringContext& Context, TArray<FRayTracingInstance>& OutRayTracingInstances )
 {
+	if (DynamicRayTracingGeometries.Num() <= 0)
+	{
+		return;
+	}
+
 	uint8 PrimitiveDPG = GetStaticDepthPriorityGroup();
 	const uint32 LODIndex = GetLOD(Context.ReferenceView);
 	const FStaticMeshLODResources& LODModel = RenderData->LODResources[LODIndex];
