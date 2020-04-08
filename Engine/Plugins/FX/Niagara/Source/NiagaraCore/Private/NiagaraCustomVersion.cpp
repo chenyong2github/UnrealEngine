b5753e9841371d71ce836d2f69a0f8f6f5c837dd
stu mckenna <Stu.McKenna@epicgames.com>
2020-04-08 19:31:16 -0400

    - Fix for simulation stages that end in the name Particles not compiling. - This would cause our Array substitution to fail, instead look for the occurance of Particles as a whole namespace.
    #rb rob.krajcarski
    #jira UESP-3127
    #lockdown marcus.wassmer
    
    #ROBOMERGE-SOURCE: CL 12681471 in //UE4/Release-4.25/... via CL 12681473
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v675-12543919)
    
    [CL 12681476 by stu mckenna in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xD4934669, 0x2CE84BE3, 0x96F0768B, 0xF8D092BB);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3B068750, 0x6D605E4E, 0x82FEC7E6, 0xC997B754);
