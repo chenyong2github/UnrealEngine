1e398f6c68042d4172cea03d5f22311da028ed9e
mihnea balta <Mihnea.Balta@epicgames.com>
2020-04-08 13:30:44 -0400

    Added a method for computing the maximum number of per-particle components needed by a renderer. This is necessary so that the number can be clamped to not exceed the maximum allowed GPU buffer size.
    Also cleaned up GetBoundAttributes(), since all the subclasses were doing the same thing.
    
    #jira UE-90387
    #rb Rob.Krajcarski
    #rnx
    #lockdown Christina.Riveron
    
    #ROBOMERGE-SOURCE: CL 12675906 in //UE4/Release-4.25/... via CL 12675909
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v675-12543919)
    
    [CL 12675919 by mihnea balta in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Public/NiagaraLightRendererProperties.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Public/NiagaraLightRendererProperties.h
@@ -33,8 +33,6 @@ public:
 #if WITH_EDITORONLY_DATA
 	virtual bool IsMaterialValidForRenderer(UMaterial* Material, FText& InvalidMessage) override;
 	virtual void FixMaterial(UMaterial* Material) override;
-	virtual const TArray<FNiagaraVariable>& GetBoundAttributes() override;
-	virtual const TArray<FNiagaraVariable>& GetRequiredAttributes() override;
 	virtual const TArray<FNiagaraVariable>& GetOptionalAttributes() override;
 	virtual void GetRendererWidgets(const FNiagaraEmitterInstance* InEmitter, TArray<TSharedPtr<SWidget>>& OutWidgets, TSharedPtr<FAssetThumbnailPool> InThumbnailPool) const override;
 	virtual void GetRendererTooltipWidgets(const FNiagaraEmitterInstance* InEmitter, TArray<TSharedPtr<SWidget>>& OutWidgets, TSharedPtr<FAssetThumbnailPool> InThumbnailPool) const override;
