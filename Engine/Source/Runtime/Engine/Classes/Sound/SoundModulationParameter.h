98d804ac4951b24d9eb06a96afa3f618ce23ee18
rob gay <Rob.Gay@epicgames.com>
2020-04-10 17:23:08 -0400

    IWYU fix #rb none #rnx
    #ROBOMERGE-SOURCE: CL 12733271 via CL 12733272 via CL 12733273
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v676-12543919)
    
    [CL 12733277 by rob gay in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Sound/SoundModulationParameter.h
+++ Engine/Source/Runtime/Engine/Classes/Sound/SoundModulationParameter.h
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "Audio.h"
 #include "CoreMinimal.h"
 #include "DSP/BufferVectorOperations.h"
 #include "IAudioModulation.h"
