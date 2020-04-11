7d6e3ad8926d4406e3f7679c032bcf91f9ba6daa
ben marsh <Ben.Marsh@epicgames.com>
2020-04-10 22:52:55 -0400

    Remove unnecessary whitelist from CrashReportClient module.
    #jira
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 12737887 via CL 12737889 via CL 12737893 via CL 12737900
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v676-12543919)
    
    [CL 12739156 by ben marsh in Dev-Networking branch]

--- Engine/Source/Programs/CrashReportClient/CrashReportClient.Build.cs
+++ Engine/Source/Programs/CrashReportClient/CrashReportClient.Build.cs
@@ -55,7 +55,5 @@ public class CrashReportClient : ModuleRules
 		}
 
 		PrivateIncludePaths.Add("Runtime/Launch/Private");		// For LaunchEngineLoop.cpp include
-
-		WhitelistRestrictedFolders.Add("Private/NotForLicensees");
 	}
 }
