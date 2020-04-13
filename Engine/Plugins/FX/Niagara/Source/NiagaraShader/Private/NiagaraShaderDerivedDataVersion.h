cdf6d3b52b511bf9f7027e1082d690c4a1094514
ben ingram <Ben.Ingram@epicgames.com>
2020-04-13 17:04:25 -0400

    #jira none Add freezable hash table, use to accelerate lookup of shaders within ShaderMap - Shaders are searched by TypeName and PermutationId, so move those fields out of FShader and into separate arrays inside ShaderMap, to reduce cache misses while finding shaders #rb none
    #ROBOMERGE-OWNER: ben.ingram
    #ROBOMERGE-AUTHOR: ben.ingram
    #ROBOMERGE-SOURCE: CL 12765795 via CL 12766018 via CL 12766118
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v678-12743200)
    
    [CL 12766138 by ben ingram in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShaderDerivedDataVersion.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShaderDerivedDataVersion.h
@@ -11,4 +11,4 @@ NiagaraShaderDerivedDataVersion.h: Shader derived data version for Niagara.
 // In case of merge conflicts with DDC versions, you MUST generate a new GUID and set this new
 // guid as version
 
-#define NIAGARASHADERMAP_DERIVEDDATA_VER		TEXT("3136AAAD-A9DE-4368-9F86-E89840E7CC87")
+#define NIAGARASHADERMAP_DERIVEDDATA_VER		TEXT("DF5C47C0-F984-42E7-8960-20F53987595B")
