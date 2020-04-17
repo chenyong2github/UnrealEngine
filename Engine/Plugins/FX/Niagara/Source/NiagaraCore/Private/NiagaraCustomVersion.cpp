ae529012efd93e16ba6493558aec2a52b4012d59
simon tovey <Simon.Tovey@epicgames.com>
2020-04-17 11:28:27 -0400

    Fix for oddness with particle attributes defined in sim stages.
    Dataset Compiled data was reading from the wrong script so had an attribute set inconstent with the shader.
    
    #rb Rob.Krajcarski
    #JIRA UE-91937
    #lockdown Marcus.Wassmer
    
    #ROBOMERGE-SOURCE: CL 12899774 in //UE4/Release-4.25/... via CL 12899780
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v681-12776863)
    
    [CL 12899784 by simon tovey in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xAF99B544, 0x54DE442B, 0x85C1C157, 0x42CE5D9C);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x09A0B20A, 0xE82D4D9B, 0xB9D852FC, 0x2B5165BA);
