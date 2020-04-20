fe9f8a9abcbe7f65cd35e62a1bd0e5312cdba436
rob gay <Rob.Gay@epicgames.com>
2020-04-19 23:22:40 -0400

    Move OSCServerProxy packet processing to be pumped on tick and not forwarding request to task graph.  Avoids crash caused by callback on destructed server proxy. #jira UE-92165 #rb aaron.mcleran #test -game, editor, stand-alone client in QAGame, stress test spamming messages while starting and stopping OSCServer #lockdown cristina.riveron
    #ROBOMERGE-SOURCE: CL 12912089 in //UE4/Release-4.25/... via CL 12912091 via CL 12912092
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v682-12900288)
    
    [CL 12912094 by rob gay in Main branch]

--- Engine/Plugins/Runtime/OSC/Source/OSC/Private/OSCServerProxy.h
+++ Engine/Plugins/Runtime/OSC/Source/OSC/Private/OSCServerProxy.h
@@ -4,41 +4,61 @@
 #include "CoreMinimal.h"
 
 #include "Common/UdpSocketReceiver.h"
+#include "HAL/ThreadSafeBool.h"
 #include "Interfaces/IPv4/IPv4Address.h"
 #include "Interfaces/IPv4/IPv4Endpoint.h"
+#include "Tickable.h"
 #include "UObject/Object.h"
 
 #include "OSCServer.h"
 
 
-class OSC_API FOSCServerProxy : public IOSCServerProxy
+class OSC_API FOSCServerProxy : public IOSCServerProxy, public FTickableGameObject
 {
 public:
 	FOSCServerProxy(UOSCServer& InServer);
 	virtual ~FOSCServerProxy();
 
+	// Begin IOSCServerProxy interface
 	bool GetMulticastLoopback() const override;
 	FString GetIpAddress() const override;
 	int32 GetPort() const override;
 	bool IsActive() const override;
 
-	void Listen(const FString& ServerName) override;
+	void Listen(const FString& InServerName) override;
 
-	bool SetAddress(const FString& ReceiveIPAddress, int32 Port) override;
-	void SetMulticastLoopback(bool bMulticastLoopback) override;
+	bool SetAddress(const FString& InReceiveIPAddress, int32 InPort) override;
+	void SetMulticastLoopback(bool bInMulticastLoopback) override;
 
 	void Stop() override;
 
-	void AddWhitelistedClient(const FString& IPAddress) override;
-	void RemoveWhitelistedClient(const FString& IPAddress) override;
+	void AddWhitelistedClient(const FString& InIPAddress) override;
+	void RemoveWhitelistedClient(const FString& InIPAddress) override;
 	void ClearWhitelistedClients() override;
 	TSet<FString> GetWhitelistedClients() const override;
 
-	void SetWhitelistClientsEnabled(bool bEnabled) override;
+	void SetWhitelistClientsEnabled(bool bInEnabled) override;
+
+#if WITH_EDITOR
+	void SetTickableInEditor(bool bInTickInEditor) override;
+#endif // WITH_EDITOR
+	// End IOSCServerProxy interface
 
 	/** Callback that receives data from a socket. */
-	void OnPacketReceived(const FArrayReaderPtr& Data, const FIPv4Endpoint& Endpoint);
+	void OnPacketReceived(const FArrayReaderPtr& InData, const FIPv4Endpoint& InEndpoint);
 
+	// Begin FTickableGameObject interface
+	virtual void Tick(float InDeltaTime) override;
+	virtual bool IsTickable() const override { return IsActive(); }
+	virtual TStatId GetStatId() const override;
+	virtual UWorld* GetTickableGameObjectWorld() const override;
+
+#if WITH_EDITOR
+	virtual bool IsTickableInEditor() const override;
+#endif // WITH_EDITOR
+	// End FTickableGameObject interface
+
+	/** Parent server UObject */
 	UOSCServer* Server;
 
 	/** Socket used to listen for OSC packets. */
@@ -61,4 +81,9 @@ public:
 
 	/** Whether or not to use client whitelist */
 	bool bWhitelistClients;
+
+#if WITH_EDITOR
+	bool bTickInEditor;
+#endif // WITH_EDITOR
+
 };
--- Engine/Plugins/Runtime/OSC/Source/OSC/Public/OSCPacket.h
+++ Engine/Plugins/Runtime/OSC/Source/OSC/Public/OSCPacket.h
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "Interfaces/IPv4/IPv4Endpoint.h"
 #include "OSCTypes.h"
 #include "OSCStream.h"
 #include "OSCAddress.h"
@@ -12,14 +13,14 @@ class FOSCStream;
 class IOSCPacket
 {
 public:
-	IOSCPacket();
-	virtual ~IOSCPacket();
+	IOSCPacket() = default;
+	virtual ~IOSCPacket() = default;
 
 	/** Write packet data into stream */
-	virtual void WriteData(FOSCStream& Stream) = 0;
+	virtual void WriteData(FOSCStream& OutStream) = 0;
 	
 	/** Read packet data from stream */
-	virtual void ReadData(FOSCStream& Stream) = 0;
+	virtual void ReadData(FOSCStream& OutStream) = 0;
 	
 	/** Returns true if packet is message */
 	virtual bool IsMessage() = 0;
@@ -27,6 +28,12 @@ public:
 	/** Returns true if packet is bundle */
 	virtual bool IsBundle() = 0;
 
+	/** Get endpoint responsible for creation/forwarding of packet */
+	virtual const FIPv4Endpoint& GetEndpoint() const;
+
 	/** Create an OSC packet according to the input data. */
-	static TSharedPtr<IOSCPacket> CreatePacket(const uint8* PacketType);
+	static TSharedPtr<IOSCPacket> CreatePacket(const uint8* InPacketType, const FIPv4Endpoint& InEndpoint);
+
+protected:
+	FIPv4Endpoint Endpoint;
 };
