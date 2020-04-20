f63526e5047e4b43ea6a6f947ce579e2323d534c
Benn Gallagher <Benn.Gallagher@epicgames.com>
2020-04-20 12:39:05 -0400

    CIS - missing include for stat definition #rb none #jira UE-92209
    [CL 12926211 by Benn Gallagher in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionDetector.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionDetector.cpp
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #include "Chaos/Collision/CollisionDetector.h"
 #include "Chaos/Collision/ParticlePairBroadPhase.h"
+#include "Chaos/Collision/SpatialAccelerationBroadPhase.h"
 #include "ChaosLog.h"
 #include "ChaosStats.h"
 
