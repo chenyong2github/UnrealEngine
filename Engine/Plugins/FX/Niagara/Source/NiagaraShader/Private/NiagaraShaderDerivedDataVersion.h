93bc758092578e673041e82105b5ebe10fc99608
stu mckenna <Stu.McKenna@epicgames.com>
2020-04-21 17:07:39 -0400

    - Merging latest Niagara from Main and fixing an issue with GPU systems
    #rb none
    #jira none
    #rnx
    
    
    #ROBOMERGE-OWNER: stu.mckenna
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 12962211 via CL 12962401
    #ROBOMERGE-BOT: (v682-12900288)
    
    [CL 12962617 by stu mckenna in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShaderDerivedDataVersion.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShaderDerivedDataVersion.h
@@ -11,4 +11,4 @@ NiagaraShaderDerivedDataVersion.h: Shader derived data version for Niagara.
 // In case of merge conflicts with DDC versions, you MUST generate a new GUID and set this new
 // guid as version
 
-#define NIAGARASHADERMAP_DERIVEDDATA_VER		TEXT("C082A601-ECB4-4561-AED5-D131CB079201")
+#define NIAGARASHADERMAP_DERIVEDDATA_VER		TEXT("6D128353-3658-7247-82E1-32B1D0A006A7")
