8a7547d0ba6a5caae4ce5e45487b7923fade64a9
mihnea balta <Mihnea.Balta@epicgames.com>
2020-04-21 05:03:57 -0400

    Fixed buffer types in the sort shaders.
    These were causing validation errors in D3D11 and may be the cause for the Mac autotest failures.
    
    #rb none
    #jira FORT-279057
    [FYI] Jamie.Hayes
    
    
    #ROBOMERGE-OWNER: mihnea.balta
    #ROBOMERGE-AUTHOR: mihnea.balta
    #ROBOMERGE-SOURCE: CL 12948861 via CL 12948869
    #ROBOMERGE-BOT: (v682-12900288)
    
    [CL 12948881 by mihnea balta in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x09A0B20A, 0xE82D4D9B, 0xB9D852FC, 0x2B5165BA);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x946BD8DA, 0x1EDE4093, 0x8C1DE8A7, 0x73AEC56E);
