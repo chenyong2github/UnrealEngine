be2fff33d74403471da3e933c346297ed955d384
carl lloyd <carl.lloyd@epicgames.com>
2020-04-21 14:38:55 -0400

    Fix for incorrect image loads
    [at]Rolando.Caloca [at]Jack.Porter
    #jira none
    #rb Jack.Porter
    
    #ROBOMERGE-SOURCE: CL 12950743 in //UE4/Release-4.25/... via CL 12950761 via CL 12958414
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v682-12900288)
    
    [CL 12958450 by carl lloyd in Main branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -2018,8 +2018,7 @@ void FDeferredShadingSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 		// if we didn't to the prepass above, then we will need to clear now, otherwise, it's already been cleared and rendered to
 		ERenderTargetLoadAction ColorLoadAction = ERenderTargetLoadAction::ELoad;
 
-		bool bRequiresLoad = IsMetalPlatform(ShaderPlatform) || IsVulkanMobileSM5Platform(ShaderPlatform);
-		ERenderTargetLoadAction DepthLoadAction = bClearDepth ? ERenderTargetLoadAction::EClear : (!bRequiresLoad ? ERenderTargetLoadAction::ENoAction : ERenderTargetLoadAction::ELoad);
+		ERenderTargetLoadAction DepthLoadAction = bClearDepth ? ERenderTargetLoadAction::EClear : ERenderTargetLoadAction::ELoad;
 
 		const bool bClearBlack = ViewFamily.EngineShowFlags.ShaderComplexity || ViewFamily.EngineShowFlags.StationaryLightOverlap;
 		const float ClearAlpha = GetSceneColorClearAlpha();
@@ -2074,8 +2073,7 @@ void FDeferredShadingSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 		// Make sure we have began the renderpass
 		ERenderTargetLoadAction DepthLoadAction = bDepthWasCleared ? ERenderTargetLoadAction::ELoad : ERenderTargetLoadAction::EClear;
 
-		bool bRequiresLoad = IsMetalPlatform(ShaderPlatform) || IsVulkanMobileSM5Platform(ShaderPlatform);
-		SceneContext.BeginRenderingGBuffer(RHICmdList, (!bRequiresLoad ? ERenderTargetLoadAction::ENoAction : ERenderTargetLoadAction::ELoad), DepthLoadAction, BasePassDepthStencilAccess, ViewFamily.EngineShowFlags.ShaderComplexity);
+		SceneContext.BeginRenderingGBuffer(RHICmdList, ERenderTargetLoadAction::ELoad, DepthLoadAction, BasePassDepthStencilAccess, ViewFamily.EngineShowFlags.ShaderComplexity);
 	}
 	// Wait for Async SSAO before rendering base pass with forward rendering
 	if (IsForwardShadingEnabled(ShaderPlatform))
@@ -2608,8 +2606,7 @@ void FDeferredShadingSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 		SetupSceneTextureUniformParameters(SceneContext, FeatureLevel, ESceneTextureSetupMode::SceneDepth | ESceneTextureSetupMode::GBuffers, SceneTextureParameters);
 		TUniformBufferRef<FSceneTexturesUniformParameters> SceneTextureUniformBuffer = TUniformBufferRef<FSceneTexturesUniformParameters>::CreateUniformBufferImmediate(SceneTextureParameters, UniformBuffer_SingleFrame);
 
-		bool bRequiresLoad = IsMetalPlatform(ShaderPlatform) || IsVulkanMobileSM5Platform(ShaderPlatform);
-        SceneContext.BeginRenderingSceneColor(RHICmdList, (!bRequiresLoad ? ESimpleRenderTargetMode::EUninitializedColorExistingDepth : ESimpleRenderTargetMode::EExistingColorAndDepth));
+        SceneContext.BeginRenderingSceneColor(RHICmdList, ESimpleRenderTargetMode::EExistingColorAndDepth);
 		for (int32 ViewIndex = 0; ViewIndex < Views.Num(); ++ViewIndex)
 		{
 			const FViewInfo& View = Views[ViewIndex];
