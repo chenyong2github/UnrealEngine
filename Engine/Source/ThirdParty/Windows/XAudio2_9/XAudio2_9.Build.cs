52ffce63645e64093599ce2587baee2af0ab2cf8
david harvey <David.Harvey@epicgames.com>
2020-04-23 01:41:49 -0400

    disable PDB copying by default for XAudio2_9.
    #rb aaron.mcleran (pending)
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 12984998 via CL 12985000 via CL 12985002
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v682-12900288)
    
    [CL 12997263 by david harvey in Dev-Networking branch]

--- Engine/Source/ThirdParty/Windows/XAudio2_9/XAudio2_9.Build.cs
+++ Engine/Source/ThirdParty/Windows/XAudio2_9/XAudio2_9.Build.cs
@@ -16,14 +16,14 @@ public class XAudio2_9 : ModuleRules
 			PublicDelayLoadDLLs.Add("XAudio2_9redist.dll");
 			PublicAdditionalLibraries.Add(XAudio2_9Dir + "/Lib/x64/xaudio2_9redist.lib");
 			RuntimeDependencies.Add("$(EngineDir)/Binaries/ThirdParty/Windows/XAudio2_9/x64/xaudio2_9redist.dll");
-			RuntimeDependencies.Add("$(TargetOutputDir)/XAudio2_9redist.pdb", XAudio2_9Dir + "/Lib/x64/XAudio2_9redist.pdb", StagedFileType.DebugNonUFS);
+			//RuntimeDependencies.Add("$(TargetOutputDir)/XAudio2_9redist.pdb", XAudio2_9Dir + "/Lib/x64/XAudio2_9redist.pdb", StagedFileType.DebugNonUFS);
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Win32)
 		{
 			PublicDelayLoadDLLs.Add("XAudio2_9redist.dll");
 			PublicAdditionalLibraries.Add(XAudio2_9Dir + "/Lib/x86/xaudio2_9redist.lib");
 			RuntimeDependencies.Add("$(EngineDir)/Binaries/ThirdParty/Windows/XAudio2_9/x86/xaudio2_9redist.dll");
-			RuntimeDependencies.Add("$(TargetOutputDir)/XAudio2_9redist.pdb", XAudio2_9Dir + "/Lib/x86/XAudio2_9redist.pdb", StagedFileType.DebugNonUFS);
+			//RuntimeDependencies.Add("$(TargetOutputDir)/XAudio2_9redist.pdb", XAudio2_9Dir + "/Lib/x86/XAudio2_9redist.pdb", StagedFileType.DebugNonUFS);
 		}
 
 	}
