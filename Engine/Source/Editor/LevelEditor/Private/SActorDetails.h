ef61dc2ade7f02cfcff05fa7f236ee712f381868
dave belanger <Dave.Belanger@epicgames.com>
2020-04-28 16:47:35 -0400

    Hide details panel extension when the selection is empty. #rb Rex.Hill
    #ROBOMERGE-SOURCE: CL 13060431 via CL 13060433
    #ROBOMERGE-BOT: (v686-13045012)
    
    [CL 13060434 by dave belanger in Release-Engine-Staging branch]

--- Engine/Source/Editor/LevelEditor/Private/SActorDetails.h
+++ Engine/Source/Editor/LevelEditor/Private/SActorDetails.h
@@ -83,6 +83,7 @@ private:
 	TSharedPtr<class IDetailsView> DetailsView;
 	TSharedPtr<SBox> ComponentsBox;
 	TSharedPtr<class SSCSEditor> SCSEditor;
+	TSharedPtr<class SExtensionPanel> ExtensionPanel;
 
 	// The actor selected when the details panel was locked
 	TWeakObjectPtr<AActor> LockedActorSelection;
@@ -94,8 +95,8 @@ private:
 	// Used to prevent reentrant changes
 	bool bSelectionGuard;
 
-	// True if the actor details should be showing the components.
-	bool bShowingComponents = false;
+	// True if the actor details has any component to show.
+	bool bHasComponentsToShow = false;
 
 	// True if the actor "root" node in the SCS editor is currently shown as selected
 	bool bShowingRootActorNodeSelected;
