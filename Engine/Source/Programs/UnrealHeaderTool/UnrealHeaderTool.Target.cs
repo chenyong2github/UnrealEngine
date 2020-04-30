5f5e5424d21fb4fa7cc0390380eee6cfc4ecd851
stefan boberg <Stefan.Boberg@epicgames.com>
2020-04-30 17:28:24 -0400

    Compile out Garbage Collection, Core Redirect and SavePackage related code in UHT since they contribute greatly to the overall build time. Brings down a clean build of QAGame client win64 to under three minutes from just under four minutes
    #rb steve.robb, robert.manuszewski
    #ushell-cherrypick of 13054870 by Stefan.Boberg
    
    
    #ROBOMERGE-SOURCE: CL 13131491 via CL 13131495 via CL 13131499
    #ROBOMERGE-BOT: (v687-13115495)
    
    [CL 13131503 by stefan boberg in Release-Engine-Staging branch]

--- Engine/Source/Programs/UnrealHeaderTool/UnrealHeaderTool.Target.cs
+++ Engine/Source/Programs/UnrealHeaderTool/UnrealHeaderTool.Target.cs
@@ -23,7 +23,7 @@ public class UnrealHeaderToolTarget : TargetRules
 		bCompileAgainstEngine = false;
 		bCompileAgainstApplicationCore = false;
 
-		// Force execption handling across all modules.
+		// Force exception handling across all modules.
 		bForceEnableExceptions = true;
 
 		// Plugin support
@@ -36,5 +36,10 @@ public class UnrealHeaderToolTarget : TargetRules
         GlobalDefinitions.Add("HACK_HEADER_GENERATOR=1");
         GlobalDefinitions.Add("FNAME_WRITE_PROTECT_PAGES=0");
         GlobalDefinitions.Add("USE_LOCALIZED_PACKAGE_CACHE=0");
+
+		// We disable compilation of GC related code since it takes a very long time (up to 30 seconds on typical hardware at the time of writing)
+		GlobalDefinitions.Add("UE_WITH_GC=0");
+		GlobalDefinitions.Add("UE_WITH_SAVEPACKAGE=0");
+		GlobalDefinitions.Add("UE_WITH_CORE_REDIRECTS=0");
 	}
 }
