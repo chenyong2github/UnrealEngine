8c96e3c99aaa74d1062d1441acf40b2762bafce0
jon nabozny <Jon.Nabozny@epicgames.com>
2020-04-30 23:15:45 -0400

    Change Fast Array Limits to configurable CVar. Add a log on server when we know we have exceeded these limits.
    [at]Bart.Hawthorne, [at]Sergio.Gardeazabal
    #rb Bart.Hawthorne
    
    #ROBOMERGE-SOURCE: CL 13131754 via CL 13137033 via CL 13137068 via CL 13137085 via CL 13137111
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v687-13115495)
    
    [CL 13137147 by jon nabozny in Main branch]

--- Engine/Source/Runtime/Engine/Private/NetSerialization.cpp
+++ Engine/Source/Runtime/Engine/Private/NetSerialization.cpp
@@ -1,7 +1,22 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Engine/NetSerialization.h"
+#include "HAL/IConsoleManager.h"
 
 DEFINE_STAT(STAT_NetSerializeFastArray);
 DEFINE_STAT(STAT_NetSerializeFastArray_BuildMap);
 DEFINE_STAT(STAT_NetSerializeFastArray_DeltaStruct);
+
+int32 FFastArraySerializer::MaxNumberOfAllowedChangesPerUpdate = 2048;
+FAutoConsoleVariableRef FFastArraySerializer::CVarMaxNumberOfAllowedChangesPerUpdate(
+	TEXT("net.MaxNumberOfAllowedTArrayChangesPerUpdate"),
+	FFastArraySerializer::MaxNumberOfAllowedChangesPerUpdate,
+	TEXT("")
+);
+
+int32 FFastArraySerializer::MaxNumberOfAllowedDeletionsPerUpdate = 2048;
+FAutoConsoleVariableRef FFastArraySerializer::CVarMaxNumberOfAllowedDeletionsPerUpdate(
+	TEXT("net.MaxNumberOfAllowedTArrayDeletionsPerUpdate"),
+	FFastArraySerializer::MaxNumberOfAllowedDeletionsPerUpdate,
+	TEXT("")
+);
