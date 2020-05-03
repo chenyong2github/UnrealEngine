73dd012f2476af881ce79941eb2e805e31c9694d
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-05-03 16:34:00 -0400

    GeometryProcessing: further refactoring of mesh solvers. Moved different parts of LaplacianMeshSmoother file into ConstarinedMeshSmoothers.h, ConstrainedMeshDeformers.h, ConstrainedMeshDeformationSolver.h, MeshDiffusionIntegrator, and MeshDiffusionSmoothing.h. DeformMeshPolygonsTool now uses IConstrainedMeshSolver interface, and no longer includes Private headers from GeometryProcessing.
    Also made a small change to LaplacianOperators.cpp:ConstructScaledCotangentLaplacian(), which had checkSlow() that was failing on zero-elements Area matrix at boundary vertices. Code in this function does not appear to correctly handle boundary vertices (however things work out in the end because matrices are squared later?)
    
    #rb none
    #rnx
    
    
    #ROBOMERGE-SOURCE: CL 13145964 via CL 13145965
    #ROBOMERGE-BOT: (v688-13145358)
    
    [CL 13145966 by ryan schmidt in Release-Engine-Staging branch]

--- Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Solvers/Internal/LaplacianOperators.cpp
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Solvers/Internal/LaplacianOperators.cpp
@@ -27,7 +27,7 @@ THIRD_PARTY_INCLUDES_END
 //
 // Extension of TSparseMatrixAssembler suitable for eigen sparse matrix
 //
-class FEigenSparseMatrixAssembler : public TSparseMatrixAssembler<double>
+class FEigenSparseMatrixAssembler : public UE::Solvers::TSparseMatrixAssembler<double>
 {
 public:
 	typedef FSparseMatrixD::Scalar    ScalarT;
@@ -175,22 +175,28 @@ double ConstructScaledCotangentLaplacian(const FDynamicMesh3& DynamicMesh, FVert
 	FSparseMatrixD CotangentBoundary;
 	ConstructCotangentLaplacian(DynamicMesh, VertexMap, AreaMatrix, CotangentInterior, CotangentBoundary);
 
+	// note: returned AreaMatrix here is full mesh size, ie Interior + Boundary, with Boundary vertices at the end
+	// Does the code below make sense in light of that? We are constructing ScaledInvAreaMatrix and multiplying
+	// CotangentInterior and CotangentBoundary separately, but they are each different sizes??
+
 	// Find average entry in the area matrix
+	const int32 InternalSize = CotangentInterior.cols();
 	const int32 Rank = AreaMatrix.cols();
 	double AveArea = 0.;
-	for (int32 i = 0; i < Rank; ++i)
+	for (int32 i = 0; i < InternalSize; ++i)		// only non-boundary vertices have area > 0!
 	{
 		double Area = AreaMatrix.coeff(i, i);
 		checkSlow(Area > 0.);  // Area must be positive.
 		AveArea += Area;
 	}
-	AveArea /= Rank;
+	AveArea /= (double)InternalSize;
 	
 	std::vector<MatrixTripletT> ScaledInvAreaTriplets;
 	ScaledInvAreaTriplets.reserve(Rank);
 	for (int32 i = 0; i < Rank; ++i)
 	{
-		double Area = AreaMatrix.coeff(i, i);
+		bool bInternal = i < InternalSize;
+		double Area = (bInternal) ? AreaMatrix.coeff(i, i) : AveArea;		// use average area for boundary vertices?
 		double ScaledInvArea = AveArea / Area;
 		if (bClampAreas)
 		{
@@ -204,6 +210,7 @@ double ConstructScaledCotangentLaplacian(const FDynamicMesh3& DynamicMesh, FVert
 	ScaledInvAreaMatrix.setFromTriplets(ScaledInvAreaTriplets.begin(), ScaledInvAreaTriplets.end());
 	ScaledInvAreaMatrix.makeCompressed();
 
+	// ScaledInvAreaMatrix is a different max-dimension than CotangentBoundary and CotangentInterior ??
 	LaplacianBoundary = ScaledInvAreaMatrix * CotangentBoundary;
 	LaplacianBoundary.makeCompressed();
 	LaplacianInterior = ScaledInvAreaMatrix * CotangentInterior;
---
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Solvers/Internal/MatrixBase.h
@@ -0,0 +1,94 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+
+#include "FSparseMatrixD.h"
+
+
+
+enum class EMatrixSolverType
+{
+	LU /* SparseLU*/,
+	QR /* SparseQR */,
+	BICGSTAB /* Iterative Bi-conjugate gradient */,
+	PCG /* Pre-conditioned conjugate gradient  - requires symmetric positive def.*/,
+#ifndef EIGEN_MPL2_ONLY
+	LDLT /**Not included due to MPL2 */
+#endif
+};
+
+static FString MatrixSolverName(const EMatrixSolverType SolverType)
+{
+	FString String;
+	switch (SolverType)
+	{
+	case EMatrixSolverType::LU:
+		String = FString(TEXT(" Direct LU "));
+		break;
+	case EMatrixSolverType::QR:
+		String = FString(TEXT(" Direct QR "));
+		break;
+	case EMatrixSolverType::BICGSTAB:
+		String = FString(TEXT(" Iterative BiConjugate Gradient "));
+		break;
+	case EMatrixSolverType::PCG:
+		String = FString(TEXT(" Iterative Preconditioned Conjugate Gradient "));
+		break;
+#ifndef EIGEN_MPL2_ONLY
+	case EMatrixSolverType::PCG:
+		String = FString(TEXT(" Direct Cholesky "));
+		break;
+#endif
+	default:
+		check(0);
+	}
+
+	return String;
+}
+
+class FMatrixSolverSettings
+{
+public:
+	EMatrixSolverType  MatrixSolverType;
+
+	// Used by iterative solvers
+	int32  MaxIterations = 600;
+	double Tolerance = 1e-4;
+};
+
+
+
+// Pure Virtual Base Class used to wrap Eigen Solvers.
+class IMatrixSolverBase
+{
+public:
+	typedef typename FSparseMatrixD::Scalar    ScalarType;
+	typedef typename FSOAPositions::VectorType VectorType;
+
+	IMatrixSolverBase() {}
+	virtual ~IMatrixSolverBase() = 0;
+	virtual bool bIsIterative() const = 0;
+	virtual void Solve(const VectorType& BVector, VectorType& SolVector) const = 0;
+	virtual void Solve(const FSOAPositions& BVectors, FSOAPositions& SolVectors) const = 0;
+
+	virtual void SetUp(const FSparseMatrixD& Matrix, bool bIsSymmetric) = 0;
+	//virtual void SetIterations(int32 MaxIterations) = 0;
+	virtual void Reset() = 0;
+	virtual bool bSucceeded() const = 0;
+};
+
+// Additional methods particular to the iterative solvers
+class IIterativeMatrixSolverBase : public IMatrixSolverBase
+{
+public:
+	IIterativeMatrixSolverBase() {}
+
+	virtual ~IIterativeMatrixSolverBase() = 0;
+	virtual void SetIterations(int32 MaxIterations) = 0;
+	virtual void SetTolerance(double Tol) = 0;
+	virtual void SolveWithGuess(const VectorType& GuessVector, const VectorType& BVector, VectorType& SolVector)  const = 0;
+	virtual void SolveWithGuess(const FSOAPositions& GuessVector, const FSOAPositions& BVector, FSOAPositions& SolVector)  const = 0;
+
+};
