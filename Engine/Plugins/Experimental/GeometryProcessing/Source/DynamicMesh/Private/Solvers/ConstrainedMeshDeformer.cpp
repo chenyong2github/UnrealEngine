59b6650f53308fc79217ca888aecbadae90ae4c0
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-05-03 18:42:18 -0400

    GeometryProcessing: further refactoring of mesh solvers. Moved different parts of LaplacianMeshSmoother file into ConstarinedMeshSmoothers.h, ConstrainedMeshDeformers.h, ConstrainedMeshDeformationSolver.h, MeshDiffusionIntegrator, and MeshDiffusionSmoothing.h. DeformMeshPolygonsTool now uses IConstrainedMeshSolver interface, and no longer includes Private headers from GeometryProcessing.
    Also made a small change to LaplacianOperators.cpp:ConstructScaledCotangentLaplacian(), which had checkSlow() that was failing on zero-elements Area matrix at boundary vertices. Code in this function does not appear to correctly handle boundary vertices (however things work out in the end because matrices are squared later?)
    
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 13145964 via CL 13145965 via CL 13145966 via CL 13145967
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v688-13145358)
    
    [CL 13146167 by ryan schmidt in Dev-Networking branch]

--- Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Solvers/ConstrainedMeshDeformer.cpp
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Solvers/ConstrainedMeshDeformer.cpp
@@ -1,12 +1,12 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Solvers/ConstrainedMeshDeformer.h"
-#include "Solvers/Internal/LaplacianMeshSmoother.h"
+#include "Solvers/Internal/ConstrainedMeshDeformers.h"
 
 
-TUniquePtr<IConstrainedMeshSolver> UE::MeshDeformation::ConstructConstrainedMeshDeformer(const ELaplacianWeightScheme WeightScheme, const FDynamicMesh3& DynamicMesh)
+TUniquePtr<UE::Solvers::IConstrainedMeshSolver> UE::MeshDeformation::ConstructConstrainedMeshDeformer(const ELaplacianWeightScheme WeightScheme, const FDynamicMesh3& DynamicMesh)
 {
-	TUniquePtr<IConstrainedMeshSolver> Deformer(new FConstrainedMeshDeformer(DynamicMesh, WeightScheme));
+	TUniquePtr<UE::Solvers::IConstrainedMeshSolver> Deformer(new FConstrainedMeshDeformer(DynamicMesh, WeightScheme));
 	return Deformer;
 }
 
