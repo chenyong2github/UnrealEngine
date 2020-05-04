8533a2e09b66fb48d8d8ae6d25b28bd5a97a915f
robert manuszewski <Robert.Manuszewski@epicgames.com>
2020-05-04 06:05:47 -0400

    Proper fix to workaround the fact that some *GeneratedClasses mark themselves as CLASS_Native. Token stream generation code will now skip class refernces for classes marked as EInternalObjectFlags::Native which is also used by GC to skip marking objects as unreachable.
    #rb Steve.Robb
    
    
    #ROBOMERGE-SOURCE: CL 13146999 via CL 13147000 via CL 13147001
    #ROBOMERGE-BOT: (v688-13145358)
    
    [CL 13147003 by robert manuszewski in Release-Engine-Staging branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
@@ -2633,8 +2633,8 @@ void UClass::AssembleReferenceTokenStream(bool bForce)
 
 		{
 			check(ClassAddReferencedObjects != NULL);
-			const bool bKeepOuter = true;// GetFName() != NAME_Package;
-			const bool bKeepClass = true;// !(GetClassFlags() & CLASS_Native) || IsA(UDynamicClass::StaticClass());
+			const bool bKeepOuter = GetFName() != NAME_Package;
+			const bool bKeepClass = !(HasAnyInternalFlags(EInternalObjectFlags::Native) || IsA(UDynamicClass::StaticClass());
 
 			ClassAddReferencedObjectsType AddReferencedObjectsFn = nullptr;
 #if !WITH_EDITOR
