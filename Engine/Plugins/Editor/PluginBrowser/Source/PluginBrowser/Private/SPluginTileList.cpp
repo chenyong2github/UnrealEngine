9932d26d99c5549679b888f344b27e48621d415b
dave belanger <Dave.Belanger@epicgames.com>
2020-05-04 13:39:47 -0400

    [PluginBrowser] - Add "All" category that includes all plugins. Useful to search all plugins and not restrict the search to a given category or sub-category. - Bonus: Display, sort and search by plugin name if friendly name is not set. #jira UE-92639 #rb Matt.Kuhlenschmidt,Matt.Hoffman
    #ROBOMERGE-SOURCE: CL 13149999 via CL 13150012
    #ROBOMERGE-BOT: (v688-13145358)
    
    [CL 13150019 by dave belanger in Release-Engine-Staging branch]

--- Engine/Plugins/Editor/PluginBrowser/Source/PluginBrowser/Private/SPluginTileList.cpp
+++ Engine/Plugins/Editor/PluginBrowser/Source/PluginBrowser/Private/SPluginTileList.cpp
@@ -86,9 +86,9 @@ void SPluginTileList::RebuildAndFilterPluginList()
 		{
 			struct FPluginListItemSorter
 			{
-				bool operator()( const TSharedRef<IPlugin>& A, const TSharedRef<IPlugin>& B ) const
+				bool operator()(const TSharedRef<IPlugin>& A, const TSharedRef<IPlugin>& B) const
 				{
-					return A->GetDescriptor().FriendlyName < B->GetDescriptor().FriendlyName;
+					return SPluginTile::GetPluginDisplayName(&*A) < SPluginTile::GetPluginDisplayName(&*B);
 				}
 			};
 			PluginListItems.Sort( FPluginListItemSorter() );
