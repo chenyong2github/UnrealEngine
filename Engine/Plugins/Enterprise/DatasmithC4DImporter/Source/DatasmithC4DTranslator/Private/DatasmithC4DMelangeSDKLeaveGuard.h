108a3b9cabd9969736e51777994fef0cda9a8f94
johan duparc <Johan.Duparc@epicgames.com>
2020-05-05 15:45:27 -0400

    Fix compiliation issue when using c4d in combination with openEXR: - Add _HAS_EXCEPTIONS in the backuped macros before including c4d.h
    Note: c4d changes _HAS_EXCEPTIONS (see c4d_misc\general.h) which breaks subsequent usage of openEXR
    
    #rb JeanMichel-Dignard
    #jira UE-92778
    #lockdown Simon.Tourangeau
    
    #ROBOMERGE-SOURCE: CL 13187131 in //UE4/Release-4.25/... via CL 13187135 via CL 13187167
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v688-13145358)
    
    [CL 13187179 by johan duparc in Main branch]

--- Engine/Plugins/Enterprise/DatasmithC4DImporter/Source/DatasmithC4DTranslator/Private/DatasmithC4DMelangeSDKLeaveGuard.h
+++ Engine/Plugins/Enterprise/DatasmithC4DImporter/Source/DatasmithC4DTranslator/Private/DatasmithC4DMelangeSDKLeaveGuard.h
@@ -7,6 +7,7 @@ __pragma(warning(pop))
 // Restore some preprocessor identifiers.
 __pragma(pop_macro("PI"))
 __pragma(pop_macro("BYTE_MAX"))
+__pragma(pop_macro("_HAS_EXCEPTIONS"))
 
 // Leave Datasmith platform include guard.
 #include "Windows/HideWindowsPlatformTypes.h"
