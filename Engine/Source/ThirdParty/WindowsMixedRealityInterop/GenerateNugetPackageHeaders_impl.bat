824d89624e8b36457138b79aaa65798d5f9f3198
jeff fisher <Jeff.Fisher@epicgames.com>
2020-05-06 14:40:24 -0400

    UE-92511 Hololens - interop build process cleanup -Added two missing pdb files to BinaryFileList.txt -We now use nuget as visual studio intends, and don't need custom scripting steps. #rb Steve.Smith #jira UE-92511 #lockdown nick.whiting
    #ROBOMERGE-SOURCE: CL 13211383 in //UE4/Release-4.25/... via CL 13211385 via CL 13211447
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v688-13145358)
    
    [CL 13211449 by jeff fisher in Main branch]

--- Engine/Source/ThirdParty/WindowsMixedRealityInterop/GenerateNugetPackageHeaders_impl.bat
+++
@@ -1,7 +0,0 @@
-setlocal
-set WINRT_VER=%1
-set NUGET_PACKAGE=%2
-set SDK_VER=%3
-set WINMD_FILE=%4
-set OUT_DIR=%5
-packages\Microsoft.Windows.CppWinRT.%WINRT_VER%\bin\cppwinrt.exe -in packages\%NUGET_PACKAGE%\build\native\include\%SDK_VER%\%WINMD_FILE% -ref %SDK_VER% -out %OUT_DIR%
