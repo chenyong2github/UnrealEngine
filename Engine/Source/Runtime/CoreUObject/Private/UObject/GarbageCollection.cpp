8042b2709f8f5cea9a7469cfd42a2a7ceb075085
robert manuszewski <Robert.Manuszewski@epicgames.com>
2020-05-08 15:50:36 -0400

    IncrementalPurgeGarbage will now lock GC guard only once
    #rb me
    #tests Cooked PC Client and Server, bot soak tests
    
    #ROBOMERGE-SOURCE: CL 13253256 via CL 13253276 via CL 13253290 via CL 13253296
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v688-13145358)
    
    [CL 13253312 by robert manuszewski in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/GarbageCollection.cpp
@@ -1428,27 +1428,30 @@ void IncrementalPurgeGarbage(bool bUseTimeLimit, float TimeLimit)
 		}
 
 	} ResetPurgeProgress(bCompleted);
-
-	// Keep track of start time to enforce time limit unless bForceFullPurge is true;
-	GCStartTime = FPlatformTime::Seconds();
-
-	bool bTimeLimitReached = false;
-	if (GUnrechableObjectIndex < GUnreachableObjects.Num())
+	
 	{
-		{
-			FConditionalGCLock ScopedGCLock;
-			bTimeLimitReached = UnhashUnreachableObjects(bUseTimeLimit, TimeLimit);
-		}
-		if (GUnrechableObjectIndex >= GUnreachableObjects.Num())
-		{
-			FScopedCBDProfile::DumpProfile();
-		}
-	}
-
-	if (!bTimeLimitReached)
-	{
+		// Lock before settting GCStartTime as it could be slow to lock if async loading is in progress
+		// but we still want to perform some GC work otherwise we'd be keeping objects in memory for a long time
 		FConditionalGCLock ScopedGCLock;
-		bCompleted = IncrementalDestroyGarbage(bUseTimeLimit, TimeLimit);
+
+		// Keep track of start time to enforce time limit unless bForceFullPurge is true;
+		GCStartTime = FPlatformTime::Seconds();
+		bool bTimeLimitReached = false;
+
+		if (GUnrechableObjectIndex < GUnreachableObjects.Num())
+		{
+			bTimeLimitReached = UnhashUnreachableObjects(bUseTimeLimit, TimeLimit);
+
+			if (GUnrechableObjectIndex >= GUnreachableObjects.Num())
+			{
+				FScopedCBDProfile::DumpProfile();
+			}
+		}
+
+		if (!bTimeLimitReached)
+		{
+			bCompleted = IncrementalDestroyGarbage(bUseTimeLimit, TimeLimit);
+		}
 	}
 #endif // !UE_WITH_GC
 }
