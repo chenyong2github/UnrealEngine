82fad93ad8fa9f9ed70e1944d1b3417f20559fae
zach bethel <zach.bethel@epicgames.com>
2020-05-08 13:47:13 -0400

    Fixed separate translucency screen percentage breaking depth.
    #jira UE-92303
    #rb trivial
    #lockdown cristina.riveron
    
    #ROBOMERGE-SOURCE: CL 13250161 in //UE4/Release-4.25/... via CL 13250170
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v688-13145358)
    
    [CL 13250185 by zach bethel in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Renderer/Private/TranslucentRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/TranslucentRendering.cpp
@@ -1033,6 +1033,10 @@ void FDeferredShadingSceneRenderer::RenderTranslucencyInner(FRHICommandListImmed
 				SceneContext.ResolveSeparateTranslucencyModulate(RHICmdList, View);
 				EndTimingSeparateTranslucencyModulatePass(RHICmdList, View);
 			}
+			else
+			{
+				SceneContext.ResolveSeparateTranslucency(RHICmdList, View);
+			}
 
 			if (TranslucencyPass != ETranslucencyPass::TPT_TranslucencyAfterDOF && TranslucencyPass != ETranslucencyPass::TPT_TranslucencyAfterDOFModulate)
 			{
