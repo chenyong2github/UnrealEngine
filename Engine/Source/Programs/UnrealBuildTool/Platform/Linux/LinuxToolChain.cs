595a94eebb9a5b0683b1044bd5aa923e58cba825
brandon schaefer <Brandon.Schaefer@epicgames.com>
2020-05-12 14:48:31 -0400

    Set override to 4GB for LinuxToolchain.cs when cross building
    #rb none
    [FYI] Graeme.Thornton
    
    #ROBOMERGE-SOURCE: CL 13275889 via CL 13275891 via CL 13275894 via CL 13286326
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v693-13264223)
    
    [CL 13286549 by brandon schaefer in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Linux/LinuxToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Linux/LinuxToolChain.cs
@@ -71,7 +71,7 @@ namespace UnrealBuildTool
 		protected bool bGdbIndexSection = true;
 
 		/** Allows you to override the maximum binary size allowed to be passed to objcopy.exe when cross building on Windows. */
-		protected UInt64 MaxBinarySizeOverrideForObjcopy = 0;
+		protected UInt64 MaxBinarySizeOverrideForObjcopy = 4294967296;
 
 		/** Platform SDK to use */
 		protected LinuxPlatformSDK PlatformSDK;
