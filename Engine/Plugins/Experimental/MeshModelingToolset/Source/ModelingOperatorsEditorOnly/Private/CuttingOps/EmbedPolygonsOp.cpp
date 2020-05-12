73719a110f81d992395e31ebd46b5ed6f36d89e6
jimmy andrews <Jimmy.Andrews@epicgames.com>
2020-05-11 22:26:50 -0400

    Make sure 'cut and fill' doesn't mess up UVs (by just leaving the original geometry but with a new polygroup, rather than trying to recreate it with a hole fill)
    #rb none
    #rnx
    #jira UE-92930
    #lockdown simon.tourangeau
    
    #ROBOMERGE-OWNER: jimmy.andrews
    #ROBOMERGE-AUTHOR: jimmy.andrews
    #ROBOMERGE-SOURCE: CL 13269745 in //UE4/Release-4.25/... via CL 13269748 via CL 13270073
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v693-13264223)
    
    [CL 13270074 by jimmy andrews in Main branch]

--- Engine/Plugins/Experimental/MeshModelingToolset/Source/ModelingOperatorsEditorOnly/Private/CuttingOps/EmbedPolygonsOp.cpp
+++ Engine/Plugins/Experimental/MeshModelingToolset/Source/ModelingOperatorsEditorOnly/Private/CuttingOps/EmbedPolygonsOp.cpp
@@ -150,11 +150,11 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 		if (SecondHit >= SortedHitTriangles.Num())
 		{
 			// failed to find a second surface to connect to
-			return;
+			SecondHit = -1;
 		}
 	}
 
-	auto CutHole = [](FDynamicMesh3& Mesh, const FFrame3d& F, int TriStart, const FPolygon2d& PolygonArg, TArray<int>& PathVertIDs, TArray<int>& PathVertCorrespond, bool bCollapseDegenerateEdges)
+	auto CutHole = [](FDynamicMesh3& Mesh, const FFrame3d& F, int TriStart, const FPolygon2d& PolygonArg, bool bDeleteInside, TArray<int>& PathVertIDs, TArray<int>& PathVertCorrespond, bool bCollapseDegenerateEdges)
 	{
 		if (!Mesh.IsTriangle(TriStart))
 		{
@@ -168,10 +168,21 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 		}
 
 		FDynamicMeshEditor MeshEditor(&Mesh);
-		bool bDidRemove = MeshEditor.RemoveTriangles(Selection.AsArray(), true);
-		if (!bDidRemove)
+		if (bDeleteInside)
 		{
-			return false;
+			bool bDidRemove = MeshEditor.RemoveTriangles(Selection.AsArray(), true);
+			if (!bDidRemove)
+			{
+				return false;
+			}
+		}
+		else
+		{
+			int GID = Mesh.AllocateTriangleGroup();
+			for (int TID : Selection)
+			{
+				Mesh.SetTriangleGroup(TID, GID);
+			}
 		}
 
 		// remove triangles could have removed a path vertex entirely in weird cases; just consider that a failure
@@ -195,7 +206,7 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 		{
 			check(Mesh.IsVertex(PathVertIDs[Idx])); // collapse shouldn't leave invalid verts in, and we check + fail out on invalid verts above that, so seeing them here should be impossible
 			int EID = Mesh.FindEdge(PathVertIDs[LastIdx], PathVertIDs[Idx]);
-			if (!Mesh.IsEdge(EID) || !Mesh.IsBoundaryEdge(EID))
+			if (!Mesh.IsEdge(EID) || (!Mesh.IsBoundaryEdge(EID) && bDeleteInside))
 			{
 				return false;
 			}
@@ -209,7 +220,9 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 		return true;
 	};
 
-	bool bCutSide1 = CutHole(*ResultMesh, Frame, SortedHitTriangles[0].Value, Polygon, PathVertIDs1, PathVertCorrespond1, bCollapseDegenerateEdges);
+	bool bDeleteInside = Operation == EEmbeddedPolygonOpMethod::CutThrough;
+
+	bool bCutSide1 = CutHole(*ResultMesh, Frame, SortedHitTriangles[0].Value, Polygon, bDeleteInside, PathVertIDs1, PathVertCorrespond1, bCollapseDegenerateEdges);
 	RecordEmbeddedEdges(PathVertIDs1);
 	if (!bCutSide1 || PathVertIDs1.Num() < 2)
 	{
@@ -218,8 +231,13 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 
 	if (Operation == EEmbeddedPolygonOpMethod::CutThrough)
 	{
+		if (SecondHit == -1)
+		{
+			return;
+		}
+
 		TArray<int> PathVertIDs2, PathVertCorrespond2;
-		bool bCutSide2 = CutHole(*ResultMesh, Frame, SortedHitTriangles[SecondHit].Value, Polygon, PathVertIDs2, PathVertCorrespond2, bCollapseDegenerateEdges);
+		bool bCutSide2 = CutHole(*ResultMesh, Frame, SortedHitTriangles[SecondHit].Value, Polygon, bDeleteInside, PathVertIDs2, PathVertCorrespond2, bCollapseDegenerateEdges);
 		RecordEmbeddedEdges(PathVertIDs1);
 		if (!bCutSide2 || PathVertIDs2.Num() < 2)
 		{
@@ -250,28 +268,30 @@ void FEmbedPolygonsOp::CalculateResult(FProgressCancel* Progress)
 			}
 		}
 	}
-	else if (Operation == EEmbeddedPolygonOpMethod::CutAndFill)
-	{
-		FDynamicMeshEditor MeshEditor(ResultMesh.Get());
-		FEdgeLoop Loop(ResultMesh.Get());
-		Loop.InitializeFromVertices(PathVertIDs1);
+	// TODO: later perhaps revive this hole fill code?
+	//       But for now CutAndFill has been conceptually replaced with "embed polygon," which is much more useful
+	//else if (Operation == EEmbeddedPolygonOpMethod::CutAndFill)
+	//{
+		//FDynamicMeshEditor MeshEditor(ResultMesh.Get());
+		//FEdgeLoop Loop(ResultMesh.Get());
+		//Loop.InitializeFromVertices(PathVertIDs1);
 
-		
-		FSimpleHoleFiller Filler(ResultMesh.Get(), Loop);
-		int GID = ResultMesh->AllocateTriangleGroup();
-		if (Filler.Fill(GID))
-		{
-			if (ResultMesh->HasAttributes())
-			{
-				MeshEditor.SetTriangleNormals(Filler.NewTriangles, (FVector3f)Frame.Z()); // TODO: should we use the best fit plane instead of the projection plane for the normal? ... probably.
-				
-				for (int UVIdx = 0, NumUVLayers = ResultMesh->Attributes()->NumUVLayers(); UVIdx < NumUVLayers; UVIdx++)
-				{
-					MeshEditor.SetTriangleUVsFromProjection(Filler.NewTriangles, Frame, UVScaleFactor, FVector2f::Zero(), true, UVIdx);
-				}
-			}
-		}
-	}
+		//
+		//FSimpleHoleFiller Filler(ResultMesh.Get(), Loop);
+		//int GID = ResultMesh->AllocateTriangleGroup();
+		//if (Filler.Fill(GID))
+		//{
+		//	if (ResultMesh->HasAttributes())
+		//	{
+		//		MeshEditor.SetTriangleNormals(Filler.NewTriangles, (FVector3f)Frame.Z()); // TODO: should we use the best fit plane instead of the projection plane for the normal? ... probably.
+		//		
+		//		for (int UVIdx = 0, NumUVLayers = ResultMesh->Attributes()->NumUVLayers(); UVIdx < NumUVLayers; UVIdx++)
+		//		{
+		//			MeshEditor.SetTriangleUVsFromProjection(Filler.NewTriangles, Frame, UVScaleFactor, FVector2f::Zero(), true, UVIdx);
+		//		}
+		//	}
+		//}
+	//}
 
 	bEmbedSucceeded = true;
 }
