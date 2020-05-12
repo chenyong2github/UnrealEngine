b1cc3d0f72e66f28d865c2f1b9a10c528bcc4354
seth weedin <Seth.Weedin@epicgames.com>
2020-05-12 13:17:09 -0400

    Add new stereo delay effect for submixes. Intended for use in Fritter.
    #ROBOMERGE-SOURCE: CL 13258090 via CL 13258091 via CL 13258092 via CL 13258097 via CL 13258109
    #ROBOMERGE-BOT: (v693-13264223)
    
    [CL 13278226 by seth weedin in Release-Engine-Staging branch]

--- Engine/Source/Runtime/SignalProcessing/Private/DelayStereo.cpp
+++ Engine/Source/Runtime/SignalProcessing/Private/DelayStereo.cpp
@@ -5,13 +5,6 @@
 namespace Audio
 {
 	FDelayStereo::FDelayStereo()
-		: DelayMode(EStereoDelayMode::Normal)
-		, DelayTimeMsec(0.0f)
-		, Feedback(0.0f)
-		, DelayRatio(0.0f)
-		, WetLevel(0.0f)
-		, NumChannels(0)
-		, bIsInit(true)
 	{
 	}
 
@@ -46,6 +39,27 @@ namespace Audio
 		WetLevel = FMath::Clamp(InWetLevel, 0.0f, 1.0f);;
 	}
 
+	void FDelayStereo::SetDryLevel(const float InDryLevel)
+	{
+		DryLevel = FMath::Clamp(InDryLevel, 0.0f, 1.0f);;
+	}
+
+	void FDelayStereo::SetFilterEnabled(bool bInEnabled)
+	{
+		bIsFilterEnabled = bInEnabled;
+	}
+
+	void FDelayStereo::SetFilterSettings(EBiquadFilter::Type InFilterType, const float InCutoffFrequency, const float InQ)
+	{
+		FilterFreq = InCutoffFrequency;
+		FilterQ = InQ;
+
+		for (FBiquadFilter& Filter : BiquadFilters)
+		{
+			Filter.SetParams(InFilterType, InCutoffFrequency, GetBandwidthFromQ(FilterQ), 0.0f);
+		}
+	}
+
 	void FDelayStereo::Init(const float InSampleRate, int32 InNumChannels, const float InDelayLengthSec)
 	{
 		NumChannels = InNumChannels;
@@ -55,6 +69,9 @@ namespace Audio
 		{
 			int32 Index = Delays.Add(FDelay());
 			Delays[Index].Init(InSampleRate, 2.0f * InDelayLengthSec);
+
+			Index = BiquadFilters.Add(FBiquadFilter());
+			BiquadFilters[Index].Init(InSampleRate, 1, FilterType, FilterFreq, GetBandwidthFromQ(FilterQ));
 		}
 
 		Reset();
@@ -92,9 +109,18 @@ namespace Audio
 		{
 			FDelay& Delay = Delays[0];
 			float DelayOut = Delay.Read();
+
 			float DelayIn = InFrame[0] + DelayOut * Feedback;
+
+			// Feed delay out through the filter
+			if (bIsFilterEnabled)
+			{
+				FBiquadFilter& Biquad = BiquadFilters[0];
+				Biquad.ProcessAudioFrame(&DelayIn, &DelayIn);
+			}
+
 			DelayOut = Delay.ProcessAudioSample(DelayIn);
-			OutFrame[0] = InFrame[0] + WetLevel * DelayOut;
+			OutFrame[0] = DryLevel * InFrame[0] + WetLevel * DelayOut;
 		}
 		else
 		{
@@ -120,13 +146,23 @@ namespace Audio
 				RightDelayIn = InFrame[0] + LeftDelayOut * Feedback;
 			}
 
+			// Feed delay out through the filter
+			if (bIsFilterEnabled)
+			{
+				FBiquadFilter& BiquadLeft = BiquadFilters[0];
+				BiquadLeft.ProcessAudioFrame(&LeftDelayIn, &LeftDelayIn);
+
+				FBiquadFilter& BiquadRight = BiquadFilters[1];
+				BiquadLeft.ProcessAudioFrame(&RightDelayIn, &RightDelayIn);
+			}
+
 			float WetLeftOut = 0.0f;
 			float WetRightOut = 0.0f;
 			WetLeftOut = Delays[0].ProcessAudioSample(LeftDelayIn);
 			WetRightOut = Delays[1].ProcessAudioSample(RightDelayIn);
 
-			OutFrame[0] = InFrame[0] + WetLevel * WetLeftOut;
-			OutFrame[1] = InFrame[1] + WetLevel * WetRightOut;
+			OutFrame[0] = DryLevel * InFrame[0] + WetLevel * WetLeftOut;
+			OutFrame[1] = DryLevel * InFrame[1] + WetLevel * WetRightOut;
 		}
 	}
 
@@ -138,47 +174,109 @@ namespace Audio
 		{
 			FDelay& Delay = Delays[0];
 
-			for (int32 SampleIndex = 0; SampleIndex < InNumSamples; ++SampleIndex)
+			if (bIsFilterEnabled)
 			{
-				float DelayOut = Delay.Read();
-				float DelayIn = InBuffer[SampleIndex] + DelayOut * Feedback;
-				DelayOut = Delay.ProcessAudioSample(DelayIn);
-				OutBuffer[SampleIndex] = InBuffer[SampleIndex] + WetLevel * DelayOut;
+				FBiquadFilter& BiquadLeft = BiquadFilters[0];
+
+				for (int32 SampleIndex = 0; SampleIndex < InNumSamples; ++SampleIndex)
+				{
+					float DelayOut = Delay.Read();
+					float DelayIn = InBuffer[SampleIndex] + DelayOut * Feedback;
+
+					BiquadLeft.ProcessAudioFrame(&DelayIn, &DelayIn);
+
+					DelayOut = Delay.ProcessAudioSample(DelayIn);
+					OutBuffer[SampleIndex] = DryLevel * InBuffer[SampleIndex] + WetLevel * DelayOut;
+				}
+}
+			else
+			{
+				for (int32 SampleIndex = 0; SampleIndex < InNumSamples; ++SampleIndex)
+				{
+					float DelayOut = Delay.Read();
+					float DelayIn = InBuffer[SampleIndex] + DelayOut * Feedback;
+					DelayOut = Delay.ProcessAudioSample(DelayIn);
+					OutBuffer[SampleIndex] = DryLevel * InBuffer[SampleIndex] + WetLevel * DelayOut;
+				}
 			}
 		}
 		else
 		{
-			for (int32 SampleIndex = 0; SampleIndex < InNumSamples; SampleIndex += NumChannels)
+			if (bIsFilterEnabled)
 			{
-				float LeftDelayOut = Delays[0].Read();
-				float RightDelayOut = Delays[1].Read();
+				FBiquadFilter& BiquadLeft = BiquadFilters[0];
+				FBiquadFilter& BiquadRight = BiquadFilters[1];
 
-				float LeftDelayIn = 0.0f;
-				float RightDelayIn = 0.0f;
-
-				if (DelayMode == EStereoDelayMode::Normal)
+				for (int32 SampleIndex = 0; SampleIndex < InNumSamples; SampleIndex += NumChannels)
 				{
-					LeftDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
-					RightDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
-				}
-				else if (DelayMode == EStereoDelayMode::Cross)
-				{
-					LeftDelayIn = InBuffer[SampleIndex + 1] + LeftDelayOut * Feedback;
-					RightDelayIn = InBuffer[SampleIndex] + RightDelayOut * Feedback;
-				}
-				else if (DelayMode == EStereoDelayMode::PingPong)
-				{
-					LeftDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
-					RightDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
-				}
+					float LeftDelayOut = Delays[0].Read();
+					float RightDelayOut = Delays[1].Read();
 
-				float WetLeftOut = 0.0f;
-				float WetRightOut = 0.0f;
-				WetLeftOut = Delays[0].ProcessAudioSample(LeftDelayIn);
-				WetRightOut = Delays[1].ProcessAudioSample(RightDelayIn);
+					float LeftDelayIn = 0.0f;
+					float RightDelayIn = 0.0f;
 
-				OutBuffer[SampleIndex] = InBuffer[SampleIndex] + WetLevel * WetLeftOut;
-				OutBuffer[SampleIndex + 1] = InBuffer[SampleIndex + 1] + WetLevel * WetRightOut;
+					if (DelayMode == EStereoDelayMode::Normal)
+					{
+						LeftDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
+					}
+					else if (DelayMode == EStereoDelayMode::Cross)
+					{
+						LeftDelayIn = InBuffer[SampleIndex + 1] + LeftDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex] + RightDelayOut * Feedback;
+					}
+					else if (DelayMode == EStereoDelayMode::PingPong)
+					{
+						LeftDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
+					}
+
+					BiquadLeft.ProcessAudioFrame(&LeftDelayIn, &LeftDelayIn);
+					BiquadRight.ProcessAudioFrame(&RightDelayIn, &RightDelayIn);
+
+					float WetLeftOut = 0.0f;
+					float WetRightOut = 0.0f;
+					WetLeftOut = Delays[0].ProcessAudioSample(LeftDelayIn);
+					WetRightOut = Delays[1].ProcessAudioSample(RightDelayIn);
+
+					OutBuffer[SampleIndex] = DryLevel * InBuffer[SampleIndex] + WetLevel * WetLeftOut;
+					OutBuffer[SampleIndex + 1] = DryLevel * InBuffer[SampleIndex + 1] + WetLevel * WetRightOut;
+				}
+			}
+			else
+			{
+				for (int32 SampleIndex = 0; SampleIndex < InNumSamples; SampleIndex += NumChannels)
+				{
+					float LeftDelayOut = Delays[0].Read();
+					float RightDelayOut = Delays[1].Read();
+
+					float LeftDelayIn = 0.0f;
+					float RightDelayIn = 0.0f;
+
+					if (DelayMode == EStereoDelayMode::Normal)
+					{
+						LeftDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
+					}
+					else if (DelayMode == EStereoDelayMode::Cross)
+					{
+						LeftDelayIn = InBuffer[SampleIndex + 1] + LeftDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex] + RightDelayOut * Feedback;
+					}
+					else if (DelayMode == EStereoDelayMode::PingPong)
+					{
+						LeftDelayIn = InBuffer[SampleIndex + 1] + RightDelayOut * Feedback;
+						RightDelayIn = InBuffer[SampleIndex] + LeftDelayOut * Feedback;
+					}
+
+					float WetLeftOut = 0.0f;
+					float WetRightOut = 0.0f;
+					WetLeftOut = Delays[0].ProcessAudioSample(LeftDelayIn);
+					WetRightOut = Delays[1].ProcessAudioSample(RightDelayIn);
+
+					OutBuffer[SampleIndex] = DryLevel * InBuffer[SampleIndex] + WetLevel * WetLeftOut;
+					OutBuffer[SampleIndex + 1] = DryLevel * InBuffer[SampleIndex + 1] + WetLevel * WetRightOut;
+				}
 			}
 		}
 	}
