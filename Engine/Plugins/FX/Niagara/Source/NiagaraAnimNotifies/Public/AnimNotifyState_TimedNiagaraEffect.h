2621e564dc537275c069b6965b219444b0d14f5b
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-05-19 17:56:59 -0400

    Fix for Timed animnotifies with particle systems (niagara and cascade) to more reliably clean themselves up, especially when dealing with systems that have a long tail -tag newly created FXComponents with the animnotify name to identify which ones we need to clean up -only target active (including looking at NiagaraComponent's ExeuctionState) components #jira ue-93093 #rb stu.mckenna
    #ROBOMERGE-SOURCE: CL 13431371 via CL 13431434
    #ROBOMERGE-BOT: (v696-13331060)
    
    [CL 13431465 by rob krajcarski in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraAnimNotifies/Public/AnimNotifyState_TimedNiagaraEffect.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraAnimNotifies/Public/AnimNotifyState_TimedNiagaraEffect.h
@@ -41,26 +41,7 @@ class NIAGARAANIMNOTIFIES_API UAnimNotifyState_TimedNiagaraEffect : public UAnim
 	UPROPERTY(EditAnywhere, Category = NiagaraSystem, meta = (DisplayName = "Destroy Immediately", ToolTip = "Whether the Niagara system should be immediately destroyed at the end of the notify state or be allowed to finish"))
 	bool bDestroyAtEnd;
 
-#if WITH_EDITORONLY_DATA
-	// The following arrays are used to handle property changes during a state. Because we can't
-	// store any stateful data here we can't know which emitter is ours. The best metric we have
-	// is an emitter on our Mesh Component with the same template and socket name we have defined.
-	// Because these can change at any time we need to track previous versions when we are in an
-	// editor build. Refactor when stateful data is possible, tracking our component instead.
-	UPROPERTY(transient)
-	TArray<UFXSystemAsset*> PreviousTemplates;
-
-	UPROPERTY(transient)
-	TArray<FName> PreviousSocketNames;
-
-#endif
-
-#if WITH_EDITOR
-	virtual void PreEditChange(FProperty* PropertyAboutToChange) override;
-#endif
-
 	virtual void NotifyBegin(class USkeletalMeshComponent * MeshComp, class UAnimSequenceBase * Animation, float TotalDuration) override;
-	virtual void NotifyTick(class USkeletalMeshComponent * MeshComp, class UAnimSequenceBase * Animation, float FrameDeltaTime) override;
 	virtual void NotifyEnd(class USkeletalMeshComponent * MeshComp, class UAnimSequenceBase * Animation) override;
 
 	// Overridden from UAnimNotifyState to provide custom notify name.
@@ -68,7 +49,8 @@ class NIAGARAANIMNOTIFIES_API UAnimNotifyState_TimedNiagaraEffect : public UAnim
 
 protected:
 	// Spawns the NiagaraSystemComponent. Called from Notify.
-	virtual UFXSystemComponent* SpawnEffect(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation);
+	virtual UFXSystemComponent* SpawnEffect(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) const;
+
 private:
-	bool ValidateParameters(USkeletalMeshComponent* MeshComp);
+	bool ValidateParameters(USkeletalMeshComponent* MeshComp) const;
 };
