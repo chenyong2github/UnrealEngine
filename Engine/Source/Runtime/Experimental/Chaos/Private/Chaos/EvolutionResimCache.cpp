f811d718c65890c1de5c42ea1d6f3d4530b77827
robomerge <robomerge@epicgames.com>
2020-05-18 22:58:12 -0400

    More glue code to allow evolution to cache data for resim
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: robomerge
    #ROBOMERGE-SOURCE: CL 13239098 via CL 13239209 via CL 13239229 via CL 13239254
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v696-13331060)
    
    [CL 13353825 by robomerge in Dev-Networking branch]

---
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/EvolutionResimCache.cpp
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "Chaos/EvolutionResimCache.h"
+#include "Chaos/Collision/CollisionResimCache.h"
+
+namespace Chaos
+{
+	int32 UseCollisionResimCache = 0;
+	FAutoConsoleVariableRef CVarUseCollisionResimCache(TEXT("p.UseCollisionResimCache"),UseCollisionResimCache,TEXT("Whether to skip collision detection during resim"));
+
+	FEvolutionResimCache::~FEvolutionResimCache() = default;
+	
+	FEvolutionResimCache::FEvolutionResimCache()
+	: CollisionResimCache(nullptr)
+	{
+		if(UseCollisionResimCache)
+		{
+			CollisionResimCache = MakeUnique<FCollisionResimCache>();
+		}
+	}
+
+	void FEvolutionResimCache::Reset()
+	{
+		if(CollisionResimCache)
+		{
+			CollisionResimCache->Reset();
+		}
+	}
+}
