145ec6accfb1c6f03ebd64883f7208649afaa581
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-05-24 15:47:26 -0400

    Fix for crash accessing cooked out GPU emitter by cleaning out execution order indices of invalid emitters [REVIEW] [at]stu.mckenna #rb stu.mckenna
    #localization none
    #tests Recreated test case in EngineTest and validated the crash was fixed on PS4
    
    
    #ROBOMERGE-SOURCE: CL 13531502 via CL 13534238 via CL 13534280 via CL 13534325
    #ROBOMERGE-BOT: (v698-13502450)
    
    [CL 13534364 by rob krajcarski in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemInstance.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemInstance.cpp
@@ -15,6 +15,7 @@
 #include "GameFramework/PlayerController.h"
 #include "NiagaraCrashReporterHandler.h"
 #include "Async/Async.h"
+#include "Algo/RemoveIf.h"
 
 
 DECLARE_CYCLE_STAT(TEXT("System Activate [GT]"), STAT_NiagaraSystemActivate, STATGROUP_Niagara);
@@ -754,6 +755,17 @@ void FNiagaraSystemInstance::ComputeEmittersExecutionOrder()
 			{
 				if (EmitterDependencies[DepIdx] == &Emitters[OtherEmitterIdx].Get())
 				{
+					const bool HasSourceEmitter = Emitters[EmitterIdx]->GetCachedEmitter() != nullptr;
+					const bool HasDependentEmitter = Emitters[OtherEmitterIdx]->GetCachedEmitter() != nullptr;
+
+					// check to see if the emitter we're dependent on may have been culled during the cook
+					if (HasSourceEmitter && !HasDependentEmitter)
+					{
+						UE_LOG(LogNiagara, Error, TEXT("Emitter[%s] depends on Emitter[%s] which is not available (has scalability removed it during a cook?)."),
+							*GetSystem()->GetEmitterHandles()[EmitterIdx].GetName().ToString(),
+							*GetSystem()->GetEmitterHandles()[OtherEmitterIdx].GetName().ToString());
+					}
+
 					// Some DIs might read from the same emitter they're applied to. We don't care about dependencies on self.
 					if (EmitterIdx != OtherEmitterIdx)
 					{
@@ -766,27 +778,32 @@ void FNiagaraSystemInstance::ComputeEmittersExecutionOrder()
 		}
 	}
 
-	if (!bHasEmitterDependencies)
+	if (bHasEmitterDependencies)
 	{
-		return;
-	}
-
-	for (int32 EmitterIdx = 0; EmitterIdx < NumEmitters; ++EmitterIdx)
-	{
-		if (EmitterPriorities[EmitterIdx] < 0)
+		for (int32 EmitterIdx = 0; EmitterIdx < NumEmitters; ++EmitterIdx)
 		{
-			if (!ComputeEmitterPriority(EmitterIdx, EmitterPriorities, EmitterDependencyGraph))
+			if (EmitterPriorities[EmitterIdx] < 0)
 			{
-				FName EmitterName = GetSystem()->GetEmitterHandles()[EmitterIdx].GetName();
-				UE_LOG(LogNiagara, Error, TEXT("Found circular dependency involving emitter '%s' in system '%s'. The execution order will be undefined."), *EmitterName.ToString(), *GetSystem()->GetName());
-				break;
+				if (!ComputeEmitterPriority(EmitterIdx, EmitterPriorities, EmitterDependencyGraph))
+				{
+					FName EmitterName = GetSystem()->GetEmitterHandles()[EmitterIdx].GetName();
+					UE_LOG(LogNiagara, Error, TEXT("Found circular dependency involving emitter '%s' in system '%s'. The execution order will be undefined."), *EmitterName.ToString(), *GetSystem()->GetName());
+					break;
+				}
 			}
 		}
+
+		// Sort the emitter indices in the execution order array so that dependencies are satisfied. Also, emitters with the same priority value don't have any
+		// inter-dependencies, so we can use that if we ever want to parallelize emitter execution.
+		Algo::Sort(EmitterExecutionOrder, [&EmitterPriorities](int32 IdxA, int32 IdxB) { return EmitterPriorities[IdxA] < EmitterPriorities[IdxB]; });
 	}
 
-	// Sort the emitter indices in the execution order array so that dependencies are satisfied. Also, emitters with the same priority value don't have any
-	// inter-dependencies, so we can use that if we ever want to parallelize emitter execution.
-	Algo::Sort(EmitterExecutionOrder, [&EmitterPriorities](int32 IdxA, int32 IdxB) { return EmitterPriorities[IdxA] < EmitterPriorities[IdxB]; });
+	// go through and remove any entries in the EmitterExecutionOrder array for emitters where we don't have a CachedEmitter, they have
+	// likely been cooked out because of scalability
+	EmitterExecutionOrder.SetNum(Algo::StableRemoveIf(EmitterExecutionOrder, [this](int32 EmitterIdx)
+	{
+		return Emitters[EmitterIdx]->GetCachedEmitter() == nullptr;
+	}));
 }
 
 void FNiagaraSystemInstance::Reset(FNiagaraSystemInstance::EResetMode Mode)
@@ -2032,7 +2049,7 @@ void FNiagaraSystemInstance::Tick_Concurrent()
 	UNiagaraSystem* System = GetSystem();
 
 	const int32 NumEmitters = Emitters.Num();
-	checkSlow(EmitterExecutionOrder.Num() == NumEmitters);
+	checkSlow(EmitterExecutionOrder.Num() <= NumEmitters);
 
 	//Determine if any of our emitters should be ticking.
 	TBitArray<TInlineAllocator<8>> EmittersShouldTick;
