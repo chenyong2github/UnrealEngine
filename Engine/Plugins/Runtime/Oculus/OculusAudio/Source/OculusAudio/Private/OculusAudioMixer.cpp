74ed3f8ba1fe2198bf5f3e2ab42951e0b850ade5
ethan geller <Ethan.Geller@epicgames.com>
2020-05-25 16:40:19 -0400

    #jira UE-93244 Hotfix for 100% crash on launch when setting Oculus Reverb as the current reverb plugin. The Oculus Reverb submix effect attempts to grab the current FAudioDevice before the current FAudioDevice is initialized- this probably worked previously to the FAudioDeviceManager refactor, but now that we don't allow access to the FAudioDevice until it's initialized, we need to properly get init parameters to the submix effect. #rb jimmy.smith #lockdown nick.whiting
    #ROBOMERGE-SOURCE: CL 13541649 in //UE4/Release-4.25/... via CL 13541651
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v698-13502450)
    
    [CL 13541652 by ethan geller in Release-Engine-Staging branch]

--- Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAudioMixer.cpp
+++ Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAudioMixer.cpp
@@ -29,6 +29,7 @@ void OculusAudioSpatializationAudioMixer::ClearContext()
 void OculusAudioSpatializationAudioMixer::Initialize(const FAudioPluginInitializationParams InitializationParams)
 {
 	FScopeLock ScopeLock(&ContextLock);
+	InitParams = InitializationParams;
 
 	Context = FOculusAudioContextManager::GetContextForAudioDevice(InitializationParams.AudioDevicePtr);
 	if (!Context)
@@ -45,8 +46,6 @@ void OculusAudioSpatializationAudioMixer::Initialize(const FAudioPluginInitializ
 
 	const UOculusAudioSettings* Settings = GetDefault<UOculusAudioSettings>();
 	ApplyOculusAudioSettings(Settings);
-
-	InitParams = InitializationParams;
 
 	TickDelegateHandle = FTicker::GetCoreTicker().AddTicker(FTickerDelegate::CreateRaw(this, &OculusAudioSpatializationAudioMixer::Tick));
 }
