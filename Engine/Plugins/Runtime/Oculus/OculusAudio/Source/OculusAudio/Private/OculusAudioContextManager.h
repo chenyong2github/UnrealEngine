762f7048a70a9a3db8ee35adaa6cc3be50f7117c
ethan geller <Ethan.Geller@epicgames.com>
2020-05-25 19:01:56 -0400

    #jira UE-93244 Hotfix for 100% crash on launch when setting Oculus Reverb as the current reverb plugin. The Oculus Reverb submix effect attempts to grab the current FAudioDevice before the current FAudioDevice is initialized- this probably worked previously to the FAudioDeviceManager refactor, but now that we don't allow access to the FAudioDevice until it's initialized, we need to properly get init parameters to the submix effect. #rb jimmy.smith #lockdown nick.whiting
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ethan.geller
    #ROBOMERGE-SOURCE: CL 13541649 in //UE4/Release-4.25/... via CL 13541651 via CL 13541652 via CL 13541657
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v698-13502450)
    
    [CL 13542319 by ethan geller in Dev-Networking branch]

--- Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAudioContextManager.h
+++ Engine/Plugins/Runtime/Oculus/OculusAudio/Source/OculusAudio/Private/OculusAudioContextManager.h
@@ -19,17 +19,22 @@ public:
 
 	// Returns an ovrAudioContext for the given audio device, or nullptr if one does not exist.
 	static ovrAudioContext GetContextForAudioDevice(const FAudioDevice* InAudioDevice);
+	static ovrAudioContext GetContextForAudioDevice(Audio::FDeviceId InAudioDeviceId);
 
 	// Creates a new ovrAudioContext for a given audio device.
 	// the InAudioDevice ptr is no longer used for anything besides looking up contexts after this call is completed.
 	static ovrAudioContext CreateContextForAudioDevice(FAudioDevice* InAudioDevice);
+	static ovrAudioContext CreateContextForAudioDevice(Audio::FDeviceId InAudioDeviceId, int32 InBufferLength, int32 InMaxNumSources, float InSampleRate);
+
 	static void DestroyContextForAudioDevice(const FAudioDevice* InAudioDevice);
+	static void DestroyContextForAudioDevice(Audio::FDeviceId InAudioDeviceId);
+
 
 private:
 	// FIXME: can we do something better than global static variables?
 	static ovrAudioContext SerializationContext;
 	static UActorComponent* SerializationParent;
 
-	static TMap<const FAudioDevice*, ovrAudioContext> ContextMap;
+	static TMap<Audio::FDeviceId, ovrAudioContext> ContextMap;
 	static FCriticalSection ContextCritSection;
 };
