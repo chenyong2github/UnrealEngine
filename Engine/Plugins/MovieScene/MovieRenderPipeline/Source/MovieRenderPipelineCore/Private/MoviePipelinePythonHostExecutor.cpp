06ef539a46fad0c8544f85d6a6cacefeeb13e466
matt hoffman <Matt.Hoffman@epicgames.com>
2020-05-26 19:09:16 -0400

    MoviePipeline: Initial changes to Movie Pipeline and Movie Pipeline Executors to allow scripting implementations of Executors in Python or Blueprints. This exposes many functions to be blueprint callable. This adds a new "Python Host" class which will take certain native events and forward them onto the implementation. The Executor is now not executed until two frames after the map loads which gives Python enough time to initialize classes (so that you can define an Executor in Python).
    #rb Max.Chen
    #jira UE-90669
    #[fyi] Joji.Tsuruga
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: matt.hoffman
    #ROBOMERGE-SOURCE: CL 13550577 via CL 13550583 via CL 13550589 via CL 13550600
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v700-13537989)
    
    [CL 13552343 by matt hoffman in Dev-Networking branch]

---
+++ Engine/Plugins/MovieScene/MovieRenderPipeline/Source/MovieRenderPipelineCore/Private/MoviePipelinePythonHostExecutor.cpp
@@ -0,0 +1,94 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoviePipelinePythonHostExecutor.h"
+#include "Misc/CoreDelegates.h"
+#include "SocketSubsystem.h"
+#include "Sockets.h"
+#include "Interfaces/Ipv4/IPv4Address.h"
+#include "Engine/Engine.h"
+
+void UMoviePipelinePythonHostExecutor::Execute_Implementation(UMoviePipelineQueue* InPipelineQueue)
+{
+	for (const FWorldContext& WorldContext : GEngine->GetWorldContexts())
+	{
+		if (WorldContext.WorldType == EWorldType::Game)
+		{
+			LastLoadedWorld = WorldContext.World();
+		}
+	}
+
+	PipelineQueue = InPipelineQueue;
+	
+	// Register C++ only callbacks that we will forward onto BP/Python
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UMoviePipelinePythonHostExecutor::OnMapLoadFinished);
+	FCoreDelegates::OnBeginFrame.AddUObject(this, &UMoviePipelinePythonHostExecutor::OnBeginFrame);
+
+	// Attempt a socket connection so we can recieve external messages.
+	UE_LOG(LogMovieRenderPipeline, Log, TEXT("Attempting Socket connection to host..."));
+
+	ExternalSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("MoviePipelineExternalSocket"), false);
+
+	// Port needs to match in your external connection.
+	TSharedRef<FInternetAddr> Address = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+	bool bIsValid = false;
+	Address->SetIp(TEXT("127.0.0.1"), bIsValid);
+	Address->SetPort(6783);
+
+	// Attempt a connection.
+	bSocketConnected = ExternalSocket->Connect(*Address);
+
+	UE_LOG(LogMovieRenderPipeline, Log, TEXT("Socket Connected: %d"), bSocketConnected);
+
+	// Now that we've done some C++ only things, call the Python version of this.
+	ExecuteDelayed(InPipelineQueue);
+}
+	
+void UMoviePipelinePythonHostExecutor::OnMapLoadFinished(UWorld* NewWorld)
+{
+	LastLoadedWorld = NewWorld;
+
+	// This executor is only created after the world is loaded
+	OnMapLoad(NewWorld);
+}
+	
+void UMoviePipelinePythonHostExecutor::OnBeginFrame()
+{
+	if (ExternalSocket)
+	{
+		uint32 PendingDataSize = 0;
+		while (ExternalSocket->HasPendingData(PendingDataSize) && PendingDataSize > 0)
+		{
+			TArray<uint8> ReceivedData;
+			ReceivedData.SetNumZeroed(PendingDataSize + 1);
+
+			int32 BytesRead = 0;
+			if (ExternalSocket->Recv(ReceivedData.GetData(), ReceivedData.Num(), BytesRead) && BytesRead > 0)
+			{
+				ReceivedData.Last() = 0; // Ensure null terminator
+
+				// Convert the message to a string
+				const TCHAR* MessageData = UTF8_TO_TCHAR((char*)ReceivedData.GetData());
+				FString Message = FString(MessageData);
+
+				OnSocketMessageRecieved.Broadcast(Message);
+			}
+		}
+
+	}
+	
+	OnTick();
+}
+	
+void UMoviePipelinePythonHostExecutor::SendSocketMessage(const FString& InMessage)
+{
+	if(!bSocketConnected || !ExternalSocket)
+	{
+		UE_LOG(LogMovieRenderPipeline, Warning, TEXT("Attempted to send message but no socket connected, ignoring..."));
+		return;
+	}
+	
+	const char* UTF8EncodedMessage = TCHAR_TO_UTF8(*InMessage);
+
+	int32 BytesSent = 0;
+	bool bSuccess =	ExternalSocket->Send((uint8*)UTF8EncodedMessage, FCStringAnsi::Strlen(UTF8EncodedMessage), BytesSent);
+}
