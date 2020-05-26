83f11fe3231ac6e7aee7b2a9bbf72618a2c9377e
lauren barnes <lauren.barnes@epicgames.com>
2020-05-26 14:53:57 -0400

    Check that there is a valid model component before modifying it #jira UE-92946 #rb Chris.Gagnon #lockdown Simon.Tourangeau
    #ROBOMERGE-SOURCE: CL 13548772 in //UE4/Release-4.25/... via CL 13548774
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v700-13537989)
    
    [CL 13548821 by lauren barnes in Release-Engine-Staging branch]

--- Engine/Source/Editor/UnrealEd/Private/LevelEditorViewport.cpp
+++ Engine/Source/Editor/UnrealEd/Private/LevelEditorViewport.cpp
@@ -874,20 +874,20 @@ bool FLevelEditorViewportClient::AttemptApplyObjAsMaterialToSurface( UObject* Ob
 		const int32 DropY = Cursor.GetCursorPos().Y;
 
 		{
-		uint32 SurfaceIndex;
-		ModelHitProxy->ResolveSurface(View, DropX, DropY, SurfaceIndex);
-		if (SurfaceIndex != INDEX_NONE)
-		{
-			if ((Model->Surfs[SurfaceIndex].PolyFlags & PF_Selected) == 0)
+			uint32 SurfaceIndex;
+			ModelHitProxy->ResolveSurface(View, DropX, DropY, SurfaceIndex);
+			if (SurfaceIndex != INDEX_NONE)
 			{
-				// Surface was not selected so only apply to this surface
-				SelectedSurfaces.Add(SurfaceIndex);
+				if ((Model->Surfs[SurfaceIndex].PolyFlags & PF_Selected) == 0)
+				{
+					// Surface was not selected so only apply to this surface
+					SelectedSurfaces.Add(SurfaceIndex);
+				}
+				else
+				{
+					bDropedOntoSelectedSurface = true;
+				}
 			}
-			else
-			{
-				bDropedOntoSelectedSurface = true;
-			}
-		}
 		}
 
 		if( bDropedOntoSelectedSurface )
@@ -909,8 +909,11 @@ bool FLevelEditorViewportClient::AttemptApplyObjAsMaterialToSurface( UObject* Ob
 			// Apply the material to the specified surface
 			FScopedTransaction Transaction(NSLOCTEXT("UnrealEd", "DragDrop_Transaction_ApplyMaterialToSurface", "Apply Material to Surface"));
 
-			// Modify the component so that PostEditUndo can reregister the model after undo
-			ModelHitProxy->GetModelComponent()->Modify();
+			if (ModelHitProxy->GetModelComponent())
+			{
+				// Modify the component so that PostEditUndo can reregister the model after undo
+				ModelHitProxy->GetModelComponent()->Modify();
+			}
 
 			for( int32 SurfListIndex = 0; SurfListIndex < SelectedSurfaces.Num(); ++SurfListIndex )
 			{
