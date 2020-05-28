dfc44ff97cb9f6a182d9d299240aa2aa2b928a1e
tim tillotson <Tim.Tillotson@epicgames.com>
2020-05-27 20:14:46 -0400

    Fix up linux build error. In file included from /Engine/Intermediate/Build/Linux/B4D820EA/UE4Editor/Development/ScreenShotComparisonTools/Module.ScreenShotComparisonTools.cpp:3: \Engine\Source\Developer\ScreenShotComparisonTools\Private\ScreenshotComparisonSettings.cpp(4,10): erro r: non-portable path to file '"ScreenShotComparisonSettings.h"'; specified path differs in case from file name on disk [-Werror,-Wnonportable-include-path]   #include "ScreenshotComparisonSettings.h"            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [FYI] Andrew.Grant
    #ROBOMERGE-SOURCE: CL 13564940 via CL 13564941 via CL 13564944
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v701-13556912)
    
    [CL 13564945 by tim tillotson in Main branch]

--- Engine/Source/Developer/ScreenShotComparisonTools/Private/ScreenshotComparisonSettings.cpp
+++ Engine/Source/Developer/ScreenShotComparisonTools/Private/ScreenshotComparisonSettings.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 
-#include "ScreenshotComparisonSettings.h"
+#include "ScreenShotComparisonSettings.h"
 
 
 UScreenShotComparisonSettings::UScreenShotComparisonSettings(const FObjectInitializer& ObjectInitializer)
