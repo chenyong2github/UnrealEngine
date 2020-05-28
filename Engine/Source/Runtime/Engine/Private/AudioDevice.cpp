59a62b20e5a00ee5fc3a9aea4d015309770910f2
aaron mcleran <Aaron.McLeran@epicgames.com>
2020-05-28 16:59:44 -0400

    Deescalate logs for flushing the audio render thread command list and make sure we have a CVar to disable flushing the audio render thread command list on GC.
    [REVIEW] [at]ethan.geller
    #rb Ethan.Geller
    
    #localization none
    #tests ran code in QAGame
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: aaron.mcleran
    #ROBOMERGE-SOURCE: CL 13572435 via CL 13572441 via CL 13572443 via CL 13572450 via CL 13572455 via CL 13572456
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v701-13556912)
    
    [CL 13575121 by aaron mcleran in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
+++ Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
@@ -163,6 +163,13 @@ FAutoConsoleVariableRef CVarDisableBinauralSpatialization(
 	TEXT("Disables binaural spatialization.\n"),
 	ECVF_Default);
 
+static int32 FlushAudioRenderThreadOnGCCVar = 0;
+FAutoConsoleVariableRef CVarFlushAudioRenderThreadOnGC(
+	TEXT("au.FlushAudioRenderThreadOnGC"),
+	FlushAudioRenderThreadOnGCCVar,
+	TEXT("When set to 1, every time the GC runs, we flush all pending audio render thread commands.\n"),
+	ECVF_Default);
+
 namespace
 {
 	using FVirtualLoopPair = TPair<FActiveSound*, FAudioVirtualLoop>;
@@ -540,7 +547,10 @@ bool FAudioDevice::Init(Audio::FDeviceId InDeviceID, int32 InMaxSources)
 
 void FAudioDevice::OnPreGarbageCollect()
 {
-	FlushAudioRenderingCommands();
+	if (FlushAudioRenderThreadOnGCCVar)
+	{
+		FlushAudioRenderingCommands();
+	}
 }
 
 float FAudioDevice::GetLowPassFilterResonance() const
