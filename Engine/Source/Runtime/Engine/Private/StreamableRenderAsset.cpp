6257c45baee5610fea1b56ca6dae7aad70046813
uriel doyon <Uriel.Doyon@epicgames.com>
2020-06-01 19:32:27 -0400

    Fixed potential invalid array access in UStreamableRenderAsset::SetForceMipLevelsToBeResident #rb jian.ru
    #ROBOMERGE-SOURCE: CL 13593027 via CL 13593029 via CL 13593034 via CL 13593045 via CL 13593079
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v703-13590297)
    
    [CL 13593082 by uriel doyon in Main branch]

--- Engine/Source/Runtime/Engine/Private/StreamableRenderAsset.cpp
+++ Engine/Source/Runtime/Engine/Private/StreamableRenderAsset.cpp
@@ -11,9 +11,16 @@ UStreamableRenderAsset::UStreamableRenderAsset(const FObjectInitializer& ObjectI
 
 void UStreamableRenderAsset::SetForceMipLevelsToBeResident(float Seconds, int32 CinematicLODGroupMask)
 {
-	uint32 TextureGroupBitfield = (uint32)CinematicLODGroupMask;
-	int32 LODGroup = GetLODGroupForStreaming();
-	uint32 MyTextureGroup = FMath::BitFlag[LODGroup];
-	bUseCinematicMipLevels = (TextureGroupBitfield & MyTextureGroup) ? true : false;
+	const int32 LODGroup = GetLODGroupForStreaming();
+	if (CinematicLODGroupMask && LODGroup >= 0 && LODGroup < UE_ARRAY_COUNT(FMath::BitFlag))
+	{
+		const uint32 TextureGroupBitfield = (uint32)CinematicLODGroupMask;
+		bUseCinematicMipLevels = !!(TextureGroupBitfield & FMath::BitFlag[LODGroup]);
+	}
+	else
+	{
+		bUseCinematicMipLevels = false;
+	}
+
 	ForceMipLevelsToBeResidentTimestamp = FApp::GetCurrentTime() + Seconds;
 }
