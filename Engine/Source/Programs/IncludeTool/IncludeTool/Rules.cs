15a01dc2ea2bb1797e075a19e63ad06be31d0475
devin doucette <devin.doucette@epicgames.com>
2020-06-02 17:14:28 -0400

    Ignored ContainersFwd.h and StringFwd.h as forward declaration headers in IncludeTool to avoid errors
    #rb trivial (discussed with Marc.Audy on Slack)
    #rnx
    
    #ROBOMERGE-SOURCE: CL 13600879 via CL 13600881 via CL 13600889
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v703-13590297)
    
    [CL 13600906 by devin doucette in Main branch]

--- Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
+++ Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
@@ -416,6 +416,8 @@ namespace IncludeTool
 			"/Engine/Source/Runtime/SlateCore/Public/Fonts/ShapedTextFwd.h", // Typedef isn't a forward declaration
 			"/Engine/Source/Runtime/Slate/Public/Framework/Text/ShapedTextCacheFwd.h", // Typedef isn't a forward declaration
 			"/Engine/Source/Runtime/MovieScene/Public/MovieSceneFwd.h",
+			"/Engine/Source/Runtime/Core/Public/Containers/ContainersFwd.h", // invalid forward declaration - 'template<> struct TIsContiguousContainer<Type> { static constexpr bool Value = true; };'
+			"/Engine/Source/Runtime/Core/Public/Containers/StringFwd.h", // invalid forward declaration - 'template<> struct TIsContiguousContainer<Type> { static constexpr bool Value = true; };'
 			"/Engine/Source/Runtime/Core/Public/Internationalization/StringTableCoreFwd.h", // Typedef isn't a forward declaration
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ParticleHandleFwd.h", // invalid forward declaration - 'namespace Chaos'
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/GeometryParticlesfwd.h", // invalid forward declaration - 'namespace Chaos'
