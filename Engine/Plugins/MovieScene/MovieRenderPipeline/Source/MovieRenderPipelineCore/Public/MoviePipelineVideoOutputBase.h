94985265c862e3707537c7515d609884a5e12bba
matt hoffman <Matt.Hoffman@epicgames.com>
2020-06-03 14:47:46 -0400

    MoviePipeline: Ported the existing AvidDNx and AppleProRes encoders to the new Movie Render Pipeline to enable video container writing capabilities in the new system. This implements a generic AvidDNxEncoder and a generic AppleProResEncoder which are not tied to the MoviePipeline system which will allow them to easily be reused by other systems in the future. They need to be initialized with some options and then you can pass them image data to write frames before finalizing them to save the file.
    #jira UE-93125
    #rb Rod.Bogart
    
    
    #ROBOMERGE-SOURCE: CL 13610383 via CL 13610386
    #ROBOMERGE-BOT: (v703-13590297)
    
    [CL 13610391 by matt hoffman in Release-Engine-Staging branch]

---
+++ Engine/Plugins/MovieScene/MovieRenderPipeline/Source/MovieRenderPipelineCore/Public/MoviePipelineVideoOutputBase.h
@@ -0,0 +1,82 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "MoviePipelineOutputBase.h"
+#include "ImagePixelData.h"
+#include "Async/AsyncWork.h"
+#include "Async/TaskGraphInterfaces.h"
+#include "Templates/Function.h"
+#include "Stats/Stats.h"
+#include "MoviePipelineVideoOutputBase.generated.h"
+
+namespace MovieRenderPipeline
+{
+	struct IVideoCodecWriter
+	{
+		FString FileName;
+	};
+}
+
+class FMoviePipelineBackgroundMediaTasks
+{
+private:
+	FGraphEventRef LastCompletionEvent;
+
+public:
+	FGraphEventRef Execute(TUniqueFunction<void()> InFunctor)
+	{
+		if (LastCompletionEvent)
+		{
+			LastCompletionEvent = FFunctionGraphTask::CreateAndDispatchWhenReady(MoveTemp(InFunctor), GetStatId(), LastCompletionEvent);
+		}
+		else
+		{
+			LastCompletionEvent = FFunctionGraphTask::CreateAndDispatchWhenReady(MoveTemp(InFunctor), GetStatId());
+		}
+		return LastCompletionEvent;
+	}
+
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FMoviePipelineBackgroundMediaTasks, STATGROUP_ThreadPoolAsyncTasks);
+	}
+};
+
+/**
+* A base class for video codec outputs for the Movie Pipeline system. To simplify encoder implementations
+* this handles multi-threading for you and will call all of the encoding functions on a dedicated thread.
+* This allows an encoder to do more expensive operations (such as image quantization) without implementing
+* threading yourself, nor having to worry about blocking the game thread.
+*/
+UCLASS(Blueprintable, Abstract)
+class MOVIERENDERPIPELINECORE_API UMoviePipelineVideoOutputBase : public UMoviePipelineOutputBase
+{
+	GENERATED_BODY()
+
+protected:
+
+
+protected:
+	// UMoviePipelineOutputBase Interface
+	virtual void OnRecieveImageDataImpl(FMoviePipelineMergerOutputFrame* InMergedOutputFrame) override;
+	virtual bool HasFinishedProcessingImpl() override;
+	virtual void BeginFinalizeImpl() override;
+	virtual void FinalizeImpl() override;
+	virtual FText GetFooterText(UMoviePipelineExecutorJob* InJob) const override;
+	// ~UMoviePipelineOutputBase Interface
+
+	// UMoviePipelineVideoOutputBase Interface
+	virtual TUniquePtr<MovieRenderPipeline::IVideoCodecWriter> Initialize_GameThread(const FString& InFileName, FIntPoint InResolution, EImagePixelType InPixelType, ERGBFormat InPixelFormat, uint8 InBitDepth, uint8 InNumChannels)  PURE_VIRTUAL(UMoviePipelineVideoOutputBase::Initialize_GameThread, return nullptr; );
+	virtual void Initialize_EncodeThread(MovieRenderPipeline::IVideoCodecWriter* InWriter) PURE_VIRTUAL(UMoviePipelineVideoOutputBase::Initialize_EncodeThread);
+	virtual void WriteFrame_EncodeThread(MovieRenderPipeline::IVideoCodecWriter* InWriter, FImagePixelData* InPixelData) PURE_VIRTUAL(UMoviePipelineVideoOutputBase::WriteFrame_EncodeThread);
+	virtual void BeginFinalize_EncodeThread(MovieRenderPipeline::IVideoCodecWriter* InWriter) PURE_VIRTUAL(UMoviePipelineVideoOutputBase::BeginFinalize_EncodeThread);
+	virtual void Finalize_EncodeThread(MovieRenderPipeline::IVideoCodecWriter* InWriter) PURE_VIRTUAL(UMoviePipelineVideoOutputBase::Finalize_EncodeThread);
+	virtual const TCHAR* GetFilenameExtension() const PURE_VIRTUAL(UMoviePipelineVideoOutputBase::GetFilenameExtension, return TEXT(""););
+	virtual bool IsAudioSupported() const PURE_VIRTUAL(UMoviePipelineVideoOutputBase::IsAudioSupported, return false;);
+	// ~UMoviePipelineVideoOutputBase Interface
+
+private:
+	TArray<TUniquePtr<MovieRenderPipeline::IVideoCodecWriter>> AllWriters;
+
+	FGraphEventArray OutstandingTasks;
+};
