ca67da789a10f091055028d158c56871f99c7121
james hopkin <James.Hopkin@epicgames.com>
2020-06-08 09:19:44 -0400

    #robo Added copyright message and made route absolute for simplicity
    #ROBOMERGE-SOURCE: CL 13641880
    #ROBOMERGE-BOT: (v707-13641620)
    
    [CL 13641883 by james hopkin in Main branch]

--- Engine/Extras/RoboMerge/v3/public/js/preview.js
+++ Engine/Extras/RoboMerge/v3/public/js/preview.js
@@ -1,5 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+"use strict"
+
 function doit(cl, bot) {
-	$.get(`../../preview?cl=${cl}&bot=${bot}`)
+	$.get(`/preview?cl=${cl}&bot=${bot}`)
 	.then(data => {
 		$('#graph').append(showFlowGraph(JSON.parse(data).allBranches, bot.toUpperCase()));
 		$('#success-panel').show();
@@ -10,7 +13,6 @@ function doit(cl, bot) {
 		$('pre', $errorPanel).text(error.responseText.replace(/\t/g, '    '));
 		$errorPanel.show();
 	});
-
 }
 
 window.doPreview = doit;
