6b55433210f0ce565b84502cdb4402420bf1bfd8
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-06-09 16:46:19 -0400

    ModelingComponents: add utility functions to LineSetComponent to mass-update line thickness/length, and to add a set of lines via iterating over a lambda. Add UPreviewGeometry object, similar to UPreviewMesh, internally it creates an APreviewGeometryActor and then creates and manages a set of named  LineSetComponents which can be updated/managed via the string names. Point and Triangle sets will be added in future. #rb none #rnx [FYI] semion.piskarev
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 13652583 via CL 13652585 via CL 13652589 via CL 13652590
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v707-13641620)
    
    [CL 13653997 by ryan schmidt in Dev-Networking branch]

--- Engine/Plugins/Experimental/MeshModelingToolset/Source/ModelingComponents/Public/Drawing/PreviewGeometryActor.h
+++ Engine/Plugins/Experimental/MeshModelingToolset/Source/ModelingComponents/Public/Drawing/PreviewGeometryActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "InteractiveToolObjects.h"
+#include "Drawing/LineSetComponent.h"
 
 #include "PreviewGeometryActor.generated.h"
 
@@ -24,4 +25,89 @@ private:
 	}
 
 public:
+};
+
+
+/**
+ * UPreviewGeometry creates and manages an APreviewGeometryActor and a set of preview geometry Components.
+ * Preview geometry Components are identified by strings.
+ */
+UCLASS(Transient)
+class MODELINGCOMPONENTS_API UPreviewGeometry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual ~UPreviewGeometry();
+
+	/**
+	 * Create preview mesh in the World with the given transform
+	 */
+	UFUNCTION()
+	void CreateInWorld(UWorld* World, const FTransform& WithTransform);
+
+	/**
+	 * Remove and destroy preview mesh
+	 */
+	UFUNCTION()
+	void Disconnect();
+
+	/**
+	 *
+	 */
+	UFUNCTION()
+	APreviewGeometryActor* GetActor() const { return ParentActor;  }
+
+
+
+	//
+	// Line Sets
+	//
+	UFUNCTION()
+	ULineSetComponent* AddLineSet(const FString& LineSetIdentifier);
+
+	UFUNCTION()
+	ULineSetComponent* FindLineSet(const FString& LineSetIdentifier);
+
+	UFUNCTION()
+	bool RemoveLineSet(const FString& LineSetIdentifier, bool bDestroy = true);
+
+	UFUNCTION()
+	bool SetLineSetVisibility(const FString& LineSetIdentifier, bool bVisible);
+
+
+	//
+	// Line Set Utilities
+	//
+
+	/**
+	 * Find the identified line set and call UpdateFuncType(ULineSetComponent*)
+	 */
+	template<typename UpdateFuncType>
+	void UpdateLineSet(const FString& LineSetIdentifier, UpdateFuncType UpdateFunc)
+	{
+		ULineSetComponent* LineSet = FindLineSet(LineSetIdentifier);
+		if (LineSet)
+		{
+			UpdateFunc(LineSet);
+		}
+	}
+
+	/**
+	 * Add a set of lines produced by calling LineGenFunc for each index in range [0,NumIndices)
+	 */
+	void CreateOrUpdateLineSet(const FString& LineSetIdentifier, int32 NumIndices,
+		TFunctionRef<void(int32 Index, TArray<FRenderableLine>& LinesOut)> LineGenFunc,
+		int32 LinesPerIndexHint = -1);
+
+
+public:
+
+	/**  */
+	UPROPERTY()
+	APreviewGeometryActor* ParentActor = nullptr;
+
+	UPROPERTY()
+	TMap<FString, ULineSetComponent*> LineSets;
+
 };
