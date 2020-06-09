87337e5004d3c7abeb6922fbb07e6ec577571f4d
martin ridgers <Martin.Ridgers@epicgames.com>
2020-06-09 08:53:23 -0400

    Always process Trace's "-trace=" command line argument regardless of if something is going to receive trace data. Something might receive trace data at a later date.
    #jira UE-94119
    #rb johan.berg
    
    
    #ROBOMERGE-SOURCE: CL 13650746 via CL 13650747
    #ROBOMERGE-BOT: (v707-13641620)
    
    [CL 13650751 by martin ridgers in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Core/Private/ProfilingDebugging/TraceAuxiliary.cpp
+++ Engine/Source/Runtime/Core/Private/ProfilingDebugging/TraceAuxiliary.cpp
@@ -129,12 +129,6 @@ void FTraceAuxiliaryImpl::ParseCommandLine(const TCHAR* CommandLine)
 		bOk = WriteToFile();
 	}
 
-	if (!bOk)
-	{
-		State = EState::None;
-		return;
-	}
-
 	const TCHAR* ChannelSet = nullptr;
 	if (FParse::Value(CommandLine, TEXT("-trace="), Parameter, false))
 	{
@@ -144,7 +138,7 @@ void FTraceAuxiliaryImpl::ParseCommandLine(const TCHAR* CommandLine)
 	FString Channels = GetChannels(ChannelSet);
 	ToggleChannels(*Channels);
 
-	State = EState::Tracing;
+	State = bOk ? EState::Tracing : EState::None;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
