5c0dd5e0c2b426462846164c6c2e74adee234c57
bob tellez <Bob.Tellez@epicgames.com>
2020-06-10 16:31:06 -0400

    Back out changelist 13658276. CIS errors
    [FYI] Michael.Forot
    
    
    #ROBOMERGE-SOURCE: CL 13658507 via CL 13658509
    #ROBOMERGE-BOT: (v707-13641620)
    
    [CL 13658515 by bob tellez in Release-Engine-Staging branch]

--- Engine/Plugins/Experimental/HairStrands/Source/HairStrandsNiagara/Private/NiagaraDataInterfacePhysicsAsset.cpp
+++ Engine/Plugins/Experimental/HairStrands/Source/HairStrandsNiagara/Private/NiagaraDataInterfacePhysicsAsset.cpp
@@ -132,7 +132,10 @@ void CreateInternalArrays(const TWeakObjectPtr<UPhysicsAsset> PhysicsAsset, cons
 				TArray<FTransform> RestTransforms;
 				FAnimationRuntime::FillUpComponentSpaceTransforms(*RefSkeleton, RefSkeleton->GetRefBonePose(), RestTransforms);
 
-				if (RestTransforms.Num() > 0)
+				TArray<FTransform> BoneTransforms = (SkeletalMesh != nullptr) ? SkeletalMesh->GetComponentSpaceTransforms() : RestTransforms;
+				const bool bHasMasterPoseComponent = (SkeletalMesh != nullptr) && SkeletalMesh->MasterPoseComponent.IsValid();
+
+				if (BoneTransforms.Num() == RestTransforms.Num())
 				{
 					uint32 NumBoxes = 0;
 					uint32 NumSpheres = 0;
@@ -141,7 +144,7 @@ void CreateInternalArrays(const TWeakObjectPtr<UPhysicsAsset> PhysicsAsset, cons
 					{
 						const FName BoneName = BodySetup->BoneName;
 						const int32 BoneIndex = RefSkeleton->FindBoneIndex(BoneName);
-						if (BoneIndex != INDEX_NONE && BoneIndex < RestTransforms.Num())
+						if (BoneIndex != INDEX_NONE && BoneIndex < BoneTransforms.Num())
 						{
 							NumBoxes += BodySetup->AggGeom.BoxElems.Num();
 							NumSpheres += BodySetup->AggGeom.SphereElems.Num();
@@ -170,10 +173,10 @@ void CreateInternalArrays(const TWeakObjectPtr<UPhysicsAsset> PhysicsAsset, cons
 					{
 						const FName BoneName = BodySetup->BoneName;
 						const int32 BoneIndex = RefSkeleton->FindBoneIndex(BoneName);
-						if (BoneIndex != INDEX_NONE && BoneIndex < RestTransforms.Num())
+						if (BoneIndex != INDEX_NONE && BoneIndex < BoneTransforms.Num())
 						{
 							const FTransform RestTransform = RestTransforms[BoneIndex];
-							const FTransform BoneTransform = (SkeletalMesh != nullptr) ? SkeletalMesh->GetBoneTransform(BoneIndex) : RestTransform * WorldTransform;
+							const FTransform BoneTransform = bHasMasterPoseComponent ? SkeletalMesh->GetBoneTransform(BoneIndex) : BoneTransforms[BoneIndex] * WorldTransform;
 
 							for (const FKBoxElem& BoxElem : BodySetup->AggGeom.BoxElems)
 							{
@@ -225,20 +228,28 @@ void UpdateInternalArrays(const TWeakObjectPtr<UPhysicsAsset> PhysicsAsset, cons
 		const FReferenceSkeleton* RefSkeleton = &PhysicsAsset->GetPreviewMesh()->RefSkeleton;
 		if (RefSkeleton != nullptr)
 		{
-			TArray<FTransform> RestTransforms;
-			FAnimationRuntime::FillUpComponentSpaceTransforms(*RefSkeleton, RefSkeleton->GetRefBonePose(), RestTransforms);
+			TArray<FTransform> BoneTransforms;
+			if (SkeletalMesh != nullptr)
+			{
+				BoneTransforms = SkeletalMesh->GetComponentSpaceTransforms();
+			}
+			else
+			{
+				FAnimationRuntime::FillUpComponentSpaceTransforms(*RefSkeleton, RefSkeleton->GetRefBonePose(), BoneTransforms);
+			}
+			const bool bHasMasterPoseComponent = (SkeletalMesh != nullptr) && SkeletalMesh->MasterPoseComponent.IsValid();
 
 			OutAssetArrays->PreviousTransform = OutAssetArrays->CurrentTransform;
-			OutAssetArrays->PreviousInverse = OutAssetArrays->InverseTransform;
+			OutAssetArrays->PreviousInverse= OutAssetArrays->InverseTransform;
 
 			uint32 ElementCount = 0;
 			for (const UBodySetup* BodySetup : PhysicsAsset->SkeletalBodySetups)
 			{
 				const FName BoneName = BodySetup->BoneName;
 				const int32 BoneIndex = RefSkeleton->FindBoneIndex(BoneName);
-				if (BoneIndex != INDEX_NONE && BoneIndex < RestTransforms.Num())
+				if (BoneIndex != INDEX_NONE && BoneIndex < BoneTransforms.Num() )
 				{
-					const FTransform BoneTransform = (SkeletalMesh != nullptr) ? SkeletalMesh->GetBoneTransform(BoneIndex) : RestTransforms[BoneIndex] * WorldTransform;
+					const FTransform BoneTransform = bHasMasterPoseComponent ? SkeletalMesh->GetBoneTransform(BoneIndex) : BoneTransforms[BoneIndex] * WorldTransform;
 
 					for (const FKBoxElem& BoxElem : BodySetup->AggGeom.BoxElems)
 					{
@@ -315,8 +326,8 @@ void FNDIPhysicsAssetBuffer::InitRHI()
 		CreateInternalBuffer<FVector4, FVector4, 1, EPixelFormat::PF_A32B32G32R32F, true>(AssetArrays->ElementExtent.Num(), AssetArrays->ElementExtent, ElementExtentBuffer);
 		CreateInternalBuffer<FVector4, FVector4, 1, EPixelFormat::PF_A32B32G32R32F, true>(AssetArrays->PreviousInverse.Num(), AssetArrays->PreviousInverse, PreviousInverseBuffer);
 
-		//UE_LOG(LogPhysicsAsset, Warning, TEXT("Num Capsules = %d | Num Spheres = %d | Num Boxes = %d"), AssetArrays->ElementOffsets.NumElements - AssetArrays->ElementOffsets.CapsuleOffset,
-		//	AssetArrays->ElementOffsets.CapsuleOffset - AssetArrays->ElementOffsets.SphereOffset, AssetArrays->ElementOffsets.SphereOffset - AssetArrays->ElementOffsets.BoxOffset);
+		UE_LOG(LogPhysicsAsset, Warning, TEXT("Num Capsules = %d | Num Spheres = %d | Num Boxes = %d"), AssetArrays->ElementOffsets.NumElements - AssetArrays->ElementOffsets.CapsuleOffset,
+			AssetArrays->ElementOffsets.CapsuleOffset - AssetArrays->ElementOffsets.SphereOffset, AssetArrays->ElementOffsets.SphereOffset - AssetArrays->ElementOffsets.BoxOffset);
 	}
 }
 
