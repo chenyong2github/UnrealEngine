67c90c5fc79073e51b8f99c42eb4aefca9db75d4
wei liu <Wei.Liu@epicgames.com>
2020-06-11 10:24:26 -0400

    Fix a bug of PreExposure that doesn't work for mobile LDR.
    #jira none
    
    #rb Dmitriy.Dyomin, Jack.Porter, Mi.Wang
    
    #ROBOMERGE-SOURCE: CL 13665465 in //UE4/Release-4.25/... via CL 13665468
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v707-13641620)
    
    [CL 13665470 by wei liu in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessEyeAdaptation.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessEyeAdaptation.cpp
@@ -70,7 +70,7 @@ static TAutoConsoleVariable<int32> CVarEnablePreExposureOnlyInTheEditor(
 	TEXT("When pre-exposure is enabled, 0 to enable it everywhere, 1 to enable it only in the editor (default).\n")
 	TEXT("This is to because it currently has an impact on the renderthread performance\n"),
 	ECVF_ReadOnly);
-const ERHIFeatureLevel::Type BasicEyeAdaptationMinFeatureLevel = ERHIFeatureLevel::SM5;
+const ERHIFeatureLevel::Type BasicEyeAdaptationMinFeatureLevel = ERHIFeatureLevel::ES3_1;
 }
 
 // Function is static because EyeAdaptation is the only system that should be checking this value.
@@ -896,6 +896,7 @@ void FSceneViewState::UpdatePreExposure(FViewInfo& View)
 	if (IsMobilePlatform(View.GetShaderPlatform()))
 	{
 		bEnableAutoExposure &= IsMobileEyeAdaptationEnabled(View);
+		PreExposure = GetEyeAdaptationFixedExposure(View);
 	}
 	
 	if (bIsPreExposureRelevant && bEnableAutoExposure)
