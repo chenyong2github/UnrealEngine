7fdaf5171340c8b8b252963ce23e4b60c6f78050
juan canada <Juan.Canada@epicgames.com>
2020-06-12 12:27:33 -0400

    Added some checks for early detection of issues when gathering ray tracing instances
    #rb yujiang.wang
    
    #ROBOMERGE-SOURCE: CL 13670173 via CL 13670176 via CL 13670179
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v707-13641620)
    
    [CL 13670181 by juan canada in Main branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
@@ -2715,6 +2715,12 @@ void FLandscapeComponentSceneProxy::GetDynamicRayTracingInstances(FRayTracingMat
 			BatchElement.PrimitiveUniformBuffer = GetUniformBuffer();
 
 			int32 LodSubsectionSizeVerts = SubsectionSizeVerts >> CurrentLOD;
+
+			if (LodSubsectionSizeVerts <= 0)
+			{
+				continue;
+			}
+
 			uint32 NumPrimitives = FMath::Square(LodSubsectionSizeVerts - 1) * 2;
 
 			BatchElement.IndexBuffer = SharedBuffers->ZeroOffsetIndexBuffers[CurrentLOD];
