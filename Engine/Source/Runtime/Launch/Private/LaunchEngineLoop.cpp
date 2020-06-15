0370bf69731d1a4adc914138bae488b5cfddd4a3
eric mcdaniel <Eric.Mcdaniel@epicgames.com>
2020-06-15 13:11:32 -0400

    Indentation fix to properly align a code block
    - no op
    - whitespace only change
    
    #rb none, trivial
    #jira none
    #rnx
    
    #ROBOMERGE-SOURCE: CL 13677921 in //UE4/Release-4.25Plus/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v707-13641620)
    
    [CL 13678272 by eric mcdaniel in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
+++ Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
@@ -4754,12 +4754,12 @@ void FEngineLoop::Tick()
 #endif //WITH_EDITOR
 
 			if( WorldToScale != nullptr )
-		{
-				if( GNewWorldToMetersScale != WorldToScale->GetWorldSettings()->WorldToMeters )
 			{
+				if( GNewWorldToMetersScale != WorldToScale->GetWorldSettings()->WorldToMeters )
+				{
 					WorldToScale->GetWorldSettings()->WorldToMeters = GNewWorldToMetersScale;
+				}
 			}
-		}
 
 			GNewWorldToMetersScale = 0.0f;
 		}
