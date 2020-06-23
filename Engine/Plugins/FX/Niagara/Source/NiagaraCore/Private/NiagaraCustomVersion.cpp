4c3714a0fb4eee34c5ce8e5eee4e64d74a71f05e
shaun kime <Shaun.Kime@epicgames.com>
2020-06-23 13:42:28 -0400

    Back out changelist 13749980. Submitted too early accidentally.
    #ROBOMERGE-SOURCE: CL 13750025 via CL 13750034 via CL 13750037 via CL 13750042 via CL 13750053
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v708-13641880)
    
    [CL 13750059 by shaun kime in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x7094905B, 0xE6104248, 0xAB355BF1, 0x1D630275);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x25B64B58, 0x04B24E19, 0x8586C7C5, 0x6A5777E1);
