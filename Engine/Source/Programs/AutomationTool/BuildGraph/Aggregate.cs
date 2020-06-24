7e81fb3f9cd9bf45eedbcdacc5cb0fe4d4937241
Ben Marsh <Ben.Marsh@epicgames.com>
2020-06-24 19:54:03 -0400

    Reintroduce support for adding BuildGraph labels separately to aggregates. Syntax is:
        <Label Category="Clients" Name="Windows" Requires="Compile UE4Client Win64" Exclude="Compile UnrealHeaderTool Win64"/>
    
    #rb none
    
    [CL 13765754 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/BuildGraph/Aggregate.cs
+++ Engine/Source/Programs/AutomationTool/BuildGraph/Aggregate.cs
@@ -19,29 +19,17 @@ namespace AutomationTool
 		public readonly string Name;
 
 		/// <summary>
-		/// Label to show for this aggregate in Horde
-		/// </summary>
-		public readonly string Label;
-
-		/// <summary>
 		/// Set of nodes that must be run for this label to be shown.
 		/// </summary>
 		public HashSet<Node> RequiredNodes = new HashSet<Node>();
 
 		/// <summary>
-		/// Set of nodes that will be included in this label if present.
-		/// </summary>
-		public HashSet<Node> IncludedNodes = new HashSet<Node>();
-
-		/// <summary>
 		/// Constructor
 		/// </summary>
 		/// <param name="InName">Name of this aggregate</param>
-		/// <param name="InLabel">Label to show for this aggregate</param>
-		public Aggregate(string InName, string InLabel)
+		public Aggregate(string InName)
 		{
 			Name = InName;
-			Label = InLabel;
 		}
 
 		/// <summary>
