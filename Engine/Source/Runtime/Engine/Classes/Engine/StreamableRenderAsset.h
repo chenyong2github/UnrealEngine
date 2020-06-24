4855fe22faa97b8ffa68ba2b215583bee73f7b9a
jian ru <Jian.Ru@epicgames.com>
2020-06-24 16:10:49 -0400

    Remove an invalid check. The function must be called on GT timeline but it doesn't have to be called on GT (e.g. ParallelFor issued from GT)
    [REVIEW] [at]chris.bunner, [at]uriel.doyon
    #localization none
    #tests PIEed in editor with mobile preview
    
    
    #ROBOMERGE-SOURCE: CL 13763408 via CL 13763417 via CL 13763422 via CL 13763431 via CL 13763441
    #ROBOMERGE-BOT: (v712-13763167)
    
    [CL 13763451 by jian ru in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/StreamableRenderAsset.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/StreamableRenderAsset.h
@@ -171,12 +171,31 @@ public:
 			|| ForceMipLevelsToBeResidentTimestamp >= FApp::GetCurrentTime();
 	}
 
+	/**
+	* Register a callback to get notified when a certain mip or LOD is resident or evicted.
+	* @param Component The context component
+	* @param LODIdx The mip or LOD level to wait for
+	* @param TimeoutSecs Timeout in seconds
+	* @param bOnStreamIn Whether to get notified when the specified level is resident or evicted
+	* @param Callback The callback to call
+	*/
 	ENGINE_API void RegisterMipLevelChangeCallback(UPrimitiveComponent* Component, int32 LODIdx, float TimeoutSecs, bool bOnStreamIn, FLODStreamingCallback&& Callback);
 
+	/**
+	* Remove mip level change callbacks registered by a component.
+	* @param Component The context component
+	*/
 	ENGINE_API void RemoveMipLevelChangeCallback(UPrimitiveComponent* Component);
 
+	/**
+	* Not thread safe and must be called on GT timeline but not necessarily on GT (e.g. ParallelFor called from GT).
+	*/
 	ENGINE_API void RemoveAllMipLevelChangeCallbacks();
 
+	/**
+	* Invoke registered mip level change callbacks.
+	* @param DeferredTickCBAssets Non-null if not called on GT. An array that collects assets with CBs that need to be called later on GT
+	*/
 	ENGINE_API void TickMipLevelChangeCallbacks(TArray<UStreamableRenderAsset*>* DeferredTickCBAssets);
 
 	/**
