dfe27b1847ae920a20af424753bd9c3ed6fef6ca
bob tellez <Bob.Tellez@epicgames.com>
2020-06-24 00:29:10 -0400

    Back out changelist 13753913. Cook crash, edtest crash
    [FYI] Fred.Kimberley
    
    #ROBOMERGE-SOURCE: CL 13754408 via CL 13754409 via CL 13754410 via CL 13754479 via CL 13754494
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v708-13641880)
    
    [CL 13754499 by bob tellez in Main branch]

--- Engine/Source/Runtime/Engine/Private/Curves/RichCurve.cpp
+++ Engine/Source/Runtime/Engine/Private/Curves/RichCurve.cpp
@@ -228,9 +228,9 @@ FKeyHandle FRichCurve::AddKey( const float InTime, const float InValue, const bo
 
 void FRichCurve::SetKeys(const TArray<FRichCurveKey>& InKeys)
 {
-	Reset(InKeys.Num());
-	Keys.SetNum(InKeys.Num());
+	Reset();
 
+	Keys.SetNum(InKeys.Num());
 	KeyHandlesToIndices.SetKeyHandles(InKeys.Num());
 
 	for (int32 Index = 0; Index < InKeys.Num(); ++Index)
@@ -546,10 +546,10 @@ void FRichCurve::GetValueRange(float& MinValue, float& MaxValue) const
 }
 
 
-void FRichCurve::Reset(uint32 Slack)
+void FRichCurve::Reset()
 {
-	Keys.Empty(Slack);
-	KeyHandlesToIndices.Empty(Slack);
+	Keys.Empty();
+	KeyHandlesToIndices.Empty();
 }
 
 
@@ -1125,11 +1125,6 @@ bool FRichCurve::operator==(const FRichCurve& Curve) const
 	}
 
 	return true;
-}
-
-bool FRichCurve::operator!=(const FRichCurve& Curve) const
-{
-	return !(*this == Curve);
 }
 
 static ERichCurveCompressionFormat FindRichCurveCompressionFormat(const FRichCurve& Curve)
