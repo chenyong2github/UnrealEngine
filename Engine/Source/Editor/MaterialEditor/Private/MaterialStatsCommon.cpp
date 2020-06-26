3d6ef5cff0d076eddc32841d6d18056ec370c386
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2020-06-26 08:52:22 -0400

    Re-submitting with skin cache fix: Merge GPU skinning vertex factories for 4 and 8 bone influences into single factory with branch inside a vertex shader #rb chris.bunner
    #ROBOMERGE-SOURCE: CL 13778127 via CL 13778128
    #ROBOMERGE-BOT: (v712-13763167)
    
    [CL 13778129 by dmitriy dyomin in Release-Engine-Staging branch]

--- Engine/Source/Editor/MaterialEditor/Private/MaterialStatsCommon.cpp
+++ Engine/Source/Editor/MaterialEditor/Private/MaterialStatsCommon.cpp
@@ -342,7 +342,7 @@ void FMaterialStatsUtils::GetRepresentativeShaderTypesAndDescriptions(TMap<FName
 	bool bMobileHDR = MobileHDR && MobileHDR->GetValueOnAnyThread() == 1;
 
 	static const FName FLocalVertexFactoryName = FLocalVertexFactory::StaticType.GetFName();
-	static const FName FGPUFactoryName = TEXT("TGPUSkinVertexFactoryExtra");
+	static const FName FGPUFactoryName = TEXT("TGPUSkinVertexFactoryDefault");
 
 	if (TargetMaterial->IsUIMaterial())
 	{
