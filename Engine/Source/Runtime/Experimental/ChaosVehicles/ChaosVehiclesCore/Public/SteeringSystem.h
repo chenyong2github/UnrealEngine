f3fcd078e8a977570f6d0055d780ffea896de69d
bill henderson <Bill.Henderson@epicgames.com>
2020-06-29 11:53:53 -0400

    ChaosVehicles: * super simple ABS, Traction Control * Transmission Efficiency parameter * Vehicle Performance measuring, i.e. 0-60 MPH Time, Distance, Quarter Mile Drag, Time, Distance, Final Speed * Vehicle Mass override * moved vehicle system source from headers
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: bill.henderson
    #ROBOMERGE-SOURCE: CL 13727674 via CL 13727675 via CL 13727676 via CL 13727678
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v715-13780953)
    
    [CL 13784843 by bill henderson in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Experimental/ChaosVehicles/ChaosVehiclesCore/Public/SteeringSystem.h
+++ Engine/Source/Runtime/Experimental/ChaosVehicles/ChaosVehiclesCore/Public/SteeringSystem.h
@@ -13,7 +13,7 @@ PRAGMA_DISABLE_OPTIMIZATION
 namespace Chaos
 {
 
-	struct FSimpleSteeringConfig
+	struct CHAOSVEHICLESCORE_API FSimpleSteeringConfig
 	{
 		FSimpleSteeringConfig()
 			: TrackWidth(1.8f)
@@ -26,52 +26,20 @@ namespace Chaos
 		float TrackEndRadius;
 	};
 
-	class FSimpleSteeringSim : public TVehicleSystem<FSimpleSteeringConfig>
+
+	class CHAOSVEHICLESCORE_API FSimpleSteeringSim : public TVehicleSystem<FSimpleSteeringConfig>
 	{
 	public:
 
-		FSimpleSteeringSim(const FSimpleSteeringConfig* SetupIn)
-			: TVehicleSystem<FSimpleSteeringConfig>(SetupIn)
-		{
-			float TrackWidth = Setup().TrackWidth;
-			float WheelBase = Setup().WheelBase;
+		FSimpleSteeringSim(const FSimpleSteeringConfig* SetupIn);
 
-			float Beta = FSteeringUtility::CalculateBetaDegrees(TrackWidth, WheelBase);
+		void GetLeftHingeLocations(FVector2D& OutC1, FVector2D& OutP, FVector2D& OutC2);
 
-			FSteeringUtility::CalcJointPositions(TrackWidth, Beta, Setup().TrackEndRadius, C1, R1, C2, R2);
+		void GetRightHingeLocations(FVector2D& OutC1, FVector2D& OutP, FVector2D& OutC2);
 
-			FSteeringUtility::CalculateAkermannAngle(false, 0.0f, C2, R1, R2, RestAngle, LeftRodPt, RightPivot);
+		void CalculateAkermannAngle(float Input, float& OutSteerLeft, float& OutSteerRight);
 
-			// #todo: Calculate this from Max Wheel Angle
-			SteerInputScaling = 30.f;
-		}
-
-		void GetLeftHingeLocations(FVector2D& OutC1, FVector2D& OutP, FVector2D& OutC2)
-		{
-			OutC1 = LeftRodPt;
-			OutP = LeftPivot;
-			OutC2 = C2;
-			OutC1.X = -OutC1.X;
-			OutP.X = -OutP.X;
-			OutC2.X = -OutC2.X;
-		}
-
-		void GetRightHingeLocations(FVector2D& OutC1, FVector2D& OutP, FVector2D& OutC2)
-		{
-			OutC1 = RightRodPt;
-			OutP = RightPivot;
-			OutC2 = C2;
-		}
-
-		void CalculateAkermannAngle(float Input, float& OutSteerLeft, float& OutSteerRight)
-		{
-			FSteeringUtility::CalculateAkermannAngle(true, Input* SteerInputScaling, C2, R1, R2, OutSteerLeft, LeftRodPt, LeftPivot);
-			FSteeringUtility::CalculateAkermannAngle(false, Input* SteerInputScaling, C2, R1, R2, OutSteerRight, RightRodPt, RightPivot);
-
-			OutSteerLeft -= RestAngle;
-			OutSteerRight -= RestAngle;
-		}
-
+	private:
 		FVector2D C1;
 		FVector2D C2;
 		float R1;
