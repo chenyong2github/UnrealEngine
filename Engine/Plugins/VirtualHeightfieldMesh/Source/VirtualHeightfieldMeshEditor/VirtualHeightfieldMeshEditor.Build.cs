14b9172c2d7761746783e51427de2701421fc6a8
jeremy moore <Jeremy.Moore@epicgames.com>
2020-06-29 09:28:24 -0400

    Virtual Heightfield Mesh current progress
    #ROBOMERGE-SOURCE: CL 13782807 via CL 13782808 via CL 13782813
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v715-13780953)
    
    [CL 13782822 by jeremy moore in Main branch]

---
+++ Engine/Plugins/VirtualHeightfieldMesh/Source/VirtualHeightfieldMeshEditor/VirtualHeightfieldMeshEditor.Build.cs
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class VirtualHeightfieldMeshEditor : ModuleRules
+{
+	public VirtualHeightfieldMeshEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"EditorStyle",
+			"Engine",
+			"Slate",
+			"SlateCore",
+			"VirtualHeightfieldMesh",
+		});
+	}
+}
