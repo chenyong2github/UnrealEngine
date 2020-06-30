3bb0b5a3ae56c4c723b4ab33ff8a54acd1fe7e37
andrew grant <Andrew.Grant@epicgames.com>
2020-06-30 12:18:08 -0400

    Added support on Mac and Linux for user-defined .unrealrc files that allow developers to specify environment variables for UBT and UAT scripts.
    (Still todo: have UE4 apply .unrealrc during launch)
    
    Created new SetupEnvironment.sh script
    * Applies .unrealrc files from ~ and the workspace root as part of setup.
    * Optionally sets up mono if -mono is supplised
    
    Updated following scripts to call "SetupEnvironment.sh -mono" instead of SetupMono.sh directly.
    * Build.sh
    * GenerateProjectFiles.sh
    * GitDependencies.sh
    * RunMono.sh
    * RunXBuild.sh
    * XcodeBuild.sh
    
    Added SetupEnvironment.sh to installed builds dependencies
    
    Fixes issue where build.sh and xcodebuild.sh weren't first build UBT if it was out of date
    
    #rb Brandon.Schaefer
    [at]michael.trepka [at]will.damon [at]josh.adams [at]wang.hao [at]joe.barrett
    
    #ROBOMERGE-SOURCE: CL 13797247 via CL 13797253 via CL 13797260 via CL 13797267
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v717-13788717)
    
    [CL 13797271 by andrew grant in Main branch]

--- Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
+++ Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
@@ -5,7 +5,7 @@ echo
 echo Setting up Unreal Engine 4 project files...
 echo
 
-# If ran from someone other then the script location we'll have the full base path
+# If ran from somewhere other then the script location we'll have the full base path
 BASE_PATH="`dirname "$0"`"
 
 # this is located inside an extra 'Linux' path unlike the Windows variant.
@@ -20,7 +20,7 @@ if [ ! -d "$BASE_PATH/../../../Source" ]; then
  exit 1
 fi
 
-source "$BASE_PATH/SetupMono.sh" "$BASE_PATH"
+source "$BASE_PATH/SetupEnvironment.sh" -mono "$BASE_PATH"
 
 if [ -f "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" ]; then
 	xbuild "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" /property:Configuration="Development" /verbosity:quiet /nologo /p:NoWarn=1591
