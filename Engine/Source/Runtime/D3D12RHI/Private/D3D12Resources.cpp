abc0e07fbd9635e212acf26afc07c367c9ea695a
kenzo terelst <Kenzo.Terelst@xa.epicgames.com>
2020-06-30 11:43:45 -0400

    Back out new version of NvAftermath - causes issues on Win7 because of DXGIGetDebugInterface1 usage which is not available on Win7
    #rb None
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: kenzo.terelst
    #ROBOMERGE-SOURCE: CL 13747587 via CL 13747630 via CL 13747637 via CL 13747654 via CL 13747665 via CL 13749130
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v717-13788717)
    
    [CL 13796582 by kenzo terelst in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Resources.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Resources.cpp
@@ -254,13 +254,6 @@ FD3D12Resource::FD3D12Resource(FD3D12Device* ParentDevice,
 	}
 
 	InitalizeResourceState(InitialState);
-
-#if NV_AFTERMATH
-	if (GDX12NVAfterMathTrackResources)
-	{
-		GFSDK_Aftermath_DX12_RegisterResource(InResource, &AftermathHandle);
-	}
-#endif
 }
 
 FD3D12Resource::~FD3D12Resource()
@@ -269,13 +262,6 @@ FD3D12Resource::~FD3D12Resource()
 	{
 		D3DX12Residency::EndTrackingObject(GetParentDevice()->GetResidencyManager(), ResidencyHandle);
 	}
-
-#if NV_AFTERMATH
-	if (GDX12NVAfterMathTrackResources)
-	{
-		GFSDK_Aftermath_DX12_UnregisterResource(AftermathHandle);
-	}
-#endif
 }
 
 void FD3D12Resource::StartTrackingForResidency()
