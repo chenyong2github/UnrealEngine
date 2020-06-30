54437183ba91c305c2b8cfe0574e29b1e83be2d8
andrew grant <Andrew.Grant@epicgames.com>
2020-06-30 12:17:36 -0400

    Added support on Mac and Linux for user-defined .unrealrc files that allow developers to specify environment variables for UBT and UAT scripts.
    (Still todo: have UE4 apply .unrealrc during launch)
    
    Created new SetupEnvironment.sh script
    * Applies .unrealrc files from ~ and the workspace root as part of setup.
    * Optionally sets up mono if -mono is supplised
    
    Updated following scripts to call "SetupEnvironment.sh -mono" instead of SetupMono.sh directly.
    * Build.sh
    * GenerateProjectFiles.sh
    * GitDependencies.sh
    * RunMono.sh
    * RunXBuild.sh
    * XcodeBuild.sh
    
    Added SetupEnvironment.sh to installed builds dependencies
    
    Fixes issue where build.sh and xcodebuild.sh weren't first build UBT if it was out of date
    
    #rb Brandon.Schaefer
    [at]michael.trepka [at]will.damon [at]josh.adams [at]wang.hao [at]joe.barrett
    
    
    #ROBOMERGE-SOURCE: CL 13797247 via CL 13797253 via CL 13797260
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13797267 by andrew grant in Release-Engine-Staging branch]

--- Engine/Build/BatchFiles/Linux/Build.sh
+++ Engine/Build/BatchFiles/Linux/Build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Copyright Epic Games, Inc. All Rights Reserved.
-# This script gets can be used to build clean individual projects using UnrealBuildTool
+# This script gets can be used to build and clean individual projects using UnrealBuildTool
 
 set -e
 
 cd "`dirname "$0"`/../../../.." 
 
-# Setup Mono
-source Engine/Build/BatchFiles/Linux/SetupMono.sh Engine/Build/BatchFiles/Linux
+# Setup Environment and Mono
+source Engine/Build/BatchFiles/Linux/SetupEnvironment.sh -mono Engine/Build/BatchFiles/Linux
 
 # First make sure that the UnrealBuildTool is up-to-date
-if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
+if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
   echo "Failed to build to build tool (UnrealBuildTool)"
   exit 1
 fi
 
-echo "Building $1..."
+echo Running command : Engine/Binaries/DotNET/UnrealBuildTool.exe "$@"
 mono Engine/Binaries/DotNET/UnrealBuildTool.exe "$@"
 exit $?
--- Engine/Build/BatchFiles/Mac/Build.sh
+++ Engine/Build/BatchFiles/Mac/Build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+# Copyright Epic Games, Inc. All Rights Reserved.
+# This script gets can be used to build and clean individual projects using UnrealBuildTool
 
 cd "`dirname "$0"`/../../../.."
 
-# Setup Mono
-source Engine/Build/BatchFiles/Mac/SetupMono.sh Engine/Build/BatchFiles/Mac
+# Setup Environment and Mono
+source Engine/Build/BatchFiles/Mac/SetupEnvironment.sh -mono Engine/Build/BatchFiles/Mac
+
+# First make sure that the UnrealBuildTool is up-to-date
+if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
+  echo "Failed to build to build tool (UnrealBuildTool)"
+  exit 1
+fi
 
 if [ "$4" == "-buildscw" ] || [ "$5" == "-buildscw" ]; then
 	echo Building ShaderCompileWorker...
