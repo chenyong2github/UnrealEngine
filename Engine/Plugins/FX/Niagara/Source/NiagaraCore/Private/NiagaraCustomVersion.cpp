0d4c55be9fdacf3ab063f04df02883ca46ca9ecf
shaun kime <Shaun.Kime@epicgames.com>
2020-06-30 11:48:34 -0400

    Making interpolated spawning also set the previous position. Resubmit after backout.
    #rb stu.mckenna
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 13751675 via CL 13751677 via CL 13751678 via CL 13751683 via CL 13751688 via CL 13751693
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v717-13788717)
    
    [CL 13796721 by shaun kime in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x25B64B58, 0x04B24E19, 0x8586C7C5, 0x6A5777E1);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x32C7C3F4, 0x4FEE47CA, 0x96B53C89, 0x52669B4F);
