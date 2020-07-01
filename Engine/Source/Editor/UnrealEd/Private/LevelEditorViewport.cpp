833c84b03fd442e0576104807bf0d0b96d6fb75a
brooke hubert <brooke.hubert@epicgames.com>
2020-07-01 14:17:16 -0400

    Disable input in Level Editor Viewports when a map is being torn down.
    # Speculative fix v2 - the callstack in the bug looks like we think we have a valid actor pointer, but it's probably getting garbage collected. The log is outputting level unload messages right before the crash.
    # Although this fix went into the SLevelViewport, I now know that other asset editors are using the level viewport, and those could still be using the world that is being torn down. Moving this into the level editor viewport, to catch more use cases of the level editor viewport client.
    
    #Jira UE-89337
    #rb lauren.barnes
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: brooke.hubert
    #ROBOMERGE-SOURCE: CL 13811874 via CL 13811877 via CL 13811880 via CL 13812090
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13815076 by brooke hubert in Dev-Networking branch]

--- Engine/Source/Editor/UnrealEd/Private/LevelEditorViewport.cpp
+++ Engine/Source/Editor/UnrealEd/Private/LevelEditorViewport.cpp
@@ -1824,6 +1824,8 @@ FLevelEditorViewportClient::FLevelEditorViewportClient(const TSharedPtr<SLevelVi
 
 	// Sign up for notifications about users changing settings.
 	GetMutableDefault<ULevelEditorViewportSettings>()->OnSettingChanged().AddRaw(this, &FLevelEditorViewportClient::HandleViewportSettingChanged);
+
+	FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor").OnMapChanged().AddRaw(this, &FLevelEditorViewportClient::OnMapChanged);
 }
 
 //
@@ -1835,6 +1837,8 @@ FLevelEditorViewportClient::~FLevelEditorViewportClient()
 	// Unregister for all global callbacks to this object
 	FEditorSupportDelegates::CleanseEditor.RemoveAll(this);
 	FEditorDelegates::PreBeginPIE.RemoveAll(this);
+
+	FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor").OnMapChanged().RemoveAll(this);
 
 	if(GEngine)
 	{
@@ -3255,6 +3259,16 @@ void FLevelEditorViewportClient::HandleViewportSettingChanged(FName PropertyName
 	{
 		EngineShowFlags.SetSelectionOutline(GetDefault<ULevelEditorViewportSettings>()->bUseSelectionOutline);
 	}
+}
+
+void FLevelEditorViewportClient::OnMapChanged(UWorld* InWorld, EMapChangeType MapChangeType)
+{
+	if (InWorld != GetWorld())
+	{
+		return;
+	}
+
+	bDisableInput = (MapChangeType == EMapChangeType::TearDownWorld);
 }
 
 void FLevelEditorViewportClient::OnActorMoved(AActor* InActor)
