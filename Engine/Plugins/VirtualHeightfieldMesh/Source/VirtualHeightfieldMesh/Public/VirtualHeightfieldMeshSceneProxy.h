e1d294b8fa5abd690176347c277017e642d4161e
jeremy moore <Jeremy.Moore@epicgames.com>
2020-06-30 20:56:32 -0400

    Virtual Heightfield Mesh current progress
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 13782807 via CL 13782808 via CL 13782813 via CL 13782822
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13805989 by jeremy moore in Dev-Networking branch]

---
+++ Engine/Plugins/VirtualHeightfieldMesh/Source/VirtualHeightfieldMesh/Public/VirtualHeightfieldMeshSceneProxy.h
@@ -0,0 +1,54 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PrimitiveSceneProxy.h"
+
+/** Scene proxy responsible for rendering of a Virtual Heightfield Mesh. */
+class FVirtualHeightfieldMeshSceneProxy final : public FPrimitiveSceneProxy
+{
+public:
+	FVirtualHeightfieldMeshSceneProxy(class UVirtualHeightfieldMeshComponent* InComponent);
+
+protected:
+	//~ Begin FPrimitiveSceneProxy Interface
+	virtual SIZE_T GetTypeHash() const override;
+	virtual uint32 GetMemoryFootprint() const override;
+	virtual void CreateRenderThreadResources() override;
+	virtual void DestroyRenderThreadResources() override;
+	virtual void OnTransformChanged() override;
+	virtual bool HasSubprimitiveOcclusionQueries() const override;
+	virtual const TArray<FBoxSphereBounds>* GetOcclusionQueries(const FSceneView* View) const override;
+	virtual void AcceptOcclusionResults(const FSceneView* View, TArray<bool>* Results, int32 ResultsStart, int32 NumResults) override;
+	virtual FPrimitiveViewRelevance GetViewRelevance(const FSceneView* View) const override;
+	virtual void GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const override;
+	//~ End FPrimitiveSceneProxy Interface
+
+private:
+	void BuildOcclusionVolumes();
+	static void OnVirtualTextureDestroyedCB(const FVirtualTextureProducerHandle& InHandle, void* Baton);
+
+public:
+	class URuntimeVirtualTexture* RuntimeVirtualTexture;
+	class FMaterialRenderProxy* Material;
+	class UTexture2D* MinMaxTexture;
+
+	class IAllocatedVirtualTexture* AllocatedVirtualTexture;
+	bool bCallbackRegistered;
+
+	uint32 NumQuadsPerTileSide;
+	
+	FMatrix UVToWorld;
+	FMatrix WorldToUV;
+	FMatrix WorldToUVTransposeAdjoint;
+
+	class FVirtualHeightfieldMeshVertexFactory* VertexFactory;
+
+	float LODScale;
+
+	TArray<FVector2D> OcclusionData;
+	int32 NumOcclusionLods;
+	FIntPoint OcclusionGridSize;
+	TArray<FBoxSphereBounds> OcclusionVolumes;
+};
