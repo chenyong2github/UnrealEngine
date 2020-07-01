4d746766078d8e262ebac212907b1e432123d177
bob tellez <Bob.Tellez@epicgames.com>
2020-06-30 20:32:02 -0400

    Back out changelist 13750040. Cook crash
    [FYI] Fred.Kimberley
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: bob.tellez
    #ROBOMERGE-SOURCE: CL 13752086 via CL 13752087 via CL 13752088 via CL 13752091 via CL 13752095 via CL 13752097
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13804101 by bob tellez in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/CompositeDataTable.cpp
+++ Engine/Source/Runtime/Engine/Private/CompositeDataTable.cpp
@@ -19,12 +19,6 @@
 
 #define DATATABLE_CHANGE_SCOPE()	UDataTable::FScopedDataTableChange ActiveScope(this);
 
-static TAutoConsoleVariable<int32> CVarCompositeDataTableMinimalUpdateEnable(
-	TEXT("compositedatatable.minimalupdate"),
-	0,
-	TEXT("Minimizes the in memory changes when updating composite data tables. Significantly slower than the standard update."),
-	ECVF_ReadOnly);
-
 UCompositeDataTable::UCompositeDataTable(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
@@ -89,54 +83,16 @@ void UCompositeDataTable::UpdateCachedRowMap(bool bWarnOnInvalidChildren)
 		}
 	}
 
-	// verify that all parent tables have the same row struct
-	bool bParentsHaveDifferentRowStruct = false;
-	for (const UDataTable* ParentTable : ParentTables)
-	{
-		if (ParentTable && ParentTable->RowStruct != RowStruct)
-		{
-			if (bWarnOnInvalidChildren)
-			{
-				bParentsHaveDifferentRowStruct = true;
-				FString CompositeRowStructName = RowStruct ? RowStruct->GetName() : "Missing row struct";
-				FString ParentRowStructName = ParentTable->RowStruct ? ParentTable->RowStruct->GetName() : "Missing row struct";
-				UE_LOG(LogDataTable, Error, TEXT("Composite tables must have the same row struct as their parent tables. Composite Table: %s, Composite Row Struct: %s, Parent Table: %s, Parent Row Struct: %s."), *GetName(), *CompositeRowStructName, *ParentTable->GetName(), *ParentRowStructName);
-			}
-			bLeaveEmpty = true;
-			continue;
-		}
-	}
-
-	if (bParentsHaveDifferentRowStruct)
-	{
-		const FText ErrorMsg = FText::Format(LOCTEXT("ParentsIncludesOtherRowStructError", "Composite table '{0}' must have the same row struct as it's parent tables. See output log for list of invalid rows."),
-			FText::FromString(GetName()));
-#if WITH_EDITOR
-		if (!bIsLoading)
-		{
-			FMessageDialog::Open(EAppMsgType::Ok, ErrorMsg);
-		}
-		else
-#endif
-		{
-			UE_LOG(LogDataTable, Warning, TEXT("%s"), *ErrorMsg.ToString());
-		}
-	}
-
 	DATATABLE_CHANGE_SCOPE();
 
 #if WITH_EDITOR
 	FDataTableEditorUtils::BroadcastPreChange(this, FDataTableEditorUtils::EDataTableChangeInfo::RowList);
 #endif
+	UDataTable::EmptyTable();
 
-	if (bLeaveEmpty)
+	if (!bLeaveEmpty)
 	{
-		UDataTable::EmptyTable();
-	}
-	else if (GIsEditor || !CVarCompositeDataTableMinimalUpdateEnable.GetValueOnGameThread())
-	{
-		UDataTable::EmptyTable();
-
+		bool bParentsHaveDifferentRowStruct = false;
 		// iterate through all of the rows
 		for (const UDataTable* ParentTable : ParentTables)
 		{
@@ -144,88 +100,46 @@ void UCompositeDataTable::UpdateCachedRowMap(bool bWarnOnInvalidChildren)
 			{
 				continue;
 			}
-
-			// Add new rows or overwrite previous rows
-			for (TMap<FName, uint8*>::TConstIterator ParentRowMapIter(ParentTable->GetRowMap().CreateConstIterator()); ParentRowMapIter; ++ParentRowMapIter)
+			if (ParentTable->RowStruct != RowStruct)
 			{
-				if (ensure(ParentRowMapIter.Value() != nullptr))
+				if (bWarnOnInvalidChildren)
 				{
-					// UDataTable::AddRow will first remove the row if it already exists so we don't need to do anything special here
-					FTableRowBase* ParentTableRowBase = (FTableRowBase*)ParentRowMapIter.Value();
-					UDataTable::AddRow(ParentRowMapIter.Key(), *ParentTableRowBase);
+					bParentsHaveDifferentRowStruct = true;
+					FString CompositeRowStructName = RowStruct ? RowStruct->GetName() : "Missing row struct";
+					FString ParentRowStructName = ParentTable->RowStruct ? ParentTable->RowStruct->GetName() : "Missing row struct";
+					UE_LOG(LogDataTable, Error, TEXT("Composite tables must have the same row struct as their parent tables. Composite Table: %s, Composite Row Struct: %s, Parent Table: %s, Parent Row Struct: %s."), *GetName(), *CompositeRowStructName, *ParentTable->GetName(), *ParentRowStructName);
 				}
-			}
-		}
-	}
-	else
-	{
-		// build a duplicate table using the stack of parent tables
-		UDataTable* TempTable = NewObject<UDataTable>(GetTransientPackage());
-		TempTable->RowStruct = RowStruct;
-		for (const UDataTable* ParentTable : ParentTables)
-		{
-			if (ParentTable == nullptr)
-			{
 				continue;
 			}
 
 			// Add new rows or overwrite previous rows
-			for (TMap<FName, uint8*>::TConstIterator ParentRowMapIter(ParentTable->GetRowMap().CreateConstIterator()); ParentRowMapIter; ++ParentRowMapIter)
+			for (TMap<FName, uint8*>::TConstIterator RowMapIter(ParentTable->GetRowMap().CreateConstIterator()); RowMapIter; ++RowMapIter)
 			{
-				if (ensure(ParentRowMapIter.Value() != nullptr))
+				if (ensure(RowMapIter.Value() != nullptr))
 				{
 					// UDataTable::AddRow will first remove the row if it already exists so we don't need to do anything special here
-					FTableRowBase* ParentTableRowBase = (FTableRowBase*)ParentRowMapIter.Value();
-					TempTable->AddRow(ParentRowMapIter.Key(), *ParentTableRowBase);
+					FTableRowBase* TableRowBase = (FTableRowBase*)RowMapIter.Value();
+					UDataTable::AddRow(RowMapIter.Key(), *TableRowBase);
 				}
 			}
 		}
 
-		// now that we have an up to date copy of the composite table we can update the old copy row by row
-		// first remove any rows that are in the old table but not the new table
-		TArray<FName> RowsToRemove;
-		for (TMap<FName, uint8*>::TConstIterator RowMapIter(RowMap.CreateConstIterator()); RowMapIter; ++RowMapIter)
+		if (bParentsHaveDifferentRowStruct)
 		{
-			if (!TempTable->FindRowUnchecked(RowMapIter.Key()))
+			const FText ErrorMsg = FText::Format(LOCTEXT("ParentsIncludesOtherRowStructError", "Composite table '{0}' must have the same row struct as it's parent tables. See output log for list of invalid rows."),
+				FText::FromString(GetName()));
+#if WITH_EDITOR
+			if (!bIsLoading)
 			{
-				RowsToRemove.Add(RowMapIter.Key());
-			}
-		}
-
-		for (const FName& RowToRemove : RowsToRemove)
-		{
-			RowMap.Remove(RowToRemove);
-		}
-
-		// for each row in the updated table try to find it in the old table
-		// if we don't find it, add it to the old table
-		// if we do find it, see if it is different in the two tables; is so, update the old table entry
-		for (TMap<FName, uint8*>::TConstIterator TempRowMapIter(TempTable->GetRowMap().CreateConstIterator()); TempRowMapIter; ++TempRowMapIter)
-		{
-			if (uint8* OldRow = FindRowUnchecked(TempRowMapIter.Key()))
-			{
-				// data table rows are not required to have operator== implemented so we need to compare each property in the row
-				for (FProperty* Property = RowStruct->PropertyLink; Property != nullptr; Property = Property->PropertyLinkNext)
-				{
-					FString OldPropString = DataTableUtils::GetPropertyValueAsString(Property, OldRow, EDataTableExportFlags::None);
-					FString NewPropString = DataTableUtils::GetPropertyValueAsString(Property, TempRowMapIter.Value(), EDataTableExportFlags::None);
-
-					// overwrite the old row if the values don't match
-					if (OldPropString.Compare(NewPropString))
-					{
-						UDataTable::AddRow(TempRowMapIter.Key(), *(FTableRowBase*)TempRowMapIter.Value());
-						break;
-					}
-				}
+				FMessageDialog::Open(EAppMsgType::Ok, ErrorMsg);
 			}
 			else
+#endif
 			{
-				// we didn't find the row in the old table so add it
-				UDataTable::AddRow(TempRowMapIter.Key(), *(FTableRowBase*)TempRowMapIter.Value());
+				UE_LOG(LogDataTable, Warning, TEXT("%s"), *ErrorMsg.ToString());
 			}
 		}
 	}
-	
 #if WITH_EDITOR
 	FDataTableEditorUtils::BroadcastPostChange(this, FDataTableEditorUtils::EDataTableChangeInfo::RowList);
 #endif
