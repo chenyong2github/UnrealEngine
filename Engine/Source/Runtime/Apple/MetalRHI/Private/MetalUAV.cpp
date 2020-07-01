76e5dad98e23a062e4c86e65339331adbfe097e5
richard wallis <Richard.Wallis@xa.epicgames.com>
2020-06-30 20:22:24 -0400

    Don't manually nil the FMetalSurface textures in the SRV destructor, let the FMetalSurface destructor for the texture view handle it correctly.
    #jira UE-94223
    [at]will.damon [at]matt.collins
    #lockdown marcus.wassmer
    #rb will.damon, matt.collins
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: richard.wallis
    #ROBOMERGE-SOURCE: CL 13726746 in //UE4/Release-4.25/... via CL 13730719 via CL 13737127 via CL 13737160
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13802838 by richard wallis in Dev-Networking branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalUAV.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalUAV.cpp
@@ -38,12 +38,7 @@ FMetalShaderResourceView::~FMetalShaderResourceView()
 		{
 			Surface->SRVs.Remove(this);
 		}
-		
-		if (TextureView->Texture)
-		{
-			TextureView->Texture = nil;
-			TextureView->MSAATexture = nil;
-		}
+
 		delete TextureView;
 		TextureView = nullptr;
 	}
