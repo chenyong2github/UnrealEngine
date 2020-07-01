d3457279892810579786e46cca48632753435208
ben woodhouse <Ben.Woodhouse@epicgames.com>
2020-06-30 20:25:07 -0400

    Back out changelist 13735273. Cyclic dependency broke mac editor (IOS+mac) builds [FYI] Ori.Cohen,marc.audy
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ben.woodhouse
    #ROBOMERGE-SOURCE: CL 13738770 via CL 13738772 via CL 13738773 via CL 13738775
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13803412 by ben woodhouse in Dev-Networking branch]

--- Engine/Source/Runtime/Experimental/ChaosEngineBridge/Private/ChaosEngineBridgeModule.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Chaos/ChaosEngineBridgeModule.h"
-#include "CoreMinimal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FChaosEngineBridgeModule, ChaosEngineBridge);
