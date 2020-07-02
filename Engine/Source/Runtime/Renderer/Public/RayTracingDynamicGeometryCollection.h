fbb471f7fc72da6d723fc1694ce75c9921d00f2e
kenzo terelst <Kenzo.Terelst@xa.epicgames.com>
2020-07-02 11:29:48 -0400

    - Add validation to make sure that RT geometries which use the shared vertex buffers have been updated that frame and are not using stale data - Add option to not use the shared vertex buffers but the unique vertex buffers - Terrain falls back to unique geometry vertex buffers because it doesn't always update each LOD
    #rb Yuriy.ODonnell
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: kenzo.terelst
    #ROBOMERGE-SOURCE: CL 13820008 via CL 13820012 via CL 13820013 via CL 13820018
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13822777 by kenzo terelst in Dev-Networking branch]

--- Engine/Source/Runtime/Renderer/Public/RayTracingDynamicGeometryCollection.h
+++ Engine/Source/Runtime/Renderer/Public/RayTracingDynamicGeometryCollection.h
@@ -17,6 +17,7 @@ public:
 		FRayTracingDynamicGeometryUpdateParams Params,
 		uint32 PrimitiveId 
 	);
+	uint64 GetSharedBufferGenerationID() const { return SharedBufferGenerationID; }
 
 	void BeginUpdate();
 	void DispatchUpdates(FRHIComputeCommandList& ParentCmdList);
@@ -33,6 +34,10 @@ private:
 		uint32 UsedSize = 0;
 	};
 	TArray<FVertexPositionBuffer*> VertexPositionBuffers;
+
+	// Generation ID when the shared vertex buffers have been reset. The current generation ID is stored in the FRayTracingGeometry to keep track
+	// if the vertex buffer data is still valid for that frame - validated before generation the TLAS
+	uint64 SharedBufferGenerationID = 0;
 };
 
 #endif // RHI_RAYTRACING
