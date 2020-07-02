79f915c2a38a9a88a951c8c251bfb2432f1120e2
david harvey <David.Harvey@epicgames.com>
2020-07-02 11:29:53 -0400

    allow per-platform override files for .target.cs files, for example ShooterGame_Win64.target.cs
    #rb luke.thatcher, josh.adams, ben.marsh
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 13820045 via CL 13820046 via CL 13820047 via CL 13820048 via CL 13820049
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13822784 by david harvey in Dev-Networking branch]

--- Engine/Source/Programs/UnrealBuildTool/System/RulesAssembly.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/RulesAssembly.cs
@@ -478,13 +478,44 @@ namespace UnrealBuildTool
 		protected TargetRules CreateTargetRulesInstance(string TypeName, TargetInfo TargetInfo)
 		{
 			// The build module must define a type named '<TargetName>Target' that derives from our 'TargetRules' type.  
-			Type RulesType = CompiledAssembly.GetType(TypeName);
-			if (RulesType == null)
+			Type BaseRulesType = CompiledAssembly.GetType(TypeName);
+			if (BaseRulesType == null)
 			{
 				throw new BuildException("Expecting to find a type to be declared in a target rules named '{0}'.  This type must derive from the 'TargetRules' type defined by Unreal Build Tool.", TypeName);
 			}
 
+			// Look for platform/group rules that we will use instead of the basic rules
+			string PlatformRulesName = TargetInfo.Name + "_" + TargetInfo.Platform.ToString();
+			Type PlatformRulesType = CompiledAssembly.GetType(TypeName + "_" + TargetInfo.Platform.ToString());
+			if (PlatformRulesType == null)
+			{
+				foreach (UnrealPlatformGroup Group in UEBuildPlatform.GetPlatformGroups(TargetInfo.Platform))
+				{
+					// look to see if the group has an override
+					string GroupRulesName = TargetInfo.Name + "_" + Group.ToString();
+					Type GroupRulesObjectType = CompiledAssembly.GetType(TypeName + "_" + Group.ToString());
+
+					// we expect only one platform group to be found in the extensions
+					if (GroupRulesObjectType != null && PlatformRulesType != null)
+					{
+						throw new BuildException("Found multiple platform group overrides ({0} and {1}) for rules {2} without a platform specific override. Create a platform override with the class hierarchy as needed.", 
+							GroupRulesObjectType.Name, PlatformRulesType.Name, TypeName);
+					}
+					// remember the platform group if we found it, but keep searching to verify there isn't more than one
+					if (GroupRulesObjectType != null)
+					{
+						PlatformRulesName = GroupRulesName;
+						PlatformRulesType = GroupRulesObjectType;
+					}
+				}
+			}
+			if (PlatformRulesType != null && !PlatformRulesType.IsSubclassOf(BaseRulesType))
+			{
+				throw new BuildException("Expecting {0} to be a specialization of {1}", PlatformRulesType, BaseRulesType);
+			}
+
 			// Create an instance of the module's rules object, and set some defaults before calling the constructor.
+			Type RulesType = PlatformRulesType ?? BaseRulesType;
 			TargetRules Rules = (TargetRules)FormatterServices.GetUninitializedObject(RulesType);
 			if (DefaultBuildSettings.HasValue)
 			{
@@ -492,7 +523,8 @@ namespace UnrealBuildTool
 			}
 
 			// Return the target file name to the caller
-			Rules.File = TargetNameToTargetFile[TargetInfo.Name];
+			string TargetInfoName = (PlatformRulesType != null) ? PlatformRulesName : TargetInfo.Name;
+			Rules.File = TargetNameToTargetFile[TargetInfoName];
 
 			// Find the constructor
 			ConstructorInfo Constructor = RulesType.GetConstructor(new Type[] { typeof(TargetInfo) });
