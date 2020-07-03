f3bddf0eed540d3ef4d4829119a8c33babf531ae
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-07-02 23:45:29 -0400

    GeometryProcessing: Add Free-Boundary Discrete Natural Conformal Parameterization (DNCP/LSCM) implementation. - add FUVConstraint struct, FElementLinearization utility functions, IConstrainedMeshUVSolver interface, - move several PrecomputedMeshWeightData.h functions to .cpp so they can be exported (were multiply-defined if header was included in multiple places) - IMatrixSolverBase and implementations can now solve with single Eigen vectors (ie not a Mapped vector) - added ConstructFullCotangentLaplacian() function to LaplacianMatrixAssembly.h, builds cotan matrix w/o special handling of boundary vertices, configurable weight clamping and area weighting - MeshUVSolver.h/.cpp defines FConstrainedMeshUVSolver, cpp contains various functions for constructing DNCP system matrix and solving it using above new code #rb none #rnx
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 13828694 via CL 13828701 via CL 13828711 via CL 13828715
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13829934 by ryan schmidt in Dev-Networking branch]

--- Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Public/Solvers/ConstrainedMeshSolver.h
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Public/Solvers/ConstrainedMeshSolver.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "VectorTypes.h"
 
+class FDynamicMesh3;
+
 namespace UE
 {
 	namespace Solvers
@@ -47,6 +49,42 @@ namespace UE
 			// Returns the vertex locations of the deformed mesh. 
 			// Note the array may have empty elements as the index matches the Mesh based VtxId.  PositionBuffer[VtxId] = Pos.
 			virtual bool Deform(TArray<FVector3d>& PositionBuffer) = 0;
+		};
+
+
+
+
+
+		/**
+		 * Interface to a index-based UV solver for a 3D triangle mesh that supports weighted point constraints.
+		 */
+		class DYNAMICMESH_API IConstrainedMeshUVSolver
+		{
+		public:
+
+			virtual ~IConstrainedMeshUVSolver() {}
+
+			// Add or update a weighted positional constraint associated with VtxId
+			// @param bPostFix if true, this constraint will be explicitly applied after the solve
+			virtual void AddConstraint(const int32 VtxId, const double Weight, const FVector2d& Position, const bool bPostFix) = 0;
+
+			// Update or Create a constraint position associated with @param VtxId
+			// @return true if a constraint weight is associated with @param VtxId
+			virtual bool UpdateConstraintPosition(const int32 VtxId, const FVector2d& Position, const bool bPostFix) = 0;
+
+			// Update or Create a constraint weight associated with @param VtxId
+			// @return true if a constraint position is associated with @param VtxId
+			virtual bool UpdateConstraintWeight(const int32 VtxId, const double Weight) = 0;
+
+			// Clear all constraints (Positions and Weights)
+			virtual void ClearConstraints() = 0;
+
+			// Test if a non-zero weighted constraint is associated with VtxId 
+			virtual bool IsConstrained(const int32 VtxId) const = 0;
+
+			// Returns the vertex locations of the deformed mesh. 
+			// Note the array may have empty elements as the index matches the Mesh based VtxId.  PositionBuffer[VtxId] = Pos.
+			virtual bool SolveUVs(const FDynamicMesh3* DynamicMesh, TArray<FVector2d>& UVBuffer) = 0;
 		};
 
 	}
