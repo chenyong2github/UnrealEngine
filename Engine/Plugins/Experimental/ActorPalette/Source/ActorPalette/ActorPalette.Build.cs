493e80040ea6cd133a7f24380d2920d4925e5dc7
Michael Noland <michael.noland@epicgames.com>
2020-07-02 22:44:34 -0400

    Added dependency on new DeveloperSettings module to fix issue introduced by it being moved out of Engine #rnx #rb none #jira nojira
    [CL 13829675 by Michael Noland in Dev-Framework branch]

--- Engine/Plugins/Experimental/ActorPalette/Source/ActorPalette/ActorPalette.Build.cs
+++ Engine/Plugins/Experimental/ActorPalette/Source/ActorPalette/ActorPalette.Build.cs
@@ -43,7 +43,8 @@ public class ActorPalette : ModuleRules
 				"SlateCore",
 				"EditorStyle",
 				"ContentBrowser",
-				"WorkspaceMenuStructure"
+				"WorkspaceMenuStructure",
+				"DeveloperSettings"
 			}
 			);
 	}
