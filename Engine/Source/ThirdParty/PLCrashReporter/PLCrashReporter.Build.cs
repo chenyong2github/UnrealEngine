830ebeb0b6957b3d5a6b6690d7692382f668a951
andrew grant <Andrew.Grant@epicgames.com>
2020-07-11 19:39:09 -0400

    Added new (but not enabled) version of PLCrashReporter which has removed some legacy stuff and can be built for arm64 on Mac. Changes in the current version will need to be integrated here (or vice-versa) before it is a real option to ship with.
    Moved lib locations in old PLCrashReporter to a more standard format that simplifies build scripts
    
    Update PLCrashReporter.Build.cs to make it easy to change between versions
    
    #rb swarm
    #fyi michael.trepka, brandon.schaefer, peter.saurbrei, will.damon
    #jira nojira
    
    
    #ROBOMERGE-OWNER: andrew.grant
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 13844618 via CL 13844619 via CL 13844620
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13844624 by andrew grant in Release-Engine-Staging branch]

--- Engine/Source/ThirdParty/PLCrashReporter/PLCrashReporter.Build.cs
+++ Engine/Source/ThirdParty/PLCrashReporter/PLCrashReporter.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 using UnrealBuildTool;
+using System.IO;
 
 public class PLCrashReporter : ModuleRules
 {
@@ -7,31 +8,40 @@ public class PLCrashReporter : ModuleRules
 	{
 		Type = ModuleType.External;
 
-		string PLCrashReporterPath = Target.UEThirdPartySourceDirectory + "PLCrashReporter/plcrashreporter-master-5ae3b0a/";
+		string PLVersion = "plcrashreporter-master-5ae3b0a";
+		string[] PLDefines = new string[] {};
 
-		if (Target.Platform == UnrealTargetPlatform.Mac)
+		// ONLY FOR TESTING 
+		// This version builds for Mac arm64 but does not include the custom changes made to the version above
+		// Those either need reintegrated into the version below, or the changes in the version below 
+		// that cleanup all the project of old architectures and build steps could be recreated in the current
+		// version.
+		//PLVersion = "plcrashreporter-master-0c55d20-2020_07_10";
+		//PLDefines = new[] {"USE_UNTESTED_PL_CRASHREPORTER"};
+
+		string PLCrashReporterPath = Path.Combine(Target.UEThirdPartySourceDirectory,"PLCrashReporter",PLVersion);
+		string PLSourcePath = Path.Combine(PLCrashReporterPath, "Source");
+		string PLLibPath = Path.Combine(PLCrashReporterPath, "lib");
+
+		if (Target.Platform == UnrealTargetPlatform.Mac || Target.Platform == UnrealTargetPlatform.IOS)
 		{
-			PublicSystemIncludePaths.Add(PLCrashReporterPath + "Source");
+			PublicSystemIncludePaths.Add(PLSourcePath);
+
+			string LibConfig = "Release";
+
 			if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT)
 			{
-				PublicAdditionalLibraries.Add(PLCrashReporterPath + "Mac/Debug/libCrashReporter-MacOSX-Static.a");
+				LibConfig = "Debug";
 			}
 			else
 			{
-				PublicAdditionalLibraries.Add(PLCrashReporterPath + "Mac/Release/libCrashReporter-MacOSX-Static.a");
+				LibConfig = "Release";
 			}
+
+			string Lib = Path.Combine(PLLibPath, Target.Platform.ToString(), LibConfig, "libCrashReporter.a");				
+			PublicAdditionalLibraries.Add(Lib);		
+
+			PublicDefinitions.AddRange(PLDefines);	
 		}
-        else if (Target.Platform == UnrealTargetPlatform.IOS)
-        {
-            PublicSystemIncludePaths.Add(PLCrashReporterPath + "Source");
-            if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT)
-            {
-                PublicAdditionalLibraries.Add(PLCrashReporterPath + "IOS/Debug/libCrashReporter-iphoneos.a");
-            }
-            else
-            {
-                PublicAdditionalLibraries.Add(PLCrashReporterPath + "IOS/Release/libCrashReporter-iphoneos.a");
-            }
-        }
     }
 }
