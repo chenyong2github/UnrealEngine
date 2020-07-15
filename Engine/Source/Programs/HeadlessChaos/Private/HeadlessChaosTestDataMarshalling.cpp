7381433b29a55c5b7989ddc676258e2d102754cb
ori cohen <Ori.Cohen@epicgames.com>
2020-07-15 16:50:34 -0400

    More work to make chaos marshalling support async ticking
    #rb Max.Whitehead
    
    
    #ROBOMERGE-SOURCE: CL 13866879 via CL 13866884
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13866898 by ori cohen in Release-Engine-Staging branch]

--- Engine/Source/Programs/HeadlessChaos/Private/HeadlessChaosTestDataMarshalling.cpp
+++ Engine/Source/Programs/HeadlessChaos/Private/HeadlessChaosTestDataMarshalling.cpp
@@ -17,60 +17,78 @@ namespace ChaosTest
 
     using namespace Chaos;
 
-	GTEST_TEST(DataMarshalling, SyncMarshalling)
+	GTEST_TEST(DataMarshalling,Marshalling)
 	{
 		FChaosMarshallingManager Manager;
 
-		float ExternalDt = 1/60.f;
-		float InternalTime = 0;
-		float InternalDt = 1/60.f;
+		float ExternalDt = 1/30.f;
+		float InternalDt = ExternalDt;
 
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//empty initially
-		auto* FirstData = Manager.GetProducerData_External();
-		EXPECT_NE(FirstData,nullptr);	//gives valid data to write into
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//still empty because haven't called step yet
-
-		Manager.Step_External(ExternalDt);
-
-		auto* SecondData = Manager.GetProducerData_External();
-		EXPECT_NE(SecondData,nullptr);	//gives valid data to write into immediately after calling step
-
-		auto* FirstDataInternal = Manager.ConsumeData_Internal(InternalTime,InternalDt);
-		EXPECT_NE(FirstDataInternal,nullptr);	//gives valid data to read from
-		EXPECT_EQ(FirstDataInternal,FirstData);	//we got the right data
-		Manager.FreeData_Internal(FirstDataInternal);
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//empty because already consumed
-
-		//consume data again
-		Manager.Step_External(ExternalDt);
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//new data starts at time 1/60 so this gives back nullptr
-		InternalTime += InternalDt;
-		auto* SecondDataInternal = Manager.ConsumeData_Internal(InternalTime,InternalDt);
-		EXPECT_NE(SecondDataInternal,nullptr);	//now we see valid data
-		EXPECT_EQ(SecondDataInternal,SecondData);
-		Manager.FreeData_Internal(SecondDataInternal);
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//empty because already consumed
-
-		//don't write anything
-		Manager.Step_External(ExternalDt);
-		InternalTime += InternalDt;
-		auto* ThirdDataInternal = Manager.ConsumeData_Internal(InternalTime,InternalDt);
-		EXPECT_NE(ThirdDataInternal,nullptr);	//still gives valid data to read from, even if it's empty
-		Manager.FreeData_Internal(ThirdDataInternal);
-		EXPECT_EQ(Manager.ConsumeData_Internal(InternalTime,InternalDt),nullptr);	//empty because already consumed
-
-		//data is reused and we only use 2 buffers total
-		for(int i=0; i<10; ++i)
+		TSet<FPushPhysicsData*> BuffersSeen;
+		for(int Step = 0; Step < 10; ++Step)
 		{
-			auto* AnotherExternalBuffer = Manager.GetProducerData_External();
-			auto* ExpectedBuffer = i % 2 == 0 ? SecondData : FirstData;
-			EXPECT_EQ(AnotherExternalBuffer,ExpectedBuffer);	//just keeps alternating back and forth
+			//Internal and external dt match so every internal step should get 1 data, the one we wrote to
+			const auto DataWritten = Manager.GetProducerData_External();
 			Manager.Step_External(ExternalDt);
-			InternalTime += InternalDt;
-			auto* InternalBuffer = Manager.ConsumeData_Internal(InternalTime,InternalDt);
-			EXPECT_EQ(InternalBuffer,ExpectedBuffer); //internal is the same as external was
-			Manager.FreeData_Internal(InternalBuffer);
+			const auto PushData = Manager.StepInternalTime_External(InternalDt);
+			EXPECT_EQ(PushData.Num(),1);
+			EXPECT_EQ(PushData[0],DataWritten);
+			
+			BuffersSeen.Add(DataWritten);
+			EXPECT_EQ(BuffersSeen.Num(),Step == 0 ? 1 : 2);	//we should only ever use two buffers when dts match because we just keep cycling back and forth
+
+			Manager.FreeData_Internal(PushData[0]);
 		}
-		
+
+		BuffersSeen.Empty();
+		InternalDt = ExternalDt * 0.5f;
+		//tick internal dt twice as fast, should only get data every other step
+		for(int Step = 0; Step < 10; ++Step)
+		{
+			const auto DataWritten = Manager.GetProducerData_External();
+			Manager.Step_External(ExternalDt);
+			for(int InternalStep = 0; InternalStep < 2; ++InternalStep)
+			{
+				const auto PushData = Manager.StepInternalTime_External(InternalDt);
+				if(InternalStep == 0)
+				{
+					EXPECT_EQ(PushData.Num(),1);
+					EXPECT_EQ(DataWritten,PushData[0]);
+					Manager.FreeData_Internal(PushData[0]);
+
+					BuffersSeen.Add(DataWritten);
+					EXPECT_EQ(BuffersSeen.Num(),Step == 0 ? 1 : 2);	//we should only ever use two buffers when dts match because we just keep cycling back and forth
+				}
+				else
+				{
+					EXPECT_EQ(PushData.Num(),0);
+				}
+			}
+		}
+
+		BuffersSeen.Empty();
+		InternalDt = ExternalDt * 2;
+		//tick internal dt for double the interval, should get two push datas per internal tick
+		for(int Step = 0; Step < 10; ++Step)
+		{
+			const auto DataWritten1 = Manager.GetProducerData_External();
+			Manager.Step_External(ExternalDt);
+
+			const auto DataWritten2 = Manager.GetProducerData_External();
+			Manager.Step_External(ExternalDt);
+
+			const auto PushData = Manager.StepInternalTime_External(InternalDt);
+			EXPECT_EQ(PushData.Num(),2);
+			EXPECT_EQ(PushData[0],DataWritten1);
+			EXPECT_EQ(PushData[1],DataWritten2);
+
+			for(FPushPhysicsData* Data : PushData)
+			{
+				BuffersSeen.Add(Data);
+				Manager.FreeData_Internal(Data);
+			}
+
+			EXPECT_EQ(BuffersSeen.Num(),Step == 0 ? 2 : 3);	//we should only ever use three buffers
+		}
 	}
 }
