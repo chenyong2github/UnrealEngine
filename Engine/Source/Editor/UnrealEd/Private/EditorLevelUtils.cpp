0534c7c07ffce1df8f9e88fc6856c48bfa50120a
jeanfrancois dube <JeanFrancois.Dube@epicgames.com>
2020-07-14 20:54:01 -0400

    Optimize FPackageAutoSaver::UpdateDirtyListsForPackage to avoid AddUnique calls when moving and duplicating actors. #rb francis.hurteau
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: jeanfrancois.dube
    #ROBOMERGE-SOURCE: CL 13855522 via CL 13855524 via CL 13855528 via CL 13855529
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13859858 by jeanfrancois dube in Dev-Networking branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorLevelUtils.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorLevelUtils.cpp
@@ -1225,10 +1225,8 @@ void UEditorLevelUtils::SetLevelsVisibility(const TArray<ULevel*>& Levels, const
 	}
 }
 
-void UEditorLevelUtils::GetWorlds(UWorld* InWorld, TArray<UWorld*>& OutWorlds, bool bIncludeInWorld, bool bOnlyEditorVisible)
+void UEditorLevelUtils::ForEachWorlds(UWorld* InWorld, TFunctionRef<bool(UWorld*)> Operation, bool bIncludeInWorld, bool bOnlyEditorVisible)
 {
-	OutWorlds.Empty();
-
 	if (!InWorld)
 	{
 		return;
@@ -1236,7 +1234,10 @@ void UEditorLevelUtils::GetWorlds(UWorld* InWorld, TArray<UWorld*>& OutWorlds, b
 
 	if (bIncludeInWorld)
 	{
-		OutWorlds.AddUnique(InWorld);
+		if (!Operation(InWorld))
+		{
+			return;
+		}
 	}
 
 	// Iterate over the world's level array to find referenced levels ("worlds"). We don't 
@@ -1257,7 +1258,10 @@ void UEditorLevelUtils::GetWorlds(UWorld* InWorld, TArray<UWorld*>& OutWorlds, b
 					UWorld* World = Cast<UWorld>(Level->GetOuter());
 					if (World)
 					{
-						OutWorlds.AddUnique(World);
+						if (!Operation(World))
+						{
+							return;
+						}
 					}
 				}
 			}
@@ -1273,10 +1277,19 @@ void UEditorLevelUtils::GetWorlds(UWorld* InWorld, TArray<UWorld*>& OutWorlds, b
 			UWorld* World = Cast<UWorld>(Level->GetOuter());
 			if (World)
 			{
-				OutWorlds.AddUnique(World);
+				if (!Operation(World))
+				{
+					return;
+				}
 			}
 		}
 	}
+}
+
+void UEditorLevelUtils::GetWorlds(UWorld* InWorld, TArray<UWorld*>& OutWorlds, bool bIncludeInWorld, bool bOnlyEditorVisible)
+{
+	OutWorlds.Empty();
+	ForEachWorlds(InWorld, [&OutWorlds](UWorld* World) { OutWorlds.AddUnique(World); return true; }, bIncludeInWorld, bOnlyEditorVisible);
 }
 
 #undef LOCTEXT_NAMESPACE
