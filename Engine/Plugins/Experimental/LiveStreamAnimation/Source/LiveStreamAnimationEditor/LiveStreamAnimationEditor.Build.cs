4f9d0913706ac4aacb2baeaf10d151314603134e
jon nabozny <Jon.Nabozny@epicgames.com>
2020-07-14 20:20:33 -0400

    Speculative fix for Editor Mac CIS issue.
    #rb None
    #rnx
    
    
    #ROBOMERGE-SOURCE: CL 13858021 via CL 13858586 via CL 13858650
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13858748 by jon nabozny in Release-Engine-Staging branch]

--- Engine/Plugins/Experimental/LiveStreamAnimation/Source/LiveStreamAnimationEditor/LiveStreamAnimationEditor.Build.cs
+++ Engine/Plugins/Experimental/LiveStreamAnimation/Source/LiveStreamAnimationEditor/LiveStreamAnimationEditor.Build.cs
@@ -1,22 +1,23 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using UnrealBuildTool;
-
-public class LiveStreamAnimationEditor : ModuleRules
+namespace UnrealBuildTool.Rules
 {
-	public LiveStreamAnimationEditor(ReadOnlyTargetRules Target) : base(Target)
+	public class LiveStreamAnimationEditor : ModuleRules
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		public LiveStreamAnimationEditor(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(
-			new string[] {
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
 				"LiveStreamAnimation",
 				"Core",
 				"CoreUObject",
 				"UnrealEd",
 				"AssetTools",
 				"LiveLinkInterface"
-			}
-		);
+				}
+			);
+		}
 	}
 }
