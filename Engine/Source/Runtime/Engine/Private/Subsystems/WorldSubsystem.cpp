d8459a9cdf9504648e4eda3b21aac42581427665
ryan mangin <Ryan.Mangin@epicgames.com>
2020-07-17 01:14:35 -0400

    Move conditions to create WorldSubsystem into ShouldCreateSubsystem + pieces of change from Dev-Editor-OpenWorld to get WorldSubsystems creatable for (editor) preview worlds.
    #jira: none
    #rb: aaron.mcleran, patrick.enfedaque
    
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ryan.mangin
    #ROBOMERGE-SOURCE: CL 13874476 via CL 13874478 via CL 13874480 via CL 13874487 via CL 13874490
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v717-13788717)
    
    [CL 13880717 by ryan mangin in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/Subsystems/WorldSubsystem.cpp
+++ Engine/Source/Runtime/Engine/Private/Subsystems/WorldSubsystem.cpp
@@ -13,3 +13,20 @@ UWorld* UWorldSubsystem::GetWorld() const
 {
 	return Cast<UWorld>(GetOuter());
 }
+
+bool UWorldSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!Super::ShouldCreateSubsystem(Outer))
+	{
+		return false;
+	}
+
+	UWorld* World = Cast<UWorld>(Outer);
+	check(World);
+	return DoesSupportWorldType(World->WorldType);
+}
+
+bool UWorldSubsystem::DoesSupportWorldType(EWorldType::Type WorldType) const
+{
+	return WorldType == EWorldType::Game || WorldType == EWorldType::Editor || WorldType == EWorldType::PIE;
+}
