c9dc3fdce8319ff4ebdb6655dcc88bb0b72502b1
jason stasik <Jason.Stasik@epicgames.com>
2020-07-17 13:03:54 -0400

    Remove startup loading of editor compilation sounds #rb rex.hill #rnx
    #ROBOMERGE-SOURCE: CL 13884646 via CL 13884672 via CL 13884688
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13884703 by jason stasik in Release-Engine-Staging branch]

--- Engine/Source/Editor/MainFrame/Private/MainFrameModule.cpp
+++ Engine/Source/Editor/MainFrame/Private/MainFrameModule.cpp
@@ -514,14 +514,6 @@ void FMainFrameModule::StartupModule( )
 	SourceCodeAccessModule.OnOpenFileFailed().AddRaw( this, &FMainFrameModule::HandleCodeAccessorOpenFileFailed );
 #endif
 
-	// load sounds
-	CompileStartSound = LoadObject<USoundBase>(NULL, TEXT("/Engine/EditorSounds/Notifications/CompileStart_Cue.CompileStart_Cue"));
-	CompileStartSound->AddToRoot();
-	CompileSuccessSound = LoadObject<USoundBase>(NULL, TEXT("/Engine/EditorSounds/Notifications/CompileSuccess_Cue.CompileSuccess_Cue"));
-	CompileSuccessSound->AddToRoot();
-	CompileFailSound = LoadObject<USoundBase>(NULL, TEXT("/Engine/EditorSounds/Notifications/CompileFailed_Cue.CompileFailed_Cue"));
-	CompileFailSound->AddToRoot();
-
 	ModuleCompileStartTime = 0.0f;
 
 	// migrate old layout settings
@@ -561,33 +553,6 @@ void FMainFrameModule::ShutdownModule( )
 		SourceCodeAccessModule.OnOpenFileFailed().RemoveAll( this );
 	}
 #endif
-
-	if(CompileStartSound != NULL)
-	{
-		if (!GExitPurge)
-		{
-			CompileStartSound->RemoveFromRoot();
-		}
-		CompileStartSound = NULL;
-	}
-
-	if(CompileSuccessSound != NULL)
-	{
-		if (!GExitPurge)
-		{
-			CompileSuccessSound->RemoveFromRoot();
-		}
-		CompileSuccessSound = NULL;
-	}
-
-	if(CompileFailSound != NULL)
-	{
-		if (!GExitPurge)
-		{
-			CompileFailSound->RemoveFromRoot();
-		}
-		CompileFailSound = NULL;
-	}
 }
 
 
@@ -614,7 +579,7 @@ void FMainFrameModule::HandleLevelEditorModuleCompileStarted( bool bIsAsyncCompi
 
 	if ( GEditor )
 	{
-		GEditor->PlayEditorSound(CompileStartSound);
+		GEditor->PlayEditorSound(TEXT("/Engine/EditorSounds/Notifications/CompileStart_Cue.CompileStart_Cue"));
 	}
 
 	FNotificationInfo Info( NSLOCTEXT("MainFrame", "RecompileInProgress", "Compiling C++ Code") );
@@ -665,7 +630,7 @@ void FMainFrameModule::HandleLevelEditorModuleCompileFinished(const FString& Log
 		{
 			if ( GEditor )
 			{
-				GEditor->PlayEditorSound(CompileSuccessSound);
+				GEditor->PlayEditorSound(TEXT("/Engine/EditorSounds/Notifications/CompileSuccess_Cue.CompileSuccess_Cue"));
 			}
 
 			NotificationItem->SetText(NSLOCTEXT("MainFrame", "RecompileComplete", "Compile Complete!"));
@@ -685,7 +650,7 @@ void FMainFrameModule::HandleLevelEditorModuleCompileFinished(const FString& Log
 
 			if ( GEditor )
 			{
-				GEditor->PlayEditorSound(CompileFailSound);
+				GEditor->PlayEditorSound(TEXT("/Engine/EditorSounds/Notifications/CompileFailed_Cue.CompileFailed_Cue"));
 			}
 
 			if (CompilationResult == ECompilationResult::FailedDueToHeaderChange)
@@ -733,7 +698,7 @@ void FMainFrameModule::HandleHotReloadFinished( bool bWasTriggeredAutomatically 
 		NotificationItem->SetCompletionState(SNotificationItem::CS_Success);
 		NotificationItem->ExpireAndFadeout();
 	
-		GEditor->PlayEditorSound(CompileSuccessSound);
+		GEditor->PlayEditorSound(TEXT("/Engine/EditorSounds/Notifications/CompileSuccess_Cue.CompileSuccess_Cue"));
 	}
 }
 
