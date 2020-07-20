c528ff58dce77b424c932bbc6772d89f92fc5b4f
daren cheng <daren.cheng@epicgames.com>
2020-07-20 14:19:14 -0400

    Fix Instances of the same Material Parameter can be assigned different Sort Information.
    #jira UE-81846
    #rb lauren.barnes
    
    
    #ROBOMERGE-SOURCE: CL 13896860 via CL 13897005 via CL 13897127
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13897266 by daren cheng in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
@@ -4421,6 +4421,8 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 
 		DestTex->Modify();
 		DestTex->Texture = SourceTex->Texture;
+		DestTex->Group = SourceTex->Group;
+		DestTex->SortPriority = SourceTex->SortPriority;
 	}
 	else if(Source->IsA(UMaterialExpressionVectorParameter::StaticClass()))
 	{
@@ -4429,6 +4431,8 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 
 		DestVec->Modify();
 		DestVec->DefaultValue = SourceVec->DefaultValue;
+		DestVec->Group = SourceVec->Group;
+		DestVec->SortPriority = SourceVec->SortPriority;
 	}
 	else if(Source->IsA(UMaterialExpressionStaticBoolParameter::StaticClass()))
 	{
@@ -4437,6 +4441,8 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 
 		DestVec->Modify();
 		DestVec->DefaultValue = SourceVec->DefaultValue;
+		DestVec->Group = SourceVec->Group;
+		DestVec->SortPriority = SourceVec->SortPriority;
 	}
 	else if(Source->IsA(UMaterialExpressionStaticComponentMaskParameter::StaticClass()))
 	{
@@ -4448,6 +4454,8 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 		DestVec->DefaultG = SourceVec->DefaultG;
 		DestVec->DefaultB = SourceVec->DefaultB;
 		DestVec->DefaultA = SourceVec->DefaultA;
+		DestVec->Group = SourceVec->Group;
+		DestVec->SortPriority = SourceVec->SortPriority;
 	}
 	else if(Source->IsA(UMaterialExpressionScalarParameter::StaticClass()))
 	{
@@ -4456,6 +4464,8 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 
 		DestVec->Modify();
 		DestVec->DefaultValue = SourceVec->DefaultValue;
+		DestVec->Group = SourceVec->Group;
+		DestVec->SortPriority = SourceVec->SortPriority;
 	}
 	else if(Source->IsA(UMaterialExpressionFontSampleParameter::StaticClass()))
 	{
@@ -4465,10 +4475,18 @@ bool UMaterial::CopyExpressionParameters(UMaterialExpression* Source, UMaterialE
 		DestFont->Modify();
 		DestFont->Font = SourceFont->Font;
 		DestFont->FontTexturePage = SourceFont->FontTexturePage;
+		DestFont->Group = SourceFont->Group;
+		DestFont->SortPriority = SourceFont->SortPriority;
 	}
 	else
 	{
 		bRet = false;
+	}
+
+	if (bRet)
+	{
+		Destination->Desc = Source->Desc;
+		Destination->GraphNode->OnUpdateCommentText(Destination->Desc);
 	}
 
 	return bRet;
