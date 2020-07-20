88c41d3b1e930a83ae2026619a1b5b1b79c74387
chris varnsverry <Chris.Varnsverry@xa.epicgames.com>
2020-07-20 09:39:07 -0400

    - Integrate BillingApiV2 changes from UE4/Main - Add ini config to select between BillingApiV2/AIDL at build time (default BillingApiV2)
    [at]Michael.Kirzinger [at]Sam.Zamani [at]Chris.Babcock
    
    
    #ROBOMERGE-OWNER: chris.varnsverry
    #ROBOMERGE-AUTHOR: chris.varnsverry
    #ROBOMERGE-SOURCE: CL 13892059 via CL 13892073 via CL 13892080 via CL 13892321
    #ROBOMERGE-BOT: (v717-13788717)
    
    [CL 13892325 by chris varnsverry in Release-Engine-Staging branch]

--- Engine/Plugins/Online/Android/OnlineSubsystemGooglePlay/Source/Private/OnlineStoreGooglePlayCommon.h
+++ Engine/Plugins/Online/Android/OnlineSubsystemGooglePlay/Source/Private/OnlineStoreGooglePlayCommon.h
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+
 #include "Interfaces/OnlineStoreInterface.h"
 #include "Interfaces/OnlineStoreInterfaceV2.h"
 #include "Interfaces/OnlinePurchaseInterface.h"
@@ -104,6 +105,24 @@ enum class EInAppPurchaseResult : uint8
 	Cancelled,
 };
 
+#if OSSGOOGLEPLAY_WITH_AIDL
+/**
+ * Implementation of the Platform Purchase receipt. For this we provide an identifier and the encrypted data.
+ */
+class FGooglePlayPurchaseReceipt : public IPlatformPurchaseReceipt
+{
+public:
+	// Product identifier
+	FString Identifier;
+
+	// The encrypted receipt data
+	FString Data;
+};
+
+typedef FInAppPurchaseProductInfo FProvidedProductInformation;
+#else
+typedef FOnlineStoreOffer FProvidedProductInformation;
+#endif
 
  /**
   * Delegate fired when an IAP query for available offers has completed
@@ -111,7 +130,7 @@ enum class EInAppPurchaseResult : uint8
   * @param Response response from GooglePlay backend
   * @param ProvidedProductInformation list of offers returned in response to a query on available offer ids
   */
-DECLARE_MULTICAST_DELEGATE_TwoParams(FOnGooglePlayAvailableIAPQueryComplete, EGooglePlayBillingResponseCode /*Response*/, const TArray<FOnlineStoreOffer>& /*ProvidedProductInformation*/);
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnGooglePlayAvailableIAPQueryComplete, EGooglePlayBillingResponseCode /*Response*/, const TArray<FProvidedProductInformation>& /*ProvidedProductInformation*/);
 typedef FOnGooglePlayAvailableIAPQueryComplete::FDelegate FOnGooglePlayAvailableIAPQueryCompleteDelegate;
 
 /**
