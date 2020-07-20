665f368fc562413a421edfc710cbf523bea0e8c2
daren cheng <daren.cheng@epicgames.com>
2020-07-20 14:20:43 -0400

    Set the Default Sort Order of Parameter Groups to 32.
    #jira UE-95288
    #rb lauren.barnes
    
    #ROBOMERGE-SOURCE: CL 13896886 via CL 13897010 via CL 13897135 via CL 13897276
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v717-13788717)
    
    [CL 13897306 by daren cheng in Main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
@@ -2675,7 +2675,6 @@ UMaterialExpressionTextureSampleParameter::UMaterialExpressionTextureSampleParam
 
 	MenuCategories.Empty();
 	MenuCategories.Add( ConstructorStatics.NAME_Obsolete);
-	SortPriority = 0;
 	ApplyChannelNames();
 #endif
 }
@@ -7313,7 +7312,6 @@ UMaterialExpressionParameter::UMaterialExpressionParameter(const FObjectInitiali
 
 #if WITH_EDITORONLY_DATA
 	MenuCategories.Add(ConstructorStatics.NAME_Parameters);
-	SortPriority = 0;
 
 	bCollapsed = false;
 #endif
