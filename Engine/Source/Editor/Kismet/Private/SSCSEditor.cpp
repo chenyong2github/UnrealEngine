2fcbf50415ae4754fa3d7da64ce627577af8255b
Marc Audy <Marc.Audy@epicgames.com>
2020-07-23 19:45:57 -0400

    Update the scs editor tree after reseting to blueprint defaults as the instance components are removed but will still appear in the tree without the call to UpdateTree #jira UE-95507 #rb Dan.OConnor
    [CL 13940184 by Marc Audy in Dev-Framework branch]

--- Engine/Source/Editor/Kismet/Private/SSCSEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/SSCSEditor.cpp
@@ -7142,7 +7142,7 @@ void SSCSEditor::OnApplyChangesToBlueprint() const
 	}
 }
 
-void SSCSEditor::OnResetToBlueprintDefaults() const
+void SSCSEditor::OnResetToBlueprintDefaults()
 {
 	int32 NumChangedProperties = 0;
 
@@ -7194,6 +7194,8 @@ void SSCSEditor::OnResetToBlueprintDefaults() const
 			NotificationInfo.Text = LOCTEXT("ResetToBlueprintDefaults_Failed", "No properties were reset");
 			CompletionState = SNotificationItem::CS_Fail;
 		}
+
+		UpdateTree();
 
 		// Add the notification to the queue
 		const TSharedPtr<SNotificationItem> Notification = FSlateNotificationManager::Get().AddNotification(NotificationInfo);
