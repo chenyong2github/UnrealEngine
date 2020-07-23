4323a69b799478e2775c25056bd3b8698210d924
ryan hairyes <Ryan.Hairyes@epicgames.com>
2020-07-23 09:15:39 -0400

    Reimplement PlayTogether (OSS) [at]rob.cannaday #rb rob.cannaday #robomerge[RELEASE] 4.25Plus
    #ROBOMERGE-SOURCE: CL 13931543 via CL 13931546
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 13931551 by ryan hairyes in Release-Engine-Staging branch]

--- Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineSubsystemUtils/Classes/OnlineSessionClient.h
+++ Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineSubsystemUtils/Classes/OnlineSessionClient.h
@@ -35,6 +35,9 @@ protected:
 	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
 	/** Delegate for accepting session invites */
 	FOnSessionUserInviteAcceptedDelegate OnSessionUserInviteAcceptedDelegate;
+	/** Delegate for handling the play together system event.  Will be removed. */
+	UE_DEPRECATED(4.26, "PlayTogether will no longer be supported and should be removed.")
+	FOnPlayTogetherEventReceivedDelegate OnPlayTogetherEventReceivedDelegate;
 
 	// Handles to the above delegates
 	FDelegateHandle OnSessionInviteAcceptedDelegateHandle;
@@ -42,6 +45,9 @@ protected:
 	FDelegateHandle OnDestroyForJoinSessionCompleteDelegateHandle;
 	FDelegateHandle OnDestroyForMainMenuCompleteDelegateHandle;
 	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+	/** Deprecated.  Will be removed. */
+	UE_DEPRECATED(4.26, "PlayTogether will no longer be supported and should be removed.")
+	FDelegateHandle OnPlayTogetherEventReceivedDelegateHandle;
 
 	/** Handle to outstanding start session call */
 	FDelegateHandle StartSessionCompleteHandle;
