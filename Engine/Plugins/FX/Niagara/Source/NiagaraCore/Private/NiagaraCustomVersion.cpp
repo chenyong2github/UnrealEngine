e69b680ec36068a496c06faae01939b8c59093fc
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-07-23 10:56:02 -0400

    Fix for RapidIterationParameters being out of date when they are being baked out by the cooker.  Moves the generation of the correct values to the pre-compile so that when we actually trigger the translation we can use the correct values. #jira ue-95591, UE095587 #rb frank.fella
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 13930448 in //UE4/Main/...
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v718-13788717)
    
    [CL 13932316 by rob krajcarski in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x09EDF825, 0x68F84495, 0x9AB669AE, 0x0ECD4178);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3F0AD1B5, 0xA67C484C, 0x959D2D4B, 0x691016CD);
