6cb789d8bedb73f29e053ff5d8e37a74d7c27101
sebastien hillaire <Sebastien.Hillaire@epicgames.com>
2020-07-28 11:12:08 -0400

    Added a way to disable the sky light real time capture using a scalability cvar.
    #rb none
    [FYI] jordan.walker
    
    
    #ROBOMERGE-SOURCE: CL 13954918 via CL 13955199 via CL 13955237
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 13955280 by sebastien hillaire in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
@@ -80,6 +80,14 @@ FAutoConsoleVariableRef CVarSkylightIntensityMultiplier(
 	ECVF_Scalability | ECVF_RenderThreadSafe
 	);
 
+int32 GSkylightRealTimeCaptureAllowed = 1;
+FAutoConsoleVariableRef CVarSkylightRealTimeCaptureAllowed(
+	TEXT("r.SkylightRealTimeCaptureAllowed"),
+	GSkylightRealTimeCaptureAllowed,
+	TEXT("Make sure the sky light real time capture is not run on platform where it is considered out of budget. Cannot be changed at runtime."),
+	ECVF_Scalability
+);
+
 constexpr EPixelFormat SKYLIGHT_CUBEMAP_FORMAT = PF_FloatRGBA;
 
 void FSkyTextureCubeResource::InitRHI()
@@ -972,7 +980,7 @@ bool USkyLightComponent::IsOcclusionSupported() const
 
 bool USkyLightComponent::IsRealTimeCaptureEnabled() const
 {
-	return bRealTimeCapture && Mobility == EComponentMobility::Movable;
+	return bRealTimeCapture && Mobility == EComponentMobility::Movable && GSkylightRealTimeCaptureAllowed>0;
 }
 
 void USkyLightComponent::OnVisibilityChanged()
