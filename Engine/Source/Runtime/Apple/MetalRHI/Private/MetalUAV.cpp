41616ead1a7364d42fdd4641396258504c9e857c
will damon <Will.Damon@epicgames.com>
2020-07-29 10:17:24 -0400

    Remove inaccurate check that triggers with zero-sized VBs/IBs.
    Resources like FRawStaticIndexBuffer can be created with zero-size (and no real RHI backing).
    
    #rb richard.wallis
    #jira none
    #rnx
    #lockdown marcus.wassmer
    
    [CL 13963032 by will damon in Main branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalUAV.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalUAV.cpp
@@ -61,12 +61,10 @@ ns::AutoReleased<FMetalTexture> FMetalShaderResourceView::GetLinearTexture(bool 
 		if (IsValidRef(SourceVertexBuffer))
 		{
 			NewLinearTexture = SourceVertexBuffer->GetLinearTexture((EPixelFormat)Format, LinearTextureDesc);
-			check(NewLinearTexture);
 		}
 		else if (IsValidRef(SourceIndexBuffer))
 		{
 			NewLinearTexture = SourceIndexBuffer->GetLinearTexture((EPixelFormat)Format, LinearTextureDesc);
-			check(NewLinearTexture);
 		}
 	}
 	return NewLinearTexture;
