33f1453ca1a9695a5604d4e3a3ed952b99f1e619
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-07-30 09:56:23 -0400

    Fix for VM Compiler mishandling the expression: MyVec3.yz = float2(1.0f, 2.0f); #rb simon.tovey
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 13972832 via CL 13972948 via CL 13972951
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 13972973 by rob krajcarski in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x35F51C82, 0x0A277C4F, 0x9106CD9E, 0x85E40366);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x41E5CC5B, 0x258646C6, 0xB8500D95, 0xC4B893B4);
