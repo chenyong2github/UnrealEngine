cfa23cece081eccd3ff662473176ec16c6c98a9b
rex hill <Rex.Hill@epicgames.com>
2020-07-31 20:12:51 -0400

    Add movie pipeline queue subsystem for runtime #rb matt.hoffman [FYI] matt.hoffman
    #ROBOMERGE-SOURCE: CL 13973391 via CL 13973406 via CL 13973420
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v718-13788717)
    
    [CL 13998889 by rex hill in Main branch]

---
+++ Engine/Plugins/MovieScene/MovieRenderPipeline/Source/MovieRenderPipelineCore/Private/MoviePipelineQueueEngineSubsystem.cpp
@@ -0,0 +1,33 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoviePipelineQueueEngineSubsystem.h"
+#include "Modules/ModuleManager.h"
+#include "MoviePipeline.h"
+
+UMoviePipelineExecutorBase* UMoviePipelineQueueEngineSubsystem::RenderQueueWithExecutor(TSubclassOf<UMoviePipelineExecutorBase> InExecutorType)
+{
+	if(!ensureMsgf(!IsRendering(), TEXT("RenderQueueWithExecutor cannot be called while already rendering!")))
+	{
+		return nullptr;
+	}
+		
+	ActiveExecutor = NewObject<UMoviePipelineExecutorBase>(this, InExecutorType);
+	ActiveExecutor->SetMoviePipelineClass(UMoviePipeline::StaticClass());
+	ActiveExecutor->OnExecutorFinished().AddUObject(this, &UMoviePipelineQueueEngineSubsystem::OnExecutorFinished);
+	ActiveExecutor->Execute(GetQueue());
+	return ActiveExecutor;
+}
+
+void UMoviePipelineQueueEngineSubsystem::OnSequencerContextBinding(bool& bAllowBinding)
+{
+	if (ActiveExecutor && ActiveExecutor->IsRendering())
+	{
+		bAllowBinding = false;
+	}
+}
+
+void UMoviePipelineQueueEngineSubsystem::OnExecutorFinished(UMoviePipelineExecutorBase* InPipelineExecutor, bool bSuccess)
+{
+	ActiveExecutor = nullptr;
+}
+
