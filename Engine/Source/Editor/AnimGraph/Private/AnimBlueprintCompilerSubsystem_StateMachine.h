f319cf93aa4f8c2cede94f29769b498110d53d3c
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2020-08-02 17:17:30 -0400

    IWYU fixes
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: thomas.sarkanen
    #ROBOMERGE-SOURCE: CL 13933883 via CL 13933890 via CL 13933895 via CL 13994613
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v718-13788717)
    
    [CL 14004211 by thomas sarkanen in Dev-Tools-Staging branch]

--- Engine/Source/Editor/AnimGraph/Private/AnimBlueprintCompilerSubsystem_StateMachine.h
+++ Engine/Source/Editor/AnimGraph/Private/AnimBlueprintCompilerSubsystem_StateMachine.h
@@ -14,6 +14,7 @@ class UAnimGraphNode_Base;
 class UAnimStateTransitionNode;
 class UEdGraph;
 class UEdGraphNode;
+struct FAnimNotifyEvent;
 
 UCLASS()
 class UAnimBlueprintCompilerSubsystem_StateMachine : public UAnimBlueprintCompilerSubsystem
