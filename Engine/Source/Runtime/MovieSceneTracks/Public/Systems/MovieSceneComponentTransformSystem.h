68daa3d2f1ea1b42202c22510f6c3b0ff8181c9b
andrew rodham <Andrew.Rodham@epicgames.com>
2020-08-02 17:48:28 -0400

    Sequencer: Converted component transform interrogation to use intermediate transforms
     - This allows us to interrogate over-rotated transforms correctly
    
    #rb Max.Chen
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: andrew.rodham
    #ROBOMERGE-SOURCE: CL 13978226 via CL 13985406 via CL 13985465 via CL 13999154
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v718-13788717)
    
    [CL 14006640 by andrew rodham in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/MovieSceneTracks/Public/Systems/MovieSceneComponentTransformSystem.h
+++ Engine/Source/Runtime/MovieSceneTracks/Public/Systems/MovieSceneComponentTransformSystem.h
@@ -57,7 +57,7 @@ public:
 
 	UMovieSceneComponentTransformSystem(const FObjectInitializer& ObjInit);
 
-	MOVIESCENETRACKS_API void Interrogate(TArray<FTransform>& OutTransforms) const;
+	MOVIESCENETRACKS_API void Interrogate(TArray<UE::MovieScene::FIntermediate3DTransform>& OutTransforms) const;
 
 private:
 
