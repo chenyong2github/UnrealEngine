54816ef20bb057503c8c4590497fdb0ab44c92d2
chris varnsverry <Chris.Varnsverry@xa.epicgames.com>
2020-08-02 17:50:20 -0400

    - Split out the SKPaymentTransactionObserver portion of FOnlineStoreKitHelper into FPaymentTransactionObserver, allowing us to bind an observer during didFinishLaunchWithOptions, and later bind the FOnlineStoreKitHelper to that.
    [at]Josh.Markiewicz [at]Sam.Zamani
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: chris.varnsverry
    #ROBOMERGE-SOURCE: CL 13988088 via CL 13988089 via CL 13988092 via CL 13988093 via CL 14000115
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v718-13788717)
    
    [CL 14006810 by chris varnsverry in Dev-Tools-Staging branch]

--- Engine/Plugins/Online/IOS/OnlineSubsystemIOS/Source/Private/OnlineSubsystemIOS.cpp
+++ Engine/Plugins/Online/IOS/OnlineSubsystemIOS/Source/Private/OnlineSubsystemIOS.cpp
@@ -210,15 +210,15 @@ void FOnlineSubsystemIOS::InitStoreKitHelper()
 	// Give each interface a chance to bind to the store kit helper
 	StoreV2Interface->InitStoreKit(StoreHelper);
 	PurchaseInterface->InitStoreKit(StoreHelper);
-	
-	// Bind the observer after the interfaces have bound their delegates
-	[[SKPaymentQueue defaultQueue] addTransactionObserver:StoreHelper];
+
+	// Pump the event queue to handle any events that came in between launch and now.
+	[StoreHelper pumpObserverEventQueue];
 }
 
 void FOnlineSubsystemIOS::CleanupStoreKitHelper()
 {
-	// @todo MetalMRT: This needs to be analyzed with ASAN - but this pointer is garbage on shutdown...
-	// [StoreHelper release];
+	[StoreHelper release];
+	StoreHelper = nil;
 }
 
 void FOnlineSubsystemIOS::InitAppStoreHelper()
