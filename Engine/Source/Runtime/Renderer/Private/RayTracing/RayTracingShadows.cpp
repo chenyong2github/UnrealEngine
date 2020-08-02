75f1d3719e1d55d43bfe0fd7a6d3568607ef5018
patrick kelly <Patrick.Kelly@epicgames.com>
2020-08-02 17:32:27 -0400

    Changes to ray tracing, subsurface shadows.
    Removed second application of shadow term, which was preventing backscatter contribution for directional light SSS shadowing.
    
    Added CVars to help define transition distance for decoupling/coupling transmission as part of the visibility term.
    1) r.RayTracing.Shadows.LODTransitionStart (default = 4000)
    2) r.RayTracing.Shadows.LODTransitionEnd (default = 5000)
    Default settings correspond with default WPO culling distance, since the effect is most prominent on animated subsurface shadows.
    
    Before the transition range, subsurface visibility and transmission will remain decoupled
    After the transition range, subsurface visibility and transmission will be the same
    The effect is linearly interpolated within the transition distance.
    
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: patrick.kelly
    #ROBOMERGE-SOURCE: CL 13958551 via CL 13958668 via CL 13958694 via CL 13958730 via CL 13997593
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v718-13788717)
    
    [CL 14005391 by patrick kelly in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracingShadows.cpp
+++ Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracingShadows.cpp
@@ -48,6 +48,20 @@ static TAutoConsoleVariable<int32> CVarRayTracingShadowsEnableHairVoxel(
 	ECVF_RenderThreadSafe
 );
 
+static TAutoConsoleVariable<int32> CVarRayTracingShadowsLODTransitionStart(
+	TEXT("r.RayTracing.Shadows.LODTransitionStart"),
+	4000.0, // 40 m
+	TEXT("The start of an LOD transition range (default = 4000)"),
+	ECVF_RenderThreadSafe
+);
+
+static TAutoConsoleVariable<int32> CVarRayTracingShadowsLODTransitionEnd(
+	TEXT("r.RayTracing.Shadows.LODTransitionEnd"),
+	5000.0f, // 50 m
+	TEXT("The end of an LOD transition range (default = 5000)"),
+	ECVF_RenderThreadSafe
+);
+
 bool EnableRayTracingShadowTwoSidedGeometry()
 {
 	return CVarRayTracingShadowsEnableTwoSidedGeometry.GetValueOnRenderThread() != 0;
@@ -101,6 +115,8 @@ class FOcclusionRGS : public FGlobalShader
 		SHADER_PARAMETER(FIntPoint, PixelOffset)
 		SHADER_PARAMETER(uint32, bUseHairVoxel)
 		SHADER_PARAMETER(float, TraceDistance)
+		SHADER_PARAMETER(float, LODTransitionStart)
+		SHADER_PARAMETER(float, LODTransitionEnd)
 
 		SHADER_PARAMETER_STRUCT(FLightShaderParameters, Light)
 		SHADER_PARAMETER_STRUCT_INCLUDE(FSceneTextureParameters, SceneTextures)
@@ -219,6 +235,8 @@ void FDeferredShadingSceneRenderer::RenderRayTracingShadows(
 		PassParameters->LightingChannelMask = LightSceneProxy->GetLightingChannelMask();
 		LightSceneProxy->GetLightShaderParameters(PassParameters->Light);
 		PassParameters->TraceDistance = LightSceneProxy->GetTraceDistance();
+		PassParameters->LODTransitionStart = CVarRayTracingShadowsLODTransitionStart.GetValueOnRenderThread();
+		PassParameters->LODTransitionEnd = CVarRayTracingShadowsLODTransitionEnd.GetValueOnRenderThread();
 		PassParameters->TLAS = View.RayTracingScene.RayTracingSceneRHI->GetShaderResourceView();
 		PassParameters->ViewUniformBuffer = View.ViewUniformBuffer;
 		PassParameters->SceneTextures = SceneTextures;
