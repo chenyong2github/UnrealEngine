826b48edd917c858d4f5110bc5d8dddb20719efb
thomas ross <Thomas.Ross@epicgames.com>
2020-08-03 12:24:18 -0400

    Changing BackgroundHttpFileHash to have a private module dependency instead of private include for BackgroundHttpFileHash to fix linking problems when not using the BackgroundHTTP module. #tests iOS compile #rb none [FYI] Marc.Audy
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: thomas.ross
    #ROBOMERGE-SOURCE: CL 14007662 via CL 14007663 via CL 14007667 via CL 14007669 via CL 14007671
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v718-13788717)
    
    [CL 14013857 by thomas ross in Dev-Networking branch]

--- Engine/Source/Runtime/ApplicationCore/ApplicationCore.Build.cs
+++ Engine/Source/Runtime/ApplicationCore/ApplicationCore.Build.cs
@@ -69,7 +69,7 @@ public class ApplicationCore : ModuleRules
 			ModuleSymbolVisibility = ModuleRules.SymbolVisibility.VisibileForDll;
 			
 			//Need to add this as BackgroundHTTP files can end up doing work directly from our AppDelegate in iOS and thus we need acccess to correct file locations to save these very early.
-			PrivateIncludePathModuleNames.Add("BackgroundHTTPFileHash");
+			PrivateDependencyModuleNames.Add("BackgroundHTTPFileHash");
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Android || Target.Platform == UnrealTargetPlatform.Lumin)
 		{
