8a74b636045198a54055945759efab0c168d4fc3
stu mckenna <Stu.McKenna@epicgames.com>
2020-08-04 17:07:25 -0400

    - Added NiagaraScriptBase to remove peeking into a class that we do not link against - More work is required to remove all NiagaraShader libraries dependencies on the Niagara library
    #rb shaun.kime, rob.krajcarski
    
    
    #ROBOMERGE-SOURCE: CL 14029169 via CL 14029180
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 14029188 by stu mckenna in Release-Engine-Staging branch]

---
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraScriptBase.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NiagaraScriptBase.h"
+
+UNiagaraScriptBase::UNiagaraScriptBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
