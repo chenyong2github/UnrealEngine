fdf014d6bdc77c34b3b89128d508bc2b27b9e4b6
bob tellez <Bob.Tellez@epicgames.com>
2020-08-06 11:51:56 -0400

    #UE4 CIS fix
    [FYI] Jon.Nabozny
    
    
    #ROBOMERGE-SOURCE: CL 14047843 via CL 14047853
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 14047858 by bob tellez in Release-Engine-Staging branch]

--- Engine/Plugins/Experimental/LiveStreamAnimation/Source/LSALiveLink/Private/LSALiveLinkStreamingHelper.cpp
+++ Engine/Plugins/Experimental/LiveStreamAnimation/Source/LSALiveLink/Private/LSALiveLinkStreamingHelper.cpp
@@ -7,6 +7,8 @@
 #include "LSALiveLinkPacket.h"
 #include "LSALiveLinkSource.h"
 #include "LSALiveLinkSkelMeshSource.h"
+#include "Serialization/MemoryWriter.h"
+#include "LiveStreamAnimationRole.h"
 
 #include "Animation/Skeleton.h"
 
