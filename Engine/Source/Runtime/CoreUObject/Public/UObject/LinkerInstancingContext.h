bf9bb4b54149969cffdac490f6c5bd5716de59d5
francis hurteau <Francis.Hurteau@epicgames.com>
2020-08-10 17:53:14 -0400

    Dynamic Import Injection: Added a loading step in the linker load tick where an external hook provided via the instancing context can be called to inject additional package import. Those new import gets resolved as usual afterward, in Editor through the Verify process, in EDL they would require additional dependency arcs which aren't implemented here as the feature is mainly used the the External Packages (i.e OFPA).
    #rb JeanFrancois.Dube, Robert.Manuszewski
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: francis.hurteau
    #ROBOMERGE-SOURCE: CL 14027480 via CL 14028086 via CL 14028143 via CL 14028339
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v718-13788717)
    
    [CL 14072848 by francis hurteau in Dev-Networking branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/LinkerInstancingContext.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/LinkerInstancingContext.h
@@ -1,6 +1,34 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+#include "UObject/NameTypes.h"
+#include "Templates/Function.h"
+#include "Containers/Array.h"
+#include "Containers/Map.h"
+
+/**
+ * Class containing needed information to inject a package import dynamically (and potentially any import in the future)
+ * within another package.
+ * A dynamic package import will create a regular import in the linker import table to resolve normally afterward
+ */
+class FDynamicPackageImport
+{
+public:
+	FDynamicPackageImport(FName InPackageLoadName)
+		: PackageLoadName(InPackageLoadName)
+	{}
+
+	FName GetPackageName() const
+	{
+		return PackageLoadName;
+	}
+
+private:
+	FName PackageLoadName;
+};
+
+// Type of the callback for dynamic import resolving
+typedef TFunction<TArray<FDynamicPackageImport>(const FLinkerLoad&)> FDynamicImportCallback;
 
 /**
  * Helper class to remap package imports during loading.
@@ -15,6 +43,8 @@ public:
 	FLinkerInstancingContext(TMap<FName, FName> InInstanceMapping)
 		: Mapping(MoveTemp(InInstanceMapping))
 	{}
+
+	COREUOBJECT_API static FName GenerateInstancedName(FName PackageLoadName, FName DependantPackageName);
 
 	bool IsInstanced() const
 	{
@@ -41,9 +71,17 @@ public:
 		Mapping.Append(NewMapping);
 	}
 
+	void AddResolver(FDynamicImportCallback InResolver)
+	{
+		DynamicImportResolvers.Add(MoveTemp(InResolver));
+	}
+
 private:
 	friend class FLinkerLoad;
 
 	/** Map of original object name to their instance counterpart. */
 	TMap<FName, FName> Mapping;
+
+	/** List of callbacks to call to inject dynamic imports. */
+	TArray<FDynamicImportCallback> DynamicImportResolvers;
 };
