786e54c58703af53ba02be8d9ee051b78e34833f
jack porter <jack.porter@epicgames.com>
2020-08-11 09:29:41 -0400

    Restore Android PF_A8 broken by CL 14055508 which caused font corruption #rb Florin.Pascu [FYI] guillaume.abadie
    #ROBOMERGE-SOURCE: CL 14076057 via CL 14076058 via CL 14076358
    #ROBOMERGE-BOT: (v718-13788717)
    
    [CL 14076378 by jack porter in Release-Engine-Staging branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
@@ -999,7 +999,7 @@ static void InitRHICapabilitiesForGL()
 		SetupTextureFormat(PF_R8G8B8A8, FOpenGLTextureFormat(GL_RGBA8, GL_SRGB8_ALPHA8, GL_RGBA, GL_UNSIGNED_BYTE, false, false));
 		SetupTextureFormat(PF_R8G8B8A8_UINT, FOpenGLTextureFormat(GL_RGBA8UI, GL_RGBA8UI, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, false, false));
 		SetupTextureFormat(PF_G8, FOpenGLTextureFormat(GL_R8, GL_R8, GL_RED, GL_UNSIGNED_BYTE, false, false));
-		SetupTextureFormat(PF_A8, FOpenGLTextureFormat(GL_R8, GL_ALPHA, GL_ALPHA, GL_ALPHA, GL_ALPHA, GL_UNSIGNED_BYTE, false, false));
+		SetupTextureFormat(PF_A8, FOpenGLTextureFormat(GL_ALPHA, GL_ALPHA, GL_ALPHA, GL_ALPHA, GL_ALPHA, GL_UNSIGNED_BYTE, false, false));
 
 		FOpenGL::PE_SetupTextureFormat(&SetupTextureFormat); // platform extension
 
