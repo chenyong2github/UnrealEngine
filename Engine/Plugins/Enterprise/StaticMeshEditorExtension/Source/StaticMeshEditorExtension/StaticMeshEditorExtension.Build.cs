2a738e076a5705313bc4d981df7ab40a46d74417
brooke hubert <brooke.hubert@epicgames.com>
2020-08-12 12:30:31 -0400

    Adding missed build.cs file from CL 14084099
    Move FEditorModeInfo and shared mode defines to EditorFramework module.
    
    #rnx
    #Jira UE-96448
    #rb lauren.barnes jamie.dale
    
    [CL 14092451 by brooke hubert in ue5-main branch]

--- Engine/Plugins/Enterprise/StaticMeshEditorExtension/Source/StaticMeshEditorExtension/StaticMeshEditorExtension.Build.cs
+++ Engine/Plugins/Enterprise/StaticMeshEditorExtension/Source/StaticMeshEditorExtension/StaticMeshEditorExtension.Build.cs
@@ -28,6 +28,7 @@ namespace UnrealBuildTool.Rules
 					"SlateCore",
 					"StaticMeshDescription",
 					"StaticMeshEditor",
+					"EditorFramework",
 					"UnrealEd",
 					"ViewportInteraction",
 					"DataprepCore",
