499be76bfe5640c58495fa241f7630c9c7f63383
eric mcdaniel <Eric.Mcdaniel@epicgames.com>
2020-08-17 13:04:21 -0400

    Add support for back buffer wait tracking for required platforms
    - changes are wrapped with the PLATFORM_USE_BACKBUFFER_WRITE_TRANSITION_TRACKING define which is enabled on platforms which require it
    - this is a precursor change and has no fuctional change
      - follow up changes will enable the system on the required platforms
    
    #rb Jeff.Newquist, Brian.White, Kenzo.Terelst
    #jira UE-93194
    #robomerge[BOT1] 14.10
    #lockdown marcus.wassmer
    
    #ROBOMERGE-OWNER: eric.mcdaniel
    #ROBOMERGE-AUTHOR: eric.mcdaniel
    #ROBOMERGE-SOURCE: CL 14124306 in //UE4/Release-4.25Plus/... via CL 14124683
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v720-14121032)
    
    [CL 14124741 by eric mcdaniel in Main branch]

--- Engine/Source/Runtime/RHI/Public/RHICommandList.h
+++ Engine/Source/Runtime/RHI/Public/RHICommandList.h
@@ -2100,6 +2100,20 @@ FRHICOMMAND_MACRO(FRHICommandUpdateRHIResources)
 	RHI_API void Execute(FRHICommandListBase& CmdList);
 };
 
+#if PLATFORM_USE_BACKBUFFER_WRITE_TRANSITION_TRACKING
+FRHICOMMAND_MACRO(FRHICommandBackBufferWaitTrackingBeginFrame)
+{
+	uint64 FrameToken;
+
+	FORCEINLINE_DEBUGGABLE FRHICommandBackBufferWaitTrackingBeginFrame(uint64 FrameTokenIn)
+		: FrameToken(FrameTokenIn)
+	{
+	}
+	
+	RHI_API void Execute(FRHICommandListBase& CmdList);
+};
+#endif // #if PLATFORM_USE_BACKBUFFER_WRITE_TRANSITION_TRACKING
+
 FRHICOMMAND_MACRO(FRHICommandCopyBufferRegion)
 {
 	FRHIVertexBuffer* DestBuffer;
@@ -3526,6 +3540,18 @@ public:
 		}
 		ALLOC_COMMAND(FRHICommandDiscardRenderTargets)(Depth, Stencil, ColorBitMask);
 	}
+
+#if PLATFORM_USE_BACKBUFFER_WRITE_TRANSITION_TRACKING
+	FORCEINLINE_DEBUGGABLE void RHIBackBufferWaitTrackingBeginFrame(uint64 FrameToken)
+	{
+		if (Bypass())
+		{
+			GetContext().RHIBackBufferWaitTrackingBeginFrame(FrameToken);
+			return;
+		}
+		ALLOC_COMMAND(FRHICommandBackBufferWaitTrackingBeginFrame)(FrameToken);
+	}
+#endif // #if PLATFORM_USE_BACKBUFFER_WRITE_TRANSITION_TRACKING
 	
 	FORCEINLINE_DEBUGGABLE void CopyBufferRegion(FRHIVertexBuffer* DestBuffer, uint64 DstOffset, FRHIVertexBuffer* SourceBuffer, uint64 SrcOffset, uint64 NumBytes)
 	{
