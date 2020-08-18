66d9debb2e0443264850d04460d3ecd341c346d4
mark fitt <Mark.Fitt@xa.epicgames.com>
2020-08-18 09:13:13 -0400

    online Added move payload support to AppleHTTP requests.
    [at]Wes.Hunt, [at]Michael.Kirzinger
    
    
    #ROBOMERGE-SOURCE: CL 14131442 via CL 14131446 via CL 14131450
    #ROBOMERGE-BOT: (v720-14121032)
    
    [CL 14131456 by mark fitt in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Online/HTTP/Private/Apple/AppleHTTP.cpp
+++ Engine/Source/Runtime/Online/HTTP/Private/Apple/AppleHTTP.cpp
@@ -187,18 +187,37 @@ const TArray<uint8>& FAppleHttpRequest::GetContent() const
 
 void FAppleHttpRequest::SetContent(const TArray<uint8>& ContentPayload)
 {
+	if (CompletionStatus == EHttpRequestStatus::Processing)
+	{
+		UE_LOG(LogHttp, Warning, TEXT("FAppleHttpRequest::SetContent() - attempted to set content on a request that is inflight"));
+		return;
+	}
+
 	UE_LOG(LogHttp, Verbose, TEXT("FAppleHttpRequest::SetContent()"));
 	Request.HTTPBody = [NSData dataWithBytes:ContentPayload.GetData() length:ContentPayload.Num()];
 	RequestPayloadByteLength = ContentPayload.Num();
 	bIsPayloadFile = false;
+
+	ContentData.Empty();
 }
+
 
 void FAppleHttpRequest::SetContent(TArray<uint8>&& ContentPayload)
 {
-	SetContent(ContentPayload);
-	// honor standard APIs that take ownership of the payload and empty it out.
-	ContentPayload.Empty();
+	if (CompletionStatus == EHttpRequestStatus::Processing)
+	{
+		UE_LOG(LogHttp, Warning, TEXT("FAppleHttpRequest::SetContent() - attempted to set content on a request that is inflight"));
+		return;
+	}
+
+	UE_LOG(LogHttp, Verbose, TEXT("FAppleHttpRequest::SetContent()"));
+	ContentData = MoveTemp(ContentPayload);
+
+	Request.HTTPBody = [NSData dataWithBytesNoCopy:ContentData.GetData() length:ContentData.Num() freeWhenDone:false];
+	RequestPayloadByteLength = ContentData.Num();
+	bIsPayloadFile = false;
 }
+
 
 FString FAppleHttpRequest::GetContentType() const
 {
@@ -217,13 +236,21 @@ int32 FAppleHttpRequest::GetContentLength() const
 
 void FAppleHttpRequest::SetContentAsString(const FString& ContentString)
 {
+	if (CompletionStatus == EHttpRequestStatus::Processing)
+	{
+		UE_LOG(LogHttp, Warning, TEXT("FAppleHttpRequest::SetContentAsString() - attempted to set content on a request that is inflight"));
+		return;
+	}
+
 	UE_LOG(LogHttp, Verbose, TEXT("FAppleHttpRequest::SetContentAsString() - %s"), *ContentString);
 	FTCHARToUTF8 Converter(*ContentString);
-	
+
 	// The extra length computation here is unfortunate, but it's technically not safe to assume the length is the same.
 	Request.HTTPBody = [NSData dataWithBytes:(ANSICHAR*)Converter.Get() length:Converter.Length()];
 	RequestPayloadByteLength = Converter.Length();
 	bIsPayloadFile = false;
+
+	ContentData.Empty();
 }
 
 bool FAppleHttpRequest::SetContentAsStreamedFile(const FString& Filename)
@@ -239,6 +266,9 @@ bool FAppleHttpRequest::SetContentAsStreamedFile(const FString& Filename)
 
 	NSString* PlatformFilename = Filename.GetNSString();
 
+	Request.HTTPBody = nil;
+	ContentData.Empty();
+
 	struct stat FileAttrs = { 0 };
 	if (stat(PlatformFilename.fileSystemRepresentation, &FileAttrs) == 0)
 	{
@@ -252,7 +282,6 @@ bool FAppleHttpRequest::SetContentAsStreamedFile(const FString& Filename)
 	else
 	{
 		UE_LOG(LogHttp, VeryVerbose, TEXT("FAppleHttpRequest::SetContentAsStreamedFile failed to get file size"));
-		Request.HTTPBody = nil;
 		Request.HTTPBodyStream = nil;
 		RequestPayloadByteLength = 0;
 		bIsPayloadFile = false;
