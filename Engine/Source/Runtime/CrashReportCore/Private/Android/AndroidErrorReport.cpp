9c8edabbbc85db3cf6201f5743788c151911a175
allan bentham <Allan.Bentham@epicgames.com>
2020-08-18 11:59:30 -0400

    Android error report checks for the existance of PlatformProperties.txt file to merge into the crash description. #rb chris.babcock
    #ROBOMERGE-SOURCE: CL 14132931 via CL 14132944 via CL 14132947 via CL 14132950
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v720-14121032)
    
    [CL 14132954 by allan bentham in Main branch]

--- Engine/Source/Runtime/CrashReportCore/Private/Android/AndroidErrorReport.cpp
+++ Engine/Source/Runtime/CrashReportCore/Private/Android/AndroidErrorReport.cpp
@@ -30,44 +30,64 @@ void FAndroidErrorReport::ShutDown()
  FAndroidErrorReport::FAndroidErrorReport(const FString& Directory)
 	: FGenericErrorReport(Directory)
 {
-	// Check for allthreads.txt, if it exists rename it.
+	// Check for some specific files to merge into the report, if they exist we rename them.
 	// This way if anything goes wrong during processing it will not continue to cause issues on subsequent runs.
 
-	const FString StartingThreadContextsFileName(TEXT("AllThreads.txt"));
-	const FString StartingThreadContextsFilePath(ReportDirectory / StartingThreadContextsFileName);
-	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
-	bool bHasFoundThreadsFile = PlatformFile.FileExists(*StartingThreadContextsFilePath);
-	if (bHasFoundThreadsFile)
-	{
-		FString ThreadContextsFileName(TEXT("AllThreads.tmp"));
-		ThreadContextsPathName = ReportDirectory / ThreadContextsFileName;
-		PlatformFile.MoveFile(*ThreadContextsPathName, *StartingThreadContextsFilePath);
+	static const FMergeInfo MergeTypesOfInterest[] = { 
+		FMergeInfo(FString(TEXT("AllThreads")), FMergeInfo::EMergeType::Threads),
+		FMergeInfo(FString(TEXT("PlatformProperties")), FMergeInfo::EMergeType::PlatformProperties)
+	};
 
-		// mirror the renaming in ReportFilenames.
-		ReportFilenames.Add(ThreadContextsFileName);
-		ReportFilenames.RemoveSingle(StartingThreadContextsFileName);
-	}
-}
-
-static void AddThreadContexts(const FString& ThreadContextsPathName)
-{
-	// Try to load the callstacks file.
-	if (!ThreadContextsPathName.IsEmpty())
+	for (const FMergeInfo& MergeSource : MergeTypesOfInterest)
 	{
-		FXmlFile ThreadsNode(ThreadContextsPathName);
-		if (ThreadsNode.IsValid())
-		{			
-			FPrimaryCrashProperties::Get()->Threads = ThreadsNode.GetRootNode();
-			// delete the file as it has been added to the primary report.
-			IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
-			PlatformFile.DeleteFile(*ThreadContextsPathName);
+		const FString MergeFilePath(ReportDirectory / MergeSource.MergeName + TEXT(".txt"));
+		IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+		bool bHasFoundMergeFile = PlatformFile.FileExists(*MergeFilePath);
+		if (bHasFoundMergeFile)
+		{
+			FString MergeTempFileName(MergeSource.MergeName +TEXT(".tmp"));
+			FString MergeTempPathName = ReportDirectory / MergeTempFileName;
+			PlatformFile.MoveFile(*MergeTempPathName, *MergeFilePath);
+			MergeSources.Add(FMergeInfo(MergeTempPathName, MergeSource.MergeType));
+			// mirror the renaming in ReportFilenames.
+			ReportFilenames.Add(MergeTempFileName);
+			ReportFilenames.RemoveSingle(MergeSource.MergeName + TEXT(".txt"));
 		}
 	}
 }
 
 FText FAndroidErrorReport::DiagnoseReport() const
 {
-	AddThreadContexts(ThreadContextsPathName);
+	auto MergeIntoReport = [](const FString & MergeFilePathName, FMergeInfo::EMergeType MergeType)
+	{
+		// Try to load the merge source file.
+		FXmlFile MergeNode(MergeFilePathName);
+		if (MergeNode.IsValid())
+		{
+			switch (MergeType)
+			{
+				case FMergeInfo::EMergeType::Threads:
+				{
+					FPrimaryCrashProperties::Get()->Threads = MergeNode.GetRootNode();
+					break;
+				}
+				case FMergeInfo::EMergeType::PlatformProperties:
+				{
+					FPrimaryCrashProperties::Get()->PlatformPropertiesExtras = MergeNode.GetRootNode();
+					break;
+				}
+			}
+			// delete the file as it has been added to the primary report.
+			IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+			PlatformFile.DeleteFile(*MergeFilePathName);
+		}
+	};
+
+
+	for (const FMergeInfo& MergeSource : MergeSources)
+	{
+		MergeIntoReport(MergeSource.MergeName, MergeSource.MergeType);
+	}
 	return FText();
 }
 
