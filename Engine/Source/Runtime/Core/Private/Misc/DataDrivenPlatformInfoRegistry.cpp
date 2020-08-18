6684b0cbef60668b5163f02887e1fdb09f83e995
donal maccarthy <Donal.MacCarthy@epicgames.com>
2020-08-18 12:43:08 -0400

    Remove a call to FindFilesRecursive in GetDataDreivenIniFilenames to speed up loading with Pak files.
    [FYI] Josh.Adams
    
    #ROBOMERGE-SOURCE: CL 14133407 via CL 14133411 via CL 14133416 via CL 14133425
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v720-14121032)
    
    [CL 14133427 by donal maccarthy in Main branch]

--- Engine/Source/Runtime/Core/Private/Misc/DataDrivenPlatformInfoRegistry.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/DataDrivenPlatformInfoRegistry.cpp
@@ -7,8 +7,6 @@
 #include "Misc/ConfigCacheIni.h"
 #include "Modules/ModuleManager.h"
 
-
-
 static const TArray<FString>& GetDataDrivenIniFilenames()
 {
 	static bool bHasSearchedForFiles = false;
@@ -20,7 +18,17 @@ static const TArray<FString>& GetDataDrivenIniFilenames()
 
 		// look for the special files in any congfig subdirectories
 		IFileManager::Get().FindFilesRecursive(DataDrivenIniFilenames, *FPaths::EngineConfigDir(), TEXT("DataDrivenPlatformInfo.ini"), true, false);
-		IFileManager::Get().FindFilesRecursive(DataDrivenIniFilenames, *FPaths::EnginePlatformExtensionsDir(), TEXT("DataDrivenPlatformInfo.ini"), true, false, false);
+		// manually look through the platform directories - we can't use GetExtensionDirs(), since that function uses the results of this function 
+		TArray<FString> PlatformDirs;
+		IFileManager::Get().FindFiles(PlatformDirs, *FPaths::Combine(FPaths::EnginePlatformExtensionsDir(), TEXT("*")), false, true);
+		for (const FString& PlatformDir : PlatformDirs)
+		{
+			FString IniPath = FPaths::Combine(FPaths::EnginePlatformExtensionsDir(), PlatformDir, TEXT("Config/DataDrivenPlatformInfo.ini"));
+			if (IFileManager::Get().FileExists(*IniPath))
+			{
+				DataDrivenIniFilenames.Add(IniPath);
+			}
+		}
 	}
 
 	return DataDrivenIniFilenames;
@@ -45,6 +53,7 @@ bool FDataDrivenPlatformInfoRegistry::LoadDataDrivenIniFile(int32 Index, FConfig
 	if (FFileHelper::LoadFileToString(IniContents, *IniFilenames[Index]))
 	{
 		IniFile.ProcessInputFileContents(IniContents);
+
 
 		// platform extension paths are different (engine/platforms/platform/config, not engine/config/platform)
 		if (IniFilenames[Index].StartsWith(FPaths::EnginePlatformExtensionsDir()))
