4ed3baca6c531d85168549d254145aaf5cd00dd9
ben ingram <Ben.Ingram@epicgames.com>
2020-08-19 18:54:29 -0400

    Initialize output pointer parameters to nullptr in case function fails, for various material parameter functions There are cases where calling code doesn't pre-initialize output, and doesn't check return value, which can lead to crash with garbage pointer value #jira UE-94700 #rb none #lockdown michal.valient
    #ROBOMERGE-SOURCE: CL 13983039 in //UE4/Release-4.25/... via CL 14149328
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v720-14121032)
    
    [CL 14149711 by ben ingram in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
@@ -2388,6 +2388,7 @@ bool UMaterial::GetTextureParameterValue_New(const FHashedMaterialParameterInfo&
 	const int32 Index = CachedExpressionData.Parameters.FindParameterIndex(EMaterialParameterType::Texture, ParameterInfo, bOveriddenOnly);
 	if (Index == INDEX_NONE)
 	{
+		OutValue = nullptr;
 		return false;
 	}
 	OutValue = CachedExpressionData.Parameters.TextureValues[Index];
@@ -2490,6 +2491,7 @@ bool UMaterial::GetRuntimeVirtualTextureParameterValue_New(const FHashedMaterial
 	const int32 Index = CachedExpressionData.Parameters.FindParameterIndex(EMaterialParameterType::RuntimeVirtualTexture, ParameterInfo, bOveriddenOnly);
 	if (Index == INDEX_NONE)
 	{
+		OutValue = nullptr;
 		return false;
 	}
 	OutValue = CachedExpressionData.Parameters.RuntimeVirtualTextureValues[Index];
@@ -2606,6 +2608,8 @@ bool UMaterial::GetFontParameterValue_New(const FHashedMaterialParameterInfo& Pa
 	const int32 Index = CachedExpressionData.Parameters.FindParameterIndex(EMaterialParameterType::Font, ParameterInfo, bOveriddenOnly);
 	if (Index == INDEX_NONE)
 	{
+		OutFontValue = nullptr;
+		OutFontPage = INDEX_NONE;
 		return false;
 	}
 	OutFontValue = CachedExpressionData.Parameters.FontValues[Index];
