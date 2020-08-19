6409ad89f2ab8461f53b52384b80cce718947e99
Jerome Delattre <Jerome.Delattre@epicgames.com>
2020-08-19 18:32:59 -0400

    Send automated test results to Horde
    #jira UEENGQA-30627
    #rb Andrew.Grant
    #rnx
    
    [CL 14149370 by Jerome Delattre in ue5-main branch]

---
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/Utils/Gauntlet.UnrealTestResults.cs
@@ -0,0 +1,73 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System.Collections.Generic;
+using Tools.DotNETCommon;
+
+namespace Gauntlet
+{
+	// The following classes are used for json deserialization of the Unreal Automation Test Results.
+	// Because of a bug in the json deserialization used here(Tools.DotNETCommon.Json),
+	// the names of the properties need to match the case of the json properties.
+
+	public class UnrealAutomationComparisonFiles
+	{
+		public string difference;
+		public string approved;
+		public string unapproved;
+	}
+	public class UnrealAutomationArtifact
+	{
+		public string id;
+		public string name;
+		public string type;
+		public UnrealAutomationComparisonFiles files;
+	}
+	public class UnrealAutomationEvent
+	{
+		public string type;
+		public string message;
+		public string context;
+		public string artifact;
+	}
+	public class UnrealAutomationEntry
+	{
+		public UnrealAutomationEvent @event;
+		public string filename;
+		public int lineNumber;
+		public string timestamp;
+	}
+	public class UnrealAutomatedTestResult
+	{
+		public string testDisplayName;
+		public string fullTestPath;
+		public string artifactName;
+		public string state;
+		public int warnings;
+		public int errors;
+		public List<UnrealAutomationArtifact> artifacts;
+		public List<UnrealAutomationEntry> entries;
+	}
+	public class UnrealAutomatedTestPassResults
+	{
+		public string clientDescriptor;
+		public string reportCreatedOn;
+		public int succeeded;
+		public int succeededWithWarnings;
+		public int failed;
+		public int notRun;
+		public float totalDuration;
+		public bool comparisonExported;
+		public string comparisonExportDirectory;
+		public List<UnrealAutomatedTestResult> tests;
+
+		/// <summary>
+		/// Load Unreal Automated Test Results from json report
+		/// </summary>
+		/// <param name="FilePath"></param>
+		public static UnrealAutomatedTestPassResults LoadFromJson(string FilePath)
+		{
+			UnrealAutomatedTestPassResults JsonTestPassResults = Json.Load<UnrealAutomatedTestPassResults>(new FileReference(FilePath));
+			return JsonTestPassResults;
+		}
+	}
+}
