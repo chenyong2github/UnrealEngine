68529402bac3ebc3ab59a7fe44ebe4c61700968b
david hill <David.Hill@epicgames.com>
2020-08-20 18:51:39 -0400

    Skeletal Mesh Simplifier:  Log when the source mesh has edges shared by more than two triangles. This can help identify non-manifold source meshes that may produce undesired results when simplified.  Also add code to allow these edges to be locked, but don't expose that option at this time.
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: david.hill
    #ROBOMERGE-SOURCE: CL 14102650 via CL 14102656 via CL 14102664 via CL 14108651
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14159588 by david hill in Dev-Networking branch]

--- Engine/Plugins/Experimental/SkeletalReduction/Source/Private/SkeletalSimplifierMeshManager.h
+++ Engine/Plugins/Experimental/SkeletalReduction/Source/Private/SkeletalSimplifierMeshManager.h
@@ -90,6 +90,9 @@ namespace SkeletalSimplifier
 		// Apply flag to edges when the IsDifferent(AVert, BVert) == true
 		void        FlagEdges(const TFunction<bool(const SimpVertType*, const SimpVertType*)> IsDifferent, const ESimpElementFlags Flag);
 
+		// Visit each edge group, and call EdgeVisitor(VertexA, VertexB, NumAdjacentFaces)
+		void        VisitEdges(TFunction<void(SimpVertType*, SimpVertType*, int32)> EdgeVisitor);
+
 		// Change the attributes on a given simplifier vert.
 		void UpdateVertexAttributes(SimpVertType& Vertex, const MeshVertType& AttributeVert)
 		{
@@ -105,6 +108,9 @@ namespace SkeletalSimplifier
 
 		// Count the number of edges with zero length.
 		int32 CountDegenerateEdges() const;
+
+		// Fraction (0 to 1) of edge-groups with more than two adjacent tris, optionally lock these edges
+		float FractionNonManifoldEdges(bool bLockNonManifoldEdges = false);
 
 		// Hash location 
 		static uint32 HashPoint(const FVector& p)
