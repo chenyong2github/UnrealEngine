2ee242d26dfb935caeb3845fb6cd41b3c0103fe5
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-08-20 18:53:25 -0400

    Fix for shader compile error with RenderTarget DI and android #rb stu.mckenna
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 14104711 via CL 14104715 via CL 14104719 via CL 14109198
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14159612 by rob krajcarski in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xB61F491F, 0xBBC58742, 0xB2244D66, 0x147C92DF);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6AF488BE, 0xFFD345BA, 0x9FB32E8C, 0x55EF8436);
