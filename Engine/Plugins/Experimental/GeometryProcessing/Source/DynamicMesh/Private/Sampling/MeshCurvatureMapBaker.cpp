527c573e5fe0c9f6b7d366de01ce036d239903be
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-08-20 19:27:45 -0400

    GeometryProcessing: add support for various types of mesh image-map baking
    - FMeshImageBakingCache precomputes correspondences between two meshes, and image-space pixel occupancy of UV-space mesh
    - FMeshImageBaker is base class with shared baker functionality
    - FMeshNormalMapBaker bakes normal maps
    - FMeshCurvatureMapBaker bakes various types of curvature map
    - FMeshResampleImageBaker resamples an existing texture map on the detail mesh into the new UV space of the target mesh
    - FMeshOcclusionMapBaker calculates raytraced ambient occlusion
    - FMeshPropertyMapBaker bakes various mesh properties (position, normal, etc) into an image
    
    MeshModelingToolset: rewrite BakeMeshAttributeMapsTool to use above baking system, and change UX to only support baking one map type at a time
    
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 14151449 via CL 14151450 via CL 14151452 via CL 14151453
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14160441 by ryan schmidt in Dev-Networking branch]

---
+++ Engine/Plugins/Experimental/GeometryProcessing/Source/DynamicMesh/Private/Sampling/MeshCurvatureMapBaker.cpp
@@ -0,0 +1,202 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sampling/MeshCurvatureMapBaker.h"
+#include "Sampling/SphericalFibonacci.h"
+#include "Sampling/Gaussians.h"
+#include "MeshCurvature.h"
+#include "MeshWeights.h"
+#include "MeshNormals.h"
+
+#include "Math/RandomStream.h"
+
+
+
+void FMeshCurvatureMapBaker::CacheDetailCurvatures(const FDynamicMesh3* DetailMesh)
+{
+	if (! Curvatures)
+	{
+		Curvatures = MakeShared<FMeshVertexCurvatureCache>();
+		Curvatures->BuildAll(*DetailMesh);
+	}
+	check(Curvatures->Num() == DetailMesh->MaxVertexID());
+
+}
+
+
+void FMeshCurvatureMapBaker::Bake()
+{
+	const FMeshImageBakingCache* BakeCache = GetCache();
+	check(BakeCache);
+	const FDynamicMesh3* DetailMesh = BakeCache->GetDetailMesh();
+
+	CacheDetailCurvatures(DetailMesh);
+
+	ResultBuilder = MakeUnique<TImageBuilder<FVector3f>>();
+	ResultBuilder->SetDimensions(BakeCache->GetDimensions());
+
+
+	Bake_Single();
+
+
+	const FImageOccupancyMap& Occupancy = *BakeCache->GetOccupancyMap();
+
+	for (int64 k = 0; k < Occupancy.GutterTexels.Num(); k++)
+	{
+		TPair<int64, int64> GutterTexel = Occupancy.GutterTexels[k];
+		ResultBuilder->CopyPixel(GutterTexel.Value, GutterTexel.Key);
+	}
+
+	if (BlurRadius > 0.01)
+	{
+		TDiscreteKernel2f BlurKernel2d;
+		TGaussian2f::MakeKernelFromRadius(BlurRadius, BlurKernel2d);
+		TArray<FVector3f> AOBlurBuffer;
+		Occupancy.ParallelProcessingPass<FVector3f>(
+			[&](int64 Index) { return FVector3f::Zero(); },
+			[&](int64 LinearIdx, float Weight, FVector3f& CurValue) { CurValue += Weight * ResultBuilder->GetPixel(LinearIdx); },
+			[&](int64 LinearIdx, float WeightSum, FVector3f& CurValue) { CurValue /= WeightSum; },
+			[&](int64 LinearIdx, FVector3f& CurValue) { ResultBuilder->SetPixel(LinearIdx, CurValue); },
+			[&](const FVector2i& TexelOffset) { return BlurKernel2d.EvaluateFromOffset(TexelOffset); },
+			BlurKernel2d.IntRadius,
+			AOBlurBuffer);
+	}
+
+}
+
+
+void FMeshCurvatureMapBaker::Bake_Single()
+{
+	const FMeshImageBakingCache* BakeCache = GetCache();
+	const FDynamicMesh3* DetailMesh = BakeCache->GetDetailMesh();
+	const FMeshVertexCurvatureCache& Cache = *Curvatures;
+
+
+	double MinPreClamp = -TNumericLimits<double>::Max();
+	double MaxPreClamp = TNumericLimits<double>::Max();
+	if (UseClampMode == EClampMode::Positive)
+	{
+		MinPreClamp = 0;
+	}
+	else if (UseClampMode == EClampMode::Negative)
+	{
+		MaxPreClamp = 0;
+	}
+
+
+
+	auto GetCurvature = [&](int32 vid) {
+		switch (UseCurvatureType)
+		{
+		default:
+		case ECurvatureType::Mean:
+			return FMathd::Clamp(Cache[vid].Mean, MinPreClamp, MaxPreClamp);
+		case ECurvatureType::Gaussian:
+			return FMathd::Clamp(Cache[vid].Gaussian, MinPreClamp, MaxPreClamp);
+		case ECurvatureType::MaxPrincipal:
+			return FMathd::Clamp(Cache[vid].MaxPrincipal, MinPreClamp, MaxPreClamp);
+		case ECurvatureType::MinPrincipal:
+			return FMathd::Clamp(Cache[vid].MinPrincipal, MinPreClamp, MaxPreClamp);
+		}
+	};
+
+	FInterval1d CurvatureRange = Cache.MeanRange;
+	switch (UseCurvatureType)
+	{
+	default:
+	case ECurvatureType::Mean:
+		CurvatureRange = Cache.MeanRange;
+		break;
+	case ECurvatureType::Gaussian:
+		CurvatureRange = Cache.GaussianRange;
+		break;
+	case ECurvatureType::MaxPrincipal:
+		CurvatureRange = Cache.MaxPrincipalRange;
+		break;
+	case ECurvatureType::MinPrincipal:
+		CurvatureRange = Cache.MinPrincipalRange;
+		break;
+	}
+
+	double ClampMax = CurvatureRange.MaxAbsExtrema();
+	if (UseClampMode == EClampMode::Positive)
+	{
+		ClampMax = FMathd::Abs(CurvatureRange.Max);
+	} 
+	else if (UseClampMode == EClampMode::Negative)
+	{
+		ClampMax = FMathd::Abs(CurvatureRange.Min);
+	}
+
+
+	ClampMax *= RangeScale;
+	double MinClamp = MinRangeScale * ClampMax;
+	FInterval1d ClampRange(MinClamp, ClampMax);
+
+	// sample curvature of detail surface in tangent-space of base surface
+	auto CurvatureSampleFunction = [&](const FMeshImageBakingCache::FCorrespondenceSample& SampleData)
+	{
+		int32 DetailTriID = SampleData.DetailTriID;
+		if (DetailMesh->IsTriangle(DetailTriID))
+		{
+			FIndex3i DetailTri = DetailMesh->GetTriangle(DetailTriID);
+			double CurvatureA = GetCurvature(DetailTri.A);
+			double CurvatureB = GetCurvature(DetailTri.B);
+			double CurvatureC = GetCurvature(DetailTri.C);
+			double InterpCurvature = SampleData.DetailBaryCoords.X * CurvatureA
+				+ SampleData.DetailBaryCoords.Y * CurvatureB
+				+ SampleData.DetailBaryCoords.Z * CurvatureC;
+
+			return InterpCurvature;
+		}
+		return 0.0;
+	};
+
+
+
+	FVector3f NegativeColor, ZeroColor, PositiveColor;
+	GetColorMapRange(NegativeColor, ZeroColor, PositiveColor);
+
+	BakeCache->EvaluateSamples([&](const FVector2i& Coords, const FMeshImageBakingCache::FCorrespondenceSample& Sample)
+	{
+		double Curvature = CurvatureSampleFunction(Sample);
+
+		double Sign = FMathd::Sign(Curvature);
+		double T = ClampRange.GetT(FMathd::Abs(Curvature));
+
+		FVector3f CurvatureColor = (Sign < 0) ?
+			FVector3f::Lerp(ZeroColor, NegativeColor, T)
+			: FVector3f::Lerp(ZeroColor, PositiveColor, T);
+
+		ResultBuilder->SetPixel(Coords, CurvatureColor);
+	});
+}
+
+
+void FMeshCurvatureMapBaker::Bake_Multi()
+{
+
+}
+
+
+void FMeshCurvatureMapBaker::GetColorMapRange(FVector3f& NegativeColor, FVector3f& ZeroColor, FVector3f& PositiveColor)
+{
+	switch (UseColorMode)
+	{
+	default:
+	case EColorMode::BlackGrayWhite:
+		NegativeColor = FVector3f(0, 0, 0);
+		ZeroColor = FVector3f(0.5, 0.5, 0.5);
+		PositiveColor = FVector3f(1, 1, 1);
+		break;
+	case EColorMode::RedGreenBlue:
+		NegativeColor = FVector3f(1, 0, 0);
+		ZeroColor = FVector3f(0, 1, 0);
+		PositiveColor = FVector3f(0, 0, 1);
+		break;
+	case EColorMode::RedBlue:
+		NegativeColor = FVector3f(1, 0, 0);
+		ZeroColor = FVector3f(0, 0, 0);
+		PositiveColor = FVector3f(0, 0, 1);
+		break;
+	}
+}
