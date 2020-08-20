7044e1be1ecedbc4a23091947e7a3e8833de66e5
nicolas nebel <nicolas.nebel@epicgames.com>
2020-08-20 18:38:40 -0400

    Add initial motion trail editing mode for movie scene transform tracks. This will be extended later to include support for editing control rig trails.
    #rb andrew.rodham
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: nicolas.nebel
    #ROBOMERGE-SOURCE: CL 14078177 via CL 14078182 via CL 14078189 via CL 14102060
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14159335 by nicolas nebel in Dev-Networking branch]

---
+++ Engine/Plugins/Experimental/MotionTrailEditorMode/Source/MotionTrailEditorMode/Private/MotionTrailEditorModeCommands.cpp
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MotionTrailEditorModeCommands.h"
+#include "MotionTrailEditorMode.h"
+
+
+#define LOCTEXT_NAMESPACE "MotionTrailEditorModeCommands"
+
+void FMotionTrailEditorModeCommands::RegisterCommands()
+{
+	UI_COMMAND(Default, "Default", "Default trail editing tool", EUserInterfaceActionType::ToggleButton, FInputChord());
+	Commands.Add(UMotionTrailEditorMode::MotionTrailEditorMode_Default, { Default });
+}
+
+void FMotionTrailEditorModeCommands::RegisterDynamic(const FName InName, const TArray<TSharedPtr<FUICommandInfo>>& InCommands)
+{
+	Instance.Pin()->Commands.Add(InName, InCommands);
+	Instance.Pin()->CommandsChanged.Broadcast(*(Instance.Pin()));
+}
+
+void FMotionTrailEditorModeCommands::UnRegisterDynamic(const FName InName)
+{
+	Instance.Pin()->Commands.Remove(InName);
+	Instance.Pin()->CommandsChanged.Broadcast(*(Instance.Pin()));
+}
+
+#undef LOCTEXT_NAMESPACE
