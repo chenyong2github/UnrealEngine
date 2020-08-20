517291a14ec29e0248d6d4f0a1747c5fa1354ad5
mattias hornlund <Mattias.Hornlund@epicgames.com>
2020-08-20 19:13:48 -0400

    Fixed compilation error on linux introduced with CL 14130595 #rb trivial
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: mattias.hornlund
    #ROBOMERGE-SOURCE: CL 14130699 via CL 14130701 via CL 14130704 via CL 14130705
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14160145 by mattias hornlund in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/NetConnection.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/NetConnection.h
@@ -235,7 +235,7 @@ struct FDelayedIncomingPacket
 
 struct FChannelCloseInfo
 {
-	int32 Id;
+	uint32 Id;
 	EChannelCloseReason CloseReason;
 };
 typedef TArray<FChannelCloseInfo, TInlineAllocator<8>> FChannelsToClose;
