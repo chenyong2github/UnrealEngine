0a90443d9137165550e5b6b9d7827d2a033f0712
pj kack <PJ.Kack@epicgames.com>
2020-08-21 11:30:27 -0400

    AsyncLoading2: Fix small memory leak caused by not freeing deferred arcs from event nodes completed on the game thread.
    #rb carlmagnus.nordin
    #tests ps4 level streaming test
    #rnx
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: pj.kack
    #ROBOMERGE-SOURCE: CL 14162698 via CL 14162699 via CL 14162701 via CL 14162702 via CL 14163845
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v720-14121032)
    
    [CL 14163907 by pj kack in Dev-Networking branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
@@ -4455,6 +4455,10 @@ EAsyncPackageState::Type FAsyncLoadingThread2::ProcessLoadedPackagesFromGameThre
 	{
 		// We're not done until all packages have been deleted
 		Result = CompletedPackages.Num() ? EAsyncPackageState::PendingImports  : EAsyncPackageState::Complete;
+		if (Result == EAsyncPackageState::Complete && ThreadState.HasDeferredFrees())
+		{
+			ThreadState.ProcessDeferredFrees();
+		}
 	}
 
 	return Result;
