29e12d94c9bfcefe8a41565386441f3e41d4776c
ori cohen <Ori.Cohen@epicgames.com>
2020-08-25 12:52:14 -0400

    Fix compiler issues with headless chaos (Submitting for Benn.Gallagher)
    #ROBOMERGE-SOURCE: CL 14180908 via CL 14180914 via CL 14180917
    #ROBOMERGE-BOT: (v720-14121032)
    
    [CL 14180927 by ori cohen in Release-Engine-Staging branch]

--- Engine/Source/Programs/HeadlessChaos/Public/GeometryCollection/GeometryCollectionTestFramework.h
+++ Engine/Source/Programs/HeadlessChaos/Public/GeometryCollection/GeometryCollectionTestFramework.h
@@ -43,17 +43,14 @@ namespace GeometryCollectionTest
 	{
 		TGeometryCollectionWrapper() : WrapperBase(WrapperType::GeometryCollection) {}
 		TGeometryCollectionWrapper(
-			TSharedPtr<Chaos::FChaosPhysicsMaterial> PhysicalMaterialIn,
 			TSharedPtr<FGeometryCollection> RestCollectionIn,
 			TSharedPtr<FGeometryDynamicCollection> DynamicCollectionIn,
 			TGeometryCollectionPhysicsProxy<Traits>* PhysObjectIn)
 			: WrapperBase(WrapperType::GeometryCollection)
-			, PhysicalMaterial(PhysicalMaterialIn)
 			, RestCollection(RestCollectionIn)
 			, DynamicCollection(DynamicCollectionIn)
 			, PhysObject(PhysObjectIn) {}
 		static WrapperType StaticType() { return WrapperType::GeometryCollection; }
-		TSharedPtr<Chaos::FChaosPhysicsMaterial> PhysicalMaterial;
 		TSharedPtr<FGeometryCollection> RestCollection;
 		TSharedPtr<FGeometryDynamicCollection> DynamicCollection;
 		TGeometryCollectionPhysicsProxy<Traits>* PhysObject;
