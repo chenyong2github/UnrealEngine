30a1274e9aeb2ec0a7feff5dad2cd25d948536c5
zach bethel <zach.bethel@epicgames.com>
2020-08-26 11:35:44 -0400

    Fixed race condition between bHasstaticPermutationResource being mutated on the GT and read on the RT.
    #rb rolando.caloca
    #jira UE-73090
    #lockdown marcus.wassmer
    
    #ROBOMERGE-SOURCE: CL 14189065 in //UE4/Release-4.25/... via CL 14189067
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-Engine-Staging) (v720-14121032)
    
    [CL 14189075 by zach bethel in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
@@ -3928,6 +3928,9 @@ void UMaterialInstance::UpdateStaticPermutation(const FStaticParameterSet& NewPa
 
 	if (bHasStaticPermutationResource != bWantsStaticPermutationResource || bParamsHaveChanged || (bBasePropertyOverridesHaveChanged && bWantsStaticPermutationResource) || bForceStaticPermutationUpdate)
 	{
+		// This will flush the rendering thread which is necessary before changing bHasStaticPermutationResource, since the RT is reading from that directly
+		FlushRenderingCommands();
+
 		bHasStaticPermutationResource = bWantsStaticPermutationResource;
 		StaticParameters = CompareParameters;
 
@@ -3941,9 +3944,8 @@ void UMaterialInstance::UpdateStaticPermutation(const FStaticParameterSet& NewPa
 		}
 		else
 		{
-			// This will flush the rendering thread which is necessary before changing bHasStaticPermutationResource, since the RT is reading from that directly
-			// The update context will also make sure any dependent MI's with static parameters get recompiled
-			FMaterialUpdateContext LocalMaterialUpdateContext;
+			// The update context will make sure any dependent MI's with static parameters get recompiled
+			FMaterialUpdateContext LocalMaterialUpdateContext(FMaterialUpdateContext::EOptions::RecreateRenderStates);
 			LocalMaterialUpdateContext.AddMaterialInstance(this);
 		}
 	}
