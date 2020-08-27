422976b004da07f703fff34f26635258f06e381f
bob tellez <Bob.Tellez@epicgames.com>
2020-08-27 18:05:14 -0400

    #UE4 Added bImplicitSendInNonShipping to independently control bImplicitSend for non-shipping builds
    [FYI] Brandon.Schaefer
    
    
    #ROBOMERGE-SOURCE: CL 14205007 via CL 14205010 via CL 14205011 via CL 14205018
    #ROBOMERGE-BOT: (v720-14121032)
    
    [CL 14205028 by bob tellez in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformCrashContext.cpp
+++ Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformCrashContext.cpp
@@ -607,7 +607,11 @@ int32 ReportCrashForMonitor(
 		if (ReportUI != EErrorReportUI::ReportInUnattendedMode)
 		{
 			// Only check if we are in a non-editor build
+#if UE_BUILD_SHIPPING
 			GConfig->GetBool(TEXT("CrashReportClient"), TEXT("bImplicitSend"), bImplicitSend, GEngineIni);
+#else
+			GConfig->GetBool(TEXT("CrashReportClient"), TEXT("bImplicitSendInNonShipping"), bImplicitSend, GEngineIni);
+#endif
 		}
 #endif
 	}
@@ -772,7 +776,11 @@ int32 ReportCrashUsingCrashReportClient(FWindowsPlatformCrashContext& InContext,
 	if (GConfig && ReportUI != EErrorReportUI::ReportInUnattendedMode)
 	{
 		// Only check if we are in a non-editor build
+#if UE_BUILD_SHIPPING
 		GConfig->GetBool(TEXT("CrashReportClient"), TEXT("bImplicitSend"), bImplicitSend, GEngineIni);
+#else
+		GConfig->GetBool(TEXT("CrashReportClient"), TEXT("bImplicitSendInNonShipping"), bImplicitSend, GEngineIni);
+#endif
 	}
 #endif
 
