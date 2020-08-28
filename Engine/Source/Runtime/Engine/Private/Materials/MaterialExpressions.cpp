e2fdd60c65cb172b0ccaab3cd4f52f82ae30cadc
jeremy moore <Jeremy.Moore@epicgames.com>
2020-08-28 19:11:26 -0400

    Fix automatic view bias not being applied to runtime virtual texture samples #jira UE-97658
    #ROBOMERGE-SOURCE: CL 14213857 via CL 14213859 via CL 14213862 via CL 14213867 via CL 14213873
    #ROBOMERGE-BOT: (v720-14121032)
    
    [CL 14213875 by jeremy moore in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
@@ -2376,7 +2376,7 @@ int32 UMaterialExpressionRuntimeVirtualTextureSample::Compile(class FMaterialCom
 			SAMPLERTYPE_VirtualMasks,
 			MipValueIndex, INDEX_NONE, TextureMipLevelMode, SSM_Wrap_WorldGroupSettings,
 			TextureReferenceIndex[TexureLayerIndex],
-			false);
+			true);
 	}
 
 	// Compile any unpacking code
