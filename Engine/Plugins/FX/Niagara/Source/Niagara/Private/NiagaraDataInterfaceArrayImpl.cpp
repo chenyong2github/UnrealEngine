c95a2965f39b8532c3bc230b5e21f38cba309a14
stu mckenna <Stu.McKenna@epicgames.com>
2020-09-01 13:51:45 -0400

    - Rename array functions to better match with blueprint naming convention
    #rb shaun.kime, rob.krajcarski
    #rnx
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 14171820 via CL 14171822 via CL 14171826 via CL 14171830 via CL 14172462
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v723-14217278)
    
    [CL 14231815 by stu mckenna in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceArrayImpl.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceArrayImpl.cpp
@@ -2,15 +2,16 @@
 
 #include "NiagaraDataInterfaceArrayImpl.h"
 
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_GetNumName(TEXT("GetNum"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_LengthName(TEXT("Length"));
 const FName FNiagaraDataInterfaceArrayImplHelper::Function_IsValidIndexName(TEXT("IsValidIndex"));
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_GetValueName(TEXT("GetValue"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_LastIndexName(TEXT("LastIndex"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_GetName(TEXT("Get"));
 
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_Reset(TEXT("Reset"));
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_SetNumValue(TEXT("SetNum"));
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_SetValueName(TEXT("SetValue"));
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_PushValueName(TEXT("PushValue"));
-const FName FNiagaraDataInterfaceArrayImplHelper::Function_PopValueName(TEXT("PopValue"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_ClearName(TEXT("Clear"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_ResizeName(TEXT("Resize"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_SetArrayElemName(TEXT("SetArrayElem"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_AddName(TEXT("Add"));
+const FName FNiagaraDataInterfaceArrayImplHelper::Function_RemoveLastElemName(TEXT("RemoveLastElem"));
 
 FString FNiagaraDataInterfaceArrayImplHelper::GetBufferName(const FString& InterfaceName)
 {
@@ -21,5 +22,35 @@ FString FNiagaraDataInterfaceArrayImplHelper::GetBufferSizeName(const FString& I
 {
 	return TEXT("ArrayBufferSize_") + InterfaceName;
 }
+
+#if WITH_EDITORONLY_DATA
+bool FNiagaraDataInterfaceArrayImplHelper::UpgradeFunctionCall(FNiagaraFunctionSignature& FunctionSignature)
+{
+	bool bModified = false;
+
+	static const TPair<FName, FName> NodeRenames[] =
+	{
+		MakeTuple(FName("GetNum"),			FNiagaraDataInterfaceArrayImplHelper::Function_LengthName),
+		MakeTuple(FName("GetValue"),		FNiagaraDataInterfaceArrayImplHelper::Function_GetName),
+		MakeTuple(FName("Reset"),			FNiagaraDataInterfaceArrayImplHelper::Function_ClearName),
+		MakeTuple(FName("SetNum"),			FNiagaraDataInterfaceArrayImplHelper::Function_ResizeName),
+		MakeTuple(FName("SetValue"),		FNiagaraDataInterfaceArrayImplHelper::Function_SetArrayElemName),
+		MakeTuple(FName("PushValue"),		FNiagaraDataInterfaceArrayImplHelper::Function_AddName),
+		MakeTuple(FName("PopValue"),		FNiagaraDataInterfaceArrayImplHelper::Function_RemoveLastElemName),
+	};
+
+	for (const auto& Pair : NodeRenames)
+	{
+		if (Pair.Key == FunctionSignature.Name)
+		{
+			FunctionSignature.Name = Pair.Value;
+			bModified = true;
+			break;
+		}
+	}
+
+	return bModified;
+}
+#endif
 
 IMPLEMENT_TYPE_LAYOUT(FNiagaraDataInterfaceParametersCS_ArrayImpl);
