1b9f50ac601804b10da96fc9efa89befdbf8cb3c
kenzo terelst <Kenzo.Terelst@xa.epicgames.com>
2020-09-01 13:48:56 -0400

    - Add dummy back buffer texture which is used to bind the current back buffer on the RenderThread - on the RHI thread the actual current back buffer texture is retrieved from the viewport - Removes independent RenderThread and RHIThread back buffer index which might not be incremented in sync causing DXGI errors and lost device issues - Still need expected back buffer index for mGPU to know what GPU will be used for the present, this is kept in sync when present will be skipped (can have 1 frame of using incorrect GPU but present is skipped anyway then)
    #rb Emil.Persson, Jonas.Meyer, Eric.McDaniel
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: kenzo.terelst
    #ROBOMERGE-SOURCE: CL 14188715 via CL 14188723 via CL 14188727 via CL 14188733 via CL 14188742 via CL 14188837
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v723-14217278)
    
    [CL 14231587 by kenzo terelst in Dev-Networking branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12CommandContext.h
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12CommandContext.h
@@ -437,7 +437,16 @@ public:
 
 	static inline FD3D12TextureBase* RetrieveTextureBase(FRHITexture* Texture, uint32 GPUIndex)
 	{
-		return Texture ? static_cast<FD3D12TextureBase*>(Texture->GetTextureBaseRHI())->GetLinkedObject(GPUIndex) : nullptr;
+		// If it's the dummy backbuffer then swap with actual current RHI backbuffer right now
+		FRHITexture* RHITexture = Texture;
+		if (RHITexture && RHITexture->GetFlags() & TexCreate_Presentable)
+		{
+			FD3D12BackBufferReferenceTexture2D* BufferBufferReferenceTexture = (FD3D12BackBufferReferenceTexture2D*)RHITexture;
+			FD3D12Viewport* ViewPort = BufferBufferReferenceTexture->GetViewPort();
+			RHITexture = BufferBufferReferenceTexture->IsSDR() ? ViewPort->GetSDRBackBuffer_RHIThread() : ViewPort->GetBackBuffer_RHIThread();
+		}
+
+		return RHITexture ? static_cast<FD3D12TextureBase*>(RHITexture->GetTextureBaseRHI())->GetLinkedObject(GPUIndex) : nullptr;
 	}
 
 	FORCEINLINE_DEBUGGABLE FD3D12TextureBase* RetrieveTextureBase(FRHITexture* Texture)
