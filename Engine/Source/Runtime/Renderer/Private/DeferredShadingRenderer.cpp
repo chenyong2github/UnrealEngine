44f9ef66b4fa4bca1fdfc74a450d3b59e82c84f3
john white <John.White@epicgames.com>
2020-09-01 14:25:08 -0400

    Fix for early release of GTAO buffers in splitscreen
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: john.white
    #ROBOMERGE-SOURCE: CL 14186117 via CL 14186118 via CL 14186119 via CL 14187051
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v723-14217278)
    
    [CL 14234241 by john white in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -2380,6 +2380,9 @@ void FDeferredShadingSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 
 			GCompositionLighting.ProcessAfterBasePass(RHICmdList, View);
 		}
+		SceneContext.ScreenSpaceGTAOHorizons.SafeRelease();
+		SceneContext.ScreenSpaceGTAODepths.SafeRelease();
+
 		ServiceLocalQueue();
 	}
 
