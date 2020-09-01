fb439aa1d2de2e664d9af0d82c5c2f432ca9f76f
matthew craig <matthew.craig@epicgames.com>
2020-09-01 15:19:32 -0400

    Fix for warning in static analysis
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: matthew.craig
    #ROBOMERGE-SOURCE: CL 14221656 via CL 14221658 via CL 14221659 via CL 14221661 via CL 14221664 via CL 14223631
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v723-14217278)
    
    [CL 14235880 by matthew craig in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Stats.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Stats.cpp
@@ -62,7 +62,7 @@ void D3D12RHI::FD3DGPUProfiler::BeginFrame(FD3D12DynamicRHI* InRHI)
 	}
 	bPreviousLatchedGProfilingGPUHitches = bLatchedGProfilingGPUHitches;
 
-	if (!GDynamicRHI && !GDynamicRHI->RHIIsRenderingSuspended())
+	if (GDynamicRHI && !GDynamicRHI->RHIIsRenderingSuspended())
 	{
 		FrameTiming.StartTiming();
 	}
@@ -81,7 +81,7 @@ void D3D12RHI::FD3DGPUProfiler::EndFrame(FD3D12DynamicRHI* InRHI)
 		check(StackDepth == 0);
 	}
 
-	if (!GDynamicRHI && !GDynamicRHI->RHIIsRenderingSuspended())
+	if (GDynamicRHI && !GDynamicRHI->RHIIsRenderingSuspended())
 	{
 		FrameTiming.EndTiming();
 	}
