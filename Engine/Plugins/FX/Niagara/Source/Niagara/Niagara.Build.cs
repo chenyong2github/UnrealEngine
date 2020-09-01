5b2e2691afc6de7b84605a65385b914c6a750125
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-09-01 13:32:52 -0400

    Updates to static VectorField.  CPU data is now stored as a half3 (instead of float4) when ISPC is available, and an ISPC kernel has been added to the Niagara DI for sampling.  For supported platforms it reduces memory cost (16 bpp -> 6 bpp) and improves sampling time (2x speedup). #rb stu.mckenna
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 14174105 via CL 14174106 via CL 14174109 via CL 14174112 via CL 14174114
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v723-14217278)
    
    [CL 14229870 by rob krajcarski in Dev-Networking branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Niagara.Build.cs
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Niagara.Build.cs
@@ -38,6 +38,7 @@ public class Niagara : ModuleRules
                 "RHI",
                 "NiagaraVertexFactories",
                 "RenderCore",
+                "IntelISPC",
             }
         );
 
