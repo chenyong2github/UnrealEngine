c1b88c893cd003ed556b806ac182c927e9e55cfc
stu mckenna <Stu.McKenna@epicgames.com>
2020-09-01 15:11:56 -0400

    - Take into account all emitters, not just those that are running, when working out if we require view uniform buffer - This fixes an issue where multiple ticks in a single frame could jump between tick groups
    #rb shaun.kime
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 14217037 via CL 14217038 via CL 14217039 via CL 14217041 via CL 14217042 via CL 14218724
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v723-14217278)
    
    [CL 14235645 by stu mckenna in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraScriptExecutionContext.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraScriptExecutionContext.cpp
@@ -541,10 +541,12 @@ void FNiagaraGPUSystemTick::Init(FNiagaraSystemInstance* InSystemInstance)
 	CA_ASSUME(InSystemInstance != nullptr);
 	ensure(!InSystemInstance->IsComplete());
 	SystemInstanceID = InSystemInstance->GetId();
+	SharedContext = InSystemInstance->GetComputeSharedContext();
 	bRequiresDistanceFieldData = InSystemInstance->RequiresDistanceFieldData();
 	bRequiresDepthBuffer = InSystemInstance->RequiresDepthBuffer();
 	bRequiresEarlyViewData = InSystemInstance->RequiresEarlyViewData();
-	bRequiresViewUniformBuffer = false;
+	bRequiresViewUniformBuffer = InSystemInstance->RequiresViewUniformBuffer();
+ 
 	uint32 DataSizeForGPU = InSystemInstance->GPUDataInterfaceInstanceDataSize;
 
 	if (DataSizeForGPU > 0)
@@ -664,8 +666,6 @@ void FNiagaraGPUSystemTick::Init(FNiagaraSystemInstance* InSystemInstance)
 				ensure((EmitterInstance->GetExecutionState() == ENiagaraExecutionState::Inactive) || (EmitterInstance->GetExecutionState() == ENiagaraExecutionState::InactiveClear));
 				continue;
 			}
-
-			bRequiresViewUniformBuffer |= Emitter->RequiresViewUniformBuffer();
 
 			FNiagaraComputeInstanceData* InstanceData = new (&Instances[InstanceIndex]) FNiagaraComputeInstanceData;
 			InstanceIndex++;
