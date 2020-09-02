58a494d89d6e3d83f7f033a16d7fb6b0ce2fb5be
guillaume abadie <Guillaume.Abadie@epicgames.com>
2020-09-01 23:09:19 -0400

    Removes inclues in CustomResourcePool.cpp
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: guillaume.abadie
    #ROBOMERGE-SOURCE: CL 14236525 via CL 14236527 via CL 14236528 via CL 14236529 via CL 14236537 via CL 14236548 via CL 14240893
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v723-14217278)
    
    [CL 14242129 by guillaume abadie in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/RenderCore/Private/CustomResourcePool.cpp
+++ Engine/Source/Runtime/RenderCore/Private/CustomResourcePool.cpp
@@ -1,8 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "CustomResourcePool.h"
-#include "RHI.h"
-#include "RHICommandlist.h"
+
 ICustomResourcePool* GCustomResourcePool = nullptr;
 
 void ICustomResourcePool::TickPoolElements(FRHICommandListImmediate& RHICmdList)
