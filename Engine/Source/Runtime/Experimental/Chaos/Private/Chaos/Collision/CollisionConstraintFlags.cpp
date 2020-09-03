821998249a73bfb14a98dff58714789b02beecbb
brice criswell <Brice.Criswell@epicgames.com>
2020-09-03 17:04:18 -0400

    Chaos : Ignore Collisions - Re-enable solver queue processing ... pre removes ignoring relationships that are added and deleted before the solver can process the queue.
    #rb none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: brice.criswell
    #ROBOMERGE-SOURCE: CL 14245293 via CL 14255061 via CL 14255086 via CL 14255160
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v724-14254601)
    
    [CL 14257175 by brice criswell in Dev-Networking branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionConstraintFlags.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionConstraintFlags.cpp
@@ -86,62 +86,98 @@ namespace Chaos
 
 	void FIgnoreCollisionManager::ProcessPendingQueues()
 	{
-		/*
-		if (PendingActivations.Num())
+
+		// remove particles that have been created and destroyed
+		// before the queue was ever processed. 
+		TArray<FGeometryParticle*> PreculledParticles;
+		if (PendingActivations.Num() && PendingDeactivations.Num())
 		{
 			TArray<FGeometryParticle*> DeletionList;
 			for (auto& Elem : PendingActivations)
 			{
-				if (TGeometryParticleHandle<FReal, 3>* Handle0 = Elem.Key->Handle())
+				int32 DeactiveIndex = PendingDeactivations.Find(Elem.Key);
+				if (DeactiveIndex != INDEX_NONE)
 				{
-					for (int Index = Elem.Value.Num() - 1; Index >= 0; Index--)
-					{
-						auto& Val = Elem.Value[Index];
-
-						if (TGeometryParticleHandle<FReal, 3>* Handle1 = Val->Handle())
-						{
-							FUniqueIdx ID0 = Handle0->UniqueIdx();
-							FUniqueIdx ID1 = Handle1->UniqueIdx();
-							if (!IgnoresCollision(ID0, ID1))
-							{
-								Chaos::TPBDRigidParticleHandle<FReal, 3>* ParticleHandle0 = Handle0->CastToRigidParticle();
-								Chaos::TPBDRigidParticleHandle<FReal, 3>* ParticleHandle1 = Handle1->CastToRigidParticle();
-
-								ParticleHandle0->AddCollisionConstraintFlag(Chaos::ECollisionConstraintFlags::CCF_BroadPhaseIgnoreCollisions);
-								AddIgnoreCollisionsFor(ID0, ID1);
-
-								ParticleHandle1->AddCollisionConstraintFlag(Chaos::ECollisionConstraintFlags::CCF_BroadPhaseIgnoreCollisions);
-								AddIgnoreCollisionsFor(ID1, ID0);
-							}
-						}
-
-						Elem.Value.RemoveAtSwap(Index, 1);
-					}
-				}
-
-				if (!Elem.Value.Num())
 					DeletionList.Add(Elem.Key);
+					PreculledParticles.Add(Elem.Key);
+					PendingDeactivations.RemoveAtSwap(DeactiveIndex, 1);
+				}
 			}
 			for (auto* Del : DeletionList)
 				PendingActivations.Remove(Del);
 		}
 
+		// add collision relationships for particles that have valid
+		// handles, and have not already been removed from the 
+		// simulation. 
+		if (PendingActivations.Num())
+		{
+			TArray<FGeometryParticle*> DeletionList;
+			for (auto& Elem : PendingActivations)
+			{
+				if (Elem.Key)
+				{
+					if (Elem.Key->IsParticleValid())
+					{
+						if (TGeometryParticleHandle<FReal, 3>* Handle0 = Elem.Key->Handle())
+						{
+							for (int Index = Elem.Value.Num() - 1; Index >= 0; Index--)
+							{
+								if (TGeometryParticle<FReal, 3>* Val = Elem.Value[Index])
+								{
+									if (Val->IsParticleValid())
+									{
+										if (PreculledParticles.Contains(Val))
+										{
+											Elem.Value.RemoveAtSwap(Index, 1);
+										}
+										else if (TGeometryParticleHandle<FReal, 3>* Handle1 = Val->Handle())
+										{
+											FUniqueIdx ID0 = Handle0->UniqueIdx();
+											FUniqueIdx ID1 = Handle1->UniqueIdx();
+											if (!IgnoresCollision(ID0, ID1))
+											{
+												Chaos::TPBDRigidParticleHandle<FReal, 3>* ParticleHandle0 = Handle0->CastToRigidParticle();
+												Chaos::TPBDRigidParticleHandle<FReal, 3>* ParticleHandle1 = Handle1->CastToRigidParticle();
+
+												ParticleHandle0->AddCollisionConstraintFlag(Chaos::ECollisionConstraintFlags::CCF_BroadPhaseIgnoreCollisions);
+												AddIgnoreCollisionsFor(ID0, ID1);
+
+												ParticleHandle1->AddCollisionConstraintFlag(Chaos::ECollisionConstraintFlags::CCF_BroadPhaseIgnoreCollisions);
+												AddIgnoreCollisionsFor(ID1, ID0);
+											}
+
+											Elem.Value.RemoveAtSwap(Index, 1);
+										}
+									}
+								}
+							}
+						}
+						if (!Elem.Value.Num())
+							DeletionList.Add(Elem.Key);
+					}
+				}
+			}
+			for (auto* Del : DeletionList)
+				PendingActivations.Remove(Del);
+		}
+
+		// remove relationships that exist and have been initialized. 
 		if (PendingDeactivations.Num())
 		{
 			for (auto Index = PendingDeactivations.Num() - 1; Index >= 0; Index--)
 			{
-				FUniqueIdx ID0 = PendingDeactivations[Index]->UniqueIdx();
-				if (IgnoreCollisionsList.Contains(ID0))
+				if (PendingDeactivations[Index])
 				{
-					IgnoreCollisionsList.Remove(ID0);
-					PendingDeactivations.RemoveAtSwap(Index, 1);
+					FUniqueIdx ID0 = PendingDeactivations[Index]->UniqueIdx();
+					if (IgnoreCollisionsList.Contains(ID0))
+					{
+						IgnoreCollisionsList.Remove(ID0);
+					}
 				}
 			}
+			PendingDeactivations.Empty();
 		}
-		*/
-		PendingDeactivations.Empty();
-		PendingActivations.Empty();
-
 	}
 
 } // Chaos
