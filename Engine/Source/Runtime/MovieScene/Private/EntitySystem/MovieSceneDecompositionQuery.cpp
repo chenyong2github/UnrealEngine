01ca47f6a4f1a8ef68be74914b946e98b703e78b
ludovic chabant <Ludovic.Chabant@epicgames.com>
2020-09-08 15:34:28 -0400

    Sequencer: Fix decomposition of additive channel when there's another additive channel too.
    #jira UE-98088
    #rb andrew.rodham
    
    
    #ROBOMERGE-SOURCE: CL 14249922 via CL 14270231
    #ROBOMERGE-BOT: (v724-14254601)
    
    [CL 14273492 by ludovic chabant in Release-Engine-Staging branch]

--- Engine/Source/Runtime/MovieScene/Private/EntitySystem/MovieSceneDecompositionQuery.cpp
+++ Engine/Source/Runtime/MovieScene/Private/EntitySystem/MovieSceneDecompositionQuery.cpp
@@ -66,7 +66,7 @@ float FDecomposedFloat::Recompose(FMovieSceneEntityID RecomposeEntity, float Cur
 	{
 		if (bIsAdditive)
 		{
-			const float WeightedAdditiveResult = CurrentValue - ResultAbsolute.Combine(DecomposedAbsolute).WeightedValue();
+			const float WeightedAdditiveResult = CurrentValue - ResultAbsolute.Combine(DecomposedAbsolute).WeightedValue() - Result.Additive;
 			return Channel.Weight == 0.f ? WeightedAdditiveResult : WeightedAdditiveResult / Channel.Weight;
 		}
 		else
