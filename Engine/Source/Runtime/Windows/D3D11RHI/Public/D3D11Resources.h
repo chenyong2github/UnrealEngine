ec270e6a31b5d22363dc68a4be5f2fbcd3e58c5f
christopher waters <christopher.waters@epicgames.com>
2020-09-10 17:07:45 -0400

    Fixing the DirectX RHIs to create the correct view types for multisampled images. This addresses a lot of crashes we're seeing when enabling ES3 previews on Windows with Forward MSAA enabled.
    #jira UE-96632
    #rb kenzo.terelst
    
    #ROBOMERGE-SOURCE: CL 14293542 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v731-14288592)
    
    [CL 14293574 by christopher waters in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Windows/D3D11RHI/Public/D3D11Resources.h
+++ Engine/Source/Runtime/Windows/D3D11RHI/Public/D3D11Resources.h
@@ -515,7 +515,7 @@ class FD3D11BaseTexture2DArray : public FRHITexture2DArray
 public:
 	FD3D11BaseTexture2DArray(uint32 InSizeX, uint32 InSizeY, uint32 InSizeZ, uint32 InNumMips, uint32 InNumSamples, EPixelFormat InFormat, ETextureCreateFlags InFlags, const FClearValueBinding& InClearValue)
 	: FRHITexture2DArray(InSizeX,InSizeY,InSizeZ,InNumMips,InNumSamples, InFormat,InFlags,InClearValue)
-	{ check(InNumSamples == 1); }
+	{}
 };
 
 class FD3D11BaseTextureCube : public FRHITextureCube
