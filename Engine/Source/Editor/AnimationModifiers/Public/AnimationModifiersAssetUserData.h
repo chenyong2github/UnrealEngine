a338f34e7695f8687cfb42f7cf1546c997a62a4f
jurre debaare <jurre.debaare@epicgames.com>
2020-09-11 17:22:23 -0400

    Move AnimationModifiersAssetUserData to public folder and expose GetAnimationModifierInstances for outside use #jira none #rb Thomas.Sarkanen
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: jurre.debaare
    #ROBOMERGE-SOURCE: CL 14297739 in //UE4/Release-4.26/... via CL 14297767 via CL 14297788
    #ROBOMERGE-BOT: NETWORKING (Main -> Dev-Networking) (v732-14297381)
    
    [CL 14302818 by jurre debaare in Dev-Networking branch]

--- Engine/Source/Editor/AnimationModifiers/Public/AnimationModifiersAssetUserData.h
+++ Engine/Source/Editor/AnimationModifiers/Public/AnimationModifiersAssetUserData.h
@@ -10,23 +10,25 @@ class UAnimationModifier;
 
 /** Asset user data which can be added to a USkeleton or UAnimSequence to keep track of Animation Modifiers */
 UCLASS()
-class UAnimationModifiersAssetUserData : public UAssetUserData
+class ANIMATIONMODIFIERS_API UAnimationModifiersAssetUserData : public UAssetUserData
 {
 	GENERATED_BODY()
-public:
-	void AddAnimationModifier(UAnimationModifier* Instance);
-	void RemoveAnimationModifierInstance(UAnimationModifier* Instance);
-	const TArray<UAnimationModifier*>& GetAnimationModifierInstances() const;
-	void ChangeAnimationModifierIndex(UAnimationModifier* Instance, int32 Direction);
 
+	friend class SAnimationModifiersTab;
+	friend class SAnimationModifierContentBrowserWindow;
+public:
+	const TArray<UAnimationModifier*>& GetAnimationModifierInstances() const;
+protected:	 
 	/** Begin UAssetUserData overrides */
 	virtual void PostLoad() override;
 	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;	
-
 	virtual void Serialize(FArchive& Ar) override;
-
 	/** End UAssetUserData overrides */
-protected:
+
+	void AddAnimationModifier(UAnimationModifier* Instance);
+	void RemoveAnimationModifierInstance(UAnimationModifier* Instance);
+	void ChangeAnimationModifierIndex(UAnimationModifier* Instance, int32 Direction);
+private:
 	void RemoveInvalidModifiers();
 protected:
 	UPROPERTY()
