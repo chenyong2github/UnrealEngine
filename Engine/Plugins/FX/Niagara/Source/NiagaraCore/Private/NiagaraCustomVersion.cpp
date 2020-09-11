8e49849dc04a7120e6bcfeffcff3e216c3db1a35
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-09-10 23:05:41 -0400

    Fix for Niagara matrix multiplication node; make it actually a mul() rather than per component scaling #jira UE-96180 #rb jamie.hayes
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 14296045 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v731-14288592)
    
    [CL 14296079 by rob krajcarski in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE485F98A, 0xA8EAD84C, 0x90873ECF, 0xC6E2026B);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x77828628, 0x583B451B, 0x8EFA55E7, 0x1BC05E14);
