a97e0e2263b583916ebcfefb01424a893f12db0d
mike zyracki <Mike.Zyracki@epicgames.com>
2020-09-11 11:41:53 -0400

    Control Rig: Add ability to scale gizmo's so faceboard works better when zoomed in. #jira DH-1233 #rb michael.daum
    #ROBOMERGE-SOURCE: CL 14299282 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v732-14297381)
    
    [CL 14299288 by mike zyracki in Release-Engine-Staging branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditModeCommands.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditModeCommands.cpp
@@ -9,6 +9,12 @@ void FControlRigEditModeCommands::RegisterCommands()
 	UI_COMMAND(ResetTransforms, "Reset Transform", "Reset the Controls Transforms", EUserInterfaceActionType::Button, FInputChord(EKeys::G, EModifierKey::Control));
 	UI_COMMAND(ResetAllTransforms, "Reset All Transform", "Reset all of the Controls Transforms", EUserInterfaceActionType::Button, FInputChord(EKeys::G, EModifierKey::Control | EModifierKey::Shift));
 	UI_COMMAND(ToggleManipulators, "Toggle Manipulators", "Toggles visibility of manipulators in the viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::T));
+
+	UI_COMMAND(IncreaseGizmoSize, "Increase Gizmo Size", "Increase Size of the Gizmos In The Viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::Equals, EModifierKey::Shift));
+	UI_COMMAND(DecreaseGizmoSize, "Decrease Gizmo Size", "Decrease Size of the Gizmos In The Viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::Hyphen, EModifierKey::Shift));
+	UI_COMMAND(ResetGizmoSize, "Reset Gizmo Size", "Resize Gizmo Size To Default", EUserInterfaceActionType::Button, FInputChord(EKeys::Equals));
+
+
 }
 
 #undef LOCTEXT_NAMESPACE
