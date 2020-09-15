6dbf6d01a2c0aa9358ea9d8905e2cd2a6de2ace7
simon tovey <Simon.Tovey@epicgames.com>
2020-09-15 10:05:15 -0400

    Fixed transient variable leakage
    #rb Rob.Krajcarski
    
    
    #ROBOMERGE-SOURCE: CL 14278378 via CL 14278456 via CL 14279227 via CL 14279270 via CL 14279312
    #ROBOMERGE-BOT: (v738-14305781)
    
    [CL 14318776 by simon tovey in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6A3DA7EF, 0x0357EE40, 0xB7C68A2E, 0xD84F10B7);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE1C0CD09, 0x429E064B, 0xB4F3E753, 0xB81729F2);
