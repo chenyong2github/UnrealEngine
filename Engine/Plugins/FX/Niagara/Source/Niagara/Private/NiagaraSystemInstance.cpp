a6dbf54eb0c2d92bd3bc44d23885dc093498c841
jamie hayes <jamie.hayes@epicgames.com>
2020-09-17 11:50:33 -0400

    Fix crash from reactivating solo systems after they've completed
    #rb stu.mckenna
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jamie.hayes
    #ROBOMERGE-SOURCE: CL 14254461 via CL 14254467 via CL 14254469 via CL 14270663 via CL 14278740 via CL 14278820
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14338173 by jamie hayes in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemInstance.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemInstance.cpp
@@ -649,7 +649,13 @@ void FNiagaraSystemInstance::Complete()
 	if (!bPooled)
 	{
 		UnbindParameters(true);
-		SystemSimulation = nullptr;
+
+		if (!IsSolo())
+		{
+			// Release our reference to the world manager's simulation so this system doesn't pin it down in case it gets
+			// destroyed (i.e. from global scalability settings change) and so we will re-acquire it if we get reactivated
+			SystemSimulation = nullptr;
+		}
 	}
 
 	bPendingSpawn = false;
