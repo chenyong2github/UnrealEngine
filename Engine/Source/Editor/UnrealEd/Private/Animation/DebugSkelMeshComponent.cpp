e86f8756bb5c84d54032ba4d9a57eb28b5608866
kriss gossart <Kriss.Gossart@epicgames.com>
2020-09-17 11:28:46 -0400

    Cloth Painter - Do not rebuilt the DDC while previewing.
    #jira UE-99215
    #rb Alexis.Matte
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: kriss.gossart
    #ROBOMERGE-SOURCE: CL 14268331 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14337698 by kriss gossart in Dev-Tools-Staging branch]

--- Engine/Source/Editor/UnrealEd/Private/Animation/DebugSkelMeshComponent.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Animation/DebugSkelMeshComponent.cpp
@@ -723,7 +723,7 @@ void UDebugSkelMeshComponent::ResetMeshSectionVisibility()
 	}
 }
 
-void UDebugSkelMeshComponent::RebuildClothingSectionsFixedVerts()
+void UDebugSkelMeshComponent::RebuildClothingSectionsFixedVerts(bool bInvalidateDerivedDataCache)
 {
 	FSkeletalMeshModel* Resource = SkeletalMesh->GetImportedModel();
 	FScopedSkeletalMeshPostEditChange ScopedSkeletalMeshPostEditChange(SkeletalMesh);
@@ -752,8 +752,11 @@ void UDebugSkelMeshComponent::RebuildClothingSectionsFixedVerts()
 							VertData.SourceMeshVertIndices[1],
 							VertData.SourceMeshVertIndices[2]) ? 0xFFFF : 0;
 					}
-					//We must dirty the DDC key
-					SkeletalMesh->InvalidateDeriveDataCacheGUID();
+					if (bInvalidateDerivedDataCache)
+					{
+						// We must always dirty the DDC key unless previewing
+						SkeletalMesh->InvalidateDeriveDataCacheGUID();
+					}
 				}
 			}
 		}
