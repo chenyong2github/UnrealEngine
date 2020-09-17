a37f986b7b626e45ff243862013d420f84d7dd78
robert manuszewski <Robert.Manuszewski@epicgames.com>
2020-09-17 07:56:42 -0400

    Temporarily disabling the check that prevents Upackages from being created with non-null outers until a case that hits it is investigated
    #jira UE-99711
    #rb none
    
    #ROBOMERGE-SOURCE: CL 14336346 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v738-14305781)
    
    [CL 14336349 by robert manuszewski in Release-Engine-Staging branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
@@ -2326,7 +2326,7 @@ UObject* StaticAllocateObject
 	check(InClass);
 	check(GIsEditor || bCreatingCDO || !InClass->HasAnyClassFlags(CLASS_Abstract)); // this is a warning in the editor, otherwise it is illegal to create an abstract class, except the CDO
 	check(InOuter || (InClass == UPackage::StaticClass() && InName != NAME_None)); // only packages can not have an outer, and they must be named explicitly
-	checkf(InClass != UPackage::StaticClass() || !InOuter || bCreatingCDO, TEXT("Creating nested packages is not allowed: Outer=%s, Package=%s"), *GetNameSafe(InOuter), *InName.ToString());
+	//checkf(InClass != UPackage::StaticClass() || !InOuter || bCreatingCDO, TEXT("Creating nested packages is not allowed: Outer=%s, Package=%s"), *GetNameSafe(InOuter), *InName.ToString());
 	check(bCreatingCDO || !InOuter || InOuter->IsA(InClass->ClassWithin));
 	checkf(!IsGarbageCollecting(), TEXT("Unable to create new object: %s %s.%s. Creating UObjects while Collecting Garbage is not allowed!"),
 		*GetNameSafe(InClass), *GetPathNameSafe(InOuter), *InName.ToString());
