39cafef48c8303b6d9d6973c713ff8ebfa9b2e52
shaun kime <Shaun.Kime@epicgames.com>
2020-09-17 12:23:25 -0400

    Fixing missing implementation of HLSL function SampleGrid
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 14262518 via CL 14271392 via CL 14285063 via CL 14285532
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14338591 by shaun kime in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xE561B034, 0xC1814408, 0x89464D4D, 0x6A221D1A);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xA72664F6, 0x130D470B, 0x92A830DE, 0xCE38A437 );
