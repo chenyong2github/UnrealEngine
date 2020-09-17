891a9a87bca89c17a919ac7875462c65f274b651
stu mckenna <Stu.McKenna@epicgames.com>
2020-09-17 12:33:20 -0400

    - When creating a Cascade scene proxy set bParallelRenderThreadUpdate to true to avoid reading from the async data which is not ready
    #rb shaun.kime, simon.tovey
    #jira UE-95959
    #rnx
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 14256506 in //UE4/Release-4.25Plus/... via CL 14292621 via CL 14292646 via CL 14294987
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14338992 by stu mckenna in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Particles/ParticleSystemRender.cpp
+++ Engine/Source/Runtime/Engine/Private/Particles/ParticleSystemRender.cpp
@@ -7260,7 +7260,9 @@ FPrimitiveSceneProxy* UParticleSystemComponent::CreateSceneProxy()
 		}
 
 		// Create the dynamic data for rendering this particle system.
+		bParallelRenderThreadUpdate = true;
 		FParticleDynamicData* ParticleDynamicData = CreateDynamicData(GetScene()->GetFeatureLevel());
+		bParallelRenderThreadUpdate = false;
 
 		if (CanBeOccluded())
 		{
