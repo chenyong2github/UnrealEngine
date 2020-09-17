a1efa337cfd4fe726ca99884a42860e2528a1b70
rolando caloca <Rolando.Caloca@epicgames.com>
2020-09-17 12:32:03 -0400

    UE4.26 - Initial check-in of ChunkDownloader [FYI] David.Nikdel #rb David.Nikdel #jira none
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: rolando.caloca
    #ROBOMERGE-SOURCE: CL 14292209 in //UE4/Release-4.26/... via CL 14292226 via CL 14294897
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14338930 by rolando caloca in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Runtime/ChunkDownloader/Source/ChunkDownloader.Build.cs
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ChunkDownloader : ModuleRules
+{
+	public ChunkDownloader(ReadOnlyTargetRules Target) : base(Target)
+	{
+        PCHUsage = ModuleRules.PCHUsageMode.UseSharedPCHs;
+        PrivatePCHHeaderFile = "Private/ChunkDownloaderPCH.h";
+
+        PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"Core",
+                "ApplicationCore",
+                "CoreUObject",
+                "HTTP",
+			}
+		);
+		PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+			}
+		);
+
+		if (Target.Configuration == UnrealTargetConfiguration.DebugGame)
+		{
+			PCHUsage = PCHUsageMode.NoSharedPCHs;
+		}
+	}
+}
