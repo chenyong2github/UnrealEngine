8e9bfed45a84759cc157bb81097ebeaeacdc5622
francis maheux <Francis.Maheux@epicgames.com>
2020-09-18 23:09:31 -0400

    Editgrated from //UE4/Dev-VirtualProduction/ New DMXFixture plugin #Jira UEENT-3747 #rb None
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: francis.maheux
    #ROBOMERGE-SOURCE: CL 14323288 in //UE4/Release-4.26/... via CL 14323291 via CL 14323302
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14350262 by francis maheux in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/VirtualProduction/DMX/DMXFixtures/Source/DMXFixtures/Private/DMXFixtureComponent.cpp
@@ -0,0 +1,145 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "DMXFixtureComponent.h"
+#include "DMXFixtureActor.h"
+#include "DMXFixtureActorMatrix.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "RHICommandList.h"
+#include "RenderingThread.h"
+
+
+UDMXFixtureComponent::UDMXFixtureComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// init cells to 1 and set current
+	InitCells(1);
+
+	IsEnabled = true;
+	UseInterpolation = false;
+	UsingMatrixData = false;
+	SkipThreshold = 0.01f;
+	InterpolationScale = 1.0f;
+}
+
+void UDMXFixtureComponent::Initialize()
+{
+	// Get dmx signal format from dmx fixture actor
+	ADMXFixtureActor* ParentFixture = Cast<ADMXFixtureActor>(GetOwner());
+	if (ParentFixture)
+	{
+		UDMXEntityFixturePatch* Patch = ParentFixture->DMX->GetFixturePatch();
+		if (Patch)
+		{
+			TMap<FDMXAttributeName, EDMXFixtureSignalFormat> SignalFormatsMap = Patch->GetAttributeSignalFormats();
+			SetBitResolution(SignalFormatsMap);
+		}
+	}
+
+	// Get number of cells from dmx Matrix Fixture
+	ADMXFixtureActorMatrix* ParentMatrixFixture = Cast<ADMXFixtureActorMatrix>(GetOwner());
+	if (ParentMatrixFixture && UsingMatrixData)
+	{
+		int nCells = ParentMatrixFixture->XCells * ParentMatrixFixture->YCells;
+		InitCells(nCells);
+	}
+
+	// set interpolation range value
+	SetRangeValue();
+
+	// Apply interpolation speed scale
+	ApplySpeedScale();
+
+	// Run the Initialize blueprint event
+	InitializeComponent();
+}
+
+
+void UDMXFixtureComponent::OnComponentCreated()
+{
+	Initialize();
+}
+
+
+void UDMXFixtureComponent::InitCells(int NCells)
+{
+	Cells.Init(FCell(), NCells);
+	CurrentCell = &Cells[0];
+	for (auto& Cell : Cells)
+	{
+		Cell.ChannelInterpolation.Init(FInterpolationData(), 1);
+	}
+}
+
+void UDMXFixtureComponent::SetCurrentCell(int Index)
+{
+	if (Index < Cells.Num())
+	{
+		CurrentCell = &Cells[Index];
+	}
+}
+
+void UDMXFixtureComponent::ApplySpeedScale()
+{
+	for (auto& Cell : Cells)
+	{
+		for (auto& ChannelInterp : Cell.ChannelInterpolation)
+		{
+			ChannelInterp.ApplyInterpolationScale(InterpolationScale);
+		}
+	}
+}
+
+ADMXFixtureActor* UDMXFixtureComponent::GetParentFixtureActor()
+{
+	AActor* Parent = GetOwner();
+	if (Parent)
+	{ 
+		ADMXFixtureActor* ParentFixtureActor = Cast<ADMXFixtureActor>(Parent);
+		return ParentFixtureActor;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+// Warning: Input Texture must use VectorDisplacementMap compression or else RHI complains about BytesPerPixel
+// Reads pixel color in the middle of each "Texture" and output linear colors
+TArray<FLinearColor> UDMXFixtureComponent::GetTextureCenterColors(UTexture2D* Texture, int NumCells)
+{
+	TArray<FLinearColor> PixelColorArray;
+	PixelColorArray.SetNumZeroed(NumCells, true);
+
+	// use render thread to access pixel data
+	FRenderCommandFence Fence;
+	ENQUEUE_RENDER_COMMAND(GetPixelColors)
+	(
+		[Texture, &PixelColorArray, NumCells](FRHICommandListImmediate& RHICmdList)
+		{
+			FTexture2DResource* uTex2DRes = (FTexture2DResource*)Texture->Resource;
+			FIntVector Size = uTex2DRes->TextureRHI->GetSizeXYZ();
+			TArray<FColor> Data;
+			FIntRect Rect(0, 0, Size.X, Size.Y);
+			RHICmdList.ReadSurfaceData(uTex2DRes->TextureRHI, Rect, Data, FReadSurfaceDataFlags());
+
+			// sample pixel in the middle of each 'cell'
+			TArray<FLinearColor> Out;
+			int32 V = FMath::FloorToInt(Size.Y *0.5f) - 1;
+			for (int32 i = 0; i < NumCells; i++)
+			{
+				int32 U = (((i/float(NumCells)) + (0.5f/NumCells)) * Size.X) - 1;
+				Out.Add(Data[V * Size.X + U]);
+			}
+
+			PixelColorArray = std::move(Out);
+		}
+	);
+
+	// wait for render thread to finish
+	Fence.BeginFence();
+	Fence.Wait();
+
+	return PixelColorArray;
+}
---
+++ Engine/Plugins/VirtualProduction/DMX/DMXFixtures/Source/DMXFixtures/Public/DMXInterpolation.h
@@ -0,0 +1,176 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/KismetMathLibrary.h"
+
+// Interpolation that provides a damping effect and support direction changes
+struct FInterpolationData
+{
+	bool IsUpdating;
+	float CurrentValue;
+	float TargetValue;
+	float ToTravel;
+	float RangeValue;
+	float Direction;
+	float TotalTravel;
+	float PreviousT;
+	float PreviousStep;
+	
+	float CurrentSpeed;
+	float AccelerationThreshold;
+	float SpeedIncMin;
+	float SpeedIncMid;
+	float SpeedIncMax;
+	float SpeedMinimum;
+	
+	FInterpolationData()
+	{
+		IsUpdating = false;
+		RangeValue = 1.0f;
+		TargetValue = 0.0f;
+		CurrentValue = 0.0f;
+		Direction = 0.0f;
+		TotalTravel = 0.0f;
+		ToTravel = 0.0f;
+		PreviousT = 0.0f;
+		PreviousStep = 0.0f;
+		CurrentSpeed = 0.0f;
+		SpeedMinimum = 50.0f;
+		AccelerationThreshold = 0.5f;
+		SpeedIncMin = 15.0f;
+		SpeedIncMid = 20.0f;
+		SpeedIncMax = 30.0f;
+	}
+
+	bool IsTargetValid(float Value, float SkipThreshold)
+	{
+		return (FMath::Abs(TargetValue - Value) >= SkipThreshold);
+	}
+
+	void SetTarget(float NewValue)
+	{
+		TargetValue = NewValue;
+	}
+
+	bool IsInterpolationDone()
+	{
+		return (ToTravel < 0.05f);
+	}
+
+	void EndInterpolation()
+	{
+		CurrentValue = TargetValue;
+		Direction = 0.0f;
+		TotalTravel = 0.0f;
+		ToTravel = 0.0f;
+		PreviousT = 0.0f;
+		PreviousStep = 0.0f;
+		AccelerationThreshold = 0.5f;
+		IsUpdating = false;
+	}
+
+	void ApplyInterpolationScale(float Scale)
+	{
+		SpeedIncMin *= Scale;
+		SpeedIncMid *= Scale;
+		SpeedIncMax *= Scale;
+		SpeedMinimum *= Scale;
+	}
+
+	void StartTravel(float NewTarget)
+	{
+		IsUpdating = true;
+		Direction = (TargetValue < NewTarget) ? 1.0f : -1.0f;
+		float Travel = FMath::Abs(TargetValue - NewTarget);
+		TotalTravel = Travel;
+		ToTravel = Travel;
+		CurrentSpeed = SpeedIncMid;
+		TargetValue = NewTarget;
+	}
+
+	void UpdateTravel(float NewTarget)
+	{
+		float TravelDelta = (TargetValue - NewTarget) * Direction * -1.0f;
+		if (CurrentValue * Direction < NewTarget * Direction)
+		{
+			// front
+			TotalTravel = FMath::Max(TotalTravel + TravelDelta, 0.0f);
+			ToTravel = FMath::Max(ToTravel + TravelDelta, 0.0f);
+			TargetValue = NewTarget;
+
+			// shift accel/decel threshold
+			float CurrentT = UKismetMathLibrary::SafeDivide(TotalTravel - ToTravel, TotalTravel);
+			CurrentT *= 1.1f;
+			AccelerationThreshold = FMath::Clamp(CurrentT, 0.5f, 0.9f);
+		}
+		else
+		{
+			// back
+			float Travel = FMath::Abs(NewTarget - CurrentValue);
+			TotalTravel = Travel;
+			ToTravel = Travel;
+			Direction *= -1.0f;
+			TargetValue = NewTarget;
+			CurrentSpeed = 0.0f;
+		}
+	}
+
+	// TODO: instead of using the derivative of the SmoothStep, use a sine wave
+	void Travel(float DeltaSeconds)
+	{
+		TotalTravel = FMath::Max(TotalTravel, 0.0f);
+		ToTravel = FMath::Max(ToTravel, 0.0f);
+
+		float CurrentT = UKismetMathLibrary::SafeDivide(TotalTravel - ToTravel, TotalTravel);
+		CurrentT = FMath::Clamp(CurrentT, 0.0f, 1.0f);
+
+		float CurrentStep = FMath::SmoothStep(0.0f, 1.0f, CurrentT);
+		float Derivative = UKismetMathLibrary::SafeDivide(CurrentStep - PreviousStep, CurrentT - PreviousT);
+		float TravelAlpha = FMath::Clamp(TotalTravel / RangeValue, 0.0f, 1.0f);
+		float A = FMath::Lerp(SpeedIncMin, SpeedIncMid, TravelAlpha);
+		float SpeedInc = FMath::Lerp(A, SpeedIncMax, Derivative);
+
+		if (CurrentT < AccelerationThreshold)
+		{
+			// accelerate
+			CurrentSpeed = FMath::Max(CurrentSpeed + SpeedInc, 0.0f);
+		}
+		else
+		{
+			// decelerate
+			CurrentSpeed = FMath::Max(CurrentSpeed - SpeedInc, SpeedMinimum);
+		}
+
+		// save
+		PreviousStep = CurrentStep;
+		PreviousT = CurrentT;
+
+		// increment 
+		CurrentValue = CurrentValue + (CurrentSpeed * DeltaSeconds * Direction);
+		ToTravel = ToTravel - FMath::Abs(CurrentSpeed * DeltaSeconds);
+	}
+
+	void Push(float NewTarget)
+	{
+		if (IsUpdating)
+		{
+			UpdateTravel(NewTarget);
+		}
+		else
+		{
+			StartTravel(NewTarget);
+		}
+	}
+
+};
+
+
+
+struct FCell
+{
+	TArray<FInterpolationData> ChannelInterpolation;
+};
+
+
+
