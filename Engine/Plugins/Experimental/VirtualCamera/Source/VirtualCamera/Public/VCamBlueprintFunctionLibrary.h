416b2f837fa7827f4067385639a021692e66d22c
andrew grant <Andrew.Grant@epicgames.com>
2020-09-18 22:59:50 -0400

    Editgrating VirtualCamera plugin from Dev-VP
    #jira #rb na
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 14317970 in //UE4/Release-4.26/... via CL 14317972 via CL 14317985
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14349576 by andrew grant in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VirtualCamera/Public/VCamBlueprintFunctionLibrary.h
@@ -0,0 +1,124 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "AssetData.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Misc/FrameRate.h"
+#include "Misc/Timecode.h"
+#include "VCamBlueprintFunctionLibrary.generated.h"
+
+class ULevelSequence;
+class UVirtualCameraClipsMetaData;
+class USceneCaptureComponent2D;
+class UVirtualCameraUserSettings; 
+
+UCLASS(config=VirtualCamera, BlueprintType)
+class VIRTUALCAMERA_API UVCamBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	/** Returns true if not in editor or if running the game in PIE or Simulate*/
+	UFUNCTION(BlueprintPure, Category = "VirtualCamera")
+	static bool IsGameRunning();
+
+	UFUNCTION(BlueprintPure, Category = "VirtualCamera")
+	static UVirtualCameraUserSettings* GetUserSettings();
+
+	/** Get the currently opened level sequence asset */
+	UFUNCTION(BlueprintPure, Category = "VirtualCamera")
+	static ULevelSequence* GetCurrentLevelSequence();
+
+	/** Play the current level sequence */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static void PlayCurrentLevelSequence();
+
+	/** Pause the current level sequence */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static void PauseCurrentLevelSequence();
+
+	/** Set playback position for the current level sequence in frames */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static void SetCurrentLevelSequenceCurrentFrame(int32 NewFrame);
+
+	/** Get the current playback position in frames */
+	UFUNCTION(BlueprintPure, Category = "VirtualCamera")
+	static int32 GetCurrentLevelSequenceCurrentFrame();
+
+	/** Get length in frames of a level sequence */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static int32 GetLevelSequenceLengthInFrames(const ULevelSequence* LevelSequence);
+
+	/** Convert a frame from a level sequence to timecode */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static FTimecode GetLevelSequenceFrameAsTimecode(const ULevelSequence* LevelSequence, int32 InFrame);
+
+	/** Convert a frame from a level sequence to timecode using only a provided display rate */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static FTimecode GetLevelSequenceFrameAsTimecodeWithoutObject(const FFrameRate DisplayRate, int32 InFrame);
+
+	/** Check whether the sequence is actively playing. */
+	UFUNCTION(BlueprintPure, Category = "VirtualCamera")
+	static bool IsCurrentLevelSequencePlaying();
+
+	/** Imports image as a uasset */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static UTexture* ImportSnapshotTexture(FString FileName, FString SubFolderName, FString AbsolutePathPackage);
+
+	/** Saves UVirtualCameraClipsMetaData with updated selects information. */ 
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Metadata")
+	static bool ModifyLevelSequenceMetadataForSelects(UVirtualCameraClipsMetaData* LevelSequenceMetaData, bool bIsSelected);
+	
+	/** Marks a LevelSequence as dirty and saves it, persisting metadata changes */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Metadata")
+	static bool ModifyLevelSequenceMetadata(UVirtualCameraClipsMetaData* LevelSequenceMetaData);
+	
+	/** Save an asset through path. Returns true on success. */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Clips")
+	static bool EditorSaveAsset(FString AssetPath);
+
+	/** Load an asset through path. */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Clips")
+	static UObject* EditorLoadAsset(FString AssetPath);
+
+	/** Modifies a UObject's metadata tags, adding a tag if the tag does not exist. */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Clips")
+	static void ModifyObjectMetadataTags(UObject* InObject, FName InTag, FString InValue);
+	
+	/** Retrieves UObject's metadata tags */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Clips")
+	static TMap<FName, FString> GetObjectMetadataTags(UObject* InObject); 
+
+	/** Sort array of FAssetData by metadata timecode **/
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Clips ")
+	static TArray<FAssetData> SortAssetsByTimecodeAssetData(TArray<FAssetData> LevelSequenceAssets);
+
+	/** Pilot the provided actor using editor scripting */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera | Streaming ")
+	static void PilotActor(AActor* SelectedActor);
+
+	/** Updates the provided USceneCaptureComponent2D's PostProcessingSettings. Returns true on success. */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static bool UpdatePostProcessSettingsForCapture(USceneCaptureComponent2D* CaptureComponent, float DepthOfField, float FStopValue);
+
+	/** Grab the display rate from a LevelSequences' MovieScene */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static FFrameRate GetDisplayRate(ULevelSequence* LevelSequence);
+
+	/** Converts a double framerate to a FFrameRate */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static FFrameRate ConvertStringToFrameRate(FString InFrameRateString);
+
+	/** Returns true if the function was found & executed correctly. */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static bool CallFunctionByName(UObject* ObjPtr, FName FunctionName);
+
+	/** Sets the current game view */
+	UFUNCTION(BlueprintCallable, Category = "VirtualCamera")
+	static void EditorSetGameView(bool bIsToggled);
+
+};
+
+
