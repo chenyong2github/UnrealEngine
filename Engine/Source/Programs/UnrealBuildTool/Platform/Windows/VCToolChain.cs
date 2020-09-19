cce1e567d1559b5d9eda0f317f593bebd9b4b453
david harvey <David.Harvey@epicgames.com>
2020-09-18 22:52:49 -0400

    adding an option to override the global compiler arguments for a VCToolChain derived class.
    #ushell-cherrypick of 14276039 by dan.phillips
    #rb dan.phillips
    #jira MTHRL-131
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 14308407 in //UE4/Release-4.25Plus/... via CL 14315729 via CL 14315756 via CL 14317843
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14349073 by david harvey in Dev-Tools-Staging branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
@@ -145,7 +145,7 @@ namespace UnrealBuildTool
 		}
 
 
-		void AppendCLArguments_Global(CppCompileEnvironment CompileEnvironment, List<string> Arguments)
+		protected virtual void AppendCLArguments_Global(CppCompileEnvironment CompileEnvironment, List<string> Arguments)
 		{
 			// Suppress generation of object code for unreferenced inline functions. Enabling this option is more standards compliant, and causes a big reduction
 			// in object file sizes (and link times) due to the amount of stuff we inline.
