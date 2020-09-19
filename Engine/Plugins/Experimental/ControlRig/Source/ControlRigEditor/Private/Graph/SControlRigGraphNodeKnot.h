d594b8f890b3e02f713e6391fd9d3dc6b51c7219
helge mathee <Helge.Mathee@epicgames.com>
2020-09-18 22:58:23 -0400

    Integrating CL 14200391 from Dev-Anim, Author Jack.Cai
    Control Rig: Fix reroute node position not getting saved to disk.
    
    [FYI] jack.cai
    #JIRA UE-97508
    #RB helge.mathee
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 14317516 in //UE4/Release-4.26/... via CL 14317519 via CL 14317938
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14349441 by helge mathee in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/Graph/SControlRigGraphNodeKnot.h
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/DeclarativeSyntaxSupport.h"
+#include "SGraphNodeKnot.h"
+
+class SControlRigGraphNodeKnot : public SGraphNodeKnot
+{
+	SLATE_BEGIN_ARGS(SControlRigGraphNodeKnot) {} 
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, UEdGraphNode* InKnot);
+
+	// SGraphNode interface
+	virtual void EndUserInteraction() const override;
+};
