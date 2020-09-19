1334eb3c54c84dd4ca82731b62736817d9bbfdf3
jaco vandyk <Jaco.VanDyk@epicgames.com>
2020-09-18 23:45:04 -0400

    Chaos visual debugger: Seperate Windows specific files out into its own folder
    #rb none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jaco.vandyk
    #ROBOMERGE-SOURCE: CL 14297885 via CL 14297889 via CL 14335739 via CL 14335778
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14351813 by jaco vandyk in Dev-Tools-Staging branch]

--- Engine/Source/Programs/ChaosVisualDebugger/Private/ChaosVisualDebuggerMain.h
+++ Engine/Source/Programs/ChaosVisualDebugger/Private/ChaosVisualDebuggerMain.h
@@ -2,8 +2,6 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-
 // This is the platform independent entry point to the visual debugger
 // It returns the error level
 int32 ChaosVisualDebuggerMain(const TCHAR* CommandLine);
