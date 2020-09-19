cb0ca4ef64d2f7403f3f2d8161b0c2166174d343
andrew grant <Andrew.Grant@epicgames.com>
2020-09-18 22:52:06 -0400

    Fixed clang compile error due to use of both replication and API macros in struct definition
    #rb na
    [FYI] LouisPhilippe.Seguin
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 14266656 via CL 14266661 via CL 14267028 via CL 14271630 via CL 14314326 via CL 14317816
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v739-14336328)
    
    [CL 14349007 by andrew grant in Dev-Tools-Staging branch]

--- Engine/Plugins/Runtime/ReplicationGraph/Source/Public/ReplicationGraphTypes.h
+++ Engine/Plugins/Runtime/ReplicationGraph/Source/Public/ReplicationGraphTypes.h
@@ -376,7 +376,7 @@ private:
 
 /** A read only, non owning (ref counting) view to an actor replication list: essentially a raw pointer and the category of the list. These are only created *from* FActorRepListRefView */
 
-struct UE_DEPRECATED(4.27, "Replace this struct with the new FActorRepListConstView struct.") FActorRepListRawView : public TActorRepListViewBase<FActorRepList*>
+struct UE_DEPRECATED(4.27, "Replace this struct with the new FActorRepListConstView struct.")  FActorRepListRawView : public TActorRepListViewBase<FActorRepList*>
 {
 	/** Standard ctor: make raw view from ref view */
 	REPLICATIONGRAPH_API FActorRepListRawView(const FActorRepListRefView& Source) { RepList = Source.RepList.GetReference(); }
