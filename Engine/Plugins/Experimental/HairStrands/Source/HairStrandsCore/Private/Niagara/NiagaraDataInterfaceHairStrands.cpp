2600765e0279986f9f1ce9ae366ca9a0f442764a
michael forot <Michael.Forot@epicgames.com>
2020-09-23 15:18:30 -0400

    - Merge HairStrandsNiagara -> HairStrandsCore - Add cvar to enable/disable simulation - Loading only when sim is enable - Add custom CS copytexture to replace the RHI one not working on some platforms
    #jira DH-1669
    #jira DH-1587
    #jira UE-98108
    #jira DH-1618
    
    #rb none
    
    #ROBOMERGE-OWNER: michael.forot
    #ROBOMERGE-AUTHOR: michael.forot
    #ROBOMERGE-SOURCE: CL 14377685 in //UE4/Release-4.26/... via CL 14379639
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v742-14376270)
    
    [CL 14379644 by michael forot in Main branch]

--- Engine/Plugins/Experimental/HairStrands/Source/HairStrandsCore/Private/Niagara/NiagaraDataInterfaceHairStrands.cpp
+++ Engine/Plugins/Experimental/HairStrands/Source/HairStrandsCore/Private/Niagara/NiagaraDataInterfaceHairStrands.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "NiagaraDataInterfaceHairStrands.h"
+#include "Niagara/NiagaraDataInterfaceHairStrands.h"
 #include "NiagaraShader.h"
 #include "NiagaraComponent.h"
 #include "NiagaraRenderer.h"
