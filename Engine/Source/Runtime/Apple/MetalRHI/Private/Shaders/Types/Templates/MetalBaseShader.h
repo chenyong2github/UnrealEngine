169fd2797da230e9fa76362cf8913e80e23bfa78
rolando caloca <Rolando.Caloca@epicgames.com>
2020-09-23 03:57:11 -0400

    UE4.26 - Downgrade multi popup to log error for Metal #rb Will.Damon [FYI] Richard.Wallis #jira UE-99971
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: rolando.caloca
    #ROBOMERGE-SOURCE: CL 14368985 in //UE4/Release-4.26/... via CL 14368999 via CL 14369023
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v741-14365223)
    
    [CL 14374668 by rolando caloca in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/Shaders/Types/Templates/MetalBaseShader.h
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/Shaders/Types/Templates/MetalBaseShader.h
@@ -36,6 +36,7 @@ extern mtlpp::LanguageVersion ValidateVersion(uint8 Version);
 
 #pragma mark - Metal RHI Base Shader Class Template
 
+extern bool GShowAuthFail;
 
 template<typename BaseResourceType, int32 ShaderType>
 class TMetalBaseShader : public BaseResourceType, public IRefCountedObject
@@ -249,10 +250,9 @@ void TMetalBaseShader<BaseResourceType, ShaderType>::Init(TArrayView<const uint8
 					bSavedSource = FFileHelper::SaveStringToFile(Source, *ShaderPathString);
 				}
 
-				static bool bAttemptedAuth = false;
-				if (!bSavedSource && !bAttemptedAuth)
+				if (!bSavedSource && !GShowAuthFail)
 				{
-					bAttemptedAuth = true;
+					GShowAuthFail = true;
 
 					if (IFileManager::Get().MakeDirectory(*FPaths::GetPath(ShaderPathString), true))
 					{
@@ -261,8 +261,7 @@ void TMetalBaseShader<BaseResourceType, ShaderType>::Init(TArrayView<const uint8
 
 					if (!bSavedSource)
 					{
-						FPlatformMisc::MessageBoxExt(EAppMsgType::Ok,
-													 *NSLOCTEXT("MetalRHI", "ShaderDebugAuthFail", "Could not access directory required for debugging optimised Metal shaders. Falling back to slower runtime compilation of shaders for debugging.").ToString(), TEXT("Error"));
+						UE_LOG(LogRHI, Error, TEXT("Could not access directory required for debugging optimised Metal shaders. Falling back to slower runtime compilation of shaders for debugging."));
 					}
 				}
 			}
