b62be755debc4687648ffb2634fe9968b520cda3
christopher waters <christopher.waters@epicgames.com>
2020-09-23 03:59:33 -0400

    Extending FMonitorInfo to include the maximum supported resolution since it could be larger than the native resolution. This will allow features like Nvidia's Dynamic Super Resolution which can add resolutions larger than the monitor can support (and downscale in the driver).
    #jira UE-96975
    #rb Kenzo.TerElst, Mihnea.Balta
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: christopher.waters
    #ROBOMERGE-SOURCE: CL 14370526 in //UE4/Release-4.26/... via CL 14370538 via CL 14370551
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v741-14365223)
    
    [CL 14374713 by christopher waters in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/ApplicationCore/Public/GenericPlatform/GenericApplication.h
+++ Engine/Source/Runtime/ApplicationCore/Public/GenericPlatform/GenericApplication.h
@@ -334,6 +334,7 @@ struct FMonitorInfo
 	FString ID;
 	int32 NativeWidth;
 	int32 NativeHeight;
+	FIntPoint MaxResolution = FIntPoint(ForceInitToZero);
 	FPlatformRect DisplayRect;
 	FPlatformRect WorkArea;
 	bool bIsPrimary;
