38dd9b8037a6b203ca75a74816038925686036cd
luke thatcher <Luke.Thatcher@epicgames.com>
2020-09-25 10:46:12 -0400

    Merging CL 14394018 from //UE4/Release-Engine-Staging to //UE4/Main
    Fix game user settings not persisting on some console platforms.
     - The file path the ini files are written to was not targeting the writable title directory, so writes were failing as the standard engine/project save dir is inside the package, which is readonly.
     - Removed unnecessary platform target setting. Previously when this is false, GetGameUserSettingsDir() would return a path which can never be used since the package is readonly, making the option pointless.
    
    #rb Jeff.Newquist
    #jira UE-100207
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: luke.thatcher
    #ROBOMERGE-SOURCE: CL 14395286 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v742-14376270)
    
    [CL 14395994 by luke thatcher in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/GameUserSettings.cpp
+++ Engine/Source/Runtime/Engine/Private/GameUserSettings.cpp
@@ -556,7 +556,7 @@ void UGameUserSettings::SaveSettings()
 void UGameUserSettings::LoadConfigIni(bool bForceReload/*=false*/)
 {
 	// Load .ini, allowing merging
-	FConfigCacheIni::LoadGlobalIniFile(GGameUserSettingsIni, TEXT("GameUserSettings"), nullptr, bForceReload, false, true, *FConfigCacheIni::GetGameUserSettingsDir());
+	FConfigCacheIni::LoadGlobalIniFile(GGameUserSettingsIni, TEXT("GameUserSettings"), nullptr, bForceReload, false, true, *FPaths::GeneratedConfigDir());
 }
 
 void UGameUserSettings::PreloadResolutionSettings()
