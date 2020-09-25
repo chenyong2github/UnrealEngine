e9b5e1d19dda12ac5d3f927797df2e422e392b9c
semion piskarev <semion.piskarev@epicgames.com>
2020-09-25 12:56:03 -0400

    MeshModelingTools: Added edit conditions to some PolyPath and brush tool properties. Also made brush size in MeshVertexPaintingTool not reset to invalid value on tool start.
    #rb Ryan.Schmidt, Jimmy.Andrews
    #jira UE-99682, UE-99681, UE-97093
    #rnx
    
    #ROBOMERGE-SOURCE: CL 14395660 in //UE4/Release-4.26/... via CL 14395667
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v742-14376270)
    
    [CL 14397494 by semion piskarev in Main branch]

--- Engine/Source/Runtime/Experimental/InteractiveToolsFramework/Public/BaseTools/BaseBrushTool.h
+++ Engine/Source/Runtime/Experimental/InteractiveToolsFramework/Public/BaseTools/BaseBrushTool.h
@@ -23,12 +23,13 @@ public:
 	UPROPERTY(EditAnywhere, Category = Brush, meta = (DisplayName = "Size", UIMin = "0.0", UIMax = "1.0", ClampMin = "0.0", ClampMax = "10.0", DisplayPriority = 1, HideEditConditionToggle, EditCondition = "bSpecifyRadius == false"))
 	float BrushSize;
 
-	/** If true, ignore relative Brush Size and use explicit World Radius */
+	/** If true, ignore relative Brush Size and use explicit world Radius */
 	UPROPERTY(EditAnywhere, Category = Brush, AdvancedDisplay)
 	bool bSpecifyRadius;
 
 	/** Radius of brush */
-	UPROPERTY(EditAnywhere, Category = Brush, AdvancedDisplay, meta = (DisplayName = "Radius", UIMin = "1.0", UIMax = "1000.0", ClampMin = "0.1", ClampMax = "50000.0"))
+	UPROPERTY(EditAnywhere, Category = Brush, AdvancedDisplay, meta = (EditCondition = "bSpecifyRadius",
+		DisplayName = "Radius", UIMin = "1.0", UIMax = "1000.0", ClampMin = "0.1", ClampMax = "50000.0"))
 	float BrushRadius;
 
 	/** Strength of the brush (0.0 - 1.0) */
