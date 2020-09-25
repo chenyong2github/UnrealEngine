30464d990419ac7063355b53532fac767728d5d4
shaun kime <Shaun.Kime@epicgames.com>
2020-09-24 23:05:36 -0400

    Making StackContext only resolve if it is the first element in the list, not if it occurs anywhere in the namespace. This helps keep the rapid iteration params clean as they would otherwise get really nasty. #rb frank.fella #jira none
    #ROBOMERGE-OWNER: shaun.kime
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 14387322 via CL 14387323
    #ROBOMERGE-BOT: (v742-14376270)
    
    [CL 14393668 by shaun kime in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x4F1FCA96, 0xE0F740E6, 0xA40F27B9, 0x03A89AD7);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x69DC6715, 0x4E904B82, 0xBBDFD0D1, 0x785944CB);
