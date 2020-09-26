37c60a1b035bdfb734d9b7239bbec8e91c925e2f
satchit subramanian <Satchit.Subramanian@epicgames.com>
2020-09-25 21:05:57 -0400

    Apply sleep state when copying data to physics in async mode (p.ApplyAsyncSleepState 0 to turn off).
    #rb Ori.Cohen, Steven.Barnett
    [FYI] Matt.Fischman
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: satchit.subramanian
    #ROBOMERGE-SOURCE: CL 14391002 via CL 14391007 via CL 14392713 via CL 14392785 via CL 14394098 via CL 14397470
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v742-14376270)
    
    [CL 14401474 by satchit subramanian in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/PhysicsEngine/PhysicsReplication.cpp
+++ Engine/Source/Runtime/Engine/Private/PhysicsEngine/PhysicsReplication.cpp
@@ -91,6 +91,9 @@ namespace CharacterMovementCVars
 
 	static int32 AlwaysResetPhysics = 0;
 	static FAutoConsoleVariableRef CVarAlwaysResetPhysics(TEXT("p.AlwaysResetPhysics"), AlwaysResetPhysics, TEXT(""));
+
+	static int32 ApplyAsyncSleepState = 1;
+	static FAutoConsoleVariableRef CVarApplyAsyncSleepState(TEXT("p.ApplyAsyncSleepState"), ApplyAsyncSleepState, TEXT(""));
 }
 
 namespace PhysicsReplicationCVars
@@ -360,8 +363,8 @@ bool FPhysicsReplication::ApplyRigidBodyState(float DeltaSeconds, FBodyInstance*
 				AsyncDesiredState.LinearVelocity = NewState.LinVel;
 				AsyncDesiredState.AngularVelocity = NewState.AngVel;
 				AsyncDesiredState.Proxy = static_cast<FRigidParticlePhysicsProxy*>(BI->GetPhysicsActorHandle()->GetProxy());
+				AsyncDesiredState.bShouldSleep = bShouldSleep;
 
-				//should we pass in sleep state?
 				CurAsyncData->Buffer.Add(AsyncDesiredState);
 			}
 
@@ -613,8 +616,13 @@ void FPhysicsReplication::ApplyAsyncDesiredState(const float DeltaSeconds, const
 			Handle->SetV(NewLinVel);
 			Handle->SetW(FMath::DegreesToRadians(NewAngVel));
 
+			EObjectStateType ObjectStateType = EObjectStateType::Dynamic;
+			if ((CharacterMovementCVars::ApplyAsyncSleepState != 0) && State.bShouldSleep)
+			{
+				ObjectStateType = EObjectStateType::Sleeping;
+			}
 			auto* Solver = Proxy->GetSolver<FPBDRigidsSolver>();
-			Solver->GetEvolution()->SetParticleObjectState(Handle, EObjectStateType::Dynamic);
+			Solver->GetEvolution()->SetParticleObjectState(Handle, ObjectStateType);
 		}
 	}
 }
