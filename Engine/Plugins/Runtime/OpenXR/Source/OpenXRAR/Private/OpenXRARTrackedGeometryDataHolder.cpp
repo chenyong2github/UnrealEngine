4fd752fa2d2e739dd90e8fb3e2c1f4498a51416d
jeff fisher <Jeff.Fisher@epicgames.com>
2020-10-05 17:45:39 -0400

    UEVR-1959 QR code tracking for OpenXR -Adds interfaces for using QR code ar tracked geometries in OpenXR. -Fixed std:c++17, etc build settings for Hololens (used in microsoft's openxr plugin). #jira UEVR-1959 #rb Jason.Bestimt
    #ROBOMERGE-SOURCE: CL 14424797 in //UE4/Release-4.26/... via CL 14424806
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v742-14376270)
    
    [CL 14424818 by jeff fisher in Main branch]

---
+++ Engine/Plugins/Runtime/OpenXR/Source/OpenXRAR/Private/OpenXRARTrackedGeometryDataHolder.cpp
@@ -0,0 +1,74 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "IOpenXRARTrackedGeometryHolder.h"
+#include "MRMeshBufferDefines.h"
+#include "MRMeshComponent.h"
+
+UARTrackedGeometry* FOpenXRQRCodeData::ConstructNewTrackedGeometry(TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface)
+{
+	check(IsInGameThread());
+
+	UARTrackedQRCode* NewQRCode = NewObject<UARTrackedQRCode>();
+	NewQRCode->UniqueId = Id;
+	return NewQRCode;
+};
+
+void FOpenXRQRCodeData::UpdateTrackedGeometry(UARTrackedGeometry* TrackedGeometry, TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface)
+{
+	check(IsInGameThread());
+
+	UARTrackedQRCode* UpdatedQRCode = Cast<UARTrackedQRCode>(TrackedGeometry);
+	check(UpdatedQRCode != nullptr);
+
+	UpdatedQRCode->UpdateTrackedGeometry(ARSupportInterface.ToSharedRef(),
+		GFrameCounter,
+		Timestamp,
+		LocalToTrackingTransform,
+		ARSupportInterface->GetAlignmentTransform(),
+		Size,
+		QRCode,
+		Version);
+	UpdatedQRCode->SetTrackingState(TrackingState);
+}
+
+
+UARTrackedGeometry* FOpenXRMeshUpdate::ConstructNewTrackedGeometry(TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface)
+{
+	check(IsInGameThread());
+
+	UARTrackedGeometry* NewMesh = NewObject<UARTrackedGeometry>();
+	NewMesh->UniqueId = Id;
+	return NewMesh;
+};
+
+void FOpenXRMeshUpdate::UpdateTrackedGeometry(UARTrackedGeometry* TrackedGeometry, TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface)
+{
+	check(IsInGameThread());
+
+	UARTrackedGeometry* UpdatedGeometry = Cast<UARTrackedGeometry>(TrackedGeometry);
+	check(UpdatedGeometry != nullptr);
+
+	if (Vertices.Num() > 0)
+	{
+		// Update MRMesh if it's available
+		if (auto MRMesh = UpdatedGeometry->GetUnderlyingMesh())
+		{
+			// MRMesh takes ownership of the data in the arrays at this point
+			MRMesh->UpdateMesh(LocalToTrackingTransform.GetLocation(), LocalToTrackingTransform.GetRotation(), LocalToTrackingTransform.GetScale3D(), Vertices, Indices);
+		}
+	}
+
+	// Update the tracking data, it MUST be done after UpdateMesh
+	UpdatedGeometry->UpdateTrackedGeometry(ARSupportInterface.ToSharedRef(),
+		GFrameCounter,
+		FPlatformTime::Seconds(),
+		LocalToTrackingTransform,
+		ARSupportInterface->GetAlignmentTransform());
+	// Mark this as a world mesh that isn't recognized as a particular scene type, since it is loose triangles
+	UpdatedGeometry->SetObjectClassification(Type);
+	UpdatedGeometry->SetTrackingState(TrackingState);
+
+	check(false); // Meshes are currently handled with a unique codepath.
+}
