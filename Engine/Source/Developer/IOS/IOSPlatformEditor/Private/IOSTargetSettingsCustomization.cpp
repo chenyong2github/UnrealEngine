dddcee157af82281413ffa5188caccda06e567bd
axel riffard <axel.riffard@epicgames.com>
2020-10-07 05:38:40 -0400

    Remove iOS 6 to 11 references, make iOS 12 minimum support iOS version, add iOS14 #jira UE-100088 #rb jack.porter
    #ROBOMERGE-SOURCE: CL 14436601 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v744-14430964)
    
    [CL 14436605 by axel riffard in Release-Engine-Staging branch]

--- Engine/Source/Developer/IOS/IOSPlatformEditor/Private/IOSTargetSettingsCustomization.cpp
+++ Engine/Source/Developer/IOS/IOSPlatformEditor/Private/IOSTargetSettingsCustomization.cpp
@@ -1758,7 +1758,7 @@ void FIOSTargetSettingsCustomization::SetShaderStandard(int32 Value)
 	{
 		FText Message;
 		
-		uint8 EnumValue = (uint8)EIOSVersion::IOS_11;
+		uint8 EnumValue = (uint8)EIOSVersion::IOS_12;
 		if (MinOSPropertyHandle.IsValid())
 		{
 			MinOSPropertyHandle->GetValue(EnumValue);
@@ -1770,34 +1770,10 @@ void FIOSTargetSettingsCustomization::SetShaderStandard(int32 Value)
 			MRTPropertyHandle->GetValue(bMRTEnabled);
 		}
 		
-		if (Value == 1 && ((EIOSVersion)EnumValue < EIOSVersion::IOS_9))
-		{
-			Message = LOCTEXT("iOSMetalShaderVersion1_1","Enabling Metal Shader Standard v1.1 increases the minimum operating system requirement for Metal from iOS 8.0 or later to iOS 9.0 or later. This does not affect tvOS.");
-			SetMinVersion((int32)EIOSVersion::IOS_9);
-		}
-		else if (Value < 3 && bMRTEnabled)
-		{
-			FPropertyAccess::Result ResMRT = ShaderVersionPropertyHandle->SetValue((uint8)3);
-			check(ResMRT == FPropertyAccess::Success);
-
-			Message = LOCTEXT("MetalMRTStandardv1.2","Enabling the Desktop Forward Renderer Metal requires Shader Standard v2.0 which increases the minimum operating system requirement for Metal from iOS 10.0 or later to iOS 11.0 or later.");
-			SetMinVersion((int32)EIOSVersion::IOS_11);
-		}
-		else if (Value == 3 && (EIOSVersion)EnumValue < EIOSVersion::IOS_11)
-		{
-			Message = LOCTEXT("iOSMetalShaderVersion2_0","Enabling Metal Shader Standard v2.0 increases the minimum operating system requirement for Metal from iOS 10.0/tvOS 10.0 or later to iOS/tvOS 11.0 or later.");
-			SetMinVersion((int32)EIOSVersion::IOS_11);
-		}
-        else if (Value == 4 && (EIOSVersion)EnumValue < EIOSVersion::IOS_12)
-        {
-            Message = LOCTEXT("iOSMetalShaderVersion2_1","Enabling Metal Shader Standard v2.1 increases the minimum operating system requirement for Metal from iOS 10.0/tvOS 10.0 or later to iOS/tvOS 12.0 or later.");
-            SetMinVersion((int32)EIOSVersion::IOS_12);
-        }
-
 		// make sure we never set the min version to less than current supported
-		if (((EIOSVersion)EnumValue < EIOSVersion::IOS_11))
+		if (((EIOSVersion)EnumValue < EIOSVersion::IOS_12))
 		{
-			SetMinVersion((int32)EIOSVersion::IOS_11);
+			SetMinVersion((int32)EIOSVersion::IOS_12);
 		}
 
 		
@@ -1828,9 +1804,9 @@ void FIOSTargetSettingsCustomization::UpdateOSVersionWarning()
 		{
 			uint8 EnumValue;
 			MinOSPropertyHandle->GetValue(EnumValue);
-			if (EnumValue < (uint8)EIOSVersion::IOS_11)
+			if (EnumValue < (uint8)EIOSVersion::IOS_12)
 			{
-				SetMinVersion((int32)EIOSVersion::IOS_11);
+				SetMinVersion((int32)EIOSVersion::IOS_12);
 				
 				FText Message;
 				Message = LOCTEXT("MetalMRTStandardv1.2","Enabling the Desktop Forward Renderer Metal requires Shader Standard v2.0 which increases the minimum operating system requirement for Metal from iOS 10.0 or later to iOS 11.0 or later.");
@@ -1856,9 +1832,9 @@ void FIOSTargetSettingsCustomization::UpdateMetalMRTWarning()
 		{
 			uint8 EnumValue;
 			MinOSPropertyHandle->GetValue(EnumValue);
-			if (EnumValue < (uint8)EIOSVersion::IOS_11)
+			if (EnumValue < (uint8)EIOSVersion::IOS_12)
 			{
-				SetMinVersion((int32)EIOSVersion::IOS_11);
+				SetMinVersion((int32)EIOSVersion::IOS_12);
 				
 				FText Message;
 				Message = LOCTEXT("MetalMRTStandardv1.2","Enabling the Desktop Forward Renderer Metal requires Shader Standard v2.0 which increases the minimum operating system requirement for Metal from iOS 10.0 or later to iOS 11.0 or later.");
