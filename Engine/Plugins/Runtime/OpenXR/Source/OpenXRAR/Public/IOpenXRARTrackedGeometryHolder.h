3a1032670b3243fdffe1a5ac7cc03a24c7c48975
jeff fisher <Jeff.Fisher@epicgames.com>
2020-10-08 19:13:43 -0400

    UEVR-1959 QR code tracking for OpenXR -Adds interfaces for using QR code ar tracked geometries in OpenXR. -Fixed std:c++17, etc build settings for Hololens (used in microsoft's openxr plugin). #jira UEVR-1959 #rb Jason.Bestimt
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jeff.fisher
    #ROBOMERGE-SOURCE: CL 14424797 in //UE4/Release-4.26/... via CL 14424806 via CL 14424818
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14452534 by jeff fisher in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Runtime/OpenXR/Source/OpenXRAR/Public/IOpenXRARTrackedGeometryHolder.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ARTypes.h"
+#include "ARSupportInterface.h"
+
+class IOpenXRARTrackedGeometryHolder : public IModularFeature
+{
+public:
+	static FName GetModularFeatureName()
+	{
+		static FName FeatureName = FName(TEXT("OpenXRARTrackedGeometryHolder"));
+		return FeatureName;
+	}
+
+	virtual ~IOpenXRARTrackedGeometryHolder() {}
+
+	virtual void ARTrackedGeometryAdded(struct FOpenXRARTrackedGeometryData* InData) = 0;
+	virtual void ARTrackedGeometryUpdated(struct FOpenXRARTrackedGeometryData* InData) = 0;
+	virtual void ARTrackedGeometryRemoved(struct FOpenXRARTrackedGeometryData* InData) = 0;
+};
+
+class IOpenXRARTrackedMeshHolder
+{
+public:
+	virtual ~IOpenXRARTrackedMeshHolder() {}
+
+	virtual void StartMeshUpdates() = 0;
+	virtual struct FOpenXRMeshUpdate* AllocateMeshUpdate(FGuid InGuidMeshUpdate) = 0;
+	virtual void RemoveMesh(FGuid InGuidMeshUpdate) = 0;
+	virtual void EndMeshUpdates() = 0;
+};
+
+// Base class for ARTrackedGeometryData
+// These structs are designed to be constructed by openxr plugin code on any thread and then used by UE4 on the Game Thread.
+// All their data members need to be safe to create on any thread.
+struct FOpenXRARTrackedGeometryData : public FNoncopyable
+{
+	enum class EDataType : int8
+	{
+		Unknown,
+		Mesh,
+		QRCode
+	};
+	const EDataType DataType;
+	FGuid Id;
+	EARTrackingState TrackingState = EARTrackingState::Unknown;
+	FTransform LocalToTrackingTransform;
+
+protected:
+	// Force derived classes to init the type.
+	FOpenXRARTrackedGeometryData(EDataType InDataType) :
+		DataType(InDataType)
+	{
+	}
+
+public:
+	virtual ~FOpenXRARTrackedGeometryData() {}
+
+	OPENXRAR_API virtual UARTrackedGeometry* ConstructNewTrackedGeometry(TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) = 0;
+	OPENXRAR_API virtual void UpdateTrackedGeometry(UARTrackedGeometry* TrackedGeometry, TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) = 0;
+};
+
+struct FOpenXRQRCodeData : public FOpenXRARTrackedGeometryData
+{
+
+	FVector2D Size = {};
+	FString QRCode;
+	int32 Version = 0;
+	double Timestamp = 0;
+
+	FOpenXRQRCodeData() :
+		FOpenXRARTrackedGeometryData(EDataType::QRCode)
+	{
+	}
+
+	OPENXRAR_API virtual UARTrackedGeometry* ConstructNewTrackedGeometry(TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) override;
+	OPENXRAR_API virtual void UpdateTrackedGeometry(UARTrackedGeometry* TrackedGeometry, TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) override;
+};
+
+struct FOpenXRMeshUpdate : public FOpenXRARTrackedGeometryData
+{
+	EARObjectClassification Type = EARObjectClassification::NotApplicable;
+	TArray<FVector> Vertices;
+	TArray<MRMESH_INDEX_TYPE> Indices;
+
+	FOpenXRMeshUpdate() :
+		FOpenXRARTrackedGeometryData(EDataType::Mesh)
+	{
+	}
+
+	OPENXRAR_API virtual UARTrackedGeometry* ConstructNewTrackedGeometry(TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) override;
+	OPENXRAR_API virtual void UpdateTrackedGeometry(UARTrackedGeometry* TrackedGeometry, TSharedPtr<FARSupportInterface, ESPMode::ThreadSafe> ARSupportInterface) override;
+};
