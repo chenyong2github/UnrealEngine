394518583103b4ecb7bc9dfba9d9f10e0289a274
ryan schmidt <Ryan.Schmidt@epicgames.com>
2020-10-08 21:21:03 -0400

    ModelingMode: add ToolSceneQueriesUtil::FindNearestVisibleObjectHit(), which runs a multi-object LineTrace into the World and then finds the nearest hit that is still visible in the Editor. Replace LineTrace calls in various Tools/Mechanics/etc with calls to this function, so that hidden-in-editor objects are not hit by various interactions in the Tools.
    InteractiveToolsFramework: add modified copy of above function to EdModeInteractiveToolsContext, for use in scene geometry snap queries (cannot share this code because the ITF cannot depend on ModelingTools plugin that ToolSceneQueriesUtil lives in)
    
    DrawPolygonTool now respects shift-to-ignore-snapping during setting height.
    
    #rb david.hill
    #rnx
    #jira UE-100740
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 14448048 in //UE4/Release-4.26/... via CL 14448079 via CL 14448090
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14456092 by ryan schmidt in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/ModelingToolsEditorMode/Source/ModelingToolsEditorMode/ModelingToolsEditorMode.Build.cs
+++ Engine/Plugins/Experimental/ModelingToolsEditorMode/Source/ModelingToolsEditorMode/ModelingToolsEditorMode.Build.cs
@@ -51,6 +51,7 @@ public class ModelingToolsEditorMode : ModuleRules
 				"StaticMeshDescription",
                 "InteractiveToolsFramework",
 				"EditorInteractiveToolsFramework",
+				"ModelingComponents",
 				"MeshModelingTools",
 				"MeshModelingToolsEditorOnly",
 
