c26a642288ed7a5eeeb82cd458b3c4b003f5bdc1
ryan mangin <Ryan.Mangin@epicgames.com>
2020-10-08 20:57:30 -0400

    Speculative Fix / defensive code change for crash in USoundCue::EvaluateNodes
    #jira: FORT-319443
    #rb: aaron.mcleran, jimmy.smith
    
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: ryan.mangin
    #ROBOMERGE-SOURCE: CL 14438638 via CL 14438650 via CL 14438658 via CL 14438665
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14455630 by ryan mangin in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/SoundCue.cpp
+++ Engine/Source/Runtime/Engine/Private/SoundCue.cpp
@@ -289,6 +289,11 @@ void USoundCue::EvaluateNodes(bool bAddToRoot)
 
 	TFunction<void(USoundNode*)> EvaluateNodes_Internal = [&](USoundNode* SoundNode)
 	{
+		if (SoundNode == nullptr)
+		{
+			return;
+		}
+
 		if (USoundNodeAssetReferencer* AssetReferencerNode = Cast<USoundNodeAssetReferencer>(SoundNode))
 		{
 			AssetReferencerNode->ConditionalPostLoad();
@@ -296,19 +301,16 @@ void USoundCue::EvaluateNodes(bool bAddToRoot)
 		}
 		else if (USoundNodeQualityLevel* QualityLevelNode = Cast<USoundNodeQualityLevel>(SoundNode))
 		{
-			if (CachedQualityLevel < QualityLevelNode->ChildNodes.Num())
+			if (QualityLevelNode->ChildNodes.IsValidIndex(CachedQualityLevel))
 			{
 				EvaluateNodes_Internal(QualityLevelNode->ChildNodes[CachedQualityLevel]);
 			}
 		}
-		else if (SoundNode)
+		else
 		{
 			for (USoundNode* ChildNode : SoundNode->ChildNodes)
 			{
-				if (ChildNode)
-				{
-					EvaluateNodes_Internal(ChildNode);
-				}
+				EvaluateNodes_Internal(ChildNode);
 			}
 		}
 	};
