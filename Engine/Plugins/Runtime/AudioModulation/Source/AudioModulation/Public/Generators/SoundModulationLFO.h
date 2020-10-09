ce5217877f0b93c44e667235bed2f36b357f7a1d
Rob Gay <Rob.Gay@epicgames.com>
2020-10-09 11:38:30 -0400

    Edigrate from 4 to 5: - Clean-up Generator property UX - Add invert flag to EnvelopeFollower to allow AudioBus to gate source #rb phil.popp #jira none
    [CL 14460466 by Rob Gay in ue5-main branch]

--- Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Public/Generators/SoundModulationLFO.h
+++ Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Public/Generators/SoundModulationLFO.h
@@ -32,27 +32,27 @@ struct FSoundModulationLFOParams
 	GENERATED_USTRUCT_BODY()
 
 	/** Shape of oscillating waveform */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 20, EditCondition = "!bBypass"))
 	FSoundModulationLFOShape Shape;
 
 	/** Amplitude of oscillator */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 30, EditCondition = "!bBypass", UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
 	float Amplitude = 0.5f;
 
 	/** Frequency of oscillator */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (UIMin = "0", UIMax = "20", ClampMin = "0", ClampMax = "20"))
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 40, EditCondition = "!bBypass", UIMin = "0", UIMax = "20", ClampMin = "0", ClampMax = "20"))
 	float Frequency = 1.0f;
 
 	/** Offset of oscillator */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 50, EditCondition = "!bBypass", UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
 	float Offset = 0.5;
 
 	/** Whether or not to loop the oscillation more than once */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 60, EditCondition = "!bBypass"))
 	bool bLooping = 1;
 
 	/** If true, bypasses LFO bus from being modulated by parameters, patches, or mixed (LFO remains active and computed). */
-	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, Category = Modulation, BlueprintReadWrite, meta = (DisplayPriority = 10))
 	bool bBypass = 0;
 };
 
