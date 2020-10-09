c8e98fa0b454b57265de1db231ff705e419cb708
andrew rodham <Andrew.Rodham@epicgames.com>
2020-10-09 14:14:25 -0400

    Sequencer: Creating attach tracks with spawnable parents now correctly initinizes a binding ID for the spawnable, rather than creating a new broken posessable
    #rb Max.Chen, Ludovic.Chabant, Matt.Hoffman
    [FYI] Colton.George
    
    
    #ROBOMERGE-SOURCE: CL 14462930 via CL 14462931 via CL 14462934
    #ROBOMERGE-BOT: (v748-14444958)
    
    [CL 14462941 by andrew rodham in Release-Engine-Staging branch]

--- Engine/Source/Runtime/MovieScene/Private/MovieSceneSpawnableAnnotation.cpp
+++ Engine/Source/Runtime/MovieScene/Private/MovieSceneSpawnableAnnotation.cpp
@@ -9,12 +9,13 @@
 
 static FUObjectAnnotationSparse<FMovieSceneSpawnableAnnotation,true> SpawnedObjectAnnotation;
 
-void FMovieSceneSpawnableAnnotation::Add(UObject* SpawnedObject, const FGuid& ObjectBindingID, UMovieSceneSequence* InOriginatingSequence)
+void FMovieSceneSpawnableAnnotation::Add(UObject* SpawnedObject, const FGuid& ObjectBindingID, FMovieSceneSequenceID SequenceID, UMovieSceneSequence* InOriginatingSequence)
 {
 	if (SpawnedObject)
 	{
 		FMovieSceneSpawnableAnnotation Annotation;
 		Annotation.ObjectBindingID = ObjectBindingID;
+		Annotation.SequenceID = SequenceID;
 		Annotation.OriginatingSequence = InOriginatingSequence;
 
 		SpawnedObjectAnnotation.AddAnnotation(SpawnedObject, MoveTemp(Annotation));
