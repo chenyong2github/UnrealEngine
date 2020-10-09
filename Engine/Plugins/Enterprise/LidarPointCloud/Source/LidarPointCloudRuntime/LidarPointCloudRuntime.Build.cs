954aceac4db6f0111174badf434bf5b85b9c561c
michal cieciura <michal.cieciura@xa.epicgames.com>
2020-10-08 20:39:44 -0400

    Lidar: Added switch to compile out the LAZ support
    #jira UEENT-2434
    #rb Julien.StJean
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: michal.cieciura
    #ROBOMERGE-SOURCE: CL 14432840 in //UE4/Release-4.26/... via CL 14432849 via CL 14432851
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14455193 by michal cieciura in Dev-Tools-Staging branch]

--- Engine/Plugins/Enterprise/LidarPointCloud/Source/LidarPointCloudRuntime/LidarPointCloudRuntime.Build.cs
+++ Engine/Plugins/Enterprise/LidarPointCloud/Source/LidarPointCloudRuntime/LidarPointCloudRuntime.Build.cs
@@ -41,10 +41,17 @@ namespace UnrealBuildTool.Rules
 
 			// Currently, E57 is only supported on Windows
 			bool bSupportLibE57 = true;
+			
+			// Currently, LAZ is only supported on Windows and Mac
+			bool bSupportLasZip = true;
 
 			if (Target.Platform == UnrealTargetPlatform.Win64)
 			{
-				RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Source", "ThirdParty", "LasZip", "Win64", "laszip.dll"));
+				if (bSupportLasZip)
+				{
+					RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Source", "ThirdParty", "LasZip", "Win64", "laszip.dll"));
+				}
+
 				if(bSupportLibE57)
 				{
 					RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Source", "ThirdParty", "LibE57", "Win64", "xerces-c_3_2.dll"));
@@ -53,14 +60,20 @@ namespace UnrealBuildTool.Rules
 			}
 			else if (Target.Platform == UnrealTargetPlatform.Mac)
 			{
-				RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Source", "ThirdParty", "LasZip", "Mac", "laszip.dylib"));
+				if (bSupportLasZip)
+				{
+					RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Source", "ThirdParty", "LasZip", "Mac", "laszip.dylib"));
+				}
+				
 				bSupportLibE57 = false;
 			}
 			else
 			{
+				bSupportLasZip = false;
 				bSupportLibE57 = false;
 			}
 
+			AppendStringToPublicDefinition("LASZIPSUPPORTED", bSupportLasZip ? "1" : "0");
 			AppendStringToPublicDefinition("LIBE57SUPPORTED", bSupportLibE57 ? "1" : "0");
 		}
 	}
