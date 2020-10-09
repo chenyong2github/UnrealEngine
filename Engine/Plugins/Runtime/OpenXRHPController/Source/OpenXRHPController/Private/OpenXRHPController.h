ca133b82716ed76fc33ee3ff831c1b2b7ffbbfca
steve smith <steve.smith@epicgames.com>
2020-10-08 21:29:43 -0400

    This adds interaction profile support to the upcoming HP Reverb G2's controllers for developers who wish to natively target the new button and axis mappings (a,b,x,y buttons/ analog grip) Submitted by Microsoft #jira UEVR-1972 #rb Steve.smith
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: steve.smith
    #ROBOMERGE-SOURCE: CL 14450434 in //UE4/Release-4.26/... via CL 14450450 via CL 14450452
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14456277 by steve smith in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Runtime/OpenXRHPController/Source/OpenXRHPController/Private/OpenXRHPController.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+#pragma once
+
+#include "IOpenXRExtensionPlugin.h"
+
+class FOpenXRHPControllerModule :
+	public IModuleInterface,
+	public IOpenXRExtensionPlugin
+{
+public:
+	FOpenXRHPControllerModule() { }
+
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+	bool GetRequiredExtensions(TArray<const ANSICHAR*>& OutExtensions) override;
+	bool GetInteractionProfile(XrInstance InInstance, FString& OutKeyPrefix, XrPath& OutPath, bool& OutHasHaptics) override;
+};
+
