4d6925f99aee16db02d6011f8abadab3c01c1408
Jason Adcock <jason.adcock@xa.epicgames.com>
2020-10-09 21:28:08 -0400

    Merging in fix for NetTests in EngineTest. #rb none #jira UE-99418
    [CL 14467601 by Jason Adcock in 4.26 branch]

--- Engine/Source/Runtime/Engine/Public/Net/RepLayout.h
+++ Engine/Source/Runtime/Engine/Public/Net/RepLayout.h
@@ -711,17 +711,18 @@ enum class ERepLayoutCmdType : uint8
 enum class ERepParentFlags : uint32
 {
 	None						= 0,
-	IsLifetime					= (1 << 0),	//! This property is valid for the lifetime of the object (almost always set).
-	IsConditional				= (1 << 1),	//! This property has a secondary condition to check
-	IsConfig					= (1 << 2),	//! This property is defaulted from a config file
-	IsCustomDelta				= (1 << 3),	//! This property uses custom delta compression. Mutually exclusive with IsNetSerialize.
-	IsNetSerialize				= (1 << 4), //! This property uses a custom net serializer. Mutually exclusive with IsCustomDelta.
-	IsStructProperty			= (1 << 5),	//! This property is a FStructProperty.
-	IsZeroConstructible			= (1 << 6),	//! This property is ZeroConstructible.
-	IsFastArray					= (1 << 7),	//! This property is a FastArraySerializer. This can't be a ERepLayoutCmdType, because
-											//! these Custom Delta structs will have their inner properties tracked.
-	HasObjectProperties			= (1 << 8), //! This property is tracking UObjects (may be through nested properties).
-	HasNetSerializeProperties	= (1 << 9), //! This property contains Net Serialize properties (may be through nested properties).
+	IsLifetime					= (1 << 0),	 //! This property is valid for the lifetime of the object (almost always set).
+	IsConditional				= (1 << 1),	 //! This property has a secondary condition to check
+	IsConfig					= (1 << 2),	 //! This property is defaulted from a config file
+	IsCustomDelta				= (1 << 3),	 //! This property uses custom delta compression. Mutually exclusive with IsNetSerialize.
+	IsNetSerialize				= (1 << 4),  //! This property uses a custom net serializer. Mutually exclusive with IsCustomDelta.
+	IsStructProperty			= (1 << 5),	 //! This property is a FStructProperty.
+	IsZeroConstructible			= (1 << 6),	 //! This property is ZeroConstructible.
+	IsFastArray					= (1 << 7),	 //! This property is a FastArraySerializer. This can't be a ERepLayoutCmdType, because
+											 //! these Custom Delta structs will have their inner properties tracked.
+	HasObjectProperties			= (1 << 8),  //! This property is tracking UObjects (may be through nested properties).
+	HasNetSerializeProperties	= (1 << 9),  //! This property contains Net Serialize properties (may be through nested properties).
+	HasDynamicArrayProperties   = (1 << 10), //! This property contains Dynamic Array properties (may be through nested properties).
 };
 
 ENUM_CLASS_FLAGS(ERepParentFlags)
