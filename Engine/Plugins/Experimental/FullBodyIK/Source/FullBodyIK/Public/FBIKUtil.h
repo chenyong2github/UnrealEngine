a06111b11ccdab0177ada5e1b436d505c33d395c
lina halper <lina.halper@epicgames.com>
2020-10-09 20:55:05 -0400

    Fix depth causing incorrect order of chain  - while looking at jira UE-96926, I noticed crash happening when depth is not reaching the root in the first effector  - the reason is that it only gets the chain up to max depth, and causing the order of joint to be incorrect  - so here I changed, so that it gets all chain all the time, but later we cull based on the max of depth  - one more thing I changed, so that it only does add effectors outside of editor once. In this case, some of information such as Depth will be not animatable since it will be only cached first time.
    #jira: UE-96926
    #rb: Halfdan.Ingvarsson
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: lina.halper
    #ROBOMERGE-SOURCE: CL 14464639 in //UE4/Release-4.26/... via CL 14464824 via CL 14464878
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14467254 by lina halper in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/FullBodyIK/Source/FullBodyIK/Public/FBIKUtil.h
+++ Engine/Plugins/Experimental/FullBodyIK/Source/FullBodyIK/Public/FBIKUtil.h
@@ -12,7 +12,7 @@ namespace FBIKUtil
 	/** Utility functions */
 	FVector FULLBODYIK_API GetScaledRotationAxis(const FQuat& InQuat);
 	bool FULLBODYIK_API CanCrossProduct(const FVector& Vector1, const FVector& Vector2);
-	bool FULLBODYIK_API GetBoneChain(FRigHierarchyContainer* Hierarchy, const FRigElementKey& Root, const FRigElementKey& Current, int32 MaxDepth, TArray<FRigElementKey>& ChainIndices);
+	bool FULLBODYIK_API GetBoneChain(FRigHierarchyContainer* Hierarchy, const FRigElementKey& Root, const FRigElementKey& Current, TArray<FRigElementKey>& ChainIndices);
 };
 
 
