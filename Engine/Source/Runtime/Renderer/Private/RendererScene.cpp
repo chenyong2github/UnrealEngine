e40c10d062abee1f127eb840cfd8928dc87cf9e5
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2020-10-12 02:05:56 -0400

    Fixed: Indirect lighting cache does not work correctly on mobile with a multiple lighting scenarios #jira UE-100921 #rb none
    #ROBOMERGE-SOURCE: CL 14471003 in //UE4/Release-4.26/... via CL 14471011
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v748-14444958)
    
    [CL 14471012 by dmitriy dyomin in Main branch]

--- Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
+++ Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
@@ -2343,6 +2343,9 @@ void FVolumetricLightmapSceneData::AddLevelVolume(const FPrecomputedVolumetricLi
 	}
 
 	InVolume->Data->AddToSceneData(&GlobalVolumetricLightmapData);
+	
+	// Invalidate CPU lightmap lookup cache
+	CPUInterpolationCache.Empty();
 }
 
 void FVolumetricLightmapSceneData::RemoveLevelVolume(const FPrecomputedVolumetricLightmap* InVolume)
@@ -2355,6 +2358,9 @@ void FVolumetricLightmapSceneData::RemoveLevelVolume(const FPrecomputedVolumetri
 	{
 		PersistentLevelVolumetricLightmap = nullptr;
 	}
+
+	// Invalidate CPU lightmap lookup cache
+	CPUInterpolationCache.Empty();
 }
 
 const FPrecomputedVolumetricLightmap* FVolumetricLightmapSceneData::GetLevelVolumetricLightmap() const
