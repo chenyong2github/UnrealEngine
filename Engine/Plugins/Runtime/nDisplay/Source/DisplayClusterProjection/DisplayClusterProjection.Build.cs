f4c91b73a6db9b53ab8bdf58f843b1e58d446337
Andrey Yamashev <andrey.yamashev@xa.epicgames.com>
2020-10-12 17:13:18 -0400

    nDisplay: fixed CIS issues in Main
    #jira none
    #rb none
    
    [CL 14475180 by Andrey Yamashev in 4.26 branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterProjection/DisplayClusterProjection.Build.cs
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterProjection/DisplayClusterProjection.Build.cs
@@ -45,7 +45,9 @@ public class DisplayClusterProjection : ModuleRules
 		}
 
 		AddEngineThirdPartyPrivateStaticDependencies(Target, "DX11");
-
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "NVAftermath");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "IntelMetricsDiscovery");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "IntelExtensionsFramework");
 		AddThirdPartyDependencies(ROTargetRules);
 	}
 
