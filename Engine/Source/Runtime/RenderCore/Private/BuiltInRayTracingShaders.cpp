3bdff9776b1d56bd636e674f2281e6a2480deca9
yuriy odonnell <Yuriy.ODonnell@epicgames.com>
2020-10-13 09:38:54 -0400

    Fixed payload structure mismatch in ray tracing PSOs. DXR specification requires that all payloads referenced by any invoked shaders are the same.
    #rb Juan.Canada
    #jira UE-98223
    
    #ROBOMERGE-OWNER: yuriy.odonnell
    #ROBOMERGE-AUTHOR: yuriy.odonnell
    #ROBOMERGE-SOURCE: CL 14478930 in //UE4/Release-4.25Plus/...
    #ROBOMERGE-BOT: NEXTGEN (Release-4.25Plus -> Release-4.26) (v748-14444958)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 14479132 by yuriy odonnell in 4.26 branch]

--- Engine/Source/Runtime/RenderCore/Private/BuiltInRayTracingShaders.cpp
+++ Engine/Source/Runtime/RenderCore/Private/BuiltInRayTracingShaders.cpp
@@ -9,6 +9,7 @@ IMPLEMENT_GLOBAL_SHADER( FIntersectionMainRG,	"/Engine/Private/RayTracing/RayTra
 IMPLEMENT_SHADER_TYPE(, FIntersectionMainCHS,	TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("IntersectionMainCHS"),	SF_RayHitGroup);
 IMPLEMENT_SHADER_TYPE(, FDefaultMainCHS,		TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("DefaultMainCHS"),		SF_RayHitGroup);
 IMPLEMENT_SHADER_TYPE(, FDefaultMainCHSOpaqueAHS, TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("closesthit=DefaultMainCHS anyhit=DefaultOpaqueAHS"), SF_RayHitGroup);
-IMPLEMENT_SHADER_TYPE(, FDefaultMainMS,			TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("DefaultMainMS"),			SF_RayMiss);
+IMPLEMENT_SHADER_TYPE(, FDefaultPayloadMS,		TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("DefaultPayloadMS"),			SF_RayMiss);
+IMPLEMENT_SHADER_TYPE(, FPackedMaterialClosestHitPayloadMS, TEXT("/Engine/Private/RayTracing/RayTracingBuiltInShaders.usf"), TEXT("PackedMaterialClosestHitPayloadMS"), SF_RayMiss);
 
 #endif // RHI_RAYTRACING
