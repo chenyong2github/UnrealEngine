20e572e19474fdda8633f60dbc406454b04556a2
steve smith <steve.smith@epicgames.com>
2020-10-13 15:15:14 -0400

    Fix crash in Unreal test framework. Unreal test framework (AutomationTest.h/.cpp) needs at last one frame in the callstack. HL has no access to callstack generation, so it returned an empty stack array. That led to the crash. Submitted by Microsoft #rb steve.smith #jira UEVR-1958
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: steve.smith
    #ROBOMERGE-SOURCE: CL 14476503 in //UE4/Release-4.26/... via CL 14476519 via CL 14476533
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14483598 by steve smith in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Core/Private/HoloLens/HoloLensStackWalk.cpp
+++ Engine/Source/Runtime/Core/Private/HoloLens/HoloLensStackWalk.cpp
@@ -6,3 +6,10 @@
 
 #include "HoloLensPlatformStackWalk.h"
 
+TArray<FProgramCounterSymbolInfo> FHololensPlatformStackWalk::GetStack(int32 IgnoreCount, int32 MaxDepth, void* Context)
+{
+	TArray<FProgramCounterSymbolInfo> Stack;
+	FProgramCounterSymbolInfo dummy;
+	Stack.Push(dummy);
+	return Stack;
+}
