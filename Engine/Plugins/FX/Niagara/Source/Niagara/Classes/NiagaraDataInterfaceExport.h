214e06442af85f0af3b13d02343e520790e49268
stu mckenna <Stu.McKenna@epicgames.com>
2020-10-12 23:41:23 -0400

    - Implement export data interface for GPU systems - Add gpu readback manager to allow enqueuing a readback that is not depending on a system ticking
    #rb shaun.kime, jamie.hayes, michael.galetzka
    
    
    #ROBOMERGE-SOURCE: CL 14477452 via CL 14477457
    #ROBOMERGE-BOT: (v748-14444958)
    
    [CL 14477458 by stu mckenna in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceExport.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceExport.h
@@ -38,15 +38,11 @@ public:
 	void ReceiveParticleData(const TArray<FBasicParticleData>& Data, UNiagaraSystem* NiagaraSystem);
 };
 
-struct ExportInterface_InstanceData
+UENUM()
+enum class ENDIExport_GPUAllocationMode : uint8
 {
-	TWeakObjectPtr<UObject> CallbackHandler;
-
-	/** We use a lock-free queue here because multiple threads might try to push data to it at the same time. */
-	TQueue<FBasicParticleData, EQueueMode::Mpsc> GatheredData;
-
-	/** A binding to the user ptr we're reading the handler from. */
-	FNiagaraParameterDirectBinding<UObject*> UserParamBinding;
+	FixedSize,
+	PerParticle,
 };
 
 /** This Data Interface can be used to gather particles at execution time and call either a 
@@ -57,9 +53,22 @@ class NIAGARA_API UNiagaraDataInterfaceExport : public UNiagaraDataInterface
 	GENERATED_UCLASS_BODY()
 
 public:
+	DECLARE_NIAGARA_DI_PARAMETER();
+
 	/** Reference to a user parameter that should receive the particle data after the simulation tick. The supplied parameter object needs to implement the INiagaraParticleCallbackHandler interface. */
 	UPROPERTY(EditAnywhere, Category = "Export")
 	FNiagaraUserParameterBinding CallbackHandlerParameter;
+
+	UPROPERTY(EditAnywhere, Category = "Export GPU")
+	ENDIExport_GPUAllocationMode GPUAllocationMode = ENDIExport_GPUAllocationMode::FixedSize;
+
+	/** When using fixed size allocation this is how many particles can export data per tick. */
+	UPROPERTY(EditAnywhere, Category = "Export GPU", meta = (EditCondition = "GPUAllocationMode == ENDIExport_GPUAllocationMode::FixedSize"))
+	int GPUAllocationFixedSize = 64;
+
+	/** When using per particle allocation we use the current particle count * this value to determine how many particles can export data per tick. */
+	UPROPERTY(EditAnywhere, Category = "Export GPU", meta = (EditCondition = "GPUAllocationMode == ENDIExport_GPUAllocationMode::PerParticle", UIMin="0.0", ClampMin="0.0"))
+	float GPUAllocationPerParticleSize = 1.0f;
 
 	//UObject Interface
 	virtual void PostInitProperties() override;
@@ -70,12 +79,14 @@ public:
 	virtual void GetVMExternalFunction(const FVMExternalFunctionBindingInfo& BindingInfo, void* InstanceData, FVMExternalFunction &OutFunc) override;
 	virtual bool InitPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
 	virtual void DestroyPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
-	virtual int32 PerInstanceDataSize() const override { return sizeof(ExportInterface_InstanceData); }
+	virtual int32 PerInstanceDataSize() const override;
 	virtual bool PerInstanceTick(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance, float DeltaSeconds) override;
 	virtual bool PerInstanceTickPostSimulate(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance, float DeltaSeconds) override;
 	virtual bool Equals(const UNiagaraDataInterface* Other) const override;
+	virtual void GetCommonHLSL(FString& OutHLSL) override;
+	virtual void GetParameterDefinitionHLSL(const FNiagaraDataInterfaceGPUParamInfo& ParamInfo, FString& OutHLSL) override;
 	virtual bool GetFunctionHLSL(const FNiagaraDataInterfaceGPUParamInfo& ParamInfo, const FNiagaraDataInterfaceGeneratedFunction& FunctionInfo, int FunctionInstanceIndex, FString& OutHLSL) override;
-	virtual bool CanExecuteOnTarget(ENiagaraSimTarget Target) const override { return Target == ENiagaraSimTarget::CPUSim; }
+	virtual bool CanExecuteOnTarget(ENiagaraSimTarget Target) const override { return true; }
 
 	virtual bool HasPreSimulateTick() const override { return true; }
 	virtual bool HasPostSimulateTick() const override { return true; }
@@ -87,7 +98,4 @@ public:
 
 protected:
 	virtual bool CopyToInternal(UNiagaraDataInterface* Destination) const override;
-	
-private:
-	static const FName StoreDataName;
 };
