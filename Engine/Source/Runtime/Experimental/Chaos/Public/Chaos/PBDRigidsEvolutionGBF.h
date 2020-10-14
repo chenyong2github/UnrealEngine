60e29768ddfc860a195f979a212e9db478729a05
Chris Caulfield <Chris.Caulfield@epicgames.com>
2020-10-14 00:38:57 -0400

    Chaos - fix box margin calculation
    #rb none
    #jira none
    
    [CL 14488950 by Chris Caulfield in 4.26 branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDRigidsEvolutionGBF.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDRigidsEvolutionGBF.h
@@ -95,7 +95,7 @@ namespace Chaos
 		static constexpr int32 DefaultNumCollisionPairIterations = 3;
 		static constexpr int32 DefaultNumPushOutIterations = 2;
 		static constexpr int32 DefaultNumCollisionPushOutPairIterations = 3;
-		static constexpr float DefaultCollisionMarginFraction = 0.02f;
+		static constexpr float DefaultCollisionMarginFraction = 0.01f;
 		static constexpr float DefaultCollisionMarginMax = 2.0f;
 		static constexpr float DefaultCollisionCullDistance = 5.0f;
 
