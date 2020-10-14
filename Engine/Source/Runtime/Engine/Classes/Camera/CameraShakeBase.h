2fa896ec13cb68b7503234a854b27ca18843687c
ludovic chabant <Ludovic.Chabant@epicgames.com>
2020-10-14 18:35:30 -0400

    Cameras: Remove unused field.
    #jira none
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 14487153 via CL 14487154
    #ROBOMERGE-BOT: (v748-14444958)
    
    [CL 14498017 by ludovic chabant in 4.26 branch]

--- Engine/Source/Runtime/Engine/Classes/Camera/CameraShakeBase.h
+++ Engine/Source/Runtime/Engine/Classes/Camera/CameraShakeBase.h
@@ -70,7 +70,6 @@ struct ENGINE_API FCameraShakeUpdateResult
 		, Rotation(FRotator::ZeroRotator)
 		, FOV(0.f)
 		, Flags(ECameraShakeUpdateResultFlags::Default)
-		, bIsFinished(false)
 	{}
 
 	/** Location offset for the view, or new absolute location if ApplyAsAbsolute flag is set */
@@ -82,9 +81,6 @@ struct ENGINE_API FCameraShakeUpdateResult
 
 	/** Flags for how the base class should handle the result */
 	ECameraShakeUpdateResultFlags Flags;
-
-	/** Whether the camera shake is finished, for when duration isn't handled by the base class (see UCameraShakeBase::GetShakeInfo) */
-	bool bIsFinished;
 };
 
 /**
