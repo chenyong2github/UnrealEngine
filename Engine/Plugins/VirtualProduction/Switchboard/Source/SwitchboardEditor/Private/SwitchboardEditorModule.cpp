9c16855d77c0fa397525b100f448b326ca8fd488
thomas kilkenny <thomas.kilkenny@epicgames.com>
2020-10-15 09:50:07 -0400

    - Switchboard BP Listener added - SwitchboardEditorSettings.h moved to Public - GetSwitchboardEditorSettings() added as Static function to USwitchboardEditorSettings for BP use. - SwitchboardEditorSettings moved from "Editor" to "project" and from "DefaultPerProjectUserSettings.ini" to "DefaultEngine.ini" - Switchboard OSC Listener added to USwitchboardEditorSettings. This listener is startted after the engine finishesn initializing
    #Jira UE-100617
    #rb Daniel.Rahier
    [FYI] Jacob.Buck
    
    #ROBOMERGE-SOURCE: CL 14501101 in //UE4/Release-4.26/... via CL 14501116
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v748-14444958)
    
    [CL 14501118 by thomas kilkenny in Main branch]

--- Engine/Plugins/VirtualProduction/Switchboard/Source/SwitchboardEditor/Private/SwitchboardEditorModule.cpp
+++ Engine/Plugins/VirtualProduction/Switchboard/Source/SwitchboardEditor/Private/SwitchboardEditorModule.cpp
@@ -1,16 +1,19 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "CoreMinimal.h"
+#include "Editor/Blutility/Classes/EditorUtilityObject.h"
+#include "EditorUtilitySubsystem.h"
+#include "ISettingsModule.h"
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
-
-#include "ISettingsModule.h"
-#include "SwitchboardMenuEntry.h"
 #include "SwitchboardEditorSettings.h"
+#include "SwitchboardMenuEntry.h"
 
 
 #define LOCTEXT_NAMESPACE "SwitchboardEditorModule"
 
+
+FDelegateHandle DeferredStartDelegateHandle;
 
 class FSwitchboardEditorModule : public IModuleInterface
 {
@@ -21,12 +24,14 @@ public:
 
 		if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
 		{
-			SettingsModule->RegisterSettings("Editor", "Plugins", "Switchboard",
+			SettingsModule->RegisterSettings("Project", "Plugins", "Switchboard",
 				LOCTEXT("SettingsName", "Switchboard"),
 				LOCTEXT("Description", "Configure the Switchboard launcher."),
 				GetMutableDefault<USwitchboardEditorSettings>()
 			);
 		}
+
+		DeferredStartDelegateHandle = FCoreDelegates::OnFEngineLoopInitComplete.AddRaw(this, &FSwitchboardEditorModule::RunDefaultOSCListener);
 	}
 
 	virtual void ShutdownModule() override
@@ -41,6 +46,27 @@ public:
 			FSwitchboardMenuEntry::Unregister();
 		}
 	}
+
+	void RunDefaultOSCListener()
+	{
+		if (DeferredStartDelegateHandle.IsValid())
+		{
+			FCoreDelegates::OnEndFrame.Remove(DeferredStartDelegateHandle);
+			DeferredStartDelegateHandle.Reset();
+		};
+
+		USwitchboardEditorSettings* SwitchboardEditorSettings = USwitchboardEditorSettings::GetSwitchboardEditorSettings();
+		FSoftObjectPath SwitchboardOSCListener = SwitchboardEditorSettings->SwitchboardOSCListener;
+		if (SwitchboardOSCListener.IsValid())
+		{
+			UObject* SwitchboardOSCListenerObject = SwitchboardOSCListener.TryLoad();
+			if (SwitchboardOSCListenerObject && !SwitchboardOSCListenerObject->IsPendingKillOrUnreachable() && GEditor)
+			{
+				GEditor->GetEditorSubsystem<UEditorUtilitySubsystem>()->TryRun(SwitchboardOSCListenerObject);
+			}
+		}
+	}
+
 };
 
 IMPLEMENT_MODULE(FSwitchboardEditorModule, SwitchboardEditor);
