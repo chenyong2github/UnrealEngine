c789d496f503e3c6bce0d965d16e0e346dc4433e
helge mathee <Helge.Mathee@epicgames.com>
2020-10-16 17:12:46 -0400

    Control Rig: Draw bones in Control Rig editor when there's no skeletal mesh
    #rb jack.cai
    #jira UE-100989
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 14489792 in //UE4/Release-4.26/... via CL 14489794 via CL 14489805
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v748-14444958)
    
    [CL 14509591 by helge mathee in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditorEditMode.h
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditorEditMode.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "ControlRigEditMode.h"
 #include "IPersonaEditMode.h"
+#include "Preferences/PersonaOptions.h"
+#include "Persona/Private/AnimationEditorViewportClient.h"
 
 #pragma once
 
@@ -16,8 +18,20 @@ public:
 	/** FControlRigEditMode interface */
 	virtual bool IsInLevelEditor() const { return false; }
 
+	// FEdMode interface
+	virtual void Render(const FSceneView* View, FViewport* Viewport, FPrimitiveDrawInterface* PDI) override;
+
 	/* IPersonaEditMode interface */
 	virtual bool GetCameraTarget(FSphere& OutTarget) const override;
 	virtual class IPersonaPreviewScene& GetAnimPreviewScene() const override;
 	virtual void GetOnScreenDebugInfo(TArray<FText>& OutDebugInfo) const override;
+
+	/** If set to true the edit mode will additionally render all bones */
+	bool bDrawHierarchyBones = false;
+
+	/** Drawing options */
+	UPersonaOptions* ConfigOption = nullptr;
+
+	/** Flag to know if a bone is visible or not */
+	TArray<bool> BoneHasSelectedChild;
 };
