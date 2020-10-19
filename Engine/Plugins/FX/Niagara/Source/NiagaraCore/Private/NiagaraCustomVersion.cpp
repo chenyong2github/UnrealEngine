4e4f109cfd1ab9369fd715958fbd6af9309c764e
stu mckenna <Stu.McKenna@epicgames.com>
2020-10-19 16:12:46 -0400

    - Remove branch on compressed attributes if we do not have attribute compression enabled - Fix reading from the wrong component when using half type
    #rb shaun.kime, jamie.hayes, rob.krajcarski
    #rnx
    
    
    #ROBOMERGE-OWNER: stu.mckenna
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 14497937 via CL 14497951 via CL 14517446
    #ROBOMERGE-BOT: (v748-14444958)
    
    [CL 14517478 by stu mckenna in Release-Engine-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x9BE0E098, 0x35D54CA9, 0xB3704A2E, 0xAD81ACED);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x2D79E696, 0xE0C6514D, 0x9566F2D9, 0x2A8E0A78);
