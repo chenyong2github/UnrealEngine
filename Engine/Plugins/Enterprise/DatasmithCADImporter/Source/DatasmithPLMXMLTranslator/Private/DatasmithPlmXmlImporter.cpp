c0d3b4b884751b1f7cb4d52666b99d9fa8723ff3
kerim borchaev <Kerim.Borchaev@xa.epicgames.com>
2020-10-19 13:55:24 -0400

    PlmXml: replaced errors by warnings in cased when it's actually doesn't prevent from parsing
    #jira UE-91555
    #rb JeanLuc.Corenthin
    
    [CL 14516251 by kerim borchaev in Dev-Tools-Staging branch]

--- Engine/Plugins/Enterprise/DatasmithCADImporter/Source/DatasmithPLMXMLTranslator/Private/DatasmithPlmXmlImporter.cpp
+++ Engine/Plugins/Enterprise/DatasmithCADImporter/Source/DatasmithPLMXMLTranslator/Private/DatasmithPlmXmlImporter.cpp
@@ -1180,7 +1180,7 @@ namespace PlmXml
 		const FXmlNode* ProductDefNode = RootNode->FindChildNode("ProductDef");
 		if (!ProductDefNode)
 		{
-			UE_LOG(LogDatasmithPlmXmlImport, Error, TEXT("PlmXml file has no ProductDef tag"));
+			UE_LOG(LogDatasmithPlmXmlImport, Warning, TEXT("PlmXml file has no ProductDef tag"));
 			return false;
 		}
 
@@ -1188,7 +1188,7 @@ namespace PlmXml
 		const FXmlNode* InstanceGraphNode = ProductDefNode->FindChildNode("InstanceGraph");
 		if (!InstanceGraphNode)
 		{
-			UE_LOG(LogDatasmithPlmXmlImport, Error, TEXT("PlmXml file has no InstanceGraph node"));
+			UE_LOG(LogDatasmithPlmXmlImport, Warning, TEXT("PlmXml file has no InstanceGraph node"));
 			return false;
 		}
 
