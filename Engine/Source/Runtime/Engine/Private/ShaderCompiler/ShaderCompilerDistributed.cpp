c4b7deb5fa599de21600e9bb9f4bfd973f90998c
marc audy <Marc.Audy@epicgames.com>
2020-10-20 14:14:23 -0400

    Add missing copyright #rb #rnx #jira [FYI] Sergio.Gardeazabal
    #ROBOMERGE-SOURCE: CL 14524695 in //UE4/Release-4.26/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26 -> Release-Engine-Staging) (v748-14444958)
    
    [CL 14525727 by marc audy in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompilerDistributed.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompilerDistributed.cpp
@@ -1,3 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "ShaderCompiler.h"
 #include "HAL/FileManager.h"
 #include "DistributedBuildInterface/Public/DistributedBuildControllerInterface.h"
