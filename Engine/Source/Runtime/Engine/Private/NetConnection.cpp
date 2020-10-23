7fcc2b1c8b7a3d2553a2351d2e0217d79ebcd645
brian bekich <Brian.Bekich@epicgames.com>
2020-10-23 12:15:12 -0400

    Adding more information to ensure in SetAllowExistingChannelIndex Handling case where we tried to remap around a broken channel from backwards compatibility in the replay
    #rb ryan.gerleve
    
    #ROBOMERGE-SOURCE: CL 14530041 via CL 14530042 via CL 14530717 via CL 14556807 via CL 14556861
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v750-14555174)
    
    [CL 14557523 by brian bekich in Main branch]

--- Engine/Source/Runtime/Engine/Private/NetConnection.cpp
+++ Engine/Source/Runtime/Engine/Private/NetConnection.cpp
@@ -2905,8 +2905,26 @@ void UNetConnection::SetAllowExistingChannelIndex(bool bAllow)
 
 		for (auto& It : ChannelIndexMap)
 		{
+			// It is possible the index we want to swap back to wasn't cleaned up because the channel was marked broken by backwards compatibility
+			if (Channels[It.Key] && Channels[It.Key]->Broken)
+			{
+				if (UActorChannel* ActorChannel = Cast<UActorChannel>(Channels[It.Key]))
+				{
+					// look for a queued close bunch
+					for (FInBunch* InBunch : ActorChannel->QueuedBunches)
+					{
+						if (InBunch && InBunch->bClose)
+						{
+							UE_LOG(LogNet, Warning, TEXT("SetAllowExistingChannelIndex:  Cleaning up broken channel: %s"), *ActorChannel->Describe());
+							ActorChannel->ConditionalCleanUp(true, InBunch->CloseReason);
+							break;
+						}
+					}
+				}
+			}
+
 			// this channel should still exist, but the location we want to swap it back to should be empty
-			if (ensure(Channels[It.Value] && !Channels[It.Key]))
+			if (ensureMsgf(Channels[It.Value] && !Channels[It.Key], TEXT("Source should exist: [%s] Destination should be null: [%s]"), Channels[It.Value] ? *Channels[It.Value]->Describe() : TEXT("null"), Channels[It.Key] ? *Channels[It.Key]->Describe() : TEXT("null")))
 			{
 				Channels[It.Value]->ChIndex = It.Key;
 
