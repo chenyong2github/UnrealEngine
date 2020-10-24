04331bd42acef5187fcf61be763e53db6a1536fc
windkey <windkey.kao[at]neobards.com>
2020-10-24 17:18:48 -0400

    PR #7417: AddToolbarExtender for StringTableEditor. public HandlePostChange for. (Contributed by windkey)
    #jira UE-101288
    #rb me
    
    #ROBOMERGE-SOURCE: CL 14564140 in //UE4/Release-4.26/... via CL 14564173
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v750-14555174)
    
    [CL 14569009 by jamie dale in Main branch]

--- Engine/Source/Editor/StringTableEditor/Public/IStringTableEditor.h
+++ Engine/Source/Editor/StringTableEditor/Public/IStringTableEditor.h
@@ -8,4 +8,9 @@
 /** String Table Editor public interface */
 class IStringTableEditor : public FAssetEditorToolkit
 {
+public:
+	/**
+	 * Notify the String Table editor that it needs to refresh due to an external String Table change.
+	 */
+	virtual void RefreshStringTableEditor(const FString& NewSelection = FString()) = 0;
 };
