9ed41c0ec971858764de83d222a020100453d061
Sebastien Hillaire <Sebastien.Hillaire@epicgames.com>
2020-10-26 18:15:54 -0400

    Added reflection pass switch node. Useful to tweak visual for less volumetric clouds details according to sample count in reflection capture for instance for the sake of performance.
    #rb none
    #jira none
    
    #ushell-cherrypick of 14335966 by Sebastien.Hillaire
    
    [CL 14582519 by Sebastien Hillaire in 4.26 branch]

---
+++ Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpressionReflectionCapturePassSwitch.h
@@ -0,0 +1,32 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "MaterialExpressionIO.h"
+#include "Materials/MaterialExpression.h"
+#include "MaterialExpressionReflectionCapturePassSwitch.generated.h"
+
+UCLASS(meta = (DisplayName = "ReflectionCapturePassSwitch"))
+class UMaterialExpressionReflectionCapturePassSwitch : public UMaterialExpression
+{
+	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(meta = (RequiredInput = "true", ToolTip = "Input will be used when rendering into non-reflection passes."))
+	FExpressionInput Default;
+
+	UPROPERTY(meta = (RequiredInput = "true", ToolTip = "Input will be used when rendering into reflection passes."))
+	FExpressionInput Reflection;
+
+	//~ Begin UMaterialExpression Interface
+#if WITH_EDITOR
+	virtual int32 Compile(class FMaterialCompiler* Compiler, int32 OutputIndex) override;
+	virtual void GetCaption(TArray<FString>& OutCaptions) const override;
+	virtual void GetExpressionToolTip(TArray<FString>& OutToolTip) override;
+#endif
+	//~ End UMaterialExpression Interface
+};
+
+
