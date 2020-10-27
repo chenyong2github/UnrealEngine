56e75d48c821ccf690616c0fee25e3d817b5dc46
flo schar <flo@geodesic.games>
2020-10-27 19:23:14 -0400

    DMX - Fix various issues when enabling or disabling send and receive DMX
    #jira UE-102049
    #rb Dominik.Peacock
    
    #ROBOMERGE-SOURCE: CL 14594833 in //UE4/Release-4.26/... via CL 14594847
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v751-14579041)
    
    [CL 14594873 by flo schar in Main branch]

--- Engine/Plugins/VirtualProduction/DMX/DMXProtocol/Source/DMXProtocolSACN/Private/DMXProtocolSACNReceivingRunnable.cpp
+++ Engine/Plugins/VirtualProduction/DMX/DMXProtocol/Source/DMXProtocolSACN/Private/DMXProtocolSACNReceivingRunnable.cpp
@@ -69,11 +69,22 @@ void FDMXProtocolSACNReceivingRunnable::GameThread_InputDMXFragment(uint16 Unive
 
 	for (const TPair<uint32, uint8>& ChannelValue : DMXFragment)
 	{
-		Channels[ChannelValue.Key] = ChannelValue.Value;
+		Channels[ChannelValue.Key - 1] = ChannelValue.Value;
 	}
 
-	double TimeStamp = FPlatformTime::Seconds();
-	GameThreadOnlyBuffer.FindOrAdd(UniverseID) = MakeShared<FDMXSignal>(FApp::GetTimecode(), UniverseID, Channels);
+	TSharedPtr<FDMXSignal>* ExistingSignalPtr = GameThreadOnlyBuffer.Find(UniverseID);
+	if (ExistingSignalPtr)
+	{
+		// Copy fragments into existing
+		for (const TPair<uint32, uint8>& ChannelValue : DMXFragment)
+		{
+			(*ExistingSignalPtr)->ChannelData[ChannelValue.Key - 1] = ChannelValue.Value;
+		}
+	}
+	else
+	{
+		GameThreadOnlyBuffer.Add(UniverseID, MakeShared<FDMXSignal>(FApp::GetTimecode(), UniverseID, Channels));
+	}
 }
 
 void FDMXProtocolSACNReceivingRunnable::SetRefreshRate(uint32 NewReceivingRefreshRate)
