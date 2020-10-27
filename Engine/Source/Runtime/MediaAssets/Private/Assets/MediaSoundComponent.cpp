0910228ce4ea6e732127e6bd758e42605aa6eda0
phil popp <Phil.Popp@epicgames.com>
2020-10-27 12:56:50 -0400

    Remove ensure complete from spectrum analyzer #rb Jimmy.Smith
    #ROBOMERGE-SOURCE: CL 14589170 via CL 14589178 via CL 14589180 via CL 14589192
    #ROBOMERGE-BOT: (v751-14579041)
    
    [CL 14589203 by phil popp in Release-Engine-Staging branch]

--- Engine/Source/Runtime/MediaAssets/Private/Assets/MediaSoundComponent.cpp
+++ Engine/Source/Runtime/MediaAssets/Private/Assets/MediaSoundComponent.cpp
@@ -46,6 +46,7 @@ UMediaSoundComponent::UMediaSoundComponent(const FObjectInitializer& ObjectIniti
 	, RateAdjustment(1.0f)
 	, Resampler(new FMediaAudioResampler)
 	, LastPlaySampleTime(FTimespan::MinValue())
+	, SpectrumAnalyzer(Audio::FAsyncSpectrumAnalyzer::CreateAsyncSpectrumAnalyzer())
 	, EnvelopeFollowerAttackTime(10)
 	, EnvelopeFollowerReleaseTime(100)
 	, CurrentEnvelopeValue(0.0f)
@@ -319,7 +320,7 @@ bool UMediaSoundComponent::Init(int32& SampleRate)
 	Super::Init(SampleRate);
 
 	// Initialize the settings for the spectrum analyzer
-	SpectrumAnalyzer.Init(SampleRate);
+	SpectrumAnalyzer->Init(SampleRate);
 
 	if (Channels == EMediaSoundChannels::Mono)
 	{
@@ -412,8 +413,8 @@ int32 UMediaSoundComponent::OnGenerateAudio(float* OutAudio, int32 NumSamples)
 
 			if (bSpectralAnalysisEnabled)
 			{
-				SpectrumAnalyzer.PushAudio(BufferToUseForAnalysis, NumFrames);
-				SpectrumAnalyzer.PerformAnalysisIfPossible(true, true);
+				SpectrumAnalyzer->PushAudio(BufferToUseForAnalysis, NumFrames);
+				SpectrumAnalyzer->PerformAsyncAnalysisIfPossible(true);
 			}
 
 			{
@@ -477,7 +478,7 @@ void UMediaSoundComponent::SetSpectralAnalysisSettings(TArray<float> InFrequenci
 	}
 
 	SpectrumAnalyzerSettings.FFTSize = SpectrumAnalyzerSize;
-	SpectrumAnalyzer.SetSettings(SpectrumAnalyzerSettings);
+	SpectrumAnalyzer->SetSettings(SpectrumAnalyzerSettings);
 
 	FrequenciesToAnalyze = InFrequenciesToAnalyze;
 }
@@ -486,17 +487,18 @@ TArray<FMediaSoundComponentSpectralData> UMediaSoundComponent::GetSpectralData()
 {
 	if (bSpectralAnalysisEnabled)
 	{
+		// Locks analyzer output buffer during access to frequency data.
+		Audio::FSpectrumAnalyzerScopeLock AnalyzerBufferLock(&SpectrumAnalyzer.Get());
+
 		TArray<FMediaSoundComponentSpectralData> SpectralData;
-		SpectrumAnalyzer.LockOutputBuffer();
 
 		for (float Frequency : FrequenciesToAnalyze)
 		{
 			FMediaSoundComponentSpectralData Data;
 			Data.FrequencyHz = Frequency;
-			Data.Magnitude = SpectrumAnalyzer.GetMagnitudeForFrequency(Frequency);
+			Data.Magnitude = SpectrumAnalyzer->GetMagnitudeForFrequency(Frequency);
 			SpectralData.Add(Data);
 		}
-		SpectrumAnalyzer.UnlockOutputBuffer();
 
 		return SpectralData;
 	}
