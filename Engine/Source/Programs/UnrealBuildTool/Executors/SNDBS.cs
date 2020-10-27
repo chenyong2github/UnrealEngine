dda88bd3e7cae0fbdd0bf6732226c7e9883c35c5
luke thatcher <Luke.Thatcher@epicgames.com>
2020-10-27 08:39:13 -0400

    Improvements to SN-DBS  - Removed the loose template ini files checked in, replacing them with string literals in code which are dumped to the build directory when a build is started. This allows other users of SN-DBS to add custom tool templates as required.  - Added support for the "explicit_input_files" node in the build script json.  - General refactor of SNDBS.cs, including simplifying the include rewrite rules to make it somewhat easier to read with regex syntax highlighting.
    #rb Jeff.Newquist
    
    
    #ROBOMERGE-SOURCE: CL 14586107 via CL 14586109 via CL 14586111 via CL 14586113
    #ROBOMERGE-BOT: (v751-14579041)
    #ROBOMERGE[RELEASE]: 4.26
    
    [CL 14586118 by luke thatcher in Release-Engine-Staging branch]

--- Engine/Source/Programs/UnrealBuildTool/Executors/SNDBS.cs
+++ Engine/Source/Programs/UnrealBuildTool/Executors/SNDBS.cs
@@ -11,52 +11,37 @@ using Tools.DotNETCommon;
 
 namespace UnrealBuildTool
 {
-	class SNDBS : ActionExecutor
+	sealed class SNDBS : ActionExecutor
 	{
+		public override string Name => "SNDBS";
+
 		private static readonly string SCERoot = Environment.GetEnvironmentVariable("SCE_ROOT_DIR");
 		private static readonly string SNDBSExecutable = Path.Combine(SCERoot ?? string.Empty, "Common", "SN-DBS", "bin", "dbsbuild.exe");
 
-		private static readonly string IncludeRewriteRulesDirectory = Path.Combine(UnrealBuildTool.EngineDirectory.FullName, "Build", "SNDBS");
-		private static readonly string IncludeRewriteRulesFilePath = Path.Combine(IncludeRewriteRulesDirectory, "include-rewrite-rules.ini");
+		private static readonly DirectoryReference IntermediateDir = DirectoryReference.Combine(UnrealBuildTool.EngineDirectory, "Intermediate", "Build", "SNDBS");
+		private static readonly FileReference IncludeRewriteRulesFile = FileReference.Combine(IntermediateDir, "include-rewrite-rules.ini");
+		private static readonly FileReference ScriptFile = FileReference.Combine(IntermediateDir, "sndbs.json");
 
-		private static readonly string ScriptFilePath = Path.Combine(UnrealBuildTool.EngineDirectory.FullName, "Intermediate", "Build", "sndbs.json");
+		private Dictionary<string, string> ActiveTemplates = BuiltInTemplates.ToDictionary(p => p.Key, p => p.Value);
 
-		private static readonly string TemplatesInputDir = Path.Combine(UnrealBuildTool.EngineDirectory.FullName, "Build", "SNDBSTemplates");
-		private static readonly string TemplatesOutputDir = Path.Combine(UnrealBuildTool.EngineDirectory.FullName, "Programs", "UnrealBuildTool", "SNDBSTemplates");
+		public bool EnableEcho { get; set; } = false;
 
-		public override string Name => "SNDBS";
+		public SNDBS AddTemplate(string ExeName, string TemplateContents)
+		{
+			ActiveTemplates.Add(ExeName, TemplateContents);
+			return this;
+		}
 
 		public static bool IsAvailable()
 		{
-			if(SCERoot == null || !File.Exists(SNDBSExecutable))
+			// Check the executable exists on disk
+			if (SCERoot == null || !File.Exists(SNDBSExecutable))
 			{
 				return false;
 			}
 
-			ServiceController[] services = ServiceController.GetServices();
-			foreach (ServiceController service in services)
-			{
-				if (service.ServiceName.StartsWith("SNDBS") && service.Status == ServiceControllerStatus.Running)
-				{
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		private sealed class SNDBSJob
-		{
-			public string title { get; set; }
-			public string command { get; set; }
-			public string working_directory { get; set; }
-			public string[] dependencies { get; set; }
-			public bool run_locally { get; set; }
-		}
-
-		private sealed class SNDBSScript
-		{
-			public IDictionary<string, SNDBSJob> jobs { get; set; }
+			// Check the service is running
+			return ServiceController.GetServices().Any(s => s.ServiceName.StartsWith("SNDBS") && s.Status == ServiceControllerStatus.Running);
 		}
 
 		public override bool ExecuteActions(List<Action> Actions, bool bLogDetailedActionStats)
@@ -64,111 +49,201 @@ namespace UnrealBuildTool
 			if (Actions.Count == 0)
 				return true;
 
+			// Clean the intermediate directory in case there are any leftovers from previous builds
+			if (DirectoryReference.Exists(IntermediateDir))
+			{
+				DirectoryReference.Delete(IntermediateDir, true);
+			}
+
+			DirectoryReference.CreateDirectory(IntermediateDir);
+			if (!DirectoryReference.Exists(IntermediateDir))
+			{
+				throw new BuildException($"Failed to create directory \"{IntermediateDir}\".");
+			}
+
 			// Build the json script file to describe all the actions and their dependencies
 			var ActionIds = Actions.ToDictionary(a => a, a => Guid.NewGuid().ToString());
-			var Script = new SNDBSScript()
+			File.WriteAllText(ScriptFile.FullName, Json.Serialize(new Dictionary<string, object>()
 			{
-				jobs = Actions.ToDictionary(a => ActionIds[a], a => new SNDBSJob
+				["jobs"] = Actions.ToDictionary(a => ActionIds[a], a =>
 				{
-					title = a.StatusDescription,
-					command = $"\"{a.CommandPath}\" {a.CommandArguments}",
-					working_directory = a.WorkingDirectory.FullName,
-					dependencies = a.PrerequisiteActions.Select(p => ActionIds[p]).ToArray(),
-					run_locally = !(a.bCanExecuteRemotely && a.bCanExecuteRemotelyWithSNDBS)
+					var Job = new Dictionary<string, object>()
+					{
+						["title"] = a.StatusDescription,
+						["command"] = $"\"{a.CommandPath}\" {a.CommandArguments}",
+						["working_directory"] = a.WorkingDirectory.FullName,
+						["dependencies"] = a.PrerequisiteActions.Select(p => ActionIds[p]).ToArray(),
+						["run_locally"] = !(a.bCanExecuteRemotely && a.bCanExecuteRemotelyWithSNDBS)
+					};
+
+					if (a.PrerequisiteItems.Count > 0)
+					{
+						Job["explicit_input_files"] = a.PrerequisiteItems.Select(i => new Dictionary<string, object>()
+						{
+							["filename"] = i.AbsolutePath
+						}).ToList();
+					}
+
+					if (EnableEcho)
+					{
+						var EchoString = string.Join(" ", string.IsNullOrWhiteSpace(a.CommandDescription) ? string.Empty : $"[{a.CommandDescription}]", a.StatusDescription);
+						if (!string.IsNullOrWhiteSpace(EchoString))
+						{
+							Job["echo"] = EchoString;
+						}
+					}
+
+					return Job;
 				})
-			};
+			}));
 
-			var ScriptJson = fastJSON.JSON.Instance.ToJSON(Script, new fastJSON.JSONParameters() { UseExtensions = false });
-			File.WriteAllText(ScriptFilePath, ScriptJson);
-
-			GenerateSNDBSIncludeRewriteRules();
-			PrepareToolTemplates(Actions);
+			PrepareToolTemplates();
+			bool bHasRewrites = GenerateSNDBSIncludeRewriteRules();
 
 			var LocalProcess = new Process();
-			LocalProcess.StartInfo = new ProcessStartInfo(SNDBSExecutable, $"-q -p UE4Code -s \"{ScriptFilePath}\" -templates \"{TemplatesOutputDir}\" --include-rewrite-rules \"{IncludeRewriteRulesFilePath}\"");
+			LocalProcess.StartInfo = new ProcessStartInfo(SNDBSExecutable, $"-q -p \"Unreal Engine Tasks\" -s \"{ScriptFile}\" -templates \"{IntermediateDir}\"{(bHasRewrites ? $" --include-rewrite-rules \"{IncludeRewriteRulesFile}\"" : "")}");
 			LocalProcess.OutputDataReceived += (Sender, Args) => Log.TraceInformation("{0}", Args.Data);
 			LocalProcess.ErrorDataReceived += (Sender, Args) => Log.TraceInformation("{0}", Args.Data);
 			return Utils.RunLocalProcess(LocalProcess) == 0;
 		}
 
-		private void PrepareToolTemplates(IEnumerable<Action> Actions)
+		private void PrepareToolTemplates()
 		{
-			if (!Directory.Exists(TemplatesOutputDir))
+			foreach (var Template in ActiveTemplates)
 			{
-				Directory.CreateDirectory(TemplatesOutputDir);
-			}
+				var TemplateFile = FileReference.Combine(IntermediateDir, $"{Template.Key}.sn-dbs-tool.ini");
+				var TemplateText = Template.Value;
 
-			var UniqueTools = new HashSet<string>(Actions.Select(a => a.CommandPath.FullName));
-			foreach (var Tool in UniqueTools)
-			{
-				var TemplateFileName = $"{Path.GetFileName(Tool)}.sn-dbs-tool.ini";
-
-				var TemplateInputFilePath = Path.Combine(TemplatesInputDir, TemplateFileName);
-				var TemplateOutputFilePath = Path.Combine(TemplatesOutputDir, TemplateFileName);
-
-				// If no base template exists, don't try to generate one.
-				if (!File.Exists(TemplateInputFilePath))
-				{
-					continue;
-				}
-
-				string TemplateText = File.ReadAllText(TemplateInputFilePath);
-
-				TemplateText = TemplateText.Replace("{COMMAND_PATH}", Path.GetDirectoryName(Tool));
 				foreach (DictionaryEntry Variable in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process))
 				{
-					string VariableName = string.Format("{{{0}}}", Variable.Key);
-					TemplateText = TemplateText.Replace(VariableName, Variable.Value.ToString());
+					TemplateText = TemplateText.Replace($"{{{Variable.Key}}}", Variable.Value.ToString());
 				}
 
-				File.WriteAllText(TemplateOutputFilePath, TemplateText);
+				File.WriteAllText(TemplateFile.FullName, TemplateText);
 			}
 		}
 
-		private void GenerateSNDBSIncludeRewriteRules()
+		private bool GenerateSNDBSIncludeRewriteRules()
 		{
-			// Get all registered platforms. Most will just use the name as is, but some may have an override so
-			// add all distinct entries to the list.
-			IEnumerable<UnrealTargetPlatform> Platforms = UEBuildPlatform.GetRegisteredPlatforms();
-			List<string> PlatformNames = new List<string>();
-			foreach (UnrealTargetPlatform Platform in Platforms)
+			// Get all registered, distinct platform names.
+			var Platforms = UEBuildPlatform.GetRegisteredPlatforms()
+				.Select(Platform => UEBuildPlatform.GetBuildPlatform(Platform).GetPlatformName())
+				.Distinct()
+				.ToList();
+
+			if (Platforms.Count > 0)
 			{
-				UEBuildPlatform PlatformData = UEBuildPlatform.GetBuildPlatform(Platform);
-				if (!PlatformNames.Contains(PlatformData.GetPlatformName()))
+				// language=regex
+				var Lines = new[]
 				{
-					PlatformNames.Add(PlatformData.GetPlatformName());
-				}
-			}
+					@"pattern1=^COMPILED_PLATFORM_HEADER\(\s*([^ ,]+)\s*\)",
+					$"expansions1={string.Join("|", Platforms.Select(Name => $"{Name}/{Name}$1|{Name}$1"))}",
 
-			if (!Directory.Exists(IncludeRewriteRulesDirectory))
-			{
-				Directory.CreateDirectory(IncludeRewriteRulesDirectory);
-			}
+					@"pattern2=^COMPILED_PLATFORM_HEADER_WITH_PREFIX\(\s*([^ ,]+)\s*,\s*([^ ,]+)\s*\)",
+					$"expansions2={string.Join("|", Platforms.Select(Name => $"$1/{Name}/{Name}$2|$1/{Name}$2"))}",
 
-			List<string> IncludeRewriteRulesText = new List<string>();
-			IncludeRewriteRulesText.Add("[computed-include-rules]");
-			{
-				IncludeRewriteRulesText.Add(@"pattern1=^COMPILED_PLATFORM_HEADER\(\s*([^ ,]+)\)");
-				IEnumerable<string> PlatformExpansions = PlatformNames.Select(p => string.Format("{0}/{0}$1|{0}$1", p));
-				IncludeRewriteRulesText.Add(string.Format("expansions1={0}", string.Join("|", PlatformExpansions)));
-			}
-			{
-				IncludeRewriteRulesText.Add(@"pattern2=^COMPILED_PLATFORM_HEADER_WITH_PREFIX\(\s*([^ ,]+)\s*,\s*([^ ,]+)\)");
-				IEnumerable<string> PlatformExpansions = PlatformNames.Select(p => string.Format("$1/{0}/{0}$2|$1/{0}$2", p));
-				IncludeRewriteRulesText.Add(string.Format("expansions2={0}", string.Join("|", PlatformExpansions)));
-			}
-			{
-				IncludeRewriteRulesText.Add(@"pattern3=^[A-Z]{5}_PLATFORM_HEADER_NAME\(\s*([^ ,]+)\)");
-				IEnumerable<string> PlatformExpansions = PlatformNames.Select(p => string.Format("{0}/{0}$1|{0}$1", p));
-				IncludeRewriteRulesText.Add(string.Format("expansions3={0}", string.Join("|", PlatformExpansions)));
-			}
-			{
-				IncludeRewriteRulesText.Add(@"pattern4=^[A-Z]{5}_PLATFORM_HEADER_NAME_WITH_PREFIX\(\s*([^ ,]+)\s*,\s*([^ ,]+)\)");
-				IEnumerable<string> PlatformExpansions = PlatformNames.Select(p => string.Format("$1/{0}/{0}$2|$1/{0}$2", p));
-				IncludeRewriteRulesText.Add(string.Format("expansions4={0}", string.Join("|", PlatformExpansions)));
-			}
+					@"pattern3=^[A-Z]{5}_PLATFORM_HEADER_NAME\(\s*([^ ,]+)\s*\)",
+					$"expansions3={string.Join("|", Platforms.Select(Name => $"{Name}/{Name}$1|{Name}$1"))}",
 
-			File.WriteAllText(IncludeRewriteRulesFilePath, string.Join(Environment.NewLine, IncludeRewriteRulesText));
+					@"pattern4=^[A-Z]{5}_PLATFORM_HEADER_NAME_WITH_PREFIX\(\s*([^ ,]+)\s*,\s*([^ ,]+)\s*\)",
+					$"expansions4={string.Join("|", Platforms.Select(Name => $"$1/{Name}/{Name}$2|$1/{Name}$2"))}"
+				};
+
+				File.WriteAllText(IncludeRewriteRulesFile.FullName, string.Join(Environment.NewLine, new[] { "[computed-include-rules]" }.Concat(Lines)));
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
+
+		/// <summary>
+		/// SN-DBS templates that are automatically included in the build.
+		/// </summary>
+		private static readonly IReadOnlyDictionary<string, string> BuiltInTemplates = new Dictionary<string, string>()
+		{
+			["cl-filter.exe"] = @"
+[tool]
+family=msvc
+vc_major_version=14
+use_surrogate=true
+force_synchronous_pdb_writes=true
+error_report_mode=prompt
+
+[group]
+server={VC_COMPILER_DIR}\mspdbsrv.exe
+
+[files]
+main=cl-filter.exe
+file01={VC_COMPILER_DIR}\c1.dll
+file01={VC_COMPILER_DIR}\c1ui.dll
+file02={VC_COMPILER_DIR}\c1xx.dll
+file03={VC_COMPILER_DIR}\c2.dll
+file04={VC_COMPILER_DIR}\mspdb140.dll
+file05={VC_COMPILER_DIR}\mspdbcore.dll
+file06={VC_COMPILER_DIR}\mspdbsrv.exe
+file07={VC_COMPILER_DIR}\mspft140.dll
+file08={VC_COMPILER_DIR}\vcmeta.dll
+file09={VC_COMPILER_DIR}\*\clui.dll
+file10={VC_COMPILER_DIR}\*\mspft140ui.dll
+file11={VC_COMPILER_DIR}\localespc.dll
+file12={VC_COMPILER_DIR}\cppcorecheck.dll
+file13={VC_COMPILER_DIR}\experimentalcppcorecheck.dll
+file14={VC_COMPILER_DIR}\espxengine.dll
+file15={VC_COMPILER_DIR}\c1.exe
+
+[output-file-patterns]
+outputfile01=\s*""([^ "",]+\.cpp\.txt)\""
+
+[output-file-rules]
+rule01=*\sqmcpp*.log|discard=true
+rule02=*\vctoolstelemetry*.dat|discard=true
+rule03=*\Microsoft\Windows\Temporary Internet Files\*|discard=true
+rule04=*\Microsoft\Windows\INetCache\*|discard=true
+
+[input-file-rules]
+rule01=*\sqmcpp*.log|ignore_transient_errors=true;ignore_unexpected_input=true
+rule02=*\vctoolstelemetry*.dat|ignore_transient_errors=true;ignore_unexpected_input=true
+rule03=*\Microsoft\Windows\Temporary Internet Files\*|ignore_transient_errors=true;ignore_unexpected_input=true
+rule04=*\Microsoft\Windows\INetCache\*|ignore_transient_errors=true;ignore_unexpected_input=true
+
+[system-file-filters]
+filter01=msvcr*.dll
+filter02=msvcp*.dll
+filter03=vcruntime140*.dll
+filter04=appcrt140*.dll
+filter05=desktopcrt140*.dll
+filter06=concrt140*.dll",
+			["clang++.exe"] = @"
+[tool]
+family=clang-cl
+include_path01=..\include
+include_path02=..\include\c++\v1
+include_path03=..\lib\clang\*\include
+
+[files]
+main=clang++.exe
+file01=clang-shared.dll
+file02=libclang.dll
+file03=NXMangledNamePrinter.dll
+file04=..\lib\*
+
+[output-file-patterns]
+outputfile01=\s*""([^ "",]+\.cpp\.txt)\""
+
+[output-file-rules]
+rule01=*.log|discard=true
+rule02=*.dat|discard=true
+rule03=*.tmp|discard=true
+
+[system-file-filters]
+filter01=msvcr*.dll
+filter02=msvcp*.dll
+filter03=vcruntime140*.dll
+filter04=appcrt140*.dll
+filter05=desktopcrt140*.dll
+filter06=concrt140*.dll",
+		};
 	}
 }
