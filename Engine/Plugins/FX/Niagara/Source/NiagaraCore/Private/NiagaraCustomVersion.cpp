310de64afc1584c7743f8709fe16f6624ccd203f
will damon <Will.Damon@epicgames.com>
2020-10-29 16:03:34 -0400

    Create conceptual model of FNDIExportContext to work around SPIRV-Cross.
    SPIRV-Cross does not understand that the field of a struct may require atomic texture2d emulation. To work around this, pass the fields directly as parameters instead of packing the fields into a struct and passing that to the NDIExport_StoreData function.
    
    #rb lukas.hermanns stu.mckenna
    #jira UE-101882
    #rnx
    
    #ROBOMERGE-OWNER: will.damon
    #ROBOMERGE-AUTHOR: will.damon
    #ROBOMERGE-SOURCE: CL 14614191 in //UE4/Release-4.26/... via CL 14614824
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v751-14579041)
    
    [CL 14614829 by will damon in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xB4254D1C, 0xE8A44F24, 0x8484CC38, 0xBA9EB149);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xC4B5E2AE, 0x2301494D, 0x9F4D0064, 0xA353D088);
