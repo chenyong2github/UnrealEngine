52751e46fe01d53f9bfae7aa94bbe12c7aa7c347
zach bethel <zach.bethel@epicgames.com>
2020-10-29 17:46:13 -0400

    CIS fix.
    #rb none
    
    [CL 14616101 by zach bethel in ue5-main branch]

--- Engine/Plugins/Experimental/GPULightmass/Source/GPULightmass/Private/LightmapRenderer.cpp
+++ Engine/Plugins/Experimental/GPULightmass/Source/GPULightmass/Private/LightmapRenderer.cpp
@@ -680,7 +680,7 @@ void FSceneRenderState::SetupRayTracingScene()
 			// Expanded version of View.InitRHIResources() - need to do SetupSkyIrradianceEnvironmentMapConstants manually because the estimation of skylight is dependent on GetSkySHDiffuse
 			View.CachedViewUniformShaderParameters = MakeUnique<FViewUniformShaderParameters>();
 
-			FSceneRenderTargets& SceneContext = FSceneRenderTargets::Get(FRHICommandListExecutor::GetImmediateCommandList());
+			FSceneRenderTargets& SceneContext = FSceneRenderTargets::Get();
 
 			FBox UnusedVolumeBounds[TVC_MAX];
 			View.SetupUniformBufferParameters(
