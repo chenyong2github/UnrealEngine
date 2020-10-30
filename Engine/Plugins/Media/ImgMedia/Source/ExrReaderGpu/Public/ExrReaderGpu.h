c9fe10a132465b22fb2919da94375c7b4f807b9c
kevin ortegren <Kevin.Ortegren@epicgames.com>
2020-10-30 08:47:47 -0400

    Moved implementation to .cpp and removed DECLARE_LOG_CATEGORY_EXTERN from the header that was causing redefinition.
    #rb none
    [FYI] alejandro.arango, ruslan.idrisov
    #rnx
    #jira none
    
    #ROBOMERGE-SOURCE: CL 14619525 in //UE4/Release-4.26/... via CL 14619538
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v753-14616146)
    
    [CL 14619548 by kevin ortegren in Main branch]

--- Engine/Plugins/Media/ImgMedia/Source/ExrReaderGpu/Public/ExrReaderGpu.h
+++ Engine/Plugins/Media/ImgMedia/Source/ExrReaderGpu/Public/ExrReaderGpu.h
@@ -5,8 +5,6 @@
 #if defined(PLATFORM_WINDOWS) && PLATFORM_WINDOWS
 #include "CoreTypes.h"
 
-/** Log category. */
-DECLARE_LOG_CATEGORY_EXTERN(LogExrReaderGpu, Log, All);
 
 /**
 * This class is a collection of distant and compressed takes on 
@@ -50,120 +48,27 @@ private:
 public:
 
 	/** Discards the header and fills the buffer with plain pixel data. */
-	static bool GenerateTextureData(uint16* Buffer, FString FilePath, int32 TextureWidth, int32 TextureHeight, int32& PixelSize, int32 NumChannels)
-	{
-		check(Buffer != nullptr);
-
-		FILE* FileHandle;
-		errno_t Error = fopen_s(&FileHandle, TCHAR_TO_ANSI(*FilePath), "rb");
-
-		if (FileHandle == NULL || Error != 0)
-		{
-			return false;
-		}
-
-		fseek(FileHandle, 0, SEEK_END);
-		int64 FileLength = ftell(FileHandle);
-		rewind(FileHandle);
-
-		// Reading header (throwaway) and line offset data. We just need line offset data.
-		TArray<int64> LineOffsets;
-		{
-			ReadMagicNumberAndVersionField(FileHandle);
-			ReadHeaderData(FileHandle);
-			LineOffsets.SetNum(TextureHeight);
-
-			// At the moment we support only increasing Y.
-			ELineOrder LineOrder = INCREASING_Y;
-			ReadLineOffsets(FileHandle, LineOrder, LineOffsets);
-		}
-
-		fseek(FileHandle, LineOffsets[0], SEEK_SET);
-		fread(Buffer, TextureWidth * PixelSize + PLANAR_RGB_SCANLINE_PADDING, TextureHeight, FileHandle);
-
-		fclose(FileHandle);
-		return true;
-	}
+	static bool GenerateTextureData(uint16* Buffer, FString FilePath, int32 TextureWidth, int32 TextureHeight, int32& PixelSize, int32 NumChannels);
 
 	/** 
 	* This function is used to open file and keep a handle to it so the file can be read in chunks.
 	* Reading in chunks allows the process of reading to be canceled midway through reading. 
 	* This function reads and discards the header and keeps the pointers to the scanlines. 
 	*/
-	bool OpenExrAndPrepareForPixelReading(FString FilePath, int32 TextureWidth, int32 TextureHeight, int32& PixelSize, int32 NumChannels)
-	{
-		if (FileHandle != nullptr)
-		{
-			UE_LOG(LogExrReaderGpu, Error, TEXT("The file has already been open for reading but never closed."));
-			return false;
-		}
-
-		errno_t Error = fopen_s(&FileHandle, TCHAR_TO_ANSI(*FilePath), "rb");
-
-		if (FileHandle == NULL || Error != 0)
-		{
-			return false;
-		}
-
-		// Reading header (throwaway) and line offset data. We just need line offset data.
-		{
-			ReadMagicNumberAndVersionField(FileHandle);
-			if (!ReadHeaderData(FileHandle))
-			{
-				return false;
-			}
-			LineOffsets.SetNum(TextureHeight);
-
-			// At the moment we support only increasing Y.
-			ELineOrder LineOrder = INCREASING_Y;
-			ReadLineOffsets(FileHandle, LineOrder, LineOffsets);
-		}
-
-		fseek(FileHandle, LineOffsets[0], SEEK_SET);
-		
-		return true;
-	}
+	bool OpenExrAndPrepareForPixelReading(FString FilePath, int32 TextureWidth, int32 TextureHeight, int32& PixelSize, int32 NumChannels);
 
 	/** 
 	* Read a chunk of an Exr file previously open via OpenExrAndPrepareForPixelReading.
 	* The read starts from the last point of file reading.
 	*/
-	bool ReadExrImageChunk(void* Buffer, int32 ChunkSize)
-	{
-		if (FileHandle == nullptr)
-		{
-			UE_LOG(LogExrReaderGpu, Error, TEXT("File is not open for reading. Please use OpenExrAndPrepareForPixelReading"));
-			return false;
-		}
-		if (Buffer == nullptr)
-		{
-			UE_LOG(LogExrReaderGpu, Error, TEXT("Buffer provided is invalid. Please provide a valid buffer. "));
-			return false;
-		}
-
-		size_t Result = fread(Buffer, ChunkSize, 1, FileHandle);
-		if (Result != 1)
-		{
-			UE_LOG(LogExrReaderGpu, Error, TEXT("Issue reading EXR chunk. "));
-		}
-		return Result == 1;
-	}
+	bool ReadExrImageChunk(void* Buffer, int32 ChunkSize);
 
 	/**
 	* After we have finished reading the file that was open via OpenExrAndPrepareForPixelReading
 	* We need to close it to avoid memory leaks
 	*/
-	bool CloseExrFile()
-	{
-		if (FileHandle == nullptr)
-		{
-			UE_LOG(LogExrReaderGpu, Error, TEXT("File is not open for reading. Please use OpenExrAndPrepareForPixelReading"));
-			return false;
-		}
-		fclose(FileHandle);
-		LineOffsets.Empty();
-		return true;
-	}
+	bool CloseExrFile();
+
 private:
 	/**
 	* One approach to reading is to first open a file, read in chunks and then close the file.
