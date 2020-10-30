1f269fc655c9da001aae046dd3640fc71a5524b9
Aaron McLeran <Aaron.McLeran@epicgames.com>
2020-10-30 15:49:43 -0400

    Extracting motosynth to an experimental plugin
    #rb Dan.Reynolds
    #jira UE-102298
    
    [CL 14621010 by Aaron McLeran in 4.26 branch]

---
+++ Engine/Plugins/Experimental/MotoSynth/Source/MotoSynthEditor/MotoSynthEditor.Build.cs
@@ -0,0 +1,45 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+namespace UnrealBuildTool.Rules
+{
+	public class MotoSynthEditor : ModuleRules
+	{
+		public MotoSynthEditor(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PublicDependencyModuleNames.AddRange
+			(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"UnrealEd",
+					"AudioEditor",
+					"MotoSynth",
+					"AudioMixer",
+					"ToolMenus",
+					"EditorStyle",
+					"Slate",
+					"SlateCore",
+					"ContentBrowser",
+				}
+			);
+
+			PrivateDependencyModuleNames.AddRange
+			(
+				new string[]
+				{
+					"DeveloperSettings",
+				}
+			);
+
+			PrivateIncludePathModuleNames.AddRange
+			(
+				new string[]
+				{
+					"AssetTools"
+				}
+			);
+		}
+	}
+}
