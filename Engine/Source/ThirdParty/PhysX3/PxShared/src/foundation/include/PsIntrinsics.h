c0b6ab150ad2e41363988f3d2ffe13c79199c1b7
david harvey <David.Harvey@epicgames.com>
2020-10-30 09:57:01 -0400

    PxShared platform-specific intrinsic headers copied to common location.
    (not simply moving them at the moment because it will break BuildPhysX for all these platforms & fixing that up would be a much more involved refactor)
    
    #jira UE-102168
    #rb benn.gallagher, brian.white
    
    #ROBOMERGE-SOURCE: CL 14619725 in //UE4/Release-4.26/... via CL 14619733
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v753-14616146)
    
    [CL 14619734 by david harvey in Main branch]

--- Engine/Source/ThirdParty/PhysX3/PxShared/src/foundation/include/PsIntrinsics.h
+++ Engine/Source/ThirdParty/PhysX3/PxShared/src/foundation/include/PsIntrinsics.h
@@ -37,7 +37,7 @@
 #elif(PX_LINUX || PX_ANDROID || PX_APPLE_FAMILY || PX_PS4)
 #include "unix/PsUnixIntrinsics.h"
 #elif PX_XBOXONE
-#include "XboxOne/PsXboxOneIntrinsics.h"
+#include "PsXboxIntrinsics.h" //@epic: change path
 #elif PX_SWITCH
 #include "switch/PsSwitchIntrinsics.h"
 #else
