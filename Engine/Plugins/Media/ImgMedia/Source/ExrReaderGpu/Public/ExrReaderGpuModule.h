a72e598c6b1fe63bd4285f547f43250efc8a0cab
kevin ortegren <Kevin.Ortegren@epicgames.com>
2020-10-30 06:13:31 -0400

    Build fix, adding missing DECLARE_LOG_CATEGORY_EXTERN
    [FYI] Ruslan.Idrisov
    #rnx
    #jira none
    #rb trivial
    
    #ROBOMERGE-SOURCE: CL 14619236 in //UE4/Release-4.26/... via CL 14619238
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v753-14616146)
    
    [CL 14619241 by kevin ortegren in Main branch]

--- Engine/Plugins/Media/ImgMedia/Source/ExrReaderGpu/Public/ExrReaderGpuModule.h
+++ Engine/Plugins/Media/ImgMedia/Source/ExrReaderGpu/Public/ExrReaderGpuModule.h
@@ -1,9 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
+#include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-
 #if defined(PLATFORM_WINDOWS) && PLATFORM_WINDOWS
+
+DECLARE_LOG_CATEGORY_EXTERN(LogExrReaderGpu, Log, All);
 
 class FExrReaderGpuModule : public IModuleInterface
 {
