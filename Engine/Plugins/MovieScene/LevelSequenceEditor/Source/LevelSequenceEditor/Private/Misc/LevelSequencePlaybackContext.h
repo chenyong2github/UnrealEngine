164b7aa52fca19e3087c720406bd38ba785bfa39
ludovic chabant <Ludovic.Chabant@epicgames.com>
2020-11-02 14:34:44 -0500

    Sequencer: The level sequence playback context picker now lets you pick actual level sequence actors instead of just worlds.
    Having a playback context that leads to the actor will let us do more contextual things in the editor.
    
    #rb max.chen
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: ludovic.chabant
    #ROBOMERGE-SOURCE: CL 14518176 via CL 14550434 via CL 14550501 via CL 14550568
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v753-14616146)
    
    [CL 14633906 by ludovic chabant in Dev-Tools-Staging branch]

--- Engine/Plugins/MovieScene/LevelSequenceEditor/Source/LevelSequenceEditor/Private/Misc/LevelSequencePlaybackContext.h
+++ Engine/Plugins/MovieScene/LevelSequenceEditor/Source/LevelSequenceEditor/Private/Misc/LevelSequencePlaybackContext.h
@@ -9,17 +9,24 @@
 #include "UObject/WeakObjectPtr.h"
 #include "UObject/WeakObjectPtrTemplates.h"
 
-class UWorld;
+class ALevelSequenceActor;
+class IMovieScenePlaybackClient;
+class ULevelSequence;
 
 /**
- * Class that manages the current UWorld context that a level-sequence editor should use for playback
+ * Class that manages the current playback context that a level-sequence editor should use for playback
  */
 class FLevelSequencePlaybackContext : public TSharedFromThis<FLevelSequencePlaybackContext>
 {
 public:
 
-	FLevelSequencePlaybackContext();
+	FLevelSequencePlaybackContext(ULevelSequence* InLevelSequence);
 	~FLevelSequencePlaybackContext();
+
+	/**
+	 * Gets the level sequence for which we are trying to find the context.
+	 */
+	ULevelSequence* GetLevelSequence() const;
 
 	/**
 	 * Build a world picker widget that allows the user to choose a world, and exit the auto-bind settings
@@ -29,12 +36,22 @@ public:
 	/**
 	 * Resolve the current world context pointer. Can never be nullptr.
 	 */
-	UWorld* Get() const;
+	UWorld* GetPlaybackContext() const;
 
 	/**
-	 * Resolve the current world context pointer as a base object. Can never be nullptr.
+	 * Returns GetPlaybackContext as a plain object.
 	 */
-	UObject* GetAsObject() const;
+	UObject* GetPlaybackContextAsObject() const;
+
+	/**
+	 * Resolve the current playback client. May be nullptr.
+	 */
+	ALevelSequenceActor* GetPlaybackClient() const;
+
+	/**
+	 * Returns GetPlaybackClient as an interface pointer.
+	 */
+	IMovieScenePlaybackClient* GetPlaybackClientAsInterface() const;
 
 	/**
 	 * Retrieve all the event contexts for the current world
@@ -42,24 +59,43 @@ public:
 	TArray<UObject*> GetEventContexts() const;
 
 	/**
-	 * Compute the new playback context based on the user's current auto-bind settings.
-	 * Will use the first encountered PIE or Simulate world if possible, else the Editor world as a fallback
-	 */
-	static UWorld* ComputePlaybackContext();
-
-	/**
 	 * Specify a new world to use as the context. Persists until the next PIE or map change event.
 	 * May be null, in which case the context will be recomputed automatically
 	 */
-	void OverrideWith(UWorld* InNewContext);
+	void OverrideWith(UWorld* InNewContext, ALevelSequenceActor* InNewClient);
 
 private:
+
+	using FContextAndClient = TTuple<UWorld*, ALevelSequenceActor*>;
+
+	/**
+	 * Compute the new playback context based on the user's current auto-bind settings.
+	 * Will use the first encountered PIE or Simulate world if possible, else the Editor world as a fallback
+	 */
+	static FContextAndClient ComputePlaybackContextAndClient(const ULevelSequence* InLevelSequence);
+
+	/**
+	 * Update the cached context and client pointers if needed.
+	 */
+	void UpdateCachedContextAndClient() const;
+
+	/**
+	 * Gets both the context and client.
+	 */
+	//TTuple<UWorld*, ALevelSequenceActor*>
+	FContextAndClient GetPlaybackContextAndClient() const;
 
 	void OnPieEvent(bool);
 	void OnMapChange(uint32);
 
 private:
 
+	/** Level sequence that we should find a context for */
+	TWeakObjectPtr<ULevelSequence> LevelSequence;
+
 	/** Mutable cached context pointer */
 	mutable TWeakObjectPtr<UWorld> WeakCurrentContext;
+
+	/** Mutable cached client pointer */
+	mutable TWeakObjectPtr<ALevelSequenceActor> WeakCurrentClient;
 };
