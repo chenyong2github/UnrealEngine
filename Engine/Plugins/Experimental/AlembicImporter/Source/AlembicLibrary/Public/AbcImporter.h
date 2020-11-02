6f8f0233a9c64dcee680c8423ef6ba5026e5aff8
anousack kitisa <Anousack.Kitisa@epicgames.com>
2020-11-02 14:07:57 -0500

    Fixed possible morph target position quantization issue when importing Alembic as SkeletalMesh.
    #jira none
    
    #rb JeanMichel.Dignard
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: anousack.kitisa
    #ROBOMERGE-SOURCE: CL 14540117 in //UE4/Release-4.26/... via CL 14540999 via CL 14541013
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v753-14616146)
    
    [CL 14632420 by anousack kitisa in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/AlembicImporter/Source/AlembicLibrary/Public/AbcImporter.h
+++ Engine/Plugins/Experimental/AlembicImporter/Source/AlembicLibrary/Public/AbcImporter.h
@@ -197,7 +197,7 @@ private:
 	bool BuildSkeletalMesh(FSkeletalMeshLODModel& LODModel, const FReferenceSkeleton& RefSkeleton, FAbcMeshSample* Sample, TArray<int32>& OutMorphTargetVertexRemapping, TArray<int32>& OutUsedVertexIndicesForMorphs);
 	
 	/** Generate morph target vertices from the PCA compressed bases */
-	void GenerateMorphTargetVertices(FAbcMeshSample* BaseSample, TArray<FMorphTargetDelta> &MorphDeltas, FAbcMeshSample* AverageSample, uint32 WedgeOffset, const TArray<int32>& RemapIndices, const TArray<int32>& UsedVertexIndicesForMorphs, const uint32 VertexOffset, const uint32 IndexOffset);
+	void GenerateMorphTargetVertices(FAbcMeshSample* BaseSample, TArray<FMorphTargetDelta> &MorphDeltas, FAbcMeshSample* AverageSample, uint32 WedgeOffset, const TArray<int32>& RemapIndices, const TArray<int32>& UsedVertexIndicesForMorphs, const uint32 VertexOffset, const uint32 IndexOffset, bool bEnableDeltaOffset, FVector& OutOffset);
 	
 	/** Set up correct morph target weights from the PCA compressed data */
 	void SetupMorphTargetCurves(USkeleton* Skeleton, FName ConstCurveName, UAnimSequence* Sequence, const TArray<float> &CurveValues, const TArray<float>& TimeValues);
