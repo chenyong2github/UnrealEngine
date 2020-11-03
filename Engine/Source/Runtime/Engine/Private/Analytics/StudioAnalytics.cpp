4389b14fdccb749d6ceaa6adaf732572167c04e1
mark lintott <mark.lintott@epicgames.com>
2020-11-03 17:06:14 -0500

    [FYI] bob.tellez
    Resubmit of changelist 14637415
    
    Added guards so the StudioAnalytics does not initialise in non-editor, Shipping or Test builds. This is inline with the orignal settings when analytics was initialised from FortniteMain.cpp
    
    
    #ROBOMERGE-OWNER: mark.lintott
    #ROBOMERGE-AUTHOR: mark.lintott
    #ROBOMERGE-SOURCE: CL 14646853 via CL 14647172
    #ROBOMERGE-BOT: (v755-14641574)
    
    [CL 14647175 by mark lintott in Release-Engine-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Analytics/StudioAnalytics.cpp
+++ Engine/Source/Runtime/Engine/Private/Analytics/StudioAnalytics.cpp
@@ -23,6 +23,7 @@ bool FStudioAnalytics::bInitialized = false;
 volatile double FStudioAnalytics::TimeEstimation = 0;
 FThread FStudioAnalytics::TimerThread;
 TSharedPtr<IAnalyticsProviderET> FStudioAnalytics::Analytics;
+TArray<FAnalyticsEventAttribute> FStudioAnalytics::DefaultAttributes;
 
 void FStudioAnalytics::SetProvider(TSharedRef<IAnalyticsProviderET> InAnalytics)
 {
@@ -32,12 +33,35 @@ void FStudioAnalytics::SetProvider(TSharedRef<IAnalyticsProviderET> InAnalytics)
 
 	Analytics = InAnalytics;
 
+	ApplyDefaultEventAttributes();
+
 	TimeEstimation = FPlatformTime::Seconds();
 
 	if (FPlatformProcess::SupportsMultithreading())
 	{
 		TimerThread = FThread(TEXT("Studio Analytics Timer Thread"), []() { RunTimer_Concurrent(); });
 	}
+}
+
+void FStudioAnalytics::ApplyDefaultEventAttributes()
+{
+	if (Analytics.IsValid())
+	{
+		// Apply all the default attributes to the provider
+		Analytics->SetDefaultEventAttributes(MoveTemp(DefaultAttributes));
+	}	
+}
+
+void FStudioAnalytics::AddDefaultEventAttribute(const FAnalyticsEventAttribute& Attribute)
+{
+	// Append a single default attribute to the existing list
+	DefaultAttributes.Emplace(Attribute);
+}
+
+void FStudioAnalytics::AddDefaultEventAttributes(TArray<FAnalyticsEventAttribute>&& Attributes)
+{
+	// Append attributes list to the existing list
+	DefaultAttributes += MoveTemp(Attributes);
 }
 
 IAnalyticsProviderET& FStudioAnalytics::GetProvider()
