1d29386b9896d08d9410a56b2b89fc4597b8901e
Joe Graf <Joe.Graf@epicgames.com>
2020-11-04 13:55:42 -0500

    Setup the initial pass through support for routing interface requests to the base OSS
    #rb: none
    
    [CL 14655338 by Joe Graf in Main branch]

--- Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOSPlus/Private/OnlineSubsystemEOSPlus.cpp
+++ Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOSPlus/Private/OnlineSubsystemEOSPlus.cpp
@@ -2,14 +2,7 @@
 
 #include "OnlineSubsystemEOSPlus.h"
 
-#include "Misc/NetworkVersion.h"
-#include "Misc/App.h"
-
-
-bool FOnlineSubsystemEOSPlus::Tick(float DeltaTime)
-{
-	return FOnlineSubsystemImpl::Tick(DeltaTime);
-}
+#include "Misc/ConfigCacheIni.h"
 
 bool FOnlineSubsystemEOSPlus::Exec(UWorld* InWorld, const TCHAR* Cmd, FOutputDevice& Ar)
 {
@@ -18,10 +11,166 @@ bool FOnlineSubsystemEOSPlus::Exec(UWorld* InWorld, const TCHAR* Cmd, FOutputDev
 
 FString FOnlineSubsystemEOSPlus::GetAppId() const
 {
-	return TEXT("");
+	return BaseOSS != nullptr ? BaseOSS->GetAppId() : TEXT("");
 }
 
 FText FOnlineSubsystemEOSPlus::GetOnlineServiceName() const
 {
 	return NSLOCTEXT("OnlineSubsystemEOSPlus", "OnlineServiceName", "EOS_Plus");
+}
+
+bool FOnlineSubsystemEOSPlus::Init()
+{
+#if PLATFORM_DESKTOP
+	FString BaseOSSName;
+	GConfig->GetString(TEXT("[OnlineSubsystemEOSPlus]"), TEXT("BaseOSSName"), BaseOSSName, GEngineIni);
+	BaseOSS = BaseOSSName.IsEmpty() ? IOnlineSubsystem::GetByPlatform() : IOnlineSubsystem::Get(FName(*BaseOSSName));
+#else
+	BaseOSS = IOnlineSubsystem::GetByPlatform();
+#endif
+	if (BaseOSS != nullptr)
+	{
+		UE_LOG_ONLINE(Error, TEXT("FOnlineSubsystemEOSPlus::Init() failed to get the platform OSS"));
+		return false;
+	}
+	EosOSS = IOnlineSubsystem::Get(EOS_SUBSYSTEM);
+	if (EosOSS != nullptr)
+	{
+		UE_LOG_ONLINE(Error, TEXT("FOnlineSubsystemEOSPlus::Init() failed to get the EOS OSS"));
+		return false;
+	}
+	return true;
+}
+
+bool FOnlineSubsystemEOSPlus::Shutdown()
+{
+	BaseOSS = nullptr;
+	EosOSS = nullptr;
+
+	return true;
+}
+
+IOnlineSessionPtr FOnlineSubsystemEOSPlus::GetSessionInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetSessionInterface() : nullptr;
+}
+
+IOnlineFriendsPtr FOnlineSubsystemEOSPlus::GetFriendsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetFriendsInterface() : nullptr;
+}
+
+IOnlineGroupsPtr FOnlineSubsystemEOSPlus::GetGroupsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetGroupsInterface() : nullptr;
+}
+
+IOnlinePartyPtr FOnlineSubsystemEOSPlus::GetPartyInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetPartyInterface() : nullptr;
+}
+
+IOnlineSharedCloudPtr FOnlineSubsystemEOSPlus::GetSharedCloudInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetSharedCloudInterface() : nullptr;
+}
+
+IOnlineUserCloudPtr FOnlineSubsystemEOSPlus::GetUserCloudInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetUserCloudInterface() : nullptr;
+}
+
+IOnlineEntitlementsPtr FOnlineSubsystemEOSPlus::GetEntitlementsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetEntitlementsInterface() : nullptr;
+}
+
+IOnlineLeaderboardsPtr FOnlineSubsystemEOSPlus::GetLeaderboardsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetLeaderboardsInterface() : nullptr;
+}
+
+IOnlineVoicePtr FOnlineSubsystemEOSPlus::GetVoiceInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetVoiceInterface() : nullptr;
+}
+
+IOnlineExternalUIPtr FOnlineSubsystemEOSPlus::GetExternalUIInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetExternalUIInterface() : nullptr;
+}
+
+IOnlineTimePtr FOnlineSubsystemEOSPlus::GetTimeInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetTimeInterface() : nullptr;
+}
+
+IOnlineIdentityPtr FOnlineSubsystemEOSPlus::GetIdentityInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetIdentityInterface() : nullptr;
+}
+
+IOnlineTitleFilePtr FOnlineSubsystemEOSPlus::GetTitleFileInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetTitleFileInterface() : nullptr;
+}
+
+IOnlineStoreV2Ptr FOnlineSubsystemEOSPlus::GetStoreV2Interface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetStoreV2Interface() : nullptr;
+}
+
+IOnlinePurchasePtr FOnlineSubsystemEOSPlus::GetPurchaseInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetPurchaseInterface() : nullptr;
+}
+
+IOnlineEventsPtr FOnlineSubsystemEOSPlus::GetEventsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetEventsInterface() : nullptr;
+}
+
+IOnlineAchievementsPtr FOnlineSubsystemEOSPlus::GetAchievementsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetAchievementsInterface() : nullptr;
+}
+
+IOnlineSharingPtr FOnlineSubsystemEOSPlus::GetSharingInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetSharingInterface() : nullptr;
+}
+
+IOnlineUserPtr FOnlineSubsystemEOSPlus::GetUserInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetUserInterface() : nullptr;
+}
+
+IOnlineMessagePtr FOnlineSubsystemEOSPlus::GetMessageInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetMessageInterface() : nullptr;
+}
+
+IOnlinePresencePtr FOnlineSubsystemEOSPlus::GetPresenceInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetPresenceInterface() : nullptr;
+}
+
+IOnlineChatPtr FOnlineSubsystemEOSPlus::GetChatInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetChatInterface() : nullptr;
+}
+
+IOnlineStatsPtr FOnlineSubsystemEOSPlus::GetStatsInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetStatsInterface() : nullptr;
+}
+
+IOnlineTurnBasedPtr FOnlineSubsystemEOSPlus::GetTurnBasedInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetTurnBasedInterface() : nullptr;
+}
+
+IOnlineTournamentPtr FOnlineSubsystemEOSPlus::GetTournamentInterface() const
+{
+	return BaseOSS != nullptr ? BaseOSS->GetTournamentInterface() : nullptr;
 }
