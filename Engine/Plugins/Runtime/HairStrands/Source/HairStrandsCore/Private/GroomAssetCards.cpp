bf3a19ee83482d6075e75a6f93f8943cfaca40ab
charles derousiers <Charles.deRousiers@epicgames.com>
2020-11-04 14:21:04 -0500

    Fix invalid reimport of hair cards/meshes.
    #rb none
    #jira DH-2079
    
    #ROBOMERGE-OWNER: charles.derousiers
    #ROBOMERGE-AUTHOR: charles.derousiers
    #ROBOMERGE-SOURCE: CL 14654870 in //UE4/Release-4.26/... via CL 14655570
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v756-14651656)
    
    [CL 14655598 by charles derousiers in Main branch]

--- Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Private/GroomAssetCards.cpp
+++ Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Private/GroomAssetCards.cpp
@@ -2,6 +2,7 @@
 
 #include "GroomAssetCards.h"
 #include "Engine/StaticMesh.h"
+#include "MeshDescription.h"
 
 FHairCardsClusterSettings::FHairCardsClusterSettings()
 {
@@ -115,18 +116,43 @@ bool FHairGroupsCardsSourceDescription::operator==(const FHairGroupsCardsSourceD
 		ImportedMesh == A.ImportedMesh;
 }
 
-bool FHairGroupsCardsSourceDescription::HasMeshChanged() const
+FString FHairGroupsCardsSourceDescription::GetMeshKey() const
 {
 #if WITH_EDITORONLY_DATA
 	if (SourceType == EHairCardsSourceType::Imported && ImportedMesh)
 	{
 		ImportedMesh->ConditionalPostLoad();
-		return ImportedMeshKey == ImportedMesh->GetRenderData()->DerivedDataKey;
+		FStaticMeshSourceModel& SourceModel = ImportedMesh->GetSourceModel(0);
+		if (SourceModel.MeshDescriptionBulkData)
+		{
+			return SourceModel.MeshDescriptionBulkData->GetIdString();
+		}
+
 	}
 	else if (SourceType == EHairCardsSourceType::Procedural && ProceduralMesh)
 	{
 		ProceduralMesh->ConditionalPostLoad();
-		return ProceduralMeshKey == ProceduralMesh->GetRenderData()->DerivedDataKey;
+		FStaticMeshSourceModel& SourceModel = ProceduralMesh->GetSourceModel(0);
+		if (SourceModel.MeshDescriptionBulkData)
+		{
+			return SourceModel.MeshDescriptionBulkData->GetIdString();
+		}
+	}
+	return TEXT("INVALID_MESH");
+#endif
+}
+
+bool FHairGroupsCardsSourceDescription::HasMeshChanged() const
+{
+#if WITH_EDITORONLY_DATA
+	if (SourceType == EHairCardsSourceType::Imported && ImportedMesh)
+	{
+		return ImportedMeshKey == GetMeshKey();
+	}
+	else if (SourceType == EHairCardsSourceType::Procedural && ProceduralMesh)
+	{
+		ProceduralMesh->ConditionalPostLoad();
+		return ProceduralMeshKey == GetMeshKey();
 	}
 #endif
 	return false;
@@ -137,13 +163,11 @@ void FHairGroupsCardsSourceDescription::UpdateMeshKey()
 #if WITH_EDITORONLY_DATA
 	if (SourceType == EHairCardsSourceType::Imported && ImportedMesh)
 	{
-		ImportedMesh->ConditionalPostLoad();
-		ImportedMeshKey = ImportedMesh->GetRenderData()->DerivedDataKey;
+		ImportedMeshKey = GetMeshKey();
 	}
 	else if (SourceType == EHairCardsSourceType::Procedural && ProceduralMesh)
 	{
-		ProceduralMesh->ConditionalPostLoad();
-		ProceduralMeshKey = ProceduralMesh->GetRenderData()->DerivedDataKey;
+		ProceduralMeshKey = GetMeshKey();
 	}
 #endif
 }
