441323855710c15dd81cdfb2205c255d28e27aae
alejandro aguilar <AlejandroAguilar@themultiplayerguys.com>
2020-11-06 11:40:55 -0500

    Adding deprecation warning disable guards
    #jira DH-2167, UE-102515
    [at]Sam.Zamani
    #lockdown cristina.riveron
    
    #ROBOMERGE-SOURCE: CL 14677619 in //UE4/Release-4.26/... via CL 14677644
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v756-14651656)
    
    [CL 14677647 by alejandro aguilar in Main branch]

--- Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineBlueprintSupport/Private/K2Node_InAppPurchaseRestore.cpp
+++ Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineBlueprintSupport/Private/K2Node_InAppPurchaseRestore.cpp
@@ -8,10 +8,12 @@
 UK2Node_InAppPurchaseRestore::UK2Node_InAppPurchaseRestore(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
 	ProxyFactoryFunctionName = GET_FUNCTION_NAME_CHECKED(UInAppPurchaseRestoreCallbackProxy, CreateProxyObjectForInAppPurchaseRestore);
 	ProxyFactoryClass = UInAppPurchaseRestoreCallbackProxy::StaticClass();
 
 	ProxyClass = UInAppPurchaseRestoreCallbackProxy::StaticClass();
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
 }
 
 #undef LOCTEXT_NAMESPACE
