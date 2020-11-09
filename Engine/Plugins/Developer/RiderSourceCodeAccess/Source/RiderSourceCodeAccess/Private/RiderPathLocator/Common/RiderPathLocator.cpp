0c32389ffa6cd451ce8c7a5b45696f68d76be507
DecoyRS <a.g.pirogov@gmail.com>
2020-11-09 10:39:52 -0500

    PR #7449: Add support for MacOS and Uproject based project model (Contributed by DecoyRS)
    #jira UE-101937
    #rb none
    
    [CL 14689498 by Ben Marsh in Main branch]

---
+++ Engine/Plugins/Developer/RiderSourceCodeAccess/Source/RiderSourceCodeAccess/Private/RiderPathLocator/Common/RiderPathLocator.cpp
@@ -0,0 +1,75 @@
+﻿#include "RiderPathLocator/RiderPathLocator.h"
+
+#include "Dom/JsonObject.h"
+#include "Internationalization/Regex.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+TArray<FInstallInfo> FRiderPathLocator::GetInstallInfosFromToolbox(const FString& ToolboxPath, const FString& Pattern)
+{
+	FString InstallPath = ToolboxPath;
+	const FString SettingJsonPath = FPaths::Combine(ToolboxPath, TEXT(".settings.json"));
+	if (FPaths::FileExists(SettingJsonPath))
+	{
+		FString JsonStr;
+		FFileHelper::LoadFileToString(JsonStr, *SettingJsonPath);
+		TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(JsonStr);
+		TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
+		if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+		{
+			FString InstallLocation;
+			if (JsonObject->TryGetStringField(TEXT("install_location"), InstallLocation))
+			{
+				if (!InstallLocation.IsEmpty())
+				{
+					InstallPath = InstallLocation;
+				}
+			}
+		}
+	}
+	
+	const FString ToolboxRiderRootPath = FPaths::Combine(InstallPath, TEXT("apps"));
+	if(!FPaths::DirectoryExists(ToolboxRiderRootPath)) return {};
+
+	return GetInstallInfos(ToolboxRiderRootPath, Pattern, true);
+}
+
+TArray<FInstallInfo> FRiderPathLocator::GetInstallInfos(const FString& ToolboxRiderRootPath, const FString& Pattern, bool IsToolbox)
+{
+	TArray<FInstallInfo> RiderInstallInfos;
+	TArray<FString> RiderPaths;
+	IFileManager::Get().FindFilesRecursive(RiderPaths, *ToolboxRiderRootPath, *Pattern, true, true);
+	for(const FString& RiderPath: RiderPaths)
+	{
+		TOptional<FInstallInfo> InstallInfo = GetInstallInfoFromRiderPath(RiderPath, IsToolbox);
+		if(InstallInfo.IsSet())
+			RiderInstallInfos.Add(InstallInfo.GetValue());
+	}
+	return RiderInstallInfos;
+}
+
+void FRiderPathLocator::ParseProductInfoJson(FInstallInfo& Info, const FString& ProductInfoJsonPath)
+{
+	FString JsonStr;
+	FFileHelper::LoadFileToString(JsonStr, *ProductInfoJsonPath);
+	TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(JsonStr);
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+	{
+		FString VersionString;
+		JsonObject->TryGetStringField(TEXT("buildNumber"), VersionString);
+		Info.Version = VersionString;
+		const TSharedPtr<FJsonObject>* CustomProperties;
+		if(JsonObject->TryGetObjectField(TEXT("customProperties"), CustomProperties))
+		{
+			FString SupportUprojectState;
+			if(CustomProperties->Get()->TryGetStringField(TEXT("SupportUproject"), SupportUprojectState))
+			{
+				if(SupportUprojectState.Equals(TEXT("Beta"))) Info.SupportUprojectState = FInstallInfo::ESupportUproject::Beta;
+				if(SupportUprojectState.Equals(TEXT("Release"))) Info.SupportUprojectState = FInstallInfo::ESupportUproject::Release;
+			}
+		}
+	}
+}
