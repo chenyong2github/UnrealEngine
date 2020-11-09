b38a7aa3b742c40faf511cdc8139aa93ce55f59e
marc audy <Marc.Audy@epicgames.com>
2020-11-09 11:35:16 -0500

    Adapt to SelectedGroupIDs being changed from Array to Set in CL# 14317869 #rb #rnx [CODEREVIEW] Ryan.Schmidt
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 14570782 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14690912 by marc audy in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/MeshModelingToolset/Source/MeshModelingToolsEditorOnly/Private/Hair/GroomCardsEditorTool.cpp
+++ Engine/Plugins/Experimental/MeshModelingToolset/Source/MeshModelingToolsEditorOnly/Private/Hair/GroomCardsEditorTool.cpp
@@ -500,11 +500,14 @@ void UGroomCardsEditorTool::OnClicked(const FInputDeviceRay& ClickPos)
 		const FGroupTopologySelection& CurSelection = CardMeshSelectionMechanic->GetActiveSelection();
 		if (CurSelection.SelectedGroupIDs.Num() == 1)
 		{
-			int32 SelectedCardGroupID = CurSelection.SelectedGroupIDs[0];
-			GetToolManager()->EmitObjectChange(this, MakeUnique<FBeginGroomCardsEditChange>(SelectedCardGroupID),
-				LOCTEXT("BeginCardEditChange", "BeginCardEdit"));
+			for (const int32 SelectedCardGroupID : CurSelection.SelectedGroupIDs)
+			{
+				GetToolManager()->EmitObjectChange(this, MakeUnique<FBeginGroomCardsEditChange>(SelectedCardGroupID),
+					LOCTEXT("BeginCardEditChange", "BeginCardEdit"));
 
-			BeginCardEdit(SelectedCardGroupID);
+				BeginCardEdit(SelectedCardGroupID);
+				break;
+			}
 		}
 
 	}
