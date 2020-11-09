82e14250bc364535f92437537a756b165d3535a3
donal maccarthy <Donal.MacCarthy@epicgames.com>
2020-11-09 10:54:44 -0500

    Add option to output SDK strings (-outputSDKs) when using the ValidatePlatforms mode in UnrealBuildTool
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: donal.maccarthy
    #ROBOMERGE-SOURCE: CL 14533946 via CL 14533955 via CL 14534319 via CL 14534347 via CL 14534371 via CL 14557739 via CL 14557859 via CL 14557920
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14689819 by donal maccarthy in Dev-Tools-Staging branch]

--- Engine/Source/Programs/UnrealBuildTool/Modes/ValidatePlatformsMode.cs
+++ Engine/Source/Programs/UnrealBuildTool/Modes/ValidatePlatformsMode.cs
@@ -28,6 +28,12 @@ namespace UnrealBuildTool
 		bool bAllPlatforms = false;
 
 		/// <summary>
+		/// Whether to output SDK versions.
+		/// </summary>
+		[CommandLine("-OutputSDKs")]
+		bool bOutputSDKs = false;
+
+		/// <summary>
 		/// Executes the tool with the given arguments
 		/// </summary>
 		/// <param name="Arguments">Command line arguments</param>
@@ -48,13 +54,19 @@ namespace UnrealBuildTool
 			foreach (UnrealTargetPlatform Platform in Platforms)
 			{
 				UEBuildPlatform BuildPlatform = UEBuildPlatform.GetBuildPlatform(Platform, true);
+				string PlatformSDKString = "";
+				if (bOutputSDKs)
+				{
+					PlatformSDKString = BuildPlatform != null ? BuildPlatform.GetRequiredSDKString() : "<UNKNOWN>";
+				}
+
 				if (BuildPlatform != null && BuildPlatform.HasRequiredSDKsInstalled() == SDKStatus.Valid)
 				{
-					Log.TraceInformation("##PlatformValidate: {0} VALID", Platform.ToString());
+					Log.TraceInformation("##PlatformValidate: {0} VALID {1}", Platform.ToString(), PlatformSDKString);
 				}
 				else
 				{
-					Log.TraceInformation("##PlatformValidate: {0} INVALID", Platform.ToString());
+					Log.TraceInformation("##PlatformValidate: {0} INVALID {1}", Platform.ToString(), PlatformSDKString);
 				}
 			} 
 			return 0;
