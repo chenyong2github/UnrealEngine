2a5981f132b0df19372534fa399bde8425c53eb0
chris norden <Chris.Norden@epicgames.com>
2020-11-09 11:24:29 -0500

    VCamCore: Added more sample functionality to ComposureWizard and documented the Blueprint Added VCamSample_CameraActor class for sample usage with ComposureWizard
    #rb david.hibbitts
    #jira none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: chris.norden
    #ROBOMERGE-SOURCE: CL 14565463 in //UE4/Release-4.26/... via CL 14565473 via CL 14569035
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14690577 by chris norden in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VCamCore/Private/VCamSample_CameraActor.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VCamSample_CameraActor.h"
+
+AVCamSample_CameraActor::AVCamSample_CameraActor(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	VCamComponent = ObjectInitializer.CreateDefaultSubobject<UVCamComponent>(this, TEXT("VCamComponent"));
+	VCamComponent->AttachToComponent(GetCineCameraComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	VCamComponent->RegisterComponent();
+}
