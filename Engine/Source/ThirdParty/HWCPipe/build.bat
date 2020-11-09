56a5f008da4e5432dcd966c35fc7f6ba182ac787
dmytro vovk <dmytro.vovk@epicgames.com>
2020-11-09 11:23:44 -0500

    HWCPipe, an ARM's library for perfomance counters integration. Exposes GPU counters on a per frame basis to CSV reports and stats profiler #jira UE-96743 #rb jack.porter
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: dmytro.vovk
    #ROBOMERGE-SOURCE: CL 14554386 via CL 14554393 via CL 14554394 via CL 14564794 via CL 14564865 via CL 14569025
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14690557 by dmytro vovk in Dev-Tools-Staging branch]

---
+++ Engine/Source/ThirdParty/HWCPipe/build.bat
@@ -0,0 +1,15 @@
+set CMAKE_BIN=%ANDROID_HOME%\cmake\3.10.2.4988404\bin
+
+REM Build ARMv7
+%CMAKE_BIN%\cmake.exe -DANDROID_ABI=armeabi-v7a -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=%CMAKE_BIN%\ninja.exe -G Ninja -DCMAKE_TOOLCHAIN_FILE=%ANDROID_NDK_ROOT%\build\cmake\android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=23 -H%CD%\include\ -Bbuild\armeabi-v7a\
+
+%CMAKE_BIN%\ninja.exe -C build\armeabi-v7a\
+
+copy build\armeabi-v7a\libhwcpipe.so lib\armeabi-v7a\libhwcpipe.so
+
+REM Build ARMv8
+%CMAKE_BIN%\cmake.exe -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=%CMAKE_BIN%\ninja.exe -G Ninja -DCMAKE_TOOLCHAIN_FILE=%ANDROID_NDK_ROOT%\build\cmake\android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=23 -H%CD%\include\ -Bbuild\arm64-v8a\
+
+%CMAKE_BIN%\ninja.exe -C build\arm64-v8a\
+
+copy build\arm64-v8a\libhwcpipe.so lib\arm64-v8a\libhwcpipe.so
