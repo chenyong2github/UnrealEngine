93a090c517fb11d664d58d5a9e113f5e5c586372
simon tovey <Simon.Tovey@epicgames.com>
2020-11-09 12:15:14 -0500

    Fix crash due to corrupt ribbons. Event spawning creates malformed particles which causes some more recent error checking code to corrupt valid ribbon particles and cause a crash. These malfomed particles can be identified with the forcekill flag.
    #jira UE-95525
    #rb Stu.Mckenna
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: simon.tovey
    #ROBOMERGE-SOURCE: CL 14582656 in //UE4/Release-4.26/... via CL 14582670 via CL 14582682
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14691833 by simon tovey in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Particles/ParticleTrail2EmitterInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Particles/ParticleTrail2EmitterInstance.cpp
@@ -492,20 +492,25 @@ void FParticleTrailsEmitterInstance_Base::KillParticles()
 					check(!TEXT("What the hell are you doing in here?"));
 				}
 
-				//Be certain we've broken links to this particle				
-				int32 Next = TRAIL_EMITTER_GET_NEXT(TrailData->Flags);
-				if (Next != TRAIL_EMITTER_NULL_NEXT)
+				//Be certain we've broken links to this particle.
+				//Some spawn paths leave zombie "forcekill" particles in the buffer so we avoid doing these. Event spawning being one.
+				//These particles have malformed flags so clearing their next/prev can corrupt other particle data.
+				if (!TRAIL_EMITTER_IS_FORCEKILL(TrailData->Flags))
 				{
-					DECLARE_PARTICLE_PTR(NextParticle, ParticleData + ParticleStride * Next);
-					FTrailsBaseTypeDataPayload* NextTrailData = ((FTrailsBaseTypeDataPayload*)((uint8*)NextParticle + TypeDataOffset));
-					NextTrailData->Flags = TRAIL_EMITTER_SET_PREV(NextTrailData->Flags, TRAIL_EMITTER_NULL_PREV);
-				}
-				int32 Prev = TRAIL_EMITTER_GET_PREV(TrailData->Flags);
-				if (Prev != TRAIL_EMITTER_NULL_PREV)
-				{
-					DECLARE_PARTICLE_PTR(PrevParticle, ParticleData + ParticleStride * Prev);
-					FTrailsBaseTypeDataPayload* PrevTrailData = ((FTrailsBaseTypeDataPayload*)((uint8*)PrevParticle + TypeDataOffset));
-					PrevTrailData->Flags = TRAIL_EMITTER_SET_NEXT(PrevTrailData->Flags, TRAIL_EMITTER_NULL_NEXT);
+					int32 Next = TRAIL_EMITTER_GET_NEXT(TrailData->Flags);
+					if (Next != TRAIL_EMITTER_NULL_NEXT)
+					{
+						DECLARE_PARTICLE_PTR(NextParticle, ParticleData + ParticleStride * Next);
+						FTrailsBaseTypeDataPayload* NextTrailData = ((FTrailsBaseTypeDataPayload*)((uint8*)NextParticle + TypeDataOffset));
+						NextTrailData->Flags = TRAIL_EMITTER_SET_PREV(NextTrailData->Flags, TRAIL_EMITTER_NULL_PREV);
+					}
+					int32 Prev = TRAIL_EMITTER_GET_PREV(TrailData->Flags);
+					if (Prev != TRAIL_EMITTER_NULL_PREV)
+					{
+						DECLARE_PARTICLE_PTR(PrevParticle, ParticleData + ParticleStride * Prev);
+						FTrailsBaseTypeDataPayload* PrevTrailData = ((FTrailsBaseTypeDataPayload*)((uint8*)PrevParticle + TypeDataOffset));
+						PrevTrailData->Flags = TRAIL_EMITTER_SET_NEXT(PrevTrailData->Flags, TRAIL_EMITTER_NULL_NEXT);
+					}
 				}
 
 				// Clear it out... just to be safe when it gets pulled back to active
@@ -532,7 +537,7 @@ void FParticleTrailsEmitterInstance_Base::KillParticles()
 				if (TRAIL_EMITTER_IS_FORCEKILL(TrailData->Flags))
 				{
 					TrailData->Flags = TRAIL_EMITTER_SET_NEXT(TrailData->Flags, TRAIL_EMITTER_NULL_NEXT);
-					TrailData->Flags =TRAIL_EMITTER_SET_PREV(TrailData->Flags, TRAIL_EMITTER_NULL_PREV);
+					TrailData->Flags = TRAIL_EMITTER_SET_PREV(TrailData->Flags, TRAIL_EMITTER_NULL_PREV);
 
 					ParticleIndices[ParticleIdx] = ParticleIndices[ActiveParticles-1];
 					ParticleIndices[ActiveParticles-1]	= CurrentIndex;
