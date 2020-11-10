a4b23675badaf204dde51e3b9e15b3a93fd9839f
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-11-10 17:25:46 -0500

    Bump to the NiagaraCustomVersion to address niagara shader serialization crashes #jira none #rb none
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 14661424 via CL 14661435 via CL 14661470 via CL 14661500 via CL 14661537 via CL 14661542
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14714581 by rob krajcarski in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x5C416572, 0xB8A64557, 0xBC15514F, 0xCF0502B1);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xBBA95252, 0xEF5D4F65, 0x8F45ED97, 0xE55536CB);
