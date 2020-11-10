75ef2960908aed390be8e2d6723583641b5b231c
yuriy odonnell <Yuriy.ODonnell@epicgames.com>
2020-11-10 13:53:45 -0500

    Fixed linking errors due to missing RENDERCORE_API declaration for TCopyResourceCS
    #jira UE-102412
    #rb Marc.Audy
    #rnx
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: yuriy.odonnell
    #ROBOMERGE-SOURCE: CL 14645650 in //UE4/Release-4.26/... via CL 14645681 via CL 14645695
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14706098 by yuriy odonnell in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/RenderCore/Private/CopyTextureShaders.cpp
+++ Engine/Source/Runtime/RenderCore/Private/CopyTextureShaders.cpp
@@ -6,7 +6,7 @@ IMPLEMENT_TYPE_LAYOUT(FCopyTextureCS);
 
 #define IMPLEMENT_COPY_RESOURCE_SHADER(SrcType,DstType,ValueType)\
 	typedef TCopyResourceCS<ECopyTextureResourceType::SrcType, ECopyTextureResourceType::DstType, ECopyTextureValueType::ValueType, 4> FCopyTextureCS_##SrcType##_##DstType##_##ValueType##4;\
-	IMPLEMENT_SHADER_TYPE2_WITH_TEMPLATE_PREFIX(template<>, FCopyTextureCS_##SrcType##_##DstType##_##ValueType##4, SF_Compute);
+	IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, RENDERCORE_API, FCopyTextureCS_##SrcType##_##DstType##_##ValueType##4, SF_Compute);
 
 #define IMPLEMENT_COPY_RESOURCE_SHADER_ALL_TYPES(SrcType,DstType)\
 	IMPLEMENT_COPY_RESOURCE_SHADER(SrcType,DstType,Float)\
