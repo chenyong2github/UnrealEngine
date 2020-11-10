4bf59db8bffb683fc6ac69fff445139f365ca1bc
frank fella <Frank.Fella@epicgames.com>
2020-11-10 18:35:20 -0500

    Niagara - Resubmit due to merge problems - Additional fixes for cl 14684035 missed due to merge problems.
    #jira UE-96455
    #rb shaun.kime
    #lockdown marcus.wassmer
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: frank.fella
    #ROBOMERGE-SOURCE: CL 14694880 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14716113 by frank fella in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/NiagaraScratchPadViewModel.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Public/ViewModels/NiagaraScratchPadViewModel.h
@@ -96,6 +96,8 @@ private:
 
 	void ScriptViewModelPinnedChanged(TWeakPtr<FNiagaraScratchPadScriptViewModel> ScriptViewModelWeak);
 
+	void ScriptViewModelChangesApplied();
+
 	void ScriptViewModelRequestDiscardChanges(TWeakPtr<FNiagaraScratchPadScriptViewModel> ScriptViewModelWeak);
 
 	void ScriptViewModelVariableSelectionChanged(TWeakPtr<FNiagaraScratchPadScriptViewModel> ScriptViewModelWeak);
