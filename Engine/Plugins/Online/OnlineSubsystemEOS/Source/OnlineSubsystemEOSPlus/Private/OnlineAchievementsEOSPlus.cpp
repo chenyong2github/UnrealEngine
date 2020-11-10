155ae6b8d18f7852cffc8f8807e1d954c558df4e
joe graf <Joe.Graf@epicgames.com>
2020-11-10 14:05:29 -0500

    Added support for mirroring platform achievement updates to EOS
    #rb: none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: joe.graf
    #ROBOMERGE-SOURCE: CL 14656750 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14706484 by joe graf in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOSPlus/Private/OnlineAchievementsEOSPlus.cpp
@@ -0,0 +1,82 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OnlineAchievementsEOSPlus.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemEOSPlus.h"
+
+FOnAchievementsWrittenDelegate Ignored;
+
+void FOnlineAchievementsEOSPlus::WriteAchievements(const FUniqueNetId& PlayerId, FOnlineAchievementsWriteRef& WriteObject, const FOnAchievementsWrittenDelegate& Delegate)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		Achievements->WriteAchievements(PlayerId, WriteObject, Delegate);
+	}
+	// Mirror the achievement data to EOS
+	IOnlineAchievementsPtr EOSAchievements = EOSPlus->EosOSS->GetAchievementsInterface();
+	if (EOSAchievements.IsValid())
+	{
+		EOSAchievements->WriteAchievements(PlayerId, WriteObject, Ignored);
+	}
+}
+
+void FOnlineAchievementsEOSPlus::QueryAchievements(const FUniqueNetId& PlayerId, const FOnQueryAchievementsCompleteDelegate& Delegate)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		Achievements->QueryAchievements(PlayerId, Delegate);
+	}
+}
+
+void FOnlineAchievementsEOSPlus::QueryAchievementDescriptions(const FUniqueNetId& PlayerId, const FOnQueryAchievementsCompleteDelegate& Delegate)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		Achievements->QueryAchievements(PlayerId, Delegate);
+	}
+}
+
+EOnlineCachedResult::Type FOnlineAchievementsEOSPlus::GetCachedAchievement(const FUniqueNetId& PlayerId, const FString& AchievementId, FOnlineAchievement& OutAchievement)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		return Achievements->GetCachedAchievement(PlayerId, AchievementId, OutAchievement);
+	}
+	return EOnlineCachedResult::NotFound;
+}
+
+EOnlineCachedResult::Type FOnlineAchievementsEOSPlus::GetCachedAchievements(const FUniqueNetId& PlayerId, TArray<FOnlineAchievement>& OutAchievements)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		return Achievements->GetCachedAchievements(PlayerId, OutAchievements);
+	}
+	return EOnlineCachedResult::NotFound;
+}
+
+EOnlineCachedResult::Type FOnlineAchievementsEOSPlus::GetCachedAchievementDescription(const FString& AchievementId, FOnlineAchievementDesc& OutAchievementDesc)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		return Achievements->GetCachedAchievementDescription(AchievementId, OutAchievementDesc);
+	}
+	return EOnlineCachedResult::NotFound;
+}
+
+#if !UE_BUILD_SHIPPING
+bool FOnlineAchievementsEOSPlus::ResetAchievements(const FUniqueNetId& PlayerId)
+{
+	IOnlineAchievementsPtr Achievements = EOSPlus->BaseOSS->GetAchievementsInterface();
+	if (Achievements.IsValid())
+	{
+		return Achievements->ResetAchievements(PlayerId);
+	}
+	return false;
+}
+#endif
