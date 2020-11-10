a4948f22e5a0f7e27a5eb134dad0f053a829bbca
steve smith <steve.smith@epicgames.com>
2020-11-10 12:57:57 -0500

    texture2darrays that are MSAA are valid, so support for mobileVR when we activate MSAA under Multiview. Submitted by Oculus #jira UE-102228 #rb zach.bethel
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: steve.smith
    #ROBOMERGE-SOURCE: CL 14612183 in //UE4/Release-4.26/... via CL 14612188 via CL 14612211
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14704446 by steve smith in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderGraphResources.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderGraphResources.cpp
@@ -116,7 +116,7 @@ bool FRDGTextureDesc::IsValid() const
 		return false;
 	}
 
-	if (Dimension != ETextureDimension::Texture2D && NumSamples > 1)
+	if (NumSamples > 1 && !(Dimension == ETextureDimension::Texture2D || Dimension == ETextureDimension::Texture2DArray))
 	{
 		return false;
 	}
