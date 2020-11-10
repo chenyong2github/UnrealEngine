22c1a35a427c99e80c36f2b5af18dedf28e87118
andrew davidson <andrew.davidson@epicgames.com>
2020-11-10 14:02:16 -0500

    Provide opt-in restriction of client triggered ability execution on server
    #jira UE-80374
    #rb David.Ratti
    [FYI] Fred.Kimberley
    #rnx
    #[robomerge] !ue4
    
    
    #ushell-cherrypick of 8882090 by andrew.davidson
    
    
    Guard against null pointer dereference in InternalServerTryActiveAbility
    
    #ushell-cherrypick of 8972220 by Andrew.Davidson
    
    #lockdown nick.whiting
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: andrew.davidson
    #ROBOMERGE-SOURCE: CL 8882005 in //Fortnite/Release-11.00/... via CL 14652982 via CL 14653027 via CL 14653042
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14706377 by andrew davidson in Dev-Tools-Staging branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/GameplayAbilitiesExec.cpp
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/GameplayAbilitiesExec.cpp
@@ -27,19 +27,39 @@ bool FGameplayAbilitiesExec::Exec(UWorld* Inworld, const TCHAR* Cmd, FOutputDevi
 
 	bool bHandled = false;
 
+	UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
+
 	if (FParse::Command(&Cmd, TEXT("ToggleIgnoreAbilitySystemCooldowns")))
 	{
-		UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
 		AbilitySystemGlobals.ToggleIgnoreAbilitySystemCooldowns();
 		bHandled = true;
 	}
 	else if (FParse::Command(&Cmd, TEXT("ToggleIgnoreAbilitySystemCosts")))
 	{
-		UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
 		AbilitySystemGlobals.ToggleIgnoreAbilitySystemCosts();
 		bHandled = true;
 	}
-
+	else if (FParse::Command(&Cmd, TEXT("ListPlayerAbilities")))
+	{
+		AbilitySystemGlobals.ListPlayerAbilities();
+		bHandled = true;
+	}
+	else if (FParse::Command(&Cmd, TEXT("ServerActivatePlayerAbility")))
+	{
+		AbilitySystemGlobals.ServerActivatePlayerAbility(FParse::Token(Cmd, false));
+		bHandled = true;
+	}
+	else if (FParse::Command(&Cmd, TEXT("ServerEndPlayerAbility")))
+	{
+		AbilitySystemGlobals.ServerEndPlayerAbility(FParse::Token(Cmd, false));
+		bHandled = true;
+	}
+	else if (FParse::Command(&Cmd, TEXT("ServerCancelPlayerAbility")))
+	{
+		AbilitySystemGlobals.ServerCancelPlayerAbility(FParse::Token(Cmd, false));
+		bHandled = true;
+	}
+	
 	return bHandled;
 }
 
