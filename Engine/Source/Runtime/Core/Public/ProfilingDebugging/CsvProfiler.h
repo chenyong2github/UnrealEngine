81327d0aa04261466051316686e0174404549286
mickael gilabert <mickael.gilabert@epicgames.com>
2020-11-10 12:09:13 -0500

    Support for external thread name in csv profiler
    #jira FORT-326010
    #rb jordan.cristiano
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: mickael.gilabert
    #ROBOMERGE-SOURCE: CL 14599023 via CL 14599052 via CL 14599055 via CL 14599259 via CL 14599418 via CL 14599423
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14701824 by mickael gilabert in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Core/Public/ProfilingDebugging/CsvProfiler.h
+++ Engine/Source/Runtime/Core/Public/ProfilingDebugging/CsvProfiler.h
@@ -242,6 +242,9 @@ public:
 	CORE_API static void RecordEventAtTimestamp(int32 CategoryIndex, const FString& EventText, uint64 Cycles64);
 
 	CORE_API static void SetMetadata(const TCHAR* Key, const TCHAR* Value);
+	
+	/** Set Thread name for a TLS. Needs to be called before the first event of that thread is sent. */
+	CORE_API static void SetThreadName(const FString& ThreadName);
 
 	static CORE_API int32 RegisterCategory(const FString& Name, bool bEnableByDefault, bool bIsGlobal);
 
