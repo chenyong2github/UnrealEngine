50825da1028ef134f836981d43fd6ebcb5148d5e
marc audy <Marc.Audy@epicgames.com>
2020-11-10 14:05:04 -0500

    Use Clang 10 by default for Windows #rb Ben.Marsh, Marcus.Wassmer #lockdown Marcus.Wassmer #jira
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 14655010 in //UE4/Release-4.26/... via CL 14655606 via CL 14655630
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14706460 by marc audy in Dev-Tools-Staging branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
@@ -668,7 +668,7 @@ namespace UnrealBuildTool
 		/// <summary>
 		/// The default compiler version to be used, if installed. 
 		/// </summary>
-		static readonly VersionNumber DefaultClangToolChainVersion = VersionNumber.Parse("9.0.0");
+		static readonly VersionNumber DefaultClangToolChainVersion = VersionNumber.Parse("10.0.0");
 
 		/// <summary>
 		/// The compiler toolchains to be used if installed, the first installed in the list will be used.
