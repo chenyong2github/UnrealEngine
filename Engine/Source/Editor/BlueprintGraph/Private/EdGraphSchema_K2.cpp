ea2721d0812f46475ccc8cd6fc749e13edbff54a
michael noland <michael.noland@epicgames.com>
2020-11-10 12:16:58 -0500

    Softened a check to an ensure in UEdGraphSchema_K2::BreakNodeLinks with additional logging to dig into causes #jira UE-97048 #rb marc.audy
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: michael.noland
    #ROBOMERGE-SOURCE: CL 14600548 in //UE4/Release-4.26/... via CL 14600565 via CL 14600575
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14702179 by michael noland in Dev-Tools-Staging branch]

--- Engine/Source/Editor/BlueprintGraph/Private/EdGraphSchema_K2.cpp
+++ Engine/Source/Editor/BlueprintGraph/Private/EdGraphSchema_K2.cpp
@@ -4353,11 +4353,15 @@ bool UEdGraphSchema_K2::ArePinTypesCompatible(const FEdGraphPinType& Output, con
 
 void UEdGraphSchema_K2::BreakNodeLinks(UEdGraphNode& TargetNode) const
 {
-	UBlueprint* Blueprint = FBlueprintEditorUtils::FindBlueprintForNodeChecked(&TargetNode);
+	UBlueprint* Blueprint = FBlueprintEditorUtils::FindBlueprintForNode(&TargetNode);
+	ensureMsgf(Blueprint != nullptr, TEXT("Node %s does not belong to a blueprint!"), *GetFullNameSafe(&TargetNode));
 
 	Super::BreakNodeLinks(TargetNode);
 	
-	FBlueprintEditorUtils::MarkBlueprintAsModified(Blueprint);
+	if (Blueprint != nullptr)
+	{
+		FBlueprintEditorUtils::MarkBlueprintAsModified(Blueprint);
+	}
 }
 
 void UEdGraphSchema_K2::BreakPinLinks(UEdGraphPin& TargetPin, bool bSendsNodeNotifcation) const
