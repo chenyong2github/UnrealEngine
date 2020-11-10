51a505abac65bc1ccf9d5d39c75793bcdfc2ecf6
carlos gonzalez <carlos.gonzalez@epicgames.com>
2020-11-10 12:27:54 -0500

    Remove IrisNormal cvar. #rb zabir.hoque,juan.canada #jira DH-1546
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: carlos.gonzalez
    #ROBOMERGE-SOURCE: CL 14604629 in //UE4/Release-4.26/... via CL 14605848 via CL 14605852
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14702857 by carlos gonzalez in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/RenderCore/Private/Shader.cpp
+++ Engine/Source/Runtime/RenderCore/Private/Shader.cpp
@@ -1328,8 +1328,7 @@ void ShaderMapAppendKeyString(EShaderPlatform Platform, FString& KeyString)
 	}
 
 	{
-		static const auto CVar = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.IrisNormal"));
-		KeyString += (CVar && CVar->GetValueOnAnyThread() != 0) ? TEXT("_Iris") : TEXT("_NoIris");
+		KeyString += TEXT("_Iris"); 
 	}
 
 	{
