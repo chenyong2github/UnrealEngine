edbee7ab0c603a8cfc8e37f5464f4f85a720f9ba
jeremy moore <Jeremy.Moore@epicgames.com>
2020-11-11 09:08:10 -0500

    Fix merge errors introduced in 14702558 #rb none [FYI] marc.audy
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 14718601 via CL 14718602 via CL 14718605
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14719326 by jeremy moore in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialUniformExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialUniformExpressions.cpp
@@ -85,9 +85,6 @@ static void GetTextureParameterValue(const FHashedMaterialParameterInfo& Paramet
 		UMaterialInterface* Interface = Context.Material.GetMaterialInterface();
 		if (!Interface || !Interface->GetTextureParameterDefaultValue(ParameterInfo, Value))
 		{
-		}
-		else
-		{
 			Value = GetIndexedTexture<UTexture>(Context.Material, TextureIndex);
 		}
 
@@ -107,31 +104,6 @@ static void GetTextureParameterValue(const FHashedMaterialParameterInfo& Paramet
 
 		UMaterialInterface* Interface = Context.Material.GetMaterialInterface();
 		if (!Interface || !Interface->GetRuntimeVirtualTextureParameterDefaultValue(ParameterInfo, Value))
-		}
-
-		OutValue = Value;
-	}
-}
-
-static void GetTextureParameterValue(const FHashedMaterialParameterInfo& ParameterInfo, int32 TextureIndex, const FMaterialRenderContext& Context, const URuntimeVirtualTexture*& OutValue)
-{
-	if (ParameterInfo.Name.IsNone())
-	{
-		OutValue = GetIndexedTexture<URuntimeVirtualTexture>(Context.Material, TextureIndex);
-	}
-	else if (!Context.MaterialRenderProxy || !Context.MaterialRenderProxy->GetTextureValue(ParameterInfo, &OutValue, Context))
-	{
-		URuntimeVirtualTexture* Value = nullptr;
-
-		if (AreExperimentalMaterialLayersEnabled())
-		{
-			UMaterialInterface* Interface = Context.Material.GetMaterialInterface();
-			if (!Interface || !Interface->GetRuntimeVirtualTextureParameterDefaultValue(ParameterInfo, Value))
-			{
-				Value = GetIndexedTexture<URuntimeVirtualTexture>(Context.Material, TextureIndex);
-			}
-		}
-		else
 		{
 			Value = GetIndexedTexture<URuntimeVirtualTexture>(Context.Material, TextureIndex);
 		}
