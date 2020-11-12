dcb43c11fa886ea4651ebb60a29801afc8705e24
Mike Zyracki <Mike.Zyracki@epicgames.com>
2020-11-12 17:09:42 -0500

    Control Rig: Add BP setters/getters to get the SkeletalMesh::DefaultAnimatingRig #lockdown cristina.riveron #jira DH-2303 #rb lina.halper
    [CL 14739790 by Mike Zyracki in 4.26 branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/SkeletalMesh.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/SkeletalMesh.h
@@ -751,7 +751,7 @@ public:
 	USkeletalMeshLODSettings* LODSettings;
 
 	/** The Default Control Rig To Animate with when used in Sequnecer. */
-	UPROPERTY(EditAnywhere, Category = AnimationRig, meta = (AllowedClasses = "ControlRigBlueprint"))
+	UPROPERTY(EditAnywhere, Category = AnimationRig, BlueprintGetter = GetDefaultAnimatingRig, BlueprintSetter = SetDefaultAnimatingRig, meta = (AllowedClasses = "ControlRigBlueprint"))
 	TSoftObjectPtr<UObject> DefaultAnimatingRig;
 
 #endif // WITH_EDITORONLY_DATA
@@ -769,6 +769,12 @@ public:
 
 	UFUNCTION(BlueprintSetter)
 	void SetLODSettings(USkeletalMeshLODSettings* InLODSettings);
+
+	UFUNCTION(BlueprintSetter)
+	void SetDefaultAnimatingRig(TSoftObjectPtr<UObject> InAnimatingRig);
+
+	UFUNCTION(BlueprintGetter)
+	TSoftObjectPtr<UObject> GetDefaultAnimatingRig();
 
 	UPROPERTY(EditAnywhere, Category=Mirroring)
 	TEnumAsByte<EAxis::Type> SkelMirrorAxis;
