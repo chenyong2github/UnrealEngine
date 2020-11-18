347a54a45187bd922116e3956f219440433543a8
devin doucette <devin.doucette@epicgames.com>
2020-11-17 21:24:34 -0500

    Added -NeverCookDir option to the cook commandlet
    Requested by kristjanvalur in PR #7480
    
    #rb Matt.Peters
    [RN] minor Core/Cooker
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: devin.doucette
    #ROBOMERGE-SOURCE: CL 14756704 via CL 14756734 via CL 14756747 via CL 14756756
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14769392 by devin doucette in Dev-Tools-Staging branch]

--- Engine/Source/Editor/UnrealEd/Private/Commandlets/CookCommandlet.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Commandlets/CookCommandlet.cpp
@@ -784,6 +784,13 @@ bool UCookCommandlet::CookByTheBook( const TArray<ITargetPlatform*>& Platforms)
 			CmdLineDirEntries.Add(Entry);
 		}
 
+		// Check for -NEVERCOOKDIR=<path to directory> entries
+		for (FString& NeverCookDir : GetSwitchValueElements(TEXT("NEVERCOOKDIR")))
+		{
+			FPaths::NormalizeDirectoryName(NeverCookDir);
+			CmdLineNeverCookDirEntries.Add(MoveTemp(NeverCookDir));
+		}
+
 		// Check for -COOKCULTURES=<culture name> entries
 		CmdLineCultEntries += GetSwitchValueElements(TEXT("COOKCULTURES"));
 	}
