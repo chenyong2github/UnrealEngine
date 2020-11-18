8cff97a0fb623a8eb1660461f87b42babd3efd87
rob krajcarski <Rob.Krajcarski@epicgames.com>
2020-11-18 14:21:58 -0500

    Fix for disabling simulation stages; will no longer generate a shader for the stage #jira UE-101303 [FYI] devon.penney #rb stu.mckenna
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 14776454 via CL 14780687 via CL 14780960
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v760-14685797)
    
    [CL 14780966 by rob krajcarski in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6A112545, 0x03744395, 0xB32F3D30, 0x513A1AA2);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x83E36BC0, 0xD5734D37, 0x981B05B0, 0x349974DA);
