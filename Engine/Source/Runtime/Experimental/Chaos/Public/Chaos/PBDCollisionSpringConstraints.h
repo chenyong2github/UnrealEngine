2035154b1bf67f7f5581a9a52df4a463bf73e588
kriss gossart <Kriss.Gossart@epicgames.com>
2020-11-18 13:07:04 -0500

    Chaos Cloth - Add DebugStep command and self-collision debug draw. Tidy up selfcollision code.
    #rb none
    #jira none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: kriss.gossart
    #ROBOMERGE-SOURCE: CL 14776888 in //UE4/Release-4.26-Chaos/... via CL 14776911 via CL 14777023
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14779155 by kriss gossart in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDCollisionSpringConstraints.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDCollisionSpringConstraints.h
@@ -12,8 +12,8 @@ template<class T, int d>
 class TPBDCollisionSpringConstraints : public TPBDCollisionSpringConstraintsBase<T, d>
 {
 	typedef TPBDCollisionSpringConstraintsBase<T, d> Base;
-	using Base::MBarys;
-	using Base::MConstraints;
+	using Base::Barys;
+	using Base::Constraints;
 
 public:
 	TPBDCollisionSpringConstraints(
@@ -30,39 +30,37 @@ public:
 
 	using Base::Init;
 
-	void Apply(TPBDParticles<T, d>& InParticles, const T Dt, const int32 InConstraintIndex) const
+	void Apply(TPBDParticles<T, d>& Particles, const T Dt, const int32 ConstraintIndex) const
 	{
-		const int32 i = InConstraintIndex;
-		const auto& Constraint = MConstraints[i];
-		int32 i1 = Constraint[0];
-		int32 i2 = Constraint[1];
-		int32 i3 = Constraint[2];
-		int32 i4 = Constraint[3];
-		auto Delta = Base::GetDelta(InParticles, i);
-		// TODO(mlentine): Figure out what the best multipilier here is
-		T Multiplier = 1;
-		if (InParticles.InvM(i1) > 0)
+		const int32 i = ConstraintIndex;
+		const TVector<int32, 4>& Constraint = Constraints[i];
+		const int32 i1 = Constraint[0];
+		const int32 i2 = Constraint[1];
+		const int32 i3 = Constraint[2];
+		const int32 i4 = Constraint[3];
+		const TVector<T, d> Delta = Base::GetDelta(Particles, i);
+		static const T Multiplier = 1;  // TODO(mlentine): Figure out what the best multiplier here is
+		if (Particles.InvM(i1) > 0)
 		{
-			InParticles.P(i1) += Multiplier * InParticles.InvM(i1) * Delta;
+			Particles.P(i1) += Multiplier * Particles.InvM(i1) * Delta;
 		}
-		if (InParticles.InvM(i2))
+		if (Particles.InvM(i2))
 		{
-			InParticles.P(i2) -= Multiplier * InParticles.InvM(i2) * MBarys[i][0] * Delta;
+			Particles.P(i2) -= Multiplier * Particles.InvM(i2) * Barys[i][0] * Delta;
 		}
-		if (InParticles.InvM(i3))
+		if (Particles.InvM(i3))
 		{
-			InParticles.P(i3) -= Multiplier * InParticles.InvM(i3) * MBarys[i][1] * Delta;
+			Particles.P(i3) -= Multiplier * Particles.InvM(i3) * Barys[i][1] * Delta;
 		}
-		if (InParticles.InvM(i4))
+		if (Particles.InvM(i4))
 		{
-			InParticles.P(i4) -= Multiplier * InParticles.InvM(i4) * MBarys[i][2] * Delta;
+			Particles.P(i4) -= Multiplier * Particles.InvM(i4) * Barys[i][2] * Delta;
 		}
 	}
 
-
 	void Apply(TPBDParticles<T, d>& InParticles, const T Dt) const
 	{
-		for (int32 i = 0; i < MConstraints.Num(); ++i)
+		for (int32 i = 0; i < Constraints.Num(); ++i)
 		{
 			Apply(InParticles, Dt, i);
 		}
