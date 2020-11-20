c5a4852acdc201212c472b6dae3c1884b198c528
joe graf <Joe.Graf@epicgames.com>
2020-11-20 13:57:48 -0500

    Added identity interface support for EOSPlus (wip)
    #rb: none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: joe.graf
    #ROBOMERGE-SOURCE: CL 14780314 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14798586 by joe graf in Dev-Tools-Staging branch]

--- Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOSPlus/Private/OnlineUserEOSPlus.cpp
+++ Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOSPlus/Private/OnlineUserEOSPlus.cpp
@@ -5,13 +5,93 @@
 #include "OnlineSubsystemEOSPlus.h"
 #include "EOSSettings.h"
 
+
+FUniqueNetIdEOSPlus::FUniqueNetIdEOSPlus(TSharedPtr<const FUniqueNetId> InBaseUniqueNetId, TSharedPtr<const FUniqueNetId> InEOSUniqueNetId)
+	: BaseUniqueNetId(InBaseUniqueNetId)
+	, EOSUniqueNetId(InEOSUniqueNetId)
+{
+	int32 TotalBytes = GetSize();
+	int32 Offset = 0;
+	RawBytes.Empty(TotalBytes);
+	if (BaseUniqueNetId.IsValid())
+	{
+		int32 BaseSize = BaseUniqueNetId->GetSize();
+		FMemory::Memcpy(RawBytes.GetData(), BaseUniqueNetId->GetBytes(), BaseSize);
+		Offset = BaseSize;
+	}
+	if (EOSUniqueNetId.IsValid())
+	{
+		int32 EOSSize = EOSUniqueNetId->GetSize();
+		FMemory::Memcpy(RawBytes.GetData() + Offset, EOSUniqueNetId->GetBytes(), EOSSize);
+	}
+}
+
+FName FUniqueNetIdEOSPlus::GetType() const
+{
+	return TEXT("EOSPlus");
+}
+
+const uint8* FUniqueNetIdEOSPlus::GetBytes() const
+{
+	return RawBytes.GetData();
+}
+
+int32 FUniqueNetIdEOSPlus::GetSize() const
+{
+	int32 Size = 0;
+	if (BaseUniqueNetId.IsValid())
+	{
+		Size += BaseUniqueNetId->GetSize();
+	}
+	if (EOSUniqueNetId.IsValid())
+	{
+		Size += EOSUniqueNetId->GetSize();
+	}
+	return Size;
+}
+
+bool FUniqueNetIdEOSPlus::IsValid() const
+{
+	return BaseUniqueNetId.IsValid();
+}
+
+FString FUniqueNetIdEOSPlus::ToString() const
+{
+	FString Local;
+	if (BaseUniqueNetId.IsValid())
+	{
+		Local += BaseUniqueNetId->ToString();
+	}
+	if (EOSUniqueNetId.IsValid())
+	{
+		Local += EOSUniqueNetId->ToString();
+	}
+	return Local;
+}
+
+FString FUniqueNetIdEOSPlus::ToDebugString() const
+{
+	FString Local;
+	if (BaseUniqueNetId.IsValid())
+	{
+		Local += BaseUniqueNetId->ToDebugString();
+	}
+	if (EOSUniqueNetId.IsValid())
+	{
+		Local += EOSUniqueNetId->ToDebugString();
+	}
+	return Local;
+}
+
 FOnlineUserEOSPlus::FOnlineUserEOSPlus(FOnlineSubsystemEOSPlus* InSubsystem)
 	: EOSPlus(InSubsystem)
 {
 	BaseUserInterface = EOSPlus->BaseOSS->GetUserInterface();
-	check(BaseUserInterface.IsValid());
+	BaseIdentityInterface = EOSPlus->BaseOSS->GetIdentityInterface();
+	check(BaseUserInterface.IsValid() && BaseIdentityInterface.IsValid());
 	EOSUserInterface = EOSPlus->EosOSS->GetUserInterface();
-	check(EOSUserInterface.IsValid());
+	EOSIdentityInterface = EOSPlus->EosOSS->GetIdentityInterface();
+	check(EOSUserInterface.IsValid() && EOSIdentityInterface.IsValid());
 
 	IntermediateOnQueryUserInfoCompleteDelegateHandle = FOnQueryUserInfoCompleteDelegate::CreateRaw(this, &FOnlineUserEOSPlus::IntermediateOnQueryUserInfoComplete);
 	FinalOnQueryUserInfoCompleteDelegateHandle = FOnQueryUserInfoCompleteDelegate::CreateRaw(this, &FOnlineUserEOSPlus::FinalOnQueryUserInfoComplete);
@@ -25,7 +105,6 @@ FOnlineUserEOSPlus::~FOnlineUserEOSPlus()
 		EOSUserInterface->ClearOnQueryUserInfoCompleteDelegates(LocalUserNum, this);
 	}
 }
-
 
 bool FOnlineUserEOSPlus::QueryUserInfo(int32 LocalUserNum, const TArray<TSharedRef<const FUniqueNetId>>& UserIds)
 {
@@ -140,13 +219,108 @@ void FOnlineUserEOSPlus::GetExternalIdMappings(const FExternalIdQueryOptions& Qu
 	if (GetDefault<UEOSSettings>()->bUseEAS || GetDefault<UEOSSettings>()->bUseEOSConnect)
 	{
 		OutLocalIds.Reset();
-		BaseUserInterface->GetExternalIdMappings(QueryOptions, ExternalIds, OutLocalIds);
+		EOSUserInterface->GetExternalIdMappings(QueryOptions, ExternalIds, OutLocalIds);
 		OutIds += OutLocalIds;
 	}
 }
 
 TSharedPtr<const FUniqueNetId> FOnlineUserEOSPlus::GetExternalIdMapping(const FExternalIdQueryOptions& QueryOptions, const FString& ExternalId)
 {
-	// @todo joeg - should we build a single user here or just use the platform or merge the info as part of the identity interface process?
-	return BaseUserInterface->GetExternalIdMapping(QueryOptions, ExternalId);
+	TSharedPtr<const FUniqueNetId> MappedId = BaseUserInterface->GetExternalIdMapping(QueryOptions, ExternalId);
+
+	if (!MappedId.IsValid() && (GetDefault<UEOSSettings>()->bUseEAS || GetDefault<UEOSSettings>()->bUseEOSConnect))
+	{
+		return EOSUserInterface->GetExternalIdMapping(QueryOptions, ExternalId);
+	}
+
+	return MappedId;
+}
+
+bool FOnlineUserEOSPlus::Login(int32 LocalUserNum, const FOnlineAccountCredentials& AccountCredentials)
+{
+	return BaseIdentityInterface->Login(LocalUserNum, AccountCredentials);
+}
+
+bool FOnlineUserEOSPlus::Logout(int32 LocalUserNum)
+{
+	EOSIdentityInterface->Logout(LocalUserNum);
+	return BaseIdentityInterface->Logout(LocalUserNum);
+}
+
+bool FOnlineUserEOSPlus::AutoLogin(int32 LocalUserNum)
+{
+	return BaseIdentityInterface->AutoLogin(LocalUserNum);
+}
+
+TSharedPtr<FUserOnlineAccount> FOnlineUserEOSPlus::GetUserAccount(const FUniqueNetId& UserId) const
+{
+	//@todo joeg - decide whether we want to wrap both accounts into one or not
+	return BaseIdentityInterface->GetUserAccount(UserId);
+}
+
+TArray<TSharedPtr<FUserOnlineAccount>> FOnlineUserEOSPlus::GetAllUserAccounts() const
+{
+	//@todo joeg - decide whether we want to wrap both accounts into one or not
+	return BaseIdentityInterface->GetAllUserAccounts();
+}
+
+TSharedPtr<const FUniqueNetId> FOnlineUserEOSPlus::GetUniquePlayerId(int32 LocalUserNum) const
+{
+	return BaseIdentityInterface->GetUniquePlayerId(LocalUserNum);
+}
+
+TSharedPtr<const FUniqueNetId> FOnlineUserEOSPlus::CreateUniquePlayerId(uint8* Bytes, int32 Size)
+{
+	return BaseIdentityInterface->CreateUniquePlayerId(Bytes, Size);
+}
+
+TSharedPtr<const FUniqueNetId> FOnlineUserEOSPlus::CreateUniquePlayerId(const FString& Str)
+{
+	return BaseIdentityInterface->CreateUniquePlayerId(Str);
+}
+
+ELoginStatus::Type FOnlineUserEOSPlus::GetLoginStatus(int32 LocalUserNum) const
+{
+	return BaseIdentityInterface->GetLoginStatus(LocalUserNum);
+}
+
+ELoginStatus::Type FOnlineUserEOSPlus::GetLoginStatus(const FUniqueNetId& UserId) const
+{
+	return BaseIdentityInterface->GetLoginStatus(UserId);
+}
+
+FString FOnlineUserEOSPlus::GetPlayerNickname(int32 LocalUserNum) const
+{
+	return BaseIdentityInterface->GetPlayerNickname(LocalUserNum);
+}
+
+FString FOnlineUserEOSPlus::GetPlayerNickname(const FUniqueNetId& UserId) const
+{
+	// Do we wrap this and map or pass through or aggregate and pass through?
+	return BaseIdentityInterface->GetPlayerNickname(UserId);
+}
+
+FString FOnlineUserEOSPlus::GetAuthToken(int32 LocalUserNum) const
+{
+	return BaseIdentityInterface->GetAuthToken(LocalUserNum);
+}
+
+void FOnlineUserEOSPlus::GetUserPrivilege(const FUniqueNetId& UserId, EUserPrivileges::Type Privilege, const FOnGetUserPrivilegeCompleteDelegate& Delegate)
+{
+	BaseIdentityInterface->GetUserPrivilege(UserId, Privilege, Delegate);
+}
+
+FString FOnlineUserEOSPlus::GetAuthType() const
+{
+	return BaseIdentityInterface->GetAuthType();
+}
+
+void FOnlineUserEOSPlus::RevokeAuthToken(const FUniqueNetId& LocalUserId, const FOnRevokeAuthTokenCompleteDelegate& Delegate)
+{
+	BaseIdentityInterface->RevokeAuthToken(LocalUserId, Delegate);
+}
+
+FPlatformUserId FOnlineUserEOSPlus::GetPlatformUserIdFromUniqueNetId(const FUniqueNetId& UniqueNetId) const
+{
+	return BaseIdentityInterface->GetPlatformUserIdFromUniqueNetId(UniqueNetId);
 }
