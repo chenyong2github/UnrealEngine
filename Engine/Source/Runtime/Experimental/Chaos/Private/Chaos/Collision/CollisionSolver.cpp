854241b364be3d9bc97dfa411e16fe8e3aba8eb1
chris caulfield <Chris.Caulfield@epicgames.com>
2020-11-20 14:10:13 -0500

    Chaos - Static Friction improvement
    #rb none
    #jira none
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: chris.caulfield
    #ROBOMERGE-SOURCE: CL 14793495 in //UE4/Release-4.26-Chaos/... via CL 14793523 via CL 14793533
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14799071 by chris caulfield in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionSolver.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/CollisionSolver.cpp
@@ -21,7 +21,6 @@ namespace Chaos
 {
 	namespace Collisions
 	{
-		extern int32 Chaos_Collision_EnergyClampEnabled;
 		extern int32 Chaos_Collision_UseShockPropagation;
 		extern float Chaos_Collision_CollisionClipTolerance;
 
@@ -36,9 +35,6 @@ namespace Chaos
 		FAutoConsoleVariableRef CVarChaos_Manifold_PushOut_PositionCorrection(TEXT("p.Chaos.Collision.Manifold.PushOut.PositionCorrection"), Chaos_Manifold_PushOut_PositionCorrection, TEXT(""));
 		FAutoConsoleVariableRef CVarChaos_Manifold_PushOut_VelocityCorrection(TEXT("p.Chaos.Collision.Manifold.PushOut.VelocityCorrectionMode"), Chaos_Manifold_PushOut_VelocityCorrection, TEXT("0 = No Velocity Correction; 1 = Normal Velocity Correction; 2 = Normal + Tangential Velocity Correction"));
 
-		bool Chaos_Manifold_PushOut_EnableShockPropagationHack = true;
-		FAutoConsoleVariableRef CVarChaos_Manifold_PushOut_EnableShockPropagationHack(TEXT("p.Chaos.Collision.Manifold.PushOut.EnableShockPropagationHack"), Chaos_Manifold_PushOut_EnableShockPropagationHack, TEXT(""));
-
 		float Chaos_Manifold_MinPushOutStiffness = 0.5f;
 		float Chaos_Manifold_MaxPushOutStiffness = 0.5f;
 		FAutoConsoleVariableRef CVarChaos_Manifold_PushOut_MinStiffness(TEXT("p.Chaos.Collision.Manifold.MinPushOutStiffness"), Chaos_Manifold_MinPushOutStiffness, TEXT(""));
@@ -50,16 +46,14 @@ namespace Chaos
 		float Chaos_Manifold_PositionTolerance = 1.e-4f;
 		FAutoConsoleVariableRef CVarChaos_Manifold_PositionTolerance(TEXT("p.Chaos.Collision.Manifold.PositionTolerance"), Chaos_Manifold_PositionTolerance, TEXT(""));
 
-		// Calculate the impulse to drive contact velocity to zero (or positiove for restitution), and handle frction.
-		// The impulse is applied to the input/output velocities.
+
+		// Calculate the impulse to drive normal contact velocity to zero (or positive for restitution), ignoring friction
 		// Some state on the in/out Manifold point is also modified for use in the push out phase.
-		void CalculateManifoldVelocityCorrection(
+		void CalculateManifoldNormalVelocityCorrection(
 			const FReal Stiffness,
 			const FContactIterationParameters& IterationParameters,
 			const FContactParticleParameters& ParticleParameters,
 			const FReal Restitution,
-			const FReal StaticFriction,
-			const FReal DynamicFriction,
 			const FReal InvM0,
 			const FMatrix33& InvI0,
 			const FReal InvM1,
@@ -113,56 +107,20 @@ namespace Chaos
 				(bIsRigidDynamic0 ? ComputeFactorMatrix3(RelativeContactPoint0, InvI0, InvM0) : FMatrix33(0)) +
 				(bIsRigidDynamic1 ? ComputeFactorMatrix3(RelativeContactPoint1, InvI1, InvM1) : FMatrix33(0));
 
-			// Calculate the impulse required to drive contact velocity to zero, including lateral movement, as if we have infinite friction
-			// Impulse = ContactVelocityError / (J.M.Jt)
-			const FVec3 ContactVelocityTarget = ContactVelocityTargetNormal * ContactNormal;
-			const FVec3 ContactVelocityChange = ContactVelocityTarget - ContactVelocity;
-			const FMatrix33 ConstraintMass = ConstraintMassInv.Inverse();
-			FVec3 Impulse = Stiffness * ConstraintMass * ContactVelocityChange;
+			// Calculate the impulse required to drive normal contact velocity to zero
+			const FReal ContactVelocityNormalChange = (ContactVelocityTargetNormal - ContactVelocityNormalLen);
+			const FReal ConstraintMassNormalInv = FVec3::DotProduct(ContactNormal, ConstraintMassInv * ContactNormal);
+			if (ConstraintMassNormalInv < SMALL_NUMBER)
+			{
+				return;
+			}
+			FVec3 Impulse = (Stiffness * ContactVelocityNormalChange / ConstraintMassNormalInv) * ContactNormal;
 
-			// Clip the impulse so that the accumulated impulse is not in the wrong direction and is in the friction cone
-			// Clipping the accumulated impulse instead of the incremental iteration impulse is very important for jitter
+			// Clip the impulse so that the accumulated impulse is not in the wrong direction
 			FVec3 NetImpulse = ManifoldPoint.NetImpulse + Impulse;
 
-			// Normal impulse
-			const FReal NetImpulseNormalLen = FVec3::DotProduct(NetImpulse, ContactNormal);
-
-			// Tangential impulse
-			const FVec3 NetImpulseTangential = NetImpulse - NetImpulseNormalLen * ContactNormal;
-			const FReal NetImpulseTangentialLen = NetImpulseTangential.Size();
-
-			// Check total accumulated impulse against static friction cone
-			// If within static friction cone use the already calculated impulse
-			bool bInsideStaticFrictionCone = true;
-			const FReal MaximumNetImpulseTangential = StaticFriction * NetImpulseNormalLen;
-			if (NetImpulseTangentialLen > FMath::Max(MaximumNetImpulseTangential, KINDA_SMALL_NUMBER))
-			{
-				// Outside static friction cone, solve for normal relative velocity and keep tangent at cone edge
-				// Note: assuming the current accumulated impulse is within the cone, then adding any vector 
-				// also within the cone is guaranteed to still be in the cone. So we don't need to clip the 
-				// accumulated impulse here, only the incremental impulse.
-				bInsideStaticFrictionCone = false;
-
-				// Projecting the impulse, like in the following commented out line, is a simplification that fails with fast sliding contacts.
-				// ClippedNetImpulse = (MaximumImpulseTangential / ImpulseTangentialSize) * ImpulseTangential + NewAccImpNormalSize * ContactNormal;
-				// I.e., reducing the tangential impulse will affect the post-impulse normal velocity, requiring a change in normal impulse, which changes the frcition cone, and so on
-				FVec3 Tangent = NetImpulseTangential / NetImpulseTangentialLen;
-				FReal DirectionalConstraintMassInv = FVec3::DotProduct(ContactNormal, ConstraintMassInv * (ContactNormal + DynamicFriction * Tangent));
-				if (FMath::Abs(DirectionalConstraintMassInv) > SMALL_NUMBER)
-				{
-					FReal RelativeNormalVelocity = FVec3::DotProduct(ContactVelocityChange, ContactNormal);
-					const FReal ImpulseMag = RelativeNormalVelocity / DirectionalConstraintMassInv;
-					Impulse = ImpulseMag * (ContactNormal + DynamicFriction * Tangent);
-				}
-				else
-				{
-					Impulse = FVec3(0);
-				}
-
-				NetImpulse = ManifoldPoint.NetImpulse + Impulse;
-			}
-
 			// Clamp the total impulse to be positive along the normal
+			// Note: if we go inactive we also undo all friction
 			const bool bActive = FVec3::DotProduct(NetImpulse, ContactNormal) > 0.0f;
 			if (!bActive)
 			{
@@ -186,60 +144,116 @@ namespace Chaos
 
 			// PushOut needs to know if we applied restitution and static friction
 			ManifoldPoint.bActive = bActive;
-			ManifoldPoint.bInsideStaticFrictionCone = bActive && bInsideStaticFrictionCone;
-			ManifoldPoint.bRestitutionEnabled = bActive && (bApplyRestitution || ManifoldPoint.bRestitutionEnabled);		// Latches to on-state
+			ManifoldPoint.bInsideStaticFrictionCone = bActive && ManifoldPoint.bInsideStaticFrictionCone;					// Reset when not active
+			ManifoldPoint.bRestitutionEnabled = bActive && (bApplyRestitution || ManifoldPoint.bRestitutionEnabled);		// Latches to on-state when active
 			ManifoldPoint.NetImpulse = NetImpulse;
 
 			// If we applied any additional impulse, we need to go again next iteration
 			const FReal ImpulseTolerance = Chaos_Manifold_ImpulseTolerance * Chaos_Manifold_ImpulseTolerance;
-			*IterationParameters.NeedsAnotherIteration = (Impulse.SizeSquared() > ImpulseTolerance);
+			*IterationParameters.NeedsAnotherIteration |= (Impulse.SizeSquared() > ImpulseTolerance);
 		}
 
-
-		// @todo(chaos): Remove tangential impulses that are counteracting each other...
-		//
-		// Because we solve the copntacts in the manifold one at a time in a loop, the static friction calculation
-		// will end up applying unecessary later impulses at each contact that counteract each other. This can
-		// lead to the friction cone test failing when it should not.
-		//
-		// Explanation:
-		// Think about a box dropping flat onto a plane. No lateral impulses are required, but actually each
-		// contact will end up with a significant lateral impulse. If we think about just the first corner
-		// contact we solve (which does not know there are any others) then the static friction solver will
-		// calculate an impulse that sets the corner velocity to zero, both along the normal and laterally. 
-		// If the impulse we just along the normal, the box would rotate leading to a non-zero lateral
-		// velocity, so the actual impulse has a lateral component to counteract this. Then we move onto
-		// the opposing corner (say) which calculates a similar impulse with a lateral component. The net 
-		// result is a set of impulses whose lateral components cancel out, but could be close to or past the
-		// static friction cone.
-		//
-		// This function is meant to minimize the impulses so that friction cone calculations are more accurate.
-		//
-		// One day it could dp something like this:
-		//	Calculate tangetial axes
-		//	Calculate Net tangential angular impulse about each axis from all manifold points
-		//	Calculate Sum of magnitude of angular impulses about each axis from all manifold points
-		//	Scale tangential angular impulses for all manifold points about each axis so that net and sum match
-		//
-		void FixTangentialImpulses(
-			FRigidBodyPointContactConstraint& Constraint,
-			const TGenericParticleHandle<FReal, 3> Particle0,
-			const TGenericParticleHandle<FReal, 3> Particle1,
+		void CalculateManifoldFrictionVelocityCorrection(
+			const FReal Stiffness,
 			const FContactIterationParameters& IterationParameters,
 			const FContactParticleParameters& ParticleParameters,
-			const bool bIsRigidDynamic0,
-			const bool bIsRigidDynamic1,
+			const FReal StaticFriction,
+			const FReal DynamicFriction,
+			const FReal InvM0,
+			const FMatrix33& InvI0,
+			const FReal InvM1,
+			const FMatrix33& InvI1,
 			const FVec3& P0, // Centre of Mass Positions and Rotations
 			const FRotation3& Q0,
 			const FVec3& P1,
-			const FRotation3& Q1)
+			const FRotation3& Q1,
+			FVec3& V0,
+			FVec3& W0,
+			FVec3& V1,
+			FVec3& W1,
+			FManifoldPoint& ManifoldPoint)
 		{
-			// We only need to do this for dynamic-kinematic pairs (well really any time we have a large mass difference, but this will do for now)
-			if (bIsRigidDynamic0 && bIsRigidDynamic1)
+			// Reject inactive contacts (no normal impulse)
+			if (!ManifoldPoint.bActive || (StaticFriction == 0.0f))
 			{
 				return;
 			}
 
+			const bool bIsRigidDynamic0 = (InvM0 > 0.0f);
+			const bool bIsRigidDynamic1 = (InvM1 > 0.0f);
+
+			// Velocity correction uses the average contact point, and not the deepest point on each body
+			const FVec3 RelativeContactPoint0 = ManifoldPoint.ContactPoint.Location - P0;
+			const FVec3 RelativeContactPoint1 = ManifoldPoint.ContactPoint.Location - P1;
+			const FVec3 ContactNormal = ManifoldPoint.ContactPoint.Normal;
+
+			const FVec3 ContactVelocity0 = V0 + FVec3::CrossProduct(W0, RelativeContactPoint0);
+			const FVec3 ContactVelocity1 = V1 + FVec3::CrossProduct(W1, RelativeContactPoint1);
+			const FVec3 ContactVelocity = ContactVelocity0 - ContactVelocity1;
+			const FReal ContactVelocityNormalLen = FVec3::DotProduct(ContactVelocity, ContactNormal);
+			const FVec3 ContactVelocityTangent = ContactVelocity - ContactVelocityNormalLen * ContactNormal;
+			const FReal ContactVelocityTangentLen = ContactVelocityTangent.Size();
+				
+			// Note: We cannot early-out at low lateral contact velocity because we may have reduced the normal
+			// impulse in a later iteration, which means we may need to reduce the friction impulse to get
+			// it back into the friction cone. However, we can skip the incremental impulse calculation
+			// and go straight to the friction cone clamping (on net impulse).
+			FVec3 Impulse = FVec3(0);
+			const FReal FrictionVelocityThreshold = 1.e-5f;
+			if (ContactVelocityTangentLen > FrictionVelocityThreshold)
+			{
+				const FVec3 ContactTangent = ContactVelocityTangent / ContactVelocityTangentLen;
+
+				// Calculate constraint-space mass
+				const FMatrix33 ConstraintMassInv =
+					(bIsRigidDynamic0 ? ComputeFactorMatrix3(RelativeContactPoint0, InvI0, InvM0) : FMatrix33(0)) +
+					(bIsRigidDynamic1 ? ComputeFactorMatrix3(RelativeContactPoint1, InvI1, InvM1) : FMatrix33(0));
+
+				// Calculate the impulse required to drive contact velocity to zero along the tangent
+				const FReal ContactVelocityTangentChange = -ContactVelocityTangentLen;
+				const FReal ConstraintMassTangentInv = FVec3::DotProduct(ContactTangent, ConstraintMassInv * ContactTangent);
+				const FReal ImpulseTangentLen = (Stiffness * ContactVelocityTangentChange / ConstraintMassTangentInv);
+				Impulse = ImpulseTangentLen * ContactTangent;
+			}
+
+			// Clamp the tangential impulse to the friction cone (looking at net impulse over all iterations)
+			bool bInsideStaticFrictionCone = true;
+			const FReal NetImpulseNormalLen = FVec3::DotProduct(ManifoldPoint.NetImpulse, ContactNormal);
+			const FVec3 NetImpulseTangent = ManifoldPoint.NetImpulse - NetImpulseNormalLen * ContactNormal;
+			const FVec3 NewNetImpulseTangent = NetImpulseTangent + Impulse;
+			const FReal NewNetImpulseTangentLen = NewNetImpulseTangent.Size();
+			const FReal MaxStaticImpulseTangentLen = StaticFriction * NetImpulseNormalLen;
+			if (NewNetImpulseTangentLen > MaxStaticImpulseTangentLen)
+			{
+				const FReal MaxDynamicImpulseTangentLen = DynamicFriction * NetImpulseNormalLen;
+				const FVec3 ClampedNewNetImpulseTangent = (MaxDynamicImpulseTangentLen / NewNetImpulseTangentLen) * NewNetImpulseTangent;
+
+				Impulse = ClampedNewNetImpulseTangent - NetImpulseTangent;
+				bInsideStaticFrictionCone = false;
+			}
+			const FVec3 NetImpulse = ManifoldPoint.NetImpulse + Impulse;
+
+			// Calculate the velocity deltas from the impulse
+			if (bIsRigidDynamic0)
+			{
+				const FVec3 AngularImpulse = FVec3::CrossProduct(RelativeContactPoint0, Impulse);
+				V0 += InvM0 * Impulse;
+				W0 += InvI0 * AngularImpulse;
+			}
+			if (bIsRigidDynamic1)
+			{
+				const FVec3 AngularImpulse = FVec3::CrossProduct(RelativeContactPoint1, Impulse);
+				V1 -= InvM1 * Impulse;
+				W1 -= InvI1 * AngularImpulse;
+			}
+
+			// PushOut needs to know if we applied restitution and static friction
+			ManifoldPoint.bInsideStaticFrictionCone = bInsideStaticFrictionCone;
+			ManifoldPoint.NetImpulse = NetImpulse;
+
+			// If we applied any additional impulse, we need to go again next iteration
+			const FReal ImpulseTolerance = Chaos_Manifold_ImpulseTolerance * Chaos_Manifold_ImpulseTolerance;
+			*IterationParameters.NeedsAnotherIteration |= (Impulse.SizeSquared() > ImpulseTolerance);
 		}
 
 
@@ -363,8 +377,6 @@ namespace Chaos
 
 
 		// Apply a velocity correction to the Manifold point for use in the PushOut phase.
-		// This version is simpler than the main velocity solve - it does not worry
-		// about friction for example.
 		void CalculateManifoldPushOutVelocityCorrection(
 			const FReal Stiffness,
 			const FContactIterationParameters& IterationParameters,
@@ -531,11 +543,27 @@ namespace Chaos
 			{
 				FManifoldPoint& ManifoldPoint = Constraint.SetActiveManifoldPoint(PointIndex, P0, Q0, P1, Q1);
 
-				CalculateManifoldVelocityCorrection(
+				CalculateManifoldNormalVelocityCorrection(
 					Stiffness,
 					IterationParameters,
 					ParticleParameters,
 					Constraint.Manifold.Restitution,
+					InvM0, InvI0, InvM1, InvI1,
+					P0, Q0, P1, Q1,
+					V0, W0, V1, W1,
+					ManifoldPoint);
+
+				Constraint.AccumulatedImpulse += ManifoldPoint.NetImpulse;
+			}
+
+			for (int32 PointIndex = 0; PointIndex < ManifoldPoints.Num(); ++PointIndex)
+			{
+				FManifoldPoint& ManifoldPoint = Constraint.SetActiveManifoldPoint(PointIndex, P0, Q0, P1, Q1);
+
+				CalculateManifoldFrictionVelocityCorrection(
+					Stiffness,
+					IterationParameters,
+					ParticleParameters,
 					FMath::Max(Constraint.Manifold.Friction, Constraint.Manifold.AngularFriction),	// Static
 					Constraint.Manifold.Friction,													// Dynamic
 					InvM0, InvI0, InvM1, InvI1,
@@ -575,17 +603,6 @@ namespace Chaos
 				Q1.Normalize();
 				FParticleUtilities::SetCoMWorldTransform(PBDRigid1, P1, Q1);
 			}
-
-			// Eliminate any tangential impulses that are opposing each other. This helps with static friction
-			FixTangentialImpulses(
-				Constraint,
-				Particle0,
-				Particle1,
-				IterationParameters,
-				ParticleParameters,
-				bIsRigidDynamic0,
-				bIsRigidDynamic1,
-				P0, Q0, P1, Q1);
 		}
 
 
@@ -614,9 +631,9 @@ namespace Chaos
 				// For now, we use the one which is "lower" (in direction of gravity) as the frozen one.
 				if (IsTemporarilyStatic0 && IsTemporarilyStatic1)
 				{
-					if (Chaos_Manifold_PushOut_EnableShockPropagationHack && (ManifoldPoints.Num() > 0))
+					if (ManifoldPoints.Num() > 0)
 					{
-						const FReal NormalThreshold = 0.2f;
+						const FReal NormalThreshold = 0.2f;	// @chaos(todo): what value here?
 						const FReal NormalGrav = FVec3::DotProduct(ManifoldPoints[0].ContactPoint.Normal, GravityDir);
 						const bool bNormalIsUp = NormalGrav < -NormalThreshold;
 						const bool bNormalIsDown = NormalGrav > NormalThreshold;
