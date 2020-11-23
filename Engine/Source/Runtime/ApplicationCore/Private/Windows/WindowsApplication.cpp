487007be094f6af0ac48fd4c4eb9180475e01e2e
vincent gauthier <Vincent.Gauthier@epicgames.com>
2020-11-23 15:10:28 -0500

    Fix to a bug on Windows when using More than one monitor and switching from Windows Mode to Fullscreen When the window is partially outside the virtual screen we could end up switching to fullscreen on one screen and still see a single frame of the game showing on the other screen.
    [REVIEW] patrick.boutot, daren.cheng
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: vincent.gauthier
    #ROBOMERGE-SOURCE: CL 14806914 via CL 14806918 via CL 14806920 via CL 14806923 via CL 14806926
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14807571 by vincent gauthier in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/ApplicationCore/Private/Windows/WindowsApplication.cpp
+++ Engine/Source/Runtime/ApplicationCore/Private/Windows/WindowsApplication.cpp
@@ -521,30 +521,36 @@ void FWindowsApplication::SetHighPrecisionMouseMode( const bool Enable, const TS
 	}
 }
 
-FPlatformRect FWindowsApplication::GetWorkArea( const FPlatformRect& CurrentWindow ) const
+FPlatformRect FWindowsApplication::GetWorkArea(const FPlatformRect& CurrentWindow) const
 {
-	RECT WindowsWindowDim;
-	WindowsWindowDim.left = CurrentWindow.Left;
-	WindowsWindowDim.top = CurrentWindow.Top;
-	WindowsWindowDim.right = CurrentWindow.Right;
-	WindowsWindowDim.bottom = CurrentWindow.Bottom;
+	// This function was changed to address the problem where Windows API MonitorFromRect 
+	// do not return the correct screen if the rect is on secondary screen and more than half 
+	// of it is outside the Virtual screen space.
+	// This implementation will return the screen that has the biggest area covered by the rect specified.
 
-	// ... figure out the best monitor for that window.
-	HMONITOR hBestMonitor = MonitorFromRect( &WindowsWindowDim, MONITOR_DEFAULTTONEAREST );
+	int32 BigestAreaCovered = 0;
+	FPlatformRect WorkArea(0, 0, 0, 0);
 
-	// Get information about that monitor...
-	MONITORINFO MonitorInfo;
-	MonitorInfo.cbSize = sizeof(MonitorInfo);
-	GetMonitorInfo( hBestMonitor, &MonitorInfo);
+	for (const FMonitorInfo& Info : InitialDisplayMetrics.MonitorInfo)
+	{
+		// Default to Primary Screen if we there is no area covered by the screen yet
+		if (BigestAreaCovered == 0 && Info.bIsPrimary)
+		{
+			WorkArea = Info.WorkArea;
+		}
 
-	// ... so that we can figure out the work area (are not covered by taskbar)
+		// Get the intersection rect.
+		FPlatformRect IntersectionRect(FMath::Max(Info.WorkArea.Left, CurrentWindow.Left), FMath::Max(Info.WorkArea.Top, CurrentWindow.Top), FMath::Min(Info.WorkArea.Right, CurrentWindow.Right), FMath::Min(Info.WorkArea.Bottom, CurrentWindow.Bottom));
 
-	FPlatformRect WorkArea;
-	WorkArea.Left = MonitorInfo.rcWork.left;
-	WorkArea.Top = MonitorInfo.rcWork.top;
-	WorkArea.Right = MonitorInfo.rcWork.right;
-	WorkArea.Bottom = MonitorInfo.rcWork.bottom;
+		// If the rectangle is invalid (size <= 0) the area is considered 0. Can happens if rects are not intersecting or badly created.
+		int32 Area = (IntersectionRect.Top < IntersectionRect.Bottom && IntersectionRect.Left < IntersectionRect.Right) ? (IntersectionRect.Right - IntersectionRect.Left) * (IntersectionRect.Bottom - IntersectionRect.Top) : 0;
 
+		if (Area > BigestAreaCovered)
+		{
+			BigestAreaCovered = Area;
+			WorkArea = Info.WorkArea;
+		}
+	}
 	return WorkArea;
 }
 
