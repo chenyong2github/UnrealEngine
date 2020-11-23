0470fa0d1d33236d4b77346f4ffdf8d1b8c45b33
rlabrecque <rlabrecque[at]finalstrikegames.com>
2020-11-23 18:38:01 -0500

    PR #7507: BSDSockets - Add warning when destination protocol does not match protocol (Contributed by rlabrecque)
    #jira UE-103008
    #rb ryan.gerleve
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: alex.koumandarakis
    #ROBOMERGE-SOURCE: CL 14807627 in //UE4/Main/...
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v760-14685797)
    
    [CL 14808258 by alex koumandarakis in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Sockets/Private/BSDSockets/SocketsBSD.cpp
+++ Engine/Source/Runtime/Sockets/Private/BSDSockets/SocketsBSD.cpp
@@ -172,6 +172,8 @@ bool FSocketBSD::SendTo(const uint8* Data, int32 Count, int32& BytesSent, const 
 	// TODO: Consider converting IPv4 addresses to v6 when needed
 	if (Destination.GetProtocolType() != GetProtocol())
 	{
+		UE_LOG(LogSockets, Warning, TEXT("Destination protocol of '%s' does not match protocol: '%s' for address: '%s'"),
+			*Destination.GetProtocolType().ToString(), *GetProtocol().ToString(), *Destination.ToString(true));
 		return false;
 	}
 
