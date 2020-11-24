b670076b10f45aea0d39f6e5517f3ec3f00a4035
Marc Audy <Marc.Audy@epicgames.com>
2020-11-24 19:17:04 -0400

    Copyright fixups #fyi Ben.Marsh
    [CL 14811501 by Marc Audy in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildToolTests/PreprocessorExpressionTests.cs
+++ Engine/Source/Programs/UnrealBuildToolTests/PreprocessorExpressionTests.cs
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
