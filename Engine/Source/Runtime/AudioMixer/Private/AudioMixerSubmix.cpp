b9882b714916ecd18f24de5d6f791e22b488c28a
Aaron McLeran <Aaron.McLeran@epicgames.com>
2020-11-25 17:58:40 -0400

    Fix for broken reverb in surround
    #jira UE-103745
    #rb Phil.popp
    
    [CL 14814103 by Aaron McLeran in ue5-main branch]

--- Engine/Source/Runtime/AudioMixer/Private/AudioMixerSubmix.cpp
+++ Engine/Source/Runtime/AudioMixer/Private/AudioMixerSubmix.cpp
@@ -1314,8 +1314,6 @@ namespace Audio
 				InputData.AudioBuffer = &DownmixedBuffer;
 				SubmixEffect->ProcessAudio(InputData, OutputData);
 
-				// Mix back up to the input channel count when we copy the effect output to the input
-				DownmixBuffer(ChannelCountOverride, DownmixedBuffer, NumChannels, InAudioBuffer);
 			}
 			else
 			{
@@ -1323,10 +1321,10 @@ namespace Audio
 				InputData.NumChannels = NumChannels;
 				InputData.AudioBuffer = &InAudioBuffer;
 				SubmixEffect->ProcessAudio(InputData, OutputData);
-
-				// Copy the output to the input
-				FMemory::Memcpy((void*)InAudioBuffer.GetData(), (void*)OutputData.AudioBuffer->GetData(), sizeof(float) * NumSamples);
 			}
+
+			// Copy the output to the input
+			FMemory::Memcpy((void*)InAudioBuffer.GetData(), (void*)OutputData.AudioBuffer->GetData(), sizeof(float) * NumSamples);
 
 			// Mix in the dry signal directly
 			const float DryLevel = SubmixEffect->GetDryLevel();
