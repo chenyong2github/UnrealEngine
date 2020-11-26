7af66b1f8eef136f06290b7a7e87e00c9eef2f34
ludovic chabant <Ludovic.Chabant@epicgames.com>
2020-11-25 22:53:57 -0500

    Sequencer: Break Matinee converter tool into multiple files.
    This will prevent the main cpp file from doubling in size as we add the camera shake convert tools into the mix.
    
    #rb max.chen
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: ludovic.chabant
    #ROBOMERGE-SOURCE: CL 14810675 via CL 14814255 via CL 14814355 via CL 14814404
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v763-14813663)
    
    [CL 14815080 by ludovic chabant in Dev-Tools-Staging branch]

---
+++ Engine/Plugins/MovieScene/MatineeToLevelSequence/Source/MatineeToLevelSequence/Private/CameraAnimToTemplateSequenceConverter.h
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+class FMatineeConverter;
+class IAssetTools;
+class UCameraAnim;
+class UFactory;
+struct FToolMenuContext;
+
+class FCameraAnimToTemplateSequenceConverter
+{
+public:
+	FCameraAnimToTemplateSequenceConverter(const FMatineeConverter* InMatineeConverter);
+
+	void ConvertCameraAnim(const FToolMenuContext& MenuContext);
+	UObject* ConvertCameraAnim(IAssetTools& AssetTools, UFactory* CameraAnimationSequenceFactoryNew, UCameraAnim* CameraAnim, int32& NumWarnings);
+
+private:
+	UObject* ConvertSingleCameraAnimToTemplateSequence(UCameraAnim* CameraAnimToConvert, IAssetTools& AssetTools, UFactory* CameraAnimationSequenceFactoryNew, bool bPromptCreateAsset, int32& NumWarnings);
+
+private:
+	const FMatineeConverter* MatineeConverter;
+};
+
