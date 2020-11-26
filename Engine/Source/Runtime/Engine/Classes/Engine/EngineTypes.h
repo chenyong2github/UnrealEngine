48a4965b79051d984db8dbf1dc37b6da34a92cba
Sebastien Hillaire <Sebastien.Hillaire@epicgames.com>
2020-11-26 04:55:13 -0400

    Hide strata MSM from UI.
    #rb Charles.deRousiers
    
    [CL 14815511 by Sebastien Hillaire in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
@@ -476,7 +476,7 @@ enum EMaterialShadingModel
 	MSM_Eye						UMETA(DisplayName="Eye"),
 	MSM_SingleLayerWater		UMETA(DisplayName="SingleLayerWater"),
 	MSM_ThinTranslucent			UMETA(DisplayName="Thin Translucent"),
-	MSM_Strata					UMETA(DisplayName="Strata"),
+	MSM_Strata					UMETA(DisplayName="Strata", Hidden),
 	/** Number of unique shading models. */
 	MSM_NUM						UMETA(Hidden),
 	/** Shading model will be determined by the Material Expression Graph,
