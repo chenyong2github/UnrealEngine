c05fe15ad2220fad77405defbc534cf2f80fbc0e
peter knepley <peter.knepley@epicgames.com>
2020-11-30 10:36:32 -0500

    Fix casing in include that breaks linux compile
    #jira
    
    #ROBOMERGE-SOURCE: CL 14820728 via CL 14820729 via CL 14820734
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v763-14813663)
    
    [CL 14820758 by peter knepley in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Particles/ParticlePerfStatsManager.h
+++ Engine/Source/Runtime/Engine/Classes/Particles/ParticlePerfStatsManager.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "Particles/ParticlePerfStats.h"
-#include "ProfilingDebugging/CSVProfiler.h"
+#include "ProfilingDebugging/CsvProfiler.h"
 #include "RenderingThread.h"
 
 #if WITH_PARTICLE_PERF_STATS
