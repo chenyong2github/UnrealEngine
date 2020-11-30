0a7797874addd35c80374e4f4aed4e83907a3f52
Alexis Matte <alexis.matte@epicgames.com>
2020-11-30 11:16:37 -0500

    Fix CIS build for skeletalmesh members deprecation #jira UE-103800, UE-103801 #rb trivial
    [CL 14820958 by Alexis Matte in Main branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
@@ -4335,7 +4335,7 @@ int32 USkeletalMesh::GetMaxNumOptionalLODs(const ITargetPlatform* TargetPlatform
 
 void USkeletalMesh::SetLODSettings(USkeletalMeshLODSettings* InLODSettings)
 {
-PRAGMA_DISABLE_DEPRECATION_WARNINGS
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #if WITH_EDITORONLY_DATA
 	LODSettings = InLODSettings;
 	if (LODSettings)
@@ -4343,6 +4343,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 		LODSettings->SetLODSettingsToMesh(this);
 	}
 #endif // WITH_EDITORONLY_DATA
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
 }
 
 void USkeletalMesh::SetDefaultAnimatingRig(TSoftObjectPtr<UObject> InAnimatingRig)
