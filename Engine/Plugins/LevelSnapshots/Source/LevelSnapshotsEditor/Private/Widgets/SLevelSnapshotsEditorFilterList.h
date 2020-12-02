8bbacae2e182477a00e95d38c99f3d75df67d421
Dominik Peacock <dominik@geodesic.games>
2020-12-02 06:41:42 -0500

    Add missing file
    #rb none
    
    [CL 14834439 by Dominik Peacock in Dev-Tools-Staging branch]

--- Engine/Plugins/LevelSnapshots/Source/LevelSnapshotsEditor/Private/Widgets/SLevelSnapshotsEditorFilterList.h
+++ Engine/Plugins/LevelSnapshots/Source/LevelSnapshotsEditor/Private/Widgets/SLevelSnapshotsEditorFilterList.h
@@ -2,30 +2,35 @@
 
 #pragma once
 
+#include "CoreMinimal.h"
 #include "Widgets/DeclarativeSyntaxSupport.h"
 #include "Widgets/SCompoundWidget.h"
 
-class SWrapBox;
-
 class FLevelSnapshotsEditorFilters;
+class SWrapBox;
+class UConjunctionFilter;
+class UNegatableFilter;
 
+/* Lists a bunch of filters. */
 class SLevelSnapshotsEditorFilterList : public SCompoundWidget
 {
 public:
+	
 	~SLevelSnapshotsEditorFilterList();
 
 	SLATE_BEGIN_ARGS(SLevelSnapshotsEditorFilterList)
 	{}
-
 	SLATE_END_ARGS()
 
-	void Construct(const FArguments& InArgs, const TSharedRef<FLevelSnapshotsEditorFilters>& InFilters);
-
-	void AddFilter(const FName& InName,  ULevelSnapshotFilter* InFilter);
+	void Construct(const FArguments& InArgs, UConjunctionFilter* InManagedAndCondition, const TSharedRef<FLevelSnapshotsEditorFilters>& InEditorFilterModel);
 
 private:
+
+	void AddChild(UNegatableFilter* AddedFilter, TSharedRef<FLevelSnapshotsEditorFilters> InEditorFilterModel) const;
+	
 	/** The horizontal box which contains all the filters */
 	TSharedPtr<SWrapBox> FilterBox;
 
-	TWeakPtr<FLevelSnapshotsEditorFilters> FiltersModelPtr;
+	TWeakObjectPtr<UConjunctionFilter> ManagedAndCondition;
+	FDelegateHandle AddDelegateHandle;
 };
