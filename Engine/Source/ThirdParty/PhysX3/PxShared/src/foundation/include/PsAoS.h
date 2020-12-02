86fae2a75965fb3fd38c1bd596dc58441b12ae5e
David Harvey <David.Harvey@epicgames.com>
2020-12-02 06:01:09 -0500

    Updating some include paths now that some PxShared platform-specific files have moved
    #jira UE-102284
    #rb eric.mcdaniel
    
    [CL 14834363 by David Harvey in Main branch]

--- Engine/Source/ThirdParty/PhysX3/PxShared/src/foundation/include/PsAoS.h
+++ Engine/Source/ThirdParty/PhysX3/PxShared/src/foundation/include/PsAoS.h
@@ -39,7 +39,7 @@
 // @MIXEDREALITY_CHANGE : END 
 #include "unix/PsUnixAoS.h"
 #elif PX_XBOXONE
-#include "XboxOne/PsXboxOneAoS.h"
+#include "PsXboxAoS.h" //@epic: change path
 #else
 #error "Platform not supported!"
 #endif
