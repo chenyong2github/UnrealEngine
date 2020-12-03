b3112d93a24693826eb4ad3a35c3b3c4e8b64850
devin doucette <devin.doucette@epicgames.com>
2020-12-03 13:26:23 -0500

    IpNetDriver: Fixed InitConnect failing during garbage collection
    #rb LouisPhilippe.Seguin
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: devin.doucette
    #ROBOMERGE-SOURCE: CL 14820636 via CL 14820638 via CL 14820639 via CL 14820673
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v763-14813663)
    
    [CL 14846488 by devin doucette in Dev-Tools-Staging branch]

--- Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineSubsystemUtils/Private/IpNetDriver.cpp
+++ Engine/Plugins/Online/OnlineSubsystemUtils/Source/OnlineSubsystemUtils/Private/IpNetDriver.cpp
@@ -1063,7 +1063,9 @@ bool UIpNetDriver::InitConnect( FNetworkNotify* InNotify, const FURL& ConnectURL
 		// Create a weakobj so that we can pass the Connection safely to the lambda for later
 		TWeakObjectPtr<UIpConnection> SafeConnectionPtr(IPConnection);
 
-		auto AsyncResolverHandler = [SafeConnectionPtr, SocketSubsystem, DestinationPort](FAddressInfoResult Results) {
+		auto AsyncResolverHandler = [SafeConnectionPtr, SocketSubsystem, DestinationPort](FAddressInfoResult Results)
+		{
+			FGCScopeGuard Guard;
 
 			// Check if we still have a valid pointer
 			if (!SafeConnectionPtr.IsValid())
