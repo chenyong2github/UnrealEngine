feac3cdea3f3f0e9ced8a0f16fd493169a270d33
emil kirichev <Emil.Kirichev@xa.epicgames.com>
2020-12-04 13:46:12 -0500

    Dataprep: Add Plane Cut operator
    #rb JeanLuc.Corenthin
    #jira UEENT-3676
    
    [CL 14856905 by emil kirichev in Dev-Tools-Staging branch]

--- Engine/Plugins/Experimental/Enterprise/DataprepGeometryOperations/Source/Private/DataprepGeometryOperations.h
+++ Engine/Plugins/Experimental/Enterprise/DataprepGeometryOperations/Source/Private/DataprepGeometryOperations.h
@@ -9,9 +9,10 @@
 #include "Properties/RemeshProperties.h"
 #include "CleaningOps/RemeshMeshOp.h"
 #include "CleaningOps/SimplifyMeshOp.h"
+#include "DynamicMesh3.h"
+#include "ModelingOperators.h"
 #include "BakeTransformTool.h"
 
-//
 #include "DataprepGeometryOperations.generated.h"
 
 class AStaticMeshActor;
@@ -19,7 +20,6 @@ class IMeshBuilderModule;
 class UStaticMesh;
 class UStaticMeshComponent;
 class UWorld;
-
 
 UCLASS(Experimental, Category = ObjectOperation, Meta = (DisplayName="Remesh", ToolTip = "Experimental - Remesh input meshes") )
 class UDataprepRemeshOperation : public UDataprepEditingOperation
@@ -167,4 +167,82 @@ public:
 protected:
 	virtual void OnExecution_Implementation(const FDataprepContext& InContext) override;
 	//~ End UDataprepOperation Interface
+};
+
+UENUM()
+enum class EPlaneCutKeepSide : uint8
+{
+	Positive,
+	Negative,
+	Both
+};
+
+UCLASS(Experimental, Category = MeshOperation, Meta = (DisplayName="Plane Cut", ToolTip = "Experimental - Plane cut input meshes") )
+class UDataprepPlaneCutOperation : public UDataprepEditingOperation
+{
+	GENERATED_BODY()
+
+public:
+
+	UDataprepPlaneCutOperation()
+	{
+		CutPlaneOrigin = FVector(0, 0, 0);
+		CutPlaneKeepSide = EPlaneCutKeepSide::Positive;
+		CutPlaneNormalAngles = FVector(0.0f, 0.0f, 0.0f);
+		SpacingBetweenHalves = 1.0f;
+		bFillCutHole = false;
+	}
+
+	/** Origin of the cutting plane */
+	UPROPERTY(EditAnywhere, Category = PlaneCut, meta = (DisplayName = "Plane's Origin"))
+	FVector CutPlaneOrigin;
+
+	/** Euler angles of the normal to the cutting plane */
+	UPROPERTY(EditAnywhere, Category = PlaneCut, meta = (DisplayName = "Plane's Orientation"))
+	FVector CutPlaneNormalAngles;
+
+	/** Specify which section(s) of the cut to keep. If 'Keep Both' is selected, both sides of the 
+		cut are computed and a new actor added with the negative side. 
+	*/
+	UPROPERTY(EditAnywhere, Category = PlaneCut, meta = (DisplayName = "Side(s) To Keep"))
+	EPlaneCutKeepSide CutPlaneKeepSide;
+
+	/** If keeping both halves, separate the two pieces by this amount */
+	UPROPERTY(EditAnywhere, Category = PlaneCut, meta = (EditCondition = "CutPlaneKeepSide == EPlaneCutKeepSide::Both", UIMin = "0", ClampMin = "0"))
+	float SpacingBetweenHalves;
+
+	/** If true, the cut surface is filled with simple planar hole fill surface(s) */
+	UPROPERTY(EditAnywhere, Category = PlaneCut, meta = (DisplayName = "Fill Holes"))
+	bool bFillCutHole;
+
+	//~ Begin UDataprepOperation Interface
+public:
+	virtual FText GetCategory_Implementation() const override
+	{
+		return FDataprepOperationCategories::MeshOperation;
+	}
+
+protected:
+	virtual void OnExecution_Implementation(const FDataprepContext& InContext) override;
+	//~ End UDataprepOperation Interface
+
+private:
+	/**
+	 * Create new mesh component and assign new static mesh to it, and then add it to the Actor.
+	 * If Actor is a AStaticMeshActor instance, then the new mesh component will be it's existing StaticMeshComponent, and we will assign the new static mesh to it.
+	 * Otherwise, we create new StaticMeshComponent, assign NewStaticMesh, and set it as the Root Component of the Actor
+	 */
+	UStaticMeshComponent* FinalizeStaticMeshActor(
+		AStaticMeshActor* InActor, 
+		const FString& InMeshName,
+		const FMeshDescription* InMeshDescription,
+		int InNumMaterialSlots,
+		const UStaticMesh* InOriginalMesh);
+
+	// Borrowed from UPlaneCutOperatorFactory::MakeNewOperator
+	TUniquePtr<FDynamicMeshOperator> MakeNewOperator(
+		const UStaticMeshComponent* InStaticMeshComponent, 
+		TSharedPtr<FDynamicMesh3> InOriginalMesh,
+		float InMeshUVScaleFactor,
+		int InAttribIndex);
 };
