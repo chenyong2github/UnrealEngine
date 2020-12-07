3be03b5cb989447403dc4146b39d0e3b90ebe2aa
jens petersam <Jens.Petersam@epicgames.com>
2020-12-07 05:06:18 -0500

    MediaUtils: added missing media events to EventToString()
    #rb none
    #rnx
    
    #ROBOMERGE-SOURCE: CL 14868862 via CL 14868868 via CL 14868869 via CL 14868870 via CL 14868872
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v763-14813663)
    
    [CL 14868874 by jens petersam in Main branch]

--- Engine/Source/Runtime/MediaUtils/Private/MediaHelpers.cpp
+++ Engine/Source/Runtime/MediaUtils/Private/MediaHelpers.cpp
@@ -25,6 +25,9 @@ namespace MediaUtils
 		case EMediaEvent::TracksChanged: return TEXT("TracksChanged");
 
 		case EMediaEvent::Internal_PurgeVideoSamplesHint: return TEXT("Internal: PurgeVideoSamplesHint");
+		case EMediaEvent::Internal_ResetForDiscontinuity: return TEXT("Internal: ResetForDiscontinuity");
+		case EMediaEvent::Internal_RenderClockStart: return TEXT("Internal: RenderClockStart");
+		case EMediaEvent::Internal_RenderClockStop: return TEXT("Internal: RenderClockStop");
 		case EMediaEvent::Internal_VideoSamplesAvailable: return TEXT("Internal: VideoSamplesAvailable");
 		case EMediaEvent::Internal_VideoSamplesUnavailable: return TEXT("Internal: VideoSamplesUnavailable");
 		case EMediaEvent::Internal_AudioSamplesAvailable: return TEXT("Internal: AudioSamplesAvailable");
