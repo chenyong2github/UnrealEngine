9357dd43e53250ba963d4bcea8933c53ae911f21
jamie hayes <jamie.hayes@epicgames.com>
2020-12-07 13:48:56 -0500

    Fix deprecation warnings
    #rb none
    #jira UE-104145, UE-104148
    
    [CL 14871979 by jamie hayes in Main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Private/NiagaraDataInterfaceWater.cpp
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Private/NiagaraDataInterfaceWater.cpp
@@ -21,7 +21,8 @@ void UNiagaraDataInterfaceWater::PostInitProperties()
 
 	if (HasAnyFlags(RF_ClassDefaultObject))
 	{
-		FNiagaraTypeRegistry::Register(FNiagaraTypeDefinition(GetClass()), true, false, false);
+		ENiagaraTypeRegistryFlags Flags = ENiagaraTypeRegistryFlags::AllowAnyVariable | ENiagaraTypeRegistryFlags::AllowParameter;
+		FNiagaraTypeRegistry::Register(FNiagaraTypeDefinition(GetClass()), Flags);
 	}
 }
 
