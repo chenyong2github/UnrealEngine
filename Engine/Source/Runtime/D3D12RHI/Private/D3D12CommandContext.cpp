a030c303cafe902cd11ae8da2d24e84490075de1
zach bethel <zach.bethel@epicgames.com>
2020-12-08 19:07:55 -0400

    Fix for crash in D3D12 with static uniform buffers not being cleared properly.
    #rb none
    
    [CL 14884208 by zach bethel in ue5-main branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12CommandContext.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12CommandContext.cpp
@@ -501,6 +501,7 @@ void FD3D12CommandContext::ClearState()
 
 	FMemory::Memzero(BoundUniformBuffers, sizeof(BoundUniformBuffers));
 	FMemory::Memzero(DirtyUniformBuffers, sizeof(DirtyUniformBuffers));
+	FMemory::Memzero(GlobalUniformBuffers.GetData(), GlobalUniformBuffers.Num() * sizeof(FRHIUniformBuffer*));
 
 	for (int i = 0; i < UE_ARRAY_COUNT(BoundUniformBufferRefs); i++)
 	{
