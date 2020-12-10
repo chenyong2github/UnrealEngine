62229a7ea834763cce134ede7e6e6619e55024d7
steve smith <steve.smith@epicgames.com>
2020-12-10 16:56:33 -0500

    Fix CIS warning for uninitialized XrResult in OpenXR plugin. Also, proactive fix the errant follow-up warning from main. (This change copied from same fix I applied in main recently). #jira UE-103953 #rb trivial #lockdown marcus.wassmer
    [CL 14902253 by steve smith in 4.26 branch]

--- Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRHMD_Swapchain.cpp
+++ Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRHMD_Swapchain.cpp
@@ -32,18 +32,18 @@ void FOpenXRSwapchain::IncrementSwapChainIndex_RHIThread(int64 Timeout)
 	WaitInfo.next = nullptr;
 	WaitInfo.timeout = Timeout;
 
-	XrResult WaitResult;
+	XrResult WaitResult = XR_SUCCESS;
 	int RetryCount = 3;
 	do
 	{
 		XR_ENSURE(WaitResult = xrWaitSwapchainImage(Handle, &WaitInfo));
-		if (WaitResult == XR_TIMEOUT_EXPIRED)
+		if (WaitResult == XR_TIMEOUT_EXPIRED)	//-V547
 		{
 			UE_LOG(LogHMD, Warning, TEXT("Timed out waiting on swapchain image %u! Attempts remaining %d."), SwapChainIndex_RHIThread, RetryCount);
 		}
 	} while (WaitResult == XR_TIMEOUT_EXPIRED && RetryCount-- > 0);
 
-	if (WaitResult != XR_SUCCESS)
+	if (WaitResult != XR_SUCCESS) //-V547
 	{
 		// We can't continue without acquiring a new swapchain image since we won't have an image available to render to.
 		UE_LOG(LogHMD, Fatal, TEXT("Failed to wait on acquired swapchain image. This usually indicates a problem with the OpenXR runtime."));
