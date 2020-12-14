34c0e08f7e9340fe777645bd972637375390e125
christina tempelaarl <christina.tempelaarl@epicgames.com>
2020-12-14 16:48:00 -0500

    Exposed ReplaceSelectedActors function to Python. #jira UE-100062 #rb matt.kuhlenschmidt
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: christina.tempelaarl
    #ROBOMERGE-SOURCE: CL 14891367 via CL 14891370 via CL 14891377 via CL 14891393
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v766-14819879)
    
    [CL 14922645 by christina tempelaarl in Dev-Tools-Staging branch]

--- Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Public/EditorLevelLibrary.h
+++ Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Public/EditorLevelLibrary.h
@@ -130,6 +130,13 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Level Utility")
 	static void EditorInvalidateViewports();
 
+	/**
+	 * Replaces the selected Actors with the same number of a different kind of Actor using the specified factory to spawn the new Actors
+	 * note that only Location, Rotation, Drawscale, Drawscale3D, Tag, and Group are copied from the old Actors
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Level Utility")
+	static void ReplaceSelectedActors(const FString& InAssetPath);
+
 #endif
 
 	/**
