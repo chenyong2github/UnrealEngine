9abfa89e669b6b2afcb61c8d36ec64b931c7b964
pj kack <PJ.Kack@epicgames.com>
2020-12-14 17:30:30 -0500

    AsyncLoading2: Improve handling of level streaming temp packages (follow up fix for 14820320 and 14885218) Handle all package GC ref counting in the same way (which e.g. prevents GC of the upackage between CreateAsyncPackage and ProcessPackageSummary). In the async loading loop, interleave processing of event queues and deferred deletes in batches of one hundred to balance performance vs. memory. Make sure to process any remaining deferred deletes in PreGarbageCollect to make sure to release all objects that can be destroyed (this queue should always be empty or small at this point). Support level streaming requirements (see FLevelStreamingGCHelper) of being able to unload and garbage collect a level immediately after its been fully loaded on the GT.
    #rb per.larsson
    #rnx
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: pj.kack
    #ROBOMERGE-SOURCE: CL 14905146 via CL 14905147 via CL 14905148 via CL 14905151 via CL 14905158 via CL 14905159
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v766-14819879)
    
    [CL 14923248 by pj kack in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
@@ -1390,10 +1390,7 @@ private:
 		PackageRef.ClearErrorFlags();
 		if (PackageRef.AddRef())
 		{
-			if (Desc.bCanBeImported)
-			{
-				AddAsyncFlags(PackageRef.GetPackage());
-			}
+			AddAsyncFlags(PackageRef.GetPackage());
 		}
 	}
 
@@ -1414,10 +1411,7 @@ private:
 			*Desc.UPackageName.ToString(), Desc.UPackageId.Value());
 		if (PackageRef && PackageRef->ReleaseRef(Desc.UPackageId, Desc.UPackageId))
 		{
-			if (Desc.bCanBeImported)
-			{
-				ClearAsyncFlags(PackageRef->GetPackage());
-			}
+			ClearAsyncFlags(PackageRef->GetPackage());
 		}
 	}
 };
@@ -2388,6 +2382,8 @@ public:
 	virtual void NotifyUnreachableObjects(const TArrayView<FUObjectItem*>& UnreachableObjects) override;
 
 	virtual void FireCompletedCompiledInImport(void* AsyncPackage, FPackageIndex Import) override {}
+
+	void OnPreGarbageCollect();
 
 	/**
 	* [ASYNC THREAD] Finds an existing async package in the AsyncPackages by its name.
@@ -4756,7 +4752,6 @@ FAsyncLoadingThread2::~FAsyncLoadingThread2()
 void FAsyncLoadingThread2::ShutdownLoading()
 {
 	FCoreUObjectDelegates::GetPreGarbageCollectDelegate().RemoveAll(this);
-	FCoreUObjectDelegates::GetPostGarbageCollect().RemoveAll(this);
 
 	delete Thread;
 	Thread = nullptr;
@@ -4772,6 +4767,8 @@ void FAsyncLoadingThread2::StartThread()
 {
 	// Make sure the GC sync object is created before we start the thread (apparently this can happen before we call InitUObject())
 	FGCCSyncObject::Create();
+
+	FCoreUObjectDelegates::GetPreGarbageCollectDelegate().AddRaw(this, &FAsyncLoadingThread2::OnPreGarbageCollect);
 
 	if (!FAsyncLoadingThreadSettings::Get().bAsyncLoadingThreadEnabled)
 	{
@@ -4851,6 +4848,18 @@ void FAsyncLoadingThread2::FinalizeInitialLoad()
 	PendingCDOs.Empty();
 }
 
+void FAsyncLoadingThread2::OnPreGarbageCollect()
+{
+	if (!DeferredDeletePackages.IsEmpty())
+	{
+		FAsyncPackage2* Package = nullptr;
+		while (DeferredDeletePackages.Dequeue(Package))
+		{
+			DeleteAsyncPackage(Package);
+		}
+	}
+}
+
 uint32 FAsyncLoadingThread2::Run()
 {
 	LLM_SCOPE(ELLMTag::AsyncLoading);
@@ -4904,18 +4913,36 @@ uint32 FAsyncLoadingThread2::Run()
 					}
 
 					bool bShouldSuspend = false;
-					bool bPopped = false;
-					do 
+					bool bPopped = true;
+					while (bPopped)
 					{
-						bPopped = false;
-						for (FAsyncLoadEventQueue2* Queue : AltEventQueues)
+						for (int32 I = 0; bPopped && I < 100; ++I)
 						{
-							if (Queue->PopAndExecute(ThreadState))
+							bPopped = false;
+							for (FAsyncLoadEventQueue2* Queue : AltEventQueues)
 							{
-								bPopped = true;
-								bDidSomething = true;
-							}
+								if (Queue->PopAndExecute(ThreadState))
+								{
+									bPopped = true;
+									bDidSomething = true;
+								}
 
+								if (bSuspendRequested.Load(EMemoryOrder::Relaxed) || IsGarbageCollectionWaiting())
+								{
+									bShouldSuspend = true;
+									bPopped = false;
+									break;
+								}
+							}
+						}
+						if (!bShouldSuspend && !DeferredDeletePackages.IsEmpty())
+						{
+							FAsyncPackage2* Package = nullptr;
+							for (int32 I = 0; I < 100 && DeferredDeletePackages.Dequeue(Package); ++I) 
+							{
+								DeleteAsyncPackage(Package);
+							}
+							bDidSomething = true;
 							if (bSuspendRequested.Load(EMemoryOrder::Relaxed) || IsGarbageCollectionWaiting())
 							{
 								bShouldSuspend = true;
@@ -4923,7 +4950,7 @@ uint32 FAsyncLoadingThread2::Run()
 								break;
 							}
 						}
-					} while (bPopped);
+					}
 
 					if (bShouldSuspend || bSuspendRequested.Load(EMemoryOrder::Relaxed) || IsGarbageCollectionWaiting())
 					{
@@ -4966,19 +4993,6 @@ uint32 FAsyncLoadingThread2::Run()
 				{
 					TRACE_CPUPROFILER_EVENT_SCOPE(AsyncLoadingTime);
 					ThreadState.ProcessDeferredFrees();
-					bDidSomething = true;
-				}
-
-				if (!DeferredDeletePackages.IsEmpty())
-				{
-					FGCScopeGuard GCGuard;
-					TRACE_CPUPROFILER_EVENT_SCOPE(AsyncLoadingTime);
-					FAsyncPackage2* Package = nullptr;
-					int32 Count = 0;
-					while (++Count <= 100 && DeferredDeletePackages.Dequeue(Package))
-					{
-						DeleteAsyncPackage(Package);
-					}
 					bDidSomething = true;
 				}
 			}
@@ -5385,8 +5399,8 @@ void FAsyncPackage2::ClearConstructedObjects()
 	}
 	ConstructedObjects.Empty();
 
-	// the async flag of all GC'able public export objects in non-temp packages are handled by FGlobalImportStore::ClearAsyncFlags
-	const bool bShouldClearAsyncFlagForPublicExports = GUObjectArray.IsDisregardForGC(LinkerRoot) || !Desc.bCanBeImported;
+	// the async flag of all GC'able public export objects are handled by FGlobalImportStore::ClearAsyncFlags
+	const bool bShouldClearAsyncFlagForPublicExports = GUObjectArray.IsDisregardForGC(LinkerRoot);
 
 	for (FExportObject& Export : Data.Exports)
 	{
