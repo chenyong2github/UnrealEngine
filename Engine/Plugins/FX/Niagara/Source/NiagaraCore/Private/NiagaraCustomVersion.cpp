4a754c2244c9a1af03cf34b63b94780e9e3ffb0e
stu mckenna <Stu.McKenna@epicgames.com>
2020-12-14 16:48:54 -0500

    - Fix for picky shader compilers around clamp usage
    #rb jamie.hayes
    #jira UE-104219
    #rnx
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 14892210 via CL 14892218 via CL 14892223 via CL 14892315
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v766-14819879)
    
    [CL 14922668 by stu mckenna in Dev-Tools-Staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x18EBD69B, 0x05834794, 0xAC6CEE26, 0xB441BBBB);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x94C1834F, 0xAA775C48, 0xAB58592A, 0x7F42F263);
