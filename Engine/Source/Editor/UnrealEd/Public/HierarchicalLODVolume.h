58fd54f4282a945e09a82cd341e20c1f0ab48324
Sebastien Lussier <Sebastien.Lussier@epicgames.com>
2020-12-16 14:14:33 -0500

    HierarchicalLODVolume minor fixes/improvements * Now an editor only actor * Fully disabled all collisions * Unneeded property categories are now hidden from the Details panel * Not relevant for level bounds anymore #rb ryan.bickell
    [CL 14941100 by Sebastien Lussier in Main branch]

--- Engine/Source/Editor/UnrealEd/Public/HierarchicalLODVolume.h
+++ Engine/Source/Editor/UnrealEd/Public/HierarchicalLODVolume.h
@@ -7,11 +7,16 @@
 #include "GameFramework/Volume.h"
 #include "HierarchicalLODVolume.generated.h"
 
-/** An invisible volume used to manually define/create a HLOD cluster. */
-UCLASS(MinimalAPI)
+/** An invisible volume used to manually define/create an HLOD cluster. */
+UCLASS(HideCategories = (Actor, Collision, Cooking, Input, LOD, Physics, Replication, Rendering), MinimalAPI)
 class AHierarchicalLODVolume : public AVolume
 {
 	GENERATED_UCLASS_BODY()
+
+	virtual bool IsEditorOnly() const override { return true; }
+	virtual bool NeedsLoadForClient() const override { return false; }
+	virtual bool NeedsLoadForServer() const override { return false; }
+	virtual bool IsLevelBoundsRelevant() const override { return false; }
 
 public:
 	/** When set this volume will incorporate actors which bounds overlap with the volume, otherwise only actors which are completely inside of the volume are incorporated */
