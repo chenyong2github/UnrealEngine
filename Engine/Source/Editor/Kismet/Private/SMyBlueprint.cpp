3475c7166ec07d0fb6483f763d423cf6938c8a11
projectgheist <projectgheist@gmail.com>
2020-12-17 12:09:52 -0400

    PR #7572: UE-103069: Use GetDisplayNameText instead of PropertyName directly in the My Blueprint Panel (Contributed by projectgheist)
    #jira UE-104134
    #rb me
    
    [CL 14947506 by projectgheist in ue5-main branch]

--- Engine/Source/Editor/Kismet/Private/SMyBlueprint.cpp
+++ Engine/Source/Editor/Kismet/Private/SMyBlueprint.cpp
@@ -1223,7 +1223,7 @@ void SMyBlueprint::CollectAllActions(FGraphActionListBuilderBase& OutAllActions)
 
 		const FText PropertyTooltip = Property->GetToolTipText();
 		const FName PropertyName = Property->GetFName();
-		const FText PropertyDesc = FText::FromName(PropertyName);
+		const FText PropertyDesc = Property->IsNative() ? Property->GetDisplayNameText() : FText::FromName(PropertyName);
 
 		FText CategoryName = FObjectEditorUtils::GetCategoryText(Property);
 		FText PropertyCategory = FObjectEditorUtils::GetCategoryText(Property);
