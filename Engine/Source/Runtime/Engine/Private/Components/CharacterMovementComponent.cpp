f05db484fd4085c18878bda905a68fac066a1a93
jordan cristiano <Jordan.Cristiano@epicgames.com>
2021-01-05 17:47:17 -0500

    Optimization to CharFindFloor that skips a second floor check after landing from a jump if you're a simulated proxy.
    #rb zak.middleton
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jordan.cristiano
    #ROBOMERGE-SOURCE: CL 14934017 via CL 14934019 via CL 14934021 via CL 14934026 via CL 14934032 via CL 14971993 via CL 14972042 via CL 14972072
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v766-14819879)
    
    [CL 14996096 by jordan cristiano in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
@@ -5472,7 +5472,10 @@ void UCharacterMovementComponent::AdjustFloorHeight()
 		bJustTeleported |= !bMaintainHorizontalGroundVelocity || (OldFloorDist < 0.f);
 		
 		// If something caused us to adjust our height (especially a depentration) we should ensure another check next frame or we will keep a stale result.
-		bForceNextFloorCheck = true;
+		if (CharacterOwner && CharacterOwner->GetLocalRole() != ROLE_SimulatedProxy)
+		{
+			bForceNextFloorCheck = true;
+		}
 	}
 }
 
