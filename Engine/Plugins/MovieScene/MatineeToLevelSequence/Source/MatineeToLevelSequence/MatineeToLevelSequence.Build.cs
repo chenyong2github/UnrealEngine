2e5e8f587ea65a7aa0143da45cb28aed7e0ff96b
ludovic chabant <Ludovic.Chabant@epicgames.com>
2021-01-08 18:47:46 -0500

    Sequencer: Camera shake upgrader tool
    This tool upgrades any legacy Matinee camera shake asset so that its Matinee animation is converted to a CameraAnimationSequence.
    
    #rb max.chen
    #jira UE-105295
    
    #ROBOMERGE-SOURCE: CL 15027902 via CL 15028086 via CL 15028118
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v766-14819879)
    
    [CL 15028149 by ludovic chabant in Main branch]

--- Engine/Plugins/MovieScene/MatineeToLevelSequence/Source/MatineeToLevelSequence/MatineeToLevelSequence.Build.cs
+++ Engine/Plugins/MovieScene/MatineeToLevelSequence/Source/MatineeToLevelSequence/MatineeToLevelSequence.Build.cs
@@ -6,45 +6,51 @@ public class MatineeToLevelSequence : ModuleRules
 {
 	public MatineeToLevelSequence(ReadOnlyTargetRules Target) : base(Target)
 	{
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[] {
-				"AssetTools",
-			}
-        );
-        
-        PrivateDependencyModuleNames.AddRange(
+		DynamicallyLoadedModuleNames.AddRange(
 			new string[] {
-                "LevelSequence",
-                "ContentBrowser",
-				"Core",
-				"CoreUObject",
-                "EditorStyle",
-                "Engine",
-				"LevelEditor",
-				"MovieScene",
-                "MovieSceneTools",
-				"MovieSceneTracks",
-                "Slate",
-                "SlateCore",
-                "ToolMenus",
-                "UnrealEd",
-				"TimeManagement",
-                "TemplateSequence",
-				"Analytics"
-            }
-        );
-
-		PrivateIncludePathModuleNames.AddRange(
-			new string[] {
-				"AssetTools",
-                "MovieSceneTools",
-                "Settings",
+			"AssetTools",
 			}
 		);
 
-        PrivateIncludePaths.AddRange(
-            new string[] {
-            }
-        );
-    }
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+			"LevelSequence",
+			"ContentBrowser",
+			"Core",
+			"CoreUObject",
+			"EditorStyle",
+			"Engine",
+			"BlueprintGraph",
+			"GameplayCameras",
+			"InputCore",
+			"Kismet",
+			"LevelEditor",
+			"MovieScene",
+			"MovieSceneTools",
+			"MovieSceneTracks",
+			"Slate",
+			"SlateCore",
+			"ToolMenus",
+			"UnrealEd",
+			"TimeManagement",
+			"TemplateSequence",
+			"Analytics",
+			"AssetRegistry"
+			}
+		);
+
+		PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+			"AssetTools",
+			"MovieSceneTools",
+			"Settings",
+			"WorkspaceMenuStructure",
+			}
+		);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+		);
+	}
 }
