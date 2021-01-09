9132f689e5425317c3b740d06661666302dfc610
jian ru <Jian.Ru@epicgames.com>
2021-01-08 19:08:57 -0500

    Temporary fix to incorrect GPU stats on a d3d12 platform [FYI] ben.woodhouse,eric.mcdaniel,matthew.craig
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: jian.ru
    #ROBOMERGE-SOURCE: CL 14997204 via CL 14997207 via CL 14997208 via CL 14997225
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v766-14819879)
    
    [CL 15028558 by jian ru in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12DirectCommandListManager.h
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12DirectCommandListManager.h
@@ -236,7 +236,7 @@ public:
 	// This use to also take an optional PSO parameter so that we could pass this directly to Create/Reset command lists,
 	// however this was removed as we generally can't actually predict what PSO we'll need until draw due to frequent
 	// state changes. We leave PSOs to always be resolved in ApplyState().
-	FD3D12CommandListHandle ObtainCommandList(FD3D12CommandAllocator& CommandAllocator);
+	FD3D12CommandListHandle ObtainCommandList(FD3D12CommandAllocator& CommandAllocator, bool bHasBackbufferWriteTransition = false);
 	void ReleaseCommandList(FD3D12CommandListHandle& hList);
 
 	void ExecuteCommandList(FD3D12CommandListHandle& hList, bool WaitForCompletion = false);
