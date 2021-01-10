7f23a2db9675a460312ee779d1e518da8d42d264
JeanFrancois Dube <JeanFrancois.Dube@epicgames.com>
2021-01-10 12:51:44 -0500

    Fix uninitialized script struct members in landscape structs.
    #jira UE-105451
    #rb sebastien.lussier
    
    [CL 15033958 by JeanFrancois Dube in Main branch]

--- Engine/Source/Editor/LandscapeEditor/Public/LandscapeEditorObject.h
+++ Engine/Source/Editor/LandscapeEditor/Public/LandscapeEditorObject.h
@@ -237,7 +237,19 @@ struct FLandscapePatternBrushWorldSpaceSettings
 	UPROPERTY(Category = "World-Space", EditAnywhere)
 	float RepeatSize;
 
-	FLandscapePatternBrushWorldSpaceSettings() = default;
+	FLandscapePatternBrushWorldSpaceSettings()
+		: Origin(ForceInit)
+		, Rotation(0.0f)
+		, bCenterTextureOnOrigin(false)
+		, RepeatSize(0.0f)
+	{}
+
+	FLandscapePatternBrushWorldSpaceSettings(const FVector2D& InOrigin, float InRotation, bool bInCenterTextureOnOrigin, float bRepeatSize)
+		: Origin(InOrigin)
+		, Rotation(InRotation)
+		, bCenterTextureOnOrigin(bInCenterTextureOnOrigin)
+		, RepeatSize(bRepeatSize)
+	{}
 };
 
 UCLASS(MinimalAPI)
