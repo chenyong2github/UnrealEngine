65e98be71eb6c2ccc7396682b6ceeb5ff812fe64
Andrew Grant <Andrew.Grant@epicgames.com>
2021-01-11 09:37:55 -0500

    Modify Mac/Linux build scripts to skip building UBT on Installed Builds.
    #jira UE-103181, EGS-30561, EGS-30560
    #rb swarm
    #review @ben.marsh @will.damon @brandon.schaefer @Vadym.Yaremenko@epicgames.com
    #lockdown cristina.riveron
    
    [CL 15037171 by Andrew Grant in 4.26 branch]

--- Engine/Build/BatchFiles/Linux/Build.sh
+++ Engine/Build/BatchFiles/Linux/Build.sh
@@ -9,10 +9,12 @@ cd "`dirname "$0"`/../../../.."
 # Setup Environment and Mono
 source Engine/Build/BatchFiles/Linux/SetupEnvironment.sh -mono Engine/Build/BatchFiles/Linux
 
-# First make sure that the UnrealBuildTool is up-to-date
-if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
-  echo "Failed to build to build tool (UnrealBuildTool)"
-  exit 1
+# If this is a source drop of the engine make sure that the UnrealBuildTool is up-to-date
+if [ ! -f Engine/Build/InstalledBuild.txt ]; then
+  if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
+    echo "Failed to build to build tool (UnrealBuildTool)"
+    exit 1
+  fi
 fi
 
 echo Running command : Engine/Binaries/DotNET/UnrealBuildTool.exe "$@"
