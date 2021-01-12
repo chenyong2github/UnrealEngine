08ddf76fd0f8da96aa044d16070dfb4591ae4411
johan torp <Johan.Torp@epicgames.com>
2021-01-12 08:12:34 -0500

    Added coarse parallel name batch loading
    #rb pj.kack
    
    #ROBOMERGE-SOURCE: CL 15048377 via CL 15048385 via CL 15048392
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v767-15032292)
    
    [CL 15048394 by johan torp in Main branch]

--- Engine/Source/Runtime/Core/Public/UObject/NameBatchSerialization.h
+++ Engine/Source/Runtime/Core/Public/UObject/NameBatchSerialization.h
@@ -4,6 +4,7 @@
 
 #include "Containers/ArrayView.h"
 #include "UObject/NameTypes.h"
+#include "Templates/Function.h"
 
 #define ALLOW_NAME_BATCH_SAVING (UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT) && PLATFORM_LITTLE_ENDIAN && !PLATFORM_TCHAR_IS_4_BYTES
 
@@ -22,12 +23,23 @@ CORE_API void ReserveNameBatch(uint32 NameDataBytes, uint32 HashDataBytes);
 
 // Load a name blob with precalculated hashes.
 //
-// Names are rehased if hash algorithm version doesn't match.
+// Names are rehashed if hash algorithm version doesn't match.
 //
 // @param NameData, HashData must be 8-byte aligned.
 CORE_API void LoadNameBatch(TArray<FNameEntryId>& OutNames, TArrayView<const uint8> NameData, TArrayView<const uint8> HashData);
 
 // Load names and precalculated hashes from an archive
 //
-// Names are rehased if hash algorithm version doesn't match.
+// Names are rehashed if hash algorithm version doesn't match.
 CORE_API TArray<FNameEntryId> LoadNameBatch(FArchive& Ar);
+
+// Load names and precalculated hashes from an archive using multiple workers
+//
+// May load synchronously in some cases, like small batches.
+//
+// Names are rehashed if hash algorithm version doesn't match.
+//
+// @param Ar is drained synchronously
+// @param MaxWorkers > 0
+// @return function that waits before returning result, like a simple future.
+CORE_API TFunction<TArray<FNameEntryId>()> LoadNameBatchAsync(FArchive& Ar, uint32 MaxWorkers);
