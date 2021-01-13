e9d55dfee5e086928c6d0273d444bd8e7c7682f2
Zousar Shaker <zousar.shaker@epicgames.com>
2021-01-13 15:44:11 -0400

    Add comments to ContainerElementTypeCompatibility.h explaining limitations of use for the compatibility types.  Split out from review here:
    https://p4-swarm.epicgames.net/reviews/15065157
    
    #rb steve.robb
    
    [CL 15071157 by Zousar Shaker in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Containers/ContainerElementTypeCompatibility.h
+++ Engine/Source/Runtime/Core/Public/Containers/ContainerElementTypeCompatibility.h
@@ -4,6 +4,11 @@
 
 #include "CoreTypes.h"
 
+/**
+ * Temporary compatibility mechanism to be used solely for the purpose of raw pointers to wrapped pointers.
+ * Specialization and use of this type is not supported beyond the specific instances in ObjectPtr.h.
+ * These types and the container methods that leverage them will be removed in a future release.
+ */
 template <typename InElementType>
 struct TContainerElementTypeCompatibility
 {
@@ -19,12 +24,20 @@ struct TContainerElementTypeCompatibility
 	static constexpr void CopyingFromOtherType() {}
 };
 
+/**
+ * Temporary compatibility mechanism to be used solely for the purpose of raw pointers to wrapped pointers.
+ * These types and the container methods that leverage them will be removed in a future release.
+ */
 template <typename ElementType>
 struct TIsContainerElementTypeReinterpretable
 {
 	enum { Value = !TIsSame<typename TContainerElementTypeCompatibility<ElementType>::ReinterpretType, ElementType>::Value };
 };
 
+/**
+ * Temporary compatibility mechanism to be used solely for the purpose of raw pointers to wrapped pointers.
+ * These types and the container methods that leverage them will be removed in a future release.
+ */
 template <typename ElementType>
 struct TIsContainerElementTypeCopyable
 {
