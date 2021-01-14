fb6206247f751ea8523216fc7fba41c3e89ba993
michael trepka <Michael.Trepka@epicgames.com>
2021-01-14 01:10:36 -0500

    Fixes for the resolution scale slider in video settings not working properly in performance mode in fronted and when DLSS was previously enabled
    #rb Jordan.Cristiano
    
    #ROBOMERGE-OWNER: jeanmichel.dignard
    #ROBOMERGE-AUTHOR: michael.trepka
    #ROBOMERGE-SOURCE: CL 15070575 via CL 15073604 via CL 15073681 via CL 15073708 via CL 15073750
    #ROBOMERGE-BOT: TOOLS (Main -> Dev-Tools-Staging) (v770-15065767)
    
    [CL 15081226 by michael trepka in Dev-Tools-Staging branch]

--- Engine/Source/Runtime/Engine/Public/Scalability.h
+++ Engine/Source/Runtime/Engine/Public/Scalability.h
@@ -142,7 +142,7 @@ namespace Scalability
 
 	/** Applies quality levels for temporary status which will NOT be saved to user settings e.g. mobile device low-power mode.
 		Originally active settings are backed-up or restored on toggle. */
-	ENGINE_API void ToggleTemporaryQualityLevels(bool bEnable);
+	ENGINE_API void ToggleTemporaryQualityLevels(bool bEnable, const FQualityLevels& QualityLevelsOverride = FQualityLevels());
 
 	/** Are active scalability settings a temporary override. */
 	ENGINE_API bool IsTemporaryQualityLevelActive();
