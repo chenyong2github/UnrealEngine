903a94dc73b78f382f284cab01cfe7b24a2e57b5
Jurre deBaare <jurre.debaare@epicgames.com>
2021-01-14 08:40:38 -0400

    Add functionality to EditorScriptingUtilities plugin to display a modal dialog containing a details-view targetting a UObject instance #jira UE-106294 #rb Thomas.Sarkanen
    [CL 15082999 by Jurre deBaare in ue5-main branch]

--- Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Private/EditorDialogLibrary.cpp
+++ Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Private/EditorDialogLibrary.cpp
@@ -3,7 +3,158 @@
 #include "EditorDialogLibrary.h"
 #include "Misc/MessageDialog.h"
 
+#include "Framework/MultiBox/MultiBoxBuilder.h"
+#include "Framework/Application/SlateApplication.h"
+#include "PropertyEditorModule.h"
+#include "IDetailsView.h"
+#include "EditorStyleSet.h"
+#include "Widgets/Layout/SBorder.h"
+#include "Widgets/Input/SButton.h"
+#include "Widgets/Text/STextBlock.h"
+#include "Editor.h"
+#include "Widgets/Layout/SScrollBox.h"
+
+#define LOCTEXT_NAMESPACE "EditorDialogLibrary"
+
 TEnumAsByte<EAppReturnType::Type> UEditorDialogLibrary::ShowMessage(const FText& Title, const FText& Message, TEnumAsByte<EAppMsgType::Type> MessageType, TEnumAsByte<EAppReturnType::Type> DefaultValue)
 {
 	return FMessageDialog::Open(MessageType, DefaultValue, Message, &Title);
 }
+
+/** Dialog widget used to display an object its properties */
+class SObjParamDialog : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SObjParamDialog) {}
+	SLATE_END_ARGS()	
+
+	void Construct(const FArguments& InArgs, TWeakPtr<SWindow> InParentWindow, const TArray<UObject*>& Objects)
+	{
+		bOKPressed = false;
+
+		// Initialize details view
+		FDetailsViewArgs DetailsViewArgs;
+		{
+			DetailsViewArgs.bLockable = false;
+			DetailsViewArgs.bUpdatesFromSelection = false;
+			DetailsViewArgs.NameAreaSettings = FDetailsViewArgs::ENameAreaSettings::ObjectsUseNameArea;
+			DetailsViewArgs.bAllowMultipleTopLevelObjects = true;
+			DetailsViewArgs.bShowPropertyMatrixButton = false;
+		}
+
+		FPropertyEditorModule& PropertyEditorModule = FModuleManager::Get().LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");		
+		TSharedRef<IDetailsView> DetailsView = PropertyEditorModule.CreateDetailView(DetailsViewArgs);
+
+		DetailsView->SetObjects(Objects, true);
+
+		ChildSlot
+		[
+			SNew(SVerticalBox)
+			+SVerticalBox::Slot()
+			.FillHeight(1.0f)
+			[
+				SNew(SScrollBox)
+				+SScrollBox::Slot()
+				[
+					DetailsView->AsShared()
+				]
+			]
+			+SVerticalBox::Slot()
+			.AutoHeight()
+			[
+				SNew(SBorder)
+				.BorderImage(FEditorStyle::GetBrush("ToolPanel.GroupBorder"))
+				.VAlign(VAlign_Center)
+				.HAlign(HAlign_Right)
+				[
+					SNew(SHorizontalBox)
+					+SHorizontalBox::Slot()
+					.Padding(2.0f)
+					.AutoWidth()
+					[
+						SNew(SButton)
+						.ButtonStyle(FEditorStyle::Get(), "FlatButton.Success")
+						.ForegroundColor(FLinearColor::White)
+						.ContentPadding(FMargin(6, 2))
+						.OnClicked_Lambda([this, InParentWindow, InArgs]()
+						{
+							if(InParentWindow.IsValid())
+							{
+								InParentWindow.Pin()->RequestDestroyWindow();
+							}
+							bOKPressed = true;
+							return FReply::Handled(); 
+						})
+						[
+							SNew(STextBlock)
+							.TextStyle(FEditorStyle::Get(), "ContentBrowser.TopBar.Font")
+							.Text(LOCTEXT("OKButtonText", "OK"))
+						]
+					]
+					+SHorizontalBox::Slot()
+					.Padding(2.0f)
+					.AutoWidth()
+					[
+						SNew(SButton)
+						.ButtonStyle(FEditorStyle::Get(), "FlatButton")
+						.ForegroundColor(FLinearColor::White)
+						.ContentPadding(FMargin(6, 2))
+						.OnClicked_Lambda([InParentWindow]()
+						{ 
+							if(InParentWindow.IsValid())
+							{
+								InParentWindow.Pin()->RequestDestroyWindow();
+							}
+							return FReply::Handled(); 
+						})
+						[
+							SNew(STextBlock)
+							.TextStyle(FEditorStyle::Get(), "ContentBrowser.TopBar.Font")
+							.Text(LOCTEXT("CancelButtonText", "Cancel"))
+						]
+					]
+				]
+			]
+		];
+	}
+
+	bool WasOkPressed() const { return bOKPressed; }
+protected:
+	bool bOKPressed;
+};
+
+bool UEditorDialogLibrary::ShowObjectDetailsView(const FText& Title, UObject* InOutObject)
+{	
+	TArray<UObject*> ViewObjects = { InOutObject};
+	return ShowObjectsDetailsView(Title, ViewObjects);
+}
+
+bool UEditorDialogLibrary::ShowObjectsDetailsView(const FText& Title, const TArray<UObject*>& InOutObjects)
+{
+	if (!FApp::IsUnattended() && !GIsRunningUnattendedScript)
+	{
+		TArray<UObject*> ViewObjects = InOutObjects;
+		ViewObjects.Remove(nullptr);
+
+		if (ViewObjects.Num())
+		{
+			TSharedRef<SWindow> Window = SNew(SWindow)
+				.Title(Title)
+				.SizingRule(ESizingRule::Autosized)
+				.AutoCenter(EAutoCenter::PrimaryWorkArea)
+				.SupportsMinimize(false)
+				.SupportsMaximize(false);
+
+			TSharedPtr<SObjParamDialog> Dialog;
+			Window->SetContent(SAssignNew(Dialog, SObjParamDialog, Window, ViewObjects));
+			GEditor->EditorAddModalWindow(Window);
+
+			return Dialog->WasOkPressed();
+		}
+	}
+
+	return false;
+}
+
+
+#undef LOCTEXT_NAMESPACE // "EditorDialogLibrary"
--- Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Public/EditorDialogLibrary.h
+++ Engine/Plugins/Editor/EditorScriptingUtilities/Source/EditorScriptingUtilities/Public/EditorDialogLibrary.h
@@ -27,5 +27,23 @@ public:
 	*/
 	UFUNCTION(BlueprintCallable, DisplayName = "Show Message Dialog", Category = "Editor Scripting | Message Dialog")
 	static TEnumAsByte<EAppReturnType::Type> ShowMessage(const FText& Title, const FText& Message, TEnumAsByte<EAppMsgType::Type> MessageType, TEnumAsByte<EAppReturnType::Type> DefaultValue = EAppReturnType::Type::No);
+
+	/**
+	 * Open a modal message box dialog containing a details view for inspecting / modifying a UObject. 
+	 * @param Title 		The title of the created dialog window
+	 * @param InOutObject 	Object instance of ClassOfObject which is supposed to be viewed
+	 * @return The result of the users decision, true=Ok, false=Cancel, or false if running in unattended mode.
+	*/
+	UFUNCTION(BlueprintCallable, DisplayName = "Show Object Dialog", Category = "Editor Scripting | Object Dialog")
+	static bool ShowObjectDetailsView(const FText& Title, UObject* InOutObject);
+
+	/**
+	 * Open a modal message box dialog containing a details view for inspecting / modifying multiples UObjects. 
+	 * @param Title 		The title of the created dialog window
+	 * @param InOutObjects 	Array of object instances which are supposed to be viewed
+	 * @return The result of the users decision, true=Ok, false=Cancel, or false if running in unattended mode.
+	*/
+	UFUNCTION(BlueprintCallable, DisplayName = "Show Objects Dialog", Category = "Editor Scripting | Objects Dialog")
+	static bool ShowObjectsDetailsView(const FText& Title, const TArray<UObject*>& InOutObjects);
 };
 
