a8058ce4f34a635164bcfdf9e919618716e0ec42
benn gallagher <Benn.Gallagher@epicgames.com>
2021-01-21 11:01:30 -0500

    Implemented runtime and runtime-async cooking for physics (used by procedural mesh). Logic for Chaos is a little simpler as we don't have a separation like PhysXCooking has (separate dll/module). Moved all cooking functions to a new file and namespace for better separation of concenrns from DDC Added Chaos::FCookHelper to encapsulate the cooking of a whole UBodySetup #jira none #rb Max.Whitehead
    #ROBOMERGE-SOURCE: CL 15154679 in //UE4/Release-4.26-Chaos/... via CL 15154705
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15154721 by benn gallagher in Main branch]

---
+++ Engine/Source/Runtime/Engine/Private/PhysicsEngine/Experimental/ChaosCooking.h
@@ -0,0 +1,36 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#if WITH_CHAOS
+namespace Chaos
+{
+	namespace Cooking
+	{
+		ENGINE_API TUniquePtr<Chaos::FTriangleMeshImplicitObject> BuildSingleTrimesh(const FTriMeshCollisionData& Desc, TArray<int32>& OutFaceRemap, TArray<int32>& OutVertexRemap);
+
+		void BuildConvexMeshes(TArray<TUniquePtr<Chaos::FImplicitObject>>& OutTriangleMeshes, const FCookBodySetupInfo& InParams);
+		void BuildTriangleMeshes(TArray<TUniquePtr<Chaos::FTriangleMeshImplicitObject>>& OutTriangleMeshes, TArray<int32>& OutFaceRemap, TArray<int32>& OutVertexRemap, const FCookBodySetupInfo& InParams);
+	}
+
+	struct FCookHelper
+	{
+		FCookHelper() = delete;
+		FCookHelper(UBodySetup* InSetup);
+
+		TArray<TUniquePtr<Chaos::FImplicitObject>> SimpleImplicits;
+		TArray<TUniquePtr<Chaos::FTriangleMeshImplicitObject>> ComplexImplicits;
+		FBodySetupUVInfo UVInfo;
+		TArray<int32> FaceRemap;
+		TArray<int32> VertexRemap;
+
+		void Cook();
+		void CookAsync(FSimpleDelegateGraphTask::FDelegate CompletionDelegate);
+		bool HasWork() const;
+
+	private:
+		UBodySetup* SourceSetup;
+		FCookBodySetupInfo CookInfo;
+	};
+}
+#endif
