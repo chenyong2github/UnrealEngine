d1af9a5de7a3dfbd71ea1ab0eff9dd79c5096a7f
Kenzo Terelst <Kenzo.Terelst@epicgames.com>
2021-01-21 06:58:12 -0400

    Track buffer and texture pool stats on xbox as well
    #jira none
    #rb trivial
    
    [CL 15153232 by Kenzo Terelst in ue5-main branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Allocation.h
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Allocation.h
@@ -973,6 +973,7 @@ public:
 	void CleanUpAllocations() { ReadOnlyTexturePool.CleanUpAllocations(0); }
 
 	void Destroy() { ReadOnlyTexturePool.Destroy(); }
+	bool GetMemoryStats(uint64& OutTotalAllocated, uint64& OutTotalUnused) const { OutTotalAllocated = 0; OutTotalUnused = 0; }
 
 private:
 	FD3D12TextureAllocator ReadOnlyTexturePool;
