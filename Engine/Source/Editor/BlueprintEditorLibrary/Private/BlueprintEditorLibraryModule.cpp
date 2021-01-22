0f2ef874ab1823b2e650512086cd969df609888e
ben hoffman <ben.hoffman@epicgames.com>
2021-01-22 15:23:56 -0400

    remove log spam from blueprint editor library module startup
    #jira none
    #rb me
    #rnx
    
    [CL 15166068 by ben hoffman in ue5-main branch]

--- Engine/Source/Editor/BlueprintEditorLibrary/Private/BlueprintEditorLibraryModule.cpp
+++ Engine/Source/Editor/BlueprintEditorLibrary/Private/BlueprintEditorLibraryModule.cpp
@@ -7,14 +7,12 @@
 void FBlueprintEditorLibraryModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	UE_LOG(LogTemp, Warning, TEXT("BP Editor Lib Loaded"));
 }
 
 void FBlueprintEditorLibraryModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.	
-	UE_LOG(LogTemp, Warning, TEXT("BP Editor Lib Loaded"));
 }
 
 #undef LOCTEXT_NAMESPACE
