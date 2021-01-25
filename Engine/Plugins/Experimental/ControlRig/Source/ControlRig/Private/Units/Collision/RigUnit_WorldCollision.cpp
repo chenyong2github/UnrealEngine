7e4676bf9214dd9a2dc41bf30fe623a51703a82d
jack cai <jack.cai@epicgames.com>
2021-01-25 16:23:54 -0400

    Control Rig: Added two new SphereTrace nodes that support tracing by trace channels and by object types, deprecated the old SphereTrace Node
    #jira UE-103021
    #rb helge.mathee
    
    [CL 15184661 by jack cai in ue5-main branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/Units/Collision/RigUnit_WorldCollision.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/Units/Collision/RigUnit_WorldCollision.cpp
@@ -45,3 +45,96 @@ FRigUnit_SphereTraceWorld_Execute()
 		HitNormal = Context.ToWorldSpaceTransform.InverseTransformVector(HitResult.ImpactNormal);
 	}
 }
+
+FRigUnit_SphereTraceByTraceChannel_Execute()
+{
+	DECLARE_SCOPE_HIERARCHICAL_COUNTER_RIGUNIT()
+
+	bHit = false;
+	HitLocation = FVector::ZeroVector;
+	HitNormal = FVector(0.f, 0.f, 1.f);
+
+	if (Context.World == nullptr)
+	{
+		return;
+	}
+
+	const ECollisionChannel CollisionChannel = UEngineTypes::ConvertToCollisionChannel(TraceChannel); 
+	const FCollisionShape CollisionShape = FCollisionShape::MakeSphere(Radius);
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.bTraceComplex = true;
+
+	if (Context.OwningActor)
+	{
+		QueryParams.AddIgnoredActor(Context.OwningActor);
+	}
+	else if (UPrimitiveComponent* PrimitiveComponent = Cast<UPrimitiveComponent>(Context.OwningComponent))
+	{
+		QueryParams.AddIgnoredComponent(PrimitiveComponent);
+	}
+
+	FHitResult HitResult;
+	bHit = Context.World->SweepSingleByChannel(HitResult, Context.ToWorldSpace(Start), Context.ToWorldSpace(End), FQuat::Identity, CollisionChannel, CollisionShape, QueryParams);
+	
+	if (bHit)
+	{
+		HitLocation = Context.ToRigSpace(HitResult.ImpactPoint);
+		HitNormal = Context.ToWorldSpaceTransform.InverseTransformVector(HitResult.ImpactNormal);
+	}
+}
+
+FRigUnit_SphereTraceByObjectTypes_Execute()
+{
+	DECLARE_SCOPE_HIERARCHICAL_COUNTER_RIGUNIT()
+
+	bHit = false;
+	HitLocation = FVector::ZeroVector;
+	HitNormal = FVector(0.f, 0.f, 1.f);
+
+	if (Context.World == nullptr)
+	{
+		return;
+	}
+
+	const FCollisionShape CollisionShape = FCollisionShape::MakeSphere(Radius); 
+
+	// similar to KismetTraceUtils.h -> ConfigureCollisionObjectParams
+	TArray<TEnumAsByte<ECollisionChannel>> CollisionChannels;
+	CollisionChannels.AddUninitialized(ObjectTypes.Num());
+
+	for (int Index = 0; Index < ObjectTypes.Num(); Index++)
+	{ 
+		CollisionChannels[Index] = UEngineTypes::ConvertToCollisionChannel(ObjectTypes[Index]);
+	}
+
+	FCollisionObjectQueryParams ObjectParams;
+	for (const ECollisionChannel& Channel : CollisionChannels)
+	{
+		if (FCollisionObjectQueryParams::IsValidObjectQuery(Channel))
+		{
+			ObjectParams.AddObjectTypesToQuery(Channel);
+		}
+	}
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.bTraceComplex = true;
+
+	if (Context.OwningActor)
+	{
+		QueryParams.AddIgnoredActor(Context.OwningActor);
+	}
+	else if (UPrimitiveComponent* PrimitiveComponent = Cast<UPrimitiveComponent>(Context.OwningComponent))
+	{
+		QueryParams.AddIgnoredComponent(PrimitiveComponent);
+	} 
+
+	FHitResult HitResult;
+	bHit = Context.World->SweepSingleByObjectType(HitResult, Context.ToWorldSpace(Start), Context.ToWorldSpace(End), FQuat::Identity, ObjectParams, CollisionShape, QueryParams);
+
+	if (bHit)
+	{
+		HitLocation = Context.ToRigSpace(HitResult.ImpactPoint);
+		HitNormal = Context.ToWorldSpaceTransform.InverseTransformVector(HitResult.ImpactNormal);
+	}
+}
