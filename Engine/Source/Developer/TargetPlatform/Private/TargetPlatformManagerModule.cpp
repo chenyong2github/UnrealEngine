679152f632ccd9b3e264f2ceb869fd90728e2549
andrew grant <Andrew.Grant@epicgames.com>
2021-02-02 13:23:02 -0500

    Turned check(false) into a meaningful fatal error when the engine cannot resolve a requested shader version for a platform.
    #rb na
    #jira UE-107191
    #lockdown cristina.riveron
    
    #ROBOMERGE-SOURCE: CL 15282112 in //UE4/Release-4.26/... via CL 15282129 via CL 15282148
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15282173 by andrew grant in Main branch]

--- Engine/Source/Developer/TargetPlatform/Private/TargetPlatformManagerModule.cpp
+++ Engine/Source/Developer/TargetPlatform/Private/TargetPlatformManagerModule.cpp
@@ -552,7 +552,10 @@ public:
 			}
 		}
 
-		check(Result);
+		if (!Result)
+		{
+			UE_LOG(LogTargetPlatformManager, Fatal, TEXT("No ShaderFormat found for %s!"), *Name.ToString());
+		}
 
 		return *Result;
 	}
