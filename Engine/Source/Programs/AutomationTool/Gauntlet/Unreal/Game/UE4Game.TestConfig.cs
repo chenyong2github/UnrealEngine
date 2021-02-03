94b8ec97b79517515e7087e864a037a8ad9eba85
andrew grant <Andrew.Grant@epicgames.com>
2021-02-03 12:23:54 -0500

    Rewrote EngineTest build graph to improve CIS visibility and coverage
    * Removes most duplication of logic for different platforms, categories, incremental builds
    * Adds support for XSX, PS5, Switch, Lumin, Stadia (currently disabled)
    * Adds Horde labels
    
    #jira nojira
    #rb swarm
    #lockdown cristina.riveron
    
    #ROBOMERGE-OWNER: andrew.grant
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 15285270 in //UE4/Release-4.26/... via CL 15285295 via CL 15285296
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 15296673 by andrew grant in Main branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/Game/UE4Game.TestConfig.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/Game/UE4Game.TestConfig.cs
@@ -16,38 +16,13 @@ namespace UE4Game
 	public class UE4TestConfig : UnrealTestConfiguration
 	{
 		/// <summary>
-		/// Map to use
-		/// </summary>
-		[AutoParam]
-		public string Map = "";
-
-		/// <summary>
 		/// Applies these options to the provided app config
 		/// </summary>
 		/// <param name="AppConfig"></param>
 		public override void ApplyToConfig(UnrealAppConfig AppConfig, UnrealSessionRole ConfigRole, IEnumerable<UnrealSessionRole> OtherRoles)
 		{
 			base.ApplyToConfig(AppConfig, ConfigRole, OtherRoles);
-
-			if (string.IsNullOrEmpty(ConfigRole.MapOverride) == false)
-			{
-				// If map is specified, pass it to the server, or just the client if there is no server
-				if (AppConfig.ProcessType.IsServer()
-				|| (AppConfig.ProcessType.IsClient() && RoleCount(UnrealTargetRole.Server) == 0))
-				{
-					// must be the first argument!
-					AppConfig.CommandLineParams.GameMap = ConfigRole.MapOverride;
-				}
-			}
-			else if (string.IsNullOrEmpty(Map) == false)
-			{
-				// If map is specified, pass it to the server, or just the client if there is no server
-				if (AppConfig.ProcessType.IsServer()
-				|| (AppConfig.ProcessType.IsClient() && RoleCount(UnrealTargetRole.Server) == 0))
-				{
-					AppConfig.CommandLineParams.GameMap = Map;
-				}
-			}			
+	
 		}
 	}
 }
