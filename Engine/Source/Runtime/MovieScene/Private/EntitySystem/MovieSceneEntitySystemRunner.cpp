1409e32ec60d7edd0fa93bceeca7a9415153acd7
ludovic chabant <Ludovic.Chabant@epicgames.com>
2021-02-04 14:16:58 -0500

    Sequencer: Convert assert to log warning.
    #jira UE-107508
    #rb max.chen, matt.hoffman
    #lockdown Cristina.Riveron
    
    [CL 15321336 by ludovic chabant in 4.26 branch]

--- Engine/Source/Runtime/MovieScene/Private/EntitySystem/MovieSceneEntitySystemRunner.cpp
+++ Engine/Source/Runtime/MovieScene/Private/EntitySystem/MovieSceneEntitySystemRunner.cpp
@@ -93,7 +93,10 @@ void FMovieSceneEntitySystemRunner::QueueUpdate(const FMovieSceneContext& Contex
 
 void FMovieSceneEntitySystemRunner::Update(const FMovieSceneContext& Context, FInstanceHandle Instance)
 {
-	checkf(UpdateQueue.Num() == 0, TEXT("Updates are already queued! This will run those updates as well, which might not be what's intended."));
+	if (UpdateQueue.Num() > 0)
+	{
+		UE_LOG(LogMovieScene, Warning, TEXT("Updates are already queued! This will run those updates as well, which might not be what's intended."));
+	}
 
 	// Queue our one update and flush immediately.
 	QueueUpdate(Context, Instance);
