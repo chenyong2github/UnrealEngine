5fd2b89d9b5efb5cce6e98798da0f40c12feea5e
mark lintott <mark.lintott@epicgames.com>
2021-02-08 14:12:05 -0500

    #jira UE-107942 #rb trivial
    Missing intializers for bool members
    
    [CL 15358741 by mark lintott in Main branch]

--- Engine/Source/Editor/GameProjectGeneration/Classes/TemplateProjectDefs.h
+++ Engine/Source/Editor/GameProjectGeneration/Classes/TemplateProjectDefs.h
@@ -36,7 +36,7 @@ struct FTemplateReplacement
 	FString To;
 
 	UPROPERTY()
-	bool bCaseSensitive;
+	bool bCaseSensitive{false};
 };
 
 USTRUCT()
@@ -199,7 +199,7 @@ struct FTemplateCategoryDef
 
 	/** Is this a major top-level category? Major categories are displayed as full rows, eg. the Game category.*/
 	UPROPERTY()
-	bool IsMajorCategory;
+		bool IsMajorCategory{ false };
 };
 
 UCLASS(config=TemplateCategories)
