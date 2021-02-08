07c2e232f483ef04d6776577a3412a75eeee35a9
mark lintott <mark.lintott@epicgames.com>
2021-02-08 04:16:49 -0500

    #jira UE-107999 #rb trivial
    Fixed missleading comment
    
    [CL 15355347 by mark lintott in Main branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/UObjectGlobals.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/UObjectGlobals.h
@@ -2040,7 +2040,7 @@ struct COREUOBJECT_API FCoreUObjectDelegates
 	DECLARE_MULTICAST_DELEGATE_OneParam(FPreLoadMapDelegate, const FString& /* MapName */);
 	static FPreLoadMapDelegate PreLoadMap;
 
-	/** Sent at the _successful_ end of LoadMap */
+	/** Sent at the end of LoadMap */
 	DECLARE_MULTICAST_DELEGATE_OneParam(FPostLoadMapDelegate, UWorld* /* LoadedWorld */);
 	static FPostLoadMapDelegate PostLoadMapWithWorld;
 
