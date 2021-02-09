18ce6ceb65a9b3650e912fc48f164034dbf4bbef
jamie dale <Jamie.Dale@epicgames.com>
2021-02-09 15:03:10 -0400

    Fixed Python shutdown crash when Slate callbacks are bound
    Slate can shutdown prior to the Python plugin, which means it can clear an array of delegates (which may contain captured Python objects) without taking the Python GIL. TPyAutoGILPtr has been added as a way to capture Python instances that will safely take the GIL during copy, move, or destruction operations.
    
    #rb Patrick.Laflamme
    
    #ROBOMERGE-SOURCE: CL 15366274 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v771-15082668)
    
    [CL 15368939 by jamie dale in ue5-main branch]

--- Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyGIL.h
+++ Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyGIL.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "IncludePython.h"
+#include "PyPtr.h"
 
 #if WITH_PYTHON
 
@@ -30,5 +31,106 @@ private:
 	/** Internal GIL state */
 	PyGILState_STATE GILState;
 };
+
+/** Wrapper of a TPyPtr that can be safely copied/moved/destroyed by code where the GIL might not currently be held (eg, a lambda bound to a delegate) */
+template <typename TPythonType>
+class TPyAutoGILPtr
+{
+public:
+	TPyAutoGILPtr() = default;
+
+	explicit TPyAutoGILPtr(TPyPtr<TPythonType>&& InPtr)
+		: Ptr(MoveTemp(InPtr))
+	{
+	}
+
+	TPyAutoGILPtr(const TPyAutoGILPtr& InOther)
+	{
+		// This may be called after Python has already shut down
+		if (Py_IsInitialized())
+		{
+			FPyScopedGIL GIL;
+			Ptr = InOther.Ptr;
+		}
+	}
+
+	TPyAutoGILPtr(TPyAutoGILPtr&& InOther)
+	{
+		// This may be called after Python has already shut down
+		if (Py_IsInitialized())
+		{
+			FPyScopedGIL GIL;
+			Ptr = MoveTemp(InOther.Ptr);
+		}
+	}
+
+	~TPyAutoGILPtr()
+	{
+		// This may be called after Python has already shut down
+		if (Py_IsInitialized())
+		{
+			FPyScopedGIL GIL;
+			Ptr.Reset();
+		}
+		else
+		{
+			// Release ownership if Python has been shut down to avoid attempting to delete the object (which is already dead)
+			Ptr.Release();
+		}
+	}
+
+	TPyAutoGILPtr& operator=(const TPyAutoGILPtr& InOther)
+	{
+		if (this != &InOther)
+		{
+			// This may be called after Python has already shut down
+			if (Py_IsInitialized())
+			{
+				FPyScopedGIL GIL;
+				Ptr = InOther.Ptr;
+			}
+			else
+			{
+				// Release ownership if Python has been shut down to avoid attempting to delete the object (which is already dead)
+				Ptr.Release();
+			}
+		}
+		return *this;
+	}
+
+	TPyAutoGILPtr& operator=(TPyAutoGILPtr&& InOther)
+	{
+		if (this != &InOther)
+		{
+			// This may be called after Python has already shut down
+			if (Py_IsInitialized())
+			{
+				FPyScopedGIL GIL;
+				Ptr = MoveTemp(InOther.Ptr);
+			}
+			else
+			{
+				// Release ownership if Python has been shut down to avoid attempting to delete the object (which is already dead)
+				Ptr.Release();
+			}
+		}
+		return *this;
+	}
+
+	TPyPtr<TPythonType>& Get()
+	{
+		return Ptr;
+	}
+
+	const TPyPtr<TPythonType>& Get() const
+	{
+		return Ptr;
+	}
+
+private:
+	TPyPtr<TPythonType> Ptr;
+};
+
+typedef TPyAutoGILPtr<PyObject> FPyAutoGILPtr;
 
 #endif	// WITH_PYTHON
