971dbcb300b81f01d7867ae574a40891e196cc2b
brooke hubert <brooke.hubert@epicgames.com>
2021-02-09 18:50:26 -0500

    Adding default values to uprops in structs for Target Device Manager.
    #jira UE-107966
    #rb josh.adams
    
    [CL 15372755 by brooke hubert in Main branch]

--- Engine/Source/Runtime/SessionMessages/Public/SessionServiceMessages.h
+++ Engine/Source/Runtime/SessionMessages/Public/SessionServiceMessages.h
@@ -43,7 +43,7 @@ struct FSessionServicePong
 
 	/** Indicates whether the pinging user is authorized to interact with this session. */
 	UPROPERTY(EditAnywhere, Category="Message")
-	bool Authorized;
+	bool Authorized = true;
 
 	/** Holds the application's build date. */
 	UPROPERTY(EditAnywhere, Category="Message")
@@ -79,7 +79,7 @@ struct FSessionServicePong
 
 	/** Indicates whether the application is the only one in that session. */
 	UPROPERTY(EditAnywhere, Category="Message")
-	bool Standalone;
+	bool Standalone = true;
 };
 
 
@@ -108,7 +108,7 @@ struct FSessionServiceLog
 
 	/** Holds the time in seconds since the application was started. */
 	UPROPERTY(EditAnywhere, Category="Message")
-	double TimeSeconds;
+	double TimeSeconds = 0.0;
 
 	/** Holds the log message's verbosity level. */
 	UPROPERTY(EditAnywhere, Category="Message")
