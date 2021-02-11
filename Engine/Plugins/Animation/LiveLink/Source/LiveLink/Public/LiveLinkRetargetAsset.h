b4fa69116b01e3cd9b635b01731b1c3177a8bf4a
Ruslan Idrisov <ruslan.idrisov@4dpipeline.com>
2021-02-11 14:12:47 -0500

    UE-107066: Removed Pure virutal function that was depricated in 4.23 and was causing issues.
    #jira UE-107066
    
    #rb Simon.Therriault, Alejandro.Arango
    
    [CL 15390483 by Ruslan Idrisov in Main branch]

--- Engine/Plugins/Animation/LiveLink/Source/LiveLink/Public/LiveLinkRetargetAsset.h
+++ Engine/Plugins/Animation/LiveLink/Source/LiveLink/Public/LiveLinkRetargetAsset.h
@@ -35,10 +35,6 @@ class LIVELINK_API ULiveLinkRetargetAsset : public UObject
 	// Called once when the retargeter is created 
 	virtual void Initialize() {}
 
-	// Build OutPose and OutCurve from the supplied InFrame.
-	UE_DEPRECATED(4.23, "ULiveLinkRetargetAsset::BuildPoseForSubject is deprecated. Please use ULiveLinkRetargetAsset::BuildPoseFromAnimationData and ULiveLinkRetargetAsset::BuildPoseAndCurveFromBaseData instead.")
-	virtual void BuildPoseForSubject(float DeltaTime, const FLiveLinkSkeletonStaticData* InSkeletonData, const FLiveLinkAnimationFrameData* InFrameData, FCompactPose& OutPose, FBlendedCurve& OutCurve) PURE_VIRTUAL(ULiveLinkRetargetAsset::BuildPoseForSubject, );
-	
 	// Build OutPose from AnimationData if subject was from this type
 	virtual void BuildPoseFromAnimationData(float DeltaTime, const FLiveLinkSkeletonStaticData* InSkeletonData, const FLiveLinkAnimationFrameData* InFrameData, FCompactPose& OutPose) {}
 
