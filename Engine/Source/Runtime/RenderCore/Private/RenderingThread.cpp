5e077abe5804f457afe84ddda600dd67b8170f46
ben woodhouse <Ben.Woodhouse@epicgames.com>
2021-02-12 08:15:56 -0500

    Fix the CSV profiler having the wrong renderthread ID (using RT heartbeat thread instead of renderthread). This was causing renderthread stats to be reported out of sync. [FYI] Andriy.Tylychko
    #ROBOMERGE-SOURCE: CL 15397315 via CL 15397319 via CL 15397320
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15397322 by ben woodhouse in Main branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderingThread.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderingThread.cpp
@@ -561,11 +561,6 @@ public:
 	{
 		GSuspendRenderingTickables = 0;
 		OutstandingHeartbeats.Reset();
-
-#if CSV_PROFILER
-		FCsvProfiler::Get()->SetRenderThreadId(FPlatformTLS::GetCurrentThreadId());
-#endif
-
 		return true; 
 	}
 
