d9f39b4293185681883a3dd9fe83dbfaafb84eb9
max chen <Max.Chen@epicgames.com>
2021-02-12 00:33:09 -0500

    Sequencer: Added missing pure virtual implementations
    #rb none
    
    #ROBOMERGE-SOURCE: CL 15396143 via CL 15396148 via CL 15396152 via CL 15396315
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15396324 by max chen in Main branch]

--- Engine/Source/Runtime/MovieScene/Private/Tests/MovieSceneTestObjects.h
+++ Engine/Source/Runtime/MovieScene/Private/Tests/MovieSceneTestObjects.h
@@ -59,6 +59,13 @@ public:
 		}
 	}
 
+	//~ UMovieSceneSequence interface
+	virtual void BindPossessableObject(const FGuid& ObjectId, UObject& PossessedObject, UObject* Context) override {}
+	virtual void LocateBoundObjects(const FGuid& ObjectId, UObject* Context, TArray<UObject*, TInlineAllocator<1>>& OutObjects) const override {}
+	virtual UObject* GetParentObject(UObject* Object) const override { return nullptr; }
+	virtual void UnbindPossessableObjects(const FGuid& ObjectId) override {}
+	virtual void UnbindObjects(const FGuid& ObjectId, const TArray<UObject*>& InObjects, UObject* Context) override {}
+	virtual void UnbindInvalidObjects(const FGuid& ObjectId, UObject* Context) override {}
 	virtual UMovieScene* GetMovieScene() const override { return MovieScene; }
 
 	UPROPERTY()
