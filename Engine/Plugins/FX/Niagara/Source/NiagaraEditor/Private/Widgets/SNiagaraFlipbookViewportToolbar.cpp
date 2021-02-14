10bf1aee14e75809acac7148da25f3bb984c40cd
graeme thornton <Graeme.Thornton@epicgames.com>
2021-02-14 09:39:16 -0500

    Fixed non-portable #include
    #ROBOMERGE-SOURCE: CL 15403477 via CL 15403482 via CL 15403484
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15403487 by graeme thornton in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Widgets/SNiagaraFlipbookViewportToolbar.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Widgets/SNiagaraFlipbookViewportToolbar.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "SNiagaraFlipbookViewportToolBar.h"
+#include "SNiagaraFlipbookViewportToolbar.h"
 #include "SNiagaraFlipbookViewport.h"
 #include "ViewModels/NiagaraFlipbookViewModel.h"
 
