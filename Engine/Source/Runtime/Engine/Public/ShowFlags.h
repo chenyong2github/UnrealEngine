a7ca04b98d0c378a06e43f913eb5e4ef80a1a7b5
ryan mangin <Ryan.Mangin@epicgames.com>
2021-02-15 15:13:50 -0500

    Better fix for conflicting define with solaris / engine
    #jira: UE-108505
    #rb: brooke.hubert
    
    
    #ROBOMERGE-SOURCE: CL 15410056 via CL 15410058 via CL 15410063
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15410070 by ryan mangin in Main branch]

--- Engine/Source/Runtime/Engine/Public/ShowFlags.h
+++ Engine/Source/Runtime/Engine/Public/ShowFlags.h
@@ -539,8 +539,6 @@ enum class EShowFlagShippingValue
 template<EShowFlagShippingValue ShippingValue = EShowFlagShippingValue::Dynamic>
 struct TCustomShowFlag
 {
-#undef IF_CONSTEXPR
-
 #if PLATFORM_COMPILER_HAS_IF_CONSTEXPR
 #define IF_CONSTEXPR if constexpr
 #else 
