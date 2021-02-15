e8c57228506a8822b138748c0d276ffc74775b05
benoit deschenes <Benoit.Deschenes@xa.epicgames.com>
2021-02-15 09:43:30 -0500

    Fix build - Bad forward declared underlying type on enum
    #jira UE-108513
    #rb trivial
    
    [CL 15406663 by benoit deschenes in Main branch]

--- Engine/Plugins/Enterprise/DatasmithImporter/Source/DatasmithImporter/Public/DatasmithMaterialExpressions.h
+++ Engine/Plugins/Enterprise/DatasmithImporter/Source/DatasmithImporter/Public/DatasmithMaterialExpressions.h
@@ -33,7 +33,7 @@ class UMaterialInterface;
 struct FExpressionInput;
 class UMaterialExpressionMaterialFunctionCall;
 
-enum class EDatasmithMaterialExpressionType : uint64;
+enum class EDatasmithMaterialExpressionType : uint8;
 enum class EDatasmithTextureMode : uint8;
 
 enum class EDatasmithTextureSlot
