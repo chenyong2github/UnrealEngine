ca4cad75531372c7268ec0162b2bd275b7e0129e
nick darnell <Nick.Darnell@epicgames.com>
2021-02-16 14:55:20 -0500

    Gameplay Tags - Adding documentation to the native gameplay tags.
    #ROBOMERGE-SOURCE: CL 15421371 via CL 15421405 via CL 15421420
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15421432 by nick darnell in Main branch]

--- Engine/Source/Runtime/GameplayTags/Public/NativeGameplayTags.h
+++ Engine/Source/Runtime/GameplayTags/Public/NativeGameplayTags.h
@@ -26,6 +26,22 @@ public:																		\
 		Manager.RemoveNativeGameplayTagSource(#ClassType);					\
 	}
 
+/**
+ * Useful for making a dedicated groups of native tags that are registered
+ * and unregistered as a block.  You define it as follows,
+ * 
+ *	struct FMyTagsForThings : public FNativeGameplayTagSource
+ *	{
+ *		DECLARE_NATIVE_GAMEPLAY_TAG_SOURCE(FMyTagsForThings)
+ *
+ *		FGameplayTag TagForThing1 = Add(TEXT("Thing.One"));
+ * 		FGameplayTag TagForThing2 = Add(TEXT("Thing.Two"));
+ *		// Add more tags here...
+ *	};
+ * 
+ * During your module startup you can register them together in your startup by
+ * calling FMyTagsForThings::Get()->Register(), and Unregister on module shutdown.
+ */
 class GAMEPLAYTAGS_API FNativeGameplayTagSource
 {
 public:
@@ -36,6 +52,9 @@ public:
 	virtual void Unregister() const = 0;
 
 protected:
+	/**
+	 * Call this during the struct member initialization to create the tags.
+	 */
 	FGameplayTag Add(FName TagName, const FString& TagDevComment = TEXT("(Native)"));
 
 private:
@@ -46,10 +65,27 @@ private:
 
 enum class ENativeGameplayTagToken { PRIVATE_USE_MACRO_INSTEAD };
 
+/**
+ * Declares a native gameplay tag that is defined in a cpp with UE_DEFINE_GAMEPLAY_TAG to allow other modules or code to use the created tag variable.
+ */
 #define UE_DECLARE_GAMEPLAY_TAG_EXTERN(TagName) extern FNativeGameplayTag TagName;
+
+/**
+ * Defines a native gameplay tag that is externally declared in a header to allow other modules or code to use the created tag variable.
+ */
 #define UE_DEFINE_GAMEPLAY_TAG(TagName, Tag) FNativeGameplayTag TagName(UE_PLUGIN_NAME, UE_MODULE_NAME, Tag, TEXT(""), ENativeGameplayTagToken::PRIVATE_USE_MACRO_INSTEAD);
+
+/**
+ * Defines a native gameplay tag such that it's only available to the cpp file you define it in.
+ */
 #define UE_DEFINE_GAMEPLAY_TAG_STATIC(TagName, Tag) static FNativeGameplayTag TagName(UE_PLUGIN_NAME, UE_MODULE_NAME, Tag, TEXT(""), ENativeGameplayTagToken::PRIVATE_USE_MACRO_INSTEAD);
 
+/**
+ * Holds a gameplay tag that was registered during static construction of the module, and will
+ * be unregistered when the module unloads.  Each registration is based on the native tag pointer
+ * so even if two modules register the same tag and one is unloaded, the tag will still be registered
+ * by the other one.
+ */
 class GAMEPLAYTAGS_API FNativeGameplayTag : public FNoncopyable
 {
 public:
