89c4c71bfb6e5d948af3f1775f15dc1be2ceb17f
simon tovey <Simon.Tovey@epicgames.com>
2021-02-17 18:30:41 -0500

    Making instance seleciton clear in SSessionBrowser.cpp
    #rb Stu.Mckenna
    
    #ROBOMERGE-SOURCE: CL 15448121 via CL 15448123 via CL 15448127
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15448139 by simon tovey in Main branch]

--- Engine/Source/Developer/SessionFrontend/Private/Widgets/Browser/SSessionBrowser.cpp
+++ Engine/Source/Developer/SessionFrontend/Private/Widgets/Browser/SSessionBrowser.cpp
@@ -404,7 +404,8 @@ TSharedRef<ITableRow> SSessionBrowser::HandleSessionTreeViewGenerateRow(TSharedP
 
 	return SNew(SSessionBrowserTreeInstanceRow, OwnerTable)
 		.Item(StaticCastSharedRef<FSessionBrowserInstanceTreeItem>(Item.ToSharedRef()))
-		.ToolTipText(this, &SSessionBrowser::HandleSessionTreeRowGetToolTipText, Item);
+		.ToolTipText(this, &SSessionBrowser::HandleSessionTreeRowGetToolTipText, Item)
+		.ShowSelection(true);
 }
 
 
