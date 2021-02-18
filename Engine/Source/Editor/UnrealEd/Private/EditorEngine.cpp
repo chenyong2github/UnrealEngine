f006823e90c56f6b1f6f215b9e9ead458889d813
Tony Wong <Tony.Wong@epicgames.com>
2021-02-18 16:40:42 -0500

    Editor: Fixed issue with IStreamingManager getting the wrong FOVScreenSize. #rb Richard.TalbotWatkin #fyi Richard.TalbotWatkin #rnx
    Merging
    to //UE4/Main/Engine/Source/Editor/UnrealEd/Private/EditorEngine.cpp
    
    [CL 15462616 by Tony Wong in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorEngine.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorEngine.cpp
@@ -2114,7 +2114,7 @@ bool UEditorEngine::UpdateSingleViewportClient(FEditorViewportClient* InViewport
 	// otherwise content for editor view can be streamed out if there are other views (ex: thumbnails)
 	if (InViewportClient->IsPerspective())
 	{
-		IStreamingManager::Get().AddViewInformation( InViewportClient->GetViewLocation(), InViewportClient->Viewport->GetSizeXY().X, InViewportClient->Viewport->GetSizeXY().X / FMath::Tan(InViewportClient->ViewFOV) );
+		IStreamingManager::Get().AddViewInformation( InViewportClient->GetViewLocation(), InViewportClient->Viewport->GetSizeXY().X, InViewportClient->Viewport->GetSizeXY().X / FMath::Tan(FMath::DegreesToRadians(InViewportClient->ViewFOV * 0.5f)) );
 	}
 	
 	// Only allow viewports to be drawn if we are not throttling for slate UI responsiveness or if the viewport client requested a redraw
