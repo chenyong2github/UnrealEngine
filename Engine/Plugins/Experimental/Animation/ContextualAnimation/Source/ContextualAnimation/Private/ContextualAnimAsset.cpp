3539b89e3691e88c0dcea1f7a9267af753d917fc
fernando coello <fernando.castillocoello@epicgames.com>
2021-02-22 17:16:53 -0400

    Contextual Anim - Removed deprecated file #jira none #review-15492994 trivial #fyi @jose.villarroel
    [CL 15494395 by fernando coello in ue5-main branch]

--- Engine/Plugins/Experimental/Animation/ContextualAnimation/Source/ContextualAnimation/Private/ContextualAnimAsset.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ContextualAnimAsset.h"
-
-UContextualAnimAsset::UContextualAnimAsset(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer) 
-{
-
-}
