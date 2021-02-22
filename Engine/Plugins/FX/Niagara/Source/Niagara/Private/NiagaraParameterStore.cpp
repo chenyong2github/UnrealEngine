fe55dc7e4e4f3aa244ae8e3462587e549a2d9b4d
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-02-22 02:07:08 -0500

    Downgrade Warning in NiagaraParameterStore to Verbose till we can investigate further #rb frank.fella
    #ROBOMERGE-SOURCE: CL 15454625 via CL 15454629 via CL 15454634 via CL 15454641
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15487749 by rob krajcarski in Main branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraParameterStore.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraParameterStore.cpp
@@ -722,14 +722,14 @@ void FNiagaraParameterStore::SanityCheckData(bool bInitInterfaces)
 				{
 					int32 NewNum = SrcIndex - DataInterfaces.Num() + 1;
 					DataInterfaces.AddZeroed(NewNum);
-					UE_LOG(LogNiagara, Warning, TEXT("Missing data interfaces! Had to add %d data interface entries to ParameterStore on %s"), NewNum , Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
+					UE_LOG(LogNiagara, Verbose, TEXT("Missing data interfaces! Had to add %d data interface entries to ParameterStore on %s"), NewNum , Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
 
 					OwnerDirtied = true;
 				}
 				if (DataInterfaces[SrcIndex] == nullptr && bInitInterfaces && Owner)
 				{
 					DataInterfaces[SrcIndex] = NewObject<UNiagaraDataInterface>(Owner, const_cast<UClass*>(Parameter.GetType().GetClass()), NAME_None, RF_Transactional | RF_Public);
-					UE_LOG(LogNiagara, Warning, TEXT("Had to initialize data interface! %s on %s"), *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
+					UE_LOG(LogNiagara, Verbose, TEXT("Had to initialize data interface! %s on %s"), *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
 
 					OwnerDirtied = true;
 				}
@@ -740,7 +740,7 @@ void FNiagaraParameterStore::SanityCheckData(bool bInitInterfaces)
 				{
 					int32 NewNum = SrcIndex - UObjects.Num() + 1;
 					UObjects.AddZeroed(NewNum);
-					UE_LOG(LogNiagara, Warning, TEXT("Missing UObject interfaces! Had to add %d UObject entries for %s on %s"), NewNum , *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
+					UE_LOG(LogNiagara, Verbose, TEXT("Missing UObject interfaces! Had to add %d UObject entries for %s on %s"), NewNum , *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
 
 					OwnerDirtied = true;
 				}
@@ -750,7 +750,7 @@ void FNiagaraParameterStore::SanityCheckData(bool bInitInterfaces)
 				int32 Size = Parameter.GetType().GetSize();
 				if (ParameterData.Num() < (SrcIndex + Size))
 				{
-					UE_LOG(LogNiagara, Warning, TEXT("Missing parameter data! %s on %s"), *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
+					UE_LOG(LogNiagara, Verbose, TEXT("Missing parameter data! %s on %s"), *Parameter.GetName().ToString(), Owner != nullptr ? *Owner->GetPathName() : TEXT("Unknown owner"));
 
 					OwnerDirtied = true;
 				}
@@ -766,7 +766,7 @@ void FNiagaraParameterStore::SanityCheckData(bool bInitInterfaces)
 
 	if (Owner && OwnerDirtied)
 	{
-		UE_LOG(LogNiagara, Warning, TEXT("%s needs to be resaved to prevent above warnings due to the parameter state being stale."), *Owner->GetFullName());
+		UE_LOG(LogNiagara, Verbose, TEXT("%s needs to be resaved to prevent above warnings due to the parameter state being stale."), *Owner->GetFullName());
 	}
 }
 
