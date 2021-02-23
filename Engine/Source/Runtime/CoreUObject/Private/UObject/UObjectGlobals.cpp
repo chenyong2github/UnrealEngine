1b4dfba4c6262bf5a886606a3fc55e43cdcae081
PJ Kack <PJ.Kack@epicgames.com>
2021-02-23 02:50:31 -0500

    Workaround for crash when tracking STAT_ConstructObject stats before the class itself has been added to the global uobject array.
    #jira UE-107567
    #rb carlmagnus.nordin
    #lockdown Cristina.Riveron
    #rnx
    
    #ushell-cherrypick of 15365029 by PJ.Kack
    
    [CL 15498702 by PJ Kack in 4.26 branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
@@ -3190,8 +3190,8 @@ UObject* StaticConstructObject_Internal(const FStaticConstructObjectParameters& 
 	check(Result != NULL);
 	// Don't call the constructor on recycled subobjects, they haven't been destroyed.
 	if (!bRecycledSubobject)
-	{		
-		STAT(FScopeCycleCounterUObject ConstructorScope(InClass, GET_STATID(STAT_ConstructObject)));
+	{
+		STAT(FScopeCycleCounterUObject ConstructorScope(InClass->GetFName().IsNone() ? nullptr : InClass, GET_STATID(STAT_ConstructObject)));
 		(*InClass->ClassConstructor)( FObjectInitializer(Result, InTemplate, Params.bCopyTransientsFromClassDefaults, true, Params.InstanceGraph) );
 	}
 	
