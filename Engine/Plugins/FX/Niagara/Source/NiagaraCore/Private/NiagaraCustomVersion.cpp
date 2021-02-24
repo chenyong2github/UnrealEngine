8b99e917e6a3e322b7929818a3893fae5b366058
stu mckenna <Stu.McKenna@epicgames.com>
2021-02-24 15:47:21 -0500

    - Bump version to fix issue with serialization after RT changes
    #rb none
    [FYI] shaun.kime
    #rnx
    
    #ROBOMERGE-SOURCE: CL 15521155 via CL 15521165 via CL 15521174
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15521210 by stu mckenna in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x3B921B8D, 0x31F94404, 0xBE937958, 0x8C8332AD);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0E40D592, 0xF773A646, 0x8FAFB7CD, 0xC06DD9D0);
