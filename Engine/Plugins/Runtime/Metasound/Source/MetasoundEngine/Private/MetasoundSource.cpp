bc1a872f2ab74fe2d2455bb6f5d53f56fe49cd65
rob gay <Rob.Gay@epicgames.com>
2021-02-24 16:46:24 -0400

    Fix OnPlay not triggering due to wrong copy constructor being used to initialize array member #rb trivial [FYI] phil.popp
    #ROBOMERGE-SOURCE: CL 15521182 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v771-15082668)
    
    [CL 15521187 by rob gay in ue5-main branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEngine/Private/MetasoundSource.cpp
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEngine/Private/MetasoundSource.cpp
@@ -467,7 +467,7 @@ const FMetasoundFrontendArchetype& UMetasoundSource::GetBaseArchetype()
 	{
 		FMetasoundFrontendArchetype Archetype;
 		
-		FMetasoundFrontendClassInput OnPlayTrigger;
+		FMetasoundFrontendClassVertex OnPlayTrigger;
 		OnPlayTrigger.Name = UMetasoundSource::GetOnPlayInputName();
 		OnPlayTrigger.Metadata.DisplayName = FText::FromString(OnPlayTrigger.Name);
 		OnPlayTrigger.TypeName = Metasound::Frontend::GetDataTypeName<Metasound::FTrigger>();
@@ -476,7 +476,7 @@ const FMetasoundFrontendArchetype& UMetasoundSource::GetBaseArchetype()
 
 		Archetype.Interface.Inputs.Add(OnPlayTrigger);
 
-		FMetasoundFrontendClassOutput OnFinished;
+		FMetasoundFrontendClassVertex OnFinished;
 		OnFinished.Name = UMetasoundSource::GetIsFinishedOutputName();
 		OnFinished.Metadata.DisplayName = FText::FromString(OnFinished.Name);
 		OnFinished.TypeName = Metasound::Frontend::GetDataTypeName<Metasound::FTrigger>();
