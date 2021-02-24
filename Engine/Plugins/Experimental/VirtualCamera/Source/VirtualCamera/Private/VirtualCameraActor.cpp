bb4d68f59345e71385a3bea97f0095eb5760d488
lily pitts <lily.pitts@magnopus.com>
2021-02-23 20:07:30 -0500

    #jira UE-107267 #rb David.Hibbitts #lockdown cristina.riveron
    Fixing VirtualCameraActor from spawning UI when component details
    
    #ROBOMERGE-SOURCE: CL 15510167 in //UE4/Release-4.26/... via CL 15510208 via CL 15510211
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v771-15082668)
    
    [CL 15510212 by lily pitts in Main branch]

--- Engine/Plugins/Experimental/VirtualCamera/Source/VirtualCamera/Private/VirtualCameraActor.cpp
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VirtualCamera/Private/VirtualCameraActor.cpp
@@ -26,6 +26,7 @@
 #include "VirtualCameraSubsystem.h"
 #include "VPFullScreenUserWidget.h"
 #include "Widgets/SVirtualWindow.h"
+#include "VCamComponent.h"
 
 #if WITH_EDITOR
 #include "AssetData.h"
@@ -349,6 +350,12 @@ void AVirtualCameraActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
 
 bool AVirtualCameraActor::StartStreaming()
 {
+	UVCamComponent* VCamComponent = Cast<UVCamComponent>(GetComponentByClass(UVCamComponent::StaticClass()));
+	if (!VCamComponent || !VCamComponent->IsEnabled())
+	{
+		return false;
+	}
+
 	ActorWorld = GetWorld();
 	if (!ActorWorld)
 	{
@@ -358,12 +365,6 @@ bool AVirtualCameraActor::StartStreaming()
 	if (bSaveSettingsOnStopStreaming)
 	{
 		LoadSettings();
-	}
-
-	if (!CameraUMGClass)
-	{
-		FSoftClassPath DefaultUMG(DefaultCameraUMG);
-		CameraUMGClass = DefaultUMG.TryLoadClass<UUserWidget>();
 	}
 
 #if WITH_EDITOR
