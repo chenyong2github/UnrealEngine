36da5d024ef7d948e81093e41dcb46006cb419d2
Charles deRousiers <Charles.deRousiers@epicgames.com>
2021-02-25 12:40:42 -0500

    Fix groom asset rebulding when global curve/vertex decimation factor are updated.
    #rb none
    #jira UE-106137
    #lockdown marcus.wassmer
    
    [CL 15530638 by Charles deRousiers in 4.26 branch]

--- Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Private/GroomComponent.cpp
+++ Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Private/GroomComponent.cpp
@@ -2220,8 +2220,12 @@ void UGroomComponent::PostLoad()
 #if WITH_EDITOR
 	if (GroomAsset && !bIsGroomAssetCallbackRegistered)
 	{
+		// Delegate used for notifying groom data invalidation
 		GroomAsset->GetOnGroomAssetChanged().AddUObject(this, &UGroomComponent::Invalidate);
+
+		// Delegate used for notifying groom data & groom resoures invalidation
 		GroomAsset->GetOnGroomAssetResourcesChanged().AddUObject(this, &UGroomComponent::InvalidateAndRecreate);
+
 		bIsGroomAssetCallbackRegistered = true;
 	}
 
