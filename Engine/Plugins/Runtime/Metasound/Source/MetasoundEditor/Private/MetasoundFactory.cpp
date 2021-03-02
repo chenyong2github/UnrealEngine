201bac7367f1a8322e954e53f322f55be0216862
rob gay <Rob.Gay@epicgames.com>
2021-03-02 01:26:04 -0400

    Disable default UMetasoundFactory for EA as asset type as it's not fleshed out yet #rb trivial #jira none
    #ROBOMERGE-SOURCE: CL 15566743 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v771-15082668)
    
    [CL 15566748 by rob gay in ue5-main branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundFactory.cpp
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundFactory.cpp
@@ -7,34 +7,35 @@
 #include "MetasoundEditorGraphNode.h"
 #include "MetasoundEditorGraphBuilder.h"
 
-UMetasoundFactory::UMetasoundFactory(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-	SupportedClass = UMetasound::StaticClass();
-
-	bCreateNew = true;
-	bEditorImport = false;
-	bEditAfterNew = true;
-}
-
-UObject* UMetasoundFactory::FactoryCreateNew(UClass* InClass, UObject* InParent, FName Name, EObjectFlags Flags, UObject* InContext, FFeedbackContext* InFeedbackContext)
-{
-	UMetasound* NewMetasound = NewObject<UMetasound>(InParent, Name, Flags);
-
-	FMetasoundFrontendClassMetadata Metadata;
-
-	Metadata.ClassName = FMetasoundFrontendClassName{TEXT(""), *NewMetasound->GetName(), TEXT("")};
-	Metadata.Version.Major = 1;
-	Metadata.Version.Minor = 0;
-	Metadata.Type = EMetasoundFrontendClassType::Graph;
-	Metadata.Author = FText::FromString(UKismetSystemLibrary::GetPlatformUserName());
-
-	NewMetasound->SetMetadata(Metadata);
-
-	NewMetasound->ConformDocumentToMetasoundArchetype();
-
-	return NewMetasound;
-}
+// TODO: Re-enable and potentially rename once composition is supported
+// UMetasoundFactory::UMetasoundFactory(const FObjectInitializer& ObjectInitializer)
+// 	: Super(ObjectInitializer)
+// {
+// 	SupportedClass = UMetasound::StaticClass();
+// 
+// 	bCreateNew = true;
+// 	bEditorImport = false;
+// 	bEditAfterNew = true;
+// }
+// 
+// UObject* UMetasoundFactory::FactoryCreateNew(UClass* InClass, UObject* InParent, FName Name, EObjectFlags Flags, UObject* InContext, FFeedbackContext* InFeedbackContext)
+// {
+// 	UMetasound* NewMetasound = NewObject<UMetasound>(InParent, Name, Flags);
+// 
+// 	FMetasoundFrontendClassMetadata Metadata;
+// 
+// 	Metadata.ClassName = FMetasoundFrontendClassName{TEXT(""), *NewMetasound->GetName(), TEXT("")};
+// 	Metadata.Version.Major = 1;
+// 	Metadata.Version.Minor = 0;
+// 	Metadata.Type = EMetasoundFrontendClassType::Graph;
+// 	Metadata.Author = FText::FromString(UKismetSystemLibrary::GetPlatformUserName());
+// 
+// 	NewMetasound->SetMetadata(Metadata);
+// 
+// 	NewMetasound->ConformDocumentToMetasoundArchetype();
+// 
+// 	return NewMetasound;
+// }
 
 UMetasoundSourceFactory::UMetasoundSourceFactory(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Public/MetasoundFactory.h
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Public/MetasoundFactory.h
@@ -8,15 +8,16 @@
 
 #include "MetasoundFactory.generated.h"
 
-UCLASS(hidecategories=Object, MinimalAPI)
-class UMetasoundFactory : public UFactory
-{
-	GENERATED_UCLASS_BODY()
-
-	//~ Begin UFactory Interface
-	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName Name, EObjectFlags Flags, UObject* InContext, FFeedbackContext* InFeedbackContext) override;
-	//~ Begin UFactory Interface
-};
+// TODO: Re-enable and potentially rename once composition is supported
+// UCLASS(hidecategories=Object, MinimalAPI)
+// class UMetasoundFactory : public UFactory
+// {
+// 	GENERATED_UCLASS_BODY()
+// 
+// 	//~ Begin UFactory Interface
+// 	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName Name, EObjectFlags Flags, UObject* InContext, FFeedbackContext* InFeedbackContext) override;
+// 	//~ Begin UFactory Interface
+// };
 
 UCLASS(hidecategories = Object, MinimalAPI)
 class UMetasoundSourceFactory : public UFactory
