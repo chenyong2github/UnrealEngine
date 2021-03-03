3526156052ba18f4b1bac6ee8610e670bd27f59e
Wei Liu <Wei.Liu@epicgames.com>
2021-03-02 21:48:59 -0500

    Fix a bug of getting miss-matched TextureLODSettings when cooking for WindowsNoEditor.
    #jira UE-108508
    
    #rb Josh.Adams, Dmitriy.Dyomin, Jack.Porter, Mi.Wang
    
    #lockdown Jack.Porter
    
    [CL 15578530 by Wei Liu in 4.26 branch]

--- Engine/Source/Runtime/Engine/Private/DeviceProfiles/DeviceProfileManager.cpp
+++ Engine/Source/Runtime/Engine/Private/DeviceProfiles/DeviceProfileManager.cpp
@@ -594,7 +594,7 @@ void UDeviceProfileManager::LoadProfiles()
 				ITargetPlatform* Platform = TargetPlatforms[PlatformIndex];
 
 				// Set TextureLODSettings
-				const UTextureLODSettings* TextureLODSettingsObj = FindProfile(Platform->IniPlatformName(), false);
+				const UTextureLODSettings* TextureLODSettingsObj = FindProfile(Platform->PlatformName(), false);
 				Platform->RegisterTextureLODSettings(TextureLODSettingsObj);
 			}
 		}
