9af5718f2c2e617b2b1b51dd2c06e52258d52e3e
rob gay <Rob.Gay@epicgames.com>
2021-03-03 13:19:43 -0400

    IWYU Fix [FYI] maxwell.hayes #jira UE-109868 #rb trivial
    #ROBOMERGE-SOURCE: CL 15584489 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v777-15581079)
    
    [CL 15584498 by rob gay in ue5-main branch]

--- Engine/Source/Runtime/AudioCodecEngine/Public/DecoderInputFactory.h
+++ Engine/Source/Runtime/AudioCodecEngine/Public/DecoderInputFactory.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "Templates/SharedPointer.h"
+#include "Templates/SharedPointerInternals.h"
 #include "Templates/UniquePtr.h"
 #include "AudioMixer.h"
 
@@ -17,7 +19,5 @@ namespace Audio
 	using FSoundWaveProxyPtr = TSharedPtr<FSoundWaveProxy, ESPMode::ThreadSafe>;
 
 	// Just loose for now.
-	AUDIOCODECENGINE_API TUniquePtr<IDecoderInput> CreateBackCompatDecoderInput(
-		FName InOldFormatName,
-		const FSoundWaveProxyPtr& InSoundWave );
+	AUDIOCODECENGINE_API TUniquePtr<IDecoderInput> CreateBackCompatDecoderInput(FName InOldFormatName, const FSoundWaveProxyPtr& InSoundWave);
 }
