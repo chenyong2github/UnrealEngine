50b96ce7a3e67dfb980774ec84e8059fbf0c8a1f
louise rasmussen <louise.rasmussen@epicgames.com>
2021-03-02 21:10:01 -0400

    Fixes Font Editor Spacing Issues
    #JIRA UETOOL-3026
    #rb trivial
    
    #ROBOMERGE-SOURCE: CL 15577060 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v771-15082668)
    
    [CL 15577062 by louise rasmussen in ue5-main branch]

--- Engine/Source/Editor/FontEditor/Private/SCompositeFontEditor.cpp
+++ Engine/Source/Editor/FontEditor/Private/SCompositeFontEditor.cpp
@@ -1526,6 +1526,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				SNew(SVerticalBox)
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SEditableTextBox)
@@ -1535,6 +1536,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				]
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SEditableTextBox)
@@ -1544,6 +1546,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				]
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SNumericEntryBox<int32>)
@@ -1570,6 +1573,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				SNew(SVerticalBox)
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SEditableTextBox)
@@ -1579,6 +1583,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				]
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SEditableTextBox)
@@ -1588,6 +1593,7 @@ void SCharacterRangeEditor::Construct(const FArguments& InArgs)
 				]
 
 				+SVerticalBox::Slot()
+				.AutoHeight()
 				.Padding(2.0f)
 				[
 					SNew(SNumericEntryBox<int32>)
