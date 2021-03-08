6fefa57a084eaf4634a71578bbbf44243273f926
Chris Babcock <Chris.Babcock@epicgames.com>
2021-03-08 11:22:07 -0500

    Disable HWCPipe on non-ARM architectures
    #jira UE-110110
    #ue4
    #android
    #review-15641139 @Dmytro.Vovk
    #rb Dmyto.Vovk
    
    [CL 15641228 by Chris Babcock in Main branch]

--- Engine/Source/Runtime/Core/Private/Android/AndroidStats.cpp
+++ Engine/Source/Runtime/Core/Private/Android/AndroidStats.cpp
@@ -7,7 +7,7 @@
 #include "HAL/IConsoleManager.h"
 #include "ProfilingDebugging/CsvProfiler.h"
 
-#if !UE_BUILD_SHIPPING && !PLATFORM_LUMIN
+#if !UE_BUILD_SHIPPING && !PLATFORM_LUMIN && (PLATFORM_ARM || PLATFORM_ARM64)
 #	define HWCPIPE_SUPPORTED 1
 #else
 #	define HWCPIPE_SUPPORTED 0
