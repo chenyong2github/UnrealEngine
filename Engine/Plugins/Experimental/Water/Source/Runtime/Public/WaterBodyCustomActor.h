18d5606459d5ac20df7acac9ba9dfca3dcb6e61f
kate ellis <kate.ellis@epicgames.com>
2021-03-08 21:08:15 -0500

    Back out changelist 15649597
    #ROBOMERGE-SOURCE: CL 15649881 via CL 15649889 via CL 15649892 via CL 15649901 via CL 15649908
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v779-15635321)
    
    [CL 15649917 by kate ellis in Main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Public/WaterBodyCustomActor.h
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Public/WaterBodyCustomActor.h
@@ -19,7 +19,6 @@ public:
 	virtual void Reset() override;
 	virtual void OnUpdateBody(bool bWithExclusionVolumes) override;
 	virtual TArray<UPrimitiveComponent*> GetCollisionComponents() const override;
-	void SetMaterial(UMaterialInterface* Material);
 
 private:
 	UPROPERTY(NonPIEDuplicateTransient)
@@ -42,7 +41,6 @@ protected:
 	/** AWaterBody Interface */
 	virtual void InitializeBody() override;
 	virtual bool IsBodyInitialized() const override { return !!CustomGenerator; }
-	virtual void BeginUpdateWaterBody() override;
 	virtual void UpdateWaterBody(bool bWithExclusionVolumes) override;
 
 #if WITH_EDITOR
