94decf198225e9875b2bdb47d43b209a238620b4
kevin ortegren <Kevin.Ortegren@epicgames.com>
2021-03-08 20:09:07 -0500

    Make sure that Custom Water Body Actors apply their fresh MID on the generated Static Mesh Component in game at run time. The MID is transient, so it will not make it over serialization.
    #rb jonathan.bard
    [REVIEW] [at]jonathan.bard
    #rnx
    
    #ROBOMERGE-SOURCE: CL 15649597 via CL 15649598 via CL 15649599 via CL 15649603 via CL 15649610
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v779-15635321)
    
    [CL 15649623 by kevin ortegren in Main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Public/WaterBodyCustomActor.h
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Public/WaterBodyCustomActor.h
@@ -19,6 +19,7 @@ public:
 	virtual void Reset() override;
 	virtual void OnUpdateBody(bool bWithExclusionVolumes) override;
 	virtual TArray<UPrimitiveComponent*> GetCollisionComponents() const override;
+	void SetMaterial(UMaterialInterface* Material);
 
 private:
 	UPROPERTY(NonPIEDuplicateTransient)
@@ -41,6 +42,7 @@ protected:
 	/** AWaterBody Interface */
 	virtual void InitializeBody() override;
 	virtual bool IsBodyInitialized() const override { return !!CustomGenerator; }
+	virtual void BeginUpdateWaterBody() override;
 	virtual void UpdateWaterBody(bool bWithExclusionVolumes) override;
 
 #if WITH_EDITOR
