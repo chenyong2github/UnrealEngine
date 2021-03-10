0eb4ac1c264542a33f8cea79d61fa9ecf08941c8
Alejandro Arango <alejandro.arango@epicgames.com>
2021-03-09 19:39:11 -0500

    TimeManagement: Added a genlocked fixed frame rate custom timestep.
    It is very similar to the Fixed Rate in Project Settings implemented in UnrealEngine.cpp, except that it keeps up with platform time by accounting for any missed frames when fps drops below the specified fixed rate.
    Also removed a property in FixedFrameRateCustomTimeStep deprecated since 4.21.
    
    #rb Simon.Therriault
    #jira UE-110111
    
    #ushell-cherrypick of 15660598 by Alejandro.Arango
    
    [CL 15662306 by Alejandro Arango in Main branch]

--- Engine/Source/Runtime/TimeManagement/Public/FixedFrameRateCustomTimeStep.h
+++ Engine/Source/Runtime/TimeManagement/Public/FixedFrameRateCustomTimeStep.h
@@ -18,12 +18,6 @@ class TIMEMANAGEMENT_API UFixedFrameRateCustomTimeStep : public UEngineCustomTim
 {
 	GENERATED_UCLASS_BODY()
 
-protected:
-	/** The fixed FrameRate */
-	UE_DEPRECATED(4.21, "The FixedFrameRateCustomTimeStep.FixedFrameRate is replaced by the function GetFixedFrameRate and will be removed from the codebase in a future release. Please use the function GetFixedFrameRate().")
-	UPROPERTY()
-	FFrameRate FixedFrameRate;
-
 public:
 	/** Get The fixed FrameRate */
 	virtual FFrameRate GetFixedFrameRate() const PURE_VIRTUAL(UFixedFrameRateCustomTimeStep::GetFixedFrameRate, return GetFixedFrameRate_PureVirtual(););
