c87fb8935d919ab325fadd00ff81256e746cf10f
jeremy moore <Jeremy.Moore@epicgames.com>
2021-03-10 20:51:40 -0400

    Change LumenSceneRender logic for whether nanite is enabled to match DeferredShadingRenderer. Previously if nanite show flags were serialized as off then it was possible to crash on start up because nanite global resources wouldn't be initialized before use in lumen. #rb krzysztof.narkowicz
    #ROBOMERGE-SOURCE: CL 15671901 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v779-15635321)
    
    [CL 15671906 by jeremy moore in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
@@ -2048,7 +2048,7 @@ void FDeferredShadingSceneRenderer::UpdateLumenScene(FRDGBuilder& GraphBuilder)
 				LumenCardRenderer.CardIdsToRender.Add(CardRenderData.CardIndex);
 			}
 
-			if (DoesPlatformSupportNanite(ShaderPlatform) && bAnyNaniteMeshes)
+			if (UseNanite(ShaderPlatform) && ViewFamily.EngineShowFlags.NaniteMeshes && bAnyNaniteMeshes)
 			{
 				TRACE_CPUPROFILER_EVENT_SCOPE(NaniteMeshPass);
 				QUICK_SCOPE_CYCLE_COUNTER(NaniteMeshPass);
