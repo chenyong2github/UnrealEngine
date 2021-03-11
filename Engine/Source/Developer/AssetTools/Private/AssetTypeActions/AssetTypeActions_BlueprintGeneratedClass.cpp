1b0e9852e955dd5baaaae2b41b851f3dfbbbb865
Dave Belanger <Dave.Belanger@epicgames.com>
2021-03-10 21:33:22 -0400

    fix wrong copyright #rb none #jira none
    [CL 15672369 by Dave Belanger in ue5-main branch]

--- Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_BlueprintGeneratedClass.cpp
+++ Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_BlueprintGeneratedClass.cpp
@@ -1,4 +1,4 @@
- // Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "AssetTypeActions/AssetTypeActions_BlueprintGeneratedClass.h"
 #include "Engine/BlueprintGeneratedClass.h"
