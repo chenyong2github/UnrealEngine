c034223334b729b386dbc2b0ffc795b3c870e0a7
bob tellez <Bob.Tellez@epicgames.com>
2021-03-11 19:48:14 -0500

    #UE4 Disable LibJpegTurbo for now
    [FYI] Chris.Norden
    
    #ROBOMERGE-SOURCE: CL 15671176 via CL 15671187 via CL 15671189 via CL 15671196 via CL 15683388
    #ROBOMERGE-BOT: RELEASE (Release-Engine-Staging -> Main) (v781-15675533)
    
    [CL 15683414 by bob tellez in Main branch]

--- Engine/Source/Runtime/ImageWrapper/ImageWrapper.Build.cs
+++ Engine/Source/Runtime/ImageWrapper/ImageWrapper.Build.cs
@@ -26,10 +26,11 @@ public class ImageWrapper : ModuleRules
 
 		// Add LibJpegTurbo for supported platforms
 		// **** NOTE - Only Win64 has been tested - other platforms are usable at your own risk, but have not been tested
-		if ((Target.Platform == UnrealTargetPlatform.Win64))/* ||
+		/*
+		if ((Target.Platform == UnrealTargetPlatform.Win64)) ||
 			(Target.Platform == UnrealTargetPlatform.Win32) ||
 			(Target.Platform == UnrealTargetPlatform.Mac) ||
-			(Target.IsInPlatformGroup(UnrealPlatformGroup.Unix) && Target.Architecture.StartsWith("x86_64")))*/
+			(Target.IsInPlatformGroup(UnrealPlatformGroup.Unix) && Target.Architecture.StartsWith("x86_64")))
 		{
 			PublicDefinitions.Add("WITH_LIBJPEGTURBO=1");
 			AddEngineThirdPartyPrivateStaticDependencies(Target, "LibJpegTurbo");
@@ -38,6 +39,8 @@ public class ImageWrapper : ModuleRules
 		{
 			PublicDefinitions.Add("WITH_LIBJPEGTURBO=0");
 		}
+		*/
+		PublicDefinitions.Add("WITH_LIBJPEGTURBO=0");
 
 		// Add openEXR lib for windows builds.
 		if ((Target.Platform == UnrealTargetPlatform.Win64) ||
