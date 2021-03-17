a2252c69e1672449af7dfa394d9f4a15cca13577
Arne Schober <Arne.Schober@epicgames.com>
2021-03-17 11:53:16 -0400

    U5 - make static analysis happy
    [CL 15725783 by Arne Schober in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Async/Fundamental/Scheduler.h
+++ Engine/Source/Runtime/Core/Public/Async/Fundamental/Scheduler.h
@@ -146,6 +146,7 @@ namespace LowLevelTasks
 
 			void Push(FSleepEvent* Item)
 			{
+				checkSlow(Item != nullptr);
 				checkSlow(reinterpret_cast<uintptr_t>(Item) < (1ull << 48));
 				checkSlow((reinterpret_cast<uintptr_t>(Item) & 0x7) == 0);
 				checkSlow(Item->Next == nullptr);
