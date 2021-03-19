86e8f97341703dd6b77533e5146f63b1b9034b10
Denys Dubinin <dennis@geodesic.games>
2021-03-19 10:22:25 -0400

    RemoteControlProtocolOSC
    #rb Jeremie.Roy
    
    [CL 15747065 by Denys Dubinin in Main branch]

---
+++ Engine/Plugins/VirtualProduction/RemoteControlProtocolOSC/Source/RemoteControlProtocolOSC/Private/RemoteControlProtocolOSC.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "RemoteControlProtocol.h"
+#include "RemoteControlProtocolBinding.h"
+
+#include "RemoteControlProtocolOSC.generated.h"
+
+struct FOSCMessage;
+
+/**
+ * OSC protocol entity for remote control binding
+ */
+USTRUCT()
+struct FRemoteControlOSCProtocolEntity : public FRemoteControlProtocolEntity
+{	
+	GENERATED_BODY()
+
+public:
+	//~ Begin FRemoteControlProtocolEntity interface
+	virtual FName GetRangePropertyName() const override { return NAME_FloatProperty; }
+	//~ End FRemoteControlProtocolEntity interface
+
+public:
+	/** OSC address in the form '/Container1/Container2/Method' */
+	UPROPERTY(EditAnywhere, Category = Mapping)
+	FName PathName;
+};
+
+/**
+ * OSC protocol implementation for Remote Control
+ */
+class FRemoteControlProtocolOSC : public FRemoteControlProtocol
+{
+public:
+	//~ Begin IRemoteControlProtocol interface
+	virtual void Bind(FRemoteControlProtocolEntityPtr InRemoteControlProtocolEntityPtr) override;
+	virtual void Unbind(FRemoteControlProtocolEntityPtr InRemoteControlProtocolEntityPtr) override;
+	virtual void UnbindAll() override;
+	virtual UScriptStruct* GetProtocolScriptStruct() const override { return FRemoteControlOSCProtocolEntity::StaticStruct(); }
+	//~ End IRemoteControlProtocol interface
+
+	/** Recieve OSC server message handler */
+	void OSCReceivedMessageEvent(const FOSCMessage& Message, const FString& IPAddress, uint16 Port);
+
+private:
+	/** Map of the OSC bindings */
+	TMap<FName, TArray<FRemoteControlProtocolEntityWeakPtr>> Bindings;
+
+public:
+	/** OSC protocol name */
+	static const FName ProtocolName;
+};
---
+++ Engine/Plugins/VirtualProduction/RemoteControlProtocolOSC/Source/RemoteControlProtocolOSC/Private/RemoteControlProtocolOSCModule.cpp
@@ -0,0 +1,67 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Modules/ModuleManager.h"
+
+#include "IRemoteControlProtocolModule.h"
+#include "RemoteControlProtocolOSC.h"
+#include "RemoteControlProtocolOSCSettings.h"
+
+#if WITH_EDITOR
+#include "ISettingsModule.h"
+#endif
+
+#define LOCTEXT_NAMESPACE "FRemoteControlProtocolOSCModule"
+
+/**
+ * OSC remote control module
+ */
+class FRemoteControlProtocolOSCModule : public IModuleInterface
+{
+public:
+	//~ Begin IModuleInterface
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+	//~ End IModuleInterface
+};
+
+void FRemoteControlProtocolOSCModule::StartupModule()
+{
+	FModuleManager::Get().LoadModuleChecked(TEXT("OSC"));
+
+#if WITH_EDITOR
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+
+	// Register OSC Remote Control global settings
+	if (SettingsModule != nullptr)
+	{
+		SettingsModule->RegisterSettings("Project", "Plugins", "Remote Control OSC Protocol",
+			LOCTEXT("ProjectSettings_Label", "Remote Control OSC Protocol"),
+			LOCTEXT("ProjectSettings_Description", "Configure OSC remote control plugin global settings"),
+			GetMutableDefault<URemoteControlProtocolOSCSettings>()
+		);
+	}
+#endif // WITH_EDITOR
+
+	URemoteControlProtocolOSCSettings* OSCSettings = GetMutableDefault<URemoteControlProtocolOSCSettings>();
+	
+	IRemoteControlProtocolModule::Get().AddProtocol(FRemoteControlProtocolOSC::ProtocolName, MakeShared<FRemoteControlProtocolOSC>());
+
+	// Init OSC servers after AddProtocol 
+	OSCSettings->InitOSCServers();
+}
+
+void FRemoteControlProtocolOSCModule::ShutdownModule()
+{
+#if WITH_EDITOR
+	// Unregister OSC Remote Control global settings
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule)
+	{
+		SettingsModule->UnregisterSettings("Project", "Plugins", "Remote Control OSC Protocol");
+	}
+#endif // WITH_EDITOR
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FRemoteControlProtocolOSCModule, RemoteControlProtocolOSC);
