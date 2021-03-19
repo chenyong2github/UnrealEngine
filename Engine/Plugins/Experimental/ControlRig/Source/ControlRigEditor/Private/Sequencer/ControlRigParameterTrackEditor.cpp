3a80b51250df3cbf08081c69d9447c0a083be4f0
Mike Zyracki <Mike.Zyracki@epicgames.com>
2021-03-19 18:14:54 -0400

    Sequencer:ControlRig: When selecting TopMost Category in Curve editor make sure we don't select all children channels. We do this by no longer suspending broadcast since that will resend the selections of the implicitly displayed curve models back to the curve editor.  Turning off sync handles this and is all we need. #jira UE-108359 #lockdown cristina.riveron #rb max.chen
    [CL 15751923 by Mike Zyracki in 4.26 branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/Sequencer/ControlRigParameterTrackEditor.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/Sequencer/ControlRigParameterTrackEditor.cpp
@@ -1315,7 +1315,6 @@ void FControlRigParameterTrackEditor::OnCurveDisplayChanged(FCurveModel* CurveMo
 					TGuardValue<bool> Guard(bIsDoingSelection, true);
 
 					bool bSync = GetSequencer()->GetSequencerSettings()->ShouldSyncCurveEditorSelection();
-					GetSequencer()->SuspendSelectionBroadcast();
 					GetSequencer()->GetSequencerSettings()->SyncCurveEditorSelection(false);
 					if (UnDisplayedChannels.Num() > 0)
 					{
@@ -1327,7 +1326,6 @@ void FControlRigParameterTrackEditor::OnCurveDisplayChanged(FCurveModel* CurveMo
 					}
 					GetSequencer()->RefreshTree();
 					GetSequencer()->GetSequencerSettings()->SyncCurveEditorSelection(bSync);
-					GetSequencer()->ResumeSelectionBroadcast();
 				}
 				UnDisplayedChannels.SetNum(0);
 				DisplayedChannels.SetNum(0);
