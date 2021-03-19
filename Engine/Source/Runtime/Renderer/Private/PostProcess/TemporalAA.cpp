f8ff90446a7ee4d11c1ac7d118367205a57e108e
Guillaume Abadie <Guillaume.Abadie@epicgames.com>
2021-03-19 12:29:56 -0400

    Temporily disable Gen5TAA on PCD3D_SM6 to avoid deal breaker artifacts
    #rb chris.waters
    #jira UE-111389
    
    [CL 15748313 by Guillaume Abadie in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalAA.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalAA.cpp
@@ -104,8 +104,9 @@ inline bool DoesPlatformSupportTemporalHistoryUpscale(EShaderPlatform Platform)
 
 inline bool DoesPlatformSupportGen5TAA(EShaderPlatform Platform)
 {
+	// TODO(UE-111389): Fixes artifact on SP_PCD3D_SM6
 	return (
-		(IsPCPlatform(Platform) && IsFeatureLevelSupported(Platform, ERHIFeatureLevel::SM5)) ||
+		(IsPCPlatform(Platform) && IsFeatureLevelSupported(Platform, ERHIFeatureLevel::SM5) && Platform != SP_PCD3D_SM6) ||
 		FDataDrivenShaderPlatformInfo::GetSupportsGen5TemporalAA(Platform));
 }
 
