bcf6a084e592d0de5405391f73c2e07fc957a970
andrean franc <andrean.franc@3lateral.com>
2021-03-22 08:41:54 -0400

    Change unnecessary UE4 comment to just UE #jira UE-111055 #rb trivial
    [CL 15758555 by andrean franc in Main branch]

--- Engine/Plugins/Animation/RigLogic/Source/RigLogicModule/Public/DNAReader.h
+++ Engine/Plugins/Animation/RigLogic/Source/RigLogicModule/Public/DNAReader.h
@@ -149,7 +149,7 @@ public:
 };
 
 /**
-	@brief UE4 interface for DNA Reader wrappers.
+	@brief UE interface for DNA Reader wrappers.
 */
 class IDNAReader : public IBehaviorReader, public IGeometryReader
 {
