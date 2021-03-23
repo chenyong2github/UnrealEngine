23ad9216a82bd66145a993af66d9b9bbabc639b8
dave hunter <dave.hunter@epicgames.com>
2021-03-23 10:28:58 -0400

    Update PLCrashReporter to add support for multiple Xcode versions.
    #rb will.damon
    #jira UE-111493
    #rnx
    
    [CL 15776886 by dave hunter in Main branch]

--- Engine/Source/ThirdParty/PLCrashReporter/PLCrashReporter.Build.cs
+++ Engine/Source/ThirdParty/PLCrashReporter/PLCrashReporter.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 using UnrealBuildTool;
 using System.IO;
+using Tools.DotNETCommon;
 
 public class PLCrashReporter : ModuleRules
 {
@@ -9,12 +10,35 @@ public class PLCrashReporter : ModuleRules
 		Type = ModuleType.External;
 
 		string PLGitRepoRoot = "PLCrashReporter";
+
 		string[] PLDefines = new string[] {};
 
 		string PLCrashReporterPath = Path.Combine(Target.UEThirdPartySourceDirectory,"PLCrashReporter");
 		string PLSourcePath = Path.Combine(PLCrashReporterPath,PLGitRepoRoot,"Source");
-		string PLLibPath = Path.Combine(PLCrashReporterPath, "lib");
 		string LibConfig = "Release";
+
+		string XcodeVersionOutput = Utils.RunLocalProcessAndReturnStdOut("xcodebuild", "-version");
+		string XcodeVersion;
+		using (var Reader = new StringReader(XcodeVersionOutput.Substring(6)))
+		{
+			XcodeVersion = Reader.ReadLine();
+		}
+
+		string[] VersionComponents = XcodeVersion.Split('.');
+		string CurrentLibFolder = "lib-Xcode-" + XcodeVersion;
+		string PLLibPath = Path.Combine(PLCrashReporterPath, "lib", CurrentLibFolder);
+
+		if ( !Directory.Exists( Path.Combine(Directory.GetCurrentDirectory(), PLLibPath) ) )
+		{
+			string DefaultLibFolder = "lib-Xcode-12.4";
+			if ( VersionComponents[0] == "11" )
+			{
+				DefaultLibFolder = "lib-Xcode-11.3.1";
+			}
+
+			Log.TraceInformationOnce("Couldn't find PLCrashReporter in folder '{0}', using default '{1}'", CurrentLibFolder, DefaultLibFolder);
+			PLLibPath = Path.Combine(PLCrashReporterPath, "lib", DefaultLibFolder);
+		}
 
 		if (Target.Platform == UnrealTargetPlatform.Mac || Target.Platform == UnrealTargetPlatform.IOS)
 		{
@@ -30,8 +54,7 @@ public class PLCrashReporter : ModuleRules
 			}
 
 			string Lib = Path.Combine(PLLibPath, Target.Platform.ToString(), LibConfig, "libCrashReporter.a");
-			PublicAdditionalLibraries.Add(Lib);		
-
+			PublicAdditionalLibraries.Add(Lib);
 			PublicDefinitions.AddRange(PLDefines);	
 		}
     }
