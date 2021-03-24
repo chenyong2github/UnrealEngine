0a03320922703bb334d67a491e35c45c11845c8f
sebastien lussier <Sebastien.Lussier@epicgames.com>
2021-03-24 16:01:19 -0400

    UPrimitiveComponent::SetLODParentPrimitive() - Skip unneeded call to ShouldGenerateAutoLOD() if InLODParentPrimitive is null #rb patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 15797004 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v783-15756269)
    
    [CL 15806962 by sebastien lussier in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Components/PrimitiveComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/PrimitiveComponent.cpp
@@ -3840,7 +3840,7 @@ void UPrimitiveComponent::SetLODParentPrimitive(UPrimitiveComponent * InLODParen
 		return nullptr;
 	}();
 
-	if (!GIsEditor || ShouldGenerateAutoLOD(ParentLODActor ? ParentLODActor->LODLevel - 1 : INDEX_NONE))
+	if (!GIsEditor || !InLODParentPrimitive || ShouldGenerateAutoLOD(ParentLODActor ? ParentLODActor->LODLevel - 1 : INDEX_NONE))
 #endif
 	{
 		// @todo, what do we do with old parent. We can't just reset undo parent because the parent might be used by other primitive
