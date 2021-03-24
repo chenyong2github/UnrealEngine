07ca06d0bee3b46cb48b4afcff2e78ccd315b21f
daniel wright <Daniel.Wright@epicgames.com>
2021-03-23 21:46:44 -0400

    Embree 3.12.2
    #ROBOMERGE-SOURCE: CL 15784439 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v783-15756269)
    
    [CL 15789950 by daniel wright in ue5-main branch]

---
+++ Engine/Source/ThirdParty/Intel/Embree/Embree3.Build.cs
@@ -0,0 +1,45 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Embree3 : ModuleRules
+{
+	public Embree3(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		// EMBREE
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/Win64/";
+
+			PublicIncludePaths.Add(SDKDir + "include");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/embree3.lib");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/tbb.lib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/embree3.dll", SDKDir + "lib/embree3.dll");
+			RuntimeDependencies.Add("$(TargetOutputDir)/tbb12.dll", SDKDir + "lib/tbb12.dll");
+			PublicDefinitions.Add("USE_EMBREE=1");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/MacOSX/";
+
+			PublicIncludePaths.Add(SDKDir + "include");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/libembree3.3.dylib");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/libembree3.dylib");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/libtbb.12.1.dylib");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/libtbb.12.dylib");
+			PublicAdditionalLibraries.Add(SDKDir + "lib/libtbb.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libembree3.3.dylib", SDKDir + "lib/libembree3.3.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libembree3.dylib", SDKDir + "lib/libembree3.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libtbb.12.1.dylib", SDKDir + "lib/libtbb.12.1.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libtbb.12.dylib", SDKDir + "lib/libtbb.12.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libtbb.dylib", SDKDir + "lib/libtbb.dylib");
+			PublicDefinitions.Add("USE_EMBREE=1");
+		}
+		else
+		{
+			PublicDefinitions.Add("USE_EMBREE=0");
+		}
+	}
+}
