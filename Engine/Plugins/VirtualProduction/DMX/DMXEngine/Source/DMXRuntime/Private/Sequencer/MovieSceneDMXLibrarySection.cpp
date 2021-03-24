da66200b8cf9a85930724dfdd7ed1abd46425cad
Flo Schar <flo@geodesic.games>
2021-03-24 01:29:10 -0400

    DMX - Fix sequencer tries to load tracks before libraries are loaded
    #rb trivial
    #jira UE-111784
    #lockdown simon.tourangeau
    
    [CL 15792639 by Flo Schar in 4.26 branch]

--- Engine/Plugins/VirtualProduction/DMX/DMXEngine/Source/DMXRuntime/Private/Sequencer/MovieSceneDMXLibrarySection.cpp
+++ Engine/Plugins/VirtualProduction/DMX/DMXEngine/Source/DMXRuntime/Private/Sequencer/MovieSceneDMXLibrarySection.cpp
@@ -9,6 +9,7 @@
 #include "Library/DMXEntityFixturePatch.h"
 #include "Library/DMXEntityFixtureType.h"
 
+#include "AssetRegistryModule.h"
 #include "Channels/MovieSceneChannelProxy.h"
 #include "Channels/MovieSceneChannelEditorData.h"
 #include "Evaluation/Blending/MovieSceneBlendType.h"
@@ -159,7 +160,9 @@ void UMovieSceneDMXLibrarySection::Serialize(FArchive& Ar)
 
 	if (Ar.IsLoading())
 	{
-		UpdateChannelProxy();
+		// Wait for all assets being loaded, only then data can be updated from the library and entities used in this section
+		IAssetRegistry& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(FName("AssetRegistry")).Get();
+		AssetRegistry.OnFilesLoaded().AddUObject(this, &UMovieSceneDMXLibrarySection::UpdateChannelProxy, false);
 	}
 }
 
