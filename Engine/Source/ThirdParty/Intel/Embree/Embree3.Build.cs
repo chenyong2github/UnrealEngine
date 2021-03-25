a04f6e385fd2d4b8a167d5ecb7c6fc2e16682af7
daniel wright <Daniel.Wright@epicgames.com>
2021-03-24 20:55:18 -0400

    Moved Embree3 dll's to Binaries/Thirdparty to hopefully solve inconsistent Incremental build failures (skips dll copy in build dependency)
    #ROBOMERGE-SOURCE: CL 15807872 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v783-15756269)
    
    [CL 15813888 by daniel wright in ue5-main branch]

--- Engine/Source/ThirdParty/Intel/Embree/Embree3.Build.cs
+++ Engine/Source/ThirdParty/Intel/Embree/Embree3.Build.cs
@@ -8,16 +8,17 @@ public class Embree3 : ModuleRules
 	{
 		Type = ModuleType.External;
 
+		string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/";
+		string EmbreeRuntimeLibsDir = Target.UEThirdPartyBinariesDirectory + "Intel/Embree/Embree3122/";
+
 		// EMBREE
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/Win64/";
-
-			PublicIncludePaths.Add(SDKDir + "include");
-			PublicAdditionalLibraries.Add(SDKDir + "lib/embree3.lib");
-			PublicAdditionalLibraries.Add(SDKDir + "lib/tbb.lib");
-			RuntimeDependencies.Add("$(TargetOutputDir)/embree3.dll", SDKDir + "lib/embree3.dll");
-			RuntimeDependencies.Add("$(TargetOutputDir)/tbb12.dll", SDKDir + "lib/tbb12.dll");
+			PublicIncludePaths.Add(SDKDir + "Win64/include"); 
+			PublicAdditionalLibraries.Add(SDKDir + "Win64/lib/embree3.lib");
+			PublicAdditionalLibraries.Add(SDKDir + "Win64/lib/tbb.lib");
+			RuntimeDependencies.Add(EmbreeRuntimeLibsDir + "Win64/lib/embree3.dll");
+			RuntimeDependencies.Add(EmbreeRuntimeLibsDir + "Win64/lib/tbb12.dll");
 			PublicDefinitions.Add("USE_EMBREE=1");
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Mac)
@@ -26,13 +27,12 @@ public class Embree3 : ModuleRules
 			PublicDefinitions.Add("USE_EMBREE=0");
 			/*
 
-			string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/MacOSX/";
-
-			PublicIncludePaths.Add(SDKDir + "include");
-			PublicAdditionalLibraries.Add(SDKDir + "lib/libembree3.3.dylib");
-			PublicAdditionalLibraries.Add(SDKDir + "lib/libtbb.12.1.dylib");
-			RuntimeDependencies.Add("$(TargetOutputDir)/libembree3.3.dylib", SDKDir + "lib/libembree3.3.dylib");
-			RuntimeDependencies.Add("$(TargetOutputDir)/libtbb.12.1.dylib", SDKDir + "lib/libtbb.12.1.dylib");
+			PublicIncludePaths.Add(SDKDir + "MacOSX/include");
+			PublicAdditionalLibraries.Add(SDKDir + "MacOSX/lib/libembree3.3.dylib");
+			PublicAdditionalLibraries.Add(SDKDir + "MacOSX/lib/libtbb.12.1.dylib");
+			//@todo - get rid of copy, put runtime dependency in Binaries/Thirdparty/Intel/Embree3122
+			RuntimeDependencies.Add("$(TargetOutputDir)/libembree3.3.dylib", SDKDir + "MacOSX/lib/libembree3.3.dylib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/libtbb.12.1.dylib", SDKDir + "MacOSX/lib/libtbb.12.1.dylib");
 			PublicDefinitions.Add("USE_EMBREE=1");
 */
 
