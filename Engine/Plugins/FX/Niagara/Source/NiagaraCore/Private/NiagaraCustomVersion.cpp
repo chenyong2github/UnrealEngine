0bc6694313c2b9c82d73102737274a3e7a404150
jamie hayes <jamie.hayes@epicgames.com>
2021-03-30 11:59:53 -0400

    Editgrate 15863104 - Initialize Niagara Previous values to the current value at the end of the spawn script to fix motion blur issues on the first frame of a particle's life.
    #rb shaun.kime
    #jira none
    
    [CL 15863314 by jamie hayes in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x1A34E90A, 0xFE754D2C, 0x90E32170, 0x87873B89);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xCC4763BF, 0xFF974D54, 0x822D3F16, 0x2A43C4FF);
