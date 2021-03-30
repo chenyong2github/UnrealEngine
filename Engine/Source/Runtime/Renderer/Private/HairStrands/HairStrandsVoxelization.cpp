ae1947052603074c0cb723da62993e22035885ba
charles derousiers <Charles.deRousiers@epicgames.com>
2021-03-30 13:48:32 -0400

    Workaround on a (driver?) precision issue on AMD Vega serie with hair strands voxelization.
    #rb none
    #jira MH-3040
    
    #ushell-cherrypick of 15825870 by Charles.deRousiers
    
    #ROBOMERGE-SOURCE: CL 15865149 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v786-15839533)
    
    [CL 15865159 by charles derousiers in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVoxelization.cpp
+++ Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVoxelization.cpp
@@ -1370,7 +1370,9 @@ static void AddVirtualVoxelGenerateMipPass(
 	FRDGBufferUAVRef PageIndexBufferUAV = GraphBuilder.CreateUAV(VoxelResources.PageIndexBuffer, PF_R32_UINT);
 	FRDGBufferUAVRef PageIndexOccupancyBufferUAV = GraphBuilder.CreateUAV(VoxelResources.PageIndexOccupancyBuffer, PF_R32G32_UINT);
 	
-	const bool bPatchEmptyPage = GHairVirtualVoxelInvalidEmptyPageIndex > 0;
+	// Note: Do not clear empty page on AMD hardware as there are some issue precision or dispatch issue (to be refined)
+	const bool bAMDPC = IsPCPlatform(View.GetShaderPlatform()) && IsRHIDeviceAMD();
+	const bool bPatchEmptyPage = GHairVirtualVoxelInvalidEmptyPageIndex > 0 && !bAMDPC;
 	if (bPatchEmptyPage)
 	{
 		const uint32 LastMipIt = MipCount - 1;
