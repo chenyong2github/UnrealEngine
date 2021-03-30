3ff6f8e1fcba5a6032cee671c90b00b0130030b8
mark lintott <mark.lintott@epicgames.com>
2021-03-30 11:01:19 -0400

    #jira UE-111654 #rb trivial Fixed missing UE4 comment in DirectoryWatcher
    #ROBOMERGE-SOURCE: CL 15862517 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v786-15839533)
    
    [CL 15862527 by mark lintott in ue5-main branch]

--- Engine/Source/Developer/DirectoryWatcher/Public/DirectoryWatcherModule.h
+++ Engine/Source/Developer/DirectoryWatcher/Public/DirectoryWatcherModule.h
@@ -20,7 +20,7 @@ public:
 	/** Gets the directory watcher singleton or returns NULL if the platform does not support directory watching */
 	virtual IDirectoryWatcher* Get();
 
-	/** Register external changes that the OS file watcher couldn't detect (eg, a file changing in a UE4 sandbox) */
+	/** Register external changes that the OS file watcher couldn't detect (eg, a file changing in a UE sandbox) */
 	virtual void RegisterExternalChanges(TArrayView<const FFileChangeData> FileChanges) const;
 
 private:
