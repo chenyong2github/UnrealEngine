c53fd4201c44d48eae8bc853d122304374ff8bd5
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2021-03-30 11:09:21 -0400

    Suppress pointer de-referencing warning for the whole function when doing MSVC static analysis. The incoming pointer will never be null but SA won't take the hint.
    #jira none
    #fyi arne.schober
    #rnx
    
    [CL 15862729 by halfdan ingvarsson in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Async/Fundamental/Scheduler.h
+++ Engine/Source/Runtime/Core/Public/Async/Fundamental/Scheduler.h
@@ -152,33 +152,35 @@ namespace LowLevelTasks
 
 			void Push(FSleepEvent* Item)
 			{
-#if USING_CODE_ANALYSIS
-				if(Item != nullptr)
+#if defined(_MSC_VER) && USING_CODE_ANALYSIS
+#pragma warning(push)
+#pragma warning(disable:6011) // Dereferencing NULL pointer 'Item'
 #endif
+				check(Item != nullptr);
+				checkSlow(reinterpret_cast<uintptr_t>(Item) < (1ull << 48));
+				checkSlow((reinterpret_cast<uintptr_t>(Item) & 0x7) == 0);
+				checkSlow(Item->Next == nullptr);
+			
+				FTopNode LocalTop = Top.load(std::memory_order_relaxed);
+				while (true) 
 				{
-					check(Item != nullptr);
-					checkSlow(reinterpret_cast<uintptr_t>(Item) < (1ull << 48));
-					checkSlow((reinterpret_cast<uintptr_t>(Item) & 0x7) == 0);
-					checkSlow(Item->Next == nullptr);
-				
-					FTopNode LocalTop = Top.load(std::memory_order_relaxed);
-					while (true) 
+#if DO_CHECK
+					int64 LastRevision = int64(LocalTop.Revision); 
+#endif
+					Item->Next = reinterpret_cast<FSleepEvent*>(LocalTop.Address << 3);
+					if (Top.compare_exchange_weak(LocalTop, FTopNode { reinterpret_cast<uintptr_t>(Item) >> 3, uintptr_t(LocalTop.Revision + 1) }, std::memory_order_release, std::memory_order_acquire))  
 					{
-#if DO_CHECK
-						int64 LastRevision = int64(LocalTop.Revision); 
-#endif
-						Item->Next = reinterpret_cast<FSleepEvent*>(LocalTop.Address << 3);
-						if (Top.compare_exchange_weak(LocalTop, FTopNode { reinterpret_cast<uintptr_t>(Item) >> 3, uintptr_t(LocalTop.Revision + 1) }, std::memory_order_release, std::memory_order_acquire))  
-						{
-							return;
-						}
-#if DO_CHECK
-						int64 NewRevision = int64(LocalTop.Revision) < LastRevision ? ((1ll << 19) + int64(LocalTop.Revision)) : int64(LocalTop.Revision);
-						ensureMsgf((NewRevision - LastRevision) < (1ll << 18), TEXT("Dangerously close to the wraparound: %d, %d"), LastRevision, NewRevision);
-#endif
+						return;
 					}
+#if DO_CHECK
+					int64 NewRevision = int64(LocalTop.Revision) < LastRevision ? ((1ll << 19) + int64(LocalTop.Revision)) : int64(LocalTop.Revision);
+					ensureMsgf((NewRevision - LastRevision) < (1ll << 18), TEXT("Dangerously close to the wraparound: %d, %d"), LastRevision, NewRevision);
+#endif
 				}
 			}
+#if defined(_MSC_VER) && USING_CODE_ANALYSIS
+#pragma warning(pop)
+#endif
 		};
 
 	public:
