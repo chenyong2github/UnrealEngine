a134ca98b3514389b9864e896b592474bdb4b9a0
Jeff Farris <jeff.farris@epicgames.com>
2021-03-31 16:00:39 -0400

    Removed a MaterialInstance assert that trips erroneously when doing `memreport -full`
    #rb ben.ingram
    
    [CL 15880380 by Jeff Farris in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
@@ -3379,7 +3379,7 @@ void UMaterialInstance::Serialize(FArchive& Ar)
 #endif // WITH_EDITOR
 
 #if !WITH_EDITORONLY_DATA
-	checkf(bSavedCachedData, TEXT("MaterialInstance %s must have saved cached data, if editor-only data is not present"), *GetName());
+	//checkf(bSavedCachedData, TEXT("MaterialInstance %s must have saved cached data, if editor-only data is not present"), *GetName());
 #endif
 	if (bSavedCachedData)
 	{
