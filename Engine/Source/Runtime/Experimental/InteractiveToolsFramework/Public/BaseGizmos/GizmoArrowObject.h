c20b76c72439d4c47339d0859795589a76710b96
Christina TempelaarL <christina.tempelaarl@epicgames.com>
2021-04-04 19:31:18 -0400

    Added UGizmo*Object classes which can be used to define, render and hit test various geometric primitives and/or composites of primitives such as Arrow. These are similar to UGizmo*Components. #jira UETOOL-2313 #rb ryan.schmidt #preflight 606a3babed84460001bc2c89
    [CL 15912790 by Christina TempelaarL in ue5-main branch]

---
+++ Engine/Source/Runtime/Experimental/InteractiveToolsFramework/Public/BaseGizmos/GizmoArrowObject.h
@@ -0,0 +1,55 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseGizmos/GizmoBaseObject.h"
+#include "BaseGizmos/GizmoBoxObject.h"
+#include "BaseGizmos/GizmoConeObject.h"
+#include "BaseGizmos/GizmoCylinderObject.h"
+#include "InputState.h"
+#include "UObject/ObjectMacros.h"
+#include "GizmoArrowObject.generated.h"
+
+/**
+ * Simple arrow object intended to be used as part of 3D Gizmos.
+ * Draws a solid 3D arrow based with a cylinder body and
+ * cone or box head.
+ */
+UCLASS(Transient)
+class INTERACTIVETOOLSFRAMEWORK_API UGizmoArrowObject : public UGizmoBaseObject
+{
+	GENERATED_BODY()
+
+public:
+	UGizmoArrowObject();
+
+	//~ Begin UGizmoBaseObject Interface.
+	virtual void Render(IToolsContextRenderAPI* RenderAPI) override;
+	virtual FInputRayHit LineTraceObject(const FVector RayOrigin, const FVector RayDirection) override;
+	virtual void SetHoverState(bool bHoveringIn) override;
+	virtual void SetInteractingState(bool bInteractingIn) override;
+	virtual void SetWorldLocalState(bool bWorldIn) override;
+	virtual void SetVisibility(bool bVisibleIn) override;
+	virtual void SetLocalToWorldTransform(FTransform LocalToWorldTransformIn) override;
+	virtual void SetGizmoScale(float InGizmoScale) override;
+	virtual void SetMaterial(UMaterialInterface* InMaterial) override;
+	virtual void SetCurrentMaterial(UMaterialInterface* InCurrentMaterial) override;
+	//~ End UGizmoBaseObject Interface.
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = Options)
+	int32 bHasConeHead = true;
+		
+	UPROPERTY(EditAnywhere, Category = Options)
+	FVector Direction = FVector(1.0f, 0.0f, 0.0f);
+
+	// Arrow origin is located at (Direction * Offset) 
+	UPROPERTY(EditAnywhere, Category = Options)
+	float Offset = 0.0f;
+
+	TObjectPtr<UGizmoCylinderObject> CylinderObject;
+	TObjectPtr<UGizmoConeObject> ConeObject;
+	TObjectPtr<UGizmoBoxObject> BoxObject;
+};
