0e7f38b405e018ac088317bf93fbd0985fcfc005
florin pascu <Florin.Pascu@epicgames.com>
2021-04-05 15:04:53 -0400

    Set Mobile FXAA to disabled when postprocessing is disabled #jira UE-110113 #rb trivial #preflight 606b2da8a595390001baeb4e
    #ROBOMERGE-SOURCE: CL 15918953 in //UE5/Release-5.0-EarlyAccess/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0-EarlyAccess -> Main) (v786-15839533)
    
    [CL 15921191 by florin pascu in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
@@ -1894,6 +1894,7 @@ void AddMobilePostProcessingPasses(FRDGBuilder& GraphBuilder, FScene* Scene, con
 		PassSequence.SetEnabled(EPass::SunMerge, false);
 		PassSequence.SetEnabled(EPass::SeparateTranslucency, false);
 		PassSequence.SetEnabled(EPass::TAA, false);
+		PassSequence.SetEnabled(EPass::FXAA, false);
 		PassSequence.SetEnabled(EPass::PostProcessMaterialAfterTonemapping, false);
 		PassSequence.SetEnabled(EPass::FXAA, false);
 		PassSequence.Finalize();
