fbfb8b2c19a0becb48c989ce92e597b296bcf89e
Jeremy Moore <Jeremy.Moore@epicgames.com>
2021-04-06 09:53:53 -0400

    Fix static analysis warning. Also fix code comment. #jira none #rb none
    [CL 15929830 by Jeremy Moore in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/Factories/EditorFactories.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Factories/EditorFactories.cpp
@@ -4661,27 +4661,30 @@ UObject* UTextureFactory::FactoryCreateBinary
 
 	UTexture2D* Texture2D = Cast<UTexture2D>(Texture);
 
-	// If the texture is larger than a certain threshold make it VT. This is explicitly done after the
-	// application of the existing settings above, so if a texture gets reimported at a larger size it will
-	// still be properly flagged as a VT (note: What about reimporting at a lower resolution?)
-	static const auto CVarVirtualTexturesEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VirtualTextures")); 
-	check(CVarVirtualTexturesEnabled);
-
-	if (CVarVirtualTexturesEnabled->GetValueOnAnyThread())
+	if (Texture2D)
 	{
-		const int32 VirtualTextureAutoEnableThreshold = GetDefault<UTextureImportSettings>()->AutoVTSize;
-		const int32 VirtualTextureAutoEnableThresholdPixels = VirtualTextureAutoEnableThreshold * VirtualTextureAutoEnableThreshold;
+		// If the texture is larger than a certain threshold make it VT.
+		// Note that previously for re-imports we still checked size and potentially changed the VT status.
+		// But that was unintuitive for many users so now for re-imports we will end up ignoring this and respecting the existing setting below.
+		static const auto CVarVirtualTexturesEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VirtualTextures")); 
+		check(CVarVirtualTexturesEnabled);
 
-		// We do this in pixels so a 8192 x 128 texture won't get VT enabled 
-		// We use the Source size instead of simple Texture2D->GetSizeX() as this uses the size of the platform data
-		// however for a new texture platform data may not be generated yet, and for an reimport of a texture this is the size of the
-		// old texture. 
-		// Using source size gives one small caveat. It looks at the size before mipmap power of two padding adjustment.
-		// Textures with more than 1 block (UDIM textures) must be imported as VT
-		if (Texture->Source.GetNumBlocks() > 1 ||
-			Texture2D->Source.GetSizeX() * Texture2D->Source.GetSizeY() >= VirtualTextureAutoEnableThresholdPixels)
+		if (CVarVirtualTexturesEnabled->GetValueOnAnyThread())
 		{
-			Texture2D->VirtualTextureStreaming = true;
+			const int32 VirtualTextureAutoEnableThreshold = GetDefault<UTextureImportSettings>()->AutoVTSize;
+			const int32 VirtualTextureAutoEnableThresholdPixels = VirtualTextureAutoEnableThreshold * VirtualTextureAutoEnableThreshold;
+
+			// We do this in pixels so a 8192 x 128 texture won't get VT enabled 
+			// We use the Source size instead of simple Texture2D->GetSizeX() as this uses the size of the platform data
+			// however for a new texture platform data may not be generated yet, and for an reimport of a texture this is the size of the
+			// old texture. 
+			// Using source size gives one small caveat. It looks at the size before mipmap power of two padding adjustment.
+			// Textures with more than 1 block (UDIM textures) must be imported as VT
+			if (Texture->Source.GetNumBlocks() > 1 ||
+				Texture2D->Source.GetSizeX() * Texture2D->Source.GetSizeY() >= VirtualTextureAutoEnableThresholdPixels)
+			{
+				Texture2D->VirtualTextureStreaming = true;
+			}
 		}
 	}
 
