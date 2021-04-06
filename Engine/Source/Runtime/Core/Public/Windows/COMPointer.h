d75fe7c9d995ef015b29b022cb01c98eada8385f
Steve Robb <Steve.Robb@epicgames.com>
2021-04-06 16:24:49 -0400

    COMPointer.h redirection header added.
    #jira UE-112899
    #rb david.harvey
    
    [CL 15934461 by Steve Robb in Main branch]

---
+++ Engine/Source/Runtime/Core/Public/Windows/COMPointer.h
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#pragma message(__FILE__"(5): warning : #include Microsoft/COMPointer.h instead of Windows/COMPointer.h")
+
+#include "Microsoft/COMPointer.h"
