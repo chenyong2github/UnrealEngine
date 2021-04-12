30d733f94f93502407f445e1c55b851c8e2ab135
Helge Mathee <Helge.Mathee@epicgames.com>
2021-04-12 12:39:55 -0400

    Control Rig: Only set curves on anim instance that have really changed
    #rb sara.schvartzman
    #jira na
    
    [CL 15979031 by Helge Mathee in ue5-main branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/AnimNode_ControlRigBase.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRig/Private/AnimNode_ControlRigBase.cpp
@@ -181,8 +181,15 @@ void FAnimNode_ControlRigBase::UpdateOutput(UControlRig* ControlRig, FPoseContex
 			const uint16 Index = Iter.Value();
 			const FRigElementKey Key(Name, ERigElementType::Curve);
 
+			const float PreviousValue = InOutput.Curve.Get(Index);
 			const float Value = ControlRig->GetHierarchy()->GetCurveValue(Key);
-			InOutput.Curve.Set(Index, Value);
+
+			if(!FMath::IsNearlyEqual(PreviousValue, Value))
+			{
+				// this causes a side effect of marking the curve as "valid"
+				// so only apply it for curves that have really changed
+				InOutput.Curve.Set(Index, Value);
+			}
 		}
 	}
 }
