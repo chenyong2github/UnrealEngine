b0e091d465a80b03b35837098e13d84ee22becfc
Ben Marsh <Ben.Marsh@epicgames.com>
2021-04-13 13:49:31 -0400

    Remove "fake" warning causing non-deterministic build health issue.
    #rb none
    #fyi Steve.Robb
    #jira UE-113256
    
    [CL 15992066 by Ben Marsh in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Windows/COMPointer.h
+++ Engine/Source/Runtime/Core/Public/Windows/COMPointer.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
-#pragma message(__FILE__"(5): warning : #include Microsoft/COMPointer.h instead of Windows/COMPointer.h")
+// Producing a warning that is not treated as an error here calls non-deterministic incremental build warnings
+//#pragma message(__FILE__"(5): warning : #include Microsoft/COMPointer.h instead of Windows/COMPointer.h")
 
 #include "Microsoft/COMPointer.h"
