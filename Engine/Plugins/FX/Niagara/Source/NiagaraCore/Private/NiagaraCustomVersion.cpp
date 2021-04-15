d91adae4678b64d1791dade96dd75ff748f24266
Shaun Kime <Shaun.Kime@epicgames.com>
2021-04-15 18:47:45 -0400

    Handling edge case in previous variable definition where the attribute existed later in the script order, causing mismatches in outputs across scripts. #jira UE-111939 #rb jamie.hayes #preflight 6078b6db61a9060001194e46
    [CL 16027757 by Shaun Kime in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0FC7D74E, 0x22166A42, 0xBFA2127F, 0xC334B412);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xB25900FB, 0xC1D24C63, 0x8D25CC89, 0x026FF460);
