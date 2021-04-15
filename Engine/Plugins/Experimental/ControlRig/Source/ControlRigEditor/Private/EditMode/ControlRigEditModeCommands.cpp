8bd33f73443b0cc5c18f8f793a584bde4cfde3c4
mike zyracki <Mike.Zyracki@epicgames.com>
2021-04-15 16:35:36 -0400

    Control Rig: Escape removes selection and Ctrl toggles selection #jira UE-112959 #rb max.chen #lockdown Cristina.Riveron
    #ROBOMERGE-SOURCE: CL 16008230 via CL 16010285
    #ROBOMERGE-BOT: (v789-15992632)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16025934 by mike zyracki in 4.27 branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditModeCommands.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditModeCommands.cpp
@@ -10,6 +10,7 @@ void FControlRigEditModeCommands::RegisterCommands()
 	UI_COMMAND(ResetAllTransforms, "Reset All Transform", "Reset all of the Controls Transforms", EUserInterfaceActionType::Button, FInputChord(EKeys::G, EModifierKey::Control | EModifierKey::Shift));
 	UI_COMMAND(ToggleManipulators, "Toggle Manipulators", "Toggles visibility of manipulators in the viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::T));
 	UI_COMMAND(FrameSelection, "Frame Selection", "Focus the viewport on the current selection", EUserInterfaceActionType::Button, FInputChord(EKeys::F));
+	UI_COMMAND(ClearSelection, "Clear Selection", "Clear Selection", EUserInterfaceActionType::Button, FInputChord(EKeys::Escape));
 
 	UI_COMMAND(IncreaseGizmoSize, "Increase Gizmo Size", "Increase Size of the Gizmos In The Viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::Equals, EModifierKey::Shift));
 	UI_COMMAND(DecreaseGizmoSize, "Decrease Gizmo Size", "Decrease Size of the Gizmos In The Viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::Hyphen, EModifierKey::Shift));
