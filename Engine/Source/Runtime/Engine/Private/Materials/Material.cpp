cf4aca81282edd99ec5c8e9a3ac5d7c45b400c68
jason nadro <jason.nadro@epicgames.com>
2021-04-20 14:55:32 -0400

    Add function to gather referenced material parameter collections for material instances. This fixes crashes when failing to find material parameter caches by GUID.
    - Adds AppendReferencedParameterCollectionIdsTo function to MaterialInstances similar to UMaterial.
    - When we create a MaterialID be sure to call this function for MaterialInstances to gather referenced material parameter collections.
    
    #rb Lauren.Barnes, Francis.Hurteau
    [at]Ben.Ingram, [at]Lauren.Barnes
    #jira FORT-316488
    
    
    #ushell-cherrypick of 15613848 by jason.nadro
    
    #ROBOMERGE-SOURCE: CL 15613813 in //UE5/Release-5.0-EarlyAccess/... via CL 16063665
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16064519 by jason nadro in Main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
@@ -292,6 +292,7 @@ void FMaterialResource::GetShaderMapId(EShaderPlatform Platform, const ITargetPl
 	if(MaterialInstance)
 	{
 		MaterialInstance->GetBasePropertyOverridesHash(OutId.BasePropertyOverridesHash);
+		MaterialInstance->AppendReferencedParameterCollectionIdsTo(OutId.ReferencedParameterCollections);
 
 		FStaticParameterSet CompositedStaticParameters;
 		MaterialInstance->GetStaticParameterValues(CompositedStaticParameters);
