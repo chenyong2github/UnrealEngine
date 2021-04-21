6a2dca688d5c439ecc62526b0e139f634961b6aa
emil kirichev <Emil.Kirichev@xa.epicgames.com>
2021-04-21 07:17:00 -0400

    Dataprep: Fix warning about trying to mount non-existent package directory
    #jira UE-110936
    #rb JeanLuc.Corenthin
    
    #ROBOMERGE-SOURCE: CL 16071126 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16071127 by emil kirichev in Main branch]

--- Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepCore/Private/DataprepCoreModule.cpp
+++ Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepCore/Private/DataprepCoreModule.cpp
@@ -10,8 +10,10 @@ class FDataprepCoreModule : public IModuleInterface
 public:
 	virtual void StartupModule() override
 	{
+		IFileManager::Get().MakeDirectory( *DataprepCorePrivateUtils::GetRootTemporaryDir(), true );
+
 		// Register mount point for Dataprep core library root package folder
-		FPackageName::RegisterMountPoint( DataprepCorePrivateUtils::GetRootPackagePath() + TEXT("/"), DataprepCorePrivateUtils::GetRootTemporaryDir()	);
+		FPackageName::RegisterMountPoint( DataprepCorePrivateUtils::GetRootPackagePath() + TEXT("/"), DataprepCorePrivateUtils::GetRootTemporaryDir() );
 	}
 
 	virtual void ShutdownModule() override
