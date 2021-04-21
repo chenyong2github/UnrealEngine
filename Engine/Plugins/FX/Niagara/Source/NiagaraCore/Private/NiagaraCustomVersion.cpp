e54c2dbb7a9377bf431d31d2ea52d0be370834d3
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-04-21 12:20:56 -0400

    Bump Niagars's custom version to unblock people getting bogus data from the DDC #jira FROST-2298 #rb stu.mckenna
    [CL 16075396 by rob krajcarski in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x356395E7, 0x3682A948, 0x85C91E8A, 0x762086B9);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xC5531FA3, 0x1FA74F4F, 0x8B391920, 0xD86B9FE3);
