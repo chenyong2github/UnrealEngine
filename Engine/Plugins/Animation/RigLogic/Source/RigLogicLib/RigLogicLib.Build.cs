b4e0bc5f8ad63eeebad1eaa757ac31fd7a162de0
andrean franc <andrean.franc@3lateral.com>
2021-04-22 11:02:25 -0400

    Disable usage of SSSE3 intrinsics by RigLogic on platforms that might not support it #jira UE-109088 #rb halfdan.ingvarsson
    [CL 16093425 by andrean franc in 4.27 branch]

--- Engine/Plugins/Animation/RigLogic/Source/RigLogicLib/RigLogicLib.Build.cs
+++ Engine/Plugins/Animation/RigLogic/Source/RigLogicLib/RigLogicLib.Build.cs
@@ -58,7 +58,6 @@ public class RigLogicLib : ModuleRules
                 Target.Platform == UnrealTargetPlatform.Linux ||
                 Target.Platform == UnrealTargetPlatform.Mac)
         {
-            PublicDefinitions.Add("TERSE_ENABLE_SSE=1");
             PrivateDefinitions.Add("RL_BUILD_WITH_SSE=1");
         }
     }
