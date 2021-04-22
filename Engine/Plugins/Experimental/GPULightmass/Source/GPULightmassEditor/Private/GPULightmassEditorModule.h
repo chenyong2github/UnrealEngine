414e5245134c76785b7e9f445ce93b0621c609be
louise rasmussen <louise.rasmussen@epicgames.com>
2021-04-22 17:59:11 -0400

    GPU Lightmass panel, adds Realtime Toggle, workflow improvements, updates messages for clarity.
    #JIRA UE-109171
    #JIRA UE-108890
    #rb Lauren.Barnes
    [FYI] Yujiang.Wang
    #preflight 6081c7ea590e4a00011d00cb
    
    #ROBOMERGE-SOURCE: CL 16099171 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16099224 by louise rasmussen in Main branch]

--- Engine/Plugins/Experimental/GPULightmass/Source/GPULightmassEditor/Private/GPULightmassEditorModule.h
+++ Engine/Plugins/Experimental/GPULightmass/Source/GPULightmassEditor/Private/GPULightmassEditorModule.h
@@ -17,28 +17,28 @@ class STextBlock;
 
 enum class EMapChangeType : uint8;
 
-class FGPULightmassEditorModule : public IModuleInterface, public FTickableEditorObject
+class FGPULightmassEditorModule : public IModuleInterface
 {
 public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 
-	// Begin FTickableObjectBase interface
-	virtual void Tick(float DeltaTime) override;
-	virtual bool IsTickable() const override;
-	virtual TStatId GetStatId() const override;
-	// End FTickableObjectBase interface
-
 	TSharedPtr<IDetailsView> SettingsView;
 	TSharedRef<SDockTab> SpawnSettingsTab(const FSpawnTabArgs& Args);
 	void RegisterTabSpawner();
-	FReply OnStartStopClicked();
 	void OnMapChanged(UWorld* InWorld, EMapChangeType MapChangeType);
 
 	TSharedRef<FExtender> OnExtendLevelEditorBuildMenu(const TSharedRef<FUICommandList> CommandList);
 	void CreateBuildMenu(FMenuBuilder& Builder);
 
-	TSharedPtr<STextBlock> StartStopButtonText;
+	static bool IsRealtimeOn();
+	static bool IsRunning();
+	static bool IsBakeWhatYouSeeMode();
+
+	FReply OnStartClicked();
+	FReply OnSaveAndStopClicked();
+	FReply OnCancelClicked();
+
 	TSharedPtr<STextBlock> Messages;
 };
