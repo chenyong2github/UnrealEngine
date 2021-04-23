14b7993d020f5c3e04ac4647cf886783e5d74d98
Denys Dubinin <dennis@geodesic.games>
2021-04-23 07:13:36 -0400

    DMX remote control refactoring
    refactoring old comments from Flo https://p4-swarm.epicgames.net/reviews/15163081#comments
    
    #rb jeremie.roy
    #jira UE-113201
    
    [CL 16103985 by Denys Dubinin in 4.27 branch]

---
+++ Engine/Plugins/VirtualProduction/RemoteControlProtocolDMX/Source/RemoteControlProtocolDMXEditor/Private/RemoteControlProtocolDMXEditorModule.cpp
@@ -0,0 +1,40 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+
+#include "PropertyEditorModule.h"
+#include "RemoteControlProtocolDMX.h"
+#include "RemoteControlProtocolDMXEditorCustomization.h"
+#include "Modules/ModuleInterface.h"
+
+/**
+ * Remote control protocol DMX editor that allows have editor functionality for the protocol
+ */
+class FRemoteControlProtocolDMXEditorModule : public IModuleInterface
+{
+public:
+	//~ Begin IModuleInterface interface
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+	//~ End IModuleInterface interface
+};
+
+void FRemoteControlProtocolDMXEditorModule::StartupModule()
+{
+	if (UObjectInitialized() && FModuleManager::Get().IsModuleLoaded(TEXT("PropertyEditor")))
+	{
+		FPropertyEditorModule& PropertyEditorModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+		PropertyEditorModule.RegisterCustomPropertyTypeLayout(
+            FRemoteControlDMXProtocolEntityExtraSetting::StaticStruct()->GetFName(),
+            FOnGetPropertyTypeCustomizationInstance::CreateStatic(FRemoteControlProtocolDMXEditorTypeCustomization::MakeInstance));	
+	}
+}
+
+void FRemoteControlProtocolDMXEditorModule::ShutdownModule()
+{
+	FPropertyEditorModule& PropertyEditorModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	PropertyEditorModule.UnregisterCustomPropertyTypeLayout(FRemoteControlDMXProtocolEntityExtraSetting::StaticStruct()->GetFName());
+}
+
+IMPLEMENT_MODULE(FRemoteControlProtocolDMXEditorModule, RemoteControlProtocolDMXEditorModule);
