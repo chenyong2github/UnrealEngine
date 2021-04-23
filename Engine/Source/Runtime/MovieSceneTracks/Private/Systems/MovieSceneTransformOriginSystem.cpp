0cd663226f9fc492a90ad2fe37bf935cec8dcb48
andrew rodham <Andrew.Rodham@epicgames.com>
2021-04-22 20:10:27 -0400

    Sequencer: Fixed transform origins not applying rotation correctly
      - Previously the rotation and translation were being transformed separately which was incorrect since this doesn't accumulate offset rotations correctly
    
    #rb Max.Chen
    
    #ROBOMERGE-SOURCE: CL 16100727 via CL 16100733 via CL 16100741
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16100764 by andrew rodham in Main branch]

--- Engine/Source/Runtime/MovieSceneTracks/Private/Systems/MovieSceneTransformOriginSystem.cpp
+++ Engine/Source/Runtime/MovieSceneTracks/Private/Systems/MovieSceneTransformOriginSystem.cpp
@@ -19,20 +19,24 @@ namespace UE
 namespace MovieScene
 {
 
-struct FAssignTransformOriginLocation
+struct FAssignTransformOrigin
 {
 	const TSparseArray<FTransform>* TransformOriginsByInstanceID;
 
 	void ForEachAllocation(const FEntityAllocation* Allocation, TRead<FInstanceHandle> Instances, TRead<UObject*> BoundObjects,
 		TWriteOptional<float> LocationX, TWriteOptional<float> LocationY, TWriteOptional<float> LocationZ,
-		TWriteOptional<FSourceFloatChannelFlags> FlagsX, TWriteOptional<FSourceFloatChannelFlags> FlagsY, TWriteOptional<FSourceFloatChannelFlags> FlagsZ)
+		TWriteOptional<float> RotationX, TWriteOptional<float> RotationY, TWriteOptional<float> RotationZ,
+		TWriteOptional<FSourceFloatChannelFlags> FlagsLocationX, TWriteOptional<FSourceFloatChannelFlags> FlagsLocationY, TWriteOptional<FSourceFloatChannelFlags> FlagsLocationZ,
+		TWriteOptional<FSourceFloatChannelFlags> FlagsRotationX, TWriteOptional<FSourceFloatChannelFlags> FlagsRotationY, TWriteOptional<FSourceFloatChannelFlags> FlagsRotationZ)
 	{
-		TransformLocation(Instances, BoundObjects, LocationX, LocationY, LocationZ, FlagsX, FlagsY, FlagsZ, Allocation->Num());
+		TransformLocation(Instances, BoundObjects, LocationX, LocationY, LocationZ, RotationX, RotationY, RotationZ, FlagsLocationX, FlagsLocationY, FlagsLocationZ, FlagsRotationX, FlagsRotationY, FlagsRotationZ, Allocation->Num());
 	}
 
 	void TransformLocation(const FInstanceHandle* Instances, const UObject* const * BoundObjects,
 		float* OutLocationX, float* OutLocationY, float* OutLocationZ,
-		FSourceFloatChannelFlags* OutFlagsX, FSourceFloatChannelFlags* OutFlagsY, FSourceFloatChannelFlags* OutFlagsZ,
+		float* OutRotationX, float* OutRotationY, float* OutRotationZ,
+		FSourceFloatChannelFlags* OutFlagsLocationX, FSourceFloatChannelFlags* OutFlagsLocationY, FSourceFloatChannelFlags* OutFlagsLocationZ,
+		FSourceFloatChannelFlags* OutFlagsRotationX, FSourceFloatChannelFlags* OutFlagsRotationY, FSourceFloatChannelFlags* OutFlagsRotationZ,
 		int32 Num)
 	{
 		for (int32 Index = 0; Index < Num; ++Index)
@@ -52,70 +56,32 @@ struct FAssignTransformOriginLocation
 
 			FTransform Origin = (*TransformOriginsByInstanceID)[InstanceHandle.InstanceID];
 
-			// This transform has an origin
-			FVector CurrentTranslation(OutLocationX ? OutLocationX[Index] : 0.f, OutLocationY ? OutLocationY[Index] : 0.f, OutLocationZ ? OutLocationZ[Index] : 0.f);
-			FVector NewTranslation = Origin.GetRotation()*(Origin.GetScale3D()*CurrentTranslation) + Origin.GetTranslation();
+			FVector  CurrentTranslation(OutLocationX ? OutLocationX[Index] : 0.f, OutLocationY ? OutLocationY[Index] : 0.f, OutLocationZ ? OutLocationZ[Index] : 0.f);
+			FRotator CurrentRotation(OutRotationY ? OutRotationY[Index] : 0.f, OutRotationZ ? OutRotationZ[Index] : 0.f, OutRotationX ? OutRotationX[Index] : 0.f);
+
+			FTransform NewTransform = FTransform(CurrentRotation, CurrentTranslation)*Origin;
+
+			FVector  NewTranslation = NewTransform.GetTranslation();
+			FRotator NewRotation    = NewTransform.GetRotation().Rotator();
 
 			if (OutLocationX) { OutLocationX[Index] = NewTranslation.X; }
 			if (OutLocationY) { OutLocationY[Index] = NewTranslation.Y; }
 			if (OutLocationZ) { OutLocationZ[Index] = NewTranslation.Z; }
 
-			if (OutFlagsX) { OutFlagsX[Index].bNeedsEvaluate = true; }
-			if (OutFlagsY) { OutFlagsY[Index].bNeedsEvaluate = true; }
-			if (OutFlagsZ) { OutFlagsZ[Index].bNeedsEvaluate = true; }
-		}
-	}
-};
-
-
-struct FAssignTransformOriginRotation
-{
-	const TSparseArray<FTransform>* TransformOriginsByInstanceID;
-
-	void ForEachAllocation(const FEntityAllocation* Allocation, TRead<FInstanceHandle> Instances, TRead<UObject*> BoundObjects,
-		TWriteOptional<float> RotationX, TWriteOptional<float> RotationY, TWriteOptional<float> RotationZ,
-		TWriteOptional<FSourceFloatChannelFlags> FlagsX, TWriteOptional<FSourceFloatChannelFlags> FlagsY, TWriteOptional<FSourceFloatChannelFlags> FlagsZ)
-	{
-		TransformRotation(Instances, BoundObjects, RotationX, RotationY, RotationZ, FlagsX, FlagsY, FlagsZ, Allocation->Num());
-	}
-
-	void TransformRotation(const FInstanceHandle* Instances, const UObject* const * BoundObjects,
-		float* OutRotationX, float* OutRotationY, float* OutRotationZ,
-		FSourceFloatChannelFlags* OutFlagsX, FSourceFloatChannelFlags* OutFlagsY, FSourceFloatChannelFlags* OutFlagsZ,
-		int32 Num)
-	{
-		for (int32 Index = 0; Index < Num; ++Index)
-		{
-			FInstanceHandle InstanceHandle = Instances[Index];
-			if (!TransformOriginsByInstanceID->IsValidIndex(InstanceHandle.InstanceID))
-			{
-				continue;
-			}
-
-			// Do not apply transform origins to attached objects
-			const USceneComponent* SceneComponent = CastChecked<const USceneComponent>(BoundObjects[Index]);
-			if (SceneComponent->GetAttachParent() != nullptr)
-			{
-				continue;
-			}
-
-			FTransform Origin = (*TransformOriginsByInstanceID)[InstanceHandle.InstanceID];
-
-			// This transform has an origin
-			FRotator CurrentRotation(OutRotationY ? OutRotationY[Index] : 0.f, OutRotationZ ? OutRotationZ[Index] : 0.f, OutRotationX ? OutRotationX[Index] : 0.f);
-			FRotator NewRotation = Origin.GetRotation().Rotator() + CurrentRotation;
-
 			if (OutRotationX) { OutRotationX[Index] = NewRotation.Roll; }
 			if (OutRotationY) { OutRotationY[Index] = NewRotation.Pitch; }
 			if (OutRotationZ) { OutRotationZ[Index] = NewRotation.Yaw; }
 
-			if (OutFlagsX) { OutFlagsX[Index].bNeedsEvaluate = true; }
-			if (OutFlagsY) { OutFlagsY[Index].bNeedsEvaluate = true; }
-			if (OutFlagsZ) { OutFlagsZ[Index].bNeedsEvaluate = true; }
+			if (OutFlagsLocationX) { OutFlagsLocationX[Index].bNeedsEvaluate = true; }
+			if (OutFlagsLocationY) { OutFlagsLocationY[Index].bNeedsEvaluate = true; }
+			if (OutFlagsLocationZ) { OutFlagsLocationZ[Index].bNeedsEvaluate = true; }
+
+			if (OutFlagsRotationX) { OutFlagsRotationX[Index].bNeedsEvaluate = true; }
+			if (OutFlagsRotationY) { OutFlagsRotationY[Index].bNeedsEvaluate = true; }
+			if (OutFlagsRotationZ) { OutFlagsRotationZ[Index].bNeedsEvaluate = true; }
 		}
 	}
 };
-
 
 } // namespace MovieScene
 } // namespace UE
@@ -214,27 +180,20 @@ void UMovieSceneTransformOriginSystem::OnRun(FSystemTaskPrerequisites& InPrerequ
 		.WriteOptional(BuiltInComponents->FloatResult[0])
 		.WriteOptional(BuiltInComponents->FloatResult[1])
 		.WriteOptional(BuiltInComponents->FloatResult[2])
-		.WriteOptional(BuiltInComponents->FloatChannelFlags[0])
-		.WriteOptional(BuiltInComponents->FloatChannelFlags[1])
-		.WriteOptional(BuiltInComponents->FloatChannelFlags[2])
-		.CombineFilter(Filter)
-		// Must contain at least one float result
-		.FilterAny({ BuiltInComponents->FloatResult[0], BuiltInComponents->FloatResult[1], BuiltInComponents->FloatResult[2] })
-		.Dispatch_PerAllocation<FAssignTransformOriginLocation>(&Linker->EntityManager, InPrerequisites, &Subsequents, &TransformOriginsByInstanceID);
-
-		FEntityTaskBuilder()
-		.Read(BuiltInComponents->InstanceHandle)
-		.Read(BuiltInComponents->BoundObject)
 		.WriteOptional(BuiltInComponents->FloatResult[3])
 		.WriteOptional(BuiltInComponents->FloatResult[4])
 		.WriteOptional(BuiltInComponents->FloatResult[5])
+		.WriteOptional(BuiltInComponents->FloatChannelFlags[0])
+		.WriteOptional(BuiltInComponents->FloatChannelFlags[1])
+		.WriteOptional(BuiltInComponents->FloatChannelFlags[2])
 		.WriteOptional(BuiltInComponents->FloatChannelFlags[3])
 		.WriteOptional(BuiltInComponents->FloatChannelFlags[4])
 		.WriteOptional(BuiltInComponents->FloatChannelFlags[5])
 		.CombineFilter(Filter)
 		// Must contain at least one float result
-		.FilterAny({ BuiltInComponents->FloatResult[4], BuiltInComponents->FloatResult[4], BuiltInComponents->FloatResult[5] })
-		.Dispatch_PerAllocation<FAssignTransformOriginRotation>(&Linker->EntityManager, InPrerequisites, &Subsequents, &TransformOriginsByInstanceID);
+		.FilterAny({ BuiltInComponents->FloatResult[0], BuiltInComponents->FloatResult[1], BuiltInComponents->FloatResult[2],
+			BuiltInComponents->FloatResult[3], BuiltInComponents->FloatResult[4], BuiltInComponents->FloatResult[5] })
+		.Dispatch_PerAllocation<FAssignTransformOrigin>(&Linker->EntityManager, InPrerequisites, &Subsequents, &TransformOriginsByInstanceID);
 	}
 }
 
