10965717820e3b16ccdac7a758d2ce8cc580f48c
steve smith <steve.smith@epicgames.com>
2021-04-26 16:16:31 -0400

    Quality of life improvement to use an EHandKeypoint directly as an input in arrays from FXRMotionControllerData. Previously developers could cast the EHandKeypoint byte to an int using built-in math conversion blueprint functions, but this was not discoverable and is not automatically suggested when dragging an EHandKeypoint to a BP integer input. submitted by Microsoft #jira UEVR-2148 #rb Steve.Smith
    [CL 16118232 by steve smith in 4.27 branch]

--- Engine/Source/Runtime/HeadMountedDisplay/Public/HeadMountedDisplayTypes.h
+++ Engine/Source/Runtime/HeadMountedDisplay/Public/HeadMountedDisplayTypes.h
@@ -8,6 +8,7 @@
 #include "RHI.h"
 #include "RHIResources.h"
 #include "InputCoreTypes.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
 #include "HeadMountedDisplayTypes.generated.h"
 
 struct FFilterVertex;
@@ -269,6 +270,22 @@ enum class EHandKeypoint : uint8
 };
 
 const int32 EHandKeypointCount = static_cast<int32>(EHandKeypoint::LittleTip) + 1;
+
+UCLASS()
+class HEADMOUNTEDDISPLAY_API UHandKeypointConversion : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Convert EHandKeypoint to int to use directly as indices in FXRMotionControllerData arrays.
+
+	/** Interpret a HandKeypoint as an int input */
+	UFUNCTION(BlueprintPure, BlueprintInternalUseOnly, meta = (CompactNodeTitle = "->", BlueprintAutocast))
+	static int32 Conv_HandKeypointToInt32(EHandKeypoint input)
+	{
+		return static_cast<int32>(input);
+	}
+};
 
 UENUM(BlueprintType)
 enum class EXRVisualType : uint8
