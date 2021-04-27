fceff9d65ecef002bed0c11361b8d6d31a7e932d
sebastian nordgren <sebastian.nordgren@epicgames.com>
2021-04-27 06:44:36 -0400

    The SDetailsViewBase::IsPropertyVisible and IsCustomRowVisible delegates are now applied correctly. IsCustomRowVisible is now used for all things that are not properties.
    This fixes the bugs of CategoriesToShow and PropertiesToShow not being respected in UMG details views.
    
    #jira UE-96790
    #review-16115377 @lauren.barnes
    #preflight 6086e9eb3938bf0001ea30e6
    
    [CL 16122954 by sebastian nordgren in ue5-main branch]

--- Engine/Source/Editor/PropertyEditor/Private/DetailItemNode.cpp
+++ Engine/Source/Editor/PropertyEditor/Private/DetailItemNode.cpp
@@ -97,15 +97,9 @@ TSharedPtr<IPropertyHandle> FDetailItemNode::CreatePropertyHandle() const
 		{
 			return Handles[0];
 		}
-		else
-		{
-			return nullptr;
-		}
 	}
-	else
-	{
-		return nullptr;
-	}
+
+	return nullptr;
 }
 
 void FDetailItemNode::GetFilterStrings(TArray<FString>& OutFilterStrings) const
@@ -631,19 +625,25 @@ void FDetailItemNode::Tick( float DeltaTime )
 EVisibility FDetailItemNode::ComputeItemVisibility() const
 {
 	EVisibility NewVisibility = EVisibility::Visible;
-	if (Customization.HasCustomWidget())
-	{	
-		NewVisibility = Customization.WidgetDecl->VisibilityAttr.Get();
-
-		IDetailsViewPrivate* DetailsView = GetDetailsView();
-		if (DetailsView && !DetailsView->IsCustomRowVisible(FName(*Customization.WidgetDecl->FilterTextString.ToString()), FName(*GetParentCategory()->GetDisplayName().ToString())))
-		{
-			NewVisibility = EVisibility::Collapsed;
-		}
-	}
-	else if (Customization.HasPropertyNode())
+	if (Customization.HasPropertyNode())
 	{
 		NewVisibility = Customization.PropertyRow->GetPropertyVisibility();
+
+		if (NewVisibility != EVisibility::Collapsed)
+		{
+			TSharedPtr<FDetailCategoryImpl> ParentCategoryPtr = GetParentCategory();
+			if (ParentCategoryPtr.IsValid() && ParentCategoryPtr->IsParentLayoutValid())
+			{
+				if (!ParentCategoryPtr->GetParentLayout().IsPropertyVisible(CreatePropertyHandle().ToSharedRef()))
+				{
+					NewVisibility = EVisibility::Collapsed;
+				}
+			}
+		}
+	}
+	else if (Customization.HasCustomWidget())
+	{	
+		NewVisibility = Customization.WidgetDecl->VisibilityAttr.Get();
 	}
 	else if (Customization.HasGroup())
 	{
@@ -660,6 +660,32 @@ EVisibility FDetailItemNode::ComputeItemVisibility() const
 				NewVisibility = EVisibility::Visible;
 				break;
 			}
+		}
+	}
+
+	// check the details view's IsCustomRowVisible delegate if this isn't a property row
+	if (NewVisibility != EVisibility::Collapsed && 
+		GetDetailsView() != nullptr && 
+		!Customization.HasPropertyNode())
+	{
+		const FName CategoryName = GetParentCategory()->GetCategoryName();
+		FName RowName;
+		if (Customization.HasCustomWidget())
+		{
+			RowName = FName(*Customization.WidgetDecl->FilterTextString.ToString());
+		}
+		else if (Customization.HasCustomBuilder())
+		{
+			RowName = Customization.CustomBuilderRow->GetCustomBuilderName();
+		}
+		else if (Customization.HasGroup())
+		{
+			RowName = Customization.DetailGroup->GetGroupName();
+		}
+
+		if (!GetDetailsView()->IsCustomRowVisible(RowName, CategoryName))
+		{
+			NewVisibility = EVisibility::Collapsed;
 		}
 	}
 
