a32242443e2790a9e05f9a5f00c322583ff81b39
ionut matasaru <ionut.matasaru@epicgames.com>
2021-04-27 08:36:01 -0400

    [Insights] Fixed the Unreal Insights crash caused by the stats thread and of the task graph being shutdown twice.
    #rb Catalin.Dragoiu
    #fyi Andriy.Tylychko
    
    [CL 16123410 by ionut matasaru in ue5-main branch]

--- Engine/Source/Programs/UnrealInsights/Private/UnrealInsightsMain.cpp
+++ Engine/Source/Programs/UnrealInsights/Private/UnrealInsightsMain.cpp
@@ -6,9 +6,7 @@
 #include "RequiredProgramMainCPPInclude.h"
 #include "UserInterfaceCommand.h"
 
-
 IMPLEMENT_APPLICATION(UnrealInsights, "UnrealInsights");
-
 
 /**
  * Platform agnostic implementation of the main entry point.
@@ -20,14 +18,11 @@ int32 UnrealInsightsMain(const TCHAR* CommandLine)
 	// Override the stack size for the thread pool.
 	FQueuedThreadPool::OverrideStackSize = 256 * 1024;
 
-	//im: ???
-	FCommandLine::Set(CommandLine);
-
 	// Initialize core.
 	GEngineLoop.PreInit(CommandLine);
 
 	// Make sure all UObject classes are registered and default properties have been initialized.
-	//im: ??? ProcessNewlyLoadedUObjects();
+	//ProcessNewlyLoadedUObjects();
 
 	// Tell the module manager it may now process newly-loaded UObjects when new C++ modules are loaded.
 	FModuleManager::Get().StartProcessingNewlyLoadedObjects();
@@ -35,15 +30,9 @@ int32 UnrealInsightsMain(const TCHAR* CommandLine)
 	FUserInterfaceCommand::Run();
 
 	// Shut down.
-	//im: ??? FCoreDelegates::OnExit.Broadcast();
-	FEngineLoop::AppPreExit(); //im: ???
+	//FCoreDelegates::OnExit.Broadcast();
+	FEngineLoop::AppPreExit();
 	FModuleManager::Get().UnloadModulesAtShutdown();
-
-#if STATS
-	FThreadStats::StopThread();
-#endif
-
-	FTaskGraphInterface::Shutdown(); //im: ???
 
 	return 0;
 }
