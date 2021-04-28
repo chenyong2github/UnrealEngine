6d27dd246df0d5a354a05f72846e09457aa28c58
PJ Kack <PJ.Kack@epicgames.com>
2021-04-28 10:17:05 -0400

    Clang compile fix for 15735615 to avoid deprecation warning for FPackageFileSummary::Guid in implicit constructors
    #jira none
    #rb none
    #rnx
    
    #ROBOMERGE-OWNER: pj.kack
    #ROBOMERGE-AUTHOR: pj.kack
    #ROBOMERGE-SOURCE: CL 16139414 in //UE4/Release-4.27/... via CL 16139466
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Staging) (v789-15992632)
    
    #ushell-cherrypick of 16142572 by pj.kack
    #preflight 60895997f1a052000177199d
    
    [CL 16143405 by PJ Kack in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/PackageFileSummary.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/PackageFileSummary.h
@@ -218,6 +218,14 @@ public:
 	/** Constructor */
 	COREUOBJECT_API FPackageFileSummary();
 
+	// Workaround for clang deprecation warnings for deprecated members in implicit constructors
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+	FPackageFileSummary(FPackageFileSummary&&) = default;
+	FPackageFileSummary(const FPackageFileSummary&) = default;
+	FPackageFileSummary& operator=(FPackageFileSummary&&) = default;
+	FPackageFileSummary& operator=(const FPackageFileSummary&) = default;
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
 	int32 GetFileVersionUE() const
 	{
 		return FileVersionUE;
