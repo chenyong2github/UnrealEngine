9d2060db183ae3a85496e267b4f08d8f9763a1b4
Tony Wong <Tony.Wong@epicgames.com>
2021-04-28 12:33:51 -0400

    MediaFramework: Added plugin framework for an IMovieStreamer that will use MediaFramework. #JIRA: FORT-36396 #review-16131351 @thomas.engel #rb thomas.engel #rnx
    [CL 16145371 by Tony Wong in 4.27 branch]

---
+++ Engine/Plugins/Media/MediaMovieStreamer/Source/MediaMovieStreamer/MediaMovieStreamer.Build.cs
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MediaMovieStreamer : ModuleRules
+{
+	public MediaMovieStreamer(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"MoviePlayer",
+				"Slate",
+				"SlateCore",
+			}
+			);
+		
+	}
+}
---
+++ Engine/Plugins/Media/MediaMovieStreamer/Source/MediaMovieStreamer/Private/MediaMovieStreamerModule.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MediaMovieStreamerModule.h"
+
+#define LOCTEXT_NAMESPACE "FMediaMovieStreamerModule"
+
+void FMediaMovieStreamerModule::StartupModule()
+{
+}
+
+void FMediaMovieStreamerModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FMediaMovieStreamerModule, MediaMovieStreamer)
