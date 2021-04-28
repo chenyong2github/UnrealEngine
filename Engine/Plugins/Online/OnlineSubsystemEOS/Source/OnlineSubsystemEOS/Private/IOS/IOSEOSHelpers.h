9a0086f485f8a0109644bb3b859c309ca17338ec
Mark Fitt <Mark.Fitt@xa.epicgames.com>
2021-04-28 10:36:38 -0400

    #online EOSOSS Added iOS Login support.
    #jira none
    #review-15955675 @sam.zamani, @joe.graf, @alejandro.aguilar
    #rb sam.zamani
    
    [CL 16143648 by Mark Fitt in 4.27 branch]

---
+++ Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOS/Private/IOS/IOSEOSHelpers.h
@@ -0,0 +1,36 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#if WITH_EOS_SDK
+
+#include "eos_auth_types.h"
+#include "eos_ios.h"
+#include "Interfaces/OnlineExternalUIInterface.h"
+#include "EOSHelpers.h"
+
+class FOnlineSubsystemEOS;
+
+class FIOSEOSHelpers : FEOSHelpers
+{
+public:
+	virtual ~FIOSEOSHelpers();
+
+	virtual FString PlatformCreateCacheDir(const FString &ArtifactName, const FString &EOSSettingsCacheDir) override;
+	virtual void PlatformAuthCredentials(EOS_Auth_Credentials &Credentials) override;
+	virtual void PlatformTriggerLoginUI(FOnlineSubsystemEOS* EOSSubsystem, const int ControllerIndex, bool bShowOnlineOnly, bool bShowSkipButton, const FOnLoginUIClosedDelegate& Delegate) override;
+
+private:
+	void OnLoginComplete(int ControllerIndex, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& ErrorString);
+
+	EOS_IOS_Auth_CredentialsOptions CredentialsOptions;
+
+	FOnlineSubsystemEOS* EOSSubsystem;
+	FOnLoginUIClosedDelegate LoginUIClosedDelegate;
+	FDelegateHandle LoginCompleteDelegate;
+};
+
+using FPlatformEOSHelpers = FIOSEOSHelpers;
+
+#endif
+
