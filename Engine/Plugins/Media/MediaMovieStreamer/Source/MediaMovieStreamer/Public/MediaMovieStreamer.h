f5d697eda5af1b4b7383970f6aafdbe3bf6584c9
tony wong <Tony.Wong@epicgames.com>
2021-04-28 12:38:18 -0400

    MediaFramework: Added plugin framework for an IMovieStreamer that will use MediaFramework. #JIRA: FORT-36396 [at]thomas.engel #rb thomas.engel #rnx
    #ROBOMERGE-SOURCE: CL 16145371 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16145450 by tony wong in Main branch]

---
+++ Engine/Plugins/Media/MediaMovieStreamer/Source/MediaMovieStreamer/Public/MediaMovieStreamer.h
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MoviePlayer.h"
+
+/**
+ * Movie streamer that allows you to use MediaFramework.
+ */
+class FMediaMovieStreamer : public IMovieStreamer
+{
+
+};
