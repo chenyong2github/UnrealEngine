7631c5af742194a27432466f7cb9f46deaaa9c0a
jason walter <jason.walter@epicgames.com>
2021-04-29 11:33:51 -0400

    Remove old Virtual Camera
    #jira UE-109235
    #rb raymond.brunette
    
    #ROBOMERGE-SOURCE: CL 16157739 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16157742 by jason walter in Main branch]

--- Engine/Plugins/Experimental/VirtualCamera/Source/VirtualCameraEditor/Private/VirtualCameraEditorModule.cpp
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VirtualCameraEditor/Private/VirtualCameraEditorModule.cpp
@@ -1,35 +1,24 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "VirtualCameraEditorModule.h"
-#include "VirtualCameraEditorStyle.h"
 
 #include "ISettingsModule.h"
 #include "ISettingsSection.h"
 #include "Modules/ModuleManager.h"
-#include "VirtualCameraTab.h"
 #include "VirtualCameraUserSettings.h"
-#include "WorkspaceMenuStructure.h"
-#include "WorkspaceMenuStructureModule.h"
 #include "IPlacementModeModule.h"
-#include "IVPUtilitiesEditorModule.h"
-#include "VirtualCameraActor.h"
 #include "ActorFactories/ActorFactoryBlueprint.h"
+#include "IVPUtilitiesEditorModule.h"
 #include "SimpleVirtualCamera.h"
 
 
 #define LOCTEXT_NAMESPACE "FVirtualCameraEditorModule"
-
 
 class FVirtualCameraEditorModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override
 	{
-		FVirtualCameraEditorStyle::Register();
-		const IWorkspaceMenuStructure& MenuStructure = WorkspaceMenu::GetMenuStructure();
-		TSharedRef<FWorkspaceItem> DeveloperToolsGroup = MenuStructure.GetDeveloperToolsMiscCategory();
-		SVirtualCameraTab::RegisterNomadTabSpawner(DeveloperToolsGroup);
-
 		RegisterSettings();
 		RegisterPlacementModeItems();
 	}
@@ -37,12 +26,6 @@ public:
 	virtual void ShutdownModule() override
 	{
 		UnregisterSettings();
-
-		if (!IsEngineExitRequested() && UObjectInitialized())
-		{
-			FVirtualCameraEditorStyle::Unregister();
-			SVirtualCameraTab::UnregisterNomadTabSpawner();
-		}
 	}
 
 	void RegisterSettings()
@@ -85,31 +68,32 @@ public:
 					SimpleVirtualCameraAssetData,
 					FName("ClassIcon.CameraActor"),
 					TOptional<FLinearColor>(),
-					TOptional<int32>(0),
-					LOCTEXT("VSimpleVCamPlacementName", "Simple Virtual Camera")
+					TOptional<int32>(),
+					NSLOCTEXT("PlacementMode", "Simple Virtual Camera", "Simple Virtual Camera")
 					));
 
 				FAssetData VirtualCamera2ActorAssetData(
-					TEXT("/VirtualCamera/V2/VirtualCamera2Actor"), 
-					TEXT("/VirtualCamera/V2"), 
-					TEXT("VirtualCamera2Actor"), 
+					TEXT("/VirtualCamera/V2/VirtualCamera2Actor"),
+					TEXT("/VirtualCamera/V2"),
+					TEXT("VirtualCamera2Actor"),
 					TEXT("Blueprint")
 				);
-				
+
 				// register the full-fat camera
 				IPlacementModeModule::Get().RegisterPlaceableItem(Info->UniqueHandle, MakeShared<FPlaceableItem>(
 					*UActorFactoryBlueprint::StaticClass(),
 					VirtualCamera2ActorAssetData,
 					FName("ClassIcon.CameraActor"),
 					TOptional<FLinearColor>(),
-					TOptional<int32>(1),
-					LOCTEXT("VCamActorPlacementName", "VirtualCamera2 Actor")
+					TOptional<int32>(),
+					NSLOCTEXT("PlacementMode", "VirtualCamera2 Actor", "VirtualCamera2 Actor")
 				));
 			}
 		}
 	}
 };
+#undef LOCTEXT_NAMESPACE
 
 IMPLEMENT_MODULE(FVirtualCameraEditorModule, VirtualCameraEditor)
 
-#undef LOCTEXT_NAMESPACE
+
