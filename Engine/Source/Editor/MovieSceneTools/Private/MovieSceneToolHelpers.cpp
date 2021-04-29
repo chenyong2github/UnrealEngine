b23ab2b5d214b9f949b83ac099c259ea103771ba
Mike Zyracki <Mike.Zyracki@epicgames.com>
2021-04-29 14:04:41 -0400

    Sequencer :Control Rig: Take into account start frame of imported animation when inserting/replacing. #jira FROST-2281 #rb max.chen
    [CL 16159645 by Mike Zyracki in ue5-main branch]

--- Engine/Source/Editor/MovieSceneTools/Private/MovieSceneToolHelpers.cpp
+++ Engine/Source/Editor/MovieSceneTools/Private/MovieSceneToolHelpers.cpp
@@ -1879,6 +1879,7 @@ static void PrepForInsertReplaceAnimation(bool bInsert, const FFBXNodeAndChannel
 	}
 
 	FFrameNumber Diff = EndFrame - StartFrame;
+	FrameToInsertOrReplace += StartFrame;
 	if (bInsert)
 	{
 		for (FMovieSceneChannel* Channel : Channels)
