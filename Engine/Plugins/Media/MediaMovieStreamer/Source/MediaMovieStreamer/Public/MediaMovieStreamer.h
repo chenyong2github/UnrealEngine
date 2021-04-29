1f895099d75c824ff8f2d9031c115b2fdc4e42d5
tony wong <Tony.Wong@epicgames.com>
2021-04-29 16:40:53 -0400

    MediaMovieStreamer: Can set the media texture that the player is using. #JIRA: FORT-363963 #rb none #rnx
    #ROBOMERGE-SOURCE: CL 16161585 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16161596 by tony wong in Main branch]

--- Engine/Plugins/Media/MediaMovieStreamer/Source/MediaMovieStreamer/Public/MediaMovieStreamer.h
+++ Engine/Plugins/Media/MediaMovieStreamer/Source/MediaMovieStreamer/Public/MediaMovieStreamer.h
@@ -10,6 +10,7 @@ DECLARE_LOG_CATEGORY_EXTERN(LogMediaMovieStreamer, Log, All);
 class IMediaModule;
 class UMediaPlayer;
 class UMediaSource;
+class UMediaTexture;
 
 /**
  * Movie streamer that allows you to use MediaFramework during level loading, etc.
@@ -33,6 +34,13 @@ public:
 	 * @param InMediaSource Media source to play.
 	 */
 	MEDIAMOVIESTREAMER_API void SetMediaSource(UMediaSource* InMediaSource);
+
+	/**
+	 * Sets what media texture we are using.
+	 *
+	 * @param InMediaSource Media source to play.
+	 */
+	MEDIAMOVIESTREAMER_API void SetMediaTexture(UMediaTexture* InMediaTexture);
 
 	/** IMovieStreamer interface */
 	virtual bool Init(const TArray<FString>& InMoviePaths, TEnumAsByte<EMoviePlaybackType> InPlaybackType) override;
@@ -61,6 +69,8 @@ private:
 	TWeakObjectPtr<UMediaPlayer> MediaPlayer;
 	/** Holds the media source we are using. */
 	TWeakObjectPtr<UMediaSource> MediaSource;
+	/** Holds the media texture we are using. */
+	TWeakObjectPtr<UMediaTexture> MediaTexture;
 
 	/** Gets the media module to interface with MediaFramework. */
 	IMediaModule* GetMediaModule();
