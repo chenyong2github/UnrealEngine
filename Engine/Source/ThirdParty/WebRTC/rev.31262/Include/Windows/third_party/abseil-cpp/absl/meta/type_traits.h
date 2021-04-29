3dcb3c77d2214f15bc89034771ab093325cf8ff2
aurel cordonnier <aurel.cordonnier@epicgames.com>
2021-04-29 19:55:10 -0400

    fixing copyright notice #fyi Nick.Pace #robomerge 4.27
    [CL 16163729 by aurel cordonnier in ue5-main branch]

--- Engine/Source/ThirdParty/WebRTC/rev.31262/Include/Windows/third_party/abseil-cpp/absl/meta/type_traits.h
+++ Engine/Source/ThirdParty/WebRTC/rev.31262/Include/Windows/third_party/abseil-cpp/absl/meta/type_traits.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2019 Epic Games, Inc
+// Copyright Epic Games, Inc. All Rights Reserved.
 // Copyright 2017 The Abseil Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
