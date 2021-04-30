ad4e26ced5cbf2b20ca31ddb73d44000a2570779
nicholas goldstein <nicholas.goldstein@epicgames.com>
2021-04-30 12:55:25 -0400

    Fix missing include
    #rb trivial
    
    #ROBOMERGE-SOURCE: CL 16163323 via CL 16163327 via CL 16169792
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16169830 by nicholas goldstein in Main branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraEmitter.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraEmitter.h
@@ -14,6 +14,7 @@
 #include "NiagaraBoundsCalculator.h"
 #include "NiagaraRendererProperties.h"
 #include "NiagaraParameterDefinitionsBase.h"
+#include "NiagaraParameterDefinitionsSubscriber.h"
 #include "NiagaraEmitter.generated.h"
 
 class UMaterial;
