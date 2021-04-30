a7447e7142e16cbda4b45e264a65d5bdab8e9dbc
nicholas goldstein <nicholas.goldstein@epicgames.com>
2021-04-30 17:04:50 -0400

    Increment custom version for parameter definitions assets
    #rb none
    [FYI] shaun.kime
    
    #ROBOMERGE-SOURCE: CL 16173099 via CL 16173104 via CL 16173115
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16173142 by nicholas goldstein in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xA1DD8374, 0xA097684B, 0x81E12313, 0x3AD4A202);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xF0A6FC46, 0xE85C44D0, 0xB53EC9F3, 0xAF07259D);
