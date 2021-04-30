489ed891cb99de828e60a0f68310928d11a084b6
juan canada <Juan.Canada@epicgames.com>
2021-04-30 07:41:23 -0400

    Fixed crash due pushing ray tracing mesh commands from a different thread. FRayTracingMeshCommandOneFrameArray now does not use SceneRenderingAllocator, which is a stack allocator
    #rb mihnea.balta, charles.derousiers
    #jira MH-3890
    
    #ushell-cherrypick of 16156521 by Juan.Canada
    
    #ROBOMERGE-SOURCE: CL 16167054 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v789-15992632)
    
    [CL 16167084 by juan canada in Main branch]

--- Engine/Source/Runtime/Renderer/Public/MeshPassProcessor.h
+++ Engine/Source/Runtime/Renderer/Public/MeshPassProcessor.h
@@ -1759,7 +1759,7 @@ struct TUseBitwiseSwap<FVisibleRayTracingMeshCommand>
 	enum { Value = false };
 };
 
-typedef TArray<FVisibleRayTracingMeshCommand, SceneRenderingAllocator> FRayTracingMeshCommandOneFrameArray;
+typedef TArray<FVisibleRayTracingMeshCommand> FRayTracingMeshCommandOneFrameArray;
 
 class FRayTracingMeshCommandContext
 {
