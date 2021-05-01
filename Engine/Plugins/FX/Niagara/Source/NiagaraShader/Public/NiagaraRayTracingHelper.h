17778e717090ca4a1dc82cdaee681b043660ec79
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-05-01 09:51:23 -0400

    Unity compile fixes for NiagaraRayTracingHelper #jira none #rb none
    [CL 16177347 by rob krajcarski in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraRayTracingHelper.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraRayTracingHelper.h
@@ -14,6 +14,7 @@ class FRHIRayTracingScene;
 class FRHIShaderResourceView;
 class FRHIUniformBuffer;
 class FRHIUnorderedAccessView;
+struct FRWBuffer;
 
 // c++ mirror of the struct defined in NiagaraRayTracingCommon.ush
 struct FNiagaraRayTracingPayload
