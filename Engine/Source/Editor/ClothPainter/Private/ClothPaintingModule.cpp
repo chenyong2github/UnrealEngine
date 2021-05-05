ed123e913da9c64486a372dcc3080a77be67bf19
kriss gossart <Kriss.Gossart@epicgames.com>
2021-05-05 15:11:41 -0400

    Cloth - Add missing Cloth Paint Tool button tooltip.
    #rb trivial
    #jira UE-86947
    
    #ROBOMERGE-SOURCE: CL 16205401 in //UE4/Release-4.26-Chaos/...
    #ROBOMERGE-BOT: RELEASE (Release-4.26-Chaos -> Release-4.27) (v796-16191650)
    
    [CL 16211477 by kriss gossart in 4.27 branch]

--- Engine/Source/Editor/ClothPainter/Private/ClothPaintingModule.cpp
+++ Engine/Source/Editor/ClothPainter/Private/ClothPaintingModule.cpp
@@ -134,7 +134,7 @@ void FClothPaintingModule::RegisterMenus()
 				InSection.AddEntry(FToolMenuEntry::InitToolBarButton(
 					FClothPainterCommands::Get().TogglePaintMode,
 					TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateRaw(this, &FClothPaintingModule::GetPaintToolsButtonText, Context->SkeletalMeshEditor)),
-					FText(),
+					TAttribute<FText>::Create(TAttribute<FText>::FGetter::CreateRaw(this, &FClothPaintingModule::GetPaintToolsButtonToolTip, Context->SkeletalMeshEditor)),
 					FSlateIcon(FEditorStyle::GetStyleSetName(), "LevelEditor.MeshPaintMode.TexturePaint")
 				));	
 			}
@@ -155,6 +155,21 @@ FText FClothPaintingModule::GetPaintToolsButtonText(TWeakPtr<ISkeletalMeshEditor
 	}
 
 	return LOCTEXT("ToggleButton_Activate", "Activate Cloth Paint");
+}
+
+FText FClothPaintingModule::GetPaintToolsButtonToolTip(TWeakPtr<ISkeletalMeshEditor> InSkeletalMeshEditor) const
+{
+	TSharedPtr<SClothPaintTab> ClothTab = GetActiveClothTab(InSkeletalMeshEditor, false);
+
+	if(ClothTab.IsValid())
+	{
+		if(ClothTab->IsPaintModeActive())
+		{
+			return LOCTEXT("ToggleButton_Deactivate", "Deactivate the cloth paint tool, and go back to the current selection mode.");
+		}
+	}
+
+	return LOCTEXT("ToggleButton_Activate", "Activate the cloth paint tool, and open the Clothing window to allow selection of the clothing assets and of their paint targets.");
 }
 
 bool FClothPaintingModule::GetIsPaintToolsButtonChecked(TWeakPtr<ISkeletalMeshEditor> InSkeletalMeshEditor) const
