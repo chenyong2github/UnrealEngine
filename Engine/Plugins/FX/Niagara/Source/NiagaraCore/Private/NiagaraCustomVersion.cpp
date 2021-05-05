86cfd0b0f74647a343ff7680e45009d9351b5239
stu mckenna <Stu.McKenna@epicgames.com>
2021-05-05 15:16:26 -0400

    - Remove "bIsUObjectWrapper == Other.bIsUObjectWrapper" from comparison as it's causing issues in Niagara
    [FYI] marc.audy, shaun.kime
    #rnx
    
    #ROBOMERGE-SOURCE: CL 16188898 via CL 16190229 via CL 16211071
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v796-16191650)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16211602 by stu mckenna in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xF0A6FC46, 0xE85C44D0, 0xB53EC9F3, 0xAF07259D);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x781B5827, 0xAAE2D348, 0x92257081, 0x60F8A06E);
