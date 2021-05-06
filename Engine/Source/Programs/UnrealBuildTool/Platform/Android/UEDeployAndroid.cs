61d89e9cebc00fd8281e3e02fa4fd553dbe46860
chris varnsverry <Chris.Varnsverry@xa.epicgames.com>
2021-05-06 09:18:34 -0400

    - Disable androidx temporarily
    #jira OI-2309 OI-1951
    #rb [at]Chris.Babcock
    
    #ROBOMERGE-SOURCE: CL 16219883 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v796-16191650)
    
    [CL 16219907 by chris varnsverry in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Android/UEDeployAndroid.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Android/UEDeployAndroid.cs
@@ -3293,7 +3293,6 @@ namespace UnrealBuildTool
 			GradleProperties.AppendLine("org.gradle.daemon=false");
 			GradleProperties.AppendLine("org.gradle.jvmargs=-XX:MaxHeapSize=4096m -Xmx9216m");
 			GradleProperties.AppendLine("android.injected.testOnly=false");
-			GradleProperties.AppendLine("android.useAndroidX=true");
 			GradleProperties.AppendLine(string.Format("COMPILE_SDK_VERSION={0}", CompileSDKVersion));
 			GradleProperties.AppendLine(string.Format("BUILD_TOOLS_VERSION={0}", BuildToolsVersion));
 			GradleProperties.AppendLine(string.Format("PACKAGE_NAME={0}", PackageName));
