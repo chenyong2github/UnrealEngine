c4f7c0b09e13af3345373cb51fcd4819c7392a65
denys dubinin <dennis@geodesic.games>
2021-05-11 05:57:38 -0400

    RC Add logging for protocols
    #jira UE-113136
    #rb simon.therriault
    
    #ROBOMERGE-SOURCE: CL 16266344 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v799-16237190)
    
    [CL 16266360 by denys dubinin in Main branch]

---
+++ Engine/Plugins/VirtualProduction/RemoteControl/Source/RemoteControl/Public/RemoteControlLogger.h
@@ -0,0 +1,79 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class IMessageLogListing;
+class SWidget;
+
+/**
+ * Log verbosity level
+ */
+enum class EVerbosityLevel
+{
+	Log,
+	Warning,
+	Error
+};
+
+/**
+ * Remote control logger.
+ * Generated Log UI and collect logs from APIs and protocols
+ */
+class REMOTECONTROL_API FRemoteControlLogger : public TSharedFromThis<FRemoteControlLogger>
+{
+public:
+	/** Get singleton logger instance */
+	static FRemoteControlLogger& Get()
+	{
+		static FRemoteControlLogger* Instance = nullptr;
+		if (Instance == nullptr)
+		{
+			Instance = new FRemoteControlLogger;
+		}
+		return *Instance;
+	}
+
+	/** Callback for logging, it should return FText to log */
+	using FLogCallback = TFunctionRef<FText(void)>;
+	
+	FRemoteControlLogger();
+	virtual ~FRemoteControlLogger() = default;
+
+	/**
+	 * Get Logger SWidget
+	 */
+	TSharedRef<SWidget> GetWidget() const;
+
+	/**
+	 * Log the message
+	 *
+	 * @param InputType Type of the log, protocol, api, webapi
+	 * @param InLogTextCallback	Log callback which should return the Text to log
+	 * @param Verbosity			Log level
+	 */
+	void Log(const FName& InputType, FLogCallback InLogTextCallback, EVerbosityLevel Verbosity = EVerbosityLevel::Log);
+
+	/** 
+	 * Enable/Disable the log for Remote Control
+	 * @param bEnable enable flag
+	 */
+	void EnableLog(const bool bEnable);
+
+	/** Return the current state of the log */
+	bool IsEnabled() const { return bIsEnabled; }
+
+	/** Removes all messages from log */
+	void ClearLog() const;
+
+private:
+	/** Log listening interface */
+	TSharedPtr<IMessageLogListing> MessageLogListing;
+
+	/** Pointer to log widget */
+	TSharedPtr<SWidget> LogListingWidget;
+
+	/** Is the logger enabled */
+	bool bIsEnabled = false;
+};
