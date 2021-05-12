ffb022bd4b21a28a74baa79543cc6f4e6d68efbd
devin doucette <devin.doucette@epicgames.com>
2021-05-12 16:02:07 -0400

    DDC: Forward constructor arguments through TBuildFunctionFactory
    #rb Zousar.Shaker
    #rnx
    
    #ROBOMERGE-SOURCE: CL 16304994 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v800-16297934)
    
    [CL 16305012 by devin doucette in ue5-release-engine-test branch]

--- Engine/Source/Developer/DerivedDataCache/Public/DerivedDataBuildFunctionFactory.h
+++ Engine/Source/Developer/DerivedDataCache/Public/DerivedDataBuildFunctionFactory.h
@@ -29,7 +29,9 @@ public:
  * A build function must be registered by a build function factory before it can execute a build.
  * Register a function in the source file that implements it or in the corresponding module.
  *
- * Example: static const TBuildFunctionFactory<FExampleFunction> ExampleFunctionFactory;
+ * Examples:
+ * static const TBuildFunctionFactory<FExampleFunction> ExampleFunctionFactory;
+ * static const TBuildFunctionFactory<TExampleFunction<FType>> ExampleFunctionFactory(Name, Version);
  */
 template <typename FunctionType>
 class TBuildFunctionFactory final : private IBuildFunctionFactory
@@ -37,7 +39,9 @@ class TBuildFunctionFactory final : private IBuildFunctionFactory
 	static_assert(sizeof(FunctionType) == sizeof(IBuildFunction), "IBuildFunction must be pure and maintain no state.");
 
 public:
-	TBuildFunctionFactory()
+	template <typename... ArgTypes>
+	explicit TBuildFunctionFactory(ArgTypes&&... Args)
+		: Function(Forward<ArgTypes>(Args)...)
 	{
 		IModularFeatures::Get().RegisterModularFeature(GetFeatureName(), this);
 	}
