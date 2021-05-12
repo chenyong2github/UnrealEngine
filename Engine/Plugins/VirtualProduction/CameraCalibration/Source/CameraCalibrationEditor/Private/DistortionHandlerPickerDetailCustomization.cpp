f6c7fedc2f69e67319e16e3d8fd9f00aff83fcab
Geoffrey Douglas <geoffrey.douglas@epicgames.com>
2021-05-12 09:59:09 -0400

    - Fix CIS (missing function parameter) #rb trivial
    [CL 16298821 by Geoffrey Douglas in ue5-release-engine-staging branch]

--- Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/DistortionHandlerPickerDetailCustomization.cpp
+++ Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/DistortionHandlerPickerDetailCustomization.cpp
@@ -150,7 +150,7 @@ void FDistortionHandlerPickerDetailCustomization::OnDistortionHandlerSelected(FD
 	InDistortionHandlerPicker->DistortionProducerID = DistortionProducerID;
 	InDistortionHandlerPicker->HandlerDisplayName = HandlerDisplayName;
 
-	StructPropertyHandle->NotifyPostChange();
+	StructPropertyHandle->NotifyPostChange(EPropertyChangeType::Unspecified);
 }
 
 bool FDistortionHandlerPickerDetailCustomization::IsDistortionHandlerSelected(FDistortionHandlerPicker* InDistortionHandlerPicker, FString InDisplayName) const
