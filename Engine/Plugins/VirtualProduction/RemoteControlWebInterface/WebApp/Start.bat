7f668dd867ea0a8c440a449540c0f4719301abd6
Amir Keren <amir@erizos.tv>
2021-05-12 05:37:23 -0400

    RemoteControlWebInterface: Looking for nodejs executable in default install folder if it is not in the PATH env variable #rb none #jira UE-115481 #fyi ryan.mayeda
    [CL 16297114 by Amir Keren in 4.27 branch]

--- Engine/Plugins/VirtualProduction/RemoteControlWebInterface/WebApp/Start.bat
+++ Engine/Plugins/VirtualProduction/RemoteControlWebInterface/WebApp/Start.bat
@@ -1,20 +1,17 @@
 @echo off
 
-rem   Check if nodejs is in the env variable PATH 
-for %%X in (node.exe) do (set node=%%~$PATH:X)
-if not defined node (
-  echo Did not find nodejs in PATH, looking in default installation folder
-  if exist "%ProgramFiles%\nodejs\node.exe" (set node="%ProgramFiles%\nodejs\node.exe")
-)
+rem Add default nodejs installation folder to path, in case it was not added / overwritten
+SET PATH=%PATH%;%ProgramFiles%\nodejs\
 
+rem Check if nodejs is in the env variable PATH 
+for %%X in (node.exe) do (set node=%%~$PATH:X)
 if not defined node (
   echo ERROR: Couldn't find node.js installed, Please install latest nodejs from https://nodejs.org/en/download/
   exit 1
 )
 
-
 rem Let's check if it is a modern nodejs
-%node% -e "process.exit( process.versions.node.split('.')[0] );"
+node -e "process.exit( process.versions.node.split('.')[0] );"
 echo Found Node.js version %errorlevel% (%node%)
 
 if %errorlevel% LSS 8 (
@@ -23,4 +20,4 @@ if %errorlevel% LSS 8 (
 )
 
 rem redirecting all command line arguments to node script
-"%node%" Scripts/start.js %*
+node Scripts/start.js %*
