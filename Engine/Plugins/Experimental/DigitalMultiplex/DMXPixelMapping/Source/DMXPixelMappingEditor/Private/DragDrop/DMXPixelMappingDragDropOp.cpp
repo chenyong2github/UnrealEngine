ac6338a76e680e1295b8977f2784e02702da609a
Flo Schar <flo@geodesic.games>
2021-05-13 09:36:57 -0400

    DMX - Implement pixel mapping component multi selection drag drop, collapse matrix components by default.
    #jira UE-95632
    #rb Simon.Therriault, Denys.Dubinin, Dominik.Peacock
    
    [CL 16312231 by Flo Schar in 4.27 branch]

--- Engine/Plugins/Experimental/DigitalMultiplex/DMXPixelMapping/Source/DMXPixelMappingEditor/Private/DragDrop/DMXPixelMappingDragDropOp.cpp
+++ Engine/Plugins/Experimental/DigitalMultiplex/DMXPixelMapping/Source/DMXPixelMappingEditor/Private/DragDrop/DMXPixelMappingDragDropOp.cpp
@@ -17,120 +17,26 @@ TSharedRef<FDMXPixelMappingDragDropOp> FDMXPixelMappingDragDropOp::New(const TSh
 	Operation->Parent = InParent;
 	Operation->DefaultHoverText = InTemplate->Name;
 	Operation->CurrentHoverText = InTemplate->Name;
-	Operation->DragOffset = FVector2D::ZeroVector;
 
 	Operation->Construct();
 
 	return Operation;
 }
 
-TSharedRef<FDMXPixelMappingDragDropOp> FDMXPixelMappingDragDropOp::New(const TSet<FDMXPixelMappingComponentReference>& InComponentReferences)
+TSharedRef<FDMXPixelMappingDragDropOp> FDMXPixelMappingDragDropOp::New(const TArray<FDMXPixelMappingDragDropOp::FDraggingComponentReference>& InDraggingComponentReferences)
 {
 	TSharedRef<FDMXPixelMappingDragDropOp> Operation = MakeShared<FDMXPixelMappingDragDropOp>();
 
-	Operation->ComponentReferences = InComponentReferences;
+	Operation->DraggingComponentReferences = InDraggingComponentReferences;
 
-	for (const FDMXPixelMappingComponentReference& ComponentReference : InComponentReferences)
+	for (const FDMXPixelMappingDragDropOp::FDraggingComponentReference& DraggingComponentReference : InDraggingComponentReferences)
 	{
-		Operation->DefaultHoverText = FText::FromString(ComponentReference.GetComponent()->GetName());
-		Operation->CurrentHoverText = FText::FromString(ComponentReference.GetComponent()->GetName());
+		Operation->DefaultHoverText = FText::FromString(DraggingComponentReference.ComponentReference.GetComponent()->GetName());
+		Operation->CurrentHoverText = FText::FromString(DraggingComponentReference.ComponentReference.GetComponent()->GetName());
 		break;
 	}
-	Operation->DragOffset = FVector2D::ZeroVector;
 
 	Operation->Construct();
 
 	return Operation;
-}
-
-void FDMXPixelMappingDragDropOp::UpdateDragOffset(const FVector2D& DragStartScreenspacePosition)
-{
-	FArrangedWidget ArrangedWidget = GetArrangedWidgetFromComponent();
-	DragOffset = ArrangedWidget.Geometry.AbsoluteToLocal(DragStartScreenspacePosition);
-}
-
-void FDMXPixelMappingDragDropOp::SetComponentReferences(const TSet<FDMXPixelMappingComponentReference>& InComponentReferences)
-{
-	ComponentReferences = InComponentReferences;
-	for (const FDMXPixelMappingComponentReference& ComponentReference : InComponentReferences)
-	{
-		DefaultHoverText = FText::FromString(ComponentReference.GetComponent()->GetName());
-		CurrentHoverText = FText::FromString(ComponentReference.GetComponent()->GetName());
-		break;
-	}
-}
-
-FVector2D FDMXPixelMappingDragDropOp::GetDragOffset() const
-{
-	return DragOffset;
-}
-
-UDMXPixelMappingOutputComponent* FDMXPixelMappingDragDropOp::TryGetOutputComponent() const
-{
-	for (const FDMXPixelMappingComponentReference& ComponentReference : ComponentReferences)
-	{
-		return Cast<UDMXPixelMappingOutputComponent>(ComponentReference.GetComponent());
-	}
-
-	return nullptr;
-}
-
-UDMXPixelMappingBaseComponent* FDMXPixelMappingDragDropOp::TryGetBaseComponent() const
-{
-	for (const FDMXPixelMappingComponentReference& ComponentReference : ComponentReferences)
-	{
-		return Cast<UDMXPixelMappingBaseComponent>(ComponentReference.GetComponent());
-	}
-
-	return nullptr;
-}
-
-FArrangedWidget FDMXPixelMappingDragDropOp::GetArrangedWidgetFromComponent() const
-{
-	TSharedPtr<SWidget> WidgetToArrange;
-
-	for (const FDMXPixelMappingComponentReference& ComponentReference : ComponentReferences)
-	{
-		if (UDMXPixelMappingOutputComponent* OutputComponent = Cast<UDMXPixelMappingOutputComponent>(ComponentReference.GetComponent()))
-		{
-			// Use the parent component for group item and pixel components if they're locked in designer
-			if (OutputComponent->bLockInDesigner)
-			{
-				if (OutputComponent->GetClass() == UDMXPixelMappingFixtureGroupItemComponent::StaticClass() ||
-					OutputComponent->GetClass() == UDMXPixelMappingMatrixCellComponent::StaticClass())
-				{
-					UDMXPixelMappingOutputComponent* ParentOutputComponent = CastChecked<UDMXPixelMappingOutputComponent>(OutputComponent->Parent);
-					WidgetToArrange = ParentOutputComponent->GetCachedWidget();
-				}
-			}
-
-			WidgetToArrange = OutputComponent->GetCachedWidget();
-			break;
-		}
-	}
-
-	FArrangedWidget ArrangedWidget(SNullWidget::NullWidget, FGeometry());
-	GetArrangedWidget(WidgetToArrange.ToSharedRef(), ArrangedWidget);
-
-	return ArrangedWidget;
-}
-
-bool FDMXPixelMappingDragDropOp::GetArrangedWidget(TSharedRef<SWidget> Widget, FArrangedWidget& ArrangedWidget) const
-{
-	TSharedPtr<SWindow> WidgetWindow = FSlateApplication::Get().FindWidgetWindow(Widget);
-	if (!WidgetWindow.IsValid())
-	{
-		return false;
-	}
-
-	TSharedRef<SWindow> CurrentWindowRef = WidgetWindow.ToSharedRef();
-
-	FWidgetPath WidgetPath;
-	if (FSlateApplication::Get().GeneratePathToWidgetUnchecked(Widget, WidgetPath))
-	{
-		ArrangedWidget = WidgetPath.FindArrangedWidget(Widget).Get(FArrangedWidget::GetNullWidget());
-		return true;
-	}
-
-	return false;
 }
