b7adbc67307940a7a34a83efe3f0a4bb22399e60
geoff evans <geoff.evans@epicgames.com>
2021-05-13 16:53:24 -0400

    Support for running multiple projects within the same Iteration Profile build * This will give us wider data collection to detect regressions in the cases where a single project has a blocking issue.
    AutomationTool:
    * Add support for appending semicolon delimited parameters into Options (to feed <ForEach/>)
    
    Horde:
    * Put project name into labels to isolate trends graph as builds are run on different projects
    
    #jira none
    #rb tbd
    
    [CL 16318721 by geoff evans in ue5-main branch]

--- Engine/Build/BatchFiles/IterationProfile.bat
+++ Engine/Build/BatchFiles/IterationProfile.bat
@@ -1,1 +1,1 @@
-@call %~dp0RunUAT.bat BuildGraph -Script=Engine/Build/IterationProfile.xml -Target="Run Iteration Profile Tests" -set:ProjectName=ShooterGame -set:TestWin64Editor=true %*
+@call %~dp0RunUAT.bat BuildGraph -Script=Engine/Build/IterationProfile.xml -Target="Run Iteration Profile Tests" -append:ProjectNames="EngineTest" -append:ProjectNames="ShooterGame" -set:TestWin64Editor=true %*
