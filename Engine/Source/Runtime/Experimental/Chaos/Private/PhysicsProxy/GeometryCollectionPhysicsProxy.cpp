db4a70ecf57a090e125ad31d90293a464bb138be
chris caulfield <Chris.Caulfield@epicgames.com>
2021-05-13 12:37:36 -0400

    Back-integrate 15737106
    Chaos - fix inertiia of geometry collision level set particles
    
    #rb noe
    #jira UE-109994
    [FYI] brice.criswell
    #preflight 6052d1e1c233710001e59ce0
    
    #p4v-cherrypick 15737106
    
    #ROBOMERGE-SOURCE: CL 15734993 in //UE5/Release-5.0-EarlyAccess/... via CL 16314437
    #ROBOMERGE-BOT: RELEASE (Release-4.26-Chaos -> Release-4.27) (v804-16311228)
    
    [CL 16314474 by chris caulfield in 4.27 branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/GeometryCollectionPhysicsProxy.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/GeometryCollectionPhysicsProxy.cpp
@@ -2016,7 +2016,8 @@ void FGeometryCollectionPhysicsProxy::InitializeSharedCollisionStructures(
 
 			if (InertiaComputationNeeded[GeometryIndex])
 			{
-				CalculateInertiaAndRotationOfMass(MassSpaceParticles, TriMesh->GetSurfaceElements(), Density_i, MassProperties.CenterOfMass, MassProperties.InertiaTensor, MassProperties.RotationOfMass);
+				// Note: particles already in CoM space, so passing in zero as CoM
+				CalculateInertiaAndRotationOfMass(MassSpaceParticles, TriMesh->GetSurfaceElements(), Density_i, FVec3(0), MassProperties.InertiaTensor, MassProperties.RotationOfMass);
 				CollectionInertiaTensor[TransformGroupIndex] = FVec3(MassProperties.InertiaTensor.M[0][0], MassProperties.InertiaTensor.M[1][1], MassProperties.InertiaTensor.M[2][2]);
 #if false
 				CollectionMassToLocal[TransformGroupIndex] = FTransform(MassProperties.RotationOfMass, MassProperties.CenterOfMass);
