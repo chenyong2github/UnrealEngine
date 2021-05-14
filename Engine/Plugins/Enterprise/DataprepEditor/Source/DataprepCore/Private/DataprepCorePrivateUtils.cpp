94dde89e48af18a5d452a99f48838d4ad8cdf6a8
emil kirichev <Emil.Kirichev@xa.epicgames.com>
2021-05-14 11:06:52 -0400

    Dataprep: Black marks appear on asset texture when using Dataprep Editor
    #jira UE-115443
    #rb JeanLuc.Corenthin
    
    [CL 16329914 by emil kirichev in 4.27 branch]

--- Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepCore/Private/DataprepCorePrivateUtils.cpp
+++ Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepCore/Private/DataprepCorePrivateUtils.cpp
@@ -131,6 +131,7 @@ void DataprepCorePrivateUtils::BuildStaticMeshes(TSet<UStaticMesh*>& StaticMeshe
 
 					SourceModel.BuildSettings.bRecomputeNormals = !(Attributes.GetVertexInstanceNormals().IsValid() && Attributes.GetVertexInstanceNormals().GetNumIndices() > 0);
 					SourceModel.BuildSettings.bRecomputeTangents = false;
+					SourceModel.BuildSettings.DistanceFieldResolutionScale = 0.5f;
 					//SourceModel.BuildSettings.bBuildAdjacencyBuffer = false;
 					//SourceModel.BuildSettings.bBuildReversedIndexBuffer = false;
 				}
