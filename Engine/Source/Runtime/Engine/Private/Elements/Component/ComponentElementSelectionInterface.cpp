1e8124423f4cb0ffc7f6b8053494bfe34bb38822
Jamie Dale <Jamie.Dale@epicgames.com>
2021-05-14 15:47:41 -0400

    Removed redundant element selection helpers
    #fyi Brooke.Hubert
    #preflight 609ebf6ccbf9a4000127a842
    #rnx
    
    [CL 16333920 by Jamie Dale in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Elements/Component/ComponentElementSelectionInterface.cpp
+++ Engine/Source/Runtime/Engine/Private/Elements/Component/ComponentElementSelectionInterface.cpp
@@ -4,13 +4,3 @@
 #include "Elements/Component/ComponentElementData.h"
 #include "Components/ActorComponent.h"
 #include "Elements/Framework/TypedElementList.h"
-
-int32 UComponentElementSelectionInterface::GetNumSelectedComponents(const UTypedElementList* InCurrentSelection)
-{
-	return InCurrentSelection->CountElementsOfType(NAME_Components);
-}
-
-bool UComponentElementSelectionInterface::HasSelectedComponents(const UTypedElementList* InCurrentSelection)
-{
-	return InCurrentSelection->HasElementsOfType(NAME_Components);
-}
--- Engine/Source/Runtime/Engine/Private/Elements/SMInstance/SMInstanceElementSelectionInterface.cpp
+++ Engine/Source/Runtime/Engine/Private/Elements/SMInstance/SMInstanceElementSelectionInterface.cpp
@@ -4,13 +4,3 @@
 #include "Elements/SMInstance/SMInstanceElementData.h"
 #include "Components/InstancedStaticMeshComponent.h"
 #include "Elements/Framework/TypedElementList.h"
-
-int32 USMInstanceElementSelectionInterface::GetNumSelectedSMInstances(const UTypedElementList* InCurrentSelection)
-{
-	return InCurrentSelection->CountElementsOfType(NAME_SMInstance);
-}
-
-bool USMInstanceElementSelectionInterface::HasSelectedSMInstances(const UTypedElementList* InCurrentSelection)
-{
-	return InCurrentSelection->HasElementsOfType(NAME_SMInstance);
-}
