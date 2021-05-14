247199a8dadb2d7efd93a22f3ff04c76ade06960
tim smith <tim.smith@epicgames.com>
2021-05-14 07:48:48 -0400

    Remove the compiler meta data manager. Elimination of some quick to resolve type system lookups.
    #rnx
    #rb jonathan.adamczewski
    #preflight 609e5a1dcbf9a40001fe9778
    
    #ROBOMERGE-SOURCE: CL 16328413 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v804-16311228)
    
    [CL 16328416 by tim smith in ue5-release-engine-test branch]

--- Engine/Source/Programs/UnrealHeaderTool/Private/ParserHelper.cpp
+++ Engine/Source/Programs/UnrealHeaderTool/Private/ParserHelper.cpp
@@ -9,41 +9,14 @@
 #include "ClassMaps.h"
 
 /////////////////////////////////////////////////////
-// FClassMetaData
+// FStructMetaData
 
-/**
- * Finds the metadata for the property specified
- *
- * @param	Prop	the property to search for
- *
- * @return	pointer to the metadata for the property specified, or NULL
- *			if the property doesn't exist in the list (for example, if it
- *			is declared in a package that is already compiled and has had its
- *			source stripped)
- */
-FToken* FClassMetaData::FindTokenData(FProperty* Prop)
+void FStructMetaData::AddProperty(FUnrealPropertyDefinitionInfo& PropertyDef)
 {
-	check(Prop);
-	FUnrealPropertyDefinitionInfo& PropDef = GTypeDefinitionInfoMap.FindChecked(Prop).AsPropertyChecked();
-	return &PropDef.GetToken();
-}
-
-void FClassMetaData::AddProperty(FToken&& PropertyToken)
-{
-	FProperty* Prop = PropertyToken.TokenProperty;
-	check(Prop);
-
-	FUnrealPropertyDefinitionInfo& PropDef = GTypeDefinitionInfoMap.FindChecked(Prop).AsPropertyChecked();
-	PropDef.GetToken() = MoveTemp(PropertyToken);
-
-	// Add the property to the owner
-	UObject* Outer = Prop->GetOwner<UObject>();
-	check(Outer);
-	GTypeDefinitionInfoMap.FindChecked(Outer).AsStructChecked().AddProperty(PropDef);
-
 	// update the optimization flags
 	if (!bContainsDelegates)
 	{
+		FProperty* Prop = PropertyDef.GetProperty();
 		if (Prop->IsA(FDelegateProperty::StaticClass()) || Prop->IsA(FMulticastDelegateProperty::StaticClass()))
 		{
 			bContainsDelegates = true;
@@ -62,12 +35,12 @@ void FClassMetaData::AddProperty(FToken&& PropertyToken)
 	}
 }
 
-void FClassMetaData::AddInheritanceParent(FString&& InParent, FUnrealSourceFile* UnrealSourceFile)
+void FStructMetaData::AddInheritanceParent(FString&& InParent, FUnrealSourceFile* UnrealSourceFile)
 {
 	MultipleInheritanceParents.Add(new FMultipleInheritanceBaseClass(MoveTemp(InParent)));
 }
 
-void FClassMetaData::AddInheritanceParent(UClass* ImplementedInterfaceClass, FUnrealSourceFile* UnrealSourceFile)
+void FStructMetaData::AddInheritanceParent(UClass* ImplementedInterfaceClass, FUnrealSourceFile* UnrealSourceFile)
 {
 	MultipleInheritanceParents.Add(new FMultipleInheritanceBaseClass(ImplementedInterfaceClass));
 }
@@ -213,36 +186,4 @@ bool FAdvancedDisplayParameterHandler::ShouldMarkParameter(const FString& Parame
 bool FAdvancedDisplayParameterHandler::CanMarkMore() const
 {
 	return bUseNumber ? (NumberLeaveUnmarked > 0) : (0 != ParametersNames.Num());
-}
-
-FClassMetaData* FCompilerMetadataManager::AddClassData(UStruct* Struct, FUnrealSourceFile* UnrealSourceFile)
-{
-	TUniquePtr<FClassMetaData>* pClassData = Find(Struct);
-	if (pClassData == NULL)
-	{
-		pClassData = &Emplace(Struct, new FClassMetaData());
-	}
-
-	return pClassData->Get();
-}
-
-FClassMetaData* FCompilerMetadataManager::AddInterfaceClassData(UStruct* Struct, FUnrealSourceFile* UnrealSourceFile)
-{
-	FClassMetaData* ClassData = AddClassData(Struct, UnrealSourceFile);
-	ClassData->ParsedInterface = EParsedInterface::ParsedUInterface;
-	InterfacesToVerify.Emplace(Struct, ClassData);
-	return ClassData;
-}
-
-void FCompilerMetadataManager::CheckForNoIInterfaces()
-{
-	for (const TPair<UStruct*, FClassMetaData*>& StructDataPair : InterfacesToVerify)
-	{
-		if (StructDataPair.Value->ParsedInterface == EParsedInterface::ParsedUInterface)
-		{
-			FString Name = StructDataPair.Key->GetName();
-			FError::Throwf(TEXT("UInterface 'U%s' parsed without a corresponding 'I%s'"), *Name, *Name);
-		}
-	}
-	InterfacesToVerify.Reset();
 }
