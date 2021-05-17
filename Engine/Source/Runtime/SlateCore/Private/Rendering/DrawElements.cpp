386bdfde983554e00972bb5ad30f03982295b14f
patrick boutot <Patrick.Boutot@epicgames.com>
2021-05-17 09:06:06 -0400

    Slate: Add a flag to help debug invalid resources on the render thread. It's not foolproof, ie the memory could be reused. #jira UE-114425 #rb vincent.gauthier #preflight 609d6acd046b9d0001f9d6b9
    #ROBOMERGE-SOURCE: CL 16317944 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v804-16311228)
    
    [CL 16347821 by patrick boutot in Main branch]

--- Engine/Source/Runtime/SlateCore/Private/Rendering/DrawElements.cpp
+++ Engine/Source/Runtime/SlateCore/Private/Rendering/DrawElements.cpp
@@ -561,6 +561,7 @@ void FSlateDrawElement::MakeViewport( FSlateWindowElementList& ElementList, uint
 	FSlateViewportPayload& DataPayload = ElementList.CreatePayload<FSlateViewportPayload>(Element);
 
 	DataPayload.SetViewport(Viewport, InTint);
+	check(DataPayload.RenderTargetResource == nullptr || !DataPayload.RenderTargetResource->Debug_IsDestroyed());
 
 	Element.Init(ElementList, EElementType::ET_Viewport, InLayer, PaintGeometry, InDrawEffects);
 }
