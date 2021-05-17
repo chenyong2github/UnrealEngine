443ce7a1cd1d443f5e0d16ea5d5e58bf504f0670
Stu McKenna <Stu.McKenna@epicgames.com>
2021-05-17 15:38:05 -0400

    - Add Matrix4 support for RI bake out
    #rb shaun.kime
    #jira none
    
    [CL 16357372 by Stu McKenna in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x55518ACA, 0xDAEE46EA, 0xBB4C108E, 0xAFD271AB );
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x9C6C9663, 0x7C42D545, 0x9DC47339, 0x5624F6E6);
