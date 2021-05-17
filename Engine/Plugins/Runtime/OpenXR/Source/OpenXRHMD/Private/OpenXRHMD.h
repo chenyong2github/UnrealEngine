510e23cd13d545a59928fb8b19180f70c10043db
jules blok <Jules.Blok@epicgames.com>
2021-05-17 17:52:01 -0400

    Fix StereoLayer swapchains not being cleared before updating
    #jira UEVR-1675
    #rb robert.srinivasiah
    
    #ROBOMERGE-SOURCE: CL 16337167 in //UE4/Release-4.27/... via CL 16348361
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Staging) (v804-16311228)
    
    [CL 16359938 by jules blok in ue5-release-engine-staging branch]

--- Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRHMD.h
+++ Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRHMD.h
@@ -182,6 +182,7 @@ protected:
 	void UpdateDeviceLocations(bool bUpdateOpenXRExtensionPlugins);
 	void EnumerateViews(FPipelinedFrameState& PipelineState);
 
+	void CopyTexture_RenderThread(FRHICommandListImmediate& RHICmdList, FRHITexture2D* SrcTexture, FIntRect SrcRect, FRHITexture2D* DstTexture, FIntRect DstRect, bool bClearBlack, bool bNoAlpha, ERenderTargetActions RTAction) const;
 	void CopyTexture_RenderThread(FRHICommandListImmediate& RHICmdList, FRHITexture2D* SrcTexture, FIntRect SrcRect, const FXRSwapChainPtr& DstSwapChain, FIntRect DstRect, bool bClearBlack, bool bNoAlpha) const;
 
 public:
