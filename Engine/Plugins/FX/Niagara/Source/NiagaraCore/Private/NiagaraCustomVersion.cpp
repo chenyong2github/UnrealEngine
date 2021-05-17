a8f0b507dd81cc9e5d6fe648216b835b7d577261
shaun kime <Shaun.Kime@epicgames.com>
2021-05-17 13:20:47 -0400

    Fix Engine.Emitter.SimulationPosition being backwards in respect to Emitter localspace flag. Now map directly to engine_owner_position instead of map.engine.owner.position.
    #jira UE-113653
    #rb trivial
    [FYI] Wyeth.Johnson
    
    #ROBOMERGE-OWNER: shaun.kime
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 16355007 in //UE5/Main/... via CL 16355022 via CL 16355086
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Main) (v804-16311228)
    #ROBOMERGE[RELEASE]: Release-4.27
    
    [CL 16355090 by shaun kime in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x858213A3, 0x54814AE9, 0xAFE487F1, 0x03086E57);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xD19D9D89, 0xD23D422F, 0xA16E1C27, 0xC6B3CC2C);
