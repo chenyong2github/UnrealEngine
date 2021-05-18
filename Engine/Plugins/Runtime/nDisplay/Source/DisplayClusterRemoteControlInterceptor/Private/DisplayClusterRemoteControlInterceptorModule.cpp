b5fbcd49789a88613f09b4d1a2d2aa2af18f2615
denys dubinin <dennis@geodesic.games>
2021-05-18 15:14:17 -0400

    RC and nD. Fix replication in Cluster Register Interceptor only after Engine Loop Complete
    #jira none
    #rb simon.therriault
    
    #ROBOMERGE-SOURCE: CL 16369021 in //UE4/Release-4.27/... via CL 16369046
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Staging) (v804-16311228)
    
    [CL 16373669 by denys dubinin in ue5-release-engine-staging branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterRemoteControlInterceptor/Private/DisplayClusterRemoteControlInterceptorModule.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterRemoteControlInterceptor/Private/DisplayClusterRemoteControlInterceptorModule.cpp
@@ -5,22 +5,26 @@
 #include "DisplayClusterRemoteControlInterceptor.h"
 
 #include "Modules/ModuleManager.h"
+#include "Misc/CoreDelegates.h"
 #include "Features/IModularFeatures.h"
 #include "IRemoteControlInterceptionFeature.h"
 #include "IDisplayCluster.h"
-
 
 void FDisplayClusterRemoteControlInterceptorModule::StartupModule()
 {
 	// Instantiate the interceptor feature on module start
 	Interceptor = MakeUnique<FDisplayClusterRemoteControlInterceptor>();
 
-	// Add Interceptor only in Cluster mode
-	if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster)
+	// Register Interceptor only after Engine Loop Complete 
+	FCoreDelegates::OnFEngineLoopInitComplete.AddLambda([this]()
 	{
-		// Register the interceptor feature
-		IModularFeatures::Get().RegisterModularFeature(IRemoteControlInterceptionFeatureInterceptor::GetName(), Interceptor.Get());	
-	}
+		// Add Interceptor only in Cluster mode
+		if (IDisplayCluster::Get().GetOperationMode() == EDisplayClusterOperationMode::Cluster)
+		{
+			// Register the interceptor feature
+			IModularFeatures::Get().RegisterModularFeature(IRemoteControlInterceptionFeatureInterceptor::GetName(), Interceptor.Get());	
+		}
+	});
 }
 
 void FDisplayClusterRemoteControlInterceptorModule::ShutdownModule()
