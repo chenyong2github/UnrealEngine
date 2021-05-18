3242012a804bf496fc6f951f92919e8d16672795
jeanmichel dignard <JeanMichel.Dignard@epicgames.com>
2021-05-18 17:31:49 -0400

    Adapted the USD Linux build script for Python 3 and 4.27
    #jira none
    #rb none
    
    #ROBOMERGE-SOURCE: CL 16376699 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v804-16311228)
    
    [CL 16376703 by jeanmichel dignard in Main branch]

--- Engine/Plugins/Importers/USDImporter/Source/ThirdParty/Linux/build/BuildUsd.sh
+++ Engine/Plugins/Importers/USDImporter/Source/ThirdParty/Linux/build/BuildUsd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-UE_SOURCE_DIR="./release-4.25"
+UE_SOURCE_DIR="./release-4.27"
 
 UE_THIRD_PARTY_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty"
-PYTHON_INSTALL_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python/Linux" 
-PYTHON_EXECUTABLE_DIR="$UE_SOURCE_DIR/Engine/Binaries/ThirdParty/Python/Linux"
-PYTHON_INCLUDE_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python/Linux" 
+PYTHON_INSTALL_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python3/Linux" 
+PYTHON_EXECUTABLE_DIR="$UE_SOURCE_DIR/Engine/Binaries/ThirdParty/Python3/Linux"
+PYTHON_INCLUDE_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python3/Linux" 
 
-COMPILERFLAGS="-std=c++11 -fPIC -nostdinc++ -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include/c++/v1 -I$PYTHON_INCLUDE_DIR/include/python2.7 -I$PYTHON_INCLUDE_DIR/include/x86_64-unknown-linux-gnu" 
-LINKERFLAGS="-L$UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/ -lc++ -lc++abi $PYTHON_INSTALL_DIR/lib/libpython2.7.a -lm -lc -lgcc_s -lgcc -lutil"
+COMPILERFLAGS="-std=c++11 -fPIC -nostdinc++ -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include/c++/v1 -I$PYTHON_INCLUDE_DIR/include/python3.7 -I$PYTHON_INCLUDE_DIR/include/x86_64-unknown-linux-gnu" 
+LINKERFLAGS="-L$UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/ -lc++ -lc++abi $PYTHON_INSTALL_DIR/lib/libpython3.7m.a -lm -lc -lgcc_s -lgcc -lutil"
 
 export CC=clang
 export CXX=clang++
@@ -19,13 +19,13 @@ export LIBS="$UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu
 
 CMAKE_LINKER_FLAGS="-stdlib=libc++ $LINKERFLAGS"
 
-BUILDARGS="-DCMAKE_CXX_FLAGS='$COMPILERFLAGS' -DCMAKE_EXE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_MODULE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_SHARED_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_STATIC_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR/include/python2.7 -DPYTHON_LIBRARY=$PYTHON_INSTALL_DIR/lib/libpython2.7.a -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE_DIR/bin/python2.7"
+BUILDARGS="-DCMAKE_CXX_FLAGS='$COMPILERFLAGS' -DCMAKE_EXE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_MODULE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_SHARED_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_STATIC_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR/include/python3.7m -DPYTHON_LIBRARY=$PYTHON_INSTALL_DIR/lib/libpython3.7m.a -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE_DIR/bin/python3.7"
 
 BOOST_BUILDARGS="toolset=clang cxxflags='$COMPILERFLAGS' linkflags='-stdlib=libc++ $LINKERFLAGS'"
 
 TBB_BUILDARGS="compiler=clang stdlib=libc++"
 
-python src/build_scripts/build_usd.py ./Build --no-tests --no-docs --no-imaging --build-args USD,"$BUILDARGS" Boost,"$BOOST_BUILDARGS" TBB,"$TBB_BUILDARGS"
+python3 src/build_scripts/build_usd.py ./Build --no-docs --no-tests --no-imaging --no-examples --no-tutorials --no-tools --build-args USD,"$BUILDARGS" Boost,"$BOOST_BUILDARGS" TBB,"$TBB_BUILDARGS"
 
 
 
