9f80a467953c32f1af0a7e5291de7fcb24ed096f
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-05-18 17:46:08 -0400

    Standardize step() implementation between CPU and GPU scripts making the existing step(x) implement step(0, x) #rb simon.tovey #jira UE-115365
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 16376696 via CL 16376755 via CL 16376824
    #ROBOMERGE-BOT: (v804-16311228)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16376990 by rob krajcarski in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x38F94A17, 0xC44A414C, 0x9F8AFB2C, 0x605115EB);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0EEBCD8A, 0xE45341FF, 0x84B53A98, 0x46EC4EC1);
