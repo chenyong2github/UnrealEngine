390dc2138547b51ed63c7ecb074cdd856a1fed0e
ben marsh <Ben.Marsh@epicgames.com>
2021-05-17 20:11:14 -0400

    Horde: Force update of stored config data to fix missing logos.
    #ROBOMERGE-SOURCE: CL 16362332 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v804-16311228)
    
    [CL 16362334 by ben marsh in ue5-release-engine-test branch]

--- Engine/Source/Programs/Horde/HordeServer/Services/ConfigService.cs
+++ Engine/Source/Programs/Horde/HordeServer/Services/ConfigService.cs
@@ -33,7 +33,7 @@ namespace HordeServer.Services
 		/// <summary>
 		/// Config file version number
 		/// </summary>
-		const int Version = 2;
+		const int Version = 3;
 
 		/// <summary>
 		/// Database service
