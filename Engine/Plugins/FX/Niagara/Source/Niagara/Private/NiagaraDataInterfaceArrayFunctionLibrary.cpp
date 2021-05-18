460ef22cde08101e41ff210d788430857f4678a1
stu mckenna <Stu.McKenna@epicgames.com>
2021-05-18 14:32:14 -0400

    - Allow BP get & set a single element in a Niagara array
    #rb rob.krajcarski, shaun.kime
    
    
    #ROBOMERGE-SOURCE: CL 16369125 via CL 16370072 via CL 16370165 via CL 16372181
    #ROBOMERGE-BOT: (v804-16311228)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16372498 by stu mckenna in 4.27 branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceArrayFunctionLibrary.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceArrayFunctionLibrary.cpp
@@ -3,165 +3,229 @@
 #include "NiagaraDataInterfaceArrayFunctionLibrary.h"
 #include "NiagaraDataInterfaceArrayFloat.h"
 #include "NiagaraDataInterfaceArrayInt.h"
+#include "NiagaraDataInterfaceArrayImpl.h"
 #include "NiagaraFunctionLibrary.h"
+
+template<typename TArrayType, typename TDataInterace>
+void SetNiagaraArray(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<TArrayType>& InArray)
+{
+	if (TDataInterace* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<TDataInterace>(NiagaraSystem, OverrideName))
+	{
+		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
+		ArrayDI->GetArrayReference() = InArray;
+		ArrayDI->MarkRenderDataDirty();
+	}
+}
+
+template<typename TArrayType, typename TDataInterace>
+TArray<TArrayType> GetNiagaraArray(UNiagaraComponent* NiagaraSystem, FName OverrideName)
+{
+	if (TDataInterace* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<TDataInterace>(NiagaraSystem, OverrideName))
+	{
+		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
+		return ArrayDI->GetArrayReference();
+	}
+	return TArray<TArrayType>();
+}
+
+template<typename TArrayType, typename TDataInterace>
+void SetNiagaraArrayValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const TArrayType& Value, bool bSizeToFit)
+{
+	if (TDataInterace* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<TDataInterace>(NiagaraSystem, OverrideName))
+	{
+		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
+		auto& ArrayData = ArrayDI->GetArrayReference();
+
+		if (!ArrayData.IsValidIndex(Index))
+		{
+			if (!bSizeToFit)
+			{
+				return;
+			}
+			ArrayData.AddDefaulted(Index + 1 - ArrayData.Num());
+		}
+
+		ArrayData[Index] = Value;
+		ArrayDI->MarkRenderDataDirty();
+	}
+}
+
+template<typename TArrayType, typename TDataInterace>
+TArrayType GetNiagaraArrayValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	if (TDataInterace* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<TDataInterace>(NiagaraSystem, OverrideName))
+	{
+		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
+		auto& ArrayData = ArrayDI->GetArrayReference();
+		return ArrayData.IsValidIndex(Index) ? ArrayData[Index] : TArrayType();
+	}
+	return TArrayType();
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayFloat(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<float>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayFloat* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->FloatData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<float, UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector2D(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FVector2D>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayFloat2* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->FloatData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<FVector2D, UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FVector>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayFloat3* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->FloatData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<FVector, UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector4(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FVector4>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayFloat4* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->FloatData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<FVector4, UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayColor(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FLinearColor>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayColor* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->ColorData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<FLinearColor, UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayQuat(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FQuat>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayQuat* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->QuatData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<FQuat, UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayInt32(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<int32>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayInt32* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->IntData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<int32, UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName, ArrayData);
 }
 
 void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayBool(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<bool>& ArrayData)
 {
-	if (UNiagaraDataInterfaceArrayBool* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
-		ArrayDI->BoolData = ArrayData;
-		ArrayDI->MarkRenderDataDirty();
-	}
+	SetNiagaraArray<bool, UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName, ArrayData);
 }
+
+//////////////////////////////////////////////////////////////////////////
 
 TArray<float> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayFloat(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayFloat* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->FloatData;
-	}
-	return TArray<float>();
+	return GetNiagaraArray<float, UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName);
 }
 
 TArray<FVector2D> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVector2D(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayFloat2* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->FloatData;
-	}
-	return TArray<FVector2D>();
+	return GetNiagaraArray<FVector2D, UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName);
 }
 
 TArray<FVector> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVector(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayFloat3* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->FloatData;
-	}
-	return TArray<FVector>();
+	return GetNiagaraArray<FVector, UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName);
 }
 
 TArray<FVector4> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVector4(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayFloat4* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->FloatData;
-	}
-	return TArray<FVector4>();
+	return GetNiagaraArray<FVector4, UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName);
 }
 
 TArray<FLinearColor> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayColor(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayColor* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->ColorData;
-	}
-	return TArray<FLinearColor>();
+	return GetNiagaraArray<FLinearColor, UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName);
 }
 
 TArray<FQuat> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayQuat(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayQuat* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->QuatData;
-	}
-	return TArray<FQuat>();
+	return GetNiagaraArray<FQuat, UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName);
 }
 
 TArray<int32> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayInt32(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayInt32* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->IntData;
-	}
-	return TArray<int32>();
+	return GetNiagaraArray<int32, UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName);
 }
 
 TArray<bool> UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayBool(UNiagaraComponent* NiagaraSystem, FName OverrideName)
 {
-	if (UNiagaraDataInterfaceArrayBool* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName))
-	{
-		FRWScopeLock ReadLock(ArrayDI->ArrayRWGuard, SLT_ReadOnly);
-		return ArrayDI->BoolData;
-	}
-	return TArray<bool>();
+	return GetNiagaraArray<bool, UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName);
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayFloatValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, float Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<float, UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector2DValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const FVector2D& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<FVector2D, UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVectorValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const FVector& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<FVector, UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector4Value(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const FVector4& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<FVector4, UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayColorValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const FLinearColor& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<FLinearColor, UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayQuatValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const FQuat& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<FQuat, UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayInt32Value(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, int32 Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<int32, UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+void UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayBoolValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index, const bool& Value, bool bSizeToFit)
+{
+	SetNiagaraArrayValue<bool, UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName, Index, Value, bSizeToFit);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+float UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayFloatValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<float, UNiagaraDataInterfaceArrayFloat>(NiagaraSystem, OverrideName, Index);
+}
+
+FVector2D UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVector2DValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<FVector2D, UNiagaraDataInterfaceArrayFloat2>(NiagaraSystem, OverrideName, Index);
+}
+
+FVector UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVectorValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<FVector, UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName, Index);
+}
+
+FVector4 UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayVector4Value(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<FVector4, UNiagaraDataInterfaceArrayFloat4>(NiagaraSystem, OverrideName, Index);
+}
+
+FLinearColor UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayColorValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<FLinearColor, UNiagaraDataInterfaceArrayColor>(NiagaraSystem, OverrideName, Index);
+}
+
+FQuat UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayQuatValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<FQuat, UNiagaraDataInterfaceArrayQuat>(NiagaraSystem, OverrideName, Index);
+}
+
+int32 UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayInt32Value(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<int32, UNiagaraDataInterfaceArrayInt32>(NiagaraSystem, OverrideName, Index);
+}
+
+bool UNiagaraDataInterfaceArrayFunctionLibrary::GetNiagaraArrayBoolValue(UNiagaraComponent* NiagaraSystem, FName OverrideName, int Index)
+{
+	return GetNiagaraArrayValue<bool, UNiagaraDataInterfaceArrayBool>(NiagaraSystem, OverrideName, Index);
+}
