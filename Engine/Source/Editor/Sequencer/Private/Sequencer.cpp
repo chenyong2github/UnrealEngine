e7903eb91f8d763e79af2c3bbca6d835614f8af8
max chen <Max.Chen@epicgames.com>
2021-05-19 12:56:54 -0400

    Sequencer: Fix mute row not restoring properly by ensuring only rewriting the mute and solo paths if they actually exist. Also, reparent to the root instead of null
    #jira UE-116053
    #rb ludovic.chabant
    
    
    #ROBOMERGE-SOURCE: CL 16388391 via CL 16388460 via CL 16388491
    #ROBOMERGE-BOT: (v804-16311228)
    
    [CL 16388496 by max chen in ue5-release-engine-staging branch]

--- Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
+++ Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
@@ -6966,8 +6966,11 @@ void FSequencer::OnNodePathChanged(const FString& OldPath, const FString& NewPat
 			if (NewNodePath.RemoveFromStart(PathPrefix))
 			{
 				NewNodePath = NewPath + '.' + NewNodePath;
-				MovieScene->GetSoloNodes().Remove(NodePath);
-				MovieScene->GetSoloNodes().Add(NewNodePath);
+				if (NodeTree->GetNodeAtPath(NewNodePath))
+				{
+					MovieScene->GetSoloNodes().Remove(NodePath);
+					MovieScene->GetSoloNodes().Add(NewNodePath);
+				}
 			}
 		}
 
@@ -6986,8 +6989,11 @@ void FSequencer::OnNodePathChanged(const FString& OldPath, const FString& NewPat
 			if (NewNodePath.RemoveFromStart(PathPrefix))
 			{
 				NewNodePath = NewPath + '.' + NewNodePath;
-				MovieScene->GetMuteNodes().Remove(NodePath);
-				MovieScene->GetMuteNodes().Add(NewNodePath);
+				if (NodeTree->GetNodeAtPath(NewNodePath))
+				{
+					MovieScene->GetMuteNodes().Remove(NodePath);
+					MovieScene->GetMuteNodes().Add(NewNodePath);
+				}
 			}
 		}
 	}
