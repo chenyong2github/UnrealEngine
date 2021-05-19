aa36dbb5d0a9678110fcbd98757893e00b1bfcf9
maxime mercier <Maxime.Mercier@epicgames.com>
2021-05-19 11:51:02 -0400

    Added a bunch of new unit test features to test new behavior tree code path.
    #rnx
    
    
    #ROBOMERGE-SOURCE: CL 16385271 via CL 16386003 via CL 16386160
    #ROBOMERGE-BOT: (v804-16311228)
    
    [CL 16387275 by maxime mercier in ue5-release-engine-staging branch]

--- Engine/Source/Developer/AITestSuite/Private/BehaviorTree/TestBTService_Log.cpp
+++ Engine/Source/Developer/AITestSuite/Private/BehaviorTree/TestBTService_Log.cpp
@@ -15,11 +15,22 @@ UTestBTService_Log::UTestBTService_Log(const FObjectInitializer& ObjectInitializ
 	LogDeactivation = INDEX_NONE;
 	KeyNameTick = NAME_None;
 	LogTick = INDEX_NONE;
+	KeyNameBecomeRelevant = NAME_None;
+	KeyNameCeaseRelevant = NAME_None;
+
+	// Force the service to tick every frame
+	Interval = 0.0f;
+	RandomDeviation = 0.0f;
 }
 
 void UTestBTService_Log::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+
+	if (KeyNameBecomeRelevant != NAME_None)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(KeyNameBecomeRelevant, true);
+	}
 
 	if (LogActivation >= 0)
 	{
@@ -30,6 +41,11 @@ void UTestBTService_Log::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uin
 void UTestBTService_Log::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::OnCeaseRelevant(OwnerComp, NodeMemory);
+
+	if (KeyNameCeaseRelevant != NAME_None)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(KeyNameCeaseRelevant, true);
+	}
 
 	if (LogDeactivation >= 0)
 	{
