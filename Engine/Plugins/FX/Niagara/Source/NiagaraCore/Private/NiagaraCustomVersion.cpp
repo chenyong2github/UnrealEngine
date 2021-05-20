41c868553cb980873f92aee216c03315f56d09c4
stu mckenna <Stu.McKenna@epicgames.com>
2021-05-20 18:45:17 -0400

    - Editgrate CL 16357372 - Add Matrix4 support for RI bake out
    #rb shaun.kime
    
    #ROBOMERGE-SOURCE: CL 16411869 via CL 16411879 via CL 16411886
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v804-16311228)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16411899 by stu mckenna in Main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x7F8E9CA7, 0x61806B41, 0xA6C7E8A3, 0xCB2120F5);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xEDF309F3, 0x3324CD4F, 0x949017DB, 0x3989CFD0);
