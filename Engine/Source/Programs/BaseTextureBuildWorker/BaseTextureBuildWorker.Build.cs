1311f41fcc9cec335e87c50dfc1c866f12b4be86
Zousar Shaker <zousar.shaker@epicgames.com>
2021-05-21 01:32:29 -0400

    Modified implementation of UncompressedTextureBuildWorker to match the new name of BaseTextureBuildWorker.  Also linked in Oodle texture format module and exposed an OodleTexture build function from BaseTextureBuildWorker.
    [CL 16415147 by Zousar Shaker in ue5-main branch]

--- Engine/Source/Programs/BaseTextureBuildWorker/BaseTextureBuildWorker.Build.cs
+++ Engine/Source/Programs/BaseTextureBuildWorker/BaseTextureBuildWorker.Build.cs
@@ -1,10 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
+using System.Linq;
+using EpicGames.Core;
 using UnrealBuildTool;
 
-public class UncompressedTextureBuildWorker : ModuleRules
+public class BaseTextureBuildWorker : ModuleRules
 {
-	public UncompressedTextureBuildWorker(ReadOnlyTargetRules Target) : base(Target)
+	public BaseTextureBuildWorker(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PrivateIncludePathModuleNames.AddRange(new string[]
 		{
@@ -21,5 +24,22 @@ public class UncompressedTextureBuildWorker : ModuleRules
 			"TextureFormat",
 			"TextureFormatUncompressed",
 		});
+
+		// Determine if TextureFormatOodle is enabled.
+		var TextureFormatOodleUPluginFile = FileReference.Combine(new DirectoryReference(EngineDirectory), "Plugins/Developer/TextureFormatOodle/TextureFormatOodle.uplugin");
+		var TextureFormatOodlePlugin = new PluginInfo(TextureFormatOodleUPluginFile, PluginType.Engine);
+
+		bool bTextureFormatOodlePluginEnabled =
+		Enum.GetValues(typeof(UnrealTargetConfiguration)).Cast<UnrealTargetConfiguration>().Any(config
+				=> Plugins.IsPluginEnabledForTarget(TextureFormatOodlePlugin, null, Target.Platform, config, TargetType.Program));
+
+		if (bTextureFormatOodlePluginEnabled)
+		{
+			PrivateDependencyModuleNames.AddRange(new string[]
+			{
+				"TextureFormatOodle",
+			});
+		}
+
 	}
 }
