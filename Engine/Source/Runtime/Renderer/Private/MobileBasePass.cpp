b1d5f31e9cafa9bf320cd09ce6daf37d94ff3203
Wei Liu <Wei.Liu@epicgames.com>
2021-05-21 04:55:13 -0400

    Fix a bug that movable directional light can't cast csm on movable promitives.
    #jira none
    
    #rb Dmitriy.Dyomin
    
    [CL 16415746 by Wei Liu in 4.27 branch]

--- Engine/Source/Runtime/Renderer/Private/MobileBasePass.cpp
+++ Engine/Source/Runtime/Renderer/Private/MobileBasePass.cpp
@@ -337,7 +337,7 @@ ELightMapPolicyType MobileBasePass::SelectMeshLightmapPolicy(
 			}
 			else if ((bHasValidVLM || bHasValidILC) && bPrimitiveUsesILC)
 			{
-				if (bUseStaticAndCSM && !bUsesDeferredShading)
+				if ((bUseStaticAndCSM || bMovableWithCSM) && !bUsesDeferredShading)
 				{
 					SelectedLightmapPolicy = LMP_MOBILE_DIRECTIONAL_LIGHT_CSM_AND_SH_INDIRECT;
 				}
