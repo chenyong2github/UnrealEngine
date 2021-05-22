6b2cb20afad592a362b86c5a95019938fd2d4a22
wei liu <Wei.Liu@epicgames.com>
2021-05-22 04:50:02 -0400

    Fix a bug of mobile TAA shaking.
    #jira none
    
    #rb Dmitriy.Dyomin, Guillaume.Abadie
    
    
    
    #ROBOMERGE-SOURCE: CL 16428837 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v811-16416015)
    #ROBOMERGE[bot4]: -UE5-Release-Engine-Staging
    
    [CL 16428840 by wei liu in Main branch]

--- Engine/Source/Runtime/Renderer/Private/SceneVisibility.cpp
+++ Engine/Source/Runtime/Renderer/Private/SceneVisibility.cpp
@@ -3189,10 +3189,17 @@ void FSceneRenderer::PreVisibilityFrameSetup(FRHICommandListImmediate& RHICmdLis
 		{
 			float EffectivePrimaryResolutionFraction = float(View.ViewRect.Width()) / float(View.GetSecondaryViewRectSize().X);
 
+			bool bIsMobileTAA = IsMobilePlatform(View.GetShaderPlatform()) && !SupportsDesktopTemporalAA(View.GetShaderPlatform());
+
 			// Compute number of TAA samples.
 			int32 TemporalAASamples = CVarTemporalAASamplesValue;
 			{
-				if (bTemporalUpsampling)
+				if (bIsMobileTAA)
+				{
+					// Only support 2 samples for mobile temporal AA.
+					TemporalAASamples = 2;
+				}
+				else if(bTemporalUpsampling)
 				{
 					// When doing TAA upsample with screen percentage < 100%, we need extra temporal samples to have a
 					// constant temporal sample density for final output pixels to avoid output pixel aligned converging issues.
@@ -3222,7 +3229,15 @@ void FSceneRenderer::PreVisibilityFrameSetup(FRHICommandListImmediate& RHICmdLis
 
 			// Choose sub pixel sample coordinate in the temporal sequence.
 			float SampleX, SampleY;
-			if (View.PrimaryScreenPercentageMethod == EPrimaryScreenPercentageMethod::TemporalUpscale)
+			if (bIsMobileTAA)
+			{
+				float SamplesX[] = { -8.0f / 16.0f, 0.0 / 16.0f };
+				float SamplesY[] = { /* - */ 0.0f / 16.0f, 8.0 / 16.0f };
+				check(TemporalAASamples == UE_ARRAY_COUNT(SamplesX));
+				SampleX = SamplesX[TemporalSampleIndex];
+				SampleY = SamplesY[TemporalSampleIndex];
+			}
+			else if (View.PrimaryScreenPercentageMethod == EPrimaryScreenPercentageMethod::TemporalUpscale)
 			{
 				// Uniformly distribute temporal jittering in [-.5; .5], because there is no longer any alignement of input and output pixels.
 				SampleX = Halton(TemporalSampleIndex + 1, 2) - 0.5f;
