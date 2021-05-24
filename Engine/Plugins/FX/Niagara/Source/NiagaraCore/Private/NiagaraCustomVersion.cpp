6c0267ce4b4cc7d210f7f7bd8a10fe1e1cdb5534
shaun kime <Shaun.Kime@epicgames.com>
2021-05-24 16:46:19 -0400

    Fixing situation where the VortexField debug visualization was not working with bindings to Engine.Emitter.SimulationPosition. #rb Nicholas.Goldstein
    #ROBOMERGE-OWNER: shaun.kime
    #ROBOMERGE-AUTHOR: shaun.kime
    #ROBOMERGE-SOURCE: CL 16440293 via CL 16440299 via CL 16440312
    #ROBOMERGE-BOT: (v814-16434418)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16440376 by shaun kime in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x7F8E9CA7, 0x61806B41, 0xA6C7E8A3, 0xCB2120F5);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x37B399DD, 0x825B4DE2, 0xB302EED3, 0xD7413330);
