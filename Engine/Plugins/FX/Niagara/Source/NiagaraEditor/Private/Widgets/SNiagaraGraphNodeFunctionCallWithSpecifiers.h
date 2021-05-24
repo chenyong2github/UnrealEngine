45769d03185f987e3fd8238417a8169e6e090c7d
chris conway <Chris.Conway@xa.epicgames.com>
2021-05-24 14:27:45 -0400

    Fix for "Apply" not being enabled when changing a string name in a node on Attribute Reader Function. Also fixed potential crash with too long of text entry in the same field.
    #jira UE-99337
    #rb frank.fella
    
    
    #ROBOMERGE-SOURCE: CL 16438069 via CL 16438083 via CL 16438127
    #ROBOMERGE-BOT: (v814-16434418)
    
    [CL 16438131 by chris conway in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Widgets/SNiagaraGraphNodeFunctionCallWithSpecifiers.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Widgets/SNiagaraGraphNodeFunctionCallWithSpecifiers.h
@@ -13,6 +13,8 @@ public:
 
 	void Construct(const FArguments& InArgs, UEdGraphNode* InGraphNode);
 	virtual TSharedRef<SWidget> CreateNodeContentArea() override;
+
+	void OnValueNameChanged();
 	
 	TMap<FName, FName>* FunctionSpecifiers;
 };
@@ -21,10 +23,16 @@ class SNiagaraFunctionSpecifier : public SCompoundWidget
 {
 public:
 	SLATE_BEGIN_ARGS(SNiagaraFunctionSpecifier) {}
+	SLATE_EVENT(FSimpleDelegate, OnValueNameChanged)
 	SLATE_END_ARGS();
+
+
 	void Construct(const FArguments& InArgs, FName InAttributeName, FName InValueName, TMap<FName, FName>& InSpecifiers);
 
+	bool VerifyNameTextChanged(const FText& NewText, FText& OutErrorMessage) const;
 	void OnValueNameCommitted(const FText& InText, ETextCommit::Type InCommitType);
+
+	FSimpleDelegate OnValueNameChanged;
 
 	FName AttributeName;
 	FName ValueName;
