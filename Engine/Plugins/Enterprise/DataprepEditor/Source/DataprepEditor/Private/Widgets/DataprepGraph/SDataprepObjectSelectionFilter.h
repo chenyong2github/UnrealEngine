5155426953adcb7c25b2a43260849bc453cc1cb4
emil kirichev <Emil.Kirichev@xa.epicgames.com>
2021-05-25 05:39:38 -0400

    Dataprep: Update filter from Selection : simpler UI and use object / actors path as the unique ID
    #jira UEENT-4439
    #rb JeanLuc.Corenthin
    
    [CL 16445719 by emil kirichev in 4.27 branch]

---
+++ Engine/Plugins/Enterprise/DataprepEditor/Source/DataprepEditor/Private/Widgets/DataprepGraph/SDataprepObjectSelectionFilter.h
@@ -0,0 +1,34 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Types/SlateEnums.h"
+#include "UObject/GCObject.h"
+#include "Widgets/DeclarativeSyntaxSupport.h"
+#include "Widgets/SCompoundWidget.h"
+
+class FMenuBuilder;
+class SComboButton;
+class UDataprepObjectSelectionFilter;
+
+template<class t>
+class SComboBox;
+
+struct FDataprepParametrizationActionData;
+
+class SDataprepObjectSelectionFilter : public SCompoundWidget, public FGCObject
+{
+	SLATE_BEGIN_ARGS(SDataprepObjectSelectionFilter) {}
+	SLATE_END_ARGS();
+
+	void Construct(const FArguments& InArgs, UDataprepObjectSelectionFilter& InFilter);
+
+private:
+
+	UDataprepObjectSelectionFilter* Filter;
+
+	//~ FGCObject interface
+	virtual void AddReferencedObjects(FReferenceCollector& Collector) override;
+	//~ End FGCObject interface
+};
