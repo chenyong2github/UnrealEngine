ef5af2001ba6639ace9e6cebaf5b13ff71142f90
james hopkin <James.Hopkin@epicgames.com>
2021-05-25 05:34:55 -0400

    #robo Integration windows: days of the week implementation
    #ROBOMERGE-SOURCE: CL 16445672 via CL 16445675 via CL 16445686
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Staging) (v814-16434418)
    
    [CL 16445693 by james hopkin in ue5-release-engine-staging branch]

--- Engine/Extras/RoboMerge/v3/src/robo/branch-interfaces.ts
+++ Engine/Extras/RoboMerge/v3/src/robo/branch-interfaces.ts
@@ -233,9 +233,12 @@ export function gatesSame(lhs: GateInfo | null, rhs: GateInfo | null) {
 	for (let n = 0; n < lhs.integrationWindow.length; ++n) {
 		const lhsWindow = lhs.integrationWindow[n]
 		const rhsWindow = rhs.integrationWindow[n]
+		const lhsDays = lhsWindow.daysOfTheWeek || []
+		const rhsDays = rhsWindow.daysOfTheWeek || []
 		if (lhsWindow.startHourUTC !== rhsWindow.startHourUTC ||
 				lhsWindow.durationHours !== rhsWindow.durationHours ||
-				lhsWindow.dayOfTheWeek !== rhsWindow.dayOfTheWeek) {
+				lhsDays.length !== rhsDays.length ||
+				!lhsDays.every((lhsDay, index) => lhsDay === rhsDays[index])) {
 			return false
 		}
 	}
