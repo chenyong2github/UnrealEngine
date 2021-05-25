7a873866b2a966f607c4c817eea2fe2bca91176b
alejandro arango <alejandro.arango@epicgames.com>
2021-05-24 21:47:23 -0400

    CameraCalibration: Fix pragma once in cpp.
    #rb
    #jira
    #rnx
    
    #ROBOMERGE-SOURCE: CL 16443673 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v814-16434418)
    
    [CL 16443703 by alejandro arango in Main branch]

--- Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/Calibrators/CameraNodalOffsetAlgoPoints.cpp
+++ Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/Calibrators/CameraNodalOffsetAlgoPoints.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "CameraNodalOffsetAlgoPoints.h"
 #include "AssetEditor/NodalOffsetTool.h"
 #include "CalibrationPointComponent.h"
