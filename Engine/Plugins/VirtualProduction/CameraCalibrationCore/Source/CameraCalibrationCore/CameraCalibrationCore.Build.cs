757df07f8faf2ed38ffbc9ae5be35ebfd6128ebb
Alejandro Arango <alejandro.arango@epicgames.com>
2021-05-25 02:23:34 -0400

    CameraCalibrationCore: Removed unneeded module dependency.
    #rb
    #jira
    #rnx
    
    [CL 16445033 by Alejandro Arango in 4.27 branch]

--- Engine/Plugins/VirtualProduction/CameraCalibrationCore/Source/CameraCalibrationCore/CameraCalibrationCore.Build.cs
+++ Engine/Plugins/VirtualProduction/CameraCalibrationCore/Source/CameraCalibrationCore/CameraCalibrationCore.Build.cs
@@ -24,7 +24,6 @@ namespace UnrealBuildTool.Rules
 			PrivateDependencyModuleNames.AddRange(
 				new string[]
                 {
-					"LiveLinkComponents",
                     "Projects",
 				}
 			);
