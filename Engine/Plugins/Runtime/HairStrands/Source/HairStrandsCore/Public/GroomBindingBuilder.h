ef390b1051c986c4a89ee8eadb54f63e5b6d44f5
charles derousiers <Charles.deRousiers@epicgames.com>
2021-05-25 06:35:01 -0400

    Remove GPU path for building binding hair data.
    #rb none
    #jira none
    
    #ROBOMERGE-SOURCE: CL 16445941 in //UE5/Private-Frosty/... via CL 16445950 via CL 16445953
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v814-16434418)
    
    [CL 16445956 by charles derousiers in ue5-release-engine-staging branch]

--- Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Public/GroomBindingBuilder.h
+++ Engine/Plugins/Runtime/HairStrands/Source/HairStrandsCore/Public/GroomBindingBuilder.h
@@ -17,22 +17,7 @@ struct HAIRSTRANDSCORE_API FGroomBindingBuilder
 	static FString GetVersion();
 
 	// Build binding asset data
-	static bool BuildBinding(class UGroomBindingAsset* BindingAsset, bool bUseGPU, bool bInitResources);
-
-	// Transfer mesh vertex position from a source mesh on to a target mesh.
-	// The topology of the two meshes can be different, but they need to shader the same UV layout
-	static void TransferMesh(
-		FRDGBuilder& GraphBuilder,
-		const FHairStrandsProjectionMeshData& SourceMeshData,
-		const FHairStrandsProjectionMeshData& TargetMeshData,
-		TArray<FRWBuffer>& TransferredLODsPositions);
-
-	// Project strands roots onto a mesh and compute closest triangle ID & barycentric
-	static void ProjectStrands(
-		FRDGBuilder& GraphBuilder,
-		const FTransform& LocalToWorld,
-		const FHairStrandsProjectionMeshData& TargetMeshData,
-		TArray<FHairStrandsRestRootResource*>& InRestRootResources);
+	static bool BuildBinding(class UGroomBindingAsset* BindingAsset, bool bInitResources);
 
 	// Update the root mesh projection data with unique valid mesh section IDs, based on the projection data
 	static void BuildUniqueSections(FHairStrandsRootData::FMeshProjectionLOD& LOD);
