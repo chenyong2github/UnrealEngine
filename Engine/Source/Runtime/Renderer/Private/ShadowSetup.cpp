d52a4db230fc45b500b3c8bd3ab1b7480cc30b0d
florin pascu <Florin.Pascu@epicgames.com>
2021-05-25 05:04:18 -0400

    Fix modulated shadows Grey out Modulated Shadow option if the Light is not Stationary #jira UE-116253 #rb Dmitriy.Dyomin
    #ROBOMERGE-SOURCE: CL 16445560 in //UE4/Release-4.27/...
    #ROBOMERGE-BOT: RELEASE (Release-4.27 -> Main) (v814-16434418)
    
    [CL 16445569 by florin pascu in Main branch]

--- Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
+++ Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
@@ -4226,7 +4226,9 @@ void FSceneRenderer::AllocateShadowDepthTargets(FRHICommandListImmediate& RHICmd
 					INC_DWORD_STAT(STAT_PerObjectShadows);
 				}
 
-				bool bNeedsProjection = ProjectedShadowInfo->CacheMode != SDCM_StaticPrimitivesOnly;
+				bool bNeedsProjection = ProjectedShadowInfo->CacheMode != SDCM_StaticPrimitivesOnly
+					// Filter out everything but PerObjectOpaqueShadows & Distance field shadows for ES31
+					&& (!bMobile || ProjectedShadowInfo->bPerObjectOpaqueShadow || ProjectedShadowInfo->bRayTracedDistanceField);
 
 				if (bNeedsProjection)
 				{
