8ea78101d91e57a87ddd19e486771dbf3b43cf11
ludovic chabant <Ludovic.Chabant@epicgames.com>
2021-05-27 19:25:49 -0400

    Sequencer: Template sequence asset creation asks user to pick an actor type for the root binding
    #rb max.chen
    
    
    #ROBOMERGE-SOURCE: CL 16488296 via CL 16488332
    #ROBOMERGE-BOT: (v823-16466674)
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16493969 by ludovic chabant in 4.27 branch]

--- Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Factories/TemplateSequenceFactoryNew.cpp
+++ Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Factories/TemplateSequenceFactoryNew.cpp
@@ -1,9 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Factories/TemplateSequenceFactoryNew.h"
-#include "TemplateSequence.h"
+#include "GameFramework/Actor.h"
+#include "Kismet2/SClassPickerDialog.h"
+#include "Misc/TemplateSequenceEditorUtil.h"
 #include "MovieScene.h"
 #include "MovieSceneToolsProjectSettings.h"
+#include "Sections/MovieSceneBoolSection.h"
+#include "TemplateSequence.h"
+#include "Tracks/MovieSceneSpawnTrack.h"
 
 #define LOCTEXT_NAMESPACE "MovieSceneFactory"
 
@@ -26,7 +31,47 @@ UObject* UTemplateSequenceFactoryNew::FactoryCreateNew(UClass* Class, UObject* I
 	FFrameRate TickResolution = NewTemplateSequence->GetMovieScene()->GetTickResolution();
 	NewTemplateSequence->GetMovieScene()->SetPlaybackRange((ProjectSettings->DefaultStartTime*TickResolution).FloorToFrame(), (ProjectSettings->DefaultDuration*TickResolution).FloorToFrame().Value);
 
+	if (BoundActorClass)
+	{
+		NewTemplateSequence->BoundActorClass = BoundActorClass;
+
+		UObject* DefaultObject = BoundActorClass->GetDefaultObject();
+		const FName DefaultObjectName = BoundActorClass->GetDefaultObjectName();
+
+		UMovieScene* NewMovieScene = NewTemplateSequence->GetMovieScene();
+		if (ensure(NewMovieScene && DefaultObject))
+		{
+			const FGuid NewSpawnableGuid = NewMovieScene->AddSpawnable(DefaultObjectName.ToString(), *DefaultObject);
+			
+			UMovieSceneSpawnTrack* NewSpawnTrack = Cast<UMovieSceneSpawnTrack>(NewMovieScene->AddTrack(UMovieSceneSpawnTrack::StaticClass(), NewSpawnableGuid));
+			UMovieSceneBoolSection* NewSpawnSection = Cast<UMovieSceneBoolSection>(NewSpawnTrack->CreateNewSection());
+			NewSpawnSection->GetChannel().SetDefault(true);
+			NewSpawnSection->SetRange(TRange<FFrameNumber>::All());
+			NewSpawnTrack->AddSection(*NewSpawnSection);
+			NewSpawnTrack->SetObjectId(NewSpawnableGuid);
+		}
+	}
+
 	return NewTemplateSequence;
+}
+
+bool UTemplateSequenceFactoryNew::ConfigureProperties()
+{
+	FClassViewerInitializationOptions Options;
+	Options.Mode = EClassViewerMode::ClassPicker;
+	Options.DisplayMode = EClassViewerDisplayMode::TreeView;
+	Options.bShowObjectRootClass = true;
+	Options.NameTypeToDisplay = EClassViewerNameTypeToDisplay::Dynamic;
+
+	const FText TitleText = LOCTEXT("CreateTemplateSequenceOptions", "Pick Root Object Binding Class");
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = SClassPickerDialog::PickClass(TitleText, Options, ChosenClass, AActor::StaticClass());
+	if (bPressedOk)
+	{
+		BoundActorClass = ChosenClass;
+	}
+
+	return bPressedOk;
 }
 
 bool UTemplateSequenceFactoryNew::ShouldShowInNewMenu() const
--- Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Factories/TemplateSequenceFactoryNew.h
+++ Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Factories/TemplateSequenceFactoryNew.h
@@ -15,8 +15,12 @@ class UTemplateSequenceFactoryNew : public UFactory
 {
 	GENERATED_UCLASS_BODY()
 
-public:
 	// UFactory Interface
 	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+	virtual bool ConfigureProperties() override;
 	virtual bool ShouldShowInNewMenu() const override;
+
+	// The root object binding class of the created template sequence.
+	UPROPERTY(EditAnywhere, Category=TemplateSequenceFactory)
+	TSubclassOf<UObject> BoundActorClass;
 };
