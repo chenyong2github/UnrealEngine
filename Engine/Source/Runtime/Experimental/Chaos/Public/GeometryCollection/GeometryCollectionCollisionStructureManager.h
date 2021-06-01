45b41cb1a33ea2dfcdf3afe80fbbcb0155fdf37c
brice criswell <Brice.Criswell@epicgames.com>
2021-06-01 13:28:42 -0400

    Chaos : GeoemtryCollection - Convex collisions #jira none #rb Brett.Miller #preflight 60b652e4c744b20001155e43
    #ROBOMERGE-SOURCE: CL 16517234 in //UE5/Private-Frosty/...
    #ROBOMERGE-BOT: STARSHIP (Private-Frosty -> Main) (v826-16501804)
    
    [CL 16517273 by brice criswell in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/GeometryCollection/GeometryCollectionCollisionStructureManager.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/GeometryCollection/GeometryCollectionCollisionStructureManager.h
@@ -104,6 +104,11 @@ public:
 		const float CollisionObjectReduction,
 		const ECollisionTypeEnum CollisionType);
 
+	static FImplicit* NewImplicitConvex(
+		const TArray<int32>& ConvexIndices,
+		const TManagedArray<TUniquePtr<Chaos::FConvex>>* ConvexGeometry,
+		const ECollisionTypeEnum CollisionType);
+
 	static FImplicit* NewImplicitLevelset(
 		Chaos::FErrorReporter ErrorReporter,
 		const Chaos::FParticles& MeshParticles,
