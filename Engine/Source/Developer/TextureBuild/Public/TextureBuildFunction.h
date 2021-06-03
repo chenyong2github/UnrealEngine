15bd0e1a1df28270a34e5b1656adc2dc001b267a
Devin Doucette <devin.doucette@epicgames.com>
2021-06-03 11:49:06 -0400

    Texture: Moved textures to the Texture cache bucket
    #rb Zousar.Shaker
    #rnx
    
    [CL 16548192 by Devin Doucette in ue5-main branch]

--- Engine/Source/Developer/TextureBuild/Public/TextureBuildFunction.h
+++ Engine/Source/Developer/TextureBuild/Public/TextureBuildFunction.h
@@ -9,6 +9,6 @@
 class FTextureBuildFunction : public UE::DerivedData::IBuildFunction
 {
 public:
+	TEXTUREBUILD_API virtual void Configure(UE::DerivedData::FBuildConfigContext& Context) const override;
 	TEXTUREBUILD_API virtual void Build(UE::DerivedData::FBuildContext& Context) const override;
-private:
 };
