a806130fd37ad33d4812b4a1562ee28c16bb6919
David Lesage <David.Lesage@epicgames.com>
2021-06-03 16:58:31 -0400

    Datasmith : add AP242 Xml Step, XPDM, SolidEdge formats #jira UEENT-4489 #rb jeanluc.corenthin #lockdown Simon.Tourangeau
    [CL 16553060 by David Lesage in 4.27 branch]

--- Engine/Plugins/Enterprise/DatasmithCADImporter/Source/DatasmithCoreTechParametricSurfaceData/Private/DatasmithCoreTechTranslator.cpp
+++ Engine/Plugins/Enterprise/DatasmithCADImporter/Source/DatasmithCoreTechParametricSurfaceData/Private/DatasmithCoreTechTranslator.cpp
@@ -30,6 +30,15 @@ void FDatasmithCoreTechTranslator::SetSceneImportOptions(TArray<TStrongObjectPtr
 	}
 }
 
+bool FDatasmithCoreTechTranslator::IsSourceSupported(const FDatasmithSceneSource& Source)
+{
+	if (Source.GetSourceFileExtension() != TEXT("xml"))
+	{
+		return true;
+	}
+
+	return Datasmith::CheckXMLFileSchema(Source.GetSourceFile(), TEXT("XPDMXML"), TEXT("ns3:Uos"));
+}
 
 
 
