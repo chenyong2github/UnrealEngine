19af20c27df97dda9bb002a0676a52e05f9ced32
ludovic chabant <Ludovic.Chabant@epicgames.com>
2021-06-07 12:36:22 -0400

    Cameras: Change comments on aspect-ratio-related properties.
    When a view-target uses a "maintain vertical FOV" aspect-ratio axis constraint, we take the passed in FOV (which is always horizontal), convert it to a vertical FOV using the original aspect-ratio, and then re-convert it to a horizontal FOV using the current view's aspect-ratio.
    
    This means that the aspect-ratio can now be used even when bConstrainAspectRatio is false. We therefore need to update the comments, and let the user edit the aspect-ratio regardless of the boolean flag value.
    
    #rb max.chen
    #lockdown cristina.riveron
    #7691
    #jira UE-106818
    
    
    #ROBOMERGE-OWNER: ludovic.chabant
    #ROBOMERGE-AUTHOR: ludovic.chabant
    #ROBOMERGE-SOURCE: CL 16537352 via CL 16537357
    #ROBOMERGE-BOT: (v828-16531559)
    #ROBOMERGE-CONFLICT from-shelf
    #ROBOMERGE[bot4]: -BOT4-Release-Engine-Staging
    
    [CL 16572559 by ludovic chabant in 4.27 branch]

--- Engine/Source/Runtime/Engine/Classes/Camera/CameraTypes.h
+++ Engine/Source/Runtime/Engine/Classes/Camera/CameraTypes.h
@@ -79,7 +79,7 @@ struct FMinimalViewInfo
 	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category=Camera)
 	float OrthoFarClipPlane;
 
-	// Aspect Ratio (Width/Height); ignored unless bConstrainAspectRatio is true
+	// Aspect Ratio (Width/Height)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Camera)
 	float AspectRatio;
 
