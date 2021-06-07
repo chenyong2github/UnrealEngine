85d5cd9f0801bef828e5faf79a636b60d014adcf
will damon <Will.Damon@epicgames.com>
2021-06-07 17:19:22 -0400

    Fix event stack underflow when using Apple.InstrumentsEvents
    - Also, remove dead code for unsupported OSes.
    
    #rb dave.hunter, carl.lloyd
    #jira UE-113854
    #rnx
    #fyi axel.riffard, jack.porter
    #lockdown cristina.riveron
    
    [CL 16578029 by will damon in 4.27 branch]

--- Engine/Source/Runtime/Core/Private/Apple/ApplePlatformDebugEvents.cpp
+++ Engine/Source/Runtime/Core/Private/Apple/ApplePlatformDebugEvents.cpp
@@ -36,20 +36,7 @@
 
 #if APPLE_PROFILING_ENABLED
 
-#define APPLE_PROFILING_FALLBACKS !PLATFORM_TVOS && ((!PLATFORM_MAC && __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0) || (PLATFORM_MAC && __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_12))
-
-#ifndef __IPHONE_12_0
-#define __IPHONE_12_0 120000
-#endif
-
-#ifndef __MAC_10_14
-#define __MAC_10_14 101400
-#endif
-
-#define APPLE_PROFILING_SIGNPOST ((!PLATFORM_MAC && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_12_0) || (PLATFORM_MAC && __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14))
-
-#if APPLE_PROFILING_SIGNPOST
-#if PLATFORM_MAC	
+#if PLATFORM_MAC
 	#include <os/log.h>
 	#include <os/signpost.h>
 	
@@ -82,7 +69,6 @@
 	#define PLATFORM_MAC 0
 	#define PLATFORM_IOS 1
 	#define PLATFORM_TVOS 0
-#endif
 #endif
 
 DEFINE_LOG_CATEGORY(LogInstruments)
@@ -137,34 +123,17 @@ static uint32 GetInstrumentsColor(FColor const& Color)
 	return Index;
 }
 
-#if APPLE_PROFILING_SIGNPOST
-static inline os_log_t GetLog()
+FORCEINLINE_DEBUGGABLE os_log_t GetLog()
 {
 	static os_log_t Log = os_log_create("com.epicgames.namedevents", "PointsOfInterest");
 	return Log;
 }
-#endif
 
 void FApplePlatformDebugEvents::DebugSignPost(uint16 Code, uintptr_t Arg1, uintptr_t Arg2, uintptr_t Arg3, uintptr_t Arg4)
 {
 	if (GAppleInstrumentsEvents)
 	{
-#if APPLE_PROFILING_FALLBACKS
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,12,0};
-	#else
-		static NSOperatingSystemVersion Version = {10,0,0};
-	#endif
-		static bool bKDebugAvail = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (!bKDebugAvail)
-		{
-			syscall(SYS_kdebug_trace, APPSDBG_CODE(DBG_MACH_CHUD, Code) | DBG_FUNC_NONE, Arg1, Arg2, Arg3, Arg4);
-		}
-		else
-#endif
-		{
-			kdebug_signpost(Code, Arg1, Arg2, Arg3, Arg4);
-		}
+		kdebug_signpost(Code, Arg1, Arg2, Arg3, Arg4);
 	}
 }
 
@@ -172,22 +141,7 @@ void FApplePlatformDebugEvents::DebugSignPostStart(uint16 Code, uintptr_t Arg1, 
 {
 	if (GAppleInstrumentsEvents)
 	{
-#if APPLE_PROFILING_FALLBACKS
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,12,0};
-	#else
-		static NSOperatingSystemVersion Version = {10,0,0};
-	#endif
-		static bool bKDebugAvail = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (!bKDebugAvail)
-		{
-			syscall(SYS_kdebug_trace, APPSDBG_CODE(DBG_MACH_CHUD, Code) | DBG_FUNC_START, Arg1, Arg2, Arg3, Arg4);
-		}
-		else
-#endif
-		{
-			kdebug_signpost_start(Code, Arg1, Arg2, Arg3, Arg4);
-		}
+		kdebug_signpost_start(Code, Arg1, Arg2, Arg3, Arg4);
 	}
 }
 
@@ -195,22 +149,7 @@ void FApplePlatformDebugEvents::DebugSignPostEnd(uint16 Code, uintptr_t Arg1, ui
 {
 	if (GAppleInstrumentsEvents)
 	{
-#if APPLE_PROFILING_FALLBACKS
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,12,0};
-	#else
-		static NSOperatingSystemVersion Version = {10,0,0};
-	#endif
-		static bool bKDebugAvail = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (!bKDebugAvail)
-		{
-			syscall(SYS_kdebug_trace, APPSDBG_CODE(DBG_MACH_CHUD, Code) | DBG_FUNC_END, Arg1, Arg2, Arg3, Arg4);
-		}
-		else
-#endif
-		{
-			kdebug_signpost_end(Code, Arg1, Arg2, Arg3, Arg4);
-		}
+		kdebug_signpost_end(Code, Arg1, Arg2, Arg3, Arg4);
 	}
 }
 
@@ -277,78 +216,31 @@ uint16 FApplePlatformDebugEvents::GetEventCode(FString String)
 	return Code;
 }
 
-void FApplePlatformDebugEvents::BeginNamedEvent(const struct FColor& Color,const TCHAR* Text)
+static void AppleBeginNamedEvent(const struct FColor& Color, const ANSICHAR* Text)
 {
 	if (GAppleInstrumentsEvents)
 	{
-#if APPLE_PROFILING_SIGNPOST
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,14,0};
-	#else
-		static NSOperatingSystemVersion Version = {12,0,0};
-	#endif
-		static bool bSignPostAvailable = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (bSignPostAvailable)
-		{
-			FEvent Event;
-			NSString* String = [FString(Text).GetNSString() retain];
-			Event.Tag = Text;
-			Event.Code = os_signpost_id_generate(GetLog());
-			Event.Destructor = Block_copy(^{ os_signpost_interval_end(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]); [String release]; });
-			GetEventStack()->Add(Event);
-			os_signpost_interval_begin(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]);
-		}
-		else
-#endif
-		{
-			FString Name(Text);
-			
-			FEvent Event;
-			Event.Tag = Text;
-			Event.Color = GetInstrumentsColor(Color);
-			Event.Code = GetEventCode(Name);
-			
-			GetEventStack()->Add(Event);
-			DebugSignPostStart(Event.Code, (uintptr_t)Event.Tag, 0, 0, (uintptr_t)Event.Color);
-		}
+		NSString* String = [FString(Text).GetNSString() retain];
+
+		FApplePlatformDebugEvents::FEvent Event;
+		Event.Tag = Text;
+		Event.Code = os_signpost_id_generate(GetLog());
+		Event.Destructor = Block_copy(^{ os_signpost_interval_end(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]); [String release]; });
+
+		FApplePlatformDebugEvents::GetEventStack()->Add(Event);
+
+		os_signpost_interval_begin(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]);
 	}
+}
+
+void FApplePlatformDebugEvents::BeginNamedEvent(const struct FColor& Color,const TCHAR* Text)
+{
+	AppleBeginNamedEvent(Color, TCHAR_TO_ANSI(Text));
 }
 
 void FApplePlatformDebugEvents::BeginNamedEvent(const struct FColor& Color,const ANSICHAR* Text)
 {
-	if (GAppleInstrumentsEvents)
-	{
-#if APPLE_PROFILING_SIGNPOST
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,14,0};
-	#else
-		static NSOperatingSystemVersion Version = {12,0,0};
-	#endif
-		static bool bSignPostAvailable = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (bSignPostAvailable)
-		{
-			FEvent Event;
-			NSString* String = [FString(Text).GetNSString() retain];
-			Event.Tag = Text;
-			Event.Code = os_signpost_id_generate(GetLog());
-			Event.Destructor = Block_copy(^{ os_signpost_interval_end(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]); [String release]; });
-			GetEventStack()->Add(Event);
-			os_signpost_interval_begin(GetLog(), Event.Code, "NamedEvent", "%s", [String UTF8String]);
-		}
-		else
-#endif
-		{
-			FString Name(Text);
-			
-			FEvent Event;
-			Event.Tag = Text;
-			Event.Color = GetInstrumentsColor(Color);
-			Event.Code = GetEventCode(Name);
-			
-			GetEventStack()->Add(Event);
-			DebugSignPostStart(Event.Code, (uintptr_t)Event.Tag, 0, 0, (uintptr_t)Event.Color);
-		}
-	}
+	AppleBeginNamedEvent(Color, Text);
 }
 
 void FApplePlatformDebugEvents::EndNamedEvent()
@@ -356,26 +248,19 @@ void FApplePlatformDebugEvents::EndNamedEvent()
 	if (GAppleInstrumentsEvents)
 	{
 		TArray<FEvent>* Stack = GetEventStack();
+		check(Stack);
+		if (Stack->Num() == 0)
+		{
+			// It is possible (and likely) for the CVar to be flipped mid-tick,
+			// meaning some scoped event objects will not have actually pushed
+			// anything to the event stack.
+			return;
+		}
 		FEvent Last = Stack->Last();
 		Stack->RemoveAt(Stack->Num() - 1);
-#if APPLE_PROFILING_SIGNPOST
-	#if PLATFORM_MAC
-		static NSOperatingSystemVersion Version = {10,14,0};
-	#else
-		static NSOperatingSystemVersion Version = {12,0,0};
-	#endif
-		static bool bSignPostAvailable = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:Version];
-		if (bSignPostAvailable)
-		{
-			Last.Destructor();
-			Block_release(Last.Destructor);
-		}
-		else
-#endif
-		{
-			DebugSignPostEnd(Last.Code, (uintptr_t)Last.Tag, 0, 0, (uintptr_t)Last.Color);
-		}
+		Last.Destructor();
+		Block_release(Last.Destructor);
 	}
 }
 
-#endif
+#endif // APPLE_PROFILING_ENABLED
