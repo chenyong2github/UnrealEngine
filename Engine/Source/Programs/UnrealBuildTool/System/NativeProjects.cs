dd2161f48240344f18fb4144aa7c050b67edadb0
jonathan adamczewski <jonathan.adamczewski@epicgames.com>
2021-06-09 13:32:54 -0400

    UnrealBuildTool:
    Move NativeProjects partially into BuildUtilities - class split to avoid pulling in a larger chain of dependencies.
    
    #jira none
    
    #ROBOMERGE-SOURCE: CL 16608480 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v828-16531559)
    
    [CL 16608488 by jonathan adamczewski in ue5-release-engine-test branch]

--- Engine/Source/Programs/UnrealBuildTool/System/NativeProjects.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/NativeProjects.cs
@@ -15,23 +15,8 @@ namespace UnrealBuildTool
 	/// <summary>
 	/// Utility functions for querying native projects (ie. those found via a .uprojectdirs query)
 	/// </summary>
-	public static class NativeProjects
+	public class NativeProjects : NativeProjectsBase
 	{
-		/// <summary>
-		/// Object used for synchronizing access to static fields
-		/// </summary>
-		static object LockObject = new object();
-
-		/// <summary>
-		/// The native project base directories
-		/// </summary>
-		static HashSet<DirectoryReference>? CachedBaseDirectories;
-
-		/// <summary>
-		/// Cached list of project files within all the base directories
-		/// </summary>
-		static HashSet<FileReference>? CachedProjectFiles;
-
 		/// <summary>
 		/// Cached map of target names to the project file they belong to
 		/// </summary>
@@ -42,93 +27,8 @@ namespace UnrealBuildTool
 		/// </summary>
 		public static void ClearCache()
 		{
-			CachedBaseDirectories = null;
-			CachedProjectFiles = null;
+			ClearCacheBase();
 			CachedTargetNameToProjectFile = null;
-		}
-
-		/// <summary>
-		/// Retrieve the list of base directories for native projects
-		/// </summary>
-		public static IEnumerable<DirectoryReference> EnumerateBaseDirectories()
-		{
-			if(CachedBaseDirectories == null)
-			{
-				lock(LockObject)
-				{
-					if(CachedBaseDirectories == null)
-					{
-						HashSet<DirectoryReference> BaseDirs = new HashSet<DirectoryReference>();
-						foreach (FileReference RootFile in DirectoryLookupCache.EnumerateFiles(UnrealBuild.RootDirectory))
-						{
-							if(RootFile.HasExtension(".uprojectdirs"))
-							{
-								foreach(string Line in File.ReadAllLines(RootFile.FullName))
-								{
-									string TrimLine = Line.Trim();
-									if(!TrimLine.StartsWith(";"))
-									{
-										DirectoryReference BaseProjectDir = DirectoryReference.Combine(UnrealBuild.RootDirectory, TrimLine);
-										if(BaseProjectDir.IsUnderDirectory(UnrealBuild.RootDirectory))
-										{
-											BaseDirs.Add(BaseProjectDir);
-										}
-										else
-										{
-											Log.TraceWarning("Project search path '{0}' referenced by '{1}' is not under '{2}', ignoring.", TrimLine, RootFile, UnrealBuild.RootDirectory);
-										}
-									}
-								}
-							}
-						}
-						CachedBaseDirectories = BaseDirs;
-					}
-				}
-			}
-			return CachedBaseDirectories;
-		}
-
-		/// <summary>
-		/// Returns a list of all the projects
-		/// </summary>
-		/// <returns>List of projects</returns>
-		public static IEnumerable<FileReference> EnumerateProjectFiles()
-		{
-			if(CachedProjectFiles == null)
-			{
-				lock(LockObject)
-				{
-					if(CachedProjectFiles == null)
-					{
-						HashSet<FileReference> ProjectFiles = new HashSet<FileReference>();
-						foreach(DirectoryReference BaseDirectory in EnumerateBaseDirectories())
-						{
-							if(DirectoryLookupCache.DirectoryExists(BaseDirectory))
-							{
-								foreach(DirectoryReference SubDirectory in DirectoryLookupCache.EnumerateDirectories(BaseDirectory))
-								{
-									// Ignore system directories (specifically for Mac, since temporary folders are created here)
-									string DirectoryName = SubDirectory.GetDirectoryName();
-									if (DirectoryName.StartsWith(".", StringComparison.Ordinal))
-									{
-										continue;
-									}
-
-									foreach(FileReference File in DirectoryLookupCache.EnumerateFiles(SubDirectory))
-									{
-										if(File.HasExtension(".uproject"))
-										{
-											ProjectFiles.Add(File);
-										}
-									}
-								}
-							}
-						}
-						CachedProjectFiles = ProjectFiles;
-					}
-				}
-			}
-			return CachedProjectFiles;
 		}
 
 		/// <summary>
@@ -168,47 +68,6 @@ namespace UnrealBuildTool
 				}
 			}
 			return CachedTargetNameToProjectFile.TryGetValue(InTargetName, out OutProjectFileName);
-		}
-
-		/// <summary>
-		/// Finds all target files under a given folder, and add them to the target name to project file map
-		/// </summary>
-		/// <param name="Directory">Directory to search</param>
-		/// <param name="TargetNameToProjectFile">Map from target name to project file</param>
-		/// <param name="ProjectFile">The project file for this directory</param>
-		private static void FindTargetFiles(DirectoryReference Directory, Dictionary<string, FileReference> TargetNameToProjectFile, FileReference ProjectFile)
-		{
-			// Search for all target files within this directory
-			bool bSearchSubFolders = true;
-			foreach (FileReference File in DirectoryLookupCache.EnumerateFiles(Directory))
-			{
-				if (File.HasExtension(".target.cs"))
-				{
-					string TargetName = Path.GetFileNameWithoutExtension(File.GetFileNameWithoutExtension());
-					TargetNameToProjectFile[TargetName] = ProjectFile;
-					bSearchSubFolders = false;
-				}
-			}
-
-			// If we didn't find anything, recurse through the subfolders
-			if(bSearchSubFolders)
-			{
-				foreach(DirectoryReference SubDirectory in DirectoryLookupCache.EnumerateDirectories(Directory))
-				{
-					FindTargetFiles(SubDirectory, TargetNameToProjectFile, ProjectFile);
-				}
-			}
-		}
-
-		/// <summary>
-		/// Checks if a given project is a native project
-		/// </summary>
-		/// <param name="ProjectFile">The project file to check</param>
-		/// <returns>True if the given project is a native project</returns>
-		public static bool IsNativeProject(FileReference ProjectFile)
-		{
-			EnumerateProjectFiles();
-			return CachedProjectFiles!.Contains(ProjectFile);
 		}
 	}
 }
