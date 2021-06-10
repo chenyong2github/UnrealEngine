6e084e32a614031f90242036ce81abc2990b0866
graham wihlidal <Graham.Wihlidal@epicgames.com>
2021-06-09 23:33:07 -0400

    Added material cache flags for vertex interpolator, per-instance custom data, and per-instance random usage in a material graph.
    #rb ben.ingram
    [FYI] brian.karis, rune.stubbe
    #preflight 60c17d9a1264df00017ea211
    
    
    #ROBOMERGE-SOURCE: CL 16618522
    #ROBOMERGE-BOT: (v828-16531559)
    
    [CL 16618534 by graham wihlidal in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.h
+++ Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.h
@@ -368,9 +368,6 @@ protected:
 	uint32 bIsFullyRough : 1;
 	/** true if allowed to generate code chunks. Translator operates in two phases; generate all code chunks & query meta data based on generated code chunks. */
 	uint32 bAllowCodeChunkGeneration : 1;
-	
-	/** True if this material reads any per-instance custom data */
-	uint32 bUsesPerInstanceCustomData : 1;
 
 	/** True if this material write anisotropy material property */
 	uint32 bUsesAnisotropy : 1;
