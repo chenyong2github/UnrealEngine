324b2e40e2191d7de54720e00b4f7ca847c9a442
Joe Kirchoff <joe.kirchoff@epicgames.com>
2021-06-11 13:25:19 -0400

    BazelExecutor: Fix templated function to not cause a HeaderTool warning
    #rb trivial
    #preflight 60c3872ee48bab00019a51b8
    
    [CL 16643833 by Joe Kirchoff in ue5-main branch]

--- Engine/Source/Editor/Experimental/BazelExecutor/Private/BazelCompletionQueueRunnable.h
+++ Engine/Source/Editor/Experimental/BazelExecutor/Private/BazelCompletionQueueRunnable.h
@@ -77,45 +77,39 @@ public:
 	bool AddAsyncResponse(
 		TUniquePtr<grpc::ClientContext> ClientContext,
 		TUniquePtr<grpc::ClientAsyncResponseReader<MessageType>> Reader,
-		TFinishFunction OnFinish = nullptr);
+		TFinishFunction OnFinish = nullptr)
+	{
+		static_assert(std::is_base_of<google::protobuf::Message, MessageType>::value, "MessageType is not derived from google::protobuf::Message");
+		if (!Running || !ClientContext.IsValid() || !Reader.IsValid())
+		{
+			return false;
+		}
+
+		grpc::ClientAsyncResponseReader<MessageType>* AsyncReader = Reader.Get();
+
+		// Keep Reader in scope for the lifetime of this async call
+		TFinishFunction OnFinishResponse = [Reader = MoveTemp(Reader), OnFinish = MoveTemp(OnFinish)](void* Tag, bool Ok, const grpc::Status& Status, const google::protobuf::Message& Message)
+		{
+			if (OnFinish)
+			{
+				OnFinish(Tag, Ok, Status, Message);
+			}
+		};
+
+		MessageType* Response = new MessageType();
+		grpc::Status* Status = new grpc::Status();
+
+		FQueuedItem Item;
+		Item.State = FQueuedItem::EState::Finishing;
+		Item.Status.Reset(Status);
+		Item.Message.Reset(Response);
+		Item.ClientContext = MoveTemp(ClientContext);
+		Item.Finish = MoveTemp(OnFinishResponse);
+		QueuedItems.Add(AsyncReader, MoveTemp(Item));
+		AsyncReader->StartCall();
+		AsyncReader->Finish(Response, Status, AsyncReader);
+		return true;
+	}
 
 	grpc::CompletionQueue* GetCompletionQueue();
 };
-
-template<class MessageType>
-bool FBazelCompletionQueueRunnable::AddAsyncResponse(
-	TUniquePtr<grpc::ClientContext> ClientContext,
-	TUniquePtr<grpc::ClientAsyncResponseReader<MessageType>> Reader,
-	TFinishFunction OnFinish)
-{
-	static_assert(std::is_base_of<google::protobuf::Message, MessageType>::value, "MessageType is not derived from google::protobuf::Message");
-	if (!Running || !ClientContext.IsValid() || !Reader.IsValid())
-	{
-		return false;
-	}
-
-	grpc::ClientAsyncResponseReader<MessageType>* AsyncReader = Reader.Get();
-
-	// Keep Reader in scope for the lifetime of this async call
-	TFinishFunction OnFinishResponse = [Reader = MoveTemp(Reader), OnFinish = MoveTemp(OnFinish)](void* Tag, bool Ok, const grpc::Status& Status, const google::protobuf::Message& Message)
-	{
-		if (OnFinish)
-		{
-			OnFinish(Tag, Ok, Status, Message);
-		}
-	};
-
-	MessageType* Response = new MessageType();
-	grpc::Status* Status = new grpc::Status();
-
-	FQueuedItem Item;
-	Item.State = FQueuedItem::EState::Finishing;
-	Item.Status.Reset(Status);
-	Item.Message.Reset(Response);
-	Item.ClientContext = MoveTemp(ClientContext);
-	Item.Finish = MoveTemp(OnFinishResponse);
-	QueuedItems.Add(AsyncReader, MoveTemp(Item));
-	AsyncReader->StartCall();
-	AsyncReader->Finish(Response, Status, AsyncReader);
-	return true;
-}
