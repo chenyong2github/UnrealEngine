9fa2ca142f7eec7c833b0e4da645824966b12b9e
Joe Kirchoff <joe.kirchoff@epicgames.com>
2021-06-11 12:01:59 -0400

    VcPkg: Linux libraries (currently unused, will be referenced in a later changelist)
    Splitting up checking in all the libs as this changelist is massive and impossible to presubmit.
    
    #rb trivial
    
    [CL 16642550 by Joe Kirchoff in ue5-main branch]

---
+++ Engine/Source/ThirdParty/vcpkg/LinuxAArch64/aarch64-unknown-linux-gnueabi/grpc_aarch64-unknown-linux-gnueabi/share/grpc/gRPCTargets-release.cmake
@@ -0,0 +1,109 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "gRPC::upb" for configuration "Release"
+set_property(TARGET gRPC::upb APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::upb PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_upbdefs.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::upb )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::upb "${_IMPORT_PREFIX}/lib/libgrpc_upbdefs.a" )
+
+# Import target "gRPC::address_sorting" for configuration "Release"
+set_property(TARGET gRPC::address_sorting APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::address_sorting PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaddress_sorting.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::address_sorting )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::address_sorting "${_IMPORT_PREFIX}/lib/libaddress_sorting.a" )
+
+# Import target "gRPC::gpr" for configuration "Release"
+set_property(TARGET gRPC::gpr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::gpr PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgpr.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::gpr )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::gpr "${_IMPORT_PREFIX}/lib/libgpr.a" )
+
+# Import target "gRPC::grpc" for configuration "Release"
+set_property(TARGET gRPC::grpc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc "${_IMPORT_PREFIX}/lib/libgrpc.a" )
+
+# Import target "gRPC::grpc_unsecure" for configuration "Release"
+set_property(TARGET gRPC::grpc_unsecure APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_unsecure PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_unsecure.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_unsecure )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_unsecure "${_IMPORT_PREFIX}/lib/libgrpc_unsecure.a" )
+
+# Import target "gRPC::grpc++" for configuration "Release"
+set_property(TARGET gRPC::grpc++ APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++ PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++ )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++ "${_IMPORT_PREFIX}/lib/libgrpc++.a" )
+
+# Import target "gRPC::grpc++_alts" for configuration "Release"
+set_property(TARGET gRPC::grpc++_alts APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_alts PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_alts.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_alts )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_alts "${_IMPORT_PREFIX}/lib/libgrpc++_alts.a" )
+
+# Import target "gRPC::grpc++_error_details" for configuration "Release"
+set_property(TARGET gRPC::grpc++_error_details APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_error_details PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_error_details.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_error_details )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_error_details "${_IMPORT_PREFIX}/lib/libgrpc++_error_details.a" )
+
+# Import target "gRPC::grpc++_unsecure" for configuration "Release"
+set_property(TARGET gRPC::grpc++_unsecure APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc++_unsecure PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc++_unsecure.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc++_unsecure )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc++_unsecure "${_IMPORT_PREFIX}/lib/libgrpc++_unsecure.a" )
+
+# Import target "gRPC::grpc_plugin_support" for configuration "Release"
+set_property(TARGET gRPC::grpc_plugin_support APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gRPC::grpc_plugin_support PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgrpc_plugin_support.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gRPC::grpc_plugin_support )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gRPC::grpc_plugin_support "${_IMPORT_PREFIX}/lib/libgrpc_plugin_support.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
