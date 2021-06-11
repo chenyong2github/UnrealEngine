5cd6e76ed24a77385a013e43263f1f554cf493f5
pavel punsky <Pavel.Punsky@epicgames.com>
2021-06-11 18:17:36 -0400

    Updates to PosixShim - latest version
    Includes a method to set server UTC time
    
    
    #jira UE-103284
    #rb ryan.gerleve
    #lockdown cristina.riveron
    
    [CL 16648144 by pavel punsky in 4.27 branch]

--- Engine/Source/Runtime/PlatformThirdPartyHelpers/PosixShim/Private/PosixShimDefault.cpp
+++ Engine/Source/Runtime/PlatformThirdPartyHelpers/PosixShim/Private/PosixShimDefault.cpp
@@ -1,15 +1,32 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "CoreMinimal.h"
+#include "PosixShim.h"
 
 #include <time.h>
+#include <atomic>
 
-#if !defined(UE_HAS_EPOCH_TIME_OFFSET) || !UE_HAS_EPOCH_TIME_OFFSET
+namespace
+{
+	/**
+	 * Static storage function for our local clock correction seconds. We use static storage to ensure
+	 * we don't allocate anything until used.
+	 */
+	static std::atomic<time_t>& GetCurrentLocalClockCorrectionSeconds()
+	{
+		static std::atomic<time_t> LocalClockCorrectionSeconds(0);
+		return LocalClockCorrectionSeconds;
+	}
+}
+
+void SetPosixShimUTCServerTime(const FDateTime UTCServerTime)
+{
+	const time_t LocalUtcTimeSeconds = time(nullptr);
+	const time_t LocalClockCorrectionAmountSeconds = UTCServerTime.ToUnixTimestamp() - LocalUtcTimeSeconds;
+
+	GetCurrentLocalClockCorrectionSeconds().store(LocalClockCorrectionAmountSeconds);
+}
 
 time_t UE_epoch_time_offset()
 {
-	// There is no offset by default
-	return 0;
+	return GetCurrentLocalClockCorrectionSeconds().load();
 }
-
-#endif // !defined(UE_HAS_EPOCH_TIME_OFFSET) || !UE_HAS_EPOCH_TIME_OFFSET
--- Engine/Source/Runtime/PlatformThirdPartyHelpers/PosixShim/public/PosixShim.h
+++ Engine/Source/Runtime/PlatformThirdPartyHelpers/PosixShim/public/PosixShim.h
@@ -2,7 +2,14 @@
 
 #pragma once
 
-// any configuartion / setup calls would go here
+#include "CoreMinimal.h"
 
+// any configuration / setup calls would go here
 
-
+/**
+ * Sets the current UTC server time.  This value should be set from a trusted-to-be-roughly-correct source, and
+ * should be updated from time to time.
+ *
+ * @param UTCServerTime The new server time
+ */
+POSIXSHIM_API void SetPosixShimUTCServerTime(const FDateTime UTCServerTime);
