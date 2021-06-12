86e3bee823b6a74cd80abee47b94092b3d4adf4c
Shaun Kime <Shaun.Kime@epicgames.com>
2021-06-12 14:55:48 -0400

    Updating the custom version to resolve issues in a couple of examples.
    #jira UE-118046, UE-118045
    #rb rob.krajcarski
    #lockdown cristina.riveron
    
    [CL 16652267 by Shaun Kime in 4.27 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xB74F4514, 0x6E454B94, 0x844C9159, 0x26619017);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xD7CAB3D5, 0x90304622, 0xB041EF0D, 0x2E1E214E );
