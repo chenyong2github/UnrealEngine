5a3c2380aa6d1d7397096a03f385bbf1b2b04add
Dominik Peacock <dominik@geodesic.games>
2021-06-12 13:27:38 -0400

    Fix non-unity issues
    #rb Flo.Schar
    #jira none
    #lockdown Simon.Tourangeau
    
    [CL 16652076 by Dominik Peacock in 4.27 branch]

--- Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Data/LevelSnapshot.cpp
+++ Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Data/LevelSnapshot.cpp
@@ -18,8 +18,10 @@
 #include "UObject/Package.h"
 #include "UObject/TextProperty.h"
 #include "UObject/UnrealType.h"
-#if WITH_EDITOR
+#if WITH_EDITOR && !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 #include "Logging/MessageLog.h"
+#endif
+#if WITH_EDITOR
 #include "ScopedTransaction.h"
 #endif
 
@@ -99,7 +101,7 @@ void ULevelSnapshot::ApplySnapshotToWorld(UWorld* TargetWorld, const FPropertySe
 	
 	if (MapPath != FSoftObjectPath(TargetWorld))
 	{
-#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+#if WITH_EDITOR && !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 		if (TargetWorld->IsPlayInEditor())
 		{
 			FMessageLog("PIE").Warning(
@@ -133,7 +135,7 @@ void ULevelSnapshot::SnapshotWorld(UWorld* TargetWorld)
 	if (TargetWorld->WorldType != EWorldType::Editor
 		&& TargetWorld->WorldType != EWorldType::EditorPreview) // To suppor tests in editor preview maps
 	{
-#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+#if WITH_EDITOR && !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 		if (TargetWorld->IsPlayInEditor())
 		{
 			FMessageLog("PIE").Warning(
