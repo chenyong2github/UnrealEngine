2a53abd78ee4a7d2bbc3133aa857c02b679e8b93
Yujiang Wang <Yujiang.Wang@epicgames.com>
2021-06-14 18:48:11 -0400

    Fix race condition in FPerInstanceRenderData::UpdateBoundsTransforms_Concurrent
    #rb juan.canada
    
    [CL 16666238 by Yujiang Wang in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/InstancedStaticMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/InstancedStaticMesh.cpp
@@ -991,6 +991,9 @@ void FPerInstanceRenderData::UpdateFromPreallocatedData(FStaticMeshInstanceData&
 	ENQUEUE_RENDER_COMMAND(FInstanceBuffer_UpdateFromPreallocatedData)(
 		[InInstanceBufferDataPtr, InInstanceBuffer, this](FRHICommandListImmediate& RHICmdList)
 		{
+			// The assignment to InstanceData shared pointer kills the old data
+			// If UpdateBoundsTask is in-flight it will crash
+			EnsureInstanceDataUpdated();
 			InInstanceBuffer->InstanceData = InInstanceBufferDataPtr;
 			if (!InInstanceBuffer->CondSetFlushToGPUPending())
 			{
@@ -1113,6 +1116,13 @@ const TArray<FRenderTransform>& FPerInstanceRenderData::GetPerInstanceTransforms
 
 void FPerInstanceRenderData::UpdateFromCommandBuffer(FInstanceUpdateCmdBuffer& CmdBuffer)
 {
+	// UpdateFromCommandBuffer reallocates InstanceData in InstanceBuffer
+	// If UpdateBoundsTask is in-flight it will crash
+	ENQUEUE_RENDER_COMMAND(EnsureInstanceDataUpdatedCmd)(
+		[this](FRHICommandList&) {
+		EnsureInstanceDataUpdated();
+	});
+
 	InstanceBuffer.UpdateFromCommandBuffer_Concurrent(CmdBuffer);
 	UpdateBoundsTransforms_Concurrent();
 }
