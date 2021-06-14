3a4beda20c6d6ccdac3f43a76fa92aec9ae0ecc5
jonathan adamczewski <jonathan.adamczewski@epicgames.com>
2021-06-14 10:54:00 -0400

    UnrealBuildToolTests:
    Allow overriding the assembly location, to fix computation of RootLocation when running tests.
    
    #jira none
    
    [CL 16658527 by jonathan adamczewski in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildToolTests/SourceFileTests.cs
+++ Engine/Source/Programs/UnrealBuildToolTests/SourceFileTests.cs
@@ -22,6 +22,11 @@ namespace UnrealBuildToolTests
 		[TestMethod]
 		public void Run()
 		{
+			// Test assemblies are built into a temporary directory outside of the unreal source tree, which prevents Unreal.FindRootDirectory()
+			// from being able to deduce the correct location - so override it.q
+			// The default working directory for this test is ...\Engine\Source\Programs\UnrealBuildToolTests\bin\[configuration]\netcoreapp3.1
+			Unreal.LocationOverride.RootDirectory = DirectoryReference.Combine(DirectoryReference.GetCurrentDirectory(), @"..\..\..\..\..\..\..");
+
 			List<DirectoryReference> BaseDirectories = new List<DirectoryReference>();
 			BaseDirectories.Add(DirectoryReference.Combine(UnrealBuildTool.UnrealBuildTool.EngineSourceDirectory, "Runtime"));
 			BaseDirectories.Add(DirectoryReference.Combine(UnrealBuildTool.UnrealBuildTool.EngineSourceDirectory, "Developer"));
