67b66c5c400100d7db47f10ca1e44cf87c7a309f
joe kirchoff <joe.kirchoff@epicgames.com>
2021-06-14 11:09:41 -0400

    VcPkg: Move helper functions to ModuleRules
    #rb trivial
    
    #ROBOMERGE-SOURCE: CL 16658710 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v833-16641396)
    
    [CL 16658717 by joe kirchoff in ue5-release-engine-test branch]

--- Engine/Source/ThirdParty/Protobuf/Protobuf.Build.cs
+++ Engine/Source/ThirdParty/Protobuf/Protobuf.Build.cs
@@ -1,98 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.IO;
-using System.Collections.Generic;
 
 public class Protobuf : ModuleRules
 {
-	string GetVcPackageRoot(ReadOnlyTargetRules Target, string PackageName)
-	{
-		string TargetPlatform = Target.Platform.ToString();
-		string Platform = null;
-		string Architecture = null;
-		string Linkage = string.Empty;
-		string Toolset = string.Empty;
-		if (Target.Platform == UnrealTargetPlatform.Win64)
-		{
-			Platform = "windows";
-			Architecture = Target.WindowsPlatform.Architecture.ToString().ToLowerInvariant();
-			if (Target.bUseStaticCRT)
-			{
-				Linkage = "-static";
-			}
-			else
-			{
-				Linkage = "-static-md";
-			}
-			Toolset = "-v141";
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			Architecture = "x86_64";
-			Platform = "unknown-linux-gnu";
-		}
-		else if (Target.Platform == UnrealTargetPlatform.LinuxAArch64)
-		{
-			Architecture = "aarch64";
-			Platform = "unknown-linux-gnueabi";
-		}
-
-		if (string.IsNullOrEmpty(TargetPlatform) || string.IsNullOrEmpty(Platform) || string.IsNullOrEmpty(Architecture))
-		{
-			throw new System.NotSupportedException($"Platform {Target.Name} not currently supported by vcpkg");
-		}
-
-		string Triplet = $"{Architecture}-{Platform}{Linkage}{Toolset}";
-
-		return Path.Combine("ThirdParty", "vcpkg", TargetPlatform, Triplet, $"{PackageName}_{Triplet}");
-	}
-
-	void AddVcPackage(ReadOnlyTargetRules Target, string PackageName, bool AddInclude, params string[] Libraries)
-	{
-		string VcPackageRoot = GetVcPackageRoot(Target, PackageName);
-
-		if (!Directory.Exists(VcPackageRoot))
-		{
-			throw new DirectoryNotFoundException(VcPackageRoot);
-		}
-
-		string LibraryExtension = string.Empty;
-		if (Target.Platform == UnrealTargetPlatform.Win64)
-		{
-			LibraryExtension = ".lib";
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Linux || Target.Platform == UnrealTargetPlatform.LinuxAArch64)
-		{
-			LibraryExtension = ".a";
-		}
-
-		foreach (string Library in Libraries)
-		{
-			string LibraryPath = Path.Combine(VcPackageRoot, "lib", $"{Library}{LibraryExtension}");
-			if ((Target.Platform == UnrealTargetPlatform.Linux || Target.Platform == UnrealTargetPlatform.LinuxAArch64) && !Library.StartsWith("lib"))
-			{
-				LibraryPath = Path.Combine(VcPackageRoot, "lib", $"lib{Library}{LibraryExtension}");
-			}
-			if (!File.Exists(LibraryPath))
-			{
-				throw new FileNotFoundException(LibraryPath);
-			}
-			PublicAdditionalLibraries.Add(LibraryPath);
-		}
-
-		if (AddInclude)
-		{
-			string IncludePath = Path.Combine(VcPackageRoot, "include");
-			if (!Directory.Exists(IncludePath))
-			{
-				throw new DirectoryNotFoundException(IncludePath);
-			}
-
-			PublicIncludePaths.Add(Path.Combine(VcPackageRoot, "include"));
-		}
-	}
-
 	public Protobuf(ReadOnlyTargetRules Target) : base(Target)
 	{
 		Type = ModuleType.External;
