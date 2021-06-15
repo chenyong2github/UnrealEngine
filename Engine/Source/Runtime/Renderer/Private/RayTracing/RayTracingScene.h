7e9a7f77d508d8837a2c4f0c9ab30d04b6bb31e1
yuriy odonnell <Yuriy.ODonnell@epicgames.com>
2021-06-15 07:04:46 -0400

    Added extra ray tracing scene build input validation on the render thread timeline, to attempt catching issues before RHI thread where some of the context is no longer available.
    #ROBOMERGE-SOURCE: CL 16671689 via CL 16671693
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v834-16658389)
    
    [CL 16671694 by yuriy odonnell in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracingScene.h
+++ Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracingScene.h
@@ -74,6 +74,9 @@ public:
 	// This must be filled before calling BeginCreateTask().
 	TArray<FRayTracingGeometryInstance> Instances;
 
+	// Total flattened number of ray tracing geometry instances (a single FRayTracingGeometryInstance may represent many).
+	uint32 NumNativeInstances = 0;
+
 	// Geometries which still have a pending build request but are used this frame and require a force build.
 	TArray<const FRayTracingGeometry*> GeometriesToBuild;
 
