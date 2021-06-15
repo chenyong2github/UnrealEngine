7a5254f8a21263406dac0c1cd364c19ddc176731
steve robb <Steve.Robb@epicgames.com>
2021-06-15 07:20:23 -0400

    Ensure that our compilers support if constexpr and fold expressions.
    #rb james.hopkin
    #jira none
    
    #ROBOMERGE-SOURCE: CL 16671766 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v834-16658389)
    
    [CL 16671767 by steve robb in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Core/Public/Clang/ClangPlatform.h
+++ Engine/Source/Runtime/Core/Public/Clang/ClangPlatform.h
@@ -9,13 +9,13 @@
 #if defined(__cpp_if_constexpr)
 	#define PLATFORM_COMPILER_HAS_IF_CONSTEXPR 1
 #else
-	#define PLATFORM_COMPILER_HAS_IF_CONSTEXPR 0
+	#error "Compiler is expected to support if constexpr"
 #endif
 
 #if defined(__cpp_fold_expressions)
 	#define PLATFORM_COMPILER_HAS_FOLD_EXPRESSIONS 1
 #else
-	#define PLATFORM_COMPILER_HAS_FOLD_EXPRESSIONS 0
+	#error "Compiler is expected to support fold expressions"
 #endif
 
 #define PLATFORM_RETURN_ADDRESS()			__builtin_return_address(0)
--- Engine/Source/Runtime/Core/Public/MSVC/MSVCPlatform.h
+++ Engine/Source/Runtime/Core/Public/MSVC/MSVCPlatform.h
@@ -9,13 +9,13 @@
 #if _MSC_VER >= 1920
 	#define PLATFORM_COMPILER_HAS_IF_CONSTEXPR 1
 #else
-	#define PLATFORM_COMPILER_HAS_IF_CONSTEXPR 0
+	#error "Compiler is expected to support if constexpr"
 #endif
 
 #if defined(__cpp_fold_expressions)
 	#define PLATFORM_COMPILER_HAS_FOLD_EXPRESSIONS 1
 #else
-	#define PLATFORM_COMPILER_HAS_FOLD_EXPRESSIONS 0
+	#error "Compiler is expected to support fold expressions"
 #endif
 
 #define PLATFORM_RETURN_ADDRESS()	        _ReturnAddress()
