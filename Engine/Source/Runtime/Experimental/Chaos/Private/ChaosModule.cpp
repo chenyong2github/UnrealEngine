c7272713564738e6aee2efbe60b127ac50ec95a0
brice criswell <Brice.Criswell@epicgames.com>
2021-06-15 21:51:49 -0400

    Chaos : Geometry Collection - TConvex constructor that uses UE::Geometry::TConvexHull3 (Tyson Brochu) - Updated fracture to use UE::Geometry::TConvexHull3 generation. #jira UE-105455 #rb Benn.Gallagher, Chris.Caulfield #preflight 60c94e1aaff54d00018c2312
    #ROBOMERGE-SOURCE: CL 16683441 via CL 16683442
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v835-16672529)
    
    [CL 16683447 by brice criswell in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/ChaosModule.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/ChaosModule.cpp
@@ -5,3 +5,11 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_MODULE(FChaosEngineModule, Chaos);
+
+void FChaosEngineModule::StartupModule()
+{
+	if (FModuleManager::Get().ModuleExists(TEXT("GeometryCore")))
+	{
+		FModuleManager::Get().LoadModule("GeometryCore");
+	}
+}
--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/CollisionConvexMesh.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/CollisionConvexMesh.h
@@ -7,6 +7,7 @@
 #include "TriangleMesh.h"
 #include "Particles.h"
 #include "ChaosLog.h"
+#include "CompGeom/ConvexHull3.h"
 
 #define DEBUG_HULL_GENERATION 0
 
@@ -254,109 +255,7 @@ namespace Chaos
 
 	public:
 
-		static void Build(const TArray<FVec3>& InVertices, TArray <TPlaneConcrete<FReal, 3>>& OutPlanes, TArray<TArray<int32>>& OutFaceIndices, TArray<FVec3>& OutVertices, FAABB3& OutLocalBounds)
-		{
-			OutPlanes.Reset();
-			OutVertices.Reset();
-			OutLocalBounds = FAABB3::EmptyAABB();
-
-			const int32 NumVerticesIn = InVertices.Num();
-			if(NumVerticesIn == 0)
-			{
-				return;
-			}
-
-			const TArray<FVec3>* VerticesToUse = &InVertices;
-			TArray<FVec3> ModifiedVertices;
-
-			// For triangles and planar shapes, create a very thin prism as a convex
-			auto Inflate = [](const TArray<FVec3>& Source, TArray<FVec3>& Destination, const FVec3& Normal, FReal Inflation)
-			{
-				const int32 NumSource = Source.Num();
-				Destination.Reset();
-				Destination.SetNum(NumSource * 2);
-
-				for(int32 Index = 0; Index < NumSource; ++Index)
-				{
-					Destination[Index] = Source[Index];
-					Destination[NumSource + Index] = Source[Index] + Normal * Inflation;
-				}
-			};
-
-			FVec3 PlanarNormal(0);
-			if (NumVerticesIn == 3)
-			{
-				const bool bIsValidTriangle = IsValidTriangle(InVertices[0], InVertices[1], InVertices[2], PlanarNormal);
-
-				//TODO_SQ_IMPLEMENTATION: should do proper cleanup to avoid this
-				if(ensureMsgf(bIsValidTriangle, TEXT("FConvexBuilder::Build(): Generated invalid triangle!")))
-				{
-					Inflate(InVertices, ModifiedVertices, PlanarNormal, TriQuadPrismInflation());
-					VerticesToUse = &ModifiedVertices;
-					UE_LOG(LogChaos, Verbose, TEXT("Encountered a triangle in convex hull generation. Will prepare a prism of thickness %.5f in place of a triangle."), TriQuadPrismInflation());
-				}
-				else
-				{
-					return;
-				}
-			}
-			else if (IsPlanarShape(InVertices, PlanarNormal))
-			{
-				Inflate(InVertices, ModifiedVertices, PlanarNormal, TriQuadPrismInflation());
-				VerticesToUse = &ModifiedVertices;
-				UE_LOG(LogChaos, Verbose, TEXT("Encountered a planar shape in convex hull generation. Will prepare a prism of thickness %.5f in place of a triangle."), TriQuadPrismInflation());
-			}
-
-			const int32 NumVerticesToUse = VerticesToUse->Num();
-
-			OutLocalBounds = FAABB3((*VerticesToUse)[0], (*VerticesToUse)[0]);
-			for (int32 VertexIndex = 0; VertexIndex < NumVerticesToUse; ++VertexIndex)
-			{
-				OutLocalBounds.GrowToInclude((*VerticesToUse)[VertexIndex]);
-			}
-
-			if (NumVerticesToUse >= 4)
-			{
-				TArray<TVec3<int32>> Indices;
-				Params BuildParams;
-				BuildParams.HorizonEpsilon = Chaos::FConvexBuilder::SuggestEpsilon(*VerticesToUse);
-				BuildConvexHull(*VerticesToUse, Indices, BuildParams);
-				OutPlanes.Reserve(Indices.Num());
-				TMap<int32, int32> IndexMap; // maps original particle indices to output particle indices
-				int32 NewIdx = 0;
-
-				const auto AddIndex = [&IndexMap, &NewIdx](const int32 OriginalIdx)
-				{
-					if (int32* Idx = IndexMap.Find(OriginalIdx))
-					{
-						return *Idx;
-					}
-					IndexMap.Add(OriginalIdx, NewIdx);
-					return NewIdx++;
-				};
-
-				for(const TVec3<int32>& Idx : Indices)
-				{
-					FVec3 Vs[3] = { (*VerticesToUse)[Idx[0]], (*VerticesToUse)[Idx[1]], (*VerticesToUse)[Idx[2]] };
-					const FVec3 Normal = FVec3::CrossProduct(Vs[1] - Vs[0], Vs[2] - Vs[0]).GetUnsafeNormal();
-					OutPlanes.Add(TPlaneConcrete<FReal, 3>(Vs[0], Normal));
-					TArray<int32> FaceIndices;
-					FaceIndices.SetNum(3);
-					FaceIndices[0] = AddIndex(Idx[0]);
-					FaceIndices[1] = AddIndex(Idx[1]);
-					FaceIndices[2] = AddIndex(Idx[2]);
-					OutFaceIndices.Add(FaceIndices);
-				}
-
-				OutVertices.SetNum(IndexMap.Num());
-				for(const auto& Elem : IndexMap)
-				{
-					OutVertices[Elem.Value] = (*VerticesToUse)[Elem.Key];
-				}
-			}
-
-			UE_CLOG(OutVertices.Num() == 0, LogChaos, Warning, TEXT("Convex hull generation produced zero convex particles, collision will fail for this primitive."));
-		}
+		static CHAOS_API void Build(const TArray<FVec3>& InVertices, TArray <TPlaneConcrete<FReal, 3>>& OutPlanes, TArray<TArray<int32>>& OutFaceIndices, TArray<FVec3>& OutVertices, FAABB3& OutLocalBounds);
 
 		static void BuildConvexHull(const TArray<FVec3>& InVertices, TArray<TVec3<int32>>& OutIndices, const Params& InParams = Params())
 		{
@@ -645,6 +544,7 @@ namespace Chaos
 		static CHAOS_API int32 PerformGeometryReduction;
 		static CHAOS_API int32 VerticesThreshold;
 		static CHAOS_API int32 ComputeHorizonEpsilonFromMeshExtends;
+		static CHAOS_API bool bUseGeometryTConvexHull3;
 
 	private:
 
