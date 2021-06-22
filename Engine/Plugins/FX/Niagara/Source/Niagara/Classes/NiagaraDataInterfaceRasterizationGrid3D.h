e006dcc631ac1b64d46009855edd9e3d9a4380b0
devon penney <Devon.Penney@epicgames.com>
2021-06-22 15:32:48 -0400

    rasterization grid DI that uses atomic operations built sdf generation and rendering into base flip emitter updated all examples to use fluid rendering properly #rb Stu.Mckenna
    #ROBOMERGE-SOURCE: CL 16747510 via CL 16747514
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v835-16672529)
    
    [CL 16747519 by devon penney in ue5-release-engine-test branch]

---
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceRasterizationGrid3D.h
@@ -0,0 +1,122 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "NiagaraDataInterface.h"
+#include "NiagaraCommon.h"
+#include "NiagaraDataInterfaceRW.h"
+#include "ClearQuad.h"
+#include "NiagaraStats.h"
+
+#include "NiagaraDataInterfaceRasterizationGrid3D.generated.h"
+
+class FNiagaraSystemInstance;
+
+// store all data in in a class
+// move all data management to use per instance data
+// remove references to push data to render thread
+
+class RasterizationGrid3DRWInstanceData
+{
+public:
+
+	~RasterizationGrid3DRWInstanceData()
+	{
+#if STATS
+		DEC_MEMORY_STAT_BY(STAT_NiagaraGPUDataInterfaceMemory, GPUMemory);
+#endif
+	}
+
+	void ResizeBuffers();
+
+	FIntVector NumCells = FIntVector::ZeroValue;
+	float Precision = 0;
+	int ResetValue = 0;
+
+	bool NeedsRealloc = false;
+	
+	FTextureRWBuffer3D RasterizationBuffer;
+
+#if STATS
+	int32 GPUMemory = 0;
+#endif
+};
+
+struct FNiagaraDataInterfaceProxyRasterizationGrid3D : public FNiagaraDataInterfaceProxyRW
+{	
+	virtual void PreStage(FRHICommandList& RHICmdList, const FNiagaraDataInterfaceStageArgs& Context) override;
+	virtual void ConsumePerInstanceDataFromGameThread(void* PerInstanceData, const FNiagaraSystemInstanceID& Instance) override {}
+	virtual int32 PerInstanceDataPassedToRenderThreadSize() const override { return sizeof(RasterizationGrid3DRWInstanceData); }	
+
+	virtual FIntVector GetElementCount(FNiagaraSystemInstanceID SystemInstanceID) const override;
+
+	/* List of proxy data for each system instances*/
+	// #todo(dmp): this should all be refactored to avoid duplicate code
+	TMap<FNiagaraSystemInstanceID, RasterizationGrid3DRWInstanceData> SystemInstancesToProxyData;
+};
+
+UCLASS(EditInlineNew, Category = "Grid", meta = (DisplayName = "Rasterization Grid3D"))
+class NIAGARA_API UNiagaraDataInterfaceRasterizationGrid3D : public UNiagaraDataInterfaceGrid3D
+{
+	GENERATED_UCLASS_BODY()
+
+public:	
+
+	UPROPERTY(EditAnywhere, Category = "Grid")
+	float Precision;
+
+	UPROPERTY(EditAnywhere, Category = "Grid")
+	int ResetValue;
+
+public:
+
+	DECLARE_NIAGARA_DI_PARAMETER();
+
+	virtual void PostInitProperties() override
+	{
+		Super::PostInitProperties();
+
+		//Can we register data interfaces as regular types and fold them into the FNiagaraVariable framework for UI and function calls etc?
+		if (HasAnyFlags(RF_ClassDefaultObject))
+		{
+			ENiagaraTypeRegistryFlags Flags = ENiagaraTypeRegistryFlags::AllowAnyVariable | ENiagaraTypeRegistryFlags::AllowParameter;
+			FNiagaraTypeRegistry::Register(FNiagaraTypeDefinition(GetClass()), Flags);
+		}
+	}
+
+	//~ UNiagaraDataInterface interface
+	// VM functionality
+	virtual void GetFunctions(TArray<FNiagaraFunctionSignature>& OutFunctions) override;
+	virtual void GetVMExternalFunction(const FVMExternalFunctionBindingInfo& BindingInfo, void* InstanceData, FVMExternalFunction &OutFunc) override;
+
+	virtual bool Equals(const UNiagaraDataInterface* Other) const override;
+
+	// GPU sim functionality
+#if WITH_EDITORONLY_DATA
+	virtual void GetParameterDefinitionHLSL(const FNiagaraDataInterfaceGPUParamInfo& ParamInfo, FString& OutHLSL) override;
+	virtual bool GetFunctionHLSL(const FNiagaraDataInterfaceGPUParamInfo& ParamInfo, const FNiagaraDataInterfaceGeneratedFunction& FunctionInfo, int FunctionInstanceIndex, FString& OutHLSL) override;
+#endif
+
+	virtual void ProvidePerInstanceDataForRenderThread(void* DataForRenderThread, void* PerInstanceData, const FNiagaraSystemInstanceID& SystemInstance) override {}
+	virtual bool InitPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
+	virtual void DestroyPerInstanceData(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance) override;
+	virtual bool PerInstanceTick(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance, float DeltaSeconds) override { return false;  }
+	virtual int32 PerInstanceDataSize()const override { return sizeof(RasterizationGrid3DRWInstanceData); }
+	virtual bool PerInstanceTickPostSimulate(void* PerInstanceData, FNiagaraSystemInstance* SystemInstance, float DeltaSeconds) override;
+	virtual bool HasPostSimulateTick() const override { return true; }
+	virtual bool HasPreSimulateTick() const override { return true; }
+	//~ UNiagaraDataInterface interface END
+
+	void GetNumCells(FVectorVMContext& Context);
+	void SetNumCells(FVectorVMContext& Context);
+	void SetFloatResetValue(FVectorVMContext& Context);
+
+	static const FName SetNumCellsFunctionName;
+	static const FName SetFloatResetValueFunctionName;	
+
+protected:
+	//~ UNiagaraDataInterface interface
+	virtual bool CopyToInternal(UNiagaraDataInterface* Destination) const override;
+	//~ UNiagaraDataInterface interface END	
+};
+
+
