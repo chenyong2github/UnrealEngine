5e30af53834ee92df9059122d03ec8941f2eda3c
jeremie roy <jeremie.roy@epicgames.com>
2021-06-23 15:16:46 -0400

    Fix some thumbnails not being returned with the right header
    #jira UE-118866
    #rb simon.therriault
    #lockdown simon.tourangeau
    
    [CL 16760868 by jeremie roy in 4.27 branch]

--- Engine/Plugins/VirtualProduction/RemoteControl/Source/WebRemoteControl/Private/WebRemoteControlEditorRoutes.cpp
+++ Engine/Plugins/VirtualProduction/RemoteControl/Source/WebRemoteControl/Private/WebRemoteControlEditorRoutes.cpp
@@ -159,6 +159,7 @@ bool FWebRemoteControlEditorRoutes::HandleGetThumbnailRoute(const FHttpServerReq
 				FName ResourceName = ThumbnailBrush->GetResourceName();
 				if (FFileHelper::LoadFileToArray(Response->Body, *ResourceName.ToString()))
 				{
+					WebRemoteControlUtils::AddContentTypeHeaders(Response.Get(), TEXT("image/png"));
 					Response->Code = EHttpServerResponseCodes::Ok;
 				}
 			}
