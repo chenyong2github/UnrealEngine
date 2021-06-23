2513144acd869a1726d2f99f36936ec194ed728f
fred kimberley <Fred.Kimberley@epicgames.com>
2021-06-23 16:16:41 -0400

    Remove some unneeded calls to FetchActor.
    #ROBOMERGE-SOURCE: CL 16761511
    #ROBOMERGE-BOT: (v835-16672529)
    
    [CL 16761531 by fred kimberley in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
@@ -6858,9 +6858,9 @@ void UCharacterMovementComponent::HandleImpact(const FHitResult& Impact, float T
 		PFAgent->OnMoveBlockedBy(Impact);
 	}
 
-	APawn* OtherPawn = Impact.HitObjectHandle.FetchActor<APawn>();
-	if (OtherPawn)
+	if (Impact.HitObjectHandle.DoesRepresentClass(APawn::StaticClass()))
 	{
+		APawn* OtherPawn = Impact.HitObjectHandle.FetchActor<APawn>();
 		NotifyBumpedPawn(OtherPawn);
 	}
 
