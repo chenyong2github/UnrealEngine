18343847f4cefba117743bc53376ba0750d271be
danny chapman <danny.chapman@epicgames.com>
2021-07-15 19:43:10 -0400

    Add missing functionality to Control Rig SpringInterp so Verlet can be (and is now) deprecated
    #jira UE-119082
    #rb halfdan.ingvarsson
    
    [CL 16869380 by danny chapman in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Simulation/RigUnit_SpringInterp.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Simulation/RigUnit_SpringInterp.cpp
@@ -21,10 +21,20 @@ FRigUnit_SpringInterp_Execute()
 		// Treat the input as a frequency in Hz
 		float AngularFrequency = Strength * 2.0f * PI;
 		float Stiffness = AngularFrequency * AngularFrequency;
+		float AdjustedTarget = Target;
+		if (!FMath::IsNearlyZero(Stiffness))
+		{
+			AdjustedTarget += Force / (Stiffness * RigUnitSpringInterpConstants::Mass);
+		}
+		else
+		{
+			SpringState.Velocity += Force * (Context.DeltaTime / RigUnitSpringInterpConstants::Mass);
+		}
 		Result = UKismetMathLibrary::FloatSpringInterp(
-			bUseCurrentInput ? Current : Result, Target, SpringState, Stiffness, CriticalDamping,
+			bUseCurrentInput ? Current : Result, AdjustedTarget, SpringState, Stiffness, CriticalDamping,
 			Context.DeltaTime, RigUnitSpringInterpConstants::Mass, TargetVelocityAmount, 
 			false, 0.0f, 0.0f, !bUseCurrentInput || bInitializeFromTarget);
+		Velocity = SpringState.Velocity;
 	}
 }
 
@@ -41,11 +51,21 @@ FRigUnit_SpringInterpVector_Execute()
 		// Treat the input as a frequency in Hz
 		float AngularFrequency = Strength * 2.0f * PI;
 		float Stiffness = AngularFrequency * AngularFrequency;
+		FVector AdjustedTarget = Target;
+		if (!FMath::IsNearlyZero(Stiffness))
+		{
+			AdjustedTarget += Force / (Stiffness * RigUnitSpringInterpConstants::Mass);
+		}
+		else
+		{
+			SpringState.Velocity += Force * (Context.DeltaTime / RigUnitSpringInterpConstants::Mass);
+		}
 		Result = UKismetMathLibrary::VectorSpringInterp(
-			bUseCurrentInput ? Current : Result, Target, SpringState, Stiffness, CriticalDamping,
-			Context.DeltaTime, RigUnitSpringInterpConstants::Mass, TargetVelocityAmount, 
+			bUseCurrentInput ? Current : Result, AdjustedTarget, SpringState, Stiffness, CriticalDamping,
+			Context.DeltaTime, RigUnitSpringInterpConstants::Mass, TargetVelocityAmount,
 			false, FVector(), FVector(), !bUseCurrentInput || bInitializeFromTarget);
 	}
+	Velocity = SpringState.Velocity;
 }
 
 FRigUnit_SpringInterpQuaternion_Execute()
@@ -61,10 +81,12 @@ FRigUnit_SpringInterpQuaternion_Execute()
 		// Treat the input as a frequency in Hz
 		float AngularFrequency = Strength * 2.0f * PI;
 		float Stiffness = AngularFrequency * AngularFrequency;
+		SpringState.AngularVelocity += Torque * (Context.DeltaTime / RigUnitSpringInterpConstants::Mass);
 		Result = UKismetMathLibrary::QuaternionSpringInterp(
 			bUseCurrentInput ? Current : Result, Target, SpringState, Stiffness, CriticalDamping,
 			Context.DeltaTime, RigUnitSpringInterpConstants::Mass, TargetVelocityAmount, 
 			!bUseCurrentInput || bInitializeFromTarget);
+		AngularVelocity = SpringState.AngularVelocity;
 	}
 }
 
