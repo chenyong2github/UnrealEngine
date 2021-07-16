5299622734ff771e712de569aa17f8f3d9192d25
devin doucette <devin.doucette@epicgames.com>
2021-07-16 01:25:15 -0400

    DDC: Schedule non-blocking builds using tasks
    #rb Zousar.Shaker
    #rnx
    
    #ROBOMERGE-SOURCE: CL 16870688 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v836-16769935)
    
    [CL 16870691 by devin doucette in ue5-release-engine-test branch]

--- Engine/Source/Developer/DerivedDataCache/Private/DerivedDataBuildScheduler.cpp
+++ Engine/Source/Developer/DerivedDataCache/Private/DerivedDataBuildScheduler.cpp
@@ -6,13 +6,102 @@
 #include "DerivedDataBuildFunctionFactory.h"
 #include "DerivedDataBuildFunctionRegistry.h"
 #include "Misc/Guid.h"
+#include "Tasks/Task.h"
 
 namespace UE::DerivedData::Private
 {
 
 class FBuildScheduler final : public IBuildScheduler
 {
+public:
+	void DispatchCacheQuery(IBuildJob* Job, const FBuildSchedulerParams& Params) final;
+	void DispatchCacheStore(IBuildJob* Job, const FBuildSchedulerParams& Params) final;
+	void DispatchResolveKey(IBuildJob* Job) final;
+	void DispatchResolveInputMeta(IBuildJob* Job) final;
+	void DispatchResolveInputData(IBuildJob* Job, const FBuildSchedulerParams& Params) final;
+	void DispatchExecuteRemote(IBuildJob* Job, const FBuildSchedulerParams& Params) final;
+	void DispatchExecuteLocal(IBuildJob* Job, const FBuildSchedulerParams& Params) final;
 };
+
+void FBuildScheduler::DispatchCacheQuery(IBuildJob* Job, const FBuildSchedulerParams& Params)
+{
+	Job->Schedule();
+}
+
+void FBuildScheduler::DispatchCacheStore(IBuildJob* Job, const FBuildSchedulerParams& Params)
+{
+	Job->Schedule();
+}
+
+void FBuildScheduler::DispatchResolveKey(IBuildJob* Job)
+{
+	if (Job->GetPriority() == EPriority::Blocking)
+	{
+		Job->Schedule();
+	}
+	else
+	{
+		Tasks::Launch(TEXT("FBuildScheduler::DispatchResolveKey"),
+			[Job = TRequest(Job)] { Job->Schedule(); },
+			LowLevelTasks::ETaskPriority::BackgroundNormal);
+	}
+}
+
+void FBuildScheduler::DispatchResolveInputMeta(IBuildJob* Job)
+{
+	if (Job->GetPriority() == EPriority::Blocking)
+	{
+		Job->Schedule();
+	}
+	else
+	{
+		Tasks::Launch(TEXT("FBuildScheduler::DispatchResolveInputMeta"),
+			[Job = TRequest(Job)] { Job->Schedule(); },
+			LowLevelTasks::ETaskPriority::BackgroundNormal);
+	}
+}
+
+void FBuildScheduler::DispatchResolveInputData(IBuildJob* Job, const FBuildSchedulerParams& Params)
+{
+	if (Job->GetPriority() == EPriority::Blocking)
+	{
+		Job->Schedule();
+	}
+	else
+	{
+		Tasks::Launch(TEXT("FBuildScheduler::DispatchResolveInputData"),
+			[Job = TRequest(Job)] { Job->Schedule(); },
+			LowLevelTasks::ETaskPriority::BackgroundNormal);
+	}
+}
+
+void FBuildScheduler::DispatchExecuteRemote(IBuildJob* Job, const FBuildSchedulerParams& Params)
+{
+	if (Job->GetPriority() == EPriority::Blocking)
+	{
+		Job->Schedule();
+	}
+	else
+	{
+		Tasks::Launch(TEXT("FBuildScheduler::DispatchExecuteRemote"),
+			[Job = TRequest(Job)] { Job->Schedule(); },
+			LowLevelTasks::ETaskPriority::BackgroundNormal);
+	}
+}
+
+void FBuildScheduler::DispatchExecuteLocal(IBuildJob* Job, const FBuildSchedulerParams& Params)
+{
+	if (Job->GetPriority() == EPriority::Blocking)
+	{
+		Job->Schedule();
+	}
+	else
+	{
+		Tasks::Launch(TEXT("FBuildScheduler::DispatchExecuteLocal"),
+			[Job = TRequest(Job)] { Job->Schedule(); },
+			LowLevelTasks::ETaskPriority::BackgroundNormal);
+	}
+}
 
 IBuildScheduler* CreateBuildScheduler()
 {
