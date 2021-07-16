9b08e5fff0a96fe6ab1e52a84ee8e9f00b277640
danny chapman <danny.chapman@epicgames.com>
2021-07-15 20:20:16 -0400

    Undeprecate Verlet control rig node, and add Verlet to the list of SpringInterp keywords
    #rb trivial
    
    [CL 16869645 by danny chapman in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Simulation/RigUnit_Verlet.h
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Simulation/RigUnit_Verlet.h
@@ -10,7 +10,7 @@
  * Simulates a single position over time using Verlet integration. It is recommended to use SpringInterp instead as it
  * is more accurate and stable, and has more meaningful parameters.
  */
-USTRUCT(meta=(DisplayName="Verlet (Vector)", PrototypeName="Verlet", Keywords="Simulate,Integrate", Deprecated = "5.0"))
+USTRUCT(meta=(DisplayName="Verlet (Vector)", PrototypeName="Verlet", Keywords="Simulate,Integrate"))
 struct CONTROLRIG_API FRigUnit_VerletIntegrateVector : public FRigUnit_SimBase
 {
 	GENERATED_BODY()
