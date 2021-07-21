acb99cb9fcff4b1952aeb196093819f76c952412
DecoyRS <a.g.pirogov@gmail.com>
2021-07-21 10:53:02 -0400

    PR #8189: Adding support for Linux for RiderSourceCodeAccessPlugin (Contributed by DecoyRS)
    #preflight 60f823650ce95d0001e6224a
    #rb none
    #jira UE-119181
    
    [CL 16909682 by DecoyRS in 4.27 branch]

--- Engine/Plugins/Developer/RiderSourceCodeAccess/Source/RiderSourceCodeAccess/Private/RiderPathLocator/Common/RiderPathLocator.cpp
+++ Engine/Plugins/Developer/RiderSourceCodeAccess/Source/RiderSourceCodeAccess/Private/RiderPathLocator/Common/RiderPathLocator.cpp
@@ -1,7 +1,8 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
-#include "RiderPathLocator/RiderPathLocator.h"
 
+#include "RiderPathLocator/RiderPathLocator.h"
 #include "Dom/JsonObject.h"
+#include "Interfaces/IPluginManager.h"
 #include "Internationalization/Regex.h"
 #include "Misc/FileHelper.h"
 #include "Misc/Paths.h"
@@ -34,17 +35,17 @@ TArray<FInstallInfo> FRiderPathLocator::GetInstallInfosFromToolbox(const FString
 	const FString ToolboxRiderRootPath = FPaths::Combine(InstallPath, TEXT("apps"));
 	if(!FPaths::DirectoryExists(ToolboxRiderRootPath)) return {};
 
-	return GetInstallInfos(ToolboxRiderRootPath, Pattern, true);
+	return GetInstallInfos(ToolboxRiderRootPath, Pattern, FInstallInfo::EInstallType::Toolbox);
 }
 
-TArray<FInstallInfo> FRiderPathLocator::GetInstallInfos(const FString& ToolboxRiderRootPath, const FString& Pattern, bool IsToolbox)
+TArray<FInstallInfo> FRiderPathLocator::GetInstallInfos(const FString& ToolboxRiderRootPath, const FString& Pattern, FInstallInfo::EInstallType InstallType)
 {
 	TArray<FInstallInfo> RiderInstallInfos;
 	TArray<FString> RiderPaths;
 	IFileManager::Get().FindFilesRecursive(RiderPaths, *ToolboxRiderRootPath, *Pattern, true, true);
 	for(const FString& RiderPath: RiderPaths)
 	{
-		TOptional<FInstallInfo> InstallInfo = GetInstallInfoFromRiderPath(RiderPath, IsToolbox);
+		TOptional<FInstallInfo> InstallInfo = GetInstallInfoFromRiderPath(RiderPath, InstallType);
 		if(InstallInfo.IsSet())
 			RiderInstallInfos.Add(InstallInfo.GetValue());
 	}
@@ -83,4 +84,26 @@ void FRiderPathLocator::ParseProductInfoJson(FInstallInfo& Info, const FString& 
 			if(SupportUprojectStateValue.Equals(TEXT("Release"))) Info.SupportUprojectState = FInstallInfo::ESupportUproject::Release;
 		}
 	}
+}
+
+TArray<FInstallInfo> FRiderPathLocator::GetInstallInfosFromResourceFile()
+{
+	const TSharedPtr<IPlugin> Plugin = IPluginManager::Get().FindPlugin(TEXT("RiderSourceCodeAccess"));
+	if(!Plugin.IsValid()) return {};
+	
+	const FString RiderLocationsFile = FPaths::Combine(Plugin->GetBaseDir(), TEXT("Resources"), TEXT("RiderLocations.txt"));
+	TArray<FString> RiderLocations;
+	if(FFileHelper::LoadFileToStringArray(RiderLocations, *RiderLocationsFile) == false) return {};
+
+	TArray<FInstallInfo> RiderInstallInfos;
+	for(const auto& RiderLocation : RiderLocations)
+	{
+		const FString Location = RiderLocation.TrimStartAndEnd();
+		if(Location.StartsWith("#")) continue;
+		
+		TOptional<FInstallInfo> InstallInfo = GetInstallInfoFromRiderPath(Location, FInstallInfo::EInstallType::Custom);
+		if(InstallInfo.IsSet())
+			RiderInstallInfos.Add(InstallInfo.GetValue());
+	}
+	return RiderInstallInfos;
 }
