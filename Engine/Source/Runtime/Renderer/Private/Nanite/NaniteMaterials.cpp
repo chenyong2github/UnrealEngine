668a499af625fea339a0790d5ad0f1ce1e2dfcab
graham wihlidal <Graham.Wihlidal@epicgames.com>
2021-07-21 15:40:49 -0400

    Implemented (stubbed out) FNaniteMaterialCommands and associated files - part of ongoing work at refactoring and optimizing Nanite material shading to be indirect dispatch and a more compact representation.
    #ROBOMERGE-SOURCE: CL 16891977
    #ROBOMERGE-BOT: (v836-16769935)
    
    [CL 16914437 by graham wihlidal in ue5-main branch]

---
+++ Engine/Source/Runtime/Renderer/Private/Nanite/NaniteMaterials.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NaniteMaterials.h"
+#include "RHI.h"
+#include "SceneUtils.h"
+#include "ScenePrivate.h"
+#include "GPUScene.h"
+#include "ClearQuad.h"
+#include "RendererModule.h"
+
+FNaniteMaterialCommands::FNaniteMaterialCommands()
+{
+}
+
+FNaniteMaterialCommands::~FNaniteMaterialCommands()
+{
+}
