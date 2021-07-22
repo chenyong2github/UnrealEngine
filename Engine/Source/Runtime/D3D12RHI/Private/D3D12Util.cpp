bf8b706ff9e2c1acfa743df7cbcbd4b1cda431b5
mihnea balta <Mihnea.Balta@epicgames.com>
2021-07-22 14:12:35 -0400

    Fix D3D12 crash when using Windows versions older than 1903.
    We were adding D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE on transitions without checking if VRS is supported, which causes a fatal error on 1809 and earlier.
    
    #rb Steve.Smith
    #jira UE-119539
    #rnx
    
    #ROBOMERGE-SOURCE: CL 16927954 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v838-16927207)
    
    [CL 16927973 by mihnea balta in ue5-release-engine-test branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Util.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Util.cpp
@@ -1276,7 +1276,7 @@ D3D12_RESOURCE_STATES GetD3D12ResourceState(ERHIAccess InRHIAccess, bool InIsAsy
 				State |= D3D12_RESOURCE_STATE_DEPTH_READ;
 			}
 #if PLATFORM_SUPPORTS_VARIABLE_RATE_SHADING
-			if (EnumHasAnyFlags(InRHIAccess, ERHIAccess::ShadingRateSource))
+			if (EnumHasAnyFlags(InRHIAccess, ERHIAccess::ShadingRateSource) && GRHISupportsPipelineVariableRateShading)
 			{
 				State |= D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE;
 			}
