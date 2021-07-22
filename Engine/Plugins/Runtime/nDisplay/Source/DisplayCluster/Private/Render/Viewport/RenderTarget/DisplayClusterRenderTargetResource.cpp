0660dae5260e6af5dbb0b6071a37025c3a997c5f
Brandon Schaefer <Brandon.Schaefer@epicgames.com>
2021-07-22 19:16:48 -0400

    Fix compiler warning/error on clang
    #jira none
    #rb none
    #fyi Vitalii.Boiko
    
    [CL 16932585 by Brandon Schaefer in ue5-main branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Viewport/RenderTarget/DisplayClusterRenderTargetResource.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Viewport/RenderTarget/DisplayClusterRenderTargetResource.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "Render/Viewport/RenderTarget/DisplayClusterRenderTargetResource.h"
 
 #include "RHI.h"
--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterShaders/Private/WarpBlend/Math/DisplayClusterWarpBlendMath_Frustum.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterShaders/Private/WarpBlend/Math/DisplayClusterWarpBlendMath_Frustum.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "WarpBlend/Math/DisplayClusterWarpBlendMath_Frustum.h"
 
 #include "Render/IDisplayClusterRenderTexture.h"
