60d45b85edb4d70bea68edff8a2ac429c7402acc
leon huang <leon.huang@epicgames.com>
2021-07-23 00:44:12 -0400

    Updated build scripts for Flite text to speech to support easier porting to different platforms. Edited Flite cmake scripts to exclude files that interfere with cross compilation. Regenerated Windows Flite libraries from the updated cmake scripts. #jira: none #rb: Daren.Cheng, Josh.Adams #preflight 60f9d0230ce95d00012da8f2
    #ROBOMERGE-SOURCE: CL 16934412 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v838-16927207)
    
    [CL 16934440 by leon huang in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/TextToSpeech/Source/TextToSpeech/TextToSpeech.Build.cs
+++ Engine/Plugins/Experimental/TextToSpeech/Source/TextToSpeech/TextToSpeech.Build.cs
@@ -5,6 +5,17 @@ using UnrealBuildTool;
 
 public class TextToSpeech : ModuleRules
 {
+
+	// platforms that inherit from this should override and return true if they intend to use Flite
+	protected virtual bool bIsFliteUsed
+	{
+		get
+		{
+			// for now only Win64 needs to use Flite. Can be expanded to Android etc in future
+			return Target.Platform == UnrealTargetPlatform.Win64;
+		}
+	}
+
 	public TextToSpeech(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PublicDependencyModuleNames.AddRange(
@@ -30,7 +41,7 @@ public class TextToSpeech : ModuleRules
 				"Engine"
 			}
 		);
-		if (Target.Platform == UnrealTargetPlatform.Win64)
+		if (bIsFliteUsed)
 		{
 			PrivateDependencyModuleNames.AddRange(
 				new string[] 
@@ -42,12 +53,6 @@ public class TextToSpeech : ModuleRules
 			);
 			AddEngineThirdPartyPrivateStaticDependencies(Target, "Flite");
 		}
-		PrivateDefinitions.Add("USING_FLITE=" + (IsFliteUsed() ? "1" : "0"));
+		PrivateDefinitions.Add("USING_FLITE=" + (bIsFliteUsed ? "1" : "0"));
 	}
-	// platforms that inherit from this should override and return true if they intend to use Flite
-	protected virtual bool IsFliteUsed()
-	{
-		// for now only Win64 needs to use Flite. Can be expanded to Android etc in future
-		return Target.Platform == UnrealTargetPlatform.Win64;
-}
 }
