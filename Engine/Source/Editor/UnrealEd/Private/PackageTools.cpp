82fdc81ac85a7ef4c2a4ca08eb4b6e5ba688d336
jason walter <jason.walter@epicgames.com>
2021-07-23 13:53:05 -0400

    Add a flag to CreateNewMapForEditing to disable dialog. #jira UE-120231
    #rb mike.beach
    
    #ushell-cherrypick of 16865677 by jason.walter
    
    [CL 16939240 by jason walter in 4.27 branch]

--- Engine/Source/Editor/UnrealEd/Private/PackageTools.cpp
+++ Engine/Source/Editor/UnrealEd/Private/PackageTools.cpp
@@ -666,7 +666,8 @@ UPackageTools::UPackageTools(const FObjectInitializer& ObjectInitializer)
 				// Unload the current world
 				if (GIsEditor)
 				{
-					GEditor->CreateNewMapForEditing();
+					const bool bPromptForSave = InteractionMode == UPackageTools::EReloadPackagesInteractionMode::Interactive;
+					GEditor->CreateNewMapForEditing(bPromptForSave);
 				}
 				else if (UGameEngine* GameEngine = Cast<UGameEngine>(GEngine))
 				{
