ed059fdb340da717c739a1aa644898a4444f3ad8
francis hurteau <Francis.Hurteau@epicgames.com>
2021-07-23 16:16:08 -0400

    Undo changelist 16938823 until investigation of cook failures are done
    [FYI] Zousar.Shaker
    #rb trivial
    #preflight skip
    discussed with Zoursar.Shaker, undo to fix cook failure
    
    
    #ROBOMERGE-SOURCE: CL 16941401
    #ROBOMERGE-BOT: (v838-16927207)
    
    [CL 16941404 by francis hurteau in ue5-main branch]

--- Engine/Source/Developer/DerivedDataCache/Private/Tests/HttpDerivedDataBackendTest.cpp
+++ Engine/Source/Developer/DerivedDataCache/Private/Tests/HttpDerivedDataBackendTest.cpp
@@ -62,9 +62,15 @@ private:
 	}
 };
 
-// Helper function to create a number of dummy cache keys for testing
-TArray<FString> CreateTestCacheKeys(UE::DerivedData::Backends::FHttpDerivedDataBackend* InTestBackend, uint32 InNumKeys)
+IMPLEMENT_HTTPDERIVEDDATA_AUTOMATION_TEST(FConcurrentCachedDataProbablyExistsBatch, TEXT(".FHeavyConcurrentCachedDataProbablyExistsBatch"), EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
+bool FConcurrentCachedDataProbablyExistsBatch::RunTest(const FString& Parameters)
 {
+	UE::DerivedData::Backends::FHttpDerivedDataBackend* TestBackend = GetTestBackend();
+
+	const int32 ParallelTasks = 32;
+	const uint32 Iterations = 20;
+	const uint32 KeysInBatch = 4;
+
 	TArray<FString> Keys;
 	TArray<uint8> KeyContents;
 	KeyContents.Add(42);
@@ -76,25 +82,12 @@ TArray<FString> CreateTestCacheKeys(UE::DerivedData::Backends::FHttpDerivedDataB
 	HashState.GetHash(Hash);
 	const FString HashString = BytesToHex(Hash, FSHA1::DigestSize);
 
-	for (uint32 KeyIndex = 0; KeyIndex < InNumKeys; ++KeyIndex)
+	for (uint32 KeyIndex = 0; KeyIndex < KeysInBatch; ++KeyIndex)
 	{
 		FString NewKey = FString::Printf(TEXT("__AutoTest_Dummy_%u__%s"), KeyIndex, *HashString);
 		Keys.Add(NewKey);
-		InTestBackend->PutCachedData(*NewKey, KeyContents, false);
+		TestBackend->PutCachedData(*NewKey, KeyContents, false);
 	}
-	return Keys;
-}
-
-IMPLEMENT_HTTPDERIVEDDATA_AUTOMATION_TEST(FConcurrentCachedDataProbablyExistsBatch, TEXT(".FConcurrentCachedDataProbablyExistsBatch"), EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-bool FConcurrentCachedDataProbablyExistsBatch::RunTest(const FString& Parameters)
-{
-	UE::DerivedData::Backends::FHttpDerivedDataBackend* TestBackend = GetTestBackend();
-
-	const int32 ParallelTasks = 32;
-	const uint32 Iterations = 20;
-	const uint32 KeysInBatch = 4;
-
-	TArray<FString> Keys = CreateTestCacheKeys(TestBackend, KeysInBatch);
 
 	std::atomic<uint32> MismatchedResults = 0;
 	ParallelFor(ParallelTasks,
@@ -113,41 +106,6 @@ bool FConcurrentCachedDataProbablyExistsBatch::RunTest(const FString& Parameters
 	);
 	TestEqual(TEXT("Concurrent calls to CachedDataProbablyExistsBatch for a batch of keys that were put are not reliably found"), MismatchedResults, 0);
 
-	return true;
-}
-
-// This test validate that batch requests wont mismatch head and get request for the same keys in the same batch
-IMPLEMENT_HTTPDERIVEDDATA_AUTOMATION_TEST(FConcurrentExistsAndGetForSameKeyBatch, TEXT(".FConcurrentExistsAndGetForSameKeyBatch"), EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
-bool FConcurrentExistsAndGetForSameKeyBatch::RunTest(const FString& Parameters)
-{
-	UE::DerivedData::Backends::FHttpDerivedDataBackend* TestBackend = GetTestBackend();
-
-	const int32 ParallelTasks = 32;
-	const uint32 Iterations = 20;
-	const uint32 KeysInBatch = 4;
-
-	TArray<FString> Keys = CreateTestCacheKeys(TestBackend, KeysInBatch);
-
-	ParallelFor(ParallelTasks,
-		[&](int32 TaskIndex)
-		{
-			for (uint32 Iteration = 0; Iteration < Iterations; ++Iteration)
-			{
-				for (int32 KeyIndex = 0; KeyIndex < Keys.Num(); ++KeyIndex)
-				{
-					if ((TaskIndex % 2) ^ (KeyIndex % 2))
-					{
-						TestBackend->CachedDataProbablyExists(*Keys[KeyIndex]);
-					}
-					else
-					{
-						TArray<uint8> OutData;
-						TestBackend->GetCachedData(*Keys[KeyIndex], OutData);
-					}
-				}
-			}
-		}
-	);
 	return true;
 }
 
