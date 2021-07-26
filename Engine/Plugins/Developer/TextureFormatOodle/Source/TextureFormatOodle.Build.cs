41d43ca49541055dd57b290e794da3c87d466375
charles bloom <charles.bloom@epicgames.com>
2021-07-25 21:24:02 -0400

    Update to Oodle 2.9.3
    #rb none
    
    #ROBOMERGE-SOURCE: CL 16952276 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v838-16927207)
    
    [CL 16952280 by charles bloom in ue5-release-engine-test branch]

--- Engine/Plugins/Developer/TextureFormatOodle/Source/TextureFormatOodle.Build.cs
+++ Engine/Plugins/Developer/TextureFormatOodle/Source/TextureFormatOodle.Build.cs
@@ -7,7 +7,7 @@ using UnrealBuildTool;
 
 public class TextureFormatOodle : ModuleRules
 {
-	protected virtual string OodleVersion { get { return "2.9.2"; } }
+	protected virtual string OodleVersion { get { return "2.9.3"; } }
 
 	// Platform Extensions need to override these
 	protected virtual string LibRootDirectory { get { return ModuleDirectory; } }
--- Engine/Source/Runtime/OodleDataCompression/OodleDataCompression.Build.cs
+++ Engine/Source/Runtime/OodleDataCompression/OodleDataCompression.Build.cs
@@ -9,7 +9,7 @@ using UnrealBuildTool;
 //{
 	public class OodleDataCompression : ModuleRules
 	{
-		protected virtual string OodleVersion { get { return "2.9.2"; } }
+		protected virtual string OodleVersion { get { return "2.9.3"; } }
 
 		// Platform Extensions need to override these
 		protected virtual string LibRootDirectory { get { return ModuleDirectory; } }
