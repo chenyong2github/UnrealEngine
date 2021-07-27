7bb14af71dc2a932c6b0419c0b37c114a1ea3ba2
JeanLuc Corenthin <JeanLuc.Corenthin@epicgames.com>
2021-07-27 15:04:30 -0400

    Fixed issue with installing exporter for AC25 on WIndows and Mac
    #jira UE-120144
    #rb benoit.deschenes
    #lockdown simon.tourangeau
    
    [CL 16971696 by JeanLuc Corenthin in 4.27 branch]

--- Engine/Source/Programs/Enterprise/Datasmith/DatasmithARCHICADExporter/Installer/CustomActionManaged/CustomAction.cs
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithARCHICADExporter/Installer/CustomActionManaged/CustomAction.cs
@@ -57,7 +57,7 @@ namespace CustomActionManaged
 		public static bool Is64BitOperatingSystem = (IntPtr.Size == 8) || InternalIs64BitOperatingSystem();
 	}
 
-	public static class RegistryHelper
+	public class CustomActions
 	{
 		[DllImport("Advapi32.dll")]
 		static extern uint RegOpenKeyEx(
@@ -65,14 +65,14 @@ namespace CustomActionManaged
 			string lpSubKey,
 			uint ulOptions,
 			int samDesired,
-			out int phkResult);
+			out UIntPtr phkResult);
 
 		[DllImport("Advapi32.dll")]
-		static extern uint RegCloseKey(int hKey);
+		static extern uint RegCloseKey(UIntPtr hKey);
 
 		[DllImport("advapi32.dll", EntryPoint = "RegQueryValueEx")]
 		public static extern int RegQueryValueEx(
-			int hKey, string lpValueName,
+			UIntPtr hKey, string lpValueName,
 			int lpReserved,
 			ref uint lpType,
 			System.Text.StringBuilder lpData,
@@ -84,41 +84,6 @@ namespace CustomActionManaged
 		public static int QueryValue = 0x0001;
 		public static int WOW64_64Key = 0x0100;
 
-		/// <summary>
-		/// Helper function that allows the retrieval of 64bit registry values from 32bit processes.  This
-		/// is a required workaround because we are limited to using .NET 2.0 and because we run the custom
-		/// action in 32bit mode which results in registry redirection(virtualization) when using Registry.GetValue().
-		/// </summary>
-		static public string GetRegKey64(UIntPtr KeySection, String KeyName, String ValueName)
-		{
-			int Key = 0;
-			string Data = null;
-
-			try
-			{
-				if (RegOpenKeyEx(KeySection, KeyName, 0, QueryValue | WOW64_64Key, out Key) == 0)
-				{
-					uint Type = 0;
-					uint DataSize = 1024;
-					StringBuilder DataSB = new StringBuilder(1024);
-					RegQueryValueEx(Key, ValueName, 0, ref Type, DataSB, ref DataSize);
-					Data = DataSB.ToString();
-				}
-			}
-			finally
-			{
-				if (Key != 0)
-				{
-					RegCloseKey(Key);
-				}
-			}
-
-			return Data;
-		}
-	}
-
-	public class CustomActions
-	{
 		/// <summary>
 		/// Helper function for to get session properties in a safe way.  Returns the empty string if the property is not found.
 		/// </summary>
@@ -157,25 +122,96 @@ namespace CustomActionManaged
 		{
 			try
 			{
-				string[] PropertiesToUpdate = new string[] {
-					"ARCHICAD25DIR",
-					"ARCHICAD24DIR",
-					"ARCHICAD23DIR",
+				string[] MajorVersions = new string[] { "25", "24", "23", };
+
+				string[] MinorVersions = new string[] { "0", };
+
+				string[] Products = new string[] { "FULL", "SOLO", };
+
+				string[] Releases = new string[] { "R1-1", };
+
+				string[] Languages = new string[] {
+					"INT", "GER", "USA", "UKI", "HUN", "FRA", "CHE", "AUT", "CHI", "CZE", "FIN", "GRE",
+					"NED", "ITA", "JPN", "KOR", "POL", "POR", "RUS", "SPA", "SWE", "TAI", "NOR", "NZE",
+					"AUS", "TUR", "BRA", "UKR",
 				};
 
-				string[] RegistryKeyPaths = new string[] {
-					"SOFTWARE\\GRAPHISOFT SE\\ARCHICAD 25 RC1",
-					"SOFTWARE\\GRAPHISOFT SE\\ARCHICAD 24",
-					"SOFTWARE\\GRAPHISOFT SE\\ARCHICAD 23",
-				};
+				string RegistryKeyFormat = "SOFTWARE\\GRAPHISOFT Installers\\ARCHICAD\\ARCHICAD {0}.{1} {2} {3} {4}";
 
-				for (int i = 0; i < RegistryKeyPaths.Length; i++)
+				foreach(string MajorVersion in MajorVersions)
 				{
-					string Value = RegistryHelper.GetRegKey64(RegistryHelper.HKEY_LOCAL_MACHINE, RegistryKeyPaths[i], "Location");
-					if (Value != string.Empty)
+					foreach (string Product in Products)
 					{
-						SetSessionProperty(session, PropertiesToUpdate[i], Value);
-						session.Log("Updating property {0} to value {1}.", PropertiesToUpdate[i], Value);
+						// Check if ArchiCAD for this major version and this product is installed on the computer
+						bool bKeyEntryFound = false;
+
+						foreach (string MinorVersion in MinorVersions)
+						{
+							foreach (string Language in Languages)
+							{
+								foreach (string Release in Releases)
+								{
+									// Build key path from version, language and product
+									string RegistryKeyPath = string.Format(RegistryKeyFormat, MajorVersion, MinorVersion, Language, Product, Release);
+
+									UIntPtr Key = UIntPtr.Zero;
+									StringBuilder DataSB = new StringBuilder(1024);
+
+									try
+									{
+										if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, RegistryKeyPath, 0, QueryValue | WOW64_64Key, out Key) == 0)
+										{
+											uint Type = 0;
+											uint DataSize = 1024;
+											int Ret = RegQueryValueEx(Key, "InstallLocation", 0, ref Type, DataSB, ref DataSize);
+
+											bKeyEntryFound = DataSB.Length > 0;
+
+											bKeyEntryFound = true;
+										}
+									}
+									finally
+									{
+										if (Key != UIntPtr.Zero)
+										{
+											RegCloseKey(Key);
+										}
+									}
+
+									if (bKeyEntryFound)
+									{
+										string InstallLocationDir = DataSB.ToString();
+
+										string[] ColladaInOutFiles = Directory.GetFiles(InstallLocationDir, "Collada In-Out.apx", SearchOption.AllDirectories);
+										if(ColladaInOutFiles.Count() > 0)
+										{
+											string AddOnInOutDir = Path.GetDirectoryName(ColladaInOutFiles[0]);
+
+											// Update property storing install directory
+											string PropertyToUpdate = string.Format("ARCHICAD{0}{1}DIR", MajorVersion, Product);
+											SetSessionProperty(session, PropertyToUpdate, InstallLocationDir);
+
+											PropertyToUpdate = string.Format("ARCHICAD{0}{1}ADDONSDIR", MajorVersion, Product);
+											SetSessionProperty(session, PropertyToUpdate, AddOnInOutDir);
+										}
+
+										break;
+									}
+								}
+
+								// Property has been no need to look ay further
+								if (bKeyEntryFound == true)
+								{
+									break;
+								}
+							}
+
+							// Property has been no need to look ay further
+							if (bKeyEntryFound == true)
+							{
+								break;
+							}
+						}
 					}
 				}
 
