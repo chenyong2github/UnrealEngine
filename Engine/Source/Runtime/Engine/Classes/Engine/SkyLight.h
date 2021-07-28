ef89c133ba84cb1e85cbb980e5597eebd559bf20
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2021-07-28 11:18:52 -0400

    WorldPartition: Add missing ShowCategories for DataLayers category on AInfo(s) that support Data Layers
    #rb sebastien.lussier
    #preflight 6101665e2198a10001a21f0c
    
    #ROBOMERGE-SOURCE: CL 16982707 via CL 16982712
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v838-16927207)
    
    [CL 16982724 by patrick enfedaque in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/SkyLight.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/SkyLight.h
@@ -7,7 +7,7 @@
 #include "GameFramework/Info.h"
 #include "SkyLight.generated.h"
 
-UCLASS(ClassGroup=Lights, hidecategories=(Input,Collision,Replication,Info), showcategories=("Rendering", "Input|MouseInput", "Input|TouchInput"), ComponentWrapperClass, ConversionRoot, Blueprintable)
+UCLASS(ClassGroup=Lights, hidecategories=(Input,Collision,Replication,Info), showcategories=(Rendering, DataLayers, "Input|MouseInput", "Input|TouchInput"), ComponentWrapperClass, ConversionRoot, Blueprintable)
 class ENGINE_API ASkyLight : public AInfo
 {
 	GENERATED_UCLASS_BODY()
