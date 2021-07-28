8091273c3f6eb3d41808089c42b7891a1c1d8e8e
andrew lauritzen <Andrew.Lauritzen@epicgames.com>
2021-07-28 17:00:17 -0400

    Implement persistent shadow physical page pool - Eliminates the previous frame texture (~128MB in default config) and copy - Cached pages are kept in place, with a quick pass to find and allocate free pages following   - Experimented with a persistent free list but for the small numbers of pages we are talking about (2k-4k ish standard), recomputing the free list each frame was cheap and robust   - Main cost is synchronization/barriers between the new passes. Fairly low measured overhead compared to the much larger wins, but can revisit later if need be. - Removed a few things that added complexity but were not making a big difference to performance, especially with the changes:   - No longer support "panning" the directional light cascade cached pages. With the changes to the snapping and depth range invalidations this was no longer making much of a difference anyways.   - No longer do the hierarchical "eliminate parent if all 4 mip children are marked" for local lights. Tested a bit and it was not making a measurable difference in most cases with caching. Can be revived if useful later. - Moved ownership of the now persistent physical pool to CacheManager - A bunch of related cleanup and fixes
    #rb ola.olsson
    #preflight 61008a655938f90001f04260
    
    
    #ROBOMERGE-OWNER: andrew.lauritzen
    #ROBOMERGE-AUTHOR: andrew.lauritzen
    #ROBOMERGE-SOURCE: CL 16985509
    #ROBOMERGE-BOT: (v838-16927207)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16987414 by andrew lauritzen in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/VirtualShadowMaps/VirtualShadowMapCacheManager.h
+++ Engine/Source/Runtime/Renderer/Private/VirtualShadowMaps/VirtualShadowMapCacheManager.h
@@ -17,7 +17,7 @@ class FVirtualShadowMapCacheEntry
 public:
 	// True if the cache has been (re)populated, set to false on init and set to true once the cache update process has happened.
 	// Also set to false whenever key data was not valid and all cached data is invalidated.
-	bool IsValid() { return PrevVirtualShadowMapId != INDEX_NONE; }
+	bool IsValid() { return PrevVirtualShadowMapId != INDEX_NONE && bPrevRendered; }
 
 	void UpdateLocal(int32 VirtualShadowMapId,
 		const FWholeSceneProjectedShadowInitializer &InCacheValidKey);
@@ -29,20 +29,17 @@ public:
 		float ViewCenterZ,
 		float ViewRadiusZ);
 
-	/**
-	 * Returns the PrevVirtualShadowMapId if cached data is valid (bValidData), or INDEX_NONE otherwise. 
-	 */
-	uint32 GetValidPrevVirtualShadowMapId() { return PrevVirtualShadowMapId; }
-
-	FIntPoint GetPageSpaceOffset() const { return PrevPageSpaceLocation - CurrentPageSpaceLocation; }
+	void MarkRendered() { bCurrentRendered = true; }
 
 	// Previous frame data
 	FIntPoint PrevPageSpaceLocation = FIntPoint(0, 0);
 	int32 PrevVirtualShadowMapId = INDEX_NONE;
+	bool bPrevRendered = false;
 
 	// Current frame data
 	FIntPoint CurrentPageSpaceLocation = FIntPoint(0, 0);
 	int32 CurrentVirtualShadowMapId = INDEX_NONE;
+	bool bCurrentRendered = false;
 
 	// TODO: Potentially refactor this to decouple the cache key details
 	FWholeSceneProjectedShadowInitializer LocalCacheValidKey;
@@ -59,17 +56,16 @@ public:
 // Persistent buffers that we ping pong frame by frame
 struct FVirtualShadowMapArrayFrameData
 {
-	TRefCountPtr<FRDGPooledBuffer>		PageTable;
-	TRefCountPtr<FRDGPooledBuffer>		PageFlags;
-	TRefCountPtr<FRDGPooledBuffer>		HPageFlags;
+	TRefCountPtr<FRDGPooledBuffer>				PageTable;
+	TRefCountPtr<FRDGPooledBuffer>				PageFlags;
+	TRefCountPtr<FRDGPooledBuffer>				HPageFlags;
 
-	TRefCountPtr<FRDGPooledBuffer>		ShadowMapProjectionDataBuffer;
-	TRefCountPtr<FRDGPooledBuffer>		PageRectBounds;
+	TRefCountPtr<FRDGPooledBuffer>				ShadowMapProjectionDataBuffer;
+	TRefCountPtr<FRDGPooledBuffer>				PageRectBounds;
 
-	TRefCountPtr<FRDGPooledBuffer>		DynamicCasterPageFlags;
+	TRefCountPtr<FRDGPooledBuffer>				DynamicCasterPageFlags;
 
-	TRefCountPtr<IPooledRenderTarget>	PhysicalPagePool;
-	TRefCountPtr<FRDGPooledBuffer>		PhysicalPageMetaData;
+	TRefCountPtr<FRDGPooledBuffer>				PhysicalPageMetaData;
 
 	TRefCountPtr<IPooledRenderTarget>			HZBPhysical;
 	TMap<int32, FVirtualShadowMapHZBMetadata>	HZBMetadata;
@@ -78,26 +74,37 @@ struct FVirtualShadowMapArrayFrameData
 class FVirtualShadowMapArrayCacheManager
 {
 public:
+	// Enough for er lots...
+	static constexpr uint32 MaxStatFrames = 512*1024U;
+
+	// Called by VirtualShadowMapArray to potentially resize the physical pool
+	// If the requested size is not already the size, all cache data is dropped and the pool is resized.
+	TRefCountPtr<IPooledRenderTarget> SetPhysicalPoolSize(FRDGBuilder& GraphBuilder, FIntPoint RequestedSize);
+
+	void FreePhysicalPool();
+
+	// Invalidate the cache for all shadows, causing any pages to be rerendered
+	void Invalidate();
+
 	/**
 	 * Call at end of frame to extract resouces from the virtual SM array to preserve to next frame.
 	 * If bCachingEnabled is false, all previous frame data is dropped and cache (and HZB!) data will not be available for the next frame.
 	 */ 
-	void ExtractFrameData(bool bEnableCaching, FVirtualShadowMapArray &VirtualShadowMapArray, FRDGBuilder& GraphBuilder);
+	void ExtractFrameData(FRDGBuilder& GraphBuilder,
+		FVirtualShadowMapArray &VirtualShadowMapArray,
+		bool bEnableCaching);
 
 	/**
 	 * Finds an existing cache entry and moves to the active set or creates a fresh one.
 	 */
-	TSharedPtr<FVirtualShadowMapCacheEntry> FindCreateCacheEntry(int32 LightSceneId, int32 CascadeIndex);
+	TSharedPtr<FVirtualShadowMapCacheEntry> FindCreateCacheEntry(int32 LightSceneId, int32 Index = 0);
 
 	/*
 	 * Returns true if cached data is available.
 	 */
 	bool IsValid();
 
-	/**
-	 */
 	bool IsAccumulatingStats();
-
 
 	/**
 	 * This must to be executed before the instances are actually removed / updated, otherwise the wrong position will be used. 
@@ -113,23 +120,10 @@ public:
 
 	TRDGUniformBufferRef<FVirtualShadowMapUniformParameters> GetPreviousUniformBuffer(FRDGBuilder& GraphBuilder) const;
 
-	// Index the Cache entries by the light ID and cascade index
-	TMap< FIntPoint, TSharedPtr<FVirtualShadowMapCacheEntry> > CacheEntries;
-	TMap< FIntPoint, TSharedPtr<FVirtualShadowMapCacheEntry> > PrevCacheEntries;
-
 	FVirtualShadowMapArrayFrameData PrevBuffers;
 	FVirtualShadowMapUniformParameters PrevUniformParameters;
-
-	// Enough for er lots...
-	static constexpr uint32 MaxStatFrames = 512*1024U;
-	
-	// Stores stats over frames when activated.
-	TRefCountPtr<FRDGPooledBuffer>		AccumulatedStatsBuffer;
-	bool bAccumulatingStats = false;
-	FRHIGPUBufferReadback* GPUBufferReadback = nullptr;
-	
+		
 private:
-
 	// Must match shader...
 	struct FInstanceSceneDataRange
 	{
@@ -137,5 +131,27 @@ private:
 		int32 NumInstanceSceneDataEntries;
 	};
 
-	void ProcessInstanceRangeInvalidation(FRDGBuilder& GraphBuilder, const TArray<FInstanceSceneDataRange, SceneRenderingAllocator>& InstanceRangesLarge, const TArray<FInstanceSceneDataRange, SceneRenderingAllocator>& InstanceRangesSmall, const FGPUScene& GPUScene);
+	void ProcessInstanceRangeInvalidation(
+		FRDGBuilder& GraphBuilder,
+		const TArray<FInstanceSceneDataRange,
+		SceneRenderingAllocator>& InstanceRangesLarge,
+		const TArray<FInstanceSceneDataRange,
+		SceneRenderingAllocator>& InstanceRangesSmall,
+		const FGPUScene& GPUScene);
+
+	void ExtractStats(FRDGBuilder& GraphBuilder, FVirtualShadowMapArray &VirtualShadowMapArray);
+
+	// The actual physical texture data is stored here rather than in VirtualShadowMapArray (which is recreated each frame)
+	// This allows us to (optionally) persist cached pages between frames. Regardless of whether caching is enabled,
+	// we store the physical pool here.
+	TRefCountPtr<IPooledRenderTarget> PhysicalPagePool;
+
+	// Index the Cache entries by the light ID and clipmap/cubemap face index (if applicable)
+	TMap< FIntPoint, TSharedPtr<FVirtualShadowMapCacheEntry> > CacheEntries;
+	TMap< FIntPoint, TSharedPtr<FVirtualShadowMapCacheEntry> > PrevCacheEntries;
+
+	// Stores stats over frames when activated.
+	TRefCountPtr<FRDGPooledBuffer> AccumulatedStatsBuffer;
+	bool bAccumulatingStats = false;
+	FRHIGPUBufferReadback* GPUBufferReadback = nullptr;
 };
