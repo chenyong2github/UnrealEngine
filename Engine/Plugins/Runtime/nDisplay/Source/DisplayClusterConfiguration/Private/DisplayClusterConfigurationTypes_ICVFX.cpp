1ee9347865301171519bba52a1cfc0c6357cf1dd
Alejandro Arango <alejandro.arango@epicgames.com>
2021-07-29 23:00:22 -0400

    nDisplay: Assigned default marker texture and also default marker scale and distances that work well with it.
    #rb
    #jira UE-118896
    
    [CL 17004643 by Alejandro Arango in 4.27 branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterConfiguration/Private/DisplayClusterConfigurationTypes_ICVFX.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterConfiguration/Private/DisplayClusterConfigurationTypes_ICVFX.cpp
@@ -4,11 +4,10 @@
 
 
 FDisplayClusterConfigurationICVFX_ChromakeyMarkers::FDisplayClusterConfigurationICVFX_ChromakeyMarkers()
-	// Default chromakey marker color is (0,64,0)
 	: MarkerColor(0, 0.25f, 0)
 	, MarkerTileOffset(0)
 {
-	if (!MarkerTileRGBA)
+	// Default marker texture
 	{
 		const FString TexturePath = TEXT("/nDisplay/Textures/T_TrackingMarker_A.T_TrackingMarker_A");
 		MarkerTileRGBA = Cast<UTexture2D>(FSoftObjectPath(TexturePath).TryLoad());
