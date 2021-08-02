a5da46a1820d2ebb0155950206fe120ea2532cd0
jeremy moore <Jeremy.Moore@epicgames.com>
2021-08-02 15:14:03 -0400

    Add RenderCaptureInterface support for RDG deferred work. #rb none #preflight 61083e85484c100001c97eb2
    #ROBOMERGE-SOURCE: CL 17019123 via CL 17019159
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v839-17012307)
    
    [CL 17019172 by jeremy moore in ue5-release-engine-test branch]

--- Engine/Source/Runtime/RenderCore/Public/RenderCaptureInterface.h
+++ Engine/Source/Runtime/RenderCore/Public/RenderCaptureInterface.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 
+class FRDGBuilder;
 class FRHICommandListImmediate;
 
 /** Easy to use interface for IRenderCaptureProvider. */
@@ -20,12 +21,15 @@ namespace RenderCaptureInterface
 		RENDERCORE_API FScopedCapture(bool bEnable, TCHAR const* InEventName = nullptr, TCHAR const* InFileName = nullptr);
 		/** Use this constructor if on rendering thread. Use bEnable to allow control over the capture frequency. */
 		RENDERCORE_API FScopedCapture(bool bEnable, FRHICommandListImmediate* InRHICommandList, TCHAR const* InEventName = nullptr, TCHAR const* InFileName = nullptr);
-		
+		/** Use this constructor if using RenderGraph to schedule work. Use bEnable to allow control over the capture frequency. */
+		RENDERCORE_API FScopedCapture(bool bEnable, FRDGBuilder& InGraphBuilder, TCHAR const* InEventName = nullptr, TCHAR const* InFileName = nullptr);
+
 		RENDERCORE_API ~FScopedCapture();
 	
 	private:
 		bool bCapture;
 		bool bEvent;
 		FRHICommandListImmediate* RHICommandList;
+		FRDGBuilder* GraphBuilder;
 	};
 }
