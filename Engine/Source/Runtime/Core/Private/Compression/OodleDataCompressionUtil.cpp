5074a8efcedf286f6988ccb8e0f337b8881b0290
dan thompson <dan.thompson@epicgames.com>
2021-08-05 16:59:34 -0400

    Move replay compression functions only used in fortnite back to fortnite Clarify interface to Oodle compressed array functions Add support for >2gb buffers #preflight 6109e3ff4cd793000191470d #rb charles.bloom
    #ROBOMERGE-SOURCE: CL 17075612 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v853-17066230)
    
    [CL 17075735 by dan thompson in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Core/Private/Compression/OodleDataCompressionUtil.cpp
+++ Engine/Source/Runtime/Core/Private/Compression/OodleDataCompressionUtil.cpp
@@ -6,28 +6,33 @@
 #include "Serialization/MemoryReader.h"
 #include "Serialization/MemoryWriter.h"
 
-namespace FOodleDataCompressionUtil
+namespace FOodleCompressedArray
 {
-	// Our header contains 2 int32s, the compressed and decompressed sizes.
-	static constexpr int32 CompressedTArrayHeaderSize = sizeof(int32) * 2;
-
-	bool CORE_API FCompressedArray::CompressData(FOodleDataCompression::ECompressor InCompressor, FOodleDataCompression::ECompressionLevel InLevel, const void* InData, int32 InDataSize)
+	bool CORE_API CompressData(TArray<uint8>& OutCompressed, const void* InData, int32 InDataSize, FOodleDataCompression::ECompressor InCompressor, FOodleDataCompression::ECompressionLevel InLevel)
 	{
+		if (InDataSize < 0 || InData == nullptr)
+		{
+			return false;
+		}
+
+		// For 32 bit sizes we only ever need 32 bits.
+		int32 HeaderSizeNeeded = sizeof(int32) * 2;
+
 		// Size the array so that it fits our header and the memory oodle requires to do the work.
 		int32 CompSizeNeeded = TCheckValueCast<int32>(FOodleDataCompression::CompressedBufferSizeNeeded(InDataSize));
-		this->SetNum( TCheckValueCast<int32>( CompressedTArrayHeaderSize + CompSizeNeeded) );
+		OutCompressed.SetNum( TCheckValueCast<int32>( HeaderSizeNeeded + CompSizeNeeded) );
 
 		// We compress in to the buffer past our header, then write the header below.
-		void* CompPtr = this->GetData() + CompressedTArrayHeaderSize;
+		void* CompPtr = OutCompressed.GetData() + HeaderSizeNeeded;
 		int32 CompressedSize = TCheckValueCast<int32>(FOodleDataCompression::Compress(CompPtr, CompSizeNeeded, InData, InDataSize, InCompressor, InLevel));
 		if ( CompressedSize <= 0 )
 		{
 			// Probably a bad parameter.
-			this->Empty();
+			OutCompressed.Empty();
 			return false;
 		}
 		
-		int32* Sizes = (int32*)this->GetData();
+		int32* Sizes = (int32*)OutCompressed.GetData();
 
 		Sizes[0] = InDataSize;
 		Sizes[1] = CompressedSize;
@@ -38,34 +43,113 @@ namespace FOodleDataCompressionUtil
 #endif
 
 		// Trim off the end working space Oodle needed to do the compress work.
-		this->SetNum( TCheckValueCast<int32>(CompressedSize + CompressedTArrayHeaderSize) , false);
+		OutCompressed.SetNum( TCheckValueCast<int32>(CompressedSize + HeaderSizeNeeded) , false);
+		return true;
+	}
+	bool CORE_API CompressData64(TArray64<uint8>& OutCompressed, const void* InData, int64 InDataSize, FOodleDataCompression::ECompressor InCompressor, FOodleDataCompression::ECompressionLevel InLevel)
+	{
+		if (InDataSize < 0 || InData == nullptr)
+		{
+			return false;
+		}
+
+		// for large sizes use 64 bit ints for our header
+		int32 HeaderSizeNeeded = sizeof(int32) * 2;
+		if (InDataSize >= (1U << 31))
+		{
+			HeaderSizeNeeded = sizeof(int64) * 2;
+		}
+
+		// Size the array so that it fits our header and the memory oodle requires to do the work.		
+		int64 WorkingSizeNeeded = FOodleDataCompression::CompressedBufferSizeNeeded(InDataSize);
+		OutCompressed.SetNum(HeaderSizeNeeded + WorkingSizeNeeded);
+
+		// We compress in to the buffer past our header, then write the header below.
+		void* CompPtr = OutCompressed.GetData() + HeaderSizeNeeded;
+		int64 CompressedSize = FOodleDataCompression::Compress(CompPtr, WorkingSizeNeeded, InData, InDataSize, InCompressor, InLevel);
+		if (CompressedSize <= 0)
+		{
+			// Probably a bad parameter.
+			OutCompressed.Empty();
+			return false;
+		}
+
+		if (HeaderSizeNeeded == sizeof(int64)*2)
+		{
+			uint64* Sizes = (uint64*)OutCompressed.GetData();
+
+			Sizes[0] = (uint64)InDataSize | (1ULL << 63);
+			Sizes[1] = (uint64)CompressedSize;
+
+#if !PLATFORM_LITTLE_ENDIAN
+			Sizes[0] = BYTESWAP_ORDER64(Sizes[0]);
+			Sizes[1] = BYTESWAP_ORDER64(Sizes[1]);
+#endif
+		}
+		else
+		{
+			// technically we checked the high bit for the decompressed size, when we're switching
+			// on the compressed size. This should be always less, so this should never happen..
+			// but if it does, we need to know.
+			check(CompressedSize < (1U << 31));
+
+			// 32 bit sizes
+			int32* Sizes = (int32*)OutCompressed.GetData();
+
+			Sizes[0] = (int32)InDataSize;
+			Sizes[1] = (int32)CompressedSize;
+
+#if !PLATFORM_LITTLE_ENDIAN
+			Sizes[0] = BYTESWAP_ORDER32(Sizes[0]);
+			Sizes[1] = BYTESWAP_ORDER32(Sizes[1]);
+#endif
+		}
+
+		// Trim off the end working space Oodle needed to do the compress work.
+		OutCompressed.SetNum(CompressedSize + HeaderSizeNeeded, false);
 		return true;
 	}
 
-	bool CORE_API FCompressedArray::DecompressToExistingBuffer(void* InDestinationBuffer) const
+	bool CORE_API DecompressToExistingBuffer(void* InDestinationBuffer, TArray<uint8> const& InCompressed)
 	{
 		int32 DecompressedSize, CompressedSize;
-		if (PeekSizes(CompressedSize, DecompressedSize) == false)
+		int32 OffsetToCompressedData = PeekSizes(InCompressed, CompressedSize, DecompressedSize);
+		if (OffsetToCompressedData == 0)
 		{
 			return false;
 		}
 
 		// If we have a valid header, then if we don't have the actual data, it's corrupted data.
-		check(CompressedTArrayHeaderSize + CompressedSize <= Num());
+		check(OffsetToCompressedData + CompressedSize <= InCompressed.Num());
 
-		return FOodleDataCompression::Decompress(InDestinationBuffer, DecompressedSize, GetData() + CompressedTArrayHeaderSize, CompressedSize);
+		return FOodleDataCompression::Decompress(InDestinationBuffer, DecompressedSize, InCompressed.GetData() + OffsetToCompressedData, CompressedSize);
+	}
+	bool CORE_API DecompressToExistingBuffer64(void* InDestinationBuffer, TArray64<uint8> const& InCompressed)
+	{
+		int64 DecompressedSize, CompressedSize;
+		int32 OffsetToCompressedData = PeekSizes64(InCompressed, CompressedSize, DecompressedSize);
+		if (OffsetToCompressedData == 0)
+		{
+			return false;
+		}
+
+		// If we have a valid header, then if we don't have the actual data, it's corrupted data.
+		check(OffsetToCompressedData + CompressedSize <= InCompressed.Num());
+
+		return FOodleDataCompression::Decompress(InDestinationBuffer, DecompressedSize, InCompressed.GetData() + OffsetToCompressedData, CompressedSize);
 	}
 
-	bool CORE_API FCompressedArray::DecompressToAllocatedBuffer(void*& OutDestinationBuffer, int32& OutDestinationBufferSize) const
+	bool CORE_API DecompressToAllocatedBuffer(void*& OutDestinationBuffer, int32& OutDestinationBufferSize, TArray<uint8> const& InCompressed)
 	{
 		int32 DecompressedSize, CompressedSize;
-		if (PeekSizes(CompressedSize, DecompressedSize) == false)
+		int32 OffsetToCompressedData = PeekSizes(InCompressed, CompressedSize, DecompressedSize);
+		if (OffsetToCompressedData == 0)
 		{
 			return false;
 		}
 
 		// If we have a valid header, then if we don't have the actual data, it's corrupted data.
-		check(CompressedTArrayHeaderSize + CompressedSize <= Num());
+		check(OffsetToCompressedData + CompressedSize <= InCompressed.Num());
 
 		void* DestinationBuffer = FMemory::Malloc(DecompressedSize);
 		if (DestinationBuffer == 0)
@@ -76,7 +160,7 @@ namespace FOodleDataCompressionUtil
 		OutDestinationBufferSize = DecompressedSize;
 		OutDestinationBuffer = DestinationBuffer;
 
-		if (FOodleDataCompression::Decompress(DestinationBuffer, DecompressedSize, GetData() + CompressedTArrayHeaderSize, CompressedSize) == false)
+		if (FOodleDataCompression::Decompress(DestinationBuffer, DecompressedSize, InCompressed.GetData() + OffsetToCompressedData, CompressedSize) == false)
 		{
 			FMemory::Free(DestinationBuffer);
 			OutDestinationBuffer = 0;
@@ -85,55 +169,33 @@ namespace FOodleDataCompressionUtil
 
 		return true;
 	}
-
-	bool CORE_API DecompressReplayData(const TArray<uint8>& InCompressed, TArray< uint8 >& OutBuffer)
+	bool CORE_API DecompressToAllocatedBuffer64(void*& OutDestinationBuffer, int64& OutDestinationBufferSize, TArray64<uint8> const& InCompressed)
 	{
-		int32 Size = 0;
-		int32 CompressedSize = 0;
-
-		FMemoryReader Reader(InCompressed);
-		Reader << Size;
-		Reader << CompressedSize;
-
-		OutBuffer.SetNum(Size, false);
-
-		if ( Reader.Tell() + CompressedSize > InCompressed.Num() )
-			return false;
-
-		return FOodleDataCompression::Decompress(OutBuffer.GetData(), OutBuffer.Num(), InCompressed.GetData() + Reader.Tell(), CompressedSize );
-	}
-
-	bool CORE_API CompressReplayData(const TArray<uint8>& InBuffer, TArray< uint8 >& OutCompressed)
-	{
-		const void * InPtr = InBuffer.GetData();
-		const int32 InSize = InBuffer.Num();
-		int32 CompressedSize = 0;
-
-		int32 ReservedBytes = sizeof(int32)*2;
-				
-		const FOodleDataCompression::ECompressor		Compressor = FOodleDataCompression::ECompressor::Selkie;
-		const FOodleDataCompression::ECompressionLevel	Level = FOodleDataCompression::ECompressionLevel::VeryFast;
-		
-		int32 CompSizeNeeded = (int32) FOodleDataCompression::CompressedBufferSizeNeeded(InSize);
-		OutCompressed.SetNum( TCheckValueCast<int32>( ReservedBytes + CompSizeNeeded) );
-		void * CompPtr = OutCompressed.GetData() + ReservedBytes;
-
-		CompressedSize = (int32) FOodleDataCompression::Compress(CompPtr,CompSizeNeeded,InPtr,InSize,Compressor,Level);
-		if ( CompressedSize <= 0 )
+		int64 DecompressedSize, CompressedSize;
+		int32 OffsetToCompressedData = PeekSizes64(InCompressed, CompressedSize, DecompressedSize);
+		if (OffsetToCompressedData == 0)
 		{
 			return false;
 		}
-		
-		int32 InSize32 = TCheckValueCast<int32>(InSize);
-		int32 CompressedSize32 = TCheckValueCast<int32>(CompressedSize);
 
-		FMemoryWriter Writer(OutCompressed);
-		Writer << InSize32;
-		Writer << CompressedSize32;
-		
-		check( Writer.Tell() == ReservedBytes );
+		// If we have a valid header, then if we don't have the actual data, it's corrupted data.
+		check(OffsetToCompressedData + CompressedSize <= InCompressed.Num());
 
-		OutCompressed.SetNum( TCheckValueCast<int32>(CompressedSize + ReservedBytes) , false);
+		void* DestinationBuffer = FMemory::Malloc(DecompressedSize);
+		if (DestinationBuffer == 0)
+		{
+			return false;
+		}
+
+		OutDestinationBufferSize = DecompressedSize;
+		OutDestinationBuffer = DestinationBuffer;
+
+		if (FOodleDataCompression::Decompress(DestinationBuffer, DecompressedSize, InCompressed.GetData() + OffsetToCompressedData, CompressedSize) == false)
+		{
+			FMemory::Free(DestinationBuffer);
+			OutDestinationBuffer = 0;
+			return false;
+		}
 
 		return true;
 	}
