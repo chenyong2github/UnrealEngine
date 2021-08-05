05a91fb8f894b39329c172c40ecdfa4523a8db74
Josh Engebretson <Josh.Engebretson@epicgames.com>
2021-08-05 18:49:38 -0400

    Horde: Add Slack notification section in case of no job information #jira none #rnx
    [CL 17077532 by Josh Engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeServer/Notifications/Impl/SlackNotificationSink.cs
+++ Engine/Source/Programs/Horde/HordeServer/Notifications/Impl/SlackNotificationSink.cs
@@ -995,15 +995,21 @@ namespace HordeServer.Notifications.Impl
                 Attachment.FallbackText += $" - Device: {Device.Name} Pool: {Pool.Name}";
             }
 
+			Attachment.Blocks.Add(new HeaderBlock($"{Message}", false, false));
+
 			if (Stream != null && Job != null && Step != null && Node != null)
 			{
 				Uri JobStepLink = new Uri($"{Settings.DashboardUrl}job/{Job.Id}?step={Step.Id}");
 				Uri JobStepLogLink = new Uri($"{Settings.DashboardUrl}log/{Step.LogId}");
 				
-				Attachment.FallbackText += $" - {Stream.Name} - {GetJobChangeText(Job)} - {Job.Name} - {Node.Name}";
-				Attachment.Blocks.Add(new HeaderBlock($"{Message}", false, false));
+				Attachment.FallbackText += $" - {Stream.Name} - {GetJobChangeText(Job)} - {Job.Name} - {Node.Name}";				
 				Attachment.Blocks.Add(new SectionBlock($"*<{JobStepLink}|{Stream.Name} - {GetJobChangeText(Job)} - {Job.Name} - {Node.Name}>*"));
 				Attachment.Blocks.Add(new SectionBlock($"<{JobStepLogLink}|View Job Step Log>"));
+			}
+			else
+			{				
+				Attachment.FallbackText += " - No job information (Gauntlet might need to be updated in stream)";				
+				Attachment.Blocks.Add(new SectionBlock("*No job information (Gauntlet might need to be updated in stream)*"));
 			}
 
 			return SendMessageAsync(Recipient, Attachments: new[] { Attachment });
