65fb896da61c3d21201fe18bd505690355fbb0ea
jon bourim <jon.bourim@epicgames.com>
2021-08-07 19:16:17 -0400

    PoseSearch Debugger integration into the Rewind Debugger
    - Includes PoseSearch Trace logger/analyzer/provider to record Motion Matching data
    - Currently supports debugging multiple Motion-Matching nodes, including both database and state introspection
    
    #jira none
    #rnx
    #rb braeden.shosa
    #preflight 610dc3a10182eb00017ab1e0
    
    [CL 17096387 by jon bourim in ue5-main branch]

---
+++ Engine/Plugins/Experimental/Animation/PoseSearch/Source/Editor/Private/Trace/PoseSearchTraceProvider.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PoseSearchTraceProvider.h"
+
+namespace UE { namespace PoseSearch {
+
+const FName FTraceProvider::ProviderName("PoseSearchTraceProvider");
+
+FTraceProvider::FTraceProvider(TraceServices::IAnalysisSession& InSession) : Session(InSession)
+{
+}
+
+TSet<int32> FTraceProvider::GetMotionMatchingNodeIds(uint64 InAnimInstanceId) const
+{
+	return MotionMatchingStateTimelineStorage.GetNodeIds(InAnimInstanceId);
+}
+
+bool FTraceProvider::ReadMotionMatchingStateTimeline(uint64 InAnimInstanceId, int32 InNodeId, TFunctionRef<void(const FMotionMatchingStateTimeline&)> Callback) const
+{
+	Session.ReadAccessCheck();
+	return MotionMatchingStateTimelineStorage.ReadTimeline(InAnimInstanceId, InNodeId, Callback);
+}
+
+
+void FTraceProvider::AppendMotionMatchingState(const FTraceMotionMatchingStateMessage& InMessage, double InTime)
+{
+	Session.WriteAccessCheck();
+
+	TSharedRef<TraceServices::TPointTimeline<FTraceMotionMatchingStateMessage>> Timeline = MotionMatchingStateTimelineStorage.GetTimeline(Session, InMessage.AnimInstanceId, InMessage.NodeId);
+	Timeline->AppendEvent(InTime, InMessage);
+	
+	Session.UpdateDurationSeconds(InTime);
+}
+
+
+}}
---
+++ Engine/Plugins/Experimental/Animation/PoseSearch/Source/Editor/Private/Trace/PoseSearchTraceProvider.h
@@ -0,0 +1,146 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Model/PointTimeline.h"
+#include "Runtime/Private/Trace/PoseSearchTraceLogger.h"
+#include "TraceServices/Model/AnalysisSession.h"
+
+
+namespace UE { namespace PoseSearch {
+
+// Message types for appending / reading to the timeline
+/** Base message type for common data */
+struct FTraceMessage
+{
+	uint64 AnimInstanceId;
+	int32 NodeId;
+	uint16 FrameCounter;
+};
+
+/** Motion matching state message container */
+struct FTraceMotionMatchingStateMessage : FTraceMessage
+{
+	FTraceMotionMatchingState::EFlags Flags;
+	float ElapsedPoseJumpTime;
+	TArray<float> QueryVector;
+	TArray<float> QueryVectorNormalized;
+	TArray<float> BiasWeights;
+	int32 DbPoseIdx;
+	uint64 DatabaseId;
+};
+
+
+/**
+ * Provider to the widgets for pose search functionality, largely mimicking FAnimationProvider
+ */
+class FTraceProvider : public TraceServices::IProvider
+{
+public:
+	explicit FTraceProvider(TraceServices::IAnalysisSession& InSession);
+
+	/** Get all node ids of motion matching nodes appended to the object's timeline thus far */
+	TSet<int32> GetMotionMatchingNodeIds(uint64 InAnimInstanceId) const;
+
+	/** Read the object-relevant timeline info and provide it via callback */
+	using FMotionMatchingStateTimeline = TraceServices::ITimeline<FTraceMotionMatchingStateMessage>;
+	bool ReadMotionMatchingStateTimeline(uint64 InAnimInstanceId, int32 InNodeId, TFunctionRef<void(const FMotionMatchingStateTimeline&)> Callback) const;
+
+	/** Append to the timeline on our object */
+	void AppendMotionMatchingState(const FTraceMotionMatchingStateMessage& InMessage, double InTime);
+
+	static const FName ProviderName;
+
+private:
+	/**
+	 * Convenience struct for creating timelines per message type.
+	 * We store an array of timelines for every possible AnimInstance Id that gets appended.
+	 */
+	template <class MessageType, class TimelineType = TraceServices::TPointTimeline<MessageType>>
+	struct TTimelineStorage
+	{
+		// Map the timelines as AnimInstanceId -> NodeId -> Timeline
+		using FNodeToTimelineMap = TMap<int32, uint32>;
+		using FAnimInstanceToTimelineMap = TMap<uint64, FNodeToTimelineMap>;
+
+		/** Retrieves the timeline for internal use, creating it if it does not exist */
+		TSharedRef<TimelineType> GetTimeline(TraceServices::IAnalysisSession& InSession, uint64 InAnimInstanceId, int32 InNodeId)
+		{
+			const uint32* TimelineIndex = nullptr;
+			
+			FNodeToTimelineMap* NodeToTimelineMap = AnimInstanceIdToTimelines.Find(InAnimInstanceId);
+			if (NodeToTimelineMap == nullptr)
+			{
+				// Create the timeline map if this is the first time accessing with this anim instance
+				NodeToTimelineMap = &AnimInstanceIdToTimelines.Add(InAnimInstanceId, {});
+			}
+			else
+			{
+				// Anim instance already used, attempt to find the NodeId's timeline
+				TimelineIndex = NodeToTimelineMap->Find(InNodeId);
+			}
+
+			if (TimelineIndex == nullptr)
+			{
+				// Append our timeline to the storage and our object + the storage index to the map
+				TSharedRef<TimelineType> Timeline = MakeShared<TimelineType>(InSession.GetLinearAllocator());
+				const uint32 NewIndex = Timelines.Add(Timeline);
+				NodeToTimelineMap->Add(InNodeId, NewIndex);
+				return Timeline;
+			}
+
+			return Timelines[*TimelineIndex];
+		}
+
+		/** Retrieve a timeline from an anim instance + node and execute the callback */
+		bool ReadTimeline(uint64 InAnimInstanceId, int32 InNodeId, TFunctionRef<void(const TraceServices::ITimeline<MessageType>&)> Callback) const
+		{
+			const FNodeToTimelineMap* NodeToTimelineMap = AnimInstanceIdToTimelines.Find(InAnimInstanceId);
+			if (NodeToTimelineMap == nullptr)
+			{
+				return false;
+			}
+
+			const uint32* TimelineIndex = NodeToTimelineMap->Find(InNodeId);
+			if (TimelineIndex == nullptr || !Timelines.IsValidIndex(*TimelineIndex))
+			{
+				return false;
+			}
+
+			Callback(*Timelines[*TimelineIndex]);
+			return true;
+		}
+
+		TSet<int32> GetNodeIds(uint64 InAnimInstanceId) const
+		{
+			check(AnimInstanceIdToTimelines.Find(InAnimInstanceId));
+			
+			TSet<int32> NodeIds;
+			const FNodeToTimelineMap& NodeToTimelineMap = AnimInstanceIdToTimelines[InAnimInstanceId];
+
+			// For each node that has a timeline, add the node to the set
+			for(const TTuple<int32, uint32>& Pair : NodeToTimelineMap)
+			{
+				NodeIds.Add(Pair.Key);
+			}
+			
+			return NodeIds;
+		}
+
+		/** Maps AnimInstanceIds to a map of NodeIds to timelines. AnimInstanceId -> NodeId -> Timeline */
+		FAnimInstanceToTimelineMap AnimInstanceIdToTimelines;
+
+		/** Timelines per node */
+		TArray<TSharedRef<TimelineType>> Timelines;
+	};
+
+	// Storage for each message type
+	struct FMotionMatchingStateTimelineStorage : TTimelineStorage<FTraceMotionMatchingStateMessage>
+	{
+	} MotionMatchingStateTimelineStorage;
+
+
+	TraceServices::IAnalysisSession& Session;
+};
+}}
