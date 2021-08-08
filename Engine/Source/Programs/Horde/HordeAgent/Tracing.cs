4473f21e033a0590f3509d20783d179cf1d65f0d
ben marsh <Ben.Marsh@epicgames.com>
2021-08-07 21:54:35 -0400

    Missing file
    #ROBOMERGE-SOURCE: CL 17096599 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v853-17066230)
    
    [CL 17096600 by ben marsh in ue5-release-engine-test branch]

---
+++ Engine/Source/Programs/Horde/HordeAgent/Tracing.cs
@@ -0,0 +1,28 @@
+using OpenTracing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HordeAgent
+{
+	static class Tracing
+	{
+		public static ISpanBuilder WithResourceName(this ISpanBuilder Builder, string? ResourceName)
+		{
+			if (ResourceName != null)
+			{
+				Builder = Builder.WithTag(Datadog.Trace.OpenTracing.DatadogTags.ResourceName, ResourceName);
+			}
+			return Builder;
+		}
+
+		public static ISpanBuilder WithServiceName(this ISpanBuilder Builder, string? ServiceName)
+		{
+			if(ServiceName != null)
+			{
+				Builder = Builder.WithTag(Datadog.Trace.OpenTracing.DatadogTags.ServiceName, ServiceName);
+			}
+			return Builder;
+		}
+}
+}
