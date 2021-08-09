357cf7612b70911c02e67537b6db87e8cb4abb5c
brandon schaefer <Brandon.Schaefer@epicgames.com>
2021-08-09 16:40:37 -0400

    Avoid Rider spamming log if mdfind is not installed on the system Fix some coding standard issues
    #jira none
    #rb none
    [FYI] Will.Damon, Dave.Hunter
    
    #ROBOMERGE-SOURCE: CL 17107285 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v854-17104634)
    
    [CL 17107317 by brandon schaefer in ue5-release-engine-test branch]

--- Engine/Plugins/Developer/RiderSourceCodeAccess/Source/RiderSourceCodeAccess/Private/RiderPathLocator/Linux/RiderPathLocatorLinux.cpp
+++ Engine/Plugins/Developer/RiderSourceCodeAccess/Source/RiderSourceCodeAccess/Private/RiderPathLocator/Linux/RiderPathLocatorLinux.cpp
@@ -13,12 +13,18 @@
 
 TOptional<FInstallInfo> FRiderPathLocator::GetInstallInfoFromRiderPath(const FString& PathToRiderApp, FInstallInfo::EInstallType InstallType)
 {
-	if(!FPaths::DirectoryExists(PathToRiderApp)) return {};
+	if(!FPaths::DirectoryExists(PathToRiderApp))
+	{
+		return {};
+	}
 
 	const FString RiderCppPluginPath = FPaths::Combine(PathToRiderApp, TEXT("plugins"), TEXT("rider-cpp"));
 
-	if (!FPaths::DirectoryExists(RiderCppPluginPath)) return {};
-	
+	if (!FPaths::DirectoryExists(RiderCppPluginPath))
+	{
+		return {};
+	}
+
 	FInstallInfo Info;
 	Info.Path = FPaths::Combine(PathToRiderApp, TEXT("bin"), TEXT("rider.sh"));
 	Info.InstallType = InstallType;
@@ -50,7 +56,7 @@ static TArray<FInstallInfo> GetManuallyInstalledRiders()
 	const FString FHomePath = GetHomePath();
 
 	const FString LocalPathMask = FPaths::Combine(FHomePath, TEXT("Rider*"));
-	
+
 	IFileManager::Get().FindFiles(RiderPaths, *LocalPathMask, false, true);
 
 	for(const FString& RiderPath: RiderPaths)
@@ -58,7 +64,9 @@ static TArray<FInstallInfo> GetManuallyInstalledRiders()
 		FString FullPath = FPaths::Combine(FHomePath, RiderPath);
 		TOptional<FInstallInfo> InstallInfo = FRiderPathLocator::GetInstallInfoFromRiderPath(FullPath, FInstallInfo::EInstallType::Installed);
 		if(InstallInfo.IsSet())
+		{
 			Result.Add(InstallInfo.GetValue());
+		}
 	}
 	return Result;
 }
@@ -73,28 +81,41 @@ static FString GetToolboxPath()
 
 static TArray<FInstallInfo> GetInstalledRidersWithMdfind()
 {
-    int32 ReturnCode;
-    FString OutResults;
-    FString OutErrors;
-    FPlatformProcess::ExecProcess(TEXT("/usr/bin/mdfind"), TEXT("\"kMDItemKind == Application\""), &ReturnCode, &OutResults, &OutErrors);
-    if (ReturnCode != 0)
-		return {};
+	int32 ReturnCode;
+	FString OutResults;
+	FString OutErrors;
 
-    TArray<FString> RiderPaths;
+	// avoid trying to run mdfind if it doesnt exists
+	if (!FPaths::FileExists(TEXT("/usr/bin/mdfind")))
+	{
+		return {};
+	}
+
+	FPlatformProcess::ExecProcess(TEXT("/usr/bin/mdfind"), TEXT("\"kMDItemKind == Application\""), &ReturnCode, &OutResults, &OutErrors);
+	if (ReturnCode != 0)
+	{
+		return {};
+	}
+
+	TArray<FString> RiderPaths;
 	FString TmpString;
 	while(OutResults.Split(TEXT("\n"), &TmpString, &OutResults))
 	{
 		if(TmpString.Contains(TEXT("Rider")))
+		{
 			RiderPaths.Add(TmpString);
+		}
 	}
-    TArray<FInstallInfo> Result;
-    for(const FString& RiderPath: RiderPaths)
-    {
-        TOptional<FInstallInfo> InstallInfo = FRiderPathLocator::GetInstallInfoFromRiderPath(RiderPath, FInstallInfo::EInstallType::Installed);
-        if(InstallInfo.IsSet())
-            Result.Add(InstallInfo.GetValue());
-    }
-    return Result;
+	TArray<FInstallInfo> Result;
+	for(const FString& RiderPath: RiderPaths)
+	{
+		TOptional<FInstallInfo> InstallInfo = FRiderPathLocator::GetInstallInfoFromRiderPath(RiderPath, FInstallInfo::EInstallType::Installed);
+		if(InstallInfo.IsSet())
+		{
+			Result.Add(InstallInfo.GetValue());
+		}
+	}
+	return Result;
 }
 
 TSet<FInstallInfo> FRiderPathLocator::CollectAllPaths()
