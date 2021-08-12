dda9f56e33d6a0a11ee2ebdb519b45a209129311
JeanLuc Corenthin <JeanLuc.Corenthin@epicgames.com>
2021-08-12 15:34:01 -0400

    Fixed Mac build for ArchiCAD and SketchUp exporters  - Replaced C++14 option with c++17 one  - Workaround permissions issue with Renaming command with two sh scripts performing the same
    #jira UE-121196
    #rb none
    
    [CL 17157172 by JeanLuc Corenthin in ue5-main branch]

---
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithARCHICADExporter/Build/Config/app_to_bundle.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ConfigPath=`dirname "$0"`
+projectPath=$ConfigPath/..
+
+RelativeEnginePath=$projectPath/../../../../../../../Engine
+EnginePath=`python -c "import os; print(os.path.realpath('$RelativeEnginePath'))"`
+
+echo "Renaming *.app to *.bundle"
+pushd $EnginePath/Binaries/Mac/DatasmithARCHICADExporter
+mv DatasmithARCHICAD23Exporter.app DatasmithARCHICAD23Exporter.bundle
+mv DatasmithARCHICAD24Exporter.app DatasmithARCHICAD24Exporter.bundle
+mv DatasmithARCHICAD25Exporter.app DatasmithARCHICAD25Exporter.bundle
+popd
