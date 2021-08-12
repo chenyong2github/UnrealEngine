9941326c4483112139933e70b14572237fcde907
nick darnell <Nick.Darnell@epicgames.com>
2021-08-12 13:51:22 -0400

    EnhancedInput - Making the InputContext and InputAction asset actions, derive from the DataAsset version that's now exposed so that they get the change baseclass option.
    #jira none
    
    #ROBOMERGE-SOURCE: CL 17155453 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v855-17104924)
    
    [CL 17155461 by nick darnell in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/EnhancedInput/Source/InputEditor/InputEditor.Build.cs
+++ Engine/Plugins/Experimental/EnhancedInput/Source/InputEditor/InputEditor.Build.cs
@@ -35,6 +35,7 @@ namespace UnrealBuildTool.Rules
                     "Slate",
                     "SlateCore",
                     "UnrealEd",
+					"AssetTools"
                 }
             );
         }
