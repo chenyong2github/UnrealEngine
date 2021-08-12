1e760c2d16e96e654818712a45c35e5e2040828b
Lauren Barnes <lauren.barnes@epicgames.com>
2021-08-12 16:14:37 -0400

    Asset Placement Editor Mode now has a separate tab to hold the asset palette. #jira none #rb Brooke.Hubert #preflight 611560eaf19b3700012a8278
    [CL 17157967 by Lauren Barnes in ue5-main branch]

--- Engine/Source/Editor/AssetPlacementEdMode/Private/AssetPlacementEdModeToolkit.cpp
+++ Engine/Source/Editor/AssetPlacementEdMode/Private/AssetPlacementEdModeToolkit.cpp
@@ -6,6 +6,7 @@
 #include "SAssetPlacementPalette.h"
 #include "Widgets/Layout/SExpandableArea.h"
 #include "Widgets/Layout/SScrollBox.h"
+#include "Toolkits/AssetEditorModeUILayer.h"
 
 #define LOCTEXT_NAMESPACE "AssetPlacementEdModeToolkit"
 
@@ -29,36 +30,64 @@ FText FAssetPlacementEdModeToolkit::GetBaseToolkitName() const
 	return LOCTEXT("DisplayName", "AssetPlacementEdMode Tool");
 }
 
+void FAssetPlacementEdModeToolkit::InvokeUI()
+{
+	FModeToolkit::InvokeUI();
+
+	if (ModeUILayer.IsValid())
+	{
+		TSharedPtr<FAssetEditorModeUILayer> ModeUILayerPtr = ModeUILayer.Pin();
+		AssetPaletteTab = ModeUILayerPtr->GetTabManager()->TryInvokeTab(FAssetEditorModeUILayer::BottomLeftTabID);
+	}
+}
+
 TSharedPtr<SWidget> FAssetPlacementEdModeToolkit::GetInlineContent() const
 {
-	return SNew(SVerticalBox)
-		+ SVerticalBox::Slot()
-		.FillHeight(.6)
+	return 	SNew(SScrollBox)
+		+ SScrollBox::Slot()
+		[
+			DetailsView.ToSharedRef()
+		]
+		+ SScrollBox::Slot()
+		[
+			ModeDetailsView.ToSharedRef()
+		];
+}
+
+void FAssetPlacementEdModeToolkit::RequestModeUITabs()
+{
+	FModeToolkit::RequestModeUITabs();
+	if (ModeUILayer.IsValid())
+	{
+		TSharedPtr<FAssetEditorModeUILayer> ModeUILayerPtr = ModeUILayer.Pin();
+		TSharedRef<FWorkspaceItem> MenuGroup = ModeUILayerPtr->GetModeMenuCategory().ToSharedRef();
+		AssetPaletteInfo.OnSpawnTab = FOnSpawnTab::CreateSP(SharedThis(this), &FAssetPlacementEdModeToolkit::CreateAssetPalette);
+		AssetPaletteInfo.TabLabel = LOCTEXT("AssetPaletteTab", "Asset Palette");
+		AssetPaletteInfo.TabTooltip = LOCTEXT("ModesToolboxTabTooltipText", "Open the  Modes tab, which contains the active editor mode's settings.");
+		ModeUILayerPtr->SetModePanelInfo(FAssetEditorModeUILayer::BottomLeftTabID, AssetPaletteInfo);
+	}
+}
+
+
+
+TSharedRef<SDockTab> FAssetPlacementEdModeToolkit::CreateAssetPalette(const FSpawnTabArgs& Args)
+{
+	TSharedPtr<SDockTab> CreatedTab = SNew(SDockTab)
 		[
 			SNew(SScrollBox)
 			+ SScrollBox::Slot()
 			[
-				DetailsView.ToSharedRef()
-			]
-			+ SScrollBox::Slot()
-			[
-				ModeDetailsView.ToSharedRef()
-			]
-		]
-		+ SVerticalBox::Slot()
-		.AutoHeight()
-		[
-			SNew(SExpandableArea)
-			.AreaTitle(LOCTEXT("AssetPaletteHeader", "Asset Palette"))
-			.BodyContent()
-			[
-				SNew(SScrollBox)
-				+ SScrollBox::Slot()
-				[
-					SNew(SAssetPlacementPalette)
-				]
+				SNew(SAssetPlacementPalette)
 			]
 		];
+
+	const FSlateBrush* TabIcon = GetEditorModeIcon().GetSmallIcon();
+	if (CreatedTab)
+	{
+		CreatedTab->SetTabIcon(TabIcon);
+	}
+	AssetPaletteTab = CreatedTab;
+	return CreatedTab.ToSharedRef();
 }
 
 #undef LOCTEXT_NAMESPACE
