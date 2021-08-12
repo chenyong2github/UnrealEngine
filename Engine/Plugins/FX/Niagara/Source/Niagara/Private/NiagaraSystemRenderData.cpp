36c08960ac7baa2147acee5762d58c9bc5dbe0ba
michael galetzka <michael.galetzka@epicgames.com>
2021-08-12 08:20:21 -0400

    Fixed a bug where GetDynamicDataSize used GT data on the RT
    #jira UE-120153
    #rb stu.mckenna
    
    #ROBOMERGE-SOURCE: CL 17150605 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v855-17104924)
    
    [CL 17150609 by michael galetzka in ue5-release-engine-test branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemRenderData.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraSystemRenderData.cpp
@@ -104,8 +104,10 @@ FPrimitiveViewRelevance FNiagaraSystemRenderData::GetViewRelevance(const FSceneV
 
 uint32 FNiagaraSystemRenderData::GetDynamicDataSize() const
 {
+	// only the render thread should use the RT data, the game thread and other parallel running tasks use the GT data
+	const TArray<FNiagaraRenderer*>& Emitter_Renderers = IsInParallelRenderingThread() ? EmitterRenderers_RT :EmitterRenderers_GT;
 	uint32 Size = 0;
-	for (const auto Renderer : EmitterRenderers_GT)
+	for (const auto Renderer : Emitter_Renderers)
 	{
 		if (Renderer)
 		{
