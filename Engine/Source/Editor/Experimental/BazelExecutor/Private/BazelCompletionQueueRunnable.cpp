17973691b0b2788933a6064dfb83c3457e595822
dan phillips <Dan.Phillips@epicgames.com>
2021-08-17 09:51:54 -0400

    Added missing Pre/PostWindowsApi headers around #include <grpcpp/grpcpp.h> Supplied by David.Bollo of The Coalition. Fixes some non unity compile errors
    [FYI] David.Bollo
    #jira none
    
    #ROBOMERGE-SOURCE: CL 17191225 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v855-17104924)
    
    [CL 17191232 by dan phillips in ue5-release-engine-test branch]

--- Engine/Source/Editor/Experimental/BazelExecutor/Private/BazelCompletionQueueRunnable.cpp
+++ Engine/Source/Editor/Experimental/BazelExecutor/Private/BazelCompletionQueueRunnable.cpp
@@ -7,8 +7,14 @@
 THIRD_PARTY_INCLUDES_START
 UE_PUSH_MACRO("TEXT")
 #undef TEXT
+#if PLATFORM_WINDOWS
+#include "Windows/PreWindowsApi.h"
+#endif
 #include <grpcpp/grpcpp.h>
 #include "build/bazel/remote/execution/v2/remote_execution.grpc.pb.h"
+#if PLATFORM_WINDOWS
+#include "Windows/PostWindowsApi.h"
+#endif
 UE_POP_MACRO("TEXT");
 THIRD_PARTY_INCLUDES_END
 
