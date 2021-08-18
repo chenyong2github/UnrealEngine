795d5aa89db778ebf99aced22c4c9f86bbb5064a
Johan Torp <Johan.Torp@epicgames.com>
2021-08-18 06:19:33 -0400

    Fix OnOutOfMemory thread-safety issue when concurrent calls return from the function
    Change all FPlatformMemory::OnOutOfMemory implementations to [[noreturn]]
    
    #jira UE-93728
    #rb steve.robb, arciel.rekman
    
    [CL 17212898 by Johan Torp in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/IOS/IOSPlatformMemory.cpp
+++ Engine/Source/Runtime/Core/Private/IOS/IOSPlatformMemory.cpp
@@ -13,51 +13,52 @@
 
 void FIOSPlatformMemory::OnOutOfMemory(uint64 Size, uint32 Alignment)
 {
-    // Update memory stats before we enter the crash handler.
-    OOMAllocationSize = Size;
-    OOMAllocationAlignment = Alignment;
+	auto HandleOOM = [&]()
+	{
+		// Update memory stats before we enter the crash handler.
+		OOMAllocationSize = Size;
+		OOMAllocationAlignment = Alignment;
     
-    // only call this code one time - if already OOM, abort
-    if (bIsOOM)
-    {
-        return;
-    }
-    bIsOOM = true;
+		bIsOOM = true;
 
-	const int ErrorMsgSize = 256;
-	TCHAR ErrorMsg[ErrorMsgSize];
-	FPlatformMisc::GetSystemErrorMessage(ErrorMsg, ErrorMsgSize, 0);
+		const int ErrorMsgSize = 256;
+		TCHAR ErrorMsg[ErrorMsgSize];
+		FPlatformMisc::GetSystemErrorMessage(ErrorMsg, ErrorMsgSize, 0);
     
-    FPlatformMemoryStats PlatformMemoryStats = FPlatformMemory::GetStats();
-    if (BackupOOMMemoryPool)
-    {
-        FPlatformMemory::BinnedFreeToOS(BackupOOMMemoryPool, FPlatformMemory::GetBackMemoryPoolSize());
-        UE_LOG(LogMemory, Warning, TEXT("Freeing %d bytes from backup pool to handle out of memory."), FPlatformMemory::GetBackMemoryPoolSize());
+		FPlatformMemoryStats PlatformMemoryStats = FPlatformMemory::GetStats();
+		if (BackupOOMMemoryPool)
+		{
+			FPlatformMemory::BinnedFreeToOS(BackupOOMMemoryPool, FPlatformMemory::GetBackMemoryPoolSize());
+			UE_LOG(LogMemory, Warning, TEXT("Freeing %d bytes from backup pool to handle out of memory."), FPlatformMemory::GetBackMemoryPoolSize());
         
-        LLM(FLowLevelMemTracker::Get().OnLowLevelFree(ELLMTracker::Default, BackupOOMMemoryPool));
-    }
+			LLM(FLowLevelMemTracker::Get().OnLowLevelFree(ELLMTracker::Default, BackupOOMMemoryPool));
+		}
     
-    UE_LOG(LogMemory, Warning, TEXT("MemoryStats:")\
-           TEXT("\n\tAvailablePhysical %llu")\
-           TEXT("\n\t AvailableVirtual %llu")\
-           TEXT("\n\t     UsedPhysical %llu")\
-           TEXT("\n\t PeakUsedPhysical %llu")\
-           TEXT("\n\t      UsedVirtual %llu")\
-           TEXT("\n\t  PeakUsedVirtual %llu"),
-           (uint64)PlatformMemoryStats.AvailablePhysical,
-           (uint64)PlatformMemoryStats.AvailableVirtual,
-           (uint64)PlatformMemoryStats.UsedPhysical,
-           (uint64)PlatformMemoryStats.PeakUsedPhysical,
-           (uint64)PlatformMemoryStats.UsedVirtual,
-           (uint64)PlatformMemoryStats.PeakUsedVirtual);
+		UE_LOG(LogMemory, Warning, TEXT("MemoryStats:")\
+			   TEXT("\n\tAvailablePhysical %llu")\
+			   TEXT("\n\t AvailableVirtual %llu")\
+			   TEXT("\n\t     UsedPhysical %llu")\
+			   TEXT("\n\t PeakUsedPhysical %llu")\
+			   TEXT("\n\t      UsedVirtual %llu")\
+			   TEXT("\n\t  PeakUsedVirtual %llu"),
+			   (uint64)PlatformMemoryStats.AvailablePhysical,
+			   (uint64)PlatformMemoryStats.AvailableVirtual,
+			   (uint64)PlatformMemoryStats.UsedPhysical,
+			   (uint64)PlatformMemoryStats.PeakUsedPhysical,
+			   (uint64)PlatformMemoryStats.UsedVirtual,
+			   (uint64)PlatformMemoryStats.PeakUsedVirtual);
     
-    // let any registered handlers go
-    FCoreDelegates::GetMemoryTrimDelegate().Broadcast();
+		// let any registered handlers go
+		FCoreDelegates::GetMemoryTrimDelegate().Broadcast();
     
-	// ErrorMsg might be unrelated to OoM error in some cases as the code that calls OnOutOfMemory could have called other system functions that modified errno
-    UE_LOG(LogMemory, Warning, TEXT("Ran out of memory allocating %llu bytes with alignment %u. Last error msg: %s."), Size, Alignment, ErrorMsg);
+		// ErrorMsg might be unrelated to OoM error in some cases as the code that calls OnOutOfMemory could have called other system functions that modified errno
+		UE_LOG(LogMemory, Warning, TEXT("Ran out of memory allocating %llu bytes with alignment %u. Last error msg: %s."), Size, Alignment, ErrorMsg);
     
-    // make this a fatal error that ends here not in the log
-    // changed to 3 from NULL because clang noticed writing to NULL and warned about it
-    *(int32 *)3 = 123;
+		// make this a fatal error that ends here not in the log
+		// changed to 3 from NULL because clang noticed writing to NULL and warned about it
+		*(int32 *)3 = 123;
+	};
+	
+	UE_CALL_ONCE(HandleOOM);
+	FPlatformProcess::SleepInfinite(); // Unreachable
 }
