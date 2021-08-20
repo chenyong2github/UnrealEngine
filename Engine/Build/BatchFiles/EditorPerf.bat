2f1f302230433cf5d6eecd87b969dfc4ee3dd385
geoff evans <geoff.evans@epicgames.com>
2021-08-20 15:28:36 -0400

    Rechristen IterationProfile -> EditorPerf for clarity
    Part 2 - modify branched files, delete old files
    
    #rnx
    #jira none
    #rb jerome.delattre
    #preflight 611fbfeb76f1fd0001a5f04b
    
    #ROBOMERGE-SOURCE: CL 17251815 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v855-17104924)
    
    [CL 17251843 by geoff evans in ue5-release-engine-test branch]

--- Engine/Build/BatchFiles/EditorPerf.bat
+++ Engine/Build/BatchFiles/EditorPerf.bat
@@ -1,1 +1,1 @@
-@call %~dp0RunUAT.bat BuildGraph -Script=Engine/Build/IterationProfile.xml -Target="Run Iteration Profile Tests" -set:Platforms=Win64 -append:Projects="AncientGame" -append:Projects="EngineTest" -append:Projects="ShooterGame" %*
+@call %~dp0RunUAT.bat BuildGraph -Script=Engine/Build/EditorPerf.xml -Target="Run EditorPerf Tests" -set:Platforms=Win64 -append:Projects="AncientGame" -append:Projects="EngineTest" -append:Projects="ShooterGame" %*
