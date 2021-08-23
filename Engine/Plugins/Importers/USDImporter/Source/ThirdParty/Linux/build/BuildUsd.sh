e3f3929392409bfd626ea573009aeb17cc1518ae
matt johnson <matt.johnson@epicgames.com>
2021-08-23 14:16:44 -0400

    USD: update Linux USD build script after recent file moves
    This updates the shell script used to build USD for UE to accommodate some
    recent changes in where headers and libraries for Linux are found in the engine
    repo. It also ensures that the USD build script is run using the engine's Python
    interpreter, the same one being compiled and linked against.
    
    #rb jeanmichel.dignard
    
    [CL 17271714 by matt johnson in ue5-main branch]

--- Engine/Plugins/Importers/USDImporter/Source/ThirdParty/Linux/build/BuildUsd.sh
+++ Engine/Plugins/Importers/USDImporter/Source/ThirdParty/Linux/build/BuildUsd.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-UE_SOURCE_DIR="./release-4.27"
+UE_SOURCE_DIR="./release-5.0"
 
 UE_THIRD_PARTY_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty"
 PYTHON_INSTALL_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python3/Linux" 
-PYTHON_EXECUTABLE_DIR="$UE_SOURCE_DIR/Engine/Binaries/ThirdParty/Python3/Linux"
+PYTHON_EXECUTABLE_DIR="$UE_SOURCE_DIR/Engine/Binaries/ThirdParty/Python3/Linux/bin"
+PYTHON_EXECUTABLE="$PYTHON_EXECUTABLE_DIR/python3"
 PYTHON_INCLUDE_DIR="$UE_SOURCE_DIR/Engine/Source/ThirdParty/Python3/Linux" 
 
-COMPILERFLAGS="-std=c++11 -fPIC -nostdinc++ -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include -I$UE_THIRD_PARTY_DIR/Linux/LibCxx/include/c++/v1 -I$PYTHON_INCLUDE_DIR/include/python3.7 -I$PYTHON_INCLUDE_DIR/include/x86_64-unknown-linux-gnu" 
-LINKERFLAGS="-L$UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/ -lc++ -lc++abi $PYTHON_INSTALL_DIR/lib/libpython3.7m.a -lm -lc -lgcc_s -lgcc -lutil"
+COMPILERFLAGS="-std=c++11 -fPIC -nostdinc++ -I$UE_THIRD_PARTY_DIR/Unix/LibCxx/include/c++/v1 -I$PYTHON_INCLUDE_DIR/include/python3.7m"
+LINKERFLAGS="-L$UE_THIRD_PARTY_DIR/Unix/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/ -lc++ -lc++abi $PYTHON_INSTALL_DIR/lib/libpython3.7m.a -lm -lc -lgcc_s -lgcc -lutil"
 
 export CC=clang
 export CXX=clang++
 
 export CXXFLAGS="$COMPILERFLAGS"
 export LDFLAGS="-stdlib=libc++ $LINKERFLAGS"
-export LIBS="$UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++.a $UE_THIRD_PARTY_DIR/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++abi.a"
+export LIBS="$UE_THIRD_PARTY_DIR/Unix/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++.a $UE_THIRD_PARTY_DIR/Unix/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++abi.a"
 
 CMAKE_LINKER_FLAGS="-stdlib=libc++ $LINKERFLAGS"
 
-BUILDARGS="-DCMAKE_CXX_FLAGS='$COMPILERFLAGS' -DCMAKE_EXE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_MODULE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_SHARED_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_STATIC_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR/include/python3.7m -DPYTHON_LIBRARY=$PYTHON_INSTALL_DIR/lib/libpython3.7m.a -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE_DIR/bin/python3.7"
+BUILDARGS="-DCMAKE_CXX_FLAGS='$COMPILERFLAGS' -DCMAKE_EXE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_MODULE_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_SHARED_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DCMAKE_STATIC_LINKER_FLAGS='$CMAKE_LINKER_FLAGS' -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR/include/python3.7m -DPYTHON_LIBRARY=$PYTHON_INSTALL_DIR/lib/libpython3.7m.a -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE"
 
 BOOST_BUILDARGS="toolset=clang cxxflags='$COMPILERFLAGS' linkflags='-stdlib=libc++ $LINKERFLAGS'"
 
 TBB_BUILDARGS="compiler=clang stdlib=libc++"
 
-python3 src/build_scripts/build_usd.py ./Build --no-tests --no-examples --no-tutorials --no-tools --no-docs --no-imaging --build-args USD,"$BUILDARGS" Boost,"$BOOST_BUILDARGS" TBB,"$TBB_BUILDARGS"
+$PYTHON_EXECUTABLE src/build_scripts/build_usd.py ./Build --no-tests --no-examples --no-tutorials --no-tools --no-docs --no-imaging --build-args USD,"$BUILDARGS" Boost,"$BOOST_BUILDARGS" TBB,"$TBB_BUILDARGS"
