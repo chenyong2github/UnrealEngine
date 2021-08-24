9c0804ff868dcd664428de09994ea27f24b4c340
Helge Mathee <Helge.Mathee@epicgames.com>
2021-08-24 18:28:55 -0400

    Control Rig: Introduce dynamic hierarchy nodes for allows parent / space switching
    #rb audit
    #jira na
    #fyi jack.cai
    
    [CL 17295020 by Helge Mathee in ue5-main branch]

---
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Execution/RigUnit_DynamicHierarchy.cpp
@@ -0,0 +1,136 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RigUnit_DynamicHierarchy.h"
+#include "Rigs/RigHierarchyController.h"
+#include "Units/RigUnitContext.h"
+
+FRigUnit_AddParent_Execute()
+{
+	if((Context.State != EControlRigState::Update) || (ExecuteContext.Hierarchy == nullptr))
+	{
+		return;
+	}
+
+	FRigTransformElement* ChildElement = ExecuteContext.Hierarchy->Find<FRigTransformElement>(Child);
+	if(ChildElement == nullptr)
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Child item %s does not exist."), *Child.ToString())
+		return;
+	}
+
+	FRigTransformElement* ParentElement = ExecuteContext.Hierarchy->Find<FRigTransformElement>(Parent);
+	if(ParentElement == nullptr)
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Parent item %s does not exist."), *Parent.ToString())
+		return;
+	}
+
+	if(URigHierarchyController* Controller = ExecuteContext.Hierarchy->GetController(true))
+	{
+		Controller->AddParent(ChildElement, ParentElement, 0.f, true, false);
+	}
+}
+
+FRigUnit_SwitchParent_Execute()
+{
+	if((Context.State != EControlRigState::Update) || (ExecuteContext.Hierarchy == nullptr))
+	{
+		return;
+	}
+
+	FRigTransformElement* ChildElement = ExecuteContext.Hierarchy->Find<FRigTransformElement>(Child);
+	if(ChildElement == nullptr)
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Child item %s does not exist."), *Child.ToString())
+		return;
+	}
+	if(!ChildElement->IsA<FRigMultiParentElement>())
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Child item %s cannot be space switched (only Nulls and Controls can)."), *Child.ToString())
+		return;
+	}
+
+	FRigTransformElement* ParentElement = nullptr;
+
+	if(Mode == ERigSwitchParentMode::ParentItem)
+	{
+		ParentElement = ExecuteContext.Hierarchy->Find<FRigTransformElement>(Parent);
+		if(ParentElement == nullptr)
+		{
+			UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Parent item %s does not exist."), *Parent.ToString())
+			return;
+		}
+
+		if(!ParentElement->IsA<FRigTransformElement>())
+		{
+			UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Parent item %s does not have a transform."), *Parent.ToString())
+			return;
+		}
+	}
+
+	const ERigTransformType::Type TransformTypeToMaintain =
+		bMaintainGlobal ? ERigTransformType::CurrentGlobal : ERigTransformType::CurrentLocal;
+	
+	const FTransform Transform = ExecuteContext.Hierarchy->GetTransform(ChildElement, TransformTypeToMaintain);
+	
+	switch(Mode)
+	{
+		case ERigSwitchParentMode::World:
+		{
+			ExecuteContext.Hierarchy->SwitchToWorldSpace(ChildElement, false, true);
+			break;
+		}
+		case ERigSwitchParentMode::DefaultParent:
+		{
+			ExecuteContext.Hierarchy->SwitchToDefaultParent(ChildElement, false, true);
+			break;
+		}
+		case ERigSwitchParentMode::ParentItem:
+		default:
+		{
+			ExecuteContext.Hierarchy->SwitchToParent(ChildElement, ParentElement, false, true);
+			break;
+		}
+	}
+	
+	ExecuteContext.Hierarchy->SetTransform(ChildElement, Transform, TransformTypeToMaintain, true);
+}
+
+FRigUnit_HierarchyGetParentWeights_Execute()
+{
+	if((Context.State != EControlRigState::Update) || (Context.Hierarchy == nullptr))
+	{
+		return;
+	}
+
+	FRigBaseElement* ChildElement = Context.Hierarchy->Find(Child);
+	if(ChildElement == nullptr)
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Item %s does not exist."), *Child.ToString())
+		return;
+	}
+	Weights = Context.Hierarchy->GetParentWeightArray(ChildElement, false);
+}
+
+FRigUnit_HierarchySetParentWeights_Execute()
+{
+	if((Context.State != EControlRigState::Update) || (ExecuteContext.Hierarchy == nullptr))
+	{
+		return;
+	}
+
+	FRigBaseElement* ChildElement = Context.Hierarchy->Find(Child);
+	if(ChildElement == nullptr)
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Item %s does not exist."), *Child.ToString())
+		return;
+	}
+
+	if(Weights.Num() != ExecuteContext.Hierarchy->GetNumberOfParents(ChildElement))
+	{
+		UE_CONTROLRIG_RIGUNIT_REPORT_ERROR(TEXT("Provided incorrect number of weights(%d) for %s - expected %d."), Weights.Num(), *Child.ToString(), ExecuteContext.Hierarchy->GetNumberOfParents(Child))
+		return;
+	}
+
+	ExecuteContext.Hierarchy->SetParentWeightArray(ChildElement, Weights, false, true);
+}
