b55d635108947e2c64bb307567cb50807f9a8f62
jeanfrancois dube <JeanFrancois.Dube@epicgames.com>
2021-08-24 09:38:04 -0400

    [Backout] - CL17280928 [FYI] JeanFrancois.Dube Original CL Desc ----------------------------------------------------------------- WorldPartitionVolume: revert back to using GetActorBounds to get the volume loading bounds.
    #rb none
    #preflight none
    
    
    #ROBOMERGE-SOURCE: CL 17286285
    #ROBOMERGE-BOT: (v861-17282326)
    
    [CL 17286313 by jeanfrancois dube in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/WorldPartition/WorldPartitionVolume.h
+++ Engine/Source/Runtime/Engine/Public/WorldPartition/WorldPartitionVolume.h
@@ -24,8 +24,5 @@ public:
 	void LoadIntersectingCells(bool bIsFromUserChange);
 	void UnloadIntersectingCells(bool bIsFromUserChange);
 	virtual EActorGridPlacement GetDefaultGridPlacement() const override { return EActorGridPlacement::AlwaysLoaded; }
-
-private:
-	FBox GetIntersectingBounds() const;
 #endif
 };
