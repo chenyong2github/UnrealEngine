35742e61860ad2435d8dbbbde7e8bb0b777d4cab
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-08-30 14:04:39 -0400

    #jira UERES-7 NeuralNetworkInference (NNI) plugin: - Removed static warning for NNI.
    #rb Francisco.Vicente
    #codereview Francisco.Vicente
    #rnx
    
    [CL 17355493 by Gines Hidalgo in ue5-main branch]

--- Engine/Source/Programs/IncludeTool/IncludeTool/Program.cs
+++ Engine/Source/Programs/IncludeTool/IncludeTool/Program.cs
@@ -1039,7 +1039,14 @@ namespace IncludeTool
 				{
 					foreach(PreprocessorMarkup Markup in File.Markup)
 					{
-						if(Markup.IncludedFile != null && (Markup.IncludedFile.Flags & SourceFileFlags.External) == 0 && !File.Location.FullName.Contains("PhyaLib") && !File.Location.GetFileName().Contains("Recast") && !File.Location.GetFileName().Contains("FramePro") && !File.Location.FullName.Contains("libSampleRate") && !File.Location.FullName.Contains("lz4"))
+						if (Markup.IncludedFile != null && (Markup.IncludedFile.Flags & SourceFileFlags.External) == 0
+							&& !File.Location.GetFileName().Contains("FramePro")
+							&& !File.Location.GetFileName().Contains("Recast")
+							&& !File.Location.FullName.Contains("libSampleRate")
+							&& !File.Location.FullName.Contains("lz4")
+							&& !File.Location.FullName.Contains("NeuralNetworkInference")
+							&& !File.Location.FullName.Contains("PhyaLib")
+							)
 						{
 							Log.WriteLine("{0}({1}): warning: external file is including non-external file ('{2}')", IncludedFile.Location.FullName, Markup.Location.LineIdx + 1, Markup.IncludedFile.Location);
 						}
