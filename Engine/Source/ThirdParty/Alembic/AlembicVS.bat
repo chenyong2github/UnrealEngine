3c044f19ffaff29f5d0e6d1b67e83a7fb9acccf4
matt johnson <matt.johnson@epicgames.com>
2021-09-01 17:10:09 -0400

    Alembic: update Windows build after ThirdParty move and use Visual Studio 2019
    We continue to output the build products to a "2015" directory, since
    WindowsTargetRules.GetVisualStudioCompilerVersionName() will still report that
    value for Visual Studio 2017, 2019, and 2022, as they should all be backwards
    compatible.
    
    This change also configures the build to write files to a "deploy" directory for consistency
    with the Linux and Mac builds. After building, header files and libraries should then be
    copied from there into the "AlembicDeploy" directory, which is where AlembicLib.Build.cs
    looks for them. The file and directory paths in all batch scripts were also changed to use
    Windows-style paths with backslashes.
    
    #rb anousack.kitisa
    #preflight 612d17a1e68f05000143b702
    
    #ROBOMERGE-SOURCE: CL 17392644 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v865-17346139)
    
    [CL 17392652 by matt johnson in ue5-release-engine-test branch]

--- Engine/Source/ThirdParty/Alembic/AlembicVS.bat
+++ Engine/Source/ThirdParty/Alembic/AlembicVS.bat
@@ -1,5 +1,5 @@
-SET ILMBASE_ROOT=%cd%/../../../../../../Source/ThirdParty/openexr/Deploy/OpenEXR-2.3.0/OpenEXR
-SET HDF5_ROOT=%cd%/AlembicDeploy/VS2015/x64/
+SET ILMBASE_ROOT=%cd%\..\openexr\Deploy\OpenEXR-2.3.0\OpenEXR
+SET HDF5_ROOT=%cd%\deploy\VS2015\x64\
 mkdir build
 cd build
 mkdir VS2015
@@ -7,7 +7,5 @@ cd VS2015
 rmdir /s /q alembic
 mkdir alembic
 cd alembic
-cmake -G "Visual Studio 14 2015 Win64" -DZLIB_INCLUDE_DIR=../../../../../../Source/ThirdParty/zlib/v1.2.8/include/Win64/VS2015/ -DZLIB_LIBRARY=../../../../../../Source/ThirdParty/zlib/v1.2.8/lib/Win64/VS2015/ -DALEMBIC_SHARED_LIBS=OFF -DUSE_TESTS=OFF -DUSE_BINARIES=OFF  -DUSE_HDF5=ON -DALEMBIC_ILMBASE_LINK_STATIC=ON -DUSE_STATIC_HDF5=OFF  -DCMAKE_INSTALL_PREFIX=%cd%/../../../AlembicDeploy/VS2015/x64/ ../../../alembic/ 
-cd ../../../
-
-
+cmake -G "Visual Studio 16 2019" -DZLIB_INCLUDE_DIR=..\..\..\..\zlib\v1.2.8\include\Win64\VS2015\ -DZLIB_LIBRARY=..\..\..\..\zlib\v1.2.8\lib\Win64\VS2015\ -DALEMBIC_SHARED_LIBS=OFF -DUSE_TESTS=OFF -DUSE_BINARIES=OFF -DUSE_HDF5=ON -DALEMBIC_ILMBASE_LINK_STATIC=ON -DUSE_STATIC_HDF5=OFF -DCMAKE_INSTALL_PREFIX=%cd%\..\..\..\deploy\VS2015\x64\ ..\..\..\alembic\
+cd ..\..\..\
--- Engine/Source/ThirdParty/Alembic/abc.bat
+++ Engine/Source/ThirdParty/Alembic/abc.bat
@@ -1,13 +1,13 @@
 set MSBUILDEMITSOLUTION=1
 
 call HDF5VS.bat
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/HDF5/alembic.sln /p:Configuration=Debug;Platform=x64 
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/HDF5/INSTALL.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/HDF5/alembic.sln /p:Configuration=Release;Platform=x64 
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/HDF5/INSTALL.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\HDF5\HDF5.sln /p:Configuration=Debug;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\HDF5\INSTALL.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\HDF5\HDF5.sln /p:Configuration=Release;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\HDF5\INSTALL.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
 
 call AlembicVS.bat
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/alembic/alembic.sln /p:Configuration=Debug;Platform=x64 
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/alembic/INSTALL.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/alembic/alembic.sln /p:Configuration=Release;Platform=x64 
-"C:/Program Files (x86)/MSBuild/14.0/Bin/msbuild.exe" %cd%/build/VS2015/alembic/INSTALL.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\alembic\Alembic.sln /p:Configuration=Debug;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\alembic\INSTALL.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\alembic\Alembic.sln /p:Configuration=Release;Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" %cd%\build\VS2015\alembic\INSTALL.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
