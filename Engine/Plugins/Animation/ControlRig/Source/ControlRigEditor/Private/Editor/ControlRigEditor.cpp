c9916d96770695ce7a8271e9777b93adcbf27b9a
Helge Mathee <Helge.Mathee@epicgames.com>
2021-09-07 09:41:07 -0400

    Control Rig: Perform initialization after preview mesh change
    #rb na
    #jira UE-121227
    
    [CL 17444048 by Helge Mathee in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditor.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditor.cpp
@@ -4019,9 +4019,11 @@ void FControlRigEditor::HandlePreviewMeshChanged(USkeletalMesh* InOldSkeletalMes
 			if(UControlRig* DebuggedControlRig = Cast<UControlRig>(GetBlueprintObj()->GetObjectBeingDebugged()))
 			{
 				DebuggedControlRig->GetHierarchy()->Notify(ERigHierarchyNotification::HierarchyReset, nullptr);
-				DebuggedControlRig->RequestSetup();
+				DebuggedControlRig->Initialize(true);
 			}
 		}
+
+		Compile();
 	}
 }
 
