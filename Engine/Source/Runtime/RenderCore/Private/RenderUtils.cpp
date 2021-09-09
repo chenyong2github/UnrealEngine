6599d2a1dc47efd8482e9ecb8d70b7b9c9420151
Uriel Doyon <Uriel.Doyon@epicgames.com>
2021-09-09 11:37:03 -0400

    Fixed possible division by zero in CalcTextureMipWidthInBlocks() and CalcTextureMipHeightInBlocks(). #jira UE-122317 #rb trivial #lockdown cristina.riveron
    [CL 17472846 by Uriel Doyon in 4.27 branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
@@ -734,17 +734,31 @@ FIntPoint CalcMipMapExtent( uint32 TextureSizeX, uint32 TextureSizeY, EPixelForm
 SIZE_T CalcTextureMipWidthInBlocks(uint32 TextureSizeX, EPixelFormat Format, uint32 MipIndex)
 {
 	const uint32 BlockSizeX = GPixelFormats[Format].BlockSizeX;
-	const uint32 WidthInTexels = FMath::Max<uint32>(TextureSizeX >> MipIndex, 1);
-	const uint32 WidthInBlocks = (WidthInTexels + BlockSizeX - 1) / BlockSizeX;
-	return WidthInBlocks;
+	if (BlockSizeX > 0)
+	{
+		const uint32 WidthInTexels = FMath::Max<uint32>(TextureSizeX >> MipIndex, 1);
+		const uint32 WidthInBlocks = (WidthInTexels + BlockSizeX - 1) / BlockSizeX;
+		return WidthInBlocks;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 SIZE_T CalcTextureMipHeightInBlocks(uint32 TextureSizeY, EPixelFormat Format, uint32 MipIndex)
 {
 	const uint32 BlockSizeY = GPixelFormats[Format].BlockSizeY;
-	const uint32 HeightInTexels = FMath::Max<uint32>(TextureSizeY >> MipIndex, 1);
-	const uint32 HeightInBlocks = (HeightInTexels + BlockSizeY - 1) / BlockSizeY;
-	return HeightInBlocks;
+	if (BlockSizeY > 0)
+	{
+		const uint32 HeightInTexels = FMath::Max<uint32>(TextureSizeY >> MipIndex, 1);
+		const uint32 HeightInBlocks = (HeightInTexels + BlockSizeY - 1) / BlockSizeY;
+		return HeightInBlocks;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 SIZE_T CalcTextureMipMapSize( uint32 TextureSizeX, uint32 TextureSizeY, EPixelFormat Format, uint32 MipIndex )
