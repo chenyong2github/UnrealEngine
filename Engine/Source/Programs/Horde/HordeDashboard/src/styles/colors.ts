55f16977db96385d67ab02ca68479daf09ca8c25
josh engebretson <Josh.Engebretson@epicgames.com>
2021-09-10 08:28:06 -0400

    Horde: Refactoring and cleanups for dark mode #jira none #rnx
    #ROBOMERGE-AUTHOR: josh.engebretson
    #ROBOMERGE-SOURCE: CL 17480895 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v870-17433530)
    
    [CL 17480908 by josh engebretson in ue5-release-engine-test branch]

--- Engine/Source/Programs/Horde/HordeDashboard/src/styles/colors.ts
+++ Engine/Source/Programs/Horde/HordeDashboard/src/styles/colors.ts
@@ -1,66 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-import { getTheme } from "@fluentui/react";
-import { DashboardPreference, JobState, LabelOutcome, LabelState } from "../backend/Api";
-import dashboard from "../backend/Dashboard";
-
-const theme = getTheme();
-
-export enum StatusColor {
-    Success,
-    Warnings,
-    Failure,
-    Waiting,
-    Ready,
-    Skipped,
-    Aborted,
-    Running,
-    Unspecified
-}
-
-export const getDefaultStatusColors = (): Map<StatusColor, string> => {
-
-    return new Map<StatusColor, string>([
-        [StatusColor.Success, dashboard.darktheme ? "#3b7b0a" : "#52C705"],
-        [StatusColor.Warnings, dashboard.darktheme ? "#9a7b18" : "#EDC74A"],
-        [StatusColor.Failure, dashboard.darktheme ? "#882f19" : "#DE4522"],
-        [StatusColor.Running, dashboard.darktheme ? "#146579" : theme.palette.blueLight],
-        [StatusColor.Waiting, dashboard.darktheme ? "#474542" : "#A19F9D"],
-        [StatusColor.Ready, dashboard.darktheme ? "#474542" : "#A19F9D"],
-        [StatusColor.Skipped, dashboard.darktheme ? "#63625c" : "#F3F2F1"],
-        [StatusColor.Aborted, dashboard.darktheme ? "#63625c" : "#F3F2F1"],
-        [StatusColor.Unspecified, "#637087"]
-    ]);
-
-}
-
-
-
-export const getStatusColors = () => {
-
-    const defaultStatusColors = getDefaultStatusColors();
-
-    const success = dashboard.getPreference(DashboardPreference.ColorSuccess);
-    const warning = dashboard.getPreference(DashboardPreference.ColorWarning);
-    const error = dashboard.getPreference(DashboardPreference.ColorError);
-    const running = dashboard.getPreference(DashboardPreference.ColorRunning);
-
-    return new Map<StatusColor, string>([
-        [StatusColor.Success, success ? success : defaultStatusColors.get(StatusColor.Success)!],
-        [StatusColor.Warnings, warning ? warning : defaultStatusColors.get(StatusColor.Warnings)!],
-        [StatusColor.Failure, error ? error : defaultStatusColors.get(StatusColor.Failure)!],
-        [StatusColor.Running, running ? running : defaultStatusColors.get(StatusColor.Running)!],
-        [StatusColor.Waiting, defaultStatusColors.get(StatusColor.Waiting)!],
-        [StatusColor.Ready, defaultStatusColors.get(StatusColor.Ready)!],
-        [StatusColor.Skipped, defaultStatusColors.get(StatusColor.Skipped)!],
-        [StatusColor.Aborted, defaultStatusColors.get(StatusColor.Aborted)!],
-        [StatusColor.Unspecified, defaultStatusColors.get(StatusColor.Unspecified)!]
-    ]);
-}
+import { JobState, LabelOutcome, LabelState } from "../backend/Api";
+import dashboard, {StatusColor} from "../backend/Dashboard";
 
 export const getJobStateColor = (state: JobState): string => {
 
-    const colors = getStatusColors();
+    const colors = dashboard.getStatusColors();
 
     if (state === JobState.Waiting) {
         return colors.get(StatusColor.Waiting)!;
@@ -83,7 +28,7 @@ export const getLabelColor = (state: LabelState | undefined, outcome: LabelOutco
         }
     }
 
-    const colors = getStatusColors();
+    const colors = dashboard.getStatusColors();
 
     if (state === LabelState.Complete) {
         switch (outcome!) {
