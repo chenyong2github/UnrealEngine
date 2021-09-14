7d8ac77282fa3dde18d83d8ce3b89cf460f6963c
simon therriault <Simon.Therriault@epicgames.com>
2021-09-14 17:10:55 -0400

    - Bumping livelink version for ue5 and fixing advertised version in LL message bus
    #rb jeremie.roy, geoffrey.douglas
    #preflight 6141020d568b3a0001b685cc
    
    #ROBOMERGE-AUTHOR: simon.therriault
    #ROBOMERGE-SOURCE: CL 17510145 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v870-17433530)
    
    [CL 17510194 by simon therriault in ue5-release-engine-test branch]

--- Engine/Source/Runtime/LiveLinkInterface/Public/ILiveLinkClient.h
+++ Engine/Source/Runtime/LiveLinkInterface/Public/ILiveLinkClient.h
@@ -111,7 +111,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 class LIVELINKINTERFACE_API ILiveLinkClient : public ILiveLinkClient_Base_DEPRECATED
 {
 public:
-	static const int32 LIVELINK_VERSION = 2;
+	static const int32 LIVELINK_VERSION = 3;
 
 	virtual ~ILiveLinkClient() {}
 
