7f474271ff42ab309b31ee87011a8f23a73c1cc4
Ben Marsh <Ben.Marsh@epicgames.com>
2021-09-15 16:50:38 -0400

    Missing file.
    [CL 17526547 by Ben Marsh in ue5-main branch]

---
+++ Engine/Source/Programs/Shared/EpicGames.Perforce/PerforceSettings.cs
@@ -0,0 +1,39 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpicGames.Perforce
+{
+	/// <summary>
+	/// Settings for a new Perforce connection
+	/// </summary>
+	public class PerforceSettings
+	{
+		/// <summary>
+		/// Server and port to connect to
+		/// </summary>
+		public string? Port { get; set; }
+
+		/// <summary>
+		/// Username to log in with
+		/// </summary>
+		public string? User { get; set; }
+
+		/// <summary>
+		/// Name of the client to use
+		/// </summary>
+		public string? Client { get; set; }
+
+		/// <summary>
+		/// The invoking application name
+		/// </summary>
+		public string? AppName { get; set; }
+
+		/// <summary>
+		/// The invoking application version
+		/// </summary>
+		public string? AppVersion { get; set; }
+	}
+}
