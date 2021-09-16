0a7c42084d705cc2ca5f40432aba6488f1e32a5f
Roberto Deioris <roberto@geodesic.games>
2021-09-16 13:51:26 -0400

    Fix for Horde Issue 101197
    #rb simon.therriault
    #jira none
    #lockdown simon.tourangeau
    
    [CL 17541905 by Roberto Deioris in 4.27 branch]

--- Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/AssetEditor/Viewport/SSimulcamEditorViewport.cpp
+++ Engine/Plugins/VirtualProduction/CameraCalibration/Source/CameraCalibrationEditor/Private/AssetEditor/Viewport/SSimulcamEditorViewport.cpp
@@ -1,8 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#include "AssetEditor/Viewport/SSimulcamEditorViewport.h"
 #include "AssetEditor/SSimulcamViewport.h"
 #include "AssetEditor/Viewport/SimulcamEditorViewportClient.h"
-#include "AssetEditor/Viewport/SSimulcamEditorViewport.h"
 #include "Engine/Texture.h"
 #include "Engine/TextureRenderTargetVolume.h"
 #include "Engine/VolumeTexture.h"
