a68503bcc270416833d2312f1c01fa768c3381be
jens petersam <Jens.Petersam@epicgames.com>
2021-09-16 02:25:38 -0400

    ElectraPlayer: Added an IMediaOptions holder around the POD pointer to check if it is still valid after garbage collection.
    #rnx
    
    #ROBOMERGE-AUTHOR: jens.petersam
    #ROBOMERGE-SOURCE: CL 17534571 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v870-17433530)
    
    [CL 17534587 by jens petersam in ue5-release-engine-test branch]

--- Engine/Plugins/Media/ElectraPlayer/Source/ElectraPlayerPlugin/Public/IElectraPlayerPluginModule.h
+++ Engine/Plugins/Media/ElectraPlayer/Source/ElectraPlayerPlugin/Public/IElectraPlayerPluginModule.h
@@ -12,6 +12,90 @@ class IAnalyticsProviderET;
 class IMediaEventSink;
 class IMediaPlayer;
 
+
+/**
+ * This class is used to get safe access to an IMediaOptions interface.
+ * When passing IMediaOptions through media framework as a POD pointer there is the risk
+ * that it is actually from a derived class like UMediaSource which is subject to GC.
+ * Thus it is not safe to keep the POD IMediaOptions around.
+ * This class is intended to be derived and instantiated from and stored as a TSharedPtr
+ * in a derived UMediaSource class.
+ * Then, as the media player is opened and the POD IMediaOptions is passed along, a
+ * GetMediaOption("GetSafeMediaOptions") query will be made on it to get this instance.
+ * If future access to the IMediaOptions is needed it will be made through this class
+ * by first locking, getting and using the IMediaOptions pointer returned here if it
+ * not null and unlocking afterwards.
+ */
+class IElectraSafeMediaOptionInterface : public IMediaOptions::FDataContainer
+{
+public:
+	virtual ~IElectraSafeMediaOptionInterface() = default;
+	virtual void Lock() = 0;
+	virtual void Unlock() = 0;
+	virtual IMediaOptions* GetMediaOptionInterface() = 0;
+
+	class FScopedLock
+	{
+	public:
+		FScopedLock(const TSharedPtr<IElectraSafeMediaOptionInterface, ESPMode::ThreadSafe>& InSafeMediaOptionInterface)
+			: SafeMediaOptionInterface(InSafeMediaOptionInterface)
+		{
+			if (SafeMediaOptionInterface.IsValid())
+			{
+				SafeMediaOptionInterface->Lock();
+			}
+		}
+		~FScopedLock()
+		{
+			if (SafeMediaOptionInterface.IsValid())
+			{
+				SafeMediaOptionInterface->Unlock();
+			}
+		}
+	
+	private:
+		TSharedPtr<IElectraSafeMediaOptionInterface, ESPMode::ThreadSafe> SafeMediaOptionInterface;
+	};
+};
+
+
+/**
+ * A ready to use implementation of the IElectraSafeMediaOptionInterface
+ */
+class FElectraSafeMediaOptionInterface : public IElectraSafeMediaOptionInterface
+{
+public:
+	FElectraSafeMediaOptionInterface(IMediaOptions* InOwner)
+		: Owner(InOwner)
+	{ }
+	virtual ~FElectraSafeMediaOptionInterface()
+	{
+		ClearOwner();
+	}
+	virtual void ClearOwner()
+	{
+		FScopeLock lock(&OwnerLock);
+		Owner = nullptr;
+	}
+	virtual void Lock() override
+	{
+		OwnerLock.Lock();
+	}
+	virtual void Unlock() override
+	{
+		OwnerLock.Unlock();
+	}
+	virtual IMediaOptions* GetMediaOptionInterface() override
+	{
+		return Owner;
+	}
+private:
+	FCriticalSection OwnerLock;
+	IMediaOptions* Owner = nullptr;
+};
+
+
+
 //! Data type for use with media options interface
 class FElectraSeekablePositions : public IMediaOptions::FDataContainer
 {
