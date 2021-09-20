3bd472151bd193b832d2a32db38bbbe07f4334b4
Jimmy Andrews <Jimmy.Andrews@epicgames.com>
2021-09-19 23:53:19 -0400

    add random scatter options to mesh fracture tool
    #preflight 6147f1c8ee16e200014f9e68
    
    [CL 17567529 by Jimmy Andrews in ue5-main branch]

--- Engine/Plugins/Experimental/ChaosEditor/Source/FractureEditor/Private/FractureToolMeshCut.h
+++ Engine/Plugins/Experimental/ChaosEditor/Source/FractureEditor/Private/FractureToolMeshCut.h
@@ -23,6 +23,18 @@ public:
 	UPROPERTY(EditAnywhere, Category = MeshCut, meta = (DisplayName = "Cutting Actor"))
 	TLazyObjectPtr<AStaticMeshActor> CuttingActor;
 
+	/** Number of meshes to random scatter; if 0, the selected mesh will be used in its current position, with no random scattering*/
+	UPROPERTY(EditAnywhere, Category = Distribution)
+	int NumberToScatter = 0;
+
+	UPROPERTY(EditAnywhere, Category = Distribution, meta = (ClampMin = "0.001", EditCondition = "NumberToScatter > 0"))
+	float MinScaleFactor = .5;
+
+	UPROPERTY(EditAnywhere, Category = Distribution, meta = (ClampMin = "0.001", EditCondition = "NumberToScatter > 0"))
+	float MaxScaleFactor = 1.5;
+
+	UPROPERTY(EditAnywhere, Category = Distribution, meta = (EditCondition = "NumberToScatter > 0"))
+	bool RandomizeOrientation = true;
 };
 
 
@@ -39,11 +51,22 @@ public:
 	virtual FText GetTooltipText() const override;
 	virtual FSlateIcon GetToolIcon() const override;
 
+	void Render(const FSceneView* View, FViewport* Viewport, FPrimitiveDrawInterface* PDI) override;
+
 	virtual void RegisterUICommand(FFractureEditorCommands* BindingContext) override;
 
 	virtual TArray<UObject*> GetSettingsObjects() const override;
 
+	virtual void FractureContextChanged() override;
 	virtual int32 ExecuteFracture(const FFractureToolContext& FractureContext) override;
+
+protected:
+	virtual void ClearVisualizations() override
+	{
+		Super::ClearVisualizations();
+		RenderMeshTransforms.Empty();
+		TransformsMappings.Empty();
+	}
 
 private:
 	// Slicing
@@ -52,6 +75,11 @@ private:
 
 	// check if the chosen actor can be used to cut the geometry collection (i.e. if it is a valid static mesh actor with a non-empty static mesh)
 	bool IsCuttingActorValid();
+
+	void GenerateMeshTransforms(const FFractureToolContext& Context, TArray<FTransform>& MeshTransforms);
+
+	TArray<FTransform> RenderMeshTransforms;
+	FVisualizationMappings TransformsMappings;
 };
 
 
