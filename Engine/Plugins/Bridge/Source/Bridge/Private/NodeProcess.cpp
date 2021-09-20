db52230614b833a58d1b01cfd3de112f78545a4f
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2021-09-20 11:38:17 -0400

    Add Bridge Plugin (version 2021.0.3) - Windows
    [CL 17571264 by Ahmed Rizwan in ue5-main branch]

---
+++ Engine/Plugins/Bridge/Source/Bridge/Private/NodeProcess.cpp
@@ -0,0 +1,85 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "NodeProcess.h"
+#include "Misc/Paths.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+class NodeProcessAsyncTask : public FNonAbandonableTask
+{
+public:
+	/*Default constructor*/
+	NodeProcessAsyncTask()
+	{
+	}
+ 
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(NodeProcessAsyncTask, STATGROUP_ThreadPoolAsyncTasks);
+	}
+ 
+	void DoWork()
+	{
+		const FString BridgePluginName = TEXT("Bridge");
+		const FString PluginPath = FPaths::Combine(FPaths::EnginePluginsDir(), BridgePluginName);
+	#if PLATFORM_WINDOWS
+		FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(PluginPath, TEXT("ThirdParty"), TEXT("Win"), TEXT("node-bifrost.exe")));
+	#elif PLATFORM_MAC 
+		FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(PluginPath, TEXT("ThirdParty"), TEXT("Mac"), TEXT("node-bifrost.app")));
+	#elif PLATFORM_LINUX
+		FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(PluginPath, TEXT("ThirdParty"), TEXT("Linux"), TEXT("node-bifrost")));
+	#endif
+
+		FPlatformProcess::ExecElevatedProcess(*MainFilePath, NULL, NULL);
+	}
+};
+
+TSharedPtr<FNodeProcessManager> FNodeProcessManager::NodeProcessManager;
+
+TSharedPtr<FNodeProcessManager> FNodeProcessManager::Get()
+{
+	if (!NodeProcessManager.IsValid())
+	{
+		NodeProcessManager = MakeShareable(new FNodeProcessManager);
+	}
+	return NodeProcessManager;
+}
+
+const FString FNodeProcessManager::GetPluginPath() const
+{
+	return FPaths::Combine(FPaths::EnginePluginsDir(), BridgePluginName);
+}
+
+const FString FNodeProcessManager::GetProcessURL() const
+{
+#if PLATFORM_WINDOWS
+	FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(GetPluginPath(), TEXT("ThirdParty"), TEXT("Win"), TEXT("node-bifrost.exe")));
+	return *MainFilePath;
+#elif PLATFORM_MAC 
+	FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(GetPluginPath(), TEXT("ThirdParty"), TEXT("Mac"), TEXT("node-bifrost.app")));
+	return *MainFilePath;
+#elif PLATFORM_LINUX
+	FString MainFilePath = FPaths::ConvertRelativePathToFull(FPaths::Combine(GetPluginPath(), TEXT("ThirdParty"), TEXT("Linux"), TEXT("node-bifrost")));
+	return *MainFilePath;
+#endif
+}
+
+void FNodeProcessManager::StartNodeProcess()
+{
+	const FString ProcessURL = GetProcessURL();
+
+#if PLATFORM_WINDOWS || PLATFORM_LINUX
+	NodeProcessHandle = FPlatformProcess::CreateProc(*ProcessURL, NULL, true, true, true, &OutProcessId, 0, NULL, NULL, NULL);
+
+	int32 ReturnCode;
+	FPlatformProcess::GetProcReturnCode(NodeProcessHandle, &ReturnCode);
+#elif PLATFORM_MAC
+	(new FAsyncTask<NodeProcessAsyncTask>())->StartBackgroundTask();
+#endif
+}
+
+void FNodeProcessManager::RestartNodeProcess()
+{
+	(new FAsyncTask<NodeProcessAsyncTask>())->StartBackgroundTask();
+}
