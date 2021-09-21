f76421ffe6ed3a32986fb493f80b1b4235cc6cd1
ben marsh <Ben.Marsh@epicgames.com>
2021-09-21 08:40:07 -0400

    Horde: Missing file.
    #ROBOMERGE-AUTHOR: ben.marsh
    #ROBOMERGE-SOURCE: CL 17581514 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v871-17566257)
    
    [CL 17581527 by ben marsh in ue5-release-engine-test branch]

---
+++ Engine/Source/Programs/Horde/HordeCommon/ProtoMethods.cs
@@ -0,0 +1,22 @@
+using EpicGames.Horde.Compute;
+using EpicGames.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HordeCommon.Rpc.Tasks
+{
+	partial class ComputeTaskResultMessage
+	{
+		public ComputeTaskResultMessage(CbObjectAttachment Result)
+		{
+			this.Result = Result;
+		}
+
+		public ComputeTaskResultMessage(ComputeTaskOutcome Outcome, string? Detail = null)
+		{
+			this.Outcome = Outcome;
+			this.Detail = Detail;
+		}
+	}
+}
