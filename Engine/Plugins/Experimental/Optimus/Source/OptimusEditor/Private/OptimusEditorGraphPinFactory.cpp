3543b1263db2502bdf705485ae0ce4c25e7d54f8
helge mathee <Helge.Mathee@epicgames.com>
2021-09-27 10:34:24 -0400

    Control Rig / Optimus: Only apply pin factories within each plugin
    #rb halfdan.ingvarsson
    #jira UE-126888
    #preflight https://horde.devtools.epicgames.com/job/6151a8fa59c66a000102f9f1
    
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 17630300 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v871-17566257)
    
    [CL 17630306 by helge mathee in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/Optimus/Source/OptimusEditor/Private/OptimusEditorGraphPinFactory.cpp
+++ Engine/Plugins/Experimental/Optimus/Source/OptimusEditor/Private/OptimusEditorGraphPinFactory.cpp
@@ -1,12 +1,22 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "OptimusEditorGraphPinFactory.h"
+#include "OptimusEditorGraph.h"
 
 #include "NodeFactory.h"
 
 
 TSharedPtr<class SGraphPin> FOptimusEditorGraphPinFactory::CreatePin(class UEdGraphPin* InPin) const
 {
+	if(const UEdGraphNode* OwningNode = InPin->GetOwningNode())
+	{
+		// only create pins within optimus graphs
+		if(Cast<UOptimusEditorGraph>(OwningNode->GetGraph()) == nullptr)
+		{
+			return nullptr;
+		}
+	}
+
 	if (InPin)
 	{
 		// FIXME: Add specializations here.
