104e7257509ec90a54445fd6fb5280f88a77e755
koray hagen <koray.hagen@epicgames.com>
2021-09-27 19:14:38 -0400

    Fixed a packaging issue causing AnimationLocomotion runtime module to not be found in QAGame
    #rb aaron.cox
    
    #ROBOMERGE-AUTHOR: koray.hagen
    #ROBOMERGE-SOURCE: CL 17636910 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v874-17637634)
    #ROBOMERGE[STARSHIP]: UE5-Release-Engine-Staging Release-5.0
    
    [CL 17637715 by koray hagen in ue5-release-engine-test branch]

--- Engine/Plugins/Animation/AnimationLocomotionLibrary/Source/Runtime/Public/AnimationLocomotionLibraryModule.h
+++ Engine/Plugins/Animation/AnimationLocomotionLibrary/Source/Runtime/Public/AnimationLocomotionLibraryModule.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FAnimationLocomotionLibraryModule : public IModuleInterface
+class FAnimationLocomotionLibraryRuntimeModule : public IModuleInterface
 {
 public:
 
