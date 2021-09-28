28669b8fb6b9a515b1186d468273e64adc42ac55
jonathan adamczewski <jonathan.adamczewski@epicgames.com>
2021-09-28 18:49:01 -0400

    UnrealBuildTool: Update source dependency json parsing for imported modules
    #jira none
    
    #ROBOMERGE-AUTHOR: jonathan.adamczewski
    #ROBOMERGE-SOURCE: CL 17653767 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v875-17642767)
    
    [CL 17653789 by jonathan adamczewski in ue5-release-engine-test branch]

--- Engine/Source/Programs/UnrealBuildTool/Modes/BuildMode.cs
+++ Engine/Source/Programs/UnrealBuildTool/Modes/BuildMode.cs
@@ -414,10 +414,10 @@ namespace UnrealBuildTool
 									ModuleOutputs[ProducedModule] = CppModulesAction.CompiledModuleInterfaceFile;
 								}
 
-								List<string>? ImportedModules;
+								List<(string Name, string BMI)>? ImportedModules;
 								if (CppDependencies.TryGetImportedModules(ModuleDependencyAction.DependencyListFile!, out ImportedModules))
 								{
-									ModuleImports[CppModulesAction.CompiledModuleInterfaceFile] = ImportedModules;
+									ModuleImports[CppModulesAction.CompiledModuleInterfaceFile] = ImportedModules.Select(x => x.Name).ToList();
 								}
 
 								CompiledModuleInterfaces.Add(CppModulesAction.CompiledModuleInterfaceFile);
