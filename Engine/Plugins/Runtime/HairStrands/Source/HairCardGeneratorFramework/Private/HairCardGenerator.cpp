3242113a3a0b483f3ca1a611a44e56334951891f
Mike Beach <Mike.Beach@epicgames.com>
2021-09-30 19:52:33 -0400

    Fixing CIS error from misplaced #pragma once.
    #rb no.one
    
    [CL 17685402 by Mike Beach in ue5-main branch]

--- Engine/Plugins/Runtime/HairStrands/Source/HairCardGeneratorFramework/Private/HairCardGenerator.cpp
+++ Engine/Plugins/Runtime/HairStrands/Source/HairCardGeneratorFramework/Private/HairCardGenerator.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "IHairCardGenerator.h"
 
 const FName IHairCardGenerator::ModularFeatureName(TEXT("HairCardGenerator"));
