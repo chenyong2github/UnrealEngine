d4ff6d0195a3a75f55b4d86cf74c94953f82015a
ryan schmidt <Ryan.Schmidt@epicgames.com>
2021-09-30 20:07:42 -0400

    GeometryScripting: move in-editor generated-mesh functionality from DynamicMeshActor to a subclass GeneratedDynamicMeshActor in experimental GeometryScriptingEditor module. Add EditorGeometryGenerationSubsystem to provide tick for mesh regeneration, instead of relying on Actor tick-in-Editor, which can be throttled by slate interaction (and is generally problematic).
    #rb none
    #rnx
    #jira none
    #preflight 6156250b9dc4c500012f56eb
    
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 17685256 in //UE5/Release-5.0/... via CL 17685594
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v875-17642767)
    #ROBOMERGE[STARSHIP]: UE5-Main
    
    [CL 17685601 by ryan schmidt in ue5-release-engine-test branch]

---
+++ Engine/Plugins/Experimental/GeometryScripting/Source/GeometryScriptingEditor/Public/GeometryActors/GeneratedDynamicMeshActor.h
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "DynamicMeshActor.h"
+
+#include "GeneratedDynamicMeshActor.generated.h"
+
+
+/**
+ * AGeneratedDynamicMeshActor is an Editor-only subclass of ADynamicMeshActor that provides 
+ * special support for dynamic procedural generation of meshes in the Editor, eg via Blueprints. 
+ * Expensive procedural generation implemented via BP can potentially cause major problems in 
+ * the Editor, in particular with interactive performance. AGeneratedDynamicMeshActor provides
+ * special infrastructure for this use case. Essentially, instead of doing procedural generation
+ * in the Construction Script, a BP-implementable event OnRebuildGeneratedMesh is available,
+ * and doing the procedural mesh regeneration when that function fires will generally provide
+ * better in-Editor interactive performance.
+ */
+UCLASS()
+class GEOMETRYSCRIPTINGEDITOR_API AGeneratedDynamicMeshActor : public ADynamicMeshActor
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	virtual ~AGeneratedDynamicMeshActor();
+
+	/**
+	 * This event will be fired to notify the BP that the generated Mesh should
+	 * be rebuilt. GeneratedDynamicMeshActor BP subclasses should rebuild their 
+	 * meshes on this event, instead of doing so directly from the Construction Script.
+	 */
+	UFUNCTION(BlueprintImplementableEvent, CallInEditor, Category = "Events")
+	void OnRebuildGeneratedMesh();
+
+
+	/**
+	 * This function will fire the OnRebuildGeneratedMesh event if the actor has been
+	 * marked for a pending rebuild (eg via OnConstruction)
+	 */
+	virtual void ExecuteRebuildGeneratedMeshIfPending();
+
+
+public:
+
+	// AActor overrides
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void Destroyed() override;
+
+
+protected:
+	// this internal flag is set in OnConstruction, and will cause ExecuteRebuildGeneratedMesh to
+	// fire the OnRebuildGeneratedMesh event, after which the flag will be cleared
+	bool bGeneratedMeshRebuildPending = false;
+
+	// indicates that this Actor is registered with the UEditorGeometryGenerationSubsystem, which 
+	// is where the mesh rebuilds are executed
+	bool bIsRegisteredWithGenerationManager = false;
+
+	virtual void RegisterWithGenerationManager();
+	virtual void UnregisterWithGenerationManager();
+};
