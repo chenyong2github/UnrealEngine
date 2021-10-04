abc0953639c3f17aff57cb304f55b8ef15a275f3
Marco Anastasi <Marco.Anastasi@epicgames.com>
2021-10-04 05:49:42 -0400

    First pass on a skeleton for the Skein CLI * boilerplate code for all the commands that are currently listed in the spec * support for required and optional arguments and options * basic user interaction prompts * basic support for progress bars.
    #jira ucs-2215
    
    #rb wouter.burgers, stuart.hill, andy.buecker
    #fyi pierreolivier.latour
    
    [CL 17705390 by Marco Anastasi in ue5-main branch]

---
+++ Engine/Programs/SkeinCLI/SkeinCLI/AuthCmd.cs
@@ -0,0 +1,101 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkeinCLI
+{
+    [Command(Name = "auth", Description = "Executes authorization related commands")]
+    [Subcommand(typeof(AuthLoginCmd),
+        typeof(AuthLogoutCmd))]
+    class AuthCmd : SkeinCmdBase
+    {
+        private SkeinCmdBase Parent { get; set; }
+
+        public override List<string> CreateArgs()
+        {
+            var args = Parent.CreateArgs();
+            args.Add("auth");
+
+            return args;
+        }
+
+        protected override Task<int> OnExecute(CommandLineApplication app)
+        {
+            //'skein auth' is an incomplete command, so we show help here even if --help is not specified
+            app.ShowHelp();
+            return base.OnExecute(app);
+        }
+    }
+
+    [Command(Name = "login", Description = "Skein login")]
+    class AuthLoginCmd : SkeinCmdBase
+    {
+        private AuthCmd Parent { get; set; }
+
+        [Option("-u|--username", CommandOptionType.SingleValue, Description = "User Name")]
+        public string UserName { get; set; }
+
+        [Option("-p|--password", CommandOptionType.SingleValue, Description = "Password")]
+        public string Password { get; set; }
+
+        public override List<string> CreateArgs()
+        {
+            var args = Parent.CreateArgs();
+            args.Add("login");
+
+            return args;
+        }
+
+        protected override Task<int> OnExecute(CommandLineApplication app)
+        {
+            if (string.IsNullOrEmpty(UserName) && !Quiet)
+            {
+                UserName = Prompt.GetString("User Name:",
+                    promptColor: ConsoleColor.Black,
+                    promptBgColor: ConsoleColor.White);
+            }
+
+            if (string.IsNullOrEmpty(Password) && !Quiet)
+            {
+                Password = Prompt.GetPassword("Password:",
+                    promptColor: ConsoleColor.Black,
+                    promptBgColor: ConsoleColor.White);
+            }
+
+            if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password))
+            {
+                Log.Logger.ForContext<ProjectsSnapshotsGetCmd>().Information($"User Name: {UserName}, Password: {Password}");
+                Log.Logger.ForContext<ProjectsListCmd>().Information("Login process here [...]");
+            }
+            else 
+            {
+                Log.Logger.ForContext<ProjectsListCmd>().Information("Login cancelled");
+            }
+
+            
+            return base.OnExecute(app);
+        }
+    }
+
+    [Command(Name = "logout", Description = "Skein logout")]
+    class AuthLogoutCmd : SkeinCmdBase
+    {
+        private AuthCmd Parent { get; set; }
+
+        public override List<string> CreateArgs()
+        {
+            var args = Parent.CreateArgs();
+            args.Add("logout");
+
+            return args;
+        }
+
+        protected override Task<int> OnExecute(CommandLineApplication app)
+        {
+            Log.Logger.ForContext<ProjectsListCmd>().Information("Logout process here [...]");
+            return base.OnExecute(app);
+        }
+    }
+}
---
+++ Engine/Programs/SkeinCLI/SkeinCLI/SkeinCmdBase.cs
@@ -0,0 +1,34 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkeinCLI
+{
+    [HelpOption("--help")]
+    abstract class SkeinCmdBase
+    {
+        public abstract List<string> CreateArgs();
+        
+        [Option("-q|--quiet", CommandOptionType.NoValue, Description = "Execute in quiet mode")]
+        public bool Quiet { get; set; }
+
+        protected virtual Task<int> OnExecute(CommandLineApplication app)
+        {
+            var args = CreateArgs();
+            if (Quiet)
+            {
+                args.Add("-q");
+            }
+
+            Log.Logger.ForContext<SkeinCmdBase>().Debug("Executing: skein " + ArgumentEscaper.EscapeAndConcatenate(args));
+            return Task.FromResult(0);
+        }
+
+        protected void OnException(Exception ex)
+        {
+            Log.Logger.ForContext<SkeinCmdBase>().Error(ex, "Exception");
+        }
+    }
+}
