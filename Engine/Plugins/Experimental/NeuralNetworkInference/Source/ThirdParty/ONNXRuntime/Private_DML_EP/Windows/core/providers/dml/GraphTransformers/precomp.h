3dc23eff04b412d5c25175a8a27877497abd38a0
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-10-06 17:10:17 -0400

    Part 1/2: {NNI}/ONNXRuntime/Internal/core/providers/DML: Revert the `ifdef win64`, adding WITH_UE and/or reverting code to avoid future merge conflicts in the first few files.
    #preflight 615e08a834989c0001f07287
    #rb francisco.vicente
    #codereview francisco.vicente
    #rnx
    
    [CL 17741629 by Gines Hidalgo in ue5-main branch]

---
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime/Private_DML_EP/Windows/core/providers/dml/GraphTransformers/precomp.h
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <limits>
+#include <cassert>
+#include <chrono>
+#include <vector>
+
+#include <wrl/client.h>
+#include <wrl/implements.h>
+
+#ifndef WITH_UE
+#include <wil/wrl.h>
+#include <wil/result.h>
+#endif
+
+#include <gsl/gsl>
