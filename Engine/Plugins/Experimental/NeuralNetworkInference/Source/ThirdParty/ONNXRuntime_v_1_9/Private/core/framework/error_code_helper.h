f42b27867f2c475fe8aa2984bca24076cf2f8b0c
gines hidalgo <Gines.Hidalgo@epicgames.com>
2021-10-07 16:36:14 -0400

    Added initial version of ORT_1.9.0 (disabled because it is crashing in both W and L). #rb francisco.vicente _codereview francisco.vicente #rnx
    #ROBOMERGE-AUTHOR: gines.hidalgo
    #ROBOMERGE-SOURCE: CL 17754799 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v879-17706426)
    
    [CL 17754829 by gines hidalgo in ue5-release-engine-test branch]

---
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_v_1_9/Private/core/framework/error_code_helper.h
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/common/status.h"
+#include "core/common/exceptions.h"
+#include "core/session/onnxruntime_c_api.h"
+
+namespace onnxruntime {
+_Ret_notnull_ OrtStatus* ToOrtStatus(const onnxruntime::common::Status& st);
+};
+
+#ifndef ORT_NO_EXCEPTIONS
+#define API_IMPL_BEGIN try {
+#define API_IMPL_END                                                \
+  }                                                                 \
+  catch (const onnxruntime::NotImplementedException& ex) {          \
+    return OrtApis::CreateStatus(ORT_NOT_IMPLEMENTED, ex.what());   \
+  }                                                                 \
+  catch (const std::exception& ex) {                                \
+    return OrtApis::CreateStatus(ORT_RUNTIME_EXCEPTION, ex.what()); \
+  }
+#else
+#define API_IMPL_BEGIN {
+#define API_IMPL_END }
+#endif
