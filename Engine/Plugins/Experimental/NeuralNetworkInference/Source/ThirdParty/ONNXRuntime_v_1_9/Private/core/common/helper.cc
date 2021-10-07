dd08b8f6dacf06ad6db60b46b3d8d075a2dc8a00
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-10-07 17:39:09 -0400

    ORT_1.9.0: Fixed leftover #ifdef #rb francisco.vicente _codereview francisco.vicente #rnx
    [CL 17755728 by Gines Hidalgo in ue5-main branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_v_1_9/Private/core/common/helper.cc
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_v_1_9/Private/core/common/helper.cc
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#ifdef _WIN32
 #include "ThirdPartyWarningDisabler.h" // WITH_UE
 NNI_THIRD_PARTY_INCLUDES_START
 #undef check
