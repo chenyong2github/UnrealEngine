4007bd01f5da70197674c311d417d24ff3d464ca
michael balzer <michael.balzer@epicgames.com>
2021-10-08 18:25:31 -0400

    OpenVDB: Fix linker error in ClangEditor Win64 builds
    #jira UE-130979
    #preflight 61608f3851b6340001ff7702, 61608f14635a7a0001a712ef
    #rnx
    
    #ROBOMERGE-OWNER: michael.balzer
    #ROBOMERGE-AUTHOR: michael.balzer
    #ROBOMERGE-COMMAND: _robomerge[starship] UE5-Main
    #ROBOMERGE-SOURCE: CL 17763550 in //UE5/Release-5.0/... via CL 17763551
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v879-17706426)
    #ROBOMERGE[STARSHIP]: UE5-Main
    
    [CL 17763569 by michael balzer in ue5-release-engine-test branch]

--- Engine/Source/ThirdParty/OpenVDB/OpenVDB.Build.cs
+++ Engine/Source/ThirdParty/OpenVDB/OpenVDB.Build.cs
@@ -9,35 +9,41 @@ public class OpenVDB : ModuleRules
 {
     public OpenVDB(ReadOnlyTargetRules Target) : base(Target)
     {
-		string VersionString = "openvdb-8.1.0";
-		
-		string OpenVDBDir = Path.Combine(Target.UEThirdPartySourceDirectory, "OpenVDB", "Deploy", VersionString);
-		string OpenVDBIncludeDir = Path.Combine(OpenVDBDir, "include");
-		string OpenVDBLibDir = Path.Combine(OpenVDBDir, "lib");
-				
-        // We are just setting up paths for pre-compiled binaries.
-        Type = ModuleType.External;
+	    // We are just setting up paths for pre-compiled binaries.
+	    Type = ModuleType.External;
 
-       	PublicIncludePaths.Add(OpenVDBIncludeDir);
+		const string versionString = "openvdb-8.1.0";
+		
+		string OpenVdbDir = Path.Combine(Target.UEThirdPartySourceDirectory, "OpenVDB", "Deploy", versionString);
+		string OpenVdbBinDir = Path.Combine(OpenVdbDir, "bin");
+		string OpenVdbIncludeDir = Path.Combine(OpenVdbDir, "include");
+		string OpenVdbLibDir = Path.Combine(OpenVdbDir, "lib");
+
+        PublicDefinitions.Add("NOMINMAX");
+        
+       	PublicIncludePaths.Add(OpenVdbIncludeDir);
        
         // Only building for Windows
         if (Target.Platform == UnrealTargetPlatform.Win64)
         {
-			string OpenVDBWin64LibName = "libopenvdb.lib";
-			string OpenVDBWin64LibDir = Path.Combine(OpenVDBLibDir, "Win64");
-			string OpenVDBWin64Lib = Path.Combine(OpenVDBWin64LibDir, OpenVDBWin64LibName);
+	        const string openVdbWin64LibName = "openvdb.lib";
+	        string OpenVdbWin64LibDir = Path.Combine(OpenVdbLibDir, "Win64", "VS2019");
+	        string OpenVdbWin64Lib = Path.Combine(OpenVdbWin64LibDir, openVdbWin64LibName);
 			
-            PublicAdditionalLibraries.Add(OpenVDBWin64Lib);
+            PublicAdditionalLibraries.Add(OpenVdbWin64Lib);
+
+            const string openVdbWin64DllName = "openvdb.dll";
+            string OpenVdbWin64BinDir = Path.Combine(OpenVdbBinDir, "Win64", "VS2019");
+            string OpenVdbWin64Dll = Path.Combine(OpenVdbWin64BinDir, openVdbWin64DllName);
+            
+            RuntimeDependencies.Add("$(TargetOutputDir)/" + openVdbWin64DllName, OpenVdbWin64Dll);
         }
         else
         {
-            string Err = "OpenVDB can only be used under Windows 64-bit!";
-            System.Console.WriteLine(Err);
-            throw new BuildException(Err);
+            const string error = "OpenVDB can only be used under Windows 64-bit!";
+            System.Console.WriteLine(error);
+            throw new BuildException(error);
         }
-		
-		PublicDefinitions.Add("NOMINMAX");
-		PublicDefinitions.Add("OPENVDB_STATICLIB");
 
 		PublicDependencyModuleNames.AddRange(
 			new string[]
@@ -49,10 +55,11 @@ public class OpenVDB : ModuleRules
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Blosc",
-				"zlib",
 				"IntelTBB"
 			}
 		);
+
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "Blosc");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "zlib");
     }
 }
