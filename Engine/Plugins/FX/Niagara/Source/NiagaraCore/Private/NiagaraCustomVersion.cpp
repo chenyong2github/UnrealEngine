86f21c1b29b7d1ec297b75be7127dd2ca8e5f3ed
devon penney <Devon.Penney@epicgames.com>
2021-10-11 19:06:49 -0400

    fix sampling normals from mesh distance fields #rb none #jira none
    #ROBOMERGE-AUTHOR: devon.penney
    #ROBOMERGE-SOURCE: CL 17778584 in //UE5/Release-5.0/... via CL 17778586
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v881-17767770)
    #ROBOMERGE[STARSHIP]: UE5-Main
    
    [CL 17778591 by devon penney in ue5-release-engine-test branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x67F13F7B, 0xAF06430C, 0x8660D50E, 0x4BB0DB90);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x03D31049, 0xA9144EC4, 0x8D77B697, 0x6841EF4E);
