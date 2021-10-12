d603eb3d30e47de687291ce2061048efd3443513
Vitalii Boiko <vitalii@pixelalabs.com>
2021-10-12 17:01:14 -0400

    nDisplay: fixed vertex declaration to adopt to LWC
    #jira UE-131004
    #rb Peter.Tarasenko
    
    [CL 17791079 by Vitalii Boiko in ue5-main branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Containers/DisplayClusterRender_MeshResources.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Containers/DisplayClusterRender_MeshResources.cpp
@@ -8,11 +8,11 @@ void FDisplayClusterMeshVertexDeclaration::InitRHI()
 {
 	FVertexDeclarationElementList Elements;
 
-	uint32 Stride = sizeof(FDisplayClusterMeshVertex);
+	uint32 Stride = sizeof(FDisplayClusterMeshVertexType);
 
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertex, Position), VET_Float4, 0, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertex, UV), VET_Float2, 1, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertex, UV_Chromakey), VET_Float2, 2, Stride));
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertexType, Position), VET_Float4, 0, Stride));
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertexType, UV), VET_Float2, 1, Stride));
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FDisplayClusterMeshVertexType, UV_Chromakey), VET_Float2, 2, Stride));
 
 	VertexDeclarationRHI = PipelineStateCache::GetOrCreateVertexDeclaration(Elements);
 }
