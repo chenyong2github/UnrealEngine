cd51b180d809e5837cf4dfed23a1a5531fd6f11a
jeannoe morissette <jeannoe.morissette@epicgames.com>
2021-10-12 15:11:51 -0400

    Updating Vulkan version check in Windows with driver that has everything we need for Nanite/Lumen.
    #fyi Mihnea.Balta,Rolando.Caloca
    #p4v-cherrypick 17789108
    
    [CL 17789158 by jeannoe morissette in ue5-main branch]

--- Engine/Source/Runtime/VulkanRHI/Private/Windows/VulkanWindowsPlatform.cpp
+++ Engine/Source/Runtime/VulkanRHI/Private/Windows/VulkanWindowsPlatform.cpp
@@ -402,8 +402,9 @@ void FVulkanWindowsPlatform::CheckDeviceDriver(uint32 DeviceIndex, EGpuVendorId 
 			}
 		}
 
+		if ((NvidiaVersion.Major < 496) || ((NvidiaVersion.Major == 496) && (NvidiaVersion.Minor < 13)))
 		{
-			UE_LOG(LogVulkanRHI, Warning, TEXT("Nvidia drivers do not currently support Nanite/Lumen in Vulkan."));
+			UE_LOG(LogVulkanRHI, Warning, TEXT("Nvidia drivers < 496.13 do not support Nanite/Lumen in Vulkan."));
 			extern TAutoConsoleVariable<int32> GRHIAllow64bitShaderAtomicsCvar;
 			GRHIAllow64bitShaderAtomicsCvar->SetWithCurrentPriority(0);
 		}
