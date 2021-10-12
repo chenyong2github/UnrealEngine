758d1b014ff7cecccad9da32167cfc98cd224b6c
Florin Pascu <Florin.Pascu@epicgames.com>
2021-10-12 08:54:44 -0400

    Re-add r.Mobile.EarlyZPass yet again #rb none #jira none #preflight 616579c73994b80001080c72
    [CL 17783516 by Florin Pascu in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
+++ Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
@@ -108,6 +108,15 @@ TAutoConsoleVariable<int32> CVarEarlyZPass(
 	TEXT("  x: use built in heuristic (default is 3)"),
 	ECVF_Scalability);
 
+static TAutoConsoleVariable<int32> CVarMobileEarlyZPass(
+	TEXT("r.Mobile.EarlyZPass"),
+	0,
+	TEXT("Whether to use a depth only pass to initialize Z culling for the mobile base pass.\n")
+	TEXT("  0: off\n")
+	TEXT("  1: all opaque \n"),
+	ECVF_Scalability
+);
+
 static TAutoConsoleVariable<int32> CVarBasePassWriteDepthEvenWithFullPrepass(
 	TEXT("r.BasePassWriteDepthEvenWithFullPrepass"),
 	0,
@@ -691,12 +700,15 @@ static void UpdateEarlyZPassModeCVarSinkFunction()
 {
 	static int32 CachedEarlyZPass = CVarEarlyZPass.GetValueOnGameThread();
 	static int32 CachedBasePassWriteDepthEvenWithFullPrepass = CVarBasePassWriteDepthEvenWithFullPrepass.GetValueOnGameThread();
+	static int32 CachedMobileEarlyZPass = CVarMobileEarlyZPass.GetValueOnGameThread();
 
 	const int32 EarlyZPass = CVarEarlyZPass.GetValueOnGameThread();
 	const int32 BasePassWriteDepthEvenWithFullPrepass = CVarBasePassWriteDepthEvenWithFullPrepass.GetValueOnGameThread();
+	const int32 MobileEarlyZPass = CVarMobileEarlyZPass.GetValueOnGameThread();
 
 	if (EarlyZPass != CachedEarlyZPass
-		|| BasePassWriteDepthEvenWithFullPrepass != CachedBasePassWriteDepthEvenWithFullPrepass)
+		|| BasePassWriteDepthEvenWithFullPrepass != CachedBasePassWriteDepthEvenWithFullPrepass
+		|| MobileEarlyZPass != CachedMobileEarlyZPass)
 	{
 		for (TObjectIterator<UWorld> It; It; ++It)
 		{
@@ -710,6 +722,7 @@ static void UpdateEarlyZPassModeCVarSinkFunction()
 
 		CachedEarlyZPass = EarlyZPass;
 		CachedBasePassWriteDepthEvenWithFullPrepass = BasePassWriteDepthEvenWithFullPrepass;
+		CachedMobileEarlyZPass = MobileEarlyZPass;
 	}
 }
 
@@ -3556,6 +3569,12 @@ void FScene::UpdateEarlyZPassMode()
 		}
 
 		if (IsMobileDistanceFieldEnabled(GetShaderPlatform()) || IsMobileAmbientOcclusionEnabled(GetShaderPlatform()))
+		{
+			EarlyZPassMode = DDM_AllOpaque;
+		}
+
+		bool bMobileForceFullDepthPrepass = CVarMobileEarlyZPass.GetValueOnAnyThread() == 1;
+		if (bMobileForceFullDepthPrepass)
 		{
 			EarlyZPassMode = DDM_AllOpaque;
 		}
