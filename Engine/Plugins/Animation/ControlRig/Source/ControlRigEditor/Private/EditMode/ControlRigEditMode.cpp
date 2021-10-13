35924880e6e682d8230f9262ba5bfcf17d6d43fc
benoit gadreau <benoit.gadreau@epicgames.com>
2021-10-13 04:37:25 -0400

    only fit in ControlRig's viewport when necessary
    #rb helge.mathee
    #jira UE-129486
    #preflight https://horde.devtools.epicgames.com/job/61668cb02faeee00013547e1
    
    #ROBOMERGE-AUTHOR: benoit.gadreau
    #ROBOMERGE-SOURCE: CL 17797036 in //UE5/Release-5.0/... via CL 17797040 via CL 17797042
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Main) (v881-17767770)
    
    [CL 17797046 by benoit gadreau in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditMode.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/EditMode/ControlRigEditMode.cpp
@@ -1983,6 +1983,7 @@ void FControlRigEditMode::FrameSelection()
 			Bounds += Sphere.Center + FVector::OneVector * Sphere.W;
 			Bounds += Sphere.Center - FVector::OneVector * Sphere.W;
 			CurrentViewportClient->FocusViewportOnBox(Bounds);
+			return;
 		}
     }
 
