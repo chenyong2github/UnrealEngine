51086d617d84224d28d7abcee1ee358196652936
Paul Vincent <mvincent@geodesic.games>
2021-10-14 00:35:49 -0400

    Bug Fix : See @ UE-130827
    [CL 17811960 by Paul Vincent in ue5-main branch]

--- Engine/Plugins/VirtualProduction/RemoteControl/Source/RemoteControl/Public/RemoteControlInstanceMaterial.h
+++ Engine/Plugins/VirtualProduction/RemoteControl/Source/RemoteControl/Public/RemoteControlInstanceMaterial.h
@@ -68,7 +68,7 @@ public:
 	//~ Begin IRemoteControlPropertyFactory interface
 	virtual TSharedPtr<FRemoteControlProperty> CreateRemoteControlProperty(URemoteControlPreset* Preset, UObject* Object, FRCFieldPathInfo FieldPath, FRemoteControlPresetExposeArgs Args) override;
 	virtual void PostSetObjectProperties(UObject* Object, bool bInSuccess) const override;
-	virtual bool SupportExposedObject(UObject* InObject, const FRCFieldPathInfo FieldPath) const override;
+	virtual bool SupportExposedClass(UClass* Class) const override;
 	//~ End IRemoteControlPropertyFactory interface
 };
 
