6b604fb8a128ca1fe5f794763aec98f1568fec5f
mieszko zielinski <mieszko.zielinski@epicgames.com>
2021-10-25 08:19:26 -0400

    Added MoveTargetFragment default initializer processor, MassMoveTargetFragmentInitializer
    #preflight 61768e0cfaf37d0001288de7
    
    #ROBOMERGE-AUTHOR: mieszko.zielinski
    #ROBOMERGE-SOURCE: CL 17905185 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v883-17842818)
    #ROBOMERGE[STARSHIP]: UE5-Release-Engine-Staging Release-5.0
    
    [CL 17905202 by mieszko zielinski in ue5-release-engine-test branch]

---
+++ Engine/Plugins/AI/MassAI/Source/MassAIMovement/Public/MassMoveTargetFragmentInitializer.h
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "MassTranslator.h"
+#include "MassMoveTargetFragmentInitializer.generated.h"
+
+
+UCLASS()
+class MASSAIMOVEMENT_API UMassMoveTargetFragmentInitializer : public UMassFragmentInitializer
+{
+	GENERATED_BODY()
+public:
+	UMassMoveTargetFragmentInitializer();
+	
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+
+	FMassEntityQuery InitializerQuery;
+};
