f1d81309d2ad645cce10e2a712fc2123c45f44ca
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-10-25 15:03:57 -0400

    Changing ORT code to avoid having to disable some warnings on 3rdPartyDisabler.h. Avoided: - Warning C4495: nonstandard extension '__super' used: replace with explicit base class name
    #rb Francisco.Vicente
    #codereview Francisco.Vicente
    #rnx
    
    [CL 17910090 by Gines Hidalgo in ue5-main branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_2021_08_22/Private_DML/Windows/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperatorRecurrentNeuralNetwork.cpp
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_2021_08_22/Private_DML/Windows/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperatorRecurrentNeuralNetwork.cpp
@@ -186,7 +186,7 @@ public:
     {
         // Assume that enough GPU work has been queued up after the RNN operator that it is worth
         // kicking it off, to enable subsequent CPU work to be parallelized with this GPU work.
-        __super::Compute(kernelContext);
+        DmlOperator::Compute(kernelContext); // WITH_UE
         m_executionProvider->Flush();
     }
 
