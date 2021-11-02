375279866ee260b83b7b1670dca78f744be911fb
UnrealBot <unrealbot@epicgames.com>
2021-11-02 21:19:52 +0000

    Branch snapshot for CL 18022868
    [CL 18022868 in 4.27 branch]

---
+++ Samples/PixelStreaming/WebServers/SignallingWebServer/platform_scripts/cmd/setup.ps1
@@ -0,0 +1,81 @@
+# Copyright Epic Games, Inc. All Rights Reserved.
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Versions are from current working release versions
+# No version for turnserver at the moment, see below why:
+#  https://github.com/coturn/coturn/issues/680
+#  https://github.com/coturn/coturn/issues/843
+#
+# Structure for installation preparation; please note | in "how to install" -> installer will split the command
+#       Need install Package name   Version   min/any      how to get version  how to install           path to be added
+$Packages = @(@("y", "chocolatey",  "0.11.3", "min",       "choco --version",  "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 | Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | choco upgrade chocolatey"),
+              @("y", "TURN server", "",       "any",       "turnserver",       "choco install coturn --version 4.5.2 -y -x -f"),
+			  @("y", "node",        "v17.0.1","min",       "node --version",   "choco install nodejs -y -x -f"),
+			  @("y", "npm",         "8.1.2",  "min",       "npm --version",    "npm install -g npm -f"),
+			  @("y", "express fw",  "4.17.1", "min",       "npm show express version",
+                                                                               "npm install express -f"),
+			  @("y", "STUN server", "0.0.1",  "min",       "npm show cirrus-webserver version",
+			                                                                   "npm install cirrus-webserver -f")
+			  )
+
+# Install npm packages at the correct place
+Push-Location ..\..
+
+# Check what to install
+foreach ($Item in $Packages) {
+ Write-Host "Checking for " $Item[1].padRight(12) " ..." -NoNewLine
+ if ($Item[3] -eq "any") {
+  Write-Host " any version ...                " -NoNewLine
+  $IsInstalled = Get-Command $Item[4] -ErrorAction SilentlyContinue
+  if ($IsInstalled -eq $null) {
+	Write-Host " not found                      marked for installation"
+  } else {
+	Write-Host " found                          no install needed"
+	$Item[0] = "n"
+  }
+ } elseif ($item[3] -eq "min") {
+  Write-Host " minimum version: " $Item[2].padRight(12) -NoNewLine
+  $Wanted = $Item[2] -replace "[^0-9.]"
+  $Installed = Invoke-Expression -Command $Item[4] 2>&1
+  if ($Installed -eq $null) {
+   Write-Host "  not found an installed version" $Item[4]
+  } else {
+   Write-Host "  found version: " $Installed.padRight(15) -NoNewLine
+   $Current = $Installed -replace "[^0-9.]"
+   if ([System.Version]$Current -lt [System.Version]$Wanted) {
+    Write-Host "old marked for installation"
+   } else {
+    $item[0] = "n"
+    Write-Host "no install needed"
+   }
+  }
+ } else {
+  Write-Host "Code error, please check Packages setup for " $Item[1]
+  exit
+ }
+}
+
+# Do the installation
+foreach ($Item in $Packages) {
+ if ($Item[0] -eq "n") {
+  continue;
+ }
+ if ($Item[5].Substring(0, 1) -eq ":") {
+  Write-Host "Will not install " $Item[1] " because " $Item[5]
+ } else {
+  $HasPipe = $Item[5].indexOf("|")
+  if ($HasPipe) {
+   $Arr = $Item[5].Split("|");
+   foreach($InstallExe in $Arr) {
+    $InstallExe = $InstallExe.trim()
+    Invoke-Expression $InstallExe
+   }
+  } else {
+   Invoke-Expression -Command $Item[5]
+  }
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+ }
+}
+
+# Reverse ..\.. location
+Pop-Location
