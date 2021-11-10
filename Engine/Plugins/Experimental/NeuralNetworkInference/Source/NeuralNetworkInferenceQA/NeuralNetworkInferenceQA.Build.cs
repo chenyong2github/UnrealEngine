bb1262fdd9bdabb39ab983c28e2bf423eb36968f
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-11-09 19:13:58 -0500

    Cleaned *.build.cs files
    #rb Francisco.Vicente
    _codereview Francisco.Vicente
    #rnx
    
    [CL 18117749 by Gines Hidalgo in ue5-main branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/NeuralNetworkInferenceQA/NeuralNetworkInferenceQA.Build.cs
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/NeuralNetworkInferenceQA/NeuralNetworkInferenceQA.Build.cs
@@ -6,7 +6,7 @@ public class NeuralNetworkInferenceQA : ModuleRules
 {
 	public NeuralNetworkInferenceQA( ReadOnlyTargetRules Target ) : base( Target )
 	{
-        ShortName = "NNIQA"; // Could be removed when plugin moves to Experimental, NFL path is too long
+        ShortName = "NNIQA"; // Shorten to avoid path-too-long errors
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicIncludePaths.AddRange(
--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/Private/NeuralNetworkInferenceQAEditor/NeuralNetworkInferenceQAEditor.Build.cs
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/Private/NeuralNetworkInferenceQAEditor/NeuralNetworkInferenceQAEditor.Build.cs
@@ -6,7 +6,7 @@ public class NeuralNetworkInferenceQAEditor : ModuleRules
 {
 	public NeuralNetworkInferenceQAEditor( ReadOnlyTargetRules Target ) : base( Target )
 	{
-        ShortName = "NNIQAEditor"; // Could be removed when plugin moves to Experimental, NFL path is too long
+        ShortName = "NNIQAEditor"; // Shorten to avoid path-too-long errors
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicIncludePaths.AddRange(
