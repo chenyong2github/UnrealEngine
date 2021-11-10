4685c767fb41cd1c7b90d272c400758b24db89ee
devon penney <Devon.Penney@epicgames.com>
2021-11-10 15:43:55 -0500

    devon.penney, please merge this change by hand. More info at https://robomerge.epicgames.net#STARSHIP
    {"clientFile":"/src/ROBOMERGE_STARSHIP_UE5_Release_Engine_Test/Engine/Plugins/Experimental/ChaosFluids/Content/Templates/Liquid/3D/Systems/Grid3D_Flip_Hose.uasset","fromFile":"//UE5/Release-Engine-Test/Engine/Plugins/FX/NiagaraSimulationStages/Content/Templates/Liquid/3D/Systems/Grid3D_Flip_Hose.uasset","startFromRev":11,"endFromRev":12,"contentTheirs":true,"resolveType":"move","resolveFlag":"m"}
    ["Filename resolve:","at: //UE5/Release-Engine-Test/Engine/Plugins/FX/NiagaraSimulationStages/Content/Templates/Liquid/3D/Systems/Grid3D_Flip_Hose.uasset","ay: //UE5/Release-Engine-Test/Engine/Plugins/Experimental/ChaosFluids/Content/Templates/Liquid/3D/Systems/Grid3D_Flip_Hose.uasset"]
    Backout CL 18120349
    [FYI] devon.penney
    #rb none
    #preflight 618bb4359239fd601635881d
    
    #ROBOMERGE-OWNER: devon.penney
    #ROBOMERGE-AUTHOR: mark.lintott
    #ROBOMERGE-COMMAND: _robomerge UE5-Main
    #ROBOMERGE-SOURCE: CL 18129164 in //UE5/Release-5.0/... via CL 18132426
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v889-18060218)
    #ROBOMERGE[STARSHIP]: UE5-Main
    
    [CL 18137193 by devon penney in ue5-release-engine-test branch]

--- Engine/Plugins/FX/NiagaraSimulationStages/Source/NiagaraSimulationStages/Private/NiagaraSimulationStages.cpp
+++ Engine/Plugins/FX/NiagaraSimulationStages/Source/NiagaraSimulationStages/Private/NiagaraSimulationStages.cpp
@@ -2,30 +2,30 @@
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
-#include "IChaosFluids.h"
+#include "INiagaraSimulationStages.h"
 #include "Interfaces/IPluginManager.h"
 #include "Misc/Paths.h"
 #include "ShaderCore.h"
 
 
-class FChaosFluids : public IChaosFluids
+class FNiagaraSimulationStages : public INiagaraSimulationStages
 {
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 };
 
-IMPLEMENT_MODULE(FChaosFluids, ChaosFluids )
+IMPLEMENT_MODULE(FNiagaraSimulationStages, NiagaraSimulationStages )
 
 
-void FChaosFluids::StartupModule()
+void FNiagaraSimulationStages::StartupModule()
 {
-	FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("ChaosFluids"))->GetBaseDir(), TEXT("Shaders"));
-	AddShaderSourceDirectoryMapping(TEXT("/Plugin/Experimental/ChaosFluids"), PluginShaderDir);
+	FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("NiagaraSimulationStages"))->GetBaseDir(), TEXT("Shaders"));
+	AddShaderSourceDirectoryMapping(TEXT("/Plugin/FX/NiagaraSimulationStages"), PluginShaderDir);
 }
 
 
-void FChaosFluids::ShutdownModule()
+void FNiagaraSimulationStages::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
