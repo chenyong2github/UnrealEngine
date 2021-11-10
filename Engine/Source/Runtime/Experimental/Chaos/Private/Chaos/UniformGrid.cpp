ee3fc2f909852a165c41dbe0bd2ee57434375341
nuno leiria <nuno.leiria@epicgames.com>
2021-11-09 22:49:06 -0500

    Fix Mac symbol visibility
    #ROBOMERGE-OWNER: aurel.cordonnier
    #ROBOMERGE-AUTHOR: nuno.leiria
    #ROBOMERGE-COMMAND: _robomerge[starship] UE5-Main
    #ROBOMERGE-SOURCE: CL 18097313 in //UE5/Release-5.0/... via CL 18097327
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v889-18060218)
    #ROBOMERGE[STARSHIP]: UE5-Main
    
    [CL 18122965 by nuno leiria in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/UniformGrid.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/UniformGrid.cpp
@@ -257,9 +257,15 @@ bool Chaos::TUniformGrid<T, 3>::IsValid(const TVector<int32, 3>& X) const
 	return X == ClampIndex(X);
 }
 
+#if PLATFORM_MAC || PLATFORM_LINUX
+template class CHAOS_API Chaos::TUniformGridBase<Chaos::FReal, 3>;
+template class CHAOS_API Chaos::TUniformGrid<Chaos::FReal, 3>;
+template class CHAOS_API Chaos::TUniformGrid<Chaos::FReal, 2>;
+#else
 template class Chaos::TUniformGridBase<Chaos::FReal, 3>;
 template class Chaos::TUniformGrid<Chaos::FReal, 3>;
 template class Chaos::TUniformGrid<Chaos::FReal, 2>;
+#endif
 
 template FVec3 Chaos::TUniformGridBase<Chaos::FReal, 3>::LinearlyInterpolate<FVec3>(const TArrayND<FVec3, 3>&, const FVec3&) const;
-template Chaos::FReal Chaos::TUniformGridBase<Chaos::FReal, 3>::LinearlyInterpolate<Chaos::FReal>(const TArrayND<Chaos::FReal, 3>&, const FVec3&) const;
+template CHAOS_API Chaos::FReal Chaos::TUniformGridBase<Chaos::FReal, 3>::LinearlyInterpolate<Chaos::FReal>(const TArrayND<Chaos::FReal, 3>&, const FVec3&) const;
