0b537a00d3b0f3b76c0ee37b675e45f30b54887b
Gines Hidalgo <Gines.Hidalgo@epicgames.com>
2021-11-11 12:39:27 -0500

    *Module.h/cpp --> Module.h/cpp
    #rb Francisco.Vicente
    #rnx
    
    [CL 18153562 by Gines Hidalgo in ue5-main branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/NeuralNetworkInference/Private/Module.cpp
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/NeuralNetworkInference/Private/Module.cpp
@@ -1,16 +1,16 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "NeuralNetworkInferenceQAModule.h"
+#include "Module.h"
 
 // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-void FNeuralNetworkInferenceQAModule::StartupModule()
+void FNeuralNetworkInferenceModule::StartupModule()
 {
 }
 
 // This function may be called during shutdown to clean up your module. For modules that support dynamic reloading,
 // we call this function before unloading the module.
-void FNeuralNetworkInferenceQAModule::ShutdownModule()
+void FNeuralNetworkInferenceModule::ShutdownModule()
 {
 }
 
-IMPLEMENT_MODULE(FNeuralNetworkInferenceQAModule, NeuralNetworkInferenceQA);
+IMPLEMENT_MODULE(FNeuralNetworkInferenceModule, NeuralNetworkInference);
