33f883f283dace2bf67b4e75cae83b7776a5fe58
axel riffard <axel.riffard@epicgames.com>
2021-11-17 21:52:42 -0500

    Change iOS sound suspend / resume audio logic to fix sound bug on iOS 15 #jira UE-134906 #rb jack.porter #fyi aaron.mcleran, will.damon, pete.sauerbrei #lockdown cristina.riveron
    [CL 18232873 by axel riffard in 4.27 branch]

--- Engine/Source/Runtime/ApplicationCore/Private/IOS/IOSAppDelegate.cpp
+++ Engine/Source/Runtime/ApplicationCore/Private/IOS/IOSAppDelegate.cpp
@@ -545,20 +545,15 @@ static IOSAppDelegate* CachedDelegate = nil;
 		switch ([[[notification userInfo] objectForKey:AVAudioSessionInterruptionTypeKey] unsignedIntegerValue])
 		{
 			case AVAudioSessionInterruptionTypeBegan:
-				self.bAudioActive = false;
-				FAppEntry::Suspend(true);
+				// Notification that our audio was stopped by the OS. No action needed.
 				break;
 
 			case AVAudioSessionInterruptionTypeEnded:
-
 				NSNumber * interruptionOption = [[notification userInfo] objectForKey:AVAudioSessionInterruptionOptionKey];
 				if (interruptionOption != nil && interruptionOption.unsignedIntegerValue > 0)
 				{
-					FAppEntry::RestartAudio();
+					[self ToggleAudioSession:true];
 				}
-
-				FAppEntry::Resume(true);
-				[self ToggleAudioSession:true];
 				break;
 		}
 	}];
@@ -1366,6 +1361,9 @@ FCriticalSection RenderSuspend;
     [self ToggleSuspend:true];
 #endif
 
+    self.bAudioActive = false;
+    FAppEntry::Suspend(true);
+
 	FEmbeddedCommunication::KeepAwake(TEXT("Background"), false);
 
 	[FIOSAsyncTask CreateTaskWithBlock : ^ bool(void)
@@ -1411,6 +1409,9 @@ extern double GCStartTime;
 	RenderSuspend.Unlock();
 	[self ToggleSuspend : false];
     [self ToggleAudioSession:true];
+	FAppEntry::RestartAudio();
+    FAppEntry::Resume(true);
+
 
     if (bEngineInit)
     {
