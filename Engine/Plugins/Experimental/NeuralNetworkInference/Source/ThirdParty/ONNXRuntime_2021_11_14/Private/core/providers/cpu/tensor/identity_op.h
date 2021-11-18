08ba8b9d38dee5221f624784b7177e51f88994b8
gines hidalgo <Gines.Hidalgo@epicgames.com>
2021-11-17 21:44:39 -0500

    #jira UE-133087 Fixed protobuf warning #rb Francisco.Vicente [CODEREVIEW] Francisco.Vicente #rnx
    #ROBOMERGE-AUTHOR: gines.hidalgo
    #ROBOMERGE-SOURCE: CL 18232819 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Test) (v895-18170469)
    
    [CL 18232835 by gines hidalgo in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_2021_11_14/Private/core/providers/cpu/tensor/identity_op.h
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/ThirdParty/ONNXRuntime_2021_11_14/Private/core/providers/cpu/tensor/identity_op.h
@@ -3,6 +3,10 @@
 
 #pragma once
 
+#include "ThirdPartyWarningDisabler.h" // WITH_UE: Avoids cryptic protobuf/stubs/strutil.h(364): warning C4127: conditional expression is constant; note: consider using 'if constexpr' statement instead
+NNI_THIRD_PARTY_INCLUDES_START
+#undef check
+#undef TEXT
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4996)
@@ -15,6 +19,7 @@
 #include "core/framework/TensorSeq.h"
 #include "core/framework/tensorprotoutils.h"
 #include "core/providers/utils.h"
+NNI_THIRD_PARTY_INCLUDES_END // WITH_UE
 
 namespace onnxruntime {
 
