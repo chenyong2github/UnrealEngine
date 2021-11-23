775076fde733788e4e1bfdd13ac1918adf892874
jared therriault <jared@geodesic.games>
2021-11-23 18:47:12 -0500

    Console Variables Editor:
    -Add project setting to choose which value to display when the row is unchecked
    
    #rb None
    #[fyi] Jason.Walter
    
    #jira UE-135860
    
    #ROBOMERGE-AUTHOR: jared.therriault
    #ROBOMERGE-SOURCE: CL 18277662 in //UE5/Release-5.0/... via CL 18277948
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18277989 by jared therriault in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/ConsoleVariables/Source/ConsoleVariablesEditor/Public/ConsoleVariablesEditorProjectSettings.h
+++ Engine/Plugins/Experimental/ConsoleVariables/Source/ConsoleVariablesEditor/Public/ConsoleVariablesEditorProjectSettings.h
@@ -4,6 +4,13 @@
 
 #include "ConsoleVariablesEditorProjectSettings.generated.h"
 
+UENUM(BlueprintType)
+enum class EConsoleVariablesEditorRowDisplayType : uint8
+{
+	ShowCurrentValue,
+	ShowLastEnteredValue
+};
+
 UCLASS(config = Engine, defaultconfig)
 class CONSOLEVARIABLESEDITOR_API UConsoleVariablesEditorProjectSettings : public UObject
 {
@@ -18,4 +25,11 @@ public:
 	/** If true, any console variable changes will be added to the current preset as long as the plugin is loaded. */
 	UPROPERTY(Config, EditAnywhere, Category="Console Variables")
 	bool bAddAllChangedConsoleVariablesToCurrentPreset;
+
+	/** When a row is unchecked, its associated variable's value will be set to the value recorded when the plugin was loaded.
+	 *The value displayed to the user can be configured with this setting, but will not affect the actual applied value.
+	 *ShowCurrentValue displays the actual value currently applied to the variable.
+	 *ShowLastEnteredValue displays the value that will be applied when the row is checked. */
+	UPROPERTY(Config, EditAnywhere, Category="Console Variables")
+	EConsoleVariablesEditorRowDisplayType UncheckedRowDisplayType;
 };
