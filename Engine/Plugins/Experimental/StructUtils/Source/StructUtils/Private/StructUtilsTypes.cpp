24428e1db6942ec08009f7cfd8e2ea6ec7f84e57
Maxime Mercier <Maxime.Mercier@epicgames.com>
2021-11-23 12:28:12 -0500

    SharedStruct implementation, similar to TSharedPtr for Objects but now for structs.
    #jira none
    #review-18224616
    #rnx
    #preflight 619d1493405273b2c35b8531
    
    #robomerge 5.0
    
    [CL 18271781 by Maxime Mercier in ue5-main branch]

---
+++ Engine/Plugins/Experimental/StructUtils/Source/StructUtils/Private/StructUtilsTypes.cpp
@@ -0,0 +1,57 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "StructUtilsTypes.h"
+#include "UObject/Class.h"
+#include "Serialization/ArchiveCrc32.h"
+#include "InstancedStruct.h"
+
+namespace UE::StructUtils
+{
+	STRUCTUTILS_API void AddStructReferencedObjects(const FConstStructView& StructView, class FReferenceCollector& Collector)
+	{
+		if (const UScriptStruct* ScriptStructPtr = StructView.GetScriptStruct())
+		{
+			Collector.AddReferencedObject(ScriptStructPtr);
+
+			if (uint8* StructMemory = const_cast<uint8*>(StructView.GetMemory()))
+			{
+				if (ScriptStructPtr->StructFlags & STRUCT_AddStructReferencedObjects)
+				{
+					ScriptStructPtr->GetCppStructOps()->AddStructReferencedObjects()(StructMemory, Collector);
+				}
+				else
+				{
+					// The iterator will recursively loop through all structs in structs too.
+					for (TPropertyValueIterator<const FObjectProperty> It(ScriptStructPtr, StructMemory); It; ++It)
+					{
+						UObject** ObjectPtr = static_cast<UObject**>(const_cast<void*>(It.Value()));
+						Collector.AddReferencedObject(*ObjectPtr);
+					}
+				}
+			}
+		}
+	}
+
+	STRUCTUTILS_API uint32 GetStructCrc32(const UScriptStruct& ScriptStruct, const uint8* StructMemory, const uint32 CRC /*= 0*/)
+	{
+		FArchiveCrc32 Ar(HashCombine(CRC, PointerHash(&ScriptStruct)));
+		if (StructMemory)
+		{
+			UScriptStruct& NonConstScriptStruct = const_cast<UScriptStruct&>(ScriptStruct);
+			NonConstScriptStruct.SerializeItem(Ar, const_cast<uint8*>(StructMemory), nullptr);
+		}
+		return Ar.GetCrc();
+	}
+
+	STRUCTUTILS_API uint32 GetStructCrc32(const FConstStructView& StructView, const uint32 CRC /*= 0*/)
+	{
+		if (const UScriptStruct* ScriptStruct = StructView.GetScriptStruct())
+		{
+			return GetStructCrc32(*ScriptStruct, StructView.GetMemory(), CRC);
+		}
+		return 0;
+	}
+
+}
--- Engine/Plugins/Experimental/StructUtils/Source/StructUtils/Public/StructUtilsTypes.h
+++ Engine/Plugins/Experimental/StructUtils/Source/StructUtils/Public/StructUtilsTypes.h
@@ -2,6 +2,20 @@
 
 #pragma once
 
+#include "UObject/Class.h"
+
 #ifndef WITH_STRUCTUTILS_DEBUG
 #define WITH_STRUCTUTILS_DEBUG (!(UE_BUILD_SHIPPING || UE_BUILD_SHIPPING_WITH_EDITOR || UE_BUILD_TEST) && 1)
 #endif // WITH_STRUCTUTILS_DEBUG
+
+struct FConstStructView;
+class FReferenceCollector;
+
+namespace UE::StructUtils
+{
+	extern STRUCTUTILS_API void AddStructReferencedObjects(const FConstStructView& StructView, class FReferenceCollector& Collector);
+
+	extern STRUCTUTILS_API uint32 GetStructCrc32(const UScriptStruct& ScriptStruct, const uint8* StructMemory, const uint32 CRC = 0);
+
+	extern STRUCTUTILS_API uint32 GetStructCrc32(const FConstStructView& StructView, const uint32 CRC = 0);
+}
