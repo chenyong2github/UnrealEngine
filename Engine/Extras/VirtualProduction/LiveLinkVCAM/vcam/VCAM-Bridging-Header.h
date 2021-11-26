c96fd70201f94fb7a8d16c246b3b1069ee2c7f0b
brian smith <Brian.Smith@xa.epicgames.com>
2021-11-26 07:11:33 -0500

    Live Link VCAM : (re)initial checkin : the Tentacle Framework was causing horde failures as it is a 3rd party library.
    #ROBOMERGE-AUTHOR: brian.smith
    #ROBOMERGE-SOURCE: CL 18299298 in //UE5/Release-5.0/... via CL 18299310
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18299311 by brian smith in ue5-release-engine-test branch]

---
+++ Engine/Extras/VirtualProduction/LiveLinkVCAM/vcam/VCAM-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  VCAM-Bridging-Header.h
+//  vcam
+//
+//  Created by Brian Smith on 8/10/20.
+//  Copyright Epic Games. Inc. All rights reserved.
+//
+
+#ifndef VCAM_Bridging_Header_h
+#define VCAM_Bridging_Header_h
+
+#import "PassthroughShaderTypes.h"
+#import "ObjCUtility.h"
+#import <LiveLinkProvider/LiveLink.h>
+
+#endif /* VCAM_Bridging_Header_h */
