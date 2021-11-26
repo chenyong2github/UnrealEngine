db825dd5673f0439ee3980239b1dd566f5835df1
stu mckenna <Stu.McKenna@epicgames.com>
2021-11-26 15:48:21 -0500

    - Bump version to fix a UB out of date issue
    #rb trivial
    #rnx
    
    #ROBOMERGE-OWNER: stu.mckenna
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 18301517 via CL 18301606 via CL 18301610 via CL 18301823 via CL 18301847
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18301863 by stu mckenna in ue5-release-engine-test branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xD8BFE375, 0xCAF76740, 0x9E0FD451, 0xA3B5984E);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xAD773C8E, 0x9F7A1544, 0xAE4598F6, 0xC73F6125);
