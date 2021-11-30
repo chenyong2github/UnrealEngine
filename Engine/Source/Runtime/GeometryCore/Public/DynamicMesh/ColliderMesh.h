7ff5cba6b295bc8e0918c0f6a8c43acb126c4ac4
ryan schmidt <Ryan.Schmidt@epicgames.com>
2021-11-30 11:40:16 -0500

    GeometryProcessing: Add FColliderMesh, minimal 3D triangle mesh representation w/ a buit-in AABBTree, intended to be used to avoid heavier FDynamicMesh3 copies in places where only a BVH is needed. Add Ray transform/inverse functions to Transform3d
    #rb semion.piskarev
    #rnx
    #jira none
    #preflight 61a547e647752856c90b5fe9
    
    #ROBOMERGE-AUTHOR: ryan.schmidt
    #ROBOMERGE-SOURCE: CL 18325839 in //UE5/Release-5.0/... via CL 18325857
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18325877 by ryan schmidt in ue5-release-engine-test branch]

---
+++ Engine/Source/Runtime/GeometryCore/Public/DynamicMesh/ColliderMesh.h
@@ -0,0 +1,120 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Spatial/MeshAABBTree3.h"
+
+#pragma once
+
+
+
+namespace UE
+{
+namespace Geometry
+{
+
+class FDynamicMesh3;
+
+/**
+ * FColliderMesh is a minimal representation of an Indexed Triangle Mesh suitable to use
+ * with a TMeshAABBTree3. This class is intended to be used in situations where a copy
+ * of a FDynamicMesh3 would be created/kept only to use with an AABBTree or FWNTree. In such 
+ * situations, the FDynamicMesh3 copy is (relatively) heavy memory-wise, particularly if a large 
+ * number of small mesh/AABBTree pairs are stored. In those cases a FColliderMesh can be used instead.
+ * 
+ * If FBuildOptions.bBuildAABBTree is true (default), then an AABBTree will automatically be
+ * built and available via GetAABBTree()
+ * 
+ * If the source FDynamicMesh3 is not compact, it will be compacted. ID mappings can optionally
+ * be stored if the FBuildOptions are configured to request it (default false), allowing 
+ * hit-triangle/vertex IDs to be mapped back to IDs on the source mesh.
+ * 
+ * Currently there is no support for UVs or Vertex Normals.
+ * 
+ * FColliderMesh (effectively) implements the TTriangleMeshAdapter interface, and so can be used 
+ * anywhere a TTriangleMeshAdapter could be used (eg with TMeshQueries functions). 
+ */
+class GEOMETRYCORE_API FColliderMesh
+{
+public:
+	struct FBuildOptions
+	{
+		FBuildOptions()
+		{
+			bBuildAABBTree = true;
+			bBuildVertexMap = false;
+			bBuildTriangleMap = false;
+		}
+
+		/** If true, AABBTree is automatically built for the mesh on construction */
+		bool bBuildAABBTree;
+		/** If true and SourceWasCompactV() == false, mapping from FColliderMesh to source mesh vertex IDs is constructed */
+		bool bBuildVertexMap;
+		/** If true and SourceWasCompactT() == false, mapping from FColliderMesh to source mesh triangle IDs is constructed */
+		bool bBuildTriangleMap;
+	};
+
+public:
+	FColliderMesh();
+	FColliderMesh(const FDynamicMesh3& SourceMesh, const FBuildOptions& BuildOptions = FBuildOptions());
+
+	void Initialize(const FDynamicMesh3& SourceMesh, const FBuildOptions& BuildOptions = FBuildOptions());
+
+	void Reset(bool bReleaseMemory);
+
+	// mesh API required for TMeshAABBTree3
+	bool IsTriangle(int32 TriangleID) const { return TriangleID >= 0 && TriangleID < Triangles.Num(); }
+	bool IsVertex(int32 VertexID) const { return VertexID >= 0 && VertexID < Vertices.Num(); }
+	int32 MaxTriangleID() const { return Triangles.Num(); }
+	int32 MaxVertexID() const { return Vertices.Num(); }
+	int32 TriangleCount() const { return Triangles.Num(); }
+	int32 VertexCount() const { return Vertices.Num(); }
+	uint64 GetChangeStamp() const { return 0; }
+	FIndex3i GetTriangle(int32 TriangleID) const { return Triangles[TriangleID]; }
+	FVector3d GetVertex(int32 VertexID) const { return Vertices[VertexID]; }
+
+	void GetTriVertices(int32 TriangleID, FVector3d& V0, FVector3d& V1, FVector3d& V2) const
+	{
+		FIndex3i Tri = Triangles[TriangleID];
+		V0 = Vertices[Tri.A];
+		V1 = Vertices[Tri.B];
+		V2 = Vertices[Tri.C];
+	}
+
+	FVector3d GetTriNormal(int32 TriangleID) const
+	{
+		FIndex3i Tri = Triangles[TriangleID];
+		return VectorUtil::Normal(Vertices[Tri.A], Vertices[Tri.B], Vertices[Tri.C]);
+	}
+
+	bool SourceWasCompactV() const { return bSourceWasCompactV; }
+	bool HasVertexIDMap() const { return SourceVertexIDs.Num() > 0 && SourceVertexIDs.Num() == Vertices.Num(); }
+	int32 GetSourceVertexID(int32 VertexID) const;
+	bool SourceWasCompactT() const { return bSourceWasCompactT; }
+	bool HasTriangleIDMap() const { return SourceTriangleIDs.Num() > 0 && SourceTriangleIDs.Num() == Triangles.Num(); }
+	int32 GetSourceTriangleID(int32 TriangleID) const;
+
+	// spatial APIs
+
+	TMeshAABBTree3<FColliderMesh>& GetAABBTree();
+
+protected:
+	// For large meshes it might be better to optionally use TDynamicVector here (but keeping the TArray for
+	// small meshes is still also desirable due to minimum-size). Adds a branch to every access, though.
+	TArray<FVector3d> Vertices;
+	TArray<FIndex3i> Triangles;
+
+	TArray<int32> SourceVertexIDs;
+	bool bSourceWasCompactV;
+	TArray<int32> SourceTriangleIDs;
+	bool bSourceWasCompactT;
+
+	TMeshAABBTree3<FColliderMesh> AABBTree;
+
+};
+
+} // end namespace UE::Geometry
+} // end namespace UE
+
+
+
+
+
