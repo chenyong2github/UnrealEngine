ffdf3d4796f69aebd9f047bd04dd84164aacedc7
chris babcock <Chris.Babcock@epicgames.com>
2021-11-30 16:18:57 -0500

    Fully specify configrules key and provide CRC
    #android
    [REVIEW] [at]Allan.Bentham
    #rb Allan.Bentham
    
    #ROBOMERGE-AUTHOR: chris.babcock
    #ROBOMERGE-SOURCE: CL 18329983 via CL 18330402 via CL 18330983 via CL 18331222 via CL 18331495 via CL 18331655 via CL 18332062 via CL 18332121
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18332146 by chris babcock in ue5-release-engine-test branch]

--- Engine/Build/Android/Prebuilt/ConfigRulesTool/compile.bat
+++ Engine/Build/Android/Prebuilt/ConfigRulesTool/compile.bat
@@ -1,3 +1,3 @@
-%JAVA_HOME%\bin\javac.exe -d classes src\*.java
+"%JAVA_HOME%"\bin\javac.exe -d classes src\*.java
 cd classes
-%JAVA_HOME%\bin\jar cfe ../bin/ConfigRulesTool.jar ConfigRulesTool *.class
+"%JAVA_HOME%"\bin\jar cfe ../bin/ConfigRulesTool.jar ConfigRulesTool *.class
