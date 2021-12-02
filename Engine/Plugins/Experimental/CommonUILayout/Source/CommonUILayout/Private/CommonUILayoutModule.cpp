a94648120e55ec2e981a0f91e4d36e5627091948
Prajwal Manjunath <prajwal.manjunath@epicgames.com>
2021-12-02 18:27:36 -0500

    Remove CommonUILayout plugin
    #rb none
    #jira none
    
    [CL 18361453 by Prajwal Manjunath in ue5-main branch]

--- Engine/Plugins/Experimental/CommonUILayout/Source/CommonUILayout/Private/CommonUILayoutModule.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Modules/ModuleManager.h"
-#include "CommonUILayoutLog.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, CommonUILayout);
-
-DEFINE_LOG_CATEGORY(LogCommonUILayout);
