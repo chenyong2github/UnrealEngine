c8ae980adb09065f8c8398a6db7c049ef8b34dc4
aurel cordonnier <aurel.cordonnier@epicgames.com>
2021-12-03 18:46:18 -0500

    Fix for virtual textures not applying RDO when compressed with Oodle.
    Since the properties weren't copied, this means that the DDC key doesn't represent the actual bits, so the VT ddc key is invalidated.
    #preflight 61a90b6e8015016c7e409a59
    #rb devin.doucette
    
    #ROBOMERGE-OWNER: aurel.cordonnier
    #ROBOMERGE-AUTHOR: dan.thompson
    #ROBOMERGE-SOURCE: CL 18359779 via CL 18360182 via CL 18375636 via CL 18375640
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18375641 by aurel cordonnier in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
+++ Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
@@ -65,7 +65,7 @@
 
 // This GUID is mixed into DDC version for virtual textures only, this allows updating DDC version for VT without invalidating DDC for all textures
 // This is useful during development, but once large numbers of VT are present in shipped content, it will have the same problem as TEXTURE_DERIVEDDATA_VER
-#define TEXTURE_VT_DERIVEDDATA_VER	TEXT("4FA538D2450243E380565F65CE8E44B7")
+#define TEXTURE_VT_DERIVEDDATA_VER	TEXT("F36E4B807BC24Y818FE085C655C45176")
 
 #if ENABLE_COOK_STATS
 namespace TextureCookStats
