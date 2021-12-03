6390bf8d372732d8985886212296814bd688c0f1
jason nadro <jason.nadro@epicgames.com>
2021-12-03 13:47:50 -0500

    For the Scene Infos that we _don't_ update static draw lists for we still need to updated cached ray tracing instances.  This work was previously done when we re-added the prim to the scene and re-created the static draw lists which we are no longer doing for each primitive.
    #rb Tiago.Costa
    #preflight 6177efa675a26c0001976396
    #lockdown Michal.Valient
    
    #ROBOMERGE-AUTHOR: jason.nadro
    #ROBOMERGE-SOURCE: CL 17923226 via CL 18007051 via CL 18370444 via CL 18370517
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18370590 by jason nadro in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
+++ Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
@@ -4736,6 +4736,11 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 		TArray<FPrimitiveSceneInfo*> UpdatedSceneInfosWithoutStaticDrawListUpdate;
 		UpdatedSceneInfosWithoutStaticDrawListUpdate.Reserve(UpdatedInstances.Num());
 
+#if RHI_RAYTRACING
+		TArray<FPrimitiveSceneInfo*> RayTracingPrimitivesToUpdate;
+		RayTracingPrimitivesToUpdate.Reserve(UpdatedInstances.Num());
+#endif
+
 		for (const auto& UpdateInstance : UpdatedInstances)
 		{
 			FPrimitiveSceneProxy* PrimitiveSceneProxy = UpdateInstance.Key;
@@ -4765,6 +4770,9 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 			}
 			else
 			{
+#if RHI_RAYTRACING
+				RayTracingPrimitivesToUpdate.Add(PrimitiveSceneInfo);
+#endif
 				UpdatedSceneInfosWithoutStaticDrawListUpdate.Push(PrimitiveSceneInfo);
 				PrimitiveSceneInfo->RemoveFromScene(false);
 			}
@@ -4815,6 +4823,9 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 		if (UpdatedSceneInfosWithoutStaticDrawListUpdate.Num() > 0)
 		{
 			FPrimitiveSceneInfo::AddToScene(GraphBuilder.RHICmdList, this, UpdatedSceneInfosWithoutStaticDrawListUpdate, false, true, bAsyncCreateLPIs);
+#if RHI_RAYTRACING
+			FPrimitiveSceneInfo::UpdateCachedRayTracingInstances(this, RayTracingPrimitivesToUpdate);
+#endif
 		}
 	}
 
