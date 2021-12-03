48666ebb0d9c62fd59b4c0f053cb6bb511a16103
jon nabozny <Jon.Nabozny@epicgames.com>
2021-12-03 13:16:21 -0500

    Temporary workaround for a rare streaming crash: make FRayTracingGeometryInstance.GeometryRHI ref counted so it won't get streamed out.
    #rb yuriy.odonnell
    #preflight 616e795169f4810001268baf
    #lockdown Andrew.Firth
    
    #ROBOMERGE-OWNER: jon.nabozny
    #ROBOMERGE-AUTHOR: aleksander.netzel
    #ROBOMERGE-SOURCE: CL 17859155 via CL 18004062 via CL 18369939 via CL 18369948
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v895-18170469)
    
    [CL 18369983 by jon nabozny in ue5-release-engine-test branch]

--- Engine/Source/Runtime/RHI/Public/DynamicRHI.h
+++ Engine/Source/Runtime/RHI/Public/DynamicRHI.h
@@ -96,7 +96,8 @@ ENUM_CLASS_FLAGS(ERayTracingInstanceFlags);
 */
 struct FRayTracingGeometryInstance
 {
-	FRHIRayTracingGeometry* GeometryRHI = nullptr;
+	// TODO: UE-130819 Ref counting is a temporary workaround for a very rare streaming crash.
+	FRayTracingGeometryRHIRef GeometryRHI = nullptr;
 
 	// A single physical mesh may be duplicated many times in the scene with different transforms and user data.
 	// All copies share the same shader binding table entries and therefore will have the same material and shader resources.
