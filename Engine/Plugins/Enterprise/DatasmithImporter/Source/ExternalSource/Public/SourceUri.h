e845574711e52454b1d8301f7c919ee788ac7307
benoit deschenes <Benoit.Deschenes@xa.epicgames.com>
2021-12-07 14:02:39 -0500

    FSourceUri cleaning up the query constructor.
    #rb none
    
    #ROBOMERGE-AUTHOR: benoit.deschenes
    #ROBOMERGE-SOURCE: CL 18397340 in //UE5/Release-5.0/... via CL 18397352
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v896-18170469)
    
    [CL 18397376 by benoit deschenes in ue5-release-engine-test branch]

--- Engine/Plugins/Enterprise/DatasmithImporter/Source/ExternalSource/Public/SourceUri.h
+++ Engine/Plugins/Enterprise/DatasmithImporter/Source/ExternalSource/Public/SourceUri.h
@@ -31,32 +31,8 @@ namespace UE::DatasmithImporter
 
 		template<typename TContainer>
 		FSourceUri(const FString& InScheme, const FString& InPath, const TContainer& QueryContainer)
-			: Uri(InScheme + GetSchemeDelimiter() + InPath)
-		{
-			static_assert(std::is_same<typename TContainer::ElementType, TPair<FString, FString>>::value, TEXT("Query container must consist of a collection of TPair<FString, FString>."));
-
-			if (QueryContainer.Num() > 0)
-			{
-				TCHAR QueryDelimiter(GetQueryStartCharacter());
-				FString QueryString(1, &QueryDelimiter);
-				for (typename TContainer::TConstIterator It = QueryContainer.CreateConstIterator(); It;)
-				{
-					const TPair<FString, FString>& CurrentPair = *It;
-
-					if (++It)
-					{
-						// We are not on the last pair, add '&' character to split this pair with the next.
-						QueryString += FString::Printf(TEXT("%s%c%s%c"), *CurrentPair.Key, GetQueryPairCharacter(), *CurrentPair.Value, GetQueryAppendPairCharacter());
-					}
-					else
-					{
-						QueryString += FString::Printf(TEXT("%s%c%s"), *CurrentPair.Key, GetQueryPairCharacter(), *CurrentPair.Value);
-					}
-				}
-
-				Uri += QueryString;
-			}
-		}
+			: Uri(InScheme + GetSchemeDelimiter() + InPath + BuildQueryString(QueryContainer))
+		{}
 
 		/**
 		 * Generate a FSourceUri from the given filepath.
@@ -118,6 +94,36 @@ namespace UE::DatasmithImporter
 		}
 
 	private:
+
+		template<typename TContainer>
+		static FString BuildQueryString(const TContainer& QueryContainer)
+		{
+			static_assert(std::is_same<typename TContainer::ElementType, TPair<FString, FString>>::value, TEXT("Query container must consist of a collection of TPair<FString, FString>."));
+
+			if (QueryContainer.Num() > 0)
+			{
+				TCHAR QueryDelimiter(GetQueryStartCharacter());
+				FString QueryString(1, &QueryDelimiter);
+				for (typename TContainer::TConstIterator It = QueryContainer.CreateConstIterator(); It;)
+				{
+					const TPair<FString, FString>& CurrentPair = *It;
+
+					if (++It)
+					{
+						// We are not on the last pair, add '&' character to split this pair with the next.
+						QueryString += FString::Printf(TEXT("%s%c%s%c"), *CurrentPair.Key, GetQueryPairCharacter(), *CurrentPair.Value, GetQueryAppendPairCharacter());
+					}
+					else
+					{
+						QueryString += FString::Printf(TEXT("%s%c%s"), *CurrentPair.Key, GetQueryPairCharacter(), *CurrentPair.Value);
+					}
+				}
+
+				return QueryString;
+			}
+
+			return FString();
+		}
 
 		static const FString& GetSchemeDelimiter();
 
