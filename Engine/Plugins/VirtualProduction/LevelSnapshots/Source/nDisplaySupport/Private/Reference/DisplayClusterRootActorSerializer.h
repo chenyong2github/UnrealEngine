398ec63208028bce50d5602ea4aba535b1f30b90
dominik peacock <dominik@geodesic.games>
2021-12-08 05:33:18 -0500

    Ensure thrown when restoring Level Snapshots that include subobjects
    #jira UE-136739
    #rb none
    
    #ROBOMERGE-AUTHOR: dominik.peacock
    #ROBOMERGE-SOURCE: CL 18404260 in //UE5/Release-5.0/... via CL 18404263
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v896-18170469)
    
    [CL 18404265 by dominik peacock in ue5-release-engine-test branch]

--- Engine/Plugins/VirtualProduction/LevelSnapshots/Source/nDisplaySupport/Private/Reference/DisplayClusterRootActorSerializer.h
+++ Engine/Plugins/VirtualProduction/LevelSnapshots/Source/nDisplaySupport/Private/Reference/DisplayClusterRootActorSerializer.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Helpers/ReferenceSubobjectSerializer.h"
+#include "Interfaces/IPropertyComparer.h"
 
 namespace UE::LevelSnapshots
 {
@@ -12,14 +13,25 @@ namespace UE::LevelSnapshots
 
 namespace UE::LevelSnapshots::nDisplay::Private
 {
-	class FDisplayClusterRootActorSerializer : public TReferenceSubobjectSerializer<FDisplayClusterRootActorSerializer>
+	class FDisplayClusterRootActorSerializer
+		:
+		public TReferenceSubobjectSerializer<FDisplayClusterRootActorSerializer>,
+		public IPropertyComparer
 	{
 		static UClass* GetSupportedClass();
 		static void MarkPropertiesAsExplicitlyUnsupported(ILevelSnapshotsModule& Module);
 
+		const FProperty* OverrideMaterials;
+
 	public:
 
 		static void Register(ILevelSnapshotsModule& Module);
+
+		FDisplayClusterRootActorSerializer();
+
+		//~ Begin IPropertyComparer Interface
+		virtual EPropertyComparison ShouldConsiderPropertyEqual(const FPropertyComparisonParams& Params) const override;
+		//~ End IPropertyComparer Interface
 
 		//~ Begin FDisplayClusterRootActorSerializer Interface
 		UObject* FindSubobject(UObject* Owner) const;
