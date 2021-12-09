e20fcc15de12ff0357452e70071ff0bcefb55ff3
jeremy moore <Jeremy.Moore@epicgames.com>
2021-12-09 14:46:32 -0500

    Optimus: Build fix (removed file from changelist by mistake after preflight!)
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 18419328 in //UE5/Release-5.0/... via CL 18422426
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v897-18405271)
    
    [CL 18422678 by jeremy moore in ue5-release-engine-test branch]

---
+++ Engine/Plugins/Experimental/Optimus/Source/OptimusDeveloper/Private/OptimusMeshDeformer.cpp
@@ -0,0 +1,26 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OptimusMeshDeformer.h"
+
+UMeshDeformerInstance* UOptimusMeshDeformer::CreateInstance(UMeshComponent* InMeshComponent)
+{
+	if (ComputeGraph == nullptr || InMeshComponent == nullptr)
+	{
+		return nullptr;
+	}
+
+	UOptimusMeshDeformerInstance* Instance = NewObject<UOptimusMeshDeformerInstance>();
+	Instance->ComputeGraph = ComputeGraph;
+	Instance->ComputeGraphInstance.CreateDataProviders(ComputeGraph, InMeshComponent);
+	return Instance;
+}
+
+bool UOptimusMeshDeformerInstance::IsActive() const
+{
+	return ComputeGraphInstance.ValidateDataProviders(ComputeGraph);
+}
+
+void UOptimusMeshDeformerInstance::EnqueueWork(FSceneInterface* InScene, EWorkLoad WorkLoadType)
+{
+	ComputeGraphInstance.EnqueueWork(ComputeGraph, InScene);
+}
