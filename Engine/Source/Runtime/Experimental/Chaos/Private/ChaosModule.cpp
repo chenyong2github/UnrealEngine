4dff95424cc555e003e604ff83f3acbdf644ef70
ori cohen <Ori.Cohen@epicgames.com>
2021-12-09 15:49:31 -0500

    Missing file to make SingleThreadedPhysics work with Chaos
    #ROBOMERGE-AUTHOR: ori.cohen
    #ROBOMERGE-SOURCE: CL 18424580 in //UE5/Release-5.0/... via CL 18424587
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v897-18405271)
    
    [CL 18424596 by ori cohen in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/ChaosModule.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/ChaosModule.cpp
@@ -3,13 +3,24 @@
 #include "ChaosModule.h"
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
+#include "Misc/CommandLine.h"
 
 IMPLEMENT_MODULE(FChaosEngineModule, Chaos);
+
+namespace Chaos
+{
+	extern CHAOS_API int GSingleThreadedPhysics;
+}
 
 void FChaosEngineModule::StartupModule()
 {
 	if (FModuleManager::Get().ModuleExists(TEXT("GeometryCore")))
 	{
 		FModuleManager::Get().LoadModule("GeometryCore");
+	}
+
+	if(FParse::Param(FCommandLine::Get(), TEXT("SingleThreadedPhysics")))
+	{
+		Chaos::GSingleThreadedPhysics = 1;
 	}
 }
