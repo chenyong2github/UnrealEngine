5b12cdf3e76064a6cdd8de1b54675d31e83089aa
benoit deschenes <Benoit.Deschenes@xa.epicgames.com>
2021-12-09 14:52:15 -0500

    Fixing DatasmithCSharpFacade compilation on Mac
    #jira none
    #rb JeanMichel.Dignard
    JeanMichel.Dignard
    
    #ROBOMERGE-AUTHOR: benoit.deschenes
    #ROBOMERGE-SOURCE: CL 18420863 in //UE5/Release-5.0/... via CL 18422615
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v897-18405271)
    
    [CL 18422895 by benoit deschenes in ue5-release-engine-test branch]

--- Engine/Source/Developer/Datasmith/DatasmithFacade/Public/DatasmithFacadeUtils.h
+++ Engine/Source/Developer/Datasmith/DatasmithFacade/Public/DatasmithFacadeUtils.h
@@ -3,43 +3,41 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "DatasmithUtils.h"
 
+class FDatasmithUniqueNameProvider;
 
 class DATASMITHFACADE_API FDatasmithFacadeUniqueNameProvider
 {
 public:
+	FDatasmithFacadeUniqueNameProvider();
+	
 	/**
 	 * Generates a unique name
 	 * @param BaseName Name that will be suffixed with an index to be unique
 	 * @return TCHAR* Unique name. Calling "Contains()" with this name will be false. 
 	 *                Pointer is only valid until the next GenerateUniqueName() call.
 	 */
-	const TCHAR* GenerateUniqueName(const TCHAR* BaseName)
-	{
-		CachedGeneratedName = InternalNameProvider.GenerateUniqueName(BaseName);
-		return *CachedGeneratedName;
-	}
+	const TCHAR* GenerateUniqueName(const TCHAR* BaseName);
 
 	/**
 	 * Reserves space in the internal data structures to contain at least the number of name specified.
 	 * @param NumberOfName The number of name to reserve for.
 	 */
-	void Reserve( int32 NumberOfName ) { InternalNameProvider.Reserve(NumberOfName); }
+	void Reserve( int32 NumberOfName );
 
 	/**
 	 * Register a name as known
 	 * @param Name name to register
 	 */
-	void AddExistingName(const TCHAR* Name) { InternalNameProvider.AddExistingName(Name); }
+	void AddExistingName(const TCHAR* Name);
 	
 	/**
 	 * Remove a name from the list of existing name
 	 * @param Name name to unregister
 	 */
-	void RemoveExistingName(const TCHAR* Name) { InternalNameProvider.RemoveExistingName(Name); }
+	void RemoveExistingName(const TCHAR* Name);
 
 private:
-	FDatasmithUniqueNameProvider InternalNameProvider;
+	TUniquePtr<FDatasmithUniqueNameProvider> InternalNameProvider;
 	FString CachedGeneratedName;
 };
