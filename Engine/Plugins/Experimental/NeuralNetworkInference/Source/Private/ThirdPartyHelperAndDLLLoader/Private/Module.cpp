731bf3eb54844c1a9db2cce1d2638383583caec8
gines hidalgo <Gines.Hidalgo@epicgames.com>
2021-12-14 14:33:53 -0500

    DirectML DLL loader checks file exists on hard disk and ensureMsgf. #rb Francisco.Vicente [CODEREVIEW] Francisco.Vicente #rnx
    #ROBOMERGE-AUTHOR: gines.hidalgo
    #ROBOMERGE-SOURCE: CL 18457661 in //UE5/Release-5.0/... via CL 18457676
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v898-18417669)
    
    [CL 18457689 by gines hidalgo in ue5-release-engine-test branch]

--- Engine/Plugins/Experimental/NeuralNetworkInference/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
+++ Engine/Plugins/Experimental/NeuralNetworkInference/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
@@ -3,6 +3,8 @@
 #include "Module.h"
 #include "HAL/PlatformProcess.h"
 #include "Interfaces/IPluginManager.h"
+#include "Misc/Paths.h"
+#include "ThirdPartyHelperAndDLLLoaderUtils.h"
 
 // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 void FThirdPartyHelperAndDLLLoaderModule::StartupModule()
@@ -12,6 +14,14 @@ void FThirdPartyHelperAndDLLLoaderModule::StartupModule()
 	const FString DirectMLRuntimeBinPath = PluginDir / TEXT(PREPROCESSOR_TO_STRING(DIRECTML_PLATFORM_PATH));
 	const FString DirectMLDLLPath = DirectMLRuntimeBinPath / TEXT(PREPROCESSOR_TO_STRING(DIRECTML_DLL_NAME));
 
+	// Sanity check
+	if (!FPaths::FileExists(DirectMLDLLPath))
+	{
+		const FString ErrorMessage = FString::Format(TEXT("DirectML DLL file not found in \"{0}\"."),
+			{ IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*DirectMLDLLPath) });
+		UE_LOG(LogNeuralNetworkInferenceThirdPartyHelperAndDLLLoader, Warning, TEXT("FThirdPartyHelperAndDLLLoaderModule::StartupModule(): %s"), *ErrorMessage);
+		checkf(false, TEXT("%s"), *ErrorMessage);
+	}
 	FPlatformProcess::PushDllDirectory(*DirectMLRuntimeBinPath);
 	DirectMLDLLHandle = FPlatformProcess::GetDllHandle(*DirectMLDLLPath);
 	FPlatformProcess::PopDllDirectory(*DirectMLRuntimeBinPath);
