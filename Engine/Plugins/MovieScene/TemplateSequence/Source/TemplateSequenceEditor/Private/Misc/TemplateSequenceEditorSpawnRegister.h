00da6aefc564ce7d1f3632a0a3784b17c8319b2f
max chen <Max.Chen@epicgames.com>
2022-01-03 20:24:38 -0500

    Template Sequence: Disallow converting a spawnable to a possessable for template sequences. This fixes a crash when trying to convert the bound object in a template sequence to a possessable.
    #rb ludovic.chabant
    #jira UE-137953
    #preflight 61d376676c000c869bfeba0f
    
    #ROBOMERGE-AUTHOR: max.chen
    #ROBOMERGE-SOURCE: CL 18504987 in //UE5/Release-5.0/... via CL 18505000
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v899-18417669)
    
    [CL 18505016 by max chen in ue5-release-engine-test branch]

--- Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Misc/TemplateSequenceEditorSpawnRegister.h
+++ Engine/Plugins/MovieScene/TemplateSequence/Source/TemplateSequenceEditor/Private/Misc/TemplateSequenceEditorSpawnRegister.h
@@ -2,9 +2,13 @@
 
 #pragma once
 
+#include "MovieSceneSpawnable.h"
 #include "Misc/LevelSequenceEditorSpawnRegister.h"
 
 /** Movie scene spawn register that knows how to handle spawning objects (actors) for a template sequence  */
 class FTemplateSequenceEditorSpawnRegister : public FLevelSequenceEditorSpawnRegister
 {
+#if WITH_EDITOR
+	virtual bool CanConvertSpawnableToPossessable(FMovieSceneSpawnable& Spawnable) const override;
+#endif
 };
