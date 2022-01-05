20b6dc00fbf018bb68acafbe05980696e5ef9330
yoan stamant <Yoan.Stamant@epicgames.com>
2022-01-05 15:20:26 -0500

    Fixed missing copyright notice (Issue #130069) #rb trivial #preflight skip
    #ROBOMERGE-AUTHOR: yoan.stamant
    #ROBOMERGE-SOURCE: CL 18523802 in //UE5/Release-5.0/... via CL 18523815
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v899-18417669)
    
    [CL 18523834 by yoan stamant in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Engine/Private/VisualLogger/VisualLoggerFilterVolume.cpp
+++ Engine/Source/Runtime/Engine/Private/VisualLogger/VisualLoggerFilterVolume.cpp
@@ -1,3 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "VisualLogger/VisualLoggerFilterVolume.h"
 
 AVisualLoggerFilterVolume::AVisualLoggerFilterVolume(const FObjectInitializer& ObjectInitializer)
