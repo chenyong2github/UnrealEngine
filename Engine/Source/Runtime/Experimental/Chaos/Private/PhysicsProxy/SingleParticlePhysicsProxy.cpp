f0ad613f2a7704dc55959048368637506aa91c93
zhenglin geng <zhenglin.geng@epicgames.com>
2022-01-10 18:10:54 -0500

    Remove particle collision constraints after changing NonFrequentData #preflight 61dcb93c7849fe59b0dd9933
    #ROBOMERGE-AUTHOR: zhenglin.geng
    #ROBOMERGE-SOURCE: CL 18566607 in //UE5/Release-5.0/... via CL 18566616
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Staging -> Release-Engine-Test) (v899-18417669)
    
    [CL 18566619 by zhenglin geng in ue5-release-engine-test branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/SingleParticlePhysicsProxy.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/SingleParticlePhysicsProxy.cpp
@@ -70,7 +70,9 @@ void PushToPhysicsStateImp(const Chaos::FDirtyPropertiesManager& Manager, Chaos:
 			Handle->SetNonFrequentData(*NewNonFrequentData);
 
 			// geometry may have changed, we need to invalidate the particle so it can be removed from caching structures in the evolution
+			// @todo(chaos): Remove collision constraints only. Invalidate particle may remove joint constraints in constraint graph. If joint constraint is persistent, this may cause issues.
 			Evolution.InvalidateParticle(Handle);
+			Evolution.DestroyParticleCollisionsInAllocator(Handle);
 		}
 
 		auto NewVelocities = bHasKinematicData ? ParticleData.FindVelocities(Manager, DataIdx) : nullptr;
