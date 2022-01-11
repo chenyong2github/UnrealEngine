8165deb2ce5bd8912a0aa3d5e35b17586afa43c3
tiago costa <tiago.costa@epicgames.com>
2022-01-11 17:51:36 -0500

    Remove embree dependency from ResonanceAudio - Deleted geometrical_acoustics folder.
    #preflight 61dde233b97cb668807f2434
    #rb matthew.sorrels
    #jira FORT-428801
    
    #ROBOMERGE-AUTHOR: tiago.costa
    #ROBOMERGE-SOURCE: CL 18578099 in //UE5/Release-5.0/... via CL 18578107 via CL 18578121
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18578131 by tiago costa in ue5-main branch]

--- Engine/Plugins/Runtime/ResonanceAudio/Source/ResonanceAudio/ResonanceAudio.Build.cs
+++ Engine/Plugins/Runtime/ResonanceAudio/Source/ResonanceAudio/ResonanceAudio.Build.cs
@@ -90,59 +90,5 @@ public class ResonanceAudio : ModuleRules
             PrivateDefinitions.Add("PFFFT_SIMD_DISABLE=1");
             PrivateDefinitions.Add("EIGEN_HAS_CXX11_MATH=0");
         }
-
-        // Always use the official version of IntelTBB
-        string IntelTBBLibs = Target.UEThirdPartyBinariesDirectory + "Intel/TBB/";
-        string IntelEmbreeLibs = Target.UEThirdPartyBinariesDirectory + "Intel/Embree/Embree2140";
-
-		//Embree support:
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree2140/Win64/";
-
-            PublicIncludePaths.Add(SDKDir + "include");
-            PublicAdditionalLibraries.Add(SDKDir + "lib/embree.2.14.0.lib");
-            RuntimeDependencies.Add("$(TargetOutputDir)/embree.2.14.0.dll", SDKDir + "lib/embree.2.14.0.dll");
-            RuntimeDependencies.Add("$(TargetOutputDir)/tbb.dll", IntelTBBLibs + "Win64/tbb.dll");
-            RuntimeDependencies.Add("$(TargetOutputDir)/tbbmalloc.dll", IntelTBBLibs + "Win64/tbbmalloc.dll");
-			PublicDefinitions.Add("USE_EMBREE=1");
-			PrivateDefinitions.Add("USE_EMBRE_FOR_RESONANCE=1");
-		}
-        else if (Target.Platform == UnrealTargetPlatform.Mac)
-        {
-			// In platforms that don't support Embree, we implement no-op versions of the functions.
-			string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree2140/";
-			PublicIncludePaths.Add(SDKDir + "include");
-			PrivateDefinitions.Add("USE_EMBRE_FOR_RESONANCE=0");
-			PrivateDefinitions.Add("EMBREE_STATIC_LIB=1");
-		}
-		else if (Target.IsInPlatformGroup(UnrealPlatformGroup.Unix) && Target.Architecture.StartsWith("x86_64")) // no support for arm64 yet
-		{
-			string IncludeDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree2140/Linux/x86_64-unknown-linux-gnu";
-			string SDKDir = Path.Combine(IntelEmbreeLibs, "Linux/x86_64-unknown-linux-gnu/lib");
-
-			PublicIncludePaths.Add(Path.Combine(IncludeDir, "include"));
-			PublicAdditionalLibraries.Add(Path.Combine(SDKDir, "libembree.so"));
-			PublicAdditionalLibraries.Add(Path.Combine(IntelTBBLibs, "Linux/libtbb.so"));
-			// disabled for Linux atm due to a bug in libtbbmalloc on exit
-			// PublicAdditionalLibraries.Add(Path.Combine(IntelTBBLibs, "Linux/libtbbmalloc.so"));
-			RuntimeDependencies.Add(Path.Combine(SDKDir, "libembree.so"));
-			RuntimeDependencies.Add(Path.Combine(SDKDir, "libembree.so.2"));
-			RuntimeDependencies.Add(Path.Combine(IntelTBBLibs, "Linux/libtbb.so"));
-			RuntimeDependencies.Add(Path.Combine(IntelTBBLibs, "Linux/libtbb.so.2"));
-			// disabled for Linux atm due to a bug in libtbbmalloc on exit
-			// RuntimeDependencies.Add(Path.Combine(IntelTBBLibs, "Linux/libtbbmalloc.so"));
-			PublicDefinitions.Add("USE_EMBREE=1");
-			PrivateDefinitions.Add("USE_EMBRE_FOR_RESONANCE=1");
-		}
-		else
-        {
-            // In platforms that don't support Embree, we implement no-op versions of the functions.
-            string SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree2140/";
-            PublicIncludePaths.Add(SDKDir + "include");
-			PrivateDefinitions.Add("USE_EMBRE_FOR_RESONANCE=0");
-			PrivateDefinitions.Add("USE_EMBREE=0");
-			PrivateDefinitions.Add("EMBREE_STATIC_LIB=1");
-        }
     }
 }
