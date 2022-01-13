f5a8ff417cd9e49d3697a6634803aa7fab581d75
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2022-01-13 10:03:28 -0500

    Fix Android build break (A[X++] = B[X] is an undefined operation because the evaluation order of the two X references is not specified)
    [FYI] michael.forot
    #preflight 61dfc20ba63213484e18734b
    
    #ROBOMERGE-AUTHOR: halfdan.ingvarsson
    #ROBOMERGE-SOURCE: CL 18594235 in //UE5/Release-5.0/... via CL 18594249 via CL 18594253
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18596567 by halfdan ingvarsson in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Island/IslandManager.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Island/IslandManager.cpp
@@ -565,8 +565,8 @@ void FPBDIslandManager::SyncIslands(FPBDRigidsSOAs& Particles, const int32 NumCo
 			IslandSolver->GetIslandIndex() = LocalIsland;
 
 			// We update the IslandIndexing to retrieve the graph sparse and persistent index from the dense one.
-			SortedIslands[LocalIsland] = IslandIndex;
-			IslandIndexing[LocalIsland++] = SortedIslands[LocalIsland];
+			IslandIndexing[LocalIsland] = SortedIslands[LocalIsland] = IslandIndex;
+			LocalIsland++;
 
 			// We finally update the solver islands based on the new particles and constraints if
 			// the island is not persistent or not sleeping. 
