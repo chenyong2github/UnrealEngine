29ccf263b49e348988113bd6eeb5f63d52d322c3
chris caulfield <Chris.Caulfield@epicgames.com>
2022-01-14 17:16:46 -0500

    Fix MapsToCook when multiple +-separated maps are provided to the cooker
    #rb jor.kirchoff
    #jira none
    #preflight 61e1ef2b4b4bd12cbee803d9
    
    #ROBOMERGE-AUTHOR: chris.caulfield
    #ROBOMERGE-SOURCE: CL 18624077 in //UE5/Release-5.0/... via CL 18624109 via CL 18624120
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18624165 by chris caulfield in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/ProjectParams.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/ProjectParams.cs
@@ -831,6 +831,7 @@ namespace AutomationTool
 			this.SeparateDebugInfo = GetParamValueIfNotSpecified(Command, SeparateDebugInfo, this.SeparateDebugInfo, "separatedebuginfo");
 			this.MapFile = GetParamValueIfNotSpecified(Command, MapFile, this.MapFile, "mapfile");
 			this.NoCleanStage = GetParamValueIfNotSpecified(Command, NoCleanStage, this.NoCleanStage, "nocleanstage");
+			this.MapToRun = ParseParamValueIfNotSpecified(Command, MapToRun, "map", String.Empty);
 			this.AdditionalServerMapParams = ParseParamValueIfNotSpecified(Command, AdditionalServerMapParams, "AdditionalServerMapParams", String.Empty);
 			this.Foreign = GetParamValueIfNotSpecified(Command, Foreign, this.Foreign, "foreign");
 			this.ForeignCode = GetParamValueIfNotSpecified(Command, ForeignCode, this.ForeignCode, "foreigncode");
@@ -848,17 +849,6 @@ namespace AutomationTool
 
 			this.Deploy = GetParamValueIfNotSpecified(Command, Deploy, this.Deploy, "deploy");
 			this.DeployFolder = ParseParamValueIfNotSpecified(Command, DeployFolder, "deploy", null);
-
-			// if the user specified multiple -map arguments, just use the first one
-			this.MapToRun = ParseParamValueIfNotSpecified(Command, MapToRun, "map", String.Empty);
-			if (!String.IsNullOrEmpty(this.MapToRun))
-			{
-				int DelimiterIndex = this.MapToRun.IndexOf('+');
-				if (DelimiterIndex != -1)
-				{
-					this.MapToRun = this.MapToRun.Remove(DelimiterIndex);
-				}
-			}
 
 			// if the user specified -deploy but no folder, set the default
 			if (this.Deploy && string.IsNullOrEmpty(this.DeployFolder))
@@ -1029,6 +1019,16 @@ namespace AutomationTool
             {
                 this.MapsToCook.Add(this.MapToRun);
             }
+
+			// if the user specified multiple -map arguments, just use the first one
+			if (!String.IsNullOrEmpty(this.MapToRun))
+			{
+				int DelimiterIndex = this.MapToRun.IndexOf('+');
+				if (DelimiterIndex != -1)
+				{
+					this.MapToRun = this.MapToRun.Remove(DelimiterIndex);
+				}
+			}
 
 			if (InMapsToRebuildLightMaps == null)
 			{
