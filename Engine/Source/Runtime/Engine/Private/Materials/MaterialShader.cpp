55f2e93842bee06bae8a989159d67122c56d8a77
arciel rekman <arciel.rekman@epicgames.com>
2022-01-14 00:54:16 -0500

    Convert checkf to ensure as it happens more frequently than I thought.
    #rb Discussed with Kevin
    #jira UE-138906
    
    #ROBOMERGE-AUTHOR: arciel.rekman
    #ROBOMERGE-SOURCE: CL 18614753 via CL 18614755 via CL 18614759 via CL 18614863 via CL 18614870 via CL 18614871
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18614873 by arciel rekman in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialShader.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialShader.cpp
@@ -2819,7 +2819,7 @@ FMaterialShaderMap* FMaterialShaderMap::AcquireFinalizedClone()
 	Clone->bCompiledSuccessfully = bCompiledSuccessfully;
 	Clone->bIsPersistent = bIsPersistent;
 	Clone->AssignCopy(*this);
-	checkf(reinterpret_cast<uint64>(Clone->GetMaterialPath()) > reinterpret_cast<uint64>(Clone->GetContent()), TEXT("String embedded in memory image is located outside of it (image start %p > string %p). Pointer/offset overflow while freezing?"),
+	ensureMsgf(reinterpret_cast<uint64>(Clone->GetMaterialPath()) > reinterpret_cast<uint64>(Clone->GetContent()), TEXT("String embedded in memory image is located outside of it (image start %p > string %p). Pointer/offset overflow while freezing?"),
 		Clone->GetContent(), Clone->GetMaterialPath()
 		);
 #if WITH_EDITOR
