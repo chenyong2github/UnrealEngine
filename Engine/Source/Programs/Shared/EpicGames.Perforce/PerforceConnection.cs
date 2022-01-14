1ea7fefd327f46a321dae37f3bd74a9d7c672867
Ben Marsh <Ben.Marsh@epicgames.com>
2022-01-14 16:17:31 -0500

    Horde: If a call to p4 login doesn't return a proper record, call p4 login -s to get one.
    #preflight none
    
    [CL 18623341 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Perforce/PerforceConnection.cs
+++ Engine/Source/Programs/Shared/EpicGames.Perforce/PerforceConnection.cs
@@ -2353,7 +2353,15 @@ namespace EpicGames.Perforce
 				DiscardPasswordPrompt(Response);
 
 				List<PerforceResponse> ParsedResponses = await Response.ReadResponsesAsync(typeof(LoginRecord), CancellationToken);
-				return new PerforceResponse<LoginRecord>(ParsedResponses.First());
+
+				PerforceResponse FirstResponse = ParsedResponses.First();
+				if (FirstRespone.Info != null)
+				{
+					// Older versions of P4.EXE do not return a login record for succesful login, instread just returning a string. Call p4 login -s to get the login state instead.
+					return await TryGetLoginStateAsync(Connection, CancellationToken);
+				}
+
+				return new PerforceResponse<LoginRecord>(FirstResponse);
 			}
 		}
 
