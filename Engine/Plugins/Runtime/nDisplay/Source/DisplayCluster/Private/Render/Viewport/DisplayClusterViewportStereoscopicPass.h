99f9d38c8ef43a6b585024c5b93563d4c0d31743
vitalii boiko <Vitalii.Boiko@xa.epicgames.com>
2022-01-15 10:09:02 -0500

    nDisplay: fixed stereoscopic behaviour
    #jira none
    #rb Peter.Tarasenko
    #preflight none
    
    #ROBOMERGE-AUTHOR: vitalii.boiko
    #ROBOMERGE-SOURCE: CL 18628249 in //UE5/Release-5.0/... via CL 18628251 via CL 18628253
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18628254 by vitalii boiko in ue5-main branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Viewport/DisplayClusterViewportStereoscopicPass.h
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Render/Viewport/DisplayClusterViewportStereoscopicPass.h
@@ -9,14 +9,18 @@ struct FDisplayClusterViewportStereoscopicPass
 {
 	inline static EStereoscopicPass EncodeStereoscopicPass(const uint32 ContextNum, const uint32 ContextAmmount)
 	{
-		// Monoscopic rendering
-		if (ContextAmmount == 1)
+		// Stereoscopic rendering
+		if (ContextAmmount > 1)
 		{
 			switch (ContextNum)
 			{
 			case 0:
+				// Left eye
+				// PRIMARY implies the view needs its own pass
 				return EStereoscopicPass::eSSP_PRIMARY;
 			case 1:
+				// Right eye
+				// SECONDARY implies the view can be instanced
 				return EStereoscopicPass::eSSP_SECONDARY;
 			default:
 				// now stereo only with 2 context
@@ -25,6 +29,7 @@ struct FDisplayClusterViewportStereoscopicPass
 			}
 		}
 
+		// Monoscopic rendering
 		return EStereoscopicPass::eSSP_PRIMARY;
 	}
 };
