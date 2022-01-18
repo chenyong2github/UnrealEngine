4b1cfeaca64cc4f195d5739005f80e30798ad7f5
Kevin Ortegren <Kevin.Ortegren@epicgames.com>
2022-01-18 09:01:28 -0500

    PR #8494: Fix incorrect compiler used for material baking custom outputs (Contributed by aidinabedi)
    #jira UE-131145
    #fyi sebastien.lussier
    #rb none
    #rnx
    #preflight 61e6c483b56c33b8ece1db6f
    
    [CL 18639252 by Kevin Ortegren in ue5-main branch]

--- Engine/Source/Developer/MaterialBaking/Private/ExportMaterialProxy.h
+++ Engine/Source/Developer/MaterialBaking/Private/ExportMaterialProxy.h
@@ -422,7 +422,7 @@ public:
 				return MaterialInterface->CompileProperty(&ProxyCompiler, MP_ShadingModel);
 			case MP_CustomOutput:
 				 // NOTE: Currently we can assume input index is always 0, which it is for all custom outputs that are registered as material attributes
-				return CompileInputForCustomOutput(Compiler, 0, ForceCast_Exact_Replicate);
+				return CompileInputForCustomOutput(&ProxyCompiler, 0, ForceCast_Exact_Replicate);
 			default:
 				return Compiler->Constant(1.0f);
 			}
