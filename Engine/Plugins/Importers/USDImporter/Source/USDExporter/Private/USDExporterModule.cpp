125e86df48d5c8f1a85658962dfaa850ef508b7e
daniel coelho <daniel.coelho@xa.epicgames.com>
2022-01-18 04:32:35 -0500

    USD: Restrict export LOD range sliders to MAX_MESH_LOD_COUNT-1.
    #jira none
    #rb Anousack.Kitisa, JeanMichel.Dignard
    #preflight 61e56823006b17a68652897a
    
    #ROBOMERGE-AUTHOR: daniel.coelho
    #ROBOMERGE-SOURCE: CL 18633842 in //UE5/Release-5.0/... via CL 18633850 via CL 18633862
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18637390 by daniel coelho in ue5-main branch]

--- Engine/Plugins/Importers/USDImporter/Source/USDExporter/Private/USDExporterModule.cpp
+++ Engine/Plugins/Importers/USDImporter/Source/USDExporter/Private/USDExporterModule.cpp
@@ -3,6 +3,7 @@
 #include "USDExporterModule.h"
 
 #include "LevelExporterUSDOptionsCustomization.h"
+#include "USDAssetOptions.h"
 #include "USDMemory.h"
 
 #include "Modules/ModuleManager.h"
@@ -18,6 +19,21 @@ public:
 		// We intentionally use the same customization for both of these
 		PropertyModule.RegisterCustomClassLayout( TEXT( "LevelExporterUSDOptions" ), FOnGetDetailCustomizationInstance::CreateStatic( &FLevelExporterUSDOptionsCustomization::MakeInstance ) );
 		PropertyModule.RegisterCustomClassLayout( TEXT( "LevelSequenceExporterUSDOptions" ), FOnGetDetailCustomizationInstance::CreateStatic( &FLevelExporterUSDOptionsCustomization::MakeInstance ) );
+
+		// Modify the static mesh LOD range to have the proper define value as the maximum.
+		// We have to do this the hard way here because we can't use the define within the meta tag itself
+		if ( UScriptStruct* ScriptStruct = FUsdMeshAssetOptions::StaticStruct() )
+		{
+			if ( FProperty* LowestMeshLODProperty = ScriptStruct->FindPropertyByName( GET_MEMBER_NAME_CHECKED( FUsdMeshAssetOptions, LowestMeshLOD ) ) )
+			{
+				LowestMeshLODProperty->SetMetaData( TEXT( "ClampMax" ), LexToString( MAX_MESH_LOD_COUNT - 1 ) );
+			}
+
+			if ( FProperty* HighestMeshLODProperty = ScriptStruct->FindPropertyByName( GET_MEMBER_NAME_CHECKED( FUsdMeshAssetOptions, HighestMeshLOD ) ) )
+			{
+				HighestMeshLODProperty->SetMetaData( TEXT( "ClampMax" ), LexToString( MAX_MESH_LOD_COUNT - 1 ) );
+			}
+		}
 	}
 
 	virtual void ShutdownModule() override
