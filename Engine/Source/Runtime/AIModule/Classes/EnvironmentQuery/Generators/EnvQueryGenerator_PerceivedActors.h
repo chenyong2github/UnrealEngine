2fc990cb2e933a3eb73b39be7b30a8f235e3b52f
mieszko zielinski <mieszko.zielinski@epicgames.com>
2022-01-18 00:01:26 -0500

    Added a generic EQS generator collecting actors perceived by Contex (via AIPerceptionSystem)
    #preflight 61e5559f006b17a68650b5b7
    
    #ROBOMERGE-AUTHOR: mieszko.zielinski
    #ROBOMERGE-SOURCE: CL 18632135 in //UE5/Release-5.0/... via CL 18632152 via CL 18632160
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18636694 by mieszko zielinski in ue5-main branch]

---
+++ Engine/Source/Runtime/AIModule/Classes/EnvironmentQuery/Generators/EnvQueryGenerator_PerceivedActors.h
@@ -0,0 +1,40 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+//#include "Templates/SubclassOf.h"
+//#include "GameFramework/Actor.h"
+//#include "EnvironmentQuery/EnvQueryContext.h"
+#include "DataProviders/AIDataProvider.h"
+#include "EnvironmentQuery/EnvQueryGenerator.h"
+#include "Perception/AISense.h"
+#include "EnvQueryGenerator_PerceivedActors.generated.h"
+
+
+/** Gathers actors perceived by context */
+UCLASS(meta = (DisplayName = "Perceived Actors"))
+class AIMODULE_API UEnvQueryGenerator_PerceivedActors : public UEnvQueryGenerator
+{
+	GENERATED_UCLASS_BODY()
+
+	/** If set will be used to filter results */
+	UPROPERTY(EditDefaultsOnly, Category=Generator)
+	TSubclassOf<AActor> ActorClassWhitelist;
+
+	/** Additional distance limit imposed on the items generated. Perception's range limit still applies. */
+	UPROPERTY(EditDefaultsOnly, Category=Generator)
+	FAIDataProviderFloatValue SearchRadius;
+
+	/** The perception listener to use as a source of information */
+	UPROPERTY(EditAnywhere, Category=Generator)
+	TSubclassOf<UEnvQueryContext> ListenerContext;
+
+	/** If set will be used to filter gathered results so that only actors perceived with a given sense are considered */
+	UPROPERTY(EditAnywhere, Category = Generator)
+	TSubclassOf<UAISense> SenseToUse;
+
+	virtual void GenerateItems(FEnvQueryInstance& QueryInstance) const override;
+
+	virtual FText GetDescriptionTitle() const override;
+	virtual FText GetDescriptionDetails() const override;
+};
