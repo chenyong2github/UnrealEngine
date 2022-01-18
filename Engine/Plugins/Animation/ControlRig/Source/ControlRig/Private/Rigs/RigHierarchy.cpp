040fc48b540084f9b143637f892a7e023a4b694c
helge mathee <Helge.Mathee@epicgames.com>
2022-01-18 00:01:48 -0500

    Control Rig: Support undo for edit shape color
    #jira UE-139289
    #rb trivial
    #preflight https://horde.devtools.epicgames.com/job/61e56235904123989a0f779e
    
    #ROBOMERGE-AUTHOR: helge.mathee
    #ROBOMERGE-SOURCE: CL 18632283 in //UE5/Release-5.0/... via CL 18632286 via CL 18632289
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v899-18417669)
    
    [CL 18636697 by helge mathee in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Rigs/RigHierarchy.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Rigs/RigHierarchy.cpp
@@ -2603,6 +2603,11 @@ void URigHierarchy::SetControlSettings(FRigControlElement* InControlElement, FRi
 		return;
 	}
 
+	if(bSetupUndo && !HasAnyFlags(RF_Transient))
+	{
+		Modify();
+	}
+
 	InControlElement->Settings = InSettings;
 	Notify(ERigHierarchyNotification::ControlSettingChanged, InControlElement);
 	
