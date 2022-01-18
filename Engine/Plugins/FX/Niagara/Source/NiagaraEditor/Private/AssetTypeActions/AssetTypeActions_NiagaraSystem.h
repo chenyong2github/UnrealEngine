84c20167c3fc3661540184d0334d0fd2206873bd
michael galetzka <michael.galetzka@epicgames.com>
2022-01-18 18:54:36 -0500

    Make Niagara systems a basic asset type
    #jira none
    #rb stu.mckenna
    #preflight skip
    
    #ROBOMERGE-AUTHOR: michael.galetzka
    #ROBOMERGE-SOURCE: CL 18652042 in //UE5/Release-5.0/... via CL 18652061 via CL 18652091
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v900-18638592)
    
    [CL 18652132 by michael galetzka in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/AssetTypeActions/AssetTypeActions_NiagaraSystem.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/AssetTypeActions/AssetTypeActions_NiagaraSystem.h
@@ -15,5 +15,5 @@ public:
 	virtual FColor GetTypeColor() const override;
 	virtual UClass* GetSupportedClass() const override;
 	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<class IToolkitHost> EditWithinLevelEditor = TSharedPtr<IToolkitHost>()) override;
-	virtual uint32 GetCategories() override { return FNiagaraEditorModule::GetAssetCategory(); }
+	virtual uint32 GetCategories() override { return FNiagaraEditorModule::GetAssetCategory() | EAssetTypeCategories::Basic; }
 };
