e2323e58bac07c3c9de5d6f027e35e5e1c604131
zach brockway <zach.brockway@epicgames.com>
2022-01-19 18:27:28 -0500

    Switchboard: Cross-platform setup script. Unifies Windows and Linux install processes, and lays groundwork for subsequent automation.
    #jira UE-139660
    #rb matt.johnson, jason.walter
    #preflight skip
    
    #ROBOMERGE-AUTHOR: zach.brockway
    #ROBOMERGE-SOURCE: CL 18667060 in //UE5/Release-5.0/... via CL 18667066 via CL 18668152
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v901-18665521)
    
    [CL 18668310 by zach brockway in ue5-main branch]

--- Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard.bat
+++ Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard.bat
@@ -1,13 +1,12 @@
 @echo off
-setlocal
+setlocal ENABLEDELAYEDEXPANSION
 
+set _switchboardDir=%~dp0
 REM pushd and %CD% are used to normalize the relative path to a shorter absolute path.
 pushd "%~dp0..\..\..\..\.."
 set _engineDir=%CD%
 set _enginePythonPlatformDir=%_engineDir%\Binaries\ThirdParty\Python3\Win64
 set _pyVenvDir=%_engineDir%\Extras\ThirdPartyNotUE\SwitchboardThirdParty\Python
-set _cwrsyncDir=%_engineDir%\Extras\ThirdPartyNotUE\cwrsync
-set _sbCwrsyncDir=%_engineDir%\Extras\ThirdPartyNotUE\SwitchboardThirdParty\cwrsync
 popd
 
 call:main
@@ -18,21 +17,14 @@ goto:eof
 ::------------------------------------------------------------------------------
 :main
 
-REM This provides a transition from standalone installations of Python for
-REM Switchboard to the venv-based setup using the engine version of Python.
-if exist "%_pyVenvDir%" (
-    if not exist "%_pyVenvDir%\Scripts" (
-        rd /q /s "%_pyVenvDir%" 2>nul
+if not exist "%_pyVenvDir%\Scripts\pythonw.exe" (
+    echo Performing Switchboard first-time setup (using default path for Python virtual environment^)
+    call "%_enginePythonPlatformDir%\python.exe" "%~dp0\sb_setup.py" install --venv-dir="%_pyVenvDir%"
+    if !ERRORLEVEL! NEQ 0 (
+        echo Installation failed with non-zero exit code^^^!
+        pause
+        exit /B !ERRORLEVEL!
     )
-)
-
-if not exist "%_pyVenvDir%" (
-    call:setup_python_venv
-)
-
-REM We make our own working copy of cwrsync to scope our modifications to etc/fstab.
-if not exist "%_sbCwrsyncDir%\bin\rsync.exe" (
-    robocopy /NP /S "%_cwrsyncDir%" "%_sbCwrsyncDir%" 1>"%_sbCwrsyncDir%\provision.log" 2>&1
 )
 
 call:start_sb
@@ -40,57 +32,11 @@ call:start_sb
 goto:eof
 
 ::------------------------------------------------------------------------------
-:setup_python_venv
-
-1>nul 2>nul (
-    mkdir "%_pyVenvDir%"
-)
-
-1>"%_pyVenvDir%\provision.log" 2>&1 (
-    set prompt=-$s
-    echo on
-    call:setup_python_venv_impl
-    echo off
-)
-
-echo.
-goto:eof
-
-::------------------------------------------------------------------------------
-:setup_python_venv_impl
-
-pushd "%_pyVenvDir%"
-call:echo "Working path; %CD%"
-
-call:echo "1/5 : Setting up Python Virtual Environment"
-call "%_enginePythonPlatformDir%\python.exe" -m venv "%_pyVenvDir%"
-
-call "%_pyVenvDir%\Scripts\activate"
-
-call:echo "2/5 : Installing PySide2"
-python.exe -m pip install -Iv pyside2==5.15.2
-
-call:echo "3/5 : Installing python-osc"
-python.exe -m pip install -Iv python-osc==1.7.4
-
-call:echo "4/5 : Installing requests"
-python.exe -m pip install -Iv requests==2.24.0
-
-call:echo "5/5 : Installing six"
-python.exe -m pip install -Iv six==1.15.0
-
-call deactivate
-
-popd
-goto:eof
-
-::------------------------------------------------------------------------------
-:echo
-1>con echo %~1
-goto:eof
-
-::------------------------------------------------------------------------------
 :start_sb
 
-call "%_pyVenvDir%\Scripts\activate"
-start "Switchboard" /D "%~dp0" pythonw.exe -m switchboard
+set PYTHONPATH=%_switchboardDir%;%PYTHONPATH%
+start "Switchboard" "%_pyVenvDir%\Scripts\pythonw.exe" -m switchboard
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to launch Switchboard^!
+    pause
+)
--- Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard.sh
+++ Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-python -m switchboard
+
+_switchboardDir=$(dirname "$0")
+_engineDir=$(cd "$_switchboardDir/../../../../.."; pwd)
+_enginePythonDir="$_engineDir/Binaries/ThirdParty/Python3/Linux"
+_venvDir="$_engineDir/Extras/ThirdPartyNotUE/SwitchboardThirdParty/Python"
+
+if [ ! -x "$_venvDir/bin/python3" ]; then
+    "$_enginePythonDir/bin/python3" "$_switchboardDir/sb_setup.py" install --venv-dir="$_venvDir"
+    _installResult=$?
+    if [ $_installResult -ne 0 ]; then
+        echo "Installation failed with non-zero exit code!"
+        exit $_installResult
+    fi
+fi
+
+PYTHONPATH="$_switchboardDir:$PYTHONPATH" "$_venvDir/bin/python3" -m switchboard
