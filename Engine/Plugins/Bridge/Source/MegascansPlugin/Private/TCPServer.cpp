a4211ecb64a1fdea001af4c759ae625036b8a38b
ahmed rizwan <ahmed.rizwan@epicgames.quixel.com>
2022-01-19 18:25:48 -0500

    Update Bridge plugin to 2021.0.5 (2 of 3) #preflight 61e885481000e8c59a7fe1d1
    #ROBOMERGE-AUTHOR: ahmed.rizwan
    #ROBOMERGE-SOURCE: CL 18666589 in //UE5/Release-5.0/... via CL 18666608 via CL 18668108
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v901-18665521)
    
    [CL 18668264 by ahmed rizwan in ue5-main branch]

---
+++ Engine/Plugins/Bridge/Source/MegascansPlugin/Private/TCPServer.cpp
@@ -0,0 +1,101 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "TCPServer.h"
+#include <string>
+#include "NetworkMessage.h"
+#include "Async/Async.h"
+#include "Misc/ScopedSlowTask.h"
+#include "UObject/GarbageCollection.h"
+#include "AssetsImportController.h"
+
+
+TQueue<FString> FTCPServer::ImportQueue;
+
+FTCPServer::FTCPServer()
+{
+	FThreadSafeCounter  WorkerCounter;
+	FString ThreadName(FString::Printf(TEXT("MegascansPlugin%i"), WorkerCounter.Increment()));
+	ClientThread = FRunnableThread::Create(this, *ThreadName, 8 * 1024, TPri_Normal);
+
+}
+
+FTCPServer::~FTCPServer()
+{
+
+	Stop();
+
+	if (ClientThread != NULL)
+	{
+		ClientThread->Kill(true);
+		delete ClientThread;
+	}
+}
+
+
+bool FTCPServer::Init()
+{
+	Stopping = false;
+	return true;
+}
+
+
+uint32  FTCPServer::Run()
+{
+	
+	while (!Stopping)
+	{
+		FPlatformProcess::Sleep(0.3f);	
+		
+
+		if (!ImportQueue.IsEmpty())
+		{
+			if (!IsGarbageCollecting() && !GIsSavingPackage)
+			{
+				FString ImportData;
+				ImportQueue.Dequeue(ImportData);
+				
+				
+				AsyncTask(ENamedThreads::GameThread, [this, ImportData]() {
+					FAssetsImportController::Get()->DataReceived(ImportData);
+				});
+
+
+			}
+		}
+		
+	}
+
+	return 0;
+}
+
+
+bool FTCPServer::RecvMessage(FSocket* Socket, uint32 DataSize, FString& Message)
+{
+	check(Socket);
+	FArrayReaderPtr Datagram = MakeShareable(new FArrayReader(true));
+	int32 stuff = 16;
+	Datagram->Init(FMath::Min(DataSize, 65507u), 81920);
+	int32 BytesRead = 0;
+
+	if (Socket->Recv(Datagram->GetData(), Datagram->Num(), BytesRead))
+	{
+
+		char* Data = (char*)Datagram->GetData();
+		Data[BytesRead] = '\0';
+		FString message = UTF8_TO_TCHAR(Data);
+		Message = message;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool FTCPServer::HandleListenerConnectionAccepted(class FSocket* ClientSocket, const FIPv4Endpoint& ClientEndpoint)
+
+{
+	
+	PendingClients.Enqueue(ClientSocket);
+	return true;
+}
+
+
