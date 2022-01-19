b66c8ec221a45b478b11abaf12c883ef11e3d177
marc audy <Marc.Audy@epicgames.com>
2022-01-19 14:30:54 -0500

    - D3D12 RHI resource listener fixes/refactor:      * make sure the RT geometry object is registered on construction to the used resources so it's informed on stream & stream out events      * stream out sends regular event with resource location which contains invalid allocated resource data, and nullptr for resource location is only used when the owning resource will be deleted (used by views to remove back reference - not needed by RT geometry objects because these keep refcounted pointers to the used resources and can never reach the rename event on delete - checked) - Add to do for UE-139474 - validate and optionally recreate the DynamicRayTracingGeometries cached in the FStaticMeshSceneProxy on Stream in/out of the shared index buffer
    #rb Yuiry.ODonnell
    #preflight 61e80626da16597a1f34033b
    
    #ROBOMERGE-OWNER: marc.audy
    #ROBOMERGE-AUTHOR: kenzo.terelst
    #ROBOMERGE-SOURCE: CL 18657323 via CL 18657348 via CL 18657354 via CL 18657357 via CL 18657362 via CL 18662511 via CL 18662545 via CL 18662576
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v900-18638592)
    
    [CL 18662730 by marc audy in ue5-main branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Buffer.cpp
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Buffer.cpp
@@ -466,9 +466,16 @@ void FD3D12Buffer::RenameLDAChain(FD3D12ResourceLocation& NewLocation)
 void FD3D12Buffer::Swap(FD3D12Buffer& Other)
 {
 	check(!LockedData.bLocked && !Other.LockedData.bLocked);
+
 	FRHIBuffer::Swap(Other);
 	FD3D12BaseShaderResource::Swap(Other);
 	FD3D12LinkedAdapterObject<FD3D12Buffer>::Swap(Other);
+
+	check(IsHeadLink());
+	for (FLinkedObjectIterator NextBuffer(this); NextBuffer; ++NextBuffer)
+	{
+		NextBuffer->ResourceRenamed(&NextBuffer->ResourceLocation);
+	}
 }
 
 void FD3D12Buffer::ReleaseUnderlyingResource()
@@ -485,7 +492,7 @@ void FD3D12Buffer::ReleaseUnderlyingResource()
 	{
 		check(!NextBuffer->LockedData.bLocked && NextBuffer->ResourceLocation.IsValid());
 		NextBuffer->ResourceLocation.Clear();
-		NextBuffer->RemoveAllRenameListeners();
+		NextBuffer->ResourceRenamed(&NextBuffer->ResourceLocation);
 	}
 }
 
