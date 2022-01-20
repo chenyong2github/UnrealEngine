f39181887fd33694d632f0c7e8e690ba1e059a66
brandon schaefer <Brandon.Schaefer@epicgames.com>
2022-01-20 16:33:38 -0500

    Fix check to avoid when not supported
    #jira none
    #rb Arne.Schober
    #preflight none
    
    #ROBOMERGE-AUTHOR: brandon.schaefer
    #ROBOMERGE-SOURCE: CL 18681334 in //UE5/Release-5.0/... via CL 18681338 via CL 18681353
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v902-18672795)
    
    [CL 18681596 by brandon schaefer in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Experimental/ConcurrentLinearAllocator.h
+++ Engine/Source/Runtime/Core/Public/Experimental/ConcurrentLinearAllocator.h
@@ -51,13 +51,13 @@ struct FOsAllocator
 
 struct FAlignedAllocator
 {
-	static constexpr bool SupportsAlignment = !(PLATFORM_MAC || PLATFORM_IOS || PLATFORM_TVOS); //TODO: UHT on Mac OS returned false alignment.
+	static constexpr bool SupportsAlignment = !(PLATFORM_MAC || PLATFORM_IOS); //TODO: UHT on Mac OS returned false alignment.
 
 	FORCEINLINE static void* Malloc(SIZE_T Size, uint32 Alignment)
 	{
 #if DO_CHECK
 		void* Ret = AnsiMalloc(Size, Alignment);
-		check(IsAligned(Ret, Alignment));
+		check(!SupportsAlignment || IsAligned(Ret, Alignment));
 		return Ret;
 #else
 		return AnsiMalloc(Size, Alignment);
