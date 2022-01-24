e458bf3e43c4ed5d37f5e4ab9b44b574cda3c0e0
michael ekberg <michael.ekberg@epicgames.com>
2022-01-24 13:11:18 -0500

    Explicitly set the blender system on the FMargin property.
    [REVIEW] [at]Andrew.Rodham, [at]Ludovic.Chabant, [at]Austin.Ellis, [at]Fernando.Pereira, [at]Stephen.Sanders
    #tests Compiled DevelopmentEditor Win64 / Launched PIE instance and opened a Social profile which previously triggered an ensure in UMovieScenePropertyInstantiatorSystem::InitializeBlendPath. Verified no ensure.
    
    #ROBOMERGE-AUTHOR: michael.ekberg
    #ROBOMERGE-SOURCE: CL 18710106 via CL 18710138 via CL 18710160 via CL 18710187 via CL 18710896 via CL 18711190
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v903-18687472)
    
    [CL 18711248 by michael ekberg in ue5-main branch]

--- Engine/Source/Runtime/UMG/Private/Animation/MovieSceneUMGComponentTypes.cpp
+++ Engine/Source/Runtime/UMG/Private/Animation/MovieSceneUMGComponentTypes.cpp
@@ -86,6 +86,7 @@ FMovieSceneUMGComponentTypes::FMovieSceneUMGComponentTypes()
 	.AddComposite(BuiltInComponents->FloatResult[1], &FMargin::Top)
 	.AddComposite(BuiltInComponents->FloatResult[2], &FMargin::Right)
 	.AddComposite(BuiltInComponents->FloatResult[3], &FMargin::Bottom)
+	.SetBlenderSystem<UMovieScenePiecewiseFloatBlenderSystem>()
 	.Commit();
 
 	BuiltInComponents->PropertyRegistry.DefineCompositeProperty(WidgetTransform)
