6af6633967db6f9f920ca905896fda49725113b3
jack cai <jack.cai@epicgames.com>
2022-01-24 13:30:21 -0500

    Control Rig: remove unused debug code
    #jira none
    #rb trivial
    #preflight skip
    
    #ROBOMERGE-AUTHOR: jack.cai
    #ROBOMERGE-SOURCE: CL 18711277 in //UE5/Release-5.0/... via CL 18711332 via CL 18711543
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v903-18687472)
    
    [CL 18711579 by jack cai in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/ControlRig.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/ControlRig.cpp
@@ -2767,8 +2767,6 @@ void UControlRig::SetControlCustomization(const FRigElementKey& InControl, const
 
 void UControlRig::PostInitInstanceIfRequired()
 {
-	static int re_entry = 0;
-	re_entry++;
 	if(GetHierarchy() == nullptr || VM == nullptr)
 	{
 		if(HasAnyFlags(RF_ClassDefaultObject))
@@ -2781,7 +2779,6 @@ void UControlRig::PostInitInstanceIfRequired()
 			PostInitInstance(CDO);
 		}
 	}
-	re_entry--;
 }
 
 void UControlRig::PostInitInstance(UControlRig* InCDO)
