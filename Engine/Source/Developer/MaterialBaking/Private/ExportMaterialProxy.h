3a9f3c9322c4cff61f5cd84382d1d3105dec95ca
kevin ortegren <Kevin.Ortegren@epicgames.com>
2022-01-28 08:38:45 -0500

    PR #8495: Fix baking properties custom data in non-opaque or -masked materials (Contributed by aidinabedi)
    #rnx
    #jira UE-131146
    #rb none
    [FYI] sebastien.lussier
    #preflight 61f3e9246b5aea38e5b61266
    
    #ROBOMERGE-AUTHOR: kevin.ortegren
    #ROBOMERGE-SOURCE: CL 18770256 in //UE5/Release-5.0/... via CL 18770263 via CL 18770380
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v903-18687472)
    
    [CL 18770381 by kevin ortegren in ue5-main branch]

--- Engine/Source/Developer/MaterialBaking/Private/ExportMaterialProxy.h
+++ Engine/Source/Developer/MaterialBaking/Private/ExportMaterialProxy.h
@@ -394,8 +394,6 @@ public:
 			case MP_Anisotropy:
 			case MP_Metallic:
 			case MP_AmbientOcclusion:
-			case MP_CustomData0:
-			case MP_CustomData1:
 				// Only return for Opaque and Masked...
 				if (BlendMode == BLEND_Opaque || BlendMode == BLEND_Masked)
 				{
@@ -405,6 +403,8 @@ public:
 
 			case MP_Opacity:
 			case MP_OpacityMask:
+			case MP_CustomData0:
+			case MP_CustomData1:
 			{
 				return MaterialInterface->CompileProperty(&ProxyCompiler, PropertyToCompile, ForceCast_Exact_Replicate);
 			}
