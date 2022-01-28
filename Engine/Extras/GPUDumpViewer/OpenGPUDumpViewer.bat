6f2f3833663fa362e82e5e8a2509633bbd9003af
guillaume abadie <Guillaume.Abadie@epicgames.com>
2022-01-28 11:19:34 -0500

    Fixes OpenGPUDumpViewer.bat when a parent director have a space
    #rb trivial
    #preflight skip
    #jira none
    
    #ROBOMERGE-AUTHOR: guillaume.abadie
    #ROBOMERGE-SOURCE: CL 18772061 in //UE5/Release-5.0/... via CL 18772082 via CL 18772274
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v903-18687472)
    
    [CL 18772281 by guillaume abadie in ue5-main branch]

--- Engine/Extras/GPUDumpViewer/OpenGPUDumpViewer.bat
+++ Engine/Extras/GPUDumpViewer/OpenGPUDumpViewer.bat
@@ -8,7 +8,7 @@ echo "Opening chrome..."
 
 REM --allow-file-access-from-files allow to load a file from a file:// webpage required for GPUDumpViewer.html to work.
 REM --user-data-dir is required to force chrome to open a new instance so that --allow-file-access-from-files is honored.
-"C:\Program Files\Google\Chrome\Application\chrome.exe" file://%CWD%/GPUDumpViewer.html --allow-file-access-from-files --new-window --incognito --user-data-dir=%CHROME_USER_DATA%
+"C:\Program Files\Google\Chrome\Application\chrome.exe" "file://%CWD%/GPUDumpViewer.html" --allow-file-access-from-files --new-window --incognito --user-data-dir="%CHROME_USER_DATA%"
 
 echo "Closing chrome..."
 
