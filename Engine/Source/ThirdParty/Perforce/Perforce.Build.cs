b143e4f0296877b231f710301cd273d4fe7fb526
luc eygasier <luc.eygasier@epicgames.com>
2022-01-31 11:51:59 -0500

    Updates P4 Client SDK to 2021.2 for Mac
    #rb Sebastien.Lussier
    #jira UE-139017
    #preflight 61f46f38e52a8a4a91ced01f
    
    #changelist validated
    
    #ROBOMERGE-AUTHOR: luc.eygasier
    #ROBOMERGE-SOURCE: CL 18790454 in //UE5/Release-5.0/... via CL 18790764 via CL 18791294
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v908-18788545)
    
    [CL 18791511 by luc eygasier in ue5-main branch]

--- Engine/Source/ThirdParty/Perforce/Perforce.Build.cs
+++ Engine/Source/ThirdParty/Perforce/Perforce.Build.cs
@@ -19,7 +19,7 @@ public class Perforce : ModuleRules
 			string IncludeFolder = Path.Combine(Windows_P4APIPath, "Include", PlatformSubdir, VisualStudioVersionFolder);
 			PublicSystemIncludePaths.Add(IncludeFolder);
 
-			string ConfigPath = (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT) ? "Debug" :"Release";
+			string ConfigPath = (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT) ? "Debug" : "Release";
 			string LibFolder = Path.Combine(Windows_P4APIPath, "Lib", PlatformSubdir, VisualStudioVersionFolder, ConfigPath);
 			PublicAdditionalLibraries.Add(Path.Combine(LibFolder, "libclient.lib"));
 			PublicAdditionalLibraries.Add(Path.Combine(LibFolder, "librpc.lib"));
@@ -35,7 +35,8 @@ public class Perforce : ModuleRules
 
 			if (Target.Platform == UnrealTargetPlatform.Mac)
 			{
-				P4APIPath = Target.UEThirdPartySourceDirectory + "Perforce/p4api-2018.1/";
+				// SDK downloaded at http://ftp.perforce.com/perforce/r21.2/bin.macosx1015x86_64/p4api-openssl1.1.1.tgz
+				P4APIPath = Target.UEThirdPartySourceDirectory + "Perforce/p4api-2021.2/";
 				LibFolder += "mac";
 				IncludeSuffix += "/Mac";
 			}
@@ -54,6 +55,15 @@ public class Perforce : ModuleRules
 			if (Target.Platform != UnrealTargetPlatform.Win64 && Target.Platform != UnrealTargetPlatform.Mac)
 			{
 				PublicAdditionalLibraries.Add(LibPrefix + "libp4sslstub" + LibPostfixAndExt);
+			}
+
+			if (Target.Platform == UnrealTargetPlatform.Mac)
+			{
+				PublicAdditionalLibraries.Add(LibPrefix + "libp4api" + LibPostfixAndExt);
+				PublicAdditionalLibraries.Add(LibPrefix + "libp4script" + LibPostfixAndExt);
+				PublicAdditionalLibraries.Add(LibPrefix + "libp4script_c" + LibPostfixAndExt);
+				PublicAdditionalLibraries.Add(LibPrefix + "libp4script_curl" + LibPostfixAndExt);
+				PublicAdditionalLibraries.Add(LibPrefix + "libp4script_sqlite" + LibPostfixAndExt);
 			}
 
 			PublicAdditionalLibraries.Add(LibPrefix + "librpc" + LibPostfixAndExt);
