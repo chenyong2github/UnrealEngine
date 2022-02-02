9bd11767690fe233717910a34fc8f7f3ae804974
florin pascu <Florin.Pascu@epicgames.com>
2022-02-02 02:46:06 -0500

    Force DXC for NiagaraDataInterface shaders for platforms that support DXC #jira UE-139770 #rb Stu.McKenna #preflight 61f8fda08b4112f7cc8f350a
    #ROBOMERGE-AUTHOR: florin.pascu
    #ROBOMERGE-SOURCE: CL 18806586 in //UE5/Release-5.0/... via CL 18808566 via CL 18822180
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v908-18788545)
    
    [CL 18822492 by florin pascu in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterface.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterface.cpp
@@ -7,6 +7,7 @@
 #include "ShaderParameterUtils.h"
 #include "NiagaraShader.h"
 #include "NiagaraComponent.h"
+#include "ShaderCompilerCore.h"
 
 #define LOCTEXT_NAMESPACE "NiagaraDataInterface"
 
@@ -46,6 +47,15 @@ bool UNiagaraDataInterface::AppendCompileHash(FNiagaraCompileHashVisitor* InVisi
 	return true;
 }
 #endif
+
+void UNiagaraDataInterface::ModifyCompilationEnvironment(EShaderPlatform ShaderPlatform, struct FShaderCompilerEnvironment& OutEnvironment) const
+{
+	if (FDataDrivenShaderPlatformInfo::GetSupportsDxc(ShaderPlatform))
+	{
+		// Always enable DXC to avoid compile errors caused by RWBuffer/Buffer in structs. Example NiagaraDataInterfaceHairStrands.ush struct FDIHairStrandsContext
+		OutEnvironment.CompilerFlags.Add(CFLAG_ForceDXC);
+	}
+}
 
 void UNiagaraDataInterface::GetAssetTagsForContext(const UObject* InAsset, const TArray<const UNiagaraDataInterface*>& InProperties, TMap<FName, uint32>& NumericKeys, TMap<FName, FString>& StringKeys) const
 {
