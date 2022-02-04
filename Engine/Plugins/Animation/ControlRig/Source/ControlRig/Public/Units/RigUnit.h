37b6eb07846677b2af1b9868409b91ab39925820
jack cai <jack.cai@epicgames.com>
2022-02-04 13:00:49 -0500

    Control Rig: Added BlueprintInternalUseOnly="true" to the basel rig unit struct so that all derived rig unit structs don't appear in the pin type drop down menu and create confusion
    #jira UE-141083
    #rb helge.mathee sara.schvartzman ben.hoffman
    #preflight https://horde.devtools.epicgames.com/job/61fc0ed3af01d3caef14ccb1
    
    [CL 18866227 by jack cai in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Public/Units/RigUnit.h
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Public/Units/RigUnit.h
@@ -10,7 +10,7 @@
 struct FRigUnitContext;
 
 /** Base class for all rig units */
-USTRUCT(BlueprintType, meta=(Abstract, NodeColor = "0.1 0.1 0.1"))
+USTRUCT(BlueprintType, meta=(Abstract, NodeColor = "0.1 0.1 0.1", BlueprintInternalUseOnly="true"))
 struct CONTROLRIG_API FRigUnit : public FRigVMStruct
 {
 	GENERATED_BODY()
