12802e96040c51db5bc706e971096324f3c4eba0
laurent delayen <laurent.delayen@epicgames.com>
2022-02-07 11:44:09 -0500

    Exposing Retarget Base Pose editing back in editor after premature deprecation.
    #rb:halfdan.ingvarsson
    #JIRA:MH-5130
    #preflight:
    
    #ROBOMERGE-AUTHOR: laurent.delayen
    #ROBOMERGE-SOURCE: CL 18886552 in //UE5/Release-5.0/... via CL 18886562 via CL 18886683
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v910-18824042)
    
    [CL 18886692 by laurent delayen in ue5-main branch]

--- Engine/Source/Editor/Persona/Private/SRetargetSources.h
+++ Engine/Source/Editor/Persona/Private/SRetargetSources.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Widgets/DeclarativeSyntaxSupport.h"
+#include "Input/Reply.h"
 #include "Widgets/SCompoundWidget.h"
 #include "IPersonaPreviewScene.h"
+	
+#include "SPoseAssetNameWidget.h"
 
 class IEditableSkeleton;
 struct FAssetData;
@@ -22,9 +25,37 @@ public:
 
 private:
 
+	FReply OnViewRetargetBasePose() const;
+	FReply OnModifyPose();
+	FReply OnSaveRetargetBasePose();
+
 	/** The editable skeleton */
 	TWeakPtr<class IEditableSkeleton> EditableSkeletonPtr;
 
 	/** The preview scene  */
 	TWeakPtr<class IPersonaPreviewScene> PreviewScenePtr;
+
+	
+	/** Delegate for undo/redo transaction **/
+	void PostUndo();
+	FText GetToggleRetargetBasePose() const;	
+	void ResetRetargetBasePose();
+	void UseCurrentPose();
+	void ImportPose(const UPoseAsset* PoseAsset, const FName& PoseName);
+
+	TSharedRef<SWidget> OnModifyPoseContextMenu();
+
+	void SetSelectedPose(const FAssetData& InAssetData);
+	FString GetSelectedPose() const;
+	bool ShouldFilterAsset(const FAssetData& InAssetData);
+
+	TSharedPtr<SPoseAssetNameWidget> PoseAssetNameWidget;
+	TWeakObjectPtr<UPoseAsset> SelectedPoseAsset;
+	FString SelectedPoseName;
+
+	void SetPoseName(TSharedPtr<FString> PoseName, ESelectInfo::Type SelectionType);
+	FReply OnImportPose();
+
+	void TurnOnPreviewRetargetBasePose();
+	bool CanImportPose() const;
 };
