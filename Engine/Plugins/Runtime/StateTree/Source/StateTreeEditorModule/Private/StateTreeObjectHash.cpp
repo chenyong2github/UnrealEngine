92f5bd6882d5169c2e6f81a38d59bfdb452380dd
mikko mononen <Mikko.Mononen@epicgames.com>
2022-02-07 09:16:09 -0500

    Fix non-unity build and static analysis errors in StateTree object hash
    #jira UE-141143
    [REVIEW]
    #rb mieszko.zielinski
    #preflight 6200fe3d6f05911039b3542b
    #lockdown julien.marchand
    
    #ROBOMERGE-AUTHOR: mikko.mononen
    #ROBOMERGE-SOURCE: CL 18884586 in //UE5/Release-5.0/... via CL 18884589 via CL 18884708
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v910-18824042)
    
    [CL 18884927 by mikko mononen in ue5-main branch]

--- Engine/Plugins/Runtime/StateTree/Source/StateTreeEditorModule/Private/StateTreeObjectHash.cpp
+++ Engine/Plugins/Runtime/StateTree/Source/StateTreeEditorModule/Private/StateTreeObjectHash.cpp
@@ -1,13 +1,19 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "StateTreeObjectHash.h"
+#include "UObject/UnrealType.h"
 
 #if WITH_EDITORONLY_DATA
 
 bool FStateTreeObjectCRC32::ShouldSkipProperty(const FProperty* InProperty) const
 {
+	if (InProperty == nullptr)
+	{
+		return false;
+	}
+	
 	static const FName ExcludeFromHashName(TEXT("ExcludeFromHash"));
-	const bool bExclude = InProperty != nullptr && InProperty->HasMetaData(ExcludeFromHashName);
+	const bool bExclude = InProperty->HasMetaData(ExcludeFromHashName);
 	
 	return FArchiveObjectCrc32::ShouldSkipProperty(InProperty) || InProperty->HasAllPropertyFlags(CPF_Transient) || bExclude;
 }
