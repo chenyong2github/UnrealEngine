c33d7784b614c1625b8940055c5a9d1db7076d42
krzysztof narkowicz <Krzysztof.Narkowicz@epicgames.com>
2022-02-08 14:10:22 -0500

    Lumen Landscape: * Added SWRT support for holes in Landscape (masked) * Force SWRT back face hits to black to prevent leaking
    #preflight 6202956eb168cac6284cac55
    #jira UE-141781
    
    #ROBOMERGE-AUTHOR: krzysztof.narkowicz
    #ROBOMERGE-SOURCE: CL 18906343 in //UE5/Release-5.0/... via CL 18906559 via CL 18906904
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v915-18905420)
    
    [CL 18906936 by krzysztof narkowicz in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenViewState.h
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenViewState.h
@@ -150,6 +150,4 @@ public:
 BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FLumenCardPassUniformParameters, RENDERER_API)
 	SHADER_PARAMETER_STRUCT(FSceneTextureUniformParameters, SceneTextures)
 	SHADER_PARAMETER_RDG_TEXTURE(Texture2D, EyeAdaptationTexture)
-	SHADER_PARAMETER(float, OverrideDiffuseReflectivity)
-	SHADER_PARAMETER(float, DiffuseReflectivityOverride)
 END_GLOBAL_SHADER_PARAMETER_STRUCT()
