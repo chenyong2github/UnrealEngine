b9a09945faf0ab5459e9c3f54e6bc360d8b23f0f
chris kulla <chris.kulla@epicgames.com>
2022-02-09 19:27:47 -0500

    Fix crashes when using OpenImageDenoise plugin from packaged builds
    The tbb12.dll file was not being copied, leading to crashes when the denoiser was first invoked.
    
    #jira UE-141073
    #rb Juan.Canada
    #lockdown Juan.Canada
    #preflight 620400828e53010a186db039
    
    #ROBOMERGE-AUTHOR: chris.kulla
    #ROBOMERGE-SOURCE: CL 18922666 in //UE5/Release-5.0/... via CL 18927071 via CL 18928804
    #ROBOMERGE-BOT: UE5 (Release-Engine-Test -> Main) (v916-18915374)
    
    [CL 18929243 by chris kulla in ue5-main branch]

--- Engine/Source/ThirdParty/Intel/OIDN/IntelOIDN.Build.cs
+++ Engine/Source/ThirdParty/Intel/OIDN/IntelOIDN.Build.cs
@@ -10,12 +10,17 @@ public class IntelOIDN : ModuleRules
 
         if (Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) && Target.WindowsPlatform.Architecture == WindowsArchitecture.x64)
         {
-            PublicSystemIncludePaths.Add(Target.UEThirdPartySourceDirectory + "Intel/OIDN/include/");
-            PublicSystemLibraryPaths.Add(Target.UEThirdPartySourceDirectory + "Intel/OIDN/lib/");
-            PublicAdditionalLibraries.Add(Target.UEThirdPartySourceDirectory + "Intel/OIDN/lib/OpenImageDenoise.lib");
-			RuntimeDependencies.Add("$(TargetOutputDir)/OpenImageDenoise.dll", Target.UEThirdPartySourceDirectory + "Intel/OIDN/bin/OpenImageDenoise.dll");
-			//@todo - share in common place for Embree
-			//RuntimeDependencies.Add("$(TargetOutputDir)/tbb12.dll", Target.UEThirdPartySourceDirectory + "Intel/OIDN/bin/tbb12.dll");
+			string SDKDir         = Target.UEThirdPartySourceDirectory + "Intel/OIDN/";
+			string Embree3_SDKDir = Target.UEThirdPartySourceDirectory + "Intel/Embree/Embree3122/Win64/";
+
+			PublicSystemIncludePaths.Add(SDKDir + "include/");
+            PublicSystemLibraryPaths.Add(SDKDir + "lib/");
+            PublicAdditionalLibraries.Add(SDKDir + "lib/OpenImageDenoise.lib");
+			RuntimeDependencies.Add("$(TargetOutputDir)/OpenImageDenoise.dll", SDKDir + "bin/OpenImageDenoise.dll");
+
+			//@todo - find a cleaner way to share this library with Embree3
+			//RuntimeDependencies.Add("$(TargetOutputDir)/tbb12.dll", SDKDir + "bin/tbb12.dll");
+			RuntimeDependencies.Add("$(TargetOutputDir)/tbb12.dll", Embree3_SDKDir + "lib/tbb12.dll");
 			PublicDelayLoadDLLs.Add("OpenImageDenoise.dll");
 			PublicDelayLoadDLLs.Add("tbb12.dll");
 			PublicDefinitions.Add("WITH_INTELOIDN=1");
