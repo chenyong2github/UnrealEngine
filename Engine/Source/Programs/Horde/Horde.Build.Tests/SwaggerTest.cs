12d262ae2d064a59638ab5fc47cd7e13d7a89633
carl bystrom <carl.bystrom@epicgames.com>
2022-02-16 13:15:30 -0500

    Horde: Add a test for Swagger generator
    Ensures REST API code/docs can be generated for Swagger. Without this, problem will only manifest itself when calling the Swagger endpoints (via a browser usually).
    
    #preflight none
    
    [CL 19017949 by carl bystrom in ue5-main branch]

---
+++ Engine/Source/Programs/Horde/Horde.Build.Tests/SwaggerTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using HordeServerTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Horde.Build.Tests
+{
+    [TestClass]
+    public class SwaggerTest : ControllerIntegrationTest
+    {
+        [TestMethod]
+        [Ignore("Until clashes with Horde.Build and Horde.Storage are resolved")]
+        public async Task ValidateSwagger()
+        {
+	        var Res = await client.GetAsync("/swagger/v1/swagger.json");
+	        if (!Res.IsSuccessStatusCode)
+	        {
+		        string RawJson = await Res.Content.ReadAsStringAsync();
+		        JsonElement TempElement = JsonSerializer.Deserialize<JsonElement>(RawJson);
+		        string FormattedJson = JsonSerializer.Serialize(TempElement, new JsonSerializerOptions { WriteIndented = true });
+		        Console.Error.WriteLine("Error result:\n" + FormattedJson);
+		        Res.EnsureSuccessStatusCode();
+	        }
+        }
+    }
+}
