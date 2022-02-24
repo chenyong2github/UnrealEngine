67a9b68bfe26b146eb0decc6014803ca106041bd
Dan Thompson <dan.thompson@epicgames.com>
2022-02-24 16:28:17 -0500

    Adding a commandlet to export an asset registry database to a sqlite database. The normal commandlet directory is in UnrealEd which doesn't reference the sqlite plugins, and rather than add those dependencies to UnrealEd, I made a separate plugin. #rb fabian.giesen #rb charles.bloom #preflight 6217f43e7520d8f6ab9cdc3a
    [CL 19125932 by Dan Thompson in ue5-main branch]

---
+++ Engine/Plugins/Editor/AssetRegistryExport/Source/Private/AssetRegistryExportCommandlet.h
@@ -0,0 +1,15 @@
+
+#pragma once
+#include "Commandlets/Commandlet.h"
+#include "AssetRegistryExportCommandlet.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogAssetRegistryExport, Log, All);
+
+UCLASS()
+class UAssetRegistryExportCommandlet : public UCommandlet
+{
+	GENERATED_UCLASS_BODY()
+public:
+	virtual int32 Main(const FString& CmdLineParams) override;
+};
+
