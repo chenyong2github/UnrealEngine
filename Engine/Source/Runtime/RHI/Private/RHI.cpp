43d47da3749a20a4572dae1635b06fde6ea32528
Robert Millar <robert.millar@epicgames.com>
2022-02-24 13:23:27 -0500

    Fix non-unity compile issues with FDebugName.
    #jira UE-143433
    #rb none
    #preflight 6217ca97d71b6cfc9d59ab46
    
    [CL 19118950 by Robert Millar in ue5-main branch]

--- Engine/Source/Runtime/RHI/Private/RHI.cpp
+++ Engine/Source/Runtime/RHI/Private/RHI.cpp
@@ -2824,3 +2824,54 @@ void FRHITransientBuffer::Acquire(const TCHAR* InName, uint32 InPassIndex, uint6
 
 	// TODO: Add method to rename a buffer.
 }
+
+FDebugName::FDebugName()
+	: Name()
+	, Number(NAME_NO_NUMBER_INTERNAL)
+{
+}
+
+FDebugName::FDebugName(FName InName)
+	: Name(InName)
+	, Number(NAME_NO_NUMBER_INTERNAL)
+{
+}
+
+FDebugName::FDebugName(FName InName, int32 InNumber)
+	: Name(InName)
+	, Number(InNumber)
+{
+}
+
+FDebugName::FDebugName(FMemoryImageName InName, int32 InNumber)
+	: Name(InName)
+	, Number(InNumber)
+{
+}
+
+FDebugName& FDebugName::operator=(FName Other)
+{
+	Name = FMemoryImageName(Other);
+	Number = NAME_NO_NUMBER_INTERNAL;
+	return *this;
+}
+
+FString FDebugName::ToString() const
+{
+	FString Out;
+	FName(Name).AppendString(Out);
+	if (Number != NAME_NO_NUMBER_INTERNAL)
+	{
+		Out.Appendf(TEXT("_%u"), Number);
+	}
+	return Out;
+}
+
+void FDebugName::AppendString(FStringBuilderBase& Builder) const
+{
+	FName(Name).AppendString(Builder);
+	if (Number != NAME_NO_NUMBER_INTERNAL)
+	{
+		Builder << '_' << Number;
+	}
+}
