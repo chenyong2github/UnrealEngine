b6fbda72cac702c337f0a2a953ca732df743f380
UnrealBot <unrealbot@epicgames.com>
2022-02-25 15:34:30 +0000

    Branch snapshot for CL 19147307
    [CL 19147307 in ue5-main branch]

--- Engine/Binaries/ThirdParty/DotNet/download.ps1
+++
@@ -1,8 +0,0 @@
-$destFolder=".\Windows"
-if (-not (Test-Path -Path $destFolder -PathType Container))
-{
-    New-Item -Path $destFolder -ItemType "directory"
-}
-
-# Run a separate PowerShell process because the script calls exit, so it will end the current PowerShell session.
-&powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -NoPath -Channel 3.1 -InstallDir $destFolder"
