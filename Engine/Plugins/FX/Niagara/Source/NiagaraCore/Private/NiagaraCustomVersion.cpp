96aa6e6b92c833a6a0726f6e6d1152a953ea01b5
marc audy <Marc.Audy@epicgames.com>
2022-03-03 01:28:03 -0500

    Fix non-unity #rnx #jira
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 19230488 via CL 19238171 via CL 19238252 via CL 19238487
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v921-19075845)
    
    [CL 19239367 by marc audy in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "NiagaraCustomVersion.h"
-#include "Serialization/CustomVersion.h"
+#include "UObject/DevObjectVersion.h"
 
 const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFFA02D32);
 
