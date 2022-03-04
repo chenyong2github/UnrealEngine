f09821047e9831004b17dc052dfe6d28d47dfb5d
nicholas goldstein <nicholas.goldstein@epicgames.com>
2022-03-04 13:05:21 -0500

    Various syntax fixes for cascade niagara converter scripts. Rearrange code standard for max line length: now comply with 120 chars per line.
    #jira UE-143397
    #rb none, content
    #preflight none
    #lockdown cristina.riveron
    
    #ROBOMERGE-AUTHOR: nicholas.goldstein
    #ROBOMERGE-SOURCE: CL 19260983 in //UE5/Release-5.0/... via CL 19261967
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v924-19243027)
    
    [CL 19269197 by nicholas goldstein in ue5-main branch]

--- Engine/Plugins/FX/CascadeToNiagaraConverter/Source/CascadeToNiagaraConverter/Private/CascadeToNiagaraConverterModule.cpp
+++ Engine/Plugins/FX/CascadeToNiagaraConverter/Source/CascadeToNiagaraConverter/Private/CascadeToNiagaraConverterModule.cpp
@@ -28,6 +28,7 @@ void ICascadeToNiagaraConverterModule::StartupModule()
 	MessageManager->RegisterMessageTopic(FNiagaraConverterMessageTopics::VerboseConversionEventTopicName);
 	MessageManager->RegisterMessageTopic(FNiagaraConverterMessageTopics::ConversionEventTopicName);
 	MessageManager->RegisterAdditionalMessageLogTopic(FNiagaraConverterMessageTopics::ConversionEventTopicName);
+	MessageManager->RegisterAdditionalMessageLogTopic(FNiagaraConverterMessageTopics::VerboseConversionEventTopicName);
 }
 
 void ICascadeToNiagaraConverterModule::ShutdownModule()
