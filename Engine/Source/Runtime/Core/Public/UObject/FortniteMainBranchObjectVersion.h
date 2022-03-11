a642e1949409f0efc5b9dcf38988810b5ae617b7
stu mckenna <Stu.McKenna@epicgames.com>
2022-03-11 04:29:28 -0500

    (Editgrate) Fixing memstomp in HLSL translator for custom hlsl node
    [FYI] shaun.kime
    #jira UE-143910
    #rb Stu.McKenna
    #preflight 621931661d1c5017994b099c
    #lockdown marc.audy
    #rnx
    
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 19344223 via CL 19344252 via CL 19346474 via CL 19351334 via CL 19351399
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19351982 by stu mckenna in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/UObject/FortniteMainBranchObjectVersion.h
+++ Engine/Source/Runtime/Core/Public/UObject/FortniteMainBranchObjectVersion.h
@@ -219,7 +219,7 @@ struct CORE_API FFortniteMainBranchObjectVersion
 		SystemGuids.Add(DevGuids.MATERIALSHADERMAP_DERIVEDDATA_VER,  FGuid("1DA85DA5733C4A489A1BE117CDC5ACCC"));
 		SystemGuids.Add(DevGuids.NANITE_DERIVEDDATA_VER,             FGuid("49FBF137DBFB4204B6C9E5B91DA15EBD"));
 		SystemGuids.Add(DevGuids.NIAGARASHADERMAP_DERIVEDDATA_VER,   FGuid("2289D5116CF94BC0AFEAEC541468E645"));
-		SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("A366AB6E36266F4CBB2DB87D6C07A6A6"));
+		SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("68A430A98C770B4BB1331E206470CC26"));
 		SystemGuids.Add(DevGuids.SkeletalMeshDerivedDataVersion,     FGuid("ACF593EAAE354FCBB34CF44F5AA1BFD2"));
 		SystemGuids.Add(DevGuids.STATICMESH_DERIVEDDATA_VER,         FGuid("3006d21f867648a68cf4edee185446b7"));
 
