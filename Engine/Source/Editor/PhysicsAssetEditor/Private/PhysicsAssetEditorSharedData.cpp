e805df95ab6ad2257d6762addb716f549ceb3dde
marc audy <Marc.Audy@epicgames.com>
2022-03-10 22:15:25 -0500

    Fix merge error that duplicated line #rnx
    #ROBOMERGE-AUTHOR: marc.audy
    #ROBOMERGE-SOURCE: CL 19330855 in //UE5/Release-Engine-Staging/...
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19348521 by marc audy in ue5-main branch]

--- Engine/Source/Editor/PhysicsAssetEditor/Private/PhysicsAssetEditorSharedData.cpp
+++ Engine/Source/Editor/PhysicsAssetEditor/Private/PhysicsAssetEditorSharedData.cpp
@@ -2600,7 +2600,6 @@ void FPhysicsAssetEditorSharedData::EnableSimulation(bool bEnableSimulation)
 			EditorSkelComp->ResetAllBodiesSimulatePhysics();
 			EditorSkelComp->SetPhysicsBlendWeight(EditorOptions->PhysicsBlend);
 			PhysicalAnimationComponent->SetSkeletalMeshComponent(EditorSkelComp);
-		PhysicalAnimationComponent->SetSkeletalMeshComponent(EditorSkelComp);
 			// Make it start simulating
 			EditorSkelComp->WakeAllRigidBodies();
 		}
