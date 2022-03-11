bf7a7b71f5d2eb0e3c2025cf2048038fc4cf06c5
max chen <Max.Chen@epicgames.com>
2022-03-11 14:49:28 -0500

    Sequencer: Fix crash in GetSelectedKeyAreas on selection. No need to RemoveAtSwap
    #jira UE-145567
    #preflight 622b82bb0a614dcb0cfef564
    #rb ludovic.chabant
    #lockdown simon.tourangeou
    
    #ROBOMERGE-AUTHOR: max.chen
    #ROBOMERGE-SOURCE: CL 19356942 in //UE5/Release-5.0/... via CL 19358257
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19359439 by max chen in ue5-main branch]

--- Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
+++ Engine/Source/Editor/Sequencer/Private/Sequencer.cpp
@@ -7035,14 +7035,12 @@ void FSequencer::GetSelectedKeyAreas(TArray<const IKeyArea*>& OutSelectedKeyArea
 	{
 
 		TSet<TSharedRef<FSequencerDisplayNode>> ChildNodes;
-		int32 Index = 0;
 		for (TSharedRef<FSequencerDisplayNode> Node : NodesToKey.Array())
 		{
 			// No need to gather key areas from binding/tracks because they have no key areas
 			if (Node->GetType()== ESequencerNode::Object || Node->GetType() == ESequencerNode::Track)
 			{
-				NodesToKey.Array().RemoveAtSwap(Index);
-				++Index;
+				continue;
 			}
 			else
 			{
@@ -7053,7 +7051,6 @@ void FSequencer::GetSelectedKeyAreas(TArray<const IKeyArea*>& OutSelectedKeyArea
 				{
 					NodesToKey.Remove(ChildNode);
 				}
-				++Index;
 			}
 		}
 	}
