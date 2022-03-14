5c4dbfd9d0cd3b01c9a9810f34f8e9b9591897ad
flo schar <Flo.Schar@xa.epicgames.com>
2022-03-14 16:31:30 -0400

    Fix CIS Compile QAVirtualProduction Win64
    #jira UE-145757
    #rb trivial
    #preflight none
    #lockdown Simon.Tourangeau
    #rnx
    
    #ROBOMERGE-AUTHOR: flo.schar
    #ROBOMERGE-SOURCE: CL 19375238 in //UE5/Release-5.0/... via CL 19376319
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19378042 by flo schar in ue5-main branch]

--- Engine/Plugins/VirtualProduction/RemoteControlProtocolDMX/Source/RemoteControlProtocolDMX/Private/RemoteControlProtocolDMXObjectVersion.cpp
+++ Engine/Plugins/VirtualProduction/RemoteControlProtocolDMX/Source/RemoteControlProtocolDMX/Private/RemoteControlProtocolDMXObjectVersion.cpp
@@ -8,4 +8,4 @@
 const FGuid FRemoteControlProtocolDMXObjectVersion::GUID(0x077636E9, 0x44119FD9, 0x26477F9D, 0x0A7E9F6E);
 
 // Register the custom version with core
-FCustomVersionRegistration GRegisterDMXRuntimeCustomVersion(FRemoteControlProtocolDMXObjectVersion::GUID, FRemoteControlProtocolDMXObjectVersion::LatestVersion, TEXT("RemoteControlProtocolDMXObjectVersion"));
+FCustomVersionRegistration GRegisterRemoteControlProtocolDMXCustomVersion(FRemoteControlProtocolDMXObjectVersion::GUID, FRemoteControlProtocolDMXObjectVersion::LatestVersion, TEXT("RemoteControlProtocolDMXObjectVersion"));
