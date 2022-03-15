96b4827c3fb878d8d61991cb0c9d719f5a890d98
Simon Therriault <Simon.Therriault@epicgames.com>
2022-03-15 17:47:11 -0400

    - Delete Webapi plugin until proper preflight can be made
    #rb none
    #fyi george.rolfe
    #jira none
    #preflight 623103c1ac0173aa41245eee
    
    [CL 19396510 by Simon Therriault in ue5-main branch]

--- Engine/Plugins/Web/WebAPI/Source/WebAPI/Private/WebAPIUtilities.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WebAPIUtilities.h"
-
-const FText& UWebAPIUtilities::GetResponseMessage(const FWebAPIMessageResponse& MessageResponse)
-{
-	return MessageResponse.GetMessage();
-}
