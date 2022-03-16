40bf799ab9a9db4b4c8fc33bf262169dec6ff635
ben hoffman <ben.hoffman@epicgames.com>
2022-03-16 12:09:09 -0400

    Add an input component to Editor Utility actor so that you can bind to enhanced input (and other runtime input delegates) in editor utilities outside of PIE. This will make it way easier for virtual production teams to bind input of physical camera equipment to actors in the level. They need these bindings available during the level editor and during PIE, which is why Enhanced Input is being used as it already has the good support for what they want, we just need to expose it in the editor.
    Listening to input is opt in per utility, and would currently only be triggered if you create an associated Input Action that is available to the editor.
    
    #jira UE-144959
    #preflight 6230bb816d2227e2ef3f513b
    #rb Lauren.Barnes
    #rb Patrick.Boutot
    
    [CL 19406938 by ben hoffman in ue5-main branch]

--- Engine/Source/Editor/Blutility/Classes/EditorUtilityActor.h
+++ Engine/Source/Editor/Blutility/Classes/EditorUtilityActor.h
@@ -11,6 +11,7 @@
 #include "UObject/ScriptMacros.h"
 #include "EditorUtilityActor.generated.h"
 
+class UInputComponent;
 
 UCLASS(Abstract, Blueprintable, meta = (ShowWorldContextPin))
 class BLUTILITY_API AEditorUtilityActor : public AActor
@@ -20,4 +21,40 @@ class BLUTILITY_API AEditorUtilityActor : public AActor
 	// Standard function to execute
 	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Editor")
 	void Run();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	/** Returns the current InputComponent on this utility actor. This will be NULL unless bReceivesEditorInput is set to true. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Input|Editor")
+	UInputComponent* GetInputComponent() const
+	{ 
+		return EditorOnlyInputComponent.Get();
+	}
+	
+	UFUNCTION(BlueprintSetter, Category = "Input|Editor")
+	void SetReceivesEditorInput(bool bInValue);
+	
+	UFUNCTION(BlueprintGetter, BlueprintPure, Category = "Input|Editor")
+	bool GetReceivesEditorInput() const
+	{
+		return bReceivesEditorInput;
+	}	
+	
+private:
+
+	/** Creates the EditorOnlyInputComponent if it does not already exist and registers all subobject callbacks to it */
+	void CreateEditorInput();
+
+	/** Removes the EditorOnlyInputComponent from this utility actor */
+	void RemoveEditorInput();
+	
+	UPROPERTY(Transient, DuplicateTransient)
+	TObjectPtr<UInputComponent> EditorOnlyInputComponent;
+	
+	/** If set to true, then this actor will be able to recieve input delegate callbacks when in the editor. */
+	UPROPERTY(EditAnywhere, Category = "Input|Editor", BlueprintSetter = SetReceivesEditorInput, BlueprintGetter = GetReceivesEditorInput)
+	bool bReceivesEditorInput = false;
 };
