5df39e56c4c69f31ce521c67b493dad73bceeec2
juan canada <Juan.Canada@epicgames.com>
2022-03-16 03:48:14 -0400

    Fixed ray tracing could be accidentally enabled when disabled in render settings
    #rb none
    
    #ROBOMERGE-AUTHOR: juan.canada
    #ROBOMERGE-SOURCE: CL 19388081 via CL 19389413 via CL 19391099 via CL 19398966 via CL 19399071
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19403780 by juan canada in ue5-main branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
@@ -1485,13 +1485,13 @@ RENDERCORE_API void RenderUtilsInit()
 				}
 				else
 				{
-					GUseRayTracing = true;
+					//GUseRayTracing = true;
 
-					UE_LOG(LogRendererCore, Log, TEXT("Ray tracing is enabled for the game. Reason: r.RayTracing=1, and r.RayTracing.EnableInGame is not present (default true)."));
+					//UE_LOG(LogRendererCore, Log, TEXT("Ray tracing is enabled for the game. Reason: r.RayTracing=1, and r.RayTracing.EnableInGame is not present (default true)."));
 
-					//GUseRayTracing = false;
+					GUseRayTracing = false;
 
-					//UE_LOG(LogRendererCore, Log, TEXT("Ray tracing is disabled for the game. Reason: r.RayTracing=1, and r.RayTracing.EnableInGame is not present (default false)."));
+					UE_LOG(LogRendererCore, Log, TEXT("Ray tracing is disabled for the game. Reason: r.RayTracing=1, and r.RayTracing.EnableInGame is not present (default false)."));
 				}
 			}
 
