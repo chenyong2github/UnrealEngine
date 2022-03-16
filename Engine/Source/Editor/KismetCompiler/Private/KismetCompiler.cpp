d220f92d1d260b73056fb587bdd25644e61525af
phillip kavan <Phillip.Kavan@epicgames.com>
2022-03-16 12:57:05 -0400

    Fix for an editor crash after duplicating a Blueprint with a missing/unresolved parent class (regression).
    #jira UE-145063
    #rb Jamie.Dale
    #preflight 6231fd4f6e25767a218a368c
    #lockdown Nick.Whiting
    
    #ROBOMERGE-AUTHOR: phillip.kavan
    #ROBOMERGE-SOURCE: CL 19406250 in //UE5/Release-5.0/... via CL 19406927
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v926-19321884)
    
    [CL 19407615 by phillip kavan in ue5-main branch]

--- Engine/Source/Editor/KismetCompiler/Private/KismetCompiler.cpp
+++ Engine/Source/Editor/KismetCompiler/Private/KismetCompiler.cpp
@@ -5044,6 +5044,12 @@ void FKismetCompilerContext::CompileFunctions(EInternalCompilerFlags InternalFla
 
 void FKismetCompilerContext::PostCDOCompiled(const UObject::FPostCDOCompiledContext& Context)
 {
+	// Exit now if we did not create a new class.
+	if (!NewClass)
+	{
+		return;
+	}
+
 	// Notify the CDO that it has finished compiling
 	NewClass->ClassDefaultObject->PostCDOCompiled(Context);
 	if (!Context.bIsSkeletonOnly)
