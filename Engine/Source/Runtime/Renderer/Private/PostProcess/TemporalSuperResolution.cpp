76e0bba09d64481df91bb1164939f15835869486
guillaume abadie <Guillaume.Abadie@epicgames.com>
2022-03-28 16:31:19 -0400

    Uses the first HLSL template
    #rb none
    #preflight 6241edc8f4217035fa5ce91c
    
    #ROBOMERGE-AUTHOR: guillaume.abadie
    #ROBOMERGE-SOURCE: CL 19530715 via CL 19530764 via CL 19530783
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v937-19513599)
    
    [CL 19532828 by guillaume abadie in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalSuperResolution.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalSuperResolution.cpp
@@ -178,6 +178,7 @@ public:
 	{
 		OutEnvironment.CompilerFlags.Add(CFLAG_AllowRealTypes);
 		OutEnvironment.CompilerFlags.Add(CFLAG_WarningsAsErrors);
+		OutEnvironment.CompilerFlags.Add(CFLAG_HLSL2021);
 	}
 }; // class FTemporalSuperResolutionShader
 
