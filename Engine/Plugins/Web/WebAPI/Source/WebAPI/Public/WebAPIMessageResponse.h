e54c69d8116f8276150a8f542f5fc639784042c7
George Rolfe <george.rolfe@geotech.one>
2022-03-29 08:43:59 -0400

    WebAPI re-submit
    #rb simon.therriault
    #jira UETOOL-4463
    #preflight 62424a85c61d8a458f223a59
    
    [CL 19540648 by George Rolfe in ue5-main branch]

---
+++ Engine/Plugins/Web/WebAPI/Source/WebAPI/Public/WebAPIMessageResponse.h
@@ -0,0 +1,19 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WebAPIMessageResponse.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct WEBAPI_API FWebAPIMessageResponse
+{
+	GENERATED_BODY()
+
+	virtual ~FWebAPIMessageResponse() = default;
+
+	virtual const FText& GetMessage() const PURE_VIRTUAL(FWebAPIMessageResponse::GetMessage, return FText::GetEmpty(););
+};
