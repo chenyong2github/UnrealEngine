f812030464463fc64ca36707a70208cf21679ff6
Mateo Egey <Mateo.Egey@epicgames.com>
2022-04-07 07:07:17 -0400

    Added fallback to match pins via name if no pin could be found via persistent guid
    #jira UE-148261
    #rb frank.fella
    #preflight skip
    
    [CL 19666768 by Mateo Egey in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraNode.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraNode.cpp
@@ -218,7 +218,7 @@ bool UNiagaraNode::ReallocatePins(bool bMarkNeedsResynchronizeOnChange)
 
 		// When matching pins, use the pin id if either pin id is valid, otherwise match by name.
 		// we typically don't allow orphaned pins to find a match, unless types match each other again
-		auto PinMatchPredicate = [&](UEdGraphPin* Pin) 
+		auto PinMatchPredicateGuid = [&](UEdGraphPin* Pin) 
 		{
 			// early out conditions for when matching up is not wanted
 			if (Pin->Direction != OldPin->Direction)
@@ -226,13 +226,32 @@ bool UNiagaraNode::ReallocatePins(bool bMarkNeedsResynchronizeOnChange)
 				return false;
 			}
 			
-			bool bPinsHaveIds = Pin->PersistentGuid.IsValid() || OldPin->PersistentGuid.IsValid();
-			bool bAllowPin = OldPin->bOrphanedPin == false || OldPin->PinType == Pin->PinType;
-			return bAllowPin && ((bPinsHaveIds && Pin->PersistentGuid == OldPin->PersistentGuid) || (bPinsHaveIds == false && Pin->PinName == OldPin->PinName));
+			bool bPinsHaveIds = Pin->PersistentGuid.IsValid() && OldPin->PersistentGuid.IsValid();
+			bool bAllowPin = OldPin->bOrphanedPin == false && OldPin->PinType == Pin->PinType;
+			return bAllowPin && (bPinsHaveIds && Pin->PersistentGuid == OldPin->PersistentGuid);
+		};
+
+		auto PinMatchPredicateName = [&](UEdGraphPin* Pin) 
+		{
+			// early out conditions for when matching up is not wanted
+			if (Pin->Direction != OldPin->Direction)
+			{
+				return false;
+			}
+			
+			bool bAllowPin = OldPin->bOrphanedPin == false && OldPin->PinType == Pin->PinType;
+			return bAllowPin && Pin->PinName == OldPin->PinName;
 		};
 
 		OldPin->Modify();
-		if (UEdGraphPin** MatchingNewPin = Pins.FindByPredicate(PinMatchPredicate))
+		UEdGraphPin** MatchingNewPin = Pins.FindByPredicate(PinMatchPredicateGuid);
+
+		if(MatchingNewPin == nullptr)
+		{
+			MatchingNewPin = Pins.FindByPredicate(PinMatchPredicateName);
+		}
+		
+		if (MatchingNewPin)
 		{
 			// If the pin types don't match, CopyPersistentDataFromOldPin could very well overwrite our Matching pin with bad data.
 			// Let's cache it for now and reset it after copying the other relevant data off the old pin.
