dce5251aa1b63a503e71c93f3fecb9b658998b47
Devin Doucette <devin.doucette@epicgames.com>
2022-04-13 12:46:50 -0400

    Fixed missing quotes in GenerateProjectFiles.sh
    #jira UE-148739
    #preflight 6256fba77411b8ac626f5808
    #rb Brandon.Schaefer
    
    [CL 19741549 by Devin Doucette in ue5-main branch]

--- Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
+++ Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
@@ -23,9 +23,9 @@ fi
 source "$BASE_PATH/SetupEnvironment.sh" -dotnet "$BASE_PATH"
 
 # If this is a source drop of the engine make sure that the UnrealBuildTool is up-to-date
-if [ ! -f $BASE_PATH/../../InstalledBuild.txt ]; then
+if [ ! -f "$BASE_PATH/../../InstalledBuild.txt" ]; then
 	if [ -f "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" ]; then
-		dotnet msbuild /restore /target:build /property:Configuration=Development /nologo $BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj /verbosity:quiet
+		dotnet msbuild /restore /target:build /property:Configuration=Development /nologo "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" /verbosity:quiet
 		if [ $? -ne 0 ]; then
 			echo GenerateProjectFiles ERROR: Failed to build UnrealBuildTool
 			exit 1
