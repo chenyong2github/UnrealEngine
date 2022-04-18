fd9c75b71ddfeb643956332584fa2f73bcfdaec5
jeff newquist <jeff.newquist@epicgames.com>
2022-04-18 11:20:16 -0400

    UpdateTexture2D and UpdateFromBufferTexture2D bug fixes - Support SrcX, SrcY offset - Handle texture formats that don't support buffer read; copy to temporary texture instead - Only alias texture descriptor for block compressed formats to avoid breaking metadata buffers that might be bound - Disable tiling of 32_32_32 formats as this is not supported UpdateTextureShader.usf - Removed unused shaders UpdateTexture2DSubresourceCS, CopyTexture2DCS, and TCopyTexture2DCS - TUpdateTexture2DSubresourceCS now supports source offset xy UpdateTextureShaders.cpp,*.h - Removed unused shader binding code - Add TUpdateTexture2DSubresourceCS and FUpdateTexture2DSubresourceCS to support ValueType template variation for float, uint, and sint srv/uav access - Fixed FUpdateTexture3DSubresourceCS name #jira UE-22154 #rb dave.barrett #preflight 6234aefbe14776a6ebd12e4f
    [CL 19785888 by jeff newquist in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/UpdateTextureShaders.cpp
+++ Engine/Source/Runtime/Engine/Private/UpdateTextureShaders.cpp
@@ -2,21 +2,16 @@
 
 #include "UpdateTextureShaders.h"
 
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(, ENGINE_API, FUpdateTexture2DSubresouceCS, SF_Compute);
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(, ENGINE_API, FUpdateTexture3DSubresouceCS, SF_Compute);
+IMPLEMENT_TYPE_LAYOUT(FUpdateTexture2DSubresourceCS);
 
-IMPLEMENT_SHADER_TYPE(,FCopyTexture2DCS,TEXT("/Engine/Private/UpdateTextureShaders.usf"),TEXT("CopyTexture2DCS"),SF_Compute);
+IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(, ENGINE_API, FUpdateTexture3DSubresourceCS, SF_Compute);
 
-IMPLEMENT_SHADER_TYPE(template<>, TCopyTexture2DCS<1u>, TEXT("/Engine/Private/UpdateTextureShaders.usf"), TEXT("TCopyTexture2DCS"), SF_Compute);
-IMPLEMENT_SHADER_TYPE(template<>, TCopyTexture2DCS<2u>, TEXT("/Engine/Private/UpdateTextureShaders.usf"), TEXT("TCopyTexture2DCS"), SF_Compute);
-IMPLEMENT_SHADER_TYPE(template<>, TCopyTexture2DCS<4u>, TEXT("/Engine/Private/UpdateTextureShaders.usf"), TEXT("TCopyTexture2DCS"), SF_Compute);
-typedef TCopyTexture2DCS< 3u, float > FCopyTexture2DCS_Float3;
-IMPLEMENT_SHADER_TYPE(template<>, FCopyTexture2DCS_Float3, TEXT("/Engine/Private/UpdateTextureShaders.usf"), TEXT("TCopyTexture2DCS"), SF_Compute);
-
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TUpdateTexture2DSubresouceCS<1u>, SF_Compute);
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TUpdateTexture2DSubresouceCS<2u>, SF_Compute);
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TUpdateTexture2DSubresouceCS<3u>, SF_Compute);
-IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TUpdateTexture2DSubresouceCS<4u>, SF_Compute);
+#define IMPLEMENT_UPDATE_TEX2D_SUBRESOURCE_SHADER(ValueType)\
+	typedef TUpdateTexture2DSubresourceCS<EUpdateTextureValueType::ValueType> FUpdateTexture2DSubresourceCS_##ValueType;\
+	IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, FUpdateTexture2DSubresourceCS_##ValueType, SF_Compute);
+IMPLEMENT_UPDATE_TEX2D_SUBRESOURCE_SHADER(Float);
+IMPLEMENT_UPDATE_TEX2D_SUBRESOURCE_SHADER(Int32);
+IMPLEMENT_UPDATE_TEX2D_SUBRESOURCE_SHADER(Uint32);
 
 IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TCopyDataCS<2>, SF_Compute);
 IMPLEMENT_SHADER_TYPE4_WITH_TEMPLATE_PREFIX(template<>, ENGINE_API, TCopyDataCS<1>, SF_Compute);
