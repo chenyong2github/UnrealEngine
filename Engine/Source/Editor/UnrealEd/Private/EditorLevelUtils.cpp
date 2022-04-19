cbf4ed5cafff6daccdc7c20f9a6db05707cc21c4
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2022-04-19 16:24:17 -0400

    UEditorLevelUtils::PrivateRemoveLevelFromWorld: Clean up LevelCollection ref to ULevel.
    This was causing a forced Garbage Collection when deleting/unloading a LevelInstance actor in projects with 'gc.PendingKillEnabled = false'
    
    #jira UE-146309
    #preflight 625ebfc5804460ab0f04b524
    #rb jeanfrancois.dube
    
    #ROBOMERGE-AUTHOR: patrick.enfedaque
    #ROBOMERGE-SOURCE: CL 19809556 in //UE5/Release-5.0/... via CL 19814825
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v939-19570697)
    
    [CL 19816481 by patrick enfedaque in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorLevelUtils.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorLevelUtils.cpp
@@ -926,6 +926,11 @@ void UEditorLevelUtils::PrivateRemoveLevelFromWorld(ULevel* InLevel)
 		check(InLevel->bIsVisible == false);
 	}
 
+	if (FLevelCollection* LC = InLevel->GetCachedLevelCollection())
+	{
+		LC->RemoveLevel(InLevel);
+	}
+
 	InLevel->ReleaseRenderingResources();
 
 	IStreamingManager::Get().RemoveLevel(InLevel);
