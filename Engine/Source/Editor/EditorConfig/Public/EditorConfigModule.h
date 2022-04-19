e4651fb422137c2df020b8a19005db2d4f55565e
jason stasik <Jason.Stasik@epicgames.com>
2022-04-19 11:44:04 -0400

    Fix non-unity error #rb none #preflight skip
    #ROBOMERGE-AUTHOR: jason.stasik
    #ROBOMERGE-SOURCE: CL 19798556 via CL 19798564 via CL 19798565
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v939-19570697)
    
    [CL 19809766 by jason stasik in ue5-main branch]

--- Engine/Source/Editor/EditorConfig/Public/EditorConfigModule.h
+++ Engine/Source/Editor/EditorConfig/Public/EditorConfigModule.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "Modules/ModuleInterface.h"
+#include "Logging/LogMacros.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogEditorConfig, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogEditorConfig, Log, All);
 
 class FEditorConfigModule : public IModuleInterface
 {
