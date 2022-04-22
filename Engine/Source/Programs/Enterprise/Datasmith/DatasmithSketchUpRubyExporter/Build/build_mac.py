317cb2b445588ea20fd306f981a35801eb232587
jeanluc corenthin <JeanLuc.Corenthin@epicgames.com>
2022-04-22 19:55:24 -0400

    Fixed issue with wrong Ruby framework required for SketchUp 2022 exporter on Mac Solution: Remove the link directive to use the Ruby framework as it is already done for the SketchUpAPI framework
    #jira UE-144851
    #rb benoit.deschenes
    #preflight 626307f4006fa20b683ba4d4
    
    #ROBOMERGE-AUTHOR: jeanluc.corenthin
    #ROBOMERGE-SOURCE: CL 19872813 in //UE5/Release-5.0/... via CL 19874060
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v940-19807014)
    
    [CL 19878030 by jeanluc corenthin in ue5-main branch]

--- Engine/Source/Programs/Enterprise/Datasmith/DatasmithSketchUpRubyExporter/Build/build_mac.py
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithSketchUpRubyExporter/Build/build_mac.py
@@ -202,7 +202,7 @@ link_cmd.append('-bundle') # link ruby extension as a MacOs 'bundle' not dynamic
 
 #link_cmd += ['-framework', 'Ruby'] # link to RUby framework (found by framework_search_paths)
 
-# Handle SketchUp API
+# Handle not linking against SketchUpAPI and Ruby frameworks. Undefined will be solved at runtime
 link_cmd += [
     '-undefined', 'dynamic_lookup', # We are referencing SketchUp API symbols that are defined in the SketchUp app
     # looks like the following(referencing SketchUp app) is not needed when we have the above(-undefined dynamic_lookup)
