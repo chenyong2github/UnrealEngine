8f9834a32e41e70b9761843b5a32102c591d6da0
matt johnson <matt.johnson@epicgames.com>
2022-04-25 12:35:28 -0400

    OpenVDB: rebuild OpenVDB as a static library
    This avoids issues where if OpenVDB is dynamically linked, objects
    allocated using the instance of the CRT in OpenVDB might be passed
    across a shared library boundary where they could then be attempted to
    be freed using the UE deallocator, leading to crashes.
    
    Note that OpenVDB makes use of both RTTI and C++ exceptions, so
    client modules of OpenVDB should likely enable both of those features
    as well.
    
    #jira UE-139647
    #rb michael.balzer
    #preflight 6263270600654785f43d7008
    #fyi devon.penney
    
    [CL 19904236 by matt johnson in ue5-main branch]

--- Engine/Source/ThirdParty/OpenVDB/OpenVDB.Build.cs
+++ Engine/Source/ThirdParty/OpenVDB/OpenVDB.Build.cs
@@ -7,43 +7,43 @@ using System.IO;
 
 public class OpenVDB : ModuleRules
 {
-    public OpenVDB(ReadOnlyTargetRules Target) : base(Target)
-    {
-	    // We are just setting up paths for pre-compiled binaries.
-	    Type = ModuleType.External;
+	public OpenVDB(ReadOnlyTargetRules Target) : base(Target)
+	{
+		// We are just setting up paths for pre-compiled binaries.
+		Type = ModuleType.External;
+
+		// OpenVDB makes use of both RTTI and C++ exceptions, so clients of
+		// this module should likely enable both of these as well.
+		bUseRTTI = true;
+		bEnableExceptions = true;
 
 		const string versionString = "openvdb-8.1.0";
 		
-		string OpenVdbDir = Path.Combine(Target.UEThirdPartySourceDirectory, "OpenVDB", "Deploy", versionString);
-		string OpenVdbBinDir = Path.Combine(OpenVdbDir, "bin");
+		string OpenVdbDir = Path.Combine(ModuleDirectory, "Deploy", versionString);
 		string OpenVdbIncludeDir = Path.Combine(OpenVdbDir, "include");
 		string OpenVdbLibDir = Path.Combine(OpenVdbDir, "lib");
 
-        PublicDefinitions.Add("NOMINMAX");
-        
-       	PublicIncludePaths.Add(OpenVdbIncludeDir);
-       
-        // Only building for Windows
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-	        const string openVdbWin64LibName = "openvdb.lib";
-	        string OpenVdbWin64LibDir = Path.Combine(OpenVdbLibDir, "Win64", "VS2019");
-	        string OpenVdbWin64Lib = Path.Combine(OpenVdbWin64LibDir, openVdbWin64LibName);
-			
-            PublicAdditionalLibraries.Add(OpenVdbWin64Lib);
+		PublicDefinitions.Add("OPENVDB_STATICLIB");
+		PublicDefinitions.Add("OPENVDB_OPENEXR_STATICLIB");
+		PublicDefinitions.Add("NOMINMAX");
+		
+		PublicIncludePaths.Add(OpenVdbIncludeDir);
 
-            const string openVdbWin64DllName = "openvdb.dll";
-            string OpenVdbWin64BinDir = Path.Combine(OpenVdbBinDir, "Win64", "VS2019");
-            string OpenVdbWin64Dll = Path.Combine(OpenVdbWin64BinDir, openVdbWin64DllName);
-            
-            RuntimeDependencies.Add("$(TargetOutputDir)/" + openVdbWin64DllName, OpenVdbWin64Dll);
-        }
-        else
-        {
-            const string error = "OpenVDB can only be used under Windows 64-bit!";
-            System.Console.WriteLine(error);
-            throw new BuildException(error);
-        }
+		// Only building for Windows
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			const string openVdbWin64LibName = "libopenvdb.lib";
+			string OpenVdbWin64LibDir = Path.Combine(OpenVdbLibDir, "Win64", "VS2019");
+			string OpenVdbWin64Lib = Path.Combine(OpenVdbWin64LibDir, openVdbWin64LibName);
+			
+			PublicAdditionalLibraries.Add(OpenVdbWin64Lib);
+		}
+		else
+		{
+			const string error = "OpenVDB can only be used under Windows 64-bit!";
+			System.Console.WriteLine(error);
+			throw new BuildException(error);
+		}
 
 		PublicDependencyModuleNames.AddRange(
 			new string[]
@@ -61,5 +61,5 @@ public class OpenVDB : ModuleRules
 
 		AddEngineThirdPartyPrivateStaticDependencies(Target, "Blosc");
 		AddEngineThirdPartyPrivateStaticDependencies(Target, "zlib");
-    }
+	}
 }
