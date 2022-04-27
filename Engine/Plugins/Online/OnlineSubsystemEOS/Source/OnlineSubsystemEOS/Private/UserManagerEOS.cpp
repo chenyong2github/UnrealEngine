adeaf9319cce284c74f39552c0d579585f38b551
alejandro aguilar <AlejandroAguilar@themultiplayerguys.com>
2022-04-27 10:54:18 -0400

    Added missing delegate triggers for EOS Connect login
    #jira OI-4208
    #rb Sam.Zamani, Joe.Graf, Chris.Varnsverry
    #preflight 62692295487e11fc1d742fd0
    #lockdown mark.lintott
    
    #ROBOMERGE-AUTHOR: alejandro.aguilar
    #ROBOMERGE-SOURCE: CL 19938190 in //UE5/Release-5.0/... via CL 19938898
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 19939923 by alejandro aguilar in ue5-main branch]

--- Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOS/Private/UserManagerEOS.cpp
+++ Engine/Plugins/Online/OnlineSubsystemEOS/Source/OnlineSubsystemEOS/Private/UserManagerEOS.cpp
@@ -650,7 +650,9 @@ bool FUserManagerEOS::ConnectLoginNoEAS(int32 LocalUserNum)
 		{
 			if (!bWasSuccessful || !AuthToken.IsValid())
 			{
-				UE_LOG_ONLINE(Error, TEXT("ConnectLoginNoEAS(%d) failed due to the platform OSS giving an empty auth token"), LocalUserNum);
+				const FString ErrorString = FString::Printf(TEXT("ConnectLoginNoEAS(%d) failed due to the platform OSS giving an empty auth token"), LocalUserNum);
+				UE_LOG_ONLINE(Warning, TEXT("%s"), *ErrorString);
+				TriggerOnLoginCompleteDelegates(LocalUserNum, false, *FUniqueNetIdEOS::EmptyId(), ErrorString);
 				return;
 			}
 
@@ -685,7 +687,9 @@ bool FUserManagerEOS::ConnectLoginNoEAS(int32 LocalUserNum)
 				}
 				else
 				{
-					UE_LOG_ONLINE(Error, TEXT("ConnectLoginNoEAS(%d) failed with EOS result code (%s)"), LocalUserNum, ANSI_TO_TCHAR(EOS_EResult_ToString(Data->ResultCode)));
+					const FString ErrorString = FString::Printf(TEXT("ConnectLoginNoEAS(%d) failed with EOS result code (%s)"), LocalUserNum, ANSI_TO_TCHAR(EOS_EResult_ToString(Data->ResultCode)));
+					UE_LOG_ONLINE(Warning, TEXT("%s"), *ErrorString);
+					TriggerOnLoginCompleteDelegates(LocalUserNum, false, *FUniqueNetIdEOS::EmptyId(), ErrorString);
 				}
 			};
 			EOS_Connect_Login(EOSSubsystem->ConnectHandle, &Options, CallbackObj, CallbackObj->GetCallbackPtr());
