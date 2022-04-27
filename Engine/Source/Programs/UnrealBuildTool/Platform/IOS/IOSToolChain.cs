afab4fcf55a0d0c0608932ad5ba345697b7549f9
adam kinge <adam.kinge@epicgames.com>
2022-04-27 09:33:43 -0400

    Add -fno-delete-null-pointer-checks to disable clang13 optimization on iOS
    #jira UE-148706
    [REVIEW] [at]jack.porter
    #rb jack.porter
    #preflight 626864202f53f9169a8834d9
    
    #ROBOMERGE-AUTHOR: adam.kinge
    #ROBOMERGE-SOURCE: CL 19937002 in //UE5/Release-5.0/... via CL 19937468
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 19938430 by adam kinge in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 
 using System;
@@ -286,6 +286,9 @@ namespace UnrealBuildTool
 			if (GetClangVersion().Major >= 12)
 			{
 				Result += " -Wno-range-loop-analysis";
+
+				// We have 'this' vs nullptr comparisons that get optimized away for newer versions of Clang, which is undesirable until we refactor these checks.
+				Result += " -fno-delete-null-pointer-checks";
 			}
 
 			if (CompileEnvironment.ShadowVariableWarningLevel != WarningLevel.Off)
