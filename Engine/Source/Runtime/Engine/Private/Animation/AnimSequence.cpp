d52936847028f3690bf7011e616ba1adc531b180
avudnez <antoine.vugliano[at]dont-nod.com>
2022-04-28 18:47:33 -0400

    PR #9081: Avoid animations being always dirtied (Contributed by avudnez) #ushell-cherrypick of 19734073 by UnrealBot #jira UE-150436 #rb Thomas.Sarkanen #preflight 626a664e464ae4f520e49f78
    #ROBOMERGE-AUTHOR: jurre.debaare
    #ROBOMERGE-SOURCE: CL 19957669 in //UE5/Release-5.0/... via CL 19958475
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 19971456 by avudnez in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Animation/AnimSequence.cpp
+++ Engine/Source/Runtime/Engine/Private/Animation/AnimSequence.cpp
@@ -5301,6 +5301,8 @@ void UAnimSequence::OnModelModified(const EAnimDataModelNotifyType& NotifyType, 
 		}
 	};
 
+	bool bShouldMarkPackageDirty = true;
+
 	PRAGMA_DISABLE_DEPRECATION_WARNINGS
 	switch (NotifyType)
 	{
@@ -5432,6 +5434,13 @@ void UAnimSequence::OnModelModified(const EAnimDataModelNotifyType& NotifyType, 
 		{
 			const FCurveRenamedPayload& TypedPayload = Payload.GetPayload<FCurveRenamedPayload>();
 			UpdateCompressedCurveName(TypedPayload.Identifier.InternalName.UID, TypedPayload.NewIdentifier.InternalName);
+			if (TypedPayload.Identifier.InternalName.DisplayName == TypedPayload.NewIdentifier.InternalName.DisplayName
+				&& TypedPayload.Identifier.InternalName.UID == SmartName::MaxUID && TypedPayload.NewIdentifier.InternalName.UID != SmartName::MaxUID)
+			{
+				// We are renaming a Curve with a FSmartName::Invalid UID, this means it was just loaded and we are retrieving
+				// its actual UID from its DisplayName. This should *not* mark the package dirty.
+				bShouldMarkPackageDirty = false;
+			}
 			break;
 		}
 		default:
@@ -5441,8 +5450,11 @@ void UAnimSequence::OnModelModified(const EAnimDataModelNotifyType& NotifyType, 
 		}
 	}
 	PRAGMA_ENABLE_DEPRECATION_WARNINGS
-	
-	MarkPackageDirty();
+
+	if (bShouldMarkPackageDirty)
+	{
+		MarkPackageDirty();
+	}
 }
 
 
