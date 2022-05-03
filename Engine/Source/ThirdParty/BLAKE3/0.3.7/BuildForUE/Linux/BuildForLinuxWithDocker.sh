f71093a29bbb8d180a0b68c089c47018273c4ddf
Devin Doucette <devin.doucette@epicgames.com>
2022-05-03 11:31:44 -0400

    BLAKE3: Deleted v0.3.7
    #preflight 62713f34e16e280be60f96ea
    #rb none
    #rnx
    
    [CL 20026375 by Devin Doucette in ue5-main branch]

--- Engine/Source/ThirdParty/BLAKE3/0.3.7/BuildForUE/Linux/BuildForLinuxWithDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# Copyright Epic Games, Inc. All Rights Reserved.
-
-ROOT_DIR=$(readlink -f "$(dirname "$BASH_SOURCE")/../..")
-
-docker run --rm -t -v "${ROOT_DIR}":/build         centos:8 /build/BuildForUE/Linux/BuildForLinux.sh  x86_64-unknown-linux-gnu     /tmp/build
-docker run --rm -t -v "${ROOT_DIR}":/build arm64v8/centos:8 /build/BuildForUE/Linux/BuildForLinux.sh aarch64-unknown-linux-gnueabi /tmp/build
