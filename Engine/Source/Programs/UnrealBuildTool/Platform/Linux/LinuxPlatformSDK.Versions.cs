7176cb316758ac82317c3b2db72ca2038e3c6aeb
brandon schaefer <Brandon.Schaefer@epicgames.com>
2022-05-04 15:38:15 -0400

    Update to new v20 toolchain
    #jira none
    #rb none
    #preflight none
    
    #ROBOMERGE-AUTHOR: brandon.schaefer
    #ROBOMERGE-SOURCE: CL 20045613 in //UE5/Release-Engine-Staging/...
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 20047133 by brandon schaefer in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Linux/LinuxPlatformSDK.Versions.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Linux/LinuxPlatformSDK.Versions.cs
@@ -8,14 +8,14 @@ namespace UnrealBuildTool
 	{
 		public override string GetMainVersion()
 		{
-			return "v19_clang-11.0.1-centos7";
+			return "v20_clang-13.0.1-centos7";
 		}
 
 		protected override void GetValidVersionRange(out string MinVersion, out string MaxVersion)
 		{
 			// all that matters is the number after the v, according to TryConvertVersionToInt()
 			MinVersion = "v10_clang-5.0.0-centos7";
-			MaxVersion = "v19_clang-11.0.1-centos7";
+			MaxVersion = "v20_clang-13.0.1-centos7";
 		}
 
 		protected override void GetValidSoftwareVersionRange(out string? MinVersion, out string? MaxVersion)
