0164cbb79d638a9392df0ef9213937f170ac077a
Stephen Holmes <Stephen.Holmes@epicgames.com>
2022-05-04 07:13:03 -0400

    GitHub 8649 : Added ReportTouchEvent function for touch sense (AI module)
    #preflight 6262a500bc24759dc735c568
    #review
    #rb @Mieszko.Zielinski
    #8649
    #jira UE-135576
    
    [CL 20039770 by Stephen Holmes in ue5-main branch]

--- Engine/Source/Runtime/AIModule/Classes/Perception/AISense_Touch.h
+++ Engine/Source/Runtime/AIModule/Classes/Perception/AISense_Touch.h
@@ -7,6 +7,7 @@
 #include "Perception/AISense.h"
 #include "AISense_Touch.generated.h"
 
+class IAIPerceptionListenerInterface;
 class UAISense_Touch;
 
 USTRUCT()
@@ -30,6 +31,8 @@ struct AIMODULE_API FAITouchEvent
 		: Location(EventLocation), TouchReceiver(InTouchReceiver), OtherActor(InOtherActor)
 	{
 	}
+
+	IAIPerceptionListenerInterface* GetTouchedActorAsPerceptionListener() const;
 };
 
 UCLASS(ClassGroup=AI)
@@ -42,6 +45,9 @@ class AIMODULE_API UAISense_Touch : public UAISense
 
 public:		
 	void RegisterEvent(const FAITouchEvent& Event);	
+
+	UFUNCTION(BlueprintCallable, Category = "AI|Perception", meta = (WorldContext = "WorldContextObject"))
+	static void ReportTouchEvent(UObject* WorldContextObject, AActor* TouchReceiver, AActor* OtherActor, FVector Location);
 
 protected:
 	virtual float Update() override;
--- Engine/Source/Runtime/AIModule/Private/Perception/AISense_Touch.cpp
+++ Engine/Source/Runtime/AIModule/Private/Perception/AISense_Touch.cpp
@@ -1,8 +1,29 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Perception/AISense_Touch.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/Controller.h"
 #include "Perception/AIPerceptionListenerInterface.h"
 #include "Perception/AIPerceptionComponent.h"
+
+
+IAIPerceptionListenerInterface* FAITouchEvent::GetTouchedActorAsPerceptionListener() const
+{
+	IAIPerceptionListenerInterface* Listener = nullptr;
+	if (TouchReceiver)
+	{
+		Listener = Cast<IAIPerceptionListenerInterface>(TouchReceiver);
+		if (Listener == nullptr)
+		{
+			APawn* ListenerAsPawn = Cast<APawn>(TouchReceiver);
+			if (ListenerAsPawn)
+			{
+				Listener = Cast<IAIPerceptionListenerInterface>(ListenerAsPawn->GetController());
+			}
+		}
+	}
+	return Listener;
+}
 
 UAISense_Touch::UAISense_Touch(const FObjectInitializer& ObjectInitializer) :
 	Super(ObjectInitializer)
@@ -17,7 +38,7 @@ float UAISense_Touch::Update()
 	{
 		if (Event.TouchReceiver != NULL && Event.OtherActor != NULL)
 		{
-			IAIPerceptionListenerInterface* PerceptionListener = Cast<IAIPerceptionListenerInterface>(Event.TouchReceiver);
+			IAIPerceptionListenerInterface* PerceptionListener = Event.GetTouchedActorAsPerceptionListener();
 			if (PerceptionListener != NULL)
 			{
 				UAIPerceptionComponent* PerceptionComponent = PerceptionListener->GetPerceptionComponent();
@@ -45,4 +66,14 @@ void UAISense_Touch::RegisterEvent(const FAITouchEvent& Event)
 	RegisteredEvents.Add(Event);
 
 	RequestImmediateUpdate();
+}
+
+void UAISense_Touch::ReportTouchEvent(UObject* WorldContextObject, AActor* TouchReceiver, AActor* OtherActor, FVector Location)
+{
+	UAIPerceptionSystem* PerceptionSystem = UAIPerceptionSystem::GetCurrent(WorldContextObject);
+	if (PerceptionSystem)
+	{
+		FAITouchEvent Event(TouchReceiver, OtherActor, Location);
+		PerceptionSystem->OnEvent(Event);
+	}
 }
