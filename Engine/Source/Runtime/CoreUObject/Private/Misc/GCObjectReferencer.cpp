70b097884c15541d37ee9f2a682c0b107e682d81
pj kack <PJ.Kack@epicgames.com>
2022-05-04 04:20:42 -0400

    GC: Fix incorrect assert in GCObjectReferencer (fallout from 19898875). Assertion failed: GObjUnhashUnreachableIsInProgress || GObjIncrementalPurgeIsInProgress || !IsGarbageCollecting() [File:.\Runtime/CoreUObject/Private/Misc/GCObjectReferencer.cpp] With the new default lock behavior it should be allowed to add and remove references outside of the reachability analysis scope. With the legacy lock behavior checking IsGarbageCollectingAndLockingUObjectHashTables() means the same thing as IsGarbageCollecting(). When the legacy lock behavior is deleted this check can be simplified to check(!IsGarbageCollectingAndLockingUObjectHashTables()).
    #jira UE-150654
    #rb francis.hurteau, danny.couture, robert.millar
    #rnx
    
    #ROBOMERGE-OWNER: pj.kack
    #ROBOMERGE-AUTHOR: pj.kack
    #ROBOMERGE-COMMAND: _robomerge[bot4] UE5-MAIN
    #ROBOMERGE-SOURCE: CL 20038707 via CL 20038717 via CL 20038721 via CL 20038724 via CL 20038728
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 20038836 by pj kack in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/Misc/GCObjectReferencer.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Misc/GCObjectReferencer.cpp
@@ -59,7 +59,8 @@ void UGCObjectReferencer::AddReferencedObjects(UObject* InThis, FReferenceCollec
 void UGCObjectReferencer::AddObject(FGCObject* Object)
 {
 	check(Object);
-	check(GObjUnhashUnreachableIsInProgress || GObjIncrementalPurgeIsInProgress || !IsGarbageCollecting());	FScopeLock ReferencedObjectsLock(&ReferencedObjectsCritical);
+	check(GObjUnhashUnreachableIsInProgress || GObjIncrementalPurgeIsInProgress || !IsGarbageCollectingAndLockingUObjectHashTables());
+	FScopeLock ReferencedObjectsLock(&ReferencedObjectsCritical);
 	// Make sure there are no duplicates. Should be impossible...
 	checkSlow(!ReferencedObjects.Contains(Object));
 	ReferencedObjects.Add(Object);
@@ -75,7 +76,8 @@ void UGCObjectReferencer::AddObject(FGCObject* Object)
 void UGCObjectReferencer::RemoveObject(FGCObject* Object)
 {
 	check(Object);
-	check(GObjUnhashUnreachableIsInProgress || GObjIncrementalPurgeIsInProgress || !IsGarbageCollecting());	FScopeLock ReferencedObjectsLock(&ReferencedObjectsCritical);
+	check(GObjUnhashUnreachableIsInProgress || GObjIncrementalPurgeIsInProgress || !IsGarbageCollectingAndLockingUObjectHashTables());
+	FScopeLock ReferencedObjectsLock(&ReferencedObjectsCritical);
 	int32 NumRemoved = ReferencedObjects.RemoveSingleSwap(Object);
 	check(NumRemoved == 1);
 #if VERIFY_DISREGARD_GC_ASSUMPTIONS
