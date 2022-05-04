0e18163c1a82c1755bd9bd1e04fe04ec98e87f24
Jimmy Andrews <Jimmy.Andrews@epicgames.com>
2022-05-04 13:28:54 -0400

    fix performance of GeometryCollectionProximityUtility on large examples, especially w/ regular face orientations
    + move implementation details to cpp file and expose proximity tolerance as a parameter
    
    #rb rinat.abdrashitov
    #preflight 6272b1dfe95a8b960ebeb181
    
    [CL 20044329 by Jimmy Andrews in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/GeometryCollection/GeometryCollectionProximityUtility.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/GeometryCollection/GeometryCollectionProximityUtility.h
@@ -2,12 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "Containers/StaticArray.h"
-
 class FGeometryCollection;
-
-
 
 class CHAOS_API FGeometryCollectionProximityUtility
 {
@@ -16,36 +11,10 @@ public:
 
 	void UpdateProximity();
 
-private:
-	void PrepFaceData();
-	void BinFaces();
-	void FindContactingFaces();
-	void ExtendFaceProximityToGeometry();
-
-	void TransformVertices();
-	void GenerateSurfaceNormals();
-	void GenerateFaceToGeometry();
-	int32 FindBestBin(const FVector3f& SurfaceNormal) const;
-	void FindContactPairs(int32 Zenith, int32 Nadir);
-	bool AreNormalsOpposite(const FVector3f& Normal0, const FVector3f& Normal1) const;
-	bool AreFacesCoPlanar(int32 Idx0, int32 Idx1) const;
-	bool DoFacesOverlap(int32 Idx0, int32 Idx1) const;
-	static bool IdenticalTriangles(const TStaticArray<FVector2f, 3>& T0, const TStaticArray<FVector2f, 3>& T1);
-	static bool TrianglesIntersect(const TStaticArray<FVector2f,3>& T0, const TStaticArray<FVector2f,3>& T1);
-	static bool Cross(const TStaticArray<FVector2f,3>& Points, const FVector2f& B, const FVector2f& C, float Normal);
+	// maximum separation to still count as 'contact'
+	float ProximityThreshold = 0.01f;
 
 private:
 	FGeometryCollection* Collection;
-
-	int32 NumFaces;
-	TArray<FVector3f> SurfaceNormals;
-	TArray<int32> FaceToGeometry;
-	TArray<FVector3f> TransformedVertices;
-
-	constexpr static int32 NumBins = 20;
-	TArray<TArray<int32>> Bins;
-	TArray<FVector3f> BinNormals;
-
-	TArray<TArray<int32>> FaceContacts;
 };
 
