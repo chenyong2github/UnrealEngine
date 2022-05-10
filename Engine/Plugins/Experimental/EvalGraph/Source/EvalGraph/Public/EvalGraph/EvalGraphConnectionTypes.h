fa75dcab50aaa93278cbdbedacaecbe1a04b6810
Brice Criswell <Brice.Criswell@epicgames.com>
2022-05-10 19:38:24 -0400

    EvalGraph - BaseTypes #rb trivial #fyi Cedric.Caillaud #preflight 627af5d90a5817c9d91900e2
    [CL 20132097 by Brice Criswell in ue5-main branch]

--- Engine/Plugins/Experimental/EvalGraph/Source/EvalGraph/Public/EvalGraph/EvalGraphConnectionTypes.h
+++ Engine/Plugins/Experimental/EvalGraph/Source/EvalGraph/Public/EvalGraph/EvalGraphConnectionTypes.h
@@ -2,8 +2,60 @@
 #pragma once
 #include "GeometryCollection/ManagedArrayCollection.h"
 
+#include "Math/Box.h"
+#include "Math/Box2D.h"
+#include "Math/Color.h"
+#include "Math/IntVector.h"
+#include "Math/Matrix.h"
+#include "Math/TransformCalculus2D.h"
+#include "Math/OrientedBox.h"
+#include "Math/OrthoMatrix.h"
+#include "Math/Plane.h"
+#include "Math/Quat.h"
+#include "Math/TransformCalculus2D.h"
+#include "Math/QuatRotationTranslationMatrix.h"
+#include "Math/Sphere.h"
+#include "Math/TransformNonVectorized.h"
+#include "Math/TransformCalculus2D.h"
+#include "Math/TranslationMatrix.h"
+#include "Math/Vector.h"
+#include "Math/Vector2D.h"
+#include "Math/Vector4.h"
+#include "Math/Matrix.h"
+#include "Math/RotationMatrix.h"
+
+#include "Chaos/ImplicitObject.h"
+#include "Chaos/BVHParticles.h"
+#include "Chaos/ParticleHandle.h"
+#include "Chaos/ParticleHandleFwd.h"
+#include "Chaos/Convex.h"
+
+
 namespace Eg
 {
+	typedef TSharedPtr<FManagedArrayCollection, ESPMode::ThreadSafe> FManagedArrayCollectionSharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<double>, ESPMode::ThreadSafe> FManagedDoubleArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<float>, ESPMode::ThreadSafe> FManagedFloatArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<int32>, ESPMode::ThreadSafe> FManagedInt32ArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<bool>, ESPMode::ThreadSafe> FManagedBoolArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FVector3d>, ESPMode::ThreadSafe> FManagedVector3dArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FVector3f>, ESPMode::ThreadSafe> FManagedVector3fArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FTransform>, ESPMode::ThreadSafe> FManagedTransformArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FIntVector>, ESPMode::ThreadSafe> FManagedIntVectorArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FQuat4f>, ESPMode::ThreadSafe> FManagedQuat4fArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FVector2f>, ESPMode::ThreadSafe> FManagedVector2fArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FLinearColor>, ESPMode::ThreadSafe> FManagedLinearColorArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FString>, ESPMode::ThreadSafe> FManagedStringArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FBox>, ESPMode::ThreadSafe> FManagedBoxArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<TSet<int32>>, ESPMode::ThreadSafe> FManagedIntSetArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<FIntVector4>, ESPMode::ThreadSafe> FManagedIntVector4ArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<TArray<FVector2f>>, ESPMode::ThreadSafe> FManagedVector2fAArraySharedSafePtr;
+	typedef TSharedPtr<TManagedArrayBase<TArray<FVector3f>*>, ESPMode::ThreadSafe> FManagedVector3fAPtrArraySharedSafePtr;
+
+
+	typedef Chaos::TSerializablePtr<Chaos::FImplicitObject3> FImplicitObject3SerializablePtr;
+	typedef TSharedPtr<Chaos::FImplicitObject, ESPMode::ThreadSafe> FImplicitObjectSharedSafePtr;
+
 	// ---------------------------------------------------------
 	//
 	// General purpose EManagedArrayType definition. 
