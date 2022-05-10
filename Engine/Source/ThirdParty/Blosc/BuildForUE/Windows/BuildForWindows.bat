b2eea0f448ab307d1cbd1fd5bc9e2914b5593331
matt johnson <matt.johnson@epicgames.com>
2022-05-10 14:43:11 -0400

    Blosc: update build script and add build products for Blosc version 1.21.0
    Note that because the engine already embeds its own version(s) of LZ4
    which is bundled with Blosc, we apply a patch to the Blosc source that
    prepends a prefix to all of the non-static and non-inlined functions in Blosc's
    embedded version of LZ4. This ensures that the LZ4 symbols introduced by
    Blosc do not collide with those in the engine when building monolithically.
    
    A subsequent change will activate this new version of Blosc and include a
    rebuild of OpenVDB which uses it, followed by another change that removes
    the old build products for version 1.5.0.
    
    #jira UE-139647
    #rb josh.adams
    #preflight 627978609c764d1f1e7c11c2
    
    [CL 20127234 by matt johnson in ue5-main branch]

--- Engine/Source/ThirdParty/Blosc/BuildForUE/Windows/BuildForWindows.bat
+++ Engine/Source/ThirdParty/Blosc/BuildForUE/Windows/BuildForWindows.bat
@@ -1,97 +1,109 @@
-@ECHO OFF
+@echo off
+setlocal
 
 REM Copyright Epic Games, Inc. All Rights Reserved.
-
-setlocal EnableDelayedExpansion EnableExtensions
 
 if [%1]==[] goto usage
 
 set BLOSC_VERSION=%1
-set BLOSC_VERSION_FILENAME=v%BLOSC_VERSION%
 
-set BLOSC_URL=https://github.com/Blosc/c-blosc/archive/%BLOSC_VERSION_FILENAME%.zip
-set BLOSC_INTERMEDIATE_PATH=..\..\..\..\..\Intermediate\ThirdParty\c-blosc
-set BLOSC_VERSION_INTERMEDIATE_PATH=%BLOSC_INTERMEDIATE_PATH%\c-blosc-%BLOSC_VERSION%
+rem Set as VS2015 for backwards compatibility even though VS2019 is used
+rem when building.
+set COMPILER_VERSION_NAME=VS2015
+set ARCH_NAME=x64
 
-set PATH_SCRIPT=%~dp0
-set BLOSC_INSTALL_PATH=%PATH_SCRIPT%..\..\Deploy\c-blosc-%BLOSC_VERSION%
-set BLOSC_INSTALL_BIN_PATH=%BLOSC_INSTALL_PATH%\bin\Win64\VS2019
-set BLOSC_INSTALL_INCLUDE_PATH=%BLOSC_INSTALL_PATH%\include
-set BLOSC_INSTALL_LIB_PATH=%BLOSC_INSTALL_PATH%\lib\Win64\VS2019
-set PATH_TO_THIRDPARTY=%PATH_SCRIPT%..\..\..
+set BUILD_SCRIPT_LOCATION=%~dp0
+set UE_THIRD_PARTY_LOCATION=%BUILD_SCRIPT_LOCATION%..\..\..
 
-REM Clean install folder
-if exist %BLOSC_INSTALL_PATH% (rmdir %BLOSC_INSTALL_PATH% /s/q)
+set ZLIB_LOCATION=%UE_THIRD_PARTY_LOCATION%\zlib\v1.2.8
+set ZLIB_INCLUDE_LOCATION=%ZLIB_LOCATION%\include\Win64\%COMPILER_VERSION_NAME%
+set ZLIB_LIB_LOCATION=%ZLIB_LOCATION%\lib\Win64\%COMPILER_VERSION_NAME%\Release\zlibstatic.lib
 
-REM Clean intermediate folder
-if exist %BLOSC_INTERMEDIATE_PATH% (rmdir %BLOSC_INTERMEDIATE_PATH% /s/q)
+set UE_MODULE_LOCATION=%BUILD_SCRIPT_LOCATION%..\..
 
-mkdir %BLOSC_INTERMEDIATE_PATH%
-pushd %BLOSC_INTERMEDIATE_PATH%
+set SOURCE_LOCATION=%UE_MODULE_LOCATION%\c-blosc-%BLOSC_VERSION%
 
-echo Downloading %BLOSC_URL%...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%BLOSC_URL%', '%BLOSC_VERSION_FILENAME%.zip')"
+set BUILD_LOCATION=%UE_MODULE_LOCATION%\Intermediate
 
-echo Extracting %BLOSC_VERSION_FILENAME%.zip...
-powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%BLOSC_VERSION_FILENAME%.zip', '.')"
+set INSTALL_LOCATION=%UE_MODULE_LOCATION%\Deploy\c-blosc-%BLOSC_VERSION%
+set INSTALL_INCLUDE_LOCATION=%INSTALL_LOCATION%\include
+set INSTALL_WIN_LOCATION=%INSTALL_LOCATION%\%COMPILER_VERSION_NAME%
 
-pushd c-blosc-%BLOSC_VERSION%
+if exist %BUILD_LOCATION% (
+    rmdir %BUILD_LOCATION% /S /Q)
+if exist %INSTALL_INCLUDE_LOCATION% (
+    rmdir %INSTALL_INCLUDE_LOCATION% /S /Q)
+if exist %INSTALL_WIN_LOCATION% (
+    rmdir %INSTALL_WIN_LOCATION% /S /Q)
 
-echo Building Blosc %BLOSC_VERSION_FILENAME%...
+mkdir %BUILD_LOCATION%
+pushd %BUILD_LOCATION%
 
-REM Dependency - ZLib
-set PATH_TO_ZLIB=%PATH_TO_THIRDPARTY%\zlib\v1.2.8
-set PATH_TO_ZLIB_SRC=%PATH_TO_ZLIB%\include\Win64\VS2015
-set PATH_TO_ZLIB_LIB=%PATH_TO_ZLIB%\lib\Win64\VS2015\Release
-set ZLIB_LIBRARY=%PATH_TO_ZLIB_LIB%\zlibstatic.lib
+rem Note that we patch the source for the version of LZ4 that is bundled with
+rem Blosc to add a prefix to all of its functions. This ensures that the symbol
+rem names do not collide with the version(s) of LZ4 that are embedded in the
+rem engine.
 
-REM Temporary build directories (used as working directories when running CMake)
-set BLOSC_BUILD_PATH=build_win64_VS2019
+rem Copy the source into the build directory so that we can apply patches.
+set BUILD_SOURCE_LOCATION=%BUILD_LOCATION%\c-blosc-%BLOSC_VERSION%
 
-REM Build for VS2019 (64-bit)
-echo Generating Blosc solution for VS2019 (64-bit)...
-mkdir %BLOSC_BUILD_PATH%
-pushd %BLOSC_BUILD_PATH%
-cmake -G "Visual Studio 16 2019" -A x64^
-    -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF ..^
-    -DCMAKE_INSTALL_PREFIX=%BLOSC_INSTALL_PATH%^
-    -DCMAKE_PREFIX_PATH="%PATH_TO_ZLIB_SRC%"^
-    -DZLIB_LIBRARY="%ZLIB_LIBRARY%"
+robocopy %SOURCE_LOCATION% %BUILD_SOURCE_LOCATION% /E /NFL /NDL /NJH /NJS
 
-echo.
-echo Compiling Blosc Release libraries...
-cmake --build . --config Release -j8
+pushd %BUILD_SOURCE_LOCATION%
+git apply %UE_MODULE_LOCATION%\Blosc_v1.21.0_LZ4_PREFIX.patch
+if %errorlevel% neq 0 exit /B %errorlevel%
+popd
 
-echo.
-echo Compiling Blosc Debug libraries...
+set C_FLAGS="-DLZ4_PREFIX=BLOSC_"
+
+echo Configuring build for Blosc version %BLOSC_VERSION%...
+cmake -G "Visual Studio 16 2019" %BUILD_SOURCE_LOCATION%^
+    -DCMAKE_INSTALL_PREFIX="%INSTALL_LOCATION%"^
+    -DPREFER_EXTERNAL_ZLIB=ON^
+    -DZLIB_INCLUDE_DIR="%ZLIB_INCLUDE_LOCATION%"^
+    -DZLIB_LIBRARY="%ZLIB_LIB_LOCATION%"^
+    -DCMAKE_C_FLAGS="%C_FLAGS%"^
+    -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON^
+    -DBUILD_SHARED=OFF^
+    -DBUILD_TESTS=OFF^
+    -DBUILD_FUZZERS=OFF^
+    -DBUILD_BENCHMARKS=OFF^
+    -DCMAKE_DEBUG_POSTFIX=_d
+if %errorlevel% neq 0 exit /B %errorlevel%
+
+echo Building Blosc for Debug...
 cmake --build . --config Debug -j8
+if %errorlevel% neq 0 exit /B %errorlevel%
 
-echo.
-echo Installing headers to %BLOSC_INSTALL_INCLUDE_PATH%
-mkdir %BLOSC_INSTALL_INCLUDE_PATH%
-robocopy ..\blosc %BLOSC_INSTALL_INCLUDE_PATH% blosc.h /nfl /ndl /njh /njs
+echo Installing Blosc for Debug...
+cmake --install . --config Debug
+if %errorlevel% neq 0 exit /B %errorlevel%
 
-echo Installing Release output to %BLOSC_INSTALL_BIN_PATH%\Release and %BLOSC_INSTALL_LIB_PATH%\Release
-mkdir %BLOSC_INSTALL_BIN_PATH%\Release
-mkdir %BLOSC_INSTALL_LIB_PATH%\Release
-robocopy blosc\Release %BLOSC_INSTALL_BIN_PATH%\Release blosc.dll /nfl /ndl /njh /njs
-robocopy blosc\Release %BLOSC_INSTALL_LIB_PATH%\Release *.lib /nfl /ndl /njh /njs
+echo Building Blosc for Release...
+cmake --build . --config Release -j8
+if %errorlevel% neq 0 exit /B %errorlevel%
 
-echo Installing Debug output to %BLOSC_INSTALL_BIN_PATH%\Debug and %BLOSC_INSTALL_LIB_PATH%\Debug
-mkdir %BLOSC_INSTALL_BIN_PATH%\Debug
-mkdir %BLOSC_INSTALL_LIB_PATH%\Debug
-robocopy blosc\Debug %BLOSC_INSTALL_BIN_PATH%\Debug blosc.dll /nfl /ndl /njh /njs
-robocopy blosc\Debug %BLOSC_INSTALL_LIB_PATH%\Debug *.lib /nfl /ndl /njh /njs
-robocopy blosc\Debug %BLOSC_INSTALL_LIB_PATH%\Debug *.pdb /nfl /ndl /njh /njs
+echo Installing Blosc for Release...
+cmake --install . --config Release
+if %errorlevel% neq 0 exit /B %errorlevel%
 
 popd
-popd
-popd
+
+echo Removing pkgconfig files...
+rmdir /S /Q "%INSTALL_LOCATION%\lib\pkgconfig"
+
+echo Moving lib directory into place...
+set INSTALL_LIB_DIR=%COMPILER_VERSION_NAME%\%ARCH_NAME%
+set INSTALL_LIB_LOCATION=%INSTALL_LOCATION%\%INSTALL_LIB_DIR%
+mkdir %INSTALL_LIB_LOCATION%
+move "%INSTALL_LOCATION%\lib" "%INSTALL_LIB_LOCATION%"
+
+echo Done.
 
 goto :eof
 
 :usage
-echo Usage: BuildForWindows 1.5.0
+echo Usage: BuildForWindows 1.21.0
 exit /B 1
 
 endlocal
