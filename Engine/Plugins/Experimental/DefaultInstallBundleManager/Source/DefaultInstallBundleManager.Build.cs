d40f17205c92e77bf9f7b8eeae70041597b25d1a
david harvey <David.Harvey@epicgames.com>
2022-05-11 15:56:09 -0400

    experimental default install bundle manager
    #preflight 6266b6760634d0904ce1a85e
    #rnx
    #jira UE-146398
    #rb Justin.Marcus
    
    #ROBOMERGE-AUTHOR: david.harvey
    #ROBOMERGE-SOURCE: CL 20142576 via CL 20143767 via CL 20144401
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v943-19904690)
    
    [CL 20148114 by david harvey in ue5-main branch]

---
+++ Engine/Plugins/Experimental/DefaultInstallBundleManager/Source/DefaultInstallBundleManager.Build.cs
@@ -0,0 +1,34 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+// This module must be loaded "PreLoadingScreen" in the .uproject file, otherwise it will not hook in time!
+
+public class DefaultInstallBundleManager : ModuleRules
+{
+	public DefaultInstallBundleManager(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseSharedPCHs;
+		PrivatePCHHeaderFile = "Private/DefaultInstallBundleManagerPrivatePCH.h";
+
+		PublicDependencyModuleNames.AddRange(
+			new string[] {
+				"InstallBundleManager",
+				"PatchCheck",
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"Core",
+				"PakFile",
+				"BuildPatchServices",
+				"Json",
+				"Engine",
+				"RenderCore",
+				"AnalyticsET",
+				"OnlineSubsystem",
+			}
+		);
+	}
+}
