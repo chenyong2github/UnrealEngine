57ecec71d4d2cbe814972c64f478697eba3267de
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2022-05-17 15:38:06 -0400

    RigLogic: Remove #pragma once from a cpp file.
    #jira UE-152681
    #rnx
    #preflight 6283f4590a585259b1ac924a
    
    [CL 20249590 by halfdan ingvarsson in ue5-main branch]

--- Engine/Plugins/Animation/RigLogic/Source/RigLogicEditor/Private/RigLogicEditor.cpp
+++ Engine/Plugins/Animation/RigLogic/Source/RigLogicEditor/Private/RigLogicEditor.cpp
@@ -1,11 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "RigLogicEditor.h"
-#include "Features/IModularFeatures.h"
-#include "Engine/Engine.h"
-#include "Misc/Paths.h"
 #include "Modules/ModuleManager.h"
 #include "Logging/LogMacros.h"
 
