70ad2fba6d05a2ca8a906e616b7eef60ea8bb090
David Lesage <David.Lesage@epicgames.com>
2022-05-19 13:36:52 -0400

    Remove debug compilation definitions #rb none #preflight 62866c3f2c1bb3e25a4ac17d
    [CL 20283463 by David Lesage in ue5-main branch]

--- Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CoreTechSurface/CoreTechSurface.Build.cs
+++ Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CoreTechSurface/CoreTechSurface.Build.cs
@@ -8,11 +8,6 @@ namespace UnrealBuildTool.Rules
 	{
 		public CoreTechSurface(ReadOnlyTargetRules Target) : base(Target)
 		{
-			// option de compil pour dev
-			OptimizeCode = CodeOptimization.Never;
-			PCHUsage = PCHUsageMode.NoPCHs;
-			// fin option de compil pour dev
-
 			PublicDependencyModuleNames.AddRange(
 				new string[]
 				{
