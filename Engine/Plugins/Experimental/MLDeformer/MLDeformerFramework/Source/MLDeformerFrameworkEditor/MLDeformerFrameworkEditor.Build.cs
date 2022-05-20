128d453c749b60cf157be9ac4127789e3c2aad26
Marc Audy <Marc.Audy@epicgames.com>
2022-05-20 02:06:17 -0400

    Fix Clang Editor compile due to missing dependency in light of CL# 20282438 #codereview Zach.Rammell,  brooke.hubert, Thomas.Sarkanen #rnx #preflight
    [CL 20291392 by Marc Audy in ue5-main branch]

--- Engine/Plugins/Experimental/MLDeformer/MLDeformerFramework/Source/MLDeformerFrameworkEditor/MLDeformerFrameworkEditor.Build.cs
+++ Engine/Plugins/Experimental/MLDeformer/MLDeformerFramework/Source/MLDeformerFrameworkEditor/MLDeformerFrameworkEditor.Build.cs
@@ -30,6 +30,7 @@ namespace UnrealBuildTool.Rules
 					"MLDeformerFramework",
 					"Projects",
 					"PropertyEditor",
+					"AnimationEditMode",
 					"AnimGraph",
 					"ToolWidgets",
 					"GeometryCache",
