b2c14352d820866f7d96c8f061dc2287597956cb
Joe Kirchoff <joe.kirchoff@epicgames.com>
2022-05-25 19:47:09 -0400

    UnrealBuildTool: Readd -Wno-range-loop-analysis warnings for Apple platforms, there appears to be outstanding issues that presubmit did not notify of
    #rnx
    #rb trivial
    #preflight skipped
    
    [CL 20373606 by Joe Kirchoff in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/ToolChain/AppleToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/ToolChain/AppleToolChain.cs
@@ -261,6 +261,13 @@ namespace UnrealBuildTool
 				Arguments.Remove("-Wno-unused-but-set-parameter");
 				Arguments.Remove("-Wno-ordered-compare-function-pointers");
 			}
+
+			// clang 12.00 has a new warning for copies in ranged loops. Instances have all been fixed up (2020/6/26) but
+			// are likely to be reintroduced due to no equivalent on other platforms at this time so disable the warning
+			if (GetClangVersion().Major >= 12)
+			{
+				Arguments.Add("-Wno-range-loop-analysis");
+			}
 		}
 
 		/// <inheritdoc/>
