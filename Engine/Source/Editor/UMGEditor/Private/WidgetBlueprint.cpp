2435e8a5484a8d155720a2f39a4d6db28e73562c
dave jones2 <dave.jones2@epicgames.com>
2022-05-26 17:11:12 -0400

    UE-148560 - UMG property binding fixup for real numbers doesn't always work
    The fixup pass for delegates in UMG bindings had faulty logic for determining the name of a function. The "FunctionName" field on the binding may not actually reflect the true name of the function. For example, a function rename can make this field stale. This would eventually cause BP compilation errors since we didn't find the matching function graph(s) that needed fixup.
    
    Instead, we should call GetMemberName on the final path segment. This will use a GUID for determining what the actual function name is.
    
    #rb patrick.boutot
    #preflight 6262f1fbcf17922036d8d006
    #jira UE-148281
    
    #ROBOMERGE-AUTHOR: dave.jones2
    #ROBOMERGE-SOURCE: CL 20381031 in //UE5/Release-5.0/... via CL 20383028
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v949-20362246)
    
    [CL 20385737 by dave jones2 in ue5-main branch]

--- Engine/Source/Editor/UMGEditor/Private/WidgetBlueprint.cpp
+++ Engine/Source/Editor/UMGEditor/Private/WidgetBlueprint.cpp
@@ -593,7 +593,7 @@ void UWidgetBlueprint::ReplaceDeprecatedNodes()
 		// However, UMG property bindings are a special case: the BP functions that bind to the native delegate must agree on their underlying types.
 		// Specifically, bindings used with float properties *must* use the PC_Float type as the return value in a BP function.
 		// In order to correct this behavior, we need to:
-		// * Iterate throught the property bindings.
+		// * Iterate through the property bindings.
 		// * Find the corresponding delegate signature.
 		// * Find the function graph that matches the binding.
 		// * Find the result node.
@@ -614,9 +614,18 @@ void UWidgetBlueprint::ReplaceDeprecatedNodes()
 
 					if (BindableProperty)
 					{
-						auto GraphMatchesBindingPredicate = [&Binding](const UEdGraph* Graph) {
+						FName FunctionName = Binding.FunctionName;
+
+						if (!Binding.SourcePath.IsEmpty())
+						{
+							check(Binding.SourcePath.Segments.Num() > 0);
+							const FEditorPropertyPathSegment& LastSegment = Binding.SourcePath.Segments[Binding.SourcePath.Segments.Num() - 1];
+							FunctionName = LastSegment.GetMemberName();
+						}
+
+						auto GraphMatchesBindingPredicate = [FunctionName](const UEdGraph* Graph) {
 							check(Graph);
-							return (Binding.FunctionName == Graph->GetFName());
+							return (FunctionName == Graph->GetFName());
 						};
 
 						if (UEdGraph** GraphEntry = Graphs.FindByPredicate(GraphMatchesBindingPredicate))
