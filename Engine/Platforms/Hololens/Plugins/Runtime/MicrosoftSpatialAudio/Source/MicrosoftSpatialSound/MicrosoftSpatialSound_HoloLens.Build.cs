45bd3d573d077600956255ce4d3b2c871552e36f
David Harvey <David.Harvey@epicgames.com>
2022-05-26 08:57:05 -0400

    Remaining HoloLens platform extension files for engine plugins.
    #jira UE-149794
    #rnx
    #rb trivial
    #preflight 628f773c911f194ea72fc444
    
    [CL 20378298 by David Harvey in ue5-main branch]

---
+++ Engine/Platforms/Hololens/Plugins/Runtime/MicrosoftSpatialAudio/Source/MicrosoftSpatialSound/MicrosoftSpatialSound_HoloLens.Build.cs
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+namespace UnrealBuildTool.Rules
+{
+	public class MicrosoftSpatialSound_HoloLens : MicrosoftSpatialSound
+	{
+		public MicrosoftSpatialSound_HoloLens(ReadOnlyTargetRules Target) : base(Target)
+		{
+		}
+	}
+}
