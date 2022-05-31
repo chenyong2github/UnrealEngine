1abae7891d714d7399af25b5f9769ea2e5041f67
Brandon Schaefer <Brandon.Schaefer@epicgames.com>
2022-05-31 11:57:28 -0400

    Check for a nullptr for the asset class
    #jira UE-154147
    #rb Lauren.Barnes
    #preflight none
    
    [CL 20438041 by Brandon Schaefer in ue5-main branch]

--- Engine/Source/Editor/LevelEditor/Private/LevelEditorCreateActorMenu.cpp
+++ Engine/Source/Editor/LevelEditor/Private/LevelEditorCreateActorMenu.cpp
@@ -97,8 +97,9 @@ static void GetMenuEntryText(const FAssetData& Asset, const TArray<FActorFactory
 	if (AssetMenuOptions.Num() == 1)
 	{
 		const FActorFactoryAssetProxy::FMenuItem& MenuItem = AssetMenuOptions[0];
+		UClass* MenuItemClass = Cast<UClass>(MenuItem.AssetData.GetAsset());
 
-		if (IsClass && Cast<UClass>(MenuItem.AssetData.GetAsset())->IsChildOf(AActor::StaticClass()))
+		if (IsClass && MenuItemClass && MenuItemClass->IsChildOf(AActor::StaticClass()))
 		{
 			AActor* DefaultActor = Cast<AActor>(Cast<UClass>(MenuItem.AssetData.GetAsset())->ClassDefaultObject);
 			OutActorTypeDisplayName = FText::FromString(FName::NameToDisplayString(DefaultActor->GetClass()->GetName(), false));
