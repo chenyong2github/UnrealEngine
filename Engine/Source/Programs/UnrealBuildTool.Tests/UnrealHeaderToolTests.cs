95fa16785ee80202dca02b004135e780fd57f544
Ben Marsh <Ben.Marsh@epicgames.com>
2022-06-02 13:12:11 -0400

    Fix UHT test suite not building/throwing an exception on startup when run as part of MSTest. Now runs ok, but tests fail.
    #fyi Tim.Smith
    #preflight none
    
    [CL 20471797 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool.Tests/UnrealHeaderToolTests.cs
+++ Engine/Source/Programs/UnrealBuildTool.Tests/UnrealHeaderToolTests.cs
@@ -1,9 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
+using UnrealBuildBase;
 using EpicGames.Core;
 using EpicGames.UHT.Utils;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnrealBuildTool.Modes;
+using System.Reflection;
 
 namespace UnrealBuildToolTests
 {
@@ -13,6 +17,9 @@ namespace UnrealBuildToolTests
 		[TestMethod]
 		public void Run()
 		{
+			FileReference AssemblyFile = new FileReference(Assembly.GetExecutingAssembly().Location);
+			Unreal.LocationOverride.RootDirectory = DirectoryReference.Combine(AssemblyFile.Directory, "../../../../..");
+
 			string[] Arguments = new string[] { };
 			CommandLineArguments CommandLineArguments = new CommandLineArguments(Arguments);
 			UhtGlobalOptions Options = new UhtGlobalOptions(CommandLineArguments);
@@ -24,7 +31,8 @@ namespace UnrealBuildToolTests
 			IUhtConfig Config = new UhtConfigImpl(CommandLineArguments);
 
 			// Run the tests
-			Assert.IsTrue(UhtTestHarness.RunTests(Tables, Config, Options));
+			ILoggerFactory factory = LoggerFactory.Create(x => x.AddEpicDefault());
+			Assert.IsTrue(UhtTestHarness.RunTests(Tables, Config, Options, factory.CreateLogger<UhtTestHarness>()));
 		}
 	}
 }
