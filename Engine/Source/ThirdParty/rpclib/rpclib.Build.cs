0ec9757b78d51d9a36c284ad7167f1b3f853fbea
Joe Kirchoff <joe.kirchoff@epicgames.com>
2022-06-02 20:54:29 -0400

    Update ThirdParty rpclib library to build with BuildCMakeLib #rb none #rnx #preflight 629957d6f62914987e9e65e8
    [CL 20479632 by Joe Kirchoff in ue5-main branch]

--- Engine/Source/ThirdParty/rpclib/rpclib.Build.cs
+++ Engine/Source/ThirdParty/rpclib/rpclib.Build.cs
@@ -6,15 +6,28 @@ using UnrealBuildTool;
 [SupportedPlatforms("Win64", "Mac", "Linux")]
 public class RPCLib : ModuleRules
 {
+	protected readonly string Version = "2.2.1";
+
 	public RPCLib(ReadOnlyTargetRules Target) : base(Target)
 	{
 		Type = ModuleType.External;
 
-		if (!IsVcPackageSupported)
-		{
-			return;
-		}
+		string VersionPath = Path.Combine(ModuleDirectory, Version);
+		string LibraryPath = Path.Combine(VersionPath, "lib");
 
-		AddVcPackage("rpclib", true, "rpc");
+		PublicSystemIncludePaths.Add(Path.Combine(VersionPath, "include"));
+
+		if (Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "Unix", Target.Architecture, "Release", "librpc.a"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "Mac", "Release", "librpc.a"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "Win64", Target.WindowsPlatform.Architecture.ToString().ToLowerInvariant(), "Release", "rpc.lib"));
+		}
 	}
 }
