204b24f720891a87452777cf380092eb191d9b3c
geoffrey douglas <geoffrey.douglas@epicgames.com>
2022-06-07 16:20:06 -0400

    Add primary node check to determine if subject being replicated comes from a source that is marked as pending kill #jira UE-154838 #rb simon.therriault #preflight 629f58ea233ae0a8f8f6b493 #lockdown
    #ROBOMERGE-AUTHOR: geoffrey.douglas
    #ROBOMERGE-SOURCE: CL 20539860 in //UE5/Release-5.0/... via CL 20542866
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v954-20466795)
    
    [CL 20544565 by geoffrey douglas in ue5-main branch]

--- Engine/Plugins/Runtime/LiveLinkOvernDisplay/Source/LiveLinkOverNDisplay/Private/NDisplayLiveLinkSubjectReplicator.cpp
+++ Engine/Plugins/Runtime/LiveLinkOvernDisplay/Source/LiveLinkOverNDisplay/Private/NDisplayLiveLinkSubjectReplicator.cpp
@@ -223,6 +223,15 @@ void FNDisplayLiveLinkSubjectReplicator::OnDataSynchronization(FArchive& Ar)
 					const FName SubjectName = SubjectKey.SubjectName;
 					TSubclassOf<ULiveLinkRole> SubjectRole = LiveLinkClient->GetSubjectRole_AnyThread(SubjectKey);
 
+					// Check if the subject's source is pending kill. If it is, the subject itself will soon be killed and we can skip evaluating it.
+					TArray<FGuid> ValidSources = LiveLinkClient->GetSources(false);
+					const bool bIsSourceValid = ValidSources.ContainsByPredicate([SubjectKey](const FGuid& Other) { return Other == SubjectKey.Source; });
+					if (!bIsSourceValid)
+					{
+						UE_LOG(LogNDisplayLiveLinkSubjectReplicator, Verbose, TEXT("Master could not evaluate Subject '%s'. Its source is marked as pending kill."), *SubjectName.ToString());
+						continue;
+					}
+
 					FLiveLinkSubjectFrameData SubjectFrameData;
 					if (LiveLinkClient->EvaluateFrame_AnyThread(SubjectName, SubjectRole, SubjectFrameData))
 					{
