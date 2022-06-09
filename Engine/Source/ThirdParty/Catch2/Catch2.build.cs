ff980fa8541b10941fe1c36c8d644a08d7496e36
David Harvey <David.Harvey@epicgames.com>
2022-06-09 05:17:10 -0400

    Platform extensions have more control over the catch2 library path.
    #jira UE-139998
    #rb Chris.Constantinescu
    #rnx
    #preflight 62a1a7d69a3e81e9040218d4
    
    [CL 20572870 by David Harvey in ue5-main branch]

--- Engine/Source/ThirdParty/Catch2/Catch2.build.cs
+++ Engine/Source/ThirdParty/Catch2/Catch2.build.cs
@@ -23,43 +23,59 @@ public class Catch2 : ModuleRules
 		}
 	}
 
+	public virtual string Catch2Root
+	{
+		get
+		{
+			bool IsPlatformExtension = !(Target.Platform.IsInGroup(UnrealPlatformGroup.Desktop) || Target.Platform == UnrealTargetPlatform.Android);
+			if (IsPlatformExtension)
+			{
+				return Path.Combine(Unreal.EngineDirectory.FullName, "Platforms", Target.Platform.ToString(), "Source", "ThirdParty", "Catch2");
+			}
+			else
+			{
+				return Path.Combine(Unreal.EngineDirectory.FullName, "Source", "ThirdParty", "Catch2");
+			}
+		}
+	}
+
+	public virtual string RelativeBaseLibPath
+	{
+		get
+		{
+			bool IsPlatformExtension = !(Target.Platform.IsInGroup(UnrealPlatformGroup.Desktop) || Target.Platform == UnrealTargetPlatform.Android);
+
+			string RelativeLibPath = !IsPlatformExtension ? Target.Platform.ToString() : string.Empty;
+			string Arch = string.Empty;
+			if (Target.Platform == UnrealTargetPlatform.Android)
+			{
+				Arch = "arm64";
+			}
+			else if (Target.Platform == UnrealTargetPlatform.Linux)
+			{
+				Arch = "x86_64-unknown-linux-gnu";
+			}
+			else if (Target.Platform == UnrealTargetPlatform.Win64)
+			{
+				Arch = "x64";
+			}
+			if (!string.IsNullOrEmpty(Arch))
+			{
+				RelativeLibPath = Path.Combine(RelativeLibPath, Arch);
+			}
+
+			return RelativeLibPath;
+		}
+	}
+
 	public Catch2(ReadOnlyTargetRules Target) : base(Target)
 	{
 		Type = ModuleType.External;
 
-		bool IsPlatformExtension = !(Target.Platform.IsInGroup(UnrealPlatformGroup.Desktop) || Target.Platform == UnrealTargetPlatform.Android);
 		bool IsDebugConfig = Target.Configuration < UnrealTargetConfiguration.Development;
 
-		string Catch2Root;
-		if (IsPlatformExtension)
-		{
-			Catch2Root = Path.Combine(Unreal.EngineDirectory.FullName, "Platforms", Target.Platform.ToString(), "Source", "ThirdParty", "Catch2");
-		}
-		else
-		{
-			Catch2Root = Path.Combine(Unreal.EngineDirectory.FullName, "Source", "ThirdParty", "Catch2");
-		}
 
-		string RelativeLibPath = !IsPlatformExtension ? Target.Platform.ToString() : string.Empty;
-		string Arch = string.Empty;
-		if (Target.Platform == UnrealTargetPlatform.Android)
-		{
-			Arch = "arm64";
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			Arch = "x86_64-unknown-linux-gnu";
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Win64)
-		{
-			Arch = "x64";
-		}
-		if (!string.IsNullOrEmpty(Arch))
-		{
-			RelativeLibPath = Path.Combine(RelativeLibPath, Arch);
-		}
-
-		RelativeLibPath = Path.Combine(RelativeLibPath, IsDebugConfig ? "debug" : "release", LibName);
+		string RelativeLibPath = Path.Combine(RelativeBaseLibPath, IsDebugConfig ? "debug" : "release", LibName);
 
 		PublicAdditionalLibraries.Add(Path.Combine(Catch2Root, "v3.0.0-preview5", "lib", RelativeLibPath));
 		PublicIncludePaths.Add(Path.Combine(Unreal.EngineDirectory.FullName, "Source", "ThirdParty", "Catch2", "v3.0.0-preview5", "src"));
