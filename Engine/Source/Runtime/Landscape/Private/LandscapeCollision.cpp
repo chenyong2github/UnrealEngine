3d15569efb119a31f8302264f91f736cf38cc28d
JeanFrancois Dube <JeanFrancois.Dube@epicgames.com>
2022-06-10 08:14:10 -0400

    Landscape: fix relative location mismatch detection.
    #jira UE-153865
    #rb richard.malo
    #preflight none
    #rnx
    
    [CL 20593700 by JeanFrancois Dube in ue5-main branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeCollision.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeCollision.cpp
@@ -1855,8 +1855,8 @@ void ULandscapeHeightfieldCollisionComponent::PostLoad()
 			FVector LocalRelativeLocation = GetRelativeLocation();
 			float CheckRelativeLocationX = float(SectionBaseX - LandscapeProxy->LandscapeSectionOffset.X);
 			float CheckRelativeLocationY = float(SectionBaseY - LandscapeProxy->LandscapeSectionOffset.Y);
-			if (CheckRelativeLocationX != LocalRelativeLocation.X ||
-				CheckRelativeLocationY != LocalRelativeLocation.Y)
+			if (!FMath::IsNearlyEqual(CheckRelativeLocationX, LocalRelativeLocation.X, UE_DOUBLE_KINDA_SMALL_NUMBER) ||
+				!FMath::IsNearlyEqual(CheckRelativeLocationY, LocalRelativeLocation.Y, UE_DOUBLE_KINDA_SMALL_NUMBER))
 			{
 				UE_LOG(LogLandscape, Warning, TEXT("ULandscapeHeightfieldCollisionComponent RelativeLocation disagrees with its section base, attempted automated fix: '%s', %f,%f vs %f,%f."),
 					*GetFullName(), LocalRelativeLocation.X, LocalRelativeLocation.Y, CheckRelativeLocationX, CheckRelativeLocationY);
