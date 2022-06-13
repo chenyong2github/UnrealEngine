3af2f406f0403ea18aa6ddebc0f9014c53254f71
Brandon Schaefer <Brandon.Schaefer@epicgames.com>
2022-06-13 14:04:41 -0400

    Start of the UGSCore area. This was an original port done by Ben.M of UGS C# -> C++ at CL 3554544 (yeah its quite old and will be missing many newer UGS features!)
    #jira none
    #rb none
    #fyi Robert.Seiver
    #preflight none
    
    [CL 20630897 by Brandon Schaefer in ue5-main branch]

---
+++ Engine/Source/Programs/SlateUGS/Private/UGSCore/ModalTask.h
@@ -0,0 +1,32 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Templates/SharedPointer.h"
+
+class FModalTaskResult
+{
+public:
+	FModalTaskResult(bool bSucceeded, const FText& Message);
+	~FModalTaskResult();
+
+	bool Succeeded() const;
+	bool Failed() const;
+	const FText& GetMessage() const;
+
+	static TSharedRef<FModalTaskResult> Success();
+	static TSharedRef<FModalTaskResult> Failure(const FText& Message);
+	static TSharedRef<FModalTaskResult> Aborted();
+
+private:
+	const bool bSucceeded;
+	const FText Message;
+};
+
+class IModalTask
+{
+public:
+	virtual ~IModalTask();
+	virtual TSharedRef<FModalTaskResult> Run(FEvent* AbortEvent) = 0;
+};
---
+++ Engine/Source/Programs/SlateUGS/Private/UGSCore/Telemetry.h
@@ -0,0 +1,30 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/Timespan.h"
+
+class FTelemetryStopwatch
+{
+public:
+	FTelemetryStopwatch(const FString& InAction, const FString& InProject);
+	~FTelemetryStopwatch();
+
+	FTimespan Stop(const FString& InResult);
+	FTimespan GetElapsed() const;
+
+private:
+	const FString Action;
+	const FString Project;
+	const FDateTime StartTime;
+	FString Result;
+	FDateTime EndTime;
+};
+
+class FTelemetryWriter
+{
+public:
+	FTelemetryWriter(const FString& InSqlConnectionString, const FString& InLogFileName);
+	~FTelemetryWriter();
+};
