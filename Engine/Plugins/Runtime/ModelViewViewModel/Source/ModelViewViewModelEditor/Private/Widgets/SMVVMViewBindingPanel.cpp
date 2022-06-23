b162fd506fd37a0eaf12e60870bbeacd77d15cff
sebastian nordgren <sebastian.nordgren@epicgames.com>
2022-06-23 10:04:01 -0400

    MVVM - You can only add bindings to a widget blueprint that already has viewmodels assigned to it.
    It's impossible to create a valid binding without it, so there's not really any reason to allow it.
    
    [REVIEW] [at]patrick.boutot
    #rnx
    
    #ROBOMERGE-AUTHOR: sebastian.nordgren
    #ROBOMERGE-SOURCE: CL 20791814 via CL 20791846 via CL 20791861
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v971-20777995)
    
    [CL 20794026 by sebastian nordgren in ue5-main branch]

--- Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModelEditor/Private/Widgets/SMVVMViewBindingPanel.cpp
+++ Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModelEditor/Private/Widgets/SMVVMViewBindingPanel.cpp
@@ -164,7 +164,6 @@ void SMVVMViewBindingPanel::OnBindingListSelectionChanged(int32 Index)
 	}
 }
 
-
 void SMVVMViewBindingPanel::AddDefaultBinding()
 {
 	if (UMVVMWidgetBlueprintExtension_View* MVVMExtensionPtr = MVVMExtension.Get())
@@ -177,10 +176,29 @@ void SMVVMViewBindingPanel::AddDefaultBinding()
 	}
 }
 
-
 bool SMVVMViewBindingPanel::CanAddBinding() const
 {
-	return true;
+	if (UMVVMWidgetBlueprintExtension_View* MVVMExtensionPtr = MVVMExtension.Get())
+	{
+		if (UMVVMBlueprintView* BlueprintView = MVVMExtensionPtr->GetBlueprintView())
+		{
+			return BlueprintView->GetViewModels().Num() > 0;
+		}
+	}
+
+	return false;
+}
+
+FText SMVVMViewBindingPanel::GetAddBindingToolTip() const
+{
+	if (CanAddBinding())
+	{
+		return LOCTEXT("AddBindingTooltip", "Add an empty binding.");
+	}
+	else
+	{
+		return LOCTEXT("CannotAddBindingToolTip", "A viewmodel is required before adding bindings.");
+	}
 }
 
 void SMVVMViewBindingPanel::ShowManageViewModelsWindow()
@@ -349,7 +367,7 @@ TSharedRef<SWidget> SMVVMViewBindingPanel::GenerateEditViewWidget()
 			),
 			NAME_None,
 			LOCTEXT("AddBinding", "Add Binding"),
-			LOCTEXT("AddBindingTooltip", "Add an empty binding."),
+			TAttribute<FText>(this, &SMVVMViewBindingPanel::GetAddBindingToolTip),
 			FSlateIcon(FMVVMEditorStyle::Get().GetStyleSetName(), "BindingView.AddBinding"),
 			EUserInterfaceActionType::Button
 		);
