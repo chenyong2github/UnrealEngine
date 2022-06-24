b4d0405477c68ecad77cbd125ed217392685c022
robert millar <robert.millar@epicgames.com>
2022-06-23 20:16:21 -0400

    Add command line argument for underrun mode
    #rb none
    
    #ROBOMERGE-AUTHOR: robert.millar
    #ROBOMERGE-SOURCE: CL 20804243 via CL 20804274 via CL 20804378 via CL 20804404
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v971-20777995)
    
    [CL 20805527 by robert millar in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/HAL/MallocStomp2.cpp
+++ Engine/Source/Runtime/Core/Private/HAL/MallocStomp2.cpp
@@ -355,8 +355,9 @@ FMalloc* FMallocStomp2::OverrideIfEnabled(FMalloc* InUsedAlloc)
 		const TCHAR* CommandLine = ::GetCommandLineW();
 		FParse::Value(CommandLine, TEXT("MallocStomp2MinSize="), MinSize);
 		FParse::Value(CommandLine, TEXT("MallocStomp2MaxSize="), MaxSize);
+		bool bUseUnderrunMode = FParse::Param(CommandLine, TEXT("MallocStomp2UnderrunMode"));
 #endif
-		GMallocStomp2 = new FMallocStomp2(InUsedAlloc);
+		GMallocStomp2 = new FMallocStomp2(InUsedAlloc, bUseUnderrunMode);
 		GMallocStomp2->Init();
 		return GMallocStomp2;
 	}
