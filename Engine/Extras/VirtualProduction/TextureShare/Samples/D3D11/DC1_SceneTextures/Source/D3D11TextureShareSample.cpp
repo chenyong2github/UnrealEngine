4cd46b082440f6f0acd9e02293ac8ef5ce689ffc
peter tarasenko <p.tarasenko@pixelalabs.com>
2022-06-25 23:18:23 -0400

    TextureShare Samples - fixed missing copyright boilerplate. Horde Issue #185572 #jira none #rb trivial #preflight none
    [CL 20826578 by peter tarasenko in ue5-main branch]

--- Engine/Extras/VirtualProduction/TextureShare/Samples/D3D11/DC1_SceneTextures/Source/D3D11TextureShareSample.cpp
+++ Engine/Extras/VirtualProduction/TextureShare/Samples/D3D11/DC1_SceneTextures/Source/D3D11TextureShareSample.cpp
@@ -1,4 +1,4 @@
- // Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "D3D11TextureShareSample.h"
 #include "D3D11TextureShareSampleSetup.h"
