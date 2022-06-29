76e779babcd116761c5ef6ca137c529dc4ca7fc5
peter tarasenko <p.tarasenko@pixelalabs.com>
2022-06-29 18:05:35 -0400

    TextureShare:  - Re-add the nDisplay dependency to resolve the issue. (Independence between TextureShare and nDisplay will become possible in the future by refactoring the public nDisplay API into a separate plugin)  - Default SDK process name can now use TEXTURESHARE_PROJECT_NAME macro as source. This allows you to run multiple TextureShareSDK example applications at the same time.
    #jira UE-157856
    #rb none
    #preflight 62bcafb95d53ca5bcefa5c1f
    
    [CL 20884334 by peter tarasenko in ue5-main branch]

--- Engine/Plugins/VirtualProduction/TextureShare/Source/TextureShareCore/Private/Misc/TextureShareCoreStrings.h
+++ Engine/Plugins/VirtualProduction/TextureShare/Source/TextureShareCore/Private/Misc/TextureShareCoreStrings.h
@@ -18,7 +18,12 @@ namespace TextureShareCoreStrings
 
 		namespace ProcessName
 		{
+#if defined TEXTURESHARE_PROJECT_NAME
+			// Use TextureShare project name as process name
+			static constexpr auto SDK = TEXT(TEXTURESHARE_PROJECT_NAME);
+#else
 			static constexpr auto SDK = TEXT("TextureShareSDK");
+#endif
 			static constexpr auto UE = TEXT("UnrealEngine");
 		}
 
