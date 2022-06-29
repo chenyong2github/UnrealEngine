055abff597f272d9a32abb80a08f8056c3508b70
Joakim Lindqvist <Joakim.Lindqvist@epicgames.com>
2022-06-29 07:32:05 -0400

    Horde Storage - Use new oidc token binaries path
    [CL 20872396 by Joakim Lindqvist in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeStorage/updateAuthHeaderUser.ps1
+++ Engine/Source/Programs/Horde/HordeStorage/updateAuthHeaderUser.ps1
@@ -1,4 +1,4 @@
 $TempFile = New-TemporaryFile
 
-& "dotnet" "..\..\..\..\..\Engine\Restricted\NotForLicensees\Binaries\DotNET\OidcToken\portable\OidcToken.dll" "--Service=EpicGames-Okta-Jupiter" "--OutFile" $TempFile.FullName 
+& "dotnet" "..\..\..\..\..\Engine\Binaries\DotNET\OidcToken\portable\OidcToken.dll" "--Service=EpicGames-Okta-Jupiter" "--OutFile" $TempFile.FullName 
 $env:curl_auth_header = Get-Content -Raw -Path $TempFile.FullName | ConvertFrom-Json | ForEach-Object { "Authorization: Bearer $($_.Token)" }
