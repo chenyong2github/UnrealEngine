4d40c75b58fa46b0a4c90e40ec2f3b95a84d4eb0
robert millar <robert.millar@epicgames.com>
2022-06-30 20:54:53 -0400

    [KillPendingKill] Clean up references between scene components when actors are destroyed or stream out to avoid holding reference to garbage objects. On destruction, clear attachments between components owned by different actors. On uninitialize component, clear attachments between components in different outer hierarchies, i.e. different streaming levels.
    Note that this changes behavior so that attachments between scene components in different levels will be broken when either level is streamed out (made not visible) even if the level is not unloaded & garbage collected.
    
    #rb richard.malo,marc.audy
    
    #ROBOMERGE-AUTHOR: robert.millar
    #ROBOMERGE-SOURCE: CL 20906077 via CL 20906303 via CL 20906574 via CL 20906662
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v971-20777995)
    
    [CL 20908112 by robert millar in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/SceneComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/SceneComponent.h
@@ -905,6 +905,8 @@ public:
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual void OnRegister() override;
 	virtual void OnUnregister() override;
+	virtual void EndPlay(EEndPlayReason::Type Reason) override;
+	virtual void UninitializeComponent() override;
 	virtual bool ShouldCreateRenderState() const override { return true; }
 	virtual void UpdateComponentToWorld(EUpdateTransformFlags UpdateTransformFlags = EUpdateTransformFlags::None, ETeleportType Teleport = ETeleportType::None) override final
 	{
