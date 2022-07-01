254589fdb81a90c8897b177bb8443daffae3c593
thomas tedemalm <thomas.tedemalm@epicgames.com>
2022-07-01 04:01:27 -0400

    PCG: *Removed option to open PCG graphs in the old asset editor.
    #rb julien.lheureux, adrien.logut
    #preflight 62bea4ffc9410537288443d2
    
    [CL 20911652 by thomas tedemalm in ue5-main branch]

--- Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/AssetTypeActions/PCGGraphAssetTypeActions.cpp
+++ Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/AssetTypeActions/PCGGraphAssetTypeActions.cpp
@@ -4,10 +4,6 @@
 #include "PCGGraph.h"
 #include "PCGEditor.h"
 
-#include "HAL/IConsoleManager.h"
-
-static TAutoConsoleVariable<bool> CVarPCGUseGraphEditor(TEXT("pcg.UseGraphEditor"), true, TEXT("Wheter to use use the new graph editor or not."));
-
 FText FPCGGraphAssetTypeActions::GetName() const
 {
 	return NSLOCTEXT("AssetTypeActions", "PCGGraphAssetTypeActions", "PCG Graph");
@@ -20,19 +16,12 @@ UClass* FPCGGraphAssetTypeActions::GetSupportedClass() const
 
 void FPCGGraphAssetTypeActions::OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<class IToolkitHost> EditWithinLevelEditor)
 {
-	if (CVarPCGUseGraphEditor.GetValueOnAnyThread())
+	for (UObject* Object : InObjects)
 	{
-		for (UObject* Object : InObjects)
+		if (UPCGGraph* PCGGraph = Cast<UPCGGraph>(Object))
 		{
-			if (UPCGGraph* PCGGraph = Cast<UPCGGraph>(Object))
-			{
-				TSharedRef<FPCGEditor> PCGEditor(new FPCGEditor());
-				PCGEditor->Initialize(EToolkitMode::Standalone, EditWithinLevelEditor, PCGGraph);
-			}
+			const TSharedRef<FPCGEditor> PCGEditor = MakeShared<FPCGEditor>();
+			PCGEditor->Initialize(EToolkitMode::Standalone, EditWithinLevelEditor, PCGGraph);
 		}
-	}
-	else
-	{
-		FPCGCommonAssetTypeActions::OpenAssetEditor(InObjects, EditWithinLevelEditor);
 	}
 }
