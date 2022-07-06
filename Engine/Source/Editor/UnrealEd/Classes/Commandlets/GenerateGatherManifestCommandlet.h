5ddef93c5ede6477f8e06b043b47f13924d50794
Leon Huang <leon.huang@epicgames.com>
2022-07-06 14:22:26 -0400

    Added support for preview versions of the localization manifests to be generated and deleted during a preview localization run. This addresses false positives for loclaization warnings that can arise from comparing gathered strings to a stale manifest file. - Added a preview parameter to the Localization UAT command which allows the command to run in preview mode. - Localization commandlets now generate *_Preview.manifest files during a run into the destination path specified by the config files. - The localization automation script will delete all files that end with _Preview.manifest at the end of the gather run. - Improved checks for manifest filename extensions to ensure no malformed manifest filenames are passed into the commandlet #jira: none #rb: Jamie.Dale #preflight: 62c487352f2d0469188c7d30
    [CL 20970061 by Leon Huang in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Classes/Commandlets/GenerateGatherManifestCommandlet.h
+++ Engine/Source/Editor/UnrealEd/Classes/Commandlets/GenerateGatherManifestCommandlet.h
@@ -19,5 +19,8 @@ public:
 	//~ Begin UCommandlet Interface
 	virtual int32 Main( const FString& Params ) override;
 	//~ End UCommandlet Interface
+		//~ Begin UGatherTextCommandletBase  Interface
+	virtual bool ShouldRunInPreview(const TArray<FString>& Switches, const TMap<FString, FString>& ParamVals) const override;
+	//~ End UGatherTextCommandletBase  Interface
 #endif
 };
--- Engine/Source/Editor/UnrealEd/Private/Commandlets/GenerateGatherManifestCommandlet.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Commandlets/GenerateGatherManifestCommandlet.cpp
@@ -2,8 +2,50 @@
 
 #include "Commandlets/GenerateGatherManifestCommandlet.h"
 #include "Misc/Paths.h"
+#include "Misc/FileHelper.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogGenerateManifestCommandlet, Log, All);
+
+namespace GenerateManifestHelper
+{
+	FString GetManifestFileExtension()
+	{
+		static const FString ManifestFileExtension= TEXT(".manifest");
+		return ManifestFileExtension;
+	}
+
+	/** Returns true if the passed in file has a .manifest extension. Else returns false. */
+	bool IsManifestFileExtensionValid(const FString& InManifestFilename)
+	{
+		return InManifestFilename.EndsWith(GenerateManifestHelper::GetManifestFileExtension());
+	}
+} // namespace GenerateManifestHelper
+
+namespace GeneratePreviewManifestHelper
+{
+	FString GetPreviewManifestSuffix()
+	{
+		// Note: This suffix is also hardcoded in Localisation.automation.cs
+		// If you decide to change the suffix for preview manifest files here, also update the automation file. 
+		static const FString PreviewSuffix = TEXT("_Preview");
+		return PreviewSuffix;
+	}
+
+	/**
+	* Given a manifest filename, provide the preview version of the manifest filename.
+	* @param InOriginalManifestFilename The original manifest filename (e.g MyManifest.manifest)
+	* @return The preview version of the manifest filename (e.g MyManifest_Preview.manifest with current implementation)
+	*/
+	FString GetPreviewManifestFilename(const FString& InOriginalManifestFilename)
+	{
+		// This check should already be performed outside of this function. The passed in file is assumed to have the correct extension. 
+		check(GenerateManifestHelper::IsManifestFileExtensionValid(InOriginalManifestFilename));
+		FString PreviewFilename= InOriginalManifestFilename.LeftChop(GenerateManifestHelper::GetManifestFileExtension().Len());
+		PreviewFilename.Append(GeneratePreviewManifestHelper::GetPreviewManifestSuffix());
+		PreviewFilename.Append(GenerateManifestHelper::GetManifestFileExtension());
+		return PreviewFilename;
+	}
+} // namespace GeneratePreviewManifestHelper
 
 /**
  *	UGenerateGatherManifestCommandlet
@@ -20,6 +62,16 @@ int32 UGenerateGatherManifestCommandlet::Main( const FString& Params )
 	TArray<FString> Switches;
 	TMap<FString, FString> ParamVals;
 	UCommandlet::ParseCommandLine( *Params, Tokens, Switches, ParamVals );
+	// we will have different behavior when this commandlet is running in preview
+	// We want to generate temporary preview manifest files.
+	// The temp manifest files will be loaded as manifest dependencies to avoid localization duplicate key warnings from surfacing due to stale manifest dependencies. 
+	// The Localisation.automation.cs file is set up to delete these temp manifest files with a run of the Localize UAT command with the preview switch.
+	// Note that these temp files will persist if running this commandlet via the command line. 
+	const bool bRunningInPreview = Switches.Contains(TEXT("Preview"));
+	if (bRunningInPreview)
+	{
+		UE_LOG(LogGenerateManifestCommandlet, Log, TEXT("Commandlet is running in preview mode. Preview versions of manifests will be saved and loaded."));
+	}
 
 	// Set config file.
 	const FString* ParamVal = ParamVals.Find( FString( TEXT("Config") ) );
@@ -65,16 +117,59 @@ int32 UGenerateGatherManifestCommandlet::Main( const FString& Params )
 		return -1;
 	}
 
+	if (!GenerateManifestHelper::IsManifestFileExtensionValid(ManifestName))
+	{
+		UE_LOG(LogGenerateManifestCommandlet, Error, TEXT("Found manifest file %s is malformed. All manifest files should have a %s extension."), *ManifestName, *GenerateManifestHelper::GetManifestFileExtension());
+		return -1;
+	}
+
+	if (bRunningInPreview)
+	{
+		// we change the manifest filename to reflect the preview filename 
+		ManifestName = GeneratePreviewManifestHelper::GetPreviewManifestFilename(ManifestName);
+	}
+
 	//Grab any manifest dependencies
 	TArray<FString> ManifestDependenciesList;
 	GetPathArrayFromConfig(*SectionName, TEXT("ManifestDependencies"), ManifestDependenciesList, GatherTextConfigPath);
+
+	// Check that all the dependent manifest files have valid file extensions
+	for (const FString& ManifestDependency : ManifestDependenciesList)
+	{
+		if (!GenerateManifestHelper::IsManifestFileExtensionValid(ManifestDependency))
+		{
+			UE_LOG(LogGenerateManifestCommandlet, Error, TEXT("Found manifest dependency %s is malformed. All manifest files should have a %s extension."), *ManifestDependency, *GenerateManifestHelper::GetManifestFileExtension());
+			return -1;
+		}
+	}
+	if (bRunningInPreview)
+	{
+		// Overwrite all the manifest dependency filenames with their preview counterparts 
+		for (FString& ManifestDependency : ManifestDependenciesList)
+		{
+			FString PreviewManifestDependency= GeneratePreviewManifestHelper::GetPreviewManifestFilename(ManifestDependency);
+			if (!FPaths::FileExists(PreviewManifestDependency))
+			{
+				UE_LOG(LogGenerateManifestCommandlet, Warning, TEXT("Preview manifest dependency %s does not exist. Make sure to generate all preview manifest dependencies of this localization target before trying again."), *PreviewManifestDependency);
+				continue;
+			}
+			// make sure the preview manifest file is newer than the regular manifest file
+			// we could be dealing with a scenario where the preview manifest file is from a previous run and isn't up to date or failed to be deleted
+			else if (IFileManager::Get().GetTimeStamp(*PreviewManifestDependency) < IFileManager::Get().GetTimeStamp(*ManifestDependency))
+			{
+				UE_LOG(LogGenerateManifestCommandlet, Warning, TEXT("Preview manifest dependency %s is older than the original manifest %s. Preview manifest is out of date and should be regenerated as a dependency."), *PreviewManifestDependency, *ManifestDependency);
+				continue;
+			}
+			ManifestDependency = MoveTemp(PreviewManifestDependency);
+		}
+	}
 
 	for (const FString& ManifestDependency : ManifestDependenciesList)
 	{
 		FText OutError;
 		if (!GatherManifestHelper->AddDependency(ManifestDependency, &OutError))
 		{
-			UE_LOG(LogGenerateManifestCommandlet, Error, TEXT("Failed to add a manifest dependency: %s"), *OutError.ToString());
+			UE_LOG(LogGenerateManifestCommandlet, Error, TEXT("Failed to add manifest dependency %s. Failure reason : %s"), *ManifestDependency, *OutError.ToString());
 			return -1;
 		}
 	}
@@ -92,3 +187,10 @@ int32 UGenerateGatherManifestCommandlet::Main( const FString& Params )
 
 	return 0;
 }
+
+bool UGenerateGatherManifestCommandlet::ShouldRunInPreview(const TArray<FString>& Switches, const TMap<FString, FString>& ParamVals) const
+{
+	// we need the commandlet to run to generate the preview manifests to avoid false positives from duplicate detection during preview runs 
+	return true;
+}
+
