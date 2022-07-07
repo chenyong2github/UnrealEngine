99aa5ad899d8bc7bceb40726bcc971d048c628ba
zach bethel <zach.bethel@epicgames.com>
2022-07-06 23:10:06 -0400

    Implemented support for asynchronous creation and deletion of FRenderResource. The resource list uses a thread local free list that is coalesced later.
    #preflight 62bf468c8d5e6787591b22e5
    #rb yuriy.odonnell
    #ushell-cherrypick of 20919518 by zach.bethel
    
    #ROBOMERGE-AUTHOR: zach.bethel
    #ROBOMERGE-SOURCE: CL 20968751 via CL 20969981 via CL 20977160
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v972-20964824)
    
    [CL 20979972 by zach bethel in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/SceneRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/SceneRendering.cpp
@@ -4057,6 +4057,7 @@ static void DeleteSceneRenderers(const TArray<FSceneRenderer*>& SceneRenderers, 
 	{
 		delete SceneRenderer;
 	}
+	FRenderResource::CoalesceResourceList();
 }
 
 static void WaitForTasksAndDeleteSceneRenderers(FRHICommandListImmediate& RHICmdList, const TArray<FSceneRenderer*>& SceneRenderers)
