6b9d8b03c9227cedcb0138be36db28c530adc7c9
daren cheng <daren.cheng@epicgames.com>
2022-07-07 03:51:32 -0400

    Temp revert changes to FMath::{Abs, Min, Max} due to undesirable interactions w/ multi-dimensional vector types. Will add back in changes after putting in type traits to check for this edge case.
    [FYI] fabian.giesen
    #preflight 62c60dcb7e606620fdcfa4f3
    
    #ROBOMERGE-AUTHOR: daren.cheng
    #ROBOMERGE-SOURCE: CL 20977345 via CL 20977723 via CL 20979601
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v972-20964824)
    
    [CL 20981390 by daren cheng in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/GenericPlatform/GenericPlatformMath.h
+++ Engine/Source/Runtime/Core/Public/GenericPlatform/GenericPlatformMath.h
@@ -919,7 +919,7 @@ struct FGenericPlatformMath
 	template< class T > 
 	static CONSTEXPR FORCEINLINE T Abs( const T A )
 	{
-		return (A < (T)0) ? -A : A;
+		return (A>=(T)0) ? A : -A;
 	}
 
 	/** Returns 1, 0, or -1 depending on relation of T to 0 */
@@ -933,14 +933,14 @@ struct FGenericPlatformMath
 	template< class T > 
 	static CONSTEXPR FORCEINLINE T Max( const T A, const T B )
 	{
-		return (B < A) ? A : B;
+		return (A>=B) ? A : B;
 	}
 
 	/** Returns lower value in a generic way */
 	template< class T > 
 	static CONSTEXPR FORCEINLINE T Min( const T A, const T B )
 	{
-		return (A < B) ? A : B;
+		return (A<=B) ? A : B;
 	}
 
 	// Allow mixing of float types to promote to highest precision type
