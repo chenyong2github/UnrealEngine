375c63008445926961d7b163217c2290cdb10b92
Chris Varnsverry <Chris.Varnsverry@xa.epicgames.com>
2022-07-14 12:58:23 -0400

    - Implement UserInfo for EOS
    #jira OI-3839
    #review-21058229 @Michael.Kirzinger @Alejandro.Aguilar @Sam.Zamani
    #preflight 62d047808a3c9910d149933c
    
    [CL 21092849 by Chris Varnsverry in ue5-main branch]

--- Engine/Plugins/Online/Experimental/OnlineServicesEOS/Source/Private/Online/OnlineServicesEOS.cpp
+++ Engine/Plugins/Online/Experimental/OnlineServicesEOS/Source/Private/Online/OnlineServicesEOS.cpp
@@ -3,9 +3,10 @@
 #include "Online/OnlineServicesEOS.h"
 
 #include "Online/AuthEOS.h"
+#include "Online/ExternalUIEOS.h"
 #include "Online/FriendsEOS.h"
 #include "Online/PresenceEOS.h"
-#include "Online/ExternalUIEOS.h"
+#include "Online/UserInfoEOS.h"
 
 namespace UE::Online {
 
@@ -17,9 +18,10 @@ FOnlineServicesEOS::FOnlineServicesEOS(FName InInstanceName)
 void FOnlineServicesEOS::RegisterComponents()
 {
 	Components.Register<FAuthEOS>(*this);
+	Components.Register<FExternalUIEOS>(*this);
 	Components.Register<FFriendsEOS>(*this);
 	Components.Register<FPresenceEOS>(*this);
-	Components.Register<FExternalUIEOS>(*this);
+	Components.Register<FUserInfoEOS>(*this);
 
 	Super::RegisterComponents();
 }
