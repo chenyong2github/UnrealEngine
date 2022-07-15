171a0d6b5a8e40cb14bbda54831f834f7ca6ceb0
simon tovey <Simon.Tovey@epicgames.com>
2022-07-15 12:23:41 -0400

    Update Cascade Emitter canvas client to use the newer InputKey functions.
    #jira UE-157886
    #rb benjamin.fox
    #preflight 62c853482823f28cf2798058
    #rnx
    #p4v-cherrypick 21009064
    
    #ROBOMERGE-OWNER: simon.tovey
    #ROBOMERGE-AUTHOR: simon.tovey
    #ROBOMERGE-SOURCE: CL 21109981 via CL 21110013 via CL 21110127
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v972-20964824)
    
    [CL 21111939 by simon tovey in ue5-main branch]

--- Engine/Source/Editor/Cascade/Private/CascadeEmitterCanvasClient.cpp
+++ Engine/Source/Editor/Cascade/Private/CascadeEmitterCanvasClient.cpp
@@ -272,10 +272,6 @@ void FCascadeEmitterCanvasClient::Draw(FViewport* InViewport, FCanvas* Canvas)
 
 bool FCascadeEmitterCanvasClient::InputKey(const FInputKeyEventArgs& EventArgs)
 {
-	return InputKey(EventArgs.Viewport, EventArgs.ControllerId, EventArgs.Key, EventArgs.Event, EventArgs.AmountDepressed, EventArgs.IsGamepad());
-}
-
-{
 	bool bHandled = false;
 
 	bool bLODIsValid = true;
@@ -514,7 +510,7 @@ bool FCascadeEmitterCanvasClient::InputKey(const FInputKeyEventArgs& EventArgs)
 				if (EventArgs.Key == EKeys::RightMouseButton)
 				{
 					OpenBackgroundMenu();
-				}
+			}
 			}
 
 		}
