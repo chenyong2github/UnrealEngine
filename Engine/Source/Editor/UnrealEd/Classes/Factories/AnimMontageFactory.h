ab8d5cae5cdb8cb88b4549719719a048d091307c
tuo chen <tuo.chen@epicgames.com>
2022-07-16 07:57:36 -0400

    Mark TargetSkeleton and SourceAnimation as BlueprintReadWrite in UAnimMontageFactory for python scripting #rb none #jira none #preflight skip
    [CL 21121861 by tuo chen in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Classes/Factories/AnimMontageFactory.h
+++ Engine/Source/Editor/UnrealEd/Classes/Factories/AnimMontageFactory.h
@@ -20,11 +20,11 @@ class UAnimMontageFactory : public UFactory
 {
 	GENERATED_UCLASS_BODY()
 
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite, Category="Scripting")
 	TObjectPtr<class USkeleton> TargetSkeleton;
 
 	/* Used when creating a montage from an AnimSequence, becomes the only AnimSequence contained */
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite, Category = "Scripting")
 	TObjectPtr<class UAnimSequence> SourceAnimation;
 
 	/** The preview mesh to use with this animation */
