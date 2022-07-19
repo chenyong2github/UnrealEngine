2bea89f4f7a6deafaa4d145546d8a300f0a946ee
adam kinge <adam.kinge@epicgames.com>
2022-07-18 23:31:53 -0400

    Remove commented out code for old iOS specific compile options.
    #jira UE-147048
    #review-21105857 @jack.porter
    #preflight 62d0f0737e1c26dcd7e95ace
    
    [CL 21157604 by adam kinge in ue5-main branch]

--- Engine/Source/UnrealGame.Target.cs
+++ Engine/Source/UnrealGame.Target.cs
@@ -13,18 +13,5 @@ public class UnrealGameTarget : TargetRules
 		BuildEnvironment = TargetBuildEnvironment.Shared;
 
 		ExtraModuleNames.Add("UnrealGame");
-
-		if (Target.Platform == UnrealTargetPlatform.IOS)
-		{
-			// to make iOS projects as small as possible we excluded some items from the engine.
-			// uncomment below to make a smaller iOS build
-			/*bCompileRecast = false;
-			bCompileSpeedTree = false;
-			bCompileAPEX = false;
-			bCompileLeanAndMeanUE = true;
-			bCompilePhysXVehicle = false;
-			bCompileFreeType = false;
-			bCompileForSize = true;*/
-		}
 	}
 }
