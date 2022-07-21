0c36c1d8e1c5229bb53044d6428ea0f7c9dad1bb
chris caulfield <Chris.Caulfield@epicgames.com>
2022-07-21 13:31:01 -0400

    RBAN - change warning to a log to prevent new CIS breaks
    #rb trivial
    
    #ROBOMERGE-AUTHOR: chris.caulfield
    #ROBOMERGE-SOURCE: CL 21202017 via CL 21202104 via CL 21202177
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v972-20964824)
    
    [CL 21204039 by chris caulfield in ue5-main branch]

--- Engine/Source/Runtime/AnimGraphRuntime/Private/BoneControllers/AnimNode_RigidBody.cpp
+++ Engine/Source/Runtime/AnimGraphRuntime/Private/BoneControllers/AnimNode_RigidBody.cpp
@@ -1779,7 +1779,7 @@ void FAnimNode_RigidBody::InitializeBoneReferences(const FBoneContainer& Require
 	{
 		// If the user specified a simulation root that is not used by the skelmesh, issue a warning 
 		// (FAnimNode_RigidBody::IsValidToEvaluate will return false and the simulation will not run)
-		UE_LOG(LogRBAN, Warning, TEXT("FAnimNode_RigidBody: RBAN Simulation Base Bone \'%s\' does not exist on SkeletalMesh %s."), *BaseBoneRef.BoneName.ToString(), *GetNameSafe(RequiredBones.GetSkeletalMeshAsset()));
+		UE_LOG(LogRBAN, Log, TEXT("FAnimNode_RigidBody: RBAN Simulation Base Bone \'%s\' does not exist on SkeletalMesh %s."), *BaseBoneRef.BoneName.ToString(), *GetNameSafe(RequiredBones.GetSkeletalMeshAsset()));
 	}
 
 	bool bHasInvalidBoneReference = false;
