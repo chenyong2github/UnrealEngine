04b3b22659ff0a4a63db749db88ada5acee74445
George Rolfe <george.rolfe@geotech.one>
2022-08-07 23:23:53 -0400

    MQTT plugin added to engine
    #jira UE-155100
    #rb none
    #preflight 62f079e0086f90bbc45c6c36
    
    [CL 21266224 by George Rolfe in ue5-main branch]

---
+++ Engine/Plugins/Protocols/MQTT/Source/MQTTCore/Private/MQTTSubsystem.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MQTTSubsystem.h"
+
+#include "JsonObjectWrapper.h"
+#include "MQTTClientObject.h"
+#include "MQTTClientSettings.h"
+#include "Engine/Engine.h"
+
+UMQTTClientObject* UMQTTSubsystem::GetOrCreateClient_WithProjectURL(UObject* InParent)
+{
+	const UMQTTClientSettings* Settings = GetDefault<UMQTTClientSettings>();
+	return GetOrCreateClient(InParent, Settings->DefaultURL);
+}
+
+UMQTTClientObject* UMQTTSubsystem::GetOrCreateClient(UObject* InParent, const FMQTTURL& InURL)
+{
+	const FGuid ClientId = InURL.ToGuid();
+	
+	UMQTTSubsystem* Self = GEngine->GetEngineSubsystem<UMQTTSubsystem>();
+	if(Self)
+	{
+		UMQTTClientObject* MQTTClientObject = Self->Clients.FindOrAdd(ClientId, NewObject<UMQTTClientObject>(InParent));
+
+		// Might exist but be destroyed
+		if(!MQTTClientObject || !IsValid(MQTTClientObject))
+		{
+			Self->Clients[ClientId] = MQTTClientObject = NewObject<UMQTTClientObject>(InParent);			
+		}
+
+		ensure(IsValid(MQTTClientObject));
+		
+		MQTTClientObject->Initialize(InURL);
+		return MQTTClientObject;
+	}
+
+	return nullptr;
+}
+
+FString UMQTTSubsystem::GetPayloadString(const FMQTTClientMessage& InClientMessage)
+{
+	return InClientMessage.GetPayloadAsString();
+}
+
+bool UMQTTSubsystem::GetPayloadJson(UObject* InParent, const FMQTTClientMessage& InClientMessage, FJsonObjectWrapper& OutJson)
+{
+	// @todo: fix
+	// TSharedPtr<FJsonObject> JsonObject;
+	// if(InClientMessage.GetPayloadAsJson(JsonObject))
+	// {
+	// 	return UJsonObject::FromNative(InParent, JsonObject);
+	// }
+
+	return false;
+}
