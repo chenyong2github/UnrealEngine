6ea46a21c17015ab219416e8258952a7396caa4c
jeremie roy <jeremie.roy@epicgames.com>
2022-08-09 13:22:01 -0400

    Add display name to media capture variable
    #rb trivial
    #jira none
    #preflight 62f28682bc175ec68ceb2865
    
    [CL 21292302 by jeremie roy in ue5-main branch]

--- Engine/Plugins/Media/MediaIOFramework/Source/MediaIOCore/Public/MediaCapture.h
+++ Engine/Plugins/Media/MediaIOFramework/Source/MediaIOCore/Public/MediaCapture.h
@@ -145,8 +145,8 @@ public:
 	/**
 	 * Whether to apply a linear to sRGB conversion to the texture before outputting. 
 	 */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "MediaCapture")
-	bool bApplyLinearToSRGBConversion = false;;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "MediaCapture", meta=(DisplayName="Apply linear to sRGB conversion"))
+	bool bApplyLinearToSRGBConversion = false;
 
 	/** Automatically stop capturing after a predetermined number of images. */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "MediaCapture")
