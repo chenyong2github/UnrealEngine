e0574ef0a1dfe5eba7e0488a406d3ae3e9bf18f5
ben hoffman <ben.hoffman@epicgames.com>
2022-08-12 11:37:51 -0400

    Make the GetOrCreateUser function on the slate app use the input device ID instead of the old User index.
    #jira none
    #rb Daren.Cheng
    #rnx
    #preflight 62dadc083122bf80ae454812
    
    [CL 21356093 by ben hoffman in ue5-main branch]

--- Engine/Source/Runtime/Slate/Public/Framework/Application/SlateApplication.h
+++ Engine/Source/Runtime/Slate/Public/Framework/Application/SlateApplication.h
@@ -1037,7 +1037,7 @@ protected:
 	 */
 	TSharedRef<FSlateUser> GetOrCreateUser(FInputDeviceId DeviceId);
 	
-	FORCEINLINE TSharedRef<FSlateUser> GetOrCreateUser(const FInputEvent& InputEvent) { return GetOrCreateUser(InputEvent.GetUserIndex()); }
+	FORCEINLINE TSharedRef<FSlateUser> GetOrCreateUser(const FInputEvent& InputEvent) { return GetOrCreateUser(InputEvent.GetInputDeviceId()); }
 
 	friend class FEventRouter;
 
