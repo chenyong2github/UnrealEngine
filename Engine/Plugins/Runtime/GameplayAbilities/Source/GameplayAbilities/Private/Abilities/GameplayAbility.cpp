2d49490793dbe93f2a1ec8e7867f7b8ee2b642ad
louisphilippe seguin <LouisPhilippe.Seguin@epicgames.com>
2022-08-16 16:54:36 -0400

    SubObject * Downgrade check that detected GameplayAbilities sending RPCS before registering into a warning
    #rb trivial
    
    #ROBOMERGE-AUTHOR: louisphilippe.seguin
    #ROBOMERGE-SOURCE: CL 21409759 via CL 21409763 via CL 21409780
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v975-21357124)
    
    [CL 21414070 by louisphilippe seguin in ue5-main branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Abilities/GameplayAbility.cpp
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Abilities/GameplayAbility.cpp
@@ -122,7 +122,7 @@ bool UGameplayAbility::CallRemoteFunction(UFunction* Function, void* Parameters,
 		{
 			// If the component is using the subobject list, detect subobjects that send RPC's before they are registered.
 			const bool bIsSubObjectRegistered = !AbilitySystemComponent->IsUsingRegisteredSubObjectList() || Owner->IsActorComponentReplicatedSubObjectRegistered(AbilitySystemComponent, this);
-			checkf(bIsSubObjectRegistered, TEXT("%s sent an RPC before it was registered as a subobject on %s::%s"), *GetName(), *Owner->GetName(), *AbilitySystemComponent->GetName());
+			UE_CLOG(!bIsSubObjectRegistered, LogAbilitySystem, Warning, TEXT("%s sent an RPC before it was registered as a subobject on %s::%s"), *GetName(), *Owner->GetName(), *AbilitySystemComponent->GetName());
 		}
 #endif
 		
