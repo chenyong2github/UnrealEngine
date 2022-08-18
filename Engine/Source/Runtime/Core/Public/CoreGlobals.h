439153b3ad9e57921e98d261a4a412ac862e0c5c
henrik karlsson <henrik.karlsson@epicgames.com>
2022-08-18 00:17:32 -0400

    [Runtime] * Fixed so UE_SCOPED_ENGINE_ACTIVITY works with empty variadic arguments
    #rb self
    #preflight trivial
    
    #ROBOMERGE-AUTHOR: henrik.karlsson
    #ROBOMERGE-SOURCE: CL 21432281 via CL 21437476 via CL 21437506 via CL 21437515
    #ROBOMERGE-BOT: UE5 (Release-Engine-Staging -> Main) (v981-21435820)
    
    [CL 21438209 by henrik karlsson in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/CoreGlobals.h
+++ Engine/Source/Runtime/Core/Public/CoreGlobals.h
@@ -89,7 +89,7 @@ struct CORE_API FEngineTrackedActivityScope
 
 
 #define SCOPED_BOOT_TIMING(x) TRACE_CPUPROFILER_EVENT_SCOPE_STR(x); FScopedBootTiming ANONYMOUS_VARIABLE(BootTiming_)(x);
-#define UE_SCOPED_ENGINE_ACTIVITY(Fmt, ...) FEngineTrackedActivityScope ANONYMOUS_VARIABLE(EngineActivity_)(Fmt, __VA_ARGS__);
+#define UE_SCOPED_ENGINE_ACTIVITY(Fmt, ...) FEngineTrackedActivityScope ANONYMOUS_VARIABLE(EngineActivity_)(Fmt, ## __VA_ARGS__);
 
 
 #define GLog GetGlobalLogSingleton()
