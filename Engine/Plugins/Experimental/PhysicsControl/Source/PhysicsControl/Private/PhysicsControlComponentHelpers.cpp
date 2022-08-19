23592ff93fb62f421b0e58a0a0b7eb537f07ee91
danny chapman <danny.chapman@epicgames.com>
2022-08-19 06:31:10 -0400

    Add missing copyright
    #rb trivial
    #preflight none
    
    [CL 21455310 by danny chapman in ue5-main branch]

--- Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControl/Private/PhysicsControlComponentHelpers.cpp
+++ Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControl/Private/PhysicsControlComponentHelpers.cpp
@@ -1,3 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "PhysicsControlComponentHelpers.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SkeletalMeshComponent.h"
