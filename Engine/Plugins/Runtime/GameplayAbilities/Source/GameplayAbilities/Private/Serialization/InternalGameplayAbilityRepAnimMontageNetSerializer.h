9b3f9f74797bd0f81e3175b3b412cc363ca0f78d
Mattias Hornlund <Mattias.Hornlund@epicgames.com>
2022-08-25 09:57:13 -0400

    Iris - Custom NetSerializer for GameplayAbilitiesRepAnimMontage
    #jira https://jira.it.epicgames.com/browse/UE-161666
    #rb John.Barret
    #preflight 63077d503f75e67a070668a7
    
    [CL 21563406 by Mattias Hornlund in ue5-main branch]

---
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Serialization/InternalGameplayAbilityRepAnimMontageNetSerializer.h
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Abilities/GameplayAbilityTypes.h"
+#include "InternalGameplayAbilityRepAnimMontageNetSerializer.generated.h"
+
+USTRUCT()
+struct FGameplayAbilityRepAnimMontageNetSerializerSerializationHelper : public FGameplayAbilityRepAnimMontage
+{
+	GENERATED_BODY()
+};
