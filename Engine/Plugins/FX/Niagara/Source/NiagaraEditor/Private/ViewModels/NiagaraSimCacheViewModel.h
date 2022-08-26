0d53b08b302ed806193dfaef7dbfdc4977732ec3
stu mckenna <Stu.McKenna@epicgames.com>
2022-08-26 17:41:03 -0400

    - IWYU fix for nonunity
    #rb trivial
    #rnx
    
    [CL 21599525 by stu mckenna in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/ViewModels/NiagaraSimCacheViewModel.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/ViewModels/NiagaraSimCacheViewModel.h
@@ -3,8 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NiagaraSimCache.h"
 
-class UNiagaraSimCache;
 struct FNiagaraSystemSimCacheCaptureReply;
 struct FNiagaraSimCacheDataBuffersLayout;
 
