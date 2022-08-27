086584dafae420ac04468fa4b5f9137150afd8b7
anidave <david.vadi.dris[at]animech.com>
2022-08-27 10:32:22 -0400

    [GLTFExporter] feat(orbit-camera): add class for actor version of the orbit camera
    commit 5c2fda8da486a63639bb62eed5145e43cd1626bc
    Author: David Vadi Dris <david.vadi.dris[at]animech.com>
    Date: Tue, 4 Aug 2020 12:17:15 +0200
    
        feat(orbit-camera): add class for actor version of the orbit camera
        (cherry picked from commit db76000babd2032a459adcc52244c0ef8716b937​)
    
    #preflight none
    #skipci
    
    [CL 21608627 by anidave in ue5-main branch]

---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporterRuntime/Public/Actors/GLTFOrbitCameraActor.h
@@ -0,0 +1,85 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Camera/CameraActor.h"
+#include "GLTFOrbitCameraActor.generated.h"
+
+/**
+ * GLTF-compatible camera that will carry over settings and simulate the behavior in the resulting viewer.
+ * Focuses one actor in the scene and orbits it through mouse control.
+ */
+UCLASS(BlueprintType, Blueprintable, DisplayName = "GLTF Orbit Camera Actor")
+class GLTFEXPORTERRUNTIME_API AGLTFOrbitCameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+	//~ Begin UObject Interface
+public:
+	AGLTFOrbitCameraActor(const FObjectInitializer& ObjectInitializer);
+	//~ End UObject Interface
+
+	//~ Begin AActor Interface
+protected:
+	virtual void BeginPlay() override;
+public:
+	virtual void Tick(float DeltaSeconds) override;
+	//~ End AActor Interface
+
+private:
+	UFUNCTION()
+	void OnMouseX(float AxisValue);
+
+	UFUNCTION()
+	void OnMouseY(float AxisValue);
+
+	UFUNCTION()
+	void OnMouseWheelAxis(float AxisValue);
+
+	float ClampDistance(float Value) const;
+	float ClampPitch(float Value) const;
+	float ClampYaw(float Value) const;
+	void RemoveInertia();
+
+public:
+	/* Actor which the camera will focus on and subsequently orbit. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GLTF Orbit Camera Actor")
+	AActor* Focus;
+
+	/* Closest distance the camera can approach the focused actor. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GLTF Orbit Camera Actor")
+	float DistanceMin;
+
+	/* Farthest distance the camera can recede from the focused actor. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GLTF Orbit Camera Actor")
+	float DistanceMax;
+
+	/* Minimum angle (in degrees) that the camera can pitch relative to the focused actor. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GLTF Orbit Camera Actor")
+	float PitchAngleMin;
+
+	/* Maximum angle (in degrees) that the camera can pitch relative to the focused actor. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GLTF Orbit Camera Actor")
+	float PitchAngleMax;
+
+	/* Deceleration that occurs after orbital movement. The higher the value, the longer it takes to settle. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GLTF Orbit Camera Actor")
+	float OrbitInertia;
+
+	/* Size of the orbital movement relative to user input. The higher the value, the faster it moves. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GLTF Orbit Camera Actor")
+	float OrbitSensitivity;
+	
+	/* Size of the dolly movement relative to user input. The higher the value, the faster it moves. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GLTF Orbit Camera Actor")
+	float DistanceSensitivity;
+
+private:
+	float Distance;
+	float Pitch;
+	float Yaw;
+	float TargetDistance;
+	float TargetPitch;
+	float TargetYaw;
+	float DollyTime;
+	float DollyStartDistance;
+};
