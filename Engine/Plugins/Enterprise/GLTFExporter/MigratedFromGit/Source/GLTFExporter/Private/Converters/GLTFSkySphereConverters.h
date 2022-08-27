e6bba676fcf5e17a7d5b684a15b9f0b741e305bf
Mistale <anders.gallon[at]animech.com>
2022-08-27 11:23:51 -0400

    [GLTFExporter] refactor(sky-sphere): refactor property conversion to reduce duplicated code
    commit 9255832ca7a7ed7ebac5d0a7a364a19b3c3249dc
    Author: Anders Gallon <anders.gallon[at]animech.com>
    Date: Wed, 14 Oct 2020 14:57:16 +0200
    
        refactor(sky-sphere): refactor property conversion to reduce duplicated code
        (cherry picked from commit 610349f6c8789e5dd21860e2b2d0036b80d0cde2​)
    
    #preflight none
    #skipci
    
    [CL 21610610 by Mistale in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFSkySphereConverters.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFSkySphereConverters.h
@@ -7,6 +7,8 @@
 #include "Converters/GLTFBuilderContext.h"
 #include "Engine.h"
 
+struct FGLTFJsonColor4;
+
 class FGLTFSkySphereConverter final : public FGLTFBuilderContext, public TGLTFConverter<FGLTFJsonSkySphereIndex, const AActor*>
 {
 	using FGLTFBuilderContext::FGLTFBuilderContext;
@@ -16,4 +18,10 @@ class FGLTFSkySphereConverter final : public FGLTFBuilderContext, public TGLTFCo
 	const UTexture2D* GetSkyTexture(const UMaterialInstance* SkyMaterial) const;
 	const UTexture2D* GetCloudsTexture(const UMaterialInstance* SkyMaterial) const;
 	const UTexture2D* GetStarsTexture(const UMaterialInstance* SkyMaterial) const;
+
+	template <class ValueType>
+	void ConvertProperty(const AActor& Actor, const TCHAR* PropertyName, const TCHAR* ExportedPropertyName, ValueType& OutValue) const;
+
+	void ConvertColorProperty(const AActor& Actor, const TCHAR* PropertyName, const TCHAR* ExportedPropertyName, FGLTFJsonColor4& OutValue) const;
+	void ConvertScalarParameter(const AActor& Actor, const UMaterialInstance* Material, const TCHAR* ParameterName, const TCHAR* ExportedPropertyName, float& OutValue) const;
 };
