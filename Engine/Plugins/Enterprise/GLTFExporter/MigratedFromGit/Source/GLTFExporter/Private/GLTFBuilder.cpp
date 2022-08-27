9d1ad49cd644be81bd7f4359f77f232119675b4e
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:35:13 -0400

    [GLTFExporter] refactor: add GLTFBufferBuilder
    commit e9fa64138852d7081f54b436132f9158bb5e0089
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 28 Apr 2020 13:36:24 +0200
    
        refactor: add GLTFBufferBuilder
        (cherry picked from commit 3de571db2a507ddb47839d2d15254ef456a83e03​)
    
    #preflight none
    #skipci
    
    [CL 21605680 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFBuilder.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFBuilder.cpp
@@ -5,39 +5,18 @@
 #include "Misc/Base64.h"
 
 FGLTFBuilder::FGLTFBuilder()
-	: MergedBufferIndex(JsonRoot.Buffers.AddDefaulted(1))
+	: BufferBuilder(JsonRoot)
 {
 }
 
 FGLTFJsonBufferViewIndex FGLTFBuilder::AddBufferView(const void* RawData, uint64 ByteLength, const FString& Name, EGLTFJsonBufferTarget BufferTarget)
 {
-	FGLTFJsonBufferView BufferView;
-	BufferView.Name = Name;
-	BufferView.Buffer = MergedBufferIndex;
-	BufferView.ByteOffset = MergedBufferData.Num();
-	BufferView.ByteLength = ByteLength;
-	BufferView.Target = BufferTarget;
-
-	MergedBufferData.Append(static_cast<const uint8*>(RawData), ByteLength);
-
-	return JsonRoot.BufferViews.Add(BufferView);
-}
-
-void FGLTFBuilder::UpdateMergedBuffer()
-{
-	FGLTFJsonBuffer& Buffer = JsonRoot.Buffers[MergedBufferIndex];
-	if (Buffer.ByteLength != MergedBufferData.Num())
-	{
-		FString DataBase64 = FBase64::Encode(MergedBufferData.GetData(), MergedBufferData.Num());
-
-		Buffer.URI = TEXT("data:application/octet-stream;base64,") + DataBase64;
-		Buffer.ByteLength = MergedBufferData.Num();
-	}
+	return BufferBuilder.AddBufferView(RawData, ByteLength, Name, BufferTarget);
 }
 
 void FGLTFBuilder::Serialize(FArchive& Archive)
 {
-	UpdateMergedBuffer();
+	BufferBuilder.UpdateMergedBuffer();
 	JsonRoot.Serialize(&Archive, true);
 }
 
