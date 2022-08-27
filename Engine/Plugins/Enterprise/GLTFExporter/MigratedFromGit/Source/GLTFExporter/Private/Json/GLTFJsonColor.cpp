8077069e205f6e439a87fae1c47a59ae159ef14e
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 12:37:58 -0400

    [GLTFExporter] fix: add template<> to avoid template specialization error in some compilers
    commit 48c97a444f08127fce89fe2245c842a4cc94fec3
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 21 Sep 2021 11:55:40 +0200
    
        fix: add template<> to avoid template specialization error in some compilers
        (cherry picked from commit 44c6ed2451d039d402750874fcb0986b81b36134​)
    
    #preflight none
    #skipci
    
    [CL 21613957 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonColor.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonColor.cpp
@@ -2,8 +2,8 @@
 
 #include "Json/GLTFJsonColor.h"
 
-const FGLTFJsonColor3 TGLTFJsonColor<FGLTFColor3>::Black({ 0, 0, 0 });
-const FGLTFJsonColor3 TGLTFJsonColor<FGLTFColor3>::White({ 1, 1, 1 });
+template <> const FGLTFJsonColor3 FGLTFJsonColor3::Black({ 0, 0, 0 });
+template <> const FGLTFJsonColor3 FGLTFJsonColor3::White({ 1, 1, 1 });
 
-const FGLTFJsonColor4 TGLTFJsonColor<FGLTFColor4>::Black({ 0, 0, 0, 1 });
-const FGLTFJsonColor4 TGLTFJsonColor<FGLTFColor4>::White({ 1, 1, 1, 1 });
+template <> const FGLTFJsonColor4 FGLTFJsonColor4::Black({ 0, 0, 0, 1 });
+template <> const FGLTFJsonColor4 FGLTFJsonColor4::White({ 1, 1, 1, 1 });
--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonVector.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonVector.cpp
@@ -2,11 +2,11 @@
 
 #include "Json/GLTFJsonVector.h"
 
-const FGLTFJsonVector2 TGLTFJsonVector<FGLTFVector2>::Zero({ 0, 0 });
-const FGLTFJsonVector2 TGLTFJsonVector<FGLTFVector2>::One ({ 1, 1 });
+template <> const FGLTFJsonVector2 FGLTFJsonVector2::Zero({ 0, 0 });
+template <> const FGLTFJsonVector2 FGLTFJsonVector2::One ({ 1, 1 });
 
-const FGLTFJsonVector3 TGLTFJsonVector<FGLTFVector3>::Zero({ 0, 0, 0 });
-const FGLTFJsonVector3 TGLTFJsonVector<FGLTFVector3>::One ({ 1, 1, 1 });
+template <> const FGLTFJsonVector3 FGLTFJsonVector3::Zero({ 0, 0, 0 });
+template <> const FGLTFJsonVector3 FGLTFJsonVector3::One ({ 1, 1, 1 });
 
-const FGLTFJsonVector4 TGLTFJsonVector<FGLTFVector4>::Zero({ 0, 0, 0, 0 });
-const FGLTFJsonVector4 TGLTFJsonVector<FGLTFVector4>::One ({ 1, 1, 1, 1 });
+template <> const FGLTFJsonVector4 FGLTFJsonVector4::Zero({ 0, 0, 0, 0 });
+template <> const FGLTFJsonVector4 FGLTFJsonVector4::One ({ 1, 1, 1, 1 });
