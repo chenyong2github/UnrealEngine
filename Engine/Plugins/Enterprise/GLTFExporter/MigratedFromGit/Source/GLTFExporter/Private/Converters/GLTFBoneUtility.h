03e3ba207862e9d0da95a35fd5e128205b31fa9b
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 13:04:16 -0400

    [GLTFExporter] refactor: move some animation sequence operations into utility functions
    commit d8eb163ac340593c1fda6ac659542d4ebabe4b07
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 18 Jan 2022 14:58:00 +0100
    
        refactor: move some animation sequence operations into utility functions
        (cherry picked from commit 3274b97d92e98f3e8c93f5abe48ff088aec22932​)
    
    #preflight none
    #skipci
    
    [CL 21615407 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFBoneUtility.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFBoneUtility.h
@@ -8,5 +8,9 @@ struct FGLTFBoneUtility
 {
 	static FTransform GetBindTransform(const FReferenceSkeleton& RefSkeleton, int32 BoneIndex);
 
-	static void InitializeToSkeleton(FBoneContainer& BoneContainer, const USkeleton* Skeleton);
+	static void GetFrameTimestamps(const UAnimSequence* AnimSequence, TArray<float>& OutFrameTimestamps);
+
+	static void GetBoneIndices(const USkeleton* Skeleton, TArray<FBoneIndexType>& OutBoneIndices);
+
+	static void GetBoneTransformsByFrame(const UAnimSequence* AnimSequence, const TArray<float>& FrameTimestamps, const TArray<FBoneIndexType>& BoneIndices, TArray<TArray<FTransform>>& OutBoneTransformsByFrame);
 };
