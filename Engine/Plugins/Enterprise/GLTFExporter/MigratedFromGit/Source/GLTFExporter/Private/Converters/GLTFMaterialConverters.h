9e10553928905e0e04c88c55401c48afb6c4ad30
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 09:02:50 -0400

    [GLTFExporter] feat(material): add initial limited texture support in material converter
    commit dd3ac97cc9e32a275de2315ddbd8da70fc750338
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Sun, 24 May 2020 23:53:09 +0200
    
        feat(material): add initial limited texture support in material converter
        (cherry picked from commit 743f502895521e2c8070bd0109bb4997b8518869​)
    
    #preflight none
    #skipci
    
    [CL 21606656 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFMaterialConverters.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFMaterialConverters.h
@@ -3,8 +3,7 @@
 #pragma once
 
 #include "Json/GLTFJsonIndex.h"
-#include "Json/GLTFJsonColor3.h"
-#include "Json/GLTFJsonColor4.h"
+#include "Json/GLTFJsonMaterial.h"
 #include "Converters/GLTFConverter.h"
 #include "Engine.h"
 
@@ -12,8 +11,14 @@ class FGLTFMaterialConverter final : public TGLTFConverter<FGLTFJsonMaterialInde
 {
 	FGLTFJsonMaterialIndex Add(FGLTFConvertBuilder& Builder, const FString& Name, const UMaterialInterface* Material) override;
 
+	bool TryGetSimpleBaseColorOpacity(FGLTFConvertBuilder& Builder, FGLTFJsonMaterial& OutValue, const FColorMaterialInput& BaseColorInput, const FScalarMaterialInput& OpacityInput, const UMaterialInstance* MaterialInstance) const;
+	bool TryGetSimpleMetallicRoughness(FGLTFConvertBuilder& Builder, FGLTFJsonPBRMetallicRoughness& OutValue, const FScalarMaterialInput& MetallicInput, const FScalarMaterialInput& RoughnessInput, const UMaterialInstance* MaterialInstance) const;
+
 	bool TryGetConstantColor(FGLTFJsonColor3& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
 	bool TryGetConstantColor(FGLTFJsonColor4& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
 	bool TryGetConstantColor(FLinearColor& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
 	bool TryGetConstantScalar(float& OutValue, const FScalarMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
+
+	bool TryGetSourceTexture(FGLTFConvertBuilder& Builder, FGLTFJsonTextureInfo& OutValue, const FExpressionInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
+	bool TryGetSourceTexture(const UTexture2D*& OutTexture, int32& OutTexCoord, int32& OutMask, const FExpressionInput& MaterialInput, const UMaterialInstance* MaterialInstance) const;
 };
