a71810537c0d4a7b188c0391fa405f0fa66d4b46
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:29:35 -0400

    [GLTFExporter] refactor: move populating options to own function
    commit d5ee23c563daa456c87854ef573f62a53aad3a6f
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Fri, 17 Apr 2020 12:59:22 +0200
    
        refactor: move populating options to own function
    
    #preflight none
    #skipci
    
    [CL 21605477 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/GLTFExporter.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/GLTFExporter.h
@@ -6,6 +6,7 @@
 #include "UObject/ObjectMacros.h"
 #include "UObject/UObjectGlobals.h"
 #include "Exporters/Exporter.h"
+#include "GLTFExportOptions.h"
 #include "GLTFExporter.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogGLTFExporter, Log, All);
@@ -22,4 +23,11 @@ public:
 	//~ Begin UExporter Interface
 	virtual bool ExportBinary(UObject* Object, const TCHAR* Type, FArchive& Ar, FFeedbackContext* Warn, int32 FileIndex = 0, uint32 PortFlags = 0) override;
 	//~ End UExporter Interface
+
+protected:
+
+	bool FillExportOptions();
+
+	UPROPERTY(transient)
+	UGLTFExportOptions* ExportOptions;
 };
