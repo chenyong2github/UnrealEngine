b951a57b682017d255a97252626715c674abf512
Mistale <anders.gallon[at]animech.com>
2022-08-27 08:42:31 -0400

    [GLTFExporter] refactor: use tab-indentation in .cs-file
    commit 931ade1812bf623e1695e44dc580fd5017b9acd7
    Author: Anders Gallon <anders.gallon[at]animech.com>
    Date: Wed, 13 May 2020 15:21:41 +0200
    
        refactor: use tab-indentation in .cs-file
        (cherry picked from commit 781a5fce45ebe85aecfe97f933a4317616137589​)
    
    #preflight none
    #skipci
    
    [CL 21605987 by Mistale in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/GLTFExporter.Build.cs
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/GLTFExporter.Build.cs
@@ -2,52 +2,52 @@
 
 namespace UnrealBuildTool.Rules
 {
-    public class GLTFExporter : ModuleRules
-    {
-        public GLTFExporter(ReadOnlyTargetRules Target) : base(Target)
-        {
-            PublicIncludePaths.AddRange(
-                new string[] {
-                }
-                );
+	public class GLTFExporter : ModuleRules
+	{
+		public GLTFExporter(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PublicIncludePaths.AddRange(
+				new string[] {
+				}
+				);
 
-            PrivateIncludePaths.AddRange(
-                new string[] {
-                }
-                );
+			PrivateIncludePaths.AddRange(
+				new string[] {
+				}
+				);
 
-            PublicDependencyModuleNames.AddRange(
-                new string[]
-                {
-                }
-                );
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+				}
+				);
 
-            PrivateDependencyModuleNames.AddRange(
-                new string[]
-                {
-                    "Core",
-                    "CoreUObject",
-                    "Engine",
-                    "UnrealEd",
-                    "MeshDescription",
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"UnrealEd",
+					"MeshDescription",
 					"StaticMeshDescription",
-                    "MeshUtilities",
-                    "MessageLog",
-                    "Json",
-                    "MaterialEditor",
-                    "Slate",
-                    "SlateCore",
-                    "Mainframe",
-                    "InputCore",
-                    "EditorStyle"
-                }
-                );
+					"MeshUtilities",
+					"MessageLog",
+					"Json",
+					"MaterialEditor",
+					"Slate",
+					"SlateCore",
+					"Mainframe",
+					"InputCore",
+					"EditorStyle"
+				}
+				);
 
-            DynamicallyLoadedModuleNames.AddRange(
-                new string[]
-                {
-                }
-                );
-        }
-    }
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+				}
+				);
+		}
+	}
 }
