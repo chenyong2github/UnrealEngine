1b69037ea4ca71a5a84dae445a08894c609e0038
anidave <david.vadi.dris[at]animech.com>
2022-08-27 10:00:38 -0400

    [GLTFExporter] feat(interaction-hotspot): simplify collision volume
    commit 2131fd81c4a2fe5a4222b86cff64474fd7ce1b09
    Author: David Vadi Dris <david.vadi.dris[at]animech.com>
    Date: Tue, 2 Jun 2020 16:40:32 +0200
    
        feat(interaction-hotspot): simplify collision volume
        (cherry picked from commit a9c8ce192a80e4127bf4a2411461b8489a21cbea​)
    
    #preflight none
    #skipci
    
    [CL 21607377 by anidave in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/Components/GLTFInteractionHotspotComponent.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/Components/GLTFInteractionHotspotComponent.h
@@ -11,32 +11,13 @@ class UBodySetup;
 class UTexture2D;
 
 /**
- * 
+ * A component to set up hotspots which appear as billboards and allow playback of skeletal animations when cursor input is enabled.
  */
 UCLASS(BlueprintType, Blueprintable, meta = (BlueprintSpawnableComponent), hidecategories = (Sprite), DisplayName = "GLTF Interaction Hotspot Component")
 class GLTFEXPORTER_API UGLTFInteractionHotspotComponent : public UBillboardComponent
 {
 	GENERATED_UCLASS_BODY()
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	ASkeletalMeshActor* SkeletalMeshActor;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UAnimSequence* AnimationSequence;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UTexture2D* DefaultSprite;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UTexture2D* HighlightSprite;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UTexture2D* ClickSprite;
-
-	/** Description of collision */
-	UPROPERTY(transient, duplicatetransient)
-	UBodySetup* ShapeBodySetup;
-
 	//~ Begin UObject Interface.
 #if WITH_EDITOR
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
@@ -50,6 +31,13 @@ protected:
 	virtual void OnRegister() override;
 	//~ End UActorComponent Interface.
 
+	//~ Begin UBillboardComponent Interface.
+public:
+	virtual void SetSprite(class UTexture2D* NewSprite) override;
+	//~ End UBillboardComponent Interface.
+
+	void SetRadius(float NewRadius);
+
 private:
 	UFUNCTION()
 	void BeginCursorOver(UPrimitiveComponent* TouchedComponent);
@@ -59,4 +47,31 @@ private:
 
 	UFUNCTION()
 	void Clicked(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed);
+
+	void UpdateCollisionVolume();
+	float GetBillboardBoundingRadius() const;
+	FTransform GetWorldTransform() const;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	ASkeletalMeshActor* SkeletalMeshActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	UAnimSequence* AnimationSequence;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	UTexture2D* DefaultSprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	UTexture2D* HighlightSprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	UTexture2D* ClickSprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|GLTF Interaction Hotspot")
+	float Radius;
+
+private:
+	UPROPERTY(transient, duplicatetransient)
+	UBodySetup* ShapeBodySetup;
 };
