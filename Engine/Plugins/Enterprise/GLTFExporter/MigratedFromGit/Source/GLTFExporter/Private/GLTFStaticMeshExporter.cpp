7da5d6de9d26419b4ed23a8a56210804d5c33a68
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:31:37 -0400

    [GLTFExporter] feat: initial json export of StaticMesh without any vertex data
    commit 7f6d82744db385f77e44ff70de1cfd903ec6e452
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Fri, 24 Apr 2020 08:11:29 +0200
    
        feat: initial json export of StaticMesh without any vertex data
    
    #preflight none
    #skipci
    
    [CL 21605523 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFStaticMeshExporter.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFStaticMeshExporter.cpp
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GLTFStaticMeshExporter.h"
+#include "GLTFJsonRoot.h"
 #include "Engine/StaticMesh.h"
 
 UGLTFStaticMeshExporter::UGLTFStaticMeshExporter(const FObjectInitializer& ObjectInitializer)
@@ -19,6 +20,35 @@ bool UGLTFStaticMeshExporter::ExportBinary(UObject* Object, const TCHAR* Type, F
 		return false;
 	}
 
-	// TODO: implement
+	FGLTFJsonRoot Root;
+
+	FGLTFJsonBuffer Buffer;
+	FGLTFJsonIndex BufferIndex = Root.Buffers.Add(Buffer);
+
+	FGLTFJsonBufferView BufferView;
+	BufferView.Buffer = BufferIndex;
+	FGLTFJsonIndex BufferViewIndex = Root.BufferViews.Add(BufferView);
+
+	FGLTFJsonAccessor Accessor;
+	Accessor.BufferView = BufferViewIndex;
+	FGLTFJsonIndex AccessorIndex = Root.Accessors.Add(Accessor);
+
+	FGLTFJsonMesh Mesh;
+	FGLTFJsonPrimitive Primitive;
+	Primitive.Attributes.Position = AccessorIndex;
+	Mesh.Primitives.Add(Primitive);
+	FGLTFJsonIndex MeshIndex = Root.Meshes.Add(Mesh);
+
+	FGLTFJsonNode Node;
+	Node.Mesh = MeshIndex;
+	FGLTFJsonIndex NodeIndex = Root.Nodes.Add(Node);
+
+	FGLTFJsonScene Scene;
+	Scene.Nodes.Add(NodeIndex);
+	FGLTFJsonIndex SceneIndex = Root.Scenes.Add(Scene);
+
+	Root.DefaultScene = SceneIndex;
+
+	Root.Serialize(&Archive, true);
 	return true;
 }
