76a37e3d91f5cf3299efd8cb2c0a619e29f32f13
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 12:43:41 -0400

    [GLTFExporter] fix: define plugin version info as preprocessors to avoid PluginManager (which isn't available in runtime)
    commit 463d5e5e8b17bc1c8064f049b2f09f4a8d54049e
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Sun, 31 Oct 2021 01:27:35 +0200
    
        fix: define plugin version info as preprocessors to avoid PluginManager (which isn't available in runtime)
        (cherry picked from commit f66eb8e4e3f7fc10b034f871c1ebe75aa37df9a8​)
    
    #preflight none
    #skipci
    
    [CL 21614273 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/GLTFExporter.Build.cs
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/GLTFExporter.Build.cs
@@ -6,6 +6,17 @@ namespace UnrealBuildTool.Rules
 	{
 		public GLTFExporter(ReadOnlyTargetRules Target) : base(Target)
 		{
+		    // NOTE: ugly hack to access plugin info (should propose change to engine)
+		    var BindingFlags = System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic;
+		    var FieldInfo = typeof(ModuleRules).GetField("Plugin", BindingFlags);
+			var Plugin = FieldInfo != null ? FieldInfo.GetValue(this) as PluginInfo : null;
+
+			if (Plugin != null && Plugin.Descriptor != null)
+			{
+				PrivateDefinitions.Add("GLTFEXPORTER_FRIENDLY_NAME=TEXT(\"" + Plugin.Descriptor.FriendlyName + "\")");
+				PrivateDefinitions.Add("GLTFEXPORTER_VERSION_NAME=TEXT(\"" + Plugin.Descriptor.VersionName + "\")");
+			}
+
 			PrivateDependencyModuleNames.AddRange(
 				new string[]
 				{
