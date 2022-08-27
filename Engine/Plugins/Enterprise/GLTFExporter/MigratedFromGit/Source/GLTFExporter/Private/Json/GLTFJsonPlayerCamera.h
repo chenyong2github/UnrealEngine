3718ad46d4ad7c5f5438917d60e8b9d56b3c1c00
Mistale <anders.gallon[at]animech.com>
2022-08-27 11:37:38 -0400

    [GLTFExporter] refactor(player-cameras): rename properties in extension to match camera-actor
    commit 8e8146926ba8c2b60127f152fd9f4c80f398ddf1
    Author: Anders Gallon <anders.gallon[at]animech.com>
    Date: Thu, 29 Oct 2020 09:31:22 +0100
    
        refactor(player-cameras): rename properties in extension to match camera-actor
        (cherry picked from commit 834e583e16617ce09c6e01bdf2b1a88d63b8dafe​)
    
    #preflight none
    #skipci
    
    [CL 21611318 by Mistale in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonPlayerCamera.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonPlayerCamera.h
@@ -10,11 +10,11 @@ struct FGLTFJsonPlayerCamera
 	FGLTFJsonNodeIndex Focus;
 	float              MaxDistance;
 	float              MinDistance;
-	float              MaxAngle;
-	float              MinAngle;
-	float              DistanceSensitivity;
-	float              OrbitSensitivity;
-	float              OrbitInertia;
+	float              MaxPitch;
+	float              MinPitch;
+	float              DollySensitivity;
+	float              RotationSensitivity;
+	float              RotationInertia;
 	float              DollyDuration;
 
 	template <class CharType = TCHAR, class PrintPolicy = TPrettyJsonPrintPolicy<CharType>>
@@ -25,11 +25,11 @@ struct FGLTFJsonPlayerCamera
 		JsonWriter.WriteValue(TEXT("focus"), Focus);
 		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("maxDistance"), MaxDistance);
 		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("minDistance"), MinDistance);
-		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("maxAngle"), MaxAngle);
-		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("minAngle"), MinAngle);
-		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("distanceSensitivity"), DistanceSensitivity);
-		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("orbitSensitivity"), OrbitSensitivity);
-		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("orbitInertia"), OrbitInertia);
+		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("maxPitch"), MaxPitch);
+		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("minPitch"), MinPitch);
+		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("dollySensitivity"), DollySensitivity);
+		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("rotationSensitivity"), RotationSensitivity);
+		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("rotationInertia"), RotationInertia);
 		FGLTFJsonUtility::WriteExactValue(JsonWriter, TEXT("dollyDuration"), DollyDuration);
 
 		JsonWriter.WriteObjectEnd();
