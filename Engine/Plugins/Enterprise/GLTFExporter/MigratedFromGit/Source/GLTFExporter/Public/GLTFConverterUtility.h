35993abe96c8d7afcf7b9d6aded5d60f4855073f
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:47:51 -0400

    [GLTFExporter] fix: conversion from UE endian-dependenct color layout to endian-independent glTF
    commit 3113537fea4df1e4aa8bf876460612951c492f63
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Sun, 17 May 2020 15:09:43 +0200
    
        fix: conversion from UE endian-dependenct color layout to endian-independent glTF
        (cherry picked from commit 829f77db65697bc3af65fd6cbbdb449f39566a72​)
    
    #preflight none
    #skipci
    
    [CL 21606183 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/GLTFConverterUtility.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/GLTFConverterUtility.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "GLTFColor.h"
 #include "Engine/EngineTypes.h"
 
 struct GLTFEXPORTER_API FGLTFConverterUtility
@@ -34,9 +35,10 @@ struct GLTFEXPORTER_API FGLTFConverterUtility
 		return FVector4(ConvertVector(Tangent), 1.0f);
 	}
 
-	static inline FColor ConvertColor(const FColor& Color)
+	static inline FGLTFColor ConvertColor(const FColor& Color)
 	{
-		// UE4 uses ABGR while glTF uses RGBA.
+		// UE4 uses ABGR or ARGB depending on endianness.
+		// glTF always uses RGBA independent of endianness.
 		return { Color.R, Color.G, Color.B, Color.A };
 	}
 
