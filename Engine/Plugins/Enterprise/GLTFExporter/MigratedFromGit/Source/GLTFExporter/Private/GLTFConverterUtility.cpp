b9742312cd97068aea34f775eb39bf65a3443fff
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:40:42 -0400

    [GLTFExporter] refactor: move conversion utilities to own class
    commit bc3feda90f910c57474c9403e67a6e4fee99982b
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Mon, 11 May 2020 21:24:40 +0200
    
        refactor: move conversion utilities to own class
        (cherry picked from commit 6860233e806e151a77d5638aa93b841a2ffbd3dc​)
    
    #preflight none
    #skipci
    
    [CL 21605907 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFConverterUtility.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFConverterUtility.cpp
@@ -1,14 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "GLTFConversionUtilities.h"
+#include "GLTFConverterUtility.h"
 
-bool IsSkySphereBlueprint(const UBlueprint* Blueprint)
+bool FGLTFConverterUtility::IsSkySphereBlueprint(const UBlueprint* Blueprint)
 {
 	static const UBlueprint* SkySphere = LoadObject<UBlueprint>(nullptr, TEXT("/Engine/EngineSky/BP_Sky_Sphere.BP_Sky_Sphere"), nullptr, LOAD_None, nullptr);
 	return Blueprint != nullptr && Blueprint == SkySphere;
 }
 
-bool IsHDRIBackdropBlueprint(const UBlueprint* Blueprint)
+bool FGLTFConverterUtility::IsHDRIBackdropBlueprint(const UBlueprint* Blueprint)
 {
 	static const UBlueprint* HDRIBackdrop = LoadObject<UBlueprint>(nullptr, TEXT("/HDRIBackdrop/Blueprints/HDRIBackdrop.HDRIBackdrop"), nullptr, LOAD_None, nullptr);
 	return Blueprint != nullptr && Blueprint == HDRIBackdrop;
