63417da8cc1208502ccc60c6efbcbf9bbaa217ad
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 11:33:02 -0400

    [GLTFExporter] refactor: warn about non-constrained aspect-ratio for orthographic camera
    commit 8bc7160cba5fcf6bae1e51566a15fb4ec40ac939
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Sun, 25 Oct 2020 15:55:29 +0100
    
        refactor: warn about non-constrained aspect-ratio for orthographic camera
        (cherry picked from commit c2cbd03175f4a8464f312ed80c4961b4f87920c5​)
    
    #preflight none
    #skipci
    
    [CL 21611073 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFCameraConverters.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFCameraConverters.cpp
@@ -4,7 +4,6 @@
 #include "Builders/GLTFContainerBuilder.h"
 #include "Converters/GLTFConverterUtility.h"
 #include "Converters/GLTFNameUtility.h"
-#include "Converters/GLTFCameraUtility.h"
 #include "Actors/GLTFCameraActor.h"
 
 FGLTFJsonCameraIndex FGLTFCameraConverter::Convert(const UCameraComponent* CameraComponent)
@@ -15,15 +14,30 @@ FGLTFJsonCameraIndex FGLTFCameraConverter::Convert(const UCameraComponent* Camer
 
 	FMinimalViewInfo DesiredView;
 	const_cast<UCameraComponent*>(CameraComponent)->GetCameraView(0, DesiredView);
+	const float ExportScale = Builder.ExportOptions->ExportScale;
 
 	switch (Camera.Type)
 	{
 		case EGLTFJsonCameraType::Orthographic:
-			Camera.Orthographic = FGLTFCameraUtility::ConvertOrthographic(DesiredView, Builder.ExportOptions->ExportScale);
+			if (!DesiredView.bConstrainAspectRatio)
+			{
+				Builder.AddWarningMessage(FString::Printf(TEXT("Aspect ratio for orthographic camera component %s (in actor %s) will be constrainted in glTF"), *CameraComponent->GetName(), *CameraComponent->GetOwner()->GetName()));
+			}
+			Camera.Orthographic.XMag = FGLTFConverterUtility::ConvertLength(DesiredView.OrthoWidth, ExportScale);
+			Camera.Orthographic.YMag = FGLTFConverterUtility::ConvertLength(DesiredView.OrthoWidth / DesiredView.AspectRatio, ExportScale); // TODO: is this correct?
+			Camera.Orthographic.ZFar = FGLTFConverterUtility::ConvertLength(DesiredView.OrthoFarClipPlane, ExportScale);
+			Camera.Orthographic.ZNear = FGLTFConverterUtility::ConvertLength(DesiredView.OrthoNearClipPlane, ExportScale);
 			break;
 
 		case EGLTFJsonCameraType::Perspective:
-			Camera.Perspective = FGLTFCameraUtility::ConvertPerspective(DesiredView, Builder.ExportOptions->ExportScale);
+			if (DesiredView.bConstrainAspectRatio)
+			{
+				Camera.Perspective.AspectRatio = DesiredView.AspectRatio;
+			}
+			Camera.Perspective.YFov = FGLTFConverterUtility::ConvertFieldOfView(DesiredView.FOV, DesiredView.AspectRatio);
+			// NOTE: even thought ZFar is optional, if we don't set it, then most gltf viewers won't handle it well.
+			Camera.Perspective.ZFar = FGLTFConverterUtility::ConvertLength(WORLD_MAX, ExportScale); // TODO: Unreal doesn't have max draw distance per view?
+			Camera.Perspective.ZNear = FGLTFConverterUtility::ConvertLength(GNearClippingPlane, ExportScale);
 			break;
 
 		case EGLTFJsonCameraType::None:
@@ -44,8 +58,8 @@ FGLTFJsonCameraIndex FGLTFCameraConverter::Convert(const UCameraComponent* Camer
 		{
 			FGLTFJsonOrbitCamera OrbitCamera;
 			OrbitCamera.Focus = Builder.GetOrAddNode(CameraActor->Focus);
-			OrbitCamera.MaxDistance = FGLTFConverterUtility::ConvertLength(CameraActor->DistanceMax, Builder.ExportOptions->ExportScale);
-			OrbitCamera.MinDistance = FGLTFConverterUtility::ConvertLength(CameraActor->DistanceMin, Builder.ExportOptions->ExportScale);
+			OrbitCamera.MaxDistance = FGLTFConverterUtility::ConvertLength(CameraActor->DistanceMax, ExportScale);
+			OrbitCamera.MinDistance = FGLTFConverterUtility::ConvertLength(CameraActor->DistanceMin, ExportScale);
 			OrbitCamera.MaxAngle = CameraActor->PitchAngleMax;
 			OrbitCamera.MinAngle = CameraActor->PitchAngleMin;
 			OrbitCamera.DistanceSensitivity = CameraActor->DistanceSensitivity;
