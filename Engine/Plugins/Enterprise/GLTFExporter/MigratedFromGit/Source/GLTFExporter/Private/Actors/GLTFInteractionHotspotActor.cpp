4f9afe61efdc56d70916d088eafdc430d3b8343e
anidave <david.vadi.dris[at]animech.com>
2022-08-27 10:02:34 -0400

    [GLTFExporter] refactor(interaction-hotspot): Expose interaction hotspot properties in actor directly via component rather than manual forwarding
    commit 70725bc65417c7826c197aa1f5f59f5730cfdba3
    Author: David Vadi Dris <david.vadi.dris[at]animech.com>
    Date: Wed, 17 Jun 2020 09:10:50 +0200
    
        refactor(interaction-hotspot): Expose interaction hotspot properties in actor directly via component rather than manual forwarding
        (cherry picked from commit e0bbe7f2a501497ae9cc3d3e9b57ab923a650a5d​)
    
    #preflight none
    #skipci
    
    [CL 21607459 by anidave in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Actors/GLTFInteractionHotspotActor.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Actors/GLTFInteractionHotspotActor.cpp
@@ -3,50 +3,8 @@
 #include "Actors/GLTFInteractionHotspotActor.h"
 
 AGLTFInteractionHotspotActor::AGLTFInteractionHotspotActor(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer),
-	DefaultSprite(nullptr),
-	HighlightSprite(nullptr),
-	ToggledSprite(nullptr)
+	: Super(ObjectInitializer)
 {
-	// TODO: Figure out how to make hotspot component the root without affecting its collision volume
-	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
-	RootComponent = SceneComponent;
-
 	InteractionHotspotComponent = CreateDefaultSubobject<UGLTFInteractionHotspotComponent>(TEXT("InteractionHotspotComponent"));
-	InteractionHotspotComponent->SetupAttachment(SceneComponent);
-	ForwardPropertiesToComponent();
-}
-
-#if WITH_EDITOR
-void AGLTFInteractionHotspotActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-
-	ForwardPropertiesToComponent();
-}
-#endif // WITH_EDITOR
-
-void AGLTFInteractionHotspotActor::ForwardPropertiesToComponent()
-{
-	// TODO: Forward full transform to component
-	if (InteractionHotspotComponent->Animations != Animations)
-	{
-		InteractionHotspotComponent->Animations = Animations;
-	}
-	
-	if (InteractionHotspotComponent->DefaultSprite != DefaultSprite)
-	{
-		InteractionHotspotComponent->DefaultSprite = DefaultSprite;
-		InteractionHotspotComponent->SetSprite(DefaultSprite);
-	}
-	
-	if (InteractionHotspotComponent->HighlightSprite != HighlightSprite)
-	{
-		InteractionHotspotComponent->HighlightSprite = HighlightSprite;
-	}
-	
-	if (InteractionHotspotComponent->ToggledSprite != ToggledSprite)
-	{
-		InteractionHotspotComponent->ToggledSprite = ToggledSprite;
-	}
+	RootComponent = InteractionHotspotComponent;
 }
