d2837fc6cfc8b836360ce848db1033142849577a
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:59:45 -0400

    [GLTFExporter] feat(material): add support for material instance parameter lookup
    commit f1ea6c275b9e270906e53d5853abbc8e686ac937
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 19 May 2020 05:00:08 +0200
    
        feat(material): add support for material instance parameter lookup
        (cherry picked from commit 6cfa398bfcc42423721aeb00ee2a1f74ac8fa5aa​)
    
    #preflight none
    #skipci
    
    [CL 21606558 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFMaterialConverters.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFMaterialConverters.cpp
@@ -16,102 +16,209 @@ FGLTFJsonMaterialIndex FGLTFMaterialConverter::Add(FGLTFConvertBuilder& Builder,
 	FGLTFJsonMaterial JsonMaterial;
 	JsonMaterial.Name = Name;
 
+	JsonMaterial.AlphaMode = FGLTFConverterUtility::ConvertBlendMode(MaterialInterface->GetBlendMode());
+	JsonMaterial.AlphaCutoff = MaterialInterface->GetOpacityMaskClipValue();
+	JsonMaterial.DoubleSided = MaterialInterface->IsTwoSided();
+
 	const UMaterial* Material = MaterialInterface->GetMaterial();
-	JsonMaterial.PBRMetallicRoughness.BaseColorFactor = FGLTFConverterUtility::ConvertColor(GetConstantColor(Material->BaseColor));
-	JsonMaterial.PBRMetallicRoughness.MetallicFactor = GetConstantScalar(Material->Metallic);
-	JsonMaterial.PBRMetallicRoughness.RoughnessFactor = GetConstantScalar(Material->Roughness);
-	JsonMaterial.EmissiveFactor = FGLTFConverterUtility::ConvertColor(GetConstantColor(Material->EmissiveColor));
+	const UMaterialInstance* MaterialInstance = Cast<UMaterialInstance>(MaterialInterface);
 
-	JsonMaterial.AlphaMode = FGLTFConverterUtility::ConvertBlendMode(Material->BlendMode);
-	if (JsonMaterial.AlphaMode == EGLTFJsonAlphaMode::Blend)
+	if (!TryGetConstantColor(JsonMaterial.PBRMetallicRoughness.BaseColorFactor, Material->BaseColor, MaterialInstance))
 	{
-		JsonMaterial.PBRMetallicRoughness.BaseColorFactor.A = GetConstantScalar(Material->Opacity);
-	}
-	else if (JsonMaterial.AlphaMode == EGLTFJsonAlphaMode::Mask)
-	{
-		JsonMaterial.PBRMetallicRoughness.BaseColorFactor.A = GetConstantScalar(Material->OpacityMask);
+		// TODO: temporary fallback until texture and baking support
+		JsonMaterial.PBRMetallicRoughness.BaseColorFactor = FGLTFConverterUtility::ConvertColor(FLinearColor(Material->BaseColor.Constant));
 	}
 
-	JsonMaterial.AlphaCutoff = Material->OpacityMaskClipValue;
-	JsonMaterial.DoubleSided = Material->TwoSided;
+	if (JsonMaterial.AlphaMode == EGLTFJsonAlphaMode::Blend || JsonMaterial.AlphaMode == EGLTFJsonAlphaMode::Mask)
+	{
+		const FScalarMaterialInput& OpacityInput = JsonMaterial.AlphaMode == EGLTFJsonAlphaMode::Blend ? Material->Opacity : Material->OpacityMask;
+		if (!TryGetConstantScalar(JsonMaterial.PBRMetallicRoughness.BaseColorFactor.A, OpacityInput, MaterialInstance))
+		{
+			// TODO: temporary fallback until texture and baking support
+			JsonMaterial.PBRMetallicRoughness.BaseColorFactor.A = OpacityInput.Constant;
+		}
+	}
+
+	if (!TryGetConstantScalar(JsonMaterial.PBRMetallicRoughness.MetallicFactor, Material->Metallic, MaterialInstance))
+	{
+		// TODO: temporary fallback until texture and baking support
+		JsonMaterial.PBRMetallicRoughness.MetallicFactor = Material->Metallic.Constant;
+	}
+
+	if (!TryGetConstantScalar(JsonMaterial.PBRMetallicRoughness.RoughnessFactor, Material->Roughness, MaterialInstance))
+	{
+		// TODO: temporary fallback until texture and baking support
+		JsonMaterial.PBRMetallicRoughness.RoughnessFactor = Material->Roughness.Constant;
+	}
+
+	if (!TryGetConstantColor(JsonMaterial.EmissiveFactor, Material->EmissiveColor, MaterialInstance))
+	{
+		// TODO: temporary fallback until texture and baking support
+		JsonMaterial.EmissiveFactor = FGLTFConverterUtility::ConvertColor(FLinearColor(Material->EmissiveColor.Constant));
+	}
 
 	return Builder.AddMaterial(JsonMaterial);
 }
 
-FLinearColor FGLTFMaterialConverter::GetConstantColor(const FColorMaterialInput& MaterialInput) const
+bool FGLTFMaterialConverter::TryGetConstantColor(FGLTFJsonColor3& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const
 {
-	const UMaterialExpression* Expression = MaterialInput.Expression;
-	if (Expression != nullptr)
+	FLinearColor Value;
+	if (TryGetConstantColor(Value, MaterialInput, MaterialInstance))
 	{
-		if (const UMaterialExpressionVectorParameter* VectorParameter = Cast<UMaterialExpressionVectorParameter>(Expression))
-		{
-			return VectorParameter->DefaultValue;
-		}
-
-		if (const UMaterialExpressionScalarParameter* ScalarParameter = Cast<UMaterialExpressionScalarParameter>(Expression))
-		{
-			return { ScalarParameter->DefaultValue, ScalarParameter->DefaultValue, ScalarParameter->DefaultValue };
-		}
-
-		if (const UMaterialExpressionConstant4Vector* Constant4Vector =Cast<UMaterialExpressionConstant4Vector>(Expression))
-		{
-			return Constant4Vector->Constant;
-		}
-
-		if (const UMaterialExpressionConstant3Vector* Constant3Vector = Cast<UMaterialExpressionConstant3Vector>(Expression))
-		{
-			return Constant3Vector->Constant;
-		}
-
-		if (const UMaterialExpressionConstant2Vector* Constant2Vector =Cast<UMaterialExpressionConstant2Vector>(Expression))
-		{
-			return { Constant2Vector->R, Constant2Vector->G, 0 }; // TODO: is this case ever possible?
-		}
-
-		if (const UMaterialExpressionConstant* Constant = Cast<UMaterialExpressionConstant>(Expression))
-		{
-			return { Constant->R, Constant->R, Constant->R };
-		}
+		OutValue = FGLTFConverterUtility::ConvertColor(Value);
+		return true;
 	}
 
-	return MaterialInput.Constant;
+	return false;
 }
 
-float FGLTFMaterialConverter::GetConstantScalar(const FScalarMaterialInput& MaterialInput) const
+bool FGLTFMaterialConverter::TryGetConstantColor(FGLTFJsonColor4& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const
 {
-	const UMaterialExpression* Expression = MaterialInput.Expression;
-	if (Expression != nullptr)
+	FLinearColor Value;
+	if (TryGetConstantColor(Value, MaterialInput, MaterialInstance))
 	{
-		if (const UMaterialExpressionVectorParameter* VectorParameter = Cast<UMaterialExpressionVectorParameter>(Expression))
-		{
-			return VectorParameter->DefaultValue.R;
-		}
-
-		if (const UMaterialExpressionScalarParameter* ScalarParameter = Cast<UMaterialExpressionScalarParameter>(Expression))
-		{
-			return ScalarParameter->DefaultValue;
-		}
-
-		if (const UMaterialExpressionConstant4Vector* Constant4Vector =Cast<UMaterialExpressionConstant4Vector>(Expression))
-		{
-			return Constant4Vector->Constant.R;
-		}
-
-		if (const UMaterialExpressionConstant3Vector* Constant3Vector = Cast<UMaterialExpressionConstant3Vector>(Expression))
-		{
-			return Constant3Vector->Constant.R;
-		}
-
-		if (const UMaterialExpressionConstant2Vector* Constant2Vector =Cast<UMaterialExpressionConstant2Vector>(Expression))
-		{
-			return Constant2Vector->R;
-		}
-
-		if (const UMaterialExpressionConstant* Constant = Cast<UMaterialExpressionConstant>(Expression))
-		{
-			return Constant->R;
-		}
+		OutValue = FGLTFConverterUtility::ConvertColor(Value);
+		return true;
 	}
 
-	return MaterialInput.Constant;
+	return false;
+}
+
+bool FGLTFMaterialConverter::TryGetConstantColor(FLinearColor& OutValue, const FColorMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const
+{
+	const UMaterialExpression* Expression = MaterialInput.Expression;
+	if (Expression == nullptr)
+	{
+		OutValue = MaterialInput.Constant;
+		return true;
+	}
+
+	if (const UMaterialExpressionVectorParameter* VectorParameter = Cast<UMaterialExpressionVectorParameter>(Expression))
+	{
+		FLinearColor Value = VectorParameter->DefaultValue;
+
+		if (MaterialInstance != nullptr)
+		{
+			const FHashedMaterialParameterInfo ParameterInfo(VectorParameter->GetParameterName());
+			if (!MaterialInstance->GetVectorParameterValue(ParameterInfo, Value))
+			{
+				// TODO: should not happen
+			}
+		}
+
+		OutValue = Value;
+		return true;
+	}
+
+	if (const UMaterialExpressionScalarParameter* ScalarParameter = Cast<UMaterialExpressionScalarParameter>(Expression))
+	{
+		float Value = ScalarParameter->DefaultValue;
+
+		if (MaterialInstance != nullptr)
+		{
+			const FHashedMaterialParameterInfo ParameterInfo(ScalarParameter->GetParameterName());
+			if (!MaterialInstance->GetScalarParameterValue(ParameterInfo, Value))
+			{
+				// TODO: should not happen
+			}
+		}
+
+		OutValue = { Value, Value, Value, Value };
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant4Vector* Constant4Vector =Cast<UMaterialExpressionConstant4Vector>(Expression))
+	{
+		OutValue = Constant4Vector->Constant;
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant3Vector* Constant3Vector = Cast<UMaterialExpressionConstant3Vector>(Expression))
+	{
+		OutValue = Constant3Vector->Constant;
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant2Vector* Constant2Vector =Cast<UMaterialExpressionConstant2Vector>(Expression))
+	{
+		OutValue = { Constant2Vector->R, Constant2Vector->G, 0, 0 }; // TODO: is this case ever possible?
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant* Constant = Cast<UMaterialExpressionConstant>(Expression))
+	{
+		OutValue = { Constant->R, Constant->R, Constant->R, Constant->R };
+		return true;
+	}
+
+	return false;
+}
+
+bool FGLTFMaterialConverter::TryGetConstantScalar(float& OutValue, const FScalarMaterialInput& MaterialInput, const UMaterialInstance* MaterialInstance) const
+{
+	const UMaterialExpression* Expression = MaterialInput.Expression;
+	if (Expression == nullptr)
+	{
+		return false;
+	}
+
+	if (const UMaterialExpressionVectorParameter* VectorParameter = Cast<UMaterialExpressionVectorParameter>(Expression))
+	{
+		FLinearColor Value = VectorParameter->DefaultValue;
+
+		if (MaterialInstance != nullptr)
+		{
+			const FHashedMaterialParameterInfo ParameterInfo(VectorParameter->GetParameterName());
+			if (!MaterialInstance->GetVectorParameterValue(ParameterInfo, Value))
+			{
+				// TODO: should not happen
+			}
+		}
+
+		OutValue = Value.R;
+		return true;
+	}
+
+	if (const UMaterialExpressionScalarParameter* ScalarParameter = Cast<UMaterialExpressionScalarParameter>(Expression))
+	{
+		float Value = ScalarParameter->DefaultValue;
+
+		if (MaterialInstance != nullptr)
+		{
+			const FHashedMaterialParameterInfo ParameterInfo(ScalarParameter->GetParameterName());
+			if (!MaterialInstance->GetScalarParameterValue(ParameterInfo, Value))
+			{
+				// TODO: should not happen
+			}
+		}
+
+		OutValue = Value;
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant4Vector* Constant4Vector =Cast<UMaterialExpressionConstant4Vector>(Expression))
+	{
+		OutValue = Constant4Vector->Constant.R;
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant3Vector* Constant3Vector = Cast<UMaterialExpressionConstant3Vector>(Expression))
+	{
+		OutValue = Constant3Vector->Constant.R;
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant2Vector* Constant2Vector =Cast<UMaterialExpressionConstant2Vector>(Expression))
+	{
+		OutValue = Constant2Vector->R; // TODO: is this case ever possible?
+		return true;
+	}
+
+	if (const UMaterialExpressionConstant* Constant = Cast<UMaterialExpressionConstant>(Expression))
+	{
+		OutValue = Constant->R;
+		return true;
+	}
+
+	return false;
 }
