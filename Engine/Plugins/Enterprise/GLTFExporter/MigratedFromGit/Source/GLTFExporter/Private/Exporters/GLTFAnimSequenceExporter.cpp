53b08aa1b1cbe3c65e7c995104312ae95cfe00f7
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 11:01:29 -0400

    [GLTFExporter] feat(skeletal-animations): implement animation sequence exporter
    commit 4fbe5ad1d35ab5516c01c29f1a35e5ae1713085d
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Thu, 11 Jun 2020 10:26:51 +0200
    
        feat(skeletal-animations): implement animation sequence exporter
        (cherry picked from commit 8595a1a86eedf8a4663760a03d8221353aaf5a79​)
    
    #preflight none
    #skipci
    
    [CL 21609804 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Exporters/GLTFAnimSequenceExporter.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Exporters/GLTFAnimSequenceExporter.cpp
@@ -1,18 +1,40 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Exporters/GLTFAnimSequenceExporter.h"
+#include "Exporters/GLTFExporterUtility.h"
+#include "Builders/GLTFContainerBuilder.h"
 #include "Animation/AnimSequence.h"
 
 UGLTFAnimSequenceExporter::UGLTFAnimSequenceExporter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	// TODO: uncomment when support is implemented
-	// SupportedClass = UAnimSequence::StaticClass();
+	SupportedClass = UAnimSequence::StaticClass();
 }
 
 bool UGLTFAnimSequenceExporter::AddObject(FGLTFContainerBuilder& Builder, const UObject* Object)
 {
 	const UAnimSequence* AnimSequence = CastChecked<UAnimSequence>(Object);
-	// TODO: implement
+
+	const USkeletalMesh* SkeletalMesh = FGLTFExporterUtility::GetPreviewMesh(AnimSequence);
+	if (SkeletalMesh == nullptr)
+	{
+		// report error
+		return false;
+	}
+
+	const FGLTFJsonMeshIndex MeshIndex = Builder.GetOrAddMesh(SkeletalMesh);
+
+	FGLTFJsonNode Node;
+	Node.Mesh = MeshIndex;
+	const FGLTFJsonNodeIndex NodeIndex = Builder.AddNode(Node);
+	Builder.GetNode(NodeIndex).Skin = Builder.GetOrAddSkin(NodeIndex, SkeletalMesh);
+
+	Builder.GetOrAddAnimation(NodeIndex, SkeletalMesh, AnimSequence);
+
+	FGLTFJsonScene Scene;
+	Scene.Nodes.Add(NodeIndex);
+	const FGLTFJsonSceneIndex SceneIndex = Builder.AddScene(Scene);
+
+	Builder.DefaultScene = SceneIndex;
 	return true;
 }
