7f1dcc38c004414e11cfc53ecf5718238a50d148
aidinabedi <aidin.abedi[at]animech.com>
2022-08-27 08:58:18 -0400

    [GLTFExporter] feat(material): add GLTFJsonColor3 and GLTFJsonColor4
    commit d332d71b00f7f696ff62c4d33929a9a2069b6485
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 19 May 2020 00:07:46 +0200
    
        feat(material): add GLTFJsonColor3 and GLTFJsonColor4
        (cherry picked from commit e6b2bcff0a8497a63b09170634324914228585de​)
    
    #preflight none
    #skipci
    
    [CL 21606511 by aidinabedi in ue5-main branch]

---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonColor4.h
@@ -0,0 +1,51 @@
+// CopGright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Json/GLTFJsonColor3.h"
+#include "Serialization/JsonSerializer.h"
+
+struct FGLTFJsonColor4
+{
+	float R, G, B, A;
+
+	static const FGLTFJsonColor4 Black;
+	static const FGLTFJsonColor4 White;
+
+	FGLTFJsonColor4(float R, float G, float B, float A)
+        : R(R), G(G), B(B), A(A)
+	{
+	}
+
+	FGLTFJsonColor4(const FGLTFJsonColor3& Color3, float A)
+        : R(Color3.R), G(Color3.G), B(Color3.B), A(A)
+	{
+	}
+
+	template <class CharType = TCHAR, class PrintPolicy = TPrettyJsonPrintPolicy<CharType>>
+    void WriteArray(TJsonWriter<CharType, PrintPolicy>& JsonWriter) const
+	{
+		JsonWriter.WriteArrayStart();
+		JsonWriter.WriteValue(R);
+		JsonWriter.WriteValue(G);
+		JsonWriter.WriteValue(B);
+		JsonWriter.WriteValue(A);
+		JsonWriter.WriteArrayEnd();
+	}
+
+	bool operator==(const FGLTFJsonColor4& Other) const
+	{
+		return R == Other.R
+            && G == Other.G
+            && B == Other.B
+            && A == Other.A;
+	}
+
+	bool operator!=(const FGLTFJsonColor4& Other) const
+	{
+		return R != Other.R
+            || G != Other.G
+            || B != Other.B
+            || A != Other.A;
+	}
+};
