b3b3f65317634816decb765b33619a57dc3e0006
aidinabedi <aidinabedi@gmail.com>
2022-08-28 05:25:13 -0400

    [GLTFExporter] fix: add template<> to avoid template specialization error in some compilers
    commit 48c97a444f08127fce89fe2245c842a4cc94fec3
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 21 Sep 2021 11:55:40 +0200
    
        fix: add template<> to avoid template specialization error in some compilers
        (cherry picked from commit 44c6ed2451d039d402750874fcb0986b81b36134​)
    
    #preflight none
    #skipci
    
    [CL 21638436 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonMatrix.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonMatrix.cpp
@@ -2,20 +2,20 @@
 
 #include "Json/GLTFJsonMatrix.h"
 
-const FGLTFJsonMatrix2 TGLTFJsonMatrix<FGLTFMatrix2>::Identity
+template <> const FGLTFJsonMatrix2 FGLTFJsonMatrix2::Identity
 ({
 	1, 0,
 	0, 1
 });
 
-const FGLTFJsonMatrix3 TGLTFJsonMatrix<FGLTFMatrix3>::Identity
+template <> const FGLTFJsonMatrix3 FGLTFJsonMatrix3::Identity
 ({
 	1, 0, 0,
 	0, 1, 0,
 	0, 0, 1
 });
 
-const FGLTFJsonMatrix4 TGLTFJsonMatrix<FGLTFMatrix4>::Identity
+template <> const FGLTFJsonMatrix4 FGLTFJsonMatrix4::Identity
 ({
 	1, 0, 0, 0,
 	0, 1, 0, 0,
