a653ea2a445bd1ae85b87a0e2565deb36ed3e1e3
Mistale <anders.gallon@animech.com>
2022-08-28 06:13:57 -0400

    [GLTFExporter] chore: add custom shader for rendering individual cubemap faces
    commit 756292aeae8b23d8a8c8ff7e8b0fc907dbc3d41f
    Author: Anders Gallon <anders.gallon[at]animech.com>
    Date: Wed, 2 Feb 2022 15:18:51 +0100
    
        chore: add custom shader for rendering individual cubemap faces
        (cherry picked from commit bb3d1ec76e6fb3b70e59b16acab822b6f1b6f3a2​)
    
    #preflight none
    #skipci
    
    [CL 21640117 by Mistale in ue5-main branch]

---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Converters/GLTFCubemapFacePreview.h
@@ -0,0 +1,33 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BatchedElements.h"
+
+class FGLTFCubemapFacePreview : public FBatchedElementParameters
+{
+
+public:
+
+	FGLTFCubemapFacePreview(FRHITexture* TextureCube, int32 CubeFaceIndex)
+		: TextureCube(TextureCube)
+		, CubeFaceIndex(CubeFaceIndex)
+	{
+	}
+
+	/** Binds vertex and pixel shaders for this element */
+	virtual void BindShaders(
+		FRHICommandList& RHICmdList,
+		FGraphicsPipelineStateInitializer& GraphicsPSOInit,
+		ERHIFeatureLevel::Type InFeatureLevel,
+		const FMatrix& InTransform,
+		const float InGamma,
+		const FMatrix& ColorWeights,
+		const FTexture* Texture) override;
+
+private:
+
+	FRHITexture* TextureCube;
+	int32 CubeFaceIndex;
+};
