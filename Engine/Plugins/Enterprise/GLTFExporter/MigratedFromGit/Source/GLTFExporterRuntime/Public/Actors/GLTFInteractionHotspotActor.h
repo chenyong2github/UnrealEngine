f7989136e03da07e1cf4c6b8a2ad7cf7b24337f9
aidinabedi <aidinabedi@gmail.com>
2022-08-28 03:12:54 -0400

    [GLTFExporter] refactor(hotspot): add blueptint specifiers to UCLASS in actor and tweak HideCategories (doesn't seem to affect the actor though)
    commit dfb2631605ba7c2f87d62c07e83b5157e8946462
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Wed, 24 Jun 2020 23:24:10 +0200
    
        refactor(hotspot): add blueptint specifiers to UCLASS in actor and tweak HideCategories (doesn't seem to affect the actor though)
        (cherry picked from commit a90fc40e3ab0d5cd87d84f8f9238cb90d85e25e1​)
        # Conflicts from cherry-pick:
        #	Plugins/GLTFExporter/Source/GLTFExporterRuntime/Public/Actors/GLTFInteractionHotspotActor.h
        #	Plugins/GLTFExporter/Source/GLTFExporterRuntime/Public/Components/GLTFInteractionHotspotComponent.h
    
    #preflight none
    #skipci
    
    [CL 21633108 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporterRuntime/Public/Actors/GLTFInteractionHotspotActor.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporterRuntime/Public/Actors/GLTFInteractionHotspotActor.h
@@ -9,7 +9,7 @@
 /**
  * Actor wrapper for the GLTF hotspot component. Appears as a billboard and allows playback of skeletal animations when cursor input is enabled.
  */
-UCLASS(DisplayName = "GLTF Interaction Hotspot Actor", HideCategories = (Sprite))
+UCLASS(BlueprintType, Blueprintable, HideCategories = (Sprite, Physics, Collision, Navigation), DisplayName = "GLTF Interaction Hotspot Actor")
 class GLTFEXPORTERRUNTIME_API AGLTFInteractionHotspotActor : public AActor
 {
 	GENERATED_BODY()
