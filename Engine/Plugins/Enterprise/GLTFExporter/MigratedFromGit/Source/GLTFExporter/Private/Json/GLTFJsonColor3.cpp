69d218b85d3076613459c30e9027d9e76f164455
aidinabedi <aidinabedi@gmail.com>
2022-08-28 02:52:41 -0400

    [GLTFExporter] feat(material): add GLTFJsonColor3 and GLTFJsonColor4
    commit d332d71b00f7f696ff62c4d33929a9a2069b6485
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Tue, 19 May 2020 00:07:46 +0200
    
        feat(material): add GLTFJsonColor3 and GLTFJsonColor4
        (cherry picked from commit e6b2bcff0a8497a63b09170634324914228585de​)
    
    #preflight none
    #skipci
    
    [CL 21632301 by aidinabedi in ue5-main branch]

---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonColor3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Json/GLTFJsonColor3.h"
+
+const FGLTFJsonColor3 FGLTFJsonColor3::Black(0, 0, 0);
+const FGLTFJsonColor3 FGLTFJsonColor3::White(1, 1, 1);
---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Json/GLTFJsonColor4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Json/GLTFJsonColor4.h"
+
+const FGLTFJsonColor4 FGLTFJsonColor4::Black(0, 0, 0, 1);
+const FGLTFJsonColor4 FGLTFJsonColor4::White(1, 1, 1, 1);
