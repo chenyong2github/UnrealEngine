66b12f0ed42911fec9850dfaba083823fc793bc0
aidinabedi <aidinabedi@gmail.com>
2022-08-28 02:34:17 -0400

    [GLTFExporter] feat(level): add partially implemented GLTFSceneBuilder and GLTFNodeBuilder with some debug logging
    commit 2167927cda7c95c202e6cc348923d0543659f1cb
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Sun, 10 May 2020 18:37:16 +0200
    
        feat(level): add partially implemented GLTFSceneBuilder and GLTFNodeBuilder with some debug logging
        (cherry picked from commit ed32d7183f6750f17c9ad576a3cf8af0f289fab6​)
    
    #preflight none
    #skipci
    
    [CL 21631617 by aidinabedi in ue5-main branch]

---
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Public/GLTFSceneBuilder.h
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GLTFJsonIndex.h"
+#include "GLTFContainerBuilder.h"
+#include "Engine.h"
+
+struct GLTFEXPORTER_API FGLTFNodeBuilder
+{
+	FString Name;
+
+	const USceneComponent* SceneComponent;
+
+	bool bTopLevel;
+
+	TArray<FGLTFNodeBuilder> AttachedComponents;
+	
+	FGLTFNodeBuilder(const USceneComponent* SceneComponent, bool bSelectedOnly, bool bTopLevel = false);
+
+	FGLTFJsonNodeIndex AddNode(FGLTFContainerBuilder& Container);
+
+	void _DebugLog();
+};
+
+struct GLTFEXPORTER_API FGLTFSceneBuilder
+{
+	FString Name;
+
+	TArray<FGLTFNodeBuilder> TopLevelComponents;
+
+	FGLTFSceneBuilder(const UWorld* World, bool bSelectedOnly);
+
+	FGLTFJsonSceneIndex AddScene(FGLTFContainerBuilder& Container);
+
+	void _DebugLog();
+};
