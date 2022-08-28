a889cddab9e6395c3e4c41564f3500ac743876f1
aidinabedi <aidinabedi@gmail.com>
2022-08-28 03:02:50 -0400

    [GLTFExporter] refactor(texture): generalize FGLTFImageBuilder::AddImage to support more EPixelFormat
    commit 87070611d857edca75903323f195317cf597c84d
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Mon, 8 Jun 2020 22:48:44 +0200
    
        refactor(texture): generalize FGLTFImageBuilder::AddImage to support more EPixelFormat
        (cherry picked from commit 9fc51cb41d3ffb04fd7c62f21f67f61af15e4a29​)
    
    #preflight none
    #skipci
    
    [CL 21632687 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Builders/GLTFImageBuilder.h
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/Builders/GLTFImageBuilder.h
@@ -12,7 +12,7 @@ public:
 
 	FGLTFJsonImageIndex AddImage(const void* RawData, int64 ByteLength, int32 Width, int32 Height, ERGBFormat RawFormat, int32 BitDepth, bool bFloatFormat, const FString& Name = TEXT(""), EGLTFJsonMimeType MimeType = EGLTFJsonMimeType::PNG, int32 Quality = 100);
 
-	FGLTFJsonImageIndex AddImage(const TArray<FColor>& Pixels, int32 Width, int32 Height, EPixelFormat PixelFormat = EPixelFormat::PF_B8G8R8A8, const FString& Name = TEXT(""), EGLTFJsonMimeType MimeType = EGLTFJsonMimeType::PNG, int32 Quality = 100);
+	FGLTFJsonImageIndex AddImage(const void* RawData, int64 ByteLength, int32 Width, int32 Height, EPixelFormat PixelFormat = EPixelFormat::PF_B8G8R8A8, const FString& Name = TEXT(""), EGLTFJsonMimeType MimeType = EGLTFJsonMimeType::PNG, int32 Quality = 100);
 
 	FGLTFJsonImageIndex AddImage(const FTextureSource& Image, const FString& Name = TEXT(""), EGLTFJsonMimeType MimeType = EGLTFJsonMimeType::PNG, int32 Quality = 100);
 
