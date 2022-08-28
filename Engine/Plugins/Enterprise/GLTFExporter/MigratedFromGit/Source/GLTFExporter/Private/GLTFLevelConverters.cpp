7768f43af7342bd32186307aff2657b9e9cb1f35
aidinabedi <aidinabedi@gmail.com>
2022-08-28 02:41:49 -0400

    [GLTFExporter] refactor(level): remove old commented include
    commit 49ccf77e834c4ff85356aa9020cf211f1b06ad47
    Author: Aidin Abedi <aidin.abedi[at]animech.com>
    Date: Wed, 13 May 2020 17:24:49 +0200
    
        refactor(level): remove old commented include
        (cherry picked from commit 94ec583d22e13398bc64bae4f36a2bfdc732485d​)
    
    #preflight none
    #skipci
    
    [CL 21631909 by aidinabedi in ue5-main branch]

--- Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFLevelConverters.cpp
+++ Engine/Plugins/Enterprise/GLTFExporter/MigratedFromGit/Source/GLTFExporter/Private/GLTFLevelConverters.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-//#include "GLTFLevelConverter.h"
-
 #include "GLTFLevelConverters.h"
 #include "GLTFContainerBuilder.h"
 #include "GLTFConverterUtility.h"
