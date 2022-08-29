5b3af1dc9f22d95ea4f731e7c987a2ce55bb5033
ionut matasaru <ionut.matasaru@epicgames.com>
2022-08-29 03:48:31 -0400

    Updated icon for UnrealTraceServer.exe
    #jira UE-157650
    #rb Martin.Ridgers
    #preflight skip
    
    [CL 21664702 by ionut matasaru in ue5-main branch]

--- Engine/Source/Programs/UnrealTraceServer/src/Icon/make_ico.py
+++ Engine/Source/Programs/UnrealTraceServer/src/Icon/make_ico.py
@@ -1,11 +1,15 @@
+# Copyright Epic Games, Inc. All Rights Reserved.
+
 from PIL import Image
 
-images = []
-sizes = [255, 128, 64, 32, 48, 16]
-for size in sizes:
-    print(f"Loading ue_{size}.png")
-    image = Image.open(f"ue_{size}.png")
-    images.append(image)
+def MakeIcon(prefix, output):
+    sizes = [256, 64, 48, 32, 24, 16]
+    images = []
+    for size in sizes:
+        print(f"Loading {prefix}_{size}.png")
+        image = Image.open(f"{prefix}_{size}.png")
+        images.append(image)
+    print(f"Creating {output}")
+    images[0].save(f"{output}", append_images=images)
 
-print("Creating ../Unreal.ico")
-images[0].save("../Unreal.ico", append_images=images)
+MakeIcon("icon", "icon.ico")
