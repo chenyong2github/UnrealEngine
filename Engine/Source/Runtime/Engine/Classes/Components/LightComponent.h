99f87d07d6bc49e4afc6135ed72caa1b6d447363
Charles deRousiers <Charles.deRousiers@epicgames.com>
2022-08-30 16:57:01 -0400

    When a local light use an IES profile and use the IES profile intensity, ensures user can't change the light unit to avoid invalid brightness conversion.
    #rb none
    #jira UE-80444
    #preflight 630e7376e54ec9d5818edad8
    
    [CL 21709401 by Charles deRousiers in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/LightComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/LightComponent.h
@@ -167,7 +167,7 @@ class ENGINE_API ULightComponent : public ULightComponentBase
 	TObjectPtr<class UTextureLightProfile> IESTexture;
 
 	/** true: take light brightness from IES profile, false: use the light brightness - the maximum light in one direction is used to define no masking. Use with InverseSquareFalloff. Will be disabled if a valid IES profile texture is not supplied. */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=LightProfiles, meta=(DisplayName = "Use IES Intensity"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=LightProfiles, meta=(DisplayName = "Use IES Intensity", EditCondition="IESTexture!=nullptr"))
 	uint32 bUseIESBrightness : 1;
 
 	/** Global scale for IES brightness contribution. Only available when "Use IES Brightness" is selected, and a valid IES profile texture is set */
