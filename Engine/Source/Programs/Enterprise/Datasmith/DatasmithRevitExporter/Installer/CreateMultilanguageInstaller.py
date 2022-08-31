8b68b5d56e1bf1962bc8fa11c64a6dc0ff4e5081
JeanLuc Corenthin <JeanLuc.Corenthin@epicgames.com>
2022-08-31 09:17:59 -0400

    Remove non-inclusive words from exporters' installer
    #jira UE-158635, UE-158636, UE-158637, UE-158639
    #rb none
    #preflight 630e8445660db81edb9e29ec
    
    [CL 21719725 by JeanLuc Corenthin in ue5-main branch]

--- Engine/Source/Programs/Enterprise/Datasmith/DatasmithRevitExporter/Installer/CreateMultilanguageInstaller.py
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithRevitExporter/Installer/CreateMultilanguageInstaller.py
@@ -19,21 +19,21 @@ en_msi_path = en_msi_dir/msi_name
 
 assert en_msi_path.is_file(), f"Expected to see 'en-us' msi in {en_msi_path}"
 
-master_msi_dir = build_dir/'Master'
-master_msi_path = master_msi_dir/msi_name
+final_msi_dir = build_dir/'Final'
+final_msi_path = final_msi_dir/msi_name
 
-master_msi_dir.mkdir(parents=True, exist_ok=True)
+final_msi_dir.mkdir(parents=True, exist_ok=True)
 
-shutil.copy(en_msi_dir/msi_name, master_msi_path)
+shutil.copy(en_msi_dir/msi_name, final_msi_path)
 
-assert master_msi_path.is_file(), f"Expected to see master msi copied from root culture msi '{master_msi_path}'"
+assert final_msi_path.is_file(), f"Expected to see master msi copied from root culture msi '{final_msi_path}'"
 
 def add_language(culture_name, culture_code):
     cmd = [
         str(scripts_dir/'CreateAndEmbedLangTransform.bat'),
         str(en_msi_dir), msi_name, 
         culture_name, str(culture_code),
-        str(master_msi_dir)]
+        str(final_msi_dir)]
     subprocess.check_call(cmd)
 
 for l in 'de-de:1031, ko-kr:1042, es-es:1034, fr-fr:1036, ja-jp:1041, pt-pt:2070, zh-cn:2052'.split(', '):
