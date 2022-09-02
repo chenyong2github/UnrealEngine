38402319bf35474de0bb7766ed8e29a0a5b303c0
Tim Smith <tim.smith@epicgames.com>
2022-09-02 10:01:50 -0400

    Fixed minor issue with LiveCoding/HotReload where the first character of the old class name would be removed.
    #rb self
    #preflight 63120963f51ef182ab741e45
    
    [CL 21758413 by Tim Smith in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/DeferredRegistry.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/DeferredRegistry.cpp
@@ -28,8 +28,6 @@ void ReloadProcessObject(UEnum* Enum, const TCHAR* RenamePrefix)
 
 void ReloadProcessObject(UClass* Class, const TCHAR* RenamePrefix)
 {
-	FString NameWithoutPrefix = UObjectBase::RemoveClassPrefix(*Class->GetName());
-
 	// Make sure enums de-register their names BEFORE we create the new class, otherwise there will be name conflicts.
 	// This needs to be done before renaming the class (moving it to transient package) as enum names are stored with the package name
 	TArray<UObject*> ClassSubobjects;
@@ -47,7 +45,7 @@ void ReloadProcessObject(UClass* Class, const TCHAR* RenamePrefix)
 	Class->ClearFlags(RF_Standalone | RF_Public);
 	Class->GetDefaultObject()->RemoveFromRoot();
 	Class->GetDefaultObject()->ClearFlags(RF_Standalone | RF_Public);
-	const FName OldClassRename = MakeUniqueObjectName(GetTransientPackage(), Class->GetClass(), *FString::Printf(TEXT("%s_%s"), RenamePrefix, *NameWithoutPrefix));
+	const FName OldClassRename = MakeUniqueObjectName(GetTransientPackage(), Class->GetClass(), *FString::Printf(TEXT("%s_%s"), RenamePrefix, *Class->GetName()));
 	Class->Rename(*OldClassRename.ToString(), GetTransientPackage());
 	Class->SetFlags(RF_Transient);
 	Class->AddToRoot();
