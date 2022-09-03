789ab719ba432e8d8b17cd64c693b86b56c381cf
zach brockway <zach.brockway@epicgames.com>
2022-09-03 00:15:34 -0400

    Switchboard: Bump Listener version to 2.2.0 to reflect recent UGS sync support additions.
    [FYI] Mike.Beach
    
    [CL 21775349 by zach brockway in ue5-main branch]

--- Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard/devices/unreal/version_helpers.py
+++ Engine/Plugins/VirtualProduction/Switchboard/Source/Switchboard/switchboard/devices/unreal/version_helpers.py
@@ -3,7 +3,7 @@
 import typing
 
 
-LISTENER_COMPATIBLE_VERSION = (2,1)
+LISTENER_COMPATIBLE_VERSION = (2,2)
 
 # Version where redeploy support was initially introduced.
 LISTENER_MIN_REDEPLOY_VERSION = (1,4,0)
