cd4dda93e3947fb2178ba41ed6406c16c7fd4f70
sondra moyls <sondra.moyls@epicgames.com>
2022-09-08 21:30:04 -0400

    Tooltip fixup #preflight trivial
    [CL 21910528 by sondra moyls in ue5-main branch]

--- Engine/Source/Runtime/AudioMixer/Public/AudioMixerBlueprintLibrary.h
+++ Engine/Source/Runtime/AudioMixer/Public/AudioMixerBlueprintLibrary.h
@@ -343,7 +343,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Audio|Effects", meta = (WorldContext = "WorldContextObject"))
 	static void AddSourceEffectToPresetChain(const UObject* WorldContextObject, USoundEffectSourcePresetChain* PresetChain, FSourceEffectChainEntry Entry);
 
-	/** Adds source effect entry to preset chain. Only affects the instance of preset chain. */
+	/** Removes source effect entry from preset chain. Only affects the instance of preset chain. */
 	UFUNCTION(BlueprintCallable, Category = "Audio|Effects", meta = (WorldContext = "WorldContextObject"))
 	static void RemoveSourceEffectFromPresetChain(const UObject* WorldContextObject, USoundEffectSourcePresetChain* PresetChain, int32 EntryIndex);
 
