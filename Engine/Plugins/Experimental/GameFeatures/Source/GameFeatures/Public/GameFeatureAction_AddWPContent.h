2419a84481c2dcc408c12e4f70c00a29e5d0c31d
philippe deseve <philippe.deseve@epicgames.com>
2022-09-14 11:36:08 -0400

    Add a GameFeatureAction to inject a content bundle
    #rb richard.malo
    
    [CL 22008221 by philippe deseve in ue5-main branch]

---
+++ Engine/Plugins/Experimental/GameFeatures/Source/GameFeatures/Public/GameFeatureAction_AddWPContent.h
@@ -0,0 +1,38 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeatureAction.h"
+
+#include "GameFeatureAction_AddWPContent.generated.h"
+
+class UGameFeatureData;
+class IPlugin;
+class FContentBundleClient;
+class UContentBundleDescriptor;
+
+/**
+ *
+ */
+UCLASS(meta = (DisplayName = "Add World Partition Content"))
+class UGameFeatureAction_AddWPContent : public UGameFeatureAction
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	//~ Begin UGameFeatureAction interface
+	virtual void OnGameFeatureRegistering() override;
+	virtual void OnGameFeatureUnregistering() override;
+	virtual void OnGameFeatureActivating() override;
+	virtual void OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context) override;
+	//~ End UGameFeatureAction interface
+
+	UGameFeatureData* GetGameFeatureData() const;
+
+private:
+	UPROPERTY(EditAnywhere, Category = ContentBundle)
+	TObjectPtr<UContentBundleDescriptor> ContentBundleDescriptor;
+
+	TSharedPtr<FContentBundleClient> ContentBundleClient;
+};
