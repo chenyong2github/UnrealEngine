551f8202402c134f3ffa1e4d4efb768561a78b88
Joakim Lindqvist <Joakim.Lindqvist@epicgames.com>
2022-09-15 06:03:44 -0400

    Horde Storage - re-added fallback for the internal jupiter namespace so that we do not have to configure it. It can still be explicitly overriden should we want to.
    #preflight none
    
    [CL 22025936 by Joakim Lindqvist in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeStorage/Jupiter.Common/Implementation/NamespacePolicyResolver.cs
+++ Engine/Source/Programs/Horde/HordeStorage/Jupiter.Common/Implementation/NamespacePolicyResolver.cs
@@ -15,10 +15,15 @@ namespace Jupiter.Common
     public class NamespacePolicyResolver : INamespacePolicyResolver
     {
         private readonly IOptionsMonitor<NamespaceSettings> _namespaceSettings;
+        private readonly NamespacePolicy _internalNamespacePolicy;
 
         public NamespacePolicyResolver(IOptionsMonitor<NamespaceSettings> namespaceSettings)
         {
             _namespaceSettings = namespaceSettings;
+            _internalNamespacePolicy = new NamespacePolicy()
+            {
+                StoragePool = "persistent"
+            };
         }
 
         public NamespacePolicy GetPoliciesForNs(NamespaceId ns)
@@ -28,12 +33,18 @@ namespace Jupiter.Common
             {
                 return settings;
             }
-
+            
             // attempt to find the default mapping
             if (_namespaceSettings.CurrentValue.Policies.TryGetValue("*",
                     out NamespacePolicy? defaultSettings))
             {
                 return defaultSettings;
+            }
+
+            // if no override has been specified for the internal jupiter namespace then we default it
+            if (ns == INamespacePolicyResolver.JupiterInternalNamespace)
+            {
+                return _internalNamespacePolicy;
             }
 
             throw new UnknownNamespaceException($"Unable to find a valid policy for namespace {ns}");
