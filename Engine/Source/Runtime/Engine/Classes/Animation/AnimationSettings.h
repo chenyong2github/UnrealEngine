daefba4ad5488de9b1e6407040767da53e8ae33f
lucas dower <lucas.dower@epicgames.com>
2022-09-16 12:45:05 -0400

    Deprecate unused bRaiseMaxErrorToExisting variable in UAnimationSettings
    #jira UE-160701
    #rb jurre.debaare
    #preflight 632474623752284a317e6f46
    
    [CL 22052253 by lucas dower in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Animation/AnimationSettings.h
+++ Engine/Source/Runtime/Engine/Classes/Animation/AnimationSettings.h
@@ -47,6 +47,7 @@ class ENGINE_API UAnimationSettings : public UDeveloperSettings
 	bool bFirstRecompressUsingCurrentOrDefault;
 
 	/** If true and the existing compression error is greater than Alternative Compression Threshold, then Alternative Compression Threshold will be effectively raised to the existing error level */
+	UE_DEPRECATED(5.1, "This is being removed because it is unused")
 	UPROPERTY(config, EditAnywhere, Category = Compression)
 	bool bRaiseMaxErrorToExisting;
 
