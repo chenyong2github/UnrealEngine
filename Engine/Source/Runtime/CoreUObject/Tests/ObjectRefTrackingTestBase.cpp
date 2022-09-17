e4d61cb40ad53be7d28fe6ba36b182f6669b757e
nick darnell <Nick.Darnell@epicgames.com>
2022-09-16 20:27:03 -0400

    Removing some unused stuff from ObjectRefTrackingTestBase for builds with LOW_LEVEL_TESTS in them.
    [CL 22064011 by nick darnell in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Tests/ObjectRefTrackingTestBase.cpp
+++ Engine/Source/Runtime/CoreUObject/Tests/ObjectRefTrackingTestBase.cpp
@@ -4,10 +4,6 @@
 
 #include "ObjectRefTrackingTestBase.h"
 
-#if UE_WITH_OBJECT_HANDLE_TRACKING
-ObjectHandleReferenceResolvedFunction* FObjectRefTrackingTestBase::PrevResolvedFunc = nullptr;
-ObjectHandleReadFunction* FObjectRefTrackingTestBase::PrevReadFunc = nullptr;
-#endif
 thread_local uint32 FObjectRefTrackingTestBase::NumResolves = 0;
 thread_local uint32 FObjectRefTrackingTestBase::NumFailedResolves = 0;
 thread_local uint32 FObjectRefTrackingTestBase::NumReads = 0;
