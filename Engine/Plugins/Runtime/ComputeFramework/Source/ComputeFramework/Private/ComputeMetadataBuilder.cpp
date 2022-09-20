899500376802f145c37dd46fa30b4368457ea6d6
jack cai <jack.cai@epicgames.com>
2022-09-20 15:43:17 -0400

    ComputeFramework, fix non-unity
    #jira none
    #horde https://horde.devtools.epicgames.com/job/63296512f855485d69953910?step=6f3a
    #rb tirivial
    #preflight skip
    
    [CL 22100353 by jack cai in ue5-main branch]

--- Engine/Plugins/Runtime/ComputeFramework/Source/ComputeFramework/Private/ComputeMetadataBuilder.cpp
+++ Engine/Plugins/Runtime/ComputeFramework/Source/ComputeFramework/Private/ComputeMetadataBuilder.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "ComputeFramework/Public/ComputeFramework/ComputeMetadataBuilder.h"
 
 #include "ShaderParameterMetadataBuilder.h"
