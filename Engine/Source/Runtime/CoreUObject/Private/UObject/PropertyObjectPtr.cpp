f602862479e7ee2044c09441a0bca35029664079
nick darnell <Nick.Darnell@epicgames.com>
2022-09-20 13:50:06 -0400

    Core - Reverting the base type of FObjectProperty's FObjectPropertyBase to UObject* instead of TObjectPtr<UObject>, there's some range garbage collection issue this causes where things arent cleaned up in the expected fashion.  This may be limited to script, but unsure.  Working on it.  Reverting for now to fix the EngineTest tests.
    #jira UE-164443
    [REVIEW] [at]Zousar.Shaker, [at]Joe.Pribele
    
    [CL 22097502 by nick darnell in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/PropertyObjectPtr.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/PropertyObjectPtr.cpp
@@ -151,7 +151,7 @@ TObjectPtr<UObject> FObjectPtrProperty::GetObjectPtrPropertyValue(const void* Pr
 
 UObject* FObjectPtrProperty::GetObjectPropertyValue(const void* PropertyValueAddress) const
 {
-	return GetPropertyValue(PropertyValueAddress).Get();
+	return GetPropertyValue(PropertyValueAddress);
 }
 
 UObject* FObjectPtrProperty::GetObjectPropertyValue_InContainer(const void* ContainerAddress, int32 ArrayIndex) const
