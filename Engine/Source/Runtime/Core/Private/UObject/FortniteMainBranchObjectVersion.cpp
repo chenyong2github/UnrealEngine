8f02cdaf791feeb75b7ae2da6a6915c2641e1905
jamie hayes <jamie.hayes@epicgames.com>
2022-09-21 07:36:12 -0400

    Bump the StaticMesh DDC version to flush out issues with cache built before a Nanite fix from CL 22098795.
    [FYI] graham.wihlidal, rune.stubbe
    #jira UE-162652
    #preflight 632a1c09a236a029dee4c7bd
    
    [CL 22110183 by jamie hayes in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/UObject/FortniteMainBranchObjectVersion.cpp
+++ Engine/Source/Runtime/Core/Private/UObject/FortniteMainBranchObjectVersion.cpp
@@ -13,7 +13,7 @@ TMap<FGuid, FGuid> FFortniteMainBranchObjectVersion::GetSystemGuids()
 	SystemGuids.Add(DevGuids.NIAGARASHADERMAP_DERIVEDDATA_VER, FGuid("2289D5116CF94BC0AFEAEC541468E645"));
 	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("04A14D478F6449BC92BD142968DCB2BB"));
 	SystemGuids.Add(DevGuids.SkeletalMeshDerivedDataVersion, FGuid("ACF593EAAE354FCBB34CF44F5AA1BFD2"));
-	SystemGuids.Add(DevGuids.STATICMESH_DERIVEDDATA_VER, FGuid("9A48CE6B46204777968322F98F82D11F"));
+	SystemGuids.Add(DevGuids.STATICMESH_DERIVEDDATA_VER, FGuid("2B18DC81AAF64015A7901B5A3F8394C0"));
 
 	return SystemGuids;
 }
