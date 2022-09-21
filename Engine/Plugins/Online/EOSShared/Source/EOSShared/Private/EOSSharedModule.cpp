97933b009416a356494ae8782fd602a2ad7ac466
chris varnsverry <Chris.Varnsverry@xa.epicgames.com>
2022-09-21 14:33:33 -0400

    - Don't create/register EOSSDKManager in commandlet runs.
    #jira UE-164441
    [REVIEW] [at]Rob.Cannaday [at]Michael.Kirzinger [at]Sam.Zamani
    #preflight 632b106dfc7f1efbdf9488b3
    
    [CL 22118664 by chris varnsverry in ue5-main branch]

--- Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/EOSSharedModule.cpp
+++ Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/EOSSharedModule.cpp
@@ -6,6 +6,8 @@
 #include "Misc/ConfigCacheIni.h"
 #include "Misc/CoreDelegates.h"
 #include "Modules/ModuleManager.h"
+#include "CoreGlobals.h"
+#include "EOSShared.h"
 
 #include COMPILED_PLATFORM_HEADER(EOSSDKManager.h)
 
@@ -16,6 +18,13 @@ IMPLEMENT_MODULE(FEOSSharedModule, EOSShared);
 void FEOSSharedModule::StartupModule()
 {
 #if WITH_EOS_SDK
+	if (IsRunningCommandlet())
+	{
+		// No need to do anything when running in commandlet mode. We won't register the EOSSDKManager, EOS will not be initialized, no platforms will be created, etc.
+		UE_LOG(LogEOSSDK, Log, TEXT("IsRunningCommandlet=true, skipping EOSSDK initialization."))
+		return;
+	}
+
 	SDKManager = MakeUnique<FPlatformEOSSDKManager>();
 	check(SDKManager);
 
