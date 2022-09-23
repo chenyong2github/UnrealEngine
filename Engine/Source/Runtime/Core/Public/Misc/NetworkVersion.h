2eb4a5dfff65b9b7b0a20cd6a89b549930d111e7
stan hormell <stan.hormell@epicgames.com>
2022-09-22 22:20:09 -0400

    Setting NetCL back to Zero
    [CL 22151130 by stan hormell in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Misc/NetworkVersion.h
+++ Engine/Source/Runtime/Core/Public/Misc/NetworkVersion.h
@@ -8,7 +8,7 @@
 #include "Logging/LogMacros.h"
 
 // The version number used for determining network compatibility. If zero, uses the engine compatible version.
-#define ENGINE_NET_VERSION  21735710
+#define ENGINE_NET_VERSION  0
 
 // The version number used for determining replay compatibility
 #define ENGINE_REPLAY_VERSION  ENGINE_NET_VERSION
