b9e9b0f2883405a72d4b34b41d999be7eb6f0179
dmytro vovk <dmytro.vovk@epicgames.com>
2022-09-22 22:18:55 -0400

    Fixed linker error for Android's build of LowLevelTests #jira UE-164814 #rb Chris.Babcock #preflight 632ccc3d100305080682e9ea
    [CL 22151094 by dmytro vovk in ue5-main branch]

--- Engine/Source/Programs/LowLevelTests/LowLevelTests.Target.cs
+++ Engine/Source/Programs/LowLevelTests/LowLevelTests.Target.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 [SupportedPlatforms(UnrealPlatformClass.All)]
@@ -9,5 +10,16 @@ public class LowLevelTestsTarget : TestTargetRules
 	{
 		bWithLowLevelTestsOverride = true;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+			string VersionScriptFile = Path.GetTempPath() + "LLTWorkaroundScrip.ldscript";
+			using (StreamWriter Writer = File.CreateText(VersionScriptFile))
+			{
+				// Workaround for a linker bug when building LowLevelTests for Android
+				Writer.WriteLine("{ local: *; };");
+			}
+			AdditionalLinkerArguments = " -Wl,--version-script=\"" + VersionScriptFile + "\"";
+		}
 	}
 }
