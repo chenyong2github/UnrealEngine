ff253be007ae0a10767b4ee73907e8d06df6f809
michael lentine <Michael.Lentine@epicgames.com>
2022-09-24 13:30:49 -0400

    Remove ensure for degenerate triangles.
    #rb trivial
    
    [CL 22173137 by michael lentine in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/TriangleMeshImplicitObject.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/TriangleMeshImplicitObject.cpp
@@ -1556,7 +1556,7 @@ FVec3 FTriangleMeshImplicitObject::GetFaceNormal(const int32 FaceIdx) const
 			if(Normal.SafeNormalize() < UE_SMALL_NUMBER)
 			{
 				UE_LOG(LogChaos, Warning, TEXT("Degenerate triangle %d: (%f %f %f) (%f %f %f) (%f %f %f)"), FaceIdx, A.X, A.Y, A.Z, B.X, B.Y, B.Z, C.X, C.Y, C.Z);
-				ensure(false);
+				CHAOS_ENSURE(false);
 				return FVec3(0, 0, 1);
 			}
 
