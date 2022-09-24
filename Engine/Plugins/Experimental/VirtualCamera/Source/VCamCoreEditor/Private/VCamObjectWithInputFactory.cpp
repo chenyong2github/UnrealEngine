03344d487cd685866c3076ce06fe2ea23ff78c3f
david hibbitts <David.Hibbitts@epicgames.com>
2022-09-24 13:27:30 -0400

    Updated text for vcam object creation dialogs
    #jira UE-163857
    #preflight 632e452f671a1a24b597d16c
    
    [CL 22173108 by david hibbitts in ue5-main branch]

--- Engine/Plugins/Experimental/VirtualCamera/Source/VCamCoreEditor/Private/VCamObjectWithInputFactory.cpp
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VCamCoreEditor/Private/VCamObjectWithInputFactory.cpp
@@ -28,16 +28,6 @@ bool UVCamObjectWithInputFactory::ConfigureProperties()
 			return FReply::Handled();
 		}
 
-		FReply OnCancel()
-		{
-			if (PickerWindow.IsValid())
-			{
-				PickerWindow->RequestDestroyWindow();
-			}
-			bWasCanceled = true;
-			return FReply::Handled();
-		}
-
 		bool WasCanceled() const
 		{
 			return bWasCanceled;
@@ -48,7 +38,7 @@ bool UVCamObjectWithInputFactory::ConfigureProperties()
 			AssetThumbnailPool = MakeShared<FAssetThumbnailPool>(1);
 			PickerWindow = SNew(SWindow)
 				.Title(LOCTEXT("PickerTitle", "Select Input Mapping Context"))
-				.ClientSize(FVector2D(350, 100))
+				.ClientSize(FVector2D(450, 150))
 				.SupportsMinimize(false)
 				.SupportsMaximize(false)
 				[
@@ -58,26 +48,48 @@ bool UVCamObjectWithInputFactory::ConfigureProperties()
 					[
 						SNew(SVerticalBox)
 						+ SVerticalBox::Slot()
+						.AutoHeight()
+						[
+							SNew(STextBlock)
+							.Text(LOCTEXT("DialogText", "Your class needs an Input Mapping Context if you want to accept hardware input. You can add or change this selection later in the Class Defaults."))
+							.AutoWrapText(true)
+						]
+						+ SVerticalBox::Slot()
 						.FillHeight(1.f)
 						[
-							SNew(SObjectPropertyEntryBox)
-							.DisplayThumbnail(true)
-							.ThumbnailPool(AssetThumbnailPool)
-							.AllowClear(true)
-							.DisplayUseSelected(false)
-							.DisplayBrowse(false)
-							.AllowedClass(UInputMappingContext::StaticClass())
-							.ObjectPath_Lambda([Factory]() -> FString
-							{
-								return (Factory && Factory->InputMappingContext) ? Factory->InputMappingContext->GetPathName() : TEXT("None");
-							})
-							.OnObjectChanged_Lambda([Factory](const FAssetData& AssetData) -> void
-							{
-								if (Factory)
+							SNew(SHorizontalBox)
+							+ SHorizontalBox::Slot()
+							.AutoWidth()
+							.HAlign(HAlign_Left)
+							.VAlign(VAlign_Center)
+							.Padding(FMargin(0, 0, 20, 0))
+							[
+								SNew(STextBlock)
+								.Text(LOCTEXT("InputMappingContext", "Input Mapping Context"))
+							]
+							+ SHorizontalBox::Slot()
+							.FillWidth(1.f)
+							.VAlign(VAlign_Center)
+							[
+								SNew(SObjectPropertyEntryBox)
+								.DisplayThumbnail(true)
+								.ThumbnailPool(AssetThumbnailPool)
+								.AllowClear(true)
+								.DisplayUseSelected(false)
+								.DisplayBrowse(false)
+								.AllowedClass(UInputMappingContext::StaticClass())
+								.ObjectPath_Lambda([Factory]() -> FString
 								{
-									Factory->InputMappingContext = Cast<UInputMappingContext>(AssetData.GetAsset());
-								}
-							})
+									return (Factory && Factory->InputMappingContext) ? Factory->InputMappingContext->GetPathName() : TEXT("None");
+								})
+								.OnObjectChanged_Lambda([Factory](const FAssetData& AssetData) -> void
+								{
+									if (Factory)
+									{
+										Factory->InputMappingContext = Cast<UInputMappingContext>(AssetData.GetAsset());
+									}
+								})
+							]
 						]
 						+ SVerticalBox::Slot()
 						.HAlign(HAlign_Right)
@@ -87,17 +99,29 @@ bool UVCamObjectWithInputFactory::ConfigureProperties()
 							SNew(SHorizontalBox)
 							+ SHorizontalBox::Slot()
 							.AutoWidth()
+							.Padding(FMargin(5,0))
 							[
 								SNew(SButton)
 								.Text(LOCTEXT("OK", "OK"))
+								.ToolTipText(LOCTEXT("OkTooltip", "Continues creation using the specified Input Mapping Context"))
 								.OnClicked(this, &FVCamWidgetFactoryUI::OnCreate)
 							]
 							+ SHorizontalBox::Slot()
 							.AutoWidth()
+							.Padding(FMargin(5,0))
 							[
 								SNew(SButton)
-								.Text(LOCTEXT("Cancel", "Cancel"))
-								.OnClicked(this, &FVCamWidgetFactoryUI::OnCancel)
+								.Text(LOCTEXT("Skip", "Skip"))
+								.ToolTipText(LOCTEXT("SkipTooltip", "Continues creation without assigning an Input Mapping Context"))
+								.OnClicked_Lambda([Factory, this]() -> FReply
+								{
+									// If we skipped then create the widget but with no IMC set
+									if (Factory)
+									{
+										Factory->InputMappingContext = nullptr;
+									}
+									return OnCreate();
+								})
 							]
 						]
 					]
@@ -112,7 +136,7 @@ bool UVCamObjectWithInputFactory::ConfigureProperties()
 	private:
 		TSharedPtr<SWindow> PickerWindow;
 		TSharedPtr<FAssetThumbnailPool> AssetThumbnailPool;
-		bool bWasCanceled = false;
+		bool bWasCanceled = true;
 	};
 
 	const TSharedRef<FVCamWidgetFactoryUI> InputMappingSelector = MakeShared<FVCamWidgetFactoryUI>();
