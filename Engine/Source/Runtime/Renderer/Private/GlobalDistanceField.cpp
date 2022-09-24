d1ca6d0cdc4e34c46260ecdbf6eb8561d1f82626
krzysztof narkowicz <Krzysztof.Narkowicz@epicgames.com>
2022-09-23 20:45:46 -0400

    Lumen - optimized global distance field tracing by skipping coverage data sampling if it�s not required. Coverage data is used only for foliage partial transparency, but we were sampling it everywhere adding overhead to every trace even when there was no foliage around.
    During page composite pass, pages containing coverage values below 1 are marked using a single bit inside PageTable. This bit is then checked during the tracing pass to decide whether we should load a sparse coverage page or not.
    
    Previously we were marking empty areas as coverage 0, but it had some side effects and isn�t compatible with this marking scheme. Now all empty space is marked as coverage=1 and mobility cache composition was accordingly modified.
    
    Performance wins on FortGPUTestBed, all tracing passes, current gen console, 1080p
    Epic - 0.17ms
    High - 0.1ms
    
    Unfortunately this doesn�t fully remove coverage overhead in levels without foliage. Overhead after this optimization:
    Epic - 0.1ms
    High - 0.06ms
    
    [CL 22165335 by krzysztof narkowicz in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/GlobalDistanceField.cpp
+++ Engine/Source/Runtime/Renderer/Private/GlobalDistanceField.cpp
@@ -1534,7 +1534,7 @@ class FCompositeObjectsIntoPagesCS : public FGlobalShader
 		RDG_BUFFER_ACCESS(ComposeIndirectArgBuffer, ERHIAccess::IndirectArgs)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, ComposeTileBuffer)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, HeightfieldMarkedPageBuffer)
-		SHADER_PARAMETER_RDG_TEXTURE(Texture3D<uint>, PageTableLayerTexture)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(Texture3D<uint>, RWPageTableCombinedTexture)
 		SHADER_PARAMETER_RDG_TEXTURE(Texture3D<uint>, ParentPageTableLayerTexture)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, CullGridObjectHeader)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, CullGridObjectArray)
@@ -2496,7 +2496,7 @@ void UpdateGlobalDistanceFieldVolume(
 								PassParameters->RWCoverageAtlasTexture = CoverageAtlasTexture ? GraphBuilder.CreateUAV(CoverageAtlasTexture) : nullptr;
 								PassParameters->ComposeIndirectArgBuffer = PageComposeIndirectArgBuffer;
 								PassParameters->ComposeTileBuffer = GraphBuilder.CreateSRV(PageComposeTileBuffer, PF_R32_UINT);
-								PassParameters->PageTableLayerTexture = PageTableLayerTexture;
+								PassParameters->RWPageTableCombinedTexture = GraphBuilder.CreateUAV(PageTableCombinedTexture && ParentPageTableLayerTexture ? PageTableCombinedTexture : PageTableLayerTexture);
 								PassParameters->ParentPageTableLayerTexture = ParentPageTableLayerTexture;
 								PassParameters->CullGridObjectHeader = GraphBuilder.CreateSRV(CullGridObjectHeader, PF_R32_UINT);
 								PassParameters->CullGridObjectArray = GraphBuilder.CreateSRV(CullGridObjectArray, PF_R32_UINT);
