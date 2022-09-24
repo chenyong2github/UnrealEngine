3e03f0744d2f045b6e29c23e744c6c1475fb034e
eric knapik <Eric.Knapik@epicgames.com>
2022-09-23 20:46:54 -0400

    #jira: none
    Fix an issue where engine was not depended on when depending on gamefeaturePlugins.
    Only depending on Engine when not running as a program.
    
    [FYI] Yiliang.Siew, Marc.Audy
    #preflight 632e018e7b582f58abc5800f
    
    [CL 22165416 by eric knapik in ue5-main branch]

--- Engine/Plugins/Experimental/GameFeatures/Source/GameFeatures/Public/GameFeaturesSubsystem.h
+++ Engine/Plugins/Experimental/GameFeatures/Source/GameFeatures/Public/GameFeaturesSubsystem.h
@@ -305,7 +305,7 @@ public:
 	virtual void Deinitialize() override;
 	//~End of UEngineSubsystem interface
 
-	static FORCENOINLINE UGameFeaturesSubsystem& Get() { return *GEngine->GetEngineSubsystem<UGameFeaturesSubsystem>(); }
+	static UGameFeaturesSubsystem& Get() { return *GEngine->GetEngineSubsystem<UGameFeaturesSubsystem>(); }
 
 public:
 	/** Loads the specified game feature data and its bundles */
