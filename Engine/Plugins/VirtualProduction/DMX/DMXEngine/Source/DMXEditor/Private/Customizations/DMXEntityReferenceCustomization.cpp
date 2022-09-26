ec5fe38a775fae1ca8835ec54e3bf6c9b842106a
flo schar <Flo.Schar@xa.epicgames.com>
2022-09-26 14:03:51 -0400

    DMX - Fix ShowOnlyInnerProperties doesn't work for FDMXFixturePatchRef
    #jira UE-165075
    #rb trivial
    #preflight 6331bab90d6a7155ba329fdf
    
    [CL 22189749 by flo schar in ue5-main branch]

--- Engine/Plugins/VirtualProduction/DMX/DMXEngine/Source/DMXEditor/Private/Customizations/DMXEntityReferenceCustomization.cpp
+++ Engine/Plugins/VirtualProduction/DMX/DMXEngine/Source/DMXEditor/Private/Customizations/DMXEntityReferenceCustomization.cpp
@@ -24,22 +24,27 @@ void FDMXEntityReferenceCustomization::CustomizeHeader(TSharedRef<IPropertyHandl
 {
 	StructHandle = InPropertyHandle;
 
-	// If we should display any children, leave the standard header. Otherwise, create custom picker
-	TSharedRef<SWidget> ValueContent = !ShouldDisplayLibrary()
-		? CreateEntityPickerWidget(InPropertyHandle)
-		: InPropertyHandle->CreatePropertyValueWidget(false);
+	constexpr TCHAR ShowOnlyInnerPropertiesMetaDataName[] = TEXT("ShowOnlyInnerProperties");
+	bool bShowHeader = !InPropertyHandle->HasMetaData(ShowOnlyInnerPropertiesMetaDataName);
+	if (bShowHeader)
+	{
+		// If we should display any children, leave the standard header. Otherwise, create custom picker
+		TSharedRef<SWidget> ValueContent = !ShouldDisplayLibrary()
+			? CreateEntityPickerWidget(InPropertyHandle)
+			: InPropertyHandle->CreatePropertyValueWidget(false);
 
-	InHeaderRow
-		.NameContent()
-		[
-			InPropertyHandle->CreatePropertyNameWidget()
-		]
-		.ValueContent()
-		.MinDesiredWidth(200.0f)
-		.MaxDesiredWidth(400.0f)
-		[
-			ValueContent
-		];
+		InHeaderRow
+			.NameContent()
+			[
+				InPropertyHandle->CreatePropertyNameWidget()
+			]
+			.ValueContent()
+			.MinDesiredWidth(200.0f)
+			.MaxDesiredWidth(400.0f)
+			[
+				ValueContent
+			];
+	}
 }
 
 void FDMXEntityReferenceCustomization::CustomizeChildren(TSharedRef<IPropertyHandle> InPropertyHandle, IDetailChildrenBuilder& InChildBuilder, IPropertyTypeCustomizationUtils& CustomizationUtils)
