4f8c9cc5fc16f9134e7c284695d98c34aa752517
jurre debaare <jurre.debaare@epicgames.com>
2022-09-27 10:55:17 -0400

    REDO: UEFN - Users are able to apply Animation Modifiers via the right-click context menu #fix moved animation modifier menu section into module itself - gated by whether or not AnimationModifier class is allowed #rb Thomas.Sarkanen #preflight 6332c289a4769ad714e78a8c
    [CL 22206223 by jurre debaare in ue5-main branch]

--- Engine/Source/Editor/AnimationModifiers/Private/AnimationModifiersModule.h
+++ Engine/Source/Editor/AnimationModifiers/Private/AnimationModifiersModule.h
@@ -2,16 +2,27 @@
 
 #pragma once
 
-#include "Containers/Array.h"
 #include "IAnimationModifiersModule.h"
 #include "Templates/SharedPointer.h"
 #include "UObject/NameTypes.h"
 #include "WorkflowOrientedApp/WorkflowCentricApplication.h"
+#include "AssetTypeActions_Base.h"
 
 class FApplicationMode;
 class UAnimSequence;
 class UFactory;
 class UObject;
+
+class FAssetTypeActions_AnimationModifier : public FAssetTypeActions_Base
+{
+public:
+	// IAssetTypeActions Implementation
+	virtual FText GetName() const override { return NSLOCTEXT("AssetTypeActions", "AssetTypeActions_AnimationModifer", "Animation Modifier"); }
+	virtual FColor GetTypeColor() const override { return FColor(50,162,232); }
+	virtual UClass* GetSupportedClass() const override { return UAnimationModifier::StaticClass(); }
+	virtual bool HasActions(const TArray<UObject*>& InObjects) const override { return false; }
+	virtual uint32 GetCategories() override { return EAssetTypeCategories::Animation; }
+};
 
 /** Animation modifiers module, handles injecting of the AnimationModifiersTab into animation and skeleton editor modes */
 class FAnimationModifiersModule : public IAnimationModifiersModule
@@ -32,6 +43,8 @@ protected:
 	/** Callback for extending an application mode */
 	TSharedRef<FApplicationMode> ExtendApplicationMode(const FName ModeName, TSharedRef<FApplicationMode> InMode);
 
+	void RegisterMenus();
+
 	/** Weak list of application modes for which a tab factory was registered */
 	TArray<TWeakPtr<FApplicationMode>> RegisteredApplicationModes;
 
@@ -40,4 +53,6 @@ protected:
 	/** Callbacks used to add and apply default animation modifier classes */
 	void OnAssetPostImport(UFactory* ImportFactory, UObject* ImportedObject);
 	void OnAssetPostReimport(UObject* ReimportedObject);
+	
+	FAssetTypeActions_AnimationModifier AssetAction;
 };
