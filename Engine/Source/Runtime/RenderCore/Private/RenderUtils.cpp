dc21b3cdcc1cac98954e7dd0b26db8915cfb26f2
David Harvey <David.Harvey@epicgames.com>
2022-09-29 05:31:55 -0400

    add missing PixelFormatToChannelFlags entry for PF_P010
     - mapped to 'none', assuming it's a similar format to NV12
    
    #rnx
    #preflight skip
    #fyi Luke.Thatcher, Thomas.Engel
    
    [CL 22244938 by David Harvey in ue5-main branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderUtils.cpp
@@ -939,6 +939,7 @@ EPixelFormatChannelFlags GetPixelFormatValidChannels(EPixelFormat InPixelFormat)
 		EPixelFormatChannelFlags::R,		// PF_R8_SINT
 		EPixelFormatChannelFlags::R,		// PF_R64_UINT
 		EPixelFormatChannelFlags::RGB,		// PF_R9G9B9EXP5
+		EPixelFormatChannelFlags::None,		// PF_P010
 	};
 	static_assert(UE_ARRAY_COUNT(PixelFormatToChannelFlags) == (uint8)PF_MAX, "Missing pixel format");
 	return (InPixelFormat < PF_MAX) ? PixelFormatToChannelFlags[(uint8)InPixelFormat] : EPixelFormatChannelFlags::None;
