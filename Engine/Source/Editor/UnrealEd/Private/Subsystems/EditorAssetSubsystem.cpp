894cf463d2aecc94a166cbf57f3e1117c7e2d00f
sebastien lussier <Sebastien.Lussier@epicgames.com>
2022-10-05 15:29:45 -0400

    [Backout] - CL22333376 [FYI] JeanFrancois.Dube Original CL Desc ----------------------------------------------------------------- Added external actors support to EditorAssetUtils::FindAssetDataFromAnyPath, by first trying to resolve the asset data before attempting to get to the owning object.
    #rb sebastien.lussier
    #preflight nobe
    #rnx
    
    [CL 22361387 by sebastien lussier in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/Subsystems/EditorAssetSubsystem.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Subsystems/EditorAssetSubsystem.cpp
@@ -51,28 +51,22 @@ namespace UE::EditorAssetUtils
 
 	TValueOrError<FAssetData, FString> FindAssetDataFromAnyPath(const FString& AnyAssetPath)
 	{
+		FString FailureReason;
+		FString ObjectPath = EditorScriptingHelpers::ConvertAnyPathToObjectPath(AnyAssetPath, FailureReason);
+		if (ObjectPath.IsEmpty())
+		{
+			return MakeError(FailureReason);
+		}
+
 		FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-
-		// First remove class name from asset path and see if an asset exists for it
-		FString TextPath = FPackageName::ExportTextPathToObjectPath(AnyAssetPath);
-		FAssetData AssetData = AssetRegistryModule.Get().GetAssetByObjectPath(FSoftObjectPath(TextPath));
-
-		// If not, try to convert the path to an object path, potentially resolving the owning object
+		FAssetData AssetData = AssetRegistryModule.Get().GetAssetByObjectPath(FSoftObjectPath(ObjectPath));
 		if (!AssetData.IsValid())
 		{
-			FString FailureReason;
-			FString ObjectPath = EditorScriptingHelpers::ConvertAnyPathToObjectPath(AnyAssetPath, FailureReason);
-			if (ObjectPath.IsEmpty())
-			{
-				return MakeError(FailureReason);
-			}
-
-			AssetData = AssetRegistryModule.Get().GetAssetByObjectPath(FSoftObjectPath(ObjectPath));
-			if (!AssetData.IsValid())
-			{
-				return MakeError(FString::Printf(TEXT("The AssetData '%s' could not be found in the Asset Registry."), *ObjectPath));
-			}
+			return MakeError(FString::Printf(TEXT("The AssetData '%s' could not be found in the Asset Registry."), *ObjectPath));
 		}
+		
+		return MakeValue(AssetData);
+	}
 		
 		return MakeValue(AssetData);
 	}
