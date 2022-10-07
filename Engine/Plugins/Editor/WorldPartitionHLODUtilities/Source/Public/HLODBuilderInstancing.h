b0f09974927e45f1c3682d39fb0d0a3b6ca4ebcf
sebastien lussier <Sebastien.Lussier@epicgames.com>
2022-10-06 20:55:28 -0400

    World Partition - HLOD: Instancing now has a "Disallow Nanite" option * Necessary on Nanite meshes that have multiple LODs, otherwise the setting the mesh last LOD as the component ForcedLOD will not work #rb patrick.enfedaque #preflight 633ef966607039cbc1035e49
    [CL 22390029 by sebastien lussier in ue5-main branch]

--- Engine/Plugins/Editor/WorldPartitionHLODUtilities/Source/Public/HLODBuilderInstancing.h
+++ Engine/Plugins/Editor/WorldPartitionHLODUtilities/Source/Public/HLODBuilderInstancing.h
@@ -6,6 +6,22 @@
 #include "HLODBuilderInstancing.generated.h"
 
 
+UCLASS(Blueprintable, Config = Engine, PerObjectConfig)
+class WORLDPARTITIONHLODUTILITIES_API UHLODBuilderInstancingSettings : public UHLODBuilderSettings
+{
+	GENERATED_UCLASS_BODY()
+
+	virtual uint32 GetCRC() const override;
+
+	/**
+	 * If enabled, the components created for the HLODs will not use Nanite.
+	 * Necessary if you want to use the last LOD & the mesh is Nanite enabled, as forced LODs are ignored by Nanite
+	 */
+	UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadWrite, Category = Rendering)
+	bool bDisallowNanite;
+};
+
+
 /**
  * Build a AWorldPartitionHLOD whose components are ISMC
  */
@@ -18,5 +34,6 @@ public:
 	virtual bool RequiresCompiledAssets() const override { return false; }
 	virtual bool RequiresWarmup() const override { return false; }
 
+	virtual TSubclassOf<UHLODBuilderSettings> GetSettingsClass() const override;
 	virtual TArray<UActorComponent*> Build(const FHLODBuildContext& InHLODBuildContext, const TArray<UActorComponent*>& InSourceComponents) const override;
 };
