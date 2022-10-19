a7a8a5d3dfe926e39036ea05d7d525f9970e37b2
rex hill <Rex.Hill@epicgames.com>
2022-10-19 14:36:27 -0400

    Move IAssetReferenceFilter out of EditorEngine.h into its own header file #preflight skip #rnx
    [CL 22633124 by rex hill in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Classes/Editor/EditorEngine.h
+++ Engine/Source/Editor/UnrealEd/Classes/Editor/EditorEngine.h
@@ -31,6 +31,7 @@
 #include "UnrealEngine.h"
 #include "Templates/PimplPtr.h"
 #include "Templates/UniqueObj.h"
+#include "Editor/AssetReferenceFilter.h"
 
 #include "EditorEngine.generated.h"
 
@@ -272,15 +273,6 @@ struct FPreviewPlatformInfo
 struct FAssetReferenceFilterContext
 {
 	TArray<FAssetData> ReferencingAssets;
-};
-
-/** Used in filtering allowed references between assets. Implement a subclass of this and return it in OnMakeAssetReferenceFilter */
-class IAssetReferenceFilter
-{
-public:
-	virtual ~IAssetReferenceFilter() { }
-	/** Filter function to pass/fail an asset. Called in some situations that are performance-sensitive so is expected to be fast. */
-	virtual bool PassesFilter(const FAssetData& AssetData, FText* OutOptionalFailureReason = nullptr) const = 0;
 };
 
 /**
