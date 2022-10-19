ce5336e6851dfb7820fc4862553d4c01903bd4da
bryan sefcik <bryan.sefcik@epicgames.com>
2022-10-19 03:05:53 -0400

    Created a build.cs file for DistributedBuildInterface so it can be referenced by other modules.
    #jira
    
    [CL 22619938 by bryan sefcik in ue5-main branch]

---
+++ Engine/Source/Developer/DistributedBuildInterface/DistributedBuildInterface.build.cs
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DistributedBuildInterface : ModuleRules
+{
+	public DistributedBuildInterface(ReadOnlyTargetRules Target) : base(Target)
+	{
+	}
+}
