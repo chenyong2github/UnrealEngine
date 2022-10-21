a8fda7f78f82f78f0978035d8a01ec7fc91b0a79
fabian giesen <fabian.giesen@epicgames.com>
2022-10-21 17:58:34 -0400

    Checked integer template.
    Adds new FCheckedSignedInt<SignedType> template with named instantiations FCheckedInt32 and FCheckedInt64 that perform range, overflow-checked integer arithmetic and know they're invalid when an error occured anywhere along the way. They also test for illegal shift amounts and division by zero.
    
    This class is intended for processing integers from untrusted sources, especially for size computations, where overflows might otherwise be used to craft buffer overflow exploits.
    
    #rb devin.doucette
    #preflight 63531188e6096564af92c37b
    
    [CL 22702469 by fabian giesen in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Tests/Math/UnrealMathTest.cpp
+++ Engine/Source/Runtime/Core/Private/Tests/Math/UnrealMathTest.cpp
@@ -17,6 +17,7 @@
 #include "Math/Quat.h"
 #include "Math/QuatRotationTranslationMatrix.h"
 #include "Math/Color.h"
+#include "Math/CheckedInt.h"
 #include "Misc/AutomationTest.h"
 #include "Async/ParallelFor.h"
 #include "Misc/ScopeLock.h"
@@ -4806,6 +4807,139 @@ bool FColorConversionHeavyTest::RunTest(const FString& Parameters)
 	}
 
 	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathCheckedIntTest, "System.Core.Math.CheckedInt", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter)
+bool FMathCheckedIntTest::RunTest(const FString& Parameters)
+{
+	// Do an exhaustive test of all checked signed int operations with int8s. This is a small enough space of possibilities
+	// that it's easy to try all options exhaustively, and it's likewise easy to compute reference results in int32 arithmetic
+	// without having to worry about overflows. TCheckedSignedInt uses the same logic and implementation for all type
+	// instantiations (as of this writing), so this is a good way to get coverage on all relevant code paths.
+	using FCheckedInt8 = TCheckedSignedInt<int8>;
+	int NumErrorsFound = 0;
+	const int NumErrorsMax = 100; // Stop reporting errors after this in case something is completely busted.
+
+	// Helper functions to log test results
+	auto CheckArithResult = [this, &NumErrorsFound, NumErrorsMax](const TCHAR* Operation, int32 A, int32 B, FCheckedInt8 CheckedResult, int32 UncheckedValue)
+	{
+		FCheckedInt8 UncheckedResult{ UncheckedValue };
+
+		// If either operand was invalid, the unchecked result is also invalid
+		if (!FCheckedInt8(A).IsValid() || !FCheckedInt8(B).IsValid())
+		{
+			UncheckedResult = FCheckedInt8::Invalid();
+		}
+
+		if (CheckedResult != UncheckedResult && NumErrorsFound < NumErrorsMax)
+		{
+			NumErrorsFound++;
+
+			const int UncheckedNum = UncheckedResult.Get(-128);
+			const int CheckedNum = CheckedResult.Get(-128);
+
+			AddError(FString::Printf(TEXT("Arith %s A=%d B=%d Expected=(%d,Valid=%d) Got=(%d,Valid=%d)"),
+				Operation, A, B, UncheckedNum, UncheckedResult.IsValid(), CheckedNum, CheckedResult.IsValid()));
+		}
+	};
+
+	auto CheckCompareResult = [this, &NumErrorsFound, NumErrorsMax](const TCHAR* Operation, int32 A, int32 B, bool bAcceptInvalid, bool bTestResult, bool bRefResult)
+	{
+		// If either operands was invalid, override the ref result
+		if (!FCheckedInt8(A).IsValid() || !FCheckedInt8(B).IsValid())
+			bRefResult = bAcceptInvalid;
+
+		if (bTestResult != bRefResult && NumErrorsFound < NumErrorsMax)
+		{
+			NumErrorsFound++;
+
+			AddError(FString::Printf(TEXT("Cmp %s A=%d B=%d ref=%d test=%d\n"), Operation, A, B, bRefResult, bTestResult));
+		}
+	};
+
+	// Note: we include 128 in the ranges here to explicitly try out-of-range (invalid) values.
+	for (int32 A = -128; A <= 128; ++A)
+	{
+		for (int32 B = -128; B <= 128; ++B)
+		{
+			FCheckedInt8 CheckedA{ A };
+			FCheckedInt8 CheckedB{ B };
+
+			// Compute reference results for the operations that are not defined everywhere
+			int32 ADivB = -256;
+			int32 AModB = -256;
+
+			// -128 / -1 is undefined in int8 arithmetic because it overflows; when we're
+			// computing it with int32s, it of course works just fine, but we want to make sure
+			// our test results are correct.
+			if (B != 0 && (A != -128 || B != -1))
+			{
+				ADivB = A / B;
+				AModB = A % B;
+			}
+
+			// Shifts are only defined for in-range shift amounts, which for our checked ints
+			// is [0,NumBits-1]. This is tighter than usual C++ arithmetic for the FCheckedInt8 we
+			// use for testing since normal C++ promotes to int and shifts down (so usually [0,31]
+			// count limit) so we need to test for these explicitly.
+			int32 ALshB = -256;
+			int32 ARshB = -256;
+			if (B >= 0 && B <= 7)
+			{
+				ALshB = A << B;
+				ARshB = A >> B;
+			}
+
+			CheckArithResult(TEXT("add1"), A, B, CheckedA + CheckedB, A + B);
+			CheckArithResult(TEXT("add2"), A, B, CheckedA + B, A + B);
+			CheckArithResult(TEXT("add3"), A, B, A + CheckedB, A + B);
+			CheckArithResult(TEXT("sub1"), A, B, CheckedA - CheckedB, A - B);
+			CheckArithResult(TEXT("sub2"), A, B, CheckedA - B, A - B);
+			CheckArithResult(TEXT("sub3"), A, B, A - CheckedB, A - B);
+			CheckArithResult(TEXT("mul1"), A, B, CheckedA * CheckedB, A * B);
+			CheckArithResult(TEXT("mul2"), A, B, CheckedA * B, A * B);
+			CheckArithResult(TEXT("mul3"), A, B, A * CheckedB, A * B);
+			CheckArithResult(TEXT("div1"), A, B, CheckedA / CheckedB, ADivB);
+			CheckArithResult(TEXT("div2"), A, B, CheckedA / B, ADivB);
+			CheckArithResult(TEXT("div3"), A, B, A / CheckedB, ADivB);
+			CheckArithResult(TEXT("mod1"), A, B, CheckedA % CheckedB, AModB);
+			CheckArithResult(TEXT("mod2"), A, B, CheckedA % B, AModB);
+			CheckArithResult(TEXT("mod3"), A, B, A % CheckedB, AModB);
+			CheckArithResult(TEXT("lsh1"), A, B, CheckedA << CheckedB, ALshB);
+			CheckArithResult(TEXT("lsh2"), A, B, CheckedA << B, ALshB);
+			CheckArithResult(TEXT("lsh3"), A, B, A << CheckedB, ALshB);
+			CheckArithResult(TEXT("rsh1"), A, B, CheckedA >> CheckedB, ARshB);
+			CheckArithResult(TEXT("rsh2"), A, B, CheckedA >> B, ARshB);
+			CheckArithResult(TEXT("rsh3"), A, B, A >> CheckedB, ARshB);
+			CheckArithResult(TEXT("neg"), A, A, -CheckedA, -A);
+			CheckArithResult(TEXT("abs"), A, A, CheckedA.Abs(), FMath::Abs(A));
+			
+			CheckCompareResult(TEXT("valid_lt1"), A, B, false, CheckedA.ValidAndLessThan(CheckedB), A < B);
+			CheckCompareResult(TEXT("valid_lt2"), A, B, false, CheckedA.ValidAndLessThan(B), A < B);
+			CheckCompareResult(TEXT("valid_le1"), A, B, false, CheckedA.ValidAndLessOrEqual(CheckedB), A <= B);
+			CheckCompareResult(TEXT("valid_le1"), A, B, false, CheckedA.ValidAndLessOrEqual(B), A <= B);
+			CheckCompareResult(TEXT("valid_gt1"), A, B, false, CheckedA.ValidAndGreaterThan(CheckedB), A > B);
+			CheckCompareResult(TEXT("valid_gt1"), A, B, false, CheckedA.ValidAndGreaterThan(B), A > B);
+			CheckCompareResult(TEXT("valid_ge1"), A, B, false, CheckedA.ValidAndGreaterOrEqual(CheckedB), A >= B);
+			CheckCompareResult(TEXT("valid_ge2"), A, B, false, CheckedA.ValidAndGreaterOrEqual(B), A >= B);
+			
+			CheckCompareResult(TEXT("invalid_lt1"), A, B, true, CheckedA.InvalidOrLessThan(CheckedB), A < B);
+			CheckCompareResult(TEXT("invalid_lt2"), A, B, true, CheckedA.InvalidOrLessThan(B), A < B);
+			CheckCompareResult(TEXT("invalid_le1"), A, B, true, CheckedA.InvalidOrLessOrEqual(CheckedB), A <= B);
+			CheckCompareResult(TEXT("invalid_le2"), A, B, true, CheckedA.InvalidOrLessOrEqual(B), A <= B);
+			CheckCompareResult(TEXT("invalid_gt1"), A, B, true, CheckedA.InvalidOrGreaterThan(CheckedB), A > B);
+			CheckCompareResult(TEXT("invalid_gt2"), A, B, true, CheckedA.InvalidOrGreaterThan(B), A > B);
+			CheckCompareResult(TEXT("invalid_ge1"), A, B, true, CheckedA.InvalidOrGreaterOrEqual(CheckedB), A >= B);
+			CheckCompareResult(TEXT("invalid_ge2"), A, B, true, CheckedA.InvalidOrGreaterOrEqual(B), A >= B);
+		}
+	}
+
+	if (NumErrorsFound > NumErrorsMax)
+	{
+		AddError(FString::Printf(TEXT("Too many errors (%d), stopped reporting after %d instances.\n"), NumErrorsFound, NumErrorsMax));
+	}
+
+	return NumErrorsFound == 0;
 }
 
 #endif //WITH_DEV_AUTOMATION_TESTS
