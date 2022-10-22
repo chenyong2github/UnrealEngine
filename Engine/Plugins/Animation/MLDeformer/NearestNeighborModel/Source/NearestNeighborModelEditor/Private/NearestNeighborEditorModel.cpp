9d2844d5ba48c12fc0450ced5d861888295bbb98
zhenglin geng <zhenglin.geng@epicgames.com>
2022-10-22 07:13:36 -0400

    Fix crash if skeletal mesh is changed #rb none [FYI] charles.anderson #preflight 6352d4a07261e565c4827f10
    [CL 22710493 by zhenglin geng in ue5-main branch]

--- Engine/Plugins/Animation/MLDeformer/NearestNeighborModel/Source/NearestNeighborModelEditor/Private/NearestNeighborEditorModel.cpp
+++ Engine/Plugins/Animation/MLDeformer/NearestNeighborModel/Source/NearestNeighborModelEditor/Private/NearestNeighborEditorModel.cpp
@@ -464,6 +464,13 @@ namespace UE::NearestNeighborModel
 			return ERROR;
 		}
 
+		const int32 NumImportedModelVerts = FMath::Max(NearestNeighborModel->GetVertexMap()) + 1;
+		const int32 NumBaseMeshVerts = Model->GetNumBaseMeshVerts();
+		if (NumImportedModelVerts != NumBaseMeshVerts)
+		{
+			UE_LOG(LogNearestNeighborModel, Error, TEXT("Vertex count mismatch: imported model of SkeletalMesh has %d vertices and cached SkeletalMesh has %d vertices"), NumImportedModelVerts, NumBaseMeshVerts);
+			return;
+		}
 
 		const int32 LOD = 0;
 		TArray<UMorphTarget*> MorphTargets;
