3592bf75157cc93528860ee3e3c0c7e5bb71712f
Elliot Colp <Elliot.Colp@epicgames.com>
2022-10-25 21:14:41 -0400

    [EpicStageApp] Miscellaneous color grading properties and automatically override values when changed #jira UE-163937 #rb Alejandro.Arango #preflight 63588812767e539cca956396
    [CL 22772467 by Elliot Colp in ue5-main branch]

--- Engine/Extras/VirtualProduction/EpicStageApp/windows/runner/ThirdParty/main.cpp
+++ Engine/Extras/VirtualProduction/EpicStageApp/windows/runner/ThirdParty/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(1366, 1024);
   if (!window.CreateAndShow(L"epic_stage_app", origin, size)) {
     return EXIT_FAILURE;
   }
