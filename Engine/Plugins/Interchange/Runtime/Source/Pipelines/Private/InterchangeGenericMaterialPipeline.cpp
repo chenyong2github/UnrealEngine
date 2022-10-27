5aed3b46274ef4e128e4f1672c2400ace653dadb
jeanluc corenthin <JeanLuc.Corenthin@epicgames.com>
2022-10-26 22:30:28 -0400

    Fixed test failure when loading MaterialX files
    #jira UE-168250
    #rb alexis.matte
    #preflight 63584f0b555771b1419181e6
    #lockdown jeanmichel.dignard
    
    [CL 22803110 by jeanluc corenthin in ue5-main branch]

--- Engine/Plugins/Interchange/Runtime/Source/Pipelines/Private/InterchangeGenericMaterialPipeline.cpp
+++ Engine/Plugins/Interchange/Runtime/Source/Pipelines/Private/InterchangeGenericMaterialPipeline.cpp
@@ -136,14 +136,12 @@ void UInterchangeGenericMaterialPipeline::PreDialogCleanup(const FName PipelineS
 
 bool UInterchangeGenericMaterialPipeline::IsSettingsAreValid(TOptional<FText>& OutInvalidReason) const
 {
-	using namespace UE::Interchange;
-
 	if (TexturePipeline && !TexturePipeline->IsSettingsAreValid(OutInvalidReason))
 	{
 		return false;
 	}
 
-	return InterchangeGenericMaterialPipeline::Private::AreRequiredPackagesLoaded() &&  Super::IsSettingsAreValid(OutInvalidReason);
+	return Super::IsSettingsAreValid(OutInvalidReason);
 }
 
 void UInterchangeGenericMaterialPipeline::AdjustSettingsForContext(EInterchangePipelineContext ImportType, TObjectPtr<UObject> ReimportAsset)
@@ -173,6 +171,13 @@ void UInterchangeGenericMaterialPipeline::AdjustSettingsForContext(EInterchangeP
 		{
 			HidePropertiesOfCategory(OuterMostPipeline, this, HideCategoryName);
 		}
+	}
+
+	using namespace UE::Interchange;
+
+	if (!InterchangeGenericMaterialPipeline::Private::AreRequiredPackagesLoaded())
+	{
+		UE_LOG(LogInterchangePipeline, Warning, TEXT("UInterchangeGenericMaterialPipeline: Some required packages are missing. Material import might be wrong"));
 	}
 }
 
