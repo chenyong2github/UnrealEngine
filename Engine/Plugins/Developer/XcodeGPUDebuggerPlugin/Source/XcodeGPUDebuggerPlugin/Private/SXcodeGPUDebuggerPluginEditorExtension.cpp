485796ef9b037b1e4089f1749b10cf06a1e53808
henrik karlsson <henrik.karlsson@epicgames.com>
2022-10-28 14:03:29 -0400

    [XcodeGPUDebuggerPlugin] * Speculative fix (added include) for mac compile error
    #rb none
    #preflight skipped
    
    [CL 22832946 by henrik karlsson in ue5-main branch]

--- Engine/Plugins/Developer/XcodeGPUDebuggerPlugin/Source/XcodeGPUDebuggerPlugin/Private/SXcodeGPUDebuggerPluginEditorExtension.cpp
+++ Engine/Plugins/Developer/XcodeGPUDebuggerPlugin/Source/XcodeGPUDebuggerPlugin/Private/SXcodeGPUDebuggerPluginEditorExtension.cpp
@@ -12,6 +12,7 @@
 #include "Editor/UnrealEd/Public/SEditorViewportToolBarMenu.h"
 #include "Editor/UnrealEd/Public/SViewportToolBarComboMenu.h"
 #include "Editor/UnrealEd/Public/Kismet2/DebuggerCommands.h"
+#include "RHI.h"
 #include "Styling/AppStyle.h"
 #include "Framework/Application/SlateApplication.h"
 #include "Framework/MultiBox/MultiBoxBuilder.h"
