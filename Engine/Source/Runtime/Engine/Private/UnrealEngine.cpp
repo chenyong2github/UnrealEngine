40fa760e21d16050b8f77afec1b23470b068297b
jason nadro <jason.nadro@epicgames.com>
2022-11-02 23:07:49 -0400

    ScalabilityCVarsSinkCallback should leverage On Demand Shader Compilation (ODSC) and only compile visible shaders instead of _all_ remaining shaders.
    - If the material quality level changes we still need to potentially create new material resources for that quality level.
    - Don't submit compile jobs when we call `AllMaterialsCacheResourceShadersForRendering`.  This results in just creating the render resources if they haven't been made yet.
    - ODSC will handle compiling any missing shaders.
    
    #rb Dan.Elksnitis
    #preflight 6362d2e86000216d2e23d3b5
    
    [CL 22937972 by jason nadro in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -817,6 +817,8 @@ void ScalabilityCVarsSinkCallback()
 	{
 		bool bRecreateRenderstate = false;
 		bool bCacheResourceShaders = false;
+		// Don't cache _all_ shaders explicitly and let On Demand Shader Compilation (ODSC) handle compiling the shaders visible.
+		const bool bCacheAllRemainingShaders = false;
 
 		if (LocalScalabilityCVars.DetailMode != GCachedScalabilityCVars.DetailMode)
 		{
@@ -825,6 +827,8 @@ void ScalabilityCVarsSinkCallback()
 
 		if (LocalScalabilityCVars.MaterialQualityLevel != GCachedScalabilityCVars.MaterialQualityLevel)
 		{
+			// If the material quality level changes we need to potentially create new FMaterialRenderResources
+			// for the new quality level.
 			bCacheResourceShaders = true;
 		}
 
@@ -841,8 +845,8 @@ void ScalabilityCVarsSinkCallback()
 			if (bCacheResourceShaders)
 			{
 				// For all materials, UMaterial::CacheResourceShadersForRendering
-				UMaterial::AllMaterialsCacheResourceShadersForRendering();
-				UMaterialInstance::AllMaterialsCacheResourceShadersForRendering(true);
+				UMaterial::AllMaterialsCacheResourceShadersForRendering(false, bCacheAllRemainingShaders);
+				UMaterialInstance::AllMaterialsCacheResourceShadersForRendering(true, bCacheAllRemainingShaders);
 			}
 		}
 		else
