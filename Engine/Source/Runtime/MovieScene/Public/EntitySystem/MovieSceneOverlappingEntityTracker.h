ec009297c6e340bab20593bca7d393d7501add37
andrew rodham <Andrew.Rodham@epicgames.com>
2022-11-04 16:40:25 -0400

    Sequencer: Added safety check to ensure that invalid IDs are not supplied from overlapping entity trackers
    #rb Max.Chen, Ludovic.Chabant
    
    [CL 22994718 by andrew rodham in ue5-main branch]

--- Engine/Source/Runtime/MovieScene/Public/EntitySystem/MovieSceneOverlappingEntityTracker.h
+++ Engine/Source/Runtime/MovieScene/Public/EntitySystem/MovieSceneOverlappingEntityTracker.h
@@ -220,9 +220,17 @@ struct TOverlappingEntityTrackerImpl
 				const uint16 OutputIndex = static_cast<uint16>(InvalidOutput.GetIndex());
 
 				InputArray.Reset();
-				for (auto Inputs = OutputToEntity.CreateConstKeyIterator(OutputIndex); Inputs; ++Inputs)
+				for (auto Inputs = OutputToEntity.CreateKeyIterator(OutputIndex); Inputs; ++Inputs)
 				{
-					InputArray.Add(Inputs.Value());
+					if (ensure(Linker->EntityManager.IsAllocated(Inputs.Value())))
+					{
+						InputArray.Add(Inputs.Value());
+					}
+					else
+					{
+						EntityToOutput.Remove(Inputs.Value());
+						Inputs.RemoveCurrent();
+					}
 				}
 
 				FOutput& Output = Outputs[OutputIndex];
