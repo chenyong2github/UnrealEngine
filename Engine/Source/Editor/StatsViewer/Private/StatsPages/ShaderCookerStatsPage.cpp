0e35e34a596b08d4189c415b31412a9c10809ef7
florin pascu <Florin.Pascu@epicgames.com>
2022-11-04 08:20:45 -0400

    Fix Shader cook statistics #jira UE-169169 #rb Jack.Porter #preflight 6364ebfcf233f06a12c0e32a
    [CL 22985065 by florin pascu in ue5-main branch]

--- Engine/Source/Editor/StatsViewer/Private/StatsPages/ShaderCookerStatsPage.cpp
+++ Engine/Source/Editor/StatsViewer/Private/StatsPages/ShaderCookerStatsPage.cpp
@@ -125,7 +125,7 @@ void FShaderCookerStats::Initialize(uint32 Index)
 		const EShaderPlatform Platform = static_cast<EShaderPlatform>(PlatformIndex);
 
 		const FName ShaderFormatName = FDataDrivenShaderPlatformInfo::IsValid(Platform)
-			? ShaderPlatformToShaderFormatName(Platform) : NAME_None;
+			? FDataDrivenShaderPlatformInfo::GetShaderFormat(Platform) : NAME_None;
 
 		if (ShaderFormatName != NAME_None)
 		{
