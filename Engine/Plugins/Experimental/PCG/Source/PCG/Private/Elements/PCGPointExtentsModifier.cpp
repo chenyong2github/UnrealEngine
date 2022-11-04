517d5a867da9c9ac472b2b43454eddb95c110277
clint levijoki <clint.levijoki@epicgames.com>
2022-11-04 16:39:25 -0400

    Added PCGPointExtentsModifier to convert one of Jimmy's BP nodes to native. This allows for adjustment of the BoundsMin and BoundsMax values. [REVIEW] [at]julien.lheureux #preflight 636513345c51adc95fde544f
    [CL 22994670 by clint levijoki in ue5-main branch]

---
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Private/Elements/PCGPointExtentsModifier.cpp
@@ -0,0 +1,88 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Elements/PCGPointExtentsModifier.h"
+
+#include "PCGHelpers.h"
+#include "Helpers/PCGSettingsHelpers.h"
+
+FPCGElementPtr UPCGPointExtentsModifierSettings::CreateElement() const
+{
+	return MakeShared<FPCGPointExtentsModifier>();
+}
+
+TArray<FPCGPinProperties> UPCGPointExtentsModifierSettings::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(PCGPinConstants::DefaultInputLabel, EPCGDataType::Spatial);	
+	PinProperties.Emplace(PCGPinConstants::DefaultParamsLabel, EPCGDataType::Param);
+	return PinProperties;
+}
+
+TArray<FPCGPinProperties> UPCGPointExtentsModifierSettings::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(PCGPinConstants::DefaultOutputLabel, EPCGDataType::Spatial);
+	return PinProperties;
+}
+
+bool FPCGPointExtentsModifier::ExecuteInternal(FPCGContext* Context) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGPointExtentsModifier::Execute);
+
+	const UPCGPointExtentsModifierSettings* Settings = Context->GetInputSettings<UPCGPointExtentsModifierSettings>();
+	check(Settings);
+
+	TArray<FPCGTaggedData> Inputs = Context->InputData.GetInputsByPin(PCGPinConstants::DefaultInputLabel);
+	TArray<FPCGTaggedData>& Outputs = Context->OutputData.TaggedData;
+	UPCGParamData* Params = Context->InputData.GetParams();
+
+	const EPCGPointExtentsModifierMode Mode = PCG_GET_OVERRIDEN_VALUE(Settings, Mode, Params);
+	const FVector Extents = PCG_GET_OVERRIDEN_VALUE(Settings, Extents, Params);
+
+	switch (Mode)
+	{
+	case EPCGPointExtentsModifierMode::Minimum:
+		ProcessPoints(Context, Inputs, Outputs, [Extents](const FPCGPoint& InPoint, FPCGPoint& OutPoint){
+			OutPoint = InPoint;
+			OutPoint.SetExtents(FVector::Min(Extents, InPoint.GetExtents()));
+			return true;
+		});
+		break;
+
+	case EPCGPointExtentsModifierMode::Maximum:
+		ProcessPoints(Context, Inputs, Outputs, [Extents](const FPCGPoint& InPoint, FPCGPoint& OutPoint){
+			OutPoint = InPoint;
+			OutPoint.SetExtents(FVector::Max(Extents, InPoint.GetExtents()));
+			return true;
+		});
+		break;
+
+	case EPCGPointExtentsModifierMode::Add:
+		ProcessPoints(Context, Inputs, Outputs, [Extents](const FPCGPoint& InPoint, FPCGPoint& OutPoint){
+			OutPoint = InPoint;
+			OutPoint.SetExtents(Extents + InPoint.GetExtents());
+			return true;
+		});
+		break;
+
+	case EPCGPointExtentsModifierMode::Multiply:
+		ProcessPoints(Context, Inputs, Outputs, [Extents](const FPCGPoint& InPoint, FPCGPoint& OutPoint){
+			OutPoint = InPoint;
+			OutPoint.SetExtents(Extents * InPoint.GetExtents());
+			return true;
+		});
+		break;
+
+	case EPCGPointExtentsModifierMode::Set:
+		ProcessPoints(Context, Inputs, Outputs, [Extents](const FPCGPoint& InPoint, FPCGPoint& OutPoint){
+			OutPoint = InPoint;
+			OutPoint.SetExtents(Extents);
+			return true;
+		});
+		break;
+	}
+
+	Outputs.Append(Context->InputData.GetAllSettings());
+	
+	return true;
+}
---
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Public/Elements/PCGPointExtentsModifier.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGSettings.h"
+#include "PCGElement.h"
+#include "PCGNode.h"
+
+#include "Elements/PCGPointProcessingElementBase.h"
+
+#include "PCGPointExtentsModifier.generated.h"
+
+UENUM()
+enum class EPCGPointExtentsModifierMode : uint8
+{
+	Set,
+	Minimum,
+	Maximum,
+	Add,
+	Multiply
+};
+
+UCLASS(BlueprintType, ClassGroup = (Procedural))
+class PCG_API UPCGPointExtentsModifierSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	// ~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return FName(TEXT("ExtentsModifierNode")); }
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Spatial; }
+#endif
+
+	virtual TArray<FPCGPinProperties> InputPinProperties() const;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const;
+
+protected:
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings)
+	FVector Extents = FVector::One();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings)
+	EPCGPointExtentsModifierMode Mode = EPCGPointExtentsModifierMode::Set;
+};
+
+class FPCGPointExtentsModifier : public FPCGPointProcessingElementBase
+{
+protected:
+	virtual bool ExecuteInternal(FPCGContext* Context) const;
+};
