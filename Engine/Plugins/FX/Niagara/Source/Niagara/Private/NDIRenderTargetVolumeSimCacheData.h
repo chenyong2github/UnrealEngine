c22e95f1446c296acdadb418895eb29d9de338f3
stu mckenna <Stu.McKenna@epicgames.com>
2022-11-09 20:08:19 -0500

    - Restore 5.2 change
    #rb trivial
    #rnx
    
    [CL 23068873 by stu mckenna in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NDIRenderTargetVolumeSimCacheData.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NDIRenderTargetVolumeSimCacheData.h
@@ -1,9 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 
-#include "Serialization/BulkData.h"
-#include "PixelFormat.h"
-
 #include "NDIRenderTargetVolumeSimCacheData.generated.h"
 
 USTRUCT()
