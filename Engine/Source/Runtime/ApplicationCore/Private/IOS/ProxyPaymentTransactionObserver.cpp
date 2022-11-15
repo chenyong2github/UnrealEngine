fa8f4245eab0a6277b89df6b04c549bd3255d91e
Rafa Lecina <rafa.lecina@xa.epicgames.com>
2022-11-15 04:43:10 -0500

    Add missing dealloc instance method found by static analysis
    #jira UE-169916
    #review @Sam.Zamani, @Bertrand.Carre, @Chris.Varnsverry
    #preflight
    
    [CL 23133521 by Rafa Lecina in ue5-main branch]

--- Engine/Source/Runtime/ApplicationCore/Private/IOS/ProxyPaymentTransactionObserver.cpp
+++ Engine/Source/Runtime/ApplicationCore/Private/IOS/ProxyPaymentTransactionObserver.cpp
@@ -21,6 +21,11 @@
 	return self;
 }
 
+-(void) dealloc
+{
+    [super dealloc];
+}
+
 -(void)paymentQueue:(SKPaymentQueue*)queue updatedTransactions : (NSArray*)transactions
 {
     if(self.ossObserver)
