163742f4eccbe1dfe39cda8de92b80dd880ec3ca
Dominik Peacock <dominik.peacock@geotech.one>
2022-11-16 05:29:52 -0500

    Fix input profile settings not getting saved
    #jira UE-170124
    #rb Jason.Walter
    #preflight trivial
    
    [CL 23152083 by Dominik Peacock in ue5-main branch]

--- Engine/Plugins/Experimental/VirtualCamera/Source/VCamInput/Public/VCamInputSettings.h
+++ Engine/Plugins/Experimental/VirtualCamera/Source/VCamInput/Public/VCamInputSettings.h
@@ -20,28 +20,27 @@ struct VCAMINPUT_API FVCamInputProfile
 	bool operator==(const FVCamInputProfile& OtherProfile) const;
 };
 
-UCLASS(config=Game)
+UCLASS(config=Game, DefaultConfig)
 class VCAMINPUT_API UVCamInputSettings : public UDeveloperSettings
 {
 	GENERATED_BODY()
-
 public:
+	
 	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, BlueprintSetter=SetDefaultInputProfile, Category="Input", meta=(GetOptions=GetInputProfileNames))
 	FName DefaultInputProfile;
-
-	UFUNCTION(BlueprintCallable, Category="VCam Input")
-	void SetDefaultInputProfile(const FName NewDefaultInputProfile);
 	
 	UPROPERTY(EditAnywhere, Config, BlueprintReadWrite, BlueprintSetter=SetInputProfiles, Category="Input")
 	TMap<FName, FVCamInputProfile> InputProfiles;
-
+	
+	UFUNCTION(BlueprintCallable, Category="VCam")
+	static UVCamInputSettings* GetVCamInputSettings();
+	
+	UFUNCTION(BlueprintCallable, Category="VCam Input")
+	void SetDefaultInputProfile(const FName NewDefaultInputProfile);
+	
 	UFUNCTION(BlueprintCallable, Category="VCam Input")
 	void SetInputProfiles(const TMap<FName, FVCamInputProfile>& NewInputProfiles);
 
 	UFUNCTION(BlueprintCallable, Category="VCam Input")
 	TArray<FName> GetInputProfileNames() const;
-	
-private:
-	UFUNCTION(BlueprintCallable, Category="VCam")
-	static UVCamInputSettings* GetVCamInputSettings();
 };
