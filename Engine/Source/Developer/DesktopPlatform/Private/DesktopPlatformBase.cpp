9f8a7dfb32f5c80e4347ce88fd79222b2f1359a5
Joakim Lindqvist <Joakim.Lindqvist@epicgames.com>
2022-11-17 06:39:30 -0500

    OidcToken - Added error page when there is a problem logging in, polished the look of the success page. Also updated the text in the popup prompt for login to read a bit better.
    #preflight none
    
    [CL 23174150 by Joakim Lindqvist in ue5-main branch]

--- Engine/Source/Developer/DesktopPlatform/Private/DesktopPlatformBase.cpp
+++ Engine/Source/Developer/DesktopPlatform/Private/DesktopPlatformBase.cpp
@@ -923,8 +923,8 @@ bool FDesktopPlatformBase::GetOidcTokenStatus(const FString& RootDir, const FStr
 
 bool FDesktopPlatformBase::GetOidcAccessTokenInteractive(const FString& RootDir,  const FString& Arguments, FFeedbackContext* Warn, int32& OutReturnCode)
 {
-	FText OidcInteractivePromptTitle = NSLOCTEXT("OidcToken", "OidcToken_InteractiveLaunchPromptTitle", "Unreal Engine - User Login");
-	FText OidcInteractiveLaunchPromptText = NSLOCTEXT("OidcToken", "OidcToken_InteractiveLaunch", "Authentication is required for Http Derived Data Cache to work. Your system browser will now be opened for you to finish the login process.");
+	FText OidcInteractivePromptTitle = NSLOCTEXT("OidcToken", "OidcToken_InteractiveLaunchPromptTitle", "Unreal Engine - Authentication Required");
+	FText OidcInteractiveLaunchPromptText = NSLOCTEXT("OidcToken", "OidcToken_InteractiveLaunch", "Your team’s preferred DDC (Distributed Data Cache) requires you to log in. Click OK to open the authentication page in your web browser.\n\nYou can cancel authentication and work with a different shared or local DDC instead. However, this may cause delays while the editor prepares the assets you need.");
 	EAppReturnType::Type userAcknowledgedResult = FPlatformMisc::MessageBoxExt(EAppMsgType::OkCancel, *OidcInteractiveLaunchPromptText.ToString(), *OidcInteractivePromptTitle.ToString());
 
 	if (userAcknowledgedResult != EAppReturnType::Ok)
