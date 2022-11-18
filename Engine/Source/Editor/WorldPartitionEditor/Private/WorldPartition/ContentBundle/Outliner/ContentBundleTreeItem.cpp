d0bd24987e40cdcbaa27198ac83e055922a875c9
richard malo <Richard.Malo@epicgames.com>
2022-11-18 10:22:26 -0500

    Added (Current) on the current/editing Content Bundle (like Data Layers and Folder). #rb patrick.enfedaque
    [CL 23194458 by richard malo in ue5-main branch]

--- Engine/Source/Editor/WorldPartitionEditor/Private/WorldPartition/ContentBundle/Outliner/ContentBundleTreeItem.cpp
+++ Engine/Source/Editor/WorldPartitionEditor/Private/WorldPartition/ContentBundle/Outliner/ContentBundleTreeItem.cpp
@@ -41,7 +41,7 @@ FString FContentBundleTreeItem::GetDisplayString() const
 	TSharedPtr<FContentBundleEditor> ContentBundleEditorPin = ContentBundleEditor.Pin();
 	if (ContentBundleEditorPin != nullptr)
 	{
-		return ContentBundleEditorPin->GetDisplayName();
+		return FString::Printf(TEXT("%s%s"), *ContentBundleEditorPin->GetDisplayName(), ContentBundleEditorPin->IsBeingEdited() ? TEXT(" (Current)") : TEXT(""));
 	}
 
 	return LOCTEXT("ContentBundleMissingForTreeItem", "(Invalid Content Bundle)").ToString();
