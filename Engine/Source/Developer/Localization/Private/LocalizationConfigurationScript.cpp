1f3141bdff37816790884cb80114c088f9f9697c
forsakenyang <forsakenyang@vip.qq.com>
2022-11-29 19:03:04 -0500

    PR #9270: Bugfix: remove unnecessary quotes of OutputKeys in FooTarget_Gather.ini when gather texts from meta data via Localization Dashboard. (Contributed by forsakenyang)
    #jira: UE-156187
    #rb: Jamie.Dale
    #preflight: 63868662b21cdd3a267b432f
    #9270
    
    [CL 23320722 by forsakenyang in ue5-main branch]

--- Engine/Source/Developer/Localization/Private/LocalizationConfigurationScript.cpp
+++ Engine/Source/Developer/Localization/Private/LocalizationConfigurationScript.cpp
@@ -424,7 +424,7 @@ namespace LocalizationConfigurationScript
 			{
 				ConfigSection.Add( TEXT("InputKeys"), Specification.MetaDataKey.Name );
 				ConfigSection.Add( TEXT("OutputNamespaces"), Specification.TextNamespace );
-				ConfigSection.Add( TEXT("OutputKeys"), FString::Printf(TEXT("\"%s\""), *Specification.TextKeyPattern.Pattern) );
+				ConfigSection.Add(TEXT("OutputKeys"), Specification.TextKeyPattern.Pattern);
 			}
 
 			ConfigSection.Add( TEXT("ShouldGatherFromEditorOnlyData"), Target->Settings.GatherFromMetaData.ShouldGatherFromEditorOnlyData ? TEXT("true") : TEXT("false") );
