7c175bb38759a39fa9927f4a24b8ae22dc6baa6e
Jimmy Andrews <Jimmy.Andrews@epicgames.com>
2022-11-30 23:16:34 -0500

    Add fTetWild (adapted to work with UE) as a new third party library in GeometryAlgorithms
    #rb ryan.kautzman
    #preflight 6387ba677b4bd3f05727dbaf
    
    [CL 23346740 by Jimmy Andrews in ue5-main branch]

--- Engine/Plugins/Runtime/GeometryProcessing/Source/GeometryAlgorithms/GeometryAlgorithms.Build.cs
+++ Engine/Plugins/Runtime/GeometryProcessing/Source/GeometryAlgorithms/GeometryAlgorithms.Build.cs
@@ -18,6 +18,23 @@ public class GeometryAlgorithms : ModuleRules
 				"GeometryCore"
 			}
 			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"Eigen",
+			}
+		);
 		UnsafeTypeCastWarningLevel = WarningLevel.Error;
+
+		// fTetWild can optinally use TBB for multithreading, but it is not consistently faster to do so; currently default-disabled
+		bool bUseTBB = false;
+		if (bUseTBB && // Note TBB is not supported on all platforms, so we only enable it on windows and mac
+			(Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) || Target.Platform == UnrealTargetPlatform.Mac))
+		{
+			PrivateDefinitions.Add("FLOAT_TETWILD_USE_TBB");
+			AddEngineThirdPartyPrivateStaticDependencies(Target,
+				"IntelTBB"
+			);
+		}
 	}
 }
