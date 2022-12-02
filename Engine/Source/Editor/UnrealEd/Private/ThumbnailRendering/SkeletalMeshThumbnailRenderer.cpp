5871617817cb284f4abf065028c1ea39223987a5
Alexis Matte <alexis.matte@epicgames.com>
2022-12-02 09:53:01 -0500

    Make sure skeletalmesh thumbnail renderer do not stall the main thread.
    #jira none
    #rb trivial
    #rnx
    #preflight 638a0e003b617fd4be4420e1
    
    [CL 23372421 by Alexis Matte in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/ThumbnailRendering/SkeletalMeshThumbnailRenderer.cpp
+++ Engine/Source/Editor/UnrealEd/Private/ThumbnailRendering/SkeletalMeshThumbnailRenderer.cpp
@@ -52,7 +52,7 @@ bool USkeletalMeshThumbnailRenderer::CanVisualizeAsset(UObject* Object)
 		return false;
 	}
 	USkeletalMesh* SkeletalMesh = Cast<USkeletalMesh>(Object);
-	const bool bValidRenderData = SkeletalMesh && SkeletalMesh->GetResourceForRendering();
+	const bool bValidRenderData = SkeletalMesh && !SkeletalMesh->IsCompiling() && SkeletalMesh->GetResourceForRendering();
 	return bValidRenderData;
 }
 
