f496907abfddec291016e611d9cd901595a85d4c
christopher waters <christopher.waters@epicgames.com>
2022-12-06 19:50:10 -0500

    Moving ERHIAccess into its own header.
    #preflight 638f5b061776b8c21c997220
    
    [CL 23421936 by christopher waters in ue5-main branch]

--- Engine/Source/Runtime/RHI/Public/RHI.h
+++ Engine/Source/Runtime/RHI/Public/RHI.h
@@ -20,6 +20,7 @@
 #include "GpuProfilerTrace.h"
 #include "RHIShaderPlatform.h"
 #include "RHIFeatureLevel.h"
+#include "RHIAccess.h"
 
 class FResourceArrayInterface;
 class FResourceBulkDataInterface;
@@ -812,83 +813,6 @@ public:
 		return Base::operator[](GetRHIPipelineIndex(Pipeline));
 	}
 };
-
-enum class ERHIAccess
-{
-	// Used when the previous state of a resource is not known,
-	// which implies we have to flush all GPU caches etc.
-	Unknown = 0,
-
-	// Read states
-	CPURead             	= 1 <<  0,
-	Present             	= 1 <<  1,
-	IndirectArgs        	= 1 <<  2,
-	VertexOrIndexBuffer 	= 1 <<  3,
-	SRVCompute          	= 1 <<  4,
-	SRVGraphics         	= 1 <<  5,
-	CopySrc             	= 1 <<  6,
-	ResolveSrc          	= 1 <<  7,
-	DSVRead					= 1 <<  8,
-
-	// Read-write states
-	UAVCompute          	= 1 <<  9,
-	UAVGraphics         	= 1 << 10,
-	RTV                 	= 1 << 11,
-	CopyDest            	= 1 << 12,
-	ResolveDst          	= 1 << 13,
-	DSVWrite            	= 1 << 14,
-
-	// Ray tracing acceleration structure states.
-	// Buffer that contains an AS must always be in either of these states.
-	// BVHRead -- required for AS inputs to build/update/copy/trace commands.
-	// BVHWrite -- required for AS outputs of build/update/copy commands.
-	BVHRead                  = 1 << 15,
-	BVHWrite                 = 1 << 16,
-
-	// Invalid released state (transient resources)
-	Discard					= 1 << 17,
-
-	// Shading Rate Source
-	ShadingRateSource	= 1 << 18,
-
-	Last = ShadingRateSource,
-	None = Unknown,
-	Mask = (Last << 1) - 1,
-
-	// A mask of the two possible SRV states
-	SRVMask = SRVCompute | SRVGraphics,
-
-	// A mask of the two possible UAV states
-	UAVMask = UAVCompute | UAVGraphics,
-
-	// A mask of all bits representing read-only states which cannot be combined with other write states.
-	ReadOnlyExclusiveMask = CPURead | Present | IndirectArgs | VertexOrIndexBuffer | SRVGraphics | SRVCompute | CopySrc | ResolveSrc | BVHRead,
-
-	// A mask of all bits representing read-only states on the compute pipe which cannot be combined with other write states.
-	ReadOnlyExclusiveComputeMask = CPURead | IndirectArgs | SRVCompute | CopySrc | BVHRead,
-
-	// A mask of all bits representing read-only states which may be combined with other write states.
-	ReadOnlyMask = ReadOnlyExclusiveMask | DSVRead | ShadingRateSource,
-
-	// A mask of all bits representing readable states which may also include writable states.
-	ReadableMask = ReadOnlyMask | UAVMask,
-
-	// A mask of all bits representing write-only states which cannot be combined with other read states.
-	WriteOnlyExclusiveMask = RTV | CopyDest | ResolveDst,
-
-	// A mask of all bits representing write-only states which may be combined with other read states.
-	WriteOnlyMask = WriteOnlyExclusiveMask | DSVWrite,
-
-	// A mask of all bits representing writable states which may also include readable states.
-	WritableMask = WriteOnlyMask | UAVMask | BVHWrite
-};
-ENUM_CLASS_FLAGS(ERHIAccess)
-
-/** Mask of states which are allowed to be considered for state merging. */
-extern RHI_API ERHIAccess GRHIMergeableAccessMask;
-
-/** Mask of states which are allowed to be considered for multi-pipeline state merging. This should be a subset of GRHIMergeableAccessMask. */
-extern RHI_API ERHIAccess GRHIMultiPipelineMergeableAccessMask;
 
 /** to customize the RHIReadSurfaceData() output */
 class FReadSurfaceDataFlags
@@ -1766,43 +1690,6 @@ struct FRHICopyTextureInfo
 	uint32 NumMips = 1;
 };
 
-inline constexpr bool IsReadOnlyExclusiveAccess(ERHIAccess Access)
-{
-	return EnumHasAnyFlags(Access, ERHIAccess::ReadOnlyExclusiveMask) && !EnumHasAnyFlags(Access, ~ERHIAccess::ReadOnlyExclusiveMask);
-}
-
-inline constexpr bool IsReadOnlyAccess(ERHIAccess Access)
-{
-	return EnumHasAnyFlags(Access, ERHIAccess::ReadOnlyMask) && !EnumHasAnyFlags(Access, ~ERHIAccess::ReadOnlyMask);
-}
-
-inline constexpr bool IsWriteOnlyAccess(ERHIAccess Access)
-{
-	return EnumHasAnyFlags(Access, ERHIAccess::WriteOnlyMask) && !EnumHasAnyFlags(Access, ~ERHIAccess::WriteOnlyMask);
-}
-
-inline constexpr bool IsWritableAccess(ERHIAccess Access)
-{
-	return EnumHasAnyFlags(Access, ERHIAccess::WritableMask);
-}
-
-inline constexpr bool IsReadableAccess(ERHIAccess Access)
-{
-	return EnumHasAnyFlags(Access, ERHIAccess::ReadableMask);
-}
-
-inline constexpr bool IsInvalidAccess(ERHIAccess Access)
-{
-	return
-		((EnumHasAnyFlags(Access, ERHIAccess::ReadOnlyExclusiveMask) && EnumHasAnyFlags(Access, ERHIAccess::WritableMask)) ||
-		 (EnumHasAnyFlags(Access, ERHIAccess::WriteOnlyExclusiveMask) && EnumHasAnyFlags(Access, ERHIAccess::ReadableMask)));
-}
-
-inline constexpr bool IsValidAccess(ERHIAccess Access)
-{
-	return !IsInvalidAccess(Access);
-}
-
 enum class ERHITransitionCreateFlags
 {
 	None = 0,
@@ -1831,7 +1718,6 @@ enum class EResourceTransitionFlags
 };
 ENUM_CLASS_FLAGS(EResourceTransitionFlags);
 
-RHI_API FString GetRHIAccessName(ERHIAccess Access);
 RHI_API FString GetResourceTransitionFlagsName(EResourceTransitionFlags Flags);
 RHI_API FString GetRHIPipelineName(ERHIPipeline Pipeline);
 
