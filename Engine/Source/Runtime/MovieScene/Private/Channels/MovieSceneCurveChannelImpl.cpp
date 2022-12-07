fb7f7ebcbea1ce9d12250362e394e329d5684f49
bernard lambert <bernard.lambert@frimastudio.com>
2022-12-06 19:45:36 -0500

    Disabling the CachedChannelEvaluation logic since it was breaking animations playing in reverse on certain platforms.
    [REVIEW] [at]Mic.Rooney [at]Andrew.Rodham
    [FYI] [at]Mathieu.Chamberland [at]Maxime.Vaillancourt
    
    [CL 23421647 by bernard lambert in ue5-main branch]

--- Engine/Source/Runtime/MovieScene/Private/Channels/MovieSceneCurveChannelImpl.cpp
+++ Engine/Source/Runtime/MovieScene/Private/Channels/MovieSceneCurveChannelImpl.cpp
@@ -36,7 +36,7 @@ static FAutoConsoleVariableRef CVarCachedChannelEvaluationParityThreshold(
 	TEXT("Threshold for testing Evaluate parity with cached/uncached routines."),
 	ECVF_Default);
 
-MOVIESCENE_API bool GEnableCachedChannelEvaluation = true;
+MOVIESCENE_API bool GEnableCachedChannelEvaluation = false;
 static FAutoConsoleVariableRef CVarEnableCachedChannelEvaluation(
 	TEXT("Sequencer.EnableCachedChannelEvaluation"),
 	GEnableCachedChannelEvaluation,
