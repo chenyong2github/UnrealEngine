3506ea5a1eb1a117200b0a2e734c8e50ab1b3c67
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2022-12-08 07:38:08 -0500

    Fix 'reachable garbage object' errors in anim BP thumbnail preview scenes
    Cherry-picking fix for dangling ptrs to preview instances in debug skeletal DebugSkelMeshComponent, also removing the explicit MarkAsGarbage call for preview instances.
    
    Original change, CL 23407330:
    DebugSkelMeshComponent: Clear preview instance correctly
    
    #rb thomas.sarkanen,Helge.Mathee
    #jira UE-171723
    #preflight 638f214cc0652bbec24f3f6d
    #preflight 6391ba7f9549ddaa2821510b
    
    [CL 23445097 by thomas sarkanen in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/ThumbnailHelpers.cpp
+++ Engine/Source/Editor/UnrealEd/Private/ThumbnailHelpers.cpp
@@ -898,15 +898,7 @@ bool FAnimBlueprintThumbnailScene::SetAnimBlueprint(class UAnimBlueprint* InBlue
 			{
 				bSetSucessfully = true;
 
-				UAnimInstance* PreviousInstance = PreviewActor->GetSkeletalMeshComponent()->GetAnimInstance();
-
 				PreviewActor->GetSkeletalMeshComponent()->SetAnimInstanceClass(InBlueprint->GeneratedClass);
-
-				if (PreviousInstance && PreviousInstance != PreviewActor->GetSkeletalMeshComponent()->GetAnimInstance())
-				{
-					//Mark this as gone!
-					PreviousInstance->MarkAsGarbage();
-				}
 
 				FTransform MeshTransform = FTransform::Identity;
 
