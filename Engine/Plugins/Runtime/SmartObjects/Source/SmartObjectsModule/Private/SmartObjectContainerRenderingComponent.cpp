eee5fd6311504c709eee6662d1cdf7b1abe792a2
Mieszko Zielinski <mieszko.zielinski@epicgames.com>
2022-12-09 12:24:23 -0500

    clang non-unity compilation fix
    #rb trivial
    #preflight 639368170236bc17cea82428
    #preflight 63936b6b0236bc17cea95b60
    
    [CL 23465122 by Mieszko Zielinski in ue5-main branch]

--- Engine/Plugins/Runtime/SmartObjects/Source/SmartObjectsModule/Private/SmartObjectContainerRenderingComponent.cpp
+++ Engine/Plugins/Runtime/SmartObjects/Source/SmartObjectsModule/Private/SmartObjectContainerRenderingComponent.cpp
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SmartObjectContainerRenderingComponent.h"
+#include "Engine/CollisionProfile.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(SmartObjectContainerRenderingComponent)
 
