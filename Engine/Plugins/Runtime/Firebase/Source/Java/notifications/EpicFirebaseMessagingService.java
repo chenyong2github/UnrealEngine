335ea429d89233901058acb1c28c068425d946c7
chris babcock <Chris.Babcock@epicgames.com>
2022-12-15 17:03:41 -0500

    Update Firebase to deal with IMMUTABLE crash
    #jira UE-173021
    #android
    [REVIEW] [at]Sean.Campbell
    #rb Sean.Campbell
    
    #preflight 639b7e8035203bc7aa7af7eb
    
    [CL 23532647 by chris babcock in ue5-main branch]

--- Engine/Plugins/Runtime/Firebase/Source/Java/notifications/EpicFirebaseMessagingService.java
+++ Engine/Plugins/Runtime/Firebase/Source/Java/notifications/EpicFirebaseMessagingService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -248,4 +249,96 @@ public class EpicFirebaseMessagingService extends FirebaseMessagingService {
 		JSONObject o = new JSONObject(payload);
 		return o.optString(ATTR_TYPE);
 	}
+
+	// Formally in EpicFirebaseInstanceIDService.java
+	private static final String PREFS_FILE_FIREBASE = "com.epicgames.firebase";
+	private static final String KEY_FIREBASE_TOKEN = "firebasetoken";
+	private static final String KEY_IS_UPDATED_TOKEN = "isUpdatedToken";
+	private static final String KEY_IS_REGISTERED = "isRegistered";
+
+	@Override
+	public void onNewToken(@NonNull String firebaseToken) {
+		Log.debug("Refreshed Firebase token: " + firebaseToken);
+		if (TextUtils.isEmpty(firebaseToken)) {
+			Log.error("Firebase token is empty or null");
+		} else {
+			saveFirebaseToken(this, firebaseToken);
+		}
+	}
+
+	private static void saveFirebaseToken(@NonNull Context context, @NonNull String firebaseToken) {
+		Log.debug("Firebase token to save : " + firebaseToken);
+		String storedToken = getFirebaseTokenFromCache(context);
+		boolean isUpdatedToken = !TextUtils.isEmpty(storedToken);
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPreferences.edit();
+		Log.debug("Firebase token isUpdated : " + isUpdatedToken);
+		editor.putBoolean(KEY_IS_UPDATED_TOKEN, isUpdatedToken).apply();
+		editor.putBoolean(KEY_IS_REGISTERED, false).apply();
+		editor.putString(KEY_FIREBASE_TOKEN, firebaseToken).apply();
+	}
+
+	@SuppressWarnings("unused")
+	static boolean isFirebaseTokenUpdated(@NonNull Context context) {
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		boolean isUpdated = sharedPreferences.getBoolean(KEY_IS_UPDATED_TOKEN, true);
+		Log.debug("Firebase token isUpdatedToken is " + isUpdated);
+		return isUpdated;
+	}
+
+	public static boolean isFirebaseTokenRegistered(@NonNull Context context) {
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		return sharedPreferences.getBoolean(KEY_IS_REGISTERED, false);
+	}
+
+	public static void setFirebaseTokenRegistered(@NonNull Context context, boolean isRegistered) {
+		Log.debug("Firebase token isRegistered setting to " + isRegistered);
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPreferences.edit();
+		editor.putBoolean(KEY_IS_REGISTERED, isRegistered);
+		editor.apply();
+	}
+
+	public static void unregisterFirebaseToken(@NonNull Context context) {
+		setFirebaseTokenRegistered(context, false);
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPreferences.edit();
+		editor.remove(KEY_FIREBASE_TOKEN);
+		editor.apply();
+		Log.debug("Firebase token cleared");
+	}
+
+	private static String getFirebaseTokenFromCache(@NonNull Context context) {
+		SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_FILE_FIREBASE, Context.MODE_PRIVATE);
+		return sharedPreferences.getString(KEY_FIREBASE_TOKEN, null);
+	}
+	
+	@Nullable
+	public static String getFirebaseToken(@NonNull Context context) {
+		String token = getFirebaseTokenFromCache(context);
+		Log.debug("Firebase token retrieved from cache: " + token);
+		if(TextUtils.isEmpty(token)) {
+			/*
+			Task<String> tokenTask = FirebaseMessaging.getInstance().getToken();
+			tokenTask.addOnCompleteListener(new OnCompleteListener<String>() {
+				@Override
+				public void onComplete(@NonNull Task<String> task) {
+					if (task.isSuccessful()) {
+						String token = task.getResult();
+						if(!TextUtils.isEmpty(token)) {
+							Log.debug("Firebase token retrieved from Firebase: " + token);
+							saveFirebaseToken(context, token);
+						}
+					}
+				}
+			});
+			// wait on task
+			Tasks.await(tokenTask);
+			token = getFirebaseTokenFromCache(context);
+			token = (token == null) ? "" : token;
+			*/
+		}
+		return token;
+	}
+
 }
