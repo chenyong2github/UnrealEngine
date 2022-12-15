f23650d0aeb243aca1ac5771e13efd55a1cfdf7d
Charles deRousiers <Charles.deRousiers@epicgames.com>
2022-12-15 09:01:20 -0500

    Fix CIS issue.
    #rb none
    #jira none
    #preflight 639b270643330e63e55e2794
    
    [CL 23526266 by Charles deRousiers in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVisibility.cpp
+++ Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVisibility.cpp
@@ -4210,9 +4210,7 @@ void RenderHairStrandsVisibilityBuffer(
 				RenderMode == HairVisibilityRenderMode_ComputeRaster ||
 				RenderMode == HairVisibilityRenderMode_ComputeRasterForward);
 
-			const bool bRunColorAndDepthPatching = 
-				(SceneMaterial0 && SceneMaterial1) &&
-				SceneColorTexture && (RenderMode != HairVisibilityRenderMode_ComputeRaster || RenderMode != HairVisibilityRenderMode_ComputeRasterForward);
+			const bool bRunColorAndDepthPatching = SceneMaterial0 != nullptr && SceneMaterial1 != nullptr && SceneColorTexture != nullptr && (RenderMode != HairVisibilityRenderMode_ComputeRaster || RenderMode != HairVisibilityRenderMode_ComputeRasterForward);
 
 			FRDGTextureRef HairOnlyDepthTexture = GraphBuilder.CreateTexture(SceneDepthTexture->Desc, TEXT("Hair.HairOnlyDepthTexture"));
 			FRDGTextureRef CoverageTexture = nullptr;
