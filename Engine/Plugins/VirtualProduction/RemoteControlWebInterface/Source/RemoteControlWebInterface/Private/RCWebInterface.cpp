fb431ad3be3dc254853f0e9d7dd51f0c0c1a2167
jeremie roy <jeremie.roy@epicgames.com>
2023-01-04 15:11:58 -0500

    Add logging to rc web interface startup Disable errors in rc interface temporarily until I figure out why the web app process still gets started on build machines
    #preflight 63b5bf4eff7b9ad703fdafed
    #rb none
    #jira UE-173304
    
    [CL 23579433 by jeremie roy in ue5-main branch]

--- Engine/Plugins/VirtualProduction/RemoteControlWebInterface/Source/RemoteControlWebInterface/Private/RCWebInterface.cpp
+++ Engine/Plugins/VirtualProduction/RemoteControlWebInterface/Source/RemoteControlWebInterface/Private/RCWebInterface.cpp
@@ -26,6 +26,15 @@ namespace RCWebInterface
 {
 	bool IsWebInterfaceEnabled()
 	{
+		const bool bIsRunningTest = FParse::Param(FCommandLine::Get(), TEXT("-gauntlet"));
+		if (bIsRunningTest)
+		{
+			// Temporary workaround to disable this module when running automated tests.
+			return false;
+		}
+
+		UE_LOG(LogRemoteControlWebInterface, Display, TEXT("Remote Control Web Interface startup, IsBuildMachine: %s IsRunningTest: %s"), GIsBuildMachine ? TEXT("Yes") : TEXT("No"), bIsRunningTest ? TEXT("Yes") : TEXT("No"));
+		
 		bool bIsEditor = false;
 
 #if WITH_EDITOR
@@ -33,7 +42,7 @@ namespace RCWebInterface
 #endif
 
 		// By default, remote control web interface is disabled in -game, packaged game, and on build machines.
-		return (!GIsBuildMachine && !IsRunningCommandlet() && bIsEditor) || FParse::Param(FCommandLine::Get(), TEXT("RCWebInterfaceEnable"));
+		return !GIsBuildMachine || (!IsRunningCommandlet() && bIsEditor) || FParse::Param(FCommandLine::Get(), TEXT("RCWebInterfaceEnable"));
 	}
 }
 
