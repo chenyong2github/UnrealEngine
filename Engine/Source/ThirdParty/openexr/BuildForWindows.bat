f969661ee1ab8d43f993deb12a22576b76c3fa66
bryan sefcik <bryan.sefcik@epicgames.com>
2023-01-04 19:43:05 -0500

    [openexr] Added Windows ARM64 support. #preflight 63b61a25c6265e21bc99bb0b
    [CL 23584025 by bryan sefcik in ue5-main branch]

--- Engine/Source/ThirdParty/openexr/BuildForWindows.bat
+++ Engine/Source/ThirdParty/openexr/BuildForWindows.bat
@@ -3,10 +3,12 @@ setlocal
 
 set OPENEXR_VERSION=3.1.5
 
+if [%1]==[] goto usage
+
 rem Set as VS2015 for backwards compatibility even though VS2019 is used
 rem when building.
 set COMPILER_VERSION_NAME=VS2015
-set ARCH_NAME=x64
+set ARCH_NAME=%1
 
 set UE_THIRD_PARTY_LOCATION=%cd%\..
 set IMATH_CMAKE_LOCATION=%UE_THIRD_PARTY_LOCATION%\Imath\Deploy\Imath-3.1.3\%COMPILER_VERSION_NAME%\%ARCH_NAME%\lib\cmake\Imath
@@ -28,7 +30,7 @@ set INSTALL_LIB_DIR=%COMPILER_VERSION_NAME%\%ARCH_NAME%\lib
 
 set INSTALL_LOCATION=%UE_MODULE_LOCATION%\Deploy\openexr-%OPENEXR_VERSION%
 set INSTALL_INCLUDE_LOCATION=%INSTALL_LOCATION%\%INSTALL_INCLUDEDIR%
-set INSTALL_WIN_LOCATION=%INSTALL_LOCATION%\%COMPILER_VERSION_NAME%
+set INSTALL_WIN_LOCATION=%INSTALL_LOCATION%\%COMPILER_VERSION_NAME%\%ARCH_NAME%
 
 if exist %BUILD_LOCATION% (
     rmdir %BUILD_LOCATION% /S /Q)
@@ -41,7 +43,8 @@ mkdir %BUILD_LOCATION%
 pushd %BUILD_LOCATION%
 
 echo Configuring build for OpenEXR version %OPENEXR_VERSION%...
-cmake -G "Visual Studio 16 2019" %SOURCE_LOCATION%^
+cmake -G "Visual Studio 17 2022" %SOURCE_LOCATION%^
+    -A %ARCH_NAME%^
     -DCMAKE_INSTALL_PREFIX="%INSTALL_LOCATION%"^
     -DCMAKE_PREFIX_PATH="%IMATH_CMAKE_LOCATION%"^
     -DZLIB_INCLUDE_DIR="%ZLIB_INCLUDE_LOCATION%"^
@@ -75,5 +78,10 @@ if %errorlevel% neq 0 exit /B %errorlevel%
 popd
 
 echo Done.
+exit /B 0
+
+:usage
+echo Arch: x64 or ARM64
+exit /B 1
 
 endlocal
