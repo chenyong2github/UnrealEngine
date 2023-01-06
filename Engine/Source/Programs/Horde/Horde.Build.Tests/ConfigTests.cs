03e7cce1687edda56381ec739a228bc061a7256e
Ben Marsh <Ben.Marsh@epicgames.com>
2023-01-06 17:03:09 -0500

    Horde: Add delays to confile file refresh tests, to ensure OS granularity on timestamps is large enough.
    #preflight none
    
    [CL 23603979 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Build.Tests/ConfigTests.cs
+++ Engine/Source/Programs/Horde/Horde.Build.Tests/ConfigTests.cs
@@ -112,10 +112,12 @@ namespace Horde.Build.Tests
 
 			// Check it returns the same object if the timestamp hasn't changed
 			IConfigData file1 = await source.GetAsync(fooUri, cancellationToken);
+			await Task.Delay(TimeSpan.FromSeconds(1));
 			IConfigData file2 = await source.GetAsync(fooUri, cancellationToken);
 			Assert.IsTrue(ReferenceEquals(file1, file2));
 
 			// Check it returns a new object if the timestamp HAS changed
+			await Task.Delay(TimeSpan.FromSeconds(1));
 			await FileReference.WriteAllBytesAsync(new FileReference(fooUri.LocalPath), data, cancellationToken);
 			IConfigData file3 = await source.GetAsync(fooUri, cancellationToken);
 			Assert.IsTrue(!ReferenceEquals(file1, file3));
