02c4d0db99da048fa845f1c51a22413adfddcfde
rinat abdrashitov <rinat.abdrashitov@epicgames.com>
2023-01-06 21:13:00 -0500

    Fixed missing copyright warning
    #rb trivial
    #jira none
    #preflight none
    
    [CL 23606579 by rinat abdrashitov in ue5-main branch]

--- Engine/Plugins/Runtime/GeometryProcessing/Source/DynamicMesh/Private/Operations/TransferBoneWeights.cpp
+++ Engine/Plugins/Runtime/GeometryProcessing/Source/DynamicMesh/Private/Operations/TransferBoneWeights.cpp
@@ -1,4 +1,4 @@
-// // Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Operations/TransferBoneWeights.h"
 #include "DynamicMesh/DynamicMesh3.h"
