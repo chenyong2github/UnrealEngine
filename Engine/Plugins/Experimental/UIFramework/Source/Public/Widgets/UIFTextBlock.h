26410b24b1a77de10377e4503b4a64b1dc66d92f
patrick boutot <Patrick.Boutot@epicgames.com>
2023-01-11 14:45:48 -0500

    Add shadow, color and other properties to all UI text widget. #preflight 63becacbd862fdd347574669
    [CL 23651682 by patrick boutot in ue5-main branch]

--- Engine/Plugins/Experimental/UIFramework/Source/Public/Widgets/UIFTextBlock.h
+++ Engine/Plugins/Experimental/UIFramework/Source/Public/Widgets/UIFTextBlock.h
@@ -12,13 +12,13 @@ class UTextBlock;
 /**
  *
  */
-UCLASS(DisplayName = "TextBlock UIFramework")
-class UIFRAMEWORK_API UUIFrameworkTextBlock : public UUIFrameworkWidget
+UCLASS(Abstract, HideDropDown)
+class UIFRAMEWORK_API UUIFrameworkTextBase : public UUIFrameworkWidget
 {
 	GENERATED_BODY()
 
 public:
-	UUIFrameworkTextBlock();
+	UUIFrameworkTextBase();
 
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
 	void SetText(FText Text);
@@ -30,6 +30,15 @@ public:
 	}
 	
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
+	void SetTextColor(FLinearColor TextColor);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Framework")
+	FLinearColor GetTextColor() const
+	{
+		return TextColor;
+	}
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
 	void SetJustification(ETextJustify::Type Justification);
 
 	UFUNCTION(BlueprintCallable, Category = "UI Framework")
@@ -37,20 +46,108 @@ public:
 	{
 		return Justification;
 	}
+	
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
+	void SetOverflowPolicy(ETextOverflowPolicy OverflowPolicy);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Framework")
+	ETextOverflowPolicy GetOverflowPolicy() const
+	{
+		return OverflowPolicy;
+	}
 
 	virtual void LocalOnUMGWidgetCreated() override;
 
 private:
 	UFUNCTION()
 	void OnRep_Text();
+	
+	UFUNCTION()
+	void OnRep_TextColor();
 
 	UFUNCTION()
 	void OnRep_Justification();
 
+	UFUNCTION()
+	void OnRep_OverflowPolicy();
+
+protected:
+	virtual void SetTextToWidget(const FText&)
+	{
+	}
+	virtual void SetTextColorToWidget(FLinearColor)
+	{
+	}
+	virtual void SetJustificationToWidget(ETextJustify::Type)
+	{
+	}
+	virtual void SetOverflowPolicyToWidget(ETextOverflowPolicy)
+	{
+	}
+
 private:
-	UPROPERTY(/*ExposeOnSpawn, */ReplicatedUsing=OnRep_Text)
+	UPROPERTY(/*ExposeOnSpawn, */ReplicatedUsing = OnRep_Text)
 	FText Text;
 
-	UPROPERTY(ReplicatedUsing=OnRep_Justification)
+	UPROPERTY(ReplicatedUsing = OnRep_TextColor)
+	FLinearColor TextColor = FLinearColor::Black;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Justification)
 	TEnumAsByte<ETextJustify::Type> Justification;
+
+	UPROPERTY(ReplicatedUsing = OnRep_OverflowPolicy)
+	ETextOverflowPolicy OverflowPolicy = ETextOverflowPolicy::Clip;
+};
+
+/**
+ *
+ */
+UCLASS(DisplayName = "TextBlock UIFramework")
+class UIFRAMEWORK_API UUIFrameworkTextBlock : public UUIFrameworkTextBase
+{
+	GENERATED_BODY()
+
+public:
+	UUIFrameworkTextBlock();
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
+	void SetShadowOffset(FVector2f ShadowOffset);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Framework")
+	FVector2f GetShadowOffset() const
+	{
+		return ShadowOffset;
+	}
+	
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "UI Framework")
+	void SetShadowColor(FLinearColor ShadowColor);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Framework")
+	FLinearColor GetShadowColor() const
+	{
+		return ShadowColor;
+	}
+
+	virtual void LocalOnUMGWidgetCreated() override;
+
+private:
+	UFUNCTION()
+	void OnRep_ShadowOffset();
+	UFUNCTION()
+	void OnRep_ShadowColor();
+
+protected:
+	virtual void SetTextToWidget(const FText&) override;
+	virtual void SetJustificationToWidget(ETextJustify::Type) override;
+	virtual void SetOverflowPolicyToWidget(ETextOverflowPolicy) override;
+	virtual void SetTextColorToWidget(FLinearColor) override;
+	virtual void SetShadowOffsetToWidget(FVector2f);
+	virtual void SetShadowColorToWidget(FLinearColor);
+
+private:
+	UPROPERTY(ReplicatedUsing = OnRep_ShadowOffset)
+	FVector2f ShadowOffset = FVector2f::ZeroVector;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_ShadowColor)
+	FLinearColor ShadowColor = FLinearColor::Black;
 };
