12f79ed022eaa5db9528105c0bad57d31e3155fd
paul chipchase <paul.chipchase@epicgames.com>
2023-01-11 04:59:24 -0500

    Move the remaining enums to the types header to reduce noise in the system header.
    #rb trivial
    #rnx
    #preflight 63be868daf3ebedd9965d2bd
    
    [CL 23643427 by paul chipchase in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Virtualization/VirtualizationSystem.h
+++ Engine/Source/Runtime/Core/Public/Virtualization/VirtualizationSystem.h
@@ -45,63 +45,6 @@ struct FPayloadActivityInfo
 	FActivity Cache;
 };
 
-/** Describes the type of storage to use for a given action */
-enum class EStorageType : int8
-{
-	/** Deprecated value, replaced by EStorageType::Cache */
-	Local UE_DEPRECATED(5.1, "Use EStorageType::Cache instead")  = -1,
-	/** Store in the local cache backends, this can be called from any thread */
-	Cache = 0,
-	/** Store in the persistent backends, this can only be called from the game thread due to limitations with ISourceControlModule. */
-	Persistent
-};
-
-/** 
- * The result of a query.
- * Success indicates that the query worked and that the results are valid and can be used.
- * Any other value indicates that the query failed in some manner and that the results cannot be trusted and should be discarded.
- */
-enum class EQueryResult : int8
-{
-	/** The query succeeded and the results are valid */
-	Success = 0,
-	/** The query failed with an unspecified error */
-	Failure_Unknown,
-	/** The query failed because the current virtualization system has not implemented it */
-	Failure_NotImplemented
-};
-
-/** Describes the status of a payload in regards to a backend storage system */
-enum class EPayloadStatus : int8
-{
-	/** The payload id was not value */
-	Invalid = -1,
-	/** The payload was not found in any backend for the given storage type */
-	NotFound = 0,
-	/** The payload was found in at least one backend but was not found in all backends available for the given storage type */
-	FoundPartial,
-	/** The payload was found in all of the backends available for the given storage type */
-	FoundAll
-};
-
-/** The result of the virtualization process */
-enum class EVirtualizationResult : uint8
-{
-	/** The virtualization process ran with no problems to completion. */
-	Success = 0,
-	/** The process failed before completion and nothing was virtualized */
-	Failed
-};
-
-/** The result of the re-hydration process */
-enum class ERehydrationResult : uint8
-{
-	/** The re-hydration process ran with no problems to completion. */
-	Success = 0,
-	/** The process failed before completion and nothing was re-hydrate */
-	Failed,
-};
-
 /** Info about a rehydration operation */
 struct FRehydrationInfo
 {
@@ -486,16 +429,6 @@ struct FVirtualizationResult
 
 };
 
-/** Options used when virtualizing packages */
-enum class EVirtualizationOptions : uint32
-{
-	None = 0,
-	/** Attempt to check out files from revision control if needed */
-	Checkout = 1 << 0
-};
-
-ENUM_CLASS_FLAGS(EVirtualizationOptions);
-
 /** Contains the results of the package virtualization process */
 struct FRehydrationResult
 {
@@ -535,16 +468,6 @@ struct FRehydrationResult
 	}
 };
 
-/** Options used when rehydrating packages */
-enum class ERehydrationOptions : uint32
-{
-	None = 0,
-	/** Attempt to check out files from revision control if needed */
-	Checkout = 1 << 0
-};
-
-ENUM_CLASS_FLAGS(ERehydrationOptions);
-
 /** 
  * The set of parameters to be used when initializing the virtualization system. The 
  * members must remain valid for the duration of the call to ::Initialize. It is not
@@ -566,17 +489,6 @@ struct FInitParams
 	/** The config file to load the settings from (will default to GEngineIni) */
 	const FConfigFile& ConfigFile;
 };
-
-/** Optional flags that change how the VA system is initialized */
-enum class EInitializationFlags : uint32
-{
-	/** No flags are set */
-	None			= 0,
-	/** Forces the initialization to occur, ignoring and of the lazy initialization flags */
-	ForceInitialize = 1 << 0
-};
-
-ENUM_CLASS_FLAGS(EInitializationFlags);
 
 /**
  * Creates the global IVirtualizationSystem if it has not already been set up. This can be called explicitly
