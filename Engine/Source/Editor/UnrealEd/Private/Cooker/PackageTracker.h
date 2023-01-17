092fc21dcd131f76d56e790907513b0fc7de40ba
matt peters <Matt.Peters@epicgames.com>
2023-01-16 21:09:11 -0500

    MPCook: Fix bad robomerge (2); removal of FPackageTracker.Lock should from other branch have been suppressed from reaching this branch. #rb None, trivial #rnx #preflight None, trivial
    [CL 23734841 by matt peters in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/Cooker/PackageTracker.h
+++ Engine/Source/Editor/UnrealEd/Private/Cooker/PackageTracker.h
@@ -217,10 +217,12 @@ public:
 	}
 	void AddExpectedNeverLoadPackages(TArrayView<FName> PackageNames)
 	{
+		FWriteScopeLock ScopeLock(Lock);
 		ExpectedNeverLoadPackages.Append(PackageNames);
 	}
 	void ClearExpectedNeverLoadPackages()
 	{
+		FWriteScopeLock ScopeLock(Lock);
 		ExpectedNeverLoadPackages.Empty();
 	}
 private:
