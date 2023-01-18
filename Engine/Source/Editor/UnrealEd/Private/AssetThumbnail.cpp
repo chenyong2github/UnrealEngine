5d9736ac6390d5540e7219c632056bcfb8e21f0d
dave belanger <Dave.Belanger@epicgames.com>
2023-01-18 15:49:17 -0500

    Fixed regression fetching asset thumbnails from the package #rb none #preflight skip
    [CL 23763261 by dave belanger in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/AssetThumbnail.cpp
+++ Engine/Source/Editor/UnrealEd/Private/AssetThumbnail.cpp
@@ -1253,6 +1253,7 @@ bool FAssetThumbnailPool::LoadThumbnail(TSharedRef<FThumbnailInfo> ThumbnailInfo
 		}
 	}
 
+	FThumbnailMap ThumbnailMap;
 	// If we could not render a fresh thumbnail, see if we already have a cached one to load
 	const FObjectThumbnail* FoundThumbnail = ThumbnailTools::FindCachedThumbnail(AssetData.GetFullName());
 
