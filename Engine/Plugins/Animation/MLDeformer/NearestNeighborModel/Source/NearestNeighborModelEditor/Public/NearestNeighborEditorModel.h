a4b8a5f3e1ba2c43df362dab4166fe2baeebd334
zhenglin geng <zhenglin.geng@epicgames.com>
2023-01-20 21:23:25 -0500

    Undo //UE5/Release-Engine-Staging/Engine/Plugins/Animation/MLDeformer/NearestNeighborModel/... changelist 23801366 #jira none #rb none #preflight none
    [CL 23801931 by zhenglin geng in ue5-main branch]

--- Engine/Plugins/Animation/MLDeformer/NearestNeighborModel/Source/NearestNeighborModelEditor/Public/NearestNeighborEditorModel.h
+++ Engine/Plugins/Animation/MLDeformer/NearestNeighborModel/Source/NearestNeighborModelEditor/Public/NearestNeighborEditorModel.h
@@ -65,6 +65,8 @@ namespace UE::NearestNeighborModel
 
 		void UpdateNearestNeighborActors();
 		void KMeansClusterPoses();
+		TPair<UAnimSequence*, uint8> CreateAnimOfClusterCenters(const FString& PackageName, const TArray<int32>& KmeansResults);
+		uint8 GetKMeansClusterResult() const { return KMeansClusterResult; }
 
 		uint8 InitMorphTargets();
 		void RefreshMorphTargets();
@@ -103,9 +105,8 @@ namespace UE::NearestNeighborModel
 
 	private:
 		UWorld* EditorWorld = nullptr;
-		uint8 ClothPartUpdateResult = 0;
-		uint8 NearestNeighborUpdateResult = 0;
 		uint8 MorphTargetUpdateResult = 0;
+		uint8 KMeansClusterResult = 0;
 		int32 NumTrainingFramesOverride = -1;
 	};
 }	// namespace UE::NearestNeighborModel
