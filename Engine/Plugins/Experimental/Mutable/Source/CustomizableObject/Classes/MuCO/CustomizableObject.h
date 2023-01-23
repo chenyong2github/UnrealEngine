4c8da375a51cc138de2062de6fcc5df0baff5dd6
gerard martin <gerard.martin@epicgames.com>
2023-01-23 12:51:06 -0500

    [Mutable] Solve Mutable Mesh deserialization crash.
    [REVIEW] [at]alexei.lebedev, [at]pere.rifa
    #rnx
    
    [CL 23815961 by gerard martin in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Classes/MuCO/CustomizableObject.h
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Classes/MuCO/CustomizableObject.h
@@ -1204,7 +1204,7 @@ private:
 	// This is a manual version number for the binary blobs in this asset.
 	// Increasing it invalidates all the previously compiled models.
 	// Warning: If while merging code both versions have changed, take the highest+1.
-	static const int32 CurrentSupportedVersion = 365;
+	static const int32 CurrentSupportedVersion = 366;
 
 public:
 
