0979c789c7ace6ff662e1bcb2bf6cf06ef06fd0d
Charles deRousiers <Charles.deRousiers@epicgames.com>
2023-01-23 03:41:50 -0500

    Fix clumpID visualization.
    #rb none
    #jira none
    #preflight skip
    
    [CL 23808953 by Charles deRousiers in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.cpp
@@ -9451,7 +9451,7 @@ int32 FHLSLMaterialTranslator::GetHairSeed()
 
 int32 FHLSLMaterialTranslator::GetHairClumpID()
 {
-	return AddCodeChunk(MCT_UInt1, TEXT("MaterialExpressionGetHairClumpID(Parameters)"));
+	return AddCodeChunk(MCT_Float1, TEXT("MaterialExpressionGetHairClumpID(Parameters)"));
 }
 
 int32 FHLSLMaterialTranslator::GetHairTangent(bool bUseTangentSpace)
