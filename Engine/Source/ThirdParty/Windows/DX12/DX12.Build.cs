5f6fcda6b35a5bd641e7157239ed6665e358936b
Josh Adams <josh.adams@epicgames.com>
2023-01-24 12:09:44 -0500

    - Fixed D3D12 being disabled in windows #rb trivial #preflight skip
    [CL 23832564 by Josh Adams in ue5-main branch]

--- Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
+++ Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
@@ -10,8 +10,8 @@ public class DX12 : ModuleRules
 {
 	protected bool IsX64Target { get => Target.Architecture.bIsX64; }
 	protected virtual bool bUsesWindowsD3D12 { get => Target.Platform.IsInGroup(UnrealPlatformGroup.Windows); }
-	protected virtual bool bUsesWindowsD3D12Libs { get => Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) && !IsX64Target; }
-	protected virtual bool bUsesAgilitySDK { get => Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) && !IsX64Target; }
+	protected virtual bool bUsesWindowsD3D12Libs { get => Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) && IsX64Target; }
+	protected virtual bool bUsesAgilitySDK { get => Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) && IsX64Target; }
 
 	public DX12(ReadOnlyTargetRules Target) : base(Target)
 	{
