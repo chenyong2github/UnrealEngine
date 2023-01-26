851712de10d7f5a18e2db16f3cea0e4e87007a40
nebojsa dragosavac <nebojsa.dragosavac@3lateral.com>
2023-01-26 06:00:48 -0500

    Fix warning display that both NNE and NNI are used
    #jira UE-175089
    #preflight none
    #rb nico.ranieri
    
    [CL 23865791 by nebojsa dragosavac in ue5-main branch]

--- Engine/Plugins/Experimental/NNI/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
+++ Engine/Plugins/Experimental/NNI/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
@@ -11,11 +11,15 @@
 void FThirdPartyHelperAndDLLLoaderModule::StartupModule()
 {
 #ifdef WITH_DIRECTML
+	TSharedPtr<IPlugin> NNEPlugin = IPluginManager::Get().FindPlugin(TEXT("NNE"));
+	if (NNEPlugin.IsValid() && NNEPlugin->IsEnabled())
+	{
 		UE_LOG(
-			LogNeuralNetworkInferenceThirdPartyHelperAndDLLLoader, 
-			Warning, 
+			LogNeuralNetworkInferenceThirdPartyHelperAndDLLLoader,
+			Display,
 			TEXT("Please disable NNE plugin, both NeuralNetorkInference and NNE plugins can't be used at a same time"));
-		
+	}
+
 	const FString DirectMLRuntimeBinPath = FString(FPlatformProcess::BaseDir()) / TEXT(PREPROCESSOR_TO_STRING(DIRECTML_PATH));
 	const FString DirectMLDLLPath = DirectMLRuntimeBinPath / TEXT("DirectML.dll");
 
