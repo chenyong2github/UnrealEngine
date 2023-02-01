fddb6bf0c66b0e1d356374b3925ebea8f1a979fb
paul chipchase <paul.chipchase@epicgames.com>
2023-02-01 04:47:13 -0500

    Improve code documentation for the VA PrecachePayloads commandlet.
    #rb trivial
    #jira none
    #rnx
    #preflight 63da305f8c7003aa8c7fdc92
    
    [CL 23945502 by paul chipchase in ue5-main branch]

--- Engine/Source/Editor/VirtualizationEditor/Private/PrecachePayloadsCommandlet.h
+++ Engine/Source/Editor/VirtualizationEditor/Private/PrecachePayloadsCommandlet.h
@@ -7,6 +7,18 @@
 
 #include "PrecachePayloadsCommandlet.generated.h"
 
+/**
+ * Attempts to pull all virtualized content in the current project. Since the virtualization
+ * system will push any payloads pulled from persistent storage to cached storage this 
+ * has the side effect of making sure that all payloads are stored in cached storage. This
+ * can be used to reduce the cost of virtualized payload access for a user at the cost of
+ * disk space.
+ *
+ *
+ * Because the commandlet is the VirtualizationEditor module it needs to be invoked
+ * with the command line:
+ * -run=VirtualizationEditor.PrecachePayloads
+ */
 UCLASS()
 class UPrecachePayloadsCommandlet
 	: public UCommandlet
