06166dc93455a0b104348746afe24cc7f1448812
Charles deRousiers <Charles.deRousiers@epicgames.com>
2023-02-01 14:52:48 -0500

    * Increase bucket size to improve OIT triangle sorting precision. * Fix dispatch count for the writing pass, not issuing the correct count * Removing experimental label.
    #rb none
    #jira UE-162405
    #preflight 63dab4d1dd029837760ac9a7
    
    [CL 23955151 by Charles deRousiers in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/StaticMeshComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/StaticMeshComponent.h
@@ -280,7 +280,7 @@ public:
 #endif
 
 	/** Enable dynamic sort mesh's triangles to remove ordering issue when rendered with a translucent material */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadOnly, Category = Lighting, meta = (UIMin = "0", UIMax = "1", DisplayName = "Sort Triangles (Experimental)"))
+	UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadOnly, Category = Lighting, meta = (UIMin = "0", UIMax = "1", DisplayName = "Sort Triangles"))
 	uint8 bSortTriangles : 1;
 
 	/**
--- Engine/Source/Runtime/Renderer/Private/OIT/OIT.cpp
+++ Engine/Source/Runtime/Renderer/Private/OIT/OIT.cpp
@@ -22,7 +22,7 @@
 static TAutoConsoleVariable<int32> CVarOIT_SortedTriangles_Enable(
 	TEXT("r.OIT.SortedTriangles"), 
 	1, 
-	TEXT("Enable per-instance triangle sorting to avoid invalid triangle ordering (experimental)."),
+	TEXT("Enable per-instance triangle sorting to avoid invalid triangle ordering."),
 	ECVF_ReadOnly | ECVF_RenderThreadSafe);
 
 static TAutoConsoleVariable<int32> CVarOIT_SortedTriangles_Debug(
@@ -256,7 +256,7 @@ static void AddInternalOITComposePass(
 class FSortedIndexBuffer : public FIndexBuffer
 {
 public:
-	static const uint32 SliceCount = 32;
+	static const uint32 SliceCount = 256u;
 
 	FSortedIndexBuffer(uint32 InId, const FBufferRHIRef& InSourceIndexBuffer, uint32 InNumIndices, const TCHAR* InDebugName)
 	: SourceIndexBuffer(InSourceIndexBuffer)
@@ -421,18 +421,6 @@ void FOITSceneData::Deallocate(FIndexBuffer* InIndexBuffer)
 	}
 }
 
-static uint32 GetGroupSize()
-{
-	if (IsRHIDeviceNVIDIA())
-	{
-		return 32u;
-	}
-	else  // IsRHIDeviceAMD() and others
-	{
-		return 64u;
-	}
-}
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Sort triangle indices to order them front-to-back or back-to-front
 
@@ -441,9 +429,8 @@ class FOITSortTriangleIndex_ScanCS : public FGlobalShader
 	DECLARE_GLOBAL_SHADER(FOITSortTriangleIndex_ScanCS);
 	SHADER_USE_PARAMETER_STRUCT(FOITSortTriangleIndex_ScanCS, FGlobalShader);
 
-	class FGroupSize : SHADER_PERMUTATION_SPARSE_INT("PERMUTATION_GROUP_SIZE", 32, 64); 
 	class FDebug : SHADER_PERMUTATION_BOOL("PERMUTATION_DEBUG");
-	using FPermutationDomain = TShaderPermutationDomain<FGroupSize, FDebug>;
+	using FPermutationDomain = TShaderPermutationDomain<FDebug>;
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
 
@@ -457,6 +444,7 @@ class FOITSortTriangleIndex_ScanCS : public FGlobalShader
 
 		SHADER_PARAMETER(FMatrix44f, LocalToWorld)
 		SHADER_PARAMETER(FMatrix44f, WorldToView)
+		SHADER_PARAMETER(FMatrix44f, LocalToView)
 
 		SHADER_PARAMETER(uint32, SourcePrimitiveType)
 		SHADER_PARAMETER(uint32, NumPrimitives)
@@ -668,8 +656,7 @@ static void AddOITSortTriangleIndexPass(
 		const float ViewBoundMaxZ = ViewBounds.GetBox().Max.Z;
 
 		FOITSortTriangleIndex_ScanCS::FParameters* Parameters = GraphBuilder.AllocParameters<FOITSortTriangleIndex_ScanCS::FParameters>();
-		Parameters->LocalToWorld			= FMatrix44f(MeshBatch.Proxy->GetLocalToWorld());	// LWC_TODO: Precision loss?
-		Parameters->WorldToView				= FMatrix44f(View.ViewMatrices.GetViewMatrix());
+		Parameters->LocalToView				= FMatrix44f(MeshBatch.Proxy->GetLocalToWorld() * View.ViewMatrices.GetViewMatrix());
 		Parameters->SourcePrimitiveType		= Allocation.SourcePrimitiveType == PT_TriangleStrip ? 1u : 0u;
 		Parameters->NumPrimitives			= Allocation.NumPrimitives;
 		Parameters->NumIndices				= Allocation.NumIndices;
@@ -706,14 +693,12 @@ static void AddOITSortTriangleIndexPass(
 			Parameters->OutDebugData = GraphBuilder.CreateUAV(DebugData.Buffer, PF_R32_UINT);
 		}
 
-		const uint32 GroupSize = GetGroupSize();
-
 		FOITSortTriangleIndex_ScanCS::FPermutationDomain PermutationVector;
-		PermutationVector.Set<FOITSortTriangleIndex_ScanCS::FGroupSize>(GroupSize);
 		PermutationVector.Set<FOITSortTriangleIndex_ScanCS::FDebug>(bDebugEnable ? 1 : 0);
 		TShaderMapRef<FOITSortTriangleIndex_ScanCS> ComputeShader(View.ShaderMap, PermutationVector);
 		
-		const FIntVector DispatchCount = FIntVector(FMath::CeilToInt(float(Parameters->NumPrimitives) / float(GroupSize)), 1u, 1u);
+		const uint32 GroupSize = FSortedIndexBuffer::SliceCount;
+		const FIntVector DispatchCount = FIntVector(FMath::DivideAndRoundUp(Parameters->NumPrimitives, GroupSize), 1u, 1u);
 		check(DispatchCount.X < GRHIMaxDispatchThreadGroupsPerDimension.X);
 		ClearUnusedGraphResources(ComputeShader, Parameters);
 		GraphBuilder.AddPass(
@@ -759,7 +744,7 @@ static void AddOITSortTriangleIndexPass(
 		TShaderMapRef<FOITSortTriangleIndex_WriteOutCS> ComputeShader(View.ShaderMap);
 
 		const uint32 GroupSize = 256;
-		const FIntVector DispatchCount = FIntVector(GroupSize, 1u, 1u);
+		const FIntVector DispatchCount = FIntVector(FMath::DivideAndRoundUp(Parameters->NumPrimitives, GroupSize), 1u, 1u);
 		ClearUnusedGraphResources(ComputeShader, Parameters);
 		GraphBuilder.AddPass(
 			RDG_EVENT_NAME("OIT::SortTriangleIndices(Write)"),
