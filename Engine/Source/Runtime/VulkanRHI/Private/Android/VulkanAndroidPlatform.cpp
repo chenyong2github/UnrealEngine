bde1078b4fbc813e9277ac913720cfe2f09dd3ed
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2023-02-02 01:25:43 -0500

    Fixed: Issues with 90Hz framerate settings #rb jack.porter
    [CL 23964728 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/VulkanRHI/Private/Android/VulkanAndroidPlatform.cpp
+++ Engine/Source/Runtime/VulkanRHI/Private/Android/VulkanAndroidPlatform.cpp
@@ -735,15 +735,32 @@ bool FAndroidVulkanFramePacer::SupportsFramePaceInternal(int32 QueryFramePace, i
 	// check if we want to use CPU frame pacing at less than a multiple of supported refresh rate
 	if (FAndroidPlatformRHIFramePacer::CVarSupportNonVSyncMultipleFrameRates.GetValueOnAnyThread() == 1)
 	{
+		int32 ClosestRefreshRate = 0;
+		
 		for (int32 Rate : RefreshRates)
 		{
 			if (Rate > QueryFramePace)
 			{
-				UE_LOG(LogRHI, Log, TEXT("Supports %d using refresh rate %d with CPU frame pacing"), QueryFramePace, Rate);
-				OutRefreshRate = Rate;
-				OutSyncInterval = 0;
-				return true;
+				if (ClosestRefreshRate < QueryFramePace)
+				{
+					ClosestRefreshRate = Rate;
+				}
+
+				// prefer 30hz multiple
+				if ((Rate % 30) == 0)
+				{
+					ClosestRefreshRate = Rate;
+					break;
+				}
 			}
+		}
+
+		if (ClosestRefreshRate > QueryFramePace)
+		{
+			OutRefreshRate = ClosestRefreshRate;
+			OutSyncInterval = 0;
+			UE_LOG(LogRHI, Log, TEXT("Supports %d using refresh rate %d with CPU frame pacing"), QueryFramePace, OutRefreshRate);
+			return true;
 		}
 	}
 
