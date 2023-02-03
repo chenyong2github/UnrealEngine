615845994273139ee8022d53b4493dcacc76a433
karen jirak <karen.jirak@epicgames.com>
2023-02-02 19:02:09 -0500

    Fix for copyright notices
    #rb Lauren.Barnes
    #preflight skip
    
    [CL 23984713 by karen jirak in ue5-main branch]

--- Engine/Source/Runtime/Slate/Private/Framework/MultiBox/SUniformToolbarButtonBlock.cpp
+++ Engine/Source/Runtime/Slate/Private/Framework/MultiBox/SUniformToolbarButtonBlock.cpp
@@ -1,3 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "Framework/MultiBox/SUniformToolbarButtonBlock.h"
 
 FUniformToolbarButtonBlock::FUniformToolbarButtonBlock(FButtonArgs ButtonArgs) :
