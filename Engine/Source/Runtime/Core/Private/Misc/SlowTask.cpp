701f4ff2ed0d99c08089f2b842b06fbdd7f0e8bd
dave belanger <Dave.Belanger@epicgames.com>
2023-02-07 18:29:16 -0500

    Log SlowTask messages in out-of-order destruction ensure #rb Rex.Hill #preflight 63e2ae797467a6960f5ac1da
    [CL 24062610 by dave belanger in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Misc/SlowTask.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/SlowTask.cpp
@@ -114,16 +114,17 @@ void FSlowTask::Destroy()
 		}
 
 		FSlowTaskStack& Stack = Context.ScopeStack;
-		checkSlow(Stack.Num() != 0 && Stack.Last() == this);
-
-		FSlowTask* Task = Stack.Last();
-		if (ensureMsgf(Task == this, TEXT("Out-of-order scoped task construction/destruction")))
+		if (ensure(Stack.Num() != 0))
 		{
-			Stack.Pop(false);
-		}
-		else
-		{
-			Stack.RemoveSingleSwap(this, false);
+			FSlowTask* Task = Stack.Last();
+			if (ensureMsgf(Task == this, TEXT("Out-of-order slow task construction/destruction: destroying '%s' but '%s' is at the top of the stack"), *DefaultMessage.ToString(), *Task->DefaultMessage.ToString()))
+			{
+				Stack.Pop(false);
+			}
+			else
+			{
+				Stack.RemoveSingleSwap(this, false);
+			}
 		}
 
 		if (Stack.Num() != 0)
