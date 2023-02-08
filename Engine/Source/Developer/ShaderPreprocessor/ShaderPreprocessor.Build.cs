4d1c3ab0b89f398132a0e2486d27a3ec135fc02f
Joe Kirchoff <joe.kirchoff@epicgames.com>
2023-02-08 16:42:12 -0500

    Disable sndbs distribution for ShaderPreprocessor module
    #rnx
    #rb trivial
    #preflight 63e41634ea7ad6869835614b
    
    [CL 24083177 by Joe Kirchoff in ue5-main branch]

--- Engine/Source/Developer/ShaderPreprocessor/ShaderPreprocessor.Build.cs
+++ Engine/Source/Developer/ShaderPreprocessor/ShaderPreprocessor.Build.cs
@@ -16,5 +16,8 @@ public class ShaderPreprocessor : ModuleRules
 		AddEngineThirdPartyPrivateStaticDependencies(Target, "MCPP");
 
 		PrivateDefinitions.Add("STB_CONFIG=../StbConfig.h");
+
+		// TODO: Disable SN-DBS distribution until ../StbConfig.h include from STB_CONFIG can be resolved correctly
+		bBuildLocallyWithSNDBS = true;
 	}
 }
