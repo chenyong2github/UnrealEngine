aa710842c3eea685de8c390bd4d82bb07f7de2ce
brian smith <Brian.Smith@xa.epicgames.com>
2023-02-08 03:57:26 -0500

    Live Link VCAM v1.3.1 (build 5) : fixed sending of characters to UE.
    [CL 24069747 by brian smith in ue5-main branch]

--- Engine/Extras/VirtualProduction/LiveLinkVCAM/vcam/WebRTC/Extensions/KeyboardControls.swift
+++ Engine/Extras/VirtualProduction/LiveLinkVCAM/vcam/WebRTC/Extensions/KeyboardControls.swift
@@ -21,10 +21,10 @@ final class KeyboardControls: KeyboardDelegate {
     func submitString(_ entry: String) {
         var bytes: [UInt8] = []
         bytes.append(PixelStreamingToStreamerMessage.TextboxEntry.rawValue)
-        bytes.append(UInt8(entry.count))
+        bytes.append(contentsOf: UInt16(entry.count).toBytes())
         
         for char in entry {
-            bytes.append(contentsOf: char.utf16[char.utf16.index(char.utf16.startIndex, offsetBy: 0)].bigEndian.toBytes())
+            bytes.append(contentsOf: char.utf16[char.utf16.index(char.utf16.startIndex, offsetBy: 0)].toBytes())
         }
 
         self.webRTCClient.sendData(Data(bytes))
