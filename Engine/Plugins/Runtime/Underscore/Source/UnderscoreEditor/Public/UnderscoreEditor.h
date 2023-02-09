d2041ca3274a194207a40537d1e136b024ec6882
Dan Reynolds <Dan.Reynolds@epicgames.com>
2023-02-09 16:18:45 -0500

    Underscore primary plugin/module check-in.
    #jira UE-176384
    #preflight 63e41c023c44c83044e85b8e
    #rb max.hayes, ryan.mangin
    
    [CL 24106159 by Dan Reynolds in ue5-main branch]

---
+++ Engine/Plugins/Runtime/Underscore/Source/UnderscoreEditor/Public/UnderscoreEditor.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
+#include "AssetToolsModule.h"
+
+class FUnderscoreEditorModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	TSharedPtr<IAssetTypeActions> UnderscoreSectionAssetTypeActions;
+};
---
+++ Engine/Plugins/Runtime/Underscore/Source/UnderscoreEditor/UnderscoreEditor.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnderscoreEditor : ModuleRules
+{
+	public UnderscoreEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivatePCHHeaderFile = "Public/UnderscoreEditor.h";
+
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"EditorFramework",
+				"UnrealEd",
+				"AssetTools",
+				"ContentBrowser",
+				"Underscore",
+				"SlateCore",
+
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"Settings",
+				"UnrealEd",
+				"InputCore",
+				"EditorWidgets",
+			}
+			);
+	}
+}
