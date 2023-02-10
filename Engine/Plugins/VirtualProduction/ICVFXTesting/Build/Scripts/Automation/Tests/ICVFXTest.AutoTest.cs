6ef16ef03e274dd82529a655334f3aa020750a51
jeremie roy <jeremie.roy@epicgames.com>
2023-02-10 11:40:01 -0500

    Move SaloonTest script files to ICVFX Testing plugin
    #rb geoffrey.douglas
    #jira UE-175138
    #preflight none
    
    [CL 24121067 by jeremie roy in ue5-main branch]

---
+++ Engine/Plugins/VirtualProduction/ICVFXTesting/Build/Scripts/Automation/Tests/ICVFXTest.AutoTest.cs
@@ -0,0 +1,182 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using EpicGame;
+using Gauntlet;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnrealBuildTool;
+
+using Log = EpicGames.Core.Log;
+
+namespace ICVFXTest
+{
+	/// <summary>
+	/// CI testing
+	/// </summary>
+	public class AutoTest : ICVFXTestNode
+	{
+		public AutoTest(Gauntlet.UnrealTestContext InContext)
+			: base(InContext)
+		{
+		}
+		public virtual int GetMaxGPUCount()
+		{
+			return base.GetConfiguration().MaxGPUCount;
+		}
+
+		public virtual string GetTestSuffix()
+		{
+			return "Base";
+		}
+
+		public virtual bool IsLumenEnabled()
+		{
+			return base.GetConfiguration().Lumen;
+		}
+		public virtual bool UseVulkan()
+		{
+			return false;
+		}
+
+		public virtual bool UseNanite()
+		{
+			return base.GetConfiguration().Nanite;
+		}
+
+		public override ICVFXTestConfig GetConfiguration()
+		{
+			ICVFXTestConfig Config = base.GetConfiguration();
+			Config.MaxDuration = Context.TestParams.ParseValue("MaxDuration", 60 * 60);  // 1 hour max
+
+			UnrealTestRole ClientRole = Config.RequireRole(UnrealTargetRole.Client);
+            ClientRole.Controllers.Add("AutoTest");
+
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "ICVFXTest.MaxRunCount " + Config.MaxRunCount);
+
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "ICVFXTest.SoakTime " + Config.SoakTime);
+
+			if (Config.EnableTrace && Config.TraceRootFolder.Length != 0)
+			{
+				ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "ICVFXTest.TraceFileName " + $"{Config.TraceRootFolder}/{GetTestSuffix()}.utrace");
+			}
+
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.nanite " + (UseNanite() ? 1 : 0));
+
+			// Todo: verify this is actually used in an ndisplay context and if we need to set an override option as well.
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.ScreenPercentage " + Config.ViewportScreenPercentage);
+
+			bool bLumenEnabled = IsLumenEnabled();
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.RayTracing " + (bLumenEnabled ? 1 : 0));
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.DynamicGlobalIlluminationMethod " + (bLumenEnabled ? 1 : 0));
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.ReflectionMethod " + (bLumenEnabled? 2 : 0));
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.Lumen.HardwareRayTracing " + (bLumenEnabled ? 1 : 0));
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.Lumen.HardwareRayTracing.LightingMode " + (bLumenEnabled ? 2 : 0));
+
+			// Temporary until we figure out why we hang
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "FX.AllowGPUParticles 0");
+
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "r.Shadow.Virtual.Enable " + (Config.DisableVirtualShadowMaps ? 0 : 1));
+			
+			ClientRole.CommandLineParams.Add("-MaxGPUCount=" + GetMaxGPUCount());
+
+			if (Config.SkipTestSequence)
+			{
+				ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "ICVFXTest.SkipTestSequence true");
+			}
+
+			if (Config.StatCommands)
+			{
+				ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "stat fps, stat unitgraph");
+			}
+			
+			if (Config.FPSChart)
+			{
+				ClientRole.CommandLineParams.Add("ICVFXTest.FPSChart");
+			}
+			
+			if (Config.MemReport)
+			{
+				ClientRole.CommandLineParams.Add("ICVFXTest.MemReport");
+			}
+
+			if (Config.VideoCapture)
+			{
+				ClientRole.CommandLineParams.Add("ICVFXTest.VideoCapture");
+			}
+
+			if (Config.MapOverride?.Length != 0)
+			{
+				ClientRole.MapOverride = Config.MapOverride;
+				Log.TraceLog($"Map Override: { Config.MapOverride }");
+			}
+
+			if (Config.D3DDebug)
+			{
+				ClientRole.CommandLineParams.Add("-d3ddebug");
+				Gauntlet.Log.Info("Running with D3DDebug");
+			}
+
+			if (Config.GPUCrashDebugging)
+			{
+				ClientRole.CommandLineParams.Add("-gpucrashdebugging");
+				Gauntlet.Log.Info("Running with GPUCrashDebugging");
+			}
+
+			if (Config.DisplayConfigPath?.Length != 0)
+			{
+				ClientRole.CommandLineParams.AddOrAppendParamValue("dc_cfg", Config.DisplayConfigPath);
+			}
+
+			if (Config.DisplayClusterNodeName?.Length != 0)
+			{
+				ClientRole.CommandLineParams.AddOrAppendParamValue("dc_node", Config.DisplayClusterNodeName);
+			}
+
+			if (UseVulkan())
+			{
+				ClientRole.CommandLineParams.Add("-vulkan");
+			}
+			else
+			{
+				ClientRole.CommandLineParams.Add("-dx12");
+			}
+
+			if (Config.RDGImmediate)
+			{
+				ClientRole.CommandLineParams.Add("-rdgimmediate");
+			}
+			
+			if (Config.NoRHIThread)
+			{
+				ClientRole.CommandLineParams.Add("-norhithread");
+			}
+
+			if (Config.EnableTrace)
+			{
+				// Start as late as possible to reduce trace file size.s
+				ClientRole.CommandLineParams.Add("-traceautostart=0");
+				ClientRole.CommandLineParams.Add("-trace=cpu,counters,gpu");
+			}
+
+
+			ClientRole.CommandLineParams.Add("-messaging -dc_cluster -nosplash -fixedseed -NoVerifyGC -noxrstereo -xrtrackingonly -RemoteControlIsHeadless -dc_dev_mono -ini:Engine:[/Script/Engine.Engine]:GameEngine=/Script/DisplayCluster.DisplayClusterGameEngine,[/Script/Engine.Engine]:GameViewportClientClassName=/Script/DisplayCluster.DisplayClusterViewportClient,[/Script/Engine.UserInterfaceSettings]:bAllowHighDPIInGameMode=True -ini:Game:[/Script/EngineSettings.GeneralProjectSettings]:bUseBorderlessWindow=True -unattended -NoScreenMessages -handleensurepercent=0 -UDPMESSAGING_TRANSPORT_MULTICAST=\"230.0.0.1:6666\" -UDPMESSAGING_TRANSPORT_UNICAST=\"127.0.0.1:0\" -UDPMESSAGING_TRANSPORT_STATIC=\"127.0.0.1:9030\" -ExecCmds=\"DisableAllScreenMessages\" -windowed -DPCVars=\"Slate.bAllowNotifications=0,p.Chaos.Solver.Deterministic=1\"");
+			// Todo: Is there a way to not specify this resolution? At the moment this matches what's in the display config file.
+			ClientRole.CommandLineParams.Add("-forceres WinX=0 WinY=0 -ResX=1920 -ResY=1080 -DisablePlugins=\"RenderDocPlugin\"");
+
+			Gauntlet.Log.Info(ClientRole.CommandLineParams.ToString());
+
+			return Config;
+		}
+
+		protected override void InitHandledErrors()
+        {
+			base.InitHandledErrors();
+			HandledErrors.Add(new HandledError("AutoTest", "AutoTest failure:", "LogICVFXTest", true));
+		}
+
+		public override ITestReport CreateReport(TestResult Result, UnrealTestContext Context, UnrealBuildSource Build, IEnumerable<UnrealRoleResult> Artifacts, string ArtifactPath)
+		{
+			return base.CreateReport(Result, Context, Build, Artifacts, ArtifactPath);
+		}
+	}
+}
---
+++ Engine/Plugins/VirtualProduction/ICVFXTesting/Build/Scripts/Automation/Tests/ICVFXTest.PerformanceReport.cs
@@ -0,0 +1,328 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using AutomationTool;
+using EpicGames.Core;
+using Gauntlet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnrealBuildBase;
+using UnrealBuildTool;
+using Log = EpicGames.Core.Log;
+
+namespace ICVFXTest
+{
+	/// <summary>
+	/// CI testing
+	/// </summary>
+	public class PerformanceReport : AutoTest
+	{
+		public PerformanceReport(UnrealTestContext InContext)
+			: base(InContext)
+		{
+		}
+
+		public override ICVFXTestConfig GetConfiguration()
+		{
+			ICVFXTestConfig Config = base.GetConfiguration();
+			UnrealTestRole ClientRole = Config.RequireRole(UnrealTargetRole.Client);
+			ClientRole.CommandLineParams.Add("csvGpuStats");
+			ClientRole.CommandLineParams.Add("csvMetadata", $"\"testname=${Config.TestName}\"");
+			ClientRole.CommandLineParams.AddOrAppendParamValue("execcmds", "t.FPSChart.DoCSVProfile 1");
+			ClientRole.CommandLineParams.Add("ICVFXTest.FPSChart");
+
+			return Config;
+		}
+
+		protected override void InitHandledErrors()
+        {
+			base.InitHandledErrors();
+		}
+
+		/// <summary>
+		/// Produces a detailed csv report using PerfReportTool.
+		/// Also, stores perf data in the perf cache, and generates a historic report using the data the cache contains.
+		/// </summary>
+		private void GeneratePerfReport(UnrealTargetPlatform Platform, string ArtifactPath, string TempDir)
+		{
+			var ReportCacheDir = GetConfiguration().PerfCacheFolder;
+
+			if (GetTestSuffix().Length != 0)
+			{
+				ReportCacheDir += "_" + GetTestSuffix(); // We don't want to mix test results
+			}
+
+			var ToolPath = FileReference.Combine(Unreal.EngineDirectory, "Binaries", "DotNET", "CsvTools", "PerfreportTool.exe");
+			if (!FileReference.Exists(ToolPath))
+			{
+				Log.TraceError($"Failed to find perf report utility at this path: \"{ToolPath}\".");
+				return;
+			}
+			var ReportConfigDir = Path.Combine(Unreal.RootDirectory.FullName, "Engine", "Plugins", "VirtualProduction", "ICVFXTesting", "Build", "Scripts", "PerfReport");
+			var ReportPath = Path.Combine(ArtifactPath, "Reports", "Performance");
+
+		var CsvsPaths = new[]
+			{
+				Path.Combine(ArtifactPath, "EditorGame", "Profiling", "FPSChartStats"),
+				Path.Combine(TempDir, "DeviceCache", Platform.ToString(), TestInstance.ClientApps[0].Device.ToString(), "UserDir")
+			};
+
+
+		var DiscoveredCsvs = new List<string>();
+			foreach (var CsvsPath in CsvsPaths)
+			{
+				if (Directory.Exists(CsvsPath))
+				{
+					DiscoveredCsvs.AddRange(
+						from CsvFile in Directory.GetFiles(CsvsPath, "*.csv", SearchOption.AllDirectories)
+						where CsvFile.Contains("csvprofile", StringComparison.InvariantCultureIgnoreCase)
+						select CsvFile);
+				}
+			}
+
+			if (DiscoveredCsvs.Count == 0)
+			{
+				Log.TraceError($"Test completed successfully but no csv profiling results were found. Searched paths were:\r\n  {string.Join("\r\n  ", CsvsPaths.Select(s => $"\"{s}\""))}");
+				return;
+			}
+
+			// Find the newest csv file and get its directory
+			// (PerfReportTool will only output cached data in -csvdir mode)
+			var NewestFile =
+				(from CsvFile in DiscoveredCsvs
+				 let Timestamp = File.GetCreationTimeUtc(CsvFile)
+				 orderby Timestamp descending
+				 select CsvFile).First();
+			var NewestDir = Path.GetDirectoryName(NewestFile);
+
+			Log.TraceInformation($"Using perf report cache directory \"{ReportCacheDir}\".");
+			Log.TraceInformation($"Using perf report output directory \"{ReportPath}\".");
+			Log.TraceInformation($"Using csv results directory \"{NewestDir}\". Generating historic perf report data...");
+
+			// Make sure the cache and output directories exist
+			if (!Directory.Exists(ReportCacheDir))
+			{
+				try { Directory.CreateDirectory(ReportCacheDir); }
+				catch (Exception Ex)
+				{
+					Log.TraceError($"Failed to create perf report cache directory \"{ReportCacheDir}\". {Ex}");
+					return;
+				}
+			}
+			if (!Directory.Exists(ReportPath))
+			{
+				try { Directory.CreateDirectory(ReportPath); }
+				catch (Exception Ex)
+				{
+					Log.TraceError($"Failed to create perf report output directory \"{ReportPath}\". {Ex}");
+					return;
+				}
+			}
+
+			// Win64 is actually called "Windows" in csv profiles
+			var PlatformNameFilter = Platform == UnrealTargetPlatform.Win64 ? "Windows" : $"{Platform}";
+
+			// Produce the detailed report, and update the perf cache
+			CommandUtils.RunAndLog(ToolPath.FullName, $"-csvdir \"{NewestDir}\" -o \"{ReportPath}\" -reportxmlbasedir \"{ReportConfigDir}\" -summaryTableCache \"{ReportCacheDir}\" -searchpattern csvprofile* -metadatafilter platform=\"{PlatformNameFilter}\"", out int ErrorCode);
+			if (ErrorCode != 0)
+			{
+				Log.TraceError($"PerfReportTool returned error code \"{ErrorCode}\" while generating detailed report.");
+			}
+
+			// Now generate the all-time historic summary report
+			HistoricReport("HistoricReport_AllTime", new[]
+			{
+				$"platform={PlatformNameFilter}"
+			});
+
+			// 14 days historic report
+			HistoricReport($"HistoricReport_14Days", new[]
+			{
+				$"platform={PlatformNameFilter}",
+				$"starttimestamp>={DateTimeOffset.Now.ToUnixTimeSeconds() - (14 * 60L * 60L * 24L)}"
+			});
+
+			// 7 days historic report
+			HistoricReport($"HistoricReport_7Days", new[]
+			{
+				$"platform={PlatformNameFilter}",
+				$"starttimestamp>={DateTimeOffset.Now.ToUnixTimeSeconds() - (7 * 60L * 60L * 24L)}"
+			});
+
+			void HistoricReport_Alt(string Name, IEnumerable<string> Filter)
+			{
+				var Args = new[]
+				{
+					$"-summarytablecachein \"{ReportCacheDir}\"",
+					$"-summaryTableFilename \"{Name  + GetTestSuffix()}.html\"",
+					$"-reportxmlbasedir \"{ReportConfigDir}\"",
+					$"-o \"{base.GetConfiguration().SummaryReportPath}\"",
+					$"-metadatafilter \"{string.Join(" and ", Filter)}\"",
+					"-summaryTable autoPerfReportStandard",
+					"-condensedSummaryTable autoPerfReportStandard",
+					"-emailtable",
+					"-recurse"
+				};
+
+				var ArgStr = string.Join(" ", Args);
+
+				CommandUtils.RunAndLog(ToolPath.FullName, ArgStr, out ErrorCode);
+				if (ErrorCode != 0)
+				{
+					Log.TraceError($"PerfReportTool returned error code \"{ErrorCode}\" while generating historic report.");
+				}
+			}
+
+			// 14 days historic report
+			HistoricReport_Alt($"HistoricReport_14Days_Summary", new[]
+			{
+				$"platform={PlatformNameFilter}",
+				$"starttimestamp>={DateTimeOffset.Now.ToUnixTimeSeconds() - (14 * 60L * 60L * 24L)}"
+			});
+
+			void HistoricReport(string Name, IEnumerable<string> Filter)
+			{
+				var Args = new[]
+				{
+					$"-summarytablecachein \"{ReportCacheDir}\"",
+					$"-summaryTableFilename \"{Name  + GetTestSuffix()}.html\"",
+					$"-reportxmlbasedir \"{ReportConfigDir}\"",
+					$"-o \"{ReportPath}\"",
+					$"-metadatafilter \"{string.Join(" and ", Filter)}\"",
+					"-summaryTable autoPerfReportStandard",
+					"-condensedSummaryTable autoPerfReportStandard",
+					"-emailtable",
+					"-recurse"
+				};
+
+				var ArgStr = string.Join(" ", Args);
+
+				CommandUtils.RunAndLog(ToolPath.FullName, ArgStr, out ErrorCode);
+				if (ErrorCode != 0)
+				{
+					Log.TraceError($"PerfReportTool returned error code \"{ErrorCode}\" while generating historic report.");
+				}
+			}
+		}
+
+		public override ITestReport CreateReport(TestResult Result, UnrealTestContext Context, UnrealBuildSource Build, IEnumerable<UnrealRoleResult> Artifacts, string ArtifactPath)
+		{
+			if (Result == TestResult.Passed)
+			{
+				Log.TraceInformation($"Generating performance reports using PerfReportTool.");
+				GeneratePerfReport(Context.GetRoleContext(UnrealTargetRole.Client).Platform, ArtifactPath, Context.Options.TempDir);
+			}
+			else
+			{
+				Log.TraceWarning($"Skipping performance report generation because the perf report test failed.");
+			}
+
+			return base.CreateReport(Result, Context, Build, Artifacts, ArtifactPath);
+		}
+	}
+  
+	//
+	// Horrible hack to repeat the perf tests 3 times...
+	// There is no way to pass "-repeat=N" to Gauntlet via the standard engine build scripts, nor is
+	// it possible to override the number of iterations per-test via the GetConfiguration() function.
+	//
+	// In theory we can pass the "ICVFXTest.PerformanceReport" test name to Gauntlet 3 times via Horde scripts,
+	// but the standard build scripts will attempt to define 3 nodes all with the same name, which won't work.
+	//
+	// These three classes allow us to run 3 copies of the PerformanceReport test, but ensures they all have 
+	// different names to fit into the build script / Gauntlet structure.
+	//
+
+	public class PerformanceReport_1 : PerformanceReport
+	{
+		public PerformanceReport_1(UnrealTestContext InContext) : base(InContext) { }
+	}
+
+	public class PerformanceReport_2 : PerformanceReport
+	{
+		public PerformanceReport_2(UnrealTestContext InContext) : base(InContext) { }
+	}
+
+	public class PerformanceReport_3 : PerformanceReport
+	{
+		public PerformanceReport_3(UnrealTestContext InContext) : base(InContext) { }
+	}
+
+	public class PerformanceReport_MGPU : PerformanceReport
+	{
+		public PerformanceReport_MGPU(UnrealTestContext InContext) : base(InContext)
+		{
+	
+		}
+		public override int GetMaxGPUCount() 
+		{
+			return 2;
+		}
+
+		public override string GetTestSuffix()
+		{
+			return "MGPU";
+		}
+	}
+	public class PerformanceReport_NaniteLumen : PerformanceReport
+	{
+		public PerformanceReport_NaniteLumen(UnrealTestContext InContext) : base(InContext)
+		{
+
+		}
+		public override bool IsLumenEnabled()
+		{
+			return true;
+		}
+		public override bool UseNanite()
+		{
+			return true;
+		}
+
+		public override string GetTestSuffix()
+		{
+			return "NaniteLumen";
+		}
+	}
+
+	public class PerformanceReport_Vulkan : PerformanceReport
+	{
+		public PerformanceReport_Vulkan(UnrealTestContext InContext) : base(InContext)
+		{
+
+		}
+
+		public override string GetTestSuffix()
+		{
+			return "Vulkan";
+		}
+
+		public override bool UseVulkan()
+		{
+			return true;
+		}
+	}
+
+	public class PerformanceReport_Nanite : PerformanceReport
+	{
+		public PerformanceReport_Nanite(UnrealTestContext InContext) : base(InContext)
+		{
+
+		}
+
+		public override string GetTestSuffix()
+		{
+			return "Nanite";
+		}
+
+		public override bool UseVulkan()
+		{
+			return true;
+		}
+		public override bool UseNanite()
+		{
+			return true;
+		}
+	}
+}
