6a6c3a00de71feff6f7fceb58ee436708ff7c8cc
jeremie roy <jeremie.roy@epicgames.com>
2023-02-09 19:51:24 -0500

    Port Media Capture fixes from UE5/Main to 5.2
    Fix media capture not working outside of aja
    Fix Number of Texture Buffers >= 2 crash
    Enable AnyThread capture for SharedMemoryMediaCapture
    
    #jira UE-175103 UE-174942 UE-175062
    #rb simon.therriault
    #preflight 63e5335a83340ac6cbfa9874
    
    [CL 24111057 by jeremie roy in ue5-main branch]

--- Engine/Plugins/Media/AjaMedia/Source/ThirdParty/AjaLib/ntv2sdkwin/BuildForWindows.bat
+++ Engine/Plugins/Media/AjaMedia/Source/ThirdParty/AjaLib/ntv2sdkwin/BuildForWindows.bat
@@ -2,7 +2,7 @@ rem Copyright Epic Games, Inc. All Rights Reserved.
 
 rem Setup part
 setlocal
-set AJANTV2_VERSION=16.1
+set AJANTV2_VERSION=16.2-bugfix5
 set AJANTV2_LIB_NAME=AjaNTV2-%AJANTV2_VERSION%
 set AJANTV2_LIB_FOLDER=ntv2
 set AJANTV2_FOLDER=ntv2-%AJANTV2_VERSION%
@@ -12,13 +12,13 @@ set AJANTV2_BUILD_FOLDER=cmake-build
 
 rem Download library source if not present
 if not exist %AJANTV2_LIB_NAME%.zip (
-    powershell -Command "Invoke-WebRequest https://github.com/aja-video/ntv2/archive/refs/tags/16.1.zip -OutFile %AJANTV2_LIB_NAME%.zip"
+	powershell -Command "Invoke-WebRequest https://github.com/aja-video/ntv2/archive/refs/tags/v16.2-bugfix5.zip -OutFile %AJANTV2_LIB_NAME%.zip"
 )
 rem Remove previously extracted build library folder
 if exist .\%AJANTV2_LIB_FOLDER% (
    rd /S /Q .\%AJANTV2_LIB_FOLDER%
 )
-    
+
 echo Extracting %AJANTV2_LIB_NAME%.zip...
 powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%AJANTV2_LIB_NAME%.zip', '%AJANTV2_LIB_FOLDER%')"
 cd /d .\%AJANTV2_LIB_FOLDER%\%AJANTV2_FOLDER%
@@ -30,13 +30,13 @@ echo Configuring x64 build environment...
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat" 
 
 echo Configuring x64 debug cmake config...
-cmake DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" -A x64 ..
+cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" -A x64 ..
  
 echo Building x64 Debug build...
 cmake --build . --config Debug
 
 echo Configuring x64 Release cmake config...
-cmake DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -A x64 ..
+cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -A x64 ..
 
 echo Building x64 Release build...
 cmake --build . --config Release
