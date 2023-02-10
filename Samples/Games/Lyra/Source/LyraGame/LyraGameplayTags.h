1bc78ed4ee91e906d13ac4deb69ea5e5d84b14fc
jodon karlik <jodon.karlik@epicgames.com>
2023-02-09 19:37:47 -0500

    Exporting the Lyra Gameplay Tags so developers can use them outside of Lyra.
    #jira UE-176721
    #preflight 63e565e8a84392ab1167223d
    
    [CL 24110684 by jodon karlik in ue5-main branch]

--- Samples/Games/Lyra/Source/LyraGame/LyraGameplayTags.h
+++ Samples/Games/Lyra/Source/LyraGame/LyraGameplayTags.h
@@ -6,55 +6,55 @@
 
 namespace LyraGameplayTags
 {
-	FGameplayTag FindTagByString(const FString& TagString, bool bMatchPartialString = false);
+	LYRAGAME_API	FGameplayTag FindTagByString(const FString& TagString, bool bMatchPartialString = false);
 
 	// Declare all of the custom native tags that Lyra will use
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_IsDead);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Cooldown);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Cost);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_TagsBlocked);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_TagsMissing);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Networking);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_ActivationGroup);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_IsDead);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Cooldown);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Cost);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_TagsBlocked);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_TagsMissing);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_Networking);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_ActivateFail_ActivationGroup);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Behavior_SurvivesDeath);
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look_Mouse);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look_Stick);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Crouch);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_AutoRun);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Behavior_SurvivesDeath);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_Spawned);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_DataAvailable);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_DataInitialized);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_GameplayReady);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look_Mouse);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look_Stick);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Crouch);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_AutoRun);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Death);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Reset);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_RequestReset);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_Spawned);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_DataAvailable);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_DataInitialized);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InitState_GameplayReady);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Damage);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Heal);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Death);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Reset);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_RequestReset);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cheat_GodMode);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cheat_UnlimitedHealth);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Damage);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Heal);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Crouching);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_AutoRunning);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death_Dying);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death_Dead);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cheat_GodMode);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cheat_UnlimitedHealth);
+
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Crouching);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_AutoRunning);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death_Dying);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Status_Death_Dead);
 
 	// These are mappings from MovementMode enums to GameplayTags associated with those enums (below)
-	extern const TMap<uint8, FGameplayTag> MovementModeTagMap;
-	extern const TMap<uint8, FGameplayTag> CustomMovementModeTagMap;
+	LYRAGAME_API	extern const TMap<uint8, FGameplayTag> MovementModeTagMap;
+	LYRAGAME_API	extern const TMap<uint8, FGameplayTag> CustomMovementModeTagMap;
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Walking);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_NavWalking);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Falling);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Swimming);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Flying);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Walking);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_NavWalking);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Falling);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Swimming);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Flying);
 
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Custom);
+	LYRAGAME_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Movement_Mode_Custom);
 };
