98db862bee6d358f81a2236ee2aeee7d790e31fa
Ben Marsh <Ben.Marsh@epicgames.com>
2023-02-12 11:21:16 -0500

    Horde: Fix DI for JobHandler objects.
    #preflight none
    
    [CL 24157901 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Agent/Program.cs
+++ Engine/Source/Programs/Horde/Horde.Agent/Program.cs
@@ -185,10 +185,12 @@ namespace Horde.Agent
 			services.AddSingleton<IJobExecutorFactory, LocalExecutorFactory>();
 			services.AddSingleton<IJobExecutorFactory, TestExecutorFactory>();
 
+			services.AddSingleton<JobHandler>();
+
 			services.AddSingleton<LeaseHandler, ComputeHandler>();
 			services.AddSingleton<LeaseHandler, ComputeHandlerV2>();
 			services.AddSingleton<LeaseHandler, ConformHandler>();
-			services.AddSingleton<LeaseHandler, JobHandler>();
+			services.AddSingleton<LeaseHandler, JobHandler>(x => x.GetRequiredService<JobHandler>());
 			services.AddSingleton<LeaseHandler, RestartHandler>();
 			services.AddSingleton<LeaseHandler, ShutdownHandler>();
 			services.AddSingleton<LeaseHandler, UpgradeHandler>();
