23e24f444edb5854d03fdca81e4616c1c3bd8eae
kriss gossart <Kriss.Gossart@epicgames.com>
2023-02-15 07:26:03 -0500

    Chaos Cloth - Fixed crashes when creating a new empty Cloth Asset.
    #rb Tyson.Brochu
    #preflight 63ecbfabe92f139c51f9db47
    #jira none
    #rnx
    
    [CL 24228251 by kriss gossart in ue5-main branch]

--- Engine/Plugins/Experimental/ChaosClothAssetEditor/Source/ChaosClothAssetTools/Private/ChaosClothAsset/ClothAssetFactory.cpp
+++ Engine/Plugins/Experimental/ChaosClothAssetEditor/Source/ChaosClothAssetTools/Private/ChaosClothAsset/ClothAssetFactory.cpp
@@ -2,6 +2,7 @@
 
 #include "ClothAssetFactory.h"
 #include "ChaosClothAsset/ClothAsset.h"
+#include "ChaosClothAsset/ClothAdapter.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(ClothAssetFactory)
 
@@ -15,8 +16,30 @@ UChaosClothAssetFactory::UChaosClothAssetFactory(const FObjectInitializer& Objec
 
 UObject* UChaosClothAssetFactory::FactoryCreateNew(UClass* Class, UObject* Parent, FName Name, EObjectFlags Flags, UObject* /*Context*/, FFeedbackContext* /*Warn*/)
 {
-	UChaosClothAsset* const NewClothAsset = NewObject<UChaosClothAsset>(Parent, Class, Name, Flags | RF_Transactional | RF_Public | RF_Standalone);
-	NewClothAsset->MarkPackageDirty();
-	return NewClothAsset;
+	using namespace UE::Chaos::ClothAsset;
+
+	UChaosClothAsset* const ClothAsset = NewObject<UChaosClothAsset>(Parent, Class, Name, Flags | RF_Transactional | RF_Public | RF_Standalone);
+	ClothAsset->MarkPackageDirty();
+
+	// Add an empty default LOD, to avoid LOD mismatch with render data
+	TSharedPtr<FClothCollection> ClothCollection = ClothAsset->GetClothCollection();
+
+	FClothAdapter ClothAdapter(ClothCollection);
+	ClothAdapter.AddLod();
+
+	// Set reference skeleton in both the collection and the asset
+	ClothCollection->AddElements(1, FClothCollection::SkeletonsGroup);
+	ClothCollection->SkeletonAssetPathName[0] = TEXT("/Engine/EditorMeshes/SkeletalMesh/DefaultSkeletalMesh_Skeleton.DefaultSkeletalMesh_Skeleton");
+
+	if (const USkeleton* const Skeleton = LoadObject<USkeleton>(nullptr, *ClothCollection->SkeletonAssetPathName[0], nullptr, LOAD_None, nullptr))
+	{
+		constexpr bool bRebuildClothSimulationModel = false;  // Avoid rebuilding the asset twice
+		ClothAsset->SetReferenceSkeleton(Skeleton->GetReferenceSkeleton(), bRebuildClothSimulationModel);
+	}
+
+	// Build static data
+	ClothAsset->Build();
+
+	return ClothAsset;
 }
 
