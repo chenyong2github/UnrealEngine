82d52d2117f743afdf405b3ee9815294ca151c99
bryan sefcik <bryan.sefcik@epicgames.com>
2023-02-16 04:16:30 -0500

    Added missing file from GC change.
    #preflight
    
    [CL 24253389 by bryan sefcik in ue5-main branch]

--- Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Public/GeometryCollection/GeometryCollectionComponent.h
+++ Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Public/GeometryCollection/GeometryCollectionComponent.h
@@ -1063,7 +1063,7 @@ protected:
 
 	TObjectPtr<AGeometryCollectionISMPoolActor> AssignedISMPool = nullptr;
 	int32 ISMPoolMeshGroupIndex = INDEX_NONE;
-	int32 ISMPoolRootProxyMeshId = INDEX_NONE;
+	TArray<int32> ISMPoolRootProxyMeshIds;
 
 	/** Populate the static geometry structures for the render thread. */
 	void InitConstantData(TUniquePtr<FGeometryCollectionConstantData>& ConstantData) const;
