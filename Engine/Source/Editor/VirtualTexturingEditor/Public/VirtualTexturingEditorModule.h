b1253628a0c9b2c0efc9f2a023ef229edb31ad5e
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2023-02-21 06:00:42 -0500

    Fixed: interface for backward compat from CL# 24335980
    [CL 24336795 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Editor/VirtualTexturingEditor/Public/VirtualTexturingEditorModule.h
+++ Engine/Source/Editor/VirtualTexturingEditor/Public/VirtualTexturingEditorModule.h
@@ -14,10 +14,12 @@ class IVirtualTexturingEditorModule : public IModuleInterface
 {
 public:
 	/** Returns true if the component describes a runtime virtual texture that has streaming low mips. */
+	virtual bool HasStreamedMips(URuntimeVirtualTextureComponent* InComponent) const = 0;
 	virtual bool HasStreamedMips(EShadingPath ShadingPath, class URuntimeVirtualTextureComponent* InComponent) const = 0;
 	/** Build the contents of the streaming low mips. */
+	virtual bool BuildStreamedMips(URuntimeVirtualTextureComponent* InComponent) const = 0;
 	virtual bool BuildStreamedMips(EShadingPath ShadingPath, class URuntimeVirtualTextureComponent* InComponent) const = 0;
-	
+		
 	virtual void ConvertVirtualTextures(const TArray<UTexture2D *>& Textures, bool bConvertBackToNonVirtual, const TArray<UMaterial *>* RelatedMaterials /* = nullptr */) const = 0;
 	virtual void ConvertVirtualTexturesWithDialog(const TArray<UTexture2D*>& Textures, bool bConvertBackToNonVirtual) const = 0;
 };
