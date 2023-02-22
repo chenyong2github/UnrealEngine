dd2e32231b314172296b1e309da25f3592db2ac8
vincent gauthier <Vincent.Gauthier@epicgames.com>
2023-02-22 18:02:19 -0500

    Fix to Placement Mode localization of Display Name to use the asset class name.
    #jira UE-177120
    
    [CL 24372023 by vincent gauthier in ue5-main branch]

--- Engine/Source/Editor/PlacementMode/Public/IPlacementModeModule.h
+++ Engine/Source/Editor/PlacementMode/Public/IPlacementModeModule.h
@@ -136,11 +136,8 @@ struct FPlaceableItem
 	void AutoSetNativeAndDisplayName()
 	{
 		UClass* Class = AssetData.GetClass() == UClass::StaticClass() ? Cast<UClass>(AssetData.GetAsset()) : nullptr;
-		const bool bIsVolume = Class && Class->IsChildOf<AVolume>();
-		const bool bIsShape = Class ? false : AssetData.IsInstanceOf(UStaticMesh::StaticClass());
 
-		// Use the class unless its a volume or shape.  Those need custom names as the factory that spawns them does not properly represent what is being spawned.
-		if (Class && !bIsVolume && !bIsShape)
+		if (Class)
 		{
 			Class->GetName(NativeName);
 			DisplayName = Class->GetDisplayNameText();
