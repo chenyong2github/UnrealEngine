c9f5d44f85bd9daf0d357e3816a7d9533279a55e
christopher waters <christopher.waters@epicgames.com>
2023-02-22 18:02:09 -0500

    Fixing bad module reference.
    [CL 24372010 by christopher waters in ue5-main branch]

--- Engine/Source/Developer/CookedEditor/CookedEditor.Build.cs
+++ Engine/Source/Developer/CookedEditor/CookedEditor.Build.cs
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using UnrealBuildTool;
 using System.IO;
+using UnrealBuildTool;
 
 public class CookedEditor : ModuleRules
 {
@@ -28,13 +28,13 @@ public class CookedEditor : ModuleRules
 				"TargetPlatform",
 			});
 
+		PrivateIncludePathModuleNames.Add("WindowsTargetPlatform");
 
 		// we currently need to pull in desktop templates that are private
 		// @todo move these out to Public so they can be extended like this
 		string EnginePath = Path.GetFullPath(Target.RelativeEnginePath);
 		PublicIncludePaths.AddRange(
 			new string[] {
-				Path.Combine(EnginePath, "Source/Developer/Windows/WindowsTargetPlatform/Private"),
 				Path.Combine(EnginePath, "Source/Developer/Linux/LinuxTargetPlatform/Private"),
 				Path.Combine(EnginePath, "Source/Developer/Mac/MacTargetPlatform/Private"),
 			}
