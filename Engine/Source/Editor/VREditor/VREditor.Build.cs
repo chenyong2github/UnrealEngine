27486505fc4d5891432981a25a195ed6c26ce921
christopher waters <christopher.waters@epicgames.com>
2023-02-27 14:05:38 -0500

    Moving loose files in VREditor into Private and Public.
    #preflight 63fcd7e5437ce3e7f32c91e3
    
    [CL 24428678 by christopher waters in ue5-main branch]

--- Engine/Source/Editor/VREditor/VREditor.Build.cs
+++ Engine/Source/Editor/VREditor/VREditor.Build.cs
@@ -12,8 +12,6 @@ namespace UnrealBuildTool.Rules
 				}
 			);
 
-			PublicIncludePaths.Add(ModuleDirectory);
-
             PrivateDependencyModuleNames.AddRange(
                 new string[] {
                     "AppFramework",
