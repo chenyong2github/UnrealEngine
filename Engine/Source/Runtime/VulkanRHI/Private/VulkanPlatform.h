a57ef911af2d126cacdfe9562a9cc82b1dc85356
christopher waters <christopher.waters@epicgames.com>
2023-02-28 15:46:51 -0500

    Fixing vulkan platform header names we can use COMPILED_PLATFORM_HEADER instead of our own version.
    #preflight 63fe3b5cef1b24bf9416ed05
    
    [CL 24450218 by christopher waters in ue5-main branch]

--- Engine/Source/Runtime/VulkanRHI/Private/VulkanPlatform.h
+++ Engine/Source/Runtime/VulkanRHI/Private/VulkanPlatform.h
@@ -4,11 +4,4 @@
 
 #include "HAL/PreprocessorHelpers.h"
 
-// Vulkan is currently organized just a little differently than COMPILED_PLATFORM_HEADER does things.
-#if PLATFORM_IS_EXTENSION
-	#define VULKAN_COMPILED_PLATFORM_HEADER(Suffix) PREPROCESSOR_TO_STRING(Suffix)
-#else
-	#define VULKAN_COMPILED_PLATFORM_HEADER(Suffix) PREPROCESSOR_TO_STRING(PLATFORM_HEADER_NAME/Suffix)
-#endif
-
-#include VULKAN_COMPILED_PLATFORM_HEADER(VulkanPlatformDefines.h)
+#include COMPILED_PLATFORM_HEADER(VulkanPlatformDefines.h)
