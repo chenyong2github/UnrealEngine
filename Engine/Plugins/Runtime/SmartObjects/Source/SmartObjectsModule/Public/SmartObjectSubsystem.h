4e10674ae033bc45a573852ebbcf6ff6d7c4b6c4
nat parkinson <Nat.Parkinson@epicgames.com>
2023-03-01 15:39:26 -0500

    [Backout] - CL24461299 to fix nonunity errors [FYI] mikko.mononen Original CL Desc ----------------------------------------------------------------- SmartObjects: Fix nonunity build
    #rb trivial
    #preflight 63ff52c4a134e0b059f957ec
    
    [CL 24469390 by nat parkinson in ue5-main branch]

--- Engine/Plugins/Runtime/SmartObjects/Source/SmartObjectsModule/Public/SmartObjectSubsystem.h
+++ Engine/Plugins/Runtime/SmartObjects/Source/SmartObjectsModule/Public/SmartObjectSubsystem.h
@@ -7,7 +7,6 @@
 #include "WorldConditionContext.h"
 #include "Subsystems/WorldSubsystem.h"
 #include "AI/Navigation/NavigationTypes.h"
-#include "AI/Navigation/NavQueryFilter.h"
 #include "SmartObjectSubsystem.generated.h"
 
 class UCanvas;
