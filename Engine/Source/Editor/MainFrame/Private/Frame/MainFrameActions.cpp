b9ecefcdbc95b4f80b59f40e0de7241c62e07df4
wouter burgers <wouter.burgers@epicgames.com>
2023-03-01 12:33:10 -0500

    SourceControl / UEFN: The F5 shortcut for getting latest should be specific to when Skein is enabled within UEFN.
    #preflight none
    
    [CL 24464995 by wouter burgers in ue5-main branch]

--- Engine/Source/Editor/MainFrame/Private/Frame/MainFrameActions.cpp
+++ Engine/Source/Editor/MainFrame/Private/Frame/MainFrameActions.cpp
@@ -111,7 +111,7 @@ void FMainFrameCommands::RegisterCommands()
 	UI_COMMAND( SubmitContent, "Submit Content", "Opens a dialog with check in options for content and levels.", EUserInterfaceActionType::Button, FInputChord() );
 	ActionList->MapAction( SubmitContent, FExecuteAction::CreateLambda([]() { FSourceControlWindows::ChoosePackagesToCheckIn(); }), FCanExecuteAction::CreateStatic(&FSourceControlWindows::CanChoosePackagesToCheckIn ), FGetActionCheckState(), FIsActionButtonVisible::CreateStatic(FSourceControlWindows::ShouldChoosePackagesToCheckBeVisible) );
 
-	UI_COMMAND( SyncContent, "Sync Content", "Saves all unsaved levels and assets to disk and then downloads the latest versions from revision control.", EUserInterfaceActionType::Button, FInputChord( EKeys::F5 ) );
+	UI_COMMAND( SyncContent, "Sync Content", "Saves all unsaved levels and assets to disk and then downloads the latest versions from revision control.", EUserInterfaceActionType::Button, FInputChord() );
 	ActionList->MapAction(SyncContent, FExecuteAction::CreateLambda([]() { FSourceControlWindows::SyncLatest(); }), FCanExecuteAction::CreateStatic( &FSourceControlWindows::CanSyncLatest ) );
 
 	UI_COMMAND( ConnectToSourceControl, "Connect to Revision Control...", "Connect to a revision control system for tracking changes to your content and levels.", EUserInterfaceActionType::Button, FInputChord() );
