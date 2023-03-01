c6b7cbaccbb4fb244d536b4f736fc708a2fdbe94
tim kennedy <tim.kennedy@epicgames.com>
2023-03-01 12:35:09 -0500

    #JIRA: none #preflight 63fe91f8ae54ee4ce9e7a51a ** Had to do a new CL because of the move rules preventing me from submitting the previous CL**
    So that the new Gauntlet RPC tools do not go out to licensees prematurely, moving all RPC related files to:
    
    FortniteGame/Build/Scripts/Automation/Utility/RPC/Gauntlet/Framework/
    FortniteGame/Build/Scripts/Automation/Utility/RPC/Gauntlet/Framework/Utils
    FortniteGame/Build/Scripts/Automation/Utility/RPC/Gauntlet/Framework/Utils/RPC
    
    This preserves Gauntlet's file structure, meaning that if we decide to make this available to license holders, we can simply move the contents of the Gauntlet folder into the Gauntlet project and the structure will be compatible. There are two classes that needed a file created to be moved - GauntletHttpClient and GauntletHttpResponse - they are now in Gauntlet.GauntletHttpClient.cs
    
    [CL 24465068 by tim kennedy in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Framework/Utils/Gauntlet.HttpRequest.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Framework/Utils/Gauntlet.HttpRequest.cs
@@ -51,46 +51,5 @@ namespace Gauntlet
 			}
 			public bool IsSuccessStatusCode { get { return StatusCode == HttpStatusCode.OK; } }
 		}
-		public class GauntletHttpClient : HttpClient
-		{
-			public async Task<HttpResponseMessage> GetRequestAsync(string Route)
-			{
-				LogOutgoingRequestURI(Route);
-				return await GetAsync(Route);
-			}
-
-			public async Task<HttpResponseMessage> SendRequestAsync(HttpRequestMessage Msg)
-			{
-				LogOutgoingRequestURI(Msg.RequestUri.ToString());
-				await LogOutgoingRequestPayload(Msg);
-				return await SendAsync(Msg);
-			}
-
-			private async Task LogOutgoingRequestPayload(HttpRequestMessage Msg)
-			{
-				string Payload = await Msg.Content.ReadAsStringAsync();
-
-				if (string.IsNullOrEmpty(Payload))
-				{
-					return;
-				}
-
-				Log.Verbose("Request Payload: {0}", Payload);
-			}
-
-			public void LogOutgoingRequestURI(string Route)
-			{
-				Log.Verbose("Making Http Request to URI:{0}{1}", BaseAddress, Route);
-			}
-		}
-	}
-
-	/// <summary>
-	/// Generic HTTP response class that 
-	/// </summary>
-	public class GauntletHttpResponse
-	{
-		public bool Success { get; set; }
-		public string Reason { get; set; }
 	}
 }
