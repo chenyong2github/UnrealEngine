467cb7cc636024952e3b3418d24bc48623c81478
Joe Kirchoff <joe.kirchoff@epicgames.com>
2023-03-08 13:12:33 -0500

    Suppress third party warnings in NNEOnnxruntime pch #preflight 6408cdedcf5fe1e28ce7e5fb
    [CL 24562976 by Joe Kirchoff in ue5-main branch]

--- Engine/Plugins/Experimental/NNE/Source/ThirdParty/onnxruntime/Onnxruntime/Private/ORTPrivatePch.h
+++ Engine/Plugins/Experimental/NNE/Source/ThirdParty/onnxruntime/Onnxruntime/Private/ORTPrivatePch.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+#include "HAL/Platform.h"
+
+THIRD_PARTY_INCLUDES_START
+
 #include "onnx/defs/schema.h"
 #include "onnx/defs/shape_inference.h"
 #include "core/framework/data_types.h"
@@ -10,3 +14,5 @@
 #include "core/providers/cpu/math/element_wise_ops.h"
 #include "core/providers/cpu/tensor/utils.h"
 #include "onnx/onnx_pb.h"
+
+THIRD_PARTY_INCLUDES_END
