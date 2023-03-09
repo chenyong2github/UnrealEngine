7e580ef63c07b28d51d4b4393fce1b1b6f18f4d0
jamie hayes <jamie.hayes@epicgames.com>
2023-03-09 13:34:35 -0500

    - Implement "WPO Disable Distance" for non-Nanite static meshes, and unify `FMaterialVertexParameters.bEvaluateWorldPositionOffset` for all vertex factories - Add instance draw distance and global clip plane culling to GPU instance culling and VSM culling shaders to prevent VS invocations for invisible instances. - Additional clean-up to GPU Scene data and culling code.
    #rb ola.olsson
    #preflight 6407c662ba12ba6416b8245b
    
    [CL 24579289 by jamie hayes in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/PrimitiveSceneProxy.h
+++ Engine/Source/Runtime/Engine/Public/PrimitiveSceneProxy.h
@@ -1493,12 +1493,6 @@ private:
 	/** Used for precomputed visibility */
 	int32 VisibilityId;
 
-	/** The primitive's cull distance. */
-	float MaxDrawDistance;
-
-	/** The primitive's minimum cull distance. */
-	float MinDrawDistance;
-
 	/** The primitive's uniform buffer. */
 	TUniformBufferRef<FPrimitiveUniformShaderParameters> UniformBuffer;
 
@@ -1535,6 +1529,13 @@ private:
 	void SetHiddenEdViews_RenderThread( uint64 InHiddenEditorViews );
 
 protected:
+
+	/** The primitive's cull distance. */
+	float MaxDrawDistance;
+
+	/** The primitive's minimum cull distance. */
+	float MinDrawDistance;
+	
 	/**
 	 * Updates the primitive proxy's cached transforms for all instances given a buffer of instance updates.
 	 * @param CmdBuffer - A record of all the add, update and remove instances for the proxy to apply to its internal data.
