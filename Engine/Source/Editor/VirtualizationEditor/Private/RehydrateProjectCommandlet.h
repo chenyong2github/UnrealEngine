6a7f58fedf2b2e0a7465e4052b1df9323df4f0a9
paul chipchase <paul.chipchase@epicgames.com>
2023-03-10 09:43:59 -0500

    Fix a typo in the VA commandlet documentation.
    #rb trivial
    #jira none
    #rnx
    #preflight 640b3ef2d778f88975290bdb
    
    [CL 24591048 by paul chipchase in ue5-main branch]

--- Engine/Source/Editor/VirtualizationEditor/Private/RehydrateProjectCommandlet.h
+++ Engine/Source/Editor/VirtualizationEditor/Private/RehydrateProjectCommandlet.h
@@ -11,7 +11,7 @@
  * Finds all packages in the project and attempts to rehydrate them. If revision control
  * is enabled then the commandlet will attempt to checkout the packages that need modification.
  *
- * Because the commmandlet is the VirtualizationEditor module it needs to be invoked
+ * Because the commandlet is the VirtualizationEditor module it needs to be invoked
  * with the command line:
  * -run="VirtualizationEditor.RehydrateProject"
  */
