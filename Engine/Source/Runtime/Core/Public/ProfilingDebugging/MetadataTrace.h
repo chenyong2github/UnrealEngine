33788a9f5c8a40522f61e18f1d51c53124bd4c35
ionut matasaru <ionut.matasaru@epicgames.com>
2023-03-14 13:53:45 -0400

    [Insights] MetadataTrace: Added UE_TRACE_METADATA_RESTORE_SCOPE(MetadataId) macro. Also did minor fixes to comments and code style.
    #rb none
    #fyi Johan.Berg
    #preflight 6410974a924023147d3e69eb
    
    [CL 24639983 by ionut matasaru in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/ProfilingDebugging/MetadataTrace.h
+++ Engine/Source/Runtime/Core/Public/ProfilingDebugging/MetadataTrace.h
@@ -5,9 +5,6 @@
 #include "Trace/Config.h"
 #include "Trace/Trace.h"
 
-namespace UE { namespace Trace { class FChannel; } }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 # if !defined(UE_TRACE_METADATA_ENABLED)
 	#if !IS_PROGRAM
 		#define	UE_TRACE_METADATA_ENABLED UE_TRACE_ENABLED
@@ -21,10 +18,10 @@ namespace UE { namespace Trace { class FChannel; } }
 // Only needed when enabled
 #include "Trace/Trace.inl"
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace UE { namespace Trace { class FChannel; } }
 
 /**
- * Channel that asset meta data is output on
+ * Channel that enables the metadata tracing
  */
 CORE_API UE_TRACE_CHANNEL_EXTERN(MetadataChannel);
 
@@ -39,14 +36,11 @@ CORE_API UE_TRACE_EVENT_BEGIN_EXTERN(MetadataStack, RestoreStack)
 	UE_TRACE_EVENT_FIELD(uint32, Id)
 UE_TRACE_EVENT_END()
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
 class FMetadataTrace
 {
 public:
 	CORE_API static uint32 SaveStack();
 };
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // For now just forward to the event definition macros.
 #define UE_TRACE_METADATA_EVENT_BEGIN_EXTERN(ScopeName, ...) UE_TRACE_EVENT_BEGIN_EXTERN(Metadata, ScopeName, ##__VA_ARGS__)
@@ -55,6 +49,10 @@ public:
 #define UE_TRACE_METADATA_EVENT_FIELD(Type, Name) UE_TRACE_EVENT_FIELD(Type, Name)
 #define UE_TRACE_METADATA_EVENT_REFERENCE_FIELD(RefLogger, RefEventName, Name) UE_TRACE_EVENT_REFERENCE_FIELD(RefLogger, RefEventName, Name)
 #define UE_TRACE_METADATA_EVENT_END() UE_TRACE_EVENT_END()
+
+/**
+ * A scope to push/pop data on the metadata stack.
+ */
 #define UE_TRACE_METADATA_SCOPE(ScopeName, ChannelExpr) UE_TRACE_LOG_SCOPED(Metadata, ScopeName, ChannelExpr)
 
 /**
@@ -79,24 +77,30 @@ class FMetadataRestoreScope
 {
 public:
 	CORE_API FMetadataRestoreScope(uint32 SavedMetadataIdentifier);
-	CORE_API ~FMetadataRestoreScope();;
+	CORE_API ~FMetadataRestoreScope();
 private:
 	void ActivateScope(uint32 InStackId);
 	UE::Trace::Private::FScopedLogScope Inner;
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#else
+/**
+ * Temporarily applies a previously saved metadata stack. See UE_TRACE_METADATA_SAVE_STACK.
+ * When this scope is exited, the previous metadata stack is restored.
+ */
+#define UE_TRACE_METADATA_RESTORE_SCOPE(SavedMetadataIdentifier) \
+	FMetadataRestoreScope PREPROCESSOR_JOIN(__MetadataRestoreScope, __LINE__)(SavedMetadataIdentifier);
 
-#define UE_TRACE_METADATA_EVENT_BEGIN_EXTERN(ScopeName, ...) 
-#define UE_TRACE_METADATA_EVENT_DEFINE(ScopeName) 
-#define UE_TRACE_METADATA_EVENT_BEGIN(ScopeName, ...) 
-#define UE_TRACE_METADATA_EVENT_FIELD(Type, Name) 
-#define UE_TRACE_METADATA_EVENT_END() 
-#define UE_TRACE_METADATA_SCOPE(ScopeName) 
+#else // UE_TRACE_METADATA_ENABLED
+
+#define UE_TRACE_METADATA_EVENT_BEGIN_EXTERN(ScopeName, ...)
+#define UE_TRACE_METADATA_EVENT_DEFINE(ScopeName)
+#define UE_TRACE_METADATA_EVENT_BEGIN(ScopeName, ...)
+#define UE_TRACE_METADATA_EVENT_FIELD(Type, Name)
+#define UE_TRACE_METADATA_EVENT_END()
+#define UE_TRACE_METADATA_SCOPE(ScopeName)
 
 #define UE_TRACE_METADATA_CLEAR_SCOPE()
 #define UE_TRACE_METADATA_SAVE_STACK() 0
-#define UE_TRACE_METADATA_RESTORE_STACK(...)
+#define UE_TRACE_METADATA_RESTORE_SCOPE(SavedMetadataIdentifier)
 
-#endif
+#endif // UE_TRACE_METADATA_ENABLED
