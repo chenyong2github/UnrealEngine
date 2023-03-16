b7c639d97cfa389c6ce50928b3f0f0e75bd6d1bf
laura hermanns <laura.hermanns@epicgames.com>
2023-03-16 17:00:36 -0400

    Re-insert accidentally removed DLL shutdown for DXC on Linux.
    #jira UE-180141
    #preflight 6413727f7d1280ffe7e50138
    #rnx
    
    [CL 24679467 by laura hermanns in ue5-main branch]

--- Engine/Source/ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/tools/clang/tools/dxcompiler/DXCompiler.cpp
+++ Engine/Source/ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/tools/clang/tools/dxcompiler/DXCompiler.cpp
@@ -85,6 +85,22 @@ HRESULT __attribute__ ((constructor)) DllMain() {
   return InitMaybeFail();
 }
 
+// UE Change Begin: Allow to manually shutdown compiler to avoid dangling mutex on Linux.
+#if defined(DXC_EXPLICIT_DLLSHUTDOWN)
+void DllShutdown() {
+  static bool shutdown;
+  assert(!shutdown && "DllShutdown already called for libdxcompiler.so");
+  if (!shutdown) {
+    DxcSetThreadMallocToDefault();
+    ::hlsl::options::cleanupHlslOptTable();
+    ::llvm::sys::fs::CleanupPerThreadFileSystem();
+    ::llvm::llvm_shutdown();
+    DxcClearThreadMalloc();
+    DxcCleanupThreadMalloc();
+    shutdown = true;
+  }
+}
+#else
 void __attribute__ ((destructor)) DllShutdown() {
   DxcSetThreadMallocToDefault();
   ::hlsl::options::cleanupHlslOptTable();
@@ -93,6 +109,8 @@ void __attribute__ ((destructor)) DllShutdown() {
   DxcClearThreadMalloc();
   DxcCleanupThreadMalloc();
 }
+#endif // DXC_EXPLICIT_DLLSHUTDOWN
+// UE Change End: Allow to manually shutdown compiler to avoid dangling mutex on Linux.
 #else // LLVM_ON_UNIX
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD Reason, LPVOID reserved) {
   BOOL result = TRUE;
