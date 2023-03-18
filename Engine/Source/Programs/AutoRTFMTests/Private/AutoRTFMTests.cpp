078d449282b26eb85583cdeffef5c0909e4a3562
brandon schaefer <Brandon.Schaefer@epicgames.com>
2023-03-18 01:39:43 -0400

    Fix include warnings issue
    [FYI] Neil.Henning
    #preflight none
    
    [CL 24702026 by brandon schaefer in ue5-main branch]

--- Engine/Source/Programs/AutoRTFMTests/Private/AutoRTFMTests.cpp
+++ Engine/Source/Programs/AutoRTFMTests/Private/AutoRTFMTests.cpp
@@ -1,8 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Modules/ModuleManager.h"
-#include "LaunchEngineLoop.h"   // GEngineLoop
-
+#include "CoreMinimal.h"
+#include "CoreGlobals.h"
 #include "RequiredProgramMainCPPInclude.h" // required for ue programs
 
 IMPLEMENT_APPLICATION(AutoRTFMTests, "AutoRTFMTests");
