b722dbe12a898b8c404df227570c4d8562e3f05d
bryan sefcik <bryan.sefcik@epicgames.com>
2023-03-22 00:43:18 -0400

    Updated xinput module to work for arm64 devices.
    #fyi joe.kirchoff
    #preflight 641a271232723d4a20c15495
    
    [CL 24744251 by bryan sefcik in ue5-main branch]

--- Engine/Source/ThirdParty/Windows/XInput/XInput.Build.cs
+++ Engine/Source/ThirdParty/Windows/XInput/XInput.Build.cs
@@ -1,4 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+using EpicGames.Core;
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class XInput : ModuleRules
@@ -10,11 +13,26 @@ public class XInput : ModuleRules
 		Type = ModuleType.External;
 
 		// Ensure correct include and link paths for xinput so the correct dll is loaded (xinput1_3.dll)
-
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			PublicAdditionalLibraries.Add(DirectX.GetLibDir(Target) + "XInput.lib");
-			PublicSystemIncludePaths.Add(DirectX.GetIncludeDir(Target));
+			if (Target.Architecture.bIsX64)
+			{
+				PublicAdditionalLibraries.Add(DirectX.GetLibDir(Target) + "XInput.lib");
+				PublicSystemIncludePaths.Add(DirectX.GetIncludeDir(Target));
+			}
+			else
+			{
+				Version WindowsSdkVersion;
+				DirectoryReference WindowsSdkDir;
+				if (!WindowsExports.TryGetWindowsSdkDir(null, out WindowsSdkVersion, out WindowsSdkDir))
+				{
+					throw new BuildException("Windows SDK must be installed in order to build this target.");
+				}
+
+				// add basic XInput library
+				string WindowsSdkLibDir = Path.Combine(WindowsSdkDir.ToString(), "lib", WindowsSdkVersion.ToString(), "um", Target.Architecture.WindowsSystemLibDir);
+				PublicAdditionalLibraries.Add(Path.Combine(WindowsSdkLibDir, "xinput.lib"));
+			}
 		}
 	}
 }
