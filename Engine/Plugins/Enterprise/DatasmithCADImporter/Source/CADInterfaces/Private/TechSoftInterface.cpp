3d559210ae4768f6392b02940be6cceac6625a72
David Lesage <David.Lesage@epicgames.com>
2023-03-23 12:46:53 -0400

    Rollback 24762504 for TechSoftInterface #rb trivial #preflight 641c80847a393e211a8b93cb
    [CL 24763987 by David Lesage in ue5-main branch]

--- Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CADInterfaces/Private/TechSoftInterface.cpp
+++ Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CADInterfaces/Private/TechSoftInterface.cpp
@@ -97,19 +97,7 @@ FString GetTechSoftVersion()
 FUniqueTechSoftModelFile LoadModelFileFromFile(const A3DImport& Import, A3DStatus& Status)
 {
 	A3DAsmModelFile* ModelFile = nullptr;
-#if !PLATFORM_EXCEPTIONS_DISABLED
-	try
-#endif
-	{
-		Status = A3DAsmModelFileLoadFromFile(Import.GetFilePath(), &Import.m_sLoadData, &ModelFile);
-	}
-#if !PLATFORM_EXCEPTIONS_DISABLED
-	catch (...)
-	{
-		UE_LOG(LogCADInterfaces, Warning, TEXT("Failed to load %s. An exception is thrown."), *Import.GetFilePath());
-		return FUniqueTechSoftModelFile();
-	}
-#endif
+	Status = A3DAsmModelFileLoadFromFile(Import.GetFilePath(), &Import.m_sLoadData, &ModelFile);
 
 	switch (Status)
 	{
