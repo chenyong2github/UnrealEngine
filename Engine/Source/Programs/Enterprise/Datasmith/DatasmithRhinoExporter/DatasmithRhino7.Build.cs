f260800c61a1933c5793a75c3384251b4e1a6b2b
benoit deschenes <benoit.deschenes@epicgames.com>
2023-04-04 11:39:14 -0400

    [DatasmithExporter] - Fix inconsistent supported platform tag.
    #rb trivial
    #preflight 642b46436a37fb35e9df90a5
    
    [CL 24911868 by benoit deschenes in ue5-main branch]

--- Engine/Source/Programs/Enterprise/Datasmith/DatasmithRhinoExporter/DatasmithRhino7.Build.cs
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithRhinoExporter/DatasmithRhino7.Build.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace UnrealBuildTool.Rules
 {
-	[SupportedPlatforms("Win64")]
+	[SupportedPlatforms("Win64", "Mac")]
 	public class DatasmithRhino7 : DatasmithRhinoBase
 	{
 		public DatasmithRhino7(ReadOnlyTargetRules Target)
