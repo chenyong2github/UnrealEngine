af8ebcae75c666ad391218719901787016d02595
bryan sefcik <bryan.sefcik@epicgames.com>
2023-04-12 13:03:40 -0400

    [UnrealEd] Removed public dependency on AddContentDialog to break circular dependency "AddContentDialog -> UnrealEd -> AddContentDialog"
    #preflight 6436c3f9f12d5de7056ae963
    
    [CL 25009957 by bryan sefcik in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/UnrealEd.Build.cs
+++ Engine/Source/Editor/UnrealEd/UnrealEd.Build.cs
@@ -322,7 +322,6 @@ public class UnrealEd : ModuleRules
 				"CollectionManager",
 				"ContentBrowser",
 				"BlueprintGraph",
-				"AddContentDialog",
 				"MeshUtilities",
 				"KismetCompiler",
 				"NavigationSystem",
