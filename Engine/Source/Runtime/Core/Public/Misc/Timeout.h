f2a2b7d9cffd38b6325edc69a6eaf8ce6ed68a0e
Andriy Tylychko <Andriy.Tylychko@epicgames.com>
2023-04-12 06:45:15 -0400

    making  `FTimespan::MaxValue()` a special "infinity" value for `FTimeout`. this also happen to be MAX_UINT and so plays well with OS waiting with timeout functions #rb none #preflight 643687b40c19befb6c6f75a1
    [CL 25004618 by Andriy Tylychko in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Misc/Timeout.h
+++ Engine/Source/Runtime/Core/Public/Misc/Timeout.h
@@ -24,7 +24,7 @@ namespace UE
 
 		explicit operator bool() const
 		{
-			return GetElapsedTime() >= Timeout;
+			return GetRemainingTime() <= FTimespan::Zero();
 		}
 
 		FTimespan GetElapsedTime() const
@@ -34,7 +34,22 @@ namespace UE
 
 		FTimespan GetRemainingTime() const
 		{
-			return Timeout - GetElapsedTime();
+			return Timeout == FTimespan::MaxValue() ? FTimespan::MaxValue() : Timeout - GetElapsedTime();
+		}
+
+		static FTimeout Never()
+		{
+			return FTimeout{ FTimespan::MaxValue() };
+		}
+
+		FTimespan GetTimeoutValue() const
+		{
+			return Timeout;
+		}
+
+		friend bool operator==(FTimeout Left, FTimeout Right)
+		{
+			return Left.Timeout == Right.Timeout && (Left.Timeout == FTimespan::MaxValue() || Left.Start == Right.Start);
 		}
 
 	private:
