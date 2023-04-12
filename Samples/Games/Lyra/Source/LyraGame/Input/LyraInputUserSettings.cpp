1f291e522742c9f98567791fec1638befc5653cb
ben hoffman <ben.hoffman@epicgames.com>
2023-04-12 14:14:18 -0400

    Lyra: Add a Lyra Player Mappable key settings, Lyra Input user settings, and Lyra player mappable key profile base classes. There are no customizations in them for now, but this way it is easier for a game based on Lyra to customize their input settings.
    For now, these are being submitted but not enabled until the content gets cleaned up to use the new player mappable key settings.
    
    #jira UE-180031
    #rb trivial
    #rnx
    #preflight 6436d69a00398d6f885bd888
    
    [CL 25011372 by ben hoffman in ue5-main branch]

---
+++ Samples/Games/Lyra/Source/LyraGame/Input/LyraInputUserSettings.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Input/LyraInputUserSettings.h"
+
+void ULyraInputUserSettings::ApplySettings()
+{
+	Super::ApplySettings();
+
+	UE_LOG(LogTemp, Warning, TEXT("APPLY SETTING"));
+}
