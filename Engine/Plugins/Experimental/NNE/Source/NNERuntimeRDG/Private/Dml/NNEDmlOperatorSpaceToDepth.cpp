a52b0aaa8ed612ff5173b66547dad854c4a3a508
giacomo serafini <giacomo.serafini@epicgames.com>
2023-04-20 04:48:28 -0400

    NNE - Add RDG Dml DepthToSpace&SpaceToDepth
    #rnx
    #preflight 643fe3afbf10538466ab2138
    #jira UE-183656
    
    [CL 25123392 by giacomo serafini in ue5-main branch]

---
+++ Engine/Plugins/Experimental/NNE/Source/NNERuntimeRDG/Private/Dml/NNEDmlOperatorSpaceToDepth.cpp
@@ -0,0 +1,77 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#ifdef NNE_USE_DIRECTML
+
+#include "NNEDmlOperator.h"
+#include "NNEDmlOperatorUtils.h"
+
+namespace UE::NNERuntimeRDG::Private::Dml
+{
+
+class FOperatorDmlSpaceToDepth : public FOperatorDml
+{
+
+
+public:
+
+	static FOperatorDml* Create()
+	{
+		return new FOperatorDmlSpaceToDepth();
+	}
+
+	//
+	//
+	//
+	virtual bool Initialize(IDMLDevice* Device, TArrayView<const NNECore::Internal::FTensor> InputTensors, TArrayView<const NNECore::Internal::FTensor> OutputTensors, const NNECore::FAttributeMap& Attributes) override
+	{
+		check(InputTensors.Num() == 1);
+		check(OutputTensors.Num() == 1);
+
+		int32 BlockSize;
+
+		const FNNEAttributeValue* BlockSizeAttr = Attributes.GetAttributeValue(TEXT("blocksize"));
+		if (BlockSizeAttr)
+		{
+			BlockSize = BlockSizeAttr->GetValue<int32>();
+		}
+		else
+		{
+			UE_LOG(LogNNE, Error, TEXT("blocksize attribute is required"));
+			return false;
+		}
+
+
+		const NNECore::Internal::FTensor& InputTensorDesc = InputTensors[0];
+		const NNECore::Internal::FTensor& OutputTensorDesc = OutputTensors[0];
+
+		DmlUtil::FTensorDesc	DmlInputTensorDesc{};
+		DmlUtil::FTensorDesc	DmlOutputTensorDesc{};
+
+		if (!DmlInputTensorDesc.InitFromTensor(InputTensorDesc, 4))
+		{
+			UE_LOG(LogNNE, Warning, TEXT("Failed to initialize input tensor for DML inference"));
+			return false;
+		}
+		if (!DmlOutputTensorDesc.InitFromTensor(OutputTensorDesc, 4))
+		{
+			UE_LOG(LogNNE, Warning, TEXT("Failed to initialize output tensor for DML inference"));
+			return false;
+		}
+
+		DML_SPACE_TO_DEPTH1_OPERATOR_DESC DmlSpaceToDepthOpDesc{};
+
+		DmlSpaceToDepthOpDesc.InputTensor = &DmlInputTensorDesc.Desc;
+		DmlSpaceToDepthOpDesc.OutputTensor = &DmlOutputTensorDesc.Desc;
+		DmlSpaceToDepthOpDesc.BlockSize = BlockSize;
+		DmlSpaceToDepthOpDesc.Order = DML_DEPTH_SPACE_ORDER_DEPTH_COLUMN_ROW;
+
+		return CreateOperator(Device, DML_OPERATOR_DESC{ DML_OPERATOR_SPACE_TO_DEPTH1, &DmlSpaceToDepthOpDesc });
+	}
+};
+
+// Register SpaceToDepth operator on Module startup
+NNE_DML_REGISTER_OP(SpaceToDepth)
+
+} // namespace UE::NNERuntimeRDG::Private::Dml
+
+#endif // NNE_USE_DIRECTML
