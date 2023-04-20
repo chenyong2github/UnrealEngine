56f8b20c4281a63934bb47ac001f530722d3b248
samuele rigamonti <samuele.rigamonti@epicgames.com>
2023-04-20 06:05:57 -0400

    motion matching - fix for Warning: Plugins/Experimental/Animation/MotionTrajectory/Source/MotionTrajectory/Private/MotionTrajectory.cpp(10): Missing '#undef LOCTEXT_NAMESPACE' for '#define LOCTEXT_NAMESPACE'
    #rb aaron.cox
    #preflight 6440d305a9720904a6f0b9bb
    
    [CL 25124222 by samuele rigamonti in ue5-main branch]

--- Engine/Plugins/Experimental/Animation/MotionTrajectory/Source/MotionTrajectory/Private/MotionTrajectory.cpp
+++ Engine/Plugins/Experimental/Animation/MotionTrajectory/Source/MotionTrajectory/Private/MotionTrajectory.cpp
@@ -1,13 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "MotionTrajectory.h"
-
-#include "Engine/World.h"
-#include "GameFramework/Pawn.h"
-#include "Kismet/KismetSystemLibrary.h"
 #include "Modules/ModuleManager.h"
-
-#define LOCTEXT_NAMESPACE "MotionTrajectory"
 
 DEFINE_LOG_CATEGORY(LogMotionTrajectory);
 
