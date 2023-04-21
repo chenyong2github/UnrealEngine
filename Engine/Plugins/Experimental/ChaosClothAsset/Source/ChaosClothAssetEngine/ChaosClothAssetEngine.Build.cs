0a2cc0dd7da42588548d85302f2ee333db6a0a25
zhenglin geng <zhenglin.geng@epicgames.com>
2023-04-20 21:05:37 -0400

    ChaosClothAssetEngine: fix module dependencies
    [CL 25141367 by zhenglin geng in ue5-main branch]

--- Engine/Plugins/Experimental/ChaosClothAsset/Source/ChaosClothAssetEngine/ChaosClothAssetEngine.Build.cs
+++ Engine/Plugins/Experimental/ChaosClothAsset/Source/ChaosClothAssetEngine/ChaosClothAssetEngine.Build.cs
@@ -19,6 +19,8 @@ public class ChaosClothAssetEngine : ModuleRules
 				"Chaos",
 				"ChaosClothAsset",
 				"ChaosCloth",
+				"ChaosCaching",
+				"ClothingSystemRuntimeCommon",
 				"ClothingSystemRuntimeInterface",
 				"DataflowEngine"
 			}
