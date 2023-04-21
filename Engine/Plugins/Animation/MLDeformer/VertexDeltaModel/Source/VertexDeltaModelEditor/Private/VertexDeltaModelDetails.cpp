105fc994b7e8acc879c1e97ee7a6c46f41f6ac8e
john vanderburg <john.vanderburg@epicgames.com>
2023-04-21 11:28:31 -0400

    [MLDeformer] Show a warning in the Vertex Delta Model that it shouldn't be used in production.
    #jira UE-181435
    #trivial
    #preflight 6442a667b14f1faacf113fef
    #preflight 6442a667b14f1faacf113fef
    
    [CL 25146327 by john vanderburg in ue5-main branch]

--- Engine/Plugins/Animation/MLDeformer/VertexDeltaModel/Source/VertexDeltaModelEditor/Private/VertexDeltaModelDetails.cpp
+++ Engine/Plugins/Animation/MLDeformer/VertexDeltaModel/Source/VertexDeltaModelEditor/Private/VertexDeltaModelDetails.cpp
@@ -3,6 +3,9 @@
 #include "VertexDeltaModelDetails.h"
 #include "VertexDeltaModel.h"
 #include "DetailCategoryBuilder.h"
+#include "DetailWidgetRow.h"
+#include "Widgets/Layout/SBox.h"
+#include "SWarningOrErrorBox.h"
 
 #define LOCTEXT_NAMESPACE "VertexDeltaModelDetails"
 
@@ -26,6 +29,18 @@ namespace UE::VertexDeltaModel
 		TrainingSettingsCategoryBuilder->AddProperty(GET_MEMBER_NAME_CHECKED(UVertexDeltaModel, NumIterations));
 		TrainingSettingsCategoryBuilder->AddProperty(GET_MEMBER_NAME_CHECKED(UVertexDeltaModel, BatchSize));
 		TrainingSettingsCategoryBuilder->AddProperty(GET_MEMBER_NAME_CHECKED(UVertexDeltaModel, LearningRate));
+
+		FDetailWidgetRow& ExampleWarningRow = TrainingSettingsCategoryBuilder->AddCustomRow(FText::FromString("VertexDeltaModelWarning"))
+			.WholeRowContent()
+			[
+				SNew(SBox)
+				.Padding(FMargin(0.0f, 4.0f))
+				[
+					SNew(SWarningOrErrorBox)
+					.MessageStyle(EMessageStyle::Warning)
+					.Message(LOCTEXT("VertexDeltaModelWarningText", "The Vertex Delta Model is a GPU based example model that shouldn't be used in production. Please use the Neural Morph Model with the mode set to 'Global' to achieve similar results in production."))
+				]
+			];
 	}
 }	// namespace UE::VertexDeltaModel
 
