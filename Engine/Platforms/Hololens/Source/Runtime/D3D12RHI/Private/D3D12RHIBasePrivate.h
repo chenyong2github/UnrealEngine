a9447e051c31d5a4f206d0c7d673c3ba4d8625c8
christopher waters <christopher.waters@epicgames.com>
2023-04-24 18:14:46 -0400

    Moving more D3D12 defines - D3D12_USE_DUMMY_BACKBUFFER from Public to Private. - FD3D12_TEXTURE_DATA_PITCH_ALIGNMENT into the platform abstractions. - FD3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER from Public to Private.
    #preflight 6446d9942804595a04464506
    
    [CL 25172642 by christopher waters in ue5-main branch]

--- Engine/Platforms/Hololens/Source/Runtime/D3D12RHI/Private/D3D12RHIBasePrivate.h
+++ Engine/Platforms/Hololens/Source/Runtime/D3D12RHI/Private/D3D12RHIBasePrivate.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include "D3D12ThirdParty.h"
+
 // Standard Hololens implementation, used to mark the type as 'final'.
 struct FD3D12Payload final : public FD3D12PayloadBase
 {
@@ -16,12 +18,13 @@ struct FD3D12Payload final : public FD3D12PayloadBase
 
 // Hololens uses the graphics command list interface for the copy queue.
 typedef ID3D12GraphicsCommandList ID3D12CopyCommandList;
+
 #define D3D12RHI_PLATFORM_COPY_COMMAND_LIST_TYPE D3D12_COMMAND_LIST_TYPE_COPY
+
+#define FD3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER
 
 // Hololens uses standard D3D12 query heaps for timestamps
 #define D3D12RHI_PLATFORM_USES_TIMESTAMP_QUERIES 1
-
-#include "D3D12ThirdParty.h"
 
 #define D3D12RHI_RESOURCE_FLAG_ALLOW_INDIRECT_BUFFER D3D12_RESOURCE_FLAG_NONE
 #define D3D12RHI_HEAP_FLAG_ALLOW_INDIRECT_BUFFERS		D3D12_HEAP_FLAG_NONE
@@ -34,6 +37,7 @@ typedef ID3D12GraphicsCommandList ID3D12CopyCommandList;
 #define FD3D12_HEAP_FLAG_CREATE_NOT_ZEROED D3D12_HEAP_FLAG_NONE
 
 #define USE_STATIC_ROOT_SIGNATURE			0
+#define D3D12_USE_DUMMY_BACKBUFFER			1
 
 #include "D3D12Util.h"
 
--- Engine/Source/Runtime/D3D12RHI/Private/Windows/D3D12RHIBasePrivate.h
+++ Engine/Source/Runtime/D3D12RHI/Private/Windows/D3D12RHIBasePrivate.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include "Windows/D3D12ThirdParty.h"
+
 // Standard Windows implementation, used to mark the type as 'final'.
 struct FD3D12Payload final : public FD3D12PayloadBase
 {
@@ -16,12 +18,13 @@ struct FD3D12Payload final : public FD3D12PayloadBase
 
 // Windows uses the graphics command list interface for the copy queue.
 typedef ID3D12GraphicsCommandList ID3D12CopyCommandList;
+
 #define D3D12RHI_PLATFORM_COPY_COMMAND_LIST_TYPE D3D12_COMMAND_LIST_TYPE_COPY
+
+#define FD3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER
 
 // Windows uses standard D3D12 query heaps for timestamps
 #define D3D12RHI_PLATFORM_USES_TIMESTAMP_QUERIES 1
-
-#include "Windows/D3D12ThirdParty.h"
 
 #define D3D12RHI_RESOURCE_FLAG_ALLOW_INDIRECT_BUFFER D3D12_RESOURCE_FLAG_NONE
 #define D3D12RHI_HEAP_FLAG_ALLOW_INDIRECT_BUFFERS		D3D12_HEAP_FLAG_NONE
@@ -30,6 +33,7 @@ typedef ID3D12GraphicsCommandList ID3D12CopyCommandList;
 #define D3D12RHI_NEEDS_SHADER_FEATURE_CHECKS 1
 
 #define USE_STATIC_ROOT_SIGNATURE			0
+#define D3D12_USE_DUMMY_BACKBUFFER			1
 
 #include "D3D12Util.h"
 #include "WindowsD3D12DiskCache.h"
