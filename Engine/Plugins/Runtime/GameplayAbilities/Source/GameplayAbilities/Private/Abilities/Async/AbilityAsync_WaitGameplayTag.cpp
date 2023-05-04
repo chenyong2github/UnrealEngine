5a62c08fae8cc38f97dbd090e8e277148ad1d096
yuval bar <yuval.bar@epicgames.com>
2023-05-04 18:13:50 -0400

    Add log warning when WaitGameplayTag is used before an ASC is valid, resulting in not registering for gameplay tag count changes.
    [REVIEW] [at]Andrew.Thayer, [at]Todd.Eckert
    
    [CL 25344022 by yuval bar in ue5-main branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Abilities/Async/AbilityAsync_WaitGameplayTag.cpp
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Abilities/Async/AbilityAsync_WaitGameplayTag.cpp
@@ -1,8 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Abilities/Async/AbilityAsync_WaitGameplayTag.h"
-#include "AbilitySystemGlobals.h"
 #include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AbilitySystemLog.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(AbilityAsync_WaitGameplayTag)
 
@@ -27,6 +28,7 @@ void UAbilityAsync_WaitGameplayTag::Activate()
 	}
 	else
 	{
+		ABILITY_LOG(Warning, TEXT("%s: AbilitySystemComponent is nullptr! Could not register for gameplay tag event with Tag = %s."), *GetName(), *Tag.GetTagName().ToString());
 		EndAction();
 	}
 }
