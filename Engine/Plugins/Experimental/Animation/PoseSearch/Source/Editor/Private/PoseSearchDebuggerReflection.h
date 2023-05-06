23c4b6736e53a6415e1e727bc14c78434dd02b67
samuele rigamonti <samuele.rigamonti@epicgames.com>
2023-05-05 21:36:32 -0400

    motion matching - adding units to UPoseSearchDebuggerReflection properties
    [REVIEW] [at]jose.villarroel, [at]keith.yerex, [at]aaron.cox, [at]roland.munguia
    #preflight 64552f67386cf3c2734228f2
    
    [CL 25363334 by samuele rigamonti in ue5-main branch]

--- Engine/Plugins/Experimental/Animation/PoseSearch/Source/Editor/Private/PoseSearchDebuggerReflection.h
+++ Engine/Plugins/Experimental/Animation/PoseSearch/Source/Editor/Private/PoseSearchDebuggerReflection.h
@@ -15,24 +15,24 @@ class POSESEARCHEDITOR_API UPoseSearchDebuggerReflection : public UObject
 
 public:
 	/** Time since last PoseSearch */
-	UPROPERTY(VisibleAnywhere, Category="Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category="Motion Matching State", meta = (ForceUnits = "s"))
 	float ElapsedPoseSearchTime = 0.0f;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "s"))
 	float AssetPlayerTime = 0.0f;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "s"))
 	float LastDeltaTime = 0.0f;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "cm/s"))
 	float SimLinearVelocity;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "deg/s"))
 	float SimAngularVelocity;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "cm/s"))
 	float AnimLinearVelocity;
 
-	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State")
+	UPROPERTY(VisibleAnywhere, Category = "Motion Matching State", meta = (ForceUnits = "deg/s"))
 	float AnimAngularVelocity;
 };
