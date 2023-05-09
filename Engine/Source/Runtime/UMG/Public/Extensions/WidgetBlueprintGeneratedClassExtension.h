202641fa550d0c0173920b17c004ea1758285347
Patrick Boutot <Patrick.Boutot@epicgames.com>
2023-05-09 07:34:37 -0400

    UMG: Add comments on WIdgetBllueprintGeneratedClassExtension's function #rb none #rnx #preflight none
    [CL 25385329 by Patrick Boutot in ue5-main branch]

--- Engine/Source/Runtime/UMG/Public/Extensions/WidgetBlueprintGeneratedClassExtension.h
+++ Engine/Source/Runtime/UMG/Public/Extensions/WidgetBlueprintGeneratedClassExtension.h
@@ -17,12 +17,21 @@ class UMG_API UWidgetBlueprintGeneratedClassExtension : public UObject
 	GENERATED_BODY()
 	
 public:
-	/** Extend the UUserWidget::Initialize function */
+	/**
+	 * Extend the UUserWidget::Initialize function.
+	 * Called once when the UUserWidget is created.
+	 */
 	virtual void Initialize(UUserWidget* UserWidget) { }
 	/** Extend the UUserWidget::PreConstruct function */
 	virtual void PreConstruct(UUserWidget* UserWidget, bool IsDesignTime) { }
-	/** Extend the UUserWidget::Construct function */
+	/**
+	 * Extend the UUserWidget::Construct function.
+	 * Called after when the SWidget is created. (TakeWidget)
+	 */
 	virtual void Construct(UUserWidget* UserWidget) { }
-	/** Extend the UUserWidget::Destruct function */
+	/**
+	 * Extend the UUserWidget::Destruct function.
+	 * Called when the SWidget is going to be destroy. (ReleaseSlateWidget)
+	 */
 	virtual void Destruct(UUserWidget* UserWidget) { }
 };
