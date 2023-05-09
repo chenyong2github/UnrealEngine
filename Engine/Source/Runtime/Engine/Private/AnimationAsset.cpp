8c496b15c5a08166401742bfd6ca2f70faa73383
euan carmichael <euan.carmichael@epicgames.com>
2023-05-09 15:31:52 -0400

    Animation editor will now correctly apply preview mesh from a compatible skeleton #jira UE-185278, #rb Thomas.Sarkanen #preflight 645a9e52a73096fab78a0487
    [CL 25393782 by euan carmichael in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/AnimationAsset.cpp
+++ Engine/Source/Runtime/Engine/Private/AnimationAsset.cpp
@@ -387,7 +387,7 @@ USkeletalMesh* UAnimationAsset::GetPreviewMesh(bool bFindIfNotSet)
 #if WITH_EDITORONLY_DATA
 	USkeletalMesh* PreviewMesh = PreviewSkeletalMesh.LoadSynchronous();
 	// if somehow skeleton changes, just nullify it. 
-	if (PreviewMesh && PreviewMesh->GetSkeleton() != Skeleton)
+	if (PreviewMesh && !PreviewMesh->GetSkeleton()->IsCompatibleForEditor(Skeleton))
 	{
 		PreviewMesh = nullptr;
 		SetPreviewMesh(nullptr);
