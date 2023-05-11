a017640ea2a1f382440d7fb80888f51922d24bba
alfaroh corneyiii <alfaroh.corneyiii@epicgames.com>
2023-05-11 14:22:58 -0400

    [Backout] - CL25430596 #fyi alfaroh.corneyiii Original CL Desc ----------------------------------------------------------------- Turn Steam Audio plugin into a stub plugin #jira UE-185384 #rb aaron.mcleran #preflight #preflight 645c2c9d592448f08b4275e8
    [CL 25432268 by alfaroh corneyiii in ue5-main branch]

---
+++ Engine/Plugins/Runtime/Steam/SteamAudio/Source/SteamAudioEditor/SteamAudioEditor.Build.cs
@@ -0,0 +1,58 @@
+//
+// Copyright (C) Valve Corporation. All rights reserved.
+//
+
+namespace UnrealBuildTool.Rules
+{
+	public class SteamAudioEditor : ModuleRules
+	{
+		public SteamAudioEditor(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[] {
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"InputCore",
+					"EditorFramework",
+					"UnrealEd",
+					"LevelEditor",
+					
+					"RenderCore",
+					"RHI",
+					"AudioEditor",
+					"AudioMixer",
+					"SteamAudio"
+				}
+			);
+
+			PrivateIncludePathModuleNames.AddRange(
+				new string[] {
+				    "AssetTools",
+				    "Landscape"
+			});
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
+					"Slate",
+					"SlateCore",
+					"UnrealEd",
+					"AudioEditor",
+					"LevelEditor",
+					"Landscape",
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"InputCore",
+					"PropertyEditor",
+					"Projects",
+					
+					"SteamAudio"
+				 }
+			);
+
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "libPhonon");
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "DX11Audio");
+		}
+	}
+}
