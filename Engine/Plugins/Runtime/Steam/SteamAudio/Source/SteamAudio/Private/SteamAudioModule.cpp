2e909729227bc59a55253bd825fa34f5c482aec8
alfaroh corneyiii <alfaroh.corneyiii@epicgames.com>
2023-05-11 17:54:38 -0400

    Steam Audio Deprecation (resubmit) #jira none #rb trivial #preflight 645c2c9d592448f08b4275e8
    [CL 25437167 by alfaroh corneyiii in ue5-main branch]

--- Engine/Plugins/Runtime/Steam/SteamAudio/Source/SteamAudio/Private/SteamAudioModule.cpp
+++ Engine/Plugins/Runtime/Steam/SteamAudio/Source/SteamAudio/Private/SteamAudioModule.cpp
@@ -3,16 +3,15 @@
 //
 
 #include "SteamAudioModule.h"
-#include "PhononCommon.h"
 #include "Misc/Paths.h"
 #include "Features/IModularFeatures.h"
+
+DEFINE_LOG_CATEGORY(LogSteamAudio);
 
 IMPLEMENT_MODULE(SteamAudio::FSteamAudioModule, SteamAudio)
 
 namespace SteamAudio
 {
-	void* FSteamAudioModule::PhononDllHandle = nullptr;
-
 	static bool bModuleStartedUp = false;
 
 	FSteamAudioModule::FSteamAudioModule()
@@ -31,34 +30,7 @@ namespace SteamAudio
 
 		UE_LOG(LogSteamAudio, Log, TEXT("FSteamAudioModule Startup"));
 
-		UE_LOG(LogSteamAudio, Warning, TEXT("The Steam Audio plugin is deprecated and will be removed in a future engine release. See the Plugins Browser for more information."))
-
-		// Make folder paths globally available
-		BasePath = FPaths::ProjectContentDir() + "SteamAudio/";
-		RuntimePath = BasePath + "Runtime/";
-		DynamicRuntimePath = RuntimePath + "Dynamic/";
-		EditorOnlyPath = BasePath + "EditorOnly/";
-		DynamicEditorOnlyPath = EditorOnlyPath + "Dynamic/";
-
-		//Register the Steam Audio plugin factories
-		IModularFeatures::Get().RegisterModularFeature(FSpatializationPluginFactory::GetModularFeatureName(), &SpatializationPluginFactory);
-		IModularFeatures::Get().RegisterModularFeature(FReverbPluginFactory::GetModularFeatureName(), &ReverbPluginFactory);
-		IModularFeatures::Get().RegisterModularFeature(FOcclusionPluginFactory::GetModularFeatureName(), &OcclusionPluginFactory);
-
-#if PLATFORM_WINDOWS
-		if (!FSteamAudioModule::PhononDllHandle)
-		{
-#if PLATFORM_32BITS
-			FString PathToDll = FPaths::EngineDir() / TEXT("Binaries/ThirdParty/Phonon/Win32/");
-#else
-			FString PathToDll = FPaths::EngineDir() / TEXT("Binaries/ThirdParty/Phonon/Win64/");
-#endif
-
-			FString DLLToLoad = PathToDll + TEXT("phonon.dll");
-			FSteamAudioModule::PhononDllHandle = LoadDll(DLLToLoad, true);
-		}
-#endif
-		iplCreateContext(UnrealLog, nullptr, nullptr, &GlobalContext);
+		UE_LOG(LogSteamAudio, Warning, TEXT("The Steam Audio plugin is deprecated and will be removed in a future engine release. Get Valve's version here: https://valvesoftware.github.io/steam-audio/doc/unreal/getting-started.html"))
 	}
 
 	void FSteamAudioModule::ShutdownModule()
@@ -68,80 +40,6 @@ namespace SteamAudio
 		check(bModuleStartedUp == true);
 
 		bModuleStartedUp = false;
-
-		iplDestroyContext(&GlobalContext);
-		iplCleanup();
-
-#if PLATFORM_WINDOWS
-		if (FSteamAudioModule::PhononDllHandle)
-		{
-			FPlatformProcess::FreeDllHandle(FSteamAudioModule::PhononDllHandle);
-			FSteamAudioModule::PhononDllHandle = nullptr;
-		}
-#endif
-	}
-
-	IAudioPluginFactory* FSteamAudioModule::GetPluginFactory(EAudioPlugin PluginType)
-	{
-		switch (PluginType)
-		{
-		case EAudioPlugin::SPATIALIZATION:
-			return &SpatializationPluginFactory;
-			break;
-		case EAudioPlugin::REVERB:
-			return &ReverbPluginFactory;
-			break;
-		case EAudioPlugin::OCCLUSION:
-			return &OcclusionPluginFactory;
-			break;
-		default:
-			return nullptr;
-			break;
-		}
-	}
-
-	void FSteamAudioModule::RegisterAudioDevice(FAudioDevice* AudioDeviceHandle)
-	{
-		if (!RegisteredAudioDevices.Contains(AudioDeviceHandle))
-		{
-			TAudioPluginListenerPtr NewPhononPluginManager = TAudioPluginListenerPtr(new FPhononPluginManager());
-			AudioDeviceHandle->RegisterPluginListener(NewPhononPluginManager);
-			RegisteredAudioDevices.Add(AudioDeviceHandle);
-		}
-	}
-
-	void FSteamAudioModule::UnregisterAudioDevice(FAudioDevice* AudioDeviceHandle)
-	{
-		RegisteredAudioDevices.Remove(AudioDeviceHandle);
-	}
-
-	TAudioOcclusionPtr FOcclusionPluginFactory::CreateNewOcclusionPlugin(FAudioDevice* OwningDevice)
-	{
-		//Register audio device to the Steam Module
-		FSteamAudioModule* Module = &FModuleManager::GetModuleChecked<FSteamAudioModule>("SteamAudio");
-		if (Module != nullptr)
-		{
-			Module->RegisterAudioDevice(OwningDevice);
-		}
-		
-		return TAudioOcclusionPtr(new FPhononOcclusion());
-	}
-
-	TAudioReverbPtr FReverbPluginFactory::CreateNewReverbPlugin(FAudioDevice* OwningDevice)
-	{
-		//Register the audio device to the steam module:
-		FSteamAudioModule* Module = &FModuleManager::GetModuleChecked<FSteamAudioModule>("SteamAudio");
-		if (Module != nullptr)
-		{
-			Module->RegisterAudioDevice(OwningDevice);
-		}
-
-		return TAudioReverbPtr(new FPhononReverb());
-	}
-
-	TAudioSpatializationPtr FSpatializationPluginFactory::CreateNewSpatializationPlugin(FAudioDevice* OwningDevice)
-	{
-		return TAudioSpatializationPtr(new FPhononSpatialization());
 	}
 
 }
