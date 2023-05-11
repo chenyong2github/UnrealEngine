3fc91d2947a466647a238615013df1191701afd4
chris caulfield <Chris.Caulfield@epicgames.com>
2023-05-10 20:28:34 -0400

    Disable static analysis for IslandManagerFwd.h (error: invalid forward declaration - 'namespace Chaos')
    #rb none
    #preflight none
    
    [CL 25420167 by chris caulfield in ue5-main branch]

--- Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
+++ Engine/Source/Programs/IncludeTool/IncludeTool/Rules.cs
@@ -438,6 +438,7 @@ namespace IncludeTool
 			"/Engine/Source/Runtime/Core/Public/Internationalization/StringTableCoreFwd.h", // Typedef isn't a forward declaration
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/GeometryParticlesfwd.h", // invalid forward declaration - 'namespace Chaos'
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ImplicitFwd.h", // invalid forward declaration - 'namespace Chaos'
+			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/Island/IslandManagerFwd.h", // invalid forward declaration - 'namespace Chaos'
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ParticleHandleFwd.h", // invalid forward declaration - 'namespace Chaos'
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDRigidsEvolutionFwd.h", // invalid forward declaration - 'namespace Chaos'
 			"/Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/PBDSoftsEvolutionFwd.h", // invalid forward declaration - 'namespace Chaos'
