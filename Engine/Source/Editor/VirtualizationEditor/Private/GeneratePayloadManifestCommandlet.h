54b51c99e3b03625d54a8a48c9d3b32a4afa4f60
paul chipchase <paul.chipchase@epicgames.com>
2023-05-15 04:03:12 -0400

    Fix some typos in code documentation
    #rb trivial
    #jira none
    #rnx
    #preflight 6461dc5ab87b09aadddc17c8
    
    [CL 25470354 by paul chipchase in ue5-main branch]

--- Engine/Source/Editor/VirtualizationEditor/Private/GeneratePayloadManifestCommandlet.h
+++ Engine/Source/Editor/VirtualizationEditor/Private/GeneratePayloadManifestCommandlet.h
@@ -10,10 +10,10 @@
 /**
  * Creates a csv file containing info about all of the payloads in a set of packages.
  * By default the commandlet will parse the payloads of all packages in the current
- * project but this can be overriden with the cmdline switch -PackageDir=XYZ which
+ * project but this can be overridden with the cmdline switch -PackageDir=XYZ which
  * will allow the commandlet to parse the payloads of the packages in a given directory.
  * 
- * Because the commmandlet is the VirtualizationEditor module it needs to be invoked 
+ * Because the commandlet is the VirtualizationEditor module it needs to be invoked 
  * with the command line:
  * -run="VirtualizationEditor.GeneratePayloadManifest"
  */
