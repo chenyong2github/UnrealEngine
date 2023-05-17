01d7ca0c151f82553370d8859b8a72fb522bf347
Thomas Engel <Thomas.Engel@epicgames.com>
2023-05-17 16:04:39 -0400

    Adding NV12 CPU-side-buffer support for Apple platforms to Electra #fyi jens.petersam #jira none #rnx #preflight 64652dce6b1406b54a671250
    [CL 25515124 by Thomas Engel in ue5-main branch]

--- Engine/Plugins/Media/ElectraUtil/Source/ElectraSamples/Private/ElectraTextureSampleAVF.cpp
+++ Engine/Plugins/Media/ElectraUtil/Source/ElectraSamples/Private/ElectraTextureSampleAVF.cpp
@@ -396,7 +396,8 @@ EMediaTextureSampleFormat FElectraTextureSample::GetFormat() const
             break;
         }
         case    EPixelFormat::PF_BC4:   return EMediaTextureSampleFormat::BC4;
-        default:
+		case    EPixelFormat::PF_NV12:  return EMediaTextureSampleFormat::CharNV21;
+		default:
         {
             check(!"Unsupported pixel format");
         }
