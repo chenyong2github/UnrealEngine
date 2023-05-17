167c9f038864a739bf4ce18fdb28d6f71ba80482
vincent robert <vincent.robert@epicgames.com>
2023-05-17 09:33:15 -0400

    Fix compilation error UnrealEditor NonUnity Win64 #preflight None
    [CL 25506040 by vincent robert in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/PhysicsEngine/Experimental/ChaosBlueprintLibrary.cpp
+++ Engine/Source/Runtime/Engine/Private/PhysicsEngine/Experimental/ChaosBlueprintLibrary.cpp
@@ -1,10 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #include "PhysicsEngine/ChaosBlueprintLibrary.h"
+#include "Engine/Engine.h"
 #include "Physics/Experimental/ChaosEventRelay.h"
 
 const UChaosEventRelay* UChaosBlueprintLibrary::GetEventRelayFromContext(UObject* ContextObject)
 {
-	if (ContextObject)
+	if (GEngine && ContextObject)
 	{
 		UWorld* World = GEngine->GetWorldFromContextObject(ContextObject, EGetWorldErrorMode::ReturnNull);
 		if (World)
