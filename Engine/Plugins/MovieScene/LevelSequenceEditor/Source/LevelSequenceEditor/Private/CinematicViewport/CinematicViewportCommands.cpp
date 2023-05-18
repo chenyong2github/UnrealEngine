4b249045b637dd0466eca98baf30192df8d4b0eb
max chen <Max.Chen@epicgames.com>
2023-05-18 17:56:36 -0400

    Cinematic Viewport: Change to Ctrl + Num pad shortcuts rather than Ctrl + Shift + Num pad
    #jira UE-186271
    #rb none
    #preflight 6466830b9c4cf249515ac728
    
    [CL 25534836 by max chen in ue5-main branch]

--- Engine/Plugins/MovieScene/LevelSequenceEditor/Source/LevelSequenceEditor/Private/CinematicViewport/CinematicViewportCommands.cpp
+++ Engine/Plugins/MovieScene/LevelSequenceEditor/Source/LevelSequenceEditor/Private/CinematicViewport/CinematicViewportCommands.cpp
@@ -11,15 +11,15 @@
 
 void FCinematicViewportCommands::RegisterCommands()
 {
-	UI_COMMAND( Disabled, "Disabled", "Disable the composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control | EModifierKey::Shift, EKeys::NumPadZero) );
-	UI_COMMAND( Grid3x3, "Grid 3x3", "Enable the grid (3x3) composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadSix) );
-	UI_COMMAND( Grid2x2, "Grid 2x2", "Enable the grid (2x2) composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadSeven) );
-	UI_COMMAND( Crosshair, "Crosshair", "Enable the crosshair composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadEight) );
-	UI_COMMAND( Rabatment, "Rabatment", "Enable the rabatment composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadNine));
-	UI_COMMAND( ActionSafe, "Action Safe", "Enable the action safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadOne));
-	UI_COMMAND( TitleSafe, "Title Safe", "Enable the title safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadTwo));
-	UI_COMMAND( CustomSafe, "Custom Safe", "Enable the custom safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadThree));
-	UI_COMMAND( Letterbox, "Letterbox", "Enable the letterbox composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Shift | EModifierKey::Control, EKeys::NumPadFour));
+	UI_COMMAND( Disabled, "Disabled", "Disable the composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadZero) );
+	UI_COMMAND( Grid3x3, "Grid 3x3", "Enable the grid (3x3) composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadSix) );
+	UI_COMMAND( Grid2x2, "Grid 2x2", "Enable the grid (2x2) composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadSeven) );
+	UI_COMMAND( Crosshair, "Crosshair", "Enable the crosshair composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadEight) );
+	UI_COMMAND( Rabatment, "Rabatment", "Enable the rabatment composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadNine));
+	UI_COMMAND( ActionSafe, "Action Safe", "Enable the action safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadOne));
+	UI_COMMAND( TitleSafe, "Title Safe", "Enable the title safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadTwo));
+	UI_COMMAND( CustomSafe, "Custom Safe", "Enable the custom safe composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadThree));
+	UI_COMMAND( Letterbox, "Letterbox", "Enable the letterbox composition overlay", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::NumPadFour));
 }
 
 #undef LOCTEXT_NAMESPACE
