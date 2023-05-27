fba8d7356600d92ad4112774c5ec97cec3875603
Ben Marsh <Ben.Marsh@epicgames.com>
2023-05-27 10:28:27 -0400

    Horde: Reorganizing project.
    #preflight none
    
    [CL 25659196 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde/Commands/LoginCommand.cs
+++ Engine/Source/Programs/Horde/Horde/Commands/LoginCommand.cs
@@ -1,14 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EpicGames.Core;
 using Microsoft.Extensions.Logging;
 
-namespace Horde.Commands.Login
+namespace Horde.Commands
 {
 	[Command("login", "Logs in to a Horde server")]
 	class LoginCommand : Command
