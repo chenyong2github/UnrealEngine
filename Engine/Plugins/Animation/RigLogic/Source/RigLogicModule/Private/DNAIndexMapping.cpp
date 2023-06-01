8edd0f520792b7333ee56a273ac50342390dc0f8
andrean franc <andrean.franc@3lateral.com>
2023-06-01 07:27:17 -0400

    Forward neural network mask curves to RigLogic from both anim node and control rig implementations #preflight 6478762f0848b7126d232382
    [CL 25723328 by andrean franc in ue5-main branch]

--- Engine/Plugins/Animation/RigLogic/Source/RigLogicModule/Private/DNAIndexMapping.cpp
+++ Engine/Plugins/Animation/RigLogic/Source/RigLogicModule/Private/DNAIndexMapping.cpp
@@ -31,18 +31,18 @@ static FString CreateCurveName(const FString& NameToSplit, const FString& Format
 	return CurveName;
 }
 
-void FDNAIndexMapping::MapControlCurves(const IBehaviorReader* DNABehavior, const USkeleton* Skeleton)
+void FDNAIndexMapping::MapControlCurves(const IDNAReader* DNAReader, const USkeleton* Skeleton)
 {
 	LLM_SCOPE_BYNAME(TEXT("Animation/RigLogic"));
 
-	const uint32 ControlCount = DNABehavior->GetRawControlCount();
+	const uint32 ControlCount = DNAReader->GetRawControlCount();
 
 	ControlAttributeCurves.Empty();
 	ControlAttributeCurves.Reserve(ControlCount);
 
 	for (uint32_t ControlIndex = 0; ControlIndex < ControlCount; ++ControlIndex)
 	{
-		const FString DNAControlName = DNABehavior->GetRawControlName(ControlIndex);
+		const FString DNAControlName = DNAReader->GetRawControlName(ControlIndex);
 		const FString AnimatedControlName = CreateCurveName(DNAControlName, TEXT("<obj>_<attr>"));
 		if (AnimatedControlName == TEXT(""))
 		{
@@ -52,15 +52,42 @@ void FDNAIndexMapping::MapControlCurves(const IBehaviorReader* DNABehavior, cons
 	}
 }
 
-void FDNAIndexMapping::MapJoints(const IBehaviorReader* DNABehavior, const USkeletalMeshComponent* SkeletalMeshComponent)
+void FDNAIndexMapping::MapNeuralNetworkMaskCurves(const IDNAReader* DNAReader, const USkeleton* Skeleton)
 {
 	LLM_SCOPE_BYNAME(TEXT("Animation/RigLogic"));
 
-	const uint16 JointCount = DNABehavior->GetJointCount();
+	const uint16 NeuralNetworkCount = DNAReader->GetNeuralNetworkCount();
+
+	NeuralNetworkMaskCurves.Empty();
+	NeuralNetworkMaskCurves.Reserve(NeuralNetworkCount);
+
+	const uint16 MeshCount = DNAReader->GetMeshCount();
+	for (uint16 MeshIndex = 0; MeshIndex < MeshCount; ++MeshIndex)
+	{
+		const uint16 MeshRegionCount = DNAReader->GetMeshRegionCount(MeshIndex);
+		for (uint16 RegionIndex = 0; RegionIndex < MeshRegionCount; ++RegionIndex)
+		{
+			const FString& MeshRegionName = DNAReader->GetMeshRegionName(MeshIndex, RegionIndex);
+			TArrayView<const uint16> NeuralNetworkIndices = DNAReader->GetNeuralNetworkIndicesForMeshRegion(MeshIndex, RegionIndex);
+			const FString MaskCurveName = TEXT("CTRL_ML_") + MeshRegionName;
+			for (const auto NeuralNetworkIndex : NeuralNetworkIndices)
+			{
+				NeuralNetworkMaskCurves.Add(*MaskCurveName, NeuralNetworkIndex);
+			}
+		}
+	}
+}
+
+
+void FDNAIndexMapping::MapJoints(const IDNAReader* DNAReader, const USkeletalMeshComponent* SkeletalMeshComponent)
+{
+	LLM_SCOPE_BYNAME(TEXT("Animation/RigLogic"));
+
+	const uint16 JointCount = DNAReader->GetJointCount();
 	JointsMapDNAIndicesToMeshPoseBoneIndices.Reset(JointCount);
 	for (uint16 JointIndex = 0; JointIndex < JointCount; ++JointIndex)
 	{
-		const FString JointName = DNABehavior->GetJointName(JointIndex);
+		const FString JointName = DNAReader->GetJointName(JointIndex);
 		const FName BoneName = FName(*JointName);
 		const int32 BoneIndex = SkeletalMeshComponent->GetBoneIndex(BoneName);
 		// BoneIndex may be INDEX_NONE, but it's handled properly by the Evaluate method
@@ -68,11 +95,11 @@ void FDNAIndexMapping::MapJoints(const IBehaviorReader* DNABehavior, const USkel
 	}
 }
 
-void FDNAIndexMapping::MapMorphTargets(const IBehaviorReader* DNABehavior, const USkeleton* Skeleton, const USkeletalMesh* SkeletalMesh)
+void FDNAIndexMapping::MapMorphTargets(const IDNAReader* DNAReader, const USkeleton* Skeleton, const USkeletalMesh* SkeletalMesh)
 {
 	LLM_SCOPE_BYNAME(TEXT("Animation/RigLogic"));
 
-	const uint16 LODCount = DNABehavior->GetLODCount();
+	const uint16 LODCount = DNAReader->GetLODCount();
 	const TMap<FName, int32>& MorphTargetIndexMap = SkeletalMesh->GetMorphTargetIndexMap();
 	const TArray<UMorphTarget*>& MorphTargets = SkeletalMesh->GetMorphTargets();
 
@@ -81,15 +108,15 @@ void FDNAIndexMapping::MapMorphTargets(const IBehaviorReader* DNABehavior, const
 
 	for (uint16 LODIndex = 0; LODIndex < LODCount; ++LODIndex)
 	{
-		TArrayView<const uint16> MappingIndicesForLOD = DNABehavior->GetMeshBlendShapeChannelMappingIndicesForLOD(LODIndex);
+		TArrayView<const uint16> MappingIndicesForLOD = DNAReader->GetMeshBlendShapeChannelMappingIndicesForLOD(LODIndex);
 
 		MorphTargetCurvesPerLOD[LODIndex].Reserve(MappingIndicesForLOD.Num());
 
 		for (uint16 MappingIndex : MappingIndicesForLOD)
 		{
-			const FMeshBlendShapeChannelMapping Mapping = DNABehavior->GetMeshBlendShapeChannelMapping(MappingIndex);
-			const FString MeshName = DNABehavior->GetMeshName(Mapping.MeshIndex);
-			const FString BlendShapeName = DNABehavior->GetBlendShapeChannelName(Mapping.BlendShapeChannelIndex);
+			const FMeshBlendShapeChannelMapping Mapping = DNAReader->GetMeshBlendShapeChannelMapping(MappingIndex);
+			const FString MeshName = DNAReader->GetMeshName(Mapping.MeshIndex);
+			const FString BlendShapeName = DNAReader->GetBlendShapeChannelName(Mapping.BlendShapeChannelIndex);
 			const FString MorphTargetStr = MeshName + TEXT("__") + BlendShapeName;
 			const FName MorphTargetName(*MorphTargetStr);
 			const int32* MorphTargetIndex = MorphTargetIndexMap.Find(MorphTargetName);
@@ -102,24 +129,24 @@ void FDNAIndexMapping::MapMorphTargets(const IBehaviorReader* DNABehavior, const
 	}
 }
 
-void FDNAIndexMapping::MapMaskMultipliers(const IBehaviorReader* DNABehavior, const USkeleton* Skeleton)
+void FDNAIndexMapping::MapMaskMultipliers(const IDNAReader* DNAReader, const USkeleton* Skeleton)
 {
 	LLM_SCOPE_BYNAME(TEXT("Animation/RigLogic"));
 
-	const uint16 LODCount = DNABehavior->GetLODCount();
+	const uint16 LODCount = DNAReader->GetLODCount();
 
 	MaskMultiplierCurvesPerLOD.Reset();
 	MaskMultiplierCurvesPerLOD.AddDefaulted(LODCount);
 
 	for (uint16 LODIndex = 0; LODIndex < LODCount; ++LODIndex)
 	{
-		TArrayView<const uint16> IndicesPerLOD = DNABehavior->GetAnimatedMapIndicesForLOD(LODIndex);
+		TArrayView<const uint16> IndicesPerLOD = DNAReader->GetAnimatedMapIndicesForLOD(LODIndex);
 
 		MaskMultiplierCurvesPerLOD[LODIndex].Reserve(IndicesPerLOD.Num());
 
 		for (uint16 AnimMapIndex : IndicesPerLOD)
 		{
-			const FString AnimatedMapName = DNABehavior->GetAnimatedMapName(AnimMapIndex);
+			const FString AnimatedMapName = DNAReader->GetAnimatedMapName(AnimMapIndex);
 			const FString MaskMultiplierNameStr = CreateCurveName(AnimatedMapName, TEXT("<obj>_<attr>"));
 			if (MaskMultiplierNameStr == "")
 			{
