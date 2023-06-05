0d75f73521c868aad81bab04747dbf2d6978d335
charles bloom <charles.bloom@epicgames.com>
2023-06-05 14:21:22 -0400

    ImageCore.build.cs clarifying comment
    #rb none
    #rnx
    
    [CL 25798720 by charles bloom in ue5-main branch]

--- Engine/Source/Runtime/ImageCore/ImageCore.Build.cs
+++ Engine/Source/Runtime/ImageCore/ImageCore.Build.cs
@@ -7,7 +7,9 @@ public class ImageCore : ModuleRules
 	public ImageCore(ReadOnlyTargetRules Target) : base(Target)
 	{
 		// include only, no link :
-		// for TextureDefines.h :
+		// ImageCoreUtils.h includes TextureDefines.h from Engine, so technically it should be a "PublicIncludePathModuleNames" include,
+		// but that breaks the build because there are other modules that use ImageCore that have conflicts with Engine headers.
+		// So, leave it as PrivateIncludePathModuleNames, but then any module that uses ImageCoreUtils needs to PrivateIncludePathModuleNames Engine.
 		// PublicIncludePathModuleNames
 		PrivateIncludePathModuleNames.AddRange(new string[]
 		{
