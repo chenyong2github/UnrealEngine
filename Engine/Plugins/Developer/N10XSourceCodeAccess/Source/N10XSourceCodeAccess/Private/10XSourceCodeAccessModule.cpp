08bfda7e7c964f02a1f659f4a5cc9a2c0282f7b4
christian savoie <christian.savoie@epicgames.com>
2023-06-07 22:56:54 -0400

    Add Source Code Access plugin for the 10X editor * Had to use N10X in some places due to code restrictions * Using Hardcoded path for now
    [REVIEW] [at]marc.audy
    #tests Double clicked nodes to have source code opened properly.
    #pf 6481108de8609a90465e556d
    
    [CL 25864857 by christian savoie in ue5-main branch]

---
+++ Engine/Plugins/Developer/N10XSourceCodeAccess/Source/N10XSourceCodeAccess/Private/10XSourceCodeAccessModule.cpp
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "10XSourceCodeAccessModule.h"
+#include "Features/IModularFeatures.h"
+#include "Modules/ModuleManager.h"
+#include "HAL/LowLevelMemTracker.h"
+#include "10XSourceCodeAccessor.h"
+
+LLM_DEFINE_TAG(10XSourceCodeAccess);
+
+IMPLEMENT_MODULE( F10XSourceCodeAccessModule, 10XSourceCodeAccess );
+
+F10XSourceCodeAccessModule::F10XSourceCodeAccessModule()
+	: SourceCodeAccessor(MakeShareable(new F10XSourceCodeAccessor()))
+{
+}
+
+void F10XSourceCodeAccessModule::StartupModule()
+{
+	LLM_SCOPE_BYTAG(10XSourceCodeAccess);
+
+	SourceCodeAccessor->Startup();
+
+	IModularFeatures::Get().RegisterModularFeature(TEXT("SourceCodeAccessor"), &SourceCodeAccessor.Get() );
+}
+
+void F10XSourceCodeAccessModule::ShutdownModule()
+{
+	IModularFeatures::Get().UnregisterModularFeature(TEXT("SourceCodeAccessor"), &SourceCodeAccessor.Get());
+
+	SourceCodeAccessor->Shutdown();
+}
+
+F10XSourceCodeAccessor& F10XSourceCodeAccessModule::GetAccessor()
+{
+	return SourceCodeAccessor.Get();
+}
