39c1a81ede4a837c2d3e6eb9f9e27a811bf7fc64
matt peters <Matt.Peters@epicgames.com>
2023-06-16 02:42:11 -0400

    ExternalPackages: Add more feedback for failed ensure(ExternalPackage) in GetExternalPackage. #rnx #rb Francis.Hurteau
    [CL 26038537 by matt peters in 5.3 branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectBase.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectBase.cpp
@@ -257,7 +257,8 @@ UPackage* UObjectBase::GetExternalPackage() const
 	{
 		ExternalPackage = GetObjectExternalPackageThreadSafe(this);
 		// if the flag is set there should be an override set.
-		ensure(ExternalPackage);
+		ensureMsgf(ExternalPackage, TEXT("Object %s ExternalPackage is invalid: RF_ExternalPackage is set, but ExternalPackage is nullptr."),
+			*static_cast<const UObjectBaseUtility*>(this)->GetPathName());
 	}
 	return ExternalPackage;
 }
