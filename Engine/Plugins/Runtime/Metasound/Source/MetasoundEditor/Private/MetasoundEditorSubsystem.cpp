7eb840abc4a255954e8d98f8ccbd61d8c3421869
helen yang <helen.yang@epicgames.com>
2023-06-16 03:23:15 -0400

    Nonunity fix for copying over Sound Wave properties for MetaSound presets
    #rb trivial
    
    #rnx
    
    [CL 26039445 by helen yang in 5.3 branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundEditorSubsystem.cpp
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundEditorSubsystem.cpp
@@ -10,6 +10,8 @@
 #include "MetasoundEditorSettings.h"
 #include "MetasoundFactory.h"
 #include "MetasoundUObjectRegistry.h"
+#include "Sound/SoundSourceBusSend.h"
+#include "Sound/SoundSubmixSend.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(MetasoundEditorSubsystem)
 
