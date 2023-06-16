02c2f74dd87806a31d5c4d33a510acec88a3d60c
valentin ritzi <Valentin.Ritzi@epicgames.com>
2023-06-16 12:35:43 -0400

    [QuicMessaging] Moving QuicMessaging to experimental
    Moving QuicMessaging to the experimental plugins folder.
    
    #rb stuart.hill, federico.lopez
    
    [CL 26057208 by valentin ritzi in 5.3 branch]

---
+++ Engine/Plugins/Experimental/QuicMessaging/Source/QuicMessaging/QuicMessaging.Build.cs
@@ -0,0 +1,71 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+using System.IO;
+
+namespace UnrealBuildTool.Rules
+{
+	public class QuicMessaging : ModuleRules
+	{
+		public QuicMessaging(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[] {
+					"Core",
+					"QuicMessagingTransport"
+				}
+			);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
+					"CoreUObject",
+					"Json",
+					"JsonUtilities",
+					"Cbor",
+					"Messaging",
+					"Networking",
+					"Serialization",
+					"TraceLog",
+					"Sockets",
+					"Projects",
+					"MsQuicRuntime"
+				}
+			);
+			
+			PrivateIncludePathModuleNames.AddRange(
+				new string[] {
+					"MessagingCommon",
+					"MsQuicRuntime"
+				}
+			);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					"QuicMessaging/Private",
+					"QuicMessaging/Private/Transport",
+					"QuicMessaging/Private/Serialization",
+					"QuicMessaging/Public/Shared",
+					"QuicMessaging/Public",
+				}
+			);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[] {
+				}
+			);
+
+			if (Target.Type == TargetType.Editor) {
+
+				DynamicallyLoadedModuleNames.AddRange(
+					new string[] {
+						"Settings",
+					}
+				);
+
+				PrivateIncludePathModuleNames.AddRange(
+					new string[] {
+						"Settings",
+					}
+				);
+			}
+		}
+	}
+}
