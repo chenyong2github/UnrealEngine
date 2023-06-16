bc52b17ec26a4278d8dd39b66018c4fee20c865a
marc audy <Marc.Audy@epicgames.com>
2023-06-16 11:19:49 -0400

    Remove #pragma once in cpp #rnx #jira
    [CL 26053205 by marc audy in 5.3 branch]

--- Engine/Source/Editor/GraphEditor/Private/MaterialNodes/SGraphSubstrateMaterial.cpp
+++ Engine/Source/Editor/GraphEditor/Private/MaterialNodes/SGraphSubstrateMaterial.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "SGraphSubstrateMaterial.h"
 #include "Internationalization/Text.h"
 #include "Rendering/RenderingCommon.h"
