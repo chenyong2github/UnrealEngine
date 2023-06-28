493cf6d60a50efa60a5dc5815a52bec33e3bf782
helge mathee <Helge.Mathee@epicgames.com>
2023-06-28 10:58:47 -0400

    RigVM: Move compiler context to RigVMDeveloper
    #rb sara.schvartzman
    #jira UE-180111
    
    [CL 26285387 by helge mathee in 5.3 branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprintCompiler.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprintCompiler.cpp
@@ -2,20 +2,7 @@
 
 #include "ControlRigBlueprintCompiler.h"
 #include "ControlRig.h"
-#include "KismetCompiler.h"
 #include "ControlRigBlueprint.h"
-#include "Units/RigUnit.h"
-#include "Units/Execution/RigUnit_BeginExecution.h"
-#include "Graph/ControlRigGraph.h"
-#include "Graph/ControlRigGraphNode.h"
-#include "Graph/ControlRigGraphSchema.h"
-#include "RigVMBlueprintGeneratedClass.h"
-#include "Kismet2/KismetReinstanceUtilities.h"
-#include "Widgets/Notifications/SNotificationList.h"
-#include "Framework/Notifications/NotificationManager.h"
-
-DEFINE_LOG_CATEGORY_STATIC(LogControlRigCompiler, Log, All);
-#define LOCTEXT_NAMESPACE "ControlRigBlueprintCompiler"
 
 bool FControlRigBlueprintCompiler::CanCompile(const UBlueprint* Blueprint)
 {
@@ -37,55 +24,11 @@ void FControlRigBlueprintCompiler::Compile(UBlueprint* Blueprint, const FKismetC
 	Compiler.Compile();
 }
 
-void FControlRigBlueprintCompilerContext::MarkCompilationFailed(const FString& Message)
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	UControlRigBlueprint* ControlRigBlueprint = Cast<UControlRigBlueprint>(Blueprint);
-	if (ControlRigBlueprint)
-	{
-		Blueprint->Status = BS_Error;
-		Blueprint->MarkPackageDirty();
-		UE_LOG(LogControlRigCompiler, Error, TEXT("%s"), *Message);
-		MessageLog.Error(*Message);
-
-#if WITH_EDITOR
-		FNotificationInfo Info(FText::FromString(Message));
-		Info.Image = FCoreStyle::Get().GetBrush(TEXT("MessageLog.Error"));
-		Info.bFireAndForget = true;
-		Info.FadeOutDuration = 5.0f;
-		Info.ExpireDuration = 5.0f;
-		TSharedPtr<SNotificationItem> NotificationPtr = FSlateNotificationManager::Get().AddNotification(Info);
-		NotificationPtr->SetCompletionState(SNotificationItem::CS_Success);
-#endif
-	}
-}
-
-void FControlRigBlueprintCompilerContext::PostCompile()
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	UControlRigBlueprint* ControlRigBlueprint = Cast<UControlRigBlueprint>(Blueprint);
-	if (ControlRigBlueprint)
-	{
-		if (!ControlRigBlueprint->bIsRegeneratingOnLoad)
-		{
-			// todo: can we find out somehow if we are cooking?
-			ControlRigBlueprint->RecompileVM();
-		}
-	}
-
-	{
-		DECLARE_SCOPE_HIERARCHICAL_COUNTER(FKismetCompilerContext::PostCompile)
-		FKismetCompilerContext::PostCompile();
-	}
-}
-
 void FControlRigBlueprintCompilerContext::CopyTermDefaultsToDefaultObject(UObject* DefaultObject)
 {
 	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
 
-	FKismetCompilerContext::CopyTermDefaultsToDefaultObject(DefaultObject);
+	FRigVMBlueprintCompilerContext::CopyTermDefaultsToDefaultObject(DefaultObject);
 
 	UControlRigBlueprint* ControlRigBlueprint = Cast<UControlRigBlueprint>(Blueprint);
 	if (ControlRigBlueprint)
@@ -94,7 +37,6 @@ void FControlRigBlueprintCompilerContext::CopyTermDefaultsToDefaultObject(UObjec
 		// and in UControlRig::InitializeFromCDO,
 		// other Control Rig Instances are then initialized From the CDO 
 		UControlRig* ControlRig = CastChecked<UControlRig>(DefaultObject);
-		ControlRig->PostInitInstanceIfRequired();
 		
 		// copy hierarchy
 		{
@@ -118,60 +60,3 @@ void FControlRigBlueprintCompilerContext::CopyTermDefaultsToDefaultObject(UObjec
 		ControlRig->Influences = ControlRigBlueprint->Influences;
 	}
 }
-
-void FControlRigBlueprintCompilerContext::EnsureProperGeneratedClass(UClass*& TargetUClass)
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	if( TargetUClass && !((UObject*)TargetUClass)->IsA(URigVMBlueprintGeneratedClass::StaticClass()) )
-	{
-		FKismetCompilerUtilities::ConsignToOblivion(TargetUClass, Blueprint->bIsRegeneratingOnLoad);
-		TargetUClass = nullptr;
-	}
-}
-
-void FControlRigBlueprintCompilerContext::SpawnNewClass(const FString& NewClassName)
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	NewControlRigBlueprintGeneratedClass = FindObject<URigVMBlueprintGeneratedClass>(Blueprint->GetOutermost(), *NewClassName);
-
-	if (NewControlRigBlueprintGeneratedClass == nullptr)
-	{
-		NewControlRigBlueprintGeneratedClass = NewObject<URigVMBlueprintGeneratedClass>(Blueprint->GetOutermost(), FName(*NewClassName), RF_Public | RF_Transactional);
-	}
-	else
-	{
-		// Already existed, but wasn't linked in the Blueprint yet due to load ordering issues
-		FBlueprintCompileReinstancer::Create(NewControlRigBlueprintGeneratedClass);
-	}
-	NewClass = NewControlRigBlueprintGeneratedClass;
-}
-
-void FControlRigBlueprintCompilerContext::OnNewClassSet(UBlueprintGeneratedClass* ClassToUse)
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	NewControlRigBlueprintGeneratedClass = CastChecked<URigVMBlueprintGeneratedClass>(ClassToUse);
-}
-
-void FControlRigBlueprintCompilerContext::CleanAndSanitizeClass(UBlueprintGeneratedClass* ClassToClean, UObject*& InOldCDO)
-{
-	DECLARE_SCOPE_HIERARCHICAL_COUNTER_FUNC()
-
-	FKismetCompilerContext::CleanAndSanitizeClass(ClassToClean, InOldCDO);
-
-	// Make sure our typed pointer is set
-	check(ClassToClean == NewClass && NewControlRigBlueprintGeneratedClass == NewClass);
-}
-
-void FControlRigBlueprintCompilerContext::PreCompileUpdateBlueprintOnLoad(UBlueprint* BP)
-{
-	if (UControlRigBlueprint* RigBlueprint = Cast<UControlRigBlueprint>(BP))
-	{
-		RigBlueprint->CreateMemberVariablesOnLoad();
-	}
-}
-
-
-#undef LOCTEXT_NAMESPACE
--- Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Public/ControlRigBlueprintCompiler.h
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Public/ControlRigBlueprintCompiler.h
@@ -2,15 +2,9 @@
 
 #pragma once
 
-#include "KismetCompilerModule.h"
-#include "KismetCompiler.h"
+#include "RigVMBlueprintCompiler.h"
 
-class UControlRigGraph;
-class UControlRigGraphNode;
-class UControlRigBlueprint;
-class URigVMBlueprintGeneratedClass;
-
-class CONTROLRIGDEVELOPER_API FControlRigBlueprintCompiler : public IBlueprintCompiler
+class CONTROLRIGDEVELOPER_API FControlRigBlueprintCompiler : public FRigVMBlueprintCompiler
 {
 public:
 	/** IBlueprintCompiler interface */
@@ -18,40 +12,14 @@ public:
 	virtual void Compile(UBlueprint* Blueprint, const FKismetCompilerOptions& CompileOptions, FCompilerResultsLog& Results) override;
 };
 
-class CONTROLRIGDEVELOPER_API FControlRigBlueprintCompilerContext : public FKismetCompilerContext
+class CONTROLRIGDEVELOPER_API FControlRigBlueprintCompilerContext : public FRigVMBlueprintCompilerContext
 {
 public:
 	FControlRigBlueprintCompilerContext(UBlueprint* SourceSketch, FCompilerResultsLog& InMessageLog, const FKismetCompilerOptions& InCompilerOptions)
-		: FKismetCompilerContext(SourceSketch, InMessageLog, InCompilerOptions)
-		, NewControlRigBlueprintGeneratedClass(nullptr)
+		: FRigVMBlueprintCompilerContext(SourceSketch, InMessageLog, InCompilerOptions)
 	{
 	}
 
 	// FKismetCompilerContext interface
-	virtual void ValidateLink(const UEdGraphPin* PinA, const UEdGraphPin* PinB) const override {}
-	virtual void ValidatePin(const UEdGraphPin* Pin) const override {}
-	virtual void ValidateNode(const UEdGraphNode* Node) const override {}
-	virtual bool CanIgnoreNode(const UEdGraphNode* Node) const override { return true; }
-	virtual bool ShouldForceKeepNode(const UEdGraphNode* Node) const override { return false; }
-	virtual void PrecompileFunction(FKismetFunctionContext& Context, EInternalCompilerFlags InternalFlags) override {}
-	virtual void PostCompile() override;
 	virtual void CopyTermDefaultsToDefaultObject(UObject* DefaultObject) override;
-	virtual void EnsureProperGeneratedClass(UClass*& TargetUClass) override;
-	virtual void SpawnNewClass(const FString& NewClassName) override;
-	virtual void OnNewClassSet(UBlueprintGeneratedClass* ClassToUse) override;
-	virtual void PruneIsolatedNodes(const TArray<UEdGraphNode*>& RootSet, TArray<UEdGraphNode*>& GraphNodes) override {}
-	virtual void CleanAndSanitizeClass(UBlueprintGeneratedClass* ClassToClean, UObject*& InOldCDO) override;
-	virtual void MergeUbergraphPagesIn(UEdGraph* Ubergraph) override {};
-	virtual void PreCompileUpdateBlueprintOnLoad(UBlueprint* BP) override;
-	virtual void CreateFunctionList() override {}
-	virtual void ProcessOneFunctionGraph(UEdGraph* SourceGraph, bool bInternalFunction = false) override {}
-
-private:
-
-	// used to fail a compilation and mark the blueprint in error
-	void MarkCompilationFailed(const FString& Message);
-
-private:
-	/** the new class we are generating */
-	URigVMBlueprintGeneratedClass* NewControlRigBlueprintGeneratedClass;
 };
