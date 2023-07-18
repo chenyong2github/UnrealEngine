1b0f9c0253199dddd79126432eb67bd1d664e744
Nick Pace <Nick.Pace@xa.epicgames.com>
2023-07-18 19:55:31 -0400

    Merging CL24768117 from Dev-TensorWorks:
    Updating the backbuffer input to also handle the case of PIE sessions.
    
    #rb luke.bermingham, william.belcher
    #jira UE-174063
    
    [CL 26452870 by Nick Pace in 5.3 branch]

--- Engine/Plugins/Media/PixelStreaming/Source/PixelStreamingBlueprint/Private/PixelStreamingStreamerVideoInputBackBuffer.cpp
+++ Engine/Plugins/Media/PixelStreaming/Source/PixelStreamingBlueprint/Private/PixelStreamingStreamerVideoInputBackBuffer.cpp
@@ -1,10 +1,21 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "PixelStreamingStreamerVideoInputBackBuffer.h"
+#include "PixelStreamingVideoInputPIEViewport.h"
 #include "PixelStreamingVideoInputBackBuffer.h"
 
 UPixelStreamingStreamerVideoInputBackBuffer::UPixelStreamingStreamerVideoInputBackBuffer(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	VideoInput = FPixelStreamingVideoInputBackBuffer::Create();
+	// detect if we're in PIE mode or not
+	bool IsGame = false;
+	FParse::Bool(FCommandLine::Get(), TEXT("game"), IsGame);
+	if (GIsEditor && !IsGame)
+	{
+		VideoInput = FPixelStreamingVideoInputPIEViewport::Create();
+	}
+	else
+	{
+		VideoInput = FPixelStreamingVideoInputBackBuffer::Create();
+	}
 }
