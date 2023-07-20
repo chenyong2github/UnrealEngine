e3ca7e79d155adaa0c9128b07be1f2cbab08561f
robert seiver <robert.seiver@epicgames.com>
2023-07-20 11:33:25 -0400

    Fix GPUDumpViewer so it will still work on Linux when it exists within a directory with quotes somewhere along the path
    #rb trivial
    #jira UE-190312
    
    [CL 26487103 by robert seiver in 5.3 branch]

--- Engine/Extras/GPUDumpViewer/OpenGPUDumpViewer.sh
+++ Engine/Extras/GPUDumpViewer/OpenGPUDumpViewer.sh
@@ -12,7 +12,7 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-pushd $SCRIPTPATH
+pushd "$SCRIPTPATH"
 
 
 GetAllChildProcesses() {
