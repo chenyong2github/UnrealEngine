a1e6d4e39c187a530f042e45b74d18c00ef87d36
eranfu <1119369173@qq.com>
2023-07-26 10:12:38 -0400

    PR #10512: Fix the module name of PhysicsControlEditorModule
    #rb trivial
    
    [CL 26605738 by eranfu in 5.3 branch]

--- Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControlEditor/Private/PhysicsControlEditorModule.cpp
+++ Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControlEditor/Private/PhysicsControlEditorModule.cpp
@@ -63,6 +63,6 @@ void FPhysicsControlEditorModule::ShutdownModule()
 
 #undef LOCTEXT_NAMESPACE
 	
-IMPLEMENT_MODULE(FPhysicsControlEditorModule, PhysicsControl)
+IMPLEMENT_MODULE(FPhysicsControlEditorModule, PhysicsControlEditor)
 
 
