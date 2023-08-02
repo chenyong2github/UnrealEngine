15477a910e05f2f368d895d36d7599bfd8c2b586
Dmitriy Dyomin <Dmitriy.Dyomin@epicgames.com>
2023-08-02 10:04:30 -0400

    Fixed: Compile UnrealEditor Win64, VS2019 Latest Toolchain - TextureFormatETC2.dll Unresolved externals #jira UE-191802 #rb none
    [CL 26770580 by Dmitriy Dyomin in 5.3 branch]

--- Engine/Source/ThirdParty/astcenc/4.2.0/BuildForUE/BuildForWindows.bat
+++ Engine/Source/ThirdParty/astcenc/4.2.0/BuildForUE/BuildForWindows.bat
@@ -20,7 +20,7 @@ echo Generating solution (64-bit)...
 if exist "%UE_BUILD_PATH%" (rmdir "%UE_BUILD_PATH%" /s/q)
 mkdir "%UE_BUILD_PATH%"
 cd "%UE_BUILD_PATH%"
-"%_vsinstall%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 17 2022" -A x64 -DISA_SSE41=ON -DCLI=OFF "%PATH_TO_CMAKE_FILE%"
+"%_vsinstall%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A x64 -DISA_SSE41=ON -DCLI=OFF "%PATH_TO_CMAKE_FILE%"
 
 echo Building astcenc solution for (64-bit, Release)...
 "%_msbuild%msbuild.exe" Source/astcenc-sse4.1-static.vcxproj /t:build /p:Configuration=Release
--- Engine/Source/ThirdParty/etc2comp/BuildForUE/BuildForWindows.bat
+++ Engine/Source/ThirdParty/etc2comp/BuildForUE/BuildForWindows.bat
@@ -20,7 +20,7 @@ echo Generating solution (64-bit)...
 if exist "%UE_BUILD_PATH%" (rmdir "%UE_BUILD_PATH%" /s/q)
 mkdir "%UE_BUILD_PATH%"
 cd "%UE_BUILD_PATH%"
-"%_vsinstall%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 17 2022" -A x64 "%PATH_TO_CMAKE_FILE%"
+"%_vsinstall%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A x64 "%PATH_TO_CMAKE_FILE%"
 
 echo Building EtcLib solution for (64-bit, Release)...
 "%_msbuild%msbuild.exe" EtcLib/EtcLib.vcxproj /t:build /p:Configuration=Release
