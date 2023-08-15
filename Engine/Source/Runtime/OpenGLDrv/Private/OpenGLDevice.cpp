ce4510a15f6a4f5b67bfbbbfe844b210ed584f95
Wei Liu <Wei.Liu@epicgames.com>
2023-08-15 03:09:49 -0400

    Disable Niagara GPU particles on Adreno devcies with an old driver to fix a GPU crash. #jira UE-189286 #rb Dmitriy.Dyomin, Chris.Babcock
    [CL 27097883 by Wei Liu in 5.3 branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
@@ -1323,6 +1323,31 @@ FOpenGLDynamicRHI::FOpenGLDynamicRHI()
 			}
 		}
 	}
+
+#if PLATFORM_ANDROID
+	// Temp disable gpu particles for OpenGL because of issues on Adreno devices with old driver version
+	if (GRHIVendorId == 0x5143)
+	{
+		auto* CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("fx.NiagaraAllowGPUParticles"));
+		if (CVar)
+		{
+			int32 DriverVersion = INT32_MAX;
+			FString SubVersionString, DriverVersionString;
+			FString VersionString = FString(ANSI_TO_TCHAR((const ANSICHAR*)glGetString(GL_VERSION)));
+			
+			if (VersionString.Split(TEXT("V@"), nullptr, &SubVersionString) && SubVersionString.Split(TEXT(" "), &DriverVersionString, nullptr))
+			{
+				DriverVersion = FCString::Atoi(*DriverVersionString);
+			}
+
+			if (DriverVersion < 415)
+			{
+				CVar->Set(false);
+				UE_LOG(LogRHI, Log, TEXT("GPU particles are disabled on this device because the driver version %d is less than 415"), DriverVersion);
+			}
+		}
+	}
+#endif
 	
 	// Disable SingleRHIThreadStall for GL occlusion queiresn, which should be set for D3D11 only. Enabling it causes RT->RHIT deadlock
 	{
